f165cc5c6614f0e979b8ea81bcd66b7b9d69e4a8
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2017-12-17 13:10:17 -0600

    Pass RDX arguments

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -100,7 +100,7 @@ def create_ui_commands(pcm_speed, hud, civic, accord, crv, idx):
     commands.append(make_can_msg(0x39f, msg_0x39f, idx, 0))
   return commands
 
-def create_radar_commands(v_ego, civic, accord, crv, idx):
+def create_radar_commands(v_ego, civic, accord, crv, rdx, idx):
   """Creates an iterable of CAN messages for the radar system."""
   commands = []
   v_ego_kph = np.clip(int(round(v_ego * CV.MS_TO_KPH)), 0, 255)
@@ -123,8 +123,8 @@ def create_radar_commands(v_ego, civic, accord, crv, idx):
     msg_0x301 = "\x00\x00\x50\x02\x51\x00\x00"
     commands.append(make_can_msg(0x300, msg_0x300, idx, 1))
   elif rdx:
-    msg_0x301 = " \x0f\x57\x4f\x02\x5a\x00\x00"
-    commands.append(make_can_msg(0x300, msg_0x300, idx, 1))  
+    msg_0x301 = "\x0f\x57\x4f\x02\x5a\x00\x00"
+    commands.append(make_can_msg(0x300, msg_0x300, idx, 1))
   else:
     msg_0x301 = "\x0f\x18\x51\x02\x5a\x00\x00"
     commands.append(make_can_msg(0x300, msg_0x300, idx, 1))
