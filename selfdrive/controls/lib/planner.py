327850eb11c98f169ed91b1ace05223897d7c25f
Ryan Huber <rhuber@gmail.com>
2018-02-19 16:30:36 +0000

    follow more closer test

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -36,7 +36,7 @@ _A_CRUISE_MIN_BP = [   0., 5.,  10., 20.,  40.]
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
 _A_CRUISE_MAX_V = [1., 1., .8, .5, .3]
-_A_CRUISE_MAX_V_FOLLOWING = [1.5, 1.5, 1.2, .7, .3]
+_A_CRUISE_MAX_V_FOLLOWING = [1.5, 1.5, 1.4, 1.2, 1.0]
 _A_CRUISE_MAX_BP = [0.,  5., 10., 20., 40.]
 
 # Lookup table for turns
