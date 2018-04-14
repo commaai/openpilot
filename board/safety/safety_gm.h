4afe6cfa507dfbe550f13c9255b89e43138076f5
Vehicle Researcher <user@comma.ai>
2018-04-14 06:06:42 +0000

    Squashed 'panda/' changes from 3125232..2253dd3
    2253dd3 fix volt ign detect
    3b299d7 add ignition and refactor
    af9af6d Merge pull request #110 from Jamezz/volt
    13e850e more correct
    f295063 add new define to tests
    fec9758 gate that with debug
    5516ebf one more ifdef
    cac7b31 only panda has float
    938d474 fpu enable
    ffbf0c7 cleaner
    de30f27 Revert "need f to not be double"
    4142acf need f to not be double
    3eb15c8 refactor to share code
    a4c8b64 change to O2 to fix make recover
    711fd11 Enable compiler optimizations, fix things it breaks
    2e6f774 block IPAS in main toyota safety mode
    e7a2b3a add ipas tests
    894572c fix tests
    367c9ad add safety toyota ipas
    95919b9 Bounty: panda high quality CAN autobaud (#96)
    6557cd2 Toyota Safety: allow controls only on rising edge of cruise_engaged
    02c1ddf Revert "added steer override check when IPAS is in control (#106)"
    9f925ba Fix the merge mess
    23d3833 Merge from comma upstream
    a0cc51a Undo safety mode override
    ea1c1dc make wlan interface name generic
    6dbd8c9 Implement WebUSB and upgrade WinUSB to 2.0 (#107)
    4fc83a5 Add safety hook for ignition and have GM use gear selector to determine ignition
    52b2ac0 switch from travis to circleci
    48e2374 build panda esp image
    065572a circleci build stm image
    7a1f319 add panda python package test and fix safety test
    021dde7 move saftey test helper files into safety folder
    ce0545f add ci files
    6a3307c no LIN over ELM
    7d21acb added steer override check when IPAS is in control (#106)
    1c88caf Safety code testing (#104)
    f4efd1f Merge pull request #101 from adhintz/master
    c02618b Merge pull request #102 from quillford/master
    1ba5f8a added link to wiki for user scripts
    de2b19e add support for multiple buses to can_unique and can_bittransition output data in sorted order.
    
    git-subtree-dir: panda
    git-subtree-split: 2253dd3c48e21abb82fe161d6f58237490111206
    
    old-commit-hash: e6e6ad2e1fd1c0bed381a4cbabf8d8afd9f64716

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -16,8 +16,10 @@ int gm_speed = 0;
 // silence everything if stock ECUs are still online
 int gm_ascm_detected = 0;
 
-static void gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
+int gm_ignition_started = 0;
 
+static void gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
+  int bus_number = (to_push->RDTR >> 4) & 0xFF;
   uint32_t addr;
   if (to_push->RIR & 4) {
     // Extended
@@ -29,6 +31,12 @@ static void gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     addr = to_push->RIR >> 21;
   }
 
+  if (addr == 0x135 && bus_number == 0) {
+    //Gear selector (used for determining ignition)
+    int gear = to_push->RDLR & 0x7;
+    gm_ignition_started = gear > 0; //Park = 0. If out of park, we're "on."
+  }
+
   // sample speed, really only care if car is moving or not
   // rear left wheel speed
   if (addr == 842) {
@@ -36,7 +44,6 @@ static void gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   }
 
   // check if stock ASCM ECU is still online
-  int bus_number = (to_push->RDTR >> 4) & 0xFF;
   if (bus_number == 0 && addr == 715) {
     gm_ascm_detected = 1;
     controls_allowed = 0;
@@ -170,6 +177,11 @@ static int gm_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 
 static void gm_init(int16_t param) {
   controls_allowed = 0;
+  gm_ignition_started = 0;
+}
+
+static int gm_ign_hook() {
+  return gm_ignition_started;
 }
 
 static int gm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
@@ -181,6 +193,7 @@ const safety_hooks gm_hooks = {
   .rx = gm_rx_hook,
   .tx = gm_tx_hook,
   .tx_lin = gm_tx_lin_hook,
+  .ignition = gm_ign_hook,
   .fwd = gm_fwd_hook,
 };
 
---
+++ tests/safety/test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+python -m unittest discover .
