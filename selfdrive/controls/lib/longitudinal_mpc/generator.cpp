14004ef0528409d069d031526d77eb34c19e1467
Ryan Huber <rhuber@gmail.com>
2018-05-23 08:48:27 +0000

    closer follow

--- selfdrive/controls/lib/longitudinal_mpc/generator.cpp
+++ selfdrive/controls/lib/longitudinal_mpc/generator.cpp
@@ -5,7 +5,8 @@ const int controlHorizon = 50;
 using namespace std;
 
 #define G 9.81
-#define TR 1.8
+#define TR .9
+//#define TR 1.8
 
 #define RW(v_ego, v_l) (v_ego * TR - (v_l - v_ego) * TR + v_ego*v_ego/(2*G) - v_l*v_l / (2*G))
 #define NORM_RW_ERROR(v_ego, v_l, p) ((RW(v_ego, v_l) + 4.0 - p)/(sqrt(v_ego + 0.5) + 0.1))
