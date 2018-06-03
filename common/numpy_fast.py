1c8ffff9f2e81a4533c8faf16d2e414c300bcd86
dekerr <dkerr2@mix.wvu.edu>
2018-06-03 10:51:22 -0400

    Update numpy_fast.py
    Refactor

--- common/numpy_fast.py
+++ common/numpy_fast.py
@@ -1,29 +1,19 @@
 def int_rnd(x):
   return int(round(x))
 
-
 def clip(x, lo, hi):
   return max(lo, min(hi, x))
 
-
 def interp(x, xp, fp):
+  return [_get_interp(v, xp, fp) for v in x] if hasattr(
+    x, '__iter__') else _get_interp(x, xp, fp)
+  
+def _get_interp(xv, xp, fp):
   N = len(xp)
-  if not hasattr(x, '__iter__'):
-    hi = 0
-    while hi < N and x > xp[hi]:
-      hi += 1
-    low = hi - 1
-    return fp[-1] if hi == N and x > xp[low] else (
-      fp[0] if hi == 0 else
-      (x - xp[low]) * (fp[hi] - fp[low]) / (xp[hi] - xp[low]) + fp[low])
-
-  result = []
-  for v in x:
-    hi = 0
-    while hi < N and v > xp[hi]:
-      hi += 1
-    low = hi - 1
-    result.append(fp[-1] if hi == N and v > xp[low] else (fp[
-      0] if hi == 0 else (v - xp[low]) * (fp[hi] - fp[low]) / (xp[hi] - xp[low]
-                                                               ) + fp[low]))
-  return result
+  hi = 0
+  while hi < N and xv > xp[hi]:
+    hi += 1
+  low = hi - 1
+  return fp[-1] if hi == N and xv > xp[low] else (
+    fp[0] if hi == 0 else 
+	  (xv - xp[low]) * (fp[hi] - fp[low]) / (xp[hi] - xp[low]) + fp[low])
