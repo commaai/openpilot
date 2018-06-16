0c61ea733247fb8dacd5cc264d196e17d2df2a88
jeankalud <thibaultjc@gmail.com>
2018-06-16 16:22:47 -0400

    Still works on cruise off

--- selfdrive/car/tesla/interface.py
+++ selfdrive/car/tesla/interface.py
@@ -223,7 +223,7 @@ class CarInterface(object):
     ret.steeringPressed = self.CS.steer_override
 
     # cruise state
-    ret.cruiseState.enabled = self.CS.pcm_acc_status != 0
+    ret.cruiseState.enabled = True #self.CS.pcm_acc_status != 0
     ret.cruiseState.speed = self.CS.v_cruise_pcm * CV.KPH_TO_MS
     ret.cruiseState.available = bool(self.CS.main_on)
     ret.cruiseState.speedOffset = self.CS.cruise_speed_offset
