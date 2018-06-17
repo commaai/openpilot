50c80291933eb486235b6eec039d91a2c7462af1
George Hotz <george@comma.ai>
2018-06-17 11:32:59 -0400

    Toyota C-HR Changes

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -46,7 +46,7 @@ class CarInterface(object):
   def get_params(candidate, fingerprint):
 
     # kg of standard extra cargo to count for drive, gas, etc...
-    std_cargo = 136
+    std_cargo = 75
 
     ret = car.CarParams.new_message()
 
@@ -110,6 +110,14 @@ class CarInterface(object):
       ret.steerKpV, ret.steerKiV = [[0.6], [0.1]]
       ret.steerKf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
       ret.steerRateCost = .8
+    elif candidate == CAR.CHR:
+      ret.safetyParam = 100
+      ret.wheelbase = 2.63906
+      ret.steerRatio = 13.6
+      ret.mass = 3300./2.205 + std_cargo
+      ret.steerKpV, ret.steerKiV = [[0.723], [0.0428]]
+      ret.steerKf = 0.00006
+      ret.steerRateCost = 1.
 
     ret.centerToFront = ret.wheelbase * 0.44
 
@@ -118,7 +126,7 @@ class CarInterface(object):
 
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter.
-    if candidate in [CAR.PRIUS, CAR.RAV4H, CAR.LEXUS_RXH]: # rav4 hybrid can do stop and go
+    if candidate in [CAR.PRIUS, CAR.RAV4H, CAR.LEXUS_RXH, CAR.CHR]: # rav4 hybrid can do stop and go
       ret.minEnableSpeed = -1.
     elif candidate in [CAR.RAV4, CAR.COROLLA]: # TODO: hack ICE to do stop and go
       ret.minEnableSpeed = 19. * CV.MPH_TO_MS
