43133a6ff03e195b990b480615124e92e6dfa601
arne182 <arneschwarck@gmail.com>
2018-06-18 12:49:30 +0200

    Add Blindspot side value

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -236,6 +236,7 @@ class CarInterface(object):
     ret.leftBlinker = bool(self.CS.left_blinker_on)
     ret.rightBlinker = bool(self.CS.right_blinker_on)
     ret.blindspot = self.CS.blind_spot_on
+    ret.blindspotside = self.CS.blind_spot_side
     ret.doorOpen = not self.CS.door_all_closed
     ret.seatbeltUnlatched = not self.CS.seatbelt
 
