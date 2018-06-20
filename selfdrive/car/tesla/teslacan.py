38cc5f79f86f59e339a3f6ae432e6f6853bb140d
jeankalud <thibaultjc@gmail.com>
2018-06-20 14:44:55 -0400

    Better teslacan, auto crc, thanks to @BogGyver

--- selfdrive/car/tesla/teslacan.py
+++ selfdrive/car/tesla/teslacan.py
@@ -1,27 +1,37 @@
 import struct
+from ctypes import create_string_buffer
 
-import common.numpy_fast as np
-from selfdrive.config import Conversions as CV
+
+def add_tesla_checksum(msg_id,msg):
+ """Calculates the checksum for the data part of the Tesla message"""
+ checksum = ((msg_id) & 0xFF) + ((msg_id >> 8) & 0xFF)
+ for i in range(0,len(msg),1):
+  checksum = (checksum + ord(msg[i])) & 0xFF
+ return checksum
 
 
 def create_steering_control(enabled, apply_steer, idx):
-  """Creates a CAN message for the Tesla DBC DAS_steeringControl."""
-  if enabled == False:
-    steering_type = 0
-  else:
-    steering_type = 1
-  type_counter = steering_type << 6
-  type_counter += idx
-  checksum = ((apply_steer & 0xFF) + ((apply_steer >> 8) & 0xFF) + type_counter + 0x8C) & 0xFF  
-  msg = struct.pack("!hBB", apply_steer, type_counter, checksum)
-  
-  return [0x488, 0, msg, 2]
+ """Creates a CAN message for the Tesla DBC DAS_steeringControl."""
+ msg_id = 0x488
+ msg_len = 4
+ msg = create_string_buffer(msg_len)
+ if enabled == False:
+  steering_type = 0
+ else:
+  steering_type = 1
+ type_counter = steering_type << 6
+ type_counter += idx
+ struct.pack_into('!hB', msg, 0,  apply_steer, type_counter)
+ struct.pack_into('B', msg, msg_len-1, add_tesla_checksum(msg_id,msg))
+ return [msg_id, 0, msg.raw, 2]
 
 
 def create_epb_enable_signal(idx):
   """Creates a CAN message to simulate EPB enable message"""
-  checksum = (0x1 + idx + 0x16) & 0xFF
-  msg = struct.pack("!BBB", 1, idx, checksum)
-
-  return [0x214, 0, msg, 2]
+  msg_id = 0x214
+  msg_len = 3
+  msg = create_string_buffer(msg_len)
+  struct.pack_into('BB', msg, 0,  1, idx)
+  struct.pack_into('B', msg, msg_len-1, add_tesla_checksum(msg_id,msg))
+  return [msg_id, 0, msg.raw, 2]
   
