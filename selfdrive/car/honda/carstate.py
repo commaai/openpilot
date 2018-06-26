2471a1eb7d9bbe6640b5c2e432e30e6e1d9a4e92
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-06-26 10:50:25 -0400

    - Cleanup steer status comments - Adds steering warning for interceptor cars in low speed lockout.

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -219,11 +219,20 @@ class CarState(object):
                                       cp.vl["DOORS_STATUS"]['DOOR_OPEN_RL'], cp.vl["DOORS_STATUS"]['DOOR_OPEN_RR']])
     self.seatbelt = not cp.vl["SEATBELT_STATUS"]['SEATBELT_DRIVER_LAMP'] and cp.vl["SEATBELT_STATUS"]['SEATBELT_DRIVER_LATCHED']
 
-    # 2 = temporary 3= TBD 4 = temporary, hit a bump 5 (permanent) 6 = temporary 7 (permanent)
+    # STEER STATUS
+    # "(2) Temporary", "(3) Temporary, Low Speed Lockout", "(4) Temporary, Vehicle Stability", "(5) Permanent", "(6) Temporary", "(7) Permanent"
     # TODO: Use values from DBC to parse this field
     self.steer_error = cp.vl["STEER_STATUS"]['STEER_STATUS'] not in [0, 2, 3, 4, 6]
     self.steer_not_allowed = cp.vl["STEER_STATUS"]['STEER_STATUS'] != 0
-    self.steer_warning = cp.vl["STEER_STATUS"]['STEER_STATUS'] not in [0, 3]   # 3 is low speed lockout, not worth a warning
+    self.steer_warning = cp.vl["STEER_STATUS"]['STEER_STATUS'] not in [0, 3]  # 3 is low speed lockout, not worth a warning
+    
+    # Adds Steering Warning for Low Speed Lockout on Interceptor-Enabled Vehicles
+    #
+    # For safety, it's best we warn the driver that the steering is 
+    # disabled on cars requiring interceptor for stop and go.
+    if self.CP.enableGasInterceptor and self.CP.carFingerprint in (CAR.ACURA_RDX, CAR.ODYSSEY, CAR.PILOT, CAR.RIDGELINE):
+      self.steer_warning = cp.vl["STEER_STATUS"]['STEER_STATUS'] != 0
+
     self.brake_error = cp.vl["STANDSTILL"]['BRAKE_ERROR_1'] or cp.vl["STANDSTILL"]['BRAKE_ERROR_2']
     self.esp_disabled = cp.vl["VSA_STATUS"]['ESP_DISABLED']
 
@@ -276,6 +285,7 @@ class CarState(object):
     self.gear_shifter = parse_gear_shifter(can_gear_shifter, self.CP.carFingerprint)
 
     self.pedal_gas = cp.vl["POWERTRAIN_DATA"]['PEDAL_GAS']
+
     # crv doesn't include cruise control
     if self.CP.carFingerprint in (CAR.CRV, CAR.ODYSSEY, CAR.ACURA_RDX, CAR.RIDGELINE):
       self.car_gas = self.pedal_gas
