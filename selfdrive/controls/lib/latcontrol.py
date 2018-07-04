20cd575b668a60d19bb79c609fd4a2a3b5ce6d2c
arne182 <arneschwarck@gmail.com>
2018-07-04 18:35:52 +0200

    Lane Change angle upped to 15°

--- selfdrive/controls/lib/latcontrol.py
+++ selfdrive/controls/lib/latcontrol.py
@@ -88,10 +88,10 @@ class LatControl(object):
       self.angle_steers_des_mpc = float(math.degrees(delta_desired * VM.CP.steerRatio) + angle_offset)
       if leftBlinker:
         if self.blindspot_blink_counter_left_check > 150:
-          self.angle_steers_des_mpc += 10
+          self.angle_steers_des_mpc += 15
       if rightBlinker:
         if self.blindspot_blink_counter_right_check > 150:
-          self.angle_steers_des_mpc -= 10
+          self.angle_steers_des_mpc -= 15
       self.angle_steers_des_time = cur_time
       self.mpc_updated = True
 
@@ -128,7 +128,7 @@ class LatControl(object):
           print "debug: blindspot detected"
         self.blindspot_blink_counter_right_check += 1
         if self.blindspot_blink_counter_right_check > 150:
-          self.angle_steers_des -= 10
+          self.angle_steers_des -= 15
       else:
         self.blindspot_blink_counter_right_check = 0
       
@@ -138,7 +138,7 @@ class LatControl(object):
           print "debug: blindspot detected"
         self.blindspot_blink_counter_left_check += 1
         if self.blindspot_blink_counter_left_check > 150:
-          self.angle_steers_des += 10
+          self.angle_steers_des += 15
       else:
         self.blindspot_blink_counter_left_check = 0
     self.sat_flag = self.pid.saturated
