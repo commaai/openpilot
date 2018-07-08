d9d692b91a6afa79ce68b6d15ee17514dd649866
Steve Radich <stever@bitshop.com>
2018-07-08 13:22:29 -0400

    Temp fix on warn for steer error

--- selfdrive/car/tesla/interface.py
+++ selfdrive/car/tesla/interface.py
@@ -326,7 +326,8 @@ class CarInterface(object):
     if (ret.gasPressed and not self.gas_pressed_prev) or \
        (ret.brakePressed and (not self.brake_pressed_prev or ret.vEgo > 0.001)):
       #events.append(create_event('pedalPressed', [ET.NO_ENTRY, ET.USER_DISABLE]))
-      events.append(create_event('steerTempUnavailable', [ET.NO_ENTRY, ET.IMMEDIATE_DISABLE]))
+      #Note: This event is thrown for steering override (needs more refactoring)
+      events.append(create_event('steerTempUnavailable', [ET.NO_ENTRY, ET.WARNING]))
 
     if ret.gasPressed:
       events.append(create_event('pedalPressed', [ET.PRE_ENABLE]))
