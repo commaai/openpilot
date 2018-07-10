6e2d759d8c32711f742caa574e257d0b2df70645
Steve Radich <stever@bitshop.com>
2018-07-10 17:58:03 -0400

    Correct logic, decrease handoff to 5 degrees

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -148,15 +148,15 @@ class CarController(object):
       self.human_steered_frame = frame
       enable_steer_control = False
     else:
-      if (self.human_steered_frame - frame < 500): # Need more human testing of handoff timing
+      if (frame - self.human_steered_frame < 50): # Need more human testing of handoff timing
         # Find steering difference between visiond model and human (no need to do every frame if we run out of CPU):
         steer_current=STEER_MAX-(CS.angle_steers*10)  # Formula to convert current steering angle to match apply_steer calculated number
         angle = abs(apply_steer-steer_current)
 
-        # If OP steering > 7 degrees different from human than count that as human still steering..
+        # If OP steering > 5 degrees different from human than count that as human still steering..
         # Tesla rack doesn't report accurate enough, i.e. lane switch we show no human steering when they
         # still are crossing road at an angle clearly they don't want OP to take over
-        if (angle > 70):
+        if (angle > 50):
           self.human_steered_frame = frame
           enable_steer_control = False
 
