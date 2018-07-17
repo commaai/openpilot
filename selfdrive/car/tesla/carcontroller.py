bba4f5d2277fa8e372f492e48d0d4e510b7cf3fd
Jean-Claude Thibault <jeankalud@users.noreply.github.com>
2018-07-17 12:13:57 -0400

    New angle limiter and reverted to signal disable for lane change

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -117,30 +117,21 @@ class CarController(object):
     # *** compute control surfaces ***
     STEER_MAX = 0x4000 #16384
 
-    # Prototype Angle Max. slope of 180 degree at 8.3 m/s (30 km/h) and 25 degree at 33.3 m/s (120 KM/h)
-    # = -62x + 2314.6
-    #   Angle max = -62*V(m/s) + 2314.6
-    # Gives for example: 
-    #                    180 degree at  30 km/h
-    #                    145 degree at  50 km/h
-    #                     94 degree at  80 km/h
-    #                     59 degree at 100 km/h
-    #                     42 degree at 110 km/h
-    #                     25 degree at 120 km/h
-    USER_STEER_MAX = (-62.0 * CS.v_ego) + 2314.6
-
-    # Prevent steering while stopped
-    MIN_STEERING_VEHICLE_VELOCITY = 0.05 # m/s
-    vehicle_moving = (CS.v_ego >= MIN_STEERING_VEHICLE_VELOCITY)
+    # Angle Max. slope versus car speed
+    # Graphical view: https://slack-files.com/T02Q83UUV-FBQFZR5PW-7962eb2adb
+    # and https://slack-files.com/T02Q83UUV-FBQ6SPPRP-b110efb723
+    # Model 1: USER_STEER_MAX = (-62.0 * CS.v_ego) + 2314.6
+    # Model 2: USER_STEER_MAX  = 2.43 * CS.v_ego * CS.v_ego - 193.52 * CS.v_ego + 4000
+    # Model 3: USER_STEER_MAX  = 1.485 * CS.v_ego * CS.v_ego - 154.51 * CS.v_ego + 4000
+    USER_STEER_MAX  = 1.485 * CS.v_ego * CS.v_ego - 154.51 * CS.v_ego + 4000
     
-    enable_steer_control = (enabled and not CS.steer_not_allowed) # See below for human override logic
-    humanControl = False
-    if (frame - CS.frame_humanSteered < 50):
-      enable_steer_control = False
-      humanControl = True
+    # Basic highway lane change logic
+    changing_lanes = CS.right_blinker_on or CS.left_blinker_on
     
+    enable_steer_control = (enabled and not changing_lanes)
+        
     # Angle
-    apply_steer = int(clip((-actuators.steerAngle * 10) + STEER_MAX, STEER_MAX - (USER_STEER_MAX*2), STEER_MAX + (USER_STEER_MAX*2))) # steer torque is converted back to CAN reference (positive when steering right)
+    apply_steer = int(clip((-actuators.steerAngle * 10) + STEER_MAX, STEER_MAX - USER_STEER_MAX, STEER_MAX + USER_STEER_MAX)) # steer angle is converted back to CAN reference (positive when steering right)
 
     # Send CAN commands.
     can_sends = []
@@ -149,7 +140,6 @@ class CarController(object):
     if (frame % send_step) == 0:
       idx = (frame/send_step) % 16 
       can_sends.append(teslacan.create_steering_control(enable_steer_control, apply_steer, idx))
-      if (not humanControl):
-        can_sends.append(teslacan.create_epb_enable_signal(idx))
+      can_sends.append(teslacan.create_epb_enable_signal(idx))
 
       sendcan.send(can_list_to_can_capnp(can_sends, msgtype='sendcan').to_bytes())
