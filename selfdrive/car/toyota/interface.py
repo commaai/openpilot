280840f2e4b647371d94ad94de8dc616d3e67496
ErichMoraga <33645296+ErichMoraga@users.noreply.github.com>
2018-07-21 22:20:22 -0500

    Lessened prev. PID increase.  Decreased steerActuatorDelay.
    ret.steerKpV, ret.steerKiV = [[0.45], [0.015]]
    ret.steerKf = 0.000065   # full torque for 10 deg at 80mph means 0.00007818594

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -88,7 +88,7 @@ class CarInterface(object):
       ret.steerRatio = 15.00   # unknown end-to-end spec
       tire_stiffness_factor = 0.6371
       ret.mass = 3370 * CV.LB_TO_KG + std_cargo
-      ret.steerKpV, ret.steerKiV = [[0.5], [0.02]]
+      ret.steerKpV, ret.steerKiV = [[0.45], [0.015]]
       ret.steerKf = 0.00007   # full torque for 10 deg at 80mph means 0.00007818594
       # TODO: Prius seem to have very laggy actuators. Understand if it is lag or hysteresis
       ret.steerActuatorDelay = 0.25
