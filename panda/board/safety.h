c1b54d0fcc6531d32466a6846b480594b65af401
Drew Hintz <adhintz@google.com>
2018-07-26 07:59:03 +0000

    give chrysler safety a new value

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -88,8 +88,7 @@ typedef struct {
 #define SAFETY_HONDA_BOSCH 4
 #define SAFETY_FORD 5
 #define SAFETY_CADILLAC 6
-// TODO(adhintz) Give Chrysler its own safety id and flash panda.
-#define SAFETY_CHRYSLER 6
+#define SAFETY_CHRYSLER 7
 #define SAFETY_TOYOTA_IPAS 0x1335
 #define SAFETY_TOYOTA_NOLIMITS 0x1336
 #define SAFETY_ALLOUTPUT 0x1337
--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -39,8 +39,7 @@
 #define SAFETY_HONDA_BOSCH 4
 #define SAFETY_FORD 5
 #define SAFETY_CADILLAC 6
-// TODO(adhintz) give Chrysler its own safety value and flash Panda.
-#define SAFETY_CHRYSLER 6
+#define SAFETY_CHRYSLER 7
 #define SAFETY_TOYOTA_NOLIMITS 0x1336
 #define SAFETY_ALLOUTPUT 0x1337
 
