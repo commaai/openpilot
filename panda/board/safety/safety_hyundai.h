67b683795814966314fc01e5633291b83801f7ee
Andrew Frahn <nullku7@gmail.com>
2018-08-01 21:23:03 +1000

    Panda Compiled, Programmed, and car still steered.  NO OTHER TESTING DONE.  This may work, it may do nothing!

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -2,12 +2,14 @@
 //
 // Based on GM Safety
 
-const int HYUNDAI_STEER_ZERO = 1024
+const int HYUNDAI_STEER_ZERO = 1024;
 const int HYUNDAI_MAX_STEER = 200; // This may be lifted highter AFTER a valid proof from users deems it necessary
 const int HYUNDAI_MAX_RATE_UP = 5;
 const int HYUNDAI_MAX_RATE_DOWN = 10;
 const int HYUNDAI_DRIVER_TORQUE_ALLOWANCE = 100;
 const int HYUNDAI_DRIVER_TORQUE_FACTOR = 100;
+const int HYUNDAI_MAX_RT_DELTA = 128;
+const int32_t HYUNDAI_RT_INTERVAL = 250000;
 
 int hyundai_rt_torque_last = 0;
 int hyundai_desired_torque_last = 0;
@@ -23,15 +25,22 @@ static void hyundai_init(int16_t param) {
 
 static int hyundai_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 
+  uint32_t addr;
+  if (to_send->RIR & 4) {
+    addr = to_send->RIR >> 3;
+  } else {
+    addr = to_send->RIR >> 21;
+  }
+  
   // LKA STEER: safety check
-  if (addr == 340) {
+  if (addr == 0x340) {
     int rdlr = to_send->RDLR;
     //  Torque Request starts at bit 16 for 11 bits
     int desired_torque = ((rdlr >> 24 & 0x7) << 8) + (rdlr >> 16 & 0xFF);
     uint32_t ts = TIM2->CNT;
     int violation = 0;
 
-    if (current_controls_allowed) {
+    if (controls_allowed) {
 
       // *** global torque limit check ***
       violation |= max_limit_check(desired_torque, HYUNDAI_STEER_ZERO + HYUNDAI_MAX_STEER,
@@ -65,9 +74,17 @@ static int hyundai_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 }
 
 static void hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
+  uint32_t addr;
+  if (to_push->RIR & 4) {
+    addr = to_push->RIR >> 3;
+  } else {
+    addr = to_push->RIR >> 21;
+  }
+  
+  
   // Driver Override - Message 0x394, bits 45 to 46
-  if (addr == 394) {
-    int driver_override = (to_send->RDHR >> 13) & 0x3
+  if (addr == 0x394) {
+    int driver_override = ((to_push->RDHR >> 13) & 0x3);
     if (driver_override > 0) {
       controls_allowed = 0;
     } else {
