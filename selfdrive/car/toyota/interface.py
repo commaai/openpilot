26b72dac426a1716aacee0d42120fa6937e8e2b2
ErichMoraga <33645296+ErichMoraga@users.noreply.github.com>
2018-08-08 11:47:31 -0500

    PRIUS_PRIME to CAR.PRIUS_PRIME

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -146,7 +146,7 @@ class CarInterface(object):
 
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter.
-    if candidate in [CAR.PRIUS, PRIUS_PRIME, CAR.RAV4H, CAR.LEXUS_RXH, CAR.CHR, CAR.CHRH, CAR.CAMRY, CAR.CAMRYH]: # rav4 hybrid can do stop and go
+    if candidate in [CAR.PRIUS, CAR.PRIUS_PRIME, CAR.RAV4H, CAR.LEXUS_RXH, CAR.CHR, CAR.CHRH, CAR.CAMRY, CAR.CAMRYH]: # rav4 hybrid can do stop and go
       ret.minEnableSpeed = -1.
     elif candidate in [CAR.RAV4, CAR.COROLLA]: # TODO: hack ICE to do stop and go
       ret.minEnableSpeed = 19. * CV.MPH_TO_MS
