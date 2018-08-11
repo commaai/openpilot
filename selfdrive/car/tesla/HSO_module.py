d1549b3a6f1c43c6f2de8ec44b60b225ce5b44f2
BogGyver <bogdan.butoi@gmail.com>
2018-08-11 13:54:24 -0400

    fixed HSO_module logic

--- selfdrive/car/tesla/HSO_module.py
+++ selfdrive/car/tesla/HSO_module.py
@@ -15,26 +15,29 @@ class HSOController(object):
 
     def update_stat(self,CS,enabled,actuators,frame):
         human_control = False
-        if (CS.steer_override>0):
+        if (CS.cstm_btns.get_button_status("steer") >0) and enabled:
+          #if steering but not by ALCA
+          if (CS.right_blinker_on or CS.left_blinker_on) and (self.CC.ALCA.laneChange_enabled <= 1):
             self.frame_humanSteered = frame
-            customAlert.custom_alert_message("Manual Steering Enabled",CS,50)
-        else:
-            if (frame - self.frame_humanSteered < 5.): # Need more human testing of handoff timing
-                if (CS.cstm_btns.get_button_status("steer") >0) and enabled:
-                    if (frame - self.frame_humanSteered < 50): # Need more human testing of handoff timing
-                    # Find steering difference between visiond model and human (no need to do every frame if we run out of CPU):
-                        steer_current=(CS.angle_steers)  # Formula to convert current steering angle to match apply_steer calculated number
-                        apply_steer = int(-actuators.steerAngle)
-                        angle = abs(apply_steer-steer_current)
-                        if angle > 5.:
-                            self.frame_humanSteered = frame
-                            customAlert.custom_alert_message("Manual Steering Enabled",CS,50)
+            customAlert.custom_alert_message("Manual Steering Enabled",CS,51)
+          if (CS.steer_override > 0): 
+            self.frame_humanSteered = frame
+            customAlert.custom_alert_message("Manual Steering Enabled",CS,51)
+          else:
+            if (frame - self.frame_humanSteered < 50): # Need more human testing of handoff timing
+              # Find steering difference between visiond model and human (no need to do every frame if we run out of CPU):
+              steer_current=(CS.angle_steers)  # Formula to convert current steering angle to match apply_steer calculated number
+              apply_steer = int(-actuators.steerAngle)
+              angle = abs(apply_steer-steer_current)
+              if angle > 5.:
+                self.frame_humanSteered = frame
+                customAlert.custom_alert_message("Manual Steering Enabled",CS,51)
         if enabled:
             if CS.cstm_btns.get_button_status("steer") > 0:
               if (frame - self.frame_humanSteered < 50):
                 human_control = True
                 CS.cstm_btns.set_button_status("steer",3)
-                customAlert.custom_alert_message("Manual Steering Enabled",CS,50)
+                customAlert.custom_alert_message("Manual Steering Enabled",CS,51)
               else:
                 CS.cstm_btns.set_button_status("steer",2)
         else:
