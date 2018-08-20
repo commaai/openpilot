98900ff6856faedc4ae0de844196cf136abd407f
BogGyver <bogdan.butoi@gmail.com>
2018-08-20 12:12:29 -0400

    mute messages

--- selfdrive/ui/bbui.h
+++ selfdrive/ui/bbui.h
@@ -407,12 +407,9 @@ void bb_draw_buttons( UIState *s) {
 
 void bb_ui_draw_custom_alert( UIState *s) {
     if ((strlen(s->b.custom_message) > 0) && (strlen(s->scene.alert_text1)==0)){
-      if ((bb_get_button_status(s,"msg") == 0) && (s->b.custom_message_status<=3)) {
+      if (!((bb_get_button_status(s,"msg") == 0) && (s->b.custom_message_status<=3))) {
         bb_ui_draw_vision_alert(s, ALERTSIZE_SMALL, s->b.custom_message_status,
                               s->b.custom_message,"");
-      } else {
-        bb_ui_draw_vision_alert(s, ALERTSIZE_SMALL, s->b.custom_message_status,
-                              "","");
       }
     } 
 }
