4c423271d59ddcad6387aeb943903a35b7e840d8
David Abrahams <you@example.com>
2018-08-22 16:41:20 +0000

    Use standard serialization library in UI button module

--- selfdrive/car/modules/UIBT_module.py
+++ selfdrive/car/modules/UIBT_module.py
@@ -2,13 +2,9 @@
 import struct
 from ctypes import create_string_buffer
 import os
+import pickle
 from datetime import datetime
 
-
-buttons_file_rw = "wb"
-buttons_file_r = "rb"
-btn_msg_len = 23
-btn_msg_struct = "6s6s11s" #name=5 char string, label = 5 char string, satus = 1 char, label2 = 10 char string
 
 class UIButton:
     def __init__(self,btn_name,btn_label,btn_status,btn_label2):
@@ -19,46 +15,23 @@ class UIButton:
 
 
 class UIButtons:
-    def write_buttons_labels_to_file(self):
-        fo = open(self.buttons_labels_path, buttons_file_rw)
-        for btn in self.btns:
-            fo.write(struct.pack(btn_msg_struct,btn.btn_name,btn.btn_label,btn.btn_label2))
-        fo.close()
-
-    def read_buttons_labels_from_file(self):
-        fi =  open(self.buttons_labels_path, buttons_file_r)
-        indata = fi.read()
-        fi.close()
-        if len(indata) == btn_msg_len * 6 :
-            #we have all the data
-            self.btns = []
-            for i in range(0, len(indata), btn_msg_len):
-                name,label,label2 = struct.unpack(btn_msg_struct, indata[i:i+btn_msg_len])  
-                self.btns.append(UIButton(name.rstrip("\0"),label.rstrip("\0"),0,label2.rstrip("\0")))
-            #now read the last saved statuses
-        else:
-            #we don't have all the data, ignore
-            print "labels file is bad"
-
-
     def write_buttons_out_file(self):
         if self.hasChanges:
-            fo = open(self.buttons_status_out_path, buttons_file_rw)
-            for btn in self.btns:
-                fo.write(struct.pack("B",btn.btn_status + 48))
-            fo.close()
+            try:
+                with open(self.buttons_status_out_path, "wb") as fo:
+                    pickle.dump(self.btns, fo)
+            except Exception as e:
+                print "Failed to write button file %s" % self.buttons_status_out_path
+                print str(e)
         self.hasChanges = False
 
     def read_buttons_out_file(self):
-        fi =  open(self.buttons_status_out_path, buttons_file_r)
-        indata = fi.read()
-        fi.close()
-        if len(indata) == 6:
-            for i in range(0,len(indata)):
-                self.btns[i].btn_status = ord(indata[i]) - 48
-        else:
-            #something wrong with the file
-            print "status file is bad"
+        try:
+            with open(self.buttons_status_out_path, "rb") as fo:
+                self.btns = pickle.load(fo)
+        except Exception as e:
+            print "Failed to read button file %s" % self.buttons_status_out_path
+            print str(e)
 
     def send_button_info(self):
         if self.isLive:
@@ -71,20 +44,17 @@ class UIButtons:
         self.CS = carstate
         self.car_folder = folder
         self.car_name = car
-        self.buttons_labels_path = "/data/openpilot/selfdrive/car/"+self.car_folder+"/buttons.msg"
-        self.buttons_status_out_path = "/data/openpilot/selfdrive/car/"+self.car_folder+"/buttons.cc.msg"
+        self.buttons_status_out_path = "/data/openpilot/selfdrive/car/"+self.car_folder+"/buttons.pickle"
         self.btns = []
         self.hasChanges = True
         self.last_in_read_time = datetime.min 
-        if os.path.exists(self.buttons_labels_path):
+        if os.path.exists(self.buttons_status_out_path):
             #there is a file, load it
-            self.read_buttons_labels_from_file()
             self.read_buttons_out_file()
         else:
             #there is no file, create it
             self.btns = self.CS.init_ui_buttons()
             self.hasChanges = True
-            self.write_buttons_labels_to_file()
             self.write_buttons_out_file()
         #send events to initiate UI
         self.isLive = True
