ec4d02cba7f4cf6e45242098906da7b04d297cca
Thomas Helms <di.jamezz@gmail.com>
2018-08-23 14:33:31 -0700

    Detab

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -229,8 +229,8 @@ class CarInterface(object):
         be.pressed = False
         but = self.CS.prev_cruise_buttons
       if but == CruiseButtons.RES_ACCEL:
-      	if not (cruiseEnabled and self.CS.standstill):
-          be.type = 'accelCruise' #Suppress resume button if we're resuming from stop so we don't adjust speed.
+        if not (cruiseEnabled and self.CS.standstill):
+          be.type = 'accelCruise' # Suppress resume button if we're resuming from stop so we don't adjust speed.
       elif but == CruiseButtons.DECEL_SET:
         be.type = 'decelCruise'
       elif but == CruiseButtons.CANCEL:
