196b93214a86d13ef325c0eb53f4cd9502efcea7
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2018-08-28 19:03:50 -0400

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -255,7 +255,7 @@ class CarInterface(object):
     events = []
     if not self.CS.can_valid:
       self.can_invalid_count += 1
-      if self.can_invalid_count >= 5:
+      if self.can_invalid_count >= 500:
         events.append(create_event('commIssue', [ET.NO_ENTRY, ET.IMMEDIATE_DISABLE]))
     else:
       self.can_invalid_count = 0
