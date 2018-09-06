f04a228f316ab4e0bd54eb815ba1680e6a824188
David Abrahams <you@example.com>
2018-09-06 19:01:04 +0000

    ACC: syntax fix

--- selfdrive/car/tesla/ACC_module.py
+++ selfdrive/car/tesla/ACC_module.py
@@ -312,7 +312,7 @@ class ACCController(object):
           elif (available_speed > half_press_kph
                 and current_time_ms > self.automated_cruise_action_time
                 and lead_dist > safe_dist_m):
-            lead_is_far = lead_dist > 2 safe_dist_m
+            lead_is_far = lead_dist > 2 * safe_dist_m
             closing = future_rel_speed < -2
             lead_is_pulling_away = future_rel_speed > 5 
             if lead_is_far and not closing or lead_is_pulling_away:
