44517bb1155091cbdc358094c05f6e107eefd531
arne182 <arneschwarck@gmail.com>
2018-09-16 20:39:17 +0200

    Update carstate.py
    Blindspot needs to be true twice.

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -109,6 +109,8 @@ class CarState(object):
     self.speedlimit = 25
     self.approachradius = 100
     self.includeradius = 22
+    self.blind_spot_on = 0
+    self.blind_spot_on_prev = 0
     #BB UIEvents
     self.UE = UIEvents(self)
 
@@ -200,7 +202,10 @@ class CarState(object):
     self.left_blinker_on = cp.vl["STEERING_LEVERS"]['TURN_SIGNALS'] == 1
     self.right_blinker_on = cp.vl["STEERING_LEVERS"]['TURN_SIGNALS'] == 2
     self.blind_spot_side = cp.vl["DEBUG"]['BLINDSPOTSIDE']
-    self.blind_spot_on = bool(cp.vl["DEBUG"]['BLINDSPOT'])
+    self.blind_spot_on_prev = bool(cp.vl["DEBUG"]['BLINDSPOT'])
+    if bool(cp.vl["DEBUG"]['BLINDSPOT']) and self.blind_spot_on_prev:
+      self.blind_spot_on = 1
+    
     
     # we could use the override bit from dbc, but it's triggered at too high torque values
     self.steer_override = abs(cp.vl["STEER_TORQUE_SENSOR"]['STEER_TORQUE_DRIVER']) > 100
