1e357e30fc9ad2f0795fadd4bf4a46ae13531567
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2018-09-17 22:59:52 -0400

    Update carstate.py

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -44,7 +44,7 @@ def get_powertrain_can_parser(CP, canbus):
     signals += [
       ("ACCCmdActive", "ASCMActiveCruiseControlStatus", 0)
     ]
-    
+
   return CANParser(DBC[CP.carFingerprint]['pt'], signals, [], canbus.powertrain)
 
 class CarState(object):
