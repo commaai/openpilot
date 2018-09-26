707813a063375f7363d8a8a5df36b538d1e40b8a
Drew Hintz <adhintz@google.com>
2018-09-26 05:55:20 +0000

    Revert "add sleep at startup to see if that helps"
    This reverts commit 89d15dbd06780851f34ebc9fe7042c0967f99e91.

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -12,8 +12,6 @@ function launch {
      exec "${BASH_SOURCE[0]}"
   fi
 
-  sleep 30s
-
   # no cpu rationing for now
   echo 0-3 > /dev/cpuset/background/cpus
   echo 0-3 > /dev/cpuset/system-background/cpus
