6a91f8c6fb78603733bc1ae9e5e310586bb63124
arne182 <arneschwarck@gmail.com>
2018-10-08 11:15:15 +0200

    correct set_button_status

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -287,7 +287,7 @@ class CarController(object):
       can_sends.append(create_ipas_steer_command(self.packer, 0, 0, True))
     if CS.cstm_btns.get_button_status("tr"):
       distance = 0b01110011 #x73 comma with toggle - toggle is 5th bit from right
-      CS.cstm_btns.set_button_status("tr") = false
+      CS.cstm_btns.set_button_status("tr", false)
     else:
       distance = 0b01100011 #x63 comma with toggle - toggle is 5th bit from right
     # accel cmd comes from DSU, but we can spam can to cancel the system even if we are using lat only control
