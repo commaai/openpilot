db6d126180ab4126335a38411ad7ecc9ddec1ce5
BogGyver <bogdan.butoi@gmail.com>
2018-10-08 18:26:34 -0400

    fix UIBT and force Tesla Model S when fingerprint fails

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -76,7 +76,8 @@ def fingerprint(logcan, timeout):
 
     # bail if no cars left or we've been waiting too long
     elif len(candidate_cars) == 0 or (timeout and (ts - st_passive) > timeout):
-      return None, finger
+      #return None, finger
+      return "TESLA MODEL S", finger
 
     time.sleep(0.01)
 
