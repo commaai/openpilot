520bc6949a684d5017297811db2831a0bb6b0925
arne182 <arneschwarck@gmail.com>
2018-10-10 22:32:38 +0200

    remove merge markers

--- selfdrive/controls/lib/longitudinal_mpc/longitudinal_mpc.c
+++ selfdrive/controls/lib/longitudinal_mpc/longitudinal_mpc.c
@@ -118,7 +118,6 @@ void init_with_simulation(double v_ego, double x_l_0, double v_l_0, double a_l_0
   for (i = 0; i < NYN; ++i)  acadoVariables.yN[ i ] = 0.0;
 }
 
-=======
 int run_mpc(state_t * x0, log_t * solution, double l, double a_l_0, double TR){
   int i;
 
