5641fc986d53c7d0e745c7bdaa4a454770778b0d
Andrew Frahn <emmertex@gmail.com>
2018-10-15 01:30:18 +1100

    Revert to using CLU15 message for Gear Selection for Compatability Reasons (#362)
    * hyundai WIP
    
    * steer_driver_factor is 1
    
    * removed unnecessary file
    
    * removed unnecessary code
    
    * Update carcontroller.py
    
    bug fix
    
    * safety tuning and fixed interface stiffness
    
    * better lateral tuning, some fixes
    
    * Fix set speed
    
    * added camera state reading, autoresume from stop, cancel on accel, hud alerts
    
    * WIP
    
    * Updated for Kia Sorento *WIP*
    
    * Cleanup
    
    * clean2
    
    * Bug Fixes
    
    * pre-merge
    
    * Add all the cars!
    
    * Panda to auto-detect Camera Bus
    
    * Move Checksum Check
    
    * Final Sorento Tuning
    
    * Make CAN3 for Cam default
    
    * Update README.md
    
    * update panda, minor aesthetic updates
    
    * few other minor changes
    
    * added steer not allowed alert
    
    * bup panda version to force panda update
    
    * fixed camera alerts
    
    * Revert to using CLU15 for Gear Selection
    
    * Missing Defines
    
    Didn’t realise this was removed as well!
    
    * Change Gear Selection Definitions
    
    Add logic to use transmission message rather than cluster message for all gear selections UNLESS the car in question does not have the known good transmission messages
    
    * Revert Camera CAN Bus
    
    * self.candidate
    
    * fixed fingerpint, tested on Sorento
    
    * Update for Elantra on Hyundai-Dev Branch
    
    Latest Elantra values that correlate to progress Community branch.

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -44,6 +44,11 @@ def get_can_parser(CP):
     ("CF_Clu_AmpInfo", "CLU11", 0),
     ("CF_Clu_AliveCnt1", "CLU11", 0),
 
+    ("CF_Clu_InhibitD", "CLU15", 0),
+    ("CF_Clu_InhibitP", "CLU15", 0),
+    ("CF_Clu_InhibitN", "CLU15", 0),
+    ("CF_Clu_InhibitR", "CLU15", 0),
+
     ("CF_Lvr_Gear","LVR12",0),
 
     ("ACCEnable", "TCS13", 0),
@@ -204,7 +209,7 @@ class CarState(object):
       self.pedal_gas = cp.vl["EMS12"]['TPS']
     self.car_gas = cp.vl["EMS12"]['TPS']
 
-    # Gear Selecton - This should be compatible with all Kia/Hyundai with Auto's
+    # Gear Selecton - This is not compatible with all Kia/Hyundai's, But is the best way for those it is compatible with
     gear = cp.vl["LVR12"]["CF_Lvr_Gear"]
     if gear == 5:
       self.gear_shifter = "drive"
@@ -216,6 +221,18 @@ class CarState(object):
       self.gear_shifter = "reverse"
     else:
       self.gear_shifter = "unknown"
+
+    # Gear Selection via Cluster - For those Kia/Hyundai which are not fully discovered, we can use the Cluster Indicator for Gear Selection, as this seems to be standard over all cars, but is not the preferred method.
+    if cp.vl["CLU15"]["CF_Clu_InhibitD"] == 1:
+      self.gear_shifter_cluster = "drive"
+    elif cp.vl["CLU15"]["CF_Clu_InhibitN"] == 1:
+      self.gear_shifter_cluster = "neutral"
+    elif cp.vl["CLU15"]["CF_Clu_InhibitP"] == 1:
+      self.gear_shifter_cluster = "park"
+    elif cp.vl["CLU15"]["CF_Clu_InhibitR"] == 1:
+      self.gear_shifter_cluster = "reverse"
+    else:
+      self.gear_shifter_cluster = "unknown"
 
     # save the entire LKAS11 and CLU11
     self.lkas11 = cp_cam.vl["LKAS11"]
