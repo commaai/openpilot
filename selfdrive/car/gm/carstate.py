d8f4cca06fc8380620a14c2017362fcdb40f3a3a
arne182 <arneschwarck@gmail.com>
2018-10-14 21:41:37 +0200

    Update carstate.py

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -81,12 +81,12 @@ class CarState(object):
     #BB init ui buttons
   def init_ui_buttons(self):
     btns = []
-    btns.append(UIButton("","",0,"",0))
+    btns.append(UIButton("lka","LKA",0,"",0))
     btns.append(UIButton("","",0,"",1))
     btns.append(UIButton("","",0,"",2))
     btns.append(UIButton("","",0,"",3))
     btns.append(UIButton("","",0,"",4))
-    btns.append(UIButton("lka","LKA",0,"",5))
+    btns.append(UIButton("","",0,"",5))
     return btns
  
   def update(self, pt_cp):
