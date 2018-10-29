fa554c1d3d53849d3e7971dc99b51189be10334d
pjlao307 <pjlao307@gmail.com>
2018-10-29 11:06:21 -0700

    Turn on calibration bounding box to help track down the OP lines issue

--- selfdrive/ui/ui.c
+++ selfdrive/ui/ui.c
@@ -1515,6 +1515,7 @@ static void ui_draw_vision(UIState *s) {
   nvgTranslate(s->vg, ui_viz_rx+ui_viz_ro, box_y + (box_h-inner_height)/2.0);
   nvgScale(s->vg, (float)viz_w / s->fb_w, (float)inner_height / s->fb_h);
   if (!scene->frontview && !scene->fullview) {
+    ui_draw_transformed_box(s, 0xFF00FF00);
     ui_draw_world(s);
     
   }
