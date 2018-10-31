9c375718c1ebcac4405cafef234284fa3ae47c59
arne182 <arneschwarck@gmail.com>
2018-10-31 14:58:40 +0100

    Allow OP to brake in a curve

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -64,7 +64,7 @@ def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
 
   a_total_max = interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
   a_y = v_ego**2 * angle_steers * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
-  a_x_allowed = math.sqrt(max(a_total_max**2 - a_y**2, 0.))
+  a_x_allowed = a_total_max - a_y
 
   a_target[1] = min(a_target[1], a_x_allowed)
   return a_target
