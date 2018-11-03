b10a2d39e53c6c8a07627043a3ee67b9b977ab17
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2018-11-02 22:43:31 -0400

    Update carstate.py

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -1,196 +1,143 @@
-from common.numpy_fast import interp
-from common.realtime import sec_since_boot
+import numpy as np
+from cereal import car
+from common.kalman.simple_kalman import KF1D
 from selfdrive.config import Conversions as CV
-from selfdrive.boardd.boardd import can_list_to_can_capnp
-from selfdrive.car import apply_std_steer_torque_limits
-from selfdrive.car.gm import gmcan
-from selfdrive.car.gm.values import CAR, DBC
-from selfdrive.can.packer import CANPacker
+from selfdrive.can.parser import CANParser
+from selfdrive.car.gm.values import DBC, CAR, parse_gear_shifter, \
+                                    CruiseButtons, is_eps_status_ok, \
+                                    STEER_THRESHOLD
 
+def get_powertrain_can_parser(CP, canbus):
+  # this function generates lists for signal, messages and initial values
+  signals = [
+    # sig_name, sig_address, default
+    ("BrakePedalPosition", "EBCMBrakePedalPosition", 0),
+    ("FrontLeftDoor", "BCMDoorBeltStatus", 0),
+    ("FrontRightDoor", "BCMDoorBeltStatus", 0),
+    ("RearLeftDoor", "BCMDoorBeltStatus", 0),
+    ("RearRightDoor", "BCMDoorBeltStatus", 0),
+    ("LeftSeatBelt", "BCMDoorBeltStatus", 0),
+    ("RightSeatBelt", "BCMDoorBeltStatus", 0),
+    ("TurnSignals", "BCMTurnSignals", 0),
+    ("AcceleratorPedal", "AcceleratorPedal", 0),
+    ("ACCButtons", "ASCMSteeringButton", CruiseButtons.UNPRESS),
+    ("SteeringWheelAngle", "PSCMSteeringAngle", 0),
+    ("FLWheelSpd", "EBCMWheelSpdFront", 0),
+    ("FRWheelSpd", "EBCMWheelSpdFront", 0),
+    ("RLWheelSpd", "EBCMWheelSpdRear", 0),
+    ("RRWheelSpd", "EBCMWheelSpdRear", 0),
+    ("PRNDL", "ECMPRDNL", 0),
+    ("LKADriverAppldTrq", "PSCMStatus", 0),
+    ("LKATorqueDeliveredStatus", "PSCMStatus", 0),
+  ]
 
-class CarControllerParams():
-  def __init__(self, car_fingerprint):
-    if car_fingerprint == CAR.VOLT:
-      self.STEER_MAX = 300
-      self.STEER_STEP = 2              # how often we update the steer cmd
-      self.STEER_DELTA_UP = 7          # ~0.75s time to peak torque (255/50hz/0.75s)
-      self.STEER_DELTA_DOWN = 17       # ~0.3s from peak torque to zero
-    elif car_fingerprint == CAR.CADILLAC_CT6:
-      self.STEER_MAX = 150
-      self.STEER_STEP = 1              # how often we update the steer cmd
-      self.STEER_DELTA_UP = 2          # 0.75s time to peak torque
-      self.STEER_DELTA_DOWN = 5        # 0.3s from peak torque to zero
+  if CP.carFingerprint == CAR.VOLT:
+    signals += [
+      ("RegenPaddle", "EBCMRegenPaddle", 0),
+      ("TractionControlOn", "ESPStatus", 0),
+      ("EPBClosed", "EPBStatus", 0),
+      ("CruiseMainOn", "ECMEngineStatus", 0),
+      ("CruiseState", "AcceleratorPedal2", 0),
+    ]
+  elif CP.carFingerprint == CAR.CADILLAC_CT6:
+    signals += [
+      ("ACCCmdActive", "ASCMActiveCruiseControlStatus", 0)
+    ]
 
-    self.STEER_DRIVER_ALLOWANCE = 50   # allowed driver torque before start limiting
-    self.STEER_DRIVER_MULTIPLIER = 4   # weight driver torque heavily
-    self.STEER_DRIVER_FACTOR = 100     # from dbc
-    self.NEAR_STOP_BRAKE_PHASE = 0.5 # m/s, more aggressive braking near full stop
+  return CANParser(DBC[CP.carFingerprint]['pt'], signals, [], canbus.powertrain)
 
-    # Takes case of "Service Adaptive Cruise" and "Service Front Camera"
-    # dashboard messages.
-    self.ADAS_KEEPALIVE_STEP = 100
-    self.CAMERA_KEEPALIVE_STEP = 100
+class CarState(object):
+  def __init__(self, CP, canbus):
+    # initialize can parser
 
-    # pedal lookups, only for Volt
-    MAX_GAS = 3072              # Only a safety limit
-    ZERO_GAS = 2048
-    MAX_BRAKE = 350             # Should be around 3.5m/s^2, including regen
-    self.MAX_ACC_REGEN = 1404  # ACC Regen braking is slightly less powerful than max regen paddle
-    self.GAS_LOOKUP_BP = [-0.25, 0., 0.5]
-    self.GAS_LOOKUP_V = [self.MAX_ACC_REGEN, ZERO_GAS, MAX_GAS]
-    self.BRAKE_LOOKUP_BP = [-1., -0.25]
-    self.BRAKE_LOOKUP_V = [MAX_BRAKE, 0]
+    self.car_fingerprint = CP.carFingerprint
+    self.cruise_buttons = CruiseButtons.UNPRESS
+    self.left_blinker_on = False
+    self.prev_left_blinker_on = False
+    self.right_blinker_on = False
+    self.prev_right_blinker_on = False
 
+    # vEgo kalman filter
+    dt = 0.01
+    self.v_ego_kf = KF1D(x0=np.matrix([[0.], [0.]]),
+                         A=np.matrix([[1., dt], [0., 1.]]),
+                         C=np.matrix([1., 0.]),
+                         K=np.matrix([[0.12287673], [0.29666309]]))
+    self.v_ego = 0.
 
-def actuator_hystereses(final_pedal, pedal_steady):
-  # hyst params... TODO: move these to VehicleParams
-  pedal_hyst_gap = 0.01    # don't change pedal command for small oscilalitons within this value
+  def update(self, pt_cp):
 
-  # for small pedal oscillations within pedal_hyst_gap, don't change the pedal command
-  if final_pedal == 0.:
-    pedal_steady = 0.
-  elif final_pedal > pedal_steady + pedal_hyst_gap:
-    pedal_steady = final_pedal - pedal_hyst_gap
-  elif final_pedal < pedal_steady - pedal_hyst_gap:
-    pedal_steady = final_pedal + pedal_hyst_gap
-  final_pedal = pedal_steady
+    self.can_valid = pt_cp.can_valid
+    self.prev_cruise_buttons = self.cruise_buttons
+    self.cruise_buttons = pt_cp.vl["ASCMSteeringButton"]['ACCButtons']
 
-  return final_pedal, pedal_steady
+    self.v_wheel_fl = pt_cp.vl["EBCMWheelSpdFront"]['FLWheelSpd'] * CV.KPH_TO_MS
+    self.v_wheel_fr = pt_cp.vl["EBCMWheelSpdFront"]['FRWheelSpd'] * CV.KPH_TO_MS
+    self.v_wheel_rl = pt_cp.vl["EBCMWheelSpdRear"]['RLWheelSpd'] * CV.KPH_TO_MS
+    self.v_wheel_rr = pt_cp.vl["EBCMWheelSpdRear"]['RRWheelSpd'] * CV.KPH_TO_MS
+    speed_estimate = float(np.mean([self.v_wheel_fl, self.v_wheel_fr, self.v_wheel_rl, self.v_wheel_rr]))
 
+    self.v_ego_raw = speed_estimate
+    v_ego_x = self.v_ego_kf.update(speed_estimate)
+    self.v_ego = float(v_ego_x[0])
+    self.a_ego = float(v_ego_x[1])
 
-class CarController(object):
-  def __init__(self, canbus, car_fingerprint, allow_controls):
-    self.pedal_steady = 0.
-    self.start_time = sec_since_boot()
-    self.chime = 0
-    self.steer_idx = 0
-    self.apply_steer_last = 0
-    self.car_fingerprint = car_fingerprint
-    self.allow_controls = allow_controls
-    self.lka_icon_status_last = (False, False)
+    self.standstill = self.v_ego_raw < 0.01
 
-    # Setup detection helper. Routes commands to
-    # an appropriate CAN bus number.
-    self.canbus = canbus
-    self.params = CarControllerParams(car_fingerprint)
+    self.angle_steers = pt_cp.vl["PSCMSteeringAngle"]['SteeringWheelAngle']
+    self.gear_shifter = parse_gear_shifter(pt_cp.vl["ECMPRDNL"]['PRNDL'])
+    self.user_brake = pt_cp.vl["EBCMBrakePedalPosition"]['BrakePedalPosition']
 
-    self.packer_pt = CANPacker(DBC[car_fingerprint]['pt'])
-    self.packer_ch = CANPacker(DBC[car_fingerprint]['chassis'])
+    self.pedal_gas = pt_cp.vl["AcceleratorPedal"]['AcceleratorPedal']
+    self.user_gas_pressed = self.pedal_gas > 0
 
-  def update(self, sendcan, enabled, CS, frame, actuators, \
-             hud_v_cruise, hud_show_lanes, hud_show_car, chime, chime_cnt):
-    """ Controls thread """
+    self.steer_torque_driver = pt_cp.vl["PSCMStatus"]['LKADriverAppldTrq']
+    self.steer_override = abs(self.steer_torque_driver) > STEER_THRESHOLD
 
-    # Sanity check.
-    if not self.allow_controls:
-      return
+    # 0 - inactive, 1 - active, 2 - temporary limited, 3 - failed
+    self.lkas_status = pt_cp.vl["PSCMStatus"]['LKATorqueDeliveredStatus']
+    self.steer_not_allowed = not is_eps_status_ok(self.lkas_status, self.car_fingerprint)
 
-    P = self.params
+    # 1 - open, 0 - closed
+    self.door_all_closed = (pt_cp.vl["BCMDoorBeltStatus"]['FrontLeftDoor'] == 0 and
+      pt_cp.vl["BCMDoorBeltStatus"]['FrontRightDoor'] == 0 and
+      pt_cp.vl["BCMDoorBeltStatus"]['RearLeftDoor'] == 0 and
+      pt_cp.vl["BCMDoorBeltStatus"]['RearRightDoor'] == 0)
 
-    # Send CAN commands.
-    can_sends = []
-    canbus = self.canbus
+    # 1 - latched
+    self.seatbelt = pt_cp.vl["BCMDoorBeltStatus"]['LeftSeatBelt'] == 1
 
-    ### STEER ###
+    self.steer_error = False
 
-    if (frame % P.STEER_STEP) == 0:
-      lkas_enabled = enabled and not CS.steer_not_allowed and CS.v_ego > 3.
-      if lkas_enabled:
-        apply_steer = actuators.steer * P.STEER_MAX
-        apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.steer_torque_driver, P)
-      else:
-        apply_steer = 0
+    self.brake_error = False
+    self.can_valid = True
 
-      self.apply_steer_last = apply_steer
-      idx = (frame / P.STEER_STEP) % 4
-
-      if self.car_fingerprint == CAR.VOLT:
-        can_sends.append(gmcan.create_steering_control(self.packer_pt,
-          canbus.powertrain, apply_steer, idx, lkas_enabled))
-      if self.car_fingerprint == CAR.CADILLAC_CT6:
-        can_sends += gmcan.create_steering_control_ct6(self.packer_pt,
-          canbus, apply_steer, CS.v_ego, idx, lkas_enabled)
-
-    ### GAS/BRAKE ###
+    self.prev_left_blinker_on = self.left_blinker_on
+    self.prev_right_blinker_on = self.right_blinker_on
+    self.left_blinker_on = pt_cp.vl["BCMTurnSignals"]['TurnSignals'] == 1
+    self.right_blinker_on = pt_cp.vl["BCMTurnSignals"]['TurnSignals'] == 2
 
     if self.car_fingerprint == CAR.VOLT:
-      # no output if not enabled, but keep sending keepalive messages
-      # treat pedals as one
-      final_pedal = actuators.gas - actuators.brake
+      self.park_brake = pt_cp.vl["EPBStatus"]['EPBClosed']
+      self.main_on = pt_cp.vl["ECMEngineStatus"]['CruiseMainOn']
+      self.acc_active = False
+      self.esp_disabled = pt_cp.vl["ESPStatus"]['TractionControlOn'] != 1
+      self.regen_pressed = bool(pt_cp.vl["EBCMRegenPaddle"]['RegenPaddle'])
+      self.pcm_acc_status = pt_cp.vl["AcceleratorPedal2"]['CruiseState']
+    else: 
+      self.park_brake = False
+      self.main_on = False
+      self.acc_active = pt_cp.vl["ASCMActiveCruiseControlStatus"]['ACCCmdActive']
+      self.esp_disabled = False
+      self.regen_pressed = False
+      self.pcm_acc_status = int(self.acc_active)
 
-      # *** apply pedal hysteresis ***
-      final_brake, self.brake_steady = actuator_hystereses(
-        final_pedal, self.pedal_steady)
+    # Brake pedal's potentiometer returns near-zero reading
+    # even when pedal is not pressed.
+    if self.user_brake < 10:
+      self.user_brake = 0
 
-      if not enabled:
-        # Stock ECU sends max regen when not enabled.
-        apply_gas = P.MAX_ACC_REGEN
-        apply_brake = 0
-      else:
-        apply_gas = int(round(interp(final_pedal, P.GAS_LOOKUP_BP, P.GAS_LOOKUP_V)))
-        apply_brake = int(round(interp(final_pedal, P.BRAKE_LOOKUP_BP, P.BRAKE_LOOKUP_V)))
+    # Regen braking is braking
+    self.brake_pressed = self.user_brake > 10 or self.regen_pressed
 
-      # Gas/regen and brakes - all at 25Hz
-      if (frame % 4) == 0:
-        idx = (frame / 4) % 4
-
-        at_full_stop = enabled and CS.standstill
-        near_stop = enabled and (CS.v_ego < P.NEAR_STOP_BRAKE_PHASE)
-        can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, canbus.chassis, apply_brake, idx, near_stop, at_full_stop))
-
-        at_full_stop = enabled and CS.standstill
-        can_sends.append(gmcan.create_gas_regen_command(self.packer_pt, canbus.powertrain, apply_gas, idx, enabled, at_full_stop))
-
-      # Send dashboard UI commands (ACC status), 25hz
-      if (frame % 4) == 0:
-        can_sends.append(gmcan.create_acc_dashboard_command(self.packer_pt, canbus.powertrain, enabled, hud_v_cruise * CV.MS_TO_KPH, hud_show_car))
-
-      # Radar needs to know current speed and yaw rate (50hz),
-      # and that ADAS is alive (10hz)
-      time_and_headlights_step = 10
-      tt = sec_since_boot()
-
-      if frame % time_and_headlights_step == 0:
-        idx = (frame / time_and_headlights_step) % 4
-        can_sends.append(gmcan.create_adas_time_status(canbus.obstacle, int((tt - self.start_time) * 60), idx))
-        can_sends.append(gmcan.create_adas_headlights_status(canbus.obstacle))
-
-      speed_and_accelerometer_step = 2
-      if frame % speed_and_accelerometer_step == 0:
-        idx = (frame / speed_and_accelerometer_step) % 4
-        can_sends.append(gmcan.create_adas_steering_status(canbus.obstacle, idx))
-        can_sends.append(gmcan.create_adas_accelerometer_speed_status(canbus.obstacle, CS.v_ego, idx))
-
-      if frame % P.ADAS_KEEPALIVE_STEP == 0:
-        can_sends += gmcan.create_adas_keepalive(canbus.powertrain)
-
-    # Show green icon when LKA torque is applied, and
-    # alarming orange icon when approaching torque limit.
-    # If not sent again, LKA icon disappears in about 5 seconds.
-    # Conveniently, sending camera message periodically also works as a keepalive.
-    lka_active = CS.lkas_status == 1
-    lka_critical = lka_active and abs(actuators.steer) > 0.9
-    lka_icon_status = (lka_active, lka_critical)
-    if frame % P.CAMERA_KEEPALIVE_STEP == 0 \
-        or lka_icon_status != self.lka_icon_status_last:
-      can_sends.append(gmcan.create_lka_icon_command(canbus.sw_gmlan, lka_active, lka_critical))
-      self.lka_icon_status_last = lka_icon_status
-
-    # Send chimes
-    if self.chime != chime:
-      duration = 0x3c
-
-      # There is no 'repeat forever' chime command
-      # TODO: Manage periodic re-issuing of chime command
-      # and chime cancellation
-      if chime_cnt == -1:
-        chime_cnt = 10
-
-      if chime != 0:
-        can_sends.append(gmcan.create_chime_command(canbus.sw_gmlan, chime, duration, chime_cnt))
-
-      # If canceling a repeated chime, cancel command must be
-      # issued for the same chime type and duration
-      self.chime = chime
-
-    sendcan.send(can_list_to_can_capnp(can_sends, msgtype='sendcan').to_bytes())
+    self.gear_shifter_valid = self.gear_shifter == car.CarState.GearShifter.drive
