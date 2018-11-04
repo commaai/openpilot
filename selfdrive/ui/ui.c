fffebbb5a7f9ff5bced17b4c17992a135f20aac1
Joey Lao <jlao@geneticalliance.org>
2018-11-03 18:41:27 -0700

    Add tuning mod code to stock commaai/devel

--- selfdrive/ui/ui.c
+++ selfdrive/ui/ui.c
@@ -225,6 +225,8 @@ typedef struct UIState {
   float light_sensor;
 } UIState;
 
+#include "tuning.h"
+
 static int last_brightness = -1;
 static void set_brightness(UIState *s, int brightness) {
   if (last_brightness != brightness && (s->awake || brightness == 0)) {
@@ -1768,7 +1770,8 @@ int main() {
 
     // awake on any touch
     int touch_x = -1, touch_y = -1;
-    int touched = touch_poll(&touch, &touch_x, &touch_y, s->awake ? 0 : 100);
+    int key_up = 0;
+    int touched = custom_touch_poll(&touch, &touch_x, &touch_y, s->awake ? 0 : 100, &key_up);
     if (touched == 1) {
       // touch event will still happen :(
       set_awake(s, true);
@@ -1785,6 +1788,7 @@ int main() {
       ui_draw(s);
       glFinish();
       should_swap = true;
+      tuning(s, touch_x, touch_y, key_up);
     }
 
     pthread_mutex_unlock(&s->lock);
