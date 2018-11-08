57154036406dc7685e58c9021876e2e709d65894
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2018-11-08 00:10:02 -0500

    updated delta up and down
    scaling up steer delta now that max torque in 300

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -13,8 +13,8 @@ class CarControllerParams():
     if car_fingerprint == CAR.VOLT:
       self.STEER_MAX = 300
       self.STEER_STEP = 2              # how often we update the steer cmd
-      self.STEER_DELTA_UP = 7          # ~0.75s time to peak torque (255/50hz/0.75s)
-      self.STEER_DELTA_DOWN = 17       # ~0.3s from peak torque to zero
+      self.STEER_DELTA_UP = 8          # ~0.75s time to peak torque (255/50hz/0.75s)
+      self.STEER_DELTA_DOWN = 20       # ~0.3s from peak torque to zero
     elif car_fingerprint == CAR.CADILLAC_CT6:
       self.STEER_MAX = 150
       self.STEER_STEP = 1              # how often we update the steer cmd
