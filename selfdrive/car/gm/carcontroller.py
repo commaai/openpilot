891aba27eaf0dd547f518fa0363850bd74dba3ca
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2018-11-08 00:19:44 -0500

    fixed comment

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -13,7 +13,7 @@ class CarControllerParams():
     if car_fingerprint == CAR.VOLT:
       self.STEER_MAX = 300
       self.STEER_STEP = 2              # how often we update the steer cmd
-      self.STEER_DELTA_UP = 8          # ~0.75s time to peak torque (255/50hz/0.75s)
+      self.STEER_DELTA_UP = 8          # ~0.75s time to peak torque (300/50hz/0.75s)
       self.STEER_DELTA_DOWN = 20       # ~0.3s from peak torque to zero
     elif car_fingerprint == CAR.CADILLAC_CT6:
       self.STEER_MAX = 150
