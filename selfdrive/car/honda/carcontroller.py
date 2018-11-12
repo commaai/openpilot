1251809cbbe71518b31472030d765f294814d8d0
Greg Esmond <gernby2002@yahoo.com>
2018-11-11 18:46:51 -0600

    more cleanup for general use

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -126,7 +126,6 @@ class CarController(object):
     # steer torque is converted back to CAN reference (positive when steering right)
     apply_gas = clip(actuators.gas, 0., 1.)
     apply_brake = int(clip(self.brake_last * BRAKE_MAX, 0, BRAKE_MAX - 1))
-    #apply_steer = int(clip(-actuators.steer * STEER_MAX, -0x7dff, 0x7dff))
     apply_steer = int(clip(-actuators.steer * STEER_MAX, -STEER_MAX, STEER_MAX))
 
     # any other cp.vl[0x18F]['STEER_STATUS'] is common and can happen during user override. sending 0 torque to avoid EPS sending error 5
--- selfdrive/controls/lib/latcontrol.py
+++ selfdrive/controls/lib/latcontrol.py
@@ -1,8 +1,5 @@
-import zmq
 import math
 import numpy as np
-import time
-import json
 from selfdrive.controls.lib.pid import PIController
 from selfdrive.controls.lib.drive_helpers import MPC_COST_LAT
 from selfdrive.controls.lib.lateral_mpc import libmpc_py
@@ -123,7 +120,7 @@ class LatControl(object):
       # constant for 0.05s.
       dt = min(cur_time - self.angle_steers_des_time, _DT_MPC + _DT) + _DT  # no greater than dt mpc + dt, to prevent too high extraps
       self.angle_steers_des = self.angle_steers_des_prev + (dt / _DT_MPC) * (self.angle_steers_des_mpc - self.angle_steers_des_prev)
-      #self.angle_steers_des = (smoothing * self.angle_steers_des + self.angle_steers_des_mpc) / (smoothing + 1.0)
+      #self.angle_steers_des = self.angle_steers_des_mpc
       steers_max = get_steer_max(VM.CP, v_ego)
       self.pid.pos_limit = steers_max
       self.pid.neg_limit = -steers_max
@@ -136,9 +133,8 @@ class LatControl(object):
   
       deadzone = 0.0
 
-      output_steer =  self.pid.update(self.angle_steers_des, angle_steers, check_saturation=False, override=steer_override,
+      output_steer = self.pid.update(self.angle_steers_des, angle_steers, check_saturation=(v_ego > 10), override=steer_override,
                                      feedforward=steer_feedforward, speed=v_ego, deadzone=deadzone)
     
     self.sat_flag = self.pid.saturated
-    self.prev_output_steer = output_steer
     return output_steer, float(self.angle_steers_des)
