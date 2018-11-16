d8d5b39fbb5ce32fb2b0a46e94685bca43e01edd
arne182 <arneschwarck@gmail.com>
2018-11-16 12:50:49 +0100

    Increase camera offset slightly

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -1,7 +1,7 @@
 from common.numpy_fast import interp
 from selfdrive.controls.lib.latcontrol_helpers import model_polyfit, calc_desired_path, compute_path_pinv
 
-CAMERA_OFFSET = 0.06  # m from center car to camera
+CAMERA_OFFSET = 0.09  # m from center car to camera
 
 class PathPlanner(object):
   def __init__(self):
