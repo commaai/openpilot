bf1368b4cfa6a01bcfdb8666fd5705a44d8c42d7
Rick Lan <ricklan@gmail.com>
2018-11-19 12:19:27 +1000

    ignore standstill in lexus ish

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -261,7 +261,7 @@ class CarInterface(object):
     ret.cruiseState.speed = self.CS.v_cruise_pcm * CV.KPH_TO_MS
     ret.cruiseState.available = bool(self.CS.main_on)
     ret.cruiseState.speedOffset = 0.
-    if self.CP.carFingerprint in [CAR.RAV4H, CAR.HIGHLANDERH, CAR.HIGHLANDER]:
+    if self.CP.carFingerprint in [CAR.RAV4H, CAR.HIGHLANDERH, CAR.HIGHLANDER, CAR.LEXUS_ISH]:
       # ignore standstill in hybrid vehicles, since pcm allows to restart without
       # receiving any special command
       ret.cruiseState.standstill = False
