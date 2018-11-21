2b5f8b34c4e46404ff6de862168b423ce2d36205
dekerr <dkerr2@mix.wvu.edu>
2018-11-21 02:01:25 -0500

    gm

--- selfdrive/car/gm/radar_interface.py
+++ selfdrive/car/gm/radar_interface.py
@@ -19,26 +19,60 @@ NUM_SLOTS = 20
 # messages that are present in DBC
 LAST_RADAR_MSG = RADAR_HEADER_MSG + NUM_SLOTS
 
+def create_radar_signals(*signals):
+  # accepts multiple namedtuples in the form ([('name', value)],[msg])
+  name_value = []
+  msgs   = []
+  repetitions  = []
+  for s in signals:
+    name_value += [nv for nv in s.name_value]
+    name_value_n = len(s.name_value)
+    msgs_n = [len(s.msg)]
+    repetitions += msgs_n * name_value_n
+    msgs += s.msg * name_value_n
+
+  name_value = sum([[nv] * r for nv, r in zip(name_value, repetitions)], [])
+  names = [n for n, v in name_value]
+  vals  = [v for n, v in name_value]
+  return zip(names, msgs, vals)
+
+def create_radar_checks(msgs, select, rate = [20]):
+  if select == "all":
+    return zip(msgs, rate * len(msgs))
+  if select == "last":
+    return zip([msgs[-1]], rate)
+  if select == "none":
+    return []
+  return []
+
 def create_radard_can_parser(canbus, car_fingerprint):
 
   dbc_f = DBC[car_fingerprint]['radar']
   if car_fingerprint in (CAR.VOLT, CAR.MALIBU):
     # C1A-ARS3-A by Continental
-    radar_targets = range(SLOT_1_MSG, SLOT_1_MSG + NUM_SLOTS)
-    signals = zip(['FLRRNumValidTargets',
-                   'FLRRSnsrBlckd', 'FLRRYawRtPlsblityFlt',
-                   'FLRRHWFltPrsntInt', 'FLRRAntTngFltPrsnt',
-                   'FLRRAlgnFltPrsnt', 'FLRRSnstvFltPrsntInt'] +
-                  ['TrkRange'] * NUM_SLOTS + ['TrkRangeRate'] * NUM_SLOTS +
-                  ['TrkRangeAccel'] * NUM_SLOTS + ['TrkAzimuth'] * NUM_SLOTS +
-                  ['TrkWidth'] * NUM_SLOTS + ['TrkObjectID'] * NUM_SLOTS,
-                  [RADAR_HEADER_MSG] * 7 + radar_targets * 6,
-                  [0] * 7 +
-                  [0.0] * NUM_SLOTS + [0.0] * NUM_SLOTS +
-                  [0.0] * NUM_SLOTS + [0.0] * NUM_SLOTS +
-                  [0.0] * NUM_SLOTS + [0] * NUM_SLOTS)
+    radar_trackers = range(SLOT_1_MSG, SLOT_1_MSG + NUM_SLOTS)
+    radar_messages = [RADAR_HEADER_MSG] + radar_trackers
 
-    checks = []
+    sig = namedtuple('sig','name_value msg')
+    headers = [('FLRRNumValidTargets', 0),
+               ('FLRRSnsrBlckd', 0),
+               ('FLRRYawRtPlsblityFlt', 0),
+               ('FLRRHWFltPrsntInt', 0),
+               ('FLRRAntTngFltPrsnt', 0),
+               ('FLRRAlgnFltPrsnt', 0),
+               ('FLRRSnstvFltPrsntInt', 0)]
+    header_sig = sig(headers, [RADAR_HEADER_MSG])
+
+    trackers = [('TrkRange', 0.0),
+                ('TrkRangeRate', 0.0),
+                ('TrkRangeAccel', 0.0),
+                ('TrkAzimuth', 0.0),
+                ('TrkWidth', 0.0),
+                ('TrkObjectID', 0)]
+    tracker_sig = sig(trackers, radar_trackers)
+
+    signals = create_radar_signals(header_sig, tracker_sig)
+    checks = create_radar_checks(radar_messages, select = "none")
 
     return CANParser(dbc_f, signals, checks, canbus.obstacle)
   else:
