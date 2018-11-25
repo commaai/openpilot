e943669060e33966f25e1249c4ea84a239fca2ab
arne182 <arneschwarck@gmail.com>
2018-11-25 10:36:49 +0100

    Try and fix tr 1 press

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -303,7 +303,6 @@ class CarController(object):
     
     if CS.cstm_btns.get_button_status("tr") > 0:
       distance = 0b01110011 #x73 comma with toggle - toggle is 5th bit from right
-      CS.cstm_btns.set_button_status("tr", 0)
     else:
       distance = 0b01100011 #x63 comma with toggle - toggle is 5th bit from right
     # accel cmd comes from DSU, but we can spam can to cancel the system even if we are using lat only control
