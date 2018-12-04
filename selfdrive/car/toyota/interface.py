d06c2411d024283212ffb52897c6897ed10f15ed
arne182 <arneschwarck@gmail.com>
2018-12-04 20:27:59 +0100

    Add Cmma's Tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -81,12 +81,13 @@ class CarInterface(object):
       ret.safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
       ret.wheelbase = 2.70
       ret.steerRatio = 15.00   # unknown end-to-end spec
-      tire_stiffness_factor = 0.6371   # hand-tune
-      ret.mass = 3045 * CV.LB_TO_KG + std_cargo
-      ret.steerKpV, ret.steerKiV = [[0.3], [0.0002]]
+      tire_stiffness_factor = 1.0
+      ret.mass = 3370 * CV.LB_TO_KG + std_cargo
+      ret.steerKpV, ret.steerKiV = [[0.369,0.397,0.418], [0.0056,0.0093,0.0121]]
       ret.steerKf = 0.000078   # full torque for 10 deg at 80mph means 0.00007818594
       # TODO: Prius seem to have very laggy actuators. Understand if it is lag or hysteresis
-      ret.steerActuatorDelay = 0.01
+      ret.steerActuatorDelay = 0.16
+      ret.steerKiBP, ret.steerKpBP = [[0.,27.,47.], [0.,27.,47.]]
       if ret.enableGasInterceptor:
         ret.gasMaxV = [0.2, 0.5, 0.7]
         ret.longitudinalKpV = [3.6, 2.4, 1.5]
