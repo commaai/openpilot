e478620421297b4ebdab46f3c966e5fd84c16bf1
Unknown <asidhu1@gmail.com>
2018-12-07 17:34:23 -0500

    Update sterrrateCost

--- selfdrive/car/tesla/interface.py
+++ selfdrive/car/tesla/interface.py
@@ -179,7 +179,7 @@ class CarInterface(object):
     ret.stoppingControl = True
     ret.steerLimitAlert = False
     ret.startAccel = 0.5
-    ret.steerRateCost = 1.
+    ret.steerRateCost = 2.
 
     return ret
 
