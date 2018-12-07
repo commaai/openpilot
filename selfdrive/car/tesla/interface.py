45d4c228f129420204dbed0e63aa3cb021a5f74a
David Abrahams <you@example.com>
2018-12-07 23:42:39 +0000

    PCC PID twiddling

--- selfdrive/car/tesla/interface.py
+++ selfdrive/car/tesla/interface.py
@@ -119,7 +119,7 @@ class CarInterface(object):
       ret.steerActuatorDelay = 0.09
       
       # Kp and Ki for the longitudinal control
-      KP = 0.4
+      KP = 1.0
       ret.longitudinalKpBP = [0., 5., 35.]
       ret.longitudinalKpV = [i * KP for i in [1, 0.82, 0.67]]
       KI = 0.05
