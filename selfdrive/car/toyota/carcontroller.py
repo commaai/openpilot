ab2b431da6968a71f6edb295545fe689f9859225
arne182 <arneschwarck@gmail.com>
2018-12-22 09:10:00 +0100

    Reverse on off for lane lines

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -340,7 +340,7 @@ class CarController(object):
       send_ui = False
 
     if (frame % 100 == 0 or send_ui) and ECU.CAM in self.fake_ecus:
-      can_sends.append(create_ui_command(self.packer, steer, sound1, sound2, CS.lkas_barriers, left_line + 1, right_line + 1))
+      can_sends.append(create_ui_command(self.packer, steer, sound1, sound2, CS.lkas_barriers, 2 - left_line, 2 - right_line))
 
     if frame % 100 == 0 and ECU.DSU in self.fake_ecus:
       can_sends.append(create_fcw_command(self.packer, fcw))
