a402f2dba389bca9c74e0286da7b1b7fc3e8c456
arne182 <arneschwarck@gmail.com>
2018-12-31 21:23:24 +0100

    Revert Touch.c changes from 0.5.6

--- selfdrive/common/touch.c
+++ selfdrive/common/touch.c
@@ -24,14 +24,15 @@ static int find_dev() {
     int fd = openat(dirfd(dir), de->d_name, O_RDONLY);
     assert(fd >= 0);
 
-    unsigned char ev_bits[KEY_MAX / 8 + 1];
-    err = ioctl(fd, EVIOCGBIT(EV_ABS, sizeof(ev_bits)), ev_bits);
+    char name[128] = {0};
+    err = ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name);
     assert(err >= 0);
 
-    const int x_key = ABS_MT_POSITION_X / 8;
-    const int y_key = ABS_MT_POSITION_Y / 8;
-    if ((ev_bits[x_key] & (ABS_MT_POSITION_X - x_key)) &&
-        (ev_bits[y_key] & (ABS_MT_POSITION_Y - y_key))) {
+    unsigned long ev_bits[8] = {0};
+    err = ioctl(fd, EVIOCGBIT(0, sizeof(ev_bits)), ev_bits);
+    assert(err >= 0);
+
+    if (strncmp(name, "synaptics", 9) == 0 && ev_bits[0] == 0xb) {
       ret = fd;
       break;
     }
@@ -76,7 +77,12 @@ int touch_poll(TouchState *s, int* out_x, int* out_y, int timeout) {
       } else if (event.code == ABS_MT_POSITION_Y) {
         s->last_y = event.value;
       }
-      up = true;
+      break;
+    case EV_KEY:
+      if (event.code == BTN_TOOL_FINGER && event.value == 0) {
+        // finger up
+        up = true;
+      }
       break;
     default:
       break;
