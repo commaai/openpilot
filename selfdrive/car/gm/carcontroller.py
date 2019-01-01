106a15472091381d6831800fd333a2c4c1dbf9d8
arne182 <arneschwarck@gmail.com>
2019-01-01 09:14:51 +0100

    Enable auto resume only if button is active

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -161,11 +161,11 @@ class CarController(object):
         at_full_stop = enabled and standstill and car_stopping
         near_stop = enabled and (CS.v_ego < P.NEAR_STOP_BRAKE_PHASE) and car_stopping
         can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, canbus.chassis, apply_brake, idx, near_stop, at_full_stop))
-
-        # Auto-resume from full stop by resetting ACC control
-        acc_enabled = enabled
-        if standstill and not car_stopping:
-          acc_enabled = False
+        if CS.cstm_btns.get_button_status("stop") > 0:
+          # Auto-resume from full stop by resetting ACC control
+          acc_enabled = enabled
+          if standstill and not car_stopping:
+            acc_enabled = False
 
         can_sends.append(gmcan.create_gas_regen_command(self.packer_pt, canbus.powertrain, apply_gas, idx, acc_enabled, at_full_stop))
 
