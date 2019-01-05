3f777e12824d597a40255ee57416e216594d3e10
Sid <asidhu1@gmail.com>
2019-01-05 12:28:53 -0500

    Revert "Update CLICKS for longer bootup time of EONS and avoid unwanted fast charge mode"
    This reverts commit 627f6cf4e13bd85b1a25cb1e907d7b6166a1ca88.

--- panda/board/main.c
+++ panda/board/main.c
@@ -598,8 +598,6 @@ int main() {
     uint64_t marker = 0;
     #define CURRENT_THRESHOLD 0xF00
     #define CLICKS 8
-    // Enough clicks to ensure that enumeration happened. Should be longer than bootup time of the device connected to EON
-    #define CLICKS_BOOTUP 30
   #endif
 
   for (cnt=0;;cnt++) {
@@ -626,8 +624,8 @@ int main() {
           }
           break;
         case USB_POWER_CDP:
-          // been CLICKS_BOOTUP clicks since we switched to CDP
-          if ((cnt-marker) >= CLICKS_BOOTUP) {
+          // been CLICKS clicks since we switched to CDP
+          if ((cnt-marker) >= CLICKS) {
             // measure current draw, if positive and no enumeration, switch to DCP
             if (!is_enumerated && current < CURRENT_THRESHOLD) {
               puts("USBP: no enumeration with current draw, switching to DCP mode\n");
