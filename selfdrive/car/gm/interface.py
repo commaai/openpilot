b78ed77f1dbcfdeffb05e7996b925391a663fb7e
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2019-01-08 22:40:09 -0500

    Adding GM Steering Rate (#495)
    * adding steering rate
    
    * adding steering rate

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -214,6 +214,7 @@ class CarInterface(object):
 
     # steering wheel
     ret.steeringAngle = self.CS.angle_steers
+    ret.steeringRate = self.CS.angle_steers_rate
 
     # torque and user override. Driver awareness
     # timer resets when the user uses the steering wheel.
