d845bff50715e789797630ea5fd97fc1d1fa853a
David Abrahams <you@example.com>
2019-01-10 21:49:15 +0000

    Tighter limits on pedal jerk
    The pedal interceptor will take at least 2 seconds to go from 0 to
    100%, and at least 0.5 seconds to go from 100% to 0.

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -355,7 +355,7 @@ class CarController(object):
           # Send this CAN msg first because it is racing against the real stalk.
           can_sends.insert(0, cruise_msg)
       apply_accel = 0.
-      if CS.pedal_interceptor_available and frame % 5 == 0: # pedal processed at 20Hz
+      if CS.pedal_interceptor_available and frame % 5 == 0: # pedal processed at 20Hz. This must match the "_DT" value in PCC_module.py
         apply_accel, accel_needed, accel_idx = self.PCC.update_pdl(enabled, CS, frame, actuators, pcm_speed)
         can_sends.append(teslacan.create_pedal_command_msg(apply_accel, int(accel_needed), accel_idx))
       self.last_angle = apply_angle
