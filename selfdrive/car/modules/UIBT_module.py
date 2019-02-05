0a3aa5c8887d784ac5885fc31ae7501b5da4c0b2
arne182 <arneschwarck@gmail.com>
2019-02-05 23:37:46 +0200

    Revert back

--- selfdrive/car/modules/UIBT_module.py
+++ selfdrive/car/modules/UIBT_module.py
@@ -29,19 +29,21 @@ class UIButtons:
         fi =  open(self.buttons_labels_path, buttons_file_r)
         indata = fi.read()
         fi.close()
-        file_matches = False
+        file_matches = True
         if len(indata) == btn_msg_len * 6 :
+            #check if it matches the current setup
             for i in range(0, len(indata), btn_msg_len):
                 j = int(i/btn_msg_len)
                 name,label,label2 = struct.unpack(btn_msg_struct, indata[i:i+btn_msg_len]) 
-                if self.btns[j].btn_name == name.rstrip("\0"):
-                    file_matches = True
+                if (self.btns[j].btn_name != name.rstrip("\0")):
+                    file_matches = False
+            #we have all the da;ta and it matches
+            if file_matches:
+                for i in range(0, len(indata), btn_msg_len):
+                    j = int(i/btn_msg_len)
+                    name,label,label2 = struct.unpack(btn_msg_struct, indata[i:i+btn_msg_len]) 
                     self.btns[j].btn_label = label.rstrip("\0")
-                    #check if label is actually a valid option
-                    if label2.rstrip("\0") in self.CS.btns_init[j][2]:
-                        self.btns[j].btn_label2 = label2.rstrip("\0")
-                    else:
-                        self.btns[j].btn_label2 = self.CS.btns_init[j][2][0]
+                    self.btns[j].btn_label2 = label2.rstrip("\0")
             return file_matches
         else:
             #we don't have all the data, ignore
@@ -86,7 +88,7 @@ class UIButtons:
         self.hasChanges = True
         self.last_in_read_time = datetime.min 
         if os.path.exists(self.buttons_labels_path):
-            self.init_ui_buttons()
+            self.btns = self.CS.init_ui_buttons()
             #there is a file, load it
             if self.read_buttons_labels_from_file():
                 self.read_buttons_out_file()
@@ -97,7 +99,7 @@ class UIButtons:
                 self.write_buttons_out_file()
         else:
             #there is no file, create it
-            self.init_ui_buttons()
+            self.btns = self.CS.init_ui_buttons()
             self.hasChanges = True
             self.write_buttons_labels_to_file()
             self.write_buttons_out_file()
@@ -105,17 +107,6 @@ class UIButtons:
         self.isLive = True
         self.send_button_info()
         self.CS.UE.uiSetCarEvent(self.car_folder,self.car_name)
-
-    def init_ui_buttons(self):
-        self.btns = []
-        try:
-            self.CS.init_ui_buttons()
-            print "Buttons iniatlized with custom CS code"  
-        except AttributeError:
-            # no init method
-            print "Buttons iniatlized with just base code"
-        for i in range(0,len(self.CS.btns_init)):
-            self.btns.append(UIButton(self.CS.btns_init[i][0],self.CS.btns_init[i][1],1,self.CS.btns_init[i][2][0],i))
 
     def get_button(self, btn_name):
         for button in self.btns:
@@ -144,18 +135,15 @@ class UIButtons:
 
     def set_button_status_from_ui(self,id,btn_status):
         old_btn_status = self.btns[id].btn_status
-        old_btn_label2 = self.btns[id].btn_label2
         if old_btn_status * btn_status == 0 and old_btn_status != btn_status:
             self.hasChanges = True
-        self.update_ui_buttons(id,btn_status)
+        self.CS.update_ui_buttons(id,btn_status)
         new_btn_status = self.btns[id].btn_status
         if new_btn_status * btn_status == 0 and new_btn_status != btn_status:
             self.hasChanges = True
         if self.hasChanges:
             self.CS.UE.uiButtonInfoEvent(id,self.btns[id].btn_name, \
                     self.btns[id].btn_label,self.btns[id].btn_status,self.btns[id].btn_label2)
-            if old_btn_label2 != self.btns[id].btn_label2:
-                self.write_buttons_labels_to_file()
             self.write_buttons_out_file()
         
 
@@ -164,37 +152,4 @@ class UIButtons:
         if btn:
             return btn.btn_label2
         else:
-            return -1    
-
-    def get_button_label2_index(self, btn_name):
-        btn = self.get_button(btn_name)
-        if btn:
-            b_index = -1
-            bpos = self.btns.index(btn)
-            for i in range(0,len(self.CS.btns_init[bpos][2])):
-                if btn.btn_label2 == self.CS.btns_init[bpos][2][i]:
-                    b_index = i
-            return b_index
-        else:
-            return -1
-    
-    def update_ui_buttons(self,id,btn_status):
-        if self.CS.cstm_btns.btns[id].btn_status > 0:
-        #if we have more than one Label2 then we toggle
-            if (len(self.CS.btns_init[id][2]) > 1):
-                status2 = 0
-                for i in range(0,len(self.CS.btns_init[id][2])):
-                    if self.CS.cstm_btns.btns[id].btn_label2 == self.CS.btns_init[id][2][i]:
-                        status2 = (i + 1 ) % len(self.CS.btns_init[id][2])
-                self.CS.cstm_btns.btns[id].btn_label2 = self.CS.btns_init[id][2][status2]
-                self.CS.cstm_btns.hasChanges = True
-            else:
-                self.CS.cstm_btns.btns[id].btn_status = btn_status * self.CS.cstm_btns.btns[id].btn_status
-        else:
-            self.CS.cstm_btns.btns[id].btn_status = btn_status
-        try:
-            #try to see if there is custom CS function for updating ui buttons
-            self.CS.update_ui_buttons(id,btn_status)
-        except AttributeError:
-            # no update method
-            print "Buttons updated with just base code"
+return -1 
