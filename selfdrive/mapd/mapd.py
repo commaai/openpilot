91032e8fe36e68d75ae1dde735bb68b64f501ff9
kegman <8837066+kegman@users.noreply.github.com>
2019-02-22 00:07:18 -0500

    Add 'Accept-Encoding': 'gzip' fix

--- selfdrive/mapd/mapd.py
+++ selfdrive/mapd/mapd.py
@@ -36,6 +36,7 @@ from selfdrive.version import version, dirty
 OVERPASS_API_URL = "https://overpass.kumi.systems/api/interpreter"
 OVERPASS_HEADERS = {
     'User-Agent': 'NEOS (comma.ai)'
+    'Accept-Encoding': 'gzip'
 }
 
 last_gps = None
