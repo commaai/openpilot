5b8976af6195f5365ed34f8b2dc059681a3f745a
Rick Lan <ricklan@gmail.com>
2019-03-02 17:49:46 +1000

    * change steering torque to 1600

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -2,7 +2,7 @@ int toyota_giraffe_switch_1 = 0;          // is giraffe switch 1 high?
 int toyota_camera_forwarded = 0;          // should we forward the camera bus?
 
 // global torque limit
-const int TOYOTA_MAX_TORQUE = 1500;       // max torque cmd allowed ever
+const int TOYOTA_MAX_TORQUE = 1600;       // max torque cmd allowed ever
 
 // rate based torque limit + stay within actually applied
 // packet is sent at 100hz, so this limit is 1000/sec
