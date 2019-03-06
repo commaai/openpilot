ddf881e3de293485c616e8cbdf3abb28331cf72e
arne182 <arneschwarck@gmail.com>
2019-03-06 17:25:10 +0100

    Remove wiggly option from GM, not working with 059

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -62,8 +62,6 @@ class CarState(object):
     # initialize can parser
     self.alcaLabels = ["MadMax","Normal","Wifey"]
     self.alcaMode = 2
-    self.visionLabels = ["normal","wiggly"]
-    self.visionMode = 0
     self.car_fingerprint = CP.carFingerprint
     self.cruise_buttons = CruiseButtons.UNPRESS
     self.prev_distance_button = 0
@@ -128,9 +126,6 @@ class CarState(object):
     #BB custom message counter
     self.custom_alert_counter = -1 #set to 100 for 1 second display; carcontroller will take down to zero
     
-    #BB visiond last type
-    self.last_visiond = self.cstm_btns.btns[0].btn_label2
-    
     # vEgo kalman filter
     dt = 0.01
     self.v_ego_kf = KF1D(x0=np.matrix([[0.], [0.]]),
@@ -144,7 +139,7 @@ class CarState(object):
     btns.append(UIButton("sound", "SND", 0, "", 0))
     btns.append(UIButton("alca", "ALC", 0, self.alcaLabels[self.alcaMode], 1))
     btns.append(UIButton("stop","",1,"SNG",2))
-    btns.append(UIButton("vision","VIS",0,self.visionLabels[self.visionMode],3))
+    btns.append(UIButton("","",0,"",3))
     btns.append(UIButton("gas","GAS",1,"",4))
     btns.append(UIButton("lka","LKA",1,"",5))
     return btns
@@ -158,16 +153,6 @@ class CarState(object):
             self.alcaMode = 0
           self.cstm_btns.btns[id].btn_label2 = self.alcaLabels[self.alcaMode]
           self.cstm_btns.hasChanges = True
-      if (id == 3) and (btn_status == 0) and self.cstm_btns.btns[id].btn_name=="vision":
-          if self.cstm_btns.btns[id].btn_label2 == self.visionLabels[self.visionMode]:
-            self.visionMode = (self.visionMode + 1 ) % 2
-          else:
-            self.visionMode = 0
-          self.cstm_btns.btns[id].btn_label2 = self.visionLabels[self.visionMode]
-          self.cstm_btns.hasChanges = True
-          self.last_visiond = self.cstm_btns.btns[id].btn_label2
-          args = ["/data/openpilot/selfdrive/car/modules/ch_visiond.sh", self.cstm_btns.btns[id].btn_label2]
-          subprocess.Popen(args, shell = False, stdin=None, stdout=None, stderr=None, env = dict(os.environ), close_fds=True)
       else:
         self.cstm_btns.btns[id].btn_status = btn_status * self.cstm_btns.btns[id].btn_status
     else:
