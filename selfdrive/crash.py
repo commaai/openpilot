9399502ca2b52cc5b0372056b1ce3b07d3f0e2de
arne182 <arneschwarck@gmail.com>
2019-03-06 13:38:37 +0100

    Enable Sentry logging

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -17,13 +17,19 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH"):
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
-  client = Client('https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924',
+  client = Client('https://137e8e621f114f858f4c392c52e18c6d:8aba82f49af040c8aac45e95a8484970@sentry.io/1404547',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags={'dirty': dirty})
 
   def capture_exception(*args, **kwargs):
     client.captureException(*args, **kwargs)
     cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
 
+  def capture_warning(warning_string):
+    client.captureMessage(warning_string, level='warning')
+  
+  def capture_info(info_string):
+    client.captureMessage(info_string, level='info')
+    
   def bind_user(**kwargs):
     client.user_context(kwargs)
 
