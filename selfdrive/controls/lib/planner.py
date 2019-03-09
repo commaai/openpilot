27c5bafea191872a7cf29a136525221a9bb4fcec
arne182 <arneschwarck@gmail.com>
2019-03-09 19:33:47 +0100

    Fix get_relative_velocity Call

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -429,10 +429,10 @@ class Planner(object):
     self.lead_2 = live20.live20.leadTwo
     
     try:
-      LongitudinalMpc.get_relative_velocity(self.lead_1.vRel)
+      LongitudinalMpc().get_relative_velocity(self.lead_1.vRel)
     except: #if no lead car
       try:
-        LongitudinalMpc.get_relative_velocity(0.0)
+        LongitudinalMpc().get_relative_velocity(0.0)
       except TypeError:
         None
 
