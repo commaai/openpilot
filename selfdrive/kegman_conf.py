9bd849ca93c73bd37b61b4a956f5403f235488aa
arne182 <arneschwarck@gmail.com>
2019-03-10 18:56:32 +0100

    Add "lastALCAMode":"1"

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -26,6 +26,9 @@ class kegman_conf():
       if "brake_distance_extra" not in self.config: # extra braking distance in m
         self.config.update({"brake_distance_extra":"1"})
         self.element_updated = True
+      if "lastALCAMode" not in self.config: # extra braking distance in m
+        self.config.update({"lastALCAMode":"1"})
+        self.element_updated = True
 
       # Force update battery charge limits to higher values for Big Model
       #if self.config['battChargeMin'] != "75":
@@ -37,7 +40,7 @@ class kegman_conf():
         self.write_config(self.config)
 
     else:
-      self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"85", "battChargeMax":"90", "wheelTouchSeconds":"1800", "battPercOff":"25", "carVoltageMinEonShutdown":"11800", "brakeStoppingTarget":"0.25", "angle_steers_offset":"0" , "brake_distance_extra":"1"}
+      self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"85", "battChargeMax":"90", "wheelTouchSeconds":"1800", "battPercOff":"25", "carVoltageMinEonShutdown":"11800", "brakeStoppingTarget":"0.25", "angle_steers_offset":"0" , "brake_distance_extra":"1" , "lastALCAMode":"1"}
       self.write_config(self.config)
     return self.config
 
