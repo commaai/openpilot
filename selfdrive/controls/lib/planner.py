975181e78ae3d34de6056ba2a8ec90626b5c83b2
kegman <8837066+kegman@users.noreply.github.com>
2019-03-14 13:54:45 -0400

    Loosen braking on one bar distance a tad

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -32,7 +32,7 @@ STOPPING_DISTANCE = 2  # increase distance from lead car when stopped
 
 # Braking profile changes (makes the car brake harder because it wants to be farther from the lead car - increase to brake harder)
 ONE_BAR_PROFILE = [ONE_BAR_DISTANCE, FOUR_BAR_DISTANCE]
-ONE_BAR_PROFILE_BP = [0.0, 2.5]
+ONE_BAR_PROFILE_BP = [0.0, 3.0]
 
 TWO_BAR_PROFILE = [TWO_BAR_DISTANCE, FOUR_BAR_DISTANCE]
 TWO_BAR_PROFILE_BP = [0.0, 3.5]
