83fc44f5d285a4a3917019cd6d66c4a977988afd
Douglas Schilling Landgraf <dougsland@gmail.com>
2019-03-25 13:44:52 -0400

    selfdrive: make flake8 happy services.py

--- selfdrive/services.py
+++ selfdrive/services.py
@@ -1,14 +1,19 @@
 import os
 import yaml
 
-class Service(object):
-  def __init__(self, port, should_log):
-    self.port = port
-    self.should_log = should_log
 
-service_list_path = os.path.join(os.path.dirname(__file__), "service_list.yaml")
+class Service(object):
+    def __init__(self, port, should_log):
+        self.port = port
+        self.should_log = should_log
+
+
+service_list_path = os.path.join(
+    os.path.dirname(__file__),
+    "service_list.yaml"
+)
 
 service_list = {}
 with open(service_list_path, "r") as f:
-  for k, v in yaml.load(f).items():
-    service_list[k] = Service(v[0], v[1])
+    for k, v in yaml.load(f).items():
+        service_list[k] = Service(v[0], v[1])
