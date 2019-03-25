371dca1286fea46b9e83cff9372fa01764da5a60
Douglas Schilling Landgraf <dougsland@gmail.com>
2019-03-25 10:17:55 -0400

    selfdrive: make flake8 happy pandad.py

--- selfdrive/pandad.py
+++ selfdrive/pandad.py
@@ -3,12 +3,13 @@
 import os
 from panda import ensure_st_up_to_date
 
-def main(gctx=None):
-  ensure_st_up_to_date()
 
-  os.chdir("boardd")
-  os.execvp("./boardd", ["./boardd"])
+def main(gctx=None):
+    ensure_st_up_to_date()
+
+    os.chdir("boardd")
+    os.execvp("./boardd", ["./boardd"])
+
 
 if __name__ == "__main__":
-  main()
-
+    main()
