3b37bd27782cdee11a86673512e12e69ee27b2f9
kegman <8837066+kegman@users.noreply.github.com>
2019-03-29 12:22:57 -0400

    Tighten braking for 1 and 2 Bar

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -21,10 +21,10 @@ CITY_SPEED = 19.44  # braking profile changes when below this speed based on fol
 STOPPING_DISTANCE = 2  # increase distance from lead car when stopped
 
 # Braking profile changes (makes the car brake harder because it wants to be farther from the lead car - increase to brake harder)
-ONE_BAR_PROFILE = [ONE_BAR_DISTANCE, ONE_BAR_DISTANCE]
+ONE_BAR_PROFILE = [ONE_BAR_DISTANCE, TWO_BAR_DISTANCE]
 ONE_BAR_PROFILE_BP = [0.0, 3.0]
 
-TWO_BAR_PROFILE = [TWO_BAR_DISTANCE, TWO_BAR_DISTANCE]
+TWO_BAR_PROFILE = [TWO_BAR_DISTANCE, THREE_BAR_DISTANCE]
 TWO_BAR_PROFILE_BP = [0.0, 3.5]
 
 THREE_BAR_PROFILE = [THREE_BAR_DISTANCE, THREE_BAR_DISTANCE]
