573e41197e34ae92524242ae010aa42e5a2bcf73
Drew Hintz <adhintz@google.com>
2019-04-02 21:54:54 -0700

    2019 Chrysler Pacfica and 2019 Jeep Grand Cherokee support, along with likely support for arbitrary models. This is done by copying most values from the stock LKAS camera which is active with https://github.com/commaai/panda/pull/177

--- selfdrive/car/chrysler/chryslercan.py
+++ selfdrive/car/chrysler/chryslercan.py
@@ -5,14 +5,6 @@ from selfdrive.car.chrysler.values import CAR
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 AudibleAlert = car.CarControl.HUDControl.AudibleAlert
 
-MODEL_TO_CONSTANT = {
-  CAR.PACIFICA_2017_HYBRID: 0,
-  CAR.PACIFICA_2018: 0x64,
-  CAR.PACIFICA_2018_HYBRID: 0xa8,
-  CAR.PACIFICA_2019_HYBRID: 0x68,
-  CAR.JEEP_CHEROKEE: 0xa4,
-  }
-
 def calc_checksum(data):
   """This function does not want the checksum byte in the input data.
 
@@ -53,28 +45,26 @@ def calc_checksum(data):
 def make_can_msg(addr, dat):
   return [addr, 0, dat, 0]
 
-def create_lkas_heartbit(car_fingerprint):
-  # LKAS_HEARTBIT (729) Lane-keeping heartbeat.
-  msg = '0000000820'.decode('hex')  # 2017
-  return make_can_msg(0x2d9, msg)
+def create_lkas_heartbit(packer, lkas_status_ok):
+  # LKAS_HEARTBIT 0x2d9 (729) Lane-keeping heartbeat.
+  values = {
+    "LKAS_STATUS_OK": lkas_status_ok
+  }
+  return packer.make_can_msg("LKAS_HEARTBIT", 0, values)
 
-def create_lkas_hud(packer, gear, lkas_active, hud_alert, car_fingerprint, hud_count):
+def create_lkas_hud(packer, gear, lkas_active, hud_alert, hud_count, lkas_car_model):
   # LKAS_HUD 0x2a6 (678) Controls what lane-keeping icon is displayed.
 
   if hud_alert == VisualAlert.steerRequired:
     msg = '0000000300000000'.decode('hex')
     return make_can_msg(0x2a6, msg)
 
-  color = 0  # default values are for park or neutral
-  lines = 0
+  color = 1  # default values are for park or neutral in 2017 are 0 0, but trying 1 1 for 2019
+  lines = 1
   alerts = 0
 
-  if hud_count < (3 *4):  # first 3 seconds, 4Hz
-    lines = 1
+  if hud_count < (1 *4):  # first 3 seconds, 4Hz
     alerts = 1
-  elif hud_count < (6 * 4):  # next 3 seconds, 4Hz
-    lines = 1
-    alerts = 0
   # CAR.PACIFICA_2018_HYBRID and CAR.PACIFICA_2019_HYBRID
   # had color = 1 and lines = 1 but trying 2017 hybrid style for now.
   if gear in ('drive', 'reverse', 'low'):
@@ -87,7 +77,7 @@ def create_lkas_hud(packer, gear, lkas_active, hud_alert, car_fingerprint, hud_c
 
   values = {
     "LKAS_ICON_COLOR": color,  # byte 0, last 2 bits
-    "CAR_MODEL": MODEL_TO_CONSTANT[car_fingerprint],  # byte 1
+    "CAR_MODEL": lkas_car_model,  # byte 1
     "LKAS_LANE_LINES": lines,  # byte 2, last 4 bits
     "LKAS_ALERTS": alerts,  # byte 3, last 4 bits
     }
@@ -95,11 +85,11 @@ def create_lkas_hud(packer, gear, lkas_active, hud_alert, car_fingerprint, hud_c
   return packer.make_can_msg("LKAS_HUD", 0, values)  # 0x2a6
 
 
-def create_lkas_command(packer, apply_steer, frame):
-  # LKAS_COMMAND (658) Lane-keeping signal to turn the wheel.
+def create_lkas_command(packer, apply_steer, moving_fast, frame):
+  # LKAS_COMMAND 0x292 (658) Lane-keeping signal to turn the wheel.
   values = {
     "LKAS_STEERING_TORQUE": apply_steer,
-    "LKAS_HIGH_TORQUE": 1,
+    "LKAS_HIGH_TORQUE": int(moving_fast),
     "COUNTER": frame % 0x10,
   }
 
