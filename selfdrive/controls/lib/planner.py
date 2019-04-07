55cf48456c56fe821dd3e6e9f965384e88812d92
Shane <shane@smiskol.com>
2019-04-07 17:28:26 -0500

    upped accel limits to stop oscillations

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -33,7 +33,7 @@ _A_CRUISE_MIN_BP = [   0., 5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [2.0, 1.25, .8, .5, .3]
+_A_CRUISE_MAX_V = [2.75, 2.0, 1.1, .5, .3]
 _A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 1.2, .7, .3]
 _A_CRUISE_MAX_BP = [0.,  5., 10., 20., 40.]
 
