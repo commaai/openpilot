17cd76d607efa49f3976f36346f977cb3097b905
Shane Smiskol <shane@smiskol.com>
2019-04-06 23:23:23 -0500

    Test

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -258,8 +258,8 @@ def state_control(plan, path_plan, CS, CP, state, events, v_cruise_kph, v_cruise
     elif CS.gasbuttonstatus == 2:
       CP.gasMaxV = [0.2, 0.2, 0.2]'''
 
-    CP.gasMaxV = [0.12, 0.14, 0.165]
-    CP.gasMaxBP = [0.0, 2.2352, 4.4704]
+    CP.gasMaxV = [0.125, 0.15, 0.175, .28]
+    CP.gasMaxBP = [0.0, 2.2352, 4.4704, 6.7056]
   else:
     if CS.gasbuttonstatus == 0:
       CP.gasMaxV = [0.5, 0.7, 0.9]
