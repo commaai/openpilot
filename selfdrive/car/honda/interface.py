eb0d8cd4e4bd89ce9bf5a814cb85b1389345c79d
kegman <8837066+kegman@users.noreply.github.com>
2019-04-08 16:13:35 -0400

    Ki = 0.29 for better lane centering Pilot / Ridgeline

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -306,7 +306,7 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 16.0         # as spec
       tire_stiffness_factor = 0.82
-      ret.steerKpV, ret.steerKiV = [[0.40], [0.20]]
+      ret.steerKpV, ret.steerKiV = [[0.40], [0.29]]
       ret.longitudinalKpBP = [0., 5., 35.]
       ret.longitudinalKpV = [1.2, 0.8, 0.5]
       ret.longitudinalKiBP = [0., 35.]
@@ -319,7 +319,7 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 15.59        # as spec
       tire_stiffness_factor = 0.82
-      ret.steerKpV, ret.steerKiV = [[0.40], [0.20]]
+      ret.steerKpV, ret.steerKiV = [[0.40], [0.29]]
       ret.longitudinalKpBP = [0., 5., 35.]
       ret.longitudinalKpV = [1.2, 0.8, 0.5]
       ret.longitudinalKiBP = [0., 35.]
