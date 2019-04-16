a179ecbd0b75e9bf3809939352ad1570265e8ab0
Shane <shane@smiskol.com>
2019-04-16 09:31:27 -0500

    remove debugging code

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -203,11 +203,7 @@ def start_managed_process(name):
       pid = subprocess.check_output(["pgrep", "selfdrive.controls.controlsd"]).strip("\n")
       subprocess.call(["renice", "-n", "-20", pid])
     except:  # should never occur, controlsd not running
-      try:
-        with open("/data/nice_error.txt", "w") as f:
-          f.write("True")
-      except:
-        pass
+      pass
 
 def prepare_managed_process(p):
   proc = managed_processes[p]
