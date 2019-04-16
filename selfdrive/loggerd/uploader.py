08ccd1fba627f404f0d73c7f1988d9e206ec97f6
Shane Smiskol <shane@smiskol.com>
2019-04-16 12:54:38 -0500

    move compression code

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -284,15 +284,15 @@ def uploader_fn(exit_event):
     if exit_event.is_set():
       return
 
-    d = uploader.next_file_to_compress()
-    if d is not None:
-      key, fn, _ = d
-      uploader.compress(key, fn)
-      continue
-
     if not should_upload:
       time.sleep(5)
       continue
+    else:
+      d = uploader.next_file_to_compress()
+      if d is not None:
+        key, fn, _ = d
+        uploader.compress(key, fn)
+        continue
 
     d = uploader.next_file_to_upload(with_video=True)
     if d is None:
