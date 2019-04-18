007e7f028006f5c625df08c9883af39a8a163a53
Shane <shane@smiskol.com>
2019-04-18 09:06:08 -0500

    slow mode fix

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -289,6 +289,17 @@ class CarState(object):
 
           self.cstm_btns.btns[id].btn_label2 = self.gasLabels[self.gasMode]
           self.cstm_btns.hasChanges = True
+      elif (id == None) and (btn_status == 0) and self.cstm_btns.btns[id].btn_name=="slow":
+        if self.cstm_btns.btns[id].btn_label2 == self.sloLabels[self.sloMode]:
+          self.sloMode = (self.sloMode + 1) % 2
+          self.kegman.conf['lastSloMode'] = str(self.sloMode)
+        else:
+          self.sloMode = 0
+          self.kegman.conf['lastSloMode'] = str(self.sloMode)
+        self.cstm_btns.btns[id].btn_label2 = self.sloMode[self.sloMode]
+        self.cstm_btns.hasChanges = True
+        if self.sloMode == 2:
+          self.cstm_btns.set_button_status("slow", 0)
       else:
         self.cstm_btns.btns[id].btn_status = btn_status * self.cstm_btns.btns[id].btn_status
     else:
