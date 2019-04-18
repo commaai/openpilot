fb0d000510a99141b14c0399ef4e464bfd18d394
Shane <shane@smiskol.com>
2019-04-18 13:25:38 -0500

    Revert "travis fix"
    This reverts commit d951100ba4d2585c7b623c4bf12c01f09bff19ce.

--- run_docker_tests.sh
+++ run_docker_tests.sh
@@ -4,5 +4,5 @@ set -e
 docker build -t tmppilot -f Dockerfile.openpilot .
 
 docker run --rm \
-  -v "$(pwd)"/selfdrive/test/tests/plant/out:/tmp/openpilot/selfdrive/test/tests/plant/out \
-  tmppilot /bin/sh -c 'cd /tmp/openpilot/selfdrive/test/tests/plant && OPTEST=1 ./test_longitudinal.py'
+  -v "$(pwd)"/selfdrive/test/tests/plant/out:/data/openpilot/selfdrive/test/tests/plant/out \
+  tmppilot /bin/sh -c 'cd /data/openpilot/selfdrive/test/tests/plant && OPTEST=1 ./test_longitudinal.py'
