e3784f8e03be36a0dcdf1be2e35f6f260c1df319
arne182 <arneschwarck@gmail.com>
2019-04-18 17:26:29 +0200

    small typo

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -289,8 +289,8 @@ class CarState(object):
             self.kegman.write_config(self.kegman.conf)
           self.cstm_btns.btns[id].btn_label2 = self.alcaLabels[self.sloMode]
           self.cstm_btns.hasChanges = True
-          if self.alcaMode == 0:
-            self.cstm_btns.set_button_status("alca", 0)  # this might not be needed
+          if self.sloMode == 0:
+            self.cstm_btns.set_button_status("slow", 0)  # this might not be needed
       elif (id == 5) and (btn_status == 0) and self.cstm_btns.btns[id].btn_name=="gas":
           if self.cstm_btns.btns[id].btn_label2 == self.gasLabels[self.gasMode]:
             self.gasMode = (self.gasMode + 1 ) % 3
