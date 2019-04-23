6f505693b213824d56280523765b5a01de448960
theantihero <kwj129@gmail.com>
2019-04-22 23:12:05 -0400

    Update crash.py

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -18,7 +18,7 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH"):
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
-  client = Client('https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924',
+  client = Client('https://e4317de26aeb41e3bf0e897d80984376:f56cebbe23294b6ebda899aa92420a5d@sentry.io/1410215',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags={'dirty': dirty})
 
   def capture_exception(*args, **kwargs):
