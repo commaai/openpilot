123245b5fc9dba5e6264ff54e6693ed0ac7c8f40
Arne Schwarck <arneschwarck@gmail.com>
2019-04-30 12:50:52 +0200

    Reduce alca by 10% at 60 mph

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -167,7 +167,7 @@ class CarState(object):
     # ALCA PARAMS
     # max REAL delta angle for correction vs actuator
     self.CL_MAX_ANGLE_DELTA_BP = [10., 15., 32., 55.]#[10., 44.]
-    self.CL_MAX_ANGLE_DELTA = [2.0 * 15.5 / steerRatio, 1.75 * 15.5 / steerRatio, 1.25 * 15.5 / steerRatio, 0.5 * 15.5 / steerRatio]
+    self.CL_MAX_ANGLE_DELTA = [2.0 * 15.5 / steerRatio, 1.6 * 15.5 / steerRatio, 1.1 * 15.5 / steerRatio, 0.5 * 15.5 / steerRatio]
      # adjustment factor for merging steer angle to actuator; should be over 4; the higher the smoother
     self.CL_ADJUST_FACTOR_BP = [10., 50.]
     self.CL_ADJUST_FACTOR = [16. , 8.]
