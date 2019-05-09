c39e1454fb2f1ba62734304d6281b7818f1547cf
Nick Brown <njbgaming09@gmail.com>
2019-05-09 19:13:32 -0400

    Tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -151,10 +151,10 @@ class CarInterface(object):
     elif candidate == CAR.AVALON:
       stop_and_go = False
       ret.safetyParam = 100 # see conversion factor for STEER_TORQUE_EPS in dbc file
-      ret.wheelbase = 2.70
+      ret.wheelbase = 2.82
       ret.steerRatio = 17.8
       tire_stiffness_factor = 0.444
-      ret.mass = 2860 * CV.LB_TO_KG + std_cargo  # mean between normal and hybrid
+      ret.mass = 3505 * CV.LB_TO_KG + std_cargo  # mean between normal and hybrid
       ret.steerKpV, ret.steerKiV = [[0.2], [0.05]]
       ret.steerKf = 0.00003   # full torque for 20 deg at 80mph means 0.00007818594
 
