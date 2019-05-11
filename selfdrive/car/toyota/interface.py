8fd94b496abe621958981af9f1ee06563d3992be
Nick Brown <njbgaming09@gmail.com>
2019-05-10 22:58:00 -0400

    Tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -152,10 +152,10 @@ class CarInterface(object):
       stop_and_go = False
       ret.safetyParam = 100 # see conversion factor for STEER_TORQUE_EPS in dbc file
       ret.wheelbase = 2.82
-      ret.steerRatio = 13.7
+      ret.steerRatio = 14.8 #Found at https://pressroom.toyota.com/releases/2016+avalon+product+specs.download
       tire_stiffness_factor = 0.7989
       ret.mass = 3505 * CV.LB_TO_KG + std_cargo  # mean between normal and hybrid
-      ret.steerKpV, ret.steerKiV = [[0.6], [0.4]]
+      ret.steerKpV, ret.steerKiV = [[0.6], [0.1]]
       ret.steerKf = 0.00006   # full torque for 20 deg at 80mph means 0.00007818594
 
     ret.steerRateCost = 1.
