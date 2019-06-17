13bc1da8d11ce9dfecde1be8dd0d315d85f64cc2
Willem Melching <willem.melching@gmail.com>
2019-06-17 15:17:11 -0700

    Deterministic boardd build

--- selfdrive/boardd/can_list_to_can_capnp.cc
+++ selfdrive/boardd/can_list_to_can_capnp.cc
@@ -21,12 +21,12 @@ void can_list_to_can_capnp_cpp(const std::vector<can_frame> &can_list, std::stri
   event.setLogMonoTime(nanos_since_boot());
 
   auto canData = sendCan ? event.initSendcan(can_list.size()) : event.initCan(can_list.size());
-  int i = 0;
-  for (auto it = can_list.begin(); it != can_list.end(); it++, i++) {
-    canData[i].setAddress(it->address);
-    canData[i].setBusTime(it->busTime);
-    canData[i].setDat(kj::arrayPtr((uint8_t*)it->dat.data(), it->dat.size()));
-    canData[i].setSrc(it->src);
+  int j = 0;
+  for (auto it = can_list.begin(); it != can_list.end(); it++, j++) {
+    canData[j].setAddress(it->address);
+    canData[j].setBusTime(it->busTime);
+    canData[j].setDat(kj::arrayPtr((uint8_t*)it->dat.data(), it->dat.size()));
+    canData[j].setSrc(it->src);
   }
   auto words = capnp::messageToFlatArray(msg);
   auto bytes = words.asBytes();
