b4cb3b64e89e1d3dd1d2d0291dcc033ba8bc5ea7
dekerr <dkerr2@mix.wvu.edu>
2019-06-25 18:23:07 -0400

    move steerratecost

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -69,6 +69,7 @@ class CarInterface(object):
     tireStiffnessRear_civic = 202500
 
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
+    ret.steerRateCost = 1.
     if candidate != CAR.PRIUS:
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
@@ -180,7 +181,6 @@ class CarInterface(object):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.05]]
       ret.lateralTuning.pid.kf = 0.00007818594
 
-    ret.steerRateCost = 1.
     ret.centerToFront = ret.wheelbase * 0.44
 
     #detect the Pedal address
