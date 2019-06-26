d06c2363a5de8557bbec79c8bb84d4bff009f636
Gernby <gernby2002@yahoo.com>
2019-06-26 16:38:27 -0500

    The same angle / bias should be applied to both

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -70,7 +70,7 @@ class PathPlanner(object):
     p_poly = libmpc_py.ffi.new("double[4]", list(self.MP.p_poly))
 
     # account for actuation delay
-    self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset_average, curvature_factor, VM.sR, CP.steerActuatorDelay)
+    self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset_bias, curvature_factor, VM.sR, CP.steerActuatorDelay)
 
     v_ego_mpc = max(v_ego, 5.0)  # avoid mpc roughness due to low speed
     self.libmpc.run_mpc(self.cur_state, self.mpc_solution,
@@ -85,7 +85,7 @@ class PathPlanner(object):
       delta_desired = math.radians(angle_steers - angle_offset_bias) / VM.sR
       rate_desired = 0.0
 
-    self.angle_steers_des_mpc = float(math.degrees(delta_desired * VM.sR) + live100.live100.angleModelBias)
+    self.angle_steers_des_mpc = float(math.degrees(delta_desired * VM.sR) + angle_offset_bias)
 
     #  Check for infeasable MPC solution
     mpc_nans = np.any(np.isnan(list(self.mpc_solution[0].delta)))
