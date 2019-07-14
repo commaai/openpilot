aa213ef44b0a3344ab2bc3ee5716a5a47859d00a
Greg Esmond <gernby2002@yahoo.com>
2019-07-14 20:10:42 +0000

    slowed learning rate

--- selfdrive/locationd/params_learner.cc
+++ selfdrive/locationd/params_learner.cc
@@ -36,7 +36,7 @@ ParamsLearner::ParamsLearner(cereal::CarParams::Reader car_params,
   min_sr_th = MIN_SR_TH * car_params.getSteerRatio();
   max_sr_th = MAX_SR_TH * car_params.getSteerRatio();
   alpha1 = 0.01 * learning_rate;
-  alpha2 = 0.0001 * learning_rate;
+  alpha2 = 0.0005 * learning_rate;
   alpha3 = 0.1 * learning_rate;
   alpha4 = 1.0 * learning_rate;
 }
