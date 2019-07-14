cd5991b5a0796120c3d5bd09eb85e4aea973dae8
Gernby <gernby2002@yahoo.com>
2019-07-14 16:14:24 -0500

    reverted

--- selfdrive/locationd/params_learner.cc
+++ selfdrive/locationd/params_learner.cc
@@ -43,8 +43,8 @@ ParamsLearner::ParamsLearner(cereal::CarParams::Reader car_params,
 
 bool ParamsLearner::update(double psi, double u, double sa) {
   if (u > 10.0 && fabs(sa) < (DEGREES_TO_RADIANS * 15.)) {
-    double ao_diff = 0.0; // 2.0*cF0*cR0*l*u*x*(1.0*cF0*cR0*l*u*x*(ao - sa) + psi*sR*(cF0*cR0*pow(l, 2)*x - m*pow(u, 2)*(aF*cF0 - aR*cR0)))/(pow(sR, 2)*pow(cF0*cR0*pow(l, 2)*x - m*pow(u, 2)*(aF*cF0 - aR*cR0), 2));
-    double new_ao = 0.0; //ao - alpha1 * ao_diff;
+    double ao_diff = 2.0*cF0*cR0*l*u*x*(1.0*cF0*cR0*l*u*x*(ao - sa) + psi*sR*(cF0*cR0*pow(l, 2)*x - m*pow(u, 2)*(aF*cF0 - aR*cR0)))/(pow(sR, 2)*pow(cF0*cR0*pow(l, 2)*x - m*pow(u, 2)*(aF*cF0 - aR*cR0), 2));
+    double new_ao = ao - alpha1 * ao_diff;
 
     double slow_ao_diff = 2.0*cF0*cR0*l*u*x*(1.0*cF0*cR0*l*u*x*(slow_ao - sa) + psi*sR*(cF0*cR0*pow(l, 2)*x - m*pow(u, 2)*(aF*cF0 - aR*cR0)))/(pow(sR, 2)*pow(cF0*cR0*pow(l, 2)*x - m*pow(u, 2)*(aF*cF0 - aR*cR0), 2));
     double new_slow_ao = slow_ao - alpha2 * slow_ao_diff;
