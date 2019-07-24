674cb0ce6b50e66d121eb4371e3e30f50a391fc8
Greg Esmond <gernby2002@yahoo.com>
2019-07-24 16:03:48 -0500

    updated interfaces

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -58,8 +58,8 @@ class CarInterface(object):
     ret.lateralTuning.pid.rateFFGain = 0.4
     ret.lateralTuning.pid.polyFactor = 0.0015
     ret.lateralTuning.pid.polyDampTime = 0.2
-    ret.lateralTuning.pid.polyReactTime = 0.5
-    ret.lateralTuning.pid.polyScale = [[0.0, 0.5, 1.0, 2.0, 5.0], [1.0, 0.5, 0.25, 0.1, 0.0], [1.0, 1.0, 1.0, 1.0, 1.0]]  # [abs rate, scale UP, scale DOWN]
+    ret.lateralTuning.pid.polyReactTime = 1.0
+    ret.lateralTuning.pid.polyScale = [[0.0, 0.5, 1.0, 2.0, 5.0], [1.0, 0.5, 0.25, 0.2, 0.15], [1.0, 1.0, 1.0, 1.0, 1.0]]  # [abs rate, scale UP, scale DOWN]
 
     if candidate in [CAR.IMPREZA]:
       ret.mass = 1568. + STD_CARGO_KG
