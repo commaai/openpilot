cbbb64f873d0ca09d55ebc30bcb30ca5d7e95982
Greg Esmond <gernby2002@yahoo.com>
2019-07-29 11:50:49 -0500

    improved Racer's path

--- selfdrive/controls/lib/model_parser.py
+++ selfdrive/controls/lib/model_parser.py
@@ -26,7 +26,8 @@ class ModelParser(object):
     if len(md.leftLane.poly):
       l_poly = np.array(md.leftLane.poly)
       r_poly = np.array(md.rightLane.poly)
-      p_poly = np.array(md.path.poly)
+      #p_poly = np.array(md.path.poly)
+      p_poly = np.array([0., 0., 0., 0.])
     else:
       l_poly = model_polyfit(md.leftLane.points, self._path_pinv)  # left line
       r_poly = model_polyfit(md.rightLane.points, self._path_pinv)  # right line
@@ -48,34 +49,39 @@ class ModelParser(object):
     self.lane_width_estimate += 0.005 * decay_rate * (current_lane_width - self.lane_width_estimate)
     speed_lane_width = interp(v_ego, [0., 31.], [2.9, 3.5])
     self.lane_width = self.lane_width_certainty * self.lane_width_estimate + (1.0 - self.lane_width_certainty) * speed_lane_width
-    lane_prob = 1.0 #np.interp(self.lane_width - current_lane_width, [0.0, 0.3], [0.0, 1.0])
 
     half_lane_width = self.lane_width / 2.0
-    l_center = lane_prob * l_prob * (l_poly[3] - half_lane_width)
-    r_center = lane_prob * r_prob * (r_poly[3] + half_lane_width)
-    p_prob = (self.c_prob**2 + self.p_prob**2) / (self.c_prob + self.p_prob + 0.0001)
+    l_center = l_prob * (l_poly[3] - half_lane_width)
+    r_center = r_prob * (r_poly[3] + half_lane_width)
+    p_prob = 0.0001 + (self.c_prob**2 + self.p_prob**2) / (self.c_prob + self.p_prob + 0.0001)
 
-    if l_center < 0 or r_center > 0:
-      lp_poly = (lane_prob * l_poly * l_prob + self.p_poly * p_prob) / (lane_prob * l_prob + p_prob + 0.0001)
-      rp_poly = (lane_prob * r_poly * r_prob + self.p_poly * p_prob) / (lane_prob * r_prob + p_prob + 0.0001)
+    self.p_poly[3] = np.clip(0.0, self.p_poly[3] - 0.005, self.p_poly[3] + 0.005)
+    self.p_poly[2] = np.clip(0.0, self.p_poly[2] - 0.001, self.p_poly[2] + 0.001)
+    self.p_poly[1] = np.clip(0.0, self.p_poly[1] - 0.0001, self.p_poly[1] + 0.0001)
+    #self.p_poly[0] = np.clip(0.0, self.p_poly[0] - 0.00001, self.p_poly[0] + 0.00001)
+
+    if l_center < 0.0 or r_center > 0.0:
       if l_center > -r_center:
         p_poly[3] = (r_center + p_prob * self.p_poly[3]) / (r_prob + p_prob + 0.0001)
       else:
         p_poly[3] = (l_center + p_prob * self.p_poly[3]) / (l_prob + p_prob + 0.0001)
 
-      if self.d_poly[1] < 0:
-        p_poly[2] = min(lp_poly[2], rp_poly[2])
-        p_poly[1] = min(lp_poly[1], rp_poly[1])
-        p_poly[0] = min(lp_poly[0], rp_poly[0])
+      race_line_adjust = np.interp(abs(p_poly[3]), [0.0, 0.3], [0.0, 1.0])
+      l_race_poly = (race_line_adjust * l_poly * l_prob + p_prob * self.p_poly) / (race_line_adjust * l_prob + p_prob + 0.0001)
+      r_race_poly = (race_line_adjust * r_poly * r_prob + p_prob * self.p_poly) / (race_line_adjust * r_prob + p_prob + 0.0001)
+      if self.d_poly[1] < 0.0:
+        p_poly[2] = min(l_race_poly[2], r_race_poly[2], self.p_poly[2])
+        p_poly[1] = min(l_race_poly[1], r_race_poly[1], self.d_poly[1])
+        #p_poly[0] = min(l_race_poly[0], r_race_poly[0], self.d_poly[0])
       else:
-        p_poly[2] = max(lp_poly[2], rp_poly[2])
-        p_poly[1] = max(lp_poly[1], rp_poly[1])
-        p_poly[0] = max(lp_poly[0], rp_poly[0])
+        p_poly[2] = max(l_race_poly[2], r_race_poly[2], self.p_poly[2])
+        p_poly[1] = max(l_race_poly[1], r_race_poly[1], self.d_poly[1])
+        #p_poly[0] = max(l_race_poly[0], r_race_poly[0], self.d_poly[0])
     else:
-      p_poly[3] = np.clip(0.0, self.p_poly[3] - 0.005, self.p_poly[3] + 0.005)
-      p_poly[2] = np.clip(0.0, self.p_poly[2] - 0.0005, self.p_poly[2] + 0.00051)
+      p_poly[3] = self.p_poly[3]
+      p_poly[2] = self.p_poly[2]
       p_poly[1] = self.d_poly[1]
-      p_poly[0] = self.d_poly[0]
+      #p_poly[0] = self.d_poly[0]
 
     self.lead_dist = md.lead.dist
     self.lead_prob = md.lead.prob
