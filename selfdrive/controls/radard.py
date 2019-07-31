5a374de9dae8347b87d66fcc7702f71409205462
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2019-07-30 18:01:36 -0700

    mock fix

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import gc
 import numpy as np
 import numpy.matlib
 import importlib
@@ -165,12 +166,13 @@ class RadarD(object):
 
 # fuses camera and radar data for best lead detection
 def radard_thread(gctx=None):
+  gc.disable()
   set_realtime_priority(2)
 
   # wait for stats about the car to come in from controls
   cloudlog.info("radard is waiting for CarParams")
   CP = car.CarParams.from_bytes(Params().get("CarParams", block=True))
-  mocked = CP.carName == "mock"
+  mocked = CP.carName == "mock" or CP.carName == "honda"
   cloudlog.info("radard got CarParams")
 
   # import the radar from the fingerprint
