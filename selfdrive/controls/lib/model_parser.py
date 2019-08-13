c4bf23e386eeaa9e06c34360afe8998b0d2706d7
shahzadsarwar-ctl <38666331+shahzadsarwar-ctl@users.noreply.github.com>
2019-08-12 18:12:01 -0600

    exit diving fix.

--- selfdrive/controls/lib/model_parser.py
+++ selfdrive/controls/lib/model_parser.py
@@ -2,24 +2,23 @@ from common.numpy_fast import interp
 import numpy as np
 from selfdrive.controls.lib.latcontrol_helpers import model_polyfit, calc_desired_path, compute_path_pinv
 
-CAMERA_OFFSET = 0.06  # m from center car to camera
+CAMERA_OFFSET = 0.07  # m from center car to camera
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
 
 class ModelParser(object):
   def __init__(self):
     self.d_poly = [0., 0., 0., 0.]
     self.c_poly = [0., 0., 0., 0.]
-    self.l_poly = np.array([0., 0., 0., 0.])
-    self.r_poly = np.array([0., 0., 0., 0.])
-    self.p_poly = np.array([0., 0., 0., 0.])
     self.c_prob = 0.
-    self.p_prob = 0.
     self.last_model = 0.
     self.lead_dist, self.lead_prob, self.lead_var = 0, 0, 1
     self._path_pinv = compute_path_pinv()
 
-    self.lane_width_estimate = 3.0
-    self.lane_width_certainty = 1.0
     self.lane_width = 3.0
+    self.readings = []
+    self.frame = 0
     self.l_prob = 0.
     self.r_prob = 0.
     self.x_points = np.arange(50)
@@ -28,8 +27,7 @@ class ModelParser(object):
     if len(md.leftLane.poly):
       l_poly = np.array(md.leftLane.poly)
       r_poly = np.array(md.rightLane.poly)
-      #p_poly = np.array(md.path.poly)
-      p_poly = np.array([0., 0., 0., 0.])
+      p_poly = np.array(md.path.poly)
     else:
       l_poly = model_polyfit(md.leftLane.points, self._path_pinv)  # left line
       r_poly = model_polyfit(md.rightLane.points, self._path_pinv)  # right line
@@ -39,57 +37,26 @@ class ModelParser(object):
     l_poly[3] += CAMERA_OFFSET
     r_poly[3] += CAMERA_OFFSET
 
+    p_prob = 1.  # model does not tell this probability yet, so set to 1 for now
     l_prob = md.leftLane.prob  # left line prob
     r_prob = md.rightLane.prob  # right line prob
 
-    if self.l_prob > l_prob:
-      l_poly = (l_poly * l_prob + min(1.0 - l_prob, self.l_prob) * self.l_poly) / (l_prob + min(1.0 - l_prob, self.l_prob) + 0.0001)
-      l_prob = (l_prob**2 + min(1.0 - l_prob, 0.9 * self.l_prob) * self.l_prob) / (l_prob + min(1 - l_prob, 0.9 * self.l_prob) + 0.0001)
-    if self.r_prob > r_prob:
-      r_poly = (r_poly * (r_prob) + min(1.0 - r_prob, self.r_prob) * self.r_poly) / (r_prob + min(1.0 - r_prob, self.r_prob) + 0.0001)
-      r_prob = (r_prob**2 + min(1.0 - r_prob, 0.9 * self.r_prob) * self.r_prob) / (r_prob + min(1.0 - r_prob, 0.9 * self.r_prob) + 0.0001)
-
     # Find current lanewidth
-    lr_prob = l_prob * r_prob
-    self.lane_width_certainty += 0.05 * (lr_prob - self.lane_width_certainty)
-    current_lane_width = abs(l_poly[3] - r_poly[3])
-    self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
-    speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
-    self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
-                      (1 - self.lane_width_certainty) * speed_lane_width
+    if l_prob > 0.49 and r_prob > 0.49:
+        self.frame += 1
+        if self.frame % 20 == 0:
+            self.frame = 0
+            current_lane_width = sorted((2.8, abs(l_poly[3] - r_poly[3]), 3.6))[1]
+            max_samples = 30
+            self.readings.append(current_lane_width)
+            avg = mean(self.readings)
+            self.lane_width = avg
+            if len(self.readings) == max_samples:
+                self.readings.pop(0)
 
-    half_lane_width = self.lane_width / 2.0
-    l_center = l_prob * (l_poly[3] - half_lane_width)
-    r_center = r_prob * (r_poly[3] + half_lane_width)
-    p_prob = 0.0001 + (self.c_prob**2 + self.p_prob**2) / (self.c_prob + self.p_prob + 0.0001)
-
-    self.p_poly[3] = np.clip(0.0, self.p_poly[3] - 0.005, self.p_poly[3] + 0.005)
-    self.p_poly[2] = np.clip(0.0, self.p_poly[2] - 0.001, self.p_poly[2] + 0.001)
-    self.p_poly[1] = np.clip(0.0, self.p_poly[1] - 0.0001, self.p_poly[1] + 0.0001)
-    #self.p_poly[0] = np.clip(0.0, self.p_poly[0] - 0.00001, self.p_poly[0] + 0.00001)
-
-    if l_center < 0.0 or r_center > 0.0:
-      if l_center > -r_center:
-        p_poly[3] = (r_center + p_prob * self.p_poly[3]) / (r_prob + p_prob + 0.0001)
-      else:
-        p_poly[3] = (l_center + p_prob * self.p_poly[3]) / (l_prob + p_prob + 0.0001)
-
-      race_line_adjust = np.interp(abs(p_poly[3]), [0.0, 0.3], [0.0, 1.0])
-      l_race_poly = (race_line_adjust * l_poly * l_prob + p_prob * self.p_poly) / (race_line_adjust * l_prob + p_prob + 0.0001)
-      r_race_poly = (race_line_adjust * r_poly * r_prob + p_prob * self.p_poly) / (race_line_adjust * r_prob + p_prob + 0.0001)
-      if self.d_poly[1] < 0.0:
-        p_poly[2] = min(l_race_poly[2], r_race_poly[2], self.p_poly[2])
-        p_poly[1] = min(l_race_poly[1], r_race_poly[1], self.d_poly[1])
-        #p_poly[0] = min(l_race_poly[0], r_race_poly[0], self.d_poly[0])
-      else:
-        p_poly[2] = max(l_race_poly[2], r_race_poly[2], self.p_poly[2])
-        p_poly[1] = max(l_race_poly[1], r_race_poly[1], self.d_poly[1])
-        #p_poly[0] = max(l_race_poly[0], r_race_poly[0], self.d_poly[0])
-    else:
-      p_poly[3] = self.p_poly[3]
-      p_poly[2] = self.p_poly[2]
-      p_poly[1] = self.d_poly[1]
-      #p_poly[0] = self.d_poly[0]
+    # Don't exit dive
+    if abs(l_poly[3] - r_poly[3]) > self.lane_width:
+        r_prob = r_prob / interp(l_prob, [0, 1], [1, 3])
 
     self.lead_dist = md.lead.dist
     self.lead_prob = md.lead.prob
@@ -107,3 +74,4 @@ class ModelParser(object):
 
     self.p_poly = p_poly
     self.p_prob = p_prob
+    
