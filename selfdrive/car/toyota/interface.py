99f0a1d9bdb4af0dde31ef4ef4c0ad0fafa73ae0
Mark Mendoza <markolo25@gmail.com>
2019-08-18 12:01:00 -0700

    enabled lqr for corolla hybrid

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -194,8 +194,17 @@ class CarInterface(object):
       ret.steerRatio = 13.9
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 3050. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
-      ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.init('lqr')
+      ret.lateralTuning.lqr.scale = 1500.0
+      ret.lateralTuning.lqr.ki = 0.01
+
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+      ret.lateralTuning.lqr.l = [0.03233671, 0.03185757]
+      ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+
 
     elif candidate == CAR.LEXUS_ESH_TSS2:
       stop_and_go = True
