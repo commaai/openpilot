703abc749f08838da3adc870c2fd7e8862ac0bbc
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-10-04 13:25:01 +0300

    bug

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -252,7 +252,7 @@ class CarState(object):
         self.gear_tcu = "park"
       elif gear == 14:
         self.gear_tcu = "reverse"
-      elif gear > 0 and gear2 < 9:    # unaware of anything over 8 currently
+      elif gear > 0 and gear < 9:    # unaware of anything over 8 currently
         self.gear_tcu = "drive"
       else:
         self.gear_tcu = "unknown"
