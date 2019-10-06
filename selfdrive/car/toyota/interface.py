8afbe60a03c8b82f947122baac3231496290f19c
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2019-10-06 16:40:09 +0200

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -230,10 +230,10 @@ class CarInterface(object):
       ret.lateralTuning.pid.kf = 0.00004
       ret.lateralTuning.init('lqr')
 
-       ret.lateralTuning.lqr.scale = 1500.0
+      ret.lateralTuning.lqr.scale = 1500.0
       ret.lateralTuning.lqr.ki = 0.05
 
-       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
       ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
