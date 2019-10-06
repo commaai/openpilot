c7d5e0fa3ec3c677cb9ee8937603f965ada1bf03
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2019-10-06 16:00:03 +0200

    Update interface.py
    add OC

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -58,7 +58,7 @@ class CarInterface(object):
 
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
 
-    if candidate not in [CAR.PRIUS, CAR.RAV4, CAR.RAV4H]: # These cars use LQR/INDI
+    if candidate not in [CAR.PRIUS, CAR.RAV4, CAR.RAV4H, CAR.OLD_CAR]: # These cars use LQR/INDI
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
 
@@ -219,8 +219,32 @@ class CarInterface(object):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.05]]
       ret.lateralTuning.pid.kf = 0.00006
 
+     elif candidate == CAR.OLD_CAR:
+      stop_and_go = False
+      ret.safetyParam = 100
+      ret.wheelbase = 2.455
+      ret.steerRatio = 17.
+      tire_stiffness_factor = 0.444
+      ret.mass = 6200.0 * CV.LB_TO_KG + STD_CARGO_KG
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.1], [0.04]]
+      ret.lateralTuning.pid.kf = 0.00004
+      ret.lateralTuning.init('lqr')
+
+       ret.lateralTuning.lqr.scale = 1500.0
+      ret.lateralTuning.lqr.ki = 0.05
+
+       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+      ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
+      ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+
     ret.steerRateCost = 1.
-    ret.centerToFront = ret.wheelbase * 0.44
+    if candidate == CAR.OLD_CAR:
+      ret.centerToFront = ret.wheelbase * 0.5
+    else:
+      ret.centerToFront = ret.wheelbase * 0.44
 
     #detect the Pedal address
     ret.enableGasInterceptor = 0x201 in fingerprint
