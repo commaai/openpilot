1108bb9f8758ed8129754694f7427ac2fc063dc8
u8511049 <54995625+u8511049@users.noreply.github.com>
2019-10-20 17:16:27 +0800

    add supported car for 2019 toyota altis hybrid

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -185,6 +185,16 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
       ret.lateralTuning.pid.kf = 0.00007818594
+      
+    elif candidate == CAR.ALTISH_TSS2:
+      stop_and_go = True
+      ret.safetyParam = 73
+      ret.wheelbase = 2.63906
+      ret.steerRatio = 13.9
+      tire_stiffness_factor = 0.444  # not optimized yet
+      ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
+      ret.lateralTuning.pid.kf = 0.00007818594
 
     elif candidate == CAR.LEXUS_ESH_TSS2:
       stop_and_go = True
