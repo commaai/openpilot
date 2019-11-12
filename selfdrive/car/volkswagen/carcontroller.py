79fc3ad342b6196b869c11b3d332dbf4bea4ec32
jyoung8607 <jyoung8607@gmail.com>
2019-11-12 16:32:39 -0500

    Fixes for various merge and refactoring fallout, typo fixes.

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -181,7 +181,7 @@ class CarController():
         idx = (CS.graMsgBusCounter + 1) % 16
         can_sends.append(volkswagencan.create_mqb_acc_buttons_control(self.packer_gw, canbus.extended, self.graButtonStatesToSend, CS, idx))
         self.graMsgSentCount += 1
-        if self.graMsgSentCount >= 16:
+        if self.graMsgSentCount >= P.GRA_VBP_COUNT:
           self.graButtonStatesToSend = None
           self.graMsgSentCount = 0
 
