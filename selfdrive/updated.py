0b53fe768f22be8b95c334ae8671d6a50e990b0a
Jason Young <jyoung8607@gmail.com>
2019-12-09 11:39:37 -0500

    Test commit noise

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -243,6 +243,6 @@ def main(gctx=None):
     wait_between_updates()
 
 if __name__ == "__main__":
-  # Commit noise to test updates 9283597823atrsfdw32224323e1y
+  # Commit noise to test updates 9283597823atrsfdw324323e1y
   main()
 
