683b6151ce59def5c299fa4b9ba5f70b14b85cb7
Vehicle Researcher <user@comma.ai>
2019-12-13 13:02:48 -0800

    Squashed 'opendbc/' changes from c0eba096..4f82d01e
    4f82d01e gitignore
    5cb83454 Honda FCM: diagnostic signals
    d309cdce Added linter to opendbc (#203)
    d452706f add requirements.txt
    ec3b4595 deterministic dependency order
    a265d351 Azure pipelines ci (#202)
    bce9a2e1 packer depends on libdbc
    5d5fdd6a no more python version of libdbc, everything through cython
    541705bf move CANDefine to parser code
    da25c52a add test for can define
    0ba7926b unify can packer and parser
    25d88009 consistent naming
    a5c640a5 fix linter
    be210fef remove obsolete make file
    ffd9dca7 opendbc needs cereal
    b559f63d remove more make
    d0929496 seems to work now
    41e80836 don't make
    3254d1fc think scons works
    eb78f6aa scons sort of working
    0ef1e35d fix gitignore
    e155e017 Can migration (#199)
    3eded83a Honda: correct steering torque sensor sign to be consistent with standard convention (left+)
    32f70e2f Fix outback endianness consistency (#196)
    a7da471f Update subaru_outback_2015_eyesight.dbc (#195)
    
    git-subtree-dir: opendbc
    git-subtree-split: 4f82d01ebc78109888954d9807d320e3c27896fd

---
+++ can/packer_pyx_setup.py
@@ -0,0 +1,60 @@
+import os
+import sysconfig
+import subprocess
+from distutils.core import Extension, setup  # pylint: disable=import-error,no-name-in-module
+
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+BASEDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
+
+
+def get_ext_filename_without_platform_suffix(filename):
+  name, ext = os.path.splitext(filename)
+  ext_suffix = sysconfig.get_config_var('EXT_SUFFIX')
+
+  if ext_suffix == ext:
+    return filename
+
+  ext_suffix = ext_suffix.replace(ext, '')
+  idx = name.find(ext_suffix)
+
+  if idx == -1:
+    return filename
+  else:
+    return name[:idx] + ext
+
+
+class BuildExtWithoutPlatformSuffix(build_ext):
+  def get_ext_filename(self, ext_name):
+    filename = super().get_ext_filename(ext_name)
+    return get_ext_filename_without_platform_suffix(filename)
+
+
+sourcefiles = ['packer_pyx.pyx']
+extra_compile_args = ["-std=c++11"]
+ARCH = subprocess.check_output(["uname", "-m"], encoding='utf8').rstrip()  # pylint: disable=unexpected-keyword-arg
+
+if ARCH == "aarch64":
+  extra_compile_args += ["-Wno-deprecated-register"]
+
+
+setup(name='CAN packer',
+      cmdclass={'build_ext': BuildExtWithoutPlatformSuffix},
+      ext_modules=cythonize(
+        Extension(
+          "packer_pyx",
+          language="c++",
+          sources=sourcefiles,
+          extra_compile_args=extra_compile_args,
+          include_dirs=[
+            BASEDIR,
+            os.path.join(BASEDIR, 'phonelibs', 'capnp-cpp/include'),
+          ],
+          extra_link_args=[
+            os.path.join(BASEDIR, 'opendbc', 'can', 'libdbc.so'),
+          ],
+        )
+      ),
+      nthreads=4,
+)
