59e83e4c27b3900572da99011b9b2ce6c039704a
Vehicle Researcher <user@comma.ai>
2019-12-13 13:02:45 -0800

    Squashed 'cereal/' changes from 90e48c54..b8382bbb
    b8382bbb steerLimitTimer should be car dependent
    9a229687 add pa0 temp to ThermalData
    f6f0f60e Add stock Fcw to carState
    b608683f no l/r distinction for LDW
    555f48d6 Add ldw alert
    8e8b4a4a Remove plusFrame socket in favor of UiLayoutState
    3410325c log stock AEB events
    2219f2bd Add warning about not using cython version of sec_since_boot
    8f1a5122 for legacy-testing reasons, better to define the used percent instead of avail
    e86d9545 adding low memory event
    ad238340 remove TODO
    d0962b34 log mem available and cpu perc in thermald
    3b753be9 Implement error handling and exceptions (#18)
    a7d5bb76 add explicit dependencies on services.h
    1ba64677 fix linter
    c7d215b6 Added communityFeatureDisallowed event
    492140a5 Added communityFeature bit detection to CarParams
    266a5fed log Panda fault types
    347a8661 Switch from polling on FIFOs to signal (#12)
    e25bba77 no need to double build the objects
    fe43a994 20Hz for radar time step is very standard
    2aabf1ee Added radar time step to car params
    e8ae9086 Generate capnp for java
    57126a23 cereal_shared
    da655cd3 Add uptime to health
    f6a8e394 add test with multiple subscribers
    84b3af53 comment out the debugging
    4b9c942a added power save state to health packet
    66be3708 run python unittest in ci
    52c6db87 Run scons in CI (#14)
    9414615b do need it, but only for arm
    2856c37c remove gnustl_shared
    7f05ee64 fix apks
    e3a6bded Revert "no more makefiles"
    487fbd06 don't rely on BASEDIR, and add zmq library
    223e37a5 no more makefiles
    da2ed115 don't link the wrong one
    fe9fe2a2 scons builds the python lib now
    2f81135e err, it can't build services.h
    57b03f8b now we shouldn't need that yaml crap everywhere
    f8e53277 bridge builds with services.h
    2b0cb608 noOutput safety mode is now called silent
    83880d51 add msgq tests
    bcad1848 msgq: dont block when fifo does not exists
    b4b26782 Default to zmq
    473e2912 fix compilation in docker
    30aaaddc msgq: try again when no timeout on poll but also no message
    c4f2ad53 msgq: make sure read_fifos is initalized so we dont close random fds
    4e513a85 msgq: dont clean up uninitialized sockerts
    c008b630 also remove the fifo from disk
    ef64eb27 MSGQ stability improvements when opening and closing lots of queues
    e147abcc Revert "Revert "deprecate irpwr""
    932dc32e Revert "deprecate irpwr"
    a6844150 disengage
    ec27e18c capnpc also generated the header files
    ee52ab9e deprecate irpwr
    301c74c8 Merge branch 'master' of github.com:commaai/cereal
    6da7d55a add front frame
    a5944eb4 add conflate parameter for SubSocket::create
    ca8df170 Add fault status to health
    ef4ded06 add conflate support in SubSocket constructor
    7fd314af update scons build file
    93d814e4 add saturated flags to indi and lqr logs
    50302fee add steeringRateLimited to car.capnp
    05e3513d add msgq readme
    a6759a95 faster make
    94b73778 Add struct to log FW version
    64ce0b5f add scons build
    dc9ad18a add debug print statement on SIGINT
    4a612698 Merge pull request #10 from commaai/msgq
    4873449a use recv one or none after poll
    a054864b default to msgq
    fbc4a4cf oops bad number
    5067cf4c add meta
    cbd02865 fix export prefix and make shared library world readable
    c2730541 add c exports for jni usage
    e77f41ef zmq already sets the errno correctly
    3196cf69 Fix service list path in bridge
    d35515a2 add all msgq files, but dont use as default
    a68a38fa Don't delete context from python side only
    bd46c225 Revert "zmq_ctx_term is blocking"
    a1fc26b8 zmq_ctx_term is blocking
    09021820 remote address support
    21a35361 only delete subsocket when created by same object
    34df7351 remove extra underscore from __dealloc__
    c8748f86 fix internal refs
    79b2fbf7 fixups
    23ad2563 import messaging and services
    
    git-subtree-dir: cereal
    git-subtree-split: b8382bbb2b8156f2f1d7e1c1b42b46c54d85761f
    
    old-commit-hash: e3b21173a5ed37deb8a81a0b5124c979d821416a

---
+++ messaging/messaging_pyx.pyx
@@ -0,0 +1,151 @@
+# distutils: language = c++
+# cython: c_string_encoding=ascii, language_level=3
+
+import sys
+from libcpp.string cimport string
+from libcpp cimport bool
+from libc cimport errno
+
+
+from messaging cimport Context as cppContext
+from messaging cimport SubSocket as cppSubSocket
+from messaging cimport PubSocket as cppPubSocket
+from messaging cimport Poller as cppPoller
+from messaging cimport Message as cppMessage
+
+
+class MessagingError(Exception):
+  pass
+
+
+class MultiplePublishersError(MessagingError):
+  pass
+
+
+cdef class Context:
+  cdef cppContext * context
+
+  def __cinit__(self):
+    self.context = cppContext.create()
+
+  def term(self):
+    del self.context
+    self.context = NULL
+
+  def __dealloc__(self):
+    pass
+    # Deleting the context will hang if sockets are still active
+    # TODO: Figure out a way to make sure the context is closed last
+    # del self.context
+
+
+cdef class Poller:
+  cdef cppPoller * poller
+  cdef list sub_sockets
+
+  def __cinit__(self):
+    self.sub_sockets = []
+    self.poller = cppPoller.create()
+
+  def __dealloc__(self):
+    del self.poller
+
+  def registerSocket(self, SubSocket socket):
+    self.sub_sockets.append(socket)
+    self.poller.registerSocket(socket.socket)
+
+  def poll(self, timeout):
+    sockets = []
+    cdef int t = timeout
+
+    with nogil:
+        result = self.poller.poll(t)
+
+    for s in result:
+        socket = SubSocket()
+        socket.setPtr(s)
+        sockets.append(socket)
+
+    return sockets
+
+cdef class SubSocket:
+  cdef cppSubSocket * socket
+  cdef bool is_owner
+
+  def __cinit__(self):
+    self.socket = cppSubSocket.create()
+    self.is_owner = True
+
+    if self.socket == NULL:
+      raise MessagingError
+
+  def __dealloc__(self):
+    if self.is_owner:
+      del self.socket
+
+  cdef setPtr(self, cppSubSocket * ptr):
+    if self.is_owner:
+      del self.socket
+
+    self.is_owner = False
+    self.socket = ptr
+
+  def connect(self, Context context, string endpoint, string address=b"127.0.0.1", bool conflate=False):
+    r = self.socket.connect(context.context, endpoint, address, conflate)
+
+    if r != 0:
+      if errno.errno == errno.EADDRINUSE:
+        raise MultiplePublishersError
+      else:
+        raise MessagingError
+
+  def setTimeout(self, int timeout):
+    self.socket.setTimeout(timeout)
+
+  def receive(self, bool non_blocking=False):
+    msg = self.socket.receive(non_blocking)
+
+    if msg == NULL:
+      # If a blocking read returns no message check errno if SIGINT was caught in the C++ code
+      if errno.errno == errno.EINTR:
+        print("SIGINT received, exiting")
+        sys.exit(1)
+
+      return None
+    else:
+      sz = msg.getSize()
+      m = msg.getData()[:sz]
+      del msg
+
+      return m
+
+
+cdef class PubSocket:
+  cdef cppPubSocket * socket
+
+  def __cinit__(self):
+    self.socket = cppPubSocket.create()
+    if self.socket == NULL:
+      raise MessagingError
+
+  def __dealloc__(self):
+    del self.socket
+
+  def connect(self, Context context, string endpoint):
+    r = self.socket.connect(context.context, endpoint)
+
+    if r != 0:
+      if errno.errno == errno.EADDRINUSE:
+        raise MultiplePublishersError
+      else:
+        raise MessagingError
+
+  def send(self, string data):
+    length = len(data)
+    r = self.socket.send(<char*>data.c_str(), length)
+
+    if r != length:
+      if errno.errno == errno.EADDRINUSE:
+        raise MultiplePublishersError
+      else:
+        raise MessagingError
