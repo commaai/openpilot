577c551ef1881dc2c205b6eee0296a675476ee5c
Jason Young <jyoung8607@gmail.com>
2019-12-17 08:25:32 -0500

    Test update process

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -272,5 +272,5 @@ def main(gctx=None):
     wait_between_updates()
 
 if __name__ == "__main__":
-  # Commit noise to test updates 22aiwu4ytiuawyeihas;oedfhjas983742
+  # Commit noise to test updates 22a98471092387401928740921983742
   main()
