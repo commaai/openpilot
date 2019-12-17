c577067c601f887291e2eef834a613f74ff778d9
Jason Young <jyoung8607@gmail.com>
2019-12-17 08:02:50 -0500

    Test update process

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -274,5 +274,5 @@ def main(gctx=None):
 
 
 if __name__ == "__main__":
-  # Commit noise to test updates 9283123abvdbasjdlhfhgasdfjgyhsaasdf37893
+  # Commit noise to test updates 9283asda sasdasdfad gyhsaasdf37893
   main()
