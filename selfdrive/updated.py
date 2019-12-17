0c053d91128a28288ad8398496d38d575f9a8a29
Jason Young <jyoung8607@gmail.com>
2019-12-17 12:09:53 -0500

    Test update process

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -288,5 +288,5 @@ def main(gctx=None):
   dismount_ovfs()
 
 if __name__ == "__main__":
-  # Commit noise to test updates 22599123412341234123413241232or2
+  # Commit noise to test updates 22599141230740192834232or2
   main()
