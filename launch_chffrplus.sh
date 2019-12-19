2817e3faf0c693837c21dada3a681acccfcc14ff
Willem Melching <willem.melching@gmail.com>
2019-12-18 20:45:10 -0800

    update launch script

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -6,13 +6,49 @@ export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
+if [ -z "$BASEDIR" ]; then
+  BASEDIR="/data/openpilot"
+fi
+
 if [ -z "$PASSIVE" ]; then
   export PASSIVE="1"
 fi
 
+STAGING_ROOT="/data/safe_staging"
+
 function launch {
   # Wifi scan
   wpa_cli IFNAME=wlan0 SCAN
+
+  # Check to see if there's a valid overlay-based update available. Conditions
+  # are as follows:
+  #
+  # 1. The BASEDIR init file has to exist, with a newer modtime than anything in
+  #    the BASEDIR Git repo. This checks for local development work or the user
+  #    switching branches/forks, which should not be overwritten.
+  # 2. The FINALIZED consistent file has to exist, indicating there's an update
+  #    that completed successfully and synced to disk.
+
+  if [ -f "${BASEDIR}/.overlay_init" ]; then
+    find ${BASEDIR}/.git -newer ${BASEDIR}/.overlay_init | grep -q '.' 2> /dev/null
+    if [ $? -eq 0 ]; then
+      echo "${BASEDIR} has been modified, skipping overlay update installation"
+    else
+      if [ -f "${STAGING_ROOT}/finalized/.overlay_consistent" ]; then
+        echo "Valid overlay update found, installing"
+        LAUNCHER_LOCATION="${BASH_SOURCE[0]}"
+
+        mv $BASEDIR /data/safe_staging/old_openpilot
+        mv "${STAGING_ROOT}/finalized" $BASEDIR
+
+        # The mv changed our working directory to /data/safe_staging/old_openpilot
+        cd "${BASEDIR}"
+
+        echo "Restarting launch script ${LAUNCHER_LOCATION}"
+        exec "${LAUNCHER_LOCATION}"
+      fi
+    fi
+  fi
 
   # no cpu rationing for now
   echo 0-3 > /dev/cpuset/background/cpus
