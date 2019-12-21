2dd5cf9fd9f537f886bc5beadabbdcaee6020459
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-12-21 20:49:38 +0300

    add lkas button signal

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -396,6 +396,7 @@ class CarState():
       else:
         self.gear_shifter = GearShifter.unknown
 
+    self.lkas_button_on = 7 > cp_cam.vl["LKAS11"]["CF_Lkas_LdwsSysState"] != 0
 
     # save the entire LKAS11, CLU11, SCC12 and MDPS12
     self.lkas11 = cp_cam.vl["LKAS11"]
