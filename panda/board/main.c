2a29cf2b04e5c606949d82a6ceeff7e7182504de
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-12-21 11:13:04 +0300

    set SAFETY_ALLOUTPUT as default,no sleep

--- panda/board/main.c
+++ panda/board/main.c
@@ -714,12 +714,13 @@ void TIM1_BRK_TIM9_IRQ_Handler(void) {
       puts("EON hasn't sent a heartbeat for 0x");
       puth(heartbeat_counter);
       puts(" seconds. Safety is set to SILENT mode.\n");
-      if (current_safety_mode != SAFETY_SILENT) {
-        set_safety_mode(SAFETY_SILENT, 0U);
+      if (current_safety_mode != SAFETY_ALLOUTPUT) {
+        set_safety_mode(SAFETY_ALLOUTPUT, 0U); // MDPS will hard if SAFETY_NOOUTPUT
       }
-      if (power_save_status != POWER_SAVE_STATUS_ENABLED) {
-        set_power_save_state(POWER_SAVE_STATUS_ENABLED);
-      }
+	 // MDPS will if panda sleep
+     // if (power_save_status != POWER_SAVE_STATUS_ENABLED) {
+     //   set_power_save_state(POWER_SAVE_STATUS_ENABLED);
+     // }
     }
 
     // enter CDP mode when car starts to ensure we are charging a turned off EON
@@ -812,7 +813,7 @@ int main(void) {
   // use TIM2->CNT to read
 
   // init to SILENT and can silent
-  set_safety_mode(SAFETY_SILENT, 0);
+  set_safety_mode(SAFETY_ALLOUTPUT, 0); // MDPS will hard if SAFETY_NOOUTPUT
 
   // enable CAN TXs
   current_board->enable_can_transcievers(true);
