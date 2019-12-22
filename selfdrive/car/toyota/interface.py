6aa8d02c17b2368540c3f0d3e32928949d7292d1
Shane <shane@smiskol.com>
2019-12-22 15:03:53 -0600

    test different corolla long tune

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -125,11 +125,10 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 2860. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.05]]
-      ret.lateralTuning.pid.kf = 0.00003  # full torque for 20 deg at 80mph means 0.00007818594
-      # ret.lateralTuning.pid.kf = 0.00003 * 0.639  # full torque for 20 deg at 80mph means 0.00007818594
-      # if ret.enableGasInterceptor:
-      #   ret.longitudinalTuning.kpVPedal = [1.0, 0.66, 0.42]
-      #   ret.longitudinalTuning.kiVPedal = [0.135, 0.09]
+      ret.lateralTuning.pid.kf = 0.00003 * 0.639  # full torque for 20 deg at 80mph means 0.00007818594
+      if ret.enableGasInterceptor:
+        ret.longitudinalTuning.kpV = [1.0, 0.66, 0.42]
+        # ret.longitudinalTuning.kiV = [0.135, 0.09]
 
     elif candidate == CAR.LEXUS_RXH:
       stop_and_go = True
