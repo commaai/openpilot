638b7f3f90b648c49e1e43cc01e31e22a6810180
Shane <shane@smiskol.com>
2019-12-29 18:47:39 -0600

    test no integral, reduce prop

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -58,8 +58,9 @@ class CarInterface(CarInterfaceBase):
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 9., 35]
       ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.longitudinalTuning.kpV = [i * 0.9 for i in [1.2, 0.8, 0.5]]
+      # ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.longitudinalTuning.kiV = [0.0, 0.0]
     else:
       ret.gasMaxBP = [0.]
       ret.gasMaxV = [0.5]
