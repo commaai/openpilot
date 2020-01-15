ab415aa5faa3865a78006148e90ea7972d8639d1
Vehicle Researcher <user@comma.ai>
2020-01-15 14:04:40 -0800

    Remove old panda subtree

--- panda/board/get_sdk_mac.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Need formula for gcc
-brew tap ArmMbed/homebrew-formulae
-brew install python dfu-util arm-none-eabi-gcc
-pip install --user libusb1 pycrypto requests
--- panda/tests/linter_python/flake8_panda.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-RESULT=$(python3 -m flake8 --select=F $(find ../../ -type f | grep -v "/boardesp/" | grep -v "/cppcheck/" | grep "\.py$"))
-if [[ $RESULT  ]]; then
-	echo "Pyflakes found errors in the code. Please fix and try again"
-	echo "$RESULT"
-	exit 1
-fi
