a7fa20b5667a1c77cde504e49e2c4cf652a4e64e
George Hotz <geohot@gmail.com>
2020-01-17 11:03:22 -0800

    selfdrive/*.py
    old-commit-hash: 2f9379a139e8f7e479bd7baaa0a85cb872c2eb4d

---
+++ selfdrive/logmessaged.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import zmq
+from logentries import LogentriesHandler
+import cereal.messaging as messaging
+
+def main(gctx=None):
+  # setup logentries. we forward log messages to it
+  le_token = "e8549616-0798-4d7e-a2ca-2513ae81fa17"
+  le_handler = LogentriesHandler(le_token, use_tls=False, verbose=False)
+
+  le_level = 20 #logging.INFO
+
+  ctx = zmq.Context().instance()
+  sock = ctx.socket(zmq.PULL)
+  sock.bind("ipc:///tmp/logmessage")
+
+  # and we publish them
+  pub_sock = messaging.pub_sock('logMessage')
+
+  while True:
+    dat = b''.join(sock.recv_multipart())
+    dat = dat.decode('utf8')
+
+    # print "RECV", repr(dat)
+
+    levelnum = ord(dat[0])
+    dat = dat[1:]
+
+    if levelnum >= le_level:
+      # push to logentries
+      # TODO: push to athena instead
+      le_handler.emit_raw(dat)
+
+    # then we publish them
+    msg = messaging.new_message()
+    msg.logMessage = dat
+    pub_sock.send(msg.to_bytes())
+
+if __name__ == "__main__":
+  main()
