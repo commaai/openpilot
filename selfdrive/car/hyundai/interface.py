216f9824330d668e1896d980323dbffec6796e77
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-01-18 06:13:38 +0300

    ignore CAN2 if L-CAN on  the Bus

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -183,9 +183,11 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startAccel = 0.0
 
-    ret.mdpsBus = 1 if 593 in fingerprint[1] else 0
-    ret.sasBus = 1 if 688 in fingerprint[1] else 0
-    ret.sccBus = 0 if 1056 in fingerprint[0] else 1 if 1056 in fingerprint[1] else 2 if 1056 in fingerprint[2] else -1
+    # ignore CAN2 address if L-CAN on the same BUS
+    ret.mdpsBus = 1 if 593 in fingerprint[1] and 1296 not in fingerprint[1] else 0
+    ret.sasBus = 1 if 688 in fingerprint[1] and 1296 not in fingerprint[1] else 0
+    ret.sccBus = 0 if 1056 in fingerprint[0] else 1 if 1056 in fingerprint[1] and 1296 not in fingerprint[1] \
+                                                                     else 2 if 1056 in fingerprint[2] else -1
     ret.autoLcaEnabled = 0
 
     return ret
