cd2a66da2ad4fe1a2ebb51c46b4041a24c967e3b
Willem Melching <willem.melching@gmail.com>
2020-01-28 17:09:27 -0800

    Use deploy key

--- release/go.sh
+++ release/go.sh
@@ -29,7 +29,7 @@ export GIT_COMMITTER_NAME="Vehicle Researcher"
 export GIT_COMMITTER_EMAIL="user@comma.ai"
 export GIT_AUTHOR_NAME="Vehicle Researcher"
 export GIT_AUTHOR_EMAIL="user@comma.ai"
-# export GIT_SSH_COMMAND="ssh -i /tmp/deploy_key"
+export GIT_SSH_COMMAND="ssh -i /tmp/deploy_key"
 
 echo "[-] Setting up repo T=$SECONDS"
 if [ ! -d "$TARGET_DIR" ]; then
