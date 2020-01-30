a2fe62ce77dec09a515336c55b1931f7b5148239
Willem Melching <willem.melching@gmail.com>
2020-01-30 13:31:14 -0800

    Fix source brnach in master-ci push

--- release/go.sh
+++ release/go.sh
@@ -108,7 +108,7 @@ popd
 
 if [ ! -z "$PUSH" ]; then
     echo "[-] Pushing to $PUSH T=$SECONDS"
-    git push -f origin master:$PUSH
+    git push -f origin master-ci:$PUSH
 fi
 
 echo "[-] done pushing T=$SECONDS"
