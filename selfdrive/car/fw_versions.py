bd1db9aa8eec5c5ef02be92522a5142cd92413cd
Willem Melching <willem.melching@gmail.com>
2020-02-01 13:06:05 -0800

    Add batch of firmware versions
    old-commit-hash: bfa2d030d2927cb9cfdb01f8f1e614d3790d134b

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -86,8 +86,7 @@ def match_fw_to_car(fw_versions):
       addr = ecu[1:]
       found_version = fw_versions_dict.get(addr, None)
 
-      # TODO: RAV4, COROLLA esp sometimes doesn't show up
-      if ecu_type == Ecu.esp and candidate in [TOYOTA.RAV4, TOYOTA.COROLLA] and found_version is None:
+      if ecu_type == Ecu.esp and candidate in [TOYOTA.RAV4, TOYOTA.COROLLA, TOYOTA.HIGHLANDER] and found_version is None:
         continue
 
       # TODO: COROLLA_TSS2 engine can show on two different addresses
