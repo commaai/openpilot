db45847c408052ca70f53237e55cdf085f323c1a
Drew Hintz <adhintz@google.com>
2020-02-01 15:36:47 -0800

    fix segment test name

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -68,7 +68,7 @@ routes = {
     'carFingerprint': CHRYSLER.PACIFICA_2019_HYBRID,
     'enableCamera': True,
   },
-  "8190c7275a24557b|2020-01-29--08-33-58--7": {
+  "8190c7275a24557b|2020-01-29--08-33-58": {
     'carFingerprint': CHRYSLER.PACIFICA_2020_HYBRID,
     'enableCamera': True,
   },
