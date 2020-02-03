71ac62c54b075cc23b153a856d8330d9aa434ce5
George Hotz <geohot@gmail.com>
2020-02-02 21:42:52 -0800

    window now appears on mac

--- selfdrive/ui/linux.cc
+++ selfdrive/ui/linux.cc
@@ -46,7 +46,7 @@ FramebufferState* framebuffer_init(
   glfwSwapInterval(0);
 
   // clear screen
-  glClearColor(0.2f, 0.2f, 0.2f, 1.0f );
+  glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   framebuffer_swap((FramebufferState*)window);
 
@@ -61,6 +61,7 @@ void framebuffer_set_power(FramebufferState *s, int mode) {
 
 void framebuffer_swap(FramebufferState *s) {
   glfwSwapBuffers((GLFWwindow*)s);
+  glfwPollEvents();
 }
 
 void touch_init(TouchState *s) {
