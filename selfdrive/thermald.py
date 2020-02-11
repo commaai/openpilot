900a76f2c365baa492cdca27a11725b886a103d0
ZwX1616 <zwx1616@gmail.com>
2020-02-11 14:03:40 -0800

    limit c2 fan to 5k rpm
    old-commit-hash: 4ee214376a14906808c2e2fa5daed0180b9f7581

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -130,7 +130,7 @@ def handle_fan_eon(max_cpu_temp, bat_temp, fan_speed, ignition):
 
 
 def handle_fan_uno(max_cpu_temp, bat_temp, fan_speed, ignition):
-  new_speed = int(interp(max_cpu_temp, [40.0, 80.0], [0, 100]))
+  new_speed = int(interp(max_cpu_temp, [40.0, 80.0], [0, 80]))
 
   if not ignition:
     new_speed = min(30, new_speed)
