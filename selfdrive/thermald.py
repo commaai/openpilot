671d02631405d37b11e72ed363bbf585a121116d
Erich Moraga <33645296+ErichMoraga@users.noreply.github.com>
2020-02-14 09:47:35 -0600

    Increase days of no Internet connectivity to 30
    Making good on @geohot's promise...
    https://discordapp.com/channels/469524606043160576/524328425415245827/664532003382755349

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -24,7 +24,7 @@ FW_SIGNATURE = get_expected_signature()
 ThermalStatus = log.ThermalData.ThermalStatus
 NetworkType = log.ThermalData.NetworkType
 CURRENT_TAU = 15.   # 15s time constant
-DAYS_NO_CONNECTIVITY_MAX = 7  # do not allow to engage after a week without internet
+DAYS_NO_CONNECTIVITY_MAX = 30  # do not allow to engage after a month without internet
 DAYS_NO_CONNECTIVITY_PROMPT = 4  # send an offroad prompt after 4 days with no internet
 
 
