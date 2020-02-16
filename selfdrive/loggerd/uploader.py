2dc014bb8374d9038a1a828e86ee45ecc859bab7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-02-16 21:28:27 +0000

    no more gctx

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -266,7 +266,7 @@ def uploader_fn(exit_event):
       backoff = min(backoff*2, 120)
     cloudlog.info("upload done, success=%r", success)
 
-def main(gctx=None):
+def main():
   uploader_fn(threading.Event())
 
 if __name__ == "__main__":
