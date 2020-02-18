4f8a5eff7058fc859741b252593b2ea02ca12fb8
Riccardo Biasini <riccardo.biasini@gmail.com>
2020-02-18 11:29:39 -0800

    no need to negate

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -139,7 +139,7 @@ class CarState(CarStateBase):
     ret.vEgoRaw = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
 
-    ret.standstill = not ret.vEgoRaw > 0.1
+    ret.standstill = ret.vEgoRaw < 0.1
 
     ret.steeringAngle = cp.vl["SAS11"]['SAS_Angle']
     ret.steeringRate = cp.vl["SAS11"]['SAS_Speed']
