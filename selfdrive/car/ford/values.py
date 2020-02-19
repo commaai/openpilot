d86aa23c804ad95dd1c5fcfacf7f39c9c4b439c1
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-02-19 13:43:05 -0600

    Update values.py

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -5,7 +5,35 @@ Ecu = car.CarParams.Ecu
 MAX_ANGLE = 87.  # make sure we never command the extremes (0xfff) which cause latching fault
 
 class CAR:
-  FUSION = "FORD FUSION 2018"
+  #Unsupported Ford Models
+  #CMAX = "FORD C-MAX" C-MAX is not supported on any year or trim. 
+  #ECOSPORT = "FORD ECOSPORT" EcoSport is not supported on any year or trim. 
+  
+  #Ford models w/ lkas and acc
+  EDGE = "FORD EDGE" # 2013+ 2011-2012 has acc only Shares arch with 2013+. May be able to add IPMA to HS2.
+  ESCAPE = "FORD ESCAPE" # 2017+
+  EXPEDITION = "FORD EXPEDITION" # 2018+
+  EXPLORER = "FORD EXPLORER" # 2013+. 2011-2012 has acc only. Shares arch with Explorer 2013+. May be able to add IPMA to HS2.
+  F150 = "FORD F150" # 2015+
+  FUSION = "FORD FUSION" # 2013+
+  MUSTANG = "FORD MUSTANG" # 2018+ 2015-2017 has acc only. 2015-2017 shares arch with Mustang 2018+. May be able to add IPMA to HS2. Not available on Shelby GT350 or GT500.
+  RANGER = "FORD RANGER" # 2019+
+  TAURUS = "FORD TAURUS" # 2014+ 2010-2013 has acc only. 2013 shares arch with Taurus 2014+. May be able to add IPMA to HS2.
+  #Ford models with lkas or acc but not both
+  FLEX = "FORD FLEX" # Flex does not have LKAS on any year or trim. ACC only 2013+. Uses same arch as Explorer 2013-2015. May be able to add IPMA to HS2.
+  FOCUS = "FORD FOCUS" # Focus does not have ACC on any year or trim. Steering only 2015+. Uses same arch as Escape. May be able to add CCM to HS2.
+  TRANSIT = "FORD TRANSIT" # 2020+ 2013-2017 has LKAS only.
+  #Lincoln models w/ lkas and acc
+  AVIATOR = "LINCOLN AVIATOR" # 2020+. 
+  CONTINENTAL = "LINCOLN CONTINENTAL" # 2017+
+  CORSAIR = "LINCOLN CORSAIR" # Corsair is a rebranded MKC. 2020+
+  MKC = "LINCOLN MKC" # 2015+. 
+  MKS = "LINCOLN MKS" # 2013+. 2010-2012 is ACC only. Uses Taurus DBC. 
+  MKT = "LINCOLN MKT" # 2013+. 2011-2012 is ACC only. Uses Explorer DBC. 
+  MKX = "LINCOLN MKX" # 2016+. 2011-2015 is ACC only. May be able to add IPMA to HS2. Uses Edge DBC. 
+  MKZ = "LINCOLN MKZ" # 2013+ Uses Fusion DBC
+  NAVIGATOR = "LINCOLN NAVIGATOR" # 2018+. Uses Expedition DBC. 
+  NAUTILUS = "LINCOLN NAUTILUS" # Nautilus is a rebranded MKX. 2019+
 
 FINGERPRINTS = {
   CAR.FUSION: [{
