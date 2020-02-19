3174d0f76fc9d84ad4d92eeff1668adfa196bfd6
Comma Device <device@comma.ai>
2020-02-19 02:27:31 +0000

    actually fix benchmark script
    old-commit-hash: 042415b021ae8b9ab904569881e2be1a98e00d3b

--- selfdrive/modeld/test/snpe_benchmark/benchmark.sh
+++ selfdrive/modeld/test/snpe_benchmark/benchmark.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
-clang++ -I /data/openpilot/phonelibs/snpe/include/ -lSNPE -lsymphony-cpu -lsymphonypower benchmark.cc -o benchmark
-./benchmark $1
+clang++ -I /data/openpilot/phonelibs/snpe/include/ -L/data/pythonpath/phonelibs/snpe/aarch64 -lSNPE benchmark.cc -o benchmark
+export LD_LIBRARY_PATH="/data/pythonpath/phonelibs/snpe/aarch64/:$HOME/openpilot/phonelibs/snpe/x86_64/:$LD_LIBRARY_PATH"
+exec ./benchmark $1
