9d3308f25eb86211799c04d38cdffd368ceb2268
andyh2 <andy@comma.ai>
2020-02-24 18:09:32 -0500

    space

--- tools/lib/api.py
+++ tools/lib/api.py
@@ -7,7 +7,7 @@ API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
 class CommaApi():
   def __init__(self, token=None):
     self.session = requests.Session()
-    self.session.headers['User-agent'] ='OpenpilotTools'
+    self.session.headers['User-agent'] = 'OpenpilotTools'
     if token:
       self.session.headers['Authorization'] = 'JWT ' + token
 
