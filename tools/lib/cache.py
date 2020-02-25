960bd9624c58936506dad09cc1f788fc9e578bb6
Andy <andy@comma.ai>
2020-02-24 21:24:54 -0500

    Tools: Storage API  (#1161)
    * filereader
    
    * support URLs in filereader, logreader
    
    * unused
    
    * use route files api; add auth file
    
    * Implement browser auth
    
    * Update readme, fix up cache paths
    
    * Add tests, clear token on 401
    
    * Factor out URLFile
    
    * space

--- tools/lib/cache.py
+++ tools/lib/cache.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 from tools.lib.file_helpers import mkdirs_exists_ok
 
 DEFAULT_CACHE_DIR = os.path.expanduser("~/.commacache")
@@ -6,4 +7,9 @@ DEFAULT_CACHE_DIR = os.path.expanduser("~/.commacache")
 def cache_path_for_file_path(fn, cache_prefix=None):
   dir_ = os.path.join(DEFAULT_CACHE_DIR, "local")
   mkdirs_exists_ok(dir_)
-  return os.path.join(dir_, os.path.abspath(fn).replace("/", "_"))
+  fn_parsed = urllib.parse.urlparse(fn)
+  if fn_parsed.scheme == '':
+    cache_fn = os.path.abspath(fn).replace("/", "_")
+  else:
+    cache_fn = f'{fn_parsed.hostname}_{fn_parsed.path.replace("/", "_")}'
+  return os.path.join(dir_, cache_fn)
