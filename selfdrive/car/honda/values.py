5d3307470c1ddaebce3732f8e4ef8bf5422aad15
theantihero <kwj129@gmail.com>
2020-02-27 17:33:06 -0500

    remove dbc extension

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -673,7 +673,7 @@ DBC = {
   CAR.PILOT: dbc_dict('honda_pilot_touring_2017_can_generated', 'acura_ilx_2016_nidec'),
   CAR.PILOT_2019: dbc_dict('honda_pilot_touring_2017_can_generated', 'acura_ilx_2016_nidec'),
   CAR.RIDGELINE: dbc_dict('honda_ridgeline_black_edition_2017_can_generated', 'acura_ilx_2016_nidec'),
-  CAR.INSIGHT: dbc_dict('honda_insight_ex_2019_can_generated.dbc', None),
+  CAR.INSIGHT: dbc_dict('honda_insight_ex_2019_can_generated', None),
 }
 
 STEER_THRESHOLD = {
