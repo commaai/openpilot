3b36258613d3e4a7b13bd34c646e73d0e82f0b35
dnv26 <55224281+dnv26@users.noreply.github.com>
2020-02-26 22:55:24 -0500

    bug

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -105,9 +105,9 @@ class CarController():
 
     # disable if steer angle reach 90 deg, otherwise mdps fault in some models
     if self.car_fingerprint == CAR.GENESIS:
-      lkas_active = enabled and abs(CS.angle_steers) < 90. and not self.lkas_button
+      lkas_active = enabled and abs(CS.angle_steers) < 90. and self.lkas_button
     else:
-      lkas_active = enabled and not self.lkas_button
+      lkas_active = enabled and self.lkas_button
 
     # Fix for sharp turns mdps fault and Genesis hard fault at low speed
     if CS.v_ego < 13.7 and self.car_fingerprint == CAR.GENESIS and not CS.mdps_bus:
