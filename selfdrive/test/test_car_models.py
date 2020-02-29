589b63ca4e7d54b98723d0d16f18bc7d42091259
Pierre Christen <netadvanced@gmail.com>
2020-02-29 23:57:44 +0100

    Updated VW fingerprint model to match new naming

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -418,7 +418,7 @@ routes = {
     'enableDsu': False,
   },
   "76b83eb0245de90e|2019-10-20--15-42-29": {
-    'carFingerprint': VOLKSWAGEN.GOLF,
+    'carFingerprint': VOLKSWAGEN.VW_GOLF,
     'enableCamera': True,
   },
   "791340bc01ed993d|2019-03-10--16-28-08": {
