ccf0dcdc81554e97320cdb3091dc28cc511c5d6b
George Hotz <geohot@gmail.com>
2020-02-29 10:00:20 -0800

    use readchar

--- tools/sim/lib/keyboard_ctrl.py
+++ tools/sim/lib/keyboard_ctrl.py
@@ -1,16 +1,7 @@
 import time
 import sys
 import tty, termios
-
-def getch():   # define non-Windows version
-  fd = sys.stdin.fileno()
-  old_settings = termios.tcgetattr(fd)
-  try:
-    tty.setraw(sys.stdin.fileno())
-    ch = sys.stdin.read(1)
-  finally:
-    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-  return ch
+import readchar
 
 def keyboard_poll_thread():
   import zmq
@@ -19,7 +10,7 @@ def keyboard_poll_thread():
   socket.bind('tcp://127.0.0.1:4444')
 
   while True:
-    c = getch()
+    c = readchar.readchar()
     print("got %s" % c)
     if c == '1':
       socket.send_string(str("cruise_up"))
