4695047b3d603b0675289da06bf85cd7a9d27c62
dnv26 <55224281+dnv26@users.noreply.github.com>
2020-03-01 13:04:19 -0500

    decrease video file size by 50%
    better auto-start recording implementation

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -140,7 +140,7 @@ void start_capture() {
   char filename[64];
   struct tm tm = get_time_struct();
   snprintf(filename,sizeof(filename),"%04d%02d%02d-%02d%02d%02d.mp4", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-  snprintf(cmd,sizeof(cmd),"screenrecord --size 1280x720 --bit-rate 10000000 %s/%s&",videos_dir,filename);
+  snprintf(cmd,sizeof(cmd),"screenrecord --size 1280x720 --bit-rate 5000000 %s/%s&",videos_dir,filename);
   //snprintf(cmd,sizeof(cmd),"screenrecord --size 960x540 --bit-rate 5000000 %s/%s&",videos_dir,filename);
   strcpy(filenames[captureNum],filename);
 
@@ -337,7 +337,7 @@ void dashcam( UIState *s, int touch_x, int touch_y ) {
     // Assume car is not in drive so stop recording
     stop_capture();
   }
-  if (s->scene.v_ego > 2.1 && captureState == CAPTURE_STATE_NOT_CAPTURING) {
+  if (s->scene.v_ego > 2.1 && captureState == CAPTURE_STATE_NOT_CAPTURING && !s->scene.engaged) {
     start_capture();
   } else if (s->scene.v_ego < 1.5 && !s->scene.engaged) {
     stop_capture();
