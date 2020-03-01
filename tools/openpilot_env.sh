7f21cb874ae02c471e03e1fe380bb0c850f2cf18
George Hotz <geohot@gmail.com>
2020-02-29 18:30:44 -0800

    mac fixes

--- tools/openpilot_env.sh
+++ tools/openpilot_env.sh
@@ -1,11 +1,19 @@
 if [ -z "$OPENPILOT_ENV" ]; then
-  export PATH="$HOME/.pyenv/bin:$PATH"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-
   export PYTHONPATH="$HOME/openpilot"
-  export PATH="$PATH:$HOME/openpilot/external/capnp/bin"
-  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/openpilot/external/capnp/lib"
+
+  unamestr=`uname`
+  if [[ "$unamestr" == 'Linux' ]]; then
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+
+    export PATH="$PATH:$HOME/openpilot/external/capnp/bin"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/openpilot/external/capnp/lib"
+  elif [[ "$unamestr" == 'Darwin' ]]; then
+    # msgq doesn't work on mac
+    export ZMQ=1
+    export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+  fi
 
   export OPENPILOT_ENV=1
 fi
