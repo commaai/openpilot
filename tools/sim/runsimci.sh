1313c99b072804bf37c89cff4d0ee12811c7c814
Leon <leon.hillmann@googlemail.com>
2020-03-01 21:29:06 +0100

    adding basic testingÏ

---
+++ tools/sim/runsimci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+: '
+FILE=CARLA_0.9.7.tar.gz
+if [ ! -f $FILE ]; then
+  curl -O http://carla-assets-internal.s3.amazonaws.com/Releases/Linux/$FILE
+fi
+if [ ! -d carla ]; then
+  rm -rf carla_tmp
+  mkdir -p carla_tmp
+  cd carla_tmp
+  tar xvf ../$FILE
+  easy_install PythonAPI/carla/dist/carla-0.9.7-py3.5-linux-x86_64.egg || true
+  cd ../
+  mv carla_tmp carla
+fi
+'
+easy_install carla/PythonAPI/carla/dist/carla-0.9.7-py3.5-linux-x86_64.egg || true
+
+./tests/carla_test.py
