777ca2cfd02d8ef82077fc747768537948daacf8
Willem Melching <willem.melching@gmail.com>
2020-03-05 15:12:22 -0800

    Forced dashcam in test_car_models

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -450,6 +450,9 @@ forced_dashcam_routes = [
   # Ford fusion
   "f1b4c567731f4a1b|2018-04-18--11-29-37",
   "f1b4c567731f4a1b|2018-04-30--10-15-35",
+
+  # Nissan
+  "fbbfa6af821552b9|2020-03-03--08-09-43",
 ]
 
 # TODO: replace all these with public routes
