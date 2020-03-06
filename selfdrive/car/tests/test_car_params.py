54ef38d7f614eda7383a4a4059d8f3addf0995ef
Willem Melching <willem.melching@gmail.com>
2020-03-06 14:02:40 -0800

    Also test radar interface

--- selfdrive/car/tests/test_car_params.py
+++ selfdrive/car/tests/test_car_params.py
@@ -5,15 +5,17 @@ from selfdrive.car.fingerprints import all_known_cars
 from selfdrive.car.car_helpers import interfaces
 from selfdrive.car.fingerprints import _FINGERPRINTS as FINGERPRINTS
 
+from cereal import car
+
 
 class TestCarParam(unittest.TestCase):
   def test_creating_car_params(self):
     all_cars = all_known_cars()
 
-    for car in all_cars:
-      fingerprint = FINGERPRINTS[car][0]
+    for car_name in all_cars:
+      fingerprint = FINGERPRINTS[car_name][0]
 
-      CarInterface, CarController, CarState = interfaces[car]
+      CarInterface, CarController, CarState = interfaces[car_name]
       fingerprints = {
         0: fingerprint,
         1: fingerprint,
@@ -23,16 +25,24 @@ class TestCarParam(unittest.TestCase):
       car_fw = []
 
       for has_relay in [True, False]:
-        car_params = CarInterface.get_params(car, fingerprints, has_relay, car_fw)
-        car_interface = CarInterface(car_params, CarController, CarState), car_params
-
-        RadarInterface = importlib.import_module('selfdrive.car.%s.radar_interface' % car_params.carName).RadarInterface
-        radar_interface = RadarInterface(car_params)
-
+        car_params = CarInterface.get_params(car_name, fingerprints, has_relay, car_fw)
+        car_interface, _ = CarInterface(car_params, CarController, CarState), car_params
         assert car_params
         assert car_interface
-        assert radar_interface
 
+        # Run car interface once
+        CC = car.CarControl.new_message()
+        car_interface.update(CC, [])
+
+      # Test radar interface
+      RadarInterface = importlib.import_module('selfdrive.car.%s.radar_interface' % car_params.carName).RadarInterface
+      radar_interface = RadarInterface(car_params)
+      assert radar_interface
+
+      # Run car interface once
+      radar_interface.update([])
+      if hasattr(radar_interface, '_update') and hasattr(radar_interface, 'trigger_msg'):
+        radar_interface._update([radar_interface.trigger_msg])
 
 if __name__ == "__main__":
   unittest.main()
