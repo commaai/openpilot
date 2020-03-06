60677729eb6726ae6bc226de3b48b3a21654e318
Willem Melching <willem.melching@gmail.com>
2020-03-06 13:56:27 -0800

    Also create a radar interface
    old-commit-hash: 86e0202549b3d6eb9965d9149a9cee4517c26159

--- selfdrive/car/tests/test_car_params.py
+++ selfdrive/car/tests/test_car_params.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import unittest
+import importlib
 from selfdrive.car.fingerprints import all_known_cars
 from selfdrive.car.car_helpers import interfaces
 from selfdrive.car.fingerprints import _FINGERPRINTS as FINGERPRINTS
@@ -25,8 +26,12 @@ class TestCarParam(unittest.TestCase):
         car_params = CarInterface.get_params(car, fingerprints, has_relay, car_fw)
         car_interface = CarInterface(car_params, CarController, CarState), car_params
 
+        RadarInterface = importlib.import_module('selfdrive.car.%s.radar_interface' % car_params.carName).RadarInterface
+        radar_interface = RadarInterface(car_params)
+
         assert car_params
         assert car_interface
+        assert radar_interface
 
 
 if __name__ == "__main__":
