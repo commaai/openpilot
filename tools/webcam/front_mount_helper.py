ad0a39dbaed4103bcdafc83656a55362889d0c7a
ZwX1616 <zwx1616@gmail.com>
2020-03-10 17:55:29 -0700

    typo

--- tools/webcam/front_mount_helper.py
+++ tools/webcam/front_mount_helper.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
   while (True):
     ret, img = cap.read()
     if ret:
-      img = cv2.warpPerspective(img, trans_webcam_to_eon_front, (1164,874), borderMode=cv2.BORDER_CONSTANT, borderValue=0)
-      img = img[:,-874//2:,:]
+      img = cv2.warpPerspective(img, trans_webcam_to_eon_front, (1152,864), borderMode=cv2.BORDER_CONSTANT, borderValue=0)
+      img = img[:,-864//2:,:]
       cv2.imshow('preview', img)
       cv2.waitKey(10)
