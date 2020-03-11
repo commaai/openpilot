093d6cb5ffe086f2c76213367cdbad497bdc5719
rbiasini <riccardo.biasini@gmail.com>
2020-03-11 14:32:03 -0700

    Fingerprint cleanup (#1231)
    * upd panda and opendbc
    
    * Delete unnecessary fingerprints, fix RavH occasional test issue, re-enable test error for overlapping fingerprints within the same candidate

--- selfdrive/test/test_fingerprints.py
+++ selfdrive/test/test_fingerprints.py
@@ -81,10 +81,6 @@ valid = True
 for idx1, f1 in enumerate(fingerprints_flat):
   for idx2, f2 in enumerate(fingerprints_flat):
     if idx1 < idx2 and not check_fingerprint_consistency(f1, f2):
-      if car_names[idx1] == car_names[idx2]:
-        print(f"Warning, overlap in {car_names[idx1]}")
-        continue
-
       valid = False
       print("Those two fingerprints are inconsistent {0} {1}".format(car_names[idx1], car_names[idx2]))
       print("")
