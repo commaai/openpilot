6c10a6322a81413cdad7be1f1e53c6a512b5d201
dnv26 <55224281+dnv26@users.noreply.github.com>
2020-03-16 20:30:26 -0400

    bug

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -83,7 +83,7 @@ void save_file(char *videos_dir, char *filename) {
 }
 
 void stop_capture() {
-  char videos_dir[50] = "/sdcard/videos";
+  char videos_dir[50] = "/storage/emulated/0/videos";
 
   if (captureState == CAPTURE_STATE_CAPTURING) {
     system("killall -SIGINT screenrecord");
@@ -105,7 +105,7 @@ void stop_capture() {
 void start_capture() {
   captureState = CAPTURE_STATE_CAPTURING;
   char cmd[128] = "";
-  char videos_dir[50] = "/sdcard/videos";
+  char videos_dir[50] = "/storage/emulated/0/videos";
 
   //////////////////////////////////
   // NOTE: make sure videos_dir folder exists on the device!
@@ -114,17 +114,17 @@ void start_capture() {
   if (stat(videos_dir, &st) == -1) {
     mkdir(videos_dir,0700);
   }
-  /*if (captureNum == 0 && files_created == 0) {
+  if (captureNum == 0 && files_created == 0) {
     DIR *dir;
     struct dirent *ent;
-    if ((dir = opendir ("/sdcard/videos")) != NULL) {
+    if ((dir = opendir ("/storage/emulated/0/videos")) != NULL) {
       while ((ent = readdir (dir)) != NULL) {
         strcpy(filenames[files_created++], ent->d_name);
       }
       captureNum = files_created;
       closedir (dir);
     }
-  }*/
+  }
 
   if (strlen(filenames[captureNum]) && files_created >= RECORD_FILES) {
     if (locked_files[captureNum] > 0) {
