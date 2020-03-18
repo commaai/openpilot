eb6c809e9fb4a975c20f4deeec6a3995652222c7
Comma Device <device@comma.ai>
2020-03-18 01:36:06 +0000

    hook works

--- selfdrive/modeld/test/opencl_hooks/hook.c
+++ selfdrive/modeld/test/opencl_hooks/hook.c
@@ -2,6 +2,7 @@
 #include <dlfcn.h>
 #include <CL/cl.h>
 
+int cnt = 0;
 cl_int clEnqueueNDRangeKernel(cl_command_queue command_queue,
  	cl_kernel kernel,
  	cl_uint work_dim,
@@ -15,8 +16,8 @@ cl_int clEnqueueNDRangeKernel(cl_command_queue command_queue,
   cl_int (*my_clEnqueueNDRangeKernel)(cl_command_queue, cl_kernel, cl_uint,
     const size_t *, const size_t *, const size_t *,
     cl_uint, const cl_event *, cl_event *) = NULL;
-  my_clEnqueueNDRangeKernel = dlsym(RTLD_NEXT, "clEnqueueNDRangeKernel");
-  printf("hook clEnqueueNDRangeKernel(%p) %d\n", my_clEnqueueNDRangeKernel, work_dim);
+  my_clEnqueueNDRangeKernel = dlsym(RTLD_NEXT, "REAL_clEnqueueNDRangeKernel");
+  printf("hook clEnqueueNDRangeKernel(%p) %d -- %d\n", my_clEnqueueNDRangeKernel, work_dim, cnt++);
 
   return my_clEnqueueNDRangeKernel(command_queue, kernel, work_dim,
     global_work_offset, global_work_size, local_work_size,
@@ -27,6 +28,8 @@ void *dlsym(void *handle, const char *symbol) {
   void *(*my_dlsym)(void *handle, const char *symbol) = (void*)dlopen-0x2d4;
   if (strcmp("clEnqueueNDRangeKernel", symbol) == 0) {
     return clEnqueueNDRangeKernel;
+  } else if (strcmp("REAL_clEnqueueNDRangeKernel", symbol) == 0) {
+    return my_dlsym(handle, "clEnqueueNDRangeKernel");
   } else {
     printf("dlsym %s\n", symbol);
     return my_dlsym(handle, symbol);
