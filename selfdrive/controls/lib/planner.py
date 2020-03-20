8a04048fc45986f6c24dedbcb89b95baec50747a
Harald Schafer <harald.the.engineer@gmail.com>
2020-03-20 14:34:06 -0700

    no lead

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -90,10 +90,10 @@ class Planner():
   def choose_solution(self, v_cruise_setpoint, enabled):
     if enabled:
       solutions = {'cruise': self.v_cruise}
-      if self.mpc1.prev_lead_status:
-        solutions['mpc1'] = self.mpc1.v_mpc
-      if self.mpc2.prev_lead_status:
-        solutions['mpc2'] = self.mpc2.v_mpc
+      #if self.mpc1.prev_lead_status:
+      #  solutions['mpc1'] = self.mpc1.v_mpc
+      #if self.mpc2.prev_lead_status:
+      #  solutions['mpc2'] = self.mpc2.v_mpc
       if self.mpc_model.valid:
         solutions['model'] = self.mpc_model.v_mpc
 
