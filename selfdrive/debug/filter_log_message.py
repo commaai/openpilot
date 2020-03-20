196b28e311b4a36fa6cb827affea5b45218dd1de
Willem Melching <willem.melching@gmail.com>
2020-03-20 15:59:48 -0700

    fix androidLogEntry socket name

--- selfdrive/debug/filter_log_message.py
+++ selfdrive/debug/filter_log_message.py
@@ -23,20 +23,19 @@ if __name__ == "__main__":
   parser.add_argument("socket", type=str, nargs='*', help="socket name")
   args = parser.parse_args()
 
-  if args.addr != "127.0.0.1":
-    os.environ["ZMQ"] = "1"
-    messaging.context = messaging.Context()
-
-  poller = messaging.Poller()
-  sock = messaging.sub_sock("logMessage", poller, addr=args.addr)
+  sm = messaging.SubMaster(['logMessage', 'androidLogEntry'], addr=args.addr)
 
   min_level = LEVELS[args.level]
 
   while True:
-    polld = poller.poll(1000)
-    for sock in polld:
-      evt = messaging.recv_one(sock)
-      log = json.loads(evt.logMessage)
+    sm.update()
 
+    if sm.updated['logMessage']:
+      t = sm.logMonoTime['logMessage']
+      log = json.loads(sm['logMessage'])
       if log['levelnum'] >= min_level:
-        print(f"[{evt.logMonoTime / 1e9:.6f}] {log['filename']}:{log.get('lineno', '')} - {log.get('funcname', '')}: {log['msg']}")
+        print(f"[{t / 1e9:.6f}] {log['filename']}:{log.get('lineno', '')} - {log.get('funcname', '')}: {log['msg']}")
+    if sm.updated['androidLogEntry']:
+      t = sm.logMonoTime['androidLogEntry']
+      print(f"[{t / 1e9:.6f}] - ")
+      print(sm['androidLogEntry'])
