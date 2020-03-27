a95e61edf4176c034c0537293a3cf0753c45daa1
ZwX1616 <zwx1616@gmail.com>
2020-03-26 19:19:47 -0700

    add webcam to cameras (#1201)

--- selfdrive/camerad/cameras/camera_common.h
+++ selfdrive/camerad/cameras/camera_common.h
@@ -10,7 +10,9 @@
 #define CAMERA_ID_OV8865 3
 #define CAMERA_ID_IMX298_FLIPPED 4
 #define CAMERA_ID_OV10640 5
-#define CAMERA_ID_MAX 6
+#define CAMERA_ID_LGC920 6
+#define CAMERA_ID_LGC615 7
+#define CAMERA_ID_MAX 8
 
 #ifdef __cplusplus
 extern "C" {
