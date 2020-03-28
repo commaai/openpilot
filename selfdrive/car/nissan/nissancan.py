0d03fad5b61d9ab665ed847916a5053508d406b5
Willem Melching <willem.melching@gmail.com>
2020-03-27 21:44:59 -0700

    Nissan leaf (#1275)
    * split out leaf and xtrail
    
    * Add brake pedal
    
    * This should work
    
    * Fix test car models + bump panda
    
    * Combined cruise enabled detection in single message
    
    * Proper frequency checks
    
    * Add doors
    
    * Blinkers and doors
    
    * Seatbelt
    
    * Gear
    
    * Add cancel message
    
    * Unify steering pressed
    
    * Remove angle limit
    
    * Add steer saturation alert for angle based control
    
    * Add set speed
    
    * Change wheel speed factor
    
    * Fix offset in set speed
    
    * Timeout on engage for steer saturated
    
    * Put counter back
    
    * try cancel using seatbelt
    
    * Try different cancel message
    
    * Rename cancel signal
    
    * Add LKAS alert
    
    * Add missing fingerprint values
    
    * Update test car models
    
    * Add some comments
    old-commit-hash: 1df6b67511e0872c134edef0eb8535d71e0b2cfd

--- selfdrive/car/nissan/nissancan.py
+++ selfdrive/car/nissan/nissancan.py
@@ -6,21 +6,19 @@ nissan_checksum = crcmod.mkCrcFun(0x11d, initCrc=0x00, rev=False, xorOut=0xff)
 
 
 def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_on, lkas_max_torque):
-  if car_fingerprint == CAR.XTRAIL:
-    idx = (frame % 16)
-    values = {
-      "DESIRED_ANGLE": apply_steer,
-      "SET_0x80_2": 0x80,
-      "SET_0x80": 0x80,
-      "MAX_TORQUE": lkas_max_torque if steer_on else 0,
-      "COUNTER": idx,
-      "LKA_ACTIVE": steer_on,
-    }
+  idx = (frame % 16)
+  values = {
+    "DESIRED_ANGLE": apply_steer,
+    "SET_0x80_2": 0x80,
+    "SET_0x80": 0x80,
+    "MAX_TORQUE": lkas_max_torque if steer_on else 0,
+    "COUNTER": idx,
+    "LKA_ACTIVE": steer_on,
+  }
 
-    dat = packer.make_can_msg("LKAS", 0, values)[2]
+  dat = packer.make_can_msg("LKAS", 0, values)[2]
 
-    values["CHECKSUM"] = nissan_checksum(dat[:7])
-
+  values["CHECKSUM"] = nissan_checksum(dat[:7])
   return packer.make_can_msg("LKAS", 0, values)
 
 
@@ -36,6 +34,15 @@ def create_acc_cancel_cmd(packer, cruise_throttle_msg, frame):
   values["COUNTER"] = (frame % 4)
 
   return packer.make_can_msg("CRUISE_THROTTLE", 2, values)
+
+
+def create_cancel_msg(packer, cancel_msg, cruise_cancel):
+  values = copy.copy(cancel_msg)
+
+  if cruise_cancel:
+    values["CANCEL_SEATBELT"] = 1
+
+  return packer.make_can_msg("CANCEL_MSG", 2, values)
 
 
 def create_lkas_hud_msg(packer, lkas_hud_msg, enabled, left_line, right_line, left_lane_depart, right_lane_depart):
