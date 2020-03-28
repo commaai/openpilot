64da2a519cfff9f74bd07190dd96425224a8ad1f
Andre Volmensky <andre@volmens.ky>
2020-03-28 22:36:07 +0900

    Added cruiseState.speed to Nissan X-Trail

--- selfdrive/car/nissan/carstate.py
+++ selfdrive/car/nissan/carstate.py
@@ -48,12 +48,16 @@ class CarState(CarStateBase):
     elif self.CP.carFingerprint == CAR.LEAF:
       ret.cruiseState.available = bool(cp.vl["CRUISE_THROTTLE"]["CRUISE_AVAILABLE"])
 
-    # TODO: Find mph/kph bit on XTRAIL
-    if self.CP.carFingerprint == CAR.LEAF:
+    # TODO: Find mph/kph bit on XTRAIL until then, assume xtrail is kph.
+    # Unable to change kph to mph on the xtrail, need a rogue to test it on
+    if self.CP.carFingerprint == CAR.LEAF or self.CP.carFingerprint == CAR.XTRAIL:
       speed = cp_adas.vl["PROPILOT_HUD"]["SET_SPEED"]
       if speed != 255:
+        if self.CP.carFingerprint == CAR.XTRAIL:
+          conversion = CV.KPH_TO_MS
+        else:
+          conversion = CV.MPH_TO_MS if cp.vl["HUD_SETTINGS"]["SPEED_MPH"] else CV.KPH_TO_MS
         speed -= 1  # Speed on HUD is always 1 lower than actually sent on can bus
-        conversion = CV.MPH_TO_MS if cp.vl["HUD_SETTINGS"]["SPEED_MPH"] else CV.KPH_TO_MS
         ret.cruiseState.speed = speed * conversion
 
     ret.steeringTorque = cp.vl["STEER_TORQUE_SENSOR"]["STEER_TORQUE_DRIVER"]
