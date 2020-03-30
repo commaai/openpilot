ba1e79d0eadbc542b9485766afa98ffd8d05e62b
yufeng66 <43092209+yufeng66@users.noreply.github.com>
2020-03-30 14:59:01 -0400

    update rav4h_tss2 tune  (#1260)
    * update rav4h_tss2 tune
    
    reduce pid gains for rav4h_tss2 to reduce ping pong at local speed and big turn
    
    * update tuning for rav4_tss2 in additional to rav4h_tss2

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -157,9 +157,9 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.68986
       ret.steerRatio = 14.3
       tire_stiffness_factor = 0.7933
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15], [0.05]]
       ret.mass = 3370. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.pid.kf = 0.00004
 
     elif candidate == CAR.RAV4H_TSS2:
       stop_and_go = True
@@ -167,9 +167,9 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.68986
       ret.steerRatio = 14.3
       tire_stiffness_factor = 0.7933
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15], [0.05]]
       ret.mass = 3800. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.pid.kf = 0.00004
 
     elif candidate in [CAR.COROLLA_TSS2, CAR.COROLLAH_TSS2]:
       stop_and_go = True
