64f6e0c1d163b94ea301b74a2bdfe77dbbbb2e5c
Willem Melching <willem.melching@gmail.com>
2020-04-08 14:09:24 -0700

    Init mutex just to be safe

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -919,6 +919,9 @@ int main() {
     loopback_can = true;
   }
 
+  err = pthread_mutex_init(&usb_lock, NULL);
+  assert(err == 0);
+
   // init libusb
   err = libusb_init(&ctx);
   assert(err == 0);
