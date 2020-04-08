833b75ca49354ff8e9bb793da4040fcb0c0f1c2e
Willem Melching <willem.melching@gmail.com>
2020-04-07 18:12:44 -0700

    Don't use battery temperature to determine fan type

--- selfdrive/thermald/power_monitoring.py
+++ selfdrive/thermald/power_monitoring.py
@@ -58,12 +58,11 @@ def panda_current_to_actual_current(panda_current):
 
 
 class PowerMonitoring:
-  def __init__(self, is_uno):
+  def __init__(self):
     self.last_measurement_time = None           # Used for integration delta
     self.power_used_uWh = 0                     # Integrated power usage in uWh since going into offroad
     self.next_pulsed_measurement_time = None
     self.integration_lock = threading.Lock()
-    self.is_uno = is_uno
 
   # Calculation tick
   def calculate(self, health):
@@ -90,6 +89,7 @@ class PowerMonitoring:
           self.last_measurement_time = now
           return
 
+      is_uno = health.health.hwType == log.HealthData.HwType.uno
       # Get current power draw somehow
       current_power = 0
       if get_battery_status() == 'Discharging':
@@ -132,7 +132,7 @@ class PowerMonitoring:
         self.next_pulsed_measurement_time = None
         return
 
-      elif self.next_pulsed_measurement_time is None and not self.is_uno:
+      elif self.next_pulsed_measurement_time is None and not is_uno:
         # On a charging EON with black panda, or drawing more than 400mA out of a white/grey one
         # Only way to get the power draw is to turn off charging for a few sec and check what the discharging rate is
         # We shouldn't do this very often, so make sure it has been some long-ish random time interval
