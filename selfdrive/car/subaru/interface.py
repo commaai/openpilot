5307aa2759f1d3315eca92b9bd2763616387242c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-04-10 13:48:38 -0700

    unused

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from cereal import car
 from selfdrive.config import Conversions as CV
-from selfdrive.controls.lib.drive_helpers import create_event, EventTypes as ET
 from selfdrive.car.subaru.values import CAR
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint
 from selfdrive.car.interfaces import CarInterfaceBase
