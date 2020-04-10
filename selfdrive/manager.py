2731a736d45b41a67966af7b2d74da1b667de024
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-04-10 13:55:56 -0700

    Revert "the flippening came to NEOS"
    This reverts commit 91cfb73b2914f637863335f4e5f5952620b3e977.

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -526,6 +526,9 @@ def uninstall():
 def main():
   os.environ['PARAMS_PATH'] = PARAMS
 
+  # the flippening!
+  os.system('LD_LIBRARY_PATH="" content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1')
+
   # disable bluetooth
   os.system('service call bluetooth_manager 8')
 
