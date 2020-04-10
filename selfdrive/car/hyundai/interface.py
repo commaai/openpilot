1c3f140c70cff794da7f48e15534e31a4468de39
AskAlice <alice@askalice.me>
2020-04-09 20:06:52 -0600

    add radar interface for Hyundai
    Co-authored-by: xx979xx <40252818+xx979xx@users.noreply.github.com>

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -18,7 +18,9 @@ class CarInterface(CarInterfaceBase):
 
     ret.carName = "hyundai"
     ret.safetyModel = car.CarParams.SafetyModel.hyundai
-    ret.radarOffCan = True
+    ret.sccBus = 0 if 1056 in fingerprint[0] else 1 if 1056 in fingerprint[1] and 1296 not in fingerprint[1] \
+                                                                     else 2 if 1056 in fingerprint[2] else -1   ret.radarOffCan = ret.sccBus == -1
+
 
     # Hyundai port is a community feature, since we don't own one to test
     ret.communityFeature = True
