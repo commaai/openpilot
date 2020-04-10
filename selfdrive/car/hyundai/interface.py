264eb318171524d76cfaf864740d2c59524e7d0f
AskAlice <alice@askalice.me>
2020-04-10 17:38:27 -0600

    update radarOffCan checks

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -18,12 +18,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.carName = "hyundai"
     ret.safetyModel = car.CarParams.SafetyModel.hyundai
-    #not in cereal/car.capnp but I don't wanna fork cereal so
-    #ret.sccBus = 0 if 1056 in fingerprint[0] else 1 if 1056 in fingerprint[1] and 1296 not in fingerprint[1] \
-    #                                                                 else 2 if 1056 in fingerprint[2] else - 1
-    ret.radarOffCan = False if 1056 in fingerprint[0] else False if 1056 in fingerprint[1] and 1296 not in fingerprint[1] else False if 1056 in fingerprint[2] else True
-    # ret.sccBus == -1
-
+    ret.radarOffCan = not (1056 in fingerprint[0])
 
     # Hyundai port is a community feature, since we don't own one to test
     ret.communityFeature = True
