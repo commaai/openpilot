8d791d78dd4fbc973eb61ccd9df2ed4cf9bbbcd5
Adeeb <8762862+quillford@users.noreply.github.com>
2020-04-10 16:12:58 -0700

    Abstract pcmEnable and pcmDisable events (#1350)
    * don't need individual prev state vars since whole CS is copied
    
    * don't need a whole function since toyota alerts were simplified
    
    * better here
    
    * abstract pcmEnable/pcmDisable events
    
    * the flippening came to NEOS
    
    * delete
    
    * unused
    
    * Revert "the flippening came to NEOS"
    
    This reverts commit 91cfb73b2914f637863335f4e5f5952620b3e977.

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -6,7 +6,6 @@ from opendbc.can.packer import CANPacker
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
-    self.braking = False
     self.apply_steer_last = 0
     self.ccframe = 0
     self.prev_frame = -1
--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -73,21 +73,12 @@ class CarInterface(CarInterfaceBase):
     ret.buttonEvents = []
 
     # events
-    events = self.create_common_events(ret, extra_gears=[car.CarState.GearShifter.low], gas_resume_speed = 2.)
-
-    if ret.cruiseState.enabled and not self.cruise_enabled_prev:
-      events.append(create_event('pcmEnable', [ET.ENABLE]))
-    elif not ret.cruiseState.enabled:
-      events.append(create_event('pcmDisable', [ET.USER_DISABLE]))
+    events = self.create_common_events(ret, extra_gears=[car.CarState.GearShifter.low], gas_resume_speed=2.)
 
     if ret.vEgo < self.CP.minSteerSpeed:
       events.append(create_event('belowSteerSpeed', [ET.WARNING]))
 
     ret.events = events
-
-    self.gas_pressed_prev = ret.gasPressed
-    self.brake_pressed_prev = ret.brakePressed
-    self.cruise_enabled_prev = ret.cruiseState.enabled
 
     # copy back carState packet to CS
     self.CS.out = ret.as_reader()
