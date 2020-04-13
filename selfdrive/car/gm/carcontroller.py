86e20409bc3c7f8dc03e2a7dd91f4f241af37b08
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-04-13 15:28:31 -0700

    fix carcontroller params

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -12,12 +12,11 @@ VisualAlert = car.CarControl.HUDControl.VisualAlert
 
 class CarControllerParams():
   def __init__(self):
-    self.STEER_MAX = 150
-    self.STEER_STEP = 1              # how often we update the steer cmd
-    self.STEER_DELTA_UP = 2          # 0.75s time to peak torque
-    self.STEER_DELTA_DOWN = 5        # 0.3s from peak torque to zero
-    self.MIN_STEER_SPEED = -1.       # can steer down to zero
-
+    self.STEER_MAX = 300
+    self.STEER_STEP = 2              # how often we update the steer cmd
+    self.STEER_DELTA_UP = 7          # ~0.75s time to peak torque (255/50hz/0.75s)
+    self.STEER_DELTA_DOWN = 17       # ~0.3s from peak torque to zero
+    self.MIN_STEER_SPEED = 3.
     self.STEER_DRIVER_ALLOWANCE = 50   # allowed driver torque before start limiting
     self.STEER_DRIVER_MULTIPLIER = 4   # weight driver torque heavily
     self.STEER_DRIVER_FACTOR = 100     # from dbc
