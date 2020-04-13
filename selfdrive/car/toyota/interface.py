e3fb198f81d76b4f2ede5f1a5211355cadcaaa29
wocsor <kevinroscom94@gmail.com>
2020-04-13 09:34:39 -0400

    Lexus UX 250h port (FW Fingerprinting)

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -231,6 +231,16 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
       ret.lateralTuning.pid.kf = 0.00006
 
+    elif candidate == CAR.LEXUS_UXH:
+      stop_and_go = True
+      ret.safetyParam = 73
+      ret.wheelbase = 2.640
+      ret.steerRatio = 14.7  # not optimized
+      tire_stiffness_factor = 0.444  # not optimized yet
+      ret.mass = 3605. * CV.LB_TO_KG + STD_CARGO_KG
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
+      ret.lateralTuning.pid.kf = 0.00007
+
     ret.steerRateCost = 1.
     ret.centerToFront = ret.wheelbase * 0.44
 
