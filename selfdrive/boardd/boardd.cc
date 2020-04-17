93a539e7ffb62590ca67436d5a9b7197e16d227e
Willem Melching <willem.melching@gmail.com>
2020-04-17 16:37:34 -0700

    Release messaging sockets and context on shutdown (#1391)
    * release resources when thread is done
    
    * add white space
    
    * add white space
    
    Co-authored-by: deanlee <deanlee3@gmail.com>
    old-commit-hash: 02e474580ee90713bc3426ea512278c5eb1c6b15

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -632,6 +632,9 @@ void *can_send_thread(void *crap) {
   while (!do_exit) {
     can_send(subscriber);
   }
+  
+  delete subscriber;
+  delete context;
   return NULL;
 }
 
@@ -662,6 +665,9 @@ void *can_recv_thread(void *crap) {
 
     next_frame_time += dt;
   }
+
+  delete publisher;
+  delete c;
   return NULL;
 }
 
@@ -677,6 +683,9 @@ void *can_health_thread(void *crap) {
     can_health(publisher);
     usleep(500*1000);
   }
+
+  delete publisher;
+  delete c;
   return NULL;
 }
 
@@ -911,6 +920,8 @@ void *pigeon_thread(void *crap) {
     cnt++;
   }
 
+  delete publisher;
+  delete context;
   return NULL;
 }
 
