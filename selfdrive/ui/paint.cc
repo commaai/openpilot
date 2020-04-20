ef75971b6978c57594bbcb40dc5b632166f85a0b
ZwX1616 <zwx1616@gmail.com>
2020-04-20 16:29:46 -0700

    typo

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -631,11 +631,11 @@ static void ui_draw_vision_speed(UIState *s) {
   for (int i = 0; i < 8; i++){
     char f_str[32];
     snprintf(f_str, sizeof(f_str), "%d", scene->focusconf[i]);
-    nvgText(s->vg, viz_speed_x + viz_speed_w/2 + (i - 3.5) * 160, 880, ss_str, NULL);
+    nvgText(s->vg, viz_speed_x + viz_speed_w/2 + (i - 3.5) * 160, 880, f_str, NULL);
     snprintf(f_str, sizeof(f_str), "%d", scene->focusdat[2*i]);
-    nvgText(s->vg, viz_speed_x + viz_speed_w/2 + (i - 3.5) * 160, 960, ss_str, NULL);
+    nvgText(s->vg, viz_speed_x + viz_speed_w/2 + (i - 3.5) * 160, 960, f_str, NULL);
     snprintf(f_str, sizeof(f_str), "%d", scene->focusdat[2*i+1]);
-    nvgText(s->vg, viz_speed_x + viz_speed_w/2 + (i - 3.5) * 160, 1040, ss_str, NULL);
+    nvgText(s->vg, viz_speed_x + viz_speed_w/2 + (i - 3.5) * 160, 1040, f_str, NULL);
   }
 }
 
