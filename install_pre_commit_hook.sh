b3c636e7d1ae476ae110169b5bcf924afc70374f
Willem Melching <willem.melching@gmail.com>
2020-04-24 14:26:01 -0700

    remove that install script
    old-commit-hash: 397c5065baebf08758c9e812d2d61d731eab8ccc

--- install_pre_commit_hook.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-if [ -z $(which flake8) ]; then
-  echo "Installing flake8"
-  sudo pip install flake8
-fi
-
-echo "Setting up commit hook"
-cp -u --remove-destination pre-commit.sh .git/hooks/pre-commit
