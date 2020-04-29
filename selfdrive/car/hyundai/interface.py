7dc70a57e4d7f168ae1da5fadada3736569cc51d
TK211X <33460783+TK211X@users.noreply.github.com>
2020-04-29 16:32:25 -0400

    Update Elantra Values  (#1430)
    * Update Elantra Values one last time
    
    I got the params learner values from EON. These are the values it settled on after driving over a week.
    
    * Include Stiffness Params Value.
    
    * Update interface.py
    old-commit-hash: db3864b6ca31d6954b8d2b90a91d01d7286f6996

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -65,8 +65,8 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.00006
       ret.mass = 1275. + STD_CARGO_KG
       ret.wheelbase = 2.7
-      ret.steerRatio = 13.73   #Spec
-      tire_stiffness_factor = 0.385
+      ret.steerRatio = 15.4            # 14 is Stock | Settled Params Learner values are steerRatio: 15.401566348670535
+      tire_stiffness_factor = 0.385    # stiffnessFactor settled on 1.0081302973865127
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.minSteerSpeed = 32 * CV.MPH_TO_MS
