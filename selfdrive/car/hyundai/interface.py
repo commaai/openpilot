e1a2fbea5107544235df8c2e2b006f06bb9a056b
Willem Melching <willem.melching@gmail.com>
2020-04-30 14:13:33 -0700

    Move minSteerSpeed to base interface

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -28,8 +28,6 @@ class CarInterface(CarInterfaceBase):
     ret.steerLimitTimer = 0.4
     tire_stiffness_factor = 1.
 
-    ret.minSteerSpeed = 0.
-
     if candidate == CAR.SANTA_FE:
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 3982. * CV.LB_TO_KG + STD_CARGO_KG
