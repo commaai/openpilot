37534ad6556b5f0e9f99942f462cfc4a862d9d04
Harald Schafer <harald.the.engineer@gmail.com>
2020-04-30 03:10:42 -0700

    typo

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -127,7 +127,7 @@ class PathPlanner():
       # starting
       elif self.lane_change_state == LaneChangeState.laneChangeStarting:
         # fade out over .2s
-        self.lane_change_ll_prob = max(self.lane_change_ll_prob - DT_MDTL/5, 0.0)
+        self.lane_change_ll_prob = max(self.lane_change_ll_prob - DT_MDL/5, 0.0)
         # 98% certainty
         if lane_change_prob < 0.02 and self.lane_change_ll_prob < 0.01:
           self.lane_change_state = LaneChangeState.laneChangeFinishing
