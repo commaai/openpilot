56aad43948433886b16fccb0d0929d4b29ac894f
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-05-01 21:57:33 -0700

    longcontrol less args

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -397,8 +397,7 @@ class Controls:
     v_acc_sol = plan.vStart + dt * (a_acc_sol + plan.aStart) / 2.0
 
     # Gas/Brake PID loop
-    actuators.gas, actuators.brake = self.LoC.update(self.active, CS.vEgo, CS.brakePressed, CS.standstill, CS.cruiseState.standstill,
-                                                      self.v_cruise_kph, v_acc_sol, plan.vTargetFuture, a_acc_sol, self.CP)
+    actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, plan.vTargetFuture, a_acc_sol, self.CP)
     # Steering PID loop and lateral MPC
     actuators.steer, actuators.steerAngle, lac_log = self.LaC.update(self.active, CS, self.CP, path_plan)
 
