ccf365b596c06a149df067762554a33f5576454f
Martin Lillepuu <martin@mlp.ee>
2020-05-12 18:57:12 +0300

    update fake_button frequency and logic

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -15,13 +15,14 @@ class CarControllerParams():
       self.STEER_DRIVER_MULTIPLIER = 10  # weight driver torque heavily
     if car_fingerprint in (CAR.OUTBACK, CAR.LEGACY, CAR.FORESTER):
       self.STEER_DRIVER_ALLOWANCE = 600  # allowed driver torque before start limiting
-      self.STEER_DRIVER_MULTIPLIER = 1   # weight driver torque heavily
+      self.STEER_DRIVER_MULTIPLIER = 1   # weight driver torque
     self.STEER_DRIVER_FACTOR = 1         # from dbc
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     self.apply_steer_last = 0
     self.es_distance_cnt = -1
+    self.es_accel_cnt = -1
     self.es_lkas_cnt = -1
     self.fake_button_prev = 0
     self.steer_rate_limited = False
@@ -61,30 +62,32 @@ class CarController():
     ### DISENGAGE ###
 
     # button control
-    if (frame % 5) == 0 and CS.CP.carFingerprint in (CAR.OUTBACK, CAR.LEGACY, CAR.FORESTER):
-      # 1 = main, 2 = set shallow, 3 = set deep, 4 = resume shallow, 5 = resume deep
-      # disengage ACC when OP is disengaged
-      if (pcm_cancel_cmd):
-        fake_button = 1
-      # turn main on if off and past start-up state
-      elif not CS.out.cruiseState.available and CS.ready:
-        fake_button = 1
-      else:
-        fake_button = CS.button
+    if CS.CP.carFingerprint in (CAR.OUTBACK, CAR.LEGACY, CAR.FORESTER):
+      if self.es_accel_cnt != CS.es_accel_msg["Counter"]:
+        # 1 = main, 2 = set shallow, 3 = set deep, 4 = resume shallow, 5 = resume deep
+        # disengage ACC when OP is disengaged
+        if pcm_cancel_cmd:
+          fake_button = 1
+        # turn main on if off and past start-up state
+        elif not CS.out.cruiseState.available and CS.ready:
+          fake_button = 1
+        else:
+          fake_button = CS.button
 
-      # unstick previous mocked button press
-      if fake_button != 0 and fake_button == self.fake_button_prev:
-        fake_button = 0
-      self.fake_button_prev = fake_button
+        # unstick previous mocked button press
+        if fake_button == 1 and self.fake_button_prev == 1:
+          fake_button = 0
+        self.fake_button_prev = fake_button
 
-      can_sends.append(subarucan.create_es_throttle_control(self.packer, fake_button, CS.es_accel_msg))
-
-    ### ALERTS ###
+        can_sends.append(subarucan.create_es_throttle_control(self.packer, fake_button, CS.es_accel_msg))
+        self.es_accel_cnt = CS.es_accel_msg["Counter"]
 
     if CS.CP.carFingerprint == CAR.IMPREZA:
       if self.es_distance_cnt != CS.es_distance_msg["Counter"]:
         can_sends.append(subarucan.create_es_distance(self.packer, CS.es_distance_msg, pcm_cancel_cmd))
         self.es_distance_cnt = CS.es_distance_msg["Counter"]
+
+    ### ALERTS ###
 
       if self.es_lkas_cnt != CS.es_lkas_msg["Counter"]:
         can_sends.append(subarucan.create_es_lkas(self.packer, CS.es_lkas_msg, visual_alert, left_line, right_line))
