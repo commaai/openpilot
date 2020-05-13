ab7a9de705d79fbecd28e056007913612fb253cc
Tici <robbe@comma.ai>
2020-05-13 02:38:35 +0000

    Fixed boardd

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -1023,18 +1023,6 @@ int main() {
   // init libusb
   err = libusb_init(&ctx);
   assert(err == 0);
-<<<<<<< 2ae2c40b2943c502c4c66a44ac6ae5cb773c208b
-=======
-  libusb_set_debug(ctx, 3);
-
-  // connect to the board
-<<<<<<< 3ece71bc11b8098c1ae9a1e5ff733ab1a0110e09
-  //TODO: re-enable
-  // usb_retry_connect();
->>>>>>> WIP: tici pigeon tty
-=======
-  usb_retry_connect();
->>>>>>> cleanup dev additions
 
 #if LIBUSB_API_VERSION >= 0x01000106
   libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_INFO);
@@ -1042,16 +1030,10 @@ int main() {
   libusb_set_debug(ctx, 3);
 #endif
 
-<<<<<<< 3ece71bc11b8098c1ae9a1e5ff733ab1a0110e09
-<<<<<<< 2ae2c40b2943c502c4c66a44ac6ae5cb773c208b
-=======
-  // create threads
->>>>>>> cleanup dev additions
   pthread_t can_health_thread_handle;
   err = pthread_create(&can_health_thread_handle, NULL,
                        can_health_thread, NULL);
   assert(err == 0);
-<<<<<<< 3ece71bc11b8098c1ae9a1e5ff733ab1a0110e09
 
   // connect to the board
   pthread_mutex_lock(&usb_lock);
@@ -1059,11 +1041,6 @@ int main() {
   pthread_mutex_unlock(&usb_lock);
 
   // create threads
-  pthread_t can_health_thread_handle;
-  err = pthread_create(&can_health_thread_handle, NULL,
-                       can_health_thread_handle, NULL);
-  assert(err == 0);
-
   pthread_t can_send_thread_handle;
   err = pthread_create(&can_send_thread_handle, NULL,
                        can_send_thread, NULL);
