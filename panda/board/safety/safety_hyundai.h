502b71a4119663b386d857f31dd07fc4c549cb6e
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-05-15 17:24:23 +0300

    move mdps bus check

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -59,7 +59,27 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   int bus = GET_BUS(to_push);
 
   if (valid) {
-    if (addr == 593 && bus == 0) {
+    // check if we have a LCAN on Bus1
+    if (bus == 1 && (addr == 1296 || addr == 524)) {
+      if (hyundai_forward_bus1 || !hyundai_LCAN_on_bus1) {
+        hyundai_LCAN_on_bus1 = true;
+        hyundai_forward_bus1 = false;
+      }
+    }
+    // check if we have a MDPS on Bus1 and LCAN not on the bus
+    if (bus == 1 && (addr == 593 || addr == 897) && !hyundai_LCAN_on_bus1) {
+      if (!hyundai_forward_bus1) {
+        hyundai_mdps_bus = bus;
+        hyundai_forward_bus1 = true;
+      }
+    }
+    // check if we have a SCC on Bus1 and LCAN not on the bus
+    if (bus == 1 && addr == 1057 && !hyundai_LCAN_on_bus1) {
+      if (!hyundai_forward_bus1) {
+        hyundai_forward_bus1 = true;
+      }
+    }
+    if (addr == 593 && bus == hyundai_mdps_bus) {
       int torque_driver_new = ((GET_BYTES_04(to_push) & 0x7ff) * 0.79) - 808; // scale down new driver torque signal to match previous one
       // update array of samples
       update_sample(&torque_driver, torque_driver_new);
@@ -143,26 +163,6 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     // check if stock camera ECU is on bus 0
     if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && bus == 0 && addr == 832) {
       relay_malfunction_set();
-    }
-    // check if we have a LCAN on Bus1
-    if (bus == 1 && (addr == 1296 || addr == 524)) {
-      if (hyundai_forward_bus1 || !hyundai_LCAN_on_bus1) {
-        hyundai_LCAN_on_bus1 = true;
-        hyundai_forward_bus1 = false;
-      }
-    }
-    // check if we have a MDPS on Bus1 and LCAN not on the bus
-    if (bus == 1 && (addr == 593 || addr == 897) && !hyundai_LCAN_on_bus1) {
-      if (!hyundai_forward_bus1) {
-        hyundai_mdps_bus = bus;
-        hyundai_forward_bus1 = true;
-      }
-    }
-    // check if we have a SCC on Bus1 and LCAN not on the bus
-    if (bus == 1 && addr == 1057 && !hyundai_LCAN_on_bus1) {
-      if (!hyundai_forward_bus1) {
-        hyundai_forward_bus1 = true;
-      }
     }
   }
   return valid;
