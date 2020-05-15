206b6abe7dc3ee86bc187d9a0f574a1937cd00a7
George Hotz <72895+geohot@users.noreply.github.com>
2020-05-15 13:53:01 -0700

    thneed saves 45% of a core (#1512)
    * thneed runs the model
    
    * thneed is doing the hooking
    
    * set kernel args
    
    * thneeding the bufferS
    
    * print the images well
    
    * thneeds with better buffers
    
    * includes
    
    * disasm adreno
    
    * parse packets
    
    * disasm works
    
    * disasm better
    
    * more thneeding
    
    * much thneeding
    
    * much more thneeding
    
    * thneed works i think
    
    * thneed is patient
    
    * thneed works
    
    * 7.7%
    
    * gpuobj sync
    
    * yay, it mallocs now
    
    * cleaning it up, Thneed
    
    * sync objs and set power
    
    * thneed needs inputs and outputs
    
    * thneed in modeld
    
    * special modeld runs
    
    * can't thneed the DSP
    
    * test is weird
    
    * thneed modeld uses 6.4% CPU
    
    * add thneed to release
    
    * move to debug
    
    * delete some junk from the pr
    
    * always track the timestamp
    
    * timestamp hacks in thneed
    
    * create a new command queue
    
    * fix timestamp
    
    * pretty much back to what we had, you can't use SNPE with thneed
    
    * improve thneed test
    
    * disable save log
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 302d06ee709cb65dba39e836917f5693f0819efb

---
+++ selfdrive/modeld/thneed/debug/main.cc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf5043514cf5b79912e54da6550f8a1bf3f378644827154c47ea7fd31de4093a
+size 24549
---
+++ selfdrive/modeld/thneed/debug/test.cc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:24e057ba05689d07f82bb6f5cdca78e366d9dde9f29f18765c91954af6e6ff16
+size 2832
