04047d835fd0392426eb4ff22829a1dcd5eb0567
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-05-20 00:00:13 -0700

    fix run_docker_tests.sh

--- run_docker_tests.sh
+++ run_docker_tests.sh
@@ -2,21 +2,22 @@
 set -e
 
 SETUP="cd /tmp/openpilot && "
-RUN="docker run --shm-size 1G --rm tmppilot /bin/sh -c"
+BUILD="cd /tmp/openpilot && scons -c && scons -j$(nproc) && "
+RUN="docker run --shm-size 1G --rm tmppilot /bin/bash -c"
 
 docker build -t tmppilot -f Dockerfile.openpilot .
 
 $RUN "$SETUP cd /tmp/openpilot/selfdrive/test/ && ./test_fingerprints.py"
 $RUN "$SETUP ./flake8_openpilot.sh"
 $RUN "$SETUP ./pylint_openpilot.sh"
-$RUN "$SETUP python -m unittest discover common"
-$RUN "$SETUP python -m unittest discover opendbc/can"
-$RUN "$SETUP python -m unittest discover selfdrive/boardd"
-$RUN "$SETUP python -m unittest discover selfdrive/controls"
-$RUN "$SETUP python -m unittest discover selfdrive/loggerd"
-$RUN "$SETUP python -m unittest discover selfdrive/car"
-$RUN "$SETUP python -m unittest discover selfdrive/locationd"
-$RUN "$SETUP python -m unittest discover selfdrive/athena"
-$RUN "$SETUP cd /tmp/openpilot/selfdrive/test/longitudinal_maneuvers && OPTEST=1 ./test_longitudinal.py"
-$RUN "$SETUP cd /tmp/openpilot/selfdrive/test/process_replay/ && ./test_processes.py"
-$RUN "$SETUP mkdir -p /data/params && cd /tmp/openpilot/selfdrive/test/ && ./test_car_models.py"
+$RUN "$BUILD python -m unittest discover common && \
+             python -m unittest discover opendbc/can && \
+             python -m unittest discover selfdrive/boardd && \
+             python -m unittest discover selfdrive/controls && \
+             python -m unittest discover selfdrive/loggerd && \
+             python -m unittest discover selfdrive/car && \
+             python -m unittest discover selfdrive/locationd && \
+             python -m unittest discover selfdrive/athena"
+$RUN "$BUILD cd /tmp/openpilot/selfdrive/test/longitudinal_maneuvers && OPTEST=1 ./test_longitudinal.py"
+$RUN "$BUILD cd /tmp/openpilot/selfdrive/test/process_replay/ && ./test_processes.py"
+$RUN "$BUILD mkdir -p /data/params && cd /tmp/openpilot/selfdrive/test/ && ./test_car_models.py"
