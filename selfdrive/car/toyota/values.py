6765d93c0a9b06eab9ef41bdc82e30205dbb62c8
Thaixican <thaixican+github@gmail.com>
2020-05-22 00:40:11 -0700

    Add 2020 Lexus RX Hybrid to IGNORED_FINGERPRINTS and route to test_car_models per @pd0wm

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -267,7 +267,7 @@ FINGERPRINTS = {
 }
 
 # Don't use theses fingerprints for fingerprinting, they are still needed for ECU detection
-IGNORED_FINGERPRINTS = [CAR.RAV4H_TSS2, CAR.HIGHLANDERH_TSS2]
+IGNORED_FINGERPRINTS = [CAR.RAV4H_TSS2, CAR.HIGHLANDERH_TSS2, CAR.LEXUS_RXH_TSS2]
 
 FW_VERSIONS = {
   CAR.AVALON: {
