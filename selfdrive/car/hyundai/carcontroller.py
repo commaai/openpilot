c1bfe1f5a42516f8e139b4457f6edc0e9d9702ed
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-05-25 21:16:57 +0300

    send lfa-mfa to Sonata_H

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -167,7 +167,7 @@ class CarController():
       self.last_lead_distance = 0  
 
     # 20 Hz LFA MFA message
-    if frame % 5 == 0 and self.car_fingerprint in [CAR.SONATA, CAR.PALISADE]:
+    if frame % 5 == 0 and self.car_fingerprint in [CAR.SONATA, CAR.PALISADE, CAR.SONATA_H]:
       can_sends.append(create_lfa_mfa(self.packer, frame, enabled))
 
     return can_sends
