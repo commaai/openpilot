a94c3156a26baaef27cf54c5e5009c23a2901bc4
Martin Lillepuu <martin@mlp.ee>
2020-05-25 23:17:15 +0300

    FPv2 updates, add 2017 Outback FPv2

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -106,23 +106,35 @@ FINGERPRINTS = {
 
 FW_VERSIONS = {
   CAR.IMPREZA: {
+    # 2018 Crosstrek - EU / @martinl
     # Ecu, addr, subaddr: ROM ID
-    (Ecu.esp, 0x7b0, None): [b'\x7a\x94\x3f\x90\x00'],
-    (Ecu.eps, 0x746, None): [b'\x7a\xc0\x0c\x00'],
-    (Ecu.srs, 0x780, None): [b'\x00\x92\x15\x16\x00'],
-    (Ecu.fwdCamera, 0x787, None): [b'\x00\x00\x64\xb5\x1f\x40\x20\x0e'],
-    (Ecu.engine, 0x7e0, None): [b'\xaa\x61\x66\x73\x07'],
-    (Ecu.transmission, 0x7e1, None): [b'\xe3\xe5\x46\x31\x00'],
+    (Ecu.esp, 0x7b0, None): [b'z\x94?\x90\x00']
+    (Ecu.eps, 0x746, None): [b'z\xc0\x0c\x00']
+    (Ecu.srs, 0x780, None): [b'\x00\x92\x15\x16\x00']
+    (Ecu.fwdCamera, 0x787, None): [b'\x00\x00d\xb5\x1f@ \x0e']
+    (Ecu.engine, 0x7e0, None): [b'\xaaafs\x07']
+    (Ecu.transmission, 0x7e1, None): [b'\xe3\xe5F1\x00']
   },
   CAR.FORESTER: {
+    # 2018 Subaru Forester 2.5i Touring - NA / @Oreo
     # Ecu, addr, subaddr: ROM ID
     (Ecu.esp, 0x7b0, None): [b'}\x97\x14@'],
     (Ecu.eps, 0x746, None): [b'}\xc0\x10\x00'],
     (Ecu.srs, 0x780, None): [b'\x90\x00%\x00'],
     (Ecu.fwdCamera, 0x787, None): [b'\x00\x00d5\x1f@ \t'],
     (Ecu.engine, 0x7e0, None): [b'\xba"@p\x07'],
-    #(Ecu.transmission, 0x7e1, None): [b'\xe3\xe5\x46\x31\x00'],
+    (Ecu.transmission, 0x7e1, None): [b'\xdc\xf2``\x00'],
   },
+  CAR.OUTBACK: {
+    # 2017 Outback Limited 3.6r - NA / @Anthony
+    # Ecu, addr, subaddr: ROM ID
+    (Ecu.esp, 0x7b0, None): [b'{\x9a\xac\x00'],
+    (Ecu.eps, 0x746, None): [b'k\xb0\x00\x00'],
+    (Ecu.srs, 0x780, None): [b'\xf1\x00X\x06\x00'],
+    (Ecu.fwdCamera, 0x787, None): [b'\x00\x00c\xec\x1f@ \x04'],
+    (Ecu.engine, 0x7e0, None): [b'\xb4+@p\x07'],
+    (Ecu.transmission, 0x7e1, None): [b'\xf1\x00\xa4\x10@'],
+  }
 }
 
 
