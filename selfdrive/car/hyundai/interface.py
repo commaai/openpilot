fbbcdd0b43c2ecd732d2f99c77ceff4f03fa5255
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-05-25 21:08:05 +0300

    default value

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -221,7 +221,7 @@ class CarInterface(CarInterfaceBase):
     ret.sasBus = 1 if 688 in fingerprint[1] and 1296 not in fingerprint[1] else 0
     ret.sccBus = 0 if 1056 in fingerprint[0] else 1 if 1056 in fingerprint[1] and 1296 not in fingerprint[1] \
                                                                      else 2 if 1056 in fingerprint[2] else -1
-    ret.autoLcaEnabled = 1
+    ret.autoLcaEnabled = 0
 
     return ret
 
