f1d5d020db3b1dc6d0f0e93c89a2731c54ea57e1
AskAlice <alice@askalice.me>
2020-05-28 20:50:26 -0600

    add functions for creating SCC messages
    bump panda
    update panda

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -3,6 +3,94 @@ from selfdrive.car.hyundai.values import CAR, CHECKSUM
 
 hyundai_checksum = crcmod.mkCrcFun(0x11D, initCrc=0xFD, rev=False, xorOut=0xdf)
 
+def create_scc11(packer, enabled, count):
+  objValid = 0
+  objStatus = 0
+  objDist = 150
+  if enabled:
+    objValid = 1
+    objStatus = 1
+    objDist = 3
+  values = {
+    "MainMode_ACC": enabled,
+    "SCCInfoDisplay": 0,
+    "AliveCounterACC": count,
+    "VSetDis": 0,  # km/h velosity
+    "ObjValid": objValid,
+    "DriverAlertDisplay": 0,
+    "TauGapSet": 1,
+    "Navi_SCC_Curve_Status": 0,
+    "Navi_SCC_Curve_Act": 0,
+    "Navi_SCC_Camera_Act": 0,
+    "Navi_SCC_Camera_Status": 0,
+    "ACC_ObjStatus": objStatus,
+    "ACC_ObjDist": objDist,
+    "ACC_ObjLatPos":0,
+    "ACC_ObjRelSpd":0,
+  }
+  return packer.make_can_msg("SCC11", 0, values)
+
+def create_scc12(packer, apply_accel, enabled, cnt, scc12):
+  values = {
+    "CF_VSM_Prefill": 0,
+    "CF_VSM_DecCmdAct": 0,
+    "CF_VSM_HBACmd": 0,
+    "CF_VSM_Warn": 0,
+    "CF_VSM_Stat": 0,
+    "CF_VSM_BeltCmd": 0,
+    "ACCFailInfo": 0,
+    "ACCMode": enabled,
+    "StopReq": 0,
+    "CR_VSM_DecCmd": 0,
+    "aReqMax": apply_accel+3.0 if enabled else 0,
+    "TakeOverReq": 0,
+    "PreFill": 0,
+    "aReqMin": apply_accel+3.0 if enabled else -10.23,
+    "CF_VSM_ConfMode": 0,
+    "AEB_Failinfo": 0,
+    "AEB_Status": 0,
+    "AEB_CmdAct": 0,
+    "AEB_StopReq": 0,
+    "CR_VSM_Alive": cnt,
+    "CR_VSM_ChkSum": 0,
+  }
+  dat = packer.make_can_msg("SCC12", 0, values)[2]
+  values["CR_VSM_ChkSum"] = 16 - sum([sum(divmod(i, 16)) for i in dat]) % 16
+
+  return packer.make_can_msg("SCC12", 0, values)
+
+def create_scc13(packer):
+  values = {
+    "SCCDrvModeRValue" : 2,
+    "SCC_Equip" : 1,
+    "AebDrvSetStatus" : 0,
+  }
+  return packer.make_can_msg("SCC13", 0, values)
+
+def create_scc14(packer, enabled):
+  if enabled:
+    values = {
+      "JerkUpperLimit" : 3.2,
+      "JerkLowerLimit" : 0.1,
+      "SCCMode2" : 1,
+      "ComfortBandUpper" : 0.24,
+      "ComfortBandLower" : 0.24,
+    }
+  else:
+    values = {
+      "JerkUpperLimit" : 0,
+      "JerkLowerLimit" : 0,
+      "SCCMode2" : 0,
+      "ComfortBandUpper" : 0,
+      "ComfortBandLower" : 0,
+    }
+  return packer.make_can_msg("SCC14", 0, values)
+
+def create_4a2SCC(packer):
+  values = {
+    "Paint_1": 1
+  }
+  return packer.make_can_msg("4a2SCC", 0, values)
 
 def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
                   lkas11, sys_warning, sys_state, enabled,
