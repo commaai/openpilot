e9bedcb6ad56f549136152d10b9ef89206f4fdef
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-05-30 04:40:16 -0700

    e115 e116

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -20,7 +20,7 @@ def calc_cruise_offset(offset, speed):
 
 
 def get_can_signals(CP):
-# this function generates lists for signal, messages and initial values
+  # this function generates lists for signal, messages and initial values
   signals = [
       ("XMISSION_SPEED", "ENGINE_DATA", 0),
       ("WHEEL_SPEED_FL", "WHEEL_SPEEDS", 0),
