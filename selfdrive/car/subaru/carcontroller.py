e870a5259546e69d8fbb17e90b819aa0780c1515
Martin Lillepuu <martin@mlp.ee>
2020-05-31 22:17:43 +0300

    Update preglobal driver torque scaling

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -10,12 +10,11 @@ class CarControllerParams():
     self.STEER_STEP = 2                  # how often we update the steer cmd
     self.STEER_DELTA_UP = 50             # torque increase per refresh, 0.8s to max
     self.STEER_DELTA_DOWN = 70           # torque decrease per refresh
-    if car_fingerprint in [CAR.IMPREZA, CAR.ASCENT]:
+    if car_fingerprint in [CAR.FORESTER, CAR.IMPREZA, CAR.ASCENT]:
       self.STEER_DRIVER_ALLOWANCE = 60   # allowed driver torque before start limiting
-      self.STEER_DRIVER_MULTIPLIER = 10  # weight driver torque heavily
     if car_fingerprint in [CAR.OUTBACK, CAR.LEGACY, CAR.FORESTER]:
-      self.STEER_DRIVER_ALLOWANCE = 600  # allowed driver torque before start limiting
-      self.STEER_DRIVER_MULTIPLIER = 1   # weight driver torque
+      self.STEER_DRIVER_ALLOWANCE = 75  # allowed driver torque before start limiting
+    self.STEER_DRIVER_MULTIPLIER = 10   # weight driver torque heavily
     self.STEER_DRIVER_FACTOR = 1         # from dbc
 
 class CarController():
@@ -78,7 +77,7 @@ class CarController():
         can_sends.append(subarucan.create_es_throttle_control(self.packer, fake_button, CS.es_accel_msg))
         self.es_accel_cnt = CS.es_accel_msg["Counter"]
 
-    if CS.CP.carFingerprint in [CAR.IMPREZA, CAR.ASCENT]:
+    if CS.CP.carFingerprint in [CAR.FORESTER, CAR.IMPREZA, CAR.ASCENT]:
       if self.es_distance_cnt != CS.es_distance_msg["Counter"]:
         can_sends.append(subarucan.create_es_distance(self.packer, CS.es_distance_msg, pcm_cancel_cmd))
         self.es_distance_cnt = CS.es_distance_msg["Counter"]
