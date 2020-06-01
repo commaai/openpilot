fa7d8f6e1b6c8a27dacff2bbff89b3cbe79c5609
Willem Melching <willem.melching@gmail.com>
2020-06-01 14:39:06 -0700

    remove unused line in nissan carstate

--- selfdrive/car/nissan/carstate.py
+++ selfdrive/car/nissan/carstate.py
@@ -11,7 +11,6 @@ class CarState(CarStateBase):
   def __init__(self, CP):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
-    self.seatbeltUnlatched = True
 
     self.shifter_values = can_define.dv["GEARBOX"]["GEAR_SHIFTER"]
 
