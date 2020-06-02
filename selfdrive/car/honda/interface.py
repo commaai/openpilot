5abe4f99eec3633bc30fda47c272e0f19a840a29
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-02 16:44:58 -0700

    fix speed too low

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -482,12 +482,12 @@ class CarInterface(CarInterfaceBase):
       events.add(EventName.parkBrake)
 
     if self.CP.enableCruise and ret.vEgo < self.CP.minEnableSpeed:
-      events.add(EventName.speedTooLow)
+      events.add(EventName.belowEngageSpeed)
 
     # it can happen that car cruise disables while comma system is enabled: need to
     # keep braking if needed or if the speed is very low
     if self.CP.enableCruise and not ret.cruiseState.enabled \
-       and (c.actuators.brake <= 0. or not self.CP.openpilotLongitudinalControl) and (self.CP.minEnableSpeed > 0):
+       and (c.actuators.brake <= 0. or not self.CP.openpilotLongitudinalControl):
       # non loud alert if cruise disables below 25mph as expected (+ a little margin)
       if ret.vEgo < self.CP.minEnableSpeed + 2.:
         events.add(EventName.speedTooLow)
