3c8271febefaa0ed256281c4b1633a8bc54d5df2
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-06-03 07:58:36 +0300

    fix BSM merge

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -190,9 +190,9 @@ class Controls:
                                         LaneChangeState.laneChangeFinishing]:
       self.events.add(EventName.laneChange)
     # lane change bsm alerts 
-    if self.sm['pathPlan'].laneChangeBSM == LaneChangeBSM.left:
+    if self.sm['pathPlan'].laneChangeBSM == laneChangeBSM.left:
       self.events.add(EventName.preventLCA)
-    if self.sm['pathPlan'].LaneChangeBSM == LaneChangeBSM.right:
+    if self.sm['pathPlan'].LaneChangeBSM == laneChangeBSM.right:
       self.events.add(EventName.preventLCA)
 
     if self.can_rcv_error or (not CS.canValid and self.sm.frame > 5 / DT_CTRL):
