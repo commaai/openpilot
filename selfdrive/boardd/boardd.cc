d7408eff831fb09fc73932fcf09820572146b2c2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-04 14:40:04 -0700

    remove common/messaging.h

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -18,7 +18,6 @@
 #include "cereal/gen/cpp/car.capnp.h"
 
 #include "common/util.h"
-#include "common/messaging.h"
 #include "common/params.h"
 #include "common/swaglog.h"
 #include "common/timing.h"
