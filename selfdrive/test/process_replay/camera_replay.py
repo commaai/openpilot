da306f3a140f554777ddbc938ac741bd433c5f86
Comma Device <device@comma.ai>
2020-06-04 14:15:57 -0400

    fix build

--- selfdrive/test/process_replay/camera_replay.py
+++ selfdrive/test/process_replay/camera_replay.py
@@ -4,18 +4,18 @@ import sys
 import time
 from tqdm import tqdm
 
-import cereal.messaging as messaging
 from common.android import ANDROID
+if ANDROID:
+  os.environ['QCOM_REPLAY'] = "1"
+import selfdrive.manager as manager
+
+import cereal.messaging as messaging
 from tools.lib.framereader import FrameReader
 from tools.lib.logreader import LogReader
 from selfdrive.test.openpilotci import BASE_URL, get_url
 from selfdrive.test.process_replay.compare_logs import compare_logs, save_log
 from selfdrive.test.process_replay.test_processes import format_diff
 from selfdrive.version import get_git_commit
-
-if ANDROID:
-  os.environ['QCOM_REPLAY'] = "1"
-import selfdrive.manager as manager
 
 TEST_ROUTE = "5b7c365c50084530|2020-04-15--16-13-24"
 
@@ -84,8 +84,9 @@ if __name__ == "__main__":
     ref_commit = open("model_replay_ref_commit").read().strip()
     log_fn = "%s_%s_%s.bz2" % (TEST_ROUTE, "model", ref_commit)
     cmp_log = LogReader(BASE_URL + log_fn)
-    result = compare_logs(cmp_log, log_msgs, ignore_fields=['logMonoTime', 'valid'])
-    diff1, diff2, failed = format_diff(result)
+    results = {TEST_ROUTE: {}}
+    results[TEST_ROUTE]["modeld"] = compare_logs(cmp_log, log_msgs, ignore_fields=['logMonoTime', 'valid'])
+    diff1, diff2, failed = format_diff(results, ref_commit)
 
     print(diff1)
     sys.exit(int(failed))
