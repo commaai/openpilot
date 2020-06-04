7db4e5bd0573ff8ed0cdd841d909701f27886231
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-04 14:27:54 -0700

    update run_docker_tests.sh

--- run_docker_tests.sh
+++ run_docker_tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
+# NOTE: this may be not an up-to-date list of tests and checks, see .github/workflows/test.yaml for the current list of tests
+
 SETUP="cd /tmp/openpilot && "
 BUILD="cd /tmp/openpilot && scons -c && scons -j$(nproc) && "
 RUN="docker run --shm-size 1G --rm tmppilot /bin/bash -c"
 
 docker build -t tmppilot -f Dockerfile.openpilot .
 
-$RUN "$SETUP cd /tmp/openpilot/selfdrive/test/ && ./test_fingerprints.py"
-$RUN "$SETUP ./flake8_openpilot.sh"
-$RUN "$SETUP ./pylint_openpilot.sh"
+$RUN "$SETUP cd selfdrive/test/ && ./test_fingerprints.py"
+$RUN "$SETUP git init && git add -A && pre-commit run --all"
 $RUN "$BUILD python -m unittest discover common && \
              python -m unittest discover opendbc/can && \
              python -m unittest discover selfdrive/boardd && \
