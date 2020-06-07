840d4b0aa6d490b3a2c9078053412b08d54d8985
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-06-07 23:48:23 +0300

    fix typo

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -268,7 +268,7 @@ class Controls:
     self.v_cruise_kph_last = self.v_cruise_kph
 
     # if stock cruise is completely disabled, then we can use our own set speed logic
-    self.CP.enableCruise = self.IC.CP.enableCruise
+    self.CP.enableCruise = self.CI.CP.enableCruise
     if not self.CP.enableCruise:
       self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, CS.buttonEvents, self.enabled)
     elif self.CP.enableCruise and CS.cruiseState.enabled:
