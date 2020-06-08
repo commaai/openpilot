eae2575fa00a48d6044b90898726b38eb6b2ca8f
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-06-08 11:23:40 +0300

    remove unnecessarily cancel spamming

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -398,7 +398,7 @@ class Controls:
     CC.actuators = actuators
 
     CC.cruiseControl.override = True
-    CC.cruiseControl.cancel = not self.CP.enableCruise or (not self.enabled and CS.cruiseState.enabled)
+    CC.cruiseControl.cancel = not self.enabled and CS.cruiseState.enabled
 
     # Some override values for Honda
     # brake discount removes a sharp nonlinearity
