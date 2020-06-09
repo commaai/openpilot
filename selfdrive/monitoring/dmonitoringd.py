d4858e5ff5753f8a800f3e90cb4c0f69213b87f3
Adeeb <8762862+adeebshihadeh@users.noreply.github.com>
2020-06-09 13:25:15 -0700

    Move driver monitoring out of controls (#1642)
    * move dmonitoring out of controls
    
    * run monitoring unit tests in CI

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -5,7 +5,7 @@ from common.realtime import set_realtime_priority
 from common.params import Params
 import cereal.messaging as messaging
 from selfdrive.controls.lib.events import Events
-from selfdrive.controls.lib.driver_monitor import DriverStatus, MAX_TERMINAL_ALERTS, MAX_TERMINAL_DURATION
+from selfdrive.monitoring.driver_monitor import DriverStatus, MAX_TERMINAL_ALERTS, MAX_TERMINAL_DURATION
 from selfdrive.locationd.calibration_helpers import Calibration
 
 def dmonitoringd_thread(sm=None, pm=None):
