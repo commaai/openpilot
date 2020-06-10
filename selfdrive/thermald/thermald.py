f7308cca9e3754ee8fb522b431f9fd93ad5f9821
Willem Melching <willem.melching@gmail.com>
2020-06-10 15:21:43 -0700

    Increase temperature range
    old-commit-hash: c82fda995b2b79e9956e42fe641cfe31d4b3932d

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -272,10 +272,10 @@ def thermald_thread():
     if max_cpu_temp > 107. or bat_temp >= 63.:
       # onroad not allowed
       thermal_status = ThermalStatus.danger
-    elif max_comp_temp > 92.5 or bat_temp > 60.:  # CPU throttling starts around ~90C
+    elif max_comp_temp > 100.0 or bat_temp > 60.:
       # hysteresis between onroad not allowed and engage not allowed
       thermal_status = clip(thermal_status, ThermalStatus.red, ThermalStatus.danger)
-    elif max_cpu_temp > 87.5:
+    elif max_cpu_temp > 97.0:
       # hysteresis between engage not allowed and uploader not allowed
       thermal_status = clip(thermal_status, ThermalStatus.yellow, ThermalStatus.red)
     elif max_cpu_temp > 80.0:
