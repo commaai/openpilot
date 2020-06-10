a0016a5bc27df9e7c8df1e529f83fbaafe7786d5
Martin Lillepuu <martin@mlp.ee>
2020-06-10 16:46:34 +0300

    Change obd_query output type, fix pylint warning

--- selfdrive/car/obd_query.py
+++ selfdrive/car/obd_query.py
@@ -9,15 +9,15 @@ TX_ADDR = 0x7e0
 OBD_DIAG_REQUEST = b'\x01\x1C'
 OBD_DIAG_RESPONSE = b'\x41\x1C'
 
-def get_obd_pid28(logcan, sendcan, bus, timeout=0.1, retry=5, debug=False):
+def get_car_country(logcan, sendcan, bus, timeout=0.1, retry=5, debug=False):
   print(f"OBD2 query {hex(TX_ADDR)} ...")
   for i in range(retry):
     try:
       query = IsoTpParallelQuery(sendcan, logcan, bus, [TX_ADDR], [OBD_DIAG_REQUEST], [OBD_DIAG_RESPONSE], debug=debug)
       for addr, dat in query.get_data(timeout).items():
         print("query response")
-        print(dat)
-        return dat
+        print(f"{hex(addr)} {bytes.hex(dat)}")
+        return bytes.hex(dat)
       print(f"query retry ({i+1}) ...")
     except Exception:
       cloudlog.warning(f"OBD2 query exception: {traceback.format_exc()}")
@@ -30,5 +30,5 @@ if __name__ == "__main__":
   sendcan = messaging.pub_sock('sendcan')
   logcan = messaging.sub_sock('can')
   time.sleep(1)
-  ret = get_obd_pid28(logcan, sendcan, 1, debug=False)
+  ret = get_car_country(logcan, sendcan, 1, debug=False)
   print(f"result: {ret}")
