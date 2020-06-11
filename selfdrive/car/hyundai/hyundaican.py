2d7cdb2639dffbd67445f65316f901c61f98ba7c
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-06-11 10:42:59 +0300

    update SCC values

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -61,13 +61,14 @@ def create_clu11(packer, frame, bus, clu11, button, speed):
   values["CF_Clu_AliveCnt1"] = frame // 2 % 0x10
   return packer.make_can_msg("CLU11", bus, values)
 
-def create_scc12(packer, apply_accel, enabled, cnt, scc12):
+def create_scc12(packer, apply_accel, enabled, cnt, scc_live, scc12):
   values = scc12
-  values["ACCMode"] = 1  if enabled else 0
   values["aReqRaw"] = apply_accel if enabled else 0 #aReqMax
   values["aReqValue"] = apply_accel if enabled else 0 #aReqMin
   values["CR_VSM_Alive"] = cnt
   values["CR_VSM_ChkSum"] = 0
+  if not scc_live:
+    values["ACCMode"] = 1  if enabled else 0 # 2 if gas padel pressed
 
   dat = packer.make_can_msg("SCC12", 0, values)[2]
   values["CR_VSM_ChkSum"] = 16 - sum([sum(divmod(i, 16)) for i in dat]) % 16
@@ -106,12 +107,13 @@ def create_lfa_mfa(packer, frame, enabled):
 
   return packer.make_can_msg("LFAHDA_MFC", 0, values)
 
-def create_scc11(packer, frame, enabled, set_speed, lead_visible, scc11):
+def create_scc11(packer, frame, enabled, set_speed, lead_visible, scc_live, scc11):
   values = scc11
-  values["MainMode_ACC"] = 1
   values["AliveCounterACC"] = frame // 2 % 0x10
-  values["VSetDis"] = set_speed if enabled else 150
-  values["ObjValid"] = 1 if enabled else 0
+  if not scc_live:
+    values["MainMode_ACC"] = 1
+    values["VSetDis"] = set_speed
+    values["ObjValid"] = 1 if enabled else 0
 #  values["ACC_ObjStatus"] = lead_visible
 
   return packer.make_can_msg("SCC11", 0, values)
