103a6e7cc8c1aef9d688305370abb458dd6a198e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-15 13:49:39 -0700

    block entry on non-adaptive cruise mode

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -485,6 +485,12 @@ EVENTS = {
                               duration_hud_alert=0.),
   },
 
+  # TODO: make sure wrongCruiseMode shows instead of wrongCarMode when appropriate
+  EventName.wrongCruiseMode: {
+    #ET.USER_DISABLE: EngagementAlert(AudibleAlert.chimeDisengage),
+    ET.NO_ENTRY: NoEntryAlert("Wrong Cruise Mode"),
+  },
+
   EventName.steerTempUnavailable: {
     ET.WARNING: Alert(
       "TAKE CONTROL",
