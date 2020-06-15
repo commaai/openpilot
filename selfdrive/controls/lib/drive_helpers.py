b243b78b5b1bda376e5897e43fa4dd4e8d1c1466
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-06-15 17:34:03 +0300

    change button timer

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -49,21 +49,21 @@ def update_v_cruise(v_cruise_kph, buttonEvents, enabled, metric):
         if b.pressed and not button_pressed_cnt:
           button_pressed_cnt = 1
           button_prev = b.type
-        elif not b.pressed:
-          if not long_pressed and b.type == ButtonType.accelCruise and button_pressed_cnt > 20:
+        elif not b.pressed and button_pressed_cnt:
+          if not long_pressed and b.type == ButtonType.accelCruise:
             v_cruise_kph += 1 if metric else 1 * CV.MPH_TO_KPH
-          elif not long_pressed and b.type == ButtonType.decelCruise and button_pressed_cnt > 20:
+          elif not long_pressed and b.type == ButtonType.decelCruise:
             v_cruise_kph -= 1 if metric else 1 * CV.MPH_TO_KPH
           long_pressed = False
           button_pressed_cnt = 0
-    if button_pressed_cnt > 200:
+    if button_pressed_cnt > 25:
       long_pressed = True
       V_CRUISE_DELTA = V_CRUISE_DELTA_KM if metric else V_CRUISE_DELTA_MI
       if button_prev == ButtonType.accelCruise:
         v_cruise_kph += V_CRUISE_DELTA - v_cruise_kph % V_CRUISE_DELTA
       elif button_prev == ButtonType.decelCruise:
         v_cruise_kph -= V_CRUISE_DELTA - -v_cruise_kph % V_CRUISE_DELTA
-      button_pressed_cnt %= 200
+      button_pressed_cnt %= 25
     v_cruise_kph = clip(v_cruise_kph, V_CRUISE_MIN, V_CRUISE_MAX)
 
   return v_cruise_kph
