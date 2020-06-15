6ec31856b39e038d4e58521ea8fbbd36d5e51702
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-14 22:21:15 -0400

    modify compute_gb to match HKG accel output
    factor of 10 is right for HKG accel output. This is tested on Hyundai genesis with OP long

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -9,7 +9,7 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def compute_gb(accel, speed):
-    return float(accel) / 3.0
+    return float(accel) / 10.0
 
   @staticmethod
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=[]):  # pylint: disable=dangerous-default-value
