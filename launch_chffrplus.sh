3ad7346daf1a926da9d3d184c7af8e25dfbbd6e7
George Hotz <72895+geohot@users.noreply.github.com>
2020-06-16 14:51:55 -0700

    Revert broken NEOS kernel (#1723)
    * untested
    
    * bump kernel
    old-commit-hash: 58c1504f1a54b3c544b20d5c82f4e90e88276d8a

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -93,6 +93,10 @@ function launch {
       rm -r $BASEDIR/.sconsign.dblite
     fi
     "$DIR/installer/updater/updater" "file://$DIR/installer/updater/update.json"
+  else
+    if [[ $(uname -v) == "#1 SMP PREEMPT Wed Jun 10 12:40:53 PDT 2020" ]]; then
+      "$DIR/installer/updater/updater" "file://$DIR/installer/updater/update_kernel.json"
+    fi
   fi
 
 
