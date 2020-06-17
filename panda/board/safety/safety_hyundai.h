c8d558b33a1a0082710af64082675ab83f2d687e
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-06-17 15:18:26 +0300

    bug

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -329,7 +329,7 @@ static int hyundai_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
       }
     }
     if (bus_num == 2) {
-      if (!OP_LKAS_live || (addr != 832 && addr != 1157) {
+      if (!OP_LKAS_live || (addr != 832 && addr != 1157)) {
         if ((addr != 1057) || (!OP_SCC_live)) {
           bus_fwd = hyundai_forward_bus1 ? 10 : 0;
         } else {
