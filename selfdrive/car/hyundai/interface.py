8f409cab14ec1fb9137fb6ca523b660ef1440ca0
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-06-18 19:29:06 +0300

    fix cruise fault event

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -306,7 +306,7 @@ class CarInterface(CarInterfaceBase):
 
     events = self.create_common_events(ret)
 
-    if not self.CC.longcontrol and self.CS.cruise_unavail:
+    if self.CC.longcontrol and self.CS.cruise_unavail:
       events.add(EventName.brakeUnavailable)
     if abs(ret.steeringAngle) > 90. and EventName.steerTempUnavailable not in events.events:
       events.add(EventName.steerTempUnavailable)
