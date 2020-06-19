83f6ec221f3311625bc21f4f30860490e023d41a
George Hotz <72895+geohot@users.noreply.github.com>
2020-06-19 15:53:48 -0700

    c++ify thneed to remove memory leaks (#1737)
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 01a486308d7fbcd661d8f8786c140222703f659a

--- selfdrive/modeld/thneed/thneed.h
+++ selfdrive/modeld/thneed/thneed.h
@@ -5,11 +5,14 @@
 #include <vector>
 #include <CL/cl.h>
 
+using namespace std;
+
 class Thneed;
 
 class GPUMalloc {
   public:
     GPUMalloc(int size, int fd);
+    ~GPUMalloc();
     void *alloc(int size);
   private:
     uint64_t base;
@@ -34,7 +37,7 @@ class Thneed {
     void stop();
     void execute(float **finputs, float *foutput, bool slow=false);
 
-    std::vector<cl_mem> inputs;
+    vector<cl_mem> inputs;
     cl_mem output;
 
     cl_command_queue command_queue;
@@ -43,9 +46,9 @@ class Thneed {
     // protected?
     int record;
     int timestamp;
-    GPUMalloc *ram;
-    std::vector<CachedCommand *> cmds;
-    std::vector<std::pair<int, struct kgsl_gpuobj_sync_obj *> > syncobjs;
+    unique_ptr<GPUMalloc> ram;
+    vector<unique_ptr<CachedCommand> > cmds;
+    vector<string> syncobjs;
     int fd;
 };
 
