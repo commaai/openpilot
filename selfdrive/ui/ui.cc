e111565b72a291aa658af6cc9081c29131a64294
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-19 17:55:54 -0700

    more friendly

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -862,17 +862,19 @@ int main(int argc, char* argv[]) {
         LOGE("Controls failed to start");
         s->scene.alert_text1 = "openpilot Unavailable";
         s->scene.alert_text2 = "Controls Failed to Start";
+        s->scene.alert_size = cereal::ControlsState::AlertSize::MID;
+        update_status(s, STATUS_WARNING);
       } else if (s->controls_seen) {
         // car is started, but controls is lagging or died
         LOGE("Controls unresponsive");
         s->scene.alert_text1 = "TAKE CONTROL IMMEDIATELY";
         s->scene.alert_text2 = "Controls Unresponsive";
+        s->scene.alert_size = cereal::ControlsState::AlertSize::FULL;
+        update_status(s, STATUS_ALERT);
         if (s->scene.alert_text2 != "Controls Unresponsive") {
           s->sound.play(AudibleAlert::CHIME_WARNING_REPEAT, 3); // loop sound 3 times
         }
       }
-      s->scene.alert_size = cereal::ControlsState::AlertSize::FULL;
-      update_status(s, STATUS_ALERT);
       ui_draw_vision_alert(s, s->scene.alert_size, s->status, s->scene.alert_text1.c_str(), s->scene.alert_text2.c_str());
     }
 
