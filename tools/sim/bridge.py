d11e48e1cdb7a67bf81ef50eab969936d1f60087
Seba Davalle <sebadavalle@gmail.com>
2020-06-24 12:12:41 -0300

    bridge optimized to run in low resources machines

--- tools/sim/bridge.py
+++ tools/sim/bridge.py
@@ -15,9 +15,9 @@ from lib.helpers import FakeSteeringWheel
 from selfdrive.car.honda.values import CruiseButtons
 
 parser = argparse.ArgumentParser(description='Bridge between CARLA and openpilot.')
-parser.add_argument('--autopilot', action='store_true')
-parser.add_argument('--joystick', action='store_true')
-parser.add_argument('--realmonitoring', action='store_true')
+parser.add_argument('-a','--autopilot', action='store_true')
+parser.add_argument('-j','--joystick', action='store_true')
+parser.add_argument('-r','--realmonitoring', action='store_true')
 args = parser.parse_args()
 
 pm = messaging.PubMaster(['frame', 'sensorEvents', 'can'])
@@ -27,7 +27,11 @@ W, H = 1164, 874
 def cam_callback(image):
   img = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
   img = np.reshape(img, (H, W, 4))
-  img = img[:, :, [0, 1, 2]].copy()
+  img = img[:, :, :3]
+  img = img[:, :, ::-1]
+  # img = img[:, :, [0, 1, 2]].copy()
+
+  #print(img.tostring())
 
   dat = messaging.new_message('frame')
   dat.frame = {
@@ -38,7 +42,7 @@ def cam_callback(image):
   pm.send('frame', dat)
 
 def imu_callback(imu):
-  #print(imu, imu.accelerometer)
+  print(imu, imu.accelerometer)
 
   dat = messaging.new_message('sensorEvents', 2)
   dat.sensorEvents[0].sensor = 4
@@ -80,138 +84,164 @@ def go(q):
   threading.Thread(target=health_function).start()
   threading.Thread(target=fake_driver_monitoring).start()
 
-  client = carla.Client("127.0.0.1", 2000)
-  client.set_timeout(5.0)
-  world = client.load_world('Town04')
-  settings = world.get_settings()
-  settings.fixed_delta_seconds = 0.05
-  world.apply_settings(settings)
+  world = None
+  camera = None
+  vehicle = None
+  imu = None
 
-  weather = carla.WeatherParameters(
-      cloudyness=0.1,
-      precipitation=0.0,
-      precipitation_deposits=0.0,
-      wind_intensity=0.0,
-      sun_azimuth_angle=15.0,
-      sun_altitude_angle=75.0)
-  world.set_weather(weather)
+  try:
+    client = carla.Client("127.0.0.1", 2000)
+    client.set_timeout(5.0)
 
-  blueprint_library = world.get_blueprint_library()
-  # for blueprint in blueprint_library.filter('sensor.*'):
-  #    print(blueprint.id)
-  # exit(0)
+    world = client.get_world()
+    world_map = world.get_map()
 
-  world_map = world.get_map()
-  vehicle_bp = random.choice(blueprint_library.filter('vehicle.tesla.*'))
-  vehicle = world.spawn_actor(vehicle_bp, world_map.get_spawn_points()[16])
+    blueprint_library = world.get_blueprint_library()
 
-  # make tires less slippery
-  wheel_control = carla.WheelPhysicsControl(tire_friction=5)
-  physics_control = vehicle.get_physics_control()
-  physics_control.mass = 1326
-  physics_control.wheels = [wheel_control]*4
-  physics_control.torque_curve = [[20.0, 500.0], [5000.0, 500.0]]
-  physics_control.gear_switch_time = 0.0
-  vehicle.apply_physics_control(physics_control)
+    print ("\n\n #available vehicles: \n")
+    for blueprint in blueprint_library.filter('vehicle.*'):
+       print(blueprint.id)
+    
+    print ("\n\n #available sensors: \n")
+    for blueprint in blueprint_library.filter('sensor.*'):
+       print(blueprint.id)
 
-  if args.autopilot:
-    vehicle.set_autopilot(True)
-  # print(vehicle.get_speed_limit())
+    # Get a random blueprint.
+    vehicle_bp = random.choice(blueprint_library.filter("vehicle.*"))
+    vehicle_bp.set_attribute('role_name', 'hero')
+    if vehicle_bp.has_attribute('color'):
+        color = random.choice(vehicle_bp.get_attribute('color').recommended_values)
+        vehicle_bp.set_attribute('color', color)
 
-  blueprint = blueprint_library.find('sensor.camera.rgb')
-  blueprint.set_attribute('image_size_x', str(W))
-  blueprint.set_attribute('image_size_y', str(H))
-  blueprint.set_attribute('fov', '70')
-  blueprint.set_attribute('sensor_tick', '0.05')
-  transform = carla.Transform(carla.Location(x=0.8, z=1.45))
-  camera = world.spawn_actor(blueprint, transform, attach_to=vehicle)
-  camera.listen(cam_callback)
+    while vehicle is None:
+      spawn_points = world_map.get_spawn_points()
+      spawn_point = random.choice(spawn_points) if spawn_points else carla.Transform()
+      vehicle = world.try_spawn_actor(vehicle_bp, spawn_point)
 
-  # reenable IMU
-  imu_bp = blueprint_library.find('sensor.other.imu')
-  imu = world.spawn_actor(imu_bp, transform, attach_to=vehicle)
-  imu.listen(imu_callback)
+    if args.autopilot:
+      vehicle.set_autopilot(True)
+    
+    print(vehicle.get_speed_limit())
+    if vehicle is not None:
+      print("\n\n -- > vehicle has been configured!!!")
 
-  def destroy():
-    print("clean exit")
-    imu.destroy()
-    camera.destroy()
-    vehicle.destroy()
-    print("done")
-  atexit.register(destroy)
+    # make tires less slippery
+    wheel_control = carla.WheelPhysicsControl(tire_friction=5)
+    physics_control = vehicle.get_physics_control()
+    physics_control.mass = 1326
+    physics_control.wheels = [wheel_control]*4
+    physics_control.torque_curve = [[20.0, 500.0], [5000.0, 500.0]]
+    physics_control.gear_switch_time = 0.0
+    vehicle.apply_physics_control(physics_control)
 
-  # can loop
-  sendcan = messaging.sub_sock('sendcan')
-  rk = Ratekeeper(100, print_delay_threshold=0.05)
+    blueprint = blueprint_library.find('sensor.camera.rgb')
+    blueprint.set_attribute('image_size_x', str(W))
+    blueprint.set_attribute('image_size_y', str(H))
+    blueprint.set_attribute('fov', '70')
+    blueprint.set_attribute('sensor_tick', '0.05')
+    #transform = carla.Transform(carla.Location(x=0.8, z=1.45))
 
-  # init
-  A_throttle = 2.
-  A_brake = 2.
-  A_steer_torque = 1.
-  fake_wheel = FakeSteeringWheel()
-  is_openpilot_engaged = False
-  in_reverse = False
+    transform = [carla.Transform(carla.Location(x=-5.5, z=2.8), carla.Rotation(pitch=-15)),
+    carla.Transform(carla.Location(x=1.6, z=1.7))]
 
-  throttle_out = 0
-  brake_out = 0
-  steer_angle_out = 0
+    camera = world.spawn_actor(blueprint, transform[0], attach_to=vehicle)
+    camera.listen(cam_callback)
+  
+    # reenable IMU
+    imu_bp = blueprint_library.find('sensor.other.imu')
+    imu = world.spawn_actor(imu_bp, transform[0], attach_to=vehicle)
+    
+    imu.listen(imu_callback)
 
-  while 1:
-    cruise_button = 0
+    # can loop
+    sendcan = messaging.sub_sock('sendcan')
+    rk = Ratekeeper(100, print_delay_threshold=0.05)
 
-    # check for a input message, this will not block
-    if not q.empty():
-      print("here")
-      message = q.get()
+    # init
+    A_throttle = 2.
+    A_brake = 2.
+    A_steer_torque = 1.
+    fake_wheel = FakeSteeringWheel()
+    is_openpilot_engaged = False
+    in_reverse = False
 
-      m = message.split('_')
-      if m[0] == "steer":
-        steer_angle_out = float(m[1])
-        fake_wheel.set_angle(steer_angle_out)  # touching the wheel overrides fake wheel angle
-        # print(" === steering overriden === ")
-      if m[0] == "throttle":
-        throttle_out = float(m[1]) / 100.
-        if throttle_out > 0.3:
+    throttle_out = 0
+    brake_out = 1
+    steer_angle_out = 0
+
+    vc = carla.VehicleControl(throttle=throttle_out, 
+                              steer=steer_angle_out / 3.14, 
+                              brake=brake_out, 
+                              reverse=in_reverse)
+
+    print ("vehicle control created!!!")
+
+    while 1:
+      cruise_button = 0
+
+      # check for a input message, this will not block
+      if not q.empty():
+        message = q.get()
+
+        m = message.split('_')
+        if m[0] == "steer":
+          steer_angle_out = float(m[1])
+          fake_wheel.set_angle(steer_angle_out)  # touching the wheel overrides fake wheel angle
+          # print(" === steering overriden === ")
+        if m[0] == "throttle":
+          throttle_out = float(m[1]) / 100.
+          if throttle_out > 0.3:
+            cruise_button = CruiseButtons.CANCEL
+            is_openpilot_engaged = False
+        if m[0] == "brake":
+          brake_out = float(m[1]) / 100.
+          if brake_out > 0.3:
+            cruise_button = CruiseButtons.CANCEL
+            is_openpilot_engaged = False
+        if m[0] == "reverse":
+          in_reverse = not in_reverse
           cruise_button = CruiseButtons.CANCEL
           is_openpilot_engaged = False
-      if m[0] == "brake":
-        brake_out = float(m[1]) / 100.
-        if brake_out > 0.3:
-          cruise_button = CruiseButtons.CANCEL
-          is_openpilot_engaged = False
-      if m[0] == "reverse":
-        in_reverse = not in_reverse
-        cruise_button = CruiseButtons.CANCEL
-        is_openpilot_engaged = False
-      if m[0] == "cruise":
-        if m[1] == "down":
-          cruise_button = CruiseButtons.DECEL_SET
-          is_openpilot_engaged = True
-        if m[1] == "up":
-          cruise_button = CruiseButtons.RES_ACCEL
-          is_openpilot_engaged = True
-        if m[1] == "cancel":
-          cruise_button = CruiseButtons.CANCEL
-          is_openpilot_engaged = False
+        if m[0] == "cruise":
+          if m[1] == "down":
+            cruise_button = CruiseButtons.DECEL_SET
+            is_openpilot_engaged = True
+          if m[1] == "up":
+            cruise_button = CruiseButtons.RES_ACCEL
+            is_openpilot_engaged = True
+          if m[1] == "cancel":
+            cruise_button = CruiseButtons.CANCEL
+            is_openpilot_engaged = False
 
-    vel = vehicle.get_velocity()
-    speed = math.sqrt(vel.x**2 + vel.y**2 + vel.z**2) * 3.6
-    can_function(pm, speed, fake_wheel.angle, rk.frame, cruise_button=cruise_button, is_engaged=is_openpilot_engaged)
+      vel = vehicle.get_velocity()
+      #print(f'vehicle velocity {vel}')
+      speed = math.sqrt(vel.x**2 + vel.y**2 + vel.z**2) * 3.6
+      can_function(pm, speed, fake_wheel.angle, rk.frame, cruise_button=cruise_button, is_engaged=is_openpilot_engaged)
 
-    if rk.frame % 1 == 0:  # 20Hz?
-      throttle_op, brake_op, steer_torque_op = sendcan_function(sendcan)
-      # print(" === torq, ",steer_torque_op, " ===")
-      if is_openpilot_engaged:
-        fake_wheel.response(steer_torque_op * A_steer_torque, speed)
-        throttle_out = throttle_op * A_throttle
-        brake_out = brake_op * A_brake
-        steer_angle_out = fake_wheel.angle
-        # print(steer_torque_op)
-      # print(steer_angle_out)
-      vc = carla.VehicleControl(throttle=throttle_out, steer=steer_angle_out / 3.14, brake=brake_out, reverse=in_reverse)
-      vehicle.apply_control(vc)
+      if rk.frame % 1 == 0:  # 20Hz?
+        throttle_op, brake_op, steer_torque_op = sendcan_function(sendcan)
+        #print(" === torq, ",steer_torque_op, " ===")
+        if is_openpilot_engaged:
+          fake_wheel.response(steer_torque_op * A_steer_torque, speed)
+          vc.throttle = throttle_op * A_throttle
+          vc.brake = brake_op * A_brake
+          vc.steer = round(fake_wheel.angle, 1)
+          # print(steer_torque_op)
+          # print(steer_angle_out)
 
-    rk.keep_time()
+        vehicle.apply_control(vc)
+      else:
+        print ("not controlling")
+
+      rk.keep_time()
+
+  finally:
+      if vehicle is not None:
+        vehicle.destroy()
+      if camera is not None:
+        camera.destroy()
+      if imu is not None:
+        imu.destroy()
 
 if __name__ == "__main__":
   params = Params()
