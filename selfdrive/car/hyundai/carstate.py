7aff353362306fbee24261b308af7d6d331733e9
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-26 15:16:06 -0400

    Add Hyundai BSM signals (#1782)
    * Add Hyundai BSM signals
    
    to take advantage of BSM to block lane change when BSM active.
    
    * bug, added LCA11 to canparser
    
    previous check failed, fixed by adding LCA11 to canparser
    
    * update refs
    
    Co-authored-by: Adeeb Shihadeh <8762862+adeebshihadeh@users.noreply.github.com>
    old-commit-hash: 57f29968a2662bb49807f15165fd69254da0cc93

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -115,6 +115,9 @@ class CarState(CarStateBase):
 
     ret.stockAeb = cp.vl["FCA11"]['FCA_CmdAct'] != 0
     ret.stockFcw = cp.vl["FCA11"]['CF_VSM_Warn'] == 2
+    
+    ret.leftBlindspot = cp.vl["LCA11"]["CF_Lca_IndLeft"] != 0
+    ret.rightBlindspot = cp.vl["LCA11"]["CF_Lca_IndRight"] != 0
 
     # save the entire LKAS11 and CLU11
     self.lkas11 = cp_cam.vl["LKAS11"]
@@ -171,6 +174,9 @@ class CarState(CarStateBase):
       ("ESC_Off_Step", "TCS15", 0),
 
       ("CF_Lvr_GearInf", "LVR11", 0),        # Transmission Gear (0 = N or P, 1-8 = Fwd, 14 = Rev)
+      
+      ("CF_Lca_IndLeft", "LCA11", 0),
+      ("CF_Lca_IndRight", "LCA11", 0),
 
       ("CR_Mdps_StrColTq", "MDPS12", 0),
       ("CF_Mdps_ToiActive", "MDPS12", 0),
@@ -205,6 +211,7 @@ class CarState(CarStateBase):
       ("SCC11", 50),
       ("SCC12", 50),
       ("FCA11", 50),
+      ("LCA11", 50),
     ]
 
     if CP.carFingerprint in EV_HYBRID:
