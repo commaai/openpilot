fbe1a4d829b0242323c81f7ca1f7b9cd1d968269
Oscar Moreno <60990482+moreno7798@users.noreply.github.com>
2020-06-29 22:36:40 -0400

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -5,7 +5,7 @@ Ecu = car.CarParams.Ecu
 # Steer torque limits
 class SteerLimitParams:
   STEER_MAX = 409   #409 is the max, 255 is stock
-  STEER_DELTA_UP = 3
+  STEER_DELTA_UP = 5
   STEER_DELTA_DOWN = 7
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
