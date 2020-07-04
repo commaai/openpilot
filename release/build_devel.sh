3f57b7ee7688efd38b2ba3581495b2c250409020
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-03 20:34:31 -0700

    increase jenkins timeout

--- release/build_devel.sh
+++ release/build_devel.sh
@@ -95,7 +95,8 @@ popd
 
 if [ ! -z "$CI_PUSH" ]; then
   echo "[-] Pushing to $CI_PUSH T=$SECONDS"
-  git push -f git@github.com:commaai/openpilot.git master-ci:$CI_PUSH
+  git remote set-url origin git@github.com:commaai/openpilot.git
+  git push -f origin master-ci:$CI_PUSH
 fi
 
 echo "[-] done pushing T=$SECONDS"
