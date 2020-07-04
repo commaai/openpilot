7083e8e64591b2e319624c7dd6c2f0ec0f8b0783
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-03 22:33:33 -0700

    pipenv

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
+# install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install scons capnp czmq coreutils pyenv
+brew install capnp czmq coreutils pyenv
 
+# install python
 pyenv install 3.8.2
 pyenv global 3.8.2
 pyenv rehash
+
+pip install --no-cache-dir pipenv==2018.11.26
 pipenv install --system --deploy
 
