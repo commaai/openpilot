32f03ec8a52c7c5fa56d5cce19fbbd7ecf24b7dd
Adeeb Shihadeh <8762862+adeebshihadeh@users.noreply.github.com>
2020-07-03 19:12:05 -0700

    On-device CI framework (#1784)
    * let's see if this works
    
    * fix build_release actions job
    
    * does jenkins like this config
    
    * separate jenkinsfile for release build
    
    * fix devel build
    
    * devel build should work
    
    * always pass that for now
    
    * run modeld replay
    
    * release2 build will be a separate PR
    
    * pass env to phone shell
    
    * force checkout
    
    * run on real jenkins eons
    
    * add timeout
    
    * rsync
    
    * more timeout
    
    * trailing slash
    
    * fix branch detection
    
    * debug
    
    * not sure why paramiko doesn't pass it through
    
    * newline
    
    * CI_PUSH
    
    * still not passing it
    
    * test branch
    
    * should be good now

--- release/build_devel.sh
+++ release/build_devel.sh
@@ -37,7 +37,7 @@ echo "[-] bringing master-ci and devel in sync T=$SECONDS"
 git fetch origin master-ci
 git fetch origin devel
 
-git checkout --track origin/master-ci || true
+git checkout -f --track origin/master-ci
 git reset --hard master-ci
 git checkout master-ci
 git reset --hard origin/devel
@@ -93,9 +93,9 @@ pushd panda/board/pedal
 make obj/comma.bin
 popd
 
-if [ ! -z "$PUSH" ]; then
-  echo "[-] Pushing to $PUSH T=$SECONDS"
-  git push -f origin master-ci:$PUSH
+if [ ! -z "$CI_PUSH" ]; then
+  echo "[-] Pushing to $CI_PUSH T=$SECONDS"
+  git push -f origin master-ci:$CI_PUSH
 fi
 
 echo "[-] done pushing T=$SECONDS"
