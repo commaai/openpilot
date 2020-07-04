eb44b753fea7ab75bfe8e17d3ef2b312fc5ae134
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-03 22:27:14 -0700

    python stuff

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -2,5 +2,10 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install scons capnp czmq coreutils
+brew install scons capnp czmq coreutils pyenv
+
+pyenv install 3.8.2
+pyenv global 3.8.2
+pyenv rehash
+pipenv install --system --deploy
 
