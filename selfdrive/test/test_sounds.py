f9001d33fd10b6e39b2d1692e70c4325d15e0c7e
Comma Device <device@comma.ai>
2020-07-05 16:30:07 -0700

    check writes

--- selfdrive/test/test_sounds.py
+++ selfdrive/test/test_sounds.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import time
 import nose
+import subprocess
 
 from cereal import car
 import cereal.messaging as messaging
@@ -8,6 +9,25 @@ from common.android import get_sound_card_online
 from common.realtime import DT_CTRL
 from selfdrive.test.helpers import phone_only, with_processes
 
+AudibleAlert = car.CarControl.HUDControl.AudibleAlert
+
+SOUNDS = {
+  # sound: total writes
+  AudibleAlert.none: 0,
+  AudibleAlert.chimeEngage: 85,
+  AudibleAlert.chimeDisengage: 85,
+  AudibleAlert.chimeError: 85,
+  AudibleAlert.chimePrompt: 85,
+  AudibleAlert.chimeWarning1: 80,
+  AudibleAlert.chimeWarning2: 107,
+  AudibleAlert.chimeWarningRepeat: 134,
+  AudibleAlert.chimeWarning2Repeat: 161,
+}
+
+def get_total_writes():
+  audio_flinger = subprocess.check_output('dumpsys media.audio_flinger', shell=True, encoding='utf-8').strip()
+  write_lines = [l for l in audio_flinger.split('\n') if l.strip().startswith('Total writes')]
+  return sum([int(l.split(':')[1]) for l in write_lines])
 
 @phone_only
 def test_sound_card_init():
@@ -20,19 +40,22 @@ def test_alert_sounds():
 
   pm = messaging.PubMaster(['thermal', 'controlsState'])
 
-  alert_sounds = car.CarControl.HUDControl.AudibleAlert.schema.enumerants
+  # make sure they're all defined
+  alert_sounds = car.CarControl.HUDControl.AudibleAlert.schema.enumerants.values()
+  diff = set(SOUNDS.keys()).symmetric_difference(alert_sounds)
+  assert len(diff) == 0, f"not all sounds defined in test: {diff}"
 
   # wait for procs to init
   time.sleep(5)
 
-  for sound in alert_sounds:
-    print(f"testing {sound}")
+  msg = messaging.new_message('thermal')
+  msg.thermal.started = True
+  pm.send('thermal', msg)
 
-    msg = messaging.new_message('thermal')
-    msg.thermal.started = True
-    pm.send('thermal', msg)
+  for sound, expected_writes in SOUNDS.items():
+    start_writes = get_total_writes()
 
-    for _ in range(int(3 / DT_CTRL)):
+    for _ in range(int(6 / DT_CTRL)):
       msg = messaging.new_message('controlsState')
       msg.controlsState.enabled = True
       msg.controlsState.active = True
@@ -41,6 +64,5 @@ def test_alert_sounds():
       pm.send('controlsState', msg)
       time.sleep(DT_CTRL)
 
-
-if __name__ == "__main__":
-  nose.run()
+    actual_writes = get_total_writes() - start_writes
+    assert expected_writes == actual_writes, f"{sound}: expected {expected_writes} writes, got {actual_writes}"
