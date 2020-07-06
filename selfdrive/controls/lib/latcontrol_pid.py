b23d444c18ed4a68c18ca6840a4141dac32e3f83
birdman6450 <67883352+birdman6450@users.noreply.github.com>
2020-07-05 21:36:27 -0700

    latcontrol.pid. kfBP and kfV
    latcontrol.pid. kfBP and kfV

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -8,6 +8,7 @@ class LatControlPID():
   def __init__(self, CP):
     self.pid = PIController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                             (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
+                            (CP.lateralTuning.pid.kfBP, CP.lateralTuning.pid.kfV),
                             k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, sat_limit=CP.steerLimitTimer)
     self.angle_steers_des = 0.
 
