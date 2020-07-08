4d3d76debdde88479f9ed03a9f3a29ef602aa100
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-08 12:32:17 -0700

    remove old

--- selfdrive/boardd/tests/test_boardd_loopback.py
+++ selfdrive/boardd/tests/test_boardd_loopback.py
@@ -1,47 +1,59 @@
 #!/usr/bin/env python3
-"""Run boardd with the BOARDD_LOOPBACK envvar before running this test."""
-
 import os
 import random
 import time
+from collections import defaultdict
 
+import cereal.messaging as messaging
+from cereal import car
+from common.basedir import PARAMS
+from common.params import Params
 from selfdrive.boardd.boardd import can_list_to_can_capnp
-from cereal.messaging import drain_sock, pub_sock, sub_sock
+from selfdrive.car import make_can_msg
+from selfdrive.test.helpers import with_processes
 
-def get_test_string():
-  return b"test"+os.urandom(10)
+os.environ['PARAMS_PATH'] = PARAMS
+@with_processes(['boardd'])
+def test_boardd_loopback():
 
-BUS = 0
+  # wait for boardd to init
+  time.sleep(2)
 
-def main():
-  rcv = sub_sock('can')  # port 8006
-  snd = pub_sock('sendcan')  # port 8017
-  time.sleep(0.3)  # wait to bind before send/recv
+  # boardd blocks on CarVin and CarParams
+  cp = car.CarParams.new_message()
+  cp.safetyModel = car.CarParams.SafetyModel.allOutput
+  Params().put("CarVin", b"0"*17)
+  Params().put("CarParams", cp.to_bytes())
 
-  for i in range(10):
-    print("Loop %d" % i)
-    at = random.randint(1024, 2000)
-    st = get_test_string()[0:8]
-    snd.send(can_list_to_can_capnp([[at, 0, st, 0]], msgtype='sendcan').to_bytes())
-    time.sleep(0.1)
-    res = drain_sock(rcv, True)
-    assert len(res) == 1
+  sendcan = messaging.pub_sock('sendcan')
+  can = messaging.sub_sock('can', timeout=1000)
 
-    res = res[0].can
-    assert len(res) == 2
+  time.sleep(2)
 
-    msg0, msg1 = res
+  for _ in range(20):
+    sent_msgs = defaultdict(list)
+    for _ in range(random.randrange(5)):
+      to_send = []
+      for _ in range(random.randrange(10)):
+        bus = random.randrange(3)
+        addr = random.randrange(1, 0x800)
+        dat = bytes([random.getrandbits(8) for _ in range(random.randrange(1, 9))])
+        sent_msgs[bus].append((addr, dat))
+        to_send.append(make_can_msg(addr, dat, bus))
+      sendcan.send(can_list_to_can_capnp(to_send, msgtype='sendcan'))
 
-    assert msg0.dat == st
-    assert msg1.dat == st
+    time.sleep(1)
+    recvd = messaging.drain_sock(can, wait_for_one=True)
 
-    assert msg0.address == at
-    assert msg1.address == at
+    recv_msgs = defaultdict(list)
+    for msg in recvd:
+      for m in msg.can:
+        if m.src >= 128:
+          recv_msgs[m.src].append((m.address, m.dat))
 
-    assert msg0.src == 0x80 | BUS
-    assert msg1.src == BUS
+    # check lengths
+    for bus in range(3):
+      assert len(sent_msgs[bus]) == len(recv_msgs[bus+128]), \
+             f"mismatched lengths {bus}: {len(sent_msgs[bus])} {bus+128}: {len(recv_msgs[bus+128])}"
 
-  print("Success")
 
-if __name__ == "__main__":
-  main()
