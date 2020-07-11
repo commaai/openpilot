4e67a9cf286a6c83e3af718a1b18ebb15059b489
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-07-11 13:40:27 +0300

    Revert "Update uno.h" need fix This reverts commit 4d7551b1a2bf474777857d1722ffe30c81148d48.

--- panda/board/boards/uno.h
+++ panda/board/boards/uno.h
@@ -241,7 +241,7 @@ void uno_init(void) {
   }
 
   // init multiplexer
-  can_set_obd(car_harness_status, false);
+  can_set_obd(car_harness_status, true);
 
   // Switch to phone usb mode if harness connection is powered by less than 7V
   if(adc_get_voltage() < 7000U){
