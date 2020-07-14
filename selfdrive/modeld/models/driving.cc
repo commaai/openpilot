524264a711c1690574a1e795fe105793c466432b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-13 18:28:55 -0700

    posenet is invalid on non-consecutive frames

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -332,7 +332,7 @@ void posenet_publish(PubMaster &pm, uint32_t vipc_frame_id, uint32_t frame_id,
   posenetd.setFrameId(vipc_frame_id);
 
   uint32_t frame_age = (frame_id > vipc_frame_id) ? (frame_id - vipc_frame_id) : 0;
-  event.setValid(frame_age > 0);
+  event.setValid(frame_age < 1);
 
   pm.send("cameraOdometry", msg);
 }
