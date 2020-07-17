b00094d18af2689ac3d92ae80e0065a3453cafcb
Mufeed VH <mufeedvh@gmail.com>
2020-07-18 00:47:40 +0530

    Fix insecure temporary file creation

--- common/params.py
+++ common/params.py
@@ -319,8 +319,8 @@ def write_db(params_path, key, value):
   lock.acquire()
 
   try:
-    tmp_path = tempfile.mktemp(prefix=".tmp", dir=params_path)
-    with open(tmp_path, "wb") as f:
+    tmp_path = tempfile.NamedTemporaryFile(mode="wb", prefix=".tmp", dir=params_path, delete=False)
+    with tmp_path as f:
       f.write(value)
       f.flush()
       os.fsync(f.fileno())
