d53862fe07231cb51216cabeb0a2ca7fd934151b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-17 23:11:24 -0700

    sdl

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -3,7 +3,17 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install capnp czmq coreutils pyenv llvm eigen ffmpeg
+brew install capnp \
+             czmq \
+             coreutils \
+             eigen \
+             llvm \
+             libarchive \
+             pyenv \
+             sdl2_gfx \
+             sdl2_image \
+             sdl2_mixer \
+             sdl2_ttf
 
 # install python
 pyenv install 3.8.2
