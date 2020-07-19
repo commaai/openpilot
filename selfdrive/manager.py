8b6c07dfc71adece517060669fcb1c2c45f509ac
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-19 09:12:59 -0700

    not android

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -78,7 +78,7 @@ import traceback
 from multiprocessing import Process
 
 # Run scons
-spinner = Spinner(noop=(__name__ != "main" or ANDROID))
+spinner = Spinner(noop=(__name__ != "main" or not ANDROID))
 spinner.update("0")
 
 if not prebuilt:
@@ -138,7 +138,7 @@ if not prebuilt:
         cloudlog.error("scons build failed\n" + error_s)
 
         # Show TextWindow
-        no_ui = __name__ != "__main__" or ANDROID
+        no_ui = __name__ != "__main__" or not ANDROID
         error_s = "\n \n".join(["\n".join(textwrap.wrap(e, 65)) for e in errors])
         with TextWindow("openpilot failed to build\n \n" + error_s, noop=no_ui) as t:
           t.wait_for_exit()
