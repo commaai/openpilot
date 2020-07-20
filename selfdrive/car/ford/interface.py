ec310481cbc6e15fe3fe827593687e2b52f52eab
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-19 23:58:45 -0500

    setup for indi tuning

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -25,13 +25,22 @@ class CarInterface(CarInterfaceBase):
     ret.wheelbase = 2.85
     ret.steerRatio = 14.0
     ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
+    # PID Tuning
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05], [0.002]]     # TODO: tune this
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05], [0.002]]
     ret.lateralTuning.pid.kf = 0.0   # MAX Steer angle to normalize FF
     ret.steerActuatorDelay = 0.1  # Default delay, not measured yet
     ret.steerLimitTimer = 0.8
     ret.steerRateCost = 1.0
     ret.centerToFront = ret.wheelbase * 0.44
+    
+    #INDI tuning TODO: Tune
+    #ret.lateralTuning.init('indi')
+    #ret.lateralTuning.indi.innerLoopGain = 1.0
+    #ret.lateralTuning.indi.outerLoopGain = 1.0
+    #ret.lateralTuning.indi.timeConstant = 1.0
+    #ret.lateralTuning.indi.actuatorEffectiveness = 1.0
+    #ret.steerActuatorDelay = 0.5
     tire_stiffness_factor = 0.5328
 
     # TODO: get actual value, for now starting with reasonable value for
