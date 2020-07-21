48c4dfdcd59e5ad3f03cb517a9d21615be81c4c6
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-20 19:33:11 -0500

    new signals.

--- selfdrive/car/ford/carstate.py
+++ selfdrive/car/ford/carstate.py
@@ -1,20 +1,24 @@
 from cereal import car
 from opendbc.can.parser import CANParser
-from common.numpy_fast import mean
+from opendbc.can.can_define import CANDefine
 from selfdrive.config import Conversions as CV
 from selfdrive.car.interfaces import CarStateBase
 from selfdrive.car.ford.values import DBC
 
-WHEEL_RADIUS = 0.33
-
+GearShifter = car.CarState.GearShifter
+  
 class CarState(CarStateBase):
+  def __init__(self, CP):
+    super().__init__(CP)
+    can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
+    
   def update(self, cp):
     ret = car.CarState.new_message()
-    ret.wheelSpeeds.rr = cp.vl["WheelSpeed_CG1"]['WhlRr_W_Meas'] * WHEEL_RADIUS
-    ret.wheelSpeeds.rl = cp.vl["WheelSpeed_CG1"]['WhlRl_W_Meas'] * WHEEL_RADIUS
-    ret.wheelSpeeds.fr = cp.vl["WheelSpeed_CG1"]['WhlFr_W_Meas'] * WHEEL_RADIUS
-    ret.wheelSpeeds.fl = cp.vl["WheelSpeed_CG1"]['WhlFl_W_Meas'] * WHEEL_RADIUS
-    ret.vEgoRaw = mean([ret.wheelSpeeds.rr, ret.wheelSpeeds.rl, ret.wheelSpeeds.fr, ret.wheelSpeeds.fl])
+    ret.wheelSpeeds.rr = cp.vl["WheelSpeed"]['WhlRr_W_Meas'] * CV.MPH_TO_MS
+    ret.wheelSpeeds.rl = cp.vl["WheelSpeed"]['WhlRl_W_Meas'] * CV.MPH_TO_MS
+    ret.wheelSpeeds.fr = cp.vl["WheelSpeed"]['WhlFr_W_Meas'] * CV.MPH_TO_MS
+    ret.wheelSpeeds.fl = cp.vl["WheelSpeed"]['WhlFl_W_Meas'] * CV.MPH_TO_MS
+    ret.vEgoRaw = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
     ret.standstill = not ret.vEgoRaw > 0.001
     ret.steeringAngle = cp.vl["Steering_Wheel_Data_CG1"]['SteWhlRelInit_An_Sns']
@@ -25,32 +29,86 @@ class CarState(CarStateBase):
     ret.cruiseState.available = cp.vl["Cruise_Status"]['Cruise_State'] != 0
     ret.gas = cp.vl["EngineData_14"]['ApedPosScal_Pc_Actl'] / 100.
     ret.gasPressed = ret.gas > 1e-6
-    ret.brakePressed = bool(cp.vl["Cruise_Status"]["Brake_Drv_Appl"])
-    ret.brakeLights = bool(cp.vl["BCM_to_HS_Body"]["Brake_Lights"])
-    ret.genericToggle = bool(cp.vl["Steering_Buttons"]["Dist_Incr"])
-    # TODO: we also need raw driver torque, needed for Assisted Lane Change
+    ret.brakePressed = bool(cp.vl["Cruise_Status"]['Brake_Drv_Appl'])
+    ret.brakeLights = bool(cp.vl["BCM_to_HS_Body"]['Brake_Lights'])
+    ret.genericToggle = bool(cp.vl["Steering_Buttons"]['Dist_Incr'])
+    self.latLimit = cp.vl["Lane_Keep_Assist_Status"]['LatCtlLim_D_Stat']
     self.lkas_state = cp.vl["Lane_Keep_Assist_Status"]['LaActAvail_D_Actl']
-
+    self.left_blinker_on = bool(cp.vl["Steering_Buttons"]['Left_Turn_Light'])
+    ret.leftBlinker = self.left_blinker_on > 0
+    self.right_blinker_on = bool(cp.vl["Steering_Buttons"]['Right_Turn_Light'])    
+    ret.rightBlinker = self.right_blinker_on > 0
+    ret.doorOpen = any([cp.vl["Doors"]['Door_FL_Open'],cp.vl["Doors"]['Door_FR_Open'],
+                        cp.vl["Doors"]['Door_RL_Open'], cp.vl["Doors"]['Door_RR_Open']]) 
+    ret.steeringTorque = cp.vl["EPAS_INFO"]['SteeringColumnTorque']
+    ret.seatbeltUnlatched = cp.vl["RCMStatusMessage2_FD1"]['FirstRowBuckleDriver'] == 2
+    ret.stockFcw = cp.vl["ACCDATA_3"]['FcwVisblWarn_B_Rq'] !=0
+    print ("Lateral_Limit:", self.latLimit, "lkas_state:", self.lkas_state, "steer_override:", ret.steeringPressed) #debug to check lockout state. 
+    #Gear Shifter
+    gear = cp.vl["TransGearData"]['GearLvrPos_D_Actl']
+    if gear == 0:
+      ret.gearShifter = GearShifter.park
+    elif gear == 1:
+      ret.gearShifter = GearShifter.reverse
+    elif gear == 2:
+      ret.gearShifter = GearShifter.neutral
+    elif gear in (3, 4, 5, 6, 7, 8, 9, 10, 11): #6R80 only. Not counting 10R80
+      ret.gearShifter = GearShifter.drive
+    else:
+      ret.gearShifter = GearShifter.unknown
+    #SODL/SODR BLIS
+    ret.leftBlindspot = cp.vl["Side_Detect_L_Stat"]['SodDetctLeft_D_Stat'] !=0
+    ret.rightBlindspot = cp.vl["Side_Detect_R_Stat"]['SodDetctRight_D_Stat'] !=0
+    #fordcan params
+    self.ahbcCommanded = cp.vl["Lane_Keep_Assist_Ui"]['AhbHiBeam_D_Rq']
+    self.ipmaHeater = cp.vl["Lane_Keep_Assist_Ui"]['CamraDefog_B_Req']
+    self.ahbcRamping = cp.vl["Lane_Keep_Assist_Ui"]['AhbcRampingV_D_Rq']
+    self.ipmaConfig = cp.vl["Lane_Keep_Assist_Ui"]['FeatConfigIpmaActl']
+    self.ipmaNo = cp.vl["Lane_Keep_Assist_Ui"]['FeatNoIpmaActl']
+    self.laDenyStat = cp.vl["Lane_Keep_Assist_Ui"]['LaDenyStats_B_Dsply']
+    self.ipmaStats = cp.vl["Lane_Keep_Assist_Ui"]['CamraStats_D_Dsply']
     return ret
 
   @staticmethod
   def get_can_parser(CP):
     signals = [
-      # sig_name, sig_address, default
-      ("WhlRr_W_Meas", "WheelSpeed_CG1", 0.),
-      ("WhlRl_W_Meas", "WheelSpeed_CG1", 0.),
-      ("WhlFr_W_Meas", "WheelSpeed_CG1", 0.),
-      ("WhlFl_W_Meas", "WheelSpeed_CG1", 0.),
-      ("SteWhlRelInit_An_Sns", "Steering_Wheel_Data_CG1", 0.),
-      ("Cruise_State", "Cruise_Status", 0.),
-      ("Set_Speed", "Cruise_Status", 0.),
-      ("LaActAvail_D_Actl", "Lane_Keep_Assist_Status", 0),
-      ("LaHandsOff_B_Actl", "Lane_Keep_Assist_Status", 0),
-      ("LaActDeny_B_Actl", "Lane_Keep_Assist_Status", 0),
-      ("ApedPosScal_Pc_Actl", "EngineData_14", 0.),
-      ("Dist_Incr", "Steering_Buttons", 0.),
-      ("Brake_Drv_Appl", "Cruise_Status", 0.),
-      ("Brake_Lights", "BCM_to_HS_Body", 0.),
+    # sig_name, sig_address, default
+    ("WhlRr_W_Meas", "WheelSpeed", 0.),
+    ("WhlRl_W_Meas", "WheelSpeed", 0.),
+    ("WhlFr_W_Meas", "WheelSpeed", 0.),
+    ("WhlFl_W_Meas", "WheelSpeed", 0.),
+    ("SteWhlRelInit_An_Sns", "Steering_Wheel_Data_CG1", 0.),
+    ("Cruise_State", "Cruise_Status", 0.),
+    ("Set_Speed", "Cruise_Status", 0.),
+    ("LaActAvail_D_Actl", "Lane_Keep_Assist_Status", 0),
+    ("LaHandsOff_B_Actl", "Lane_Keep_Assist_Status", 0),
+    ("LaActDeny_B_Actl", "Lane_Keep_Assist_Status", 0),
+    ("ApedPosScal_Pc_Actl", "EngineData_14", 0.),
+    ("Dist_Incr", "Steering_Buttons", 0.),
+    ("Lane_Keep_Toggle", "Steering_Buttons", 0.),
+    ("Brake_Drv_Appl", "Cruise_Status", 0.),
+    ("Brake_Lights", "BCM_to_HS_Body", 0.),
+    ("Left_Turn_Light", "Steering_Buttons", 0.),
+    ("Right_Turn_Light", "Steering_Buttons", 0.),
+    ("Door_FL_Open", "Doors", 0.),
+    ("Door_FR_Open", "Doors", 0.),
+    ("Door_RL_Open", "Doors", 0.),
+    ("Door_RR_Open", "Doors", 0.),
+    ("SteeringColumnTorque", "EPAS_INFO", 0.),
+    ("GearLvrPos_D_Actl", "TransGearData", 0.),
+    ("FirstRowBuckleDriver", "RCMStatusMessage2_FD1", 0.),
+    ("LatCtlLim_D_Stat", "Lane_Keep_Assist_Status", 0.),
+    ("SodDetctLeft_D_Stat", "Side_Detect_L_Stat", 0.),
+    ("SodDetctRight_D_Stat", "Side_Detect_R_Stat", 0.),
+    ("AhbHiBeam_D_Rq", "Lane_Keep_Assist_Ui", 0.),
+    ("CamraDefog_B_Req", "Lane_Keep_Assist_Ui", 0.),
+    ("AhbcRampingV_D_Rq", "Lane_Keep_Assist_Ui", 0.),
+    ("FcwVisblWarn_B_Rq", "ACCDATA_3", 0.),
+    ("FeatConfigIpmaActl", "Lane_Keep_Assist_Ui", 0.),
+    ("FeatNoIpmaActl", "Lane_Keep_Assist_Ui", 0.),
+    ("LaDenyStats_B_Dsply", "Lane_Keep_Assist_Ui", 0.),
+    ("CamraStats_D_Dsply", "Lane_Keep_Assist_Ui", 0.),
     ]
+    
     checks = []
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 0)
