e283c5dd8b545f0859ad01f2d75467583be6731d
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-20 20:21:57 -0500

    Update radar_interface.py

--- selfdrive/car/ford/radar_interface.py
+++ selfdrive/car/ford/radar_interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from cereal import car
 from opendbc.can.parser import CANParser
-from selfdrive.car.ford.values import DBC, FEATURES
+from selfdrive.car.ford.values import DBC
 from selfdrive.config import Conversions as CV
 from selfdrive.car.interfaces import RadarInterfaceBase
 
@@ -22,54 +22,58 @@ class RadarInterface(RadarInterfaceBase):
     super().__init__(CP)
     self.validCnt = {key: 0 for key in RADAR_MSGS}
     self.track_id = 0
-
+    self.radar_off_can = CP.radarOffCan
     self.rcp = _create_radar_can_parser(CP.carFingerprint)
     self.trigger_msg = 0x53f
     self.updated_messages = set()
-  if CP.carFingerprint in FEATURES["use_op_longitudinal"]: #TODO: Tune this. 
-    def update(self, can_strings):
-      vls = self.rcp.update_strings(can_strings)
-      self.updated_messages.update(vls)
 
-      if self.trigger_msg not in self.updated_messages:
-        return None
+  def update(self, can_strings):
+    if self.radar_off_can:
+      if 'NO_RADAR_SLEEP' not in os.environ:
+        time.sleep(0.05)
+        
+     return car.RadarData.new_message()
+  
+    vls = self.rcp.update_strings(can_strings)
+    self.updated_messages.update(vls)
 
-      ret = car.RadarData.new_message()
-      errors = []
-      if not self.rcp.can_valid:
-        errors.append("canError")
-      ret.errors = errors
+    if self.trigger_msg not in self.updated_messages:
+      return None
 
-      for ii in sorted(self.updated_messages):
-        cpt = self.rcp.vl[ii]
+    ret = car.RadarData.new_message()
+    errors = []
+    if not self.rcp.can_valid:
+      errors.append("canError")
+    ret.errors = errors
 
-        if cpt['X_Rel'] > 0.00001:
-          self.validCnt[ii] = 0    # reset counter
+    for ii in sorted(self.updated_messages):
+      cpt = self.rcp.vl[ii]
 
-        if cpt['X_Rel'] > 0.00001:
-          self.validCnt[ii] += 1
-        else:
-          self.validCnt[ii] = max(self.validCnt[ii] - 1, 0)
-        #print ii, self.validCnt[ii], cpt['VALID'], cpt['X_Rel'], cpt['Angle']
+      if cpt['X_Rel'] > 0.00001:
+        self.validCnt[ii] = 0    # reset counter
 
-        # radar point only valid if there have been enough valid measurements
-        if self.validCnt[ii] > 0:
-          if ii not in self.pts:
-            self.pts[ii] = car.RadarData.RadarPoint.new_message()
-            self.pts[ii].trackId = self.track_id
-            self.track_id += 1
-          self.pts[ii].dRel = cpt['X_Rel']  # from front of car
-          self.pts[ii].yRel = cpt['X_Rel'] * cpt['Angle'] * CV.DEG_TO_RAD  # in car frame's y axis, left is positive
-          self.pts[ii].vRel = cpt['V_Rel']
-          self.pts[ii].aRel = float('nan')
-          self.pts[ii].yvRel = float('nan')
-          self.pts[ii].measured = True
-        else:
-          if ii in self.pts:
-            del self.pts[ii]
+      if cpt['X_Rel'] > 0.00001:
+        self.validCnt[ii] += 1
+      else:
+        self.validCnt[ii] = max(self.validCnt[ii] - 1, 0)
+      #print ii, self.validCnt[ii], cpt['VALID'], cpt['X_Rel'], cpt['Angle']
 
-      ret.points = list(self.pts.values())
-      self.updated_messages.clear()
-      return ret
-  else:
-    pass
+      # radar point only valid if there have been enough valid measurements
+      if self.validCnt[ii] > 0:
+        if ii not in self.pts:
+          self.pts[ii] = car.RadarData.RadarPoint.new_message()
+          self.pts[ii].trackId = self.track_id
+          self.track_id += 1
+        self.pts[ii].dRel = cpt['X_Rel']  # from front of car
+        self.pts[ii].yRel = cpt['X_Rel'] * cpt['Angle'] * CV.DEG_TO_RAD  # in car frame's y axis, left is positive
+        self.pts[ii].vRel = cpt['V_Rel']
+        self.pts[ii].aRel = float('nan')
+        self.pts[ii].yvRel = float('nan')
+        self.pts[ii].measured = True
+      else:
+        if ii in self.pts:
+          del self.pts[ii]
+
+    ret.points = list(self.pts.values())
+    self.updated_messages.clear()
+    return ret
