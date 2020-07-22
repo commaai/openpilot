a48f855c4975565e519721a48139d0e0b0161af3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-21 22:51:34 -0700

    car interface

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -5,13 +5,15 @@ import importlib
 import unittest
 from parameterized import parameterized_class
 
-from tools.lib.logreader import LogReader
+from cereal import car
 from selfdrive.car.fingerprints import all_known_cars, _FINGERPRINTS
 from selfdrive.car.car_helpers import interfaces
 from selfdrive.test.test_car_models import routes
 from selfdrive.test.openpilotci import get_url
+from tools.lib.logreader import LogReader
 
 routes = {v['carFingerprint']: k for k, v in routes.items()}
+
 
 @parameterized_class(('car_model'), [(car,) for car in all_known_cars()][:2])
 class TestCarModel(unittest.TestCase):
@@ -31,9 +33,37 @@ class TestCarModel(unittest.TestCase):
     CarInterface, CarController, CarState = interfaces[self.car_model]
     has_relay = False
     self.car_params = CarInterface.get_params(self.car_model, fingerprints, has_relay, [])
+    assert self.car_params
 
-  def test_fingerprint(self):
-    pass
+    if self.car_params.dashcamOnly:
+      self.skipTest(f"skipping {self.car_model}, dashcam only")
+
+    self.CI = CarInterface(self.car_params, CarController, CarState)
+    assert self.CI
+
+  def test_car_params(self):
+    # make sure car params are within a valid range
+    self.assertGreater(self.car_params.mass, 1)
+    self.assertGreater(self.car_params.steerRateCost, 1e-3)
+
+    tuning = self.car_params.lateralTuning.which()
+    if tuning == 'pid':
+      self.assertTrue(len(self.car_params.lateralTuning.pid.kpV))
+    elif tuning == 'lqr':
+      self.assertTrue(len(self.car_params.lateralTuning.lqr.a))
+    elif tuning == 'indi':
+      self.assertGreater(self.car_params.lateralTuning.indi.outerLoopGain, 1e-3)
+
+  def test_car_interface(self):
+    can_invalid_cnt = 0
+    CC = car.CarControl.new_message()
+    for msg in self.lr:
+      if msg.which() != "can":
+        continue
+      CS = self.CI.update(CC, (msg.as_builder().to_bytes(),))
+      self.CI.apply(CC)
+      can_invalid_cnt += CS.canValid
+    self.assertLess(can_invalid_cnt, 20)
 
   def test_radar_interface(self):
     RadarInterface = importlib.import_module('selfdrive.car.%s.radar_interface' % self.car_params.carName).RadarInterface
