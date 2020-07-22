7bc7aec22468caf836d42c732635a8fb5ea6f651
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-22 01:14:39 -0500

    Update fordcan.py

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -25,14 +25,15 @@ def create_steer_command(packer, angle_cmd, enabled, lkas_state, angle_steers, c
 
 def create_lkas_status(packer, enabled, lkasState, steer_pressed, steer_error):
   """Creates a CAN message for the Ford LKAS State"""
+  if enabled:
+    state = lkasState
+    print("State:", state)
   values = {
     "LaActAvail_D_Actl": state,
     #"LaHandsOff_B_Actl": steer_pressed,
     #"LaActDeny_B_Actl": steer_error,
   }
-  if enabled:
-    state = lkasState
-    print("State:", state)
+
   return packer.make_can_msg("Lane_Keep_Assist_Status", 0, values)
 
 def create_lkas_ui(packer, main_on, enabled, steer_alert, defog, ahbc, ahbcramping, config, noipma, stats):
