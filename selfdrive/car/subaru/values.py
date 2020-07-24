4197170a245634573b94fd32b5e2afc926d589ac
Martin Lillepuu <martin@mlp.ee>
2020-07-24 07:58:46 +0300

    Add 2019 Impreza Premium FPv2

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -124,26 +124,32 @@ FW_VERSIONS = {
   CAR.IMPREZA: {
     # 2018 Crosstrek - EDM / @martinl
     # 2018 Impreza - ADM / @Michael
+    # 2019 Impreza Premium - UDM / @hitoryu2001
     # Ecu, addr, subaddr: ROM ID
     (Ecu.esp, 0x7b0, None): [
       b'\x7a\x94\x3f\x90\x00',
       b'\xa2 \x185\x00',
+      b'\xa2 \x193\x00',
     ],
     (Ecu.eps, 0x746, None): [
       b'\x7a\xc0\x0c\x00',
       b'z\xc0\b\x00',
+      b'\x8a\xc0\x00\x00',
     ],
     (Ecu.fwdCamera, 0x787, None): [
       b'\x00\x00\x64\xb5\x1f\x40\x20\x0e',
       b'\x00\x00d\xdc\x1f@ \x0e',
+      b'\x00\x00e\x1c\x1f@ \x14',
     ],
     (Ecu.engine, 0x7e0, None): [
       b'\xaa\x61\x66\x73\x07',
       b'\xbeacr\a',
+      b'\xc5!`r\a',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xe3\xe5\x46\x31\x00',
       b'\xe4\xe5\x061\x00',
+      b'\xe5\xf5\x04\x00\x00',
     ],
   },
   CAR.FORESTER_PREGLOBAL: {
