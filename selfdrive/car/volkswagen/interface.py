cef42fed939b37cb6bd021ef0b2712824d464b60
Jason Young <jyoung8607@gmail.com>
2020-07-31 12:31:13 -0400

    Main merge of VW community changes

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -1,5 +1,6 @@
 from cereal import car
-from selfdrive.car.volkswagen.values import CAR, BUTTON_STATES
+from selfdrive.swaglog import cloudlog
+from selfdrive.car.volkswagen.values import CAR, BUTTON_STATES, NWL, TRANS, GEAR
 from common.params import put_nonblocking
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint
 from selfdrive.car.interfaces import CarInterfaceBase
@@ -13,6 +14,9 @@ class CarInterface(CarInterfaceBase):
     self.displayMetricUnitsPrev = None
     self.buttonStatesPrev = BUTTON_STATES.copy()
 
+    # Set up an alias to PT/CAM parser for ACC depending on its detected network location
+    self.cp_acc = self.cp if CP.networkLocation == NWL.fwdCamera else self.cp_cam
+
   @staticmethod
   def compute_gb(accel, speed):
     return float(accel) / 4.0
@@ -21,23 +25,24 @@ class CarInterface(CarInterfaceBase):
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=None):
     ret = CarInterfaceBase.get_std_params(candidate, fingerprint, has_relay)
 
-    # VW port is a community feature, since we don't own one to test
-    ret.communityFeature = True
+    ret.enableCamera = True  # Stock camera detection doesn't apply to VW
+    ret.carName = "volkswagen"
+    ret.radarOffCan = True
 
-    if candidate == CAR.GOLF:
+    if candidate == CAR.GENERICMQB:
       # Set common MQB parameters that will apply globally
-      ret.carName = "volkswagen"
-      ret.radarOffCan = True
       ret.safetyModel = car.CarParams.SafetyModel.volkswagen
 
       # Additional common MQB parameters that may be overridden per-vehicle
       ret.steerRateCost = 1.0
-      ret.steerActuatorDelay = 0.05  # Hopefully all MQB racks are similar here
+      ret.steerActuatorDelay = 0.1  # Hopefully all MQB racks are similar here
       ret.steerLimitTimer = 0.4
 
       ret.lateralTuning.pid.kpBP = [0.]
       ret.lateralTuning.pid.kiBP = [0.]
 
+      # FIXME: Per-vehicle parameters need to be reintegrated.
+      # Until that time, defaulting to VW Golf Mk7 as a baseline.
       ret.mass = 1500 + STD_CARGO_KG
       ret.wheelbase = 2.64
       ret.centerToFront = ret.wheelbase * 0.45
@@ -47,8 +52,26 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiV = [0.2]
       tire_stiffness_factor = 1.0
 
-    ret.enableCamera = True  # Stock camera detection doesn't apply to VW
-    ret.transmissionType = car.CarParams.TransmissionType.automatic
+    # Determine installed network location by checking for radar-camera
+    # sensor fusion messages on bus 1.
+    if 0x238 in fingerprint[1]:
+      ret.networkLocation = NWL.fwdCamera
+    else:
+      ret.networkLocation = NWL.gateway
+
+    # Determine transmission type by CAN message(s) present on the bus
+    if 0xAD in fingerprint[0]:
+      # Getribe_11 message detected: traditional automatic or DSG gearbox
+      ret.transmissionType = TRANS.automatic
+    elif 0x187 in fingerprint[0]:
+      # EV_Gearshift message detected: e-Golf or similar direct-drive electric
+      ret.transmissionType = TRANS.direct
+    else:
+      # No trans message at all, must be a true stick-shift manual
+      ret.transmissionType = TRANS.manual
+
+    cloudlog.warning("Detected network location: %s", ret.networkLocation)
+    cloudlog.warning("Detected transmission type: %s", ret.transmissionType)
 
     # TODO: get actual value, for now starting with reasonable value for
     # civic and scaling by mass and wheelbase
@@ -71,8 +94,8 @@ class CarInterface(CarInterfaceBase):
     self.cp.update_strings(can_strings)
     self.cp_cam.update_strings(can_strings)
 
-    ret = self.CS.update(self.cp)
-    ret.canValid = self.cp.can_valid and self.cp_cam.can_valid
+    ret = self.CS.update(self.cp, self.cp_cam, self.cp_acc, self.CP.transmissionType)
+    ret.canValid = self.cp.can_valid  # FIXME: Restore cp_cam valid check after proper LKAS camera detect
     ret.steeringRateLimited = self.CC.steer_rate_limited if self.CC is not None else False
 
     # Update the device metric configuration to match the car at first startup,
