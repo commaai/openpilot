904217b6461672ceccf654ba6f1ac03752fbc904
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-08-02 15:19:27 -0700

    Update Hyundai DBC (#1968)
    * update for new DBC update
    
    * update to new DBC
    
    * Ioniq uses SCC12. Ioniq EV lim uses FCA11
    
    * update elect gear to EV_PC5 signal
    
    * remove 'alt' from label
    
    * revert
    
    * update name to ev_pc5_gears
    
    * update name to use_ev_pc5_gears
    
    * Update values.py
    
    * Update carstate.py
    
    * bump opendbc
    
    Co-authored-by: xps-genesis <65239463+xps-genesis@users.noreply.github.com>
    old-commit-hash: 059a27d12697e48b10c00fc345812704d7ba4640

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -281,7 +281,7 @@ class CarState(CarStateBase):
 
     signals = [
       # sig_name, sig_address, default
-      ("CF_Lkas_Bca_R", "LKAS11", 0),
+      ("CF_Lkas_LdwsActivemode", "LKAS11", 0),
       ("CF_Lkas_LdwsSysState", "LKAS11", 0),
       ("CF_Lkas_SysWarning", "LKAS11", 0),
       ("CF_Lkas_LdwsLHWarning", "LKAS11", 0),
