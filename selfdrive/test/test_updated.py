3ef7d21e8b1096af65f0b6bea019c5e227565eb1
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-08-06 21:20:38 -0700

    no k

--- selfdrive/test/test_updated.py
+++ selfdrive/test/test_updated.py
@@ -125,12 +125,12 @@ class TestUpdater(unittest.TestCase):
     new_dir = os.path.join(self.git_remote_dir, "this_is_a_new_dir")
     os.mkdir(new_dir)
     for _ in range(random.randrange(5, 30)):
-      for d in (new_dir, random.sample(all_dirs)):
+      for d in (new_dir, random.choice(all_dirs)):
         with tempfile.NamedTemporaryFile(dir=d, delete=False) as f:
           f.write(os.urandom(random.randrange(1, 1000000)))
 
     # modify some files
-    for f in random.sample(all_files, k=random.randrange(5, 50)):
+    for f in random.sample(all_files, random.randrange(5, 50)):
       with open(f, "w+") as ff:
         txt = ff.readlines()
         ff.seek(0)
@@ -138,11 +138,11 @@ class TestUpdater(unittest.TestCase):
           ff.write(line[::-1])
 
     # remove some files
-    for f in random.sample(all_files, k=random.randrange(5, 50)):
+    for f in random.sample(all_files, random.randrange(5, 50)):
       os.remove(f)
 
     # remove some dirs
-    for d in random.sample(all_dirs, k=random.randrange(1, 10)):
+    for d in random.sample(all_dirs, random.randrange(1, 10)):
       shutil.rmtree(d)
 
     # commit the changes
