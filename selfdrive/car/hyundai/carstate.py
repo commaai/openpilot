3cd4bf1d25380b3576420c16c1f2e94e86af689f
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-08-07 01:24:58 -0700

    use common gas pressed signal

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -55,10 +55,10 @@ class CarState(CarStateBase):
 
     if self.CP.carFingerprint in EV_HYBRID:
       ret.gas = cp.vl["E_EMS11"]['Accel_Pedal_Pos'] / 256.
-      ret.gasPressed = ret.gas > 0
     else:
       ret.gas = cp.vl["EMS12"]['PV_AV_CAN'] / 100
-      ret.gasPressed = bool(cp.vl["EMS16"]["CF_Ems_AclAct"])
+    # TODO: is gas pressed the first bit or the 1 state?
+    ret.gasPressed = cp.vl["TCS13"]['DriverOverride'] == 1
 
     # TODO: refactor gear parsing in function
     # Gear Selection via Cluster - For those Kia/Hyundai which are not fully discovered, we can use the Cluster Indicator for Gear Selection,
