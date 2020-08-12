e4c64389ba97d0e2593eb83a3146378d561167e5
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-11 21:32:34 -0400

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -147,7 +147,7 @@ ECU_FINGERPRINT = {
 }
 
 # Don't use these fingerprints for fingerprinting, they are still used for ECU detection
-IGNORED_FINGERPRINTS = [CAR.VELOSTER, CAR.GENESIS_G70, CAR.KONA]
+IGNORED_FINGERPRINTS = [CAR.VELOSTER, CAR.GENESIS_G70, CAR.KONA, CAR.KIA_OPTIMA]
 
 FW_VERSIONS = {
   CAR.SONATA: {
