2707015f6ffb5a92be33aead39b4e71227990c84
Willem Melching <willem.melching@gmail.com>
2020-08-13 16:36:52 +0200

    Not needed

--- selfdrive/ui/qt_ui.cc
+++ selfdrive/ui/qt_ui.cc
@@ -6,10 +6,6 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
 
-  QSurfaceFormat format;
-  format.setDepthBufferSize(24);
-  QSurfaceFormat::setDefaultFormat(format);
-
   MainWindow w;
   w.setFixedSize(2160, 1080);
   w.show();
