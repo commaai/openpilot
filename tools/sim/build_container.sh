67c88f3fe983fad1a68baec44973b846d4d5d0ca
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-08-16 19:57:34 -0700

    fix path

--- tools/sim/build_container.sh
+++ tools/sim/build_container.sh
@@ -3,7 +3,8 @@
 docker pull docker.io/commaai/openpilot-sim:latest || true
 
 if [ -n "$BUILD" ]; then
-  cd ../../
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  cd $DIR/../../
   docker pull docker.io/commaai/openpilotci:latest || true
   docker build --cache-from commaai/openpilotci:latest -t commaai/openpilotci:latest -f Dockerfile.openpilot .
   docker build --cache-from commaai/openpilot-sim:latest -t commaai/openpilot-sim:latest -f tools/sim/Dockerfile.sim .
