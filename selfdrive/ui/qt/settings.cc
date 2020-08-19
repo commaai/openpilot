fcb6ef4ff4aceda130e14a786b7ca37f154fd1ff
Willem Melching <willem.melching@gmail.com>
2020-08-19 15:23:51 +0200

    params toggles

--- selfdrive/ui/qt/settings.cc
+++ selfdrive/ui/qt/settings.cc
@@ -9,29 +9,85 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QScrollArea>
-#include <QCheckBox>
 #include <QScroller>
 #include <QScrollerProperties>
-#include <QtDBus>
 #include <QDebug>
 
-typedef QMap<QString, QMap<QString, QVariant> > Connection;
-Q_DECLARE_METATYPE(Connection)
+#include "common/params.h"
+
+ParamsToggle::ParamsToggle(QString param, QString title, QString description, QWidget *parent): QFrame(parent) , param(param) {
+  QVBoxLayout *layout = new QVBoxLayout;
+
+  checkbox = new QCheckBox(title);
+  QLabel *label = new QLabel(description);
+  label->setWordWrap(true);
+
+  layout->addWidget(checkbox);
+  layout->addWidget(label);
+
+  setLayout(layout);
+  setFrameStyle(QFrame::Box | QFrame::Raised);
+
+  setStyleSheet(R"(
+    QCheckBox { font-size: 40px }
+    QLabel { font-size: 20px }
+  )");
+
+
+  auto p = read_db_bytes(param.toStdString().c_str());
+  if (p.size()){
+    checkbox->setChecked(p[0] == '1');
+  }
+
+  QObject::connect(checkbox, SIGNAL(stateChanged(int)), this, SLOT(checkboxClicked(int)));
+}
+
+void ParamsToggle::checkboxClicked(int state){
+  char value[1] = {};
+  value[0] = state ? '1': '0';
+  write_db_value(param.toStdString().c_str(), value, 1);
+}
 
 SettingsWindow::SettingsWindow(QWidget *parent) : QWidget(parent) {
-  qDBusRegisterMetaType<Connection>();
-
   QWidget *container = new QWidget(this);
-  QVBoxLayout *checkbox_layout = new QVBoxLayout();
 
-  for(int i = 0; i < 25; i++){
-    QCheckBox *chk = new QCheckBox("Check Box " + QString::number(i+1));
-    checkbox_layout->addWidget(chk);
-    checkbox_layout->addSpacing(50);
-  }
-  container->setLayout(checkbox_layout);
+  QVBoxLayout *settings_list = new QVBoxLayout();
+  settings_list->addWidget(new ParamsToggle("OpenpilotEnabledToggle",
+                                            "Enable Openpilot",
+                                            "Use the openpilot system for adaptive cruise control and lane keep driver assistance. Your attention is required at all times to use this feature. Changing this setting takes effect when the car is powered off."
+                                              ));
+  settings_list->addWidget(new ParamsToggle("LaneChangeEnabled",
+                                            "Enable Lane Change Assist",
+                                            "Perform assisted lane changes with openpilot by checking your surroundings for safety, activating the turn signal and gently nudging the steering wheel towards your desired lane. openpilot is not capable of checking if a lane change is safe. You must continuously observe your surroundings to use this feature."
+                                              ));
+  settings_list->addWidget(new ParamsToggle("IsLdwEnabled",
+                                            "Enable Lane Departure Warnings",
+                                            "Receive alerts to steer back into the lane when your vehicle drifts over a detected lane line without a turn signal activated while driving over 31mph (50kph)."
+                                              ));
+  settings_list->addWidget(new ParamsToggle("RecordFront",
+                                            "Record and Upload Driver Camera",
+                                            "Upload data from the driver facing camera and help improve the driver monitoring algorithm."
+                                            ));
+  settings_list->addWidget(new ParamsToggle("IsRHD",
+                                            "Enable Right-Hand Drive",
+                                            "Allow openpilot to obey left-hand traffic conventions and perform driver monitoring on right driver seat."
+                                            ));
+  settings_list->addWidget(new ParamsToggle("IsMetric",
+                                            "Use Metric System",
+                                            "Display speed in km/h instead of mp/h."
+                                            ));
+  settings_list->addWidget(new ParamsToggle("CommunityFeaturesToggle",
+                                            "Enable Community Features",
+                                            "Use features from the open source community that are not maintained or supported by comma.ai and have not been confirmed to meet the standard safety model. These features include community supported cars and community supported hardware. Be extra cautious when using these features.{'\n"
+                                            ));
+
+  settings_list->setSpacing(25);
+
+  container->setLayout(settings_list);
+  container->setFixedWidth(1250);
 
   QScrollArea *scrollArea = new QScrollArea;
+  scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   scrollArea->setWidget(container);
 
   QScrollerProperties sp;
@@ -40,7 +96,9 @@ SettingsWindow::SettingsWindow(QWidget *parent) : QWidget(parent) {
   QScroller* qs = QScroller::scroller(scrollArea);
   qs->setScrollerProperties(sp);
 
+
   QHBoxLayout *main_layout = new QHBoxLayout;
+  main_layout->addSpacing(50);
   main_layout->addWidget(scrollArea);
 
   QPushButton * button = new QPushButton("Close");
@@ -51,64 +109,8 @@ SettingsWindow::SettingsWindow(QWidget *parent) : QWidget(parent) {
   QScroller::grabGesture(scrollArea, QScroller::LeftMouseButtonGesture);
   QObject::connect(button, SIGNAL(clicked()), this, SIGNAL(closeSettings()));
 
+  setStyleSheet(R"(
+    QPushButton { font-size: 40px }
+  )");
 
-  QString nm_path = "/org/freedesktop/NetworkManager";
-  QString nm_settings_path = "/org/freedesktop/NetworkManager/Settings";
-
-  QString nm_iface = "org.freedesktop.NetworkManager";
-  QString props_iface = "org.freedesktop.DBus.Properties";
-  QString nm_settings_iface = "org.freedesktop.NetworkManager.Settings";
-
-  QString nm_service = "org.freedesktop.NetworkManager";
-  QString device_service = "org.freedesktop.NetworkManager.Device";
-
-  QDBusConnection bus = QDBusConnection::systemBus();
-
-  // Get devices
-  QDBusInterface nm(nm_service, nm_path, nm_iface, bus);
-  QDBusMessage response = nm.call("GetDevices");
-  QVariant first =  response.arguments().at(0);
-
-  const QDBusArgument &args = first.value<QDBusArgument>();
-  args.beginArray();
-  while (!args.atEnd()) {
-    QDBusObjectPath path;
-    args >> path;
-
-    // Get device type
-    QDBusInterface device_props(nm_service, path.path(), props_iface, bus);
-    QDBusMessage response = device_props.call("Get", device_service, "DeviceType");
-    QVariant first =  response.arguments().at(0);
-    QDBusVariant dbvFirst = first.value<QDBusVariant>();
-    QVariant vFirst = dbvFirst.variant();
-    uint device_type = vFirst.value<uint>();
-    qDebug() << path.path() << device_type;
-  }
-  args.endArray();
-
-
-  // Add connection
-  Connection connection;
-  connection["connection"]["type"] = "802-11-wireless";
-  connection["connection"]["uuid"] = QUuid::createUuid().toString().remove('{').remove('}');
-  connection["connection"]["id"] = "Connection 1";
-
-  connection["802-11-wireless"]["ssid"] = QByteArray("<ssid>");
-  connection["802-11-wireless"]["mode"] = "infrastructure";
-
-  connection["802-11-wireless-security"]["key-mgmt"] = "wpa-psk";
-  connection["802-11-wireless-security"]["auth-alg"] = "open";
-  connection["802-11-wireless-security"]["psk"] = "<password>";
-
-  connection["ipv4"]["method"] = "auto";
-  connection["ipv6"]["method"] = "ignore";
-
-
-  QDBusInterface nm_settings(nm_service, nm_settings_path, nm_settings_iface, bus);
-  QDBusReply<QDBusObjectPath> result = nm_settings.call("AddConnection", QVariant::fromValue(connection));
-  if (!result.isValid()) {
-    qDebug() << result.error().name() << result.error().message();
-  } else {
-    qDebug() << result.value().path();
-  }
 }
--- selfdrive/ui/qt/settings.hpp
+++ selfdrive/ui/qt/settings.hpp
@@ -1,15 +1,28 @@
 #pragma once
 
 #include <QWidget>
+#include <QFrame>
+#include <QTimer>
+#include <QCheckBox>
 
-
-class SettingsWindow : public QWidget
-{
+class ParamsToggle : public QFrame {
   Q_OBJECT
 
+private:
+  QCheckBox *checkbox;
+  QString param;
+public:
+  explicit ParamsToggle(QString param, QString title, QString description, QWidget *parent = 0);
+public slots:
+  void checkboxClicked(int state);
+};
+
+
+
+class SettingsWindow : public QWidget {
+  Q_OBJECT
 public:
   explicit SettingsWindow(QWidget *parent = 0);
-
 signals:
   void closeSettings();
 };
