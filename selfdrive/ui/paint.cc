473fb11d3419f6f5bd7fe95f45298c8b3bd9a209
Willem Melching <willem.melching@gmail.com>
2020-08-20 17:16:44 +0200

    Qt ui for PC (#2023)
    * qt ui boilerplate
    
    * this kinda works
    
    * cleanup
    
    * render inside other widget
    
    * cleanup
    
    * more cleanup
    
    * Not needed
    
    * Handle click
    
    * Draw at 20Hz
    
    * create paint.hpp
    
    * move stuff around
    
    * update sidebar
    
    * Draw vision
    
    * this works again
    
    * add clickable settings button
    
    * Only collapse sidebar when started
    
    * always use qt on linux
    
    * fix width
    
    * scrollable area
    
    * talk to NetworkManager
    
    * code to add a connection
    
    * params toggles
    
    * small cleanup
    
    * add qt5 to dockerfile
    
    * Qt on mac
    
    * Add qt to release files
    
    * fix macos build
    
    * nore more ifdefs needed
    
    * add icons
    
    * make a bit nicer
    
    * Hide scrollbar
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: e115c514521c7705112bd0cb2e4bf865b536501a

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -2,16 +2,19 @@
 #include <assert.h>
 #include <map>
 #include <cmath>
+#include <iostream>
 #include "common/util.h"
 
 #define NANOVG_GLES3_IMPLEMENTATION
-
 #include "nanovg_gl.h"
 #include "nanovg_gl_utils.h"
 
 extern "C"{
 #include "common/glutil.h"
 }
+
+#include "paint.hpp"
+#include "sidebar.hpp"
 
 // TODO: this is also hardcoded in common/transformations/camera.py
 const mat3 intrinsic_matrix = (mat3){{
@@ -348,7 +351,7 @@ static void ui_draw_world(UIState *s) {
     return;
   }
 
-  const int inner_height = viz_w*9/16;
+  const int inner_height = float(viz_w) * vwp_h / vwp_w;
   const int ui_viz_rx = scene->ui_viz_rx;
   const int ui_viz_rw = scene->ui_viz_rw;
   const int ui_viz_ro = scene->ui_viz_ro;
@@ -358,10 +361,13 @@ static void ui_draw_world(UIState *s) {
 
   nvgTranslate(s->vg, ui_viz_rx+ui_viz_ro, box_y + (box_h-inner_height)/2.0);
   nvgScale(s->vg, (float)viz_w / s->fb_w, (float)inner_height / s->fb_h);
-  nvgTranslate(s->vg, 240.0f, 0.0);
-  nvgTranslate(s->vg, -1440.0f / 2, -1080.0f / 2);
+
+  float w = 1440.0f; // Why 1440?
+  nvgTranslate(s->vg, (vwp_w - w) / 2.0f, 0.0);
+
+  nvgTranslate(s->vg, -w / 2, -1080.0f / 2);
   nvgScale(s->vg, 2.0, 2.0);
-  nvgScale(s->vg, 1440.0f / s->rgb_width, 1080.0f / s->rgb_height);
+  nvgScale(s->vg, w / s->rgb_width, 1080.0f / s->rgb_height);
 
   // Draw lane edges and vision/mpc tracks
   ui_draw_vision_lanes(s);
