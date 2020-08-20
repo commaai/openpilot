191d2e55d08bd26b89a837708e134fa0de06ab09
Willem Melching <willem.melching@gmail.com>
2020-08-20 21:33:32 +0200

    ui: force GLES context
    old-commit-hash: 2410c7e26bbf60dda83792d273f049586c1aa001

--- selfdrive/ui/qt/ui.cc
+++ selfdrive/ui/qt/ui.cc
@@ -4,10 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+  QSurfaceFormat fmt;
+  fmt.setRenderableType(QSurfaceFormat::OpenGLES);
+  QSurfaceFormat::setDefaultFormat(fmt);
+
   QApplication a(argc, argv);
 
   MainWindow w;
-
   w.setFixedSize(vwp_w, vwp_h);
   w.show();
 
