09d1dfd6c700c3f4ef782fcd7ec0850229b8a7b0
nuwandavek <vivekaithal44@gmail.com>
2020-08-21 12:04:01 -0700

    bugfix all op-sim docker issues

--- tools/sim/build_container.sh
+++ tools/sim/build_container.sh
@@ -6,6 +6,6 @@ if [ -n "$BUILD" ]; then
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   cd $DIR/../../
   docker pull docker.io/commaai/openpilotci:latest || true
-  docker build --cache-from commaai/openpilotci:latest -t commaai/openpilotci:latest -f Dockerfile.openpilot .
-  docker build --cache-from commaai/openpilot-sim:latest -t commaai/openpilot-sim:latest -f tools/sim/Dockerfile.sim .
+  docker build --cache-from docker.io/commaai/openpilotci:latest -t commaai/openpilotci:latest -f Dockerfile.openpilot .
+  docker build --cache-from docker.io/commaai/openpilot-sim:latest -t commaai/openpilot-sim:latest -f tools/sim/Dockerfile.sim .
 fi
--- tools/sim/run_op_sim.sh
+++ tools/sim/run_op_sim.sh
@@ -2,10 +2,10 @@
 
 if [ -n "$DEVELOP" ]; then
     echo "Develop Mode"
-    echo "Run openpilot-sim with manager and bridge (mounted as volume)"
-    docker run  --shm-size 1G --rm --net=host -e PASSIVE=0 -e NOBOARD=1 -e CUDA_VISIBLE_DEVICES=  --volume="$HOME/openpilot:/tmp/openpilot-sim" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --gpus all -e DISPLAY=$DISPLAY -it commaai/openpilot-sim:latest /bin/bash -c "screen -d -m python tmp/openpilot-sim/selfdrive/manager.py && screen python tmp/openpilot-sim/tools/sim/bridge.py"
+    echo "Run openpilot-sim with manager and bridge (local openpilot mounted as volume)"
+    docker run  --shm-size 1G --rm --net=host -e PASSIVE=0 -e NOBOARD=1 -e CUDA_VISIBLE_DEVICES=  --volume="$HOME/openpilot:/tmp/openpilot" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --gpus all -e DISPLAY=$DISPLAY -it commaai/openpilot-sim:latest /bin/bash -c "cd tmp/openpilot && scons -j$(nproc) && screen -d -m python selfdrive/manager.py && screen tools/sim/bridge.py"
     
 else 
     echo "Run openpilot-sim with manager and bridge"
-    docker run  --shm-size 1G --rm --net=host -e PASSIVE=0 -e NOBOARD=1 -e CUDA_VISIBLE_DEVICES=  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --gpus all -e DISPLAY=$DISPLAY -it commaai/openpilot-sim:latest /bin/bash -c "screen -d -m python tmp/openpilot/selfdrive/manager.py && screen python tmp/openpilot/tools/sim/bridge.py"
+    docker run  --shm-size 1G --rm --net=host -e PASSIVE=0 -e NOBOARD=1 -e CUDA_VISIBLE_DEVICES=  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --gpus all -e DISPLAY=$DISPLAY -it commaai/openpilot-sim:latest /bin/bash -c "cd tmp/openpilot && scons -j$(nproc) && screen -d -m python selfdrive/manager.py && screen tools/sim/bridge.py"
 fi
