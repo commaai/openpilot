924c0e8dd861ebd01ea299d2be449d45203b1d30
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-08-23 18:30:44 -0700

    start with openpilot-base

--- tools/sim/build_container.sh
+++ tools/sim/build_container.sh
@@ -3,5 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../../
 
-docker pull docker.io/commaai/openpilot-sim:latest || true
-docker build --cache-from docker.io/commaai/openpilot-sim:latest -t commaai/openpilot-sim:latest -f tools/sim/Dockerfile.sim .
+if [ -z "$(docker images -q commaai/openpilot-base:latest)" ]; then
+  echo "pulling base docker image"
+  docker pull commaai/openpilot-base:latest || true
+fi
+docker build \
+  --cache-from commaai/openpilot-sim:latest \
+  -t commaai/openpilot-sim:latest \
+  -f tools/sim/Dockerfile.sim .
--- tools/sim/start_openpilot_docker.sh
+++ tools/sim/start_openpilot_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # expose X to the container
-sudo xhost +locat:root
+xhost +local:root
 
 docker run \
   --rm \
@@ -9,8 +9,8 @@ docker run \
   --gpus all \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=$DISPLAY \
-  -e QT_X11_NO_MITSHM=1 \
   commaai/openpilot-sim:latest \
+  #/bin/bash -c 'cd $HOME/openpilot && tmux'
   /bin/bash
 
 #docker run --shm-size 1G --rm --net=host -e PASSIVE=0 -e NOBOARD=1 -e NOSENSOR=1 --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --gpus all -e DISPLAY=$DISPLAY -it commaai/openpilot-sim:latest /bin/bash
