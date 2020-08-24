65a345b2bbfecd8ff00844756389b2910ba6e798
Willem Melching <willem.melching@gmail.com>
2020-08-24 20:48:09 +0200

    Apple hacks

--- selfdrive/clocksd/clocksd.cc
+++ selfdrive/clocksd/clocksd.cc
@@ -1,13 +1,20 @@
+#include <chrono>
+#include <thread>
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/resource.h>
-#include <sys/timerfd.h>
 #include <sys/time.h>
 
 #include <cassert>
 #include "messaging.hpp"
 #include "common/timing.h"
+
+// Apple doesn't have timerfd
+#ifndef __APPLE__
+#include <sys/timerfd.h>
+#endif
 
 #ifdef QCOM
 namespace {
@@ -22,9 +29,9 @@ namespace {
 int main() {
   setpriority(PRIO_PROCESS, 0, -13);
 
-  int err = 0;
   PubMaster pm({"clocks"});
 
+#ifndef __APPLE__
   int timerfd = timerfd_create(CLOCK_BOOTTIME, 0);
   assert(timerfd >= 0);
 
@@ -34,12 +41,17 @@ int main() {
   spec.it_value.tv_sec = 1;
   spec.it_value.tv_nsec = 0;
 
-  err = timerfd_settime(timerfd, 0, &spec, 0);
+  int err = timerfd_settime(timerfd, 0, &spec, 0);
   assert(err == 0);
 
   uint64_t expirations = 0;
   while ((err = read(timerfd, &expirations, sizeof(expirations)))) {
     if (err < 0) break;
+#else
+  // Just run at 1Hz on apple
+  while (true){
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+#endif
 
     uint64_t boottime = nanos_since_boot();
     uint64_t monotonic = nanos_monotonic();
@@ -66,6 +78,8 @@ int main() {
     pm.send("clocks", msg);
   }
 
+#ifndef __APPLE__
   close(timerfd);
+#endif
   return 0;
 }
