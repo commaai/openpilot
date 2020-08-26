622e42d504458ebfbb23e945beb1a60bbfd0e63b
robbederks <robbe.derks@gmail.com>
2020-08-26 11:30:30 +0200

    Tici sensord (#2072)
    * tici sensord: WIP
    
    * add sensor constants
    
    * rename files
    
    * base class
    
    * init sensors
    
    * publish something
    
    * dont leak memory
    
    * try reading from accel
    
    * Accel works
    
    * add time and sensor source
    
    * Update release files
    
    * If we want low BW we want 125hz
    
    * this should run gyro as well
    
    * add filter on gyro
    
    * also filter accel data
    
    * Add i2c files
    
    * cast makes macos unhappy
    
    * Same outputs as android sensord
    
    Co-authored-by: Tici <robbe@comma.ai>
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

---
+++ selfdrive/sensord/sensors/bmx055_accel.cc
@@ -0,0 +1,71 @@
+#include <cassert>
+#include "common/swaglog.h"
+#include "common/timing.h"
+
+#include "bmx055_accel.hpp"
+
+
+BMX055_Accel::BMX055_Accel(I2CBus *bus) : I2CSensor(bus) {}
+
+int BMX055_Accel::init(){
+  int ret = 0;
+  uint8_t buffer[1];
+
+  ret = read_register(BMX055_ACCEL_I2C_REG_ID, buffer, 1);
+  if(ret < 0){
+    LOGE("Reading chip ID failed: %d", ret);
+    goto fail;
+  }
+
+  if(buffer[0] != BMX055_ACCEL_CHIP_ID){
+    LOGE("Chip ID wrong. Got: %d, Expected %d", buffer[0], BMX055_ACCEL_CHIP_ID);
+    ret = -1;
+    goto fail;
+  }
+
+  // High bandwidth
+  // ret = set_register(BMX055_ACCEL_I2C_REG_HBW, BMX055_ACCEL_HBW_ENABLE);
+  // if (ret < 0){
+  //   goto fail;
+  // }
+
+  // Low bandwidth
+  ret = set_register(BMX055_ACCEL_I2C_REG_HBW, BMX055_ACCEL_HBW_DISABLE);
+  if (ret < 0){
+    goto fail;
+  }
+  ret = set_register(BMX055_ACCEL_I2C_REG_BW, BMX055_ACCEL_BW_125HZ);
+  if (ret < 0){
+    goto fail;
+  }
+
+fail:
+  return ret;
+}
+
+void BMX055_Accel::get_event(cereal::SensorEventData::Builder &event){
+  uint64_t start_time = nanos_since_boot();
+  uint8_t buffer[6];
+  int len = read_register(BMX055_ACCEL_I2C_REG_FIFO, buffer, sizeof(buffer));
+  assert(len == 6);
+
+  // 12 bit = +-2g
+  float scale = 9.81 * 2.0f / (1 << 11);
+  float x = -read_12_bit(buffer[0], buffer[1]) * scale;
+  float y = -read_12_bit(buffer[2], buffer[3]) * scale;
+  float z = read_12_bit(buffer[4], buffer[5]) * scale;
+
+  event.setSource(cereal::SensorEventData::SensorSource::BMX055);
+  event.setVersion(1);
+  event.setSensor(SENSOR_ACCELEROMETER);
+  event.setType(SENSOR_TYPE_ACCELEROMETER);
+  event.setTimestamp(start_time);
+
+  float xyz[] = {x, y, z};
+  kj::ArrayPtr<const float> vs(&xyz[0], 3);
+
+  auto svec = event.initAcceleration();
+  svec.setV(vs);
+  svec.setStatus(true);
+
+}
---
+++ selfdrive/sensord/sensors/i2c_sensor.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "i2c_sensor.hpp"
+
+int16_t read_12_bit(uint8_t lsb, uint8_t msb){
+  uint16_t combined = (uint16_t(msb) << 8) | uint16_t(lsb & 0xF0);
+  return int16_t(combined) / (1 << 4);
+}
+
+int16_t read_16_bit(uint8_t lsb, uint8_t msb){
+  uint16_t combined = (uint16_t(msb) << 8) | uint16_t(lsb);
+  return int16_t(combined);
+}
+
+
+I2CSensor::I2CSensor(I2CBus *bus) : bus(bus){
+}
+
+int I2CSensor::read_register(uint register_address, uint8_t *buffer, uint8_t len){
+  return bus->read_register(get_device_address(), register_address, buffer, len);
+}
+
+int I2CSensor::set_register(uint register_address, uint8_t data){
+  return bus->set_register(get_device_address(), register_address, data);
+}
