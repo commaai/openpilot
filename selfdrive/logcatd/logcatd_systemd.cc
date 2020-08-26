f6536e1f470444fcbe92e0deefd95fbc3fdc63ef
Willem Melching <willem.melching@gmail.com>
2020-08-26 14:47:21 +0200

    More compact and cleanup

--- selfdrive/logcatd/logcatd_systemd.cc
+++ selfdrive/logcatd/logcatd_systemd.cc
@@ -10,20 +10,17 @@
 #include "messaging.hpp"
 
 int main(int argc, char *argv[]) {
-  int r;
-  sd_journal *journal;
-
   PubMaster pm({"androidLog"});
 
+  sd_journal *journal;
   assert(sd_journal_open(&journal, 0) >= 0);
-
-  // needed so sd_journal_wait() works properly if files rotate
-  assert(sd_journal_get_fd(journal) >= 0);
-
+  assert(sd_journal_get_fd(journal) >= 0); // needed so sd_journal_wait() works properly if files rotate
   assert(sd_journal_seek_tail(journal) >= 0);
 
+  int r;
   while (true) {
     r = sd_journal_next(journal);
+    assert(r >= 0);
 
     // Wait for new message if we didn't receive anything
     if (r == 0){
@@ -33,6 +30,8 @@ int main(int argc, char *argv[]) {
       } while (r == SD_JOURNAL_NOP);
 
       r = sd_journal_next(journal);
+      assert(r >= 0);
+
       if (r == 0) continue; // Try again if we still didn't get anything
     }
 
@@ -47,33 +46,25 @@ int main(int argc, char *argv[]) {
     SD_JOURNAL_FOREACH_DATA(journal, data, length){
       std::string str((char*)data, length);
 
+      // Split "KEY=VALUE"" on "=" and put in map
       std::size_t found = str.find("=");
       if (found != std::string::npos){
-        std::string key = str.substr(0, found);
-        std::string value = str.substr(found + 1, std::string::npos);
-        kv[key] = value;
+        kv[str.substr(0, found)] = str.substr(found + 1, std::string::npos);
       }
     }
-
 
     capnp::MallocMessageBuilder msg;
     cereal::Event::Builder event = msg.initRoot<cereal::Event>();
     event.setLogMonoTime(nanos_since_boot());
+
+    // Build message
     auto androidEntry = event.initAndroidLog();
-
-    if (kv.count("_PID")){
-      androidEntry.setPid(std::atoi(kv["_PID"].c_str()));
-    }
-    if (kv.count("PRIORITY")){
-      androidEntry.setPriority(std::atoi(kv["PRIORITY"].c_str()));
-    }
-    if (kv.count("SYSLOG_IDENTIFIER")){
-      androidEntry.setTag(kv["SYSLOG_IDENTIFIER"]);
-    }
-
     androidEntry.setTs(timestamp);
-
     androidEntry.setMessage(json11::Json(kv).dump());
+    if (kv.count("_PID")) androidEntry.setPid(std::atoi(kv["_PID"].c_str()));
+    if (kv.count("PRIORITY")) androidEntry.setPriority(std::atoi(kv["PRIORITY"].c_str()));
+    if (kv.count("SYSLOG_IDENTIFIER")) androidEntry.setTag(kv["SYSLOG_IDENTIFIER"]);
+
     pm.send("androidLog", msg);
   }
 
