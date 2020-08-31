bcfbf414ffac3384ab107747728303b3d8885119
Shane Smiskol <shane@smiskol.com>
2020-08-31 17:19:28 -0500

    this actually isn't required anymore

--- selfdrive/ui/text/text.c
+++ selfdrive/ui/text/text.c
@@ -89,7 +89,7 @@ assert(font >= 0);
   int b_h = 150;
 
   nvgBeginPath(vg);
-  nvgFillColor(vg, nvgRGBA(8, 8, 8, 178));
+  nvgFillColor(vg, nvgRGBA(8, 8, 8, 255));
   nvgRoundedRect(vg, b_x, b_y, b_w, b_h, 20);
   nvgFill(vg);
 
