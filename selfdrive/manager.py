4680143e9448d257f689094dda9fe9a4dc38f300
Shane Smiskol <shane@smiskol.com>
2020-08-31 18:06:03 -0500

    clean up

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -623,11 +623,8 @@ if __name__ == "__main__":
     add_logentries_handler(cloudlog)
     cloudlog.exception("Manager failed to start")
 
-    # Show last 4 lines of traceback + last error line
+    # Show last 3 lines of traceback
     error = [line.replace('/data/openpilot', '') for line in traceback.format_exc(-3).splitlines()]
-    # error = [' ' * (len(line) - len(line.lstrip())) + line for line in error]  # double up indentation
-    # error = '\n'.join([line.replace('/data', '') for line in error])  # make line lengths shorter
-
     error = "Manager failed to start\n \n" + '\n'.join(error)
     with TextWindow(error) as t:
       t.wait_for_exit()
