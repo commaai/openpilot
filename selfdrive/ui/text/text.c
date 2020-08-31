a024a264faf8bc7121b575ae72a10bfddf3cca72
Shane Smiskol <shane@smiskol.com>
2020-08-31 16:11:12 -0500

    70% transparency for black background

--- selfdrive/ui/text/text.c
+++ selfdrive/ui/text/text.c
@@ -89,7 +89,7 @@ assert(font >= 0);
   int b_h = 150;
 
   nvgBeginPath(vg);
-  nvgFillColor(vg, nvgRGBA(8, 8, 8, 255));
+  nvgFillColor(vg, nvgRGBAf(8, 8, 8, 0.7));
   nvgRoundedRect(vg, b_x, b_y, b_w, b_h, 20);
   nvgFill(vg);
 
