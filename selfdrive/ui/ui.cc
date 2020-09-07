fe68610e40aed3954bb6ca3922d66f22144b1a4c
Willem Melching <willem.melching@gmail.com>
2020-09-07 12:20:49 +0200

    fix typo

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -201,7 +201,7 @@ void update_sockets(UIState *s) {
   if (sm.updated("health")) {
     auto health = sm["health"].getHealth();
     scene.hwType = health.getHwType();
-    s->igintion = health.getIgnitionLine() || health.getIgnitionCan();
+    s->ignition = health.getIgnitionLine() || health.getIgnitionCan();
   } else if ((s->sm->frame - s->sm->rcv_frame("health")) > 5*UI_FREQ) {
     scene.hwType = cereal::HealthData::HwType::UNKNOWN;
   }
