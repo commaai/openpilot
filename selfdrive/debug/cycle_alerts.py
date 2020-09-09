0ae5e7403ec43b657441509cfd5265590e5335b1
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-09-09 10:04:28 -0700

    QT UI: sounds (#2078)
    * move android into own dir
    
    * fix name
    
    * maybe this works? qt ui doesn't work on mac
    
    * fix that
    
    * pc sound works
    
    * fix pc build
    
    * lowercase
    
    * that needs to be real_arch
    
    * split into classes
    
    * fix typo in lib
    
    * Fix cycle alerts
    
    * Add qt multimedia libs to install scripts
    
    * Add ui/android folder
    
    * Fix android build
    
    * Raise exception if sound init fails
    
    * add missing return
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: acd1bde496904fbc0d97f4d89e630358c4d11f7f

--- selfdrive/debug/cycle_alerts.py
+++ selfdrive/debug/cycle_alerts.py
@@ -7,52 +7,63 @@ import argparse
 import time
 
 import cereal.messaging as messaging
-from selfdrive.controls.lib.events import EVENTS, Alert
+from selfdrive.car.honda.interface import CarInterface
+from selfdrive.controls.lib.events import ET, EVENTS, Alert, Events
+from selfdrive.controls.lib.alertmanager import AlertManager
 
-def now_millis(): return time.time() * 1000
 
-ALERTS = [a for _, et in EVENTS.items() for _, a in et.items() if isinstance(a, Alert)]
+def cycle_alerts(duration=200, is_metric=False):
+  alerts = list(EVENTS.keys())
+  print(alerts)
 
-#from cereal import car
-#ALERTS = [a for a in ALERTS if a.audible_alert == car.CarControl.HUDControl.AudibleAlert.chimeWarningRepeat]
-
-default_alerts = sorted(ALERTS, key=lambda alert: (alert.alert_size, len(alert.alert_text_2)))
-
-def cycle_alerts(duration_millis, alerts=None):
-  if alerts is None:
-    alerts = default_alerts
+  CP = CarInterface.get_params("HONDA CIVIC 2016 TOURING")
+  sm = messaging.SubMaster(['thermal', 'health', 'frame', 'model', 'liveCalibration',
+                            'dMonitoringState', 'plan', 'pathPlan', 'liveLocationKalman'])
 
   controls_state = messaging.pub_sock('controlsState')
+  thermal = messaging.pub_sock('thermal')
 
   idx, last_alert_millis = 0, 0
   alert = alerts[0]
+
+  events = Events()
+  AM = AlertManager()
+
+  frame = 0
+
   while 1:
-    if (now_millis() - last_alert_millis) > duration_millis:
-      alert = alerts[idx]
+    if frame % duration == 0:
       idx = (idx + 1) % len(alerts)
-      last_alert_millis = now_millis()
-      print('sending {}'.format(str(alert)))
+      events.clear()
+      events.add(alerts[idx])
+
+
+    current_alert_types = [ET.PERMANENT, ET.USER_DISABLE, ET.IMMEDIATE_DISABLE,
+                           ET.SOFT_DISABLE, ET.PRE_ENABLE, ET.NO_ENTRY,
+                           ET.ENABLE, ET.WARNING]
+    a = events.create_alerts(current_alert_types, [CP, sm, is_metric])
+    AM.add_many(frame, a)
+    AM.process_alerts(frame)
 
     dat = messaging.new_message()
     dat.init('controlsState')
 
-    dat.controlsState.alertType = alert.alert_type
-    dat.controlsState.alertText1 = alert.alert_text_1
-    dat.controlsState.alertText2 = alert.alert_text_2
-    dat.controlsState.alertSize = alert.alert_size
-    #dat.controlsState.alertStatus = alert.alert_status
-    dat.controlsState.alertSound = alert.audible_alert
+    dat.controlsState.alertText1 = AM.alert_text_1
+    dat.controlsState.alertText2 = AM.alert_text_2
+    dat.controlsState.alertSize = AM.alert_size
+    dat.controlsState.alertStatus = AM.alert_status
+    dat.controlsState.alertBlinkingRate = AM.alert_rate
+    dat.controlsState.alertType = AM.alert_type
+    dat.controlsState.alertSound = AM.audible_alert
     controls_state.send(dat.to_bytes())
 
+    dat = messaging.new_message()
+    dat.init('thermal')
+    dat.thermal.started = True
+    thermal.send(dat.to_bytes())
+
+    frame += 1
     time.sleep(0.01)
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--duration', type=int, default=1000)
-  parser.add_argument('--alert-types', nargs='+')
-  args = parser.parse_args()
-  alerts = None
-  if args.alert_types:
-    alerts = [next(a for a in ALERTS if a.alert_type==alert_type) for alert_type in args.alert_types]
-
-  cycle_alerts(args.duration, alerts=alerts)
+  cycle_alerts()
--- selfdrive/ui/sound.hpp
+++ selfdrive/ui/sound.hpp
@@ -2,31 +2,23 @@
 #include <map>
 #include "cereal/gen/cpp/log.capnp.h"
 
-#if defined(QCOM)
-#include <SLES/OpenSLES.h>
-#include <SLES/OpenSLES_Android.h>
-#endif
-
 typedef cereal::CarControl::HUDControl::AudibleAlert AudibleAlert;
 
-class Sound {
- public:
-  Sound() = default;
-  bool init(int volume);
-  bool play(AudibleAlert alert);
-  void stop();
-  void setVolume(int volume);
-  ~Sound();
+static std::map<AudibleAlert, std::pair<const char *, int>> sound_map {
+  // AudibleAlert, (file path, loop count)
+  {AudibleAlert::CHIME_DISENGAGE, {"../assets/sounds/disengaged.wav", 0}},
+  {AudibleAlert::CHIME_ENGAGE, {"../assets/sounds/engaged.wav", 0}},
+  {AudibleAlert::CHIME_WARNING1, {"../assets/sounds/warning_1.wav", 0}},
+  {AudibleAlert::CHIME_WARNING2, {"../assets/sounds/warning_2.wav", 0}},
+  {AudibleAlert::CHIME_WARNING2_REPEAT, {"../assets/sounds/warning_2.wav", 3}},
+  {AudibleAlert::CHIME_WARNING_REPEAT, {"../assets/sounds/warning_repeat.wav", 3}},
+  {AudibleAlert::CHIME_ERROR, {"../assets/sounds/error.wav", 0}},
+  {AudibleAlert::CHIME_PROMPT, {"../assets/sounds/error.wav", 0}}
+};
 
-#if defined(QCOM)
- private:
-  SLObjectItf engine_ = nullptr;
-  SLObjectItf outputMix_ = nullptr;
-  int last_volume_ = 0;
-  double last_set_volume_time_ = 0.;
-  AudibleAlert currentSound_ = AudibleAlert::NONE;
-  struct Player;
-  std::map<AudibleAlert, Player *> player_;
-  friend void SLAPIENTRY slplay_callback(SLPlayItf playItf, void *context, SLuint32 event);
-#endif
+class Sound {
+public:
+  virtual bool play(AudibleAlert alert) = 0;
+  virtual void stop() = 0;
+  virtual void setVolume(int volume) = 0;
 };
