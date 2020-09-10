702aa9cb369dcadefb4b3e2c6e256dae59265d95
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-10 17:19:26 -0400

    add resume spam counter

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -62,10 +62,10 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
   return packer.make_can_msg("LKAS11", 0, values)
 
 
-def create_clu11(packer, frame, clu11, button):
+def create_clu11(packer, frame, clu11, button, cnt):
   values = clu11
   values["CF_Clu_CruiseSwState"] = button
-  values["CF_Clu_AliveCnt1"] = frame % 0x10
+  values["CF_Clu_AliveCnt1"] = cnt
   return packer.make_can_msg("CLU11", 0, values)
 
 
