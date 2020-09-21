e7598411f05b25fd341807068266fb70a4276e6a
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-21 09:27:50 +0900

    Update test_car_models.py

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -193,7 +193,7 @@ routes = {
     'enableCamera': True,
   },
   "b2a38c712dcf90bd|2020-05-18--18-12-48": {
-    'carFingerprint': HYUNDAI.SONATA_2019,
+    'carFingerprint': HYUNDAI.SONATA19,
     'enableCamera': True,
   },
   "5875672fc1d4bf57|2020-07-23--21-33-28": {
