d461f9fa2c61598c6141eb1109469c0d86ad777f
Willem Melching <willem.melching@gmail.com>
2020-09-22 10:29:11 +0200

    no camera for VW

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -69,10 +69,10 @@ class CarInterface(CarInterfaceBase):
     # The camera CAN has no signals we use at this time, but we process it
     # anyway so we can test connectivity with can_valid
     self.cp.update_strings(can_strings)
-    self.cp_cam.update_strings(can_strings)
+    # self.cp_cam.update_strings(can_strings)
 
     ret = self.CS.update(self.cp)
-    ret.canValid = self.cp.can_valid and self.cp_cam.can_valid
+    ret.canValid = self.cp.can_valid
     ret.steeringRateLimited = self.CC.steer_rate_limited if self.CC is not None else False
 
     # TODO: add a field for this to carState, car interface code shouldn't write params
