c00d90207a379d8805bff7f64a7a94fa021934df
grekiki <gregor1234567890@gmail.com>
2020-09-22 11:17:44 +0200

    Update selfdrive/loggerd/tools/mark_all_uploaded.py
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/loggerd/tools/mark_all_uploaded.py
+++ selfdrive/loggerd/tools/mark_all_uploaded.py
@@ -2,8 +2,8 @@ import os
 from common.xattr import setxattr
 from selfdrive.loggerd.uploader import UPLOAD_ATTR_NAME, UPLOAD_ATTR_VALUE
 
-PATH = "/data/media/0/realdata/"
-for folder in os.walk(PATH):
+from selfdrive.loggerd.config import ROOT
+for folder in os.walk(ROOT):
   for file1 in folder[2]:
     full_path = os.path.join(folder[0], file1)
     setxattr(full_path, UPLOAD_ATTR_NAME, UPLOAD_ATTR_VALUE)
