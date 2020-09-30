7cf0b6d993f9b7db2af0ce56d4886d02c02ecbce
ZwX1616 <zwx1616@gmail.com>
2020-09-29 20:26:31 -0700

    first will be imperfect

--- selfdrive/loggerd/tests/test_loggerd.py
+++ selfdrive/loggerd/tests/test_loggerd.py
@@ -95,7 +95,7 @@ class TestLoggerd(unittest.TestCase):
         cmd = f"ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames \
                -of default=nokey=1:noprint_wrappers=1 {file_path}"
         expected_frames = self.segment_length * CAMERA_FPS // 2 if (EON and camera=='dcamera') else self.segment_length * CAMERA_FPS
-        frame_tolerance = FRAME_TOLERANCE+1 if (EON and camera=='dcamera') else FRAME_TOLERANCE
+        frame_tolerance = FRAME_TOLERANCE+1 if (EON and camera=='dcamera') or i==0 else FRAME_TOLERANCE
         frame_count = int(subprocess.check_output(cmd, shell=True, encoding='utf8').strip())
 
         self.assertTrue(abs(expected_frames - frame_count) <= frame_tolerance,
