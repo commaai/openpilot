4a0c65675ab124f993c5789a00728be16ddfc785
baldwalker <insigniadesign@yahoo.co.uk>
2020-10-08 09:40:41 +0100

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -139,8 +139,12 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.7
       ret.steerRatio = 13.73   #Spec
       tire_stiffness_factor = 0.385
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+      ret.lateralTuning.pid.kiBP = [0., 1., 5.]
+      ret.lateralTuning.pid.kpV = [0.01, 0.02, 0.03]
+      ret.lateralTuning.pid.kpBP = [0., 10., 30.]
+      ret.lateralTuning.pid.kiV = [0.001, 0.002, 0.003]
+      ret.lateralTuning.pid.kfBP = [0., 10., 30.]
+      ret.lateralTuning.pid.kfV = [0.00002, 0.00002, 0.00002]
     elif candidate in [CAR.IONIQ, CAR.IONIQ_EV_LTD]:
       ret.lateralTuning.pid.kf = 0.00006
       ret.mass = 1490. + STD_CARGO_KG   #weight per hyundai site https://www.hyundaiusa.com/ioniq-electric/specifications.aspx
