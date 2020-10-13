77930e145d2774fb5650424a6e5e03c129675f6e
Willem Melching <willem.melching@gmail.com>
2020-10-13 12:01:29 +0200

    recusrively create directories

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -73,12 +73,42 @@ static int fsync_dir(const char* path){
 }
 
 static int ensure_dir_exists(std::string path) {
-  struct stat st;
-  if (stat(path.c_str(), &st) == -1) {
-    return mkdir(path.c_str(), 0700);
+  // TODO: replace by std::filesystem::create_directories
+  return mkdir_p(path.c_str());
+}
+
+static int mkdir_p(const char *path) {
+  /* Adapted from http://stackoverflow.com/a/2336245/119527 */
+  const size_t len = strlen(path);
+  char _path[PATH_MAX];
+  char *p;
+  errno = 0;
+
+  /* Copy string so its mutable */
+  if (len > sizeof(_path)-1) {
+    errno = ENAMETOOLONG;
+    return -1;
+  }
+
+  strcpy(_path, path);
+  /* Iterate the string */
+  for (p = _path + 1; *p; p++) {
+    if (*p == '/') {
+      /* Temporarily truncate */
+      *p = '\0';
+      if (mkdir(_path, 0775) != 0) {
+        if (errno != EEXIST)
+          return -1;
+      }
+      *p = '/';
+    }
+  }
+  if (mkdir(_path, 0775) != 0) {
+    if (errno != EEXIST)
+      return -1;
   }
   return 0;
-}
+  }
 
 Params::Params(bool persistent_param){
   const char * path = persistent_param ? persistent_params_path : default_params_path;
