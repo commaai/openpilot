bb59db1a2c5cc9af42428a056b3a99219678d250
Jason Young <jyoung8607@gmail.com>
2020-10-13 12:31:28 -0400

    You can't triple stamp a double stamp

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -123,7 +123,7 @@ class CarInterfaceBase():
     # Disable on rising edge of gas or brake. Also disable on brake when speed > 0.
     # Optionally allow to press gas at zero speed to resume.
     # e.g. Chrysler does not spam the resume button yet, so resuming with gas is handy. FIXME!
-    if (cs_out.gasPressed and (not self.is_disengage_on_gas_enabled) and (not self.CS.out.gasPressed) and cs_out.vEgo > gas_resume_speed) or \
+    if (cs_out.gasPressed and self.is_disengage_on_gas_enabled and (not self.CS.out.gasPressed) and cs_out.vEgo > gas_resume_speed) or \
        (cs_out.brakePressed and (not self.CS.out.brakePressed or not cs_out.standstill)):
       events.add(EventName.pedalPressed)
 
