e81aa55b8974b489756c6cfca5d366100c7b3343
deanlee <deanlee3@gmail.com>
2020-10-16 14:57:32 +0000

    remove testraw into test_encoder.cc

--- selfdrive/loggerd/tests/test_encoder.cc
+++ selfdrive/loggerd/tests/test_encoder.cc
@@ -4,24 +4,77 @@
 #include <cstdio>
 #include <cstdlib>
 #include <sys/stat.h>
+#include <sys/times.h>
+#include <memory>
 #include "common/timing.h"
 #include "common/visionipc.h"
 #include "encoder.h"
+#include "raw_logger.h"
 #define MAIN_FPS 20
 #define MAIN_BITRATE 5000000
 #define QCAM_BITRATE 128000
 const int segment_length = 60;
+
+enum TestType {
+  typeEncoder = 0x01,
+  typeRawLogger = 0x02,
+  typeAll = typeEncoder | typeRawLogger
+};
+
+static clock_t lastCPU, lastSysCPU, lastUserCPU;
+static int numProcessors;
+
+void cpuusage_init() {
+  FILE *file;
+  struct tms timeSample;
+  char line[128];
+
+  lastCPU = times(&timeSample);
+  lastSysCPU = timeSample.tms_stime;
+  lastUserCPU = timeSample.tms_utime;
+
+  file = fopen("/proc/cpuinfo", "r");
+  numProcessors = 0;
+  while (fgets(line, 128, file) != NULL) {
+    if (strncmp(line, "processor", 9) == 0) numProcessors++;
+  }
+  fclose(file);
+}
+
+double cupusage_get() {
+  struct tms timeSample;
+  clock_t now;
+  double percent;
+
+  now = times(&timeSample);
+  if (now <= lastCPU || timeSample.tms_stime < lastSysCPU ||
+      timeSample.tms_utime < lastUserCPU) {
+    percent = -1.0;
+  } else {
+    percent = (timeSample.tms_stime - lastSysCPU) +
+              (timeSample.tms_utime - lastUserCPU);
+    percent /= (now - lastCPU);
+    percent /= numProcessors;
+    percent *= 100;
+  }
+  lastCPU = now;
+  lastSysCPU = timeSample.tms_stime;
+  lastUserCPU = timeSample.tms_utime;
+  return percent;
+}
+
 LogCameraInfo cameras_logged[LOG_CAMERA_ID_MAX] = {
-    [LOG_CAMERA_ID_FCAMERA] = {
-      .stream_type = VISION_STREAM_YUV,
-      .filename = "fcamera.hevc",
-      .frame_packet_name = "frame",
-      .encode_idx_name = "encodeIdx",
-      .fps = 20,
-      .bitrate = 5000000,
-      .is_h265 = true,
-      .downscale = false,
-      .has_qcamera = true},
+  [LOG_CAMERA_ID_FCAMERA] = {
+    .stream_type = VISION_STREAM_YUV,
+    .filename = "fcamera.hevc",
+    .frame_packet_name = "frame",
+    .encode_idx_name = "encodeIdx",
+    .fps = MAIN_FPS,
+    .bitrate = MAIN_BITRATE,
+    .is_h265 = true,
+    .downscale = false,
+    .has_qcamera = true
+  },
   [LOG_CAMERA_ID_QCAMERA] = {
     .filename = "qcamera.ts",
     .fps = MAIN_FPS,
@@ -36,12 +89,35 @@ LogCameraInfo cameras_logged[LOG_CAMERA_ID_MAX] = {
   },
 };
 
-int main() {
+// USAGE:
+// test_encoder 1 
+//   test encoder
+// test_encoder 2
+//   test rawlogger
+// test_encoder
+//   test encoder&rawlogger
+int main(int argc, char *argv[]) {
+  TestType test_type = typeAll;
+  if (argc > 1) {
+    int t = strtol(argv[1], nullptr, 10);
+    if (t == 1) {
+      test_type = typeEncoder;
+      printf(" test Encoder\n");
+    } else if (t == 2){
+      test_type = typeRawLogger;
+      printf(" test RawLogger\n");
+    }
+  }
+  if (test_type == typeAll) {
+     printf(" test Encoder&RawLogger\n");
+  }
+
+  std::unique_ptr<EncoderState> encoder;
+  std::unique_ptr<EncoderState> encoder_alt;
+  std::unique_ptr<RawLogger> rawlogger;
+
   const char *output_path = "./output";
   mkdir(output_path, 0777);
-
-  EncoderState encoder = {};
-  EncoderState encoder_alt = {};
 
   VisionStream stream;
   VisionStreamBufs buf_info;
@@ -54,12 +130,23 @@ int main() {
     break;
   }
 
-  encoder_init(&encoder, &cameras_logged[LOG_CAMERA_ID_FCAMERA], buf_info.width, buf_info.height);
-  encoder_init(&encoder_alt, &cameras_logged[LOG_CAMERA_ID_QCAMERA], buf_info.width, buf_info.height);
+  if (test_type & typeEncoder) {
+    encoder.reset(new EncoderState);
+    encoder_alt.reset(new EncoderState);
+    encoder_init(encoder.get(), &cameras_logged[LOG_CAMERA_ID_FCAMERA], buf_info.width, buf_info.height);
+    encoder_init(encoder_alt.get(), &cameras_logged[LOG_CAMERA_ID_QCAMERA], buf_info.width, buf_info.height);
 
-  encoder_open(&encoder, output_path);
-  encoder_open(&encoder_alt, output_path);
+    encoder_open(encoder.get(), output_path);
+    encoder_open(encoder_alt.get(), output_path);
+  }
+  if (test_type & typeRawLogger) {
+    rawlogger.reset(new RawLogger("prcamera", buf_info.width, buf_info.height, MAIN_FPS));
+    rawlogger->Rotate(output_path, 1);
+  }
+  
+  cpuusage_init();
   double t1 = millis_since_boot();
+  
   int cnt = 0;
   for (; cnt < segment_length * MAIN_FPS; cnt++) {
     VIPCBufExtra extra;
@@ -71,18 +158,30 @@ int main() {
     uint8_t *y = (uint8_t *)buf->addr;
     uint8_t *u = y + (buf_info.width * buf_info.height);
     uint8_t *v = u + (buf_info.width / 2) * (buf_info.height / 2);
-    encoder_encode_frame(&encoder, y, u, v, &extra);
-    encoder_encode_frame(&encoder_alt, y, u, v, &extra);
+    if (encoder) {
+      encoder_encode_frame(encoder.get(), y, u, v, &extra);
+      encoder_encode_frame(encoder_alt.get(), y, u, v, &extra);
+    }
+    if (rawlogger) {
+      double r1 = millis_since_boot();
+      rawlogger->LogFrame(cnt, y, u, v, nullptr);
+      double r2 = millis_since_boot();
+      printf("rawlogger->logFrame %d %.2f\n", cnt, (r2 - r1));
+    }
   }
-  encoder_close(&encoder);
-  encoder_close(&encoder_alt);
-
   double t2 = millis_since_boot();
-  printf("finish. time: %.0f ms, cnt: %d, avg: %.0fms \n", t2 - t1, cnt, (t2 - t1) / cnt);
+  printf("total time: %.0f ms, cnt: %d, avg: %.0fms/frame, avg cpu:%.2f\n", t2 - t1, cnt, (t2 - t1) / cnt, cupusage_get());
 
   visionstream_destroy(&stream);
 
-  encoder_destroy(&encoder);
-  encoder_destroy(&encoder_alt);
+  if (encoder) {
+    encoder_close(encoder.get());
+    encoder_close(encoder_alt.get());
+    encoder_destroy(encoder.get());
+    encoder_destroy(encoder_alt.get());
+  }
+  if (rawlogger) {
+    rawlogger->Close();
+  }
   return 0;
 }
