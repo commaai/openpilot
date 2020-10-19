16ea2c130f2999eab31193ba41fbe718f13b1c39
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-10-19 16:02:52 -0700

    explicitly turn display on

--- installer/updater/updater.cc
+++ installer/updater/updater.cc
@@ -231,6 +231,9 @@ struct Updater {
                           &fb_w, &fb_h);
     assert(fb);
 
+    framebuffer_set_power(fb, HWC_POWER_MODE_NORMAL);
+    set_brightness(255);
+
     vg = nvgCreateGLES3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
     assert(vg);
 
