d28f488ea56c78c4f70fd3c8c16114ffa48ba38a
Willem Melching <willem.melching@gmail.com>
2020-10-19 14:25:33 +0200

    fix typo in params path on PC

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -33,7 +33,7 @@ std::string getenv_default(const char* env_var, const char * suffix, const char*
 #if defined(QCOM) || defined(QCOM2)
 const std::string default_params_path = "/data/params";
 #else
-const std::string default_params_path = getenv_default("BASEDIR", "persists/params", "/data/params");
+const std::string default_params_path = getenv_default("BASEDIR", "/persist/params", "/data/params");
 #endif
 
 #if defined(QCOM) || defined(QCOM2)
@@ -45,7 +45,6 @@ const std::string persistent_params_path = default_params_path;
 
 volatile sig_atomic_t params_do_exit = 0;
 void params_sig_handler(int signal) {
-  std::cout << "got signal" << std::endl;
   params_do_exit = 1;
 }
 
