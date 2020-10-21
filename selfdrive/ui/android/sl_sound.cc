e9d58939d2cada20e655af7e1899eeeb5e3eebc8
ZwX1616 <zwx1616@gmail.com>
2020-10-20 17:42:19 -0700

    infinite sound (#2372)
    * make sound infinite loopable
    
    * Update selfdrive/ui/android/sl_sound.cc
    
    * fix loop
    
    * fix test threshold
    
    * more fix
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 53bd3050ee90ca18445ddc41b837f5f927cc0c6a

--- selfdrive/ui/android/sl_sound.cc
+++ selfdrive/ui/android/sl_sound.cc
@@ -15,7 +15,6 @@
 struct SLSound::Player {
   SLObjectItf player;
   SLPlayItf playItf;
-  // slplay_callback runs on a background thread,use atomic to ensure thread safe.
   std::atomic<int> repeat;
 };
 
@@ -57,8 +56,8 @@ bool SLSound::init() {
 
 void SLAPIENTRY slplay_callback(SLPlayItf playItf, void *context, SLuint32 event) {
   SLSound::Player *s = reinterpret_cast<SLSound::Player *>(context);
-  if (event == SL_PLAYEVENT_HEADATEND && s->repeat > 1) {
-    --s->repeat;
+  if (event == SL_PLAYEVENT_HEADATEND && s->repeat != 0) {
+    if (s->repeat > 0) --s->repeat;
     (*playItf)->SetPlayState(playItf, SL_PLAYSTATE_STOPPED);
     (*playItf)->SetMarkerPosition(playItf, 0);
     (*playItf)->SetPlayState(playItf, SL_PLAYSTATE_PLAYING);
@@ -69,10 +68,13 @@ bool SLSound::play(AudibleAlert alert) {
   if (currentSound_ != AudibleAlert::NONE) {
     stop();
   }
+
   auto player = player_.at(alert);
   SLPlayItf playItf = player->playItf;
-  player->repeat = sound_map[alert].second;
-  if (player->repeat > 0) {
+
+  int loops = sound_map[alert].second;
+  player->repeat = loops > 0 ? loops - 1 : loops;
+  if (player->repeat != 0) {
     ReturnOnError((*playItf)->RegisterCallback(playItf, slplay_callback, player), "Failed to register callback");
     ReturnOnError((*playItf)->SetCallbackEventsMask(playItf, SL_PLAYEVENT_HEADATEND), "Failed to set callback event mask");
   }
