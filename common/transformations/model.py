c5a36fc8ec2dfefeb3f08de5ab0348009a050cdc
ZwX1616 <zwx1616@gmail.com>
2020-10-27 16:09:17 -0700

    cleanup

--- common/transformations/model.py
+++ common/transformations/model.py
@@ -66,15 +66,14 @@ bigmodel_intrinsics = np.array([
 
 
 # SBIG model (big model with the size of small model)
-
 SBIGMODEL_INPUT_SIZE = (512, 256)
 SBIGMODEL_YUV_SIZE = (SBIGMODEL_INPUT_SIZE[0], SBIGMODEL_INPUT_SIZE[1] * 3 // 2)
 
-sbigmodel_zoom = 2.
-sbigmodel_intrinsics = np.array(
-  [[ eon_focal_length / sbigmodel_zoom,    0. , 0.5 * SBIGMODEL_INPUT_SIZE[0]],
-   [   0. ,  eon_focal_length / sbigmodel_zoom,  0.5 * (256+MEDMODEL_CY)],
-   [   0. ,                            0. ,   1.]])
+sbigmodel_fl = 455.0
+sbigmodel_intrinsics = np.array([
+  [sbigmodel_fl,  0.0,  0.5 * SBIGMODEL_INPUT_SIZE[0]],
+  [0.0,  sbigmodel_fl,      0.5 * (256 + MEDMODEL_CY)],
+  [0.0,  0.0,                                     1.0]])
 
 model_frame_from_road_frame = np.dot(model_intrinsics,
   get_view_frame_from_road_frame(0, 0, 0, model_height))
