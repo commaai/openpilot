d92ec9642e49f4e4681006c71e7d90cfc8bd7d1d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-10-26 23:09:06 -0700

    grey panda deprecation (#2406)

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -252,6 +252,14 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
       Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 15.),
   },
 
+  EventName.startupGreyPanda: {
+    ET.PERMANENT: Alert(
+      "WARNING: Grey panda is deprecated",
+      "Upgrade to comma two or black panda",
+      AlertStatus.userPrompt, AlertSize.mid,
+      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 15.),
+  },
+
   EventName.invalidGiraffeToyota: {
     ET.PERMANENT: Alert(
       "Unsupported Giraffe Configuration",
