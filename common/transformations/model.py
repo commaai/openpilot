63e78992f39bf60de43ecadec57e6f401c07ca58
ZwX1616 <zwx1616@gmail.com>
2020-10-27 14:44:54 -0700

    make models api generic

--- common/transformations/model.py
+++ common/transformations/model.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from common.transformations.camera import (FULL_FRAME_SIZE, 
+from common.transformations.camera import (FULL_FRAME_SIZE,
                                            FOCAL,
                                            get_view_frame_from_road_frame,
                                            get_view_frame_from_calib_frame,
@@ -9,10 +9,10 @@ from common.transformations.camera import (FULL_FRAME_SIZE,
 # segnet
 SEGNET_SIZE = (512, 384)
 
-segnet_frame_from_camera_frame = np.array([
-  [float(SEGNET_SIZE[0]) / FULL_FRAME_SIZE[0],  0.0],
-  [0.0,  float(SEGNET_SIZE[1]) / FULL_FRAME_SIZE[1]]])
-
+def get_segnet_frame_from_camera_frame(segnet_size=SEGNET_SIZE, full_frame_size=FULL_FRAME_SIZE):
+  return np.array([[float(segnet_size[0]) / full_frame_size[0],  0.0],
+                   [0.0,  float(segnet_size[1]) / full_frame_size[1]]])
+segnet_frame_from_camera_frame = get_segnet_frame_from_camera_frame() # xx
 
 # model
 MODEL_INPUT_SIZE = (320, 160)
@@ -20,14 +20,14 @@ MODEL_YUV_SIZE = (MODEL_INPUT_SIZE[0], MODEL_INPUT_SIZE[1] * 3 // 2)
 MODEL_CX = MODEL_INPUT_SIZE[0] / 2.
 MODEL_CY = 21.
 
-model_zoom = 1.25
+model_fl = 728.0
 model_height = 1.22
 
 # canonical model transform
 model_intrinsics = np.array([
-  [FOCAL / model_zoom,  0.0,  MODEL_CX],
-  [0.0,  FOCAL / model_zoom,  MODEL_CY],
-  [0.0,  0.0,                      1.0]])
+  [model_fl,  0.0,  MODEL_CX],
+  [0.0,  model_fl,  MODEL_CY],
+  [0.0,  0.0,            1.0]])
 
 
 # MED model
@@ -35,12 +35,11 @@ MEDMODEL_INPUT_SIZE = (512, 256)
 MEDMODEL_YUV_SIZE = (MEDMODEL_INPUT_SIZE[0], MEDMODEL_INPUT_SIZE[1] * 3 // 2)
 MEDMODEL_CY = 47.6
 
-# TODO: these zooms are a bad idea, as they are defined in eon pixel space
-medmodel_zoom = 1.0 / 1.0 # raw pixel / new pixel
+medmodel_fl = 910.0
 medmodel_intrinsics = np.array([
-  [FOCAL / medmodel_zoom,  0.0,  0.5 * MEDMODEL_INPUT_SIZE[0]],
-  [0.0,  FOCAL / medmodel_zoom,                   MEDMODEL_CY],
-  [0.0,  0.0,                                             1.0]])
+  [medmodel_fl,  0.0,  0.5 * MEDMODEL_INPUT_SIZE[0]],
+  [0.0,  medmodel_fl,                   MEDMODEL_CY],
+  [0.0,  0.0,                                   1.0]])
 
 
 # CAL model
@@ -48,22 +47,22 @@ CALMODEL_INPUT_SIZE = (512, 256)
 CALMODEL_YUV_SIZE = (CALMODEL_INPUT_SIZE[0], CALMODEL_INPUT_SIZE[1] * 3 // 2)
 CALMODEL_CY = 47.6
 
-calmodel_zoom = 1.5 / 1.0
+calmodel_fl = 606.7
 calmodel_intrinsics = np.array([
-  [FOCAL / calmodel_zoom,  0.0,  0.5 * CALMODEL_INPUT_SIZE[0]],
-  [0.0,  FOCAL / calmodel_zoom,                   CALMODEL_CY],
-  [0.0,  0.0,                                             1.0]])
+  [calmodel_fl,  0.0,  0.5 * CALMODEL_INPUT_SIZE[0]],
+  [0.0,  calmodel_fl,                   CALMODEL_CY],
+  [0.0,  0.0,                                   1.0]])
 
 
 # BIG model
 BIGMODEL_INPUT_SIZE = (1024, 512)
 BIGMODEL_YUV_SIZE = (BIGMODEL_INPUT_SIZE[0], BIGMODEL_INPUT_SIZE[1] * 3 // 2)
 
-bigmodel_zoom = 1.0 / 1.0
+bigmodel_fl = 910.0
 bigmodel_intrinsics = np.array([
-  [FOCAL / bigmodel_zoom,  0.0,  0.5 * BIGMODEL_INPUT_SIZE[0]],
-  [0.0,  FOCAL / bigmodel_zoom,             256 + MEDMODEL_CY],
-  [0.0,  0.0,                                             1.0]])
+  [bigmodel_fl,  0.0,  0.5 * BIGMODEL_INPUT_SIZE[0]],
+  [0.0,  bigmodel_fl,             256 + MEDMODEL_CY],
+  [0.0,  0.0,                                   1.0]])
 
 
 model_frame_from_road_frame = np.dot(model_intrinsics,
@@ -106,9 +105,10 @@ def get_model_height_transform(camera_frame_from_road_frame, height):
 
 # camera_frame_from_model_frame aka 'warp matrix'
 # was: calibration.h/CalibrationTransform
-def get_camera_frame_from_model_frame(camera_frame_from_road_frame, height=model_height):
+def get_camera_frame_from_model_frame(camera_frame_from_road_frame, height=model_height, camera_fl=FOCAL):
   vp = vp_from_ke(camera_frame_from_road_frame)
 
+  model_zoom = camera_fl / model_fl
   model_camera_from_model_frame = np.array([
     [model_zoom,  0.0,  vp[0] - MODEL_CX * model_zoom],
     [0.0,  model_zoom,  vp[1] - MODEL_CY * model_zoom],
