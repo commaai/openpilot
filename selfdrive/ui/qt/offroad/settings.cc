9d84a835d85896f959382a2cca9e7d531bca08ea
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-10-31 17:07:04 -0700

    Qt onboarding (#2465)
    * terms
    
    * clean up
    
    * style

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -137,8 +137,9 @@ QWidget * device_panel() {
 
   QPushButton *clear_cal_btn = new QPushButton("Reset Calibration");
   device_layout->addWidget(clear_cal_btn);
-  QObject::connect(clear_cal_btn, &QPushButton::released,
-                   [=]() { Params().delete_db_value("CalibrationParams"); });
+  QObject::connect(clear_cal_btn, &QPushButton::released, [=]() {
+    Params().delete_db_value("CalibrationParams");
+  });
 
   std::map<std::string, const char *> power_btns = {
     {"Power Off", "sudo poweroff"},
@@ -153,7 +154,6 @@ QWidget * device_panel() {
                      [=]() {std::system(b.second);});
 #endif
   }
-
 
   QWidget *widget = new QWidget;
   widget->setLayout(device_layout);
