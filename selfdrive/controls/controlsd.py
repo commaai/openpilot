6e078827b3144de27d198891108da46658e9c834
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-11-04 11:12:15 -0800

    check for safety mode mismatch (#2443)
    old-commit-hash: 89d02dcf97d60585f9c627f11fdd75387d632b83

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -201,7 +201,8 @@ class Controls:
 
     if self.can_rcv_error or (not CS.canValid and self.sm.frame > 5 / DT_CTRL):
       self.events.add(EventName.canError)
-    if self.mismatch_counter >= 200:
+    if (self.sm['health'].safetyModel != self.CP.safetyModel and self.sm.frame > 2 / DT_CTRL) or \
+       self.mismatch_counter >= 200:
       self.events.add(EventName.controlsMismatch)
     if not self.sm.alive['plan'] and self.sm.alive['pathPlan']:
       # only plan not being received: radar not communicating
