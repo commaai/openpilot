ef668e9606559cadebdc92ae08b7b92c349e08f5
Shane Smiskol <shane@smiskol.com>
2020-11-07 20:04:03 -0600

    Gets us 6 seconds before making sound (up from 2s; 9s originally)

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -15,8 +15,8 @@ EventName = car.CarEvent.EventName
 # ******************************************************************************************
 
 _AWARENESS_TIME = 35.  # passive wheel touch total timeout
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 7.
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 5.
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
 _DISTRACTED_TIME = 11.
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
