9f61dd6254d3eb1b093d4cb6c3edee30a683dc92
Jon Ander Oribe <jonan.oribe@gmail.com>
2020-11-12 23:48:45 +0100

    Simple improvements for quality gate (#2517)
    * Improvements for quality gate
    
    * Update test_xattr.py
    
    * Update test_mpc_wobble.py
    
    * python
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 887d67eb8d8d2da8e07ff3a74517b620afa9542a

--- selfdrive/debug/mpc/test_mpc_wobble.py
+++ selfdrive/debug/mpc/test_mpc_wobble.py
@@ -32,7 +32,6 @@ p_l[3] += LANE_WIDTH / 2.0
 p_r = p[:]
 p_r[3] -= LANE_WIDTH / 2.0
 
-
 l_poly = libmpc_py.ffi.new("double[4]", p_l)
 r_poly = libmpc_py.ffi.new("double[4]", p_r)
 p_poly = libmpc_py.ffi.new("double[4]", p)
