6d2f2004352f1f8ae72e975b6d5ee288a3f4e906
grekiki <gregor1234567890@gmail.com>
2020-11-16 14:24:25 +0100

    refactor

--- selfdrive/ui/qt/offroad/wifi.hpp
+++ selfdrive/ui/qt/offroad/wifi.hpp
@@ -1,41 +1,11 @@
 #pragma once
+#include "wifiManager.hpp"
 #include <QWidget>
 #include <QtDBus>
 #include <QPushButton>
 #include <QButtonGroup>
 #include <QVBoxLayout>
 #include <QStackedLayout>
-
-struct Network {
-  QString path;
-  QByteArray ssid;
-  unsigned int strength;
-  bool connected;
-};
-
-
-class WifiManager{
-  private:
-    QVector<QByteArray> seen_ssids;
-
-    QString get_adapter();
-    QList<Network> get_networks(QString adapter);
-    void connect_to_open(QByteArray ssid);
-    void connect_to_WPA(QByteArray ssid, QString password);
-    void request_scan(QString adapter);
-    QString get_active_ap(QString adapter);
-    QByteArray get_ap_ssid(QString network_path);
-    QByteArray get_property(QString network_path, QString property);
-    unsigned int get_ap_strength(QString network_path);
-
-  public:
-    QVector<Network> seen_networks;
-
-    explicit WifiManager();
-    void refreshNetworks();
-    
-};
-
 
 class WifiUI : public QWidget {
   Q_OBJECT
