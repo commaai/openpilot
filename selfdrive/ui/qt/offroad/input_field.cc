45b9d88a52971f8d953d28cd0fa84b6cdfc0ed91
grekiki <gregor1234567890@gmail.com>
2020-11-19 17:01:55 +0100

    add stuff

---
+++ selfdrive/ui/qt/offroad/input_field.cc
@@ -0,0 +1,53 @@
+#include <QEvent>
+#include <QVBoxLayout>
+#include <QLineEdit>
+
+#include "input_field.hpp"
+#include "keyboard.hpp"
+
+InputField::InputField(QWidget *parent){
+  l = new QVBoxLayout();
+
+  line = new QLineEdit("");
+  line->installEventFilter(this);
+  l->addWidget(line);
+
+  k = new Keyboard();
+  QObject::connect(k, SIGNAL(emitButton(QString)), this, SLOT(getText(QString)));
+
+  setLayout(l);
+}
+
+bool InputField::eventFilter(QObject* object, QEvent* event){
+  if(object == line && event->type() == QEvent::MouseButtonPress) {
+    k->setWindowFlags(Qt::FramelessWindowHint);
+    k->resize(2560, k->height());
+    k->move(1920, 1300); //TODO: move to the correct position for TICI
+
+    // k->setWindowState(Qt::WindowFullScreen);
+    // k->setAttribute(Qt::WA_NoSystemBackground);
+    // k->setAttribute(Qt::WA_TranslucentBackground);
+    k->show();
+  }
+  return false;
+}
+
+void InputField::getText(QString s){
+  if(!QString::compare(s,"⌫")){
+    line->setText(line->text().left(line->text().length()-1));
+  }
+
+  if(!QString::compare(s,"⏎")){
+    k->hide();
+    emitText(line->text());
+  }
+
+  QVector<QString> control_buttons {"⇧", "↑", "ABC", "⏎", "#+=", "⌫", "123"};
+  for(QString c :control_buttons){
+    if(!QString::compare(s, c)){
+      return;
+    }
+  }
+  line->insert(s.left(1));
+}
+
---
+++ selfdrive/ui/qt/offroad/input_field.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QStackedLayout>
+
+#include "keyboard.hpp"
+
+class InputField : public QWidget {
+  Q_OBJECT
+
+public:
+  explicit InputField(QWidget* parent = 0);
+  
+private:
+  Keyboard *k;
+  QLineEdit *line;
+  QVBoxLayout *l;
+  bool eventFilter(QObject* object, QEvent* event);
+
+public slots:
+  void getText(QString s);
+
+signals:
+  void emitText(QString s);
+};
