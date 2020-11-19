b9b6ed2d5258b9ac442005b8caa7904c5873ca88
Comma Device <device@comma.ai>
2020-11-19 04:03:16 +0000

    underflew

--- selfdrive/camerad/cameras/camera_qcom2.cc
+++ selfdrive/camerad/cameras/camera_qcom2.cc
@@ -1007,6 +1007,7 @@ static void set_camera_exposure(CameraState *s, float grey_frac) {
   // TODO: get stats from sensor?
   float target_grey = 0.3 - (s->analog_gain / 105.0);
   float exposure_factor = 1 + 30 * pow((target_grey - grey_frac), 3);
+  exposure_factor = std::max(exposure_factor, 0.55f);
 
   if (s->camera_num != 1) {
     s->ef_filtered = (1 - EF_LOWPASS_K) * s->ef_filtered + EF_LOWPASS_K * exposure_factor;
