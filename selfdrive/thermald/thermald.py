9d09faeff42f5832aff16f9669b456e3fc05cfc7
Shane Smiskol <shane@smiskol.com>
2020-11-20 10:53:17 -0600

    health is not None was checked twice

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -365,8 +365,7 @@ def thermald_thread():
     set_offroad_alert_if_changed("Offroad_TemperatureTooHigh", (not startup_conditions["device_temp_good"]))
     should_start = all(startup_conditions.values())
 
-    startup_conditions["hardware_supported"] = health is not None and health.health.hwType not in [log.HealthData.HwType.whitePanda,
-                                                                                                   log.HealthData.HwType.greyPanda]
+    startup_conditions["hardware_supported"] = health.health.hwType not in [log.HealthData.HwType.whitePanda, log.HealthData.HwType.greyPanda]
     set_offroad_alert_if_changed("Offroad_HardwareUnsupported", health is not None and not startup_conditions["hardware_supported"])
 
     if should_start:
