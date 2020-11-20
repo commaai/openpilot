599ee894cd8e19f0b1a4b16553441225ea887e92
George Hotz <george@comma.ai>
2020-11-20 09:09:43 -0800

    other includes to build repro on pc
    old-commit-hash: 6fc7c0c0052ae10614c70768dcfe67a5be89d3cf

--- selfdrive/modeld/test/dmon_lag/repro.cc
+++ selfdrive/modeld/test/dmon_lag/repro.cc
@@ -2,8 +2,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 #include <time.h>
 #include <sched.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static inline double millis_since_boot() {
   struct timespec t;
