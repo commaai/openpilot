67249ef8aa3da9b59ea1ebaf11bdaad62960c43d
Willem Melching <willem.melching@gmail.com>
2020-11-20 11:47:39 +0100

    Stricter check on date for RTC time sync (#2582)
    * More strict check on time
    
    * Also update thermald
    old-commit-hash: ff65dc1401681ca71f6449c5e059eb7a2fb2256c

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -309,7 +309,7 @@ def thermald_thread():
     now = datetime.datetime.utcnow()
 
     # show invalid date/time alert
-    startup_conditions["time_valid"] = now.year >= 2019
+    startup_conditions["time_valid"] = (now.year > 2020) or (now.year == 2020 and now.month >= 10)
     set_offroad_alert_if_changed("Offroad_InvalidTime", (not startup_conditions["time_valid"]))
 
     # Show update prompt
