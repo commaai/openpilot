342d4c9d7c858c937d59bf21a7016307a8b49d30
Comma Device <device@comma.ai>
2020-11-21 00:37:24 +0000

    CPU waste is better

--- scripts/waste.c
+++ scripts/waste.c
@@ -36,8 +36,23 @@ void waste(int pid) {
   double sec = seconds_since_boot();
   while (1) {
     for (int i = 0; i < 8; i++) {
-      for (int j = 0; j < 0x1000000; j+=2) {
-        out = vmlaq_f32(out, tmp[j], tmp[j+1]);
+      for (int j = 0; j < 0x1000000; j+=0x20) {
+        out = vmlaq_f32(out, tmp[j+0], tmp[j+1]);
+        out = vmlaq_f32(out, tmp[j+2], tmp[j+3]);
+        out = vmlaq_f32(out, tmp[j+4], tmp[j+5]);
+        out = vmlaq_f32(out, tmp[j+6], tmp[j+7]);
+        out = vmlaq_f32(out, tmp[j+8], tmp[j+9]);
+        out = vmlaq_f32(out, tmp[j+10], tmp[j+11]);
+        out = vmlaq_f32(out, tmp[j+12], tmp[j+13]);
+        out = vmlaq_f32(out, tmp[j+14], tmp[j+15]);
+        out = vmlaq_f32(out, tmp[j+16], tmp[j+17]);
+        out = vmlaq_f32(out, tmp[j+18], tmp[j+19]);
+        out = vmlaq_f32(out, tmp[j+20], tmp[j+21]);
+        out = vmlaq_f32(out, tmp[j+22], tmp[j+23]);
+        out = vmlaq_f32(out, tmp[j+24], tmp[j+25]);
+        out = vmlaq_f32(out, tmp[j+26], tmp[j+27]);
+        out = vmlaq_f32(out, tmp[j+28], tmp[j+29]);
+        out = vmlaq_f32(out, tmp[j+30], tmp[j+31]);
       }
     }
     double nsec = seconds_since_boot();
