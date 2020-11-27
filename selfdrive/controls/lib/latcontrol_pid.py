2106463bf7da892858ddcd17480a4e9fb1aa336d
Shane Smiskol <shane@smiskol.com>
2020-11-27 09:11:48 -0600

    Add new speed polynomial for feedforward. Fitted poly using current TSS1 Corolla kf

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -34,7 +34,8 @@ class LatControlPID():
       if CP.steerControlType == car.CarParams.SteerControlType.torque:
         # TODO: feedforward something based on path_plan.rateSteers
         steer_feedforward -= path_plan.angleOffset   # subtract the offset, since it does not contribute to resistive torque
-        steer_feedforward *= CS.vEgo**2  # proportional to realigning tire momentum (~ lateral accel)
+        _c1, _c2, _c3 = [0.35189607550172824, 7.506201251644202, 69.226826411091]
+        steer_feedforward *= _c1 * CS.vEgo ** 2 + _c2 * CS.vEgo + _c3
       deadzone = 0.0
 
       check_saturation = (CS.vEgo > 10) and not CS.steeringRateLimited and not CS.steeringPressed
