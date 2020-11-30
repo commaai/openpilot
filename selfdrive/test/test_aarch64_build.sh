0f30ef13f0b61980b7d80d20bfd199953577aed8
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-11-30 10:11:19 -0800

    aarch64 build in CI

---
+++ selfdrive/test/test_aarch64_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker pull docker.io/commaai/agnos:latest
+docker run -it -v ~/openpilot:/tmp/openpilot docker.io/commaai/agnos /bin/bash -c \
+    "su -l -c \"source ~/.bash_profile && cd /tmp/openpilot && scons -j8 --dry-run\" -m \"comma\""
