bdb1b766d03d6f2ffa87a15971f6718f4cdc5e58
George Hotz <72895+geohot@users.noreply.github.com>
2020-11-30 02:21:06 -0800

    Make the DSP work everywhere (#2621)
    * Use DSP again
    
    * 20 Hz
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

--- common/realtime.py
+++ common/realtime.py
@@ -4,15 +4,20 @@ import os
 import time
 import multiprocessing
 
-from common.hardware import PC
+from common.hardware import PC, TICI
 from common.clock import sec_since_boot  # pylint: disable=no-name-in-module, import-error
 
 
 # time step for each process
 DT_CTRL = 0.01  # controlsd
 DT_MDL = 0.05  # model
-DT_DMON = 0.1  # driver monitoring
 DT_TRML = 0.5  # thermald and manager
+
+# driver monitoring
+if TICI:
+  DT_DMON = 0.05
+else:
+  DT_DMON = 0.1
 
 
 class Priority:
