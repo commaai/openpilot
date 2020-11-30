988a688ea288c9615d3a9144fff669e31ef9f047
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-11-30 10:36:33 -0800

    fix permissions

--- selfdrive/test/test_aarch64_build.sh
+++ selfdrive/test/test_aarch64_build.sh
@@ -2,5 +2,10 @@
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker pull docker.io/commaai/agnos:latest
-docker run -v $PWD/../..:/tmp/openpilot docker.io/commaai/agnos /bin/bash -c \
-    "su -l -c \"source ~/.bash_profile && cd /tmp/openpilot && scons -j8\" -m \"comma\""
+docker run -v $PWD/../..:/tmp/openpilot docker.io/commaai/agnos /bin/bash -c  "\
+         chown -R comma /tmp/openpilot && \
+         su -l -c \" \
+         source ~/.bash_profile && \
+         cd /tmp/openpilot && \
+         scons -j8 -c \
+         \" -m \"comma\""
