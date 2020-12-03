7e7d3d9d3d04dc55488c3446f7281fe2e0a7e978
Seth Brown <brown1428@gmail.com>
2020-12-03 07:10:50 -0500

    Revert "Update controlsd.py"
    This reverts commit 108e3b4901e60aac706f28704de3f145ecff749f.

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -503,11 +503,6 @@ class Controls:
     controlsState.forceDecel = bool(force_decel)
     controlsState.canErrorCounter = self.can_error_counter
 
-    # Nelson's hack for personal branch to preserve engine braking in Corolla
-    # Setting speed may be iffy still
-    if self.v_cruise_kph != 255:
-      controlsState.vCruise = controlsState.vCruise * 0.974
-
     if self.CP.lateralTuning.which() == 'pid':
       controlsState.lateralControlState.pidState = lac_log
     elif self.CP.lateralTuning.which() == 'lqr':
