60d05b3462add1e5c210a2b8e212af7b3ac440d5
briskspirit <bs@privacy.im>
2020-12-08 09:13:34 -0500

    Fix TSS2 creep issue, no need to send standstill request

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -84,7 +84,7 @@ class CarController():
       pcm_cancel_cmd = 1
 
     # on entering standstill, send standstill request
-    if CS.out.standstill and not self.last_standstill:
+    if CS.out.standstill and not self.last_standstill and CS.CP.carFingerprint not in TSS2_CAR:
       self.standstill_req = True
     if CS.pcm_acc_status != 8:
       # pcm entered standstill or it's disabled
