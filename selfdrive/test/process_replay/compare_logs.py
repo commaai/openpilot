8992639754b3643896b289e37673f1fff5172309
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-12-08 22:01:34 -0800

    add modelV2 to model replay (#2725)
    * add modelV2 to model replay
    
    * update refs
    
    * ignore fields for modelv2
    
    * execution time
    
    * fix compare logs
    
    Co-authored-by: Comma Device <device@comma.ai>

--- selfdrive/test/process_replay/compare_logs.py
+++ selfdrive/test/process_replay/compare_logs.py
@@ -31,7 +31,7 @@ def remove_ignored_fields(msg, ignore):
   for key in ignore:
     attr = msg
     keys = key.split(".")
-    if msg.which() not in key and len(keys) > 1:
+    if msg.which() != keys[0] and len(keys) > 1:
       continue
 
     for k in keys[:-1]:
