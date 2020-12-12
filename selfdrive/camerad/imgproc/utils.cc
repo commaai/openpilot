2d15acfc6819f6b3f483786142ddc2c89f54c3b3
deanlee <deanlee3@gmail.com>
2020-12-12 20:06:46 +0000

    space

--- selfdrive/camerad/imgproc/utils.cc
+++ selfdrive/camerad/imgproc/utils.cc
@@ -9,7 +9,7 @@ void get_lapmap_one(const int16_t *lap, uint16_t *res, int x_pitch, int y_pitch)
   int16_t max = 0;
   int sum = 0;
   for (int i = 0; i < size; ++i) {
-    const int16_t v = lap[i % x_pitch + (i/x_pitch)*x_pitch];
+    const int16_t v = lap[i % x_pitch + (i / x_pitch) * x_pitch];
     sum += v;
     if (v > max) max = v;
   }
@@ -19,7 +19,7 @@ void get_lapmap_one(const int16_t *lap, uint16_t *res, int x_pitch, int y_pitch)
   // var of roi
   int var = 0;
   for (int i = 0; i < size; ++i) {
-    var += std::pow(lap[i % x_pitch + (i/x_pitch)*x_pitch] - mean, 2);
+    var += std::pow(lap[i % x_pitch + (i / x_pitch) * x_pitch] - mean, 2);
   }
 
   const float fvar = (float)var / size;
@@ -30,7 +30,7 @@ bool is_blur(const uint16_t *lapmap, const size_t size) {
   float bad_sum = 0;
   for (int i = 0; i < size; i++) {
     if (lapmap[i] < LM_THRESH) {
-      bad_sum += 1/(float)size;
+      bad_sum += 1 / (float)size;
     }
   }
   return (bad_sum > LM_PREC_THRESH);
