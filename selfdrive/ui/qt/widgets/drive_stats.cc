e9b034e808e1836c467d7a09d193dd0668f168c7
Willem Melching <willem.melching@gmail.com>
2020-12-15 16:09:45 +0100

    create JWT in C++

--- selfdrive/ui/qt/widgets/drive_stats.cc
+++ selfdrive/ui/qt/widgets/drive_stats.cc
@@ -1,3 +1,6 @@
+#include <iostream>
+
+#include <QFile>
 #include <QDebug>
 #include <QHBoxLayout>
 #include <QLabel>
@@ -8,30 +11,82 @@
 #include <QJsonObject>
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
+#include <QSslKey>
+#include <QCryptographicHash>
+
+// TODO: Which ones do we need?
+#include <openssl/rand.h>
+#include <openssl/rsa.h>
+#include <openssl/engine.h>
+#include <openssl/sha.h>
+
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/pem.h>
+#include <openssl/buffer.h>
+#include <openssl/err.h>
+#include <openssl/x509.h>
+
 
 #include "drive_stats.hpp"
 #include "common/params.h"
+#include "common/utilpp.h"
 double MILE_TO_KM = 1.60934;
 
-std::string exec(const char* cmd) {
-    char buffer[128];
-    std::string result = "";
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    try {
-        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
-            result += buffer;
-        }
-    } catch (...) {
-        pclose(pipe);
-        throw;
-    }
-    pclose(pipe);
-    return result;
+
+// TODO: create C++ API library
+#if defined(QCOM) || defined(QCOM2)
+const std::string private_key_path = "/persist/comma/id_rsa";
+#else
+const std::string private_key_path = util::getenv_default("HOME", "/.comma/persist/comma/id_rsa", "/persist/comma/id_rsa");
+#endif
+
+QByteArray rsa_sign(QByteArray data){
+  auto file = QFile(private_key_path.c_str());
+  assert(file.open(QIODevice::ReadOnly));
+
+  auto key = file.readAll();
+
+  BIO *mem = BIO_new_mem_buf(key.data(), key.size());
+  assert(mem);
+
+  RSA *rsa_private = PEM_read_bio_RSAPrivateKey(mem, NULL, NULL, NULL);
+  assert(rsa_private);
+
+  auto sig = QByteArray();
+  sig.resize(RSA_size(rsa_private));
+
+  unsigned int sig_len;
+  int ret = RSA_sign(NID_sha256, (unsigned char*)data.data(), data.size(), (unsigned char*)sig.data(), &sig_len, rsa_private);
+
+  assert(ret == 1);
+  assert(sig_len == sig.size());
+
+  free(mem);
+  free(rsa_private);
+
+  return sig;
 }
-QString bold(QString s){
+
+QString create_jwt(void){
+  auto header = QString("{\"typ\":\"JWT\",\"alg\":\"RS256\"}");
+  auto payload = QString("{\"identity\":\"8acc054ecd9c97b2\",\"nbf\":1608042239,\"iat\":1608042239,\"exp\":1608045839}");
+
+  QString jwt = header.toUtf8().toBase64() + '.' + payload.toUtf8().toBase64();
+
+  auto hash = QCryptographicHash::hash(jwt.toUtf8(), QCryptographicHash::Sha256);
+  auto sig = rsa_sign(hash);
+
+  jwt += '.' + sig.toBase64();
+
+  return jwt;
+}
+
+QString bold(QString s) {
   return "<b>" + s + "</b>";
 }
+
 QWidget *widget(QLayout *l){
   QWidget *q = new QWidget();
   q->setLayout(l);
@@ -40,7 +95,7 @@ QWidget *widget(QLayout *l){
 void DriveStats::replyFinished(QNetworkReply *l){
   QString answer = l->readAll();
   answer.chop(1);
-  // qDebug()<<answer;
+  qDebug()<<answer;
   QJsonDocument doc = QJsonDocument::fromJson(answer.toUtf8());
   if (doc.isNull()) {
     qDebug() << "Parse failed";
@@ -52,7 +107,7 @@ void DriveStats::replyFinished(QNetworkReply *l){
   auto all = json["all"].toObject();
   auto week = json["week"].toObject();
 
-  
+
   int all_distance = all["distance"].toDouble()*(metric ? MILE_TO_KM : 1);
   int all_minutes = all["minutes"].toDouble();
   int all_routes = all["routes"].toDouble();
@@ -122,7 +177,7 @@ void DriveStats::replyFinished(QNetworkReply *l){
       font-weight: 200;
     }
   )");
-  
+
 }
 DriveStats::DriveStats(QWidget *parent) : QWidget(parent){
   f = new QFrame;
@@ -130,19 +185,17 @@ DriveStats::DriveStats(QWidget *parent) : QWidget(parent){
   QVBoxLayout *v = new QVBoxLayout;
   v->addWidget(f);
   setLayout(v);
-  
 
-  std::string result = exec("python -c \"from common.api import Api; from common.params import Params; print(Api(Params().get('DongleId', encoding='utf8')).get_token());\" 2>/dev/null");
+  QString token = create_jwt();
+  qDebug() << create_jwt();
 
-  QString auth_token = QString::fromStdString(result);
-  // qDebug()<<auth_token;
   QNetworkAccessManager *manager = new QNetworkAccessManager(this);
   connect(manager, &QNetworkAccessManager::finished, this, &DriveStats::replyFinished);
 
   QNetworkRequest request;
   QString dongleId = QString::fromStdString(Params().get("DongleId"));
   request.setUrl(QUrl("https://api.commadotai.com/v1.1/devices/" + dongleId + "/stats"));
-  request.setRawHeader("Authorization", ("JWT "+auth_token).toUtf8());
+  request.setRawHeader("Authorization", ("JWT "+token).toUtf8());
 
   manager->get(request);
 }
