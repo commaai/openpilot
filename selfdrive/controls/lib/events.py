93271669d56dccb5941b807fe5d25c5338b2b317
Shane Smiskol <shane@smiskol.com>
2020-12-16 09:06:44 -0600

    don't wait for 4 seconds before it starts steering again after steering fault

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -519,7 +519,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
       "TAKE CONTROL",
       "Steering Temporarily Unavailable",
       AlertStatus.userPrompt, AlertSize.mid,
-      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.chimeWarning1, .4, 2., 3.),
+      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.chimeWarning1, .4, 2., 1.),
     ET.NO_ENTRY: NoEntryAlert("Steering Temporarily Unavailable",
                               duration_hud_alert=0.),
   },
