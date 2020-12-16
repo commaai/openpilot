3f2ade1312590ccf676a7cfd24bfc00023b54f84
andrewcopenpilot <50561935+andrewcopenpilot@users.noreply.github.com>
2020-12-16 09:29:52 -0800

    Fix to allow brakes to release on resume press (#2709)
    old-commit-hash: 4fcf8678f64be681abde5f1bb146f873e1b005da

--- selfdrive/car/gm/gmcan.py
+++ selfdrive/car/gm/gmcan.py
@@ -35,14 +35,12 @@ def create_gas_regen_command(packer, bus, throttle, idx, acc_engaged, at_full_st
   return packer.make_can_msg("ASCMGasRegenCmd", bus, values)
 
 def create_friction_brake_command(packer, bus, apply_brake, idx, near_stop, at_full_stop):
-
-  if apply_brake == 0:
-    mode = 0x1
-  else:
+  mode = 0x1
+  if apply_brake > 0:
     mode = 0xa
+    if at_full_stop:
+      mode = 0xd
 
-  if at_full_stop:
-    mode = 0xd
     # TODO: this is to have GM bringing the car to complete stop,
     # but currently it conflicts with OP controls, so turned off.
     #elif near_stop:
