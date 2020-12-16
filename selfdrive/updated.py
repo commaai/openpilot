425fc628cc4b05d51d38bee5793cbc690feed34d
Willem Melching <willem.melching@gmail.com>
2020-12-16 17:01:13 +0100

    Sync before writing hash

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -327,6 +327,7 @@ def handle_agnos_update(wait_helper):
     if downloader.sha256.hexdigest().lower() != partition['hash'].lower():
       raise Exception("Uncompressed hash mismatch")
 
+    os.sync()
     out.write(str(partition['hash'].lower()))
 
   # TODO: this should be done at the same time as the openpilot swap
