9793f19f1442dcf05fa67a39f757a57db34b2dcd
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2020-12-25 21:12:41 -0500

    deleting opendbc, starting clean slate

--- opendbc/can/can_define.py
+++
@@ -1,2 +0,0 @@
-from opendbc.can.parser_pyx import CANDefine  # pylint: disable=no-name-in-module, import-error
-assert CANDefine
--- opendbc/can/parser.py
+++
@@ -1,2 +0,0 @@
-from opendbc.can.parser_pyx import CANParser, CANDefine  # pylint: disable=no-name-in-module, import-error
-assert CANParser, CANDefine
