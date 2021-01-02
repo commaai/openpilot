9420ecdfb481cb186d6d138ffc95bab44be12752
mayfield <3902405+mayfieldiv@users.noreply.github.com>
2021-01-01 23:02:45 -0500

    TEMP: disable more

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -7,36 +7,39 @@ if [[ $(command -v brew) == "" ]]; then
 fi
 
 brew install capnp \
-             coreutils \
-             eigen \
-             ffmpeg \
-             glfw \
-             libarchive \
-             libusb \
-             libtool \
-             llvm \
-	     openssl \
-             pyenv \
-             qt5 \
-             zeromq
+            #  coreutils \
+            #  eigen \
+            #  ffmpeg \
+            #  glfw \
+            #  libarchive \
+            #  libusb \
+            #  libtool \
+            #  llvm \
+            #  openssl \
+            #  pyenv \
+            #  qt5 \
+            #  zeromq
 
-if [[ $SHELL == "/bin/zsh" ]]; then
-  RC_FILE="$HOME/.zshrc"
-elif [[ $SHELL == "/bin/bash" ]]; then
-  RC_FILE="$HOME/.bash_profile"
-fi
+find /usr/local/Homebrew -type d
+find ~/Library/Caches/Homebrew -type d
 
-if [ -z "$OPENPILOT_ENV" ] && [ -n "$RC_FILE" ] && [ -z "$CI" ]; then
-  OP_DIR=$(git rev-parse --show-toplevel)
-  echo "source $OP_DIR/tools/openpilot_env.sh" >> $RC_FILE
-  source $RC_FILE
-  echo "Added openpilot_env to RC file: $RC_FILE"
-fi
+# if [[ $SHELL == "/bin/zsh" ]]; then
+#   RC_FILE="$HOME/.zshrc"
+# elif [[ $SHELL == "/bin/bash" ]]; then
+#   RC_FILE="$HOME/.bash_profile"
+# fi
 
-pyenv install -s 3.8.2
-pyenv global 3.8.2
-pyenv rehash
-eval "$(pyenv init -)"
+# if [ -z "$OPENPILOT_ENV" ] && [ -n "$RC_FILE" ] && [ -z "$CI" ]; then
+#   OP_DIR=$(git rev-parse --show-toplevel)
+#   echo "source $OP_DIR/tools/openpilot_env.sh" >> $RC_FILE
+#   source $RC_FILE
+#   echo "Added openpilot_env to RC file: $RC_FILE"
+# fi
 
-pip install pipenv==2020.8.13
-pipenv install --system --deploy
+# pyenv install -s 3.8.2
+# pyenv global 3.8.2
+# pyenv rehash
+# eval "$(pyenv init -)"
+
+# pip install pipenv==2020.8.13
+# pipenv install --system --deploy
