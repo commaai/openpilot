a0c5ffdef47461724973cb157871b418279a902f
mayfield <3902405+mayfieldiv@users.noreply.github.com>
2021-01-02 17:58:45 -0500

    revert mac_setup

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -7,48 +7,46 @@ if [[ $(command -v brew) == "" ]]; then
 fi
 
 if [[ $(command -v ffmpeg) != "" ]]; then
-  echo "Cache worked!"
   ffmpeg -version
+  echo "Cache worked!"
 fi
 
-
-brew install ffmpeg \
- capnp \
-            #  coreutils \
-            #  eigen \
-            #  ffmpeg \
-            #  glfw \
-            #  libarchive \
-            #  libusb \
-            #  libtool \
-            #  llvm \
-            #  openssl \
-            #  pyenv \
-            #  qt5 \
-            #  zeromq
+brew install capnp \
+             coreutils \
+             eigen \
+             ffmpeg \
+             glfw \
+             libarchive \
+             libusb \
+             libtool \
+             llvm \
+             openssl \
+             pyenv \
+             qt5 \
+             zeromq
 
 if [[ $(command -v ffmpeg) != "" ]]; then
   echo "ffmpeg exists"
   ffmpeg -version
 fi
 
-# if [[ $SHELL == "/bin/zsh" ]]; then
-#   RC_FILE="$HOME/.zshrc"
-# elif [[ $SHELL == "/bin/bash" ]]; then
-#   RC_FILE="$HOME/.bash_profile"
-# fi
+if [[ $SHELL == "/bin/zsh" ]]; then
+  RC_FILE="$HOME/.zshrc"
+elif [[ $SHELL == "/bin/bash" ]]; then
+  RC_FILE="$HOME/.bash_profile"
+fi
 
-# if [ -z "$OPENPILOT_ENV" ] && [ -n "$RC_FILE" ] && [ -z "$CI" ]; then
-#   OP_DIR=$(git rev-parse --show-toplevel)
-#   echo "source $OP_DIR/tools/openpilot_env.sh" >> $RC_FILE
-#   source $RC_FILE
-#   echo "Added openpilot_env to RC file: $RC_FILE"
-# fi
+if [ -z "$OPENPILOT_ENV" ] && [ -n "$RC_FILE" ] && [ -z "$CI" ]; then
+  OP_DIR=$(git rev-parse --show-toplevel)
+  echo "source $OP_DIR/tools/openpilot_env.sh" >> $RC_FILE
+  source $RC_FILE
+  echo "Added openpilot_env to RC file: $RC_FILE"
+fi
 
-# pyenv install -s 3.8.2
-# pyenv global 3.8.2
-# pyenv rehash
-# eval "$(pyenv init -)"
+pyenv install -s 3.8.2
+pyenv global 3.8.2
+pyenv rehash
+eval "$(pyenv init -)"
 
-# pip install pipenv==2020.8.13
-# pipenv install --system --deploy
+pip install pipenv==2020.8.13
+pipenv install --system --deploy
