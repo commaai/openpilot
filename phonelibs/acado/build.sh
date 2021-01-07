e0f2111061927ad7fa865c9027e525279da9ccad
Willem Melching <willem.melching@gmail.com>
2021-01-07 15:18:23 +0100

    Rebuild acado and add scons command to regenerate mpc (#19685)
    * rebuild acado and add scons command to regenerate mpc
    
    * update scons help
    old-commit-hash: 7c43a60bda6c129c0c5a34cdb7fe74d8243729c5

---
+++ phonelibs/acado/build.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c22ce7271de55f940175f761347dfd3a791ba72f11eb44be99af2943bb2b4ae
+size 558
--- phonelibs/acado/include/acado/code_generation/templates/templates.hpp
+++ phonelibs/acado/include/acado/code_generation/templates/templates.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97d5cb9224ef7af2ee39a97800aacc3be00eaa74b6e310ab0ee9cd44d3ed65ba
-size 2859
+oid sha256:5218981faa25368edffdb1c8b352cf49665ab6f9db08f34f951573db01bc1002
+size 2791
