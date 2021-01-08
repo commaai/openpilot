65ad3b37e286505c0307495e8b02cedccbb93cd4
Comma Device <device@comma.ai>
2021-01-08 19:53:12 +0000

    start thneed load/save

---
+++ selfdrive/modeld/runners/thneedmodel.cc
@@ -0,0 +1,28 @@
+ThneedModel::ThneedModel(const char *path, float *loutput, size_t loutput_size, int runtime) {
+  thneed = Thneed(true);
+  thneed->load(path);
+
+  output = loutput;
+  output_size = loutput_size;
+
+  assert(runtime==USE_GPU_RUNTIME);
+}
+
+void ThneedModel::addRecurrent(float *state, int state_size) {
+  recurrent = state;
+  recurrent_size = state_size;
+}
+
+void ThneedModel::addTrafficConvention(float *state, int state_size) {
+  trafficConvention = state;
+}
+
+void ThneedModel::addDesire(float *state, int state_size) {
+  desire = state;
+}
+
+void ThneedModel::execute(float *net_input_buf, int buf_size) {
+  float *inputs[4] = {recurrent, trafficConvention, desire, net_input_buf};
+  thneed->execute(inputs, output);
+}
+
---
+++ selfdrive/modeld/runners/thneedmodel.h
@@ -0,0 +1,33 @@
+#ifndef THNEEDMODEL_H
+#define THNEEDMODEL_H
+
+#include "runmodel.h"
+
+#define USE_CPU_RUNTIME 0
+#define USE_GPU_RUNTIME 1
+#define USE_DSP_RUNTIME 2
+
+#include "thneed/thneed.h"
+
+class ThneedModel : public RunModel {
+public:
+  ThneedModel(const char *path, float *loutput, size_t loutput_size, int runtime);
+  void addRecurrent(float *state, int state_size);
+  void addTrafficConvention(float *state, int state_size);
+  void addDesire(float *state, int state_size);
+  void execute(float *net_input_buf, int buf_size);
+private:
+  Thneed *thneed = NULL;
+
+  float *output;
+  size_t output_size;
+
+  // recurrent and desire
+  float *recurrent;
+  size_t recurrent_size;
+  float *trafficConvention;
+  float *desire;
+};
+
+#endif
+
