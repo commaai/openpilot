4bace1b3351191f9ced4db0ead62fdd50890a71b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-09 23:28:52 -0500

    Update

--- update_requirements.sh
+++ update_requirements.sh
@@ -1,30 +1,57 @@
 #!/bin/bash -e
 
-cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null
+# Require script to be run as root
+function super-user-check() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "You need to run this script as super user."
+        exit
+    fi
+}
 
-if ! command -v pyenv &> /dev/null; then
-  echo "please install pyenv ..."
-  echo "https://github.com/pyenv/pyenv-installer"
-  echo "example:"
-  echo "sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
-  echo "curl https://pyenv.run | bash"
-  echo "echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"' >> ~/.bashrc"
-  echo "echo 'export PATH=\"\$PYENV_ROOT/bin:\$PYENV_ROOT/shims:\$PATH\"' >> ~/.bashrc"
-  echo "exec \"\$SHELL\""
-  exit 1
-fi
+# Check for root
+super-user-check
+
+# Detect Operating System
+function dist-check() {
+    if [ -e /etc/os-release ]; then
+        # shellcheck disable=SC1091
+        source /etc/os-release
+        DISTRO=$ID
+        DISTRO_VERSION=$VERSION_ID
+    fi
+}
+
+# Check Operating System
+dist-check
+
+# Pre-Checks
+function installing-system-requirements() {
+    if { ! [ -x "$(command -v pyenv)" ] || ! [ -x "$(command -v make)" ] || ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v wget)" ]; }; then
+        cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null
+        if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ]; }; then
+            apt-get update && apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+        fi
+        curl https://pyenv.run | bash
+        echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"' >>~/.bashrc
+        echo 'export PATH=\"\$PYENV_ROOT/bin:\$PYENV_ROOT/shims:\$PATH\"' >>~/.bashrc
+        exec \"\$SHELL\"
+    fi
+}
+
+# Run the function and check for requirements
+installing-system-requirements
 
 export MAKEFLAGS="-j$(nproc)"
 
 PYENV_PYTHON_VERSION=$(cat .python-version)
-if ! pyenv prefix ${PYENV_PYTHON_VERSION} &> /dev/null; then
-  echo "pyenv ${PYENV_PYTHON_VERSION} install ..."
-  CONFIGURE_OPTS=--enable-shared pyenv install -f ${PYENV_PYTHON_VERSION}
+if ! pyenv prefix ${PYENV_PYTHON_VERSION} &>/dev/null; then
+    echo "pyenv ${PYENV_PYTHON_VERSION} install ..."
+    CONFIGURE_OPTS=--enable-shared pyenv install -f ${PYENV_PYTHON_VERSION}
 fi
 
-if ! command -v pipenv &> /dev/null; then
-  echo "pipenv install ..."
-  pip install pipenv
+if ! command -v pipenv &>/dev/null; then
+    echo "pipenv install ..."
+    pip install pipenv
 fi
 
 echo "update pip"
