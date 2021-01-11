ae07d6c36407bc138830f852bbc3a9eed75e6744
Willem Melching <willem.melching@gmail.com>
2021-01-11 15:51:23 +0100

    Update selfdrive/common/visionimg.cc

--- selfdrive/common/visionimg.cc
+++ selfdrive/common/visionimg.cc
@@ -22,6 +22,7 @@ EGLImageTexture::EGLImageTexture(const VisionBuf *buf) {
                              buf->stride/bpp, buf->len/buf->stride,
                              buf->width, buf->height);
 
+  // GraphicBuffer is ref counted by EGLClientBuffer(ANativeWindowBuffer), no need and not possible to release.	
   GraphicBuffer* gb = new GraphicBuffer(buf->width, buf->height, (PixelFormat)format,
                                         GraphicBuffer::USAGE_HW_TEXTURE, buf->stride/bpp, (private_handle_t*)private_handle, false);
 
