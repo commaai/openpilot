77d388623e815e7373d05749b81662f4f05cdb36
deanlee <deanlee3@gmail.com>
2021-01-11 14:30:42 +0800

    Refactor image texture stuff into class

--- selfdrive/common/visionimg.h
+++ selfdrive/common/visionimg.h
@@ -6,20 +6,15 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #undef Status
-#else
-typedef int EGLImageKHR;
-typedef void *EGLClientBuffer;
 #endif
 
-#define VISIONIMG_FORMAT_RGB24 1
-
-typedef struct VisionImg {
-  int fd;
-  int format;
-  int width, height, stride;
-  int bpp;
-  size_t size;
-} VisionImg;
-
-GLuint visionimg_to_gl(const VisionImg *img, EGLImageKHR *pkhr, void **pph);
-void visionimg_destroy_gl(EGLImageKHR khr, void *ph);
+class EGLImageTexture {
+ public:
+  EGLImageTexture(const VisionBuf *buf);
+  ~EGLImageTexture();
+  GLuint frame_tex = 0;
+#ifdef QCOM
+  void *private_handle = nullptr;
+  EGLImageKHR img_khr = 0;
+#endif
+};
