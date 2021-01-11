b3107e153cca4aa98b623b80fdd13b8446f76d1c
Dean Lee <deanlee3@gmail.com>
2021-01-11 22:52:58 +0800

    Refactor image texture stuff into class (#19719)
    * Refactor image texture stuff into class
    
    * cleanup
    
    * Update selfdrive/common/visionimg.cc
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

--- selfdrive/common/visionimg.h
+++ selfdrive/common/visionimg.h
@@ -1,25 +1,27 @@
 #pragma once
+
 #include "visionbuf.h"
-#include "common/glutil.h"
+
+#ifdef __APPLE__
+  #include <OpenGL/gl3.h>
+#else
+  #include <GLES3/gl3.h>
+#endif
 
 #ifdef QCOM
 #include <EGL/egl.h>
+#define EGL_EGLEXT_PROTOTYPES
 #include <EGL/eglext.h>
 #undef Status
-#else
-typedef int EGLImageKHR;
-typedef void *EGLClientBuffer;
 #endif
 
-#define VISIONIMG_FORMAT_RGB24 1
-
-typedef struct VisionImg {
-  int fd;
-  int format;
-  int width, height, stride;
-  int bpp;
-  size_t size;
-} VisionImg;
-
-GLuint visionimg_to_gl(const VisionImg *img, EGLImageKHR *pkhr, void **pph);
-void visionimg_destroy_gl(EGLImageKHR khr, void *ph);
+class EGLImageTexture {
+ public:
+  EGLImageTexture(const VisionBuf *buf);
+  ~EGLImageTexture();
+  GLuint frame_tex = 0;
+#ifdef QCOM
+  void *private_handle = nullptr;
+  EGLImageKHR img_khr = 0;
+#endif
+};
