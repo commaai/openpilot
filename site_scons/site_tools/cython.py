2830842e32ec06f8a01430d8731c7d8baa588508
Willem Melching <willem.melching@gmail.com>
2021-01-11 13:50:58 +0100

    add scanner for cython dependencies

--- site_scons/site_tools/cython.py
+++ site_scons/site_tools/cython.py
@@ -1,18 +1,39 @@
+import re
 import SCons
 from SCons.Action import Action
+from SCons.Scanner import Scanner
 
+pyx_from_import_re = re.compile(r'^from\s+(\S+)\s+cimport', re.M)
+pyx_import_re = re.compile(r'^import\s+(\S+)', re.M)
+cdef_import_re = re.compile(r'^cdef extern from\s+.(\S+).:', re.M)
+
+
+def pyx_scan(node, env, path, arg=None):
+  contents = node.get_text_contents()
+  matches = pyx_from_import_re.findall(contents)
+  matches += pyx_import_re.findall(contents)
+
+  matches = [m.replace('.', '/') + '.pxd' for m in matches]
+  matches += cdef_import_re.findall(contents)
+  matches = [m for m in matches if env.File(m).exists()]
+  return env.File(matches)
+
+
+pyxscanner = Scanner(function=pyx_scan, skeys=['.pyx', '.pxd'], recursive=True)
 cythonAction = Action("$CYTHONCOM")
+
 
 def create_builder(env):
   try:
     cython = env['BUILDERS']['Cython']
   except KeyError:
     cython = SCons.Builder.Builder(
-               action = cythonAction,
-               emitter = {},
-               suffix = cython_suffix_emitter,
-               single_source = 1
-             )
+      action=cythonAction,
+      emitter={},
+      suffix=cython_suffix_emitter,
+      single_source=1
+    )
+    env.Append(SCANNERS=pyxscanner)
     env['BUILDERS']['Cython'] = cython
   return cython
 
