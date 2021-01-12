433360a8b2a69e73b694d5409b27fe4e2815a23e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-12 17:17:29 -0500

    Update update_requirements.sh

--- update_requirements.sh
+++ update_requirements.sh
@@ -2,10 +2,10 @@
 
 # Require script to be run as root
 function super-user-check() {
-    if [ "$EUID" -ne 0 ]; then
-        echo "You need to run this script as super user."
-        exit
-    fi
+  if [ "$EUID" -ne 0 ]; then
+    echo "You need to run this script as super user."
+    exit
+  fi
 }
 
 # Check for root
@@ -13,11 +13,12 @@ super-user-check
 
 # Detect Operating System
 function dist-check() {
-    if [ -e /etc/os-release ]; then
-        # shellcheck disable=SC1091
-        source /etc/os-release
-        DISTRO=$ID
-    fi
+  if [ -e /etc/os-release ]; then
+    # shellcheck disable=SC1091
+    source /etc/os-release
+    DISTRO=$ID
+    DISTRO_VERSION=$VERSION_ID
+  fi
 }
 
 # Check Operating System
@@ -25,45 +26,51 @@ dist-check
 
 # Pre-Checks
 function installing-system-requirements() {
-    if { ! [ -x "$(command -v pyenv)" ] || ! [ -x "$(command -v make)" ] || ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v wget)" ]; }; then
-        if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ]; }; then
-            apt-get update && apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
-        elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
-            yum update -y && yum install epel-release iptables curl coreutils bc jq sed e2fsprogs -y
-        elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
-            pacman -Syu --noconfirm iptables curl bc jq sed
-        fi
-        curl https://pyenv.run | bash
-        echo "export PYENV_ROOT=""$HOME":/.pyenv"" >>~/.bashrc
-        echo "export PATH=""$PYENV_ROOT":/"$PYENV_ROOT"/shims/"$PATH""" >>~/.bashrc
-        exec "$SHELL"
+  if { ! [ -x "$(command -v pyenv)" ] || ! [ -x "$(command -v make)" ] || ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v wget)" ]; }; then
+    if [ "$DISTRO" == "ubuntu" ] && [ "$DISTRO_VERSION" == "20.04" ]; then
+      apt-get update && apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+      curl https://pyenv.run | bash
+      echo "export PYENV_ROOT=""$HOME":/.pyenv"" >>~/.bashrc
+      echo "export PATH=""$PYENV_ROOT":/"$PYENV_ROOT"/shims/"$PATH""" >>~/.bashrc
+      exec "$SHELL"
+    else
+      exit
     fi
+  fi
 }
 
 # Run the function and check for requirements
 installing-system-requirements
 
 function install-pyenv() {
+  if [ "$DISTRO" == "ubuntu" ] && [ "$DISTRO_VERSION" == "20.04" ]; then
     cd "$(dirname "${BASH_SOURCE[0]}")" || exit
     PYENV_PYTHON_VERSION=$(cat .python-version)
     if ! [ -x "$(pyenv prefix "${PYENV_PYTHON_VERSION}")" ]; then
-        CONFIGURE_OPTS=--enable-shared pyenv install -f "${PYENV_PYTHON_VERSION}"
+      CONFIGURE_OPTS=--enable-shared pyenv install -f "${PYENV_PYTHON_VERSION}"
     elif ! [ -x "$(command -v pipenv)" ]; then
-        pip install pipenv
+      pip install pipenv
     fi
+  else
+    exit
+  fi
 }
 
 install-pyenv
 
 function pip-stuff() {
+  if [ "$DISTRO" == "ubuntu" ] && [ "$DISTRO_VERSION" == "20.04" ]; then
     if [ -x "$(command -v pip)" ]; then
-        pip install --upgrade pip
+      pip install --upgrade pip
     elif [ -x "$(command -v pipenv)" ]; then
-        pipenv install --dev --deploy --system
+      pipenv install --dev --deploy --system
     elif [ -x "$(command -v pyenv)" ]; then
-        pyenv rehash
-        pre-commit install
+      pyenv rehash
+      pre-commit install
     fi
+  else
+    exit
+  fi
 }
 
 pip-stuff
