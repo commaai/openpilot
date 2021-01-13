e76a01b48889c9015319f0bec88189351f9c4982
Shane Smiskol <shane@smiskol.com>
2021-01-13 16:13:29 -0600

    We don't need gas max tuning above min acc speed since we don't use pedal here

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -320,10 +320,8 @@ class CarInterface(CarInterfaceBase):
     ret.communityFeature = ret.enableGasInterceptor or ret.enableDsu or smartDsu
 
     if ret.enableGasInterceptor:
-      ret.gasMaxBP = [0., 9., 35]
-      ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.gasMaxBP = [0., MIN_ACC_SPEED]
+      ret.gasMaxV = [0.2, 0.5]
 
     return ret
 
