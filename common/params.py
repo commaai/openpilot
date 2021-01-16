c59edc064e5f67aedf09c1a26b40034579e71e24
Greg Hogan <gregjhogan@gmail.com>
2021-01-16 13:36:39 -0800

    add validation

--- common/params.py
+++ common/params.py
@@ -5,16 +5,19 @@ assert put_nonblocking
 
 if __name__ == "__main__":
   import sys
+  from common.params_pyx import keys # pylint: disable=no-name-in-module, import-error
+
   params = Params()
   if len(sys.argv) == 3:
     name = sys.argv[1]
     val = sys.argv[2]
+    assert name.encode("utf-8") in keys.keys(), f"unknown param: {name}"
     print(f"SET: {name} = {val}")
     params.put(name, val)
   elif len(sys.argv) == 2:
     name = sys.argv[1]
+    assert name.encode("utf-8") in keys.keys(), f"unknown param: {name}"
     print(f"GET: {name} = {params.get(name)}")
   else:
-    from common.params_pyx import keys # pylint: disable=no-name-in-module, import-error
     for k in keys.keys():
       print(f"GET: {k} = {params.get(k)}")
