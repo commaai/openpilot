30613c39cf8b177a30fdd0f596037bc1ffaac76a
Greg Hogan <gregjhogan@gmail.com>
2021-01-16 13:28:29 -0800

    params helpers

--- common/params.py
+++ common/params.py
@@ -2,3 +2,19 @@ from common.params_pyx import Params, UnknownKeyName, put_nonblocking # pylint: 
 assert Params
 assert UnknownKeyName
 assert put_nonblocking
+
+if __name__ == "__main__":
+  import sys
+  params = Params()
+  if len(sys.argv) == 3:
+    name = sys.argv[1]
+    val = sys.argv[2]
+    print(f"SET: {name} = {val}")
+    params.put(name, val)
+  elif len(sys.argv) == 2:
+    name = sys.argv[1]
+    print(f"GET: {name} = {params.get(name)}")
+  else:
+    from common.params_pyx import keys # pylint: disable=no-name-in-module, import-error
+    for k in keys.keys():
+      print(f"GET: {k} = {params.get(k)}")
