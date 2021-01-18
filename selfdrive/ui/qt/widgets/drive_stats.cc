0c141e45aa38f075afd44db77a9e858e42a623bf
grekiki <gregor1234567890@gmail.com>
2021-01-18 12:07:55 +0100

    Qt offroad: pairing (#19675)
    * kind of works now
    
    * QR code is now on screen
    
    * testing is needed
    
    * works, waiting for server update
    
    * reduce diff
    
    * refactoring part1
    
    * refactor part2
    
    * refactor part3
    
    * works on PC
    
    * fix build, annoying bug though...
    
    * git is annoying
    
    * more classes, still crashes after a while
    
    * better style
    
    * clearer qr code
    
    * less code and test new ssh key
    
    * no more AA
    
    * rename and collect garbage
    
    * no pairing widget without the internet connection
    
    * No network connection
    
    * more logs
    
    * refactor the network calls
    
    * no more leaking
    
    * works
    
    * cleanup
    
    * spaces
    
    * works on device
    
    * fix merge
    
    * don't run if screen is off
    
    * always initialized
    
    * stats updating, not running without scrren
    
    * formatting
    
    * nicer QR code
    
    * ensure the registration
    
    * no qr code if no IMEI or no HWSerial
    
    * all works
    
    * refactor
    
    * better networking
    
    * all functionality seems to work
    
    * small cleanup
    
    * get serial once
    
    * Small cleanup
    
    * No internet text
    
    * Don't crash if private key not found
    
    * no network should say so
    
    * new qr code every 30 minutes
    
    * 20FPS sidebar
    
    * more responsive networking and QR
    
    * more logs
    
    * no log spam
    
    * fix merge
    
    * no running onroad
    
    Co-authored-by: Comma Device <device@comma.ai>
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: 10bd761cd6fb59f8113f88cc4dc764fbc4f5abf2

--- selfdrive/ui/qt/widgets/drive_stats.cc
+++ selfdrive/ui/qt/widgets/drive_stats.cc
@@ -1,27 +1,23 @@
 #include <cassert>
 #include <iostream>
 
-#include <QFile>
 #include <QDebug>
-#include <QVBoxLayout>
-#include <QLabel>
+#include <QFile>
 #include <QJsonDocument>
 #include <QJsonObject>
-#include <QNetworkAccessManager>
+#include <QLabel>
 #include <QNetworkRequest>
-#include <QCryptographicHash>
+#include <QStackedLayout>
+#include <QTimer>
+#include <QVBoxLayout>
 
-#include <openssl/rsa.h>
-#include <openssl/bio.h>
-#include <openssl/pem.h>
-
-#include "drive_stats.hpp"
+#include "api.hpp"
 #include "common/params.h"
 #include "common/util.h"
+#include "drive_stats.hpp"
+#include "home.hpp"
 
-
-constexpr double MILE_TO_KM = 1.60934;
-
+const double MILE_TO_KM = 1.60934;
 
 #if defined(QCOM) || defined(QCOM2)
 const std::string private_key_path = "/persist/comma/id_rsa";
@@ -29,128 +25,93 @@ const std::string private_key_path = "/persist/comma/id_rsa";
 const std::string private_key_path = util::getenv_default("HOME", "/.comma/persist/comma/id_rsa", "/persist/comma/id_rsa");
 #endif
 
-
-QByteArray rsa_sign(QByteArray data) {
-  auto file = QFile(private_key_path.c_str());
-  bool r = file.open(QIODevice::ReadOnly);
-  assert(r);
-
-  auto key = file.readAll();
-
-  BIO *mem = BIO_new_mem_buf(key.data(), key.size());
-  assert(mem);
-
-  RSA *rsa_private = PEM_read_bio_RSAPrivateKey(mem, NULL, NULL, NULL);
-  assert(rsa_private);
-
-  auto sig = QByteArray();
-  sig.resize(RSA_size(rsa_private));
-
-  unsigned int sig_len;
-  int ret = RSA_sign(NID_sha256, (unsigned char*)data.data(), data.size(), (unsigned char*)sig.data(), &sig_len, rsa_private);
-
-  assert(ret == 1);
-  assert(sig_len == sig.size());
-
-  BIO_free(mem);
-  RSA_free(rsa_private);
-
-  return sig;
+void clearLayouts(QLayout* layout) {
+  while (QLayoutItem* item = layout->takeAt(0)) {
+    if (QWidget* widget = item->widget()) {
+      widget->deleteLater();
+    }
+    if (QLayout* childLayout = item->layout()) {
+      clearLayouts(childLayout);
+    }
+    delete item;
+  }
 }
 
-QString create_jwt(QString dongle_id, int expiry=3600) {
-  QJsonObject header;
-  header.insert("alg", "RS256");
-  header.insert("typ", "JWT");
+QLayout* build_stat(QString name, int stat) {
+  QVBoxLayout* layout = new QVBoxLayout;
 
-  auto t = QDateTime::currentSecsSinceEpoch();
-  QJsonObject payload;
-  payload.insert("identity", dongle_id);
-  payload.insert("nbf", t);
-  payload.insert("iat", t);
-  payload.insert("exp", t + expiry);
-
-  QString jwt =
-    QJsonDocument(header).toJson(QJsonDocument::Compact).toBase64() +
-    '.' +
-    QJsonDocument(payload).toJson(QJsonDocument::Compact).toBase64();
-
-  auto hash = QCryptographicHash::hash(jwt.toUtf8(), QCryptographicHash::Sha256);
-  auto sig = rsa_sign(hash);
-
-  jwt += '.' + sig.toBase64();
-
-  return jwt;
-}
-
-QLayout *build_stat(QString name, int stat) {
-  QVBoxLayout *layout = new QVBoxLayout;
-
-  QLabel *metric = new QLabel(QString("%1").arg(stat));
+  QLabel* metric = new QLabel(QString("%1").arg(stat));
   metric->setStyleSheet(R"(
     font-size: 72px;
     font-weight: 700;
   )");
   layout->addWidget(metric, 0, Qt::AlignLeft);
-  
-  QLabel *label = new QLabel(name);
+
+  QLabel* label = new QLabel(name);
   label->setStyleSheet(R"(
     font-size: 32px;
     font-weight: 600;
   )");
   layout->addWidget(label, 0, Qt::AlignLeft);
-  
+
   return layout;
 }
 
-void DriveStats::replyFinished(QNetworkReply *l) {
-  QString answer = l->readAll();
-  answer.chop(1);
+void DriveStats::parseError(QString response) {
+  clearLayouts(vlayout);
+  vlayout->addWidget(new QLabel("No internet connection"));
+}
 
-  QJsonDocument doc = QJsonDocument::fromJson(answer.toUtf8());
+void DriveStats::parseResponse(QString response) {
+  response.chop(1);
+  clearLayouts(vlayout);
+  QJsonDocument doc = QJsonDocument::fromJson(response.toUtf8());
   if (doc.isNull()) {
-    qDebug() << "JSON Parse failed";
+    qDebug() << "JSON Parse failed on getting past drives statistics";
+    return;
   }
+
   QString IsMetric = QString::fromStdString(Params().get("IsMetric"));
-  bool metric = (IsMetric =="1");
+  bool metric = (IsMetric == "1");
 
   QJsonObject json = doc.object();
   auto all = json["all"].toObject();
   auto week = json["week"].toObject();
 
-  QGridLayout *gl = new QGridLayout();
+  QGridLayout* gl = new QGridLayout();
 
-  int all_distance = all["distance"].toDouble()*(metric ? MILE_TO_KM : 1);
+  int all_distance = all["distance"].toDouble() * (metric ? MILE_TO_KM : 1);
   gl->addWidget(new QLabel("ALL TIME"), 0, 0, 1, 3);
   gl->addLayout(build_stat("DRIVES", all["routes"].toDouble()), 1, 0, 3, 1);
   gl->addLayout(build_stat(metric ? "KM" : "MILES", all_distance), 1, 1, 3, 1);
   gl->addLayout(build_stat("HOURS", all["minutes"].toDouble() / 60), 1, 2, 3, 1);
 
-  int week_distance = week["distance"].toDouble()*(metric ? MILE_TO_KM : 1);
+  int week_distance = week["distance"].toDouble() * (metric ? MILE_TO_KM : 1);
   gl->addWidget(new QLabel("PAST WEEK"), 6, 0, 1, 3);
   gl->addLayout(build_stat("DRIVES", week["routes"].toDouble()), 7, 0, 3, 1);
   gl->addLayout(build_stat(metric ? "KM" : "MILES", week_distance), 7, 1, 3, 1);
   gl->addLayout(build_stat("HOURS", week["minutes"].toDouble() / 60), 7, 2, 3, 1);
 
-  setLayout(gl);
+  QWidget* q = new QWidget;
+  q->setLayout(gl);
+
+  vlayout->addWidget(q);
+}
+
+DriveStats::DriveStats(QWidget* parent) : QWidget(parent) {
+  vlayout = new QVBoxLayout(this);
+  setLayout(vlayout);
   setStyleSheet(R"(
     QLabel {
       font-size: 48px;
       font-weight: 600;
     }
   )");
-}
 
-DriveStats::DriveStats(QWidget *parent) : QWidget(parent) {
-  QString dongle_id = QString::fromStdString(Params().get("DongleId"));
-  QString token = create_jwt(dongle_id);
+  QString dongleId = QString::fromStdString(Params().get("DongleId"));
+  QString url = "https://api.commadotai.com/v1.1/devices/" + dongleId + "/stats";
+  RequestRepeater* repeater = new RequestRepeater(this, url, 13);
+  QObject::connect(repeater, SIGNAL(receivedResponse(QString)), this, SLOT(parseResponse(QString)));
+  QObject::connect(repeater, SIGNAL(failedResponse(QString)), this, SLOT(parseError(QString)));
 
-  QNetworkAccessManager *manager = new QNetworkAccessManager(this);
-  connect(manager, &QNetworkAccessManager::finished, this, &DriveStats::replyFinished);
-
-  QNetworkRequest request;
-  request.setUrl(QUrl("https://api.commadotai.com/v1.1/devices/" + dongle_id + "/stats"));
-  request.setRawHeader("Authorization", ("JWT "+token).toUtf8());
-
-  manager->get(request);
 }
