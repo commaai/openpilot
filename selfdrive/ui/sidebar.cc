8227270769633dd934c4ea13b8c3e5ee6ec4fe31
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-01-18 15:50:26 -0800

    no vehicle was more confusing
    old-commit-hash: 56dfb3de2dd23f56b96524aeec7fcd6943b10116

--- selfdrive/ui/sidebar.cc
+++ selfdrive/ui/sidebar.cc
@@ -120,7 +120,7 @@ static void draw_panda_metric(UIState *s) {
   std::string panda_message = "VEHICLE\nONLINE";
   if (s->scene.hwType == cereal::HealthData::HwType::UNKNOWN) {
     panda_severity = 2;
-    panda_message = "NO\nVEHICLE";
+    panda_message = "NO\nPANDA";
   } else if (s->started) {
     if (s->scene.satelliteCount < 6) {
       panda_severity = 1;
