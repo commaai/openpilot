e63ca8cf766e121c9f5d67f61ac30733e8801300
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-01-20 12:40:41 -0500

    adding IsDisengageOnGasEnabled

--- common/params_pyx.pyx
+++ common/params_pyx.pyx
@@ -72,6 +72,7 @@ keys = {
   b"Offroad_NeosUpdate": [TxType.CLEAR_ON_MANAGER_START],
   b"Offroad_UpdateFailed": [TxType.CLEAR_ON_MANAGER_START],
   b"Offroad_HardwareUnsupported": [TxType.CLEAR_ON_MANAGER_START],
+  b"IsDisengageOnGasEnabled": [TxType.PERSISTENT],
 }
 
 def ensure_bytes(v):
