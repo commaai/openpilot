54063fba8752d0462cb656369a47f2c4f1e5b557
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-01-19 23:33:17 -0500

    adding params stuff

--- common/basedir.py
+++ common/basedir.py
@@ -6,6 +6,8 @@ from selfdrive.hardware import PC
 BASEDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
 
 if PC:
-  PERSIST = os.path.join(str(Path.home()), ".comma", "persist")
+  PERSIST = os.path.join(BASEDIR, "persist")
+  PARAMS = os.path.join(BASEDIR, "persist", "params")
 else:
   PERSIST = "/persist"
+  PARAMS = "/data/params"
