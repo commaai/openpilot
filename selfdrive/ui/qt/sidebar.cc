d3ec343a81f83bea0aa0f89c0d9eece85bdaaeb0
Marcos Scheeren <marcosscheeren@gmail.com>
2021-01-22 07:41:52 -0800

    cell signal indicators

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -9,25 +9,20 @@
 //TODO: custom qlabel text size
 
 SettingsBtn::SettingsBtn(QWidget* parent) : QAbstractButton(parent){
-  qDebug() << "SettingsBtn ctor";
-
   setMinimumHeight(160);
   setMaximumSize(300,200);
   image = QImageReader("../assets/images/button_settings.png").read().scaledToWidth(110,Qt::SmoothTransformation);
 }
 
 void SettingsBtn::paintEvent(QPaintEvent *e){
-  qDebug() << "SettingsBtn paintEvent";
   QPainter p(this);
   //New settings btn stuff
-  // p.setRenderHint(QPainter::Antialiasing, true);
-  // p.setPen(QPen(QColor(0xb2b2b2), 3, Qt::SolidLine, Qt::FlatCap));
-  // p.setBrush(Qt::black);
-  //origin at 1.5,1.5 because qt issues with pixel perfect borders
-  // p.drawRoundedRect(QRectF(1.5, 1.5, size().width()-3, size().height()-3), 30, 30);
-  p.drawImage((size().width()/2)-(image.width()/2),
-               (size().height()/2)-(image.height()/2),
-               image,0,0,0,0, Qt::AutoColor);
+  p.setRenderHint(QPainter::Antialiasing, true);
+  p.setPen(QPen(QColor(0xb2b2b2), 3, Qt::SolidLine, Qt::FlatCap));
+  p.setBrush(Qt::black);
+  // origin at 1.5,1.5 because qt issues with pixel perfect borders
+  p.drawRoundedRect(QRectF(1.5, 1.5, size().width()-3, size().height()-3), 30, 30);
+  p.drawImage((size().width()/2)-(image.width()/2), (size().height()/2)-(image.height()/2), image,0,0,0,0, Qt::AutoColor);
 }
 
 StatusWidget::StatusWidget(QString text, QWidget* parent) : QFrame(parent)
@@ -70,23 +65,57 @@ void StatusWidget::paintEvent(QPaintEvent *e){
 }
 
 
+SignalWidget::SignalWidget(QString text, int strength, QWidget* parent) : QFrame(parent),
+_strength(strength)
+{
+  setFixedSize(300,80);
+  QLabel* label = new QLabel(this);
+  label->setText(text);
+  label->setStyleSheet(R"(font-size: 30px; font-weight: 600;)");
+  label->setAlignment(Qt::AlignVCenter);
+  QHBoxLayout* layout = new QHBoxLayout();
+  layout->setSpacing(0);
+  layout->setContentsMargins(24,16,24,16);
+  layout->addWidget(label, 0, Qt::AlignLeft);
+  setLayout(layout);
+}
+
+void SignalWidget::paintEvent(QPaintEvent *e){  
+  QPainter p(this);
+  p.setRenderHint(QPainter::Antialiasing, true);
+  p.setPen(QPen(QColor(0xb2b2b2), 3, Qt::SolidLine, Qt::FlatCap));
+  for (int i = _strength; i < 5 ; i++) //draw empty dots
+  {
+    p.drawEllipse(QRectF(1.5 + _firstdot + _dotspace * i, 1.5 + _top, _dia - 3, _dia - 3));
+  }
+  p.setPen(Qt::NoPen);
+  p.setBrush(Qt::white);
+  for (int i = 0; i < _strength; i++) //draw filled dots
+  {
+    p.drawEllipse(QRectF(_firstdot + _dotspace * i, _top, _dia, _dia));
+  } 
+}
+
 Sidebar::Sidebar(QWidget* parent) : QFrame(parent){
   QVBoxLayout* main_layout = new QVBoxLayout();
   main_layout->setContentsMargins(24,16,24,16); //24 sides, 16 vertical
-  main_layout->setSpacing(0);
+  main_layout->setSpacing(16);
   setFixedSize(300,1080);
 
   SettingsBtn* s_btn = new SettingsBtn(this);
+  SignalWidget* signal = new SignalWidget("4G",2,this);
   //TODO: better temp widget layouting/font
   StatusWidget* temp = new StatusWidget("39C\nTEMP", this);
   StatusWidget* vehicle = new StatusWidget("VEHICLE\nGOOD GPS", this);
   StatusWidget* connect = new StatusWidget("CONNECT\nONLINE", this);
   
 
+  main_layout->addWidget(signal, 0,Qt::AlignTop);
   main_layout->addWidget(s_btn, 0,Qt::AlignTop);
   main_layout->addWidget(temp, 0,Qt::AlignTop);
   main_layout->addWidget(vehicle, 0,Qt::AlignTop);
   main_layout->addWidget(connect, 0,Qt::AlignTop);
+  main_layout->addStretch(1);
   setStyleSheet(R"( Sidebar { background: qlineargradient(
                                     x1: 0, y1: 0, x2: 0, y2: 1,
                                     stop: 0 #242424,
--- selfdrive/ui/qt/sidebar.hpp
+++ selfdrive/ui/qt/sidebar.hpp
@@ -14,6 +14,25 @@ private:
   QImage image;
 };
 
+class SignalWidget : public QFrame {
+  Q_OBJECT
+
+public:
+  SignalWidget(QString text, int strength, QWidget* parent = 0);
+  QLabel* label;
+  int _strength = 0;
+
+protected:
+  void paintEvent(QPaintEvent*) override;
+
+private:
+  float _firstdot = 85; //1st dot x pos
+  float _dotspace = 30; //spacing between dots
+  float _top = 30;
+  float _dia = 20; //dot diameter
+};
+
+
 class StatusWidget : public QFrame {
   Q_OBJECT
 
