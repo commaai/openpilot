dfe1ee28efadacc9b632cbe2f3b262df18a5c7e2
Willem Melching <willem.melching@gmail.com>
2021-01-22 16:15:21 +0100

    Remove boardd

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -166,7 +166,6 @@ managed_processes = {
   "tombstoned": "selfdrive.tombstoned",
   "logcatd": ("selfdrive/logcatd", ["./logcatd"]),
   "proclogd": ("selfdrive/proclogd", ["./proclogd"]),
-  "boardd": ("selfdrive/boardd", ["./boardd"]),   # not used directly
   "pandad": "selfdrive.pandad",
   "ui": ("selfdrive/ui", ["./ui"]),
   "calibrationd": "selfdrive.locationd.calibrationd",
