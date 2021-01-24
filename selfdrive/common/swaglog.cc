b1cd56c988f91812d73a1d4c3eb64404a5fe840b
deanlee <deanlee3@gmail.com>
2021-01-24 13:23:08 +0800

    continue

--- selfdrive/common/swaglog.cc
+++ selfdrive/common/swaglog.cc
@@ -5,19 +5,21 @@
 #include <string>
 #include <string.h>
 #include <assert.h>
+
 #include <mutex>
 #include <zmq.h>
 
 #include "json11.hpp"
+
+#include "common/util.h"
 #include "common/version.h"
+
 #include "swaglog.h"
 
 class LogState {
 public:
   LogState() = default;
   ~LogState();
-  void init();
-  void log(int levelnum, const char* filename, int lineno, const char* func, const char* msg, const std::string &log_s);
   std::mutex lock;
   bool inited;
   json11::Json::object ctx_j;
@@ -26,37 +28,43 @@ public:
   int print_level;
 };
 
-void LogState::init() {
-  zctx = zmq_ctx_new();
-  sock = zmq_socket(zctx, ZMQ_PUSH);
-  
-  print_level = CLOUDLOG_WARNING;
-  const char* log_print = getenv("LOGPRINT");
-  if (log_print) {
-    if (strcmp(log_print, "debug") == 0) {
-      print_level = CLOUDLOG_DEBUG;
-    } else if (strcmp(log_print, "info") == 0) {
-      print_level = CLOUDLOG_INFO;
-    } else if (strcmp(log_print, "warning") == 0) {
-      print_level = CLOUDLOG_WARNING;
-    }
-  }
-
-  // openpilot bindings
-  ctx_j = json11::Json::object{};
-  char* dongle_id = getenv("DONGLE_ID");
-  if (dongle_id) {
-    ctx_j["dongle_id"] = dongle_id;
-  }
-  ctx_j["version"] = COMMA_VERSION;
-  ctx_j["dirty"] = !getenv("CLEAN");
-  inited = true;
-}
-
 LogState::~LogState() {
   zmq_close(sock);
   zmq_ctx_destroy(zctx);
 }
+
+static LogState s = {};
+
+static void cloudlog_bind_locked(const char* k, const char* v) {
+  s.ctx_j[k] = v;
+}
+
+static void cloudlog_init() {
+  if (s.inited) return;
+  s.ctx_j = json11::Json::object {};
+  s.zctx = zmq_ctx_new();
+  s.sock = zmq_socket(s.zctx, ZMQ_PUSH);
+  zmq_connect(s.sock, "ipc:///tmp/logmessage");
+
+  s.print_level = CLOUDLOG_WARNING;
+  const char* print_level = getenv("LOGPRINT");
+  if (print_level) {
+    if (strcmp(print_level, "debug") == 0) {
+      s.print_level = CLOUDLOG_DEBUG;
+    } else if (strcmp(print_level, "info") == 0) {
+      s.print_level = CLOUDLOG_INFO;
+    } else if (strcmp(print_level, "warning") == 0) {
+      s.print_level = CLOUDLOG_WARNING;
+    }
+  }
+
+  // openpilot bindings
+  char* dongle_id = getenv("DONGLE_ID");
+  if (dongle_id) {
+    cloudlog_bind_locked("dongle_id", dongle_id);
+  }
+  cloudlog_bind_locked("version", COMMA_VERSION);
+  s.ctx_j["dirty"] = !getenv("CLEAN");
 
   // device type
   if (util::file_exists("/EON")) {
@@ -68,20 +76,18 @@ LogState::~LogState() {
   }
 
   s.inited = true;
-void LogState::log(int levelnum, const char* filename, int lineno, const char* func, const char* msg, const std::string &log_s) {
-  std::lock_guard lk(lock);
-  if (!inited) {
-    init();
-  }
-  if (levelnum >= print_level) {
+}
+
+void log(int levelnum, const char* filename, int lineno, const char* func, const char* msg, const std::string& log_s) {
+  std::lock_guard lk(s.lock);
+  cloudlog_init();
+  if (levelnum >= s.print_level) {
     printf("%s: %s\n", filename, msg);
   }
   char levelnum_c = levelnum;
-  zmq_send(sock, &levelnum_c, 1, ZMQ_NOBLOCK | ZMQ_SNDMORE);
-  zmq_send(sock, log_s.c_str(), log_s.length(), ZMQ_NOBLOCK);
+  zmq_send(s.sock, &levelnum_c, 1, ZMQ_NOBLOCK | ZMQ_SNDMORE);
+  zmq_send(s.sock, log_s.c_str(), log_s.length(), ZMQ_NOBLOCK);
 }
-
-static LogState log_state;
 
 void cloudlog_e(int levelnum, const char* filename, int lineno, const char* func,
                 const char* fmt, ...) {
@@ -95,7 +101,7 @@ void cloudlog_e(int levelnum, const char* filename, int lineno, const char* func
 
   json11::Json log_j = json11::Json::object {
     {"msg", msg_buf},
-    {"ctx", log_state.ctx_j},
+    {"ctx", s.ctx_j},
     {"levelnum", levelnum},
     {"filename", filename},
     {"lineno", lineno},
@@ -103,6 +109,12 @@ void cloudlog_e(int levelnum, const char* filename, int lineno, const char* func
     {"created", seconds_since_epoch()}
   };
   std::string log_s = log_j.dump();
-  log_state.log(levelnum, filename, lineno, func, msg_buf, log_s);
+  log(levelnum, filename, lineno, func, msg_buf, log_s);
   free(msg_buf);
+}
+
+void cloudlog_bind(const char* k, const char* v) {
+  std::lock_guard lk(s.lock);
+  cloudlog_init();
+  cloudlog_bind_locked(k, v);
 }
