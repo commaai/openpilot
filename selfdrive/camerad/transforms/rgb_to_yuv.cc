d19741714ff4a2a2c7749a0d2ed9e8d4f838c3ce
deanlee <deanlee3@gmail.com>
2021-01-26 12:44:04 +0800

    remove memset

--- selfdrive/camerad/transforms/rgb_to_yuv.cc
+++ selfdrive/camerad/transforms/rgb_to_yuv.cc
@@ -6,7 +6,6 @@
 #include "rgb_to_yuv.h"
 
 void rgb_to_yuv_init(RGBToYUVState* s, cl_context ctx, cl_device_id device_id, int width, int height, int rgb_stride) {
-  memset(s, 0, sizeof(*s));
   printf("width %d, height %d, rgb_stride %d\n", width, height, rgb_stride);
   assert(width % 2 == 0);
   assert(height % 2 == 0);
