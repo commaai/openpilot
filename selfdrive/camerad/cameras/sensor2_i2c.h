0915964626ada0982866a48a54ea54eb1a1652fb
Comma Device <device@comma.ai>
2021-01-26 05:10:31 +0000

    no

--- selfdrive/camerad/cameras/sensor2_i2c.h
+++ selfdrive/camerad/cameras/sensor2_i2c.h
@@ -315,8 +315,8 @@ struct i2c_random_wr_payload init_array_ar0231[] = {
   {0x340C, 0x802}, // 2 // 0000 0000 0010
 
   // Readout timing
-  {0x300C, 0x0646}, // LINE_LENGTH_PCK: min for 2-exp HDR
-  {0x300A, 0x09BA}, // FRAME_LENGTH_LINES
+  {0x300C, 0x074B}, // LINE_LENGTH_PCK: min for 2-exp HDR
+  {0x300A, 0x085E}, // FRAME_LENGTH_LINES
   {0x3042, 0x0000}, // EXTRA_DELAY
 
   // Readout Settings
