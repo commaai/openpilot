4fd576e7f78fdfa2f5b400252cef00b7f8990ee4
Willem Melching <willem.melching@gmail.com>
2021-01-27 22:36:40 +0100

    Put bootlog in own folder (#19939)
    * put bootlog in own folder
    
    * fix uploader
    
    * remove print statements
    
    * update test
    
    * remove comment
    
    * also write initddata
    
    * remove sentinels from test
    old-commit-hash: 87119aebe2e7051df49f6f2be9c29d2af4737656

--- selfdrive/loggerd/bootlog.cc
+++ selfdrive/loggerd/bootlog.cc
@@ -1,37 +1,60 @@
 #include <assert.h>
 #include <string>
+#include <cstdio>
+
+#include <bzlib.h>
 
 #include "common/swaglog.h"
 #include "common/util.h"
-#include "logger.h"
 #include "messaging.hpp"
+#include "logger.h"
 
 int main(int argc, char** argv) {
-  LoggerState logger;
-  logger_init(&logger, "bootlog", false);
+  char filename[64] = {'\0'};
 
-  char segment_path[4096];
-  int err = logger_next(&logger, LOG_ROOT.c_str(), segment_path, sizeof(segment_path), nullptr);
-  assert(err == 0);
-  LOGW("bootlog to %s", segment_path);
+  time_t rawtime = time(NULL);
+  struct tm timeinfo;
 
-  MessageBuilder msg;
-  auto boot = msg.initEvent().initBoot();
+  localtime_r(&rawtime, &timeinfo);
+  strftime(filename, sizeof(filename),
+           "%Y-%m-%d--%H-%M-%S.bz2", &timeinfo);
 
-  boot.setWallTimeNanos(nanos_since_epoch());
+  std::string path = LOG_ROOT + "/boot/" + std::string(filename);
+  LOGW("bootlog to %s", path.c_str());
 
-  std::string lastKmsg = util::read_file("/sys/fs/pstore/console-ramoops");
-  boot.setLastKmsg(capnp::Data::Reader((const kj::byte*)lastKmsg.data(), lastKmsg.size()));
+  MessageBuilder boot_msg;
+  logger_build_boot(boot_msg);
 
-  std::string lastPmsg = util::read_file("/sys/fs/pstore/pmsg-ramoops-0");
-  boot.setLastPmsg(capnp::Data::Reader((const kj::byte*)lastPmsg.data(), lastPmsg.size()));
+  MessageBuilder init_msg;
+  logger_build_init_data(init_msg);
 
-  std::string launchLog = util::read_file("/tmp/launch_log");
-  boot.setLaunchLog(capnp::Text::Reader(launchLog.data(), launchLog.size()));
 
-  auto bytes = msg.toBytes();
-  logger_log(&logger, bytes.begin(), bytes.size(), false);
+  // Open bootlog
+  int r = logger_mkpath((char*)path.c_str());
+  assert(r == 0);
 
-  logger_close(&logger);
+  FILE * file = fopen(path.c_str(), "wb");
+  assert(file != nullptr);
+
+  // Open as bz2
+  int bzerror;
+  BZFILE* bz_file = BZ2_bzWriteOpen(&bzerror, file, 9, 0, 30);
+  assert(bzerror == BZ_OK);
+
+  // Write initdata
+  auto bytes = init_msg.toBytes();
+  BZ2_bzWrite(&bzerror, bz_file, bytes.begin(), bytes.size());
+  assert(bzerror == BZ_OK);
+
+  // Write bootlog
+  bytes = boot_msg.toBytes();
+  BZ2_bzWrite(&bzerror, bz_file, bytes.begin(), bytes.size());
+  assert(bzerror == BZ_OK);
+
+  // Close bz2 and file
+  BZ2_bzWriteClose(&bzerror, bz_file, 0, NULL, NULL);
+  assert(bzerror == BZ_OK);
+
+  fclose(file);
   return 0;
 }
