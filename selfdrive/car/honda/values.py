f9d6e8bf0a7390e68a1ff2627bf1346fb064eb93
Chris McCammon <chrismccammon123@gmail.com>
2021-01-27 00:04:15 -0800

    Civic Hatch Diesel fw (#19929)

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -561,16 +561,42 @@ FW_VERSIONS = {
     ],
   },
   CAR.CIVIC_BOSCH_DIESEL: {
-    (Ecu.programmedFuelInjection, 0x18da10f1, None): [b'37805-59N-G830\x00\x00'],
-    (Ecu.transmission, 0x18da1ef1, None): [b'28101-59Y-G620\x00\x00'],
-    (Ecu.vsa, 0x18da28f1, None): [b'57114-TGN-E320\x00\x00'],
-    (Ecu.eps, 0x18da30f1, None): [b'39990-TFK-G020\x00\x00'],
-    (Ecu.srs, 0x18da53f1, None): [b'77959-TFK-G210\x00\x00'],
-    (Ecu.combinationMeter, 0x18da60f1, None): [b'78109-TFK-G020\x00\x00'],
-    (Ecu.fwdRadar, 0x18dab0f1, None): [b'36802-TFK-G130\x00\x00'],
-    (Ecu.shiftByWire, 0x18da0bf1, None): [b'54008-TGN-E010\x00\x00'],
-    (Ecu.fwdCamera, 0x18dab5f1, None): [b'36161-TFK-G130\x00\x00'],
-    (Ecu.gateway, 0x18daeff1, None): [b'38897-TBA-A020\x00\x00'],
+    (Ecu.programmedFuelInjection, 0x18da10f1, None): [
+      b'37805-59N-G630\x00\x00',
+      b'37805-59N-G830\x00\x00',
+    ],
+    (Ecu.transmission, 0x18da1ef1, None): [
+      b'28101-59Y-G220\x00\x00',
+      b'28101-59Y-G620\x00\x00',
+    ],
+    (Ecu.vsa, 0x18da28f1, None): [
+      b'57114-TGN-E320\x00\x00',
+    ],
+    (Ecu.eps, 0x18da30f1, None): [
+      b'39990-TFK-G020\x00\x00',
+    ],
+    (Ecu.srs, 0x18da53f1, None): [
+      b'77959-TFK-G210\x00\x00',
+      b'77959-TGN-G220\x00\x00',
+    ],
+    (Ecu.combinationMeter, 0x18da60f1, None): [
+      b'78109-TFK-G020\x00\x00',
+      b'78109-TGN-G120\x00\x00',
+    ],
+    (Ecu.fwdRadar, 0x18dab0f1, None): [
+      b'36802-TFK-G130\x00\x00',
+      b'36802-TGN-G130\x00\x00',
+    ],
+    (Ecu.shiftByWire, 0x18da0bf1, None): [
+      b'54008-TGN-E010\x00\x00',
+    ],
+    (Ecu.fwdCamera, 0x18dab5f1, None): [
+      b'36161-TFK-G130\x00\x00',
+      b'36161-TGN-G130\x00\x00',
+    ],
+    (Ecu.gateway, 0x18daeff1, None): [
+      b'38897-TBA-A020\x00\x00',
+    ],
   },
   CAR.CRV: {
     (Ecu.vsa, 0x18da28f1, None): [b'57114-T1W-A230\x00\x00',],
