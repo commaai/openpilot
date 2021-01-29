5ea45b7be44ec97f09ee8160f269728ce52a5b7f
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2021-01-29 18:54:18 +0530

    added check for jwt.encode return type

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -27,7 +27,12 @@ class Api():
       'iat': now,
       'exp': now + timedelta(hours=1)
     }
-    return bytes(jwt.encode(payload, self.private_key, algorithm='RS256'), 'utf8')
+    token = jwt.encode(payload, self.private_key, algorithm='RS256')
+    if isinstance(token, str):
+      return token
+    else if(isinstance(token, bytes)):
+      return token.decode('utf8')
+    
 
 def api_get(endpoint, method='GET', timeout=None, access_token=None, **params):
   backend = "https://api.commadotai.com/"
