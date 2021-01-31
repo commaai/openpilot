06ac6986911711c8d6c76d34c8b8dff2aee8ffa1
Dean Lee <deanlee3@gmail.com>
2021-01-31 11:11:03 +0800

    OMXEncoder: remove pthread_mutex_t lock  from member variables (#19948)
    * remove mutex
    
    * rebase master
    old-commit-hash: 9a9641a296dcc0887d902412214510f0ff912fa2

--- selfdrive/loggerd/omx_encoder.h
+++ selfdrive/loggerd/omx_encoder.h
@@ -38,7 +38,6 @@ private:
   void wait_for_state(OMX_STATETYPE state);
   static void handle_out_buf(OmxEncoder *e, OMX_BUFFERHEADERTYPE *out_buf);
 
-  pthread_mutex_t lock;
   int width, height, fps;
   char vid_path[1024];
   char lock_path[1024];
