68aebfd93ba2cf232ae4cc58c5ed372a1711cb0a
Comma Device <device@comma.ai>
2021-02-01 02:32:11 +0000

    remove apk

--- release/build_release3.sh
+++ release/build_release3.sh
@@ -79,7 +79,7 @@ find . -name '*.o' -delete
 find . -name '*.os' -delete
 find . -name '*.pyc' -delete
 find . -name '__pycache__' -delete
-rm -rf .sconsign.dblite Jenkinsfile release/
+rm -rf .sconsign.dblite Jenkinsfile release/ apk/
 
 # Restore phonelibs
 git checkout phonelibs/
