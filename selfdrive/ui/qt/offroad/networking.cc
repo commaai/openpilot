6fe06104724ea9d394944ec9ce77e0612d6c70d5
Comma Device <device@comma.ai>
2021-02-02 11:09:25 +0000

    new termms

--- selfdrive/ui/qt/offroad/networking.cc
+++ selfdrive/ui/qt/offroad/networking.cc
@@ -4,9 +4,8 @@
 #include <QPixmap>
 #include <QPushButton>
 #include <QLineEdit>
+#include <QRandomGenerator>
 #include <QtConcurrent>
-#include <QtWebEngine>
-#include <QWebEngineView>
 
 #include "networking.hpp"
 
@@ -45,23 +44,80 @@ std::string exec(const char* cmd) {
 // Networking functions
 
 Networking::Networking(QWidget* parent) : QWidget(parent){
-  QHBoxLayout* hlayout = new QHBoxLayout;
-  QWebEngineView *view = new QWebEngineView(this);
-  // view->setUrl(QUrl(QStringLiteral("https://www.comma.ai")));
-  QString html = "<html><body><h2 id=\"openpilot-terms-and-conditions\">openpilot Terms and Conditions</h2><p>The Terms and Conditions below are effective for all users</p><p>Last Updated on October 18, 2019</p><p>Please read these Terms of Use (“Terms”) carefully before using openpilot which is open-sourced software developed by Comma.ai, Inc., a corporation organized under the laws of Delaware (“comma,” “us,” “we,” or “our”).</p><p><strong>Before using and by accessing openpilot, you indicate that you have read, understood, and agree to these Terms.</strong> These Terms apply to all users and others who access or use openpilot. If others use openpilot through your user account or vehicle, you are responsible to ensure that they only use openpilot when it is safe to do so, and in compliance with these Terms and with applicable law. If you disagree with any part of the Terms, you should not access or use openpilot.</p><h3 id=\"communications\">Communications</h3><p>You agree that comma may contact you by email or telephone in connection with openpilot or for other business purposes. You may opt out of receiving email messages at any time by contacting us at support@comma.ai.</p><p>We collect, use, and share information from and about you and your vehicle in connection with openpilot. You consent to comma accessing the systems associated with openpilot, without additional notice or consent, for the purposes of providing openpilot, data collection, software updates, safety and cybersecurity, suspension or removal of your account, and as disclosed in the Privacy Policy (available at https://my.comma.ai/privacy).</p><h3 id=\"safety\">Safety</h3><p>openpilot performs the functions of Adaptive Cruise Control (ACC) and Lane Keeping Assist System (LKAS) designed for use in compatible motor vehicles. While using openpilot, it is your responsibility to obey all laws, traffic rules, and traffic regulations governing your vehicle and its operation. Access to and use of openpilot is at your own risk and responsibility, and openpilot should be accessed and/or used only when you can do so safely.</p><p>openpilot does not make your vehicle “autonomous” or capable of operation without the active monitoring of a licensed driver. It is designed to assist a licensed driver. A licensed driver must pay attention to the road, remain aware of navigation at all times, and be prepared to take immediate action. Failure to do so can cause damage, injury, or death.</p><h3 id=\"supported-locations-and-models\">Supported Locations and Models</h3><p>openpilot is compatible only with particular makes and models of vehicles. For a complete list of currently supported vehicles, visit https://comma.ai. openpilot will not function properly when installed in an incompatible vehicle. openpilot is compatible only within the geographical boundaries of the United States of America.</p><h3 id=\"indemnification\">Indemnification</h3><p><strong>To the maximum extent allowable by law, you agree to defend, indemnify and hold harmless comma, and its employees, partners, suppliers, contractors, investors, agents, officers, directors, and affiliates, from and against any and all claims, damages, causes of action, penalties, interest, demands, obligations, losses, liabilities, costs or debt, additional taxes, and expenses (including but not limited to attorneys’ fees), resulting from or arising out of (i) your use and access of, or inability to use or access, openpilot, (ii) your breach of these Terms, (iii) the inaccuracy of any information, representation or warranty made by you, (iv) activities of anyone other than you in connection with openpilot conducted through your comma device or account, (v) any other of your activities under or in connection with these Terms or openpilot.</strong></p><h3 id=\"limitation-of-liability\">Limitation of Liability</h3><p>In no event shall comma, nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from (i) your access to or use of or inability to access or use of the Software; or (ii) any conduct or content of any third party on the Software whether based on warranty, contract, tort (including negligence) or any other legal theory, whether or not we have been informed of the possibility of such damage, and even if a remedy set forth herein is found to have failed of its essential purpose.</p><h3 id=\"no-warranty-or-obligations-to-maintain-or-service\">No Warranty or Obligations to Maintain or Service</h3><p>comma provides openpilot without representations, conditions, or warranties of any kind. openpilot is provided on an “AS IS” and “AS AVAILABLE” basis, including with all faults and errors as may occur. To the extent permitted by law and unless prohibited by law, comma on behalf of itself and all persons and parties acting by, through, or for comma, explicitly disclaims all warranties or conditions, express, implied, or collateral, including any implied warranties of merchantability, satisfactory quality, and fitness for a particular purpose in respect of openpilot.</p><p>To the extent permitted by law, comma does not warrant the operation, performance, or availability of openpilot under all conditions. comma is not responsible for any failures caused by server errors, misdirected or redirected transmissions, failed internet connections, interruptions or failures in the transmission of data, any computer virus, or any acts or omissions of third parties that damage the network or impair wireless service.</p><p>We undertake reasonable measures to preserve and secure information collected through our openpilot. However, no data collection, transmission or storage system is 100% secure, and there is always a risk that your information may be intercepted without our consent. <em>In using openpilot, you acknowledge that comma is not responsible for intercepted information, and you hereby release us from any and all claims arising out of or related to the use of intercepted information in any unauthorized manner.</em></p><p>By providing openpilot, comma does not transfer or license its intellectual property or grant rights in its brand names, nor does comma make representations with respect to third-party intellectual property rights.</p><p>We are not obligated to provide any maintenance or support for openpilot, technical or otherwise. If we voluntarily provide any maintenance or support for openpilot, we may stop any such maintenance, support, or services at any time in our sole discretion.</p><h3 id=\"modification-of-software\">Modification of Software</h3><p>In no event shall comma, nor its directors, employees, partners, agents, suppliers, or affiliates, be liable if you choose to modify the software.</p><h3 id=\"changes\">Changes</h3><p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will provide at least 15 days’ notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p><p>By continuing to access or use our Software after any revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, you are no longer authorized to use the Software.</p><h3 id=\"contact-us\">Contact Us</h3><p>If you have any questions about these Terms, please contact us at support@comma.ai.</p></body></html>";
-  view->setHtml(html);
-  hlayout->addWidget(view);
-  setLayout(hlayout);
-  view->show();
+  try {
+    wifi = new WifiManager(this);
+  } catch (std::exception &e) {
+    QLabel* warning = new QLabel("Network manager is inactive!");
+    warning->setStyleSheet(R"(font-size: 65px;)");
 
+    QVBoxLayout* warning_layout = new QVBoxLayout;
+    warning_layout->addWidget(warning, 0, Qt::AlignCenter);
+    setLayout(warning_layout);
+    return;
+  }
+  connect(wifi, SIGNAL(wrongPassword(QString)), this, SLOT(wrongPassword(QString)));
+  connect(wifi, SIGNAL(successfulConnection(QString)), this, SLOT(successfulConnection(QString)));
+
+
+  s = new QStackedLayout;
+
+  inputField = new InputField(this, 8);
+  connect(inputField, SIGNAL(emitText(QString)), this, SLOT(receiveText(QString)));
+  connect(inputField, SIGNAL(cancel()), this, SLOT(abortTextInput()));
+  inputField->setContentsMargins(100,0,100,0);
+  s->addWidget(inputField);
+
+  QVBoxLayout* vlayout = new QVBoxLayout;
+  QPushButton* advancedSettings = new QPushButton("Advanced");
+  advancedSettings->setStyleSheet(R"(margin-right: 30px)");
+  advancedSettings->setFixedSize(300, 100);
+  connect(advancedSettings, &QPushButton::released, [=](){s->setCurrentIndex(2);});
+  vlayout->addSpacing(10);
+  vlayout->addWidget(advancedSettings, 0, Qt::AlignRight);
+  vlayout->addSpacing(10);
+
+  wifiWidget = new WifiUI(0, 5, wifi);
+  connect(wifiWidget, SIGNAL(connectToNetwork(Network)), this, SLOT(connectToNetwork(Network)));
+  vlayout->addWidget(wifiWidget, 1);
+
+  s->addWidget(layoutToWidget(vlayout, this));
+
+  an = new AdvancedNetworking(this, wifi);
+  connect(an, &AdvancedNetworking::backPress, [=](){s->setCurrentIndex(1);});
+  connect(an, &AdvancedNetworking::openKeyboard, [=](){emit openKeyboard();});
+  connect(an, &AdvancedNetworking::closeKeyboard, [=](){emit closeKeyboard();});
+  s->addWidget(an);
+
+  s->setCurrentIndex(1);
+
+  // Update network status
+  QTimer* timer = new QTimer(this);
+  QObject::connect(timer, SIGNAL(timeout()), this, SLOT(refresh()));
+  timer->start(5000);
+
+  setStyleSheet(R"(
+    QPushButton {
+      font-size: 50px;
+      margin: 0px;
+      padding: 15px;
+      border-radius: 25px;
+      color: #dddddd;
+      background-color: #444444;
+    }
+    QPushButton:disabled {
+      color: #777777;
+      background-color: #222222;
+    }
+  )");
+  setLayout(s);
 }
 
 void Networking::refresh(){
-  // if(!this->isVisible()){
-  //   return;
-  // }
-  // wifiWidget->refresh();
-  // an->refresh();
+  if(!this->isVisible()){
+    return;
+  }
+  wifiWidget->refresh();
+  an->refresh();
 }
 
 void Networking::connectToNetwork(Network n) {
@@ -292,89 +348,98 @@ void AdvancedNetworking::abortTextInput(){
 // WifiUI functions
 
 WifiUI::WifiUI(QWidget *parent, int page_length, WifiManager* wifi) : QWidget(parent), networks_per_page(page_length), wifi(wifi) {
+  vlayout = new QVBoxLayout;
+
+  // Scan on startup
+  QLabel *scanning = new QLabel("Scanning for networks");
+  scanning->setStyleSheet(R"(font-size: 65px;)");
+  vlayout->addWidget(scanning, 0, Qt::AlignCenter);
+  vlayout->setSpacing(25);
+
+  setLayout(vlayout);
+  page = 0;
 }
 
 void WifiUI::refresh() {
-  return;
-  // wifi->request_scan();
-  // wifi->refreshNetworks();
-  // clearLayout(vlayout);
+  wifi->request_scan();
+  wifi->refreshNetworks();
+  clearLayout(vlayout);
 
-  // connectButtons = new QButtonGroup(this); // TODO check if this is a leak
-  // QObject::connect(connectButtons, SIGNAL(buttonClicked(QAbstractButton*)), this, SLOT(handleButton(QAbstractButton*)));
+  connectButtons = new QButtonGroup(this); // TODO check if this is a leak
+  QObject::connect(connectButtons, SIGNAL(buttonClicked(QAbstractButton*)), this, SLOT(handleButton(QAbstractButton*)));
 
-  // int i = 0;
-  // int countWidgets = 0;
-  // int button_height = static_cast<int>(this->height() / (networks_per_page + 1) * 0.6);
-  // for (Network &network : wifi->seen_networks) {
-  //   QHBoxLayout *hlayout = new QHBoxLayout;
-  //   if (page * networks_per_page <= i && i < (page + 1) * networks_per_page) {
-  //     // SSID
-  //     hlayout->addSpacing(50);
-  //     QString ssid = QString::fromUtf8(network.ssid);
-  //     if(ssid.length() > 30){
-  //       ssid = ssid.left(30)+"…";
-  //     }
-  //     hlayout->addWidget(new QLabel(ssid));
+  int i = 0;
+  int countWidgets = 0;
+  int button_height = static_cast<int>(this->height() / (networks_per_page + 1) * 0.6);
+  for (Network &network : wifi->seen_networks) {
+    QHBoxLayout *hlayout = new QHBoxLayout;
+    if (page * networks_per_page <= i && i < (page + 1) * networks_per_page) {
+      // SSID
+      hlayout->addSpacing(50);
+      QString ssid = QString::fromUtf8(network.ssid);
+      if(ssid.length() > 30){
+        ssid = ssid.left(30)+"…";
+      }
+      hlayout->addWidget(new QLabel(ssid));
 
-  //     // strength indicator
-  //     unsigned int strength_scale = network.strength / 17;
-  //     QPixmap pix("../assets/images/network_" + QString::number(strength_scale) + ".png");
-  //     QLabel *icon = new QLabel();
-  //     icon->setPixmap(pix.scaledToWidth(100, Qt::SmoothTransformation));
-  //     icon->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
-  //     hlayout->addWidget(icon);
-  //     hlayout->addSpacing(20);
+      // strength indicator
+      unsigned int strength_scale = network.strength / 17;
+      QPixmap pix("../assets/images/network_" + QString::number(strength_scale) + ".png");
+      QLabel *icon = new QLabel();
+      icon->setPixmap(pix.scaledToWidth(100, Qt::SmoothTransformation));
+      icon->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
+      hlayout->addWidget(icon);
+      hlayout->addSpacing(20);
 
-  //     // connect button
-  //     QPushButton* btn = new QPushButton(network.security_type == SecurityType::UNSUPPORTED ? "Unsupported" : (network.connected == ConnectedType::CONNECTED ? "Connected" : (network.connected == ConnectedType::CONNECTING ? "Connecting" : "Connect")));
-  //     btn->setFixedWidth(400);
-  //     btn->setFixedHeight(button_height);
-  //     btn->setDisabled(network.connected == ConnectedType::CONNECTED || network.connected == ConnectedType::CONNECTING || network.security_type == SecurityType::UNSUPPORTED);
-  //     hlayout->addWidget(btn);
-  //     hlayout->addSpacing(20);
+      // connect button
+      QPushButton* btn = new QPushButton(network.security_type == SecurityType::UNSUPPORTED ? "Unsupported" : (network.connected == ConnectedType::CONNECTED ? "Connected" : (network.connected == ConnectedType::CONNECTING ? "Connecting" : "Connect")));
+      btn->setFixedWidth(400);
+      btn->setFixedHeight(button_height);
+      btn->setDisabled(network.connected == ConnectedType::CONNECTED || network.connected == ConnectedType::CONNECTING || network.security_type == SecurityType::UNSUPPORTED);
+      hlayout->addWidget(btn);
+      hlayout->addSpacing(20);
 
-  //     connectButtons->addButton(btn, i);
+      connectButtons->addButton(btn, i);
 
-  //     QWidget * w = new QWidget;
-  //     w->setLayout(hlayout);
-  //     vlayout->addWidget(w, 1);
-  //     // Don't add the last horizontal line
-  //     if (page * networks_per_page <= i+1 && i+1 < (page + 1) * networks_per_page && i+1 < wifi->seen_networks.size()) {
-  //       vlayout->addWidget(hline(), 0);
-  //     }
-  //     countWidgets++;
-  //   }
-  //   i++;
-  // }
+      QWidget * w = new QWidget;
+      w->setLayout(hlayout);
+      vlayout->addWidget(w, 1);
+      // Don't add the last horizontal line
+      if (page * networks_per_page <= i+1 && i+1 < (page + 1) * networks_per_page && i+1 < wifi->seen_networks.size()) {
+        vlayout->addWidget(hline(), 0);
+      }
+      countWidgets++;
+    }
+    i++;
+  }
 
-  // // Pad vlayout to prevert oversized network widgets in case of low visible network count
-  // for (int i = countWidgets; i < networks_per_page; i++) {
-  //   QWidget *w = new QWidget;
-  //   // That we need to add w twice was determined empiricaly
-  //   vlayout->addWidget(w, 1);
-  //   vlayout->addWidget(w, 1);
-  // }
+  // Pad vlayout to prevert oversized network widgets in case of low visible network count
+  for (int i = countWidgets; i < networks_per_page; i++) {
+    QWidget *w = new QWidget;
+    // That we need to add w twice was determined empiricaly
+    vlayout->addWidget(w, 1);
+    vlayout->addWidget(w, 1);
+  }
 
-  // QHBoxLayout *prev_next_buttons = new QHBoxLayout;//Adding constructor exposes the qt bug
-  // QPushButton* prev = new QPushButton("Previous");
-  // prev->setEnabled(page);
-  // prev->setFixedSize(400, button_height);
+  QHBoxLayout *prev_next_buttons = new QHBoxLayout;//Adding constructor exposes the qt bug
+  QPushButton* prev = new QPushButton("Previous");
+  prev->setEnabled(page);
+  prev->setFixedSize(400, button_height);
 
-  // QPushButton* next = new QPushButton("Next");
-  // next->setFixedSize(400, button_height);
+  QPushButton* next = new QPushButton("Next");
+  next->setFixedSize(400, button_height);
 
-  // // If there are more visible networks then we can show, enable going to next page
-  // next->setEnabled(wifi->seen_networks.size() > (page + 1) * networks_per_page);
+  // If there are more visible networks then we can show, enable going to next page
+  next->setEnabled(wifi->seen_networks.size() > (page + 1) * networks_per_page);
 
-  // QObject::connect(prev, SIGNAL(released()), this, SLOT(prevPage()));
-  // QObject::connect(next, SIGNAL(released()), this, SLOT(nextPage()));
-  // prev_next_buttons->addWidget(prev);
-  // prev_next_buttons->addWidget(next);
+  QObject::connect(prev, SIGNAL(released()), this, SLOT(prevPage()));
+  QObject::connect(next, SIGNAL(released()), this, SLOT(nextPage()));
+  prev_next_buttons->addWidget(prev);
+  prev_next_buttons->addWidget(next);
 
-  // QWidget *w = new QWidget;
-  // w->setLayout(prev_next_buttons);
-  // vlayout->addWidget(w, 1, Qt::AlignBottom);
+  QWidget *w = new QWidget;
+  w->setLayout(prev_next_buttons);
+  vlayout->addWidget(w, 1, Qt::AlignBottom);
 }
 
 void WifiUI::handleButton(QAbstractButton* button) {
