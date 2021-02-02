bd61557235c608bbe09af9572c940c88ac3687db
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2021-02-02 11:59:35 +0530

    Update test_get_token.py

--- common/tests/test_get_token.py
+++ common/tests/test_get_token.py
@@ -11,7 +11,7 @@ class TestApiGetToken(unittest.TestCase):
         assert os.system("openssl genrsa -out "+PERSIST+"/comma/id_rsa.tmp 2048") == 0
         assert os.system("openssl rsa -in "+PERSIST+"/comma/id_rsa.tmp -pubout -out "+PERSIST+"/comma/id_rsa.tmp.pub") == 0
         os.rename(PERSIST+"/comma/id_rsa.tmp", PERSIST+"/comma/id_rsa")
-        os.rename(PERSIST+"/comma/id_rsa.pub", PERSIST+"/comma/id_rsa.pub")
+        os.rename(PERSIST+"/comma/id_rsa.tmp.pub", PERSIST+"/comma/id_rsa.pub")
     
     def test_get_token(self):
         # This is prerequisite to create jwt token
