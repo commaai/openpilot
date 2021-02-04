fc0c48bf5b998df045c3b984a58516033e59d6c0
deanlee <deanlee3@gmail.com>
2021-02-04 18:10:56 +0800

    removed margin paramater from car_space_to_full_frame

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -37,7 +37,8 @@ const mat3 intrinsic_matrix = (mat3){{
 
 // Projects a point in car to space to the corresponding point in full frame
 // image space.
-bool car_space_to_full_frame(const UIState *s, float in_x, float in_y, float in_z, vertex_data *out, float margin) {
+bool car_space_to_full_frame(const UIState *s, float in_x, float in_y, float in_z, vertex_data *out) {
+  const float margin = 500.0f;
   const vec4 car_space_projective = (vec4){{in_x, in_y, in_z, 1.}};
   // We'll call the car space point p.
   // First project into normalized image coordinates with the extrinsics matrix.
