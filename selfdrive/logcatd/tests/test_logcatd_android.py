bb0f91791a0da06253b666dc3410b220697b72bb
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-02-07 17:29:52 -0800

    android logcatd fixes + better test (#20036)
    Co-authored-by: Comma Device <device@comma.ai>

--- selfdrive/logcatd/tests/test_logcatd_android.py
+++ selfdrive/logcatd/tests/test_logcatd_android.py
@@ -18,28 +18,30 @@ class TestLogcatdAndroid(unittest.TestCase):
     time.sleep(1)
     messaging.drain_sock(sock)
 
-    # write some log messages
-    sent_msgs = {}
-    for _ in range(random.randint(5, 50)):
-      msg = ''.join([random.choice(string.ascii_letters) for _ in range(random.randrange(2, 200))])
-      sent_msgs[msg] = ''.join([random.choice(string.ascii_letters) for _ in range(random.randrange(2, 20))])
-      os.system(f"log -t {sent_msgs[msg]} {msg}")
+    for _ in range(random.randint(2, 10)):
+      # write some log messages
+      sent_msgs = {}
+      for __ in range(random.randint(5, 50)):
+        msg = ''.join([random.choice(string.ascii_letters) for _ in range(random.randrange(2, 200))])
+        sent_msgs[msg] = ''.join([random.choice(string.ascii_letters) for _ in range(random.randrange(2, 20))])
+        os.system(f"log -t {sent_msgs[msg]} {msg}")
 
+      time.sleep(1)
+      msgs = messaging.drain_sock(sock)
+      for m in msgs:
+        self.assertTrue(m.valid)
+        self.assertLess(time.monotonic() - (m.logMonoTime / 1e9), 30)
+
+        recv_msg = m.androidLog.message.strip()
+        if recv_msg not in sent_msgs:
+          continue
+
+        self.assertEqual(m.androidLog.tag, sent_msgs[recv_msg])
+        del sent_msgs[recv_msg]
+
+      # ensure we received all the logs we sent
+      self.assertEqual(len(sent_msgs), 0)
     time.sleep(1)
-    msgs = messaging.drain_sock(sock)
-    for m in msgs:
-      self.assertTrue(m.valid)
-      self.assertLess(time.monotonic() - (m.logMonoTime / 1e9), 30)
-
-      recv_msg = m.androidLog.message.strip()
-      if recv_msg not in sent_msgs:
-        continue
-
-      self.assertEqual(m.androidLog.tag, sent_msgs[recv_msg])
-      del sent_msgs[recv_msg]
-
-    # ensure we received all the logs we sent
-    self.assertEqual(len(sent_msgs), 0)
 
 
 if __name__ == "__main__":
