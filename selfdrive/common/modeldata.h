09b63efe27164740905d79db8f83d148282b4be7
deanlee <deanlee3@gmail.com>
2021-02-10 03:46:33 +0800

    move intrinsic_matrix into modeldata.h

--- selfdrive/common/modeldata.h
+++ selfdrive/common/modeldata.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "common/mat.h"
 const int  TRAJECTORY_SIZE = 33;
 const float MIN_DRAW_DISTANCE = 10.0;
 const float MAX_DRAW_DISTANCE = 100.0;
@@ -16,3 +17,17 @@ const double X_IDXS[TRAJECTORY_SIZE] = { 0.    ,   0.1875,   0.75  ,   1.6875,  
         60.75  ,  67.6875,  75.    ,  82.6875,  90.75  ,  99.1875,
        108.    , 117.1875, 126.75  , 136.6875, 147.    , 157.6875,
        168.75  , 180.1875, 192.};
+
+#ifdef QCOM2
+const mat3 fcam_intrinsic_matrix = (mat3){{
+  2648.0, 0.0, 1928.0/2,
+  0.0, 2648.0, 1208.0/2,
+  0.0,   0.0,   1.0
+}};
+#else
+const mat3 fcam_intrinsic_matrix = (mat3){{
+  910., 0., 1164.0/2,
+  0., 910., 874.0/2,
+  0.,   0.,   1.
+}};
+#endif
