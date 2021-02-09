2f66f9852332bacf9166d52006daeef5b0e3b68a
HaraldSchafer <harald.the.engineer@gmail.com>
2021-02-09 15:36:58 -0800

    fix low speed lateral (#20053)
    * fix low speed lateral
    
    * update ref
    
    * fix failing CI jobs
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 0c900c8ce11409ddf574f5b7ff7bbab8b5536c36

--- selfdrive/controls/lib/lateral_mpc/lib_mpc_export/acado_solver.c
+++ selfdrive/controls/lib/lateral_mpc/lib_mpc_export/acado_solver.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ba1e5ddf6b2f2cedaf735f105951854666a75279b25b95e5fa64422964f4a79
+oid sha256:4d7de5aed6e5014ec35745f92356850f5b5d31b226cbcf93e9fafe0a4e29118c
 size 243595
