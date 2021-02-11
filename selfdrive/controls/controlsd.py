af95c26b262fa40d1808b4c669a5609f50fa53bd
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-02-10 16:39:39 -0800

    remove gpsd (#20027)
    old-commit-hash: fe7f3f0ec6b66e42b26451e180e3cde241e749bb

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -30,7 +30,7 @@ STEER_ANGLE_SATURATION_THRESHOLD = 2.5  # Degrees
 
 SIMULATION = "SIMULATION" in os.environ
 NOSENSOR = "NOSENSOR" in os.environ
-IGNORE_PROCESSES = set(["rtshield", "uploader", "deleter", "loggerd", "logmessaged", "tombstoned", "logcatd", "proclogd", "clocksd", "gpsd", "updated", "timezoned"])
+IGNORE_PROCESSES = set(["rtshield", "uploader", "deleter", "loggerd", "logmessaged", "tombstoned", "logcatd", "proclogd", "clocksd", "updated", "timezoned"])
 
 ThermalStatus = log.ThermalData.ThermalStatus
 State = log.ControlsState.OpenpilotState
