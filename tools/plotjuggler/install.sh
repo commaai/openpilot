766605fe533ce6130e943474fe0e1ca966f16ad0
iejMac <gig13omb@gmail.com>
2021-02-12 21:46:14 -0800

    changing release name for plotjuggler

--- tools/plotjuggler/install.sh
+++ tools/plotjuggler/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/commaai/PlotJuggler/releases/download/1.0/bin.tar.gz
+wget https://github.com/commaai/PlotJuggler/releases/download/latest/bin.tar.gz
 tar -xf bin.tar.gz
 rm bin.tar.gz
