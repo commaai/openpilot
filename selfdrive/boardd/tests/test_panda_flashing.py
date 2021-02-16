69b2a07188d9143fef4b5485ef2be86e609d230c
grekiki <gregor1234567890@gmail.com>
2021-02-16 12:17:49 +0100

    more stable tests

--- selfdrive/boardd/tests/test_panda_flashing.py
+++ selfdrive/boardd/tests/test_panda_flashing.py
@@ -5,17 +5,65 @@ import unittest
 import time
 import os
 
+from panda import BASEDIR as PANDA_BASEDIR
+
+def get_firmware_fn():
+  signed_fn = os.path.join(PANDA_BASEDIR, "board", "obj", "panda.bin.signed")
+  if os.path.exists(signed_fn):
+    return signed_fn
+  else:
+    fn = "obj/panda.bin"
+    build_st(fn, clean=False)
+    return os.path.join(PANDA_BASEDIR, "board", fn)
+
+
+def get_expected_signature():
+  try:
+    return Panda.get_signature_from_firmware(get_firmware_fn())
+  except Exception:
+    return b""
+
 class TestPandaFlashing(unittest.TestCase):
+  def setUp(self):
+    print("Setting up panda before the test")
+    panda = None
+    panda_dfu = PandaDFU.list()
+    if len(panda_dfu) > 0:
+      panda_dfu = PandaDFU(panda_dfu[0])
+      panda_dfu.recover()
+      time.sleep(1)
+
+    panda_list = Panda.list()
+    if len(panda_list) > 0:
+      panda = Panda(panda_list[0])
+    else:
+      print("Panda setup failed")
+      raise AssertionError
+
+    fw_signature = get_expected_signature()
+
+    panda_signature = b"" if panda.bootstub else panda.get_signature()
+
+    if panda.bootstub or panda_signature != fw_signature:
+      panda.flash(get_firmware_fn())
+
+    if panda.bootstub:
+      panda.recover()
+
+
   def test_panda_flashing(self):
     # Ensure we have one normal panda running
     pandas = Panda.list()
     self.assertEqual(len(pandas), 1)
     # Ensure it is running the firmware
     panda1 = Panda(pandas[0])
+    time.sleep(5)
     self.assertFalse(panda1.bootstub)
     # Move the panda into the DFU mode
+    panda1.reset(enter_bootstub=True)
     panda1.reset(enter_bootloader=True)
-    time.sleep(1)
+    print("DFU?")
+    time.sleep(5)
     # Ensure no normal pandas and one DFU panda
     self.assertEqual(len(PandaDFU.list()), 1)
     self.assertEqual(len(Panda.list()), 0)
