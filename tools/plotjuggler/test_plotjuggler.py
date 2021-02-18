579409c290fe7888e76e6a4f916e85459cc412ce
iejMac <gig13omb@gmail.com>
2021-02-18 15:01:54 -0800

    pj CI

--- tools/plotjuggler/test_plotjuggler.py
+++ tools/plotjuggler/test_plotjuggler.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import os
 import random
+import subprocess
+import time
 import unittest
+
 
 from selfdrive.test.openpilotci import get_url
 from selfdrive.test.test_car_models import routes
@@ -14,9 +17,26 @@ class TestPlotJuggler(unittest.TestCase):
 
   def test_run(self):
 
-    test_url = get_url(random.choice(routes.keys()), 0)
+    test_url = get_url(random.choice(list(routes.keys())), 0)
 
-    exit_code = os.system(f'./juggle.py "{test_url}"')
+    p = subprocess.Popen(f'./juggle.py "{test_url}"', stderr=subprocess.PIPE, shell=True)
+
+    exit_code = 1
+    state = "parsing"
+
+    while True:
+      if state == "parsing":
+        output = p.stderr.readline()
+        if output == b'Done\n':
+          state = "waiting"
+          start = time.time()
+        continue
+
+      if time.time() - start >= 15.0:
+        exit_code = 0
+        break
+
+    p.kill()
     self.assertEqual(exit_code, 0)
 
 if __name__ == "__main__":
