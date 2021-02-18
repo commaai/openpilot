2053968622cc55064c1072d76a386ed4f89ed4ee
iejMac <gig13omb@gmail.com>
2021-02-18 15:54:30 -0800

    better paths

--- tools/plotjuggler/test_plotjuggler.py
+++ tools/plotjuggler/test_plotjuggler.py
@@ -5,21 +5,21 @@ import subprocess
 import time
 import unittest
 
-
+from common.basedir import BASEDIR
 from selfdrive.test.openpilotci import get_url
 from selfdrive.test.test_car_models import routes
 
 class TestPlotJuggler(unittest.TestCase):
 
   def test_install(self):
-    exit_code = os.system("./install.sh")
+    exit_code = os.system(os.path.join(BASEDIR, "tools/plotjuggler/install.sh"))
     self.assertEqual(exit_code, 0)
 
   def test_run(self):
 
     test_url = get_url(random.choice(list(routes.keys())), 0)
 
-    p = subprocess.Popen(f'./juggle.py "{test_url}"', stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(f'{os.path.join(BASEDIR, "tools/plotjuggler/juggle.py")} "{test_url}"', stderr=subprocess.PIPE, shell=True)
 
     exit_code = 1
     state = "parsing"
