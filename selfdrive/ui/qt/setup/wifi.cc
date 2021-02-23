a6494aaac219c2d91706d09da7131b299de7dd4a
Comma Device <device@comma.ai>
2021-02-23 17:14:44 +0000

    50% done

--- selfdrive/ui/qt/setup/wifi.cc
+++ selfdrive/ui/qt/setup/wifi.cc
@@ -11,62 +11,41 @@
 #include "widgets/input_field.hpp"
 #include "qt_window.hpp"
 
-QLabel * title_label(QString text) {
-  QLabel *l = new QLabel(text);
-  l->setStyleSheet(R"(
-    font-size: 100px;
-    font-weight: 500;
-  )");
-  return l;
-}
-
-QWidget * WifiSetup::build_page(QString title, QWidget *content) {
-  QVBoxLayout *main_layout = new QVBoxLayout();
-  main_layout->setContentsMargins(50, 50, 50, 50);
-  main_layout->addWidget(title_label(title), 0, Qt::AlignLeft | Qt::AlignTop);
-
-  main_layout->addWidget(content);
-
-  QHBoxLayout *nav_layout = new QHBoxLayout();
-
-  QPushButton *finish_btn = new QPushButton("Finish");
-  nav_layout->addWidget(finish_btn, 0, Qt::AlignBottom | Qt::AlignRight);
-  QObject::connect(finish_btn, SIGNAL(released()), this, SLOT(finish()));
-
-  main_layout->addLayout(nav_layout, 0);
-
-  QWidget *widget = new QWidget();
-  widget->setLayout(main_layout);
-  return widget;
-}
-
-QWidget * WifiSetup::network_setup() {
-  Networking *wifi = new Networking(this, true);
-  return build_page("Connect to WiFi", wifi);
-}
-
 void WifiSetup::finish() {
   qApp->exit();
 }
 
 WifiSetup::WifiSetup(QWidget *parent) {
-  addWidget(network_setup());
+  QHBoxLayout *main_layout = new QHBoxLayout();
+
+  QPushButton *finish_btn = new QPushButton("Exit");
+  finish_btn->setFixedSize(400, 200);
+  main_layout->addWidget(finish_btn, 0, Qt::AlignBottom | Qt::AlignLeft);
+
+  QObject::connect(finish_btn, SIGNAL(released()), this, SLOT(finish()));
+
+  main_layout->addWidget(new Networking(this, true), 1);
+
+  setLayout(main_layout);
 
   QObject::connect(this, SIGNAL(downloadFailed()), this, SLOT(nextPage()));
 
   setStyleSheet(R"(
     * {
-      font-family: Inter;
+      background-color: #292929;
       color: white;
-      background-color: black;
-      font-size: 50px;
-      border-radius: 20px;
+    }
+    QFrame {
+      border-radius: 30px;
+      background-color: #292929;
     }
     QPushButton {
-      padding: 50px;
-      padding-right: 100px;
-      padding-left: 100px;
-      border: 7px solid white;
+      margin: 40px;
+      padding: 5px;
+      border-width: 0;
+      border-radius: 30px;
+      color: #dddddd;
+      background-color: #444444;
     }
   )");
 }
--- selfdrive/ui/qt/setup/wifi.hpp
+++ selfdrive/ui/qt/setup/wifi.hpp
@@ -2,18 +2,12 @@
 #include <QWidget>
 #include <QLineEdit>
 #include <QPushButton>
-#include <QStackedWidget>
 
-class WifiSetup : public QStackedWidget {
+class WifiSetup : public QWidget {
   Q_OBJECT
 
 public:
   explicit WifiSetup(QWidget *parent = 0);
-
-private:
-  QWidget *network_setup();
-
-  QWidget *build_page(QString title, QWidget *content);
 
 public slots:
   void finish();
