bf0d5c21409f5a6eb42ce966be94f6fbe15a6f11
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-02-24 12:09:19 -0800

    Revert "fix some refactor commits that weren't tested (#20140)"
    This reverts commit e25516e0a4ea9cfe459c400c331e1206ea3336c8.
    
    old-commit-hash: 1ef7d4a4c8aa8adb93ac64772673b0014bec0ead

--- selfdrive/ui/qt/widgets/ssh_keys.hpp
+++ selfdrive/ui/qt/widgets/ssh_keys.hpp
@@ -1,14 +1,8 @@
 #pragma once
 
-#include <QWidget>
-#include <QButtonGroup>
-#include <QVBoxLayout>
-#include <QStackedWidget>
-#include <QPushButton>
 #include <QTimer>
+#include <QWidget>
 #include <QNetworkAccessManager>
-#include <QStackedLayout>
-
 
 class SSH : public QWidget {
   Q_OBJECT
@@ -17,29 +11,22 @@ public:
   explicit SSH(QWidget* parent = 0);
 
 private:
-  QStackedLayout* slayout;
-  QString usernameGitHub;
-  QString inputFieldMessage;
   QNetworkAccessManager* manager;
   QNetworkReply* reply;
   QTimer* networkTimer;
   bool aborted;
 
+  void getSSHKeys(QString user);
+
 signals:
-  void closeSSHSettings();
-  void openKeyboard();
-  void closeKeyboard();
   void NoSSHAdded();
   void SSHAdded();
-  void inputFieldCancelled();
-  void inputFieldEmitText(QString GitHubUsername);
   void failedResponse(QString errorString);
   void gotSSHKeys();
+  void closeSSHSettings();
 
 private slots:
   void checkForSSHKey();
-  void getSSHKeys();
   void timeout();
   void parseResponse();
 };
-
