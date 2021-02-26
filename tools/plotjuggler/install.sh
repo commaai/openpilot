3dc8a313fe7460a2ac7f1e77e0b9261277917e7c
iejMac <gig13omb@gmail.com>
2021-02-26 15:10:13 -0800

    cd fix

--- tools/plotjuggler/install.sh
+++ tools/plotjuggler/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
 mkdir -p bin
+cd bin
 
-wget https://github.com/commaai/PlotJuggler/releases/download/latest/libDataLoadRlog.so.tar.gz --directory-prefix bin
-tar --directory bin -xf bin/libDataLoadRlog.so.tar.gz
-rm bin/libDataLoadRlog.so.tar.gz
+wget https://github.com/commaai/PlotJuggler/releases/download/latest/libDataLoadRlog.so.tar.gz
+tar -xf libDataLoadRlog.so.tar.gz
+rm libDataLoadRlog.so.tar.gz
 
-wget https://github.com/commaai/PlotJuggler/releases/download/latest/plotjuggler.tar.gz --directory-prefix bin
-tar --directory bin -xf bin/plotjuggler.tar.gz
-rm bin/plotjuggler.tar.gz
+wget https://github.com/commaai/PlotJuggler/releases/download/latest/plotjuggler.tar.gz
+tar -xf plotjuggler.tar.gz
+rm plotjuggler.tar.gz
