371272d94b0eac6da4309568ecb3959e74b24195
iejMac <61431446+iejMac@users.noreply.github.com>
2021-02-26 19:11:59 -0800

    PlotJuggler demo (#20172)
    * initial demo with popup bug
    
    * demo fixes
    
    * demo fixes
    
    * Update tools/plotjuggler/juggle.py
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- tools/plotjuggler/juggle.py
+++ tools/plotjuggler/juggle.py
@@ -24,7 +24,7 @@ def load_segment(segment_name):
     print(f"Error parsing {segment_name}: {e}")
     return []
 
-def juggle_file(fn, dbc=None):
+def juggle_file(fn, dbc=None, layout=None):
   env = os.environ.copy()
   env["BASEDIR"] = BASEDIR
 
@@ -32,9 +32,12 @@ def juggle_file(fn, dbc=None):
     env["DBC_NAME"] = dbc
 
   pj = os.getenv("PLOTJUGGLER_PATH", "plotjuggler")
-  subprocess.call(f'{pj} --plugin_folders {os.path.join(juggle_dir, "bin")} -d {fn}', shell=True, env=env, cwd=juggle_dir)
+  extra_args = ""
+  if layout is not None:
+    extra_args += f'-l {layout}'
+  subprocess.call(f'{pj} --plugin_folders {os.path.join(juggle_dir, "bin")} -d {fn} {extra_args}', shell=True, env=env, cwd=juggle_dir)
 
-def juggle_route(route_name, segment_number, qlog):
+def juggle_route(route_name, segment_number, qlog, layout):
 
   if route_name.startswith("http://") or route_name.startswith("https://"):
     logs = [route_name]
@@ -74,13 +77,14 @@ def juggle_route(route_name, segment_number, qlog):
   save_log(tempfile.name, all_data, compress=False)
   del all_data
 
-  juggle_file(tempfile.name, dbc)
+  juggle_file(tempfile.name, dbc, layout)
 
 def get_arg_parser():
   parser = argparse.ArgumentParser(description="PlotJuggler plugin for reading rlogs",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
   parser.add_argument("--qlog", action="store_true", help="Use qlogs")
+  parser.add_argument("--layout", nargs='?', help="Run PlotJuggler with a pre-defined layout")
   parser.add_argument("route_name", nargs='?', help="The name of the route that will be plotted.")
   parser.add_argument("segment_number", type=int, nargs='?', help="The index of the segment that will be plotted")
   return parser
@@ -91,4 +95,4 @@ if __name__ == "__main__":
     arg_parser.print_help()
     sys.exit()
   args = arg_parser.parse_args(sys.argv[1:])
-  juggle_route(args.route_name, args.segment_number, args.qlog)
+  juggle_route(args.route_name, args.segment_number, args.qlog, args.layout)
