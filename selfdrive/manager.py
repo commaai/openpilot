sshane/openpilot-archive

Updates: crash, uiview, uploader, manager, DM, updated, version

selfdrive/manager.py
@@ -11,6 +11,7 @@ import datetime
 import textwrap
 from typing import Dict, List
 from selfdrive.swaglog import cloudlog, add_logentries_handler
+from common.op_params import opParams
 
 
 from common.basedir import BASEDIR
@@ -21,6 +22,7 @@ os.environ['BASEDIR'] = BASEDIR
 
 TOTAL_SCONS_NODES = 1040
 prebuilt = os.path.exists(os.path.join(BASEDIR, 'prebuilt'))
+kill_updated = opParams().get('update_behavior').lower().strip() == 'off' or os.path.exists('/data/no_ota_updates')
 
 # Create folders needed for msgq
 try:
@@ -191,6 +193,7 @@ managed_processes = {
   "dmonitoringmodeld": ("selfdrive/modeld", ["./dmonitoringmodeld"]),
   "modeld": ("selfdrive/modeld", ["./modeld"]),
   "rtshield": "selfdrive.rtshield",
+  "lanespeedd": "selfdrive.controls.lib.lane_speed",
 }
 
 daemon_processes = {
@@ -220,11 +223,13 @@ persistent_processes = [
 
 if not PC:
   persistent_processes += [
-    'updated',
+    # 'updated',
     'logcatd',
     'tombstoned',
     'sensord',
   ]
+  if not kill_updated:
+    persistent_processes.append('updated')
 
 car_started_processes = [
   'controlsd',
@@ -237,6 +242,7 @@ car_started_processes = [
   'proclogd',
   'locationd',
   'clocksd',
+  'lanespeedd',
 ]
 
 driver_view_processes = [
@@ -482,7 +488,8 @@ def manager_thread():
     if msg.thermal.freeSpace < 0.05:
       logger_dead = True
 
-    if msg.thermal.started:
+    run_all = False
+    if msg.thermal.started or run_all:
       for p in car_started_processes:
         if p == "loggerd" and logger_dead:
           kill_managed_process(p)

commaai/openpilot

selfdrive/manager.py
@@ -11,6 +11,7 @@ import subprocess
 import textwrap
 import time
 import traceback
+from common.op_params import opParams
 
 from multiprocessing import Process
 from typing import Dict
@@ -31,6 +32,7 @@ TOTAL_SCONS_NODES = 1225
 MAX_BUILD_PROGRESS = 70
 WEBCAM = os.getenv("WEBCAM") is not None
 PREBUILT = os.path.exists(os.path.join(BASEDIR, 'prebuilt'))
+KILL_UPDATED = opParams().get('update_behavior').lower().strip() == 'off' or os.path.exists('/data/no_ota_updates')
 
 
 def unblock_stdout():
@@ -177,6 +179,7 @@ managed_processes = {
   "dmonitoringmodeld": ("selfdrive/modeld", ["./dmonitoringmodeld"]),
   "modeld": ("selfdrive/modeld", ["./modeld"]),
   "rtshield": "selfdrive.rtshield",
+  "lanespeedd": "selfdrive.controls.lib.lane_speed",
 }
 
 daemon_processes = {
@@ -208,7 +211,7 @@ persistent_processes = [
 
 if not PC:
   persistent_processes += [
-    'updated',
+    # 'updated',
     'tombstoned',
   ]
 
@@ -216,6 +219,8 @@ if EON:
   persistent_processes += [
     'sensord',
   ]
+  if not KILL_UPDATED:
+    persistent_processes.append('updated')
 
 if TICI:
   managed_processes["timezoned"] = "selfdrive.timezoned"
@@ -234,6 +239,7 @@ car_started_processes = [
   'locationd',
   'clocksd',
   'logcatd',
+  'lanespeedd',
 ]
 
 driver_view_processes = [
@@ -468,7 +474,8 @@ def manager_thread():
     if msg.deviceState.freeSpacePercent < 5:
       logger_dead = True
 
-    if msg.deviceState.started:
+    run_all = False
+    if msg.deviceState.started or run_all:
       for p in car_started_processes:
         if p == "loggerd" and logger_dead:
           kill_managed_process(p)