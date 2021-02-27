a2b2150e568305ad336201e1bfec72a4fbdff933
iejMac <gig13omb@gmail.com>
2021-02-26 16:53:12 -0800

    fixes

--- tools/plotjuggler/test_plotjuggler.py
+++ tools/plotjuggler/test_plotjuggler.py
@@ -20,15 +20,16 @@ class TestPlotJuggler(unittest.TestCase):
     test_url = get_url("ffccc77938ddbc44|2021-01-04--16-55-41", 0)
 
     # Launch PlotJuggler with the executable in the bin directory
+    os.environ["PLOTJUGGLER_PATH"] = f'{os.path.join(BASEDIR, "tools/plotjuggler/bin/plotjuggler")}'
     p = subprocess.Popen(f'QT_QPA_PLATFORM=offscreen {os.path.join(BASEDIR, "tools/plotjuggler/juggle.py")} \
     "{test_url}"', stderr=subprocess.PIPE, shell=True,
     start_new_session=True)
 
     # Wait max 60 seconds for the "Done reading Rlog data" signal from the plugin
-    output = ""
+    output = "\n"
     with Timeout(60, error_msg=output):
-      while output != b'Done reading Rlog data\n':
-        output = p.stderr.readline()
+      while output.splitlines()[-1] != "Done reading Rlog data":
+        output += p.stderr.readline().decode("utf-8")
 
     # ensure plotjuggler didn't crash after exiting the plugin
     time.sleep(15)
