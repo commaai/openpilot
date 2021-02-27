cb7d3df739496210a89fcc118710d23f31677769
Shane Smiskol <shane@smiskol.com>
2021-02-26 21:08:46 -0600

    This is renamed

--- selfdrive/debug/uiview.py
+++ selfdrive/debug/uiview.py
@@ -6,20 +6,20 @@ import subprocess
 import cereal.messaging as messaging
 from common.basedir import BASEDIR
 
-services = ['controlsState', 'thermal', 'radarState']  # the services needed to be spoofed to start ui offroad
+services = ['controlsState', 'deviceState', 'radarState']  # the services needed to be spoofed to start ui offroad
 procs = {'camerad': 'selfdrive/camerad/camerad', 'ui': 'selfdrive/ui/ui',
          'modeld': 'selfdrive/modeld/modeld', 'calibrationd': 'selfdrive/locationd/calibrationd.py'}
 started_procs = [subprocess.Popen(os.path.join(BASEDIR, procs[p]), cwd=os.path.join(BASEDIR, os.path.dirname(procs[p]))) for p in procs]  # start needed processes
 pm = messaging.PubMaster(services)
 
-dat_cs, dat_thermal, dat_radar = [messaging.new_message(s) for s in services]
+dat_cs, dat_ds, dat_radar = [messaging.new_message(s) for s in services]
 dat_cs.controlsState.rearViewCam = False  # ui checks for these two messages
-dat_thermal.thermal.started = True
+dat_ds.deviceState.started = True
 
 try:
   while True:
     pm.send('controlsState', dat_cs)
-    pm.send('thermal', dat_thermal)
+    pm.send('deviceState', dat_ds)
     pm.send('radarState', dat_radar)
     time.sleep(1 / 100)  # continually send, rate doesn't matter
 except KeyboardInterrupt:
