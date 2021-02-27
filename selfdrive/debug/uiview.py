4aa6be9b37ec18828e0f095d8814967dc1a59c67
Shane Smiskol <shane@smiskol.com>
2021-02-26 21:37:39 -0600

    Fix uiview (#336)
    * debug
    
    * debug
    
    * use stock uiview
    
    * debug
    
    * we might not need to send this
    
    * Fix script
    
    * revert

--- selfdrive/debug/uiview.py
+++ selfdrive/debug/uiview.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python3
-import os
 import time
-import signal
-import subprocess
 import cereal.messaging as messaging
-from common.basedir import BASEDIR
+from selfdrive.manager import start_managed_process, kill_managed_process
 
-services = ['controlsState', 'deviceState', 'radarState']  # the services needed to be spoofed to start ui offroad
-procs = {'camerad': 'selfdrive/camerad/camerad', 'ui': 'selfdrive/ui/ui',
-         'modeld': 'selfdrive/modeld/modeld', 'calibrationd': 'selfdrive/locationd/calibrationd.py'}
-started_procs = [subprocess.Popen(os.path.join(BASEDIR, procs[p]), cwd=os.path.join(BASEDIR, os.path.dirname(procs[p]))) for p in procs]  # start needed processes
+services = ['deviceState', 'radarState']  # the services needed to be spoofed to start ui offroad
+procs = ['camerad', 'ui', 'modeld', 'calibrationd']
+[start_managed_process(p) for p in procs]  # start needed processes
 pm = messaging.PubMaster(services)
 
-dat_cs, dat_ds, dat_radar = [messaging.new_message(s) for s in services]
-dat_cs.controlsState.rearViewCam = False  # ui checks for these two messages
-dat_ds.deviceState.started = True
+dat_deviceState, dat_radar = [messaging.new_message(s) for s in services]
+dat_deviceState.deviceState.started = True
 
 try:
   while True:
-    pm.send('controlsState', dat_cs)
-    pm.send('deviceState', dat_ds)
+    pm.send('deviceState', dat_deviceState)
     pm.send('radarState', dat_radar)
-    time.sleep(1 / 100)  # continually send, rate doesn't matter
+    time.sleep(1 / 100)  # continually send, rate doesn't matter for deviceState
 except KeyboardInterrupt:
-  [p.send_signal(signal.SIGINT) for p in started_procs]
+  [kill_managed_process(p) for p in procs]
