36ecc144ffd7456b5d4a8bae232e822b99b0bd17
iejMac <61431446+iejMac@users.noreply.github.com>
2021-02-26 17:14:37 -0800

    PlotJuggler CI with build from source (#20166)
    * CI attempt
    
    * fixing location
    
    * fix
    
    * fixes
    
    * hardcode test_url for CI
    
    * fixes
    
    * cd fix
    
    * Update tools/plotjuggler/juggle.py
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * Update tools/plotjuggler/test_plotjuggler.py
    
    * fixes
    
    * fixes
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 9144dd7c2f9c0a4d3a4b5188367b409ac3349eba

--- tools/plotjuggler/install.sh
+++ tools/plotjuggler/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 
-sudo snap install plotjuggler
+mkdir -p bin
+cd bin
+
 wget https://github.com/commaai/PlotJuggler/releases/download/latest/libDataLoadRlog.so.tar.gz
 tar -xf libDataLoadRlog.so.tar.gz
 rm libDataLoadRlog.so.tar.gz
+
+wget https://github.com/commaai/PlotJuggler/releases/download/latest/plotjuggler.tar.gz
+tar -xf plotjuggler.tar.gz
+rm plotjuggler.tar.gz
