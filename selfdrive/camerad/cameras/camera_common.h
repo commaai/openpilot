e0cfa2792f0a3e2c24e44fba67e24d3ca739722b
deanlee <deanlee3@gmail.com>
2021-02-27 12:39:20 +0800

    rebase master

--- selfdrive/camerad/cameras/camera_common.h
+++ selfdrive/camerad/cameras/camera_common.h
@@ -146,4 +146,3 @@ void cameras_init(VisionIpcServer *v, MultiCameraState *s, cl_device_id device_i
 void cameras_open(MultiCameraState *s);
 void cameras_run(MultiCameraState *s);
 void cameras_close(MultiCameraState *s);
-void camera_autoexposure(CameraState *s, float grey_frac);
