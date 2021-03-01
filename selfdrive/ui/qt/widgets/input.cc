fa0bd1990f1335f686d2ed727f3912031927820a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-02-28 20:44:00 -0800

    qt confirmation dialogs (#20202)
    * confirmation dialog widget
    
    * looks pretty good
    
    * fix qcom2
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: d8418f2abe2fba10a75238aa7692895acc5dcab5

--- selfdrive/ui/qt/widgets/input.cc
+++ selfdrive/ui/qt/widgets/input.cc
@@ -1,6 +1,6 @@
 #include <QPushButton>
 
-#include "input_field.hpp"
+#include "input.hpp"
 #include "qt_window.hpp"
 
 InputDialog::InputDialog(QString prompt_text, QWidget *parent):QDialog(parent) {
@@ -60,11 +60,7 @@ QString InputDialog::getText(const QString prompt, int minLength) {
   InputDialog d = InputDialog(prompt);
   d.setMinLength(minLength);
   const int ret = d.exec();
-  if (ret) {
-    return d.text();
-  } else {
-    return QString();
-  }
+  return ret ? d.text() : QString();
 }
 
 QString InputDialog::text() {
@@ -100,10 +96,6 @@ void InputDialog::handleInput(QString s) {
   line->insert(s.left(1));
 }
 
-void InputDialog::show(){
-  setMainWindow(this);
-}
-
 void InputDialog::setMessage(QString message, bool clearInputField){
   label->setText(message);
   if (clearInputField){
@@ -113,4 +105,63 @@ void InputDialog::setMessage(QString message, bool clearInputField){
 
 void InputDialog::setMinLength(int length){
   minLength = length;
+}
+
+
+
+ConfirmationDialog::ConfirmationDialog(QString prompt_text, QString confirm_text, QString cancel_text,
+                                       QWidget *parent):QDialog(parent) {
+  layout = new QVBoxLayout();
+  layout->setMargin(25);
+
+  prompt = new QLabel(prompt_text, this);
+  prompt->setWordWrap(true);
+  prompt->setAlignment(Qt::AlignHCenter);
+  prompt->setStyleSheet(R"(font-size: 55px; font-weight: 400;)");
+  layout->addWidget(prompt, 1, Qt::AlignTop | Qt::AlignHCenter);
+
+  // cancel + confirm buttons
+  QHBoxLayout *btn_layout = new QHBoxLayout();
+  btn_layout->setSpacing(20);
+  btn_layout->addStretch(1);
+  layout->addLayout(btn_layout);
+
+  QPushButton* cancel_btn = new QPushButton(cancel_text);
+  btn_layout->addWidget(cancel_btn, 0, Qt::AlignRight);
+  QObject::connect(cancel_btn, SIGNAL(released()), this, SLOT(reject()));
+
+  QPushButton* confirm_btn = new QPushButton(confirm_text);
+  btn_layout->addWidget(confirm_btn, 0, Qt::AlignRight);
+  QObject::connect(confirm_btn, SIGNAL(released()), this, SLOT(accept()));
+
+  setFixedSize(900, 350);
+  setStyleSheet(R"(
+    * {
+      color: black;
+      background-color: white;
+    }
+    QPushButton {
+      font-size: 40px;
+      padding: 30px;
+      padding-right: 45px;
+      padding-left: 45px;
+      border-radius: 7px;
+      background-color: #44444400;
+    }
+  )");
+
+  setLayout(layout);
+}
+
+bool ConfirmationDialog::confirm(const QString prompt_text) {
+  ConfirmationDialog d = ConfirmationDialog(prompt_text);
+  return d.exec();
+}
+
+int ConfirmationDialog::exec() {
+   // TODO: make this work without fullscreen
+#ifdef QCOM2
+  setMainWindow(this);
+#endif
+  return QDialog::exec();
 }
