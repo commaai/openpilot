bc80e57761f3ad511f6ef45d8259f15ef05ba433
deanlee <deanlee3@gmail.com>
2021-03-05 15:26:46 +0800

    struct SensorState.remove hardcoded sensor handle

--- selfdrive/sensord/sensors_qcom.cc
+++ selfdrive/sensord/sensors_qcom.cc
@@ -24,16 +24,23 @@
 // ACCELEROMETER_UNCALIBRATED is only in Android O
 // https://developer.android.com/reference/android/hardware/Sensor.html#STRING_TYPE_ACCELEROMETER_UNCALIBRATED
 
-#define SENSOR_ACCELEROMETER 1
-#define SENSOR_MAGNETOMETER 2
-#define SENSOR_GYRO 4
-#define SENSOR_MAGNETOMETER_UNCALIBRATED 3
-#define SENSOR_GYRO_UNCALIBRATED 5
-#define SENSOR_PROXIMITY 6
-#define SENSOR_LIGHT 7
-
 ExitHandler do_exit;
 volatile sig_atomic_t re_init_sensors = 0;
+
+typedef struct SensorState {
+  int handle;
+  bool offroad;
+  nsecs_t delay;
+} SensorState;
+
+static std::map<int, SensorState> sensors = {
+    {SENSOR_TYPE_GYROSCOPE, {.delay = ms2ns(10)}},
+    {SENSOR_TYPE_MAGNETIC_FIELD, {.delay = ms2ns(100)}},
+    {SENSOR_TYPE_MAGNETIC_FIELD_UNCALIBRATED, {.delay = ms2ns(100)}},
+    {SENSOR_TYPE_PROXIMITY, {.delay = ms2ns(100)}},
+    {SENSOR_TYPE_GYROSCOPE_UNCALIBRATED, {.delay = ms2ns(10), .offroad = true}},
+    {SENSOR_TYPE_ACCELEROMETER, {.delay = ms2ns(10), .offroad = true}},
+    {SENSOR_TYPE_LIGHT, {.delay = ms2ns(100), .offroad = true}}};
 
 namespace {
 
@@ -67,47 +74,31 @@ void sensor_loop() {
       exit(count);
     }
 
+    // init sensors
     for (int i = 0; i < count; i++) {
-      LOGD("sensor %4d: %4d %60s  %d-%ld us", i, list[i].handle, list[i].name, list[i].minDelay, list[i].maxDelay);
-      device->activate(device, list[i].handle, 0);
-    }
-
-    const std::map<int, int64_t> sensors = {
-      {SENSOR_GYRO_UNCALIBRATED, ms2ns(10)},
-      {SENSOR_MAGNETOMETER_UNCALIBRATED, ms2ns(100)},
-      {SENSOR_ACCELEROMETER, ms2ns(10)},
-      {SENSOR_GYRO, ms2ns(10)},
-      {SENSOR_MAGNETOMETER, ms2ns(100)},
-      {SENSOR_PROXIMITY, ms2ns(100)},
-      {SENSOR_LIGHT, ms2ns(100)}
-    };
-
-    // sensors needed while offroad
-    const std::set<int> offroad_sensors = {
-      SENSOR_LIGHT,
-      SENSOR_ACCELEROMETER,
-      SENSOR_GYRO_UNCALIBRATED,
-    };
-
-    // init all the sensors
-    for (auto &s : sensors) {
-      device->activate(device, s.first, 1);
-      device->setDelay(device, s.first, s.second);
+      auto &s = list[i];
+      LOGW("sensor %4d: %4d %60s  %d-%ld us", s.type, s.handle, s.name, s.minDelay, s.maxDelay);
+      // LOG("sensor %4d: %4d %60s  %d-%ld us", i, list[i].handle, list[i].name, list[i].minDelay, list[i].maxDelay);
+      device->activate(device, s.handle, 0);
+      auto it = sensors.find(s.type);
+      if(it != sensors.end() && it->second.handle == 0) {
+        it->second.handle = s.handle;
+        device->activate(device, it->second.handle, 1);
+        device->setDelay(device, it->second.handle, it->second.delay);
+      }
     }
 
     sensors_event_t buffer[sensors.size()];
 
     while (!do_exit) {
       int n = device->poll(device, buffer, sensors.size());
-      if (n == 0) continue;
-      if (n < 0) {
+      if (n <= 0) {
         LOG("sensor_loop poll failed: %d", n);
         continue;
       }
 
       MessageBuilder msg;
       auto sensor_events = msg.initEvent().initSensorEvents(n);
-
       for (int i = 0; i < n; i++) {
         const sensors_event_t& data = buffer[i];
 
@@ -175,10 +166,10 @@ void sensor_loop() {
       if (frame % 20 == 0 && sm.update(0) > 0) {
         bool offroad = !sm["deviceState"].getDeviceState().getStarted();
         if (low_power_mode != offroad) {
-          for (auto &s : sensors) {
-            device->activate(device, s.first, 0);
-            if (!offroad || offroad_sensors.find(s.first) != offroad_sensors.end()) {
-              device->activate(device, s.first, 1);
+          for (auto &[type, s] : sensors) {
+            device->activate(device, s.handle, 0);
+            if (!offroad || s.offroad) {
+              device->activate(device, s.handle, 1);
             }
           }
           low_power_mode = offroad;
