bfc70ce690d25c40a737f4920300de579443f9b0
iejMac <gig13omb@gmail.com>
2021-03-05 12:26:38 -0800

    this ACTUALLY works

--- tools/sim/start_openpilot_docker.sh
+++ tools/sim/start_openpilot_docker.sh
@@ -9,11 +9,11 @@ docker run --net=host\
   -it \
   --privileged \
   --gpus all \
-  --device=/dev/dri/  \
+  --device=/dev/dri \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /run/user/1000:/run/user/1000 \
   --shm-size 1G \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   commaai/openpilot-sim:latest \
-  /bin/bash -c "cd tools && cd sim && sh tmux_script.sh $*"
+  /bin/bash -c "cd /openpilot/tools && cd sim && sh tmux_script.sh $*"
