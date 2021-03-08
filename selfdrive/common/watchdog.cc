8903f5e988b2952045976f937a12de4118ee3bfe
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-03-08 13:42:11 -0800

    cleanup

--- selfdrive/common/watchdog.cc
+++ selfdrive/common/watchdog.cc
@@ -2,16 +2,15 @@
 #include <cstdint>
 #include <unistd.h>
 
-#include "common/watchdog.h"
-#include "common/util.h"
 #include "common/timing.h"
+#include "common/watchdog.h"
 
-const std::string watchdog_fn = "/dev/shm/wd_";  // + <pid>
+const std::string watchdog_fn_prefix = "/dev/shm/wd_";  // + <pid>
 
 bool watchdog_kick(){
-    std::string fn = watchdog_fn + std::to_string(getpid());
-    std::string cur_t = std::to_string(nanos_since_boot());
+  std::string fn = watchdog_fn_prefix + std::to_string(getpid());
+  std::string cur_t = std::to_string(nanos_since_boot());
 
-    int r = write_file(fn.c_str(), cur_t.data(), cur_t.length(), O_WRONLY | O_CREAT);
-    return r == 0;
+  int r = write_file(fn.c_str(), cur_t.data(), cur_t.length(), O_WRONLY | O_CREAT);
+  return r == 0;
 }
