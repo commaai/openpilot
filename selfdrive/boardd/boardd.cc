151fcfea0f30d201c7d4c7d32b8c551c21c6cb70
Willem Melching <willem.melching@gmail.com>
2021-03-08 17:46:59 +0100

    Add 10 seconds

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -531,7 +531,7 @@ void pigeon_thread() {
 
       // Set receive times to current time
       last_recv_time.clear();
-      uint64_t t = nanos_since_boot();
+      uint64_t t = nanos_since_boot() + 10000000000ULL; // Give ublox 10 seconds to start
       for (const auto& [msg_cls, dt] : cls_max_dt) {
         last_recv_time[msg_cls] = t;
       }
