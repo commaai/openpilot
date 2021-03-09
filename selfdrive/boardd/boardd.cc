4197ead74267eb19ad0d2e00f6fbebffc011fa23
Willem Melching <willem.melching@gmail.com>
2021-03-09 17:28:19 +0100

    boardd: increase CLASS_RXM timeout
    old-commit-hash: 6884d7c9bd36aabb57c9f8d3a34d407031867b05

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -486,8 +486,8 @@ void pigeon_thread() {
 
   std::unordered_map<char, uint64_t> last_recv_time;
   std::unordered_map<char, int64_t> cls_max_dt = {
-    {(char)ublox::CLASS_NAV, int64_t(500000000ULL)}, // 0.5s - msg is 10Hz
-    {(char)ublox::CLASS_RXM, int64_t(500000000ULL)}, // 0.5s - msg is 10Hz
+    {(char)ublox::CLASS_NAV, int64_t(500000000ULL)}, // 0.5s
+    {(char)ublox::CLASS_RXM, int64_t(1000000000ULL)}, // 1.0s
   };
 
   while (!do_exit && panda->connected) {
