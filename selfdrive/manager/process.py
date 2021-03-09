0ccc4448e1e98c4429d4ba6330d042f8e72a0ac6
Willem Melching <willem.melching@gmail.com>
2021-03-09 13:11:40 +0100

    manager watchdog, log exit code
    old-commit-hash: 958e7b0071c3333f0f9d668303d1f93915727e7a

--- selfdrive/manager/process.py
+++ selfdrive/manager/process.py
@@ -96,7 +96,7 @@ class ManagerProcess(ABC):
     if dt > self.watchdog_max_dt:
       # Only restart while offroad for now
       if self.watchdog_seen and ENABLE_WATCHDOG and (not started):
-        cloudlog.error(f"Watchdog timeout for {self.name}, restarting")
+        cloudlog.error(f"Watchdog timeout for {self.name} (exitcode {self.proc.exitcode}) restarting")
         self.restart()
     else:
       self.watchdog_seen = True
