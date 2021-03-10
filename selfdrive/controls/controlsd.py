6d89cec36a99067f076fedf3e1547c9f018bb219
Willem Melching <willem.melching@gmail.com>
2021-03-10 11:13:46 +0100

    turn off gpsMalfunction alert until uBlox config is fixed
    old-commit-hash: af02811d42e66f03613b5a49d6fb837eca19ce6c

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -235,9 +235,7 @@ class Controls:
     # TODO: fix simulator
     if not SIMULATION:
       if not NOSENSOR:
-        if not self.sm.alive['ubloxRaw'] and (self.sm.frame > 10. / DT_CTRL):
-          self.events.add(EventName.gpsMalfunction)
-        elif not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000) and not TICI:
+        if not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000) and not TICI:
           # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
           self.events.add(EventName.noGps)
       if not self.sm.all_alive(['roadCameraState', 'driverCameraState']) and (self.sm.frame > 5 / DT_CTRL):
