7670528be8da9ea16947b9e0d806e8ecc88a734c
Greg Hogan <gregjhogan@gmail.com>
2021-03-09 17:56:23 -0800

    better way to set pipfile
    old-commit-hash: 2fe5eff88c66caa22f615fff2919356b3178a30e

--- update_requirements.sh
+++ update_requirements.sh
@@ -32,7 +32,7 @@ pip install --upgrade pip
 pip install pipenv
 
 echo "pip packages install ..."
-[ -d "./xx" ] && cd ./xx
+[ -d "./xx" ] && export PIPENV_PIPFILE=./xx/Pipfile
 pipenv install --dev --deploy --system
 # update shims for newly installed executables (e.g. scons)
 pyenv rehash
