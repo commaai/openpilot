65bb979c34f3941044eb6a78f94037b2d4321418
ZwX1616 <zwx1616@gmail.com>
2021-03-12 17:40:50 -0800

    set_exposure_target test (#20318)
    * build
    
    * remove junk
    
    * clean up
    
    * clean up rebase
    
    * new patterns
    
    * add gts
    
    * add to jenkis
    
    * this more useful
    
    * typo
    
    * test only
    
    * Update Jenkinsfile
    
    * test flag
    
    * remove from jenkins
    
    * these should all just be common:wq
    
    * oops
    
    * unigt
    
    * add to unit tests?
    
    * build all is fine
    
    Co-authored-by: Comma Device <device@comma.ai>

--- selfdrive/camerad/cameras/camera_common.h
+++ selfdrive/camerad/cameras/camera_common.h
@@ -35,6 +35,10 @@
 #define LOG_CAMERA_ID_QCAMERA 3
 #define LOG_CAMERA_ID_MAX 4
 
+#define HLC_THRESH 222
+#define HLC_A 80
+#define HISTO_CEIL_K 5
+
 const bool env_send_driver = getenv("SEND_DRIVER") != NULL;
 const bool env_send_road = getenv("SEND_ROAD") != NULL;
 const bool env_send_wide_road = getenv("SEND_WIDE_ROAD") != NULL;
@@ -96,7 +100,7 @@ private:
 
   FrameMetadata yuv_metas[YUV_COUNT];
   VisionStreamType rgb_type, yuv_type;
-  
+
   int cur_buf_idx;
 
   SafeQueue<int> safe_queue;
@@ -112,7 +116,7 @@ public:
   std::unique_ptr<VisionBuf[]> camera_bufs;
   std::unique_ptr<FrameMetadata[]> camera_bufs_metadata;
   int rgb_width, rgb_height, rgb_stride;
-  
+
   mat3 yuv_transform;
 
   CameraBuf() = default;
@@ -127,7 +131,7 @@ typedef void (*process_thread_cb)(MultiCameraState *s, CameraState *c, int cnt);
 
 void fill_frame_data(cereal::FrameData::Builder &framed, const FrameMetadata &frame_data);
 kj::Array<uint8_t> get_frame_image(const CameraBuf *b);
-void set_exposure_target(CameraState *c, int x_start, int x_end, int x_skip, int y_start, int y_end, int y_skip);
+float set_exposure_target(const CameraBuf *b, int x_start, int x_end, int x_skip, int y_start, int y_end, int y_skip, int analog_gain, bool hist_ceil, bool hl_weighted);
 std::thread start_process_thread(MultiCameraState *cameras, CameraState *cs, process_thread_cb callback);
 void common_process_driver_camera(SubMaster *sm, PubMaster *pm, CameraState *c, int cnt);
 
