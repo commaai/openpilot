9039b1a1b3829c1e7778a9a92572e9af58bf8343
iejMac <gig13omb@gmail.com>
2021-03-12 21:44:21 -0800

    initial test

--- tools/sim/test_simulator.py
+++ tools/sim/test_simulator.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
 import os
-# import signal
-# import subprocess
-# import time
+import signal
+import subprocess
+import time
 import unittest
 
+import cereal.messaging as messaging
 from common.basedir import BASEDIR
-# from common.timeout import Timeout
+from common.timeout import Timeout
 
 class TestSimulator(unittest.TestCase):
 
-  def test_install(self):
-    exit_code = os.system(os.path.join(BASEDIR, "tools/sim/install_carla.sh"))
-    self.assertEqual(exit_code, 0)
+  def test_run(self):
 
-  # def test_run(self):
+    p_bridge = subprocess.Popen(f'{os.path.join(BASEDIR, "tools/sim/bridge.py")} --low_quality', shell=True, start_new_session=True)
+    p_openpilot = subprocess.Popen(f'QT_QPA_PLATFORM=offscreen {os.path.join(BASEDIR, "tools/sim/launch_openpilot.sh")}', shell=True, start_new_session=True)
 
-  #   # p_server = subprocess.Popen(f'{os.path.join(BASEDIR, "tools/sim/start_carla.sh")}', stderr=subprocess.PIPE, shell=True, start_new_session=True)
-  #   p_openpilot = subprocess.Popen(f'{os.path.join(BASEDIR, "tools/sim/start_openpilot_docker.sh")}')
+    sm = messaging.SubMaster(["controlsState"])
 
-  #   with Timeout(60):
-  #     while 1:
-  #       out = p_server.stderr.readline().decode("utf-8")
-  #       if out != '':
-  #         print(out)
+    with Timeout(60):
+      while sm['controlsState'].upAccelCmd == 0.0:
+        sm.update(0)
 
+    time.sleep(10)
 
-  #   os.killpg(os.getpgid(p_server.pid), signal.SIGTERM)
+    os.killpg(os.getpgid(p_bridge.pid), signal.SIGTERM)
+    os.killpg(os.getpgid(p_openpilot.pid), signal.SIGTERM)
 
 if __name__ == "__main__":
   unittest.main()
