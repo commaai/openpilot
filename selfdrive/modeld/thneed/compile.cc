1b29eda2f8460a234136b873da61c6dd1210ba82
deanlee <deanlee3@gmail.com>
2021-03-13 22:44:55 +0800

    use std::unique_ptr

--- selfdrive/modeld/thneed/compile.cc
+++ selfdrive/modeld/thneed/compile.cc
@@ -9,13 +9,13 @@
 // TODO: This should probably use SNPE directly.
 int main(int argc, char* argv[]) {
   #define OUTPUT_SIZE 0x10000
-  float *output = (float*)calloc(OUTPUT_SIZE, sizeof(float));
-  SNPEModel mdl(argv[1], output, 0, USE_GPU_RUNTIME);
+  std::unique_ptr<float[]> output = std::make_unique<float[]>(OUTPUT_SIZE);
+  SNPEModel mdl(argv[1], output.get(), 0, USE_GPU_RUNTIME);
 
   float state[TEMPORAL_SIZE] = {0};
   float desire[DESIRE_LEN] = {0};
   float traffic_convention[TRAFFIC_CONVENTION_LEN] = {0};
-  float *input = (float*)calloc(0x1000000, sizeof(float));;
+  std::unique_ptr<float[]> input = std::make_unique<float []>(0x1000000);
 
   mdl.addRecurrent(state, TEMPORAL_SIZE);
   mdl.addDesire(desire, DESIRE_LEN);
@@ -23,12 +23,11 @@ int main(int argc, char* argv[]) {
 
   // first run
   printf("************** execute 1 **************\n");
-  memset(output, 0, OUTPUT_SIZE * sizeof(float));
-  mdl.execute(input, 0);
+  memset(output.get(), 0, OUTPUT_SIZE * sizeof(float));
+  mdl.execute(input.get(), 0);
 
   // save model
   bool save_binaries = (argc > 3) && (strcmp(argv[3], "--binary") == 0);
   mdl.thneed->save(argv[2], save_binaries);
   return 0;
 }
-
