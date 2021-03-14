dfb437648ef6304cbe1c9b40b7871ce43b2aefd4
wolterhv <wolterhv@gmx.de>
2021-03-14 19:59:05 +0000

    Specify comma containers come from ghcr.io in docker pull commands. (#20342)
    old-commit-hash: 5463469f71e7861ccfbbd4d09b8e4ae56b8d3e45

--- tools/sim/build_container.sh
+++ tools/sim/build_container.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../../
 
-docker pull commaai/openpilot-base:latest
+docker pull ghcr.io/commaai/openpilot-base:latest
 docker build \
   --cache-from commaai/openpilot-sim:latest \
   -t commaai/openpilot-sim:latest \
