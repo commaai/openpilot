6e95b6fb70d62c6ce51f8bd9b9d1ddce7ce6b35f
iejMac <gig13omb@gmail.com>
2021-03-16 22:09:45 -0700

    works

--- tools/sim/start_openpilot_docker.sh
+++ tools/sim/start_openpilot_docker.sh
@@ -5,32 +5,25 @@ xhost +local:root
 
 docker pull ghcr.io/commaai/openpilot-sim:latest
 
-if [[ -z "$MOUNT_OPENPILOT" ]]; then
-  docker run --net=host\
-    --name openpilot_client \
-    --rm \
-    -it \
-    --gpus all \
-    --device=/dev/dri \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --shm-size 1G \
-    -e DISPLAY=$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    commaai/openpilot-sim:latest \
-    /bin/bash -c "cd /openpilot/tools/sim && ./tmux_script.sh $*"
-else
-  docker run --net=host\
-    --name openpilot_client \
-    --rm \
-    -it \
-    --gpus all \
-    --device=/dev/dri \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $HOME/openpilot:/root/openpilot \
-    --shm-size 1G \
-    -e DISPLAY=$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    -e PYTHONPATH=/root/openpilot:$PYTHONPATH \
-    commaai/openpilot-sim:latest \
-    /bin/bash -c "cd /root/openpilot/tools/sim && ./tmux_script.sh $*"
+export EXTRA_ARGS=""
+export OPENPILOT_DIR="/openpilot"
+if ! [[ -z "$MOUNT_OPENPILOT" ]]
+then
+  export EXTRA_ARGS="-v $HOME/openpilot:/root/openpilot -e PYTHONPATH=/root/openpilot:$PYTHONPATH"
+  export OPENPILOT_DIR="/root/openpilot"
 fi
+
+docker run --net=host\
+  --name openpilot_client \
+  --rm \
+  -it \
+  --gpus all \
+  --device=/dev/dri \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --shm-size 1G \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -w "$OPENPILOT_DIR/tools/sim" \
+  $EXTRA_ARGS \
+  commaai/openpilot-sim:latest \
+  /bin/bash -c "./tmux_script.sh $*"
