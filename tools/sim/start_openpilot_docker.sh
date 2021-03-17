8fbf48fbba0af061afeacc419780a620ce537087
iejMac <gig13omb@gmail.com>
2021-03-16 22:22:12 -0700

    fixes

--- tools/sim/start_openpilot_docker.sh
+++ tools/sim/start_openpilot_docker.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+cd $DIR
+
 # expose X to the container
 xhost +local:root
 
 docker pull ghcr.io/commaai/openpilot-sim:latest
 
-export EXTRA_ARGS=""
-export OPENPILOT_DIR="/openpilot"
+OPENPILOT_DIR="/openpilot"
 if ! [[ -z "$MOUNT_OPENPILOT" ]]
 then
-  export EXTRA_ARGS="-v $HOME/openpilot:/root/openpilot -e PYTHONPATH=/root/openpilot:$PYTHONPATH"
-  export OPENPILOT_DIR="/root/openpilot"
+  EXTRA_ARGS="-v $PWD/../..:/root/openpilot -e PYTHONPATH=/root/openpilot:$PYTHONPATH"
+  OPENPILOT_DIR="/root/openpilot"
 fi
 
 docker run --net=host\
