21cae2c939a4ba91b0bbadb5bdad041b99552052
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-03-22 17:42:44 -0700

    for all hw

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -279,14 +279,7 @@ void GLWindow::timerUpdate() {
   handle_display_state(&ui_state, false);
 
   // scale volume with speed
-#ifdef QCOM
-  const float vol_max = 1.0;
-  const float vol_min = 0.5;
-#else
-  const float vol_max = 0.45;
-  const float vol_min = 0.45;
-#endif
-  util::map_val(ui_state.scene.car_state.getVEgo(), 0.f, 20.f, vol_min, vol_max);
+  util::map_val(ui_state.scene.car_state.getVEgo(), 0.f, 20.f, Hardware::MIN_VOLUME, Hardware::MAX_VOLUME);
 
   ui_update(&ui_state);
   repaint();
