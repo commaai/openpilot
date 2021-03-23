6c40a25e2685e78d9d824e79ebee14143206f1af
Comma Device <device@comma.ai>
2021-03-22 22:41:00 -0700

    cleanup

--- selfdrive/ui/qt/widgets/offroad_alerts.cc
+++ selfdrive/ui/qt/widgets/offroad_alerts.cc
@@ -44,6 +44,7 @@ OffroadAlert::OffroadAlert(QWidget* parent) : QFrame(parent) {
   setLayout(main_layout);
   setStyleSheet(R"(
     * {
+      font-size: 48px;
       color: white;
     }
     QFrame {
@@ -52,7 +53,6 @@ OffroadAlert::OffroadAlert(QWidget* parent) : QFrame(parent) {
     }
     QPushButton {
       color: black;
-      font-size: 50px;
       font-weight: 500;
       border-radius: 30px;
       background-color: white;
@@ -72,37 +72,26 @@ void OffroadAlert::refresh() {
   parse_alerts();
   cleanStackedWidget(alerts_stack);
 
-  std::vector<char> bytes = Params().read_db_bytes("UpdateAvailable");
-  updateAvailable = bytes.size() && bytes[0] == '1';
-
+  updateAvailable = Params().read_db_bool("UpdateAvailable");
   reboot_btn->setVisible(updateAvailable);
 
   QVBoxLayout *layout = new QVBoxLayout;
+  layout->setSpacing(20);
 
   if (updateAvailable) {
-    QString release_notes = QString::fromStdString(Params().get("ReleaseNotes"));
-    QLabel *body = new QLabel(release_notes);
-    body->setStyleSheet(R"(
-      font-size: 48px;
-    )");
+    QLabel *body = new QLabel(QString::fromStdString(Params().get("ReleaseNotes")));
+    body->setStyleSheet(R"(font-size: 48px;)");
     layout->addWidget(body, 0, Qt::AlignLeft | Qt::AlignTop);
   } else {
-    // TODO: paginate the alerts
     for (const auto &alert : alerts) {
       QLabel *l = new QLabel(alert.text);
-      l->setWordWrap(true);
       l->setMargin(60);
-
-      QString style = R"(
-        font-size: 48px;
-      )";
-      style.append("background-color: " + QString(alert.severity ? "#E22C2C" : "#292929"));
-      l->setStyleSheet(style);
-
+      l->setWordWrap(true);
+      l->setStyleSheet("background-color: " + QString(alert.severity ? "#E22C2C" : "#292929"));
       layout->addWidget(l, 0, Qt::AlignTop);
     }
-    layout->setSpacing(20);
   }
+
   QWidget *w = new QWidget();
   w->setLayout(layout);
   alerts_stack->addWidget(w);
@@ -112,7 +101,6 @@ void OffroadAlert::parse_alerts() {
   alerts.clear();
   for (const QString &key : alert_keys) {
     std::vector<char> bytes = Params().read_db_bytes(key.toStdString().c_str());
-
     if (bytes.size()) {
       QJsonDocument doc_par = QJsonDocument::fromJson(QByteArray(bytes.data(), bytes.size()));
       QJsonObject obj = doc_par.object();
