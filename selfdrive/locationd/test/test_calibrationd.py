d5657234450232325fcd80d0f699b97055008b5c
deanlee <deanlee3@gmail.com>
2021-03-25 03:54:42 +0800

    remove test_read_saved_params

--- selfdrive/locationd/test/test_calibrationd.py
+++ selfdrive/locationd/test/test_calibrationd.py
@@ -21,16 +21,6 @@ class TestCalibrationd(unittest.TestCase):
     self.assertEqual(r, c.rpy)
     self.assertEqual(b, c.valid_blocks)
 
-  def test_read_saved_params(self):
-    msg = messaging.new_message('liveCalibration')
-    msg.liveCalibration.validBlocks = random.randint(1, 10)
-    msg.liveCalibration.rpyCalib = [random.random() for _ in range(3)]
-    Params().put("CalibrationParams", msg.to_bytes())
-    c = Calibrator(param_put=True)
-
-    self.assertEqual(list(msg.liveCalibration.rpyCalib), c.rpy)
-    self.assertEqual(msg.liveCalibration.validBlocks, c.valid_blocks)
-
 
 if __name__ == "__main__":
   unittest.main()
