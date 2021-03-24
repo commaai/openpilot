44a6117d7baa98bf8e7564c51a7812098ce952e0
speedking456 <johnbarrett6@icloud.com>
2021-03-24 12:49:40 -0700

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -147,7 +147,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1337. + STD_CARGO_KG
       ret.wheelbase = 2.63
-      ret.steerRatio = 14
+      ret.steerRatio = 14.4
       tire_stiffness_factor = 0.5
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
