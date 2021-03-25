53e3ba53f2ca1b0b163e9768f80aa4942746b6e1
Greg Hogan <gregjhogan@gmail.com>
2021-03-24 22:25:39 -0700

    comment about special log msg attr names

--- common/logging_extra.py
+++ common/logging_extra.py
@@ -69,6 +69,11 @@ class SwagFormatter(logging.Formatter):
 
 class SwagLogFileFormatter(SwagFormatter):
   def fix_kv(self, k, v):
+    # append type to names to preserve legacy naming in logs
+    # avoids overlapping key namespaces with different types
+    # e.g. log.info() creates 'msg' -> 'msg$s'
+    #      log.event() creates 'msg.health.logMonoTime' -> 'msg.health.logMonoTime$i'
+    #      because overlapping namespace 'msg' caused problems
     if isinstance(v, (str, bytes)):
       k += "$s"
     elif isinstance(v, float):
