959734271c056ecbcae120544251f966c35f7aee
Harald Schafer <harald.the.engineer@gmail.com>
2021-03-25 13:20:13 -0700

    new description

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -58,7 +58,7 @@ QWidget * toggles_panel() {
   toggles_list->addWidget(record_toggle);
   toggles_list->addWidget(horizontal_line());
   toggles_list->addWidget(new ParamControl("EndToEndToggle",
-                                           "Ignore lanelines (Experimental)",
+                                           "Disable use of lanelines (Alpha)",
                                            "In this mode openpilot will ignore lanelines and just drive how it thinks a human would.",
                                            "../assets/offroad/icon_road.png"));
 
