f811dcc0cb4881fc6b1139d27a0fc15141c28f69
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-03-26 01:03:42 -0700

    always qt

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -74,11 +74,7 @@ typedef enum UIStatus {
 } UIStatus;
 
 static std::map<UIStatus, NVGcolor> bg_colors = {
-#ifndef QT_GUI_LIB
-  {STATUS_OFFROAD, nvgRGBA(0x07, 0x23, 0x39, 0xf1)},
-#else
   {STATUS_OFFROAD, nvgRGBA(0x0, 0x0, 0x0, 0xff)},
-#endif
   {STATUS_DISENGAGED, nvgRGBA(0x17, 0x33, 0x49, 0xc8)},
   {STATUS_ENGAGED, nvgRGBA(0x17, 0x86, 0x44, 0xf1)},
   {STATUS_WARNING, nvgRGBA(0xDA, 0x6F, 0x25, 0xf1)},
