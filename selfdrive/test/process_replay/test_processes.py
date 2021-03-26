03e0f4f1e3cbbd7aa259ef8a3b07dd80a60c4635
Jason Young <jyoung8607@gmail.com>
2021-03-25 23:48:02 -0400

    Moving test route to correct place

--- selfdrive/test/process_replay/test_processes.py
+++ selfdrive/test/process_replay/test_processes.py
@@ -12,17 +12,16 @@ from tools.lib.logreader import LogReader
 INJECT_MODEL = 0
 
 segments = [
-  ("HYUNDAI", "02c45f73a2e5c6e9|2021-01-01--19-08-22--1"),      # HYUNDAI.SONATA
-  ("TOYOTA", "0982d79ebb0de295|2021-01-04--17-13-21--13"),      # TOYOTA.PRIUS (INDI)
-  ("TOYOTA2", "0982d79ebb0de295|2021-01-03--20-03-36--6"),      # TOYOTA.RAV4  (LQR)
-  ("HONDA", "0982d79ebb0de295|2021-01-08--10-13-10--6"),        # HONDA.CIVIC (NIDEC)
-  ("HONDA2", "a8e8bf6a3864361b|2021-01-04--03-01-18--2"),       # HONDA.ACCORD (BOSCH)
-  ("CHRYSLER", "52d86230ee29aa84|2021-01-10--17-16-34--30"),    # CHRYSLER.PACIFICA
-  ("SUBARU", "4d70bc5e608678be|2021-01-15--17-02-04--5"),       # SUBARU.IMPREZA
-  ("GM", "ae3ed0eb20960a20|2021-01-15--15-04-06--8"),           # GM.VOLT
-  ("NISSAN", "e4d79cf6b8b19a0d|2021-01-17--14-48-08--7"),       # NISSAN.XTRAIL
-  ("VOLKSWAGEN", "e2a273d7e6eecec2|2021-03-03--16-05-26--4"),   # VW.AUDI_A3
-  ("VOLKSWAGEN2", "2cef8a0b898f331a|2021-03-25--20-13-57--3"),  # VW.TIGUAN_MK2
+  ("HYUNDAI", "02c45f73a2e5c6e9|2021-01-01--19-08-22--1"),     # HYUNDAI.SONATA
+  ("TOYOTA", "0982d79ebb0de295|2021-01-04--17-13-21--13"),     # TOYOTA.PRIUS (INDI)
+  ("TOYOTA2", "0982d79ebb0de295|2021-01-03--20-03-36--6"),     # TOYOTA.RAV4  (LQR)
+  ("HONDA", "0982d79ebb0de295|2021-01-08--10-13-10--6"),       # HONDA.CIVIC (NIDEC)
+  ("HONDA2", "a8e8bf6a3864361b|2021-01-04--03-01-18--2"),      # HONDA.ACCORD (BOSCH)
+  ("CHRYSLER", "52d86230ee29aa84|2021-01-10--17-16-34--30"),   # CHRYSLER.PACIFICA
+  ("SUBARU", "4d70bc5e608678be|2021-01-15--17-02-04--5"),      # SUBARU.IMPREZA
+  ("GM", "ae3ed0eb20960a20|2021-01-15--15-04-06--8"),          # GM.VOLT
+  ("NISSAN", "e4d79cf6b8b19a0d|2021-01-17--14-48-08--7"),      # NISSAN.XTRAIL
+  ("VOLKSWAGEN", "e2a273d7e6eecec2|2021-03-03--16-05-26--4"),  # VW.AUDI_A3
 
   # Enable when port is tested and dascamOnly is no longer set
   #("MAZDA", "32a319f057902bb3|2020-04-27--15-18-58--2"),      # MAZDA.CX5
