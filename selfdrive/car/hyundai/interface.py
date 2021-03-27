554c886dd5b48da3e549a7c5f435aa3f0b055f63
speedking456 <johnbarrett6@icloud.com>
2021-03-26 18:33:10 -0700

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -149,12 +149,12 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.63
       ret.steerRatio = 14.4
       tire_stiffness_factor = 0.5
-      ret.steerActuatorDelay = 0.095
+      ret.steerActuatorDelay = 0.097
       #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [0.]
-      ret.lateralTuning.indi.innerLoopGainV = [3.5]
+      ret.lateralTuning.indi.innerLoopGainV = [3.55]
       ret.lateralTuning.indi.outerLoopGainBP = [0.]
       ret.lateralTuning.indi.outerLoopGainV = [2.0]
       ret.lateralTuning.indi.timeConstantBP = [0.]
