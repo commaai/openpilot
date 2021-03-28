e4a82f089da54e374aac3f86e28d241efb68738e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-03-28 16:46:24 -0700

    same behavior for now

--- selfdrive/ui/qt/sound.cc
+++ selfdrive/ui/qt/sound.cc
@@ -9,12 +9,10 @@ Sound::Sound() {
 }
 
 void Sound::play(AudibleAlert alert) {
-  if (!sounds[alert].isPlaying()) {
-    int loops = sound_map[alert].second> - 1 ? sound_map[alert].second : QSoundEffect::Infinite;
-    sounds[alert].setLoopCount(loops);
-    sounds[alert].setVolume(volume);
-    sounds[alert].play();
-  }
+  int loops = sound_map[alert].second> - 1 ? sound_map[alert].second : QSoundEffect::Infinite;
+  sounds[alert].setLoopCount(loops);
+  sounds[alert].setVolume(volume);
+  sounds[alert].play();
 }
 
 void Sound::stop() {
