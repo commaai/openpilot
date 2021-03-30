6f9a5abe1a1c926fead52a9f180486e7e2775f1d
ZanZaD13 <65367004+ZanZaD13@users.noreply.github.com>
2021-03-30 23:28:37 +0200

    KIA Ceed: increase max torque (#20505)
    old-commit-hash: da07c6522e61ac1e3806eb66083b3aa6c3f5ddd9

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -7,7 +7,7 @@ Ecu = car.CarParams.Ecu
 # Steer torque limits
 class CarControllerParams:
   def __init__(self, CP):
-    if CP.carFingerprint in [CAR.SONATA, CAR.PALISADE, CAR.SANTA_FE, CAR.VELOSTER, CAR.GENESIS_G70, CAR.IONIQ_EV_2020]:
+    if CP.carFingerprint in [CAR.SONATA, CAR.PALISADE, CAR.SANTA_FE, CAR.VELOSTER, CAR.GENESIS_G70, CAR.IONIQ_EV_2020, CAR.KIA_CEED]:
       self.STEER_MAX = 384
     else:
       self.STEER_MAX = 255
