e181fd7f0a9654d5d94f148d0378c5ac8be134ca
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2021-03-30 16:59:17 -0400

    VW MQB: Misc bugfixes and cleanup (#20540)
    * Add LDW->SWA signal pass-through
    
    * GC currently unused ACC_06 and LDW_02 signal data
    
    * Split out Jetta GLI
    
    * Pass in camera CAN parser now that we use it
    
    * GC unused CarController param, camelCase->PEP8
    
    * CAN valid should pass for VW now
    
    * update refs
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/car/volkswagen/volkswagencan.py
+++ selfdrive/car/volkswagen/volkswagencan.py
@@ -15,23 +15,26 @@ def create_mqb_steering_control(packer, bus, apply_steer, idx, lkas_enabled):
   }
   return packer.make_can_msg("HCA_01", bus, values, idx)
 
-def create_mqb_hud_control(packer, bus, hca_enabled, steering_pressed, hud_alert, leftLaneVisible, rightLaneVisible):
-
+def create_mqb_hud_control(packer, bus, hca_enabled, steering_pressed, hud_alert, left_lane_visible, right_lane_visible,
+                           ldw_lane_warning_left, ldw_lane_warning_right, ldw_side_dlc_tlc, ldw_dlc, ldw_tlc):
   if hca_enabled:
-    leftlanehud = 3 if leftLaneVisible else 1
-    rightlanehud = 3 if rightLaneVisible else 1
+    left_lane_hud = 3 if left_lane_visible else 1
+    right_lane_hud = 3 if right_lane_visible else 1
   else:
-    leftlanehud = 2 if leftLaneVisible else 1
-    rightlanehud = 2 if rightLaneVisible else 1
+    left_lane_hud = 2 if left_lane_visible else 1
+    right_lane_hud = 2 if right_lane_visible else 1
 
   values = {
-    "LDW_SW_Warnung_links": 0,  # FIXME: to be store-and-forwarded from the stock camera
-    "LDW_SW_Warnung_rechts": 1,  # FIXME: to be store-and-forwarded from the stock camera
     "LDW_Status_LED_gelb": 1 if hca_enabled and steering_pressed else 0,
     "LDW_Status_LED_gruen": 1 if hca_enabled and not steering_pressed else 0,
-    "LDW_Lernmodus_links": leftlanehud,
-    "LDW_Lernmodus_rechts": rightlanehud,
+    "LDW_Lernmodus_links": left_lane_hud,
+    "LDW_Lernmodus_rechts": right_lane_hud,
     "LDW_Texte": hud_alert,
+    "LDW_SW_Warnung_links": ldw_lane_warning_left,
+    "LDW_SW_Warnung_rechts": ldw_lane_warning_right,
+    "LDW_Seite_DLCTLC": ldw_side_dlc_tlc,
+    "LDW_DLC": ldw_dlc,
+    "LDW_TLC": ldw_tlc
   }
   return packer.make_can_msg("LDW_02", bus, values)
 
