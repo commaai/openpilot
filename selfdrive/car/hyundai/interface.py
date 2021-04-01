9ad40f07b80ad53f8387acc2e0a74dd4fee191a7
speedking456 <johnbarrett6@icloud.com>
2021-04-01 16:51:01 -0700

    remove tests

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -148,21 +148,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.63
       ret.steerRatio = 14.4
       tire_stiffness_factor = 0.5
-      
-      #ret.lateralTuning.init('lqr')
-
-      #ret.lateralTuning.lqr.scale = 1500.0
-      #ret.lateralTuning.lqr.ki = 0.05
-
-      #ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-      #ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-      #ret.lateralTuning.lqr.c = [1., 0.]
-      #ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-      #ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-      #ret.lateralTuning.lqr.dcGain = 0.002237852961363602
-      
       ret.steerActuatorDelay = 0.097
-      
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [0.]
       ret.lateralTuning.indi.innerLoopGainV = [4.]
