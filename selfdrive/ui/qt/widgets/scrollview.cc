9afa14c47fa40023cf1abb1956d7e2815aab9409
iejMac <61431446+iejMac@users.noreply.github.com>
2021-04-01 14:03:31 -0700

    Qt scroller class (#20549)
    * initial commit
    
    * class works
    
    * cleanup
    
    * naming
    
    * text window
    
    * fixes
    
    * alerts are now scrollable
    
    * dismiss
    
    * fixed placement
    
    * realease scrolling
    
    * better
    
    * better
    
    * revert text
    
    * naming
    
    * parent

---
+++ selfdrive/ui/qt/widgets/scrollview.cc
@@ -0,0 +1,18 @@
+#include "scrollview.hpp"
+
+ScrollView::ScrollView(QWidget *w, QWidget *parent) : QScrollArea(parent){
+  setWidget(w);
+  setWidgetResizable(true);
+  setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  setStyleSheet("background-color:transparent;");
+
+  QScroller *scroller = QScroller::scroller(this->viewport());
+  QScrollerProperties sp = scroller->scrollerProperties();
+
+  sp.setScrollMetric(QScrollerProperties::VerticalOvershootPolicy, QVariant::fromValue<QScrollerProperties::OvershootPolicy>(QScrollerProperties::OvershootAlwaysOff));
+  sp.setScrollMetric(QScrollerProperties::HorizontalOvershootPolicy, QVariant::fromValue<QScrollerProperties::OvershootPolicy>(QScrollerProperties::OvershootAlwaysOff));
+
+  scroller->grabGesture(this->viewport(), QScroller::LeftMouseButtonGesture);
+  scroller->setScrollerProperties(sp);
+}
