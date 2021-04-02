17a2c30c9fadd0fb434cfdc2722b3d858dbc47bf
iejMac <gig13omb@gmail.com>
2021-04-02 16:43:34 -0700

    horizontal style + more space for scrollbar

--- selfdrive/ui/qt/widgets/scrollview.cc
+++ selfdrive/ui/qt/widgets/scrollview.cc
@@ -8,30 +8,32 @@ ScrollView::ScrollView(QWidget *w, QWidget *parent) : QScrollArea(parent){
   setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   setStyleSheet("ScrollView { background-color:transparent; }");
 
-  verticalScrollBar()->setStyleSheet(R"(
-                                      QScrollBar:vertical {
-                                        border: 0px solid #999999;
-                                        background:transparent;
-                                        width:10px;
-                                        margin: 0px 0px 0px 0px;
-                                      }
-                                      QScrollBar::handle:vertical {
-                                        min-height: 0px;
-                                        border: 0px solid red;
-                                        border-radius: 4px;
-                                        background-color: white;
-                                      }
-                                      QScrollBar::add-line:vertical {
-                                        height: 0px;
-                                        subcontrol-position: bottom;
-                                        subcontrol-origin: margin;
-                                      }
-                                      QScrollBar::sub-line:vertical {
-                                        height: 0 px;
-                                        subcontrol-position: top;
-                                        subcontrol-origin: margin;
-                                      }
-                                      )");
+  QString style = R"(
+  QScrollBar:vertical {
+    border: 0px solid #999999;
+    background:transparent;
+    width:10px;
+    margin: 0px 0px 0px 0px;
+  }
+  QScrollBar::handle:vertical {
+    min-height: 0px;
+    border: 0px solid red;
+    border-radius: 4px;
+    background-color: white;
+  }
+  QScrollBar::add-line:vertical {
+    height: 0px;
+    subcontrol-position: bottom;
+    subcontrol-origin: margin;
+  }
+  QScrollBar::sub-line:vertical {
+    height: 0 px;
+    subcontrol-position: top;
+    subcontrol-origin: margin;
+  })";
+
+  verticalScrollBar()->setStyleSheet(style);
+  horizontalScrollBar()->setStyleSheet(style);
 
   QScroller *scroller = QScroller::scroller(this->viewport());
   QScrollerProperties sp = scroller->scrollerProperties();
