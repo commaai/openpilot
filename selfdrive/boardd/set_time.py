061f3f50d6b1bf46ab752bcb58d3419f1eaa1fe3
Comma Device <device@comma.ai>
2021-04-01 22:40:04 -0700

    set time

--- selfdrive/boardd/set_time.py
+++ selfdrive/boardd/set_time.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import datetime
+import os
 import struct
-import sys
 import usb1
 
 REQUEST_IN = usb1.ENDPOINT_IN | usb1.TYPE_VENDOR | usb1.RECIPIENT_DEVICE
@@ -9,11 +9,12 @@ REQUEST_IN = usb1.ENDPOINT_IN | usb1.TYPE_VENDOR | usb1.RECIPIENT_DEVICE
 if __name__ == "__main__":
   ctx = usb1.USBContext()
   dev = ctx.openByVendorIDAndProductID(0xbbaa, 0xddcc)
-
-  if dev is not None:
-    dat = dev.controlRead(REQUEST_IN, 0xa0, 0, 0, 8)
-    a = struct.unpack("HBBBBBB", dat)
-    d = datetime.datetime(a[0], a[1], a[2], a[4], a[5], a[6])
-    print("got", d)
-  else:
+  if dev is None:
     print("No panda found")
+    exit()
+
+  dat = dev.controlRead(REQUEST_IN, 0xa0, 0, 0, 8)
+  a = struct.unpack("HBBBBBB", dat)
+  d = datetime.datetime(a[0], a[1], a[2], a[4], a[5], a[6])
+  print("panda time", d)
+  os.system(f"TZ=UTC date -s '{d}'")
