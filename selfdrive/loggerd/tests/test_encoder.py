59f31275d0780b72969d77da506eff016b9263e2
Willem Melching <willem.melching@gmail.com>
2021-04-08 17:27:55 +0200

    fix check at end of function

--- selfdrive/loggerd/tests/test_encoder.py
+++ selfdrive/loggerd/tests/test_encoder.py
@@ -66,7 +66,7 @@ class TestEncoder(unittest.TestCase):
 
   # TODO: this should run faster than real time
   @parameterized.expand([(True, ), (False, )])
-  @with_processes(['camerad', 'sensord', 'loggerd'], init_time=3)
+  @with_processes(['camerad', 'sensord', 'loggerd'], init_time=3, ignore_stopped=['loggerd'])
   def test_log_rotation(self, record_front):
     Params().put("RecordFront", str(int(record_front)))
 
