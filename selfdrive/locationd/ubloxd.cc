3a0ac52d1107d2a051a27d89e29a68286ca54a2f
Willem Melching <willem.melching@gmail.com>
2021-04-09 17:39:54 +0200

    less includes

--- selfdrive/locationd/ubloxd.cc
+++ selfdrive/locationd/ubloxd.cc
@@ -1,21 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/time.h>
-#include <sys/cdefs.h>
-#include <sys/types.h>
-#include <assert.h>
-#include <math.h>
-#include <ctime>
-#include <chrono>
-
 #include "messaging.hpp"
 #include "common/util.h"
-#include "common/params.h"
 #include "common/swaglog.h"
-#include "common/timing.h"
 
 #include "ublox_msg.h"
 
