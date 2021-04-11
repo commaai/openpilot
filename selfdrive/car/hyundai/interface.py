f34d4dc2c53c1b6f48e20be4e1d825163466ddd2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-04-11 14:33:29 -0700

    revert to current car params

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -144,11 +144,10 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate == CAR.KIA_SELTOS:
-      ret.mass = 1337. + STD_CARGO_KG
-      ret.wheelbase = 2.63
-      ret.steerRatio = 14.56
-      tire_stiffness_factor = 1
-      ret.steerActuatorDelay = 0.096
+      ret.mass = 1310. + STD_CARGO_KG
+      ret.wheelbase = 2.6
+      ret.steerRatio = 13.73  # Spec
+      tire_stiffness_factor = 0.5
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [0.]
       ret.lateralTuning.indi.innerLoopGainV = [4.]
