b206b095a4e39fcd388847e61eab04ecd966674a
Joost Wooning <jwooning@gmail.com>
2021-04-15 15:42:26 +0200

    fix replay test

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -413,6 +413,7 @@ def cpp_replay_process(cfg, lr):
   all_msgs = sorted(lr, key=lambda msg: msg.logMonoTime)
   pub_msgs = [msg for msg in all_msgs if msg.which() in list(cfg.pub_sub.keys())]
 
+  os.environ['PROCESS_REPLAY'] = "1"
   managed_processes[cfg.proc_name].prepare()
   managed_processes[cfg.proc_name].start()
 
