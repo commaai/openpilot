7b3c88c1a5f8e2679cd5c971d11d0cd72f8fcd82
iejMac <gig13omb@gmail.com>
2021-04-14 22:36:21 -0700

    headless mode works

--- selfdrive/ui/replay/main.cc
+++ selfdrive/ui/replay/main.cc
@@ -1,26 +1,28 @@
 #include <QApplication>
 
+#include "home.hpp"
 #include "replay.hpp"
+#include "qt/qt_window.hpp"
 
 int main(int argc, char *argv[]){
-
-/*
   QSurfaceFormat fmt;
   fmt.setRenderableType(QSurfaceFormat::OpenGLES);
   QSurfaceFormat::setDefaultFormat(fmt);
-*/
+
+  QApplication a(argc, argv);
+	GLWindow *w = new GLWindow();
+  setMainWindow((QWidget*)w);
+  a.installEventFilter(w);
 
   QString route(argv[1]);
   route = route.replace("|", "/");
-  if (route == "") {
+  if (route != "") {
+    int seek = QString(argv[2]).toInt();
+    int use_api = QString::compare(QString("use_api"), route, Qt::CaseInsensitive) == 0;
+
+    Replay *replay = new Replay(route, seek, use_api);
+    replay->replay();
   }
 
-	int seek = QString(argv[2]).toInt();
-	int use_api = QString::compare(QString("use_api"), route, Qt::CaseInsensitive) == 0;
-
-  QApplication a(argc, argv);
-	Replay *replay = new Replay(route, seek, use_api);
-
-	replay->show();
   return a.exec();
 }
