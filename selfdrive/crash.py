3c1e1ef23e8b1d5562572cbf0a440999c0f52ac5
Willem Melching <willem.melching@gmail.com>
2021-04-16 14:51:40 +0200

    use sentry_sdk in crash.py

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -18,26 +18,27 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH") or PC:
     pass
 
 else:
-  from raven import Client
-  from raven.transport.http import HTTPTransport
+  import sentry_sdk
+
+  def capture_exception(*args, **kwargs):
+    exc_info = sys.exc_info()
+    if not exc_info[0] is capnp.lib.capnp.KjException:
+      sentry_sdk.capture_exception(*args, **kwargs)
+    cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
+
+  def bind_user(**kwargs):
+    sentry_sdk.set_user(kwargs)
+
+  def bind_extra(**kwargs):
+    for k, v in kwargs.items():
+      sentry_sdk.set_tag(k, v)
+
+  sentry_sdk.init("https://a8dc76b5bfb34908a601d67e2aa8bcf9@o33823.ingest.sentry.io/77924",
+                  default_integrations=False, release=version,)
 
   tags = {
     'dirty': dirty,
     'origin': origin,
     'branch': branch
   }
-  client = Client('https://a8dc76b5bfb34908a601d67e2aa8bcf9:4336ee4648984e438370a3fa3f5adda2@o33823.ingest.sentry.io/77924',
-                  install_sys_hook=False, transport=HTTPTransport, release=version, tags=tags)
-
-  def capture_exception(*args, **kwargs):
-    exc_info = sys.exc_info()
-    if not exc_info[0] is capnp.lib.capnp.KjException:
-      client.captureException(*args, **kwargs)
-    cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
-
-  def bind_user(**kwargs):
-    client.user_context(kwargs)
-
-  def bind_extra(**kwargs):
-    client.extra_context(kwargs)
-
+  bind_extra(**tags)
