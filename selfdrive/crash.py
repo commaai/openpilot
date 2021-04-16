9a6fa3596e2d3fba798cee9c696555d1bc1b0f7c
Willem Melching <willem.melching@gmail.com>
2021-04-16 14:05:09 +0200

    remove unused crash.install

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -1,7 +1,6 @@
 """Install exception handler for process crash."""
 import os
 import sys
-import threading
 import capnp
 from selfdrive.version import version, dirty, origin, branch
 
@@ -18,8 +17,6 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH") or PC:
   def bind_extra(**kwargs):
     pass
 
-  def install():
-    pass
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
@@ -44,34 +41,3 @@ else:
   def bind_extra(**kwargs):
     client.extra_context(kwargs)
 
-  def install():
-    """
-    Workaround for `sys.excepthook` thread bug from:
-    http://bugs.python.org/issue1230540
-    Call once from the main thread before creating any threads.
-    Source: https://stackoverflow.com/a/31622038
-    """
-    # installs a sys.excepthook
-    __excepthook__ = sys.excepthook
-
-    def handle_exception(*exc_info):
-      if exc_info[0] not in (KeyboardInterrupt, SystemExit):
-        capture_exception()
-      __excepthook__(*exc_info)
-    sys.excepthook = handle_exception
-
-    init_original = threading.Thread.__init__
-
-    def init(self, *args, **kwargs):
-      init_original(self, *args, **kwargs)
-      run_original = self.run
-
-      def run_with_except_hook(*args2, **kwargs2):
-        try:
-          run_original(*args2, **kwargs2)
-        except Exception:
-          sys.excepthook(*sys.exc_info())
-
-      self.run = run_with_except_hook
-
-    threading.Thread.__init__ = init
