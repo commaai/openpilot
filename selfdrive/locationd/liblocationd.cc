b554ba215498900119fcffb7c42c3a59a7afee50
Joost Wooning <jwooning@gmail.com>
2021-04-20 17:25:18 +0200

    add check on buffer size

--- selfdrive/locationd/liblocationd.cc
+++ selfdrive/locationd/liblocationd.cc
@@ -8,10 +8,11 @@ extern "C" {
   }
 
   void localizer_get_message_bytes(Localizer *localizer, uint64_t logMonoTime,
-    bool inputsOK, bool sensorsOK, bool gpsOK, char *buff)
+    bool inputsOK, bool sensorsOK, bool gpsOK, char *buff, size_t buff_size)
   {
     MessageBuilder msg_builder;
     kj::ArrayPtr<char> arr = localizer->get_message_bytes(msg_builder, logMonoTime, inputsOK, sensorsOK, gpsOK).asChars();
+    assert(buff_size >= arr.size());
     memcpy(buff, arr.begin(), arr.size());
   }
 
