6c9dade37d434efe37f5bdb27f56326abaaa33a9
Willem Melching <willem.melching@gmail.com>
2021-04-21 12:04:24 +0200

    make default crash handler work

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -2,10 +2,10 @@
 import os
 import sys
 import capnp
-from selfdrive.version import version, dirty, origin, branch
 
 from selfdrive.hardware import PC
 from selfdrive.swaglog import cloudlog
+from selfdrive.version import version
 
 if os.getenv("NOLOG") or os.getenv("NOCRASH") or PC:
   def capture_exception(*args, **kwargs):
@@ -24,6 +24,7 @@ else:
     exc_info = sys.exc_info()
     if not exc_info[0] is capnp.lib.capnp.KjException:
       sentry_sdk.capture_exception(*args, **kwargs)
+      sentry_sdk.flush()  # https://github.com/getsentry/sentry-python/issues/291
     cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
 
   def bind_user(**kwargs):
@@ -34,11 +35,4 @@ else:
       sentry_sdk.set_tag(k, v)
 
   sentry_sdk.init("https://a8dc76b5bfb34908a601d67e2aa8bcf9@o33823.ingest.sentry.io/77924",
-                  default_integrations=False, release=version,)
-
-  tags = {
-    'dirty': dirty,
-    'origin': origin,
-    'branch': branch
-  }
-  bind_extra(**tags)
+                  default_integrations=False, release=version)
