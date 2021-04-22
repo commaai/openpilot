90a79b63520cd9ba5696277b97ce7e2ed998cf64
iejMac <gig13omb@gmail.com>
2021-04-21 22:58:33 -0700

    tabs

--- selfdrive/ui/replay/main.cc
+++ selfdrive/ui/replay/main.cc
@@ -10,13 +10,13 @@ int main(int argc, char *argv[]){
   if (route == "") {
     printf("Usage: ./replay \"route\"\n");
     return 1;
-	}
+  }
 
-	int seek = QString(argv[2]).toInt();
-	int use_api = QString::compare(QString("use_api"), route, Qt::CaseInsensitive) == 0;
+  int seek = QString(argv[2]).toInt();
+  int use_api = QString::compare(QString("use_api"), route, Qt::CaseInsensitive) == 0;
 
-	Replay *replay = new Replay(route, seek, use_api);
-	replay->stream(seek);
+  Replay *replay = new Replay(route, seek, use_api);
+  replay->stream(seek);
 
   return a.exec();
 }
