4feb947d2fd4d5557c37220e44f1561c7bc47520
iejMac <gig13omb@gmail.com>
2021-04-22 14:48:47 -0700

    trailing new lines

--- selfdrive/ui/qt/request_repeater.hpp
+++ selfdrive/ui/qt/request_repeater.hpp
@@ -7,5 +7,3 @@ public:
   RequestRepeater(QWidget *parent, QString requestURL, const QString &cache_key = "", int period_seconds = 0, bool disableWithScreen = true);
   bool disableWithScreen;
 };
-
-
