07161f510f7fe16523cbb9c29dcddbe44a213d95
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2021-04-23 10:37:36 -0500

    Add Mazda 3 2014 fingerprint
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/car/mazda/mazdacan.py
+++ selfdrive/car/mazda/mazdacan.py
@@ -13,6 +13,8 @@ def create_steering_control(packer, car_fingerprint, frame, apply_steer, lkas):
   lnv = 0
   er2 = int(lkas["ERR_BIT_2"])
 
+  # Some older models do have these, newer models don't.
+  # Either way, they all work just fine if set to zero.
   steering_angle = 0
   b2 = 0
 
