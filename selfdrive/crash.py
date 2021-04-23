4c461b3f7fb9dd241c3a52c1d487959b7537643e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-04-23 11:29:38 -0700

    disable sentry for devices without a dongle ID (#20729)
    * disable sentry for devices without a dongle ID
    
    * import pc
    
    * unused
    old-commit-hash: 47ad2a485f927134ee2f93c497b1b6cb831def30

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -1,40 +1,28 @@
 """Install exception handler for process crash."""
-import os
 import sys
 import capnp
 
-from selfdrive.hardware import PC
 from selfdrive.swaglog import cloudlog
 from selfdrive.version import version
 
-if os.getenv("NOLOG") or os.getenv("NOCRASH") or PC:
-  def capture_exception(*args, **kwargs):
-    pass
+import sentry_sdk
+from sentry_sdk.integrations.threading import ThreadingIntegration
 
-  def bind_user(**kwargs):
-    pass
+def capture_exception(*args, **kwargs):
+  exc_info = sys.exc_info()
+  if not exc_info[0] is capnp.lib.capnp.KjException:
+    sentry_sdk.capture_exception(*args, **kwargs)
+    sentry_sdk.flush()  # https://github.com/getsentry/sentry-python/issues/291
+  cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
 
-  def bind_extra(**kwargs):
-    pass
+def bind_user(**kwargs):
+  sentry_sdk.set_user(kwargs)
 
-else:
-  import sentry_sdk
-  from sentry_sdk.integrations.threading import ThreadingIntegration
+def bind_extra(**kwargs):
+  for k, v in kwargs.items():
+    sentry_sdk.set_tag(k, v)
 
-  def capture_exception(*args, **kwargs):
-    exc_info = sys.exc_info()
-    if not exc_info[0] is capnp.lib.capnp.KjException:
-      sentry_sdk.capture_exception(*args, **kwargs)
-      sentry_sdk.flush()  # https://github.com/getsentry/sentry-python/issues/291
-    cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
-
-  def bind_user(**kwargs):
-    sentry_sdk.set_user(kwargs)
-
-  def bind_extra(**kwargs):
-    for k, v in kwargs.items():
-      sentry_sdk.set_tag(k, v)
-
+def init():
   sentry_sdk.init("https://a8dc76b5bfb34908a601d67e2aa8bcf9@o33823.ingest.sentry.io/77924",
                   default_integrations=False, integrations=[ThreadingIntegration(propagate_hub=True)],
                   release=version)
--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -12,7 +12,7 @@ from common.basedir import BASEDIR
 from common.params import Params
 from common.text_window import TextWindow
 from selfdrive.boardd.set_time import set_time
-from selfdrive.hardware import HARDWARE, TICI
+from selfdrive.hardware import HARDWARE, PC, TICI
 from selfdrive.manager.helpers import unblock_stdout
 from selfdrive.manager.process import ensure_running
 from selfdrive.manager.process_config import managed_processes
@@ -83,6 +83,9 @@ def manager_init():
 
   cloudlog.bind_global(dongle_id=dongle_id, version=version, dirty=dirty,
                        device=HARDWARE.get_device_type())
+
+  if not (dongle_id is None or os.getenv("NOLOG") or os.getenv("NOCRASH") or PC):
+    crash.init()
   crash.bind_user(id=dongle_id)
   crash.bind_extra(dirty=dirty, origin=origin, branch=branch, commit=commit,
                    device=HARDWARE.get_device_type())
