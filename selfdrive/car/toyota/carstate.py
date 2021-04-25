7f227ddd7498e87fe5fc97fbd305060ccb58311d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-04-25 00:51:50 -0700

    toyota fixes

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -191,7 +191,7 @@ class CarState(CarStateBase):
     # use steering message to check if panda is connected to frc
     checks = [
       ("STEERING_LKA", 42),
-      ("PRE_COLLISION", 33),
+      ("PRE_COLLISION", 0), # TODO: figure out why freq is inconsistent
     ]
 
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 2)
