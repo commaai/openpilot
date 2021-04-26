7cc1e11929886ffdc8154fed51f83c5fb9c25cf0
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-04-26 14:43:36 -0700

    honda crv: add BSM frequency check
    old-commit-hash: 1083d14f40e3164e7925a3453bee7bcd6b6151c1

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -404,10 +404,9 @@ class CarState(CarStateBase):
       signals = [("BSM_ALERT", "BSM_STATUS_RIGHT", 0),
                  ("BSM_ALERT", "BSM_STATUS_LEFT", 0)]
 
-      # TODO: get freqs
       checks = [
-        ("BSM_STATUS_LEFT", 0),
-        ("BSM_STATUS_RIGHT", 0),
+        ("BSM_STATUS_LEFT", 3),
+        ("BSM_STATUS_RIGHT", 3),
       ]
       bus_body = 0 # B-CAN is forwarded to ACC-CAN radar side (CAN 0 on fake ethernet port)
       return CANParser(DBC[CP.carFingerprint]['body'], signals, checks, bus_body)
