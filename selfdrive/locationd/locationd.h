a734ef5887a1e529d2aac6f0a91ae11a156f0c9d
Harald Schafer <harald.the.engineer@gmail.com>
2021-04-26 22:07:15 -0700

    100Hz

--- selfdrive/locationd/locationd.h
+++ selfdrive/locationd/locationd.h
@@ -17,9 +17,6 @@
 
 #include "models/live_kf.h"
 
-#define VISION_DECIMATION 2
-#define SENSOR_DECIMATION 10
-
 #define POSENET_STD_HIST_HALF 20
 
 class Localizer {
@@ -61,9 +58,4 @@ private:
   int64_t unix_timestamp_millis = 0;
   double last_gps_fix = 0;
   bool device_fell = false;
-
-  int gyro_counter = 0;
-  int acc_counter = 0;
-  int speed_counter = 0;
-  int cam_counter = 0;
 };
