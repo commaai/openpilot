efa1897d8732880ed8ca8ec32302d2e471cbed11
Willem Melching <willem.melching@gmail.com>
2021-04-30 11:37:33 +0200

    no hack there

--- selfdrive/ui/replay/main.cc
+++ selfdrive/ui/replay/main.cc
@@ -3,7 +3,6 @@
 #include "selfdrive/ui/replay/replay.h"
 
 int main(int argc, char *argv[]){
-  QCoreApplication::addLibraryPath("../../phonelibs/qt-plugins/x86_64/");
   QApplication a(argc, argv);
 
   QString route(argv[1]);
