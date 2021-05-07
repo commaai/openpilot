b7b134a2af58f3a1bd7649e7ee2ab083be3819e0
matt12eagles <matt12eagles@gmail.com>
2021-05-06 22:20:11 -0400

    Update lane_planner.py
    Move us left a tad camera offset change

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -7,7 +7,7 @@ from cereal import log
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 if EON:
-  CAMERA_OFFSET = 0.06
+  CAMERA_OFFSET = 0.09
   PATH_OFFSET = 0.0
 elif TICI:
   CAMERA_OFFSET = -0.04
