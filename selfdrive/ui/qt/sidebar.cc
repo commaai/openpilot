cf2fdc7fcaaa861026c466ab4e9091f93e8ed5d4
deanlee <deanlee3@gmail.com>
2021-05-08 14:08:05 +0800

    rebase master

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -49,7 +49,7 @@ void Sidebar::mousePressEvent(QMouseEvent *event) {
 }
 
 void Sidebar::update(const UIState &s) {
-  if (s.sm->frame % (6*UI_FREQ) == 0) {
+  if (s.sm.frame % (6*UI_FREQ) == 0) {
     connect_str = "OFFLINE";
     connect_status = warning_color;
     auto last_ping = params.get<float>("LastAthenaPingTime");
@@ -61,17 +61,18 @@ void Sidebar::update(const UIState &s) {
     repaint();
   }
 
-  net_type = s.scene.deviceState.getNetworkType();
-  strength = s.scene.deviceState.getNetworkStrength();
+  auto deviceState = s.sm["deviceState"].getDeviceState();
+  net_type = deviceState.getNetworkType();
+  strength = deviceState.getNetworkStrength();
 
   temp_status = danger_color;
-  auto ts = s.scene.deviceState.getThermalStatus();
+  auto ts = deviceState.getThermalStatus();
   if (ts == cereal::DeviceState::ThermalStatus::GREEN) {
     temp_status = good_color;
   } else if (ts == cereal::DeviceState::ThermalStatus::YELLOW) {
     temp_status = warning_color;
   }
-  temp_val = (int)s.scene.deviceState.getAmbientTempC();
+  temp_val = (int)deviceState.getAmbientTempC();
 
   panda_str = "VEHICLE\nONLINE";
   panda_status = good_color;
@@ -83,7 +84,7 @@ void Sidebar::update(const UIState &s) {
     panda_status = s.scene.gpsOK ? good_color : warning_color;
   }
 
-  if (s.sm->updated("deviceState") || s.sm->updated("pandaState")) {
+  if (s.sm.updated("deviceState") || s.sm.updated("pandaState")) {
     repaint();
   }
 }
