4942e14767df1d580d43e94bb4196dd802813059
matt12eagles <matt12eagles@gmail.com>
2021-05-09 20:28:39 -0400

    Update events.py
    Changing scary event name lol

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -242,7 +242,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
 
   EventName.startupMaster: {
     ET.PERMANENT: Alert(
-      "WARNING: This branch is not tested",
+      "Be ready to take over at any time",
       "Always keep hands on wheel and eyes on road",
       AlertStatus.userPrompt, AlertSize.mid,
       Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 15.),
