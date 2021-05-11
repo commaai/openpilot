ef860540f730d5a4ca66219eccb9af6ded18dfd4
alfhern <hz.alfonso@gmail.com>
2021-05-11 13:04:00 +0200

    Tap on Ui to add debug data

--- selfdrive/mapd/mapd.py
+++ selfdrive/mapd/mapd.py
@@ -180,6 +180,13 @@ class MapD():
     map_data_msg.valid = sm.all_alive_and_valid(service_list=['gpsLocationExternal'])
 
     map_data_msg.liveMapData.lastGpsTimestamp = self.last_gps.timestamp
+    map_data_msg.liveMapData.lastGpsLatitude = float(self.last_gps.latitude)
+    map_data_msg.liveMapData.lastGpsLongitude = float(self.last_gps.longitude)
+    map_data_msg.liveMapData.lastGpsSpeed = float(self.last_gps.speed)
+    map_data_msg.liveMapData.lastGpsBearingDeg = float(self.last_gps.bearingDeg)
+    map_data_msg.liveMapData.lastGpsAccuracy = float(self.last_gps.accuracy)
+    map_data_msg.liveMapData.lastGpsBearingAccuracyDeg = float(self.last_gps.bearingAccuracyDeg)
+
     map_data_msg.liveMapData.speedLimitValid = bool(speed_limit is not None)
     map_data_msg.liveMapData.speedLimit = float(speed_limit if speed_limit is not None else 0.0)
     map_data_msg.liveMapData.speedLimitAheadValid = bool(next_speed_limit_section is not None)
