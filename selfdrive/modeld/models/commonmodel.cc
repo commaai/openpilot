f4944ea598f3c9538426678e9fa88bee8122f0e2
deanlee <deanlee3@gmail.com>
2021-05-14 00:11:36 +0800

    rebase master

--- selfdrive/modeld/models/commonmodel.cc
+++ selfdrive/modeld/models/commonmodel.cc
@@ -2,6 +2,7 @@
 
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 
 #include <algorithm>
 
--- selfdrive/modeld/models/commonmodel.h
+++ selfdrive/modeld/models/commonmodel.h
@@ -3,6 +3,8 @@
 #include <float.h>
 #include <stdlib.h>
 
+#include <memory>
+
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #ifdef __APPLE__
 #include <OpenCL/cl.h>
