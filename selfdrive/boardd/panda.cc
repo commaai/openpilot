61b15f8787acf90340439625625326139b72224c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-05-13 23:13:01 -0700

    boardd: set can and pandaState valid flags

--- selfdrive/boardd/panda.cc
+++ selfdrive/boardd/panda.cc
@@ -152,6 +152,7 @@ int Panda::usb_bulk_read(unsigned char endpoint, unsigned char* data, int length
     if (err == LIBUSB_ERROR_TIMEOUT) {
       break; // timeout is okay to exit, recv still happened
     } else if (err == LIBUSB_ERROR_OVERFLOW) {
+      comms_healthy = false;
       LOGE_100("overflow got 0x%x", transferred);
     } else if (err != 0) {
       handle_usb_issue(err, __func__);
@@ -296,9 +297,11 @@ int Panda::can_receive(kj::Array<capnp::word>& out_buf) {
 
   size_t num_msg = recv / 0x10;
   MessageBuilder msg;
-  auto canData = msg.initEvent().initCan(num_msg);
+  auto evt = msg.initEvent();
+  evt.setValid(comms_healthy);
 
   // populate message
+  auto canData = evt.initCan(num_msg);
   for (int i = 0; i < num_msg; i++) {
     if (data[i*4] & 4) {
       // extended
