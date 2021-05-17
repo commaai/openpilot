82d14dd949e03f9ba92a7dfe2fd4966f54eb5636
deanlee <deanlee3@gmail.com>
2021-05-17 15:35:47 +0800

    remove #include<stdbool.h> for cc files

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -4,7 +4,6 @@
 #include <fcntl.h>
 #include <math.h>
 #include <poll.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
--- selfdrive/loggerd/logger.cc
+++ selfdrive/loggerd/logger.cc
@@ -2,7 +2,6 @@
 
 #include <assert.h>
 #include <errno.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
