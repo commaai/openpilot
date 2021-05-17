11e87f7f2c174251e5c5757abfd77b58fa5946cb
deanlee <deanlee3@gmail.com>
2021-05-17 15:32:23 +0800

    remove #include <pthread.h>

--- selfdrive/boardd/panda.h
+++ selfdrive/boardd/panda.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <pthread.h>
-
 #include <atomic>
 #include <cstdint>
 #include <ctime>
--- selfdrive/camerad/cameras/camera_qcom.h
+++ selfdrive/camerad/cameras/camera_qcom.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <pthread.h>
 #include <stdbool.h>
 #include <stdint.h>
 
