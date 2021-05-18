de4fcc981f5a0ec6c43d9ee0918cdf9c3dbde494
ShaneSmiskol <shane@smiskol.com>
2021-05-18 14:14:18 -0700

    unused red

--- tools/replay/ui.py
+++ tools/replay/ui.py
@@ -13,7 +13,7 @@ import cereal.messaging as messaging
 from common.basedir import BASEDIR
 from selfdrive.config import UIParams as UP
 from tools.replay.lib.ui_helpers import (_BB_TO_FULL_FRAME, _FULL_FRAME_SIZE,
-                                         _INTRINSICS, BLACK, GREEN, RED,
+                                         _INTRINSICS, BLACK, GREEN,
                                          YELLOW, Calibration,
                                          get_blank_lid_overlay, init_plots,
                                          maybe_update_radar_points, plot_lead,
