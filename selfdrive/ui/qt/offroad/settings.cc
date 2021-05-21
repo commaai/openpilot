a207482fc734af12ea5d3c05e767a1e31b44c00d
Willem Melching <willem.melching@gmail.com>
2021-05-21 16:31:51 +0200

    ecam toggle: clear calibration (#20987)
    * ecam toggle: clear calibration
    
    * whitespace
    
    * no more reboot needed
    old-commit-hash: 9bb2209c8eb88a75367696e79a565545f1fc38d4

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -71,9 +71,13 @@ TogglesPanel::TogglesPanel(QWidget *parent) : QWidget(parent) {
   if (Hardware::TICI()) {
     toggles.append(new ParamControl("EnableWideCamera",
                                     "Enable use of Wide Angle Camera",
-                                    "Use wide angle camera for driving and ui. Only takes effect after reboot.",
+                                    "Use wide angle camera for driving and ui.",
                                     "../assets/offroad/icon_openpilot.png",
                                     this));
+    QObject::connect(toggles.back(), &ToggleControl::toggleFlipped, [=](bool state) {
+      Params().remove("CalibrationParams");
+    });
+
     toggles.append(new ParamControl("EnableLteOnroad",
                                     "Enable LTE while onroad",
                                     "",
