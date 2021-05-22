083f479e08377da19bc658479569b8445ba52ebf
Comma Device <device@comma.ai>
2021-05-22 00:27:09 -0700

    no bl tags

---
+++ selfdrive/camerad/test/setup_cam_core.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# camerad gets core 6
+
+# move IRQs
+CAM_IRQS="235 237 239 241 243"
+for irq in $CAM_IRQS; do
+	sudo su -c "echo 6 > /proc/irq/$irq/smp_affinity_list"
+done
+
+# workqueues
+sudo su -c "echo 40 > /sys/devices/virtual/workqueue/qcom,cam170-cpas-cdm0/cpumask"
+sudo su -c "echo 40 > /sys/devices/virtual/workqueue/qcom,cam_virtual_cdm/cpumask"
