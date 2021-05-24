83ae184b9b097cb4898e330a7d1373fbbeeab9d9
ShaneSmiskol <shane@smiskol.com>
2021-05-24 16:00:48 -0700

    create thread for sending msg (like unlogger), so we can do blocking key receiving

--- tools/carcontrols/joystickd.py
+++ tools/carcontrols/joystickd.py
@@ -8,12 +8,16 @@
 
 import pygame  # pylint: disable=import-error
 import time
+import zmq
 import cereal.messaging as messaging
 from common.numpy_fast import clip
-# from tools.sim.lib.keyboard_ctrl import getch, IFLAG, OFLAG, CFLAG, LFLAG, ISPEED, OSPEED, CC
 from tools.lib.kbhit import KBHit
+import multiprocessing
+from uuid import uuid4
+
 
 kb = KBHit()
+
 
 class Joystick:
   def __init__(self, use_keyboard=True):
@@ -21,7 +25,6 @@ class Joystick:
     self.axes_values = {'steer': 0., 'accel': 0.}
 
     if self.use_keyboard:
-      # self.axes = {'lat'}
       self.axes = {'w': ['forward', 'accel'], 'a': ['left', 'steer'], 's': ['backward', 'accel'], 'd': ['right', 'steer']}   # add more for the joystick buttons
       self.axes_increment = 0.05  # 5% of full actuation each key press
 
@@ -29,69 +32,84 @@ class Joystick:
     else:
       raise NotImplementedError("Only keyboard is supported for now")  # TODO: support joystick
 
-  def update(self, key):
+  def update(self):
     if self.use_keyboard:
-      old_settings = termios.tcgetattr(sys.stdin)
-      try:
-        tty.setcbreak(sys.stdin.fileno())
-        got_data = False
-        while has_data():
-          got_data = True
-          key = sys.stdin.read(1)
-        # else:
-        #   return
-        # key = kb.getch().lower()
-        if not got_data:
-          return
-        if key in self.axes:
-          event, event_type = self.axes[key]
-          sign = 1. if event in ['forward', 'left'] else -1.
-          v = self.axes_values[event_type]
-          self.axes_values[event_type] = round(clip(v + sign * self.axes_increment, -1., 1.), 3)
-        elif key in self.buttons:
-          if self.buttons[key] == 'reset':
-            self.axes_values = {'steer': 0., 'accel': 0.}
-        else:
-          print('Key not assigned to an action!')
-
-        return
-      finally:
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+      key = kb.getch().lower()
+      if key in self.axes:
+        event, event_type = self.axes[key]
+        sign = 1. if event in ['forward', 'left'] else -1.
+        v = self.axes_values[event_type]
+        self.axes_values[event_type] = round(clip(v + sign * self.axes_increment, -1., 1.), 3)
+      elif key in self.buttons:
+        if self.buttons[key] == 'reset':
+          self.axes_values = {'steer': 0., 'accel': 0.}
+      else:
+        print('Key not assigned to an action!')
+      return
 
 
 import sys
 import select
-import tty
-import termios
-
-
-def has_data():
-    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
 joystick = Joystick(use_keyboard=True)
 
 
-# old_settings = termios.tcgetattr(sys.stdin)
-# try:
-#   tty.setcbreak(sys.stdin.fileno())
-#   while 1:
-#     print('no data')
-#     time.sleep(1 / 100)
-#     if has_data():
-#       c = sys.stdin.read(1)
-#       joystick.update(c)
-# finally:
-#   termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+# while True:
+#   joystick.update()
+#   print(joystick.axes_values)
 
-
-while True:
-  joystick.update(None)
-  print(joystick.axes_values)
-  time.sleep(0.01)
 BUTTONS = ['pcm_cancel_cmd', 'engaged_toggle', 'steer_required']
 
-def joystick_thread():
+
+def new_joystick_msg(axes, btns):
+  dat = messaging.new_message('testJoystick')
+  dat.testJoystick.axes = axes
+  dat.testJoystick.buttons = btns
+  return dat
+
+
+def send_thread(command_address):
+  POLL_RATE = int(1000 / 10.)  # 10 hz
+  zmq.Context._instance = None
+  context = zmq.Context.instance()
+
+  command_sock = context.socket(zmq.PULL)
+  command_sock.bind(command_address)
+
+  poller = zmq.Poller()
+  poller.register(command_sock, zmq.POLLIN)
   joystick_sock = messaging.pub_sock('testJoystick')
+
+  # starting message to send to controlsd if user doesn't type any keys
+  msg = joystick.axes_values
+  while True:
+    evts = dict(poller.poll(POLL_RATE))  # blocking
+    if command_sock in evts:
+      msg = command_sock.recv_pyobj()  # only receives axes for now
+
+    dat = new_joystick_msg([msg[a] for a in ['accel', 'steer']],
+                           [False for _ in BUTTONS])
+
+    joystick_sock.send(dat.to_bytes())
+    print(f'Sent: {dat}')
+
+
+def joystick_thread():
+  command_address = "ipc:///tmp/{}".format(uuid4())
+
+  command_sock = zmq.Context.instance().socket(zmq.PUSH)
+  command_sock.connect(command_address)
+
+  poller = zmq.Poller()
+  poller.register(command_sock, zmq.POLLIN)
+
+  send_thread_proc = multiprocessing.Process(target=send_thread, args=(command_address,))
+  send_thread_proc.start()
+
+  while 1:
+    joystick.update()
+    command_sock.send_pyobj(joystick.axes_values)
+    time.sleep(1 / 100.)  # TODO: abstract this
 
   # pygame.init()
 
