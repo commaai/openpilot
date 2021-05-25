63f3c5cfd68756d29babaf6c1466ff10365d6c67
ShaneSmiskol <shane@smiskol.com>
2021-05-25 14:10:27 -0700

    debug

--- selfdrive/camerad/snapshot/snapshot.py
+++ selfdrive/camerad/snapshot/snapshot.py
@@ -109,11 +109,11 @@ def snapshot():
                           cwd=os.path.join(BASEDIR, "selfdrive/camerad"), env=env)
   time.sleep(3.0)
 
+  TICI = True
   frame = "wideRoadCameraState" if TICI else "roadCameraState"
   front_frame = "driverCameraState" if front_camera_allowed else None
 
   focus_perc_threshold = 0. if TICI else 10 / 12.
-  TICI = True
   rear, front = get_snapshots(frame, front_frame, focus_perc_threshold)
 
   proc.send_signal(signal.SIGINT)
