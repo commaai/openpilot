b789cce58cec9ff2dc749a29f80cdcb8c6ce50ab
ShaneSmiskol <shane@smiskol.com>
2021-05-25 13:54:19 -0700

    reset

--- selfdrive/camerad/snapshot/snapshot.py
+++ selfdrive/camerad/snapshot/snapshot.py
@@ -35,7 +35,7 @@ def extract_image(dat, frame_sizes):
 def rois_in_focus(lapres):
   bad_sum = 0
   for sharpness in lapres:
-    if sharpness < LM_THRESH:
+    if sharpness >= LM_THRESH:
       bad_sum += 1 / len(lapres)
   return bad_sum
 
