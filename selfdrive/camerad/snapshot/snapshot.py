8df7a970703a421eb2d64bdff09ab2ced2f30dea
ShaneSmiskol <shane@smiskol.com>
2021-05-26 14:49:03 -0700

    Revert "add code to write multiple files for comparison"
    This reverts commit 73aed19cbd656485cb243d59b9070e9f0ac4dda8.

--- selfdrive/camerad/snapshot/snapshot.py
+++ selfdrive/camerad/snapshot/snapshot.py
@@ -2,7 +2,6 @@
 import os
 import subprocess
 import time
-import sys
 
 import numpy as np
 from PIL import Image
@@ -39,7 +38,7 @@ def rois_in_focus(lapres: List[float]) -> float:
               lapres if sharpness >= LM_THRESH])
 
 
-def get_snapshots(frame="roadCameraState", front_frame="driverCameraState", focus_perc_threshold=0., no_roi=False):
+def get_snapshots(frame="roadCameraState", front_frame="driverCameraState", focus_perc_threshold=0.):
   frame_sizes = [eon_f_frame_size, eon_d_frame_size, leon_d_frame_size, tici_f_frame_size]
   frame_sizes = {w * h: (w, h) for (w, h) in frame_sizes}
 
@@ -51,24 +50,19 @@ def get_snapshots(frame="roadCameraState", front_frame="driverCameraState", focu
 
   sm = messaging.SubMaster(sockets)
 
-  if no_roi:
-    time.sleep(3.0)
-    while min(sm.logMonoTime.values()) == 0:
-      sm.update()
-  else:
-    start_t = time.monotonic()
-    while time.monotonic() - start_t < 10:
-      sm.update()
-      # wait 4 sec for startup and AF
-      if sm.rcv_frame[frame] * DT_MDL > 4. and rois_in_focus(sm[frame].sharpnessScore) >= focus_perc_threshold:
-        break
+  start_t = time.monotonic()
+  while time.monotonic() - start_t < 10:
+    sm.update()
+    # wait 4 sec for startup and AF
+    if sm.rcv_frame[frame] * DT_MDL > 4. and rois_in_focus(sm[frame].sharpnessScore) >= focus_perc_threshold:
+      break
 
   rear = extract_image(sm[frame].image, frame_sizes) if frame is not None else None
   front = extract_image(sm[front_frame].image, frame_sizes) if front_frame is not None else None
   return rear, front
 
 
-def snapshot(no_roi):
+def snapshot():
   params = Params()
   front_camera_allowed = params.get_bool("RecordFront")
 
@@ -101,7 +95,7 @@ def snapshot(no_roi):
   front_frame = "driverCameraState" if front_camera_allowed else None
   focus_perc_threshold = 0. if TICI else 10 / 12.
 
-  rear, front = get_snapshots(frame, front_frame, focus_perc_threshold, no_roi)
+  rear, front = get_snapshots(frame, front_frame, focus_perc_threshold)
   managed_processes['camerad'].stop()
 
   params.put_bool("IsTakingSnapshot", False)
@@ -114,18 +108,10 @@ def snapshot(no_roi):
 
 
 if __name__ == "__main__":
-  no_roi = sys.argv[1].lower() == 'no_roi'
-  if no_roi:
-    print('Using previous behavior!')
-  pic, fpic = snapshot(no_roi)
+  pic, fpic = snapshot()
   if pic is not None:
     print(pic.shape)
-    suffix = 0
-    no_roi_text = 'prev' if no_roi else 'new'
-    while os.path.exists((f_name := f'/tmp/back_{no_roi_text}.{suffix}.jpg')):
-      suffix += 1
-
-    jpeg_write(f_name, pic)
+    jpeg_write("/tmp/back.jpg", pic)
     if fpic is not None:
       jpeg_write("/tmp/front.jpg", fpic)
   else:
