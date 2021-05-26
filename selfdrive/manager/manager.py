189ee62d002a765af4edf6a6689ef62c3c20599f
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-05-25 20:36:46 -0700

    tici, it's just like cici

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -39,7 +39,6 @@ def manager_init():
 
   if TICI:
     default_params.append(("EnableLteOnroad", "1"))
-    default_params.append(("IsUploadRawEnabled", "1"))
 
   if params.get_bool("RecordFrontLock"):
     params.put_bool("RecordFront", True)
