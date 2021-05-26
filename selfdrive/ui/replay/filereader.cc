76fd658e98bb6ca1bd9e11561e539e94181fc913
deanlee <deanlee3@gmail.com>
2021-05-26 21:36:24 +0800

    mv framereader to ui/replay

--- selfdrive/ui/replay/filereader.cc
+++ selfdrive/ui/replay/filereader.cc
@@ -4,6 +4,28 @@
 #include <QtNetwork>
 #include "cereal/gen/cpp/log.capnp.h"
 
+static bool decompressBZ2(std::vector<uint8_t> &dest, const char srcData[], size_t srcSize,
+                          size_t outputSizeIncrement = 0x100000U) {
+  bz_stream strm = {};
+  int ret = BZ2_bzDecompressInit(&strm, 0, 0);
+  assert(ret == BZ_OK);
+
+  strm.next_in = const_cast<char *>(srcData);
+  strm.avail_in = srcSize;
+  do {
+    strm.next_out = (char *)&dest[strm.total_out_lo32];
+    strm.avail_out = dest.size() - strm.total_out_lo32;
+    ret = BZ2_bzDecompress(&strm);
+    if (ret == BZ_OK && strm.avail_in > 0 && strm.avail_out == 0) {
+      dest.resize(dest.size() + outputSizeIncrement);
+    }
+  } while (ret == BZ_OK);
+
+  BZ2_bzDecompressEnd(&strm);
+  dest.resize(strm.total_out_lo32);
+  return ret == BZ_STREAM_END;
+}
+
 FileReader::FileReader(const QString& file_) : file(file_) {
 }
 
