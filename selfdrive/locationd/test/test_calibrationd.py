f7cf66867bff75f5b53b6d0a2919a67e6e22242f
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-05-27 12:37:52 -0700

    remove test too

--- selfdrive/locationd/test/test_calibrationd.py
+++ selfdrive/locationd/test/test_calibrationd.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import json
 import random
 import unittest
 
@@ -9,17 +8,6 @@ from selfdrive.locationd.calibrationd import Calibrator
 
 
 class TestCalibrationd(unittest.TestCase):
-
-  def test_read_saved_params_json(self):
-    r = [random.random() for _ in range(3)]
-    b = random.randint(1, 10)
-    cal_params = {"calib_radians": r,
-                  "valid_blocks": b}
-    Params().put("CalibrationParams", json.dumps(cal_params).encode('utf8'))
-    c = Calibrator(param_put=True)
-
-    self.assertEqual(r, c.rpy)
-    self.assertEqual(b, c.valid_blocks)
 
   def test_read_saved_params(self):
     msg = messaging.new_message('liveCalibration')
