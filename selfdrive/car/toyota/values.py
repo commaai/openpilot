50facbb0184b9e243c39e62f2fba0cfae3c4de87
ShaneSmiskol <shane@smiskol.com>
2021-05-28 13:33:34 -0700

    use nodsu dbc. Only difference is the addition of some signals that don't work, and max of BRAKE_PRESSURE is different

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -1654,7 +1654,7 @@ DBC = {
   CAR.HIGHLANDERH: dbc_dict('toyota_highlander_hybrid_2018_pt_generated', 'toyota_adas'),
   CAR.HIGHLANDERH_TSS2: dbc_dict('toyota_nodsu_hybrid_pt_generated', 'toyota_tss2_adas'),
   CAR.AVALON: dbc_dict('toyota_avalon_2017_pt_generated', 'toyota_adas'),
-  CAR.AVALON_2019: dbc_dict('toyota_avalon_2019_pt_generated', 'toyota_adas'),
+  CAR.AVALON_2019: dbc_dict('toyota_nodsu_pt_generated', 'toyota_adas'),
   CAR.RAV4_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.COROLLA_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.COROLLAH_TSS2: dbc_dict('toyota_nodsu_hybrid_pt_generated', 'toyota_tss2_adas'),
