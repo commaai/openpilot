d8f1433b61a6ffe1f377ddc1e83bf628cb16f19a
Harald Schafer <harald.the.engineer@gmail.com>
2021-06-02 14:11:09 -0700

    add excessive reset

--- selfdrive/locationd/locationd.h
+++ selfdrive/locationd/locationd.h
@@ -31,6 +31,7 @@ public:
   void reset_kalman(double current_time, Eigen::VectorXd init_orient, Eigen::VectorXd init_pos);
   void finite_check(double current_time = NAN);
   void time_check(double current_time = NAN);
+  void update_reset_tracker();
 
   kj::ArrayPtr<capnp::byte> get_message_bytes(MessageBuilder& msg_builder, uint64_t logMonoTime,
     bool inputsOK, bool sensorsOK, bool gpsOK);
@@ -62,5 +63,6 @@ private:
 
   int64_t unix_timestamp_millis = 0;
   double last_gps_fix = 0;
+  double reset_tracker = 0.0;
   bool device_fell = false;
 };
