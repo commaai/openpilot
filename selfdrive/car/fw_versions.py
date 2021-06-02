e0c36b17e09db6f142b730cb65f5004acd0d5c5c
Erich Moraga <33645296+ErichMoraga@users.noreply.github.com>
2021-06-01 22:21:35 -0500

    Add Chrysler

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -76,7 +76,14 @@ MAZDA_VERSION_RESPONSE =  bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER + 0x40
 
 # brand, request, response, response offset
 REQUESTS = [
-  # Hyundai
+  # Chrysler
+  (
+    "chrysler",
+    [UDS_VERSION_REQUEST],
+    [UDS_VERSION_RESPONSE],
+    DEFAULT_RX_OFFSET,
+  ),
+    # Hyundai
   (
     "hyundai",
     [HYUNDAI_VERSION_REQUEST_SHORT],
