bfb72c69c0ce14b3bcdc3a1a80f59c2f896f1699
ShaneSmiskol <shane@smiskol.com>
2021-06-03 15:50:50 -0700

    update joystickd

--- tools/joystick/joystickd.py
+++ tools/joystick/joystickd.py
@@ -1,65 +1,158 @@
 #!/usr/bin/env python
 
-# This process publishes joystick events. Such events can be suscribed by
+# This process publishes joystick events. Such events can be subscribed by
 # mocked car controller scripts.
 
-
-### this process needs pygame and can't run on the EON ###
-
-import pygame  # pylint: disable=import-error
+import sys
+import zmq
+import multiprocessing
 import cereal.messaging as messaging
 
-def joystick_thread():
+from uuid import uuid4
+from dataclasses import dataclass
+
+from common.numpy_fast import clip
+from common.params import Params
+from selfdrive.controls.lib.pid import apply_deadzone
+from tools.lib.kbhit import KBHit
+
+AXES = ['gb', 'steer']
+BUTTONS = ['cancel', 'engaged_toggle', 'steer_required']
+AXES_INCREMENT = 0.05  # 5% of full actuation each key press
+POLL_RATE = int(1000 / 10.)  # 10 hz
+kb = KBHit()
+
+
+@dataclass
+class Event:
+  type = None
+  axis = None
+  button = None
+  value = 0
+
+
+class Joystick:
+  def __init__(self, use_keyboard=True):
+    self.use_keyboard = use_keyboard
+    self.axes_values = {ax: 0. for ax in AXES}
+    self.btn_states = {btn: False for btn in BUTTONS}
+
+    self.buttons = {'r': 'reset', 'c': 'cancel', 'e': 'engaged_toggle', 't': 'steer_required'}
+    if self.use_keyboard:
+      self.axes = {'gb': ['w', 's'], 'steer': ['a', 'd']}  # first key is positive
+    else:
+      self.max_axis_value = 255  # tune based on your joystick, 0 to this
+      self.axes = {'ABS_X': 'steer', 'ABS_Y': 'gb'}
+      self.button_map = {'BTN_TRIGGER': 'cancel', 'BTN_THUMB': 'engaged_toggle',
+                         'BTN_TOP': 'steer_required', 'BTN_THUMB2': 'reset'}
+
+  def get_event(self):
+    event = Event()
+    if self.use_keyboard:
+      key = kb.getch().lower()
+      if key in self.axes['gb'] + self.axes['steer']:  # if axis event
+        event.type = 'axis'
+        event.axis = 'gb' if key in self.axes['gb'] else 'steer'
+        if self.axes[event.axis].index(key) == 0:
+          event.value = self.axes_values[event.axis] + AXES_INCREMENT
+        else:
+          event.value = self.axes_values[event.axis] - AXES_INCREMENT
+
+      elif key in self.buttons:  # if button event
+        event.type = 'button'
+        event.button = self.buttons[key]
+
+    else:  # Joystick
+      joystick_event = get_gamepad()[0]
+      if joystick_event.code in self.axes:
+        event.type = 'axis'
+        event.axis = self.axes[joystick_event.code]
+        v = ((joystick_event.state / self.max_axis_value) - 0.5) * 2  # normalize value from -1 to 1
+        dz_comp = 1 / (1 - 0.03)
+        event.value = apply_deadzone(-v, 0.03) * dz_comp
+
+      # some buttons send events on rising and falling so only allow one state
+      elif joystick_event.code in self.button_map and joystick_event.state == 0:
+        event.type = 'button'
+        event.button = self.button_map[joystick_event.code]
+
+    return event  # returns empty Event if not mapped axis or button
+
+  def update(self):
+    event = self.get_event()
+    if event.type == 'axis':
+      self.axes_values[event.axis] = round(clip(event.value, -1., 1.), 3)
+    elif event.type == 'button':
+      if event.button == 'reset':
+        self.axes_values = {ax: 0. for ax in AXES}
+        self.btn_states = {btn: False for btn in BUTTONS}
+      else:
+        self.btn_states[event.button] = not self.btn_states[event.button]
+
+
+def send_thread(command_address, joystick):
+  zmq.Context._instance = None
+  context = zmq.Context.instance()
+
+  command_sock = context.socket(zmq.PULL)
+  command_sock.bind(command_address)
+
+  poller = zmq.Poller()
+  poller.register(command_sock, zmq.POLLIN)
   joystick_sock = messaging.pub_sock('testJoystick')
 
-  pygame.init()
-
-  # Used to manage how fast the screen updates
-  clock = pygame.time.Clock()
-
-  # Initialize the joysticks
-  pygame.joystick.init()
-
-  # Get count of joysticks
-  joystick_count = pygame.joystick.get_count()
-  if joystick_count > 1:
-    raise ValueError("More than one joystick attached")
-  elif joystick_count < 1:
-    raise ValueError("No joystick found")
-
-  # -------- Main Program Loop -----------
   while True:
-    # EVENT PROCESSING STEP
-    for event in pygame.event.get():  # User did something
-      if event.type == pygame.QUIT:  # If user clicked close
-        pass
-      # Available joystick events: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-      if event.type == pygame.JOYBUTTONDOWN:
-        print("Joystick button pressed.")
-      if event.type == pygame.JOYBUTTONUP:
-        print("Joystick button released.")
+    for sock in dict(poller.poll(POLL_RATE)):
+      msg = sock.recv_pyobj()
 
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
+      dat = messaging.new_message('testJoystick')
+      dat.testJoystick.axes = [msg.axes_values[a] for a in AXES]
+      dat.testJoystick.buttons = [msg.btn_states[btn] for btn in BUTTONS]
 
-    # Usually axis run in pairs, up/down for one, and left/right for
-    # the other.
-    axes = []
-    buttons = []
+      joystick_sock.send(dat.to_bytes())
 
-    for a in range(joystick.get_numaxes()):
-      axes.append(joystick.get_axis(a))
 
-    for b in range(joystick.get_numbuttons()):
-      buttons.append(bool(joystick.get_button(b)))
+def joystick_thread(use_keyboard):
+  Params().put_bool("JoystickDebugMode", True)
+  joystick = Joystick(use_keyboard=use_keyboard)
+  command_address = "ipc:///tmp/{}".format(uuid4())
 
-    dat = messaging.new_message('testJoystick')
-    dat.testJoystick.axes = axes
-    dat.testJoystick.buttons = buttons
-    joystick_sock.send(dat.to_bytes())
+  command_sock = zmq.Context.instance().socket(zmq.PUSH)
+  command_sock.connect(command_address)
 
-    # Limit to 100 frames per second
-    clock.tick(100)
+  send_thread_proc = multiprocessing.Process(target=send_thread, args=(command_address, joystick))
+  send_thread_proc.start()
+
+  if use_keyboard:
+    print('\nGas/brake control: `W` and `Sa` keys')
+    print('Steer control: `A` and `D` keys')
+    print('Buttons:\n'
+          '- `R`: Resets axes values\n'
+          '- `C`: Cancel cruise control\n'
+          '- `E`: Toggle enabled\n'
+          '- `T`: Steer required HUD')
+  else:
+    print('Using joystick!')
+
+  # Receive joystick/key events and send to joystick send thread
+  try:
+    while 1:
+      joystick.update()
+      print()
+      print(', '.join([f'{name}: {v}' for name, v in joystick.axes_values.items()]))
+      print(', '.join([f'{name}: {v}' for name, v in joystick.btn_states.items()]))
+      command_sock.send_pyobj(joystick)
+  except KeyboardInterrupt:
+    print('Interrupted, shutting down!')
+    send_thread_proc.terminate()
+
 
 if __name__ == "__main__":
-  joystick_thread()
+  args = sys.argv[1:]
+  use_keyboard = len(args) and args[0] == '--keyboard'
+  if not use_keyboard:
+    try:
+      from inputs import get_gamepad
+    except ImportError:
+      use_keyboard = True
+  joystick_thread(use_keyboard=use_keyboard)
