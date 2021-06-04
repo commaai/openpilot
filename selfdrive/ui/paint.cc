2ddfd829134effcf9420019a92984a3133a3f20b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-06-03 23:06:14 -0700

    limit icon flickering in UI (#21132)

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -211,7 +211,7 @@ static void ui_draw_vision_speed(UIState *s) {
 }
 
 static void ui_draw_vision_event(UIState *s) {
-  if ((*s->sm)["controlsState"].getControlsState().getEngageable()) {
+  if (s->scene.engageable) {
     // draw steering wheel
     const int radius = 96;
     const int center_x = s->viz_rect.right() - radius - bdr_s * 2;
@@ -226,8 +226,7 @@ static void ui_draw_vision_face(UIState *s) {
   const int radius = 96;
   const int center_x = s->viz_rect.x + radius + (bdr_s * 2);
   const int center_y = s->viz_rect.bottom() - footer_h / 2;
-  bool is_active = (*s->sm)["driverMonitoringState"].getDriverMonitoringState().getIsActiveMode();
-  ui_draw_circle_image(s, center_x, center_y, radius, "driver_face", is_active);
+  ui_draw_circle_image(s, center_x, center_y, radius, "driver_face", s->scene.dm_active);
 }
 
 static void ui_draw_vision_header(UIState *s) {
