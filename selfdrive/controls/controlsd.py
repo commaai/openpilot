8a125f96ee3173792721c4edc8f0005aeb0be110
openpilot <83732726+ace7jae@users.noreply.github.com>
2021-06-04 19:52:12 +0900

    Update controlsd.py
    gps 에러 메세지 수정

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -278,12 +278,12 @@ class Controls:
             self.events.add(evt)
 
     # TODO: fix simulator
-    if not SIMULATION:
-      if not NOSENSOR:
-        if not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000) and \
-          (not TICI or self.enable_lte_onroad):
+    # if not SIMULATION:
+      # if not NOSENSOR:
+        # if not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000) and \
+          # (not TICI or self.enable_lte_onroad):
           # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
-          self.events.add(EventName.noGps)
+         # self.events.add(EventName.noGps)
       if not self.sm.all_alive(self.camera_packets):
         self.events.add(EventName.cameraMalfunction)
       if self.sm['modelV2'].frameDropPerc > 20:
