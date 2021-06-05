211e64e8d408e5e990a978029f5792fe97153b00
ShaneSmiskol <shane@smiskol.com>
2021-06-04 17:10:20 -0700

    keep using EMS16 msg which is available on manuals, TCS13 is not

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -60,7 +60,8 @@ class CarState(CarStateBase):
       ret.gas = cp.vl["E_EMS11"]["Accel_Pedal_Pos"] / 256.
     else:
       ret.gas = cp.vl["EMS12"]["PV_AV_CAN"] / 100
-    ret.gasPressed = cp.vl["TCS13"]["DriverOverride"] == 1
+      ret.gasPressed = bool(cp.vl["EMS16"]["CF_Ems_AclAct"])
+    ret.gasPressed = ret.gasPressed or cp.vl["TCS13"]["DriverOverride"] == 1  # TCS msg not available on manuals
 
     # TODO: refactor gear parsing in function
     # Gear Selection via Cluster - For those Kia/Hyundai which are not fully discovered, we can use the Cluster Indicator for Gear Selection,
