da73e97fe1ec2cd34dc6b8f1383befa7a43ce424
Will <willhowe235@gmail.com>
2021-06-05 21:12:35 -0400

    Adding apt package to update_requirements file.

--- update_requirements.sh
+++ update_requirements.sh
@@ -6,7 +6,7 @@ if ! command -v pyenv &> /dev/null; then
   echo "please install pyenv ..."
   echo "https://github.com/pyenv/pyenv-installer"
   echo "example:"
-  echo "sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
+  echo "sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libcurl4-gnutls-dev"
   echo "curl https://pyenv.run | bash"
   echo "echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"' >> ~/.bashrc"
   echo "echo 'export PATH=\"\$PYENV_ROOT/bin:\$PYENV_ROOT/shims:\$PATH\"' >> ~/.bashrc"
