642f1cda456a44bb7483583e9af55043b39d5320
ShaneSmiskol <shane@smiskol.com>
2021-06-07 19:58:21 -0700

    split function by use_keyboard again, but simpler

--- tools/joystick/joystickd.py
+++ tools/joystick/joystickd.py
@@ -14,8 +14,8 @@ MAX_AXIS_VALUE = 255  # tune based on your joystick, 0 to this
 class Joystick:
   def __init__(self, use_keyboard=True):
     self.use_keyboard = use_keyboard
-    self.kb = KBHit()
     if self.use_keyboard:
+      self.kb = KBHit()
       self.buttons = {'c': 'cancel', 'e': 'engaged_toggle', 't': 'steer_required', 'r': 'reset'}
       self.axes = {'w': 'gb', 's': 'gb', 'a': 'steer', 'd': 'steer'}
     else:
@@ -25,34 +25,30 @@ class Joystick:
     self.axes_values = {ax: 0. for ax in self.axes.values()}
     self.btn_states = {btn: False for btn in self.buttons.values() if btn != 'reset'}
 
+  def handle_button(self, btn_name):
+    if btn_name == 'reset':
+      self.axes_values = {ax: 0. for ax in self.axes_values}
+      self.btn_states = {btn: False for btn in self.btn_states}
+    else:
+      self.btn_states[btn_name] = not self.btn_states[btn_name]
+
   def update(self):
     # Get key or joystick event
     if self.use_keyboard:
       key = self.kb.getch().lower()
-      state = 0
-    else:
-      joystick_event = get_gamepad()[0]
-      key = joystick_event.code
-      state = joystick_event.state  # state 0 is falling edge
-
-    # Button event
-    if key in self.buttons and state == 0:
-      btn_name = self.buttons[key]
-      if btn_name == 'reset':
-        self.axes_values = {ax: 0. for ax in self.axes_values}
-        self.btn_states = {btn: False for btn in self.btn_states}
-      else:
-        self.btn_states[btn_name] = not self.btn_states[btn_name]
-
-    # Axis event
-    elif key in self.axes:
-      axis = self.axes[key]
-      if self.use_keyboard:
+      if key in self.buttons:
+        self.handle_button(self.buttons[key])
+      elif key in self.axes:
         incr = AXES_INCREMENT if key in ['w', 'a'] else -AXES_INCREMENT  # these keys increment the axes positively
-        self.axes_values[axis] = clip(self.axes_values[axis] + incr, -1, 1)
-      else:
-        norm = interp(state, [0, MAX_AXIS_VALUE], [-1., 1.])
-        self.axes_values[axis] = norm if abs(norm) > 0.05 else 0.  # center can be noisy, deadzone of 5%
+        self.axes_values[self.axes[key]] = clip(self.axes_values[self.axes[key]] + incr, -1, 1)
+
+    else:  # Joystick
+      joystick_event = get_gamepad()[0]
+      if joystick_event.code in self.buttons and joystick_event.state == 0:  # state 0 is falling edge
+        self.handle_button(self.buttons[joystick_event.code])
+      elif joystick_event.code in self.axes:
+        norm = interp(joystick_event.state, [0, MAX_AXIS_VALUE], [-1., 1.])
+        self.axes_values[self.axes[joystick_event.code]] = norm if abs(norm) > 0.05 else 0.  # center can be noisy, deadzone of 5%
 
     dat = messaging.new_message('testJoystick')
     dat.testJoystick.axes = [self.axes_values[a] for a in self.axes_values]
