d82fa763d86750b31fe3edbb278d71565f6e3580
Dean Lee <deanlee3@gmail.com>
2021-06-10 15:03:34 +0800

    remove double semicolons (#21215)

--- selfdrive/modeld/thneed/compile.cc
+++ selfdrive/modeld/thneed/compile.cc
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
   float state[TEMPORAL_SIZE] = {0};
   float desire[DESIRE_LEN] = {0};
   float traffic_convention[TRAFFIC_CONVENTION_LEN] = {0};
-  float *input = (float*)calloc(0x1000000, sizeof(float));;
+  float *input = (float*)calloc(0x1000000, sizeof(float));
 
   mdl.addRecurrent(state, TEMPORAL_SIZE);
   mdl.addDesire(desire, DESIRE_LEN);
