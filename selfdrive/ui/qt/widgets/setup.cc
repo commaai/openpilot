e85fa7f1c933baafdecb4a52c16f2003215d423b
Willem Melching <willem.melching@gmail.com>
2021-06-15 14:10:44 +0200

    UI: check if dongle id is valid before calling API (#21275)

--- selfdrive/ui/qt/widgets/setup.cc
+++ selfdrive/ui/qt/widgets/setup.cc
@@ -101,14 +101,12 @@ PrimeUserWidget::PrimeUserWidget(QWidget* parent) : QWidget(parent) {
   )");
 
   // set up API requests
-  QString dongleId = QString::fromStdString(Params().get("DongleId"));
-  if (!dongleId.length()) {
-    return;
+  std::string dongleId = Params().get("DongleId");
+  if (util::is_valid_dongle_id(dongleId)) {
+    std::string url = "https://api.commadotai.com/v1/devices/" + dongleId + "/owner";
+    RequestRepeater *repeater = new RequestRepeater(this, QString::fromStdString(url), "ApiCache_Owner", 6);
+    QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &PrimeUserWidget::replyFinished);
   }
-
-  QString url = "https://api.commadotai.com/v1/devices/" + dongleId + "/owner";
-  RequestRepeater *repeater = new RequestRepeater(this, url, "ApiCache_Owner", 6);
-  QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &PrimeUserWidget::replyFinished);
 }
 
 void PrimeUserWidget::replyFinished(const QString &response) {
@@ -232,12 +230,14 @@ SetupWidget::SetupWidget(QWidget* parent) : QFrame(parent) {
   setSizePolicy(sp_retain);
 
   // set up API requests
-  QString dongleId = QString::fromStdString(Params().get("DongleId"));
-  QString url = "https://api.commadotai.com/v1.1/devices/" + dongleId + "/";
-  RequestRepeater* repeater = new RequestRepeater(this, url, "ApiCache_Device", 5);
+  std::string dongleId = Params().get("DongleId");
+  if (util::is_valid_dongle_id(dongleId)) {
+    std::string url = "https://api.commadotai.com/v1.1/devices/" + dongleId + "/";
+    RequestRepeater* repeater = new RequestRepeater(this, QString::fromStdString(url), "ApiCache_Device", 5);
 
-  QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &SetupWidget::replyFinished);
-  QObject::connect(repeater, &RequestRepeater::failedResponse, this, &SetupWidget::parseError);
+    QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &SetupWidget::replyFinished);
+    QObject::connect(repeater, &RequestRepeater::failedResponse, this, &SetupWidget::parseError);
+  }
   hide(); // Only show when first request comes back
 }
 
