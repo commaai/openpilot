196f96f0571338c66b2a90798e85c7319ca2ab31
Willem Melching <willem.melching@gmail.com>
2021-06-15 14:10:44 +0200

    UI: check if dongle id is valid before calling API (#21275)
    old-commit-hash: e85fa7f1c933baafdecb4a52c16f2003215d423b

--- selfdrive/ui/qt/widgets/drive_stats.cc
+++ selfdrive/ui/qt/widgets/drive_stats.cc
@@ -48,10 +48,12 @@ DriveStats::DriveStats(QWidget* parent) : QWidget(parent) {
   add_stats_layouts("ALL TIME", all_);
   add_stats_layouts("PAST WEEK", week_);
 
-  QString dongleId = QString::fromStdString(Params().get("DongleId"));
-  QString url = "https://api.commadotai.com/v1.1/devices/" + dongleId + "/stats";
-  RequestRepeater* repeater = new RequestRepeater(this, url, "ApiCache_DriveStats", 30);
-  QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &DriveStats::parseResponse);
+  std::string dongle_id = Params().get("DongleId");
+  if (util::is_valid_dongle_id(dongle_id)) {
+    std::string url = "https://api.commadotai.com/v1.1/devices/" + dongle_id + "/stats";
+    RequestRepeater* repeater = new RequestRepeater(this, QString::fromStdString(url), "ApiCache_DriveStats", 30);
+    QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &DriveStats::parseResponse);
+  }
 
   setStyleSheet(R"(QLabel {font-size: 48px; font-weight: 500;})");
 }
