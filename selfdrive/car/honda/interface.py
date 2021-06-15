8071111f59371c9aee16dee3d6fb87f42464894b
Greg Hogan <gregjhogan@gmail.com>
2021-06-15 05:00:57 -0700

    default carParams.stoppingControl to True (#21248)
    * hyundai has full brake control
    
    * set default stoppingControl to True
    
    * update ref
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -429,7 +429,6 @@ class CarInterface(CarInterfaceBase):
     ret.brakeMaxBP = [5., 20.]  # m/s
     ret.brakeMaxV = [1., 0.8]   # max brake allowed
 
-    ret.stoppingControl = True
     ret.startAccel = 0.5
 
     ret.steerActuatorDelay = 0.1
