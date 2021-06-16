2bf8ce0ab4dff94a7c01a9aebc7f983187822695
Willem Melching <willem.melching@gmail.com>
2021-06-16 15:01:46 +0200

    also print falling edges

--- selfdrive/debug/can_print_changes.py
+++ selfdrive/debug/can_print_changes.py
@@ -8,12 +8,13 @@ from common.realtime import sec_since_boot
 
 
 def can_printer(bus=0):
-  """ Collects messages and prints when a bit transitions that hasn't changed before
-  This is very usefull to find signals based on user triggered actions, such as blinkers and seatbelt.
+  """Collects messages and prints when a new bit transition is observed.
+  This is very useful to find signals based on user triggered actions, such as blinkers and seatbelt.
   Leave the script running until no new transitions are seen, then perform the action."""
   logcan = messaging.sub_sock('can')
 
-  msgs = defaultdict(int)
+  low_to_high = defaultdict(int)
+  high_to_low = defaultdict(int)
 
   while 1:
     can_recv = messaging.drain_sock(logcan, wait_for_one=True)
@@ -21,12 +22,21 @@ def can_printer(bus=0):
       for y in x.can:
         if y.src == bus:
           i = int.from_bytes(y.dat, byteorder='big')
-          j = msgs[y.address]
 
-          if (i | j) != j:  # Did any new bits flip?
-            msgs[y.address] = i | j
-            print(f"{sec_since_boot():.2f}\t{hex(y.address)} ({y.address})\t{binascii.hexlify(y.dat)}")
+          l_h = low_to_high[y.address]
+          h_l = high_to_low[y.address]
 
+          change = None
+          if (i | l_h) != l_h:
+            low_to_high[y.address] = i | l_h
+            change = "+"
+
+          if (~i | h_l) != h_l:
+            high_to_low[y.address] = ~i | h_l
+            change = "-"
+
+          if change:
+            print(f"{sec_since_boot():.2f}\t{hex(y.address)} ({y.address})\t{change}{binascii.hexlify(y.dat)}")
 
 
 if __name__ == "__main__":
