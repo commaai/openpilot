fcddfbdff94d74d855f1993e3304e6c5090842dd
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-06-23 15:39:18 -0700

    remove lte toggle

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -160,7 +160,6 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CommunityFeaturesToggle", PERSISTENT},
     {"ControlsReady", CLEAR_ON_MANAGER_START | CLEAR_ON_PANDA_DISCONNECT | CLEAR_ON_IGNITION_ON},
     {"DisableRadar", PERSISTENT}, // WARNING: THIS DISABLES AEB
-    {"EnableLteOnroad", PERSISTENT},
     {"EndToEndToggle", PERSISTENT},
     {"CompletedTrainingVersion", PERSISTENT},
     {"DisablePowerDown", PERSISTENT},
