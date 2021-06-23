cdfba105ed86ec37bd41565cb95796d653ee53bc
robbederks <robbe.derks@gmail.com>
2021-06-23 14:56:59 +0200

    Tesla AP2 port (#20074)
    * squashed the PR
    
    * remove AP passthrough
    
    * disengage on steer override
    
    * bump panda
    
    * add test routes
    
    * bump panda
    
    * tesla is still dashcam only
    
    * dashcam only
    
    * rerun CI
    
    * add to release files

---
+++ selfdrive/car/tesla/interface.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+from cereal import car
+from selfdrive.car.tesla.values import CAR
+from selfdrive.car import STD_CARGO_KG, gen_empty_fingerprint, scale_rot_inertia, scale_tire_stiffness
+from selfdrive.car.interfaces import CarInterfaceBase
+
+
+class CarInterface(CarInterfaceBase):
+  @staticmethod
+  def compute_gb(accel, speed):
+    # TODO: is this correct?
+    return accel
+
+  @staticmethod
+  def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=None):
+    ret = CarInterfaceBase.get_std_params(candidate, fingerprint)
+    ret.carName = "tesla"
+    ret.safetyModel = car.CarParams.SafetyModel.tesla
+
+    # There is no safe way to do steer blending with user torque,
+    # so the steering behaves like autopilot. This is not
+    # how openpilot should be, hence dashcamOnly
+    ret.dashcamOnly = True
+
+    ret.steerControlType = car.CarParams.SteerControlType.angle
+    ret.enableCamera = True
+    ret.openpilotLongitudinalControl = False
+    ret.communityFeature = True
+
+    ret.steerActuatorDelay = 0.1
+    ret.steerRateCost = 0.5
+
+    if candidate == CAR.AP2_MODELS:
+      ret.mass = 2100. + STD_CARGO_KG
+      ret.wheelbase = 2.959
+      ret.centerToFront = ret.wheelbase * 0.5
+      ret.steerRatio = 13.5
+    else:
+      raise ValueError(f"Unsupported car: {candidate}")
+
+    ret.rotationalInertia = scale_rot_inertia(ret.mass, ret.wheelbase)
+    ret.tireStiffnessFront, ret.tireStiffnessRear = scale_tire_stiffness(ret.mass, ret.wheelbase, ret.centerToFront)
+
+    return ret
+
+  def update(self, c, can_strings):
+    self.cp.update_strings(can_strings)
+    self.cp_cam.update_strings(can_strings)
+
+    ret = self.CS.update(self.cp, self.cp_cam)
+    ret.canValid = self.cp.can_valid and self.cp_cam.can_valid
+
+    events = self.create_common_events(ret)
+
+    ret.events = events.to_msg()
+    self.CS.out = ret.as_reader()
+    return self.CS.out
+
+  def apply(self, c):
+    can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators, c.cruiseControl.cancel)
+    self.frame += 1
+    return can_sends
