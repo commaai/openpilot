ba8320c46d6a3ca80cb6d71562c0b8ed97360b9e
ClockeNessMnstr <locke.dftc@gmail.com>
2021-06-24 15:15:42 -0400

    Extend filter reset function. Add HZ mode for setting time constant alpha with a cutoff frequency. add a reset function with new initial value make division safe with 0hz or RC=0 (possibly important with dynamic updates) RC=0 is passthrough 0hz will output x0

--- common/filter_simple.py
+++ common/filter_simple.py
@@ -1,13 +1,25 @@
+from math import pi
+
 class FirstOrderFilter:
-  # first order filter
-  def __init__(self, x0, RC, dt):
+  # first order low pass filter
+  def __init__(self, x0, RC, dt, hz_mode=False):
     self.x = x0
     self.dt = dt
+    self.hz = hz_mode
     self.update_alpha(RC)
 
-  def update_alpha(self, RC):
-    self.alpha = (self.dt / RC) / (1. + self.dt / RC)
+  def update_alpha(self, val):
+    if self.hz:
+      omega = val
+      RC_inv = (2 * pi * omega) 
+      self.alpha = self.dt * RC_inv / (self.dt * RC_inv + 1)
+    else:
+      RC = val
+      self.alpha = (self.dt) / (RC + self.dt)
 
   def update(self, x):
     self.x = (1. - self.alpha) * self.x + self.alpha * x
     return self.x
+
+  def reset(self, x0):
+    self.x = x0
