3fc9435afa478a018ee54a6adbf32586c6d3c3c2
deanlee <deanlee3@gmail.com>
2021-06-26 16:19:45 +0800

    space

--- selfdrive/ui/qt/util.cc
+++ selfdrive/ui/qt/util.cc
@@ -56,11 +56,10 @@ void ClickableWidget::mouseReleaseEvent(QMouseEvent *event) {
   emit clicked();
 }
 
-
 // Fix stylesheets
 void ClickableWidget::paintEvent(QPaintEvent *)  {
-    QStyleOption opt;
-    opt.init(this);
-    QPainter p(this);
-    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+  QStyleOption opt;
+  opt.init(this);
+  QPainter p(this);
+  style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
 }
