d7c7e8e2c49c47588be5959ec4f967a275814399
Comma Device <device@comma.ai>
2021-06-28 01:49:04 -0700

    fixup release3 build

--- release/build_release3.sh
+++ release/build_release3.sh
@@ -1,48 +1,34 @@
 #!/usr/bin/bash -e
 
-BUILD_DIR=/data/openpilot
-SOURCE_DIR=/data/openpilot_source
-
-ln -snf $BUILD_DIR /data/pythonpath
-
 export GIT_COMMITTER_NAME="Vehicle Researcher"
 export GIT_COMMITTER_EMAIL="user@comma.ai"
 export GIT_AUTHOR_NAME="Vehicle Researcher"
 export GIT_AUTHOR_EMAIL="user@comma.ai"
 export GIT_SSH_COMMAND="ssh -i /data/gitkey"
 
+SOURCE_DIR=/data/openpilot
+BUILD_DIR=/data/releasepilot
+
 echo "[-] Setting up repo T=$SECONDS"
-#rm -rf $BUILD_DIR
+rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 git init
-git remote add origin git@github.com:commaai/openpilot.git || true
-
-echo "[-] fetching public T=$SECONDS"
-git prune || true
-git remote prune origin || true
+git remote add origin git@github.com:commaai/openpilot.git
 
 echo "[-] git fetch origin T=$SECONDS"
-git fetch origin
-
+#git fetch origin
 git checkout -f -B release3-staging
-git reset --hard origin/devel
-git clean -xdf
 
 # remove everything except .git
 echo "[-] erasing old files T=$SECONDS"
 find . -maxdepth 1 -not -path './.git' -not -name '.' -not -name '..' -exec rm -rf '{}' \;
 
-# reset tree and get version
-cd $SOURCE_DIR
-git clean -xdf
-git checkout -- selfdrive/common/version.h
-
 # do the files copy
 echo "[-] copying files T=$SECONDS"
 cd $SOURCE_DIR
 cp -pR --parents $(cat release/files_common) $BUILD_DIR/
-cp -pR --parents installer/continue_openpilot.sh $BUILD_DIR/
+cp -pR --parents $(cat release/files_tici) $BUILD_DIR/
 
 # in the directory
 cd $BUILD_DIR
@@ -57,9 +43,10 @@ git add -f .
 git status
 git commit -a -m "openpilot v$VERSION release"
 
+# TODO: sign with release cert
 # Build panda firmware
 pushd panda/
-scons
+scons -U .
 mv board/obj/panda.bin.signed /tmp/panda.bin.signed
 popd
 
@@ -78,7 +65,7 @@ find . -name '*.os' -delete
 find . -name '*.pyc' -delete
 find . -name '__pycache__' -delete
 rm -rf panda/board panda/certs panda/crypto
-rm -rf .sconsign.dblite Jenkinsfile release/ apk/
+rm -rf .sconsign.dblite Jenkinsfile release/
 
 # Move back signed panda fw
 mkdir -p panda/board/obj
@@ -95,9 +82,9 @@ git add -f .
 git commit --amend -m "openpilot v$VERSION"
 
 if [ ! -z "$PUSH" ]; then
-  git remote set-url origin git@github.com:commaai/openpilot.git
-
-  git push -f origin release3-staging
+  echo "[-] pushing T=$SECONDS"
+  #git remote set-url origin git@github.com:commaai/openpilot.git
+  #git push -f origin release3-staging
 
   # Create dashcam
   #git rm selfdrive/car/*/carcontroller.py
