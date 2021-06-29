c05baf7426a49d8a719093f9036b5b78ca26ad1f
Harald Schafer <harald.the.engineer@gmail.com>
2021-06-28 21:11:45 -0700

    lets not go crazy

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -14,7 +14,7 @@ BRAKE_THRESHOLD_TO_PID = 0.2
 BRAKE_STOPPING_TARGET = 0.5  # apply at least this amount of brake to maintain the vehicle stationary
 
 RATE = 100.0
-DEFAULT_LONG_LAG = 0.3
+DEFAULT_LONG_LAG = 0.2
 
 
 def long_control_state_trans(active, long_control_state, v_ego, v_target, v_pid,
