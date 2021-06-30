70f0059be4e9fe10f0c78e3b35baa7f70ed0e9cc
HaraldSchafer <harald.the.engineer@gmail.com>
2021-06-29 23:29:06 -0500

    Revert DM model change (#21442)
    * Revert "tici patch"
    
    This reverts commit 09de331f18c7460ab4e939a245256ca69f765224.
    
    * Revert "dm e2e outputs added (#21210)"
    
    This reverts commit 46e3f4528fd0dc38e6da73843ebc85cefb926f74.
    
    * Revert "improved dm model (#21113)"
    
    This reverts commit 0f48fddf41cc7e5e63878b6bd584fc091482eb1b.

--- selfdrive/modeld/models/dmonitoring.h
+++ selfdrive/modeld/models/dmonitoring.h
@@ -7,7 +7,7 @@
 #include "selfdrive/modeld/models/commonmodel.h"
 #include "selfdrive/modeld/runners/run.h"
 
-#define OUTPUT_SIZE 40
+#define OUTPUT_SIZE 38
 
 typedef struct DMonitoringResult {
   float face_orientation[3];
@@ -24,8 +24,6 @@ typedef struct DMonitoringResult {
   float partial_face;
   float distracted_pose;
   float distracted_eyes;
-  float eyes_on_road;
-  float phone_use;
   float dsp_execution_time;
 } DMonitoringResult;
 
