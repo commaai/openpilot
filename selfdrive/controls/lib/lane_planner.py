de352159bde048d33b97be25bea3d2e0652cfc79
sshane <shane@smiskol.com>
2021-06-30 01:27:00 -0700

    Update selfdrive/controls/lib/lane_planner.py
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -26,7 +26,7 @@ class LanePlanner:
     self.ll_x = np.zeros((TRAJECTORY_SIZE,))
     self.lll_y = np.zeros((TRAJECTORY_SIZE,))
     self.rll_y = np.zeros((TRAJECTORY_SIZE,))
-    self.lane_width_estimate = FirstOrderFilter(3.7, 9.5, DT_MDL)
+    self.lane_width_estimate = FirstOrderFilter(3.7, 9.95, DT_MDL)
     self.lane_width_certainty = FirstOrderFilter(1.0, 0.95, DT_MDL)
     self.lane_width = 3.7
 
