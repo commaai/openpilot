be0430900e3ce85decd2ef0a6f7d63f3e796c5b4
Willem Melching <willem.melching@gmail.com>
2021-07-01 19:23:16 +0200

    Qt logging through cloudlog

--- selfdrive/ui/main.cc
+++ selfdrive/ui/main.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <QApplication>
 #include <QSslConfiguration>
 
@@ -5,8 +6,28 @@
 #include "selfdrive/ui/qt/qt_window.h"
 #include "selfdrive/ui/qt/util.h"
 #include "selfdrive/ui/qt/window.h"
+#include "selfdrive/common/swaglog.h"
+
+void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+  static std::map<QtMsgType, int> levels = {
+    {QtMsgType::QtDebugMsg, 10},
+    {QtMsgType::QtInfoMsg, 20},
+    {QtMsgType::QtWarningMsg, 30},
+    {QtMsgType::QtCriticalMsg, 40},
+    {QtMsgType::QtSystemMsg, 40},
+    {QtMsgType::QtFatalMsg, 50},
+  };
+
+  std::string file, function;
+  if (context.file != nullptr) file = context.file;
+  if (context.function != nullptr) function = context.function;
+
+  auto bts = msg.toUtf8();
+  cloudlog_e(levels[type], file.c_str(), context.line, function.c_str(), "%s", bts.constData());
+}
 
 int main(int argc, char *argv[]) {
+  qInstallMessageHandler(customMessageHandler);
   setQtSurfaceFormat();
 
   if (Hardware::EON()) {
