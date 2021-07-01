38cffc773f74b394d34f717280996674022b1707
ShaneSmiskol <shane@smiskol.com>
2021-06-30 22:25:42 -0700

    RC->rc

--- common/filter_simple.py
+++ common/filter_simple.py
@@ -1,12 +1,12 @@
 class FirstOrderFilter:
   # first order filter
-  def __init__(self, x0, RC, dt):
+  def __init__(self, x0, rc, dt):
     self.x = x0
     self.dt = dt
-    self.update_alpha(RC)
+    self.update_alpha(rc)
 
-  def update_alpha(self, RC):
-    self.alpha = (self.dt / RC) / (1. + self.dt / RC)
+  def update_alpha(self, rc):
+    self.alpha = (self.dt / rc) / (1. + self.dt / rc)
 
   def update(self, x):
     self.x = (1. - self.alpha) * self.x + self.alpha * x
