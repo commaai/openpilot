cd6e14b985b8aab76f4eb4cde7928c7000352ff1
Willem Melching <willem.melching@gmail.com>
2021-07-06 14:45:59 +0200

    add debug prints

--- selfdrive/camerad/cameras/camera_qcom2.cc
+++ selfdrive/camerad/cameras/camera_qcom2.cc
@@ -962,6 +962,9 @@ void switch_conversion_gain(CameraState *s) {
 static void set_camera_exposure(CameraState *s, float grey_frac) {
   // TODO: get stats from sensor?
   float target_grey = 0.4 - ((float)(s->analog_gain + 4*s->dc_gain_enabled) / 48.0f);
+
+  LOGE("target: %.2f, current: %.2f", target_grey, grey_frac);
+
   float exposure_factor = 1 + 30 * pow((target_grey - grey_frac), 3);
   exposure_factor = std::max(exposure_factor, 0.56f);
 
