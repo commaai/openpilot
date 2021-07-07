1cbc21d06a9a646c85eca3e4490139558b862019
Magnus Knutas <magnusknutas@gmail.com>
2021-07-07 11:57:05 +0200

    merge interface for AP1 and AP2

--- selfdrive/car/tesla/interface.py
+++ selfdrive/car/tesla/interface.py
@@ -30,12 +30,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = 0.1
     ret.steerRateCost = 0.5
 
-    if candidate == CAR.AP2_MODELS:
-      ret.mass = 2100. + STD_CARGO_KG
-      ret.wheelbase = 2.959
-      ret.centerToFront = ret.wheelbase * 0.5
-      ret.steerRatio = 13.5
-    elif candidate == CAR.AP1_MODELS:
+    if candidate in [CAR.AP2_MODELS, CAR.AP1_MODELS]:
       ret.mass = 2100. + STD_CARGO_KG
       ret.wheelbase = 2.959
       ret.centerToFront = ret.wheelbase * 0.5
