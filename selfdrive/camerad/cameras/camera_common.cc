695eacb38151f23566a8efbddbe20905f874dc04
Willem Melching <willem.melching@gmail.com>
2021-07-08 14:53:58 +0200

    set grey fracs in message

--- selfdrive/camerad/cameras/camera_common.cc
+++ selfdrive/camerad/cameras/camera_common.cc
@@ -182,6 +182,8 @@ void fill_frame_data(cereal::FrameData::Builder &framed, const FrameMetadata &fr
   framed.setIntegLines(frame_data.integ_lines);
   framed.setGain(frame_data.gain);
   framed.setHighConversionGain(frame_data.high_conversion_gain);
+  framed.setMeasuredGreyFraction(frame_data.measured_grey_fraction);
+  framed.setTargetGreyFraction(frame_data.target_grey_fraction);
   framed.setLensPos(frame_data.lens_pos);
   framed.setLensSag(frame_data.lens_sag);
   framed.setLensErr(frame_data.lens_err);
