6e8f439dcee072661d7c70ffe779a53f9c47f4f4
Harald Schafer <harald.the.engineer@gmail.com>
2021-07-09 16:32:04 -0700

    upload original
    old-commit-hash: f60d169bb4fc56110ffc420395a1fb7d28b45094

--- selfdrive/test/update_ci_routes.py
+++ selfdrive/test/update_ci_routes.py
@@ -4,7 +4,7 @@ import subprocess
 from azure.storage.blob import BlockBlobService
 
 from selfdrive.test.test_routes import routes as test_car_models_routes
-from selfdrive.test.process_replay.test_processes import segments as replay_segments
+from selfdrive.test.process_replay.test_processes import original_segments as replay_segments
 from xx.chffr.lib import azureutil  # pylint: disable=import-error
 from xx.chffr.lib.storage import _DATA_ACCOUNT_PRODUCTION, _DATA_ACCOUNT_CI, _DATA_BUCKET_PRODUCTION  # pylint: disable=import-error
 
