1ab68e18d35538563b2b6082679fa89f2b2239db
Harald Schafer <harald.the.engineer@gmail.com>
2021-07-09 15:50:59 -0700

    fix exclude pattern
    old-commit-hash: 472442111b6077ff758ad9e46828c7c97925927b

--- selfdrive/test/update_ci_routes.py
+++ selfdrive/test/update_ci_routes.py
@@ -29,7 +29,7 @@ def upload_route(path):
     "--recursive=false",
     "--overwrite=false",
     "--exclude-pattern=*/dcamera.hevc",
-    "--exclude-pattern=*/*.mkv",
+    "--exclude-pattern=*.mkv",
   ]
   subprocess.check_call(cmd)
 
