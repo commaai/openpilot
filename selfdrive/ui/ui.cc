8b6cf406abcb45496ca4f5d57291bde6e44c4910
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-07-10 14:40:11 -0700

    remove gps accuracy from sidebar

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -174,9 +174,6 @@ static void update_state(UIState *s) {
       scene.satelliteCount = data.getMeasurementReport().getNumMeas();
     }
   }
-  if (sm.updated("gpsLocationExternal")) {
-    scene.gpsAccuracy = sm["gpsLocationExternal"].getGpsLocationExternal().getAccuracy();
-  }
   if (sm.updated("carParams")) {
     scene.longitudinal_control = sm["carParams"].getCarParams().getOpenpilotLongitudinalControl();
   }
@@ -280,9 +277,8 @@ static void update_status(UIState *s) {
 
 QUIState::QUIState(QObject *parent) : QObject(parent) {
   ui_state.sm = std::make_unique<SubMaster, const std::initializer_list<const char *>>({
-    "modelV2", "controlsState", "liveCalibration", "radarState", "deviceState", "liveLocationKalman",
+    "modelV2", "controlsState", "liveCalibration", "radarState", "deviceState", "roadCameraState",
     "pandaState", "carParams", "driverMonitoringState", "sensorEvents", "carState", "ubloxGnss",
-    "gpsLocationExternal", "roadCameraState",
   });
 
   ui_state.fb_w = vwp_w;
--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -85,7 +85,6 @@ typedef struct UIScene {
 
   // gps
   int satelliteCount;
-  float gpsAccuracy;
 
   // modelV2
   float lane_line_probs[4];
