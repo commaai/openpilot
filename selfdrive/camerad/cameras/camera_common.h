63c9b5606358c435b7291a1b35ee4faf6a7870ba
Willem Melching <willem.melching@gmail.com>
2021-07-13 23:33:50 +0200

    tici: improve AE (#21493)
    * simplify histogram calculation
    
    * add debug prints
    
    * faster AE
    
    * revert that
    
    * lower log level
    
    * use minimum recomended gains from datasheet
    
    * use define
    
    * fix min gain
    
    * dynamic grey target
    
    * fix build
    
    * add low pass
    
    * more stable rounding
    
    * increase max gain
    
    * always enable HCG
    
    * correctly set min ev
    
    * fix analog gain
    
    * remove unused variable
    
    * fix build
    
    * move recomended gain into cost function
    
    * tweak cost function a bit
    
    * small LPF on desired ev
    
    * fix typo
    
    * lower ts
    
    * clean up defines
    
    * that was used in common
    
    * add cast
    
    * less cost is fine
    
    * fix tests
    
    * whitespace
    
    * filtering is broken
    
    * put back
    
    * clip gain changes instead
    
    * cost function tweaking
    
    * Only go below recomended gain when absolutely necessary
    
    * small penalty on changing gains

--- selfdrive/camerad/cameras/camera_common.h
+++ selfdrive/camerad/cameras/camera_common.h
@@ -135,7 +135,7 @@ typedef void (*process_thread_cb)(MultiCameraState *s, CameraState *c, int cnt);
 
 void fill_frame_data(cereal::FrameData::Builder &framed, const FrameMetadata &frame_data);
 kj::Array<uint8_t> get_frame_image(const CameraBuf *b);
-float set_exposure_target(const CameraBuf *b, int x_start, int x_end, int x_skip, int y_start, int y_end, int y_skip, int analog_gain, bool hist_ceil, bool hl_weighted);
+float set_exposure_target(const CameraBuf *b, int x_start, int x_end, int x_skip, int y_start, int y_end, int y_skip);
 std::thread start_process_thread(MultiCameraState *cameras, CameraState *cs, process_thread_cb callback);
 void common_process_driver_camera(SubMaster *sm, PubMaster *pm, CameraState *c, int cnt);
 
