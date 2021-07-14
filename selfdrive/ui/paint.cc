9e6586595d389b233ada32ff21e143b0b53f0a9d
deanlee <deanlee3@gmail.com>
2021-07-14 18:41:25 +0800

    no scissor

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -243,12 +243,8 @@ static void ui_draw_vision_header(UIState *s) {
 
 static void ui_draw_vision_frame(UIState *s) {
   // Draw video frames
-  glEnable(GL_SCISSOR_TEST);
   glViewport(s->video_rect.x, s->video_rect.y, s->video_rect.w, s->video_rect.h);
-  glScissor(s->viz_rect.x, s->viz_rect.y, s->viz_rect.w, s->viz_rect.h);
   draw_frame(s);
-  glDisable(GL_SCISSOR_TEST);
-
   glViewport(0, 0, s->fb_w, s->fb_h);
 }
 
