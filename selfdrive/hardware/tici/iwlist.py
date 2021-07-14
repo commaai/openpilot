b4b178c9dd5723c257ea2f01ddc6a565d8455f3b
Willem Melching <willem.melching@gmail.com>
2021-07-14 14:10:18 +0200

    add iwlist scan

---
+++ selfdrive/hardware/tici/iwlist.py
@@ -0,0 +1,43 @@
+import subprocess
+
+
+def scan(interface="wlan0"):
+  result = []
+  try:
+    r = subprocess.check_output(["iwlist", interface, "scan"], encoding='utf8')
+
+    mac = None
+
+    for line in r.split('\n'):
+      if "Address" in line:
+        # Add previous network in case no dBm signal level was seen
+        if mac is not None:
+          result.append({"mac": mac})
+          mac = None
+
+        mac = line.split(' ')[-1]
+      elif "dBm" in line:
+        try:
+          i = line.index('Signal level=') + 13
+          rss = int(line[i:].split(' ')[0])
+          result.append({"mac": mac, "rss": rss})
+          mac = None
+        except ValueError:
+          continue
+
+    return result
+
+  except Exception as e:
+    print(e)
+    return None
+
+
+if __name__ == "__main__":
+  import sys
+  import pprint
+
+  if len(sys.argv) > 1:
+    pprint.pp(scan(sys.argv[1]))
+  else:
+    pprint.pp(scan())
+
