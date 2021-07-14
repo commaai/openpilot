5d078190840adf2e6d50cdd3937ca407538886e6
HaraldSchafer <harald.the.engineer@gmail.com>
2021-07-14 10:53:14 -0700

    Taper off high speed accel (#21599)
    * max 0.2 diff from before to preserve bad tuned cars
    
    * update ref

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -19,8 +19,8 @@ from selfdrive.swaglog import cloudlog
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distracted
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.2, 1.2, 0.8]
-A_CRUISE_MAX_BP = [0., 15., 25.]
+A_CRUISE_MAX_VALS = [1.2, 1.2, 0.8, 0.6]
+A_CRUISE_MAX_BP = [0., 15., 25., 40.]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
