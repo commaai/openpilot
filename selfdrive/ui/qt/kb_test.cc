e7828df8798ae0b98ea59b7b3e86e882ff3fd79c
ShaneSmiskol <shane@smiskol.com>
2021-07-16 16:40:45 -0700

    spoof

--- selfdrive/ui/qt/kb_test.cc
+++ selfdrive/ui/qt/kb_test.cc
@@ -16,25 +16,26 @@
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
   QHBoxLayout *main_layout = new QHBoxLayout(this);
+//  installEventFilter(this);
 
   QPushButton *btn1 = new QPushButton("Btn 1");
   btn1->setAttribute(Qt::WA_AcceptTouchEvents);
-  btn1->installEventFilter(this);
+//  btn1->installEventFilter(this);
   main_layout->addWidget(btn1);
 
   QPushButton *btn2 = new QPushButton("Btn 2");
   btn2->setAttribute(Qt::WA_AcceptTouchEvents);
-  btn2->installEventFilter(this);
+//  btn2->installEventFilter(this);
   main_layout->addWidget(btn2);
 
   QPushButton *btn3 = new QPushButton("Btn 3");
   btn3->setAttribute(Qt::WA_AcceptTouchEvents);
-  btn3->installEventFilter(this);
+//  btn3->installEventFilter(this);
   main_layout->addWidget(btn3);
 
   QPushButton *btn4 = new QPushButton("Btn 4");
   btn4->setAttribute(Qt::WA_AcceptTouchEvents);
-  btn4->installEventFilter(this);
+//  btn4->installEventFilter(this);
   main_layout->addWidget(btn4);
 
   setStyleSheet(R"(
@@ -62,12 +63,20 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event) {
 //  qDebug() << "EVENT:" << event;
 //  qDebug() << "EVENT type:" << event->type();
   if (event->type() == QEvent::TouchBegin) {
-    qDebug() << "Touch event ignored:" << event;
-    event->ignore();
-    return true;
-  } else {
-    return QWidget::eventFilter(obj, event);
+    QTouchEvent *touchEvent = static_cast<QTouchEvent *>(event);
+    QPointF pos;
+    for (const auto tp : touchEvent->touchPoints()) {
+      pos.setX(tp.startPos().x());
+      pos.setY(tp.startPos().y());
+      break;
+    }
+    Qt::MouseButtons buttons;
+    Qt::KeyboardModifiers modifiers;
+    qDebug() << "Sent mouse event with pos:" << pos;
+    QMouseEvent eventNew(QEvent::MouseButtonPress, pos, Qt::LeftButton, buttons, modifiers);  // TODO fix screen pos
+    QApplication::sendEvent(obj, &eventNew);
   }
+  return QWidget::eventFilter(obj, event);
 }
 
 
@@ -76,6 +85,7 @@ int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   MainWindow window;
   setMainWindow(&window);
+  a.installEventFilter(&window);
 
   window.setAttribute(Qt::WA_AcceptTouchEvents);
 
