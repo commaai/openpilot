b85866c67d1a592fe00b85da268e783cc8cd3e4d
Willem Melching <willem.melching@gmail.com>
2021-07-16 16:09:44 +0200

    linter

--- selfdrive/ui/qt/demo.py
+++ selfdrive/ui/qt/demo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from PyQt5.QtWidgets import QApplication, QLabel  # pylint: disable=no-name-in-module
+from PyQt5.QtWidgets import QApplication, QLabel  # pylint: disable=no-name-in-module, import-error
 from selfdrive.ui.qt.python_helpers import set_main_window
 
 
