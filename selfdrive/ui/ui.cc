074f38171a2a1e840aa27a9e92dbc67cb5cd44b0
Willem Melching <willem.melching@gmail.com>
2021-07-19 10:37:24 +0200

    Fix backlight filter dt and set ts to 10 sec (#21649)
    old-commit-hash: 1cf8396e4c4f1c7024fe6febe4f9704f23721ca9

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -14,8 +14,8 @@
 #include "selfdrive/ui/paint.h"
 #include "selfdrive/ui/qt/qt_window.h"
 
-#define BACKLIGHT_DT 0.25
-#define BACKLIGHT_TS 2.00
+#define BACKLIGHT_DT 0.05
+#define BACKLIGHT_TS 10.00
 #define BACKLIGHT_OFFROAD 75
 
 
