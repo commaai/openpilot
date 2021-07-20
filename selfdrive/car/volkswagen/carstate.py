8558cb14fcdd97357014f06f3512d55df1e0cb6b
Jason Young <jyoung8607@gmail.com>
2021-07-20 14:12:38 -0400

    cleanup and scoping fixes

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -76,15 +76,8 @@ class CarState(CarStateBase):
     # We use the speed preference for OP.
     self.displayMetricUnits = not pt_cp.vl["Einheiten_01"]["KBI_MFA_v_Einheit_02"]
 
-    # Consume blind-spot monitoring info/warning LED states, if available. The
-    # info signal (LED on) is enabled whenever a vehicle is detected in the
-    # driver's blind spot. The warning signal (LED flashing) is enabled if the
-    # driver shows possibly hazardous intent toward a BSM detected vehicle, by
-    # setting the turn signal in that direction, or (for cars with factory Lane
-    # Assist) approaches the lane boundary in that direction. Size of the BSM
-    # detection box is dynamic based on speed and road curvature.
-    # Refer to VW Self Study Program 890253: Volkswagen Driver Assist Systems,
-    # pages 32-35.
+    # Consume blind-spot monitoring info/warning LED states, if available.
+    # Infostufe: BSM LED on, Warnung: BSM LED flashing
     if self.CP.enableBsm:
       ret.leftBlindspot = bool(pt_cp.vl["SWA_01"]["SWA_Infostufe_SWA_li"]) or bool(pt_cp.vl["SWA_01"]["SWA_Warnung_SWA_li"])
       ret.rightBlindspot = bool(pt_cp.vl["SWA_01"]["SWA_Infostufe_SWA_re"]) or bool(pt_cp.vl["SWA_01"]["SWA_Warnung_SWA_re"])
