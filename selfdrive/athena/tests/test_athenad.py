f96f4af2ffe3dee002c927ccb98c884580a4bcab
Greg Hogan <gregjhogan@gmail.com>
2021-07-24 10:08:32 -0700

    add test for logs to send

--- selfdrive/athena/tests/test_athenad.py
+++ selfdrive/athena/tests/test_athenad.py
@@ -14,6 +14,7 @@ from unittest import mock
 from websocket import ABNF
 from websocket._exceptions import WebSocketConnectionClosedException
 
+from selfdrive import swaglog
 from selfdrive.athena import athenad
 from selfdrive.athena.athenad import dispatcher
 from selfdrive.athena.tests.helpers import MockWebsocket, MockParams, MockApi, EchoSocket, with_http_server
@@ -203,6 +204,17 @@ class TestAthenadMethods(unittest.TestCase):
     finally:
       end_event.set()
       thread.join()
+
+  def test_get_logs_to_send_sorted(self):
+    fl = list()
+    for i in range(10):
+      fn = os.path.join(swaglog.SWAGLOG_DIR, f'swaglog.{i:010}')
+      Path(fn).touch()
+      fl.append(os.path.basename(fn))
+
+    # ensure the list is all logs accept most recent
+    sl = athenad.get_logs_to_send_sorted()
+    self.assertListEqual(sl, fl[:-1])
 
 if __name__ == '__main__':
   unittest.main()
