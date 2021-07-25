0964871239aed73d34ac7730d62d60139ecb1236
Greg Hogan <gregjhogan@gmail.com>
2021-07-24 20:11:29 -0700

    athenad: fix log sort (#21703)
    * athenad: fix log sort
    
    * add test for logs to send
    
    * use temp dir for logs
    
    * fix changing SWAGLOG_DIR
    
    * better way to patch SWAGLOG_DIR
    
    * fix grammar

--- selfdrive/athena/athenad.py
+++ selfdrive/athena/athenad.py
@@ -311,9 +311,8 @@ def get_logs_to_send_sorted():
     # assume send failed and we lost the response if sent more than one hour ago
     if not time_sent or curr_time - time_sent > 3600:
       logs.append(log_entry)
-  # return logs in order they should be sent
   # excluding most recent (active) log file
-  return sorted(logs[:-1])
+  return sorted(logs)[:-1]
 
 
 def log_handler(end_event):
@@ -332,7 +331,7 @@ def log_handler(end_event):
       # send one log
       curr_log = None
       if len(log_files) > 0:
-        log_entry = log_files.pop()
+        log_entry = log_files.pop() # newest log file
         cloudlog.debug(f"athena.log_handler.forward_request {log_entry}")
         try:
           curr_time = int(time.time())
