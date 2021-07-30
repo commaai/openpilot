0ffe920a863e5034f305812f14bf232ef087186c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-07-30 16:21:42 -0700

    UI: update toggles on show (#21812)

--- selfdrive/ui/qt/widgets/controls.h
+++ selfdrive/ui/qt/widgets/controls.h
@@ -112,14 +112,19 @@ class ParamControl : public ToggleControl {
 
 public:
   ParamControl(const QString &param, const QString &title, const QString &desc, const QString &icon, QWidget *parent = nullptr) : ToggleControl(title, desc, icon, false, parent) {
-    if (params.getBool(param.toStdString().c_str())) {
-      toggle.togglePosition();
-    }
+    key = param.toStdString();
     QObject::connect(this, &ToggleControl::toggleFlipped, [=](bool state) {
-      params.putBool(param.toStdString().c_str(), state);
+      params.putBool(key, state);
     });
   }
 
+  void showEvent(QShowEvent *event) override {
+    if (params.getBool(key) != toggle.on) {
+      toggle.togglePosition();
+    }
+  };
+
 private:
+  std::string key;
   Params params;
 };
