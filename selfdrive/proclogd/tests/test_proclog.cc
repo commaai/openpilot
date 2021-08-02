64ecf7230c16a2409d4e9c928a145b2c4f207825
deanlee <deanlee3@gmail.com>
2021-08-02 19:07:44 +0800

    test proc/self/stat

--- selfdrive/proclogd/tests/test_proclog.cc
+++ selfdrive/proclogd/tests/test_proclog.cc
@@ -3,71 +3,81 @@
 #include "selfdrive/common/util.h"
 #include "selfdrive/proclogd/proclog.h"
 
+TEST_CASE("Parser::pidStat") {
+  std::string self_stat = util::read_file("/proc/self/stat");
+  SECTION("self stat") {
+    auto stat = Parser::pidStat(self_stat);
+    REQUIRE((stat && stat->name == "test_proclog" && stat->pid == getpid()));
+  }
+  SECTION("name with space") {
+    std::string from = "(test_proclog)";
+    size_t start_pos = self_stat.find(from);
+    self_stat.replace(start_pos, from.length(), "(test proclog)");
+    auto stat = Parser::pidStat(self_stat);
+    REQUIRE((stat && stat->name == "test proclog" && stat->pid == getpid()));
+  }
+  SECTION("more") {
+    auto stat = Parser::pidStat(self_stat + " 1 2 3 4 5");
+    REQUIRE(stat);
+    REQUIRE(stat->name == "test_proclog" && stat->pid == getpid());
+  }
+  SECTION("less") {
+    auto stat = Parser::pidStat(self_stat.substr(0, 20));
+    REQUIRE(!stat);
+  }
+  SECTION("from empty string") {
+    auto stat = Parser::pidStat("");
+    REQUIRE(!stat);
+  }
+}
+
 TEST_CASE("Parser::procStat") {
   SECTION("from string") {
-    std::string stat = "cpu 5 1 2 3 4 5 6 7 8\ncpu0 0 1 2 3 4 5 6 7 8\ncpu1 0 1 2 3 4 5 6 7 8";
+    std::string stat = "cpu 0 0 0 0 0 0 0 0 0\ncpu0 1 2 3 4 5 6 7 8\ncpu1 1 2 3 4 5 6 7 8\nothers";
     std::istringstream stream(stat);
     auto stats = Parser::procStat(stream);
     REQUIRE(stats.size() == 2);
-    REQUIRE((stats[0].id == 0 && stats[0].utime == 0 && stats[0].ntime == 1));
-    REQUIRE((stats[1].id == 1 && stats[0].utime == 0 && stats[0].ntime == 1));
+    REQUIRE((stats[0].id == 0 && stats[0].utime == 1 && stats[0].ntime == 2));
+    REQUIRE((stats[1].id == 1 && stats[0].utime == 1 && stats[0].ntime == 2));
   }
-  SECTION("read from /proc/stat") {
-    std::string stat = util::read_file("/proc/stat");
-    std::istringstream stream(stat);
+  SECTION("from /proc/stat") {
+    std::istringstream stream(util::read_file("/proc/stat"));
     auto stats = Parser::procStat(stream);
     REQUIRE(stats.size() == sysconf(_SC_NPROCESSORS_ONLN));
     for (int i = 0; i < stats.size(); ++i) {
       REQUIRE(stats[i].id == i);
     }
   }
+  SECTION("from empty string") {
+    std::istringstream stream("");
+    REQUIRE(Parser::procStat(stream).empty());
+  }
 }
 
 TEST_CASE("Parser::memInfo") {
   SECTION("from string") {
-    std::string stat = "MemTotal:    1024 kb\nMemFree:    10 kb\n";
-    std::istringstream stream(stat);
+    std::istringstream stream("MemTotal:    1024 kb\nMemFree:    10 kb\n");
     auto stats = Parser::memInfo(stream);
     REQUIRE((stats["MemTotal:"] = 1024 * 1024 && stats["MemFree:"] == 1024 * 10));
   }
   SECTION("from wrong string") {
-    std::string stat = "MemTotal:   kb \nMemFree:    10 kb\n";
-    std::istringstream stream(stat);
+    std::istringstream stream("MemTotal:   kb \nMemFree:    10 kb\n");
     auto stats = Parser::memInfo(stream);
     REQUIRE(stats.find("MemTotal:") == stats.end());
     REQUIRE(stats["MemFree:"] == 1024 * 10);
   }
-  SECTION("read from /proc/ProcStat") {
-    std::string stat = util::read_file("/proc/meminfo");
-    std::istringstream stream(stat);
+  SECTION("from /proc/ProcStat") {
+    std::istringstream stream(util::read_file("/proc/meminfo"));
     auto stats = Parser::memInfo(stream);
     std::string keys[] = {"MemTotal:", "MemFree:", "MemAvailable:", "Buffers:", "Cached:", "Active:", "Inactive:", "Shmem:"};
     for (auto &key : keys) {
       REQUIRE(stats.find(key) != stats.end());
     }
   }
-}
-
-TEST_CASE("Parser::pidStat") {
-  SECTION("normal") {
-    std::string str = "167151 (cat) R 7297 167151 7297 34818 167151 4194304 92 0 0 0 0 0 0 0 20 0 1 0 303166 11563008 130 18446744073709551615 93994557333504 93994557359153 140732482410736 0 0 0 0 0 0 0 0 0 17 20 0 0 0 0 0 93994557377168 93994557378752 93994559090688 140732482412879 140732482412899 140732482412899 140732482416619 0";
-    auto stat = Parser::pidStat(str);
-    REQUIRE((stat && stat->name == "cat" && stat->pid == 167151));
-  }
-  SECTION("name with space") {
-    std::string str = "167151 (cat 123) R 7297 167151 7297 34818 167151 4194304 92 0 0 0 0 0 0 0 20 0 1 0 303166 11563008 130 18446744073709551615 93994557333504 93994557359153 140732482410736 0 0 0 0 0 0 0 0 0 17 20 0 0 0 0 0 93994557377168 93994557378752 93994559090688 140732482412879 140732482412899 140732482412899 140732482416619 0";
-    auto stat = Parser::pidStat(str);
-    REQUIRE((stat && stat->name == "cat 123" && stat->pid == 167151));
-  }
-  SECTION("less") {
-    std::string str = "167151 (cat 123) R 7297 167151";
-    auto stat = Parser::pidStat(str);
-    REQUIRE(!stat);
-  }
-  SECTION("more") {
-    std::string str = "167151 (cat 123) R 7297 167151 7297 34818 167151 4194304 92 0 0 0 0 0 0 0 20 0 1 0 303166 11563008 130 18446744073709551615 93994557333504 93994557359153 140732482410736 0 0 0 0 0 0 0 0 0 17 20 0 0 0 0 0 93994557377168 93994557378752 93994559090688 140732482412879 140732482412899 140732482412899 140732482416619 0 1 2 3";
-    auto stat = Parser::pidStat(str);
-    REQUIRE(stat);
+  SECTION("from empty string") {
+    std::istringstream stream("");
+    auto stats = Parser::memInfo(stream);
+    REQUIRE(stats.empty());
   }
 }
 
@@ -81,13 +91,12 @@ TEST_CASE("Parser::cmdline") {
     REQUIRE(cmds[2] == "c");
   }
   SECTION("multiple null") {
-    std::string str("a\0\0\0b\0", 7);
+    std::string str("a\0\0b\0", 6);
     auto cmds = Parser::cmdline(str);
-    REQUIRE(cmds.size() == 4);
+    REQUIRE(cmds.size() == 3);
     REQUIRE(cmds[0] == "a");
     REQUIRE(cmds[1] == "");
-    REQUIRE(cmds[2] == "");
-    REQUIRE(cmds[3] == "b");
+    REQUIRE(cmds[2] == "b");
   }
   SECTION("multiple null terminate") {
     std::string str("a\0b\0c\0\0\0", 9);
@@ -95,6 +104,10 @@ TEST_CASE("Parser::cmdline") {
     REQUIRE(cmds[0] == "a");
     REQUIRE(cmds[1] == "b");
     REQUIRE(cmds[2] == "c");
+  }
+  SECTION("from empty string") {
+    auto cmds = Parser::cmdline("");
+    REQUIRE(cmds.empty());
   }
 }
 
