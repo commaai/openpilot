8354cfc53c023bfd20b7887c7ff4560fb9615b4f
George Hotz <72895+geohot@users.noreply.github.com>
2021-08-02 20:16:38 -0700

    loggerd: implement DONT_LOG flag (#21832)
    * implement DONT_LOG flag
    
    * better typing

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -146,7 +146,7 @@ private:
 };
 
 std::unordered_map<std::string, uint32_t> keys = {
-    {"AccessToken", CLEAR_ON_MANAGER_START},
+    {"AccessToken", CLEAR_ON_MANAGER_START | DONT_LOG},
     {"ApiCache_DriveStats", PERSISTENT},
     {"ApiCache_Device", PERSISTENT},
     {"ApiCache_Owner", PERSISTENT},
@@ -191,7 +191,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LastUpdateException", PERSISTENT},
     {"LastUpdateTime", PERSISTENT},
     {"LiveParameters", PERSISTENT},
-    {"MapboxToken", PERSISTENT},
+    {"MapboxToken", PERSISTENT | DONT_LOG},
     {"NavDestination", CLEAR_ON_MANAGER_START | CLEAR_ON_IGNITION_OFF},
     {"NavSettingTime24h", PERSISTENT},
     {"OpenpilotEnabledToggle", PERSISTENT},
@@ -244,6 +244,10 @@ Params::Params(const std::string &path) : params_path(path) {
 
 bool Params::checkKey(const std::string &key) {
   return keys.find(key) != keys.end();
+}
+
+ParamKeyType Params::getKeyType(const std::string &key) {
+  return static_cast<ParamKeyType>(keys[key]);
 }
 
 int Params::put(const char* key, const char* value, size_t value_size) {
