83b7356ce20d23b9d7fcfbd6030451cdf21a3ab9
Harald Schafer <harald.the.engineer@gmail.com>
2021-08-04 16:43:01 -0700

    wind brake sooner

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -190,7 +190,7 @@ class CarController():
 
         else:
           pcm_override = True
-          wind_brake = interp(CS.out.vEgo, [0.0, 3.0, 40.0], [0.0, 0.0, 0.1])
+          wind_brake = interp(CS.out.vEgo, [0.0, 3.0, 25.0], [0.0, 0.0, 0.1])
           apply_brake = clip(self.brake_last - wind_brake, 0.0, 1.0)
           apply_brake = int(clip(apply_brake * P.BRAKE_MAX, 0, P.BRAKE_MAX - 1))
           pump_on, self.last_pump_ts = brake_pump_hysteresis(apply_brake, self.apply_brake_last, self.last_pump_ts, ts)
