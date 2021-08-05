bd83d192e21d2132c3d24a863e8ef0ffc4c6b797
HaraldSchafer <harald.the.engineer@gmail.com>
2021-08-05 12:26:31 -0700

    Civic experiments2 (#21862)
    * control on throttle
    
    * throttle modifier

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -162,11 +162,11 @@ class CarController():
       apply_accel = interp(accel, P.NIDEC_ACCEL_LOOKUP_BP, P.NIDEC_ACCEL_LOOKUP_V)
 
 
-    #speed_mod_mult = interp(CS.out.vEgo, [0.0, 1.0], [2.0, 1.0])
-    pcm_speed = max(0.0, CS.out.vEgo + 2.0 * apply_accel)
+    throttle_mult = interp(CS.out.vEgo, [0.0, 2.0, 20., 40.], [2.0, .25, .5, 1.0])
+    pcm_speed = 100.0
 
     # This is needed otherwise accel to decel is not smooth
-    pcm_accel = int(clip(apply_accel/2.0, 0.0, 1.0) * 0xc6)
+    pcm_accel = int(clip(throttle_mult*apply_accel, 0.0, 1.0) * 0xc6)
 
     if not CS.CP.openpilotLongitudinalControl:
       if (frame % 2) == 0:
@@ -190,8 +190,8 @@ class CarController():
 
         else:
           pcm_override = True
-          wind_brake = interp(CS.out.vEgo, [0.0, 3.0, 25.0], [0.0, 0.0, 0.1])
-          apply_brake = clip(self.brake_last - wind_brake, 0.0, 1.0)
+          #wind_brake = interp(CS.out.vEgo, [0.0, 3.0, 25.0], [0.0, 0.0, 0.1])
+          apply_brake = clip(self.brake_last, 0.0, 1.0)
           apply_brake = int(clip(apply_brake * P.BRAKE_MAX, 0, P.BRAKE_MAX - 1))
           pump_on, self.last_pump_ts = brake_pump_hysteresis(apply_brake, self.apply_brake_last, self.last_pump_ts, ts)
           can_sends.append(hondacan.create_brake_command(self.packer, apply_brake, pump_on,
