dd72795d60480317e1ff1415be6f749aa937dd62
Harald Schafer <harald.the.engineer@gmail.com>
2021-08-10 16:58:57 -0700

    data driven

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -166,12 +166,16 @@ class CarController():
       #pcm_speed = pcm_speed
       wind_brake = 0.0
     else:
-      pcm_accel = int(1.0 * 0xc6)
       wind_brake = interp(CS.out.vEgo, [0.0, 1.0, 20.0], [0.0, 0.0, 0.1])
       if apply_accel > 0:
         pcm_speed = 100
+        CIVIC_MAX_ACCEL_VALS = [0.5, 2.4, 1.4, 0.6]
+        CIVIC_MAX_ACCEL_BP = [0, 4.0, 10., 20.]
+        max_accel = interp(CS.out.vEgo, CIVIC_MAX_ACCEL_BP, CIVIC_MAX_ACCEL_VALS)
+        pcm_accel = int((apply_accel/max_accel) * 0xc6)
       else:
         pcm_speed = max(0.0, CS.out.vEgo* (1.0 - 10 * wind_brake))
+        pcm_accel = int(0)
 
 
 
