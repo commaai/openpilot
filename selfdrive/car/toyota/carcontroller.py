56b7ff24f736d11ba6b42c4d0a3fce2c7666dbe9
Shane Smiskol <shane@smiskol.com>
2021-08-10 18:00:26 -0700

    toyota: stop steering to prevent fault

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -87,7 +87,7 @@ class CarController():
     self.steer_rate_limited = new_steer != apply_steer
 
     # Cut steering while we're in a known fault state (2s)
-    if not enabled or CS.steer_state in [9, 25]:
+    if not enabled or CS.steer_state in [9, 25] or abs(CS.out.steeringRateDeg) > 100:
       apply_steer = 0
       apply_steer_req = 0
     else:
