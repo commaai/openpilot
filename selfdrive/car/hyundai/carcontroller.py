44e376dae3323cfc615ce119c1796bfd146af472
Greg Lincoln <greg@greglincoln.com>
2021-08-12 13:45:37 -0400

    Kia Niro HEV 2021 port (#21779)
    * Initial Niro HEV 2021 port.
    
    * Add test route and update readme.
    
    * Tweak configuration.
    old-commit-hash: 1bb46d0cc863b50629ce7ce67017a5646bedb443

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -77,8 +77,8 @@ class CarController():
         self.last_resume_frame = frame
 
     # 20 Hz LFA MFA message
-    if frame % 5 == 0 and self.car_fingerprint in [CAR.SONATA, CAR.PALISADE, CAR.IONIQ, CAR.KIA_NIRO_EV, CAR.KONA_EV,
-                                                   CAR.IONIQ_EV_2020, CAR.IONIQ_PHEV, CAR.KIA_CEED, CAR.KIA_SELTOS,
+    if frame % 5 == 0 and self.car_fingerprint in [CAR.SONATA, CAR.PALISADE, CAR.IONIQ, CAR.KIA_NIRO_EV, CAR.KIA_NIRO_HEV_2021,
+                                                   CAR.IONIQ_EV_2020, CAR.IONIQ_PHEV, CAR.KIA_CEED, CAR.KIA_SELTOS, CAR.KONA_EV,
                                                    CAR.ELANTRA_2021, CAR.ELANTRA_HEV_2021, CAR.SONATA_HYBRID, CAR.KONA_HEV]:
       can_sends.append(create_lfahda_mfc(self.packer, enabled))
 
