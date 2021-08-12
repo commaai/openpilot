9c3673c44d1078248e0501d2733e3bbad513b6c5
Harald Schafer <harald.the.engineer@gmail.com>
2021-08-12 13:50:47 -0700

    accel scale is 4

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -472,7 +472,7 @@ class Controls:
     else:
       lac_log = log.ControlsState.LateralDebugState.new_message()
       if self.sm.rcv_frame['testJoystick'] > 0 and self.active:
-        actuators.accel = 44**clip(self.sm['testJoystick'].axes[0], -1, 1)
+        actuators.accel = 4.0*clip(self.sm['testJoystick'].axes[0], -1, 1)
 
         steer = clip(self.sm['testJoystick'].axes[1], -1, 1)
         # max angle is 45 for angle-based cars
