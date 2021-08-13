5f6e4d5ba416d9756812a7a26ebe8ba4a6b8bb88
Willem Melching <willem.melching@gmail.com>
2021-08-13 12:43:12 +0200

    add test back

--- common/tests/test_file_helpers.py
+++ common/tests/test_file_helpers.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from uuid import uuid4
 
+from common.file_helpers import atomic_write_on_fs_tmp
 from common.file_helpers import atomic_write_in_dir
 
 
@@ -14,6 +15,9 @@ class TestFileHelpers(unittest.TestCase):
     with open(path) as f:
       self.assertEqual(f.read(), "test")
     os.remove(path)
+
+  def test_atomic_write_on_fs_tmp(self):
+    self.run_atomic_write_func(atomic_write_on_fs_tmp)
 
   def test_atomic_write_in_dir(self):
     self.run_atomic_write_func(atomic_write_in_dir)
