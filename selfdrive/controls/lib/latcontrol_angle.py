8c03e4eec599597d08654508f1cce0415816276f
Shane Smiskol <shane@smiskol.com>
2021-08-19 00:25:08 -0600

    fix

--- selfdrive/controls/lib/latcontrol_angle.py
+++ selfdrive/controls/lib/latcontrol_angle.py
@@ -5,12 +5,6 @@ from selfdrive.controls.lib.latcontrol import LatControl
 
 
 class LatControlAngle(LatControl):
-  def __init__(self, CP):
-    super().__init__(CP)
-
-  def reset(self):
-    super().reset()
-
   def update(self, active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate):
     angle_log = log.ControlsState.LateralAngleState.new_message()
 
