e6373847c1915844670a1c01bd5298357edf4f07
John Belmonte <john@neggie.net>
2021-08-22 18:55:05 +0900

    remove redundant long parameters

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -187,10 +187,6 @@ class CarInterface(CarInterfaceBase):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
       tire_stiffness_factor = 0.75
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.05]]
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
 
     elif candidate == CAR.HRV:
       stop_and_go = False
