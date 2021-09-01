fc61f6c4a6bd4d5d4b4574705f9b8b63e41cebea
Willem Melching <willem.melching@gmail.com>
2021-09-01 14:28:50 -0700

    Ensure that first segment starts with all cameras on the same frameId (#22096)
    * test that segment starts with the same frame id
    
    * improve test
    
    * add sync logic
    
    * atomic max
    
    * review comments

--- selfdrive/loggerd/loggerd.cc
+++ selfdrive/loggerd/loggerd.cc
@@ -46,7 +46,8 @@ const int DCAM_BITRATE = Hardware::TICI() ? MAIN_BITRATE : 2500000;
 
 #define NO_CAMERA_PATIENCE 500 // fall back to time-based rotation if all cameras are dead
 
-const int SEGMENT_LENGTH = getenv("LOGGERD_TEST") ? atoi(getenv("LOGGERD_SEGMENT_LENGTH")) : 60;
+const bool LOGGERD_TEST = getenv("LOGGERD_TEST");
+const int SEGMENT_LENGTH = LOGGERD_TEST ? atoi(getenv("LOGGERD_SEGMENT_LENGTH")) : 60;
 
 ExitHandler do_exit;
 
@@ -112,6 +113,12 @@ struct LoggerdState {
   std::atomic<int> waiting_rotate;
   int max_waiting = 0;
   double last_rotate_tms = 0.;
+
+  // Sync logic for startup
+  std::atomic<bool> encoders_synced;
+  std::atomic<int> encoders_ready;
+  std::atomic<uint32_t> start_frame_id;
+  std::atomic<uint32_t> latest_frame_id;
 };
 LoggerdState s;
 
@@ -124,9 +131,11 @@ void encoder_thread(const LogCameraInfo &cam_info) {
   std::vector<Encoder *> encoders;
   VisionIpcClient vipc_client = VisionIpcClient("camerad", cam_info.stream_type, false);
 
+  bool ready = false;
+
   while (!do_exit) {
     if (!vipc_client.connect(false)) {
-      util::sleep_for(100);
+      util::sleep_for(5);
       continue;
     }
 
@@ -151,6 +160,23 @@ void encoder_thread(const LogCameraInfo &cam_info) {
       if (buf == nullptr) continue;
 
       if (cam_info.trigger_rotate) {
+        if (!ready) {
+          LOGE("%s encoder ready", cam_info.filename);
+          ++s.encoders_ready;
+          ready = true;
+        }
+
+        if (!s.encoders_synced) {
+          update_max_atomic(s.latest_frame_id, extra.frame_id);
+          continue;
+        } else {
+          // Wait for all encoders to reach the same frame id
+          if (extra.frame_id < s.start_frame_id) {
+            LOGE("%s waiting for frame %d, cur %d", cam_info.filename, s.start_frame_id.load(), extra.frame_id);
+            continue;
+          }
+        }
+
         s.last_camera_seen_tms = millis_since_boot();
       }
 
@@ -262,7 +288,8 @@ void rotate_if_needed() {
 
   double tms = millis_since_boot();
   if ((tms - s.last_rotate_tms) > SEGMENT_LENGTH * 1000 &&
-      (tms - s.last_camera_seen_tms) > NO_CAMERA_PATIENCE) {
+      (tms - s.last_camera_seen_tms) > NO_CAMERA_PATIENCE &&
+      !LOGGERD_TEST) {
     LOGW("no camera packet seen. auto rotating");
     logger_rotate();
   }
@@ -312,6 +339,15 @@ int main(int argc, char** argv) {
   uint64_t msg_count = 0, bytes_count = 0;
   double start_ts = millis_since_boot();
   while (!do_exit) {
+    // Check if all encoders are ready and start encoding at the same time
+    if (!s.encoders_synced && (s.encoders_ready == s.max_waiting)) {
+      // Small margin in case one of the encoders already dropped the next frame
+      s.start_frame_id = s.latest_frame_id + 2;
+      s.encoders_synced = true;
+      LOGE("starting encoders at frame id %d", s.start_frame_id.load());
+    }
+
+
     // poll for new messages on all sockets
     for (auto sock : poller->poll(1000)) {
       // drain socket
