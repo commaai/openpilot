3150250f0034ca68ae60e54b3394b3ec2ee0f882
sshane <shane@smiskol.com>
2021-09-01 16:33:06 -0500

    UI: roaming toggle (#21750)
    * roaming toggle
    
    * clean up
    
    * only tici
    
    * fix order
    
    * move to wifiManager
    
    * should be working
    
    * clean up
    
    clean up
    
    * use map icon
    
    * move to advanced settings
    
    * clean up
    
    * temporarily change home-only
    
    * keep setting for now
    
    * set both to defaults in case ui doesn't start
    
    * knownConnections stores conn names
    
    * Revert "knownConnections stores conn names"
    
    This reverts commit 4466a029a38f8c89876fad3c221e95e8fc572ded.
    
    * save lteConnectionPath instead
    
    * use the if statement
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 6fad0055b464492d6457597116125f50155b74a3

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -150,6 +150,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"GitRemote", PERSISTENT},
     {"GithubSshKeys", PERSISTENT},
     {"GithubUsername", PERSISTENT},
+    {"GsmRoaming", PERSISTENT},
     {"HardwareSerial", PERSISTENT},
     {"HasAcceptedTerms", PERSISTENT},
     {"IsDriverViewEnabled", CLEAR_ON_MANAGER_START},
