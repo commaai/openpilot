ae5c6aeae69f94630e29f672afc59ccbc0217df3
Dean Lee <deanlee3@gmail.com>
2021-09-02 04:30:33 +0800

    HomeWindow: cleanup mousePressEvent (#21683)
    * cleanup mousePressEvent
    
    * show/hide map in onroad
    
    * merge master
    old-commit-hash: 579fb8072a002dc0794fe8584ca0f0edc6682a35

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -54,14 +54,15 @@ class OnroadWindow : public QWidget {
 
 public:
   OnroadWindow(QWidget* parent = 0);
-  QWidget *map = nullptr;
+  bool isMapVisible() const { return map && map->isVisible(); }
 
 private:
   void paintEvent(QPaintEvent *event);
-
+  void mousePressEvent(QMouseEvent* e) override;
   OnroadAlerts *alerts;
   NvgWindow *nvg;
   QColor bg = bg_colors[STATUS_DISENGAGED];
+  QWidget *map = nullptr;
   QHBoxLayout* split;
 
 signals:
