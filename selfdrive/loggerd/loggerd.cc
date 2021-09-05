c75ce3a56bd9663a8dc83dfd314fb6e11791f3b4
deanlee <deanlee3@gmail.com>
2021-09-05 19:55:38 +0800

    clenaup

--- selfdrive/loggerd/loggerd.cc
+++ selfdrive/loggerd/loggerd.cc
@@ -123,27 +123,25 @@ struct LoggerdState {
 };
 LoggerdState s;
 
+
+// Wait for all encoders to reach the same frame id
 bool sync_encoders(CameraType cam_type, uint32_t frame_id) {
   std::unique_lock lk(s.sync_lock);
   if (s.max_waiting > 1 && s.encoders_ready != s.max_waiting) {
-    if (s.latest_frame_id < frame_id) {
-      s.latest_frame_id = frame_id;
-    }
-    if (!s.camera_ready[cam_type]) {
-      s.camera_ready[cam_type] = true;
+    if (std::exchange(s.camera_ready[cam_type], true) == false) {
       ++s.encoders_ready;
       LOGE("camera %d encoder ready", cam_type);
+    }
+    if (s.latest_frame_id < frame_id) {
+      s.latest_frame_id = frame_id;
     }
     return false;
   } else {
     // Small margin in case one of the encoders already dropped the next frame
     uint32_t start_frame_id = s.latest_frame_id + 2;
-    // Wait for all encoders to reach the same frame id
-    if (frame_id < start_frame_id) {
-      LOGE("camera %d waiting for frame %d, cur %d", cam_type, start_frame_id, frame_id);
-      return false;
-    }
-    return true;
+    bool synced = frame_id >= start_frame_id;
+    if (!synced) LOGE("camera %d waiting for frame %d, cur %d", cam_type, start_frame_id, frame_id);
+    return synced;
   }
 }
 
