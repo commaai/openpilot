38c496222112abab9dbb3f83aae6d7800634abbc
deanlee <deanlee3@gmail.com>
2021-09-07 12:29:21 +0800

    use future

--- selfdrive/loggerd/tests/test_logging.cc
+++ selfdrive/loggerd/tests/test_logging.cc
@@ -1,29 +1,28 @@
+#include <future>
+
 #include "catch2/catch.hpp"
 #include "selfdrive/common/util.h"
 #include "selfdrive/loggerd/logger.h"
 #include "selfdrive/loggerd/logging.h"
 
 TEST_CASE("sync encoders") {
-  auto thread_func = [](LoggerdState *s, CameraType cam_type, int frame_id, int start_frame[]) {
+  auto thread_func = [](LoggerdState *s, CameraType cam_type, int frame_id) -> int {
     while (!sync_encoders(s, cam_type, frame_id)) {
       ++frame_id;
       util::sleep_for(0);
     }
-    start_frame[cam_type] = frame_id;
+    return frame_id;
   };
 
   LoggerdState s;
   s.max_waiting = MAX_CAMERAS;
-  int encoder_start_frame[MAX_CAMERAS] = {};
   int start_frame_id[MAX_CAMERAS] = {10, 20, 30};
-  std::vector<std::thread> threads;
+  std::future<int> futures[MAX_CAMERAS];
   for (int i = 0; i < MAX_CAMERAS; ++i) {
-    threads.emplace_back(thread_func, &s, (CameraType)i, start_frame_id[i], encoder_start_frame);
+    futures[i] = std::async(std::launch::async, thread_func, &s, (CameraType)i, start_frame_id[i]);
   }
-  for (auto &t : threads) t.join();
-
   int start_id = *std::max_element(start_frame_id, start_frame_id + MAX_CAMERAS) + 2;
-  REQUIRE(encoder_start_frame[RoadCam] == start_id);
-  REQUIRE(encoder_start_frame[DriverCam] == start_id);
-  REQUIRE(encoder_start_frame[WideRoadCam] == start_id);
+  for (auto &f : futures) {
+    REQUIRE(f.get() == start_id);
+  }
 }
