37f3efb93e183c6f75089552247a4bba21eed959
Jason Young <jyoung8607@gmail.com>
2021-09-09 17:24:59 -0400

    FP back to 100ms; VIN state poll at 50Hz

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -120,7 +120,7 @@ def fingerprint(logcan, sendcan):
   finger = gen_empty_fingerprint()
   candidate_cars = {i: all_legacy_fingerprint_cars() for i in [0, 1]}  # attempt fingerprint on both bus 0 and 1
   frame = 0
-  frame_fingerprint = 20  # 0.2s
+  frame_fingerprint = 10  # 0.1s
   car_fingerprint = None
   done = False
 
