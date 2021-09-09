f951c42f26f7203c69ee73aa9f4c33d913a2aa60
deanlee <deanlee3@gmail.com>
2021-09-09 23:22:34 +0800

    OnroadHud

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -27,12 +27,12 @@ OnroadWindow::OnroadWindow(QWidget *parent) : QWidget(parent) {
 
   stacked_layout->addWidget(split_wrapper);
 
-  alerts = new OnroadAlerts(this);
-  alerts->setAttribute(Qt::WA_TransparentForMouseEvents, true);
-  stacked_layout->addWidget(alerts);
+  hud = new OnroadHud(this);
+  QObject::connect(this, &OnroadWindow::updateStateSignal, hud, &OnroadHud::updateState);
+  stacked_layout->addWidget(hud);
 
   // setup stacking order
-  alerts->raise();
+  hud->raise();
 
   setAttribute(Qt::WA_OpaquePaintEvent);
   QObject::connect(this, &OnroadWindow::updateStateSignal, this, &OnroadWindow::updateState);
@@ -44,7 +44,7 @@ void OnroadWindow::updateState(const UIState &s) {
   QColor bgColor = bg_colors[s.status];
   if (sm.updated("controlsState")) {
     const cereal::ControlsState::Reader &cs = sm["controlsState"].getControlsState();
-    alerts->updateAlert({QString::fromStdString(cs.getAlertText1()),
+    hud->updateAlert({QString::fromStdString(cs.getAlertText1()),
                  QString::fromStdString(cs.getAlertText2()),
                  QString::fromStdString(cs.getAlertType()),
                  cs.getAlertSize(), cs.getAlertSound()}, bgColor);
@@ -52,11 +52,11 @@ void OnroadWindow::updateState(const UIState &s) {
     // Handle controls timeout
     if (sm.rcv_frame("controlsState") < s.scene.started_frame) {
       // car is started, but controlsState hasn't been seen at all
-      alerts->updateAlert(CONTROLS_WAITING_ALERT, bgColor);
+      hud->updateAlert(CONTROLS_WAITING_ALERT, bgColor);
     } else if ((nanos_since_boot() - sm.rcv_time("controlsState")) / 1e9 > CONTROLS_TIMEOUT) {
       // car is started, but controls is lagging or died
       bgColor = bg_colors[STATUS_ALERT];
-      alerts->updateAlert(CONTROLS_UNRESPONSIVE_ALERT, bgColor);
+      hud->updateAlert(CONTROLS_UNRESPONSIVE_ALERT, bgColor);
     }
   }
   if (bg != bgColor) {
@@ -100,7 +100,7 @@ void OnroadWindow::offroadTransition(bool offroad) {
   }
 #endif
 
-  alerts->updateAlert({}, bg);
+  hud->updateAlert({}, bg);
 
   // update stream type
   bool wide_cam = Hardware::TICI() && Params().getBool("EnableWideCamera");
@@ -114,7 +114,39 @@ void OnroadWindow::paintEvent(QPaintEvent *event) {
 
 // ***** onroad widgets *****
 
-void OnroadAlerts::updateAlert(const Alert &a, const QColor &color) {
+OnroadHud::OnroadHud(QWidget *parent) : QWidget(parent) {
+  setAttribute(Qt::WA_TransparentForMouseEvents, true);
+
+  engage_img = QPixmap("../assets/img_chffr_wheel.png").scaled(img_size, img_size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+  dm_img = QPixmap("../assets/img_driver_face.png").scaled(img_size, img_size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+  connect(this, &OnroadHud::valueChanged, [=] { update(); });
+}
+
+void OnroadHud::updateState(const UIState &s) {
+  SubMaster &sm = *(s.sm);
+
+  auto v = sm["carState"].getCarState().getVEgo() * (metric ? 3.6 : 2.2369363);
+  setProperty("speed", QString::number(int(v)));
+  setProperty("speedUnit", metric ? "km/h" : "mph");
+
+  auto cs = sm["controlsState"].getControlsState();
+  const int SET_SPEED_NA = 255;
+  auto vcruise = cs.getVCruise();
+  if (vcruise != 0 && vcruise != SET_SPEED_NA) {
+    auto max = vcruise * (metric ? 1 : 0.6225);
+    setProperty("maxSpeed", QString::number((int)max));
+  } else {
+    setProperty("maxSpeed", "N/A");
+  }
+
+  setProperty("dmActive", sm["driverMonitoringState"].getDriverMonitoringState().getIsActiveMode());
+  setProperty("hideDM", cs.getAlertSize() == cereal::ControlsState::AlertSize::NONE);
+  setProperty("engageable", cs.getEngageable());
+  setProperty("status", s.status);
+}
+
+void OnroadHud::updateAlert(const Alert &a, const QColor &color) {
   if (!alert.equal(a) || color != bg) {
     alert = a;
     bg = color;
@@ -122,7 +154,63 @@ void OnroadAlerts::updateAlert(const Alert &a, const QColor &color) {
   }
 }
 
-void OnroadAlerts::paintEvent(QPaintEvent *event) {
+void OnroadHud::paintEvent(QPaintEvent *event) {
+  QPainter p(this);
+
+  // max speed
+  QRect rc(bdr_s * 2, bdr_s * 1.5, 184, 202);
+  p.setPen(QPen(QColor(0xff, 0xff, 0xff, 100), 10));
+  p.setBrush(QColor(0, 0, 0, 100));
+  p.drawRoundedRect(rc, 20, 20);
+
+  bool is_cruise_set = maxSpeed_ != "N/A";
+  configFont(p, "Open Sans", 48, "Regular");
+  drawText(p, rc.center().x(), rc.top() + bdr_s + 10, Qt::AlignTop, "MAX", is_cruise_set ? 200 : 100);
+  configFont(p, "Open Sans", 78, is_cruise_set ? "Bold" : "SemiBold");
+  drawText(p, rc.center().x(), rc.bottom() - bdr_s, Qt::AlignBottom, maxSpeed_, is_cruise_set ? 255 : 100);
+
+  // current speed
+  configFont(p, "Open Sans", 180, "Bold");
+  speed_ = "0";
+  speedUnit_ = "mph";
+  drawText(p, rect().center().x(), rc.center().y(), Qt::AlignVCenter, speed_);
+  configFont(p, "Open Sans", 65, "Regular");
+  drawText(p, rect().center().x(), rc.bottom() - 20, Qt::AlignTop, speedUnit_, 200);
+
+  // engage-ability icon
+  if (engageable_) {
+    drawIcon(p, rect().right() - radius / 2 - bdr_s * 2, radius / 2 + int(bdr_s * 1.5), engage_img, bg_colors[status_], 1.0);
+  }
+  // dm icon
+  if (!hideDM_) {
+    drawIcon(p, radius / 2 + (bdr_s * 2), rect().bottom() - footer_h / 2, dm_img, QColor(0, 0, 0, 70), dmActive_ ? 1.0 : 0.2);
+  }
+
+  drawAlert(p);
+}
+
+void OnroadHud::drawText(QPainter &p, int x, int y, Qt::Alignment flag, const QString &text, int alpha) {
+  p.setPen(QColor(0xff, 0xff, 0xff, alpha));
+  QFontMetrics fm(p.font());
+  QRect r = fm.tightBoundingRect(text);
+  r.moveCenter({x, y});
+  if (flag & Qt::AlignTop) {
+    r.moveTop(r.top() + r.height() / 2);
+  } else if (flag & Qt::AlignBottom) {
+    r.moveTop(r.top() - r.height() / 2);
+  }
+  p.drawText(r.x(), r.bottom(), text);
+}
+
+void OnroadHud::drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity) {
+  p.setPen(Qt::NoPen);
+  p.setBrush(bg);
+  p.drawEllipse(x - radius / 2, y - radius / 2, radius, radius);
+  p.setOpacity(opacity);
+  p.drawPixmap(x - img_size / 2, y - img_size / 2, img);
+}
+
+void OnroadHud::drawAlert(QPainter &p) {
   if (alert.size == cereal::ControlsState::AlertSize::NONE) {
     return;
   }
@@ -133,8 +221,6 @@ void OnroadAlerts::paintEvent(QPaintEvent *event) {
   };
   int h = alert_sizes[alert.size];
   QRect r = QRect(0, height() - h, width(), h);
-
-  QPainter p(this);
 
   // draw background + gradient
   p.setPen(Qt::NoPen);
--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -9,19 +9,40 @@
 
 // ***** onroad widgets *****
 
-class OnroadAlerts : public QWidget {
+class OnroadHud : public QWidget {
   Q_OBJECT
+  Q_PROPERTY(QString speed MEMBER speed_ NOTIFY valueChanged);
+  Q_PROPERTY(QString speedUnit MEMBER speedUnit_ NOTIFY valueChanged);
+  Q_PROPERTY(QString maxSpeed MEMBER maxSpeed_ NOTIFY valueChanged);
+  Q_PROPERTY(bool engageable MEMBER engageable_ NOTIFY valueChanged);
+  Q_PROPERTY(bool dmActive MEMBER dmActive_ NOTIFY valueChanged);
+  Q_PROPERTY(bool hideDM MEMBER hideDM_ NOTIFY valueChanged);
+  Q_PROPERTY(int status MEMBER status_ NOTIFY valueChanged);
 
 public:
-  OnroadAlerts(QWidget *parent = 0) : QWidget(parent) {};
+  OnroadHud(QWidget *parent = 0);
   void updateAlert(const Alert &a, const QColor &color);
+public slots:
+  void updateState(const UIState &s);
 
 protected:
   void paintEvent(QPaintEvent*) override;
+  void drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity);
+  void drawText(QPainter &p, int x, int y, Qt::Alignment flag, const QString &text, int alpha = 255);
+  void drawAlert(QPainter &p);
 
 private:
   QColor bg;
   Alert alert = {};
+  QPixmap engage_img, dm_img;
+  const int radius = 192;
+  const int img_size = 135;
+  QString speed_, speedUnit_, maxSpeed_;
+  bool metric = false, engageable_ = false, dmActive_ = false, hideDM_ = false;
+  int status_ = STATUS_DISENGAGED;
+
+signals:
+  void valueChanged();
 };
 
 // container window for the NVG UI
@@ -48,7 +69,7 @@ public:
 private:
   void paintEvent(QPaintEvent *event);
   void mousePressEvent(QMouseEvent* e) override;
-  OnroadAlerts *alerts;
+  OnroadHud *hud;
   NvgWindow *nvg;
   QColor bg = bg_colors[STATUS_DISENGAGED];
   QWidget *map = nullptr;
