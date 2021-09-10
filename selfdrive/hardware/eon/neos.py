dd52d9a6393d841377c01cc25e57b1be43ecbe19
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-09-10 13:47:12 -0700

    review suggestions

--- selfdrive/hardware/eon/neos.py
+++ selfdrive/hardware/eon/neos.py
@@ -2,6 +2,7 @@
 import argparse
 import hashlib
 import json
+import logging
 import os
 import requests
 
@@ -11,35 +12,25 @@ RECOVERY_DEV = "/dev/block/bootdevice/by-name/recovery"
 RECOVERY_COMMAND = "/cache/recovery/command"
 
 
-def download_file(url: str, fn: str, sha256: str, display_name: str):
-  # handle fully or partially downloaded file
-  if os.path.isfile(fn):
-    with open(fn, "rb") as f:
-      if hashlib.sha256(f.read()).hexdigest() == sha256:
-        print("already cached", url, fn)
-        return
+def download_file(url: str, fn: str, sha256: str, display_name: str, cloudlog) -> None:
+  # check if already downloaded
+  if check_hash(fn, sha256):
+    cloudlog.info(f"{display_name} already cached")
+    return
 
-  h = hashlib.sha256()
   with open(fn, "ab+") as f:
-    f.seek(0)
-    while True:
-      dat = f.read(8192)
-      if not dat:
-        break
-      h.update(dat)
-
     headers = {"Range": f"bytes={f.tell()}-"}
     r = requests.get(url, stream=True, allow_redirects=True, headers=headers)
 
     total = int(r.headers['Content-Length'])
     if 'Content-Range' in r.headers:
       total = int(r.headers['Content-Range'].split('/')[-1])
+
     for chunk in r.iter_content(chunk_size=8192):
       f.write(chunk)
-      h.update(chunk)
       print(f"Downloading {display_name}: {f.tell() / total * 100}")
 
-  if h.hexdigest() != sha256:
+  if not check_hash(fn, sha256):
     os.unlink(fn)
     raise Exception("downloaded update failed hash check")
 
@@ -48,52 +39,65 @@ def check_hash(fn: str, sha256: str, length: int = -1) -> bool:
   if not os.path.exists(fn):
     return False
 
+  h = hashlib.sha256()
   with open(fn, "rb") as f:
-    dat = f.read(length)
-  return hashlib.sha256(dat).hexdigest() == sha256
+    while True:
+      print("reading", min(max(0, length - f.tell()), 8192), fn)
+      dat = f.read(min(max(0, length - f.tell()), 8192))
+      if not dat or f.tell() == length:
+        break
+      h.update(dat)
+  return h.hexdigest() == sha256
 
 
-def download_neos_update(manifest_path: str):
+def flash_update(update_fn: str, out_path: str) -> None:
+  with open(update_fn, "rb") as update, open(out_path, "w+b") as out:
+    while True:
+      dat = update.read(8192)
+      if len(dat) == 0:
+        break
+      out.write(dat)
+
+
+def download_neos_update(manifest_path: str, cloudlog) -> None:
   with open(manifest_path) as f:
-    m = json.loads(f.read())
+    m = json.load(f)
 
   os.makedirs(NEOSUPDATE_DIR, exist_ok=True)
 
   # handle recovery updates
   if not check_hash(RECOVERY_DEV, m['recovery_hash'], m['recovery_len']):
+    cloudlog.info("recovery needs update")
     recovery_fn = os.path.join(NEOSUPDATE_DIR, os.path.basename(m['recovery_url']))
-    download_file(m['recovery_url'], recovery_fn, m['recovery_hash'], "recovery")
+    download_file(m['recovery_url'], recovery_fn, m['recovery_hash'], "recovery", cloudlog)
 
-    # flash recovery
-    with open(recovery_fn, "rb") as update, open(RECOVERY_DEV, "w+b") as recovery:
-      while True:
-        dat = update.read(4096)
-        if len(dat) == 0:
-          break
-        recovery.write(dat)
+    flash_update(recovery_fn, RECOVERY_DEV)
     assert check_hash(RECOVERY_DEV, m['recovery_hash'], m['recovery_len']), "recovery flash corrupted"
+    cloudlog.info("recovery successfully flashed")
 
   # download OTA update
   ota_fn = os.path.join(NEOSUPDATE_DIR, os.path.basename(m['ota_url']))
-  download_file(m['ota_url'], ota_fn, m['ota_hash'], "system")
+  download_file(m['ota_url'], ota_fn, m['ota_hash'], "system", cloudlog)
 
 
 def verify_update_ready(manifest_path: str) -> bool:
   with open(manifest_path) as f:
-    m = json.loads(f.read())
+    m = json.load(f)
 
   ota_fn = os.path.join(NEOSUPDATE_DIR, os.path.basename(m['ota_url']))
+  ota_downloaded = check_hash(ota_fn, m['ota_hash'])
   recovery_flashed = check_hash(RECOVERY_DEV, m['recovery_hash'], m['recovery_len'])
-  return recovery_flashed and check_hash(ota_fn, m['ota_hash'])
+  return ota_downloaded and recovery_flashed
 
 
-def perform_ota_update(manifest_path: str):
-  # reboot into recovery
+def perform_ota_update(manifest_path: str) -> None:
   with open(manifest_path) as f:
-    m = json.loads(f.read())
+    m = json.load(f)
+
+  # reboot into recovery
   ota_fn = os.path.join(NEOSUPDATE_DIR, os.path.basename(m['ota_url']))
-  with open(RECOVERY_COMMAND, "wb") as rf:
-    rf.write(bytes(f"--update_package={ota_fn}\n", encoding='utf-8'))
+  with open(RECOVERY_COMMAND, "w") as rf:
+    rf.write(f"--update_package={ota_fn}\n")
   os.system("service call power 16 i32 0 s16 recovery i32 1")
 
 
@@ -105,10 +109,12 @@ if __name__ == "__main__":
   parser.add_argument("manifest", help="Manifest json")
   args = parser.parse_args()
 
+  logging.basicConfig(level=logging.INFO)
+
   if args.swap_if_ready:
     if verify_update_ready(args.manifest):
       perform_ota_update(args.manifest)
   else:
-    download_neos_update(args.manifest)
+    download_neos_update(args.manifest, logging)
     if args.swap:
       perform_ota_update(args.manifest)
