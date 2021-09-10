8ae6b057d78fea66dbdadbd6a76a6ef6e9db16a6
Comma Device <device@comma.ai>
2021-09-10 00:22:24 +0000

    move recovery

--- selfdrive/hardware/eon/neos.py
+++ selfdrive/hardware/eon/neos.py
@@ -12,7 +12,6 @@ RECOVERY_COMMAND = "/cache/recovery/command"
 
 # TODO: check storage space before downloading
 
-
 def download_file(url: str, fn: str, sha256: str, display_name: str):
   # TODO: handle file resuming
   h = hashlib.sha256()
@@ -26,33 +25,11 @@ def download_file(url: str, fn: str, sha256: str, display_name: str):
       print(f"Downloading {display_name}: {pos / total * 100}")
   assert h == sha256, "downloaded update failed hash check"
 
+
 def check_recovery_hash(sha256, length):
   with open(RECOVERY_DEV, "rb") as f:
     dat = f.read(length)
   return hashlib.sha256(dat).hexdigest() == sha256
-
-def handle_recovery_update(manifest: dict):
-  recovery_url = manifest['recovery_url']
-  recovery_len = manifest['recovery_len']
-  recovery_hash = manifest['recovery_hash']
-
-  # skip recovery install
-  if check_recovery_hash(recovery_hash, recovery_len):
-    return
-
-  # download udpated recovery
-  recovery_fn = os.path.join(NEOSUPDATE_DIR, os.path.basename(recovery_url))
-  download_file(recovery_url, recovery_fn, recovery_hash, "recovery")
-
-  # write recovery
-  with open(recovery_fn, "rb") as update, open(RECOVERY_DEV, "w+b") as recovery:
-    while True:
-      dat = update.read(4096)
-      if len(dat) == 0:
-        break
-      recovery.write(dat)
-
-  assert check_recovery_hash(recovery_hash, recovery_len), "recovery flash corrupted"
 
 
 def download_neos_update(manifest_path: str) -> str:
@@ -63,19 +40,29 @@ def download_neos_update(manifest_path: str) -> str:
   os.makedirs(NEOSUPDATE_DIR, exist_ok=True)
 
   # handle recovery updates
-  handle_recovery_update(m)
+  if not check_recovery_hash(recovery_hash, recovery_len):
+    recovery_fn = os.path.join(NEOSUPDATE_DIR, os.path.basename(m['recovery_url']))
+    download_file(m['recovery_url'], recovery_fn, m['recovery_hash'], "recovery")
+
+    # flash recovery
+    with open(recovery_fn, "rb") as update, open(RECOVERY_DEV, "w+b") as recovery:
+      while True:
+        dat = update.read(4096)
+        if len(dat) == 0:
+          break
+        recovery.write(dat)
+    assert check_recovery_hash(recovery_hash, recovery_len), "recovery flash corrupted"
 
   # download OTA update
   ota_fn = os.path.join(NEOSUPDATE_DIR, os.path.basename(m['ota_url']))
   download_file(m['ota_url'], ota_fn, m['ota_hash'], "system")
-
   return ota_fn
 
 
 def perform_ota_update(ota_fn: str):
-  # reboot into recovery
   with open(RECOVERY_DEV, "wb") as f:
     f.write(bytes(f"--update_package={ota_fn}", encoding='utf-8'))
+  # reboot into recovery
   os.system("service call power 16 i32 0 s16 recovery i32 1")
 
 
