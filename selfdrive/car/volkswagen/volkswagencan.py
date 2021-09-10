df42014f845e5e20c8865f581d68e9db96eed28f
Jason Young <jyoung8607@gmail.com>
2021-09-10 13:41:02 -0400

    apply review suggestion

--- selfdrive/car/volkswagen/volkswagencan.py
+++ selfdrive/car/volkswagen/volkswagencan.py
@@ -22,14 +22,15 @@ def create_mqb_hud_control(packer, bus, enabled, steering_pressed, hud_alert, le
   # 1 (LKAS enabled, no lane detected) - dark gray
   # 2 (LKAS enabled, lane detected) - light gray on VW, green or white on Audi depending on year or virtual cockpit.  On a color MFD on a 2015 A3 TDI it is white, virtual cockpit on a 2018 A3 e-Tron its green.
   # 3 (LKAS enabled, lane departure detected) - white on VW, red on Audi
-  values = {
+  values = ldw_stock_values.copy()
+  values.update({
     "LDW_Status_LED_gelb": 1 if enabled and steering_pressed else 0,
     "LDW_Status_LED_gruen": 1 if enabled and not steering_pressed else 0,
     "LDW_Lernmodus_links": 3 if left_lane_depart else 1 + left_lane_visible,
     "LDW_Lernmodus_rechts": 3 if right_lane_depart else 1 + right_lane_visible,
     "LDW_Texte": hud_alert,
-  }
-  return packer.make_can_msg("LDW_02", bus, {**values, **ldw_stock_values})
+  })
+  return packer.make_can_msg("LDW_02", bus, values)
 
 def create_mqb_acc_buttons_control(packer, bus, buttonStatesToSend, CS, idx):
   values = {
