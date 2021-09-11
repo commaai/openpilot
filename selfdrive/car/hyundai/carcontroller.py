ee2d4526bcbc1bcb01db48536fe296b68b08d2a0
Willem Melching <willem.melching@gmail.com>
2021-09-10 18:45:15 -0700

    no lead

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -87,7 +87,7 @@ class CarController():
           self.last_resume_frame = frame
 
     if frame % 2 == 0 and CS.CP.openpilotLongitudinalControl:
-      lead_visible = True
+      lead_visible = False
       accel = actuators.accel if enabled else 0
       accel = clip(accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
 
