63453c951e7d125a49d8c6dc839d8f2849599b25
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-09-11 21:50:16 -0700

    update acados (#22202)
    * update acados
    
    * cleanup

--- phonelibs/acados/build.sh
+++ phonelibs/acados/build.sh
@@ -18,7 +18,7 @@ if [ ! -d acados_repo/ ]; then
 fi
 cd acados_repo
 git fetch
-git checkout 05bcbfe42818738c74572f27d06ad75a28d3b380
+git checkout d6fb3868f85239ba2de014d7234dbfea65f238e6
 git submodule update --recursive --init
 
 # build
