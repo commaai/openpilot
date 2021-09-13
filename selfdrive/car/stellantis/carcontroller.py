e33a0f0137f8c88877f3722964a7206de91533fe
Jason Young <jyoung8607@gmail.com>
2021-09-13 15:13:30 -0400

    checkpoint carcontroller

--- selfdrive/car/stellantis/carcontroller.py
+++ selfdrive/car/stellantis/carcontroller.py
@@ -1,59 +1,48 @@
 from selfdrive.car import apply_std_steer_torque_limits
-from selfdrive.car.ram.ramcan import create_lkas_command, create_wheel_buttons
-from selfdrive.car.ram.values import CAR, CarControllerParams
+from selfdrive.car.stellantis.stellantiscan import create_lkas_command, create_wheel_buttons
+from selfdrive.car.stellantis.values import CAR, CarControllerParams as P
 from opendbc.can.packer import CANPacker
 
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     self.apply_steer_last = 0
-    self.braking = False
-    #    self.alert_active = False
-    #    self.hud_count = 0
-    self.car_fingerprint = CP.carFingerprint
     self.steer_rate_limited = False
-    self.prev_frame = -1
     self.steer_command_bit = False
 
     self.packer = CANPacker(dbc_name)
 
-  def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd):  # , leftLaneVisible,
-    #   rightLaneVisible, autoHighBeamBit):  # TODO make HUD better and fix auto high beams before re-enabling this
-    P = CarControllerParams
-
-    steer_ready = CS.out.vEgo > CS.CP.minSteerSpeed
-
-    if steer_ready:
-      self.steer_command_bit = True
-    if not steer_ready:
-      self.steer_command_bit = False
-
-    bad_to_bone = enabled and steer_ready
-
-    if bad_to_bone:
-      new_steer = int(round(actuators.steer * P.STEER_MAX))
-    else:
-      new_steer = 0
-
-    apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, P)
-    self.steer_rate_limited = new_steer != apply_steer
-    self.apply_steer_last = apply_steer
-
-    # *** control msgs ***
-
+  def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd):
     can_sends = []
 
-    # *** control msgs ***
+    # **** Steering Controls ************************************************ #
 
-    #    if pcm_cancel_cmd:  # TODO: ENABLE ONCE STEERING WORKS
-    #      new_msg = create_wheel_buttons(self.packer, self.frame, cancel=True)
-    #      can_sends.append(new_msg)
-    counter = (frame / P.STEER_STEP) % 16
+    if frame % P.STEER_STEP == 0:
 
-    if frame % 2 == 0:
+      steer_ready = CS.out.vEgo > CS.CP.minSteerSpeed
+      if steer_ready:
+        self.steer_command_bit = True
+        new_steer = int(round(actuators.steer * P.STEER_MAX)) if enabled else 0
+      else:
+        self.steer_command_bit = False
+        new_steer = 0
+
+      apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, P)
+      self.steer_rate_limited = new_steer != apply_steer
+      self.apply_steer_last = apply_steer
+
+      counter = (frame / P.STEER_STEP) % 16
       can_sends.append(create_lkas_command(self.packer, int(apply_steer), counter, self.steer_command_bit))
+
+    # **** HUD Controls ***************************************************** #
 
     #    if frame % 5 == 0:
     #      can_sends.append(create_lkas_hud(self.packer, enabled, leftLaneVisible, rightLaneVisible, autoHighBeamBit))
+
+    # **** ACC Button Controls ********************************************** #
+
+    #    if pcm_cancel_cmd:
+    #      new_msg = create_wheel_buttons(self.packer, self.frame, cancel=True)
+    #      can_sends.append(new_msg)
 
     return can_sends
