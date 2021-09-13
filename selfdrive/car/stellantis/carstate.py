eec1a7f7fcb33a53fb7769f014c424464eba9bd9
Jason Young <jyoung8607@gmail.com>
2021-09-13 15:50:52 -0400

    checkpoint carstate

--- selfdrive/car/stellantis/carstate.py
+++ selfdrive/car/stellantis/carstate.py
@@ -1,86 +1,79 @@
+import numpy as np
 from cereal import car
 from opendbc.can.parser import CANParser
 from opendbc.can.can_define import CANDefine
 from selfdrive.config import Conversions as CV
 from selfdrive.car.interfaces import CarStateBase
-from selfdrive.car.ram.values import DBC, STEER_THRESHOLD
-
-GearShifter = car.CarState.GearShifter
+from selfdrive.car.stellantis.values import DBC, P
 
 
 class CarState(CarStateBase):
+  def __init__(self, CP):
+    super().__init__(CP)
+    can_define = CANDefine(DBC[CP.carFingerprint]["pt"])
+    self.shifter_values = can_define.dv["SHIFTER_ASSM"]["SHIFTER_POSITION"]
 
   def update(self, cp, cp_cam):
-
     ret = car.CarState.new_message()
 
-    self.steer_command_bit = cp_cam.vl["FORWARD_CAMERA_LKAS"]['LKAS_CONTROL_BIT']
-
-    #    ret.autoHighBeamBit = cp_cam.vl["FORWARD_CAMERA_HUD"]['AUTO_HIGH_BEAM_BIT']  # HUD needs work, going with stock, but we can read this bit for fun
-
-    ret.doorOpen = any([cp.vl["DOORS"]['DOOR_OPEN_LF'],
-                        cp.vl["DOORS"]['DOOR_OPEN_RF'],
-                        cp.vl["DOORS"]['DOOR_OPEN_LR'],
-                        cp.vl["DOORS"]['DOOR_OPEN_RR']])
-    ret.seatbeltUnlatched = cp.vl["OCCUPANT_RESTRAINT_MODULE"]['DRIVER_SEATBELT_STATUS'] == 1
-
-    ret.brakePressed = cp.vl["ABS_2"][
-                         'DRIVER_BRAKE'] > 5  # human-only... TODO: find values when ACC uses brakes - might have been lucky
-    ret.brake = 0
-    #    ret.brakeLights = ret.brakePressed
-    ret.gas = cp.vl["TPS_1"]['THROTTLE_POSITION']
-    ret.gasPressed = ret.gas > 45  # up from 5
-
-    ret.espDisabled = (cp.vl["CENTER_STACK"]['TRAC_OFF'] == 1)
-
-    MAX_SPEED = 300  # hack to prevent outrageous numbers
-    ret.wheelSpeeds.fl = MAX_SPEED if cp.vl['WHEEL_SPEEDS']['WHEEL_SPEED_LF'] >= MAX_SPEED else cp.vl['WHEEL_SPEEDS'][
-      'WHEEL_SPEED_LF']
-    ret.wheelSpeeds.rr = MAX_SPEED if cp.vl['WHEEL_SPEEDS']['WHEEL_SPEED_RR'] >= MAX_SPEED else cp.vl['WHEEL_SPEEDS'][
-      'WHEEL_SPEED_RR']
-    ret.wheelSpeeds.rl = MAX_SPEED if cp.vl['WHEEL_SPEEDS']['WHEEL_SPEED_LR'] >= MAX_SPEED else cp.vl['WHEEL_SPEEDS'][
-      'WHEEL_SPEED_LR']
-    ret.wheelSpeeds.fr = MAX_SPEED if cp.vl['WHEEL_SPEEDS']['WHEEL_SPEED_RF'] >= MAX_SPEED else cp.vl['WHEEL_SPEEDS'][
-      'WHEEL_SPEED_RF']
-    ret.vEgoRaw = (cp.vl['WHEEL_SPEEDS']['WHEEL_SPEED_LF'] + cp.vl['WHEEL_SPEEDS']['WHEEL_SPEED_RR']) / 2.
+    # Update vehicle speed and acceleration from ABS wheel speeds
+    # TODO: Verify wheel speeds match up to labeled positions
+    # TODO: Find out why these values go bonkers-high sometimes
+    ret.wheelSpeeds.fl = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FL"]
+    ret.wheelSpeeds.fr = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FR"]
+    ret.wheelSpeeds.rl = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_RL"]
+    ret.wheelSpeeds.rr = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_RR"]
+    ret.vEgoRaw = float(np.mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr]))
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    ret.standstill = not ret.vEgoRaw > 0.001
+    ret.standstill = ret.vEgoRaw < 0.01
 
-    ret.leftBlinker = bool(cp.vl["STEERING_LEVERS"]['BLINKER_LEFT'])
-    ret.rightBlinker = bool(cp.vl["STEERING_LEVERS"]['BLINKER_RIGHT'])
-    ret.steeringAngleDeg = cp.vl["EPS_1"]['STEER_ANGLE']
-    ret.steeringRateDeg = cp.vl["EPS_2"]['STEER_RATE_DRIVER']
+    # Update steering angle and driver input torque
+    ret.steeringAngleDeg = cp.vl["EPS_1"]["STEER_ANGLE"]
+    # TODO: see if there's a signal to populate steeringRateDeg
+    ret.steeringTorque = cp.vl["EPS_2"]["TORQUE_DRIVER"]
+    # TODO: populate ret.steeringTorqueEps from one of the other EPS_2 signals, TBD
+    ret.steeringPressed = abs(ret.steeringTorque) > STEER_THRESHOLD
+    # TODO: see if there's a signal to populate ret.yawRate, or calculate it
 
+    # Verify EPS readiness to accept steering commands
+    # TODO: plot out enum/can_define for EPS error/warning states, taking original Tunder conditions for now
+    ret.steerError = cp.vl["EPS_2"]["EPS_STATUS"] == 0 and ret.vEgo > self.CP.minSteerSpeed
+
+    # Update gas, brakes, and gearshift
+    ret.gas = cp.vl["TPS_1"]["THROTTLE_POSITION"]
+    ret.gasPressed = ret.gas > 10
+    ret.brake = cp.vl["ABS_2"]["DRIVER_BRAKE"]
+    ret.brakePressed = ret.brake > 5  # TODO: verify this threshold
+
+    # Update gear position
+    ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(cp.vl["SHIFTER_ASSM"]['SHIFTER_POSITION'], None))
+
+    # Update door and seatbelt status
+    ret.doorOpen = any([cp.vl["BCM"]["DOOR_OPEN_FL"], cp.vl["BCM"]["DOOR_OPEN_FR"],
+                        cp.vl["BCM"]["DOOR_OPEN_LR"], cp.vl["BCM"]["DOOR_OPEN_RR"]])
+    ret.seatbeltUnlatched = cp.vl["ORM"]["DRIVER_SEATBELT_STATUS"] == 1
+
+    # TODO: Can we find blindspot radar data?
+
+    # Update cruise control states
+    # TODO: CC state notes from Tunder:
     #    ret.cruiseState.enabled = cp_cam.vl["FORWARD_CAMERA_ACC"]['ACC_STATUS'] == 3  # ACC is green.
     #    ret.cruiseState.available = cp_cam.vl["FORWARD_CAMERA_ACC"]['ACC_STATUS'] == 1 # ACC is white... right???
-    ret.cruiseState.available = True  # for dev
-    ret.cruiseState.enabled = cp_cam.vl["FORWARD_CAMERA_ACC"]['ACC_STATUS'] == 3  # for dev
-    #    ret.cruiseState.autoHighBeamBit = cp_cam.vl["FORWARD_CAMERA_HUD"]['AUTO_HIGH_BEAM_BIT'] # this might not be a cruisestate thing
-
-    ret.cruiseState.speed = cp_cam.vl["FORWARD_CAMERA_CLUSTER"]['ACC_SET_SPEED'] * CV.KPH_TO_MS
     # CRUISE_STATE is a three bit msg, 0 is off, 1 and 2 are Non-ACC mode, 3 and 4 are ACC mode, find if there are other states too
     # ret.cruiseState.nonAdaptive = cp.vl["DASHBOARD"]['CRUISE_STATE'] in [1, 2]  do we need this at all?
+    ret.cruiseState.available = True  # TODO: there has to be a better ACC mainswitch signal/enum
+    ret.cruiseState.enabled = cp_cam.vl["DASM_ACC_CMD_1"]["ACC_STATUS"] == 3  # for dev
+    ret.cruiseState.speed = cp_cam.vl["DASM_ACC_HUD"]["ACC_SET_SPEED_KPH"] * CV.KPH_TO_MS
 
-    ret.steeringTorque = cp.vl["EPS_2"]["TORQUE_DRIVER"]
-    # ret.steeringTorqueEps = cp.vl["EPS_2"]['TORQUE_MOTOR_IN']
-    ret.steeringTorqueEps = cp.vl["EPS_1"]["TORQUE_MOTOR"]
-    ret.steeringPressed = abs(ret.steeringTorque) > STEER_THRESHOLD
-    steer_state = cp.vl["EPS_2"]["EPS_STATUS"]
-    ret.steerError = steer_state == 0 or (steer_state == 0 and ret.vEgo > self.CP.minSteerSpeed)
+    # TODO: accommodate auto high beam signal from DASM and pass through to powertrain
+    #     cp_cam.vl["FORWARD_CAMERA_HUD"]['AUTO_HIGH_BEAM_BIT']  # HUD needs work, going with stock, but we can read this bit for fun
 
-    ret.genericToggle = bool(cp.vl["STEERING_LEVERS"]['HIGH_BEAM_FLASH'])
+    # Update control button states for turn signals and ACC controls
+    # TODO: read in ACC button states for DM reset and future long control
+    ret.leftBlinker = bool(cp.vl["STEERING_LEVERS"]['BLINKER_LEFT'])
+    ret.rightBlinker = bool(cp.vl["STEERING_LEVERS"]['BLINKER_RIGHT'])
 
-    gear = cp.vl["SHIFTER_ASSM"]['SHIFTER_POSITION']
-    if gear in (4, 8):
-      ret.gearShifter = GearShifter.drive
-    elif gear == 3:
-      ret.gearShifter = GearShifter.neutral
-    elif gear == 1:
-      ret.gearShifter = GearShifter.park
-    elif gear == 2:
-      ret.gearShifter = GearShifter.reverse
-    else:
-      ret.gearShifter = GearShifter.unknown
+    ret.espDisabled = (cp.vl["CENTER_STACK"]['TRAC_OFF'] == 1)
 
     return ret
 
@@ -89,29 +82,24 @@ class CarState(CarStateBase):
     signals = [
       # sig_name, sig_address, default
       ("SHIFTER_POSITION", "SHIFTER_ASSM", 0),
-      ("DOOR_OPEN_LF", "DOORS", 0),
-      ("DOOR_OPEN_RF", "DOORS", 0),
-      ("DOOR_OPEN_LR", "DOORS", 0),
-      ("DOOR_OPEN_RR", "DOORS", 0),
+      ("DOOR_OPEN_FL", "BCM", 0),
+      ("DOOR_OPEN_FR", "BCM", 0),
+      ("DOOR_OPEN_RL", "BCM", 0),
+      ("DOOR_OPEN_RR", "BCM", 0),
       ("BRAKE_PEDAL", "ABS_1", 0),
       ("DRIVER_BRAKE", "ABS_2", 0),
       ("THROTTLE_POSITION", "TPS_1", 0),
+      ("WHEEL_SPEED_FL", "WHEEL_SPEEDS", 0),
+      ("WHEEL_SPEED_FR", "WHEEL_SPEEDS", 0),
+      ("WHEEL_SPEED_RL", "WHEEL_SPEEDS", 0),
       ("WHEEL_SPEED_RR", "WHEEL_SPEEDS", 0),
-      ("WHEEL_SPEED_LR", "WHEEL_SPEEDS", 0),
-      ("WHEEL_SPEED_RF", "WHEEL_SPEEDS", 0),
-      ("WHEEL_SPEED_LF", "WHEEL_SPEEDS", 0),
       ("STEER_ANGLE", "EPS_1", 0),
-      ("STEER_RATE_DRIVER", "EPS_2", 0),
       ("BLINKER_LEFT", "STEERING_LEVERS", 0),
       ("BLINKER_RIGHT", "STEERING_LEVERS", 0),
-      ("HIGH_BEAM_FLASH", "STEERING_LEVERS", 0),
       ("TORQUE_DRIVER", "EPS_2", 0),
       ("EPS_STATUS", "EPS_2", 0),
-      ("TORQUE_MOTOR", "EPS_1", 0),
-      #      ("TORQUE_MOTOR_IN", "EPS_2", 0),
-      #      ("COUNTER", "EPS_2", -1),
       ("TRAC_OFF", "CENTER_STACK", 0),
-      ("DRIVER_SEATBELT_STATUS", "OCCUPANT_RESTRAINT_MODULE", 0),
+      ("DRIVER_SEATBELT_STATUS", "ORM", 0),
     ]
 
     checks = [
@@ -124,10 +112,10 @@ class CarState(CarStateBase):
       ("SHIFTER_ASSM", 50),
       ("TPS_1", 50),
       ("STEERING_LEVERS", 10),
-      ("OCCUPANT_RESTRAINT_MODULE", 1),
-      ("DOORS", 1),
+      ("ORM", 1),
+      ("BCM", 1),
       ("CENTER_STACK", 20),
-      ("WHEEL_BUTTONS_CRUISE_CONTROL", 50),
+      ("ACC_BUTTONS", 50),
     ]
 
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 0)
@@ -136,19 +124,12 @@ class CarState(CarStateBase):
   def get_cam_can_parser(CP):
     signals = [
       # sig_name, sig_address, default
-      #      ("COUNTER", "FORWARD_CAMERA_LKAS", -1),
-      ("ACC_STATUS", "FORWARD_CAMERA_ACC", 0),
-      #      ("COUNTER", "FORWARD_CAMERA_ACC", -1),
-      ("ACC_SET_SPEED", "FORWARD_CAMERA_CLUSTER", -1),
-      #      ("LKAS_HUD", "FOWARD_CAMERA_HUD", -1),
-      ("LKAS_CONTROL_BIT", "FORWARD_CAMERA_LKAS", 0),
-      #      ("AUTO_HIGH_BEAM_BIT", "FORWARD_CAMERA_HUD", -1), # why didn't i check this before, is it broken?
+      ("ACC_STATUS", "DASM_ACC_CMD_1", 0),
+      ("ACC_SET_SPEED_KPH", "DASM_ACC_HUD", 0),
     ]
     checks = [
-      ("FORWARD_CAMERA_LKAS", 50),
-      #      ("FORWARD_CAMERA_HUD", 15), # HUD needs work and auto high beams - add this later
-      ("FORWARD_CAMERA_ACC", 50),
-      ("FORWARD_CAMERA_CLUSTER", 50),
+      ("DASM_ACC_CMD_1", 50),
+      ("DASM_ACC_HUD", 50),
     ]
 
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 2)
