6881688af2c2a438a90d211a3e0fedad438edd17
Willem Melching <willem.melching@gmail.com>
2021-09-29 17:43:56 +0200

    replay: send frames based on encodeIdx packet (#22361)
    * send frames based on encodeIdx
    
    * use start of frame time if set
    
    * also use end of frame if set
    
    * fix timestamp for encode packets
    
    * handle all cameras
    
    * add comment
    
    * add twice

--- selfdrive/ui/replay/logreader.h
+++ selfdrive/ui/replay/logreader.h
@@ -2,6 +2,7 @@
 
 #include <unordered_map>
 #include <vector>
+#include <cassert>
 
 #include <capnp/serialize.h>
 #include "cereal/gen/cpp/log.capnp.h"
@@ -9,10 +10,7 @@
 
 const CameraType ALL_CAMERAS[] = {RoadCam, DriverCam, WideRoadCam};
 const int MAX_CAMERAS = std::size(ALL_CAMERAS);
-struct EncodeIdx {
-  int segmentNum;
-  uint32_t frameEncodeId;
-};
+
 class Event {
 public:
   Event(cereal::Event::Which which, uint64_t mono_time) : reader(kj::ArrayPtr<capnp::word>{}) {
@@ -20,11 +18,35 @@ public:
     this->which = which;
     this->mono_time = mono_time;
   }
-  Event(const kj::ArrayPtr<const capnp::word> &amsg) : reader(amsg) {
+  Event(const kj::ArrayPtr<const capnp::word> &amsg, bool frame=false) : reader(amsg), frame(frame) {
     words = kj::ArrayPtr<const capnp::word>(amsg.begin(), reader.getEnd());
     event = reader.getRoot<cereal::Event>();
     which = event.which();
     mono_time = event.getLogMonoTime();
+
+    // 1) Send video data at t=timestampEof/timestampSof
+    // 2) Send encodeIndex packet at t=logMonoTime
+    if (frame) {
+      cereal::EncodeIndex::Reader idx;
+      if (which == cereal::Event::ROAD_ENCODE_IDX) {
+        idx = event.getRoadEncodeIdx();
+      } else if (which == cereal::Event::DRIVER_ENCODE_IDX) {
+        idx = event.getDriverEncodeIdx();
+      } else if (which == cereal::Event::WIDE_ROAD_ENCODE_IDX) {
+        idx = event.getWideRoadEncodeIdx();
+      } else {
+        assert(false);
+      }
+
+      // C2 only has eof set, and some older routes have neither
+      uint64_t sof = idx.getTimestampSof();
+      uint64_t eof = idx.getTimestampEof();
+      if (sof > 0) {
+        mono_time = sof;
+      } else if (eof > 0) {
+        mono_time = eof;
+      }
+    }
   }
   inline kj::ArrayPtr<const capnp::byte> bytes() const { return words.asBytes(); }
 
@@ -39,6 +61,7 @@ public:
   cereal::Event::Reader event;
   capnp::FlatArrayMessageReader reader;
   kj::ArrayPtr<const capnp::word> words;
+  bool frame;
 };
 
 class LogReader {
@@ -48,7 +71,6 @@ public:
   bool load(const std::string &file);
 
   std::vector<Event*> events;
-  std::unordered_map<uint32_t, EncodeIdx> eidx[MAX_CAMERAS] = {};
 
 private:
   std::vector<uint8_t> raw_;
