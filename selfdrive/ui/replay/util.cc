a18ae0810850a54d24bdca8f7f6e559509c3f827
deanlee <deanlee3@gmail.com>
2021-10-03 22:47:56 +0800

    read bz2 into stream

--- selfdrive/ui/replay/util.cc
+++ selfdrive/ui/replay/util.cc
@@ -110,23 +110,23 @@ bool httpMultiPartDownload(const std::string &url, const std::string &target_fil
   return success;
 }
 
-bool decompressBZ2(std::vector<uint8_t> &dest, const char srcData[], size_t srcSize, size_t outputSizeIncrement = 0x100000U) {
-  bz_stream strm = {};
-  int ret = BZ2_bzDecompressInit(&strm, 0, 0);
-  assert(ret == BZ_OK);
+bool readBZ2File(const std::string_view file, std::ostream &stream) {
+  int bzerror = 0;
+  BZFILE *bz_file = nullptr;
+  std::unique_ptr<FILE, decltype(&fclose)> f(fopen(file.data(), "r"), &fclose);
+  if (!f || !(bz_file = BZ2_bzReadOpen(&bzerror, f.get(), 0, 0, nullptr, 0))) {
+    return false;
+  }
 
-  strm.next_in = const_cast<char *>(srcData);
-  strm.avail_in = srcSize;
-  do {
-    strm.next_out = (char *)&dest[strm.total_out_lo32];
-    strm.avail_out = dest.size() - strm.total_out_lo32;
-    ret = BZ2_bzDecompress(&strm);
-    if (ret == BZ_OK && strm.avail_in > 0 && strm.avail_out == 0) {
-      dest.resize(dest.size() + outputSizeIncrement);
+  bzerror = BZ_OK;
+  std::array<char, 64 * 1024> buf;
+  while (bzerror == BZ_OK) {
+    int size = BZ2_bzRead(&bzerror, bz_file, buf.data(), buf.size());
+    if (bzerror == BZ_OK || bzerror == BZ_STREAM_END) {
+      stream.write(buf.data(), size);
     }
-  } while (ret == BZ_OK);
-
-  BZ2_bzDecompressEnd(&strm);
-  dest.resize(strm.total_out_lo32);
-  return ret == BZ_STREAM_END;
+  }
+  bool ret = bzerror == BZ_STREAM_END;
+  BZ2_bzReadClose(&bzerror, bz_file);
+  return ret;
 }
--- selfdrive/ui/replay/util.h
+++ selfdrive/ui/replay/util.h
@@ -6,4 +6,4 @@
 #include <vector>
 
 bool httpMultiPartDownload(const std::string &url, const std::string &target_file, int parts, std::atomic<bool> *abort = nullptr);
-bool decompressBZ2(std::vector<uint8_t> &dest, const char srcData[], size_t srcSize, size_t outputSizeIncrement = 0x100000U);
+bool readBZ2File(const std::string_view file, std::ostream &stream);
