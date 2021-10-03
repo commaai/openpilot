395d4b9cd73a0f0da8eb836ad9a6f4da51a8158a
deanlee <deanlee3@gmail.com>
2021-10-03 23:32:48 +0800

    cleanup logreader

--- selfdrive/ui/replay/logreader.cc
+++ selfdrive/ui/replay/logreader.cc
@@ -5,6 +5,37 @@
 #include "selfdrive/common/util.h"
 #include "selfdrive/ui/replay/util.h"
 
+Event::Event(const kj::ArrayPtr<const capnp::word> &amsg, bool frame) : reader(amsg), frame(frame) {
+  words = kj::ArrayPtr<const capnp::word>(amsg.begin(), reader.getEnd());
+  event = reader.getRoot<cereal::Event>();
+  which = event.which();
+  mono_time = event.getLogMonoTime();
+
+  // 1) Send video data at t=timestampEof/timestampSof
+  // 2) Send encodeIndex packet at t=logMonoTime
+  if (frame) {
+    cereal::EncodeIndex::Reader idx;
+    if (which == cereal::Event::ROAD_ENCODE_IDX) {
+      idx = event.getRoadEncodeIdx();
+    } else if (which == cereal::Event::DRIVER_ENCODE_IDX) {
+      idx = event.getDriverEncodeIdx();
+    } else if (which == cereal::Event::WIDE_ROAD_ENCODE_IDX) {
+      idx = event.getWideRoadEncodeIdx();
+    } else {
+      assert(false);
+    }
+
+    // C2 only has eof set, and some older routes have neither
+    uint64_t sof = idx.getTimestampSof();
+    uint64_t eof = idx.getTimestampEof();
+    if (sof > 0) {
+      mono_time = sof;
+    } else if (eof > 0) {
+      mono_time = eof;
+    }
+  }
+}
+
 LogReader::~LogReader() {
   for (auto e : events) delete e;
 }
