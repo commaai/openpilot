11066c6236f31f053455e9644e5f1cada93c44cd
Willem Melching <willem.melching@gmail.com>
2021-10-04 11:30:11 +0200

    Split pandaState into peripheralState and pandaState (#22385)
    * publish peripheralState from boardd
    
    * refactor consumers
    
    * rename thread
    
    * peripheralState has panda type too
    
    * add to process replay
    
    * fix device build
    
    * properly remove hardware unsupported alert
    
    * latest peripheralState
    
    * remove pandaState from thermal_monitor
    
    * put that back
    
    * add back harness check
    
    * fix cloudlog
    
    * needs pandaState
    
    * Update selfdrive/thermald/tests/test_power_monitoring.py
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * dont conflate
    
    * Update selfdrive/thermald/tests/test_power_monitoring.py
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * pigeon is peripheral
    
    * cleanup
    
    * less global
    
    * comment
    
    * move rtc to peripheral_control_thread
    
    * better diff?
    
    * whitespace
    
    * get msg
    
    * bump cereal
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: f4e2537b124d3d1c0d697a12d8b243b54b76501b

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -204,7 +204,7 @@ def calibration_incomplete_alert(CP: car.CarParams, sm: messaging.SubMaster, met
 
 
 def no_gps_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool) -> Alert:
-  gps_integrated = sm['pandaState'].pandaType in [log.PandaState.PandaType.uno, log.PandaState.PandaType.dos]
+  gps_integrated = sm['peripheralState'].pandaType in [log.PandaState.PandaType.uno, log.PandaState.PandaType.dos]
   return Alert(
     "Poor GPS reception",
     "If sky is visible, contact support" if gps_integrated else "Check GPS antenna placement",
