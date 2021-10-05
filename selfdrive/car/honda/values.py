deac9f6fe660d66f634ca276aaa375a15ea23bc4
Willem Melching <willem.melching@gmail.com>
2021-10-05 14:21:23 +0200

    Revert "switch to braking sooner"
    This reverts commit cf11dae334ccb369f625d4b13b7cd4176156a446.

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -25,7 +25,7 @@ class CarControllerParams():
   BOSCH_ACCEL_MIN = -3.5  # m/s^2
   BOSCH_ACCEL_MAX = 2.0  # m/s^2
 
-  BOSCH_GAS_LOOKUP_BP = [-0.1, 2.0]  # 2m/s^2
+  BOSCH_GAS_LOOKUP_BP = [-0.2, 2.0]  # 2m/s^2
   BOSCH_GAS_LOOKUP_V = [0, 1600]
 
   def __init__(self, CP):
