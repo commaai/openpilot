f6927906d4cbbdcd2330fc0948e5647b69e654c8
nuwandavek <vivekaithal44@gmail.com>
2021-10-08 11:32:07 -0700

    fix covariance bug in cam_odo rot update

--- selfdrive/locationd/locationd.cc
+++ selfdrive/locationd/locationd.cc
@@ -325,13 +325,13 @@ void Localizer::handle_cam_odo(double current_time, const cereal::CameraOdometry
   // Multiply by 10 to avoid to high certainty in kalman filter because of temporally correlated noise
   trans_calib_std *= 10.0;
   rot_calib_std *= 10.0;
-  VectorXd rot_device_std = rotate_std(this->device_from_calib, rot_calib_std);
-  VectorXd trans_device_std = rotate_std(this->device_from_calib, trans_calib_std);
+  VectorXd rot_device_cov = rotate_std(this->device_from_calib, rot_calib_std).array().square();
+  VectorXd trans_device_cov = rotate_std(this->device_from_calib, trans_calib_std).array().square();
 
   this->kf->predict_and_observe(current_time, OBSERVATION_CAMERA_ODO_ROTATION,
-    { (VectorXd(rot_device.rows() + rot_device_std.rows()) << rot_device, rot_device_std).finished() });
+    { (VectorXd(rot_device.rows() + rot_device_cov.rows()) << rot_device, rot_device_cov).finished() });
   this->kf->predict_and_observe(current_time, OBSERVATION_CAMERA_ODO_TRANSLATION,
-    { (VectorXd(trans_device.rows() + trans_device_std.rows()) << trans_device, trans_device_std).finished() });
+    { (VectorXd(trans_device.rows() + trans_device_cov.rows()) << trans_device, trans_device_cov).finished() });
 }
 
 void Localizer::handle_live_calib(double current_time, const cereal::LiveCalibrationData::Reader& log) {
