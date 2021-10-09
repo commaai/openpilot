11bd424dbc8672fd1d57acd7d265edc911e946b7
robin-reckmann <robin.reckmann@gmail.com>
2021-10-10 08:53:07 +0900

    Add missing ESP FW for 2018 Nissan Leaf IC

--- selfdrive/car/nissan/values.py
+++ selfdrive/car/nissan/values.py
@@ -64,6 +64,7 @@ FW_VERSIONS = {
       b'5SK0ADB\x04\x18\x00\x00\x00\x00\x00_(5\x07\x9aQ\x00\x00\x00\x80',
     ],
     (Ecu.esp, 0x740, None): [
+      b'476605SH1D',
       b'476605SK2A',
     ],
     (Ecu.eps, 0x742, None): [
