1087dd82f51046c9493010f077e469ab4ab61208
deanlee <deanlee3@gmail.com>
2021-10-10 03:11:21 +0800

    loop back to the beginning when reaches the end

--- selfdrive/ui/replay/replay.cc
+++ selfdrive/ui/replay/replay.cc
@@ -263,5 +263,10 @@ void Replay::stream() {
 
     // wait for frame to be sent before unlock.(frameReader may be deleted after unlock)
     camera_server_->waitFinish();
+
+    if (eit == events_->end() && (current_segment_ == segments_.rbegin()->first)) {
+      qInfo() << "reaches the end of route, restart from beginning";
+      emit seekTo(0, false);
+    }
   }
 }
