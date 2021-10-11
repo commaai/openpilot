d88503ea435f5962d51fdbd5d9722968251d1f1c
Kyle Greenlaw <Kyroapps@gmail.com>
2021-10-11 01:22:02 -0300

    Additional FW for 2021 Hyundai Sonata Hybrid (#22482)
    * New FW for 2021 Hyundai Sonata Hybrid
    
    * Removed duplicates
    
    * Wild cards
    
    * Update selfdrive/car/hyundai/values.py
    
    * restore old value
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: 3412388b8c91619b02fe9c8ac169300ff38963a0

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -786,18 +786,19 @@ FW_VERSIONS = {
   CAR.SONATA_HYBRID: {
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\000DNhe SCC FHCUP      1.00 1.02 99110-L5000         ',
+      b'\xf1\x8799110L5000\xf1\000DNhe SCC FHCUP      1.00 1.02 99110-L5000         ',
       ],
     (Ecu.eps, 0x7d4, None): [
-      b'\xf1\x8756310-L5500\xf1\000DN8 MDPS C 1.00 1.02 56310-L5500 4DNHC102',
+      b'\xf1\x8756310-L5500\xf1\x00DN8 MDPS C 1.00 1.02 56310-L5500 4DNHC102',
       b'\xf1\x8756310-L5450\xf1\x00DN8 MDPS C 1.00 1.02 56310-L5450 4DNHC102',
       ],
     (Ecu.fwdCamera, 0x7c4, None): [
-      b'\xf1\000DN8HMFC  AT USA LHD 1.00 1.04 99211-L1000 191016',
+      b'\xf1\x00DN8HMFC  AT USA LHD 1.00 1.04 99211-L1000 191016',
       b'\xf1\x00DN8HMFC  AT USA LHD 1.00 1.05 99211-L1000 201109',
       ],
     (Ecu.transmission, 0x7e1, None): [
-      b'\xf1\000PSBG2323  E09\000\000\000\000\000\000\000TDN2H20SA5\x97R\x88\x9e',
       b'\xf1\000PSBG2333  E14\x00\x00\x00\x00\x00\x00\x00TDN2H20SA6N\xc2\xeeW',
+      b'\xf1\x87959102T250\000\000\000\000\000\xf1\x81E09\000\000\000\000\000\000\000\xf1\000PSBG2323  E09\000\000\000\000\000\000\000TDN2H20SA5\x97R\x88\x9e',
       ],
     (Ecu.engine, 0x7e0, None): [
       b'\xf1\x87391162J012',
