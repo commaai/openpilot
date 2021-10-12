0c8368b50130aeea679dd5b0c8533ae7890ca00c
deanlee <deanlee3@gmail.com>
2021-10-13 03:14:10 +0800

    more readable fill plan_t_attr

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -276,27 +276,26 @@ void fill_xyzt(cereal::ModelDataV2::XYZTData::Builder xyzt, const float * data,
 }
 
 void fill_model(cereal::ModelDataV2::Builder &framed, const ModelDataRaw &net_outputs) {
-  // plan
   const float *best_plan = get_plan_data(net_outputs.plan);
   float plan_t_arr[TRAJECTORY_SIZE];
   std::fill_n(plan_t_arr, TRAJECTORY_SIZE, NAN);
   plan_t_arr[0] = 0.0;
   for (int xidx=1, tidx=0; xidx<TRAJECTORY_SIZE; xidx++) {
     // increment tidx until we find an element that's further away than the current xidx
-    while (tidx < TRAJECTORY_SIZE-1 && best_plan[(tidx+1)*PLAN_MHP_COLUMNS] < X_IDXS[xidx]) {
+    for (int next_tid = tidx + 1; next_tid < TRAJECTORY_SIZE && best_plan[next_tid*PLAN_MHP_COLUMNS] < X_IDXS[xidx]; next_tid++) {
       tidx++;
     }
-    float current_x_val = best_plan[tidx*PLAN_MHP_COLUMNS];
-    float next_x_val = best_plan[(tidx+1)*PLAN_MHP_COLUMNS];
-    if (next_x_val < X_IDXS[xidx]) {
+    if (tidx == TRAJECTORY_SIZE - 1) {
       // if the plan doesn't extend far enough, set plan_t to the max value (10s), then break
-      plan_t_arr[xidx] = T_IDXS[TRAJECTORY_SIZE-1];
+      plan_t_arr[xidx] = T_IDXS[TRAJECTORY_SIZE - 1];
       break;
-    } else {
-      // otherwise, interpolate to find `t` for the current xidx
-      float p = (X_IDXS[xidx] - current_x_val) / (next_x_val - current_x_val);
-      plan_t_arr[xidx] = p * T_IDXS[tidx+1] + (1 - p) * T_IDXS[tidx];
     }
+
+    // interpolate to find `t` for the current xidx
+    float current_x_val = best_plan[tidx * PLAN_MHP_COLUMNS];
+    float next_x_val = best_plan[(tidx+1) * PLAN_MHP_COLUMNS];
+    float p = (X_IDXS[xidx] - current_x_val) / (next_x_val - current_x_val);
+    plan_t_arr[xidx] = p * T_IDXS[tidx+1] + (1 - p) * T_IDXS[tidx];
   }
 
   fill_xyzt(framed.initPosition(), best_plan, PLAN_MHP_COLUMNS, 0, plan_t_arr, true);
