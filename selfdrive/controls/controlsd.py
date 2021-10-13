f727364cddcff1c8bc759f9c288c8257d251d1b9
wolffja <wolffja@ubuntu20.04LTS>
2021-10-12 23:05:59 -0400

    controlsd: use fake peripheralState for Simulation

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -72,7 +72,7 @@ class Controls:
 
     self.sm = sm
     if self.sm is None:
-      ignore = ['peripheralState', 'driverCameraState', 'managerState'] if SIMULATION else None
+      ignore = ['driverCameraState', 'managerState'] if SIMULATION else None
       self.sm = messaging.SubMaster(['deviceState', 'pandaStates', 'peripheralState', 'modelV2', 'liveCalibration',
                                      'driverMonitoringState', 'longitudinalPlan', 'lateralPlan', 'liveLocationKalman',
                                      'managerState', 'liveParameters', 'radarState'] + self.camera_packets + joystick_packet,
