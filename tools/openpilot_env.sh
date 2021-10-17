84a4fea88a1c7ee586acefe4e36d538867fcacf4
predicate@bonzo <noreply@example.com>
2021-10-17 17:22:05 +0200

    openpilot_env: remove hard dependencies on pyenv and home directory

--- tools/openpilot_env.sh
+++ tools/openpilot_env.sh
@@ -1,25 +1,36 @@
 if [ -z "$OPENPILOT_ENV" ]; then
-  export PYTHONPATH="$HOME/openpilot:$PYTHONPATH"
-
+  if [ -z "$OP_ROOT" ]; then
+    export PYTHONPATH="$HOME/openpilot:$PYTHONPATH"
+  else
+    export PYTHONPATH="$OP_ROOT:$PYTHONPATH"
+  fi
   unamestr=`uname`
   if [[ "$unamestr" == 'Linux' ]]; then
     export PATH="$HOME/.pyenv/bin:$PATH"
-
+    if command -v pyenv &> /dev/null; then
+        PYENV_INSTALLED=1
+    fi
     # Pyenv suggests we place the below two lines in .profile before we source
     # .bashrc, but there is no simple way to guarantee we do this correctly
     # programmatically across heterogeneous systems. For end-user convenience,
     # we add the lines here as a workaround.
     # https://github.com/pyenv/pyenv/issues/1906
-    export PYENV_ROOT="$HOME/.pyenv"
-    eval "$(pyenv init --path)"
+    if [ -n "$PYENV_INSTALLED" ]; then
+        export PYENV_ROOT="$HOME/.pyenv"
+        eval "$(pyenv init --path)"
+        eval "$(pyenv virtualenv-init -)"
+    fi
 
-    eval "$(pyenv virtualenv-init -)"
   elif [[ "$unamestr" == 'Darwin' ]]; then
     # msgq doesn't work on mac
     export ZMQ=1
     export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
   fi
-  eval "$(pyenv init -)"
+
+  if command -v pyenv &> /dev/null; then
+      eval "$(pyenv init -)"
+      PYENV_INSTALLED=1
+  fi
 
   export OPENPILOT_ENV=1
 fi
