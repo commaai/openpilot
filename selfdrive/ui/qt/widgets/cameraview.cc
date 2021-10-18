60dce04b505971fe872ae066116bc52923304779
deanlee <deanlee3@gmail.com>
2021-10-18 08:10:16 +0800

    fix possible crash

--- selfdrive/ui/qt/widgets/cameraview.cc
+++ selfdrive/ui/qt/widgets/cameraview.cc
@@ -194,7 +194,7 @@ void CameraViewWidget::updateFrameMat(int w, int h) {
       }};
       frame_mat = matmul(device_transform, frame_transform);
     }
-  } else {
+  } else if (vipc_client->connected) {
     // fit frame to widget size
     float w  = (float)width() / height();
     float f = (float)vipc_client->buffers[0].width  / vipc_client->buffers[0].height;
