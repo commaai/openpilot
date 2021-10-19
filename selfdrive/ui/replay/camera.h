ed5bd9719bc056a470a5e6831d54ac96c2f14c01
Dean Lee <deanlee3@gmail.com>
2021-10-19 20:39:47 +0800

    replay/camera: publish each camera in a separate thread (#22591)
    * publish frames in a seperate thread for each camera
    
    * cleanup
    
    * cleanup
    
    * prefetch next frame
    
    * cleanup
    old-commit-hash: 4678903a6193ee707657be8b939174823f26eb5a

--- selfdrive/ui/replay/camera.h
+++ selfdrive/ui/replay/camera.h
@@ -8,7 +8,7 @@
 
 class CameraServer {
 public:
-  CameraServer(std::pair<int, int> cameras[MAX_CAMERAS] = nullptr);
+  CameraServer(std::pair<int, int> camera_size[MAX_CAMERAS] = nullptr);
   ~CameraServer();
   void pushFrame(CameraType type, FrameReader* fr, const cereal::EncodeIndex::Reader& eidx);
   inline void waitFinish() {
@@ -16,24 +16,26 @@ public:
   }
 
 protected:
-  void startVipcServer();
-  void thread();
-
   struct Camera {
-    VisionStreamType rgb_type;
+    CameraType type;
+    VisionStreamType rgb_type; 
     VisionStreamType yuv_type;
     int width;
     int height;
+    std::thread thread;
+    SafeQueue<std::pair<FrameReader*, const cereal::EncodeIndex::Reader>> queue;
+    int cached_id = -1;
+    int cached_seg = -1;
+    std::pair<VisionBuf *, VisionBuf*> cached_buf;
   };
+  void startVipcServer();
+  void cameraThread(Camera &cam);
 
   Camera cameras_[MAX_CAMERAS] = {
-      {.rgb_type = VISION_STREAM_RGB_BACK, .yuv_type = VISION_STREAM_YUV_BACK},
-      {.rgb_type = VISION_STREAM_RGB_FRONT, .yuv_type = VISION_STREAM_YUV_FRONT},
-      {.rgb_type = VISION_STREAM_RGB_WIDE, .yuv_type = VISION_STREAM_YUV_WIDE},
+      {.type = RoadCam, .rgb_type = VISION_STREAM_RGB_BACK, .yuv_type = VISION_STREAM_YUV_BACK},
+      {.type = DriverCam, .rgb_type = VISION_STREAM_RGB_FRONT, .yuv_type = VISION_STREAM_YUV_FRONT},
+      {.type = WideRoadCam, .rgb_type = VISION_STREAM_RGB_WIDE, .yuv_type = VISION_STREAM_YUV_WIDE},
   };
-
   std::atomic<int> publishing_ = 0;
-  std::thread camera_thread_;
   std::unique_ptr<VisionIpcServer> vipc_server_;
-  SafeQueue<std::tuple<CameraType, FrameReader*, const cereal::EncodeIndex::Reader>> queue_;
 };
