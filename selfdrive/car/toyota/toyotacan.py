0b6b9b595ddf646dd3afb9e64a1cae28737468e7
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-10-20 16:01:47 +1100

    Toyota longitudinal: display PCS off when DSU is unplugged and no SDSU is found (#22626)
    * display pcs off in car settings when DSU is unplugged
    
    * Update opendbc
    
    * fix checks
    
    * bump opendbc
    
    * use 0
    
    * update refs
    
    * update refs
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -60,7 +60,8 @@ def create_fcw_command(packer, fcw):
     "FCW": fcw,
     "SET_ME_X20": 0x20,
     "SET_ME_X10": 0x10,
-    "SET_ME_X80": 0x80,
+    "PCS_OFF": 1,
+    "PCS_SENSITIVITY": 0,
   }
   return packer.make_can_msg("ACC_HUD", 0, values)
 
