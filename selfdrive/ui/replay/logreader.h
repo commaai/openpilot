c512f9621577232a5bc0c6b467da24340a2e988a
deanlee <deanlee3@gmail.com>
2021-10-20 20:11:03 +0800

    no memory_resouce on device

--- selfdrive/ui/replay/logreader.h
+++ selfdrive/ui/replay/logreader.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#if __has_include(<memory_resource>)
 #include <memory_resource>
+#endif
 
 #include "cereal/gen/cpp/log.capnp.h"
 #include "selfdrive/camerad/cameras/camera_common.h"
@@ -25,12 +27,14 @@ public:
     }
   };
 
+#if __has_include(<memory_resource>)
   void *operator new(size_t size, std::pmr::monotonic_buffer_resource *mbr) {
     return mbr->allocate(size);
   }
   void operator delete(void *ptr) {
     // No-op. memory used by EventMemoryPool increases monotonically until the logReader is destroyed. 
   }
+#endif
 
   uint64_t mono_time;
   cereal::Event::Which which;
@@ -50,6 +54,8 @@ public:
 
 private:
   std::string raw_;
+#if __has_include(<memory_resource>)
   std::pmr::monotonic_buffer_resource *mbr_ = nullptr;
   void *pool_buffer_ = nullptr;
+#endif
 };
