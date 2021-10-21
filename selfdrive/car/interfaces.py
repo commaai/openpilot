9b5896a07c30e877510817e993d7c551084bb8a0
Willem Melching <willem.melching@gmail.com>
2021-10-21 11:03:32 +0200

    use classmethod

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -62,9 +62,9 @@ class CarInterfaceBase():
     # TODO: something with lateralPlan.curvatureRates
     return desired_angle * (v_ego**2)
 
-  @staticmethod
-  def get_steer_feedforward_function():
-    return CarInterfaceBase.get_steer_feedforward_default
+  @classmethod
+  def get_steer_feedforward_function(cls):
+    return cls.get_steer_feedforward_default
 
   # returns a set of default params to avoid repetition in car specific params
   @staticmethod
