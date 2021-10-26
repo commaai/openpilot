46009fa6fd61fd454d3a4025f29c6fce688b09a0
Gibby <503761+Gibby@users.noreply.github.com>
2021-10-26 18:59:24 -0400

    GM: 2016 Escalade ESV (#22670)
    * Added 2016 Escalade ESV fingerprint and data.
    
    * Updated CARS.md for GM vehicles
    
    * Uploaded Escalade fingerprint
    
    * Removed part about auto high beams for GM. Seems to work for some.
    
    * Revert other modifications in CARS.md
    
    * Revert other modifications in CARS.md
    
    * Added test route for Escalade
    
    * Added tunning for Escalade to reduce jerky steering
    
    * Apply suggestions from code review
    
    * Update selfdrive/car/gm/interface.py
    
    * cleanup test route
    
    Co-authored-by: Gibby <git@twoitguys>
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -111,6 +111,17 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatioRear = 0.
       ret.centerToFront = ret.wheelbase * 0.49
 
+    elif candidate == CAR.ESCALADE_ESV:
+      ret.minEnableSpeed = -1.  # engage speed is decided by pcm
+      ret.mass = 2739. + STD_CARGO_KG
+      ret.wheelbase = 3.302
+      ret.steerRatio = 17.3
+      ret.centerToFront = ret.wheelbase * 0.49
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[10., 41.0], [10., 41.0]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.13, 0.24], [0.01, 0.02]]
+      ret.lateralTuning.pid.kf = 0.000045
+      tire_stiffness_factor = 1.0
+            
     # TODO: get actual value, for now starting with reasonable value for
     # civic and scaling by mass and wheelbase
     ret.rotationalInertia = scale_rot_inertia(ret.mass, ret.wheelbase)
