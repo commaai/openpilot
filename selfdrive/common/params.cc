cca07c0c730eb6e0d8dab0bde099a2ed706d14de
Willem Melching <willem.melching@gmail.com>
2021-10-26 18:59:17 +0200

    navigation: use mapbox proxy server (#22660)
    * use mapbox proxy
    
    * remove MapboxToken param
    
    * do not log jwt
    
    * remove import
    
    * move to staging url
    
    * put has_prime in ui state
    
    * logic was flipped
    
    * Update selfdrive/ui/qt/maps/map.h

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -169,7 +169,6 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LastUpdateException", PERSISTENT},
     {"LastUpdateTime", PERSISTENT},
     {"LiveParameters", PERSISTENT},
-    {"MapboxToken", PERSISTENT | DONT_LOG},
     {"NavDestination", CLEAR_ON_MANAGER_START | CLEAR_ON_IGNITION_OFF},
     {"NavSettingTime24h", PERSISTENT},
     {"OpenpilotEnabledToggle", PERSISTENT},
