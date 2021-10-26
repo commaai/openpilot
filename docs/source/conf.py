76f14782a528a9dac0f4a67a9d4115e44555efd1
Willem Melching <willem.melching@gmail.com>
2021-10-26 14:59:31 +0200

    switch theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ author = 'comma.ai'
 # ones.
 extensions = [
         'sphinx.ext.autodoc',
+        'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
