fix minus sign

selfdrive/controls/lib/tests/test_vehicle_model.py
@@ -25,22 +25,7 @@ class TestVehicleModel(unittest.TestCase):
 
           self.assertAlmostEqual(sa, new_sa)
 
-  def test_dyn_ss_sol_against_yaw_rate_no_roll(self):
-    """Verify that the yaw_rate helper function matches the results
-    from the state space model."""
-
-    roll = 0
-    for u in np.linspace(1, 30, num=10):
-      for sa in np.linspace(math.radians(-20), math.radians(20), num=11):
-
-        # Compute yaw rate based on state space model
-        _, yr1 = dyn_ss_sol(sa, u, roll, self.VM)
-
-        # Compute yaw rate using direct computations
-        yr2 = self.VM.yaw_rate(sa, u, roll)
-        self.assertAlmostEqual(float(yr1), yr2)
-
-  def test_dyn_ss_sol_against_yaw_rate_with_roll(self):
+  def test_dyn_ss_sol_against_yaw_rate(self):
     """Verify that the yaw_rate helper function matches the results
     from the state space model."""
 

selfdrive/controls/lib/vehicle_model.py
@@ -210,7 +210,7 @@ def create_dyn_state_matrices(u: float, VM: VehicleModel) -> Tuple[np.ndarray, n
   B[1, 0] = (VM.cF * VM.aF - VM.chi * VM.cR * VM.aR) / VM.j / VM.sR
 
   # Roll input
-  B[0, 1] = ACCELERATION_DUE_TO_GRAVITY
+  B[0, 1] = -ACCELERATION_DUE_TO_GRAVITY
 
   return A, B
 