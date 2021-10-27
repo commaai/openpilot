6130e90054cb0ab72761df48ebe81d3a20ceb523
Chad Bailey <chadbailey904@gmail.com>
2021-10-27 03:18:33 -0500

    Experiment: sphinx docs generation updated (#22708)
    * moved build to root gitignore, added .gitkeep
    
    * Improved makefile doc build process
    
    - Removed auto-generated docs from source control
    - Moved apidoc.sh into Makefile
    - Removed make.bat (can add back if Windows support desired)
    - Added sphinx viewcode and markdown extensions
    - Added feature to source /docs in build, so any .rst file in /docs will
      override the respective file during the build process
    - Added feature to copy all markdown/rst files from source into /build/
      during build process so they can be easily referenced while writing
      docs (see examples in index.md)
    - Wrote basic starter index.md file
    
    TODO: Add new dependencies to Pipfile [dev-packages]
    
    * Revert accidental modification to Pipfile

--- docs/apidoc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sphinx-apidoc -o source ../ \
-  ../xx ../laika_repo ../rednose_repo ../pyextra ../notebooks ../panda_jungle \
-  ../third_party \
-  ../panda/examples \
-  ../scripts \
-  ../selfdrive/modeld \
-  ../selfdrive/debug \
-  $(find .. -type d -name "*test*")
--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
 
 
 # -- Project information -----------------------------------------------------
@@ -31,8 +33,10 @@ author = 'comma.ai'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.autodoc',
-        'sphinx_rtd_theme',
+        'sphinx.ext.autodoc',   # Auto-generate docs
+        'sphinx.ext.viewcode',  # Add view code link to modules
+        'sphinx_rtd_theme',     # Read The Docs theme
+        'myst_parser',          # Markdown parsing
 ]
 
 # Add any paths that contain templates here, relative to this directory.
