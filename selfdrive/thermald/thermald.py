dbe4641e04cbd1bf67da47d3a5d94db50a5111c8
Robbe Derks <robbe.derks@gmail.com>
2021-10-29 13:04:26 +0200

    Multipanda support (#22402)
    * squash all PR commits for easy rebase
    
    * merged all panda rx can into one message
    
    * fix buffers in can_send
    
    * more cleanup and minor fixes
    
    * fix even more stuff
    
    * fix non-allocated send buffer
    
    * make connecting more robust
    
    * fix bus offset
    
    * fix controls_mismatch?
    
    * simplify mismatch check
    
    * C++ style struct
    
    * fix connect loop
    
    * update ref
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 585c16cd2aba6e2504c2427aaf1855b97035eb41

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -20,12 +20,9 @@ from selfdrive.controls.lib.alertmanager import set_offroad_alert
 from selfdrive.controls.lib.pid import PIController
 from selfdrive.hardware import EON, TICI, PC, HARDWARE
 from selfdrive.loggerd.config import get_available_percent
-from selfdrive.pandad import get_expected_signature
 from selfdrive.swaglog import cloudlog
 from selfdrive.thermald.power_monitoring import PowerMonitoring
 from selfdrive.version import tested_branch, terms_version, training_version
-
-FW_SIGNATURE = get_expected_signature()
 
 ThermalStatus = log.DeviceState.ThermalStatus
 NetworkType = log.DeviceState.NetworkType
@@ -363,10 +360,6 @@ def thermald_thread():
     startup_conditions["up_to_date"] = params.get("Offroad_ConnectivityNeeded") is None or params.get_bool("DisableUpdates")
     startup_conditions["not_uninstalling"] = not params.get_bool("DoUninstall")
     startup_conditions["accepted_terms"] = params.get("HasAcceptedTerms") == terms_version
-
-    panda_signature = params.get("PandaFirmware")
-    startup_conditions["fw_version_match"] = (panda_signature is None) or (panda_signature == FW_SIGNATURE)   # don't show alert is no panda is connected (None)
-    set_offroad_alert_if_changed("Offroad_PandaFirmwareMismatch", (not startup_conditions["fw_version_match"]))
 
     # with 2% left, we killall, otherwise the phone will take a long time to boot
     startup_conditions["free_space"] = msg.deviceState.freeSpacePercent > 2
