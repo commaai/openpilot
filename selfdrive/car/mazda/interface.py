14ede5d6cd384f424f36983285a0d3c1ab9b824a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-10-29 13:07:52 -0700

    Mazda: allow engaging below min LKAS speed (#22737)
    * Mazda: allow engagement below minimum LKAS speed
    
    * bump panda
    
    * bump panda
    old-commit-hash: 4bbeb27f9b56676483776920a62ecb135a2880ed

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -86,9 +86,6 @@ class CarInterface(CarInterfaceBase):
     # events
     events = self.create_common_events(ret)
 
-    if self.CS.low_speed_lockout:
-      events.add(EventName.belowEngageSpeed)
-
     if self.CS.low_speed_alert:
       events.add(EventName.belowSteerSpeed)
 
