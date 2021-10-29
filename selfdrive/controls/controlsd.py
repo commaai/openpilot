79c22e05d1dc348b4c1aa8ff5d581b74328c0cd2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-10-29 13:38:19 -0700

    also check if above min steer speed

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -490,7 +490,7 @@ class Controls:
       actuators.accel = self.LoC.update(self.active, CS, self.CP, long_plan, pid_accel_limits)
 
       # Steering PID loop and lateral MPC
-      lat_active = self.active and not CS.steerWarning and not CS.steerError
+      lat_active = self.active and not CS.steerWarning and not CS.steerError and CS.vEgo > CS.minSteerSpeed
       desired_curvature, desired_curvature_rate = get_lag_adjusted_curvature(self.CP, CS.vEgo,
                                                                              lat_plan.psis,
                                                                              lat_plan.curvatures,
