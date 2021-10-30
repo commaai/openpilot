1848fed6e2a586f10f5d59a1472477b474ebda70
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-10-30 10:56:30 -0700

    Mazda: send dash alerts (#22730)
    * Mazda: send dash alerts
    
    * no actuators
    
    * ldw
    
    * send alerts
    
    * test
    
    * use cam parser
    
    * add generic toggle
    
    * wip
    
    * revert some stuff
    
    * try that
    
    * steer required works
    
    * cleanup
    
    * bump panda
    
    * little more cleanup
    
    * no lane lines
    
    Co-authored-by: Comma Device <device@comma.ai>

--- selfdrive/car/mazda/mazdacan.py
+++ selfdrive/car/mazda/mazdacan.py
@@ -1,4 +1,7 @@
+import copy
+
 from selfdrive.car.mazda.values import GEN1, Buttons
+
 
 def create_steering_control(packer, car_fingerprint, frame, apply_steer, lkas):
 
@@ -7,10 +10,11 @@ def create_steering_control(packer, car_fingerprint, frame, apply_steer, lkas):
   lo = tmp & 0xFF
   hi = tmp >> 8
 
+  # copy values from camera
   b1 = int(lkas["BIT_1"])
-  ldw = int(lkas["LDW"])
   er1 = int(lkas["ERR_BIT_1"])
   lnv = 0
+  ldw = 0
   er2 = int(lkas["ERR_BIT_2"])
 
   # Some older models do have these, newer models don't.
@@ -28,7 +32,7 @@ def create_steering_control(packer, car_fingerprint, frame, apply_steer, lkas):
   # bytes:     [    1  ] [ 2 ] [             3               ]  [           4         ]
   csum = 249 - ctr - hi - lo - (lnv << 3) - er1 - (ldw << 7) - ( er2 << 4) - (b1 << 5)
 
-  #bytes       [ 5 ] [ 6 ] [    7   ]
+  # bytes      [ 5 ] [ 6 ] [    7   ]
   csum = csum - ahi - amd - alo - b2
 
   if ahi == 1:
@@ -44,63 +48,72 @@ def create_steering_control(packer, car_fingerprint, frame, apply_steer, lkas):
 
   if car_fingerprint in GEN1:
     values = {
-      "LKAS_REQUEST"     : apply_steer,
-      "CTR"              : ctr,
-      "ERR_BIT_1"        : er1,
+      "LKAS_REQUEST": apply_steer,
+      "CTR": ctr,
+      "ERR_BIT_1": er1,
       "LINE_NOT_VISIBLE" : lnv,
-      "LDW"              : ldw,
-      "BIT_1"            : b1,
-      "ERR_BIT_2"        : er2,
-      "STEERING_ANGLE"   : steering_angle,
-      "ANGLE_ENABLED"    : b2,
-      "CHKSUM"           : csum
+      "LDW": ldw,
+      "BIT_1": b1,
+      "ERR_BIT_2": er2,
+      "STEERING_ANGLE": steering_angle,
+      "ANGLE_ENABLED": b2,
+      "CHKSUM": csum
     }
 
   return packer.make_can_msg("CAM_LKAS", 0, values)
 
 
+def create_alert_command(packer, cam_msg: dict, ldw: bool, steer_required: bool):
+  values = copy.copy(cam_msg)
+  values.update({
+    # TODO: what's the difference between all these? do we need to send all?
+    "HANDS_WARN_3_BITS": 0b111 if steer_required else 0,
+    "HANDS_ON_STEER_WARN": steer_required,
+    "HANDS_ON_STEER_WARN_2": steer_required,
+
+    # TODO: right lane works, left doesn't
+    # TODO: need to do something about L/R
+    "LDW_WARN_LL": 0,
+    "LDW_WARN_RL": 0,
+  })
+  return packer.make_can_msg("CAM_LANEINFO", 0, values)
+
+
 def create_button_cmd(packer, car_fingerprint, button):
 
-  if button == Buttons.CANCEL:
-    can = 1
-    res = 0
-  elif button == Buttons.RESUME:
-    can = 0
-    res = 1
-  else:
-    can = 0
-    res = 0
+  can = int(button == Buttons.CANCEL)
+  res = int(button == Buttons.RESUME)
 
   if car_fingerprint in GEN1:
     values = {
-      "CAN_OFF"           : can,
-      "CAN_OFF_INV"       : (can + 1) % 2,
+      "CAN_OFF": can,
+      "CAN_OFF_INV": (can + 1) % 2,
 
-      "SET_P"             : 0,
-      "SET_P_INV"         : 1,
+      "SET_P": 0,
+      "SET_P_INV": 1,
 
-      "RES"               : res,
-      "RES_INV"           : (res + 1) % 2,
+      "RES": res,
+      "RES_INV": (res + 1) % 2,
 
-      "SET_M"             : 0,
-      "SET_M_INV"         : 1,
+      "SET_M": 0,
+      "SET_M_INV": 1,
 
-      "DISTANCE_LESS"     : 0,
-      "DISTANCE_LESS_INV" : 1,
+      "DISTANCE_LESS": 0,
+      "DISTANCE_LESS_INV": 1,
 
-      "DISTANCE_MORE"     : 0,
-      "DISTANCE_MORE_INV" : 1,
+      "DISTANCE_MORE": 0,
+      "DISTANCE_MORE_INV": 1,
 
-      "MODE_X"            : 0,
-      "MODE_X_INV"        : 1,
+      "MODE_X": 0,
+      "MODE_X_INV": 1,
 
-      "MODE_Y"            : 0,
-      "MODE_Y_INV"        : 1,
+      "MODE_Y": 0,
+      "MODE_Y_INV": 1,
 
-      "BIT1"              : 1,
-      "BIT2"              : 1,
-      "BIT3"              : 1,
-      "CTR"               : 0
+      "BIT1": 1,
+      "BIT2": 1,
+      "BIT3": 1,
+      "CTR": 0
     }
 
     return packer.make_can_msg("CRZ_BTNS", 0, values)
