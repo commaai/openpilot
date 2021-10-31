1148f7e3239d36412bc487350caaa11e0a4ad01d
Andrew Tec <andrew.tec@pm.me>
2021-10-31 07:21:55 -0700

    another weird character

--- third_party/openmax/include/OMX_QCOMExtns.h
+++ third_party/openmax/include/OMX_QCOMExtns.h
@@ -145,13 +145,13 @@ typedef enum OMX_QCOMFramePackingFormat
   OMX_QCOM_FramePacking_Arbitrary,
 
   /*  2 - Multiple complete frames per buffer (integer number)
-   *  OMX IL 1.1.1 Figure 2-11: Case 2—Each Buffer Filled with
+   *  OMX IL 1.1.1 Figure 2-11: Case 2 Each Buffer Filled with
    *  Only Complete Frames of Data
    */
   OMX_QCOM_FramePacking_CompleteFrames,
 
   /*  3 - Only one complete frame per buffer, no partial frame
-   *  OMX IL 1.1.1 Figure 2-12: Case 3—Each Buffer Filled with
+   *  OMX IL 1.1.1 Figure 2-12: Case 3 Each Buffer Filled with
    *  Only One Frame of Compressed Data. Usually at least one
    *  complete unit of data will be delivered in a buffer for
    *  uncompressed data formats.
