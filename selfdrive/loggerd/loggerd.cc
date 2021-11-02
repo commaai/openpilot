5246f0231e6bfa572c0dd89c60e538910817faa2
Mayfield <3902405+mayfieldiv@users.noreply.github.com>
2021-11-02 12:08:53 -0400

    enable wshadow (#22756)
    * enable wshadow (#22714)
    
    * fix replay
    
    * more build fixes
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

--- selfdrive/loggerd/loggerd.cc
+++ selfdrive/loggerd/loggerd.cc
@@ -126,21 +126,21 @@ struct LoggerdState {
 LoggerdState s;
 
 // Wait for all encoders to reach the same frame id
-bool sync_encoders(LoggerdState *s, CameraType cam_type, uint32_t frame_id) {
-  if (s->camera_synced[cam_type]) return true;
+bool sync_encoders(LoggerdState *state, CameraType cam_type, uint32_t frame_id) {
+  if (state->camera_synced[cam_type]) return true;
 
-  if (s->max_waiting > 1 && s->encoders_ready != s->max_waiting) {
-    update_max_atomic(s->latest_frame_id, frame_id);
-    if (std::exchange(s->camera_ready[cam_type], true) == false) {
-      ++s->encoders_ready;
+  if (state->max_waiting > 1 && state->encoders_ready != state->max_waiting) {
+    update_max_atomic(state->latest_frame_id, frame_id);
+    if (std::exchange(state->camera_ready[cam_type], true) == false) {
+      ++state->encoders_ready;
       LOGE("camera %d encoder ready", cam_type);
     }
     return false;
   } else {
     // Small margin in case one of the encoders already dropped the next frame
-    uint32_t start_frame_id = s->latest_frame_id + 2;
+    uint32_t start_frame_id = state->latest_frame_id + 2;
     bool synced = frame_id >= start_frame_id;
-    s->camera_synced[cam_type] = synced;
+    state->camera_synced[cam_type] = synced;
     if (!synced) LOGE("camera %d waiting for frame %d, cur %d", cam_type, start_frame_id, frame_id);
     return synced;
   }
