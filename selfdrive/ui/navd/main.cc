bef686f2752bcad4bb3116c2be63157ee01eab73
Willem Melching <willem.melching@gmail.com>
2021-11-04 14:32:32 +0100

    Move navigation into separate daemon (#22767)
    * Add navd folder
    
    * prints route instructions to console
    
    * broadcast NavInstuction without lanes
    
    * show basic instructions and eta
    
    * parse out lane info
    
    * use swaglog
    
    * clip distance on ui side
    
    * draw lane directions
    
    * show route
    
    * add to process config
    
    * add to release files
    
    * small cleanup
    
    * show route without gps
    
    * pop open map on initial route
    
    * fix error messages around no gps
    
    * done
    
    * make persistent process
    
    * handle end of route
    
    * clear route on offroad
    
    * only one timer
    
    * fix layout hacks
    
    * explicit rendering at 20Hz

---
+++ selfdrive/ui/navd/main.cc
@@ -0,0 +1,36 @@
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QThread>
+#include <csignal>
+
+#include "selfdrive/ui/qt/util.h"
+#include "selfdrive/ui/navd/route_engine.h"
+
+RouteEngine* route_engine = nullptr;
+
+void sigHandler(int s) {
+  qInfo() << "Shutting down";
+  std::signal(s, SIG_DFL);
+
+  qApp->quit();
+}
+
+
+int main(int argc, char *argv[]) {
+  qInstallMessageHandler(swagLogMessageHandler);
+
+  QApplication app(argc, argv);
+  std::signal(SIGINT, sigHandler);
+  std::signal(SIGTERM, sigHandler);
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription("Navigation server. Runs stand-alone, or using pre-computer route");
+  parser.addHelpOption();
+  parser.process(app);
+  const QStringList args = parser.positionalArguments();
+
+  route_engine = new RouteEngine();
+
+  return app.exec();
+}
