1d72756b0efc6b614134c01b7e0b670968d71374
Willem Melching <willem.melching@gmail.com>
2021-11-04 14:15:05 +0100

    test (almost) all release notes and add some extra html encoding

--- common/markdown.py
+++ common/markdown.py
@@ -1,5 +1,10 @@
 from typing import List
 
+HTML_REPLACEMENTS = [
+  (r'&', r'&amp;'),
+  (r'"', r'&quot;'),
+]
+
 
 def parse_markdown(text: str, tab_length: int = 2) -> str:
   lines = text.split("\n")
@@ -15,11 +20,11 @@ def parse_markdown(text: str, tab_length: int = 2) -> str:
     return end_level
 
   for i, line in enumerate(lines):
-    if i+1 < len(lines) and lines[i+1].startswith("==="): # heading
+    if i + 1 < len(lines) and lines[i + 1].startswith("==="):  # heading
       output.append(f"<h1>{line}</h1>")
     elif line.startswith("==="):
       pass
-    elif line.lstrip().startswith("* "): # list
+    elif line.lstrip().startswith("* "):  # list
       line_level = 1 + line.count(" " * tab_length, 0, line.index("*"))
       if list_level >= line_level:
         list_level = end_outstanding_lists(list_level, line_level)
@@ -29,11 +34,15 @@ def parse_markdown(text: str, tab_length: int = 2) -> str:
           output[-1] = output[-1].replace("</li>", "")
         output.append("<ul>")
       output.append(f"<li>{line.replace('*', '', 1).lstrip()}</li>")
-    else: # catchall
+    else:
       list_level = end_outstanding_lists(list_level, 0)
       if len(line) > 0:
         output.append(line)
 
   end_outstanding_lists(list_level, 0)
-  return "\n".join(output) + "\n"
+  output_str = "\n".join(output) + "\n"
 
+  for (fr, to) in HTML_REPLACEMENTS:
+    output_str = output_str.replace(fr, to)
+
+  return output_str
