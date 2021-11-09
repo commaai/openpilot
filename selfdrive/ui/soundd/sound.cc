797bb1acb0ed548bda23b9f76458a102bf7ccf5a
Dean Lee <deanlee3@gmail.com>
2021-11-10 06:18:29 +0800

    soundd: misc cleanup + tests (#22796)
    * pass AudibleAlert to setAlert
    
    * apply reviews
    
    * remove trailing underscore
    
    * add current_alert_type
    
    * common function get_alert
    
    * same freq as ui
    
    * small cleanup
    
    * setvolumne on change
    
    * static Alert::get
    
    * test_sounds
    
    * remove old test files
    
    * loop twice
    
    * assert loopsRemaining
    
    * assert stop
    
    * init to min_volumne
    
    * Revert "remove old test files"
    
    This reverts commit 12e8bfefae1ae7f8649d3eda1e4126ff8eb1fe17.
    
    * move to ui/soundd
    
    * move test to tests/test_sound.cc
    
    * remove old tests
    
    * cleanup
    
    * lower volume
    
    * Revert "remove old tests"
    
    This reverts commit e6bb12abfa3d5cd0aa8986d2b8da0b1edc7ea31b.
    
    * return Alert
    
    * cleanup
    
    * remove #include <optional>
    
    * cleanup .gitignore
    
    * revert to std::optional
    
    * Revert "revert to std::optional"
    
    This reverts commit a66291c51f3a846652837cc4a43d5d60dd81896b.
    
    * cleanup
    
    * remove volume check
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

---
+++ selfdrive/ui/soundd/sound.cc
@@ -0,0 +1,58 @@
+#include "selfdrive/ui/soundd/sound.h"
+
+#include "cereal/messaging/messaging.h"
+#include "selfdrive/common/util.h"
+
+// TODO: detect when we can't play sounds
+// TODO: detect when we can't display the UI
+
+Sound::Sound(QObject *parent) : sm({"carState", "controlsState"}) {
+  const QString sound_asset_path = Hardware::TICI() ? "../../assets/sounds_tici/" : "../../assets/sounds/";
+  for (auto &[alert, fn, loops] : sound_list) {
+    QSoundEffect *s = new QSoundEffect(this);
+    QObject::connect(s, &QSoundEffect::statusChanged, [=]() {
+      assert(s->status() != QSoundEffect::Error);
+    });
+    s->setSource(QUrl::fromLocalFile(sound_asset_path + fn));
+    s->setVolume(Hardware::MIN_VOLUME);
+    sounds[alert] = {s, loops ? QSoundEffect::Infinite : 0};
+  }
+
+  QTimer *timer = new QTimer(this);
+  QObject::connect(timer, &QTimer::timeout, this, &Sound::update);
+  timer->start(1000 / UI_FREQ);
+};
+
+void Sound::update() {
+  sm.update(0);
+  if (sm.updated("carState")) {
+    // scale volume with speed
+    float volume = util::map_val(sm["carState"].getCarState().getVEgo(), 0.f, 20.f,
+                                 Hardware::MIN_VOLUME, Hardware::MAX_VOLUME);
+    for (auto &[s, loops] : sounds) {
+      s->setVolume(std::round(100 * volume) / 100);
+    }
+  }
+
+  setAlert(Alert::get(sm, 1));
+}
+
+void Sound::setAlert(const Alert &alert) {
+  if (!current_alert.equal(alert)) {
+    current_alert = alert;
+    // stop sounds
+    for (auto &[s, loops] : sounds) {
+      // Only stop repeating sounds
+      if (s->loopsRemaining() == QSoundEffect::Infinite) {
+        s->stop();
+      }
+    }
+
+    // play sound
+    if (alert.sound != AudibleAlert::NONE) {
+      auto &[s, loops] = sounds[alert.sound];
+      s->setLoopCount(loops);
+      s->play();
+    }
+  }
+}
