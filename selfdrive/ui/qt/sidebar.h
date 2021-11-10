be3d41386644aaaa4ca5b7cd2987ca0b6ee6031d
deanlee <deanlee3@gmail.com>
2021-11-11 06:31:00 +0800

    remove params

--- selfdrive/ui/qt/sidebar.h
+++ selfdrive/ui/qt/sidebar.h
@@ -48,7 +48,6 @@ protected:
   const QColor warning_color = QColor(218, 202, 37);
   const QColor danger_color = QColor(201, 34, 49);
 
-  Params params;
   ItemStatus connect_status, panda_status, temp_status;
   QString net_type;
   int net_strength = 0;
