2bd082e3cdf4cad008c4e5a2d1694252c14b742e
Willem Melching <willem.melching@gmail.com>
2021-11-10 13:31:37 +0100

    fix indent

--- selfdrive/common/util.cc
+++ selfdrive/common/util.cc
@@ -210,19 +210,19 @@ std::string dir_name(std::string const &path) {
 }
 
 std::string check_output(const std::string& command) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
+  std::array<char, 128> buffer;
+  std::string result;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
 
-    if (!pipe) {
-      return "";
-    }
+  if (!pipe) {
+    return "";
+  }
 
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-      result += buffer.data();
-    }
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result += buffer.data();
+  }
 
-    return result;
+  return result;
 }
 
 struct tm get_time() {
