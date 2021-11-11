baa81a5429b1dd045e303abd6a6d91522ee079f7
Greg Hogan <gregjhogan@gmail.com>
2021-11-10 17:23:17 -0800

    find process by name

--- selfdrive/debug/profiling/py-spy/profile.sh
+++ selfdrive/debug/profiling/py-spy/profile.sh
@@ -1,4 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+
 cd "$(dirname "$0")"
-sudo env PATH=$PATH py-spy record -d 5 -o /tmp/perf$1.svg -p $1 &&
-google-chrome /tmp/perf$1.svg
+
+# find process with name passed in (excluding this process)
+for PID in $(pgrep -f $1); do
+  if [ "$PID" != "$$" ]; then
+    ps -p $PID -o args
+    TRACE_PID=$PID
+    break
+  fi
+done
+
+if [ -z "$TRACE_PID" ]; then
+  echo "could not find PID for $1"
+  exit 1
+fi
+
+sudo env PATH=$PATH py-spy record -d 5 -o /tmp/perf$TRACE_PID.svg -p $TRACE_PID &&
+google-chrome /tmp/perf$TRACE_PID.svg
