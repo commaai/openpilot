13dc3ac8e4867394c0e6fa8519bf4921ad32f07d
Harald Schafer <harald.the.engineer@gmail.com>
2021-11-13 01:04:38 -0800

    tss2 as well

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -324,12 +324,12 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalActuatorDelayUpperBound = 0.7
     elif candidate in [CAR.COROLLA_TSS2, CAR.COROLLAH_TSS2, CAR.RAV4_TSS2, CAR.RAV4H_TSS2, CAR.LEXUS_NX_TSS2]:
       # Improved longitudinal tune
-      ret.longitudinalTuning.deadzoneBP = [0., 8.05]
-      ret.longitudinalTuning.deadzoneV = [.0, .14]
-      ret.longitudinalTuning.kpBP = [0., 5., 20.]
-      ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
-      ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
-      ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
+      ret.longitudinalTuning.kpBP = [0.]
+      ret.longitudinalTuning.kpV = [0.]
+      ret.longitudinalTuning.kpBP = [0.]
+      ret.longitudinalTuning.kiV = [0.]
+      ret.longitudinalActuatorDelayLowerBound = 0.7
+      ret.longitudinalActuatorDelayUpperBound = 0.7
       ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
       ret.startingAccelRate = 6.0  # release brakes fast
     else:
