85c03f11b1acb07aedd1d4c1fb796adbbf73258b
deanlee <deanlee3@gmail.com>
2021-11-16 19:16:28 +0800

    sort alphabetically

--- selfdrive/ui/paint.h
+++ selfdrive/ui/paint.h
@@ -2,7 +2,6 @@
 
 #include "selfdrive/ui/ui.h"
 
-void ui_update_params(UIState *s);
 void ui_draw(UIState *s, int w, int h);
 void ui_draw_image(const UIState *s, const Rect &r, const char *name, float alpha);
 void ui_draw_rect(NVGcontext *vg, const Rect &r, NVGcolor color, int width, float radius = 0);
@@ -10,3 +9,4 @@ void ui_fill_rect(NVGcontext *vg, const Rect &r, const NVGpaint &paint, float ra
 void ui_fill_rect(NVGcontext *vg, const Rect &r, const NVGcolor &color, float radius = 0);
 void ui_nvg_init(UIState *s);
 void ui_resize(UIState *s, int width, int height);
+void ui_update_params(UIState *s);
