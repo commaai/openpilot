d1eacc461909fbcfc03a5d5ceef965850786a633
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-11-16 13:11:32 -0800

    alerts: make soft disables an orange alert (#22925)
    old-commit-hash: 833399e9418db26df70169d37c2dff90051d89c7

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -149,7 +149,7 @@ class NoEntryAlert(Alert):
 class SoftDisableAlert(Alert):
   def __init__(self, alert_text_2):
     super().__init__("TAKE CONTROL IMMEDIATELY", alert_text_2,
-                     AlertStatus.critical, AlertSize.full,
+                     AlertStatus.userPrompt, AlertSize.full,
                      Priority.MID, VisualAlert.steerRequired,
                      AudibleAlert.chimeWarningRepeatInfinite, 2.),
 
