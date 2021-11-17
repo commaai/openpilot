a3959c3145920c7c875133bf349e0a010936c7f1
deanlee <deanlee3@gmail.com>
2021-11-18 05:53:55 +0800

    don't check chunks count

--- selfdrive/boardd/tests/test_boardd.cc
+++ selfdrive/boardd/tests/test_boardd.cc
@@ -11,26 +11,13 @@ struct PandaTest : public Panda {
   void test_can_send();
 };
 
-std::string read_chunks(uint8_t *data, int size) {
-  std::string result;
-  result.reserve(size);
-
-  int size_left = size;
-  for (int i = 0, counter = 0; i < size; i += CHUNK_SIZE, counter++) {
-    const int len = std::min(CHUNK_SIZE - 1, size_left - 1);
-    result.append((char *)&data[i + 1], len);
-    size_left -= (len + 1);
-  }
-  return result;
-}
-
 void PandaTest::test_can_send() {
-  auto can_list_size = GENERATE(1, 2, 3, 10, 20, 40, 60, 80, 100);
+  auto can_list_size = GENERATE(1, 2, 3, 10, 20, 40, 60, 80, 100, 200);
 
   MessageBuilder msg;
   uint8_t bus = 1;
   uint32_t address = 5;
-  uint8_t dat[4] = {1, 2, 3, 4};
+  uint8_t dat[] = {1, 2, 3, 4, 5};
 
   size_t total_pakets_size = 0;
   auto can_list = msg.initEvent().initSendcan(can_list_size);
@@ -47,20 +34,22 @@ void PandaTest::test_can_send() {
   int chunks = 0;
   std::string unpacked_data;
   write_can_packets(can_list.asReader(), [&](uint8_t *data, size_t size) {
-    for (int i = 0, counter = 0; i < size; i += 64, counter++) {
+    int size_left = size;
+    for (int i = 0, counter = 0; i < size; i += CHUNK_SIZE, counter++) {
       REQUIRE(data[i] == counter);
 
-      uint8_t *can = &data[i + 1];
-      int pos_0 = (len_to_dlc(sizeof(dat)) << 4 | ((bus - bus_offset) << 1));
-      REQUIRE(can[0] == pos_0);
-      REQUIRE(*(uint32_t *)&can[1] == address << 3);
+      const int len = std::min(CHUNK_SIZE - 1, size_left - 1);
+      unpacked_data.append((char *)&data[i + 1], len);
+      size_left -= (len + 1);
+
       ++chunks;
     }
-    unpacked_data += read_chunks(data, size);
   });
-  REQUIRE(unpacked_data.size() == total_pakets_size);
-  REQUIRE(chunks == (int)std::ceil(total_pakets_size / 63.0));
 
+  REQUIRE(unpacked_data.size() == total_pakets_size);
+  // REQUIRE(chunks == (int)std::ceil(total_pakets_size / 63.0));
+
+  int cnt = 0;
   INFO("test can message integrity");
   for (int pos = 0, pckt_len = 0; pos < unpacked_data.size(); pos += pckt_len) {
     const uint8_t data_len = dlc_to_len[(unpacked_data[pos] >> 4)];
@@ -68,7 +57,9 @@ void PandaTest::test_can_send() {
 
     REQUIRE(*(uint32_t *)&unpacked_data[pos + 1] == address << 3);
     REQUIRE(memcmp(dat, &unpacked_data[pos + 5], sizeof(dat)) == 0);
+    ++cnt;
   }
+  REQUIRE(cnt == can_list_size);
 }
 
 TEST_CASE("can_send") {
