df7344886b3828513edd938e8dcc7207b950e3b6
deanlee <deanlee3@gmail.com>
2021-11-19 01:23:29 +0800

    cleanup test

--- selfdrive/boardd/tests/test_boardd.cc
+++ selfdrive/boardd/tests/test_boardd.cc
@@ -1,5 +1,7 @@
 #define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include <random>
+
 #include "catch2/catch.hpp"
 #include "cereal/messaging/messaging.h"
 #include "selfdrive/boardd/panda.h"
@@ -15,10 +17,11 @@ int random_int(int min, int max) {
 }
 
 class PandaTest : public Panda {
-public:
+ public:
   PandaTest(uint32_t bus_offset);
-  void test_can_send(capnp::List<cereal::CanData>::Reader can_data_list, int total_pakets_size);
-  void test_can_recv(capnp::List<cereal::CanData>::Reader can_data_list);
+  void test_can_send(const capnp::List<cereal::CanData>::Reader &can_data_list, int total_pakets_size);
+  void test_can_recv(const capnp::List<cereal::CanData>::Reader &can_data_list);
+  void benchmark(const capnp::List<cereal::CanData>::Reader &can_data_list);
   std::map<int, std::string> test_data;
 };
 
@@ -34,16 +37,16 @@ PandaTest::PandaTest(uint32_t buf_offset_) : Panda(buf_offset_) {
   }
 }
 
-void PandaTest::test_can_send(capnp::List<cereal::CanData>::Reader can_data_list, int total_pakets_size) {
-  std::string unpacked_data;
+void PandaTest::test_can_send(const capnp::List<cereal::CanData>::Reader &can_data_list, int total_pakets_size) {
+  std::vector<uint8_t> unpacked_data;
   this->pack_can_buffer(can_data_list, [&](uint8_t *chunk, size_t size) {
     int size_left = size;
     for (int i = 0, counter = 0; i < size; i += CHUNK_SIZE, counter++) {
       REQUIRE(chunk[i] == counter);
 
-      const int len = std::min(CHUNK_SIZE - 1, size_left - 1);
-      unpacked_data.append((char *)&chunk[i + 1], len);
-      size_left -= (len + 1);
+      const int len = std::min(CHUNK_SIZE, size_left);
+      unpacked_data.insert(unpacked_data.end(), &chunk[i + 1], &chunk[i + len]);
+      size_left -= len;
     }
   });
   REQUIRE(unpacked_data.size() == total_pakets_size);
@@ -55,18 +58,18 @@ void PandaTest::test_can_send(capnp::List<cereal::CanData>::Reader can_data_list
     memcpy(&header, &unpacked_data[pos], CANPACKET_HEAD_SIZE);
     const uint8_t data_len = dlc_to_len[header.data_len_code];
     pckt_len = CANPACKET_HEAD_SIZE + data_len;
-    
+
     REQUIRE(header.addr == cnt);
 
     REQUIRE(test_data.find(data_len) != test_data.end());
-    std::string &dat = test_data[data_len];
+    const std::string &dat = test_data[data_len];
     REQUIRE(memcmp(dat.data(), &unpacked_data[pos + 5], dat.size()) == 0);
     ++cnt;
   }
   REQUIRE(cnt == can_data_list.size());
 }
 
-void PandaTest::test_can_recv(capnp::List<cereal::CanData>::Reader can_data_list) {
+void PandaTest::test_can_recv(const capnp::List<cereal::CanData>::Reader &can_data_list) {
   std::vector<can_frame> frames;
   this->pack_can_buffer(can_data_list, [&](uint8_t *data, size_t size) {
     this->unpack_can_buffer(data, size, frames);
@@ -76,13 +79,13 @@ void PandaTest::test_can_recv(capnp::List<cereal::CanData>::Reader can_data_list
   for (int i = 0; i < frames.size(); ++i) {
     REQUIRE(frames[i].address == i);
     REQUIRE(test_data.find(frames[i].dat.size()) != test_data.end());
-    std::string &dat = test_data[frames[i].dat.size()];
+    const std::string &dat = test_data[frames[i].dat.size()];
     REQUIRE(memcmp(dat.data(), frames[i].dat.data(), dat.size()) == 0);
   }
 }
 
 TEST_CASE("send/recv can packets") {
-  auto can_list_size = GENERATE(1, 3, 5, 20, 40, 60, 80, 100, 200);
+  auto can_list_size = GENERATE(1, 3, 5, 10, 30, 60, 100, 200);
   PandaTest test(0);
 
   size_t total_pakets_size = 0;
@@ -91,14 +94,14 @@ TEST_CASE("send/recv can packets") {
   for (uint8_t i = 0; i < can_list.size(); ++i) {
     auto can = can_list[i];
     uint32_t id = random_int(0, std::size(dlc_to_len) - 1);
-    std::string &dat = test.test_data[dlc_to_len[id]];
+    const std::string &dat = test.test_data[dlc_to_len[id]];
     can.setAddress(i);
     can.setDat(kj::ArrayPtr((uint8_t *)dat.data(), dat.size()));
     total_pakets_size += CANPACKET_HEAD_SIZE + dat.size();
   }
 
   INFO("test " << can_list_size << " packets, total size " << total_pakets_size);
-  
+
   SECTION("can_send") {
     test.test_can_send(can_list.asReader(), total_pakets_size);
   }
