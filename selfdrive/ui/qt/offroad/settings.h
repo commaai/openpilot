1783e4b3e15f560dd067a98e32555d5bbcd11b49
deanlee <deanlee3@gmail.com>
2021-11-19 13:08:22 +0800

    move SettingsWindow up

--- selfdrive/ui/qt/offroad/settings.h
+++ selfdrive/ui/qt/offroad/settings.h
@@ -12,7 +12,28 @@
 #include "selfdrive/ui/qt/widgets/controls.h"
 
 // ********** settings window + top-level panels **********
-class SettingsWindow;
+class SettingsWindow : public QFrame {
+  Q_OBJECT
+
+public:
+  explicit SettingsWindow(QWidget *parent = 0);
+
+protected:
+  void hideEvent(QHideEvent *event) override;
+  void showEvent(QShowEvent *event) override;
+
+signals:
+  void closeSettings();
+  void offroadTransition(bool offroad);
+  void reviewTrainingGuide();
+  void showDriverView();
+
+private:
+  QPushButton *sidebar_alert_widget;
+  QWidget *sidebar_widget;
+  QButtonGroup *nav_btns;
+  QStackedWidget *panel_widget;
+};
 
 class DevicePanel : public ListWidget {
   Q_OBJECT
@@ -47,27 +68,4 @@ private:
 
   Params params;
   QFileSystemWatcher *fs_watch;
-};
-
-class SettingsWindow : public QFrame {
-  Q_OBJECT
-
-public:
-  explicit SettingsWindow(QWidget *parent = 0);
-
-protected:
-  void hideEvent(QHideEvent *event) override;
-  void showEvent(QShowEvent *event) override;
-
-signals:
-  void closeSettings();
-  void offroadTransition(bool offroad);
-  void reviewTrainingGuide();
-  void showDriverView();
-
-private:
-  QPushButton *sidebar_alert_widget;
-  QWidget *sidebar_widget;
-  QButtonGroup *nav_btns;
-  QStackedWidget *panel_widget;
 };
