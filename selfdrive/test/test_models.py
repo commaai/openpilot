2723cc6cf8c6481a510c9347ef0526b53f679e43
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-11-18 20:42:48 -0800

    test car models: only check cruise state on PCM cruise cars

--- selfdrive/test/test_models.py
+++ selfdrive/test/test_models.py
@@ -178,7 +178,8 @@ class TestCarModel(unittest.TestCase):
       # check that openpilot and panda safety agree on the car's state
       checks['gasPressed'] += CS.gasPressed != safety.get_gas_pressed_prev()
       checks['brakePressed'] += CS.brakePressed != safety.get_brake_pressed_prev()
-      checks['controlsAllowed'] += not CS.cruiseState.enabled and safety.get_controls_allowed()
+      if self.CP.pcmCruise:
+        checks['controlsAllowed'] += not CS.cruiseState.enabled and safety.get_controls_allowed()
 
     # TODO: reduce tolerance to 0
     failed_checks = {k: v for k, v in checks.items() if v > 25}
