1d8e34024d082bbdc12336a0dbd593e8d21ddbb0
Shane Smiskol <shane@smiskol.com>
2021-11-20 10:49:16 -0600

    use v_target to determine stopping

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -107,7 +107,7 @@ class LongControl():
 
       # Toyota starts braking more when it thinks you want to stop
       # Freeze the integrator so we don't accelerate to compensate, and don't allow positive acceleration
-      prevent_overshoot = not CP.stoppingControl and CS.vEgo < 1.5 and v_target_future < 0.7 and a_target < 0
+      prevent_overshoot = not CP.stoppingControl and CS.vEgo < 1.5 and v_target > v_target_future < 0.7
       deadzone = interp(CS.vEgo, CP.longitudinalTuning.deadzoneBP, CP.longitudinalTuning.deadzoneV)
       freeze_integrator = prevent_overshoot
 
