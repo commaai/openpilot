5d6041e8134524403cd73f18836d35970b340195
deanlee <deanlee3@gmail.com>
2021-11-24 10:38:25 +0800

    loop 10 times

--- selfdrive/loggerd/tests/test_loggerd.cc
+++ selfdrive/loggerd/tests/test_loggerd.cc
@@ -10,7 +10,7 @@ const int MAX_CAMERAS = 3;
 TEST_CASE("sync_encoders") {
   auto thread_func = [](LoggerdState *s, CameraType cam_type) -> int {
     srand(time(nullptr));
-    int frame_id = rand() % 10;
+    int frame_id = rand() % 20;
     while (!sync_encoders(s, cam_type, frame_id)) {
       ++frame_id;
       usleep(0);
@@ -18,16 +18,21 @@ TEST_CASE("sync_encoders") {
     return frame_id;
   };
 
-  LoggerdState s{.max_waiting = MAX_CAMERAS};
-  std::future<int> futures[MAX_CAMERAS];
-  for (int i = 0; i < MAX_CAMERAS; ++i) {
-    futures[i] = std::async(std::launch::async, thread_func, &s, (CameraType)i);
-  }
+  for (int test_cnt = 0; test_cnt < 10; ++test_cnt) {
+    LoggerdState s{.max_waiting = MAX_CAMERAS};
+    std::future<int> futures[MAX_CAMERAS];
+    for (int i = 0; i < MAX_CAMERAS; ++i) {
+      futures[i] = std::async(std::launch::async, thread_func, &s, (CameraType)i);
+    }
 
-  // get results
-  std::vector<int> synced_frame_ids;
-  for (int i = 0; i < MAX_CAMERAS; ++i) {
-    synced_frame_ids.push_back(futures[i].get());
+    // get results
+    int frame_id = 0;
+    for (int i = 0; i < MAX_CAMERAS; ++i) {
+      if (i == 0) {
+        frame_id = futures[i].get();
+      } else {
+        REQUIRE(futures[i].get() == frame_id);
+      }
+    }
   }
-  REQUIRE(std::equal(synced_frame_ids.begin() + 1, synced_frame_ids.end(), synced_frame_ids.begin()));
 }
