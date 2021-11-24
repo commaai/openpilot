4ac67eb83c083c6c706ceed00568534ec9ca32e8
Shane Smiskol <shane@smiskol.com>
2021-11-24 11:07:38 -0600

    vEgoStopping needs to be less than or equal to vEgoStarting to avoid state oscillation

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -7,8 +7,6 @@ from selfdrive.modeld.constants import T_IDXS
 
 LongCtrlState = car.CarControl.Actuators.LongControlState
 
-STOPPING_TARGET_SPEED = 0.31
-
 # As per ISO 15622:2018 for all speeds
 ACCEL_MIN_ISO = -3.5  # m/s^2
 ACCEL_MAX_ISO = 2.0  # m/s^2
@@ -19,7 +17,7 @@ def long_control_state_trans(CP, active, long_control_state, v_ego, v_target_fut
   """Update longitudinal control state machine"""
   stopping_condition = (v_ego < 2.0 and cruise_standstill) or \
                        (v_ego < CP.vEgoStopping and
-                        (v_target_future < STOPPING_TARGET_SPEED or brake_pressed))
+                        (v_target_future < CP.vEgoStopping or brake_pressed))
 
   starting_condition = v_target_future > CP.vEgoStarting and not cruise_standstill
 
