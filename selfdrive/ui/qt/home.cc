8546481eb1e97d0db89a303102ff9cf05e78c967
Rick Lan <ricklan@gmail.com>
2021-11-26 08:30:45 +0800

    make translation friendly

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -178,6 +178,6 @@ void OffroadHome::refresh() {
   update_notif->setVisible(updateAvailable);
   alert_notif->setVisible(alerts);
   if (alerts) {
-    alert_notif->setText(QString::number(alerts) + " ALERT" + (alerts > 1 ? "S" : ""));
+    alert_notif->setText(QString::number(alerts) + (alerts > 1 ? " ALERTS" : " ALERT"));
   }
 }
