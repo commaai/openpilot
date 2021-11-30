e81ebf58d5b16faa9fee8d27f0218553729fa073
deanlee <deanlee3@gmail.com>
2021-11-30 17:48:26 +0800

    use setLoopCount(0) to stop sound

--- selfdrive/ui/soundd/sound.cc
+++ selfdrive/ui/soundd/sound.cc
@@ -59,7 +59,7 @@ void Sound::setAlert(const Alert &alert) {
     for (auto &[s, loops] : sounds) {
       // Only stop repeating sounds
       if (s->loopsRemaining() > 1 || s->loopsRemaining() == QSoundEffect::Infinite) {
-        s->stop();
+        s->setLoopCount(0);
       }
     }
 
