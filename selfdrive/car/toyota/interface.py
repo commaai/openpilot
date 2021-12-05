a418490c2d362882c8e3b58fd84a262b044e1f83
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-12-05 13:55:45 +1100

    replace FULL_SPEED_DRCC_CAR with NO_AUTO_RESUME_CAR
    for less confusion

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -2,7 +2,7 @@
 from cereal import car
 from selfdrive.config import Conversions as CV
 from selfdrive.car.toyota.tunes import LatTunes, LongTunes, set_long_tune, set_lat_tune
-from selfdrive.car.toyota.values import Ecu, CAR, TSS2_CAR, FULL_SPEED_DRCC_CAR, NO_DSU_CAR, MIN_ACC_SPEED, CarControllerParams
+from selfdrive.car.toyota.values import Ecu, CAR, TSS2_CAR, NO_AUTO_RESUME_CAR, NO_DSU_CAR, MIN_ACC_SPEED, CarControllerParams
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.interfaces import CarInterfaceBase
 
@@ -269,7 +269,7 @@ class CarInterface(CarInterfaceBase):
 
     if ret.enableGasInterceptor:
       # these cars have full speed DRCC, use stock tune
-      if candidate in FULL_SPEED_DRCC_CAR:
+      if candidate in NO_AUTO_RESUME_CAR:
         set_long_tune(ret.longitudinalTuning, LongTunes.TSS)
       else:
         set_long_tune(ret.longitudinalTuning, LongTunes.PEDAL)
