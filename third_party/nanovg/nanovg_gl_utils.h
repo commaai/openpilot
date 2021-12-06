3dc321435e408114469042aeb08db68ff4941e5a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-12-06 14:18:00 -0800

    Remove NanoVG (#23147)
    old-commit-hash: efa448d829657c3a146134f146c91b8751774d7e

--- third_party/nanovg/nanovg_gl_utils.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eb937e297077b4dd5146fcd3365f556bb64d7812d4cad7c0752ae97f8d703ec5
-size 4275
--- third_party/nanovg/stb_truetype.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5e28e92519846ef1a6118a454cdec146f6375655fb7e53b6102bfb224d16c17a
-size 195647
