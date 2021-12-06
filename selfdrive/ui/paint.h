88ebf835bbd07a12e85bf79dd0193bf2d3b23834
Dean Lee <deanlee3@gmail.com>
2021-12-07 06:02:39 +0800

    UI: move rest of NVG to Qt (#23133)
    * move ui_draw_world to qt
    
    * draw world if world_objects_visible is true
    
    * cleanup
    
    * cleanup includes
    
    * continue
    
    * cleanup
    
    * cleanup
    
    * enable  multisampling on all devices
    old-commit-hash: 20f571db3c37c6d009ca3d9a646874769aa07acd

--- selfdrive/ui/paint.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "selfdrive/ui/ui.h"
-
-void ui_draw(UIState *s, int w, int h);
-void ui_nvg_init(UIState *s);
-void ui_resize(UIState *s, int width, int height);
-void ui_update_params(UIState *s);
