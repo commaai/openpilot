061d7f16671b8dcc1d45819a8d9b971c675f26f6
deanlee <deanlee3@gmail.com>
2021-12-12 00:11:21 +0800

    move to class ModelFrame

--- selfdrive/modeld/models/commonmodel.h
+++ selfdrive/modeld/models/commonmodel.h
@@ -16,10 +16,6 @@
 #include "selfdrive/modeld/transforms/loadyuv.h"
 #include "selfdrive/modeld/transforms/transform.h"
 
-constexpr int MODEL_WIDTH = 512;
-constexpr int MODEL_HEIGHT = 256;
-constexpr int MODEL_FRAME_SIZE = MODEL_WIDTH * MODEL_HEIGHT * 3 / 2;
-
 const bool send_raw_pred = getenv("SEND_RAW_PRED") != NULL;
 
 void softmax(const float* input, float* output, size_t len);
@@ -27,14 +23,17 @@ float softplus(float input);
 float sigmoid(float input);
 
 class ModelFrame {
- public:
+public:
   ModelFrame(cl_device_id device_id, cl_context context);
   ~ModelFrame();
   float* prepare(cl_mem yuv_cl, int width, int height, const mat3& transform, cl_mem *output);
 
+  const int MODEL_WIDTH = 512;
+  const int MODEL_HEIGHT = 256;
+  const int MODEL_FRAME_SIZE = MODEL_WIDTH * MODEL_HEIGHT * 3 / 2;
   const int buf_size = MODEL_FRAME_SIZE * 2;
 
- private:
+private:
   Transform transform;
   LoadYUVState loadyuv;
   cl_command_queue q;
