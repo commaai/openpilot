648f2b0397a15fa4339d1715a1252f2b790612cc
Andrew Tec <andrew.tec@pm.me>
2021-12-12 03:35:01 -0800

    fix missing python modules

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -15,8 +15,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+# relative to build/sphinx/
+sys.path.insert(0, os.path.abspath('.'))                # sphinx/build
+sys.path.insert(0, os.path.abspath('../../../'))        # openpilot/
 
 
 
