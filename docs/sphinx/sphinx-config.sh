54731d83b9f7888fd1b111a4ddc83cd73ca8409a
Andrew Tec <andrew.tec@pm.me>
2021-12-12 03:08:12 -0800

    ignore this dir

--- docs/sphinx/sphinx-config.sh
+++ docs/sphinx/sphinx-config.sh
@@ -10,6 +10,7 @@ cd $SRC && \
     find . -type f \( -name "*.md" -o -name "*.rst" -o -name "*.png" -o -name "*.jpg" \) \
         -not -path "*/.*" \
         -not -path "./docs/build/*" \
+        -not -path "./docs/sphinx/*" \
         -not -path "./xx/*" \
         -not -path "./yy/*" \
         -exec cp --parents "{}" $OUTPUT \;
