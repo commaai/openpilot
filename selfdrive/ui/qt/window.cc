2483fc5d5dd7ad586a92b62b2ef5bc8f9094f262
Dean Lee <deanlee3@gmail.com>
2021-12-15 14:28:12 +0800

    ui: singleton uistate (#22789)
    * singleton uistate
    
    * merge master
    
    * merge master
    
    * merge master
    
    * connect uistate in sidebar
    
    * move to device

--- selfdrive/ui/qt/window.cc
+++ selfdrive/ui/qt/window.cc
@@ -12,14 +12,10 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
   main_layout->addWidget(homeWindow);
   QObject::connect(homeWindow, &HomeWindow::openSettings, this, &MainWindow::openSettings);
   QObject::connect(homeWindow, &HomeWindow::closeSettings, this, &MainWindow::closeSettings);
-  QObject::connect(&qs, &QUIState::uiUpdate, homeWindow, &HomeWindow::update);
-  QObject::connect(&qs, &QUIState::offroadTransition, homeWindow, &HomeWindow::offroadTransition);
-  QObject::connect(&qs, &QUIState::offroadTransition, homeWindow, &HomeWindow::offroadTransitionSignal);
 
   settingsWindow = new SettingsWindow(this);
   main_layout->addWidget(settingsWindow);
   QObject::connect(settingsWindow, &SettingsWindow::closeSettings, this, &MainWindow::closeSettings);
-  QObject::connect(&qs, &QUIState::offroadTransition, settingsWindow, &SettingsWindow::offroadTransition);
   QObject::connect(settingsWindow, &SettingsWindow::reviewTrainingGuide, [=]() {
     onboardingWindow->showTrainingGuide();
     main_layout->setCurrentWidget(onboardingWindow);
@@ -37,8 +33,7 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     main_layout->setCurrentWidget(onboardingWindow);
   }
 
-  QObject::connect(&qs, &QUIState::uiUpdate, &device, &Device::update);
-  QObject::connect(&qs, &QUIState::offroadTransition, [=](bool offroad) {
+  QObject::connect(uiState(), &UIState::offroadTransition, [=](bool offroad) {
     if (!offroad) {
       closeSettings();
     }
@@ -79,7 +74,7 @@ void MainWindow::openSettings() {
 void MainWindow::closeSettings() {
   main_layout->setCurrentWidget(homeWindow);
 
-  if (QUIState::ui_state.scene.started) {
+  if (uiState()->scene.started) {
     homeWindow->showSidebar(false);
   }
 }
