986709dddb6bb69718037884c60964024348f79b
Willem Melching <willem.melching@gmail.com>
2021-12-15 16:25:08 +0100

    gm cleanup

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -34,7 +34,6 @@ class CarController():
 
     # Send CAN commands.
     can_sends = []
-    new_actuators = actuators.copy()
 
     # Steering (50Hz)
     # Avoid GM EPS faults when transmitting messages too close together: skip this transmit if we just received the
@@ -56,7 +55,6 @@ class CarController():
       idx = (CS.lka_steering_cmd_counter + 1) % 4
 
       can_sends.append(gmcan.create_steering_control(self.packer_pt, CanBus.POWERTRAIN, apply_steer, idx, lkas_enabled))
-    new_actuators.steer = self.apply_steer_last / P.STEER_MAX
 
     # Gas/regen and brakes - all at 25Hz
     if (frame % 4) == 0:
@@ -74,9 +72,6 @@ class CarController():
       near_stop = enabled and (CS.out.vEgo < P.NEAR_STOP_BRAKE_PHASE)
       can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, CanBus.CHASSIS, self.apply_brake, idx, near_stop, at_full_stop))
       can_sends.append(gmcan.create_gas_regen_command(self.packer_pt, CanBus.POWERTRAIN, self.apply_gas, idx, enabled, at_full_stop))
-
-    new_actuators.gas = self.apply_gas
-    new_actuators.brake = self.apply_brake
 
     # Send dashboard UI commands (ACC status), 25hz
     if (frame % 4) == 0:
@@ -113,5 +108,10 @@ class CarController():
       steer_alert = hud_alert in [VisualAlert.steerRequired, VisualAlert.ldw]
       can_sends.append(gmcan.create_lka_icon_command(CanBus.SW_GMLAN, lka_active, lka_critical, steer_alert))
       self.lka_icon_status_last = lka_icon_status
+
+    new_actuators = actuators.copy()
+    new_actuators.steer = self.apply_steer_last / P.STEER_MAX
+    new_actuators.gas = self.apply_gas
+    new_actuators.brake = self.apply_brake
 
     return new_actuators, can_sends
