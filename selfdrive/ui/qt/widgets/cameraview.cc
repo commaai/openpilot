6ca6de25ecc150327912f10f5297065c9e21b516
deanlee <deanlee3@gmail.com>
2021-12-16 02:44:02 +0800

    gate that with Hardware::EON

--- selfdrive/ui/qt/widgets/cameraview.cc
+++ selfdrive/ui/qt/widgets/cameraview.cc
@@ -199,7 +199,7 @@ void CameraViewWidget::updateFrameMat(int w, int h) {
 }
 
 void CameraViewWidget::paintGL() {
-  if (latest_texture_id == -1 || stream_type == VISION_STREAM_RGB_FRONT) {
+  if (latest_texture_id == -1 || (stream_type == VISION_STREAM_RGB_FRONT && Hardware::EON())) {
     glClearColor(bg.redF(), bg.greenF(), bg.blueF(), bg.alphaF());
     glClear(GL_STENCIL_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
   }
