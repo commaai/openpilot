4b13d224bd343b6068a295959a33fb9ab02d332d
Willem Melching <willem.melching@gmail.com>
2021-12-15 17:03:12 +0100

    manage_athenad.py: add daemon argument to launcher
    old-commit-hash: d09cc197d11efa2181ce68ef4212cb9df5ee285c

--- selfdrive/athena/manage_athenad.py
+++ selfdrive/athena/manage_athenad.py
@@ -19,7 +19,7 @@ def main():
   try:
     while 1:
       cloudlog.info("starting athena daemon")
-      proc = Process(name='athenad', target=launcher, args=('selfdrive.athena.athenad',))
+      proc = Process(name='athenad', target=launcher, args=('selfdrive.athena.athenad', 'athenad'))
       proc.start()
       proc.join()
       cloudlog.event("athenad exited", exitcode=proc.exitcode)
