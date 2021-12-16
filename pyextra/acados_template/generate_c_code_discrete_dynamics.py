a70213091e59f52694680694b28593cf732b15f1
Willem Melching <willem.melching@gmail.com>
2021-12-16 13:09:45 +0100

    revert pyextra changes

--- pyextra/acados_template/generate_c_code_discrete_dynamics.py
+++ pyextra/acados_template/generate_c_code_discrete_dynamics.py
@@ -57,7 +57,7 @@ def generate_c_code_discrete_dynamics( model, opts ):
     elif isinstance(phi, casadi.SX):
         symbol = SX.sym
     else:
-        Exception(f"generate_c_code_disc_dyn: disc_dyn_expr must be a CasADi expression, you have type: {type(phi)}")
+        Exception("generate_c_code_disc_dyn: disc_dyn_expr must be a CasADi expression, you have type: {}".format(type(phi)))
 
     # assume nx1 = nx !!!
     lam = symbol('lam', nx, 1)
