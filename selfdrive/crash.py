0056f5ac1578ae2d1fabc3ba2488ff8d077eedfb
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-12-16 15:04:52 -0800

    set env

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -1,6 +1,6 @@
 """Install exception handler for process crash."""
 from selfdrive.swaglog import cloudlog
-from selfdrive.version import get_version
+from selfdrive.version import get_version, is_comma_remote, is_tested_branch
 
 import sentry_sdk
 from sentry_sdk.integrations.threading import ThreadingIntegration
@@ -22,6 +22,16 @@ def bind_extra(**kwargs) -> None:
     sentry_sdk.set_tag(k, v)
 
 def init() -> None:
+  env = "production"
+  if not is_comma_remote():
+    env = "fork"
+  elif not is_tested_branch():
+    env = "master"
+
   sentry_sdk.init("https://a8dc76b5bfb34908a601d67e2aa8bcf9@o33823.ingest.sentry.io/77924",
-                  default_integrations=False, integrations=[ThreadingIntegration(propagate_hub=True)],
-                  release=get_version())
+                  default_integrations=False,
+                  release=get_version(),
+                  integrations=[ThreadingIntegration(propagate_hub=True)],
+                  environment=env
+                  )
+
