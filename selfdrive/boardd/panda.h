4061f50da3dabff7a16e2a845d66efe00f9d7978
Dean Lee <deanlee3@gmail.com>
2021-12-17 08:53:48 +0800

    boardd: new class USBDevice (#23015)
    * USBDevice
    
    * merge master
    
    * rebase master
    
    * add to fiels_common
    
    * inline functions
    
    * namespace
    
    * init err
    
    * remove nested if
    
    * cleanup
    
    * bump panda for branch
    
    * change includes
    
    * bump panda branch
    
    * change name
    
    * bump master panda
    
    Co-authored-by: Igor Biletskyy <briskspirit@users.noreply.github.com>
    Co-authored-by: Igor Biletskyy <bs@privacy.im>

--- selfdrive/boardd/panda.h
+++ selfdrive/boardd/panda.h
@@ -1,26 +1,15 @@
 #pragma once
 
-#include <atomic>
-#include <cstdint>
-#include <ctime>
 #include <functional>
-#include <list>
-#include <mutex>
 #include <optional>
 #include <vector>
 
-#include <libusb-1.0/libusb.h>
-
-#include "cereal/gen/cpp/car.capnp.h"
 #include "cereal/gen/cpp/log.capnp.h"
+#include "selfdrive/boardd/usbdevice.h"
 
-#define TIMEOUT 0
-#define PANDA_BUS_CNT 4
-#define RECV_SIZE (0x4000U)
-#define USB_TX_SOFT_LIMIT   (0x100U)
-#define USBPACKET_MAX_SIZE  (0x40)
-#define CANPACKET_HEAD_SIZE 5U
-#define CANPACKET_MAX_SIZE  72U
+#define RECV_SIZE (MAX_EP1_CHUNK_PER_BULK_TRANSFER + 128U)
+#define USB_TX_SOFT_LIMIT   (USBPACKET_MAX_SIZE * 4)
+#define CANPACKET_MAX_SIZE  (CANPACKET_HEAD_SIZE + CANPACKET_DATA_SIZE_MAX)
 #define CANPACKET_REJECTED  (0xC0U)
 #define CANPACKET_RETURNED  (0x80U)
 
@@ -64,34 +53,14 @@ struct can_frame {
 	long src;
 };
 
-class Panda {
- private:
-  libusb_context *ctx = NULL;
-  libusb_device_handle *dev_handle = NULL;
-  std::mutex usb_lock;
-  std::vector<uint8_t> recv_buf;
-  void handle_usb_issue(int err, const char func[]);
-  void cleanup();
-
- public:
+class Panda : public USBDevice {
+public:
   Panda(std::string serial="", uint32_t bus_offset=0);
   ~Panda();
 
-  std::string usb_serial;
-  std::atomic<bool> connected = true;
-  std::atomic<bool> comms_healthy = true;
   cereal::PandaState::PandaType hw_type = cereal::PandaState::PandaType::UNKNOWN;
   bool has_rtc = false;
   const uint32_t bus_offset;
-
-  // Static functions
-  static std::vector<std::string> list();
-
-  // HW communication
-  int usb_write(uint8_t bRequest, uint16_t wValue, uint16_t wIndex, unsigned int timeout=TIMEOUT);
-  int usb_read(uint8_t bRequest, uint16_t wValue, uint16_t wIndex, unsigned char *data, uint16_t wLength, unsigned int timeout=TIMEOUT);
-  int usb_bulk_write(unsigned char endpoint, unsigned char* data, int length, unsigned int timeout=TIMEOUT);
-  int usb_bulk_read(unsigned char endpoint, unsigned char* data, int length, unsigned int timeout=TIMEOUT);
 
   // Panda functionality
   cereal::PandaState::PandaType get_hw_type();
@@ -120,4 +89,7 @@ protected:
   void pack_can_buffer(const capnp::List<cereal::CanData>::Reader &can_data_list,
                          std::function<void(uint8_t *, size_t)> write_func);
   bool unpack_can_buffer(uint8_t *data, int size, std::vector<can_frame> &out_vec);
+
+private:
+  std::vector<uint8_t> recv_buf;
 };
