3a14dc0481d79fd3e4bb4185fa3cee1a37a86c44
Andrew Tec <andrew.tec@pm.me>
2021-12-17 12:11:13 -0800

    update conf: * add logo and favicon from connect.comma.ai * add version

--- docs/conf.py
+++ docs/conf.py
@@ -17,15 +17,17 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-
+OPENPILOT_ROOT = os.path.abspath('../../')      # from openpilot/build/docs
+VERSION = open(os.path.join(OPENPILOT_ROOT, 'selfdrive/common/version.h')).read().split()[2].strip('"')
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'openpilot'
+project = 'openpilot docs'
 copyright = '2021, comma.ai'
 author = 'comma.ai'
-
+version = VERSION
+release = VERSION
 
 # -- General configuration ---------------------------------------------------
 
@@ -54,8 +56,18 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_show_copyright = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = '_static/logo.png'
+html_favicon = '_static/favicon.ico'
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'vcs_pageview_mode': 'blob',
+    'style_nav_header_background': '#000000',
+}
