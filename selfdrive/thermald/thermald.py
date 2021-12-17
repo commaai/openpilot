78667a6e049f5b0eb9ad4662f478a809dbf8471f
shonggie <65376508+shonggie@users.noreply.github.com>
2021-12-17 23:40:23 +1100

    Line 311
    change year 2020 >1020

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -325,7 +325,7 @@ def thermald_thread():
 
     # Ensure date/time are valid
     now = datetime.datetime.utcnow()
-    startup_conditions["time_valid"] = (now.year > 2020) or (now.year == 2020 and now.month >= 10)
+    startup_conditions["time_valid"] = (now.year > 1020) or (now.year == 1020 and now.month >= 10)
     set_offroad_alert_if_changed("Offroad_InvalidTime", (not startup_conditions["time_valid"]))
 
     startup_conditions["up_to_date"] = params.get("Offroad_ConnectivityNeeded") is None or params.get_bool("DisableUpdates") or params.get_bool("SnoozeUpdate")
