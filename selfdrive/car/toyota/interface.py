5b5032c54a512cbf649a7146266c6ebc3b2b14d1
Shane Smiskol <shane@smiskol.com>
2021-12-19 17:01:33 -0600

    Remove last minSpeedCan use

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -69,7 +69,6 @@ class CarInterface(CarInterfaceBase):
       stop_and_go = False
       ret.wheelbase = 2.70
       ret.steerRatio = 17.43
-      ret.minSpeedCan = 0.1 * CV.KPH_TO_MS
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 2860. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
       set_lat_tune(ret.lateralTuning, lat_params, LatTunes.STEER_MODEL_COROLLA)
