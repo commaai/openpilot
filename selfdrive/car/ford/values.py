5a984860d5993034cbb1d52e8310b36c4c945fb7
Cameron Clough <cameronjclough@gmail.com>
2021-12-22 14:07:33 +0000

    Ford: Add FW for 2018 Ford Focus

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -7,8 +7,31 @@ Ecu = car.CarParams.Ecu
 MAX_ANGLE = 87.  # make sure we never command the extremes (0xfff) which cause latching fault
 
 class CAR:
+  FOCUS_MK4 = "FORD FOCUS 4TH GEN"
   FUSION = "FORD FUSION 2018"
 
+
+FW_VERSIONS = {
+  CAR.FOCUS_MK4: {
+    (Ecu.eps, 0x730, None): [
+      b'JX6C-14D003-AH\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+    ],
+    (Ecu.esp, 0x760, None): [
+      b'JX61-2D053-CJ\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+    ],
+    (Ecu.fwdRadar, 0x764, None): [
+      b'JX7T-14D049-AC\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+    ],
+    (Ecu.fwdCamera, 0x706, None): [
+      b'JX7T-14F397-AH\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+    ],
+    (Ecu.engine, 0x7e0, None): [
+      b'JX6A-14C204-BPL\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+    ],
+  },
+}
+
 DBC = {
+  CAR.FOCUS_MK4: dbc_dict('ford_lincoln_base_pt', None),
   CAR.FUSION: dbc_dict('ford_fusion_2018_pt', 'ford_fusion_2018_adas'),
 }
