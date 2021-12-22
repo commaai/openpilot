4c07f202d2af08c8e2800874bd47e9d2eb90ee67
alfhern <hz.alfonso@gmail.com>
2021-12-22 15:27:12 +0100

    painter opacity should be reset in OnroadHud::drawIcon to prevent wrong opacity in future painter operations

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -258,6 +258,7 @@ void OnroadHud::drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, flo
   p.drawEllipse(x - radius / 2, y - radius / 2, radius, radius);
   p.setOpacity(opacity);
   p.drawPixmap(x - img_size / 2, y - img_size / 2, img);
+  p.setOpacity(1.0);
 }
 
 // NvgWindow
