d42d688dc1928f8d3fcdc086df1aec840068442b
mitchellgoffpc <mitchellgoffpc@gmail.com>
2021-12-22 16:00:47 -0800

    Updated thneed/compile.cc

--- selfdrive/modeld/thneed/compile.cc
+++ selfdrive/modeld/thneed/compile.cc
@@ -21,11 +21,12 @@ int main(int argc, char* argv[]) {
   mdl.addRecurrent(state, TEMPORAL_SIZE);
   mdl.addDesire(desire, DESIRE_LEN);
   mdl.addTrafficConvention(traffic_convention, TRAFFIC_CONVENTION_LEN);
+  mdl.addImage(input, 0);
 
   // first run
   printf("************** execute 1 **************\n");
   memset(output, 0, OUTPUT_SIZE * sizeof(float));
-  mdl.execute(input, 0);
+  mdl.execute();
 
   // save model
   bool save_binaries = (argc > 3) && (strcmp(argv[3], "--binary") == 0);
