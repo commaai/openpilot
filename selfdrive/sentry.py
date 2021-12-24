5082d228be0f3c8d96f4c1e785e85dc9618289de
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-12-24 11:34:34 -0800

    do init in same place

---
+++ selfdrive/sentry.py
@@ -0,0 +1,70 @@
+"""Install exception handler for process crash."""
+from common.params import Params
+from selfdrive.hardware import HARDWARE
+from selfdrive.swaglog import cloudlog
+from selfdrive.version import get_branch, get_commit, get_origin, get_version, \
+                              is_comma_remote, is_dirty, is_tested_branch
+
+import sentry_sdk
+from sentry_sdk.integrations.threading import ThreadingIntegration
+
+
+class SentryProject:
+  # python project
+  SELFDRIVE = "https://a8dc76b5bfb34908a601d67e2aa8bcf9@o33823.ingest.sentry.io/77924"
+
+  # native project
+  SELFDRIVE_NATIVE = "https://a40f22e13cbc4261873333c125fc9d38@o33823.ingest.sentry.io/157615"
+
+
+def report_tombstone(fn: str, message: str, contents: str) -> None:
+  cloudlog.error({'tombstone': message})
+
+  with sentry_sdk.configure_scope() as scope:
+    scope.set_extra("tombstone_fn", fn)
+    scope.set_extra("tombstone", contents)
+    sentry_sdk.capture_message(message=message)
+    sentry_sdk.flush()
+
+
+def capture_exception(*args, **kwargs) -> None:
+  cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
+
+  try:
+    sentry_sdk.capture_exception(*args, **kwargs)
+    sentry_sdk.flush()  # https://github.com/getsentry/sentry-python/issues/291
+  except Exception:
+    cloudlog.exception("sentry exception")
+
+
+def init(project: SentryProject) -> None:
+  # forks like to mess with this
+  comma_remote = is_comma_remote() and "commaai" in get_origin(default="")
+
+  env = "production"
+  if not comma_remote:
+    env = "fork"
+  elif not is_tested_branch():
+    env = "master"
+
+  dongle_id = Params().get("DongleId", encoding='utf-8')
+
+  integrations = []
+  if project == SentryProject.SELFDRIVE:
+    integrations = [ThreadingIntegration(propagate_hub=True)]
+  else:
+    sentry_sdk.utils.MAX_STRING_LENGTH = 8192
+
+  sentry_sdk.init(project,
+                  default_integrations=False,
+                  release=get_version(),
+                  integrations=integrations,
+                  traces_sample_rate=1.0,
+                  environment=env)
+
+  sentry_sdk.set_user({"id": dongle_id})
+  sentry_sdk.set_tag("dirty", is_dirty())
+  sentry_sdk.set_tag("origin", get_origin())
+  sentry_sdk.set_tag("branch", get_branch())
+  sentry_sdk.set_tag("commit", get_commit())
+  sentry_sdk.set_tag("device", HARDWARE.get_device_type())
