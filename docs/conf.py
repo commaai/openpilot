94f0ec19d04522d22b2f55a9bcf0740894a5ada0
Andrew <andrew.tec@pm.me>
2021-12-29 08:31:56 -0800

    add version to docs.comma.ai (#23260)
    * update conf:
    * add logo and favicon from connect.comma.ai
    * add version
    
    * proper version extract
    
    * v
    
    * Update docs/conf.py
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: c86ca387c2fd98366057a146735ef2b388f4d070

--- docs/conf.py
+++ docs/conf.py
@@ -15,16 +15,19 @@
 #
 import os
 import sys
+from selfdrive.version import get_version 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-
+VERSION = get_version()
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'openpilot'
+project = 'openpilot docs'
 copyright = '2021, comma.ai'
 author = 'comma.ai'
+version = VERSION
+release = VERSION
 language = 'en'
 
 
@@ -81,9 +84,18 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_show_copyright = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = '_static/logo.png'
+html_favicon = '_static/favicon.ico'
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'vcs_pageview_mode': 'blob',
+    'style_nav_header_background': '#000000',
+}
 html_extra_path = ['_static']
