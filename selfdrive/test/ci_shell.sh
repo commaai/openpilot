c40032a622448e2816e4c97bf72521b3a5790071
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-01-02 13:30:40 -0800

    revert that

--- selfdrive/test/ci_shell.sh
+++ selfdrive/test/ci_shell.sh
@@ -15,5 +15,5 @@ docker run \
        --volume $OP_ROOT:$OP_ROOT \
        --workdir $PWD \
        --env PYTHONPATH=$OP_ROOT \
-       openpilot-base \
+       ghcr.io/commaai/openpilot-base:latest \
        /bin/bash
