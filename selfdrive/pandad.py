e11d43393f80d808cd9477a8737011b00b1085a5
Brian Wisniewski <56554110+BrianWisniewski@users.noreply.github.com>
2022-01-03 14:00:10 -0500

    Remove unused typing import

--- selfdrive/pandad.py
+++ selfdrive/pandad.py
@@ -4,7 +4,7 @@ import os
 import usb1
 import time
 import subprocess
-from typing import List, NoReturn
+from typing import NoReturn
 from functools import cmp_to_key
 
 from panda import DEFAULT_FW_FN, DEFAULT_H7_FW_FN, MCU_TYPE_H7, Panda, PandaDFU
