348c429228839aaeb736603c317d0e3e6aaaef29
Shane Smiskol <shane@smiskol.com>
2022-01-03 18:40:59 -0700

    Add Avalon to CARS.md

--- selfdrive/test/test_routes.py
+++ selfdrive/test/test_routes.py
@@ -21,6 +21,7 @@ non_tested_cars = [
   HYUNDAI.ELANTRA_GT_I30,
   HYUNDAI.GENESIS_G90,
   HYUNDAI.KIA_OPTIMA_H,
+  TOYOTA.AVALON_TSS2,
 ]
 
 TestRoute = namedtuple('TestRoute', ['route', 'car_fingerprint'])
