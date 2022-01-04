0357cf2af0bc7449e7aefff5b0f0881f1ae2adb3
Dean Lee <deanlee3@gmail.com>
2022-01-05 06:07:36 +0800

    controlsd: remove duplicate event relayMalfunction (#23392)

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -294,9 +294,6 @@ class Controls:
       self.events.add(EventName.posenetInvalid)
     if not self.sm['liveLocationKalman'].deviceStable:
       self.events.add(EventName.deviceFalling)
-    for pandaState in self.sm['pandaStates']:
-      if log.PandaState.FaultType.relayMalfunction in pandaState.faults:
-        self.events.add(EventName.relayMalfunction)
 
     if not REPLAY:
       # Check for mismatch between openpilot and car's PCM
