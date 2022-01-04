b500034b8d1cd0f5fd1ba4f55d77c9fa95820511
Shane Smiskol <shane@smiskol.com>
2022-01-04 08:00:29 -0700

    LongitudinalMpc: Remove class accel_limit_arr (#23295)
    * remove class accel_limit_arr
    
    remove class accel_limit_arr
    
    * make it static
    
    * only set static parameters once
    
    * just use existing function
    
    style
    
    * Update selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
    
    * Update selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: 9cb0ebe37d234b292e5f2a482924a6f23ab77ba9

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -194,9 +194,6 @@ class LongitudinalMpc:
   def __init__(self, e2e=False):
     self.e2e = e2e
     self.reset()
-    self.accel_limit_arr = np.zeros((N+1, 2))
-    self.accel_limit_arr[:,0] = -1.2
-    self.accel_limit_arr[:,1] = 1.2
     self.source = SOURCES[2]
 
   def reset(self):
@@ -224,6 +221,9 @@ class LongitudinalMpc:
   def set_weights(self):
     if self.e2e:
       self.set_weights_for_xva_policy()
+      self.params[:,0] = -10.
+      self.params[:,1] = 10.
+      self.params[:,2] = 1e5
     else:
       self.set_weights_for_lead_policy()
 
@@ -348,14 +348,8 @@ class LongitudinalMpc:
     for i in range(N):
       self.solver.cost_set(i, "yref", self.yref[i])
     self.solver.cost_set(N, "yref", self.yref[N][:COST_E_DIM])
-    self.accel_limit_arr[:,0] = -10.
-    self.accel_limit_arr[:,1] = 10.
-    x_obstacle = 1e5*np.ones(N+1)
-    self.params = np.concatenate([self.accel_limit_arr,
-                                  x_obstacle[:, None],
-                                  self.prev_a[:,None]], axis=1)
+    self.params[:,3] = np.copy(self.prev_a)
     self.run()
-
 
   def run(self):
     for i in range(N+1):
