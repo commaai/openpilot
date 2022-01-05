cf6310e4cbee128e11a187b9a91443d592483ba1
Andrew Tec <andrest@trabus.com>
2022-01-04 23:36:31 -0800

    * use macos-latest * move rcc path to scons * add extra paths for odd homebrew installs * acados source change

--- tools/openpilot_env.sh
+++ tools/openpilot_env.sh
@@ -6,15 +6,6 @@ if [ -z "$OPENPILOT_ENV" ]; then
   export PYTHONPATH="$OP_ROOT:$PYTHONPATH"
   export PATH="$HOME/.pyenv/bin:$PATH"
 
-  # for qt resource compiler "rcc"
-  if [[ "$HOST" == "Darwin" ]]; then
-    if [[ "$ARCH" == "arm64" ]]; then
-      export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"
-    else # x86_64 intel
-      export PATH="/opt/local/opt/qt@5/bin:$PATH"
-    fi
-  fi
-
   # Pyenv suggests we place the below two lines in .profile before we source
   # .bashrc, but there is no simple way to guarantee we do this correctly
   # programmatically across heterogeneous systems. For end-user convenience,
