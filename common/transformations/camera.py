557882ddf30758236e42ec6f09d3f09e3a8afa80
Harald Schafer <harald.the.engineer@gmail.com>
2022-01-06 18:23:40 -0800

    Camera transforms: denormalize with no bounds at all
    old-commit-hash: dbef0a1cd4b474753b157e38a60f7b43cd9578ae

--- common/transformations/camera.py
+++ common/transformations/camera.py
@@ -133,10 +133,12 @@ def denormalize(img_pts, intrinsics=fcam_intrinsics, width=np.inf, height=np.inf
   img_pts = np.atleast_2d(img_pts)
   img_pts = np.hstack((img_pts, np.ones((img_pts.shape[0], 1), dtype=img_pts.dtype)))
   img_pts_denormalized = img_pts.dot(intrinsics.T)
-  img_pts_denormalized[img_pts_denormalized[:, 0] > width] = np.nan
-  img_pts_denormalized[img_pts_denormalized[:, 0] < 0] = np.nan
-  img_pts_denormalized[img_pts_denormalized[:, 1] > height] = np.nan
-  img_pts_denormalized[img_pts_denormalized[:, 1] < 0] = np.nan
+  if np.isfinite(width):
+    img_pts_denormalized[img_pts_denormalized[:, 0] > width] = np.nan
+    img_pts_denormalized[img_pts_denormalized[:, 0] < 0] = np.nan
+  if np.isfinite(height):
+    img_pts_denormalized[img_pts_denormalized[:, 1] > height] = np.nan
+    img_pts_denormalized[img_pts_denormalized[:, 1] < 0] = np.nan
   return img_pts_denormalized[:, :2].reshape(input_shape)
 
 
