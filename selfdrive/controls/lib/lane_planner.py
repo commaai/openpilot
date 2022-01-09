e6f9470411faa8ce387bf3f7fad5c9384ae2b4d7
Shane Smiskol <shane@smiskol.com>
2022-01-08 22:57:22 -0700

    fix

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -10,7 +10,7 @@ from selfdrive.swaglog import cloudlog
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 # model path is in the frame of EON's camera. TICI is 0.1 m away,
-# however the average measured path offset difference is -0.04 m
+# however the average measured path difference is 0.04 m
 if EON:
   CAMERA_OFFSET = -0.06
   PATH_OFFSET = 0.0
