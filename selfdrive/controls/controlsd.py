b23c1b24340491d6cd99ee128002dee9e6a19d0a
Robbe Derks <robbe.derks@gmail.com>
2022-01-10 19:29:49 +0100

    ignore statsd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -38,7 +38,8 @@ REPLAY = "REPLAY" in os.environ
 SIMULATION = "SIMULATION" in os.environ
 NOSENSOR = "NOSENSOR" in os.environ
 IGNORE_PROCESSES = {"rtshield", "uploader", "deleter", "loggerd", "logmessaged", "tombstoned",
-                    "logcatd", "proclogd", "clocksd", "updated", "timezoned", "manage_athenad"} | \
+                    "logcatd", "proclogd", "clocksd", "updated", "timezoned", "manage_athenad",
+                    "statsd"} | \
                     {k for k, v in managed_processes.items() if not v.enabled}
 
 ACTUATOR_FIELDS = set(car.CarControl.Actuators.schema.fields.keys())
