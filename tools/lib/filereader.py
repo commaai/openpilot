20670f0740ac49616a232f9c10b9d8899c1bdc35
Greg Hogan <gregjhogan@gmail.com>
2022-01-13 18:21:19 -0800

    filereader better name for cd:/ replacement
    old-commit-hash: a6b46c311e41875612e6d28f6978f4e1d1e20e46

--- tools/lib/filereader.py
+++ tools/lib/filereader.py
@@ -1,11 +1,11 @@
 import os
 from tools.lib.url_file import URLFile
 
-DATA_PREFIX = os.getenv("DATA_PREFIX", "http://data-raw.internal/")
+DATA_ENDPOINT = os.getenv("DATA_ENDPOINT", "http://data-raw.internal/")
 
 def FileReader(fn, debug=False):
   if fn.startswith("cd:/"):
-    fn = fn.replace("cd:/", DATA_PREFIX)
+    fn = fn.replace("cd:/", DATA_ENDPOINT)
   if fn.startswith("http://") or fn.startswith("https://"):
     return URLFile(fn, debug=debug)
   return open(fn, "rb")
