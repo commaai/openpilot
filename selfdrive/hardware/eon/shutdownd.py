c9aa56a720f393c54eb4711c057f6d64da609e8b
Comma Device <device@comma.ai>
2022-01-13 21:56:38 -0800

    Log android system shutdown to param

---
+++ selfdrive/hardware/eon/shutdownd.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import time
+import datetime
+
+from common.params import Params
+from selfdrive.hardware.eon.hardware import getprop
+from selfdrive.swaglog import cloudlog
+
+def main():
+  params = Params()
+  while True:
+    prop = getprop("sys.shutdown.requested")
+    if prop is not None and len(prop) > 0:
+      os.system("pkill -9 loggerd")
+      params.put("LastSystemShutdown", f"'{prop}' {datetime.datetime.now()}")
+      print("shutdown detected", repr(prop))
+
+      time.sleep(120)
+      cloudlog.error('shutdown false positive')
+      break
+
+    time.sleep(0.1)
+
+if __name__ == "__main__":
+  main()
