d8f55a81abf26a7edb0bc33546e0e02f27401b92
Dean Lee <deanlee3@gmail.com>
2022-01-14 11:20:22 +0800

    UI: reset interactivity timeout on PC (#23508)
    * reset interactive timeout on pc
    
    * cleanup
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 6dbae9593eae4d2bc55aa34fdde428fbd2933ccf

--- selfdrive/ui/qt/window.cc
+++ selfdrive/ui/qt/window.cc
@@ -80,19 +80,20 @@ void MainWindow::closeSettings() {
 }
 
 bool MainWindow::eventFilter(QObject *obj, QEvent *event) {
-  if (event->type() == QEvent::MouseButtonPress || event->type() == QEvent::TouchBegin) {
-    device.resetInteractiveTimout();
-  }
+  const QSet<QEvent::Type> evts({QEvent::MouseButtonPress, QEvent::MouseMove,
+                                 QEvent::TouchBegin, QEvent::TouchUpdate, QEvent::TouchEnd});
 
+  if (evts.contains(event->type())) {
+    device.resetInteractiveTimout();
 #ifdef QCOM
-  // filter out touches while in android activity
-  const static QSet<QEvent::Type> filter_events({QEvent::MouseButtonPress, QEvent::MouseMove, QEvent::TouchBegin, QEvent::TouchUpdate, QEvent::TouchEnd});
-  if (HardwareEon::launched_activity && filter_events.contains(event->type())) {
-    HardwareEon::check_activity();
+    // filter out touches while in android activity
     if (HardwareEon::launched_activity) {
-      return true;
+      HardwareEon::check_activity();
+      if (HardwareEon::launched_activity) {
+        return true;
+      }
     }
-  }
 #endif
+  }
   return false;
 }
