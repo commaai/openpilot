02f08b4ee898c7f5df29ca33e601dd4e7712c316
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2022-01-19 23:31:47 +1100

    remove sienna dbc
    same as avalon

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -1663,7 +1663,7 @@ DBC = {
   CAR.LEXUS_ES_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.LEXUS_ESH_TSS2: dbc_dict('toyota_nodsu_hybrid_pt_generated', 'toyota_tss2_adas'),
   CAR.LEXUS_ESH: dbc_dict('lexus_ct200h_2018_pt_generated', 'toyota_adas'),
-  CAR.SIENNA: dbc_dict('toyota_sienna_xle_2018_pt_generated', 'toyota_adas'),
+  CAR.SIENNA: dbc_dict('toyota_avalon_2017_pt_generated', 'toyota_adas'),
   CAR.LEXUS_IS: dbc_dict('lexus_is_2018_pt_generated', 'toyota_adas'),
   CAR.LEXUS_CTH: dbc_dict('lexus_ct200h_2018_pt_generated', 'toyota_adas'),
   CAR.RAV4H_TSS2: dbc_dict('toyota_nodsu_hybrid_pt_generated', 'toyota_tss2_adas'),
