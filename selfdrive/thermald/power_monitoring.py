ff05e090a46253ae4afbdeceef840db94bb50af2
Jason Shuler <jshuler@gmail.com>
2022-01-19 15:14:39 -0500

    Change power off to 2 days for debugging

--- selfdrive/thermald/power_monitoring.py
+++ selfdrive/thermald/power_monitoring.py
@@ -20,8 +20,12 @@ CAR_CHARGING_RATE_W = 45
 
 VBATT_PAUSE_CHARGING = 11.0           # Lower limit on the LPF car battery voltage
 VBATT_INSTANT_PAUSE_CHARGING = 7.0    # Lower limit on the instant car battery voltage measurements to avoid triggering on instant power loss
-MAX_TIME_OFFROAD_S = 30*3600
-MIN_ON_TIME_S = 3600
+# TODO: JJS: Revert
+# To ease remote debugging, stay on for 2 days
+MAX_TIME_OFFROAD_S = 48*3600
+# TODO: JJS: Revert
+# To ease remote debugging, stay on for 2 days
+MIN_ON_TIME_S = 48*3600
 
 class PowerMonitoring:
   def __init__(self):
