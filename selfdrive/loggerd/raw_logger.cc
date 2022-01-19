22d43211d9bc828ea4e6bd6b84d865a597825eda
Dean Lee <deanlee3@gmail.com>
2022-01-20 05:55:02 +0800

    loggerd: make test_loggerd.py run on PC (#23574)
    * run on PC
    
    * set frameId
    
    * remove todo
    
    * rawlogger: remove suffix mkv
    
    * remove unused import PC
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/loggerd/raw_logger.cc
+++ selfdrive/loggerd/raw_logger.cc
@@ -64,7 +64,7 @@ RawLogger::~RawLogger() {
 }
 
 void RawLogger::encoder_open(const char* path) {
-  vid_path = util::string_format("%s/%s.mkv", path, filename);
+  vid_path = util::string_format("%s/%s", path, filename);
 
   // create camera lock file
   lock_path = util::string_format("%s/%s.lock", path, filename);
