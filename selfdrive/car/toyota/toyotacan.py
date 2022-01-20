7b12bfbd9d06e26e5fb56f2b776e8a8a35a67261
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2022-01-20 12:05:24 +1100

    Toyota: LKAS_HUD expansion (#23573)
    * bump opendbc
    
    * add lkas_hud messages
    
    * Remove duplicate
    
    * bump opendbc
    
    * fix comment
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: f85a75cf542bbb05b929b959cb566d3f941410ea

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -69,17 +69,32 @@ def create_fcw_command(packer, fcw):
 
 def create_ui_command(packer, steer, chime, left_line, right_line, left_lane_depart, right_lane_depart, enabled):
   values = {
+    "TWO_BEEPS": chime,
+    "LDA_ALERT": steer,
     "RIGHT_LINE": 3 if right_lane_depart else 1 if right_line else 2,
     "LEFT_LINE": 3 if left_lane_depart else 1 if left_line else 2,
     "BARRIERS" : 1 if enabled else 0,
-    "SET_ME_X0C": 0x0c,
-    "SET_ME_X2C": 0x2c,
-    "SET_ME_X38": 0x38,
-    "SET_ME_X02": 0x02,
+
+    # static signals
+    "SET_ME_X02": 2,
     "SET_ME_X01": 1,
     "LKAS_STATUS": 1,
     "REPEATED_BEEPS": 0,
-    "TWO_BEEPS": chime,
-    "LDA_ALERT": steer,
+    "LANE_SWAY_FLD": 7,
+    "LANE_SWAY_BUZZER": 0,
+    "LANE_SWAY_WARNING": 0,
+    "LDA_FRONT_CAMERA_BLOCKED": 0,
+    "TAKE_CONTROL": 0,
+    "LANE_SWAY_SENSITIVITY": 2,
+    "LANE_SWAY_TOGGLE": 1,
+    "LDA_ON_MESSAGE": 0,
+    "LDA_SPEED_TOO_LOW": 0,
+    "LDA_SA_TOGGLE": 1,
+    "LDA_SENSITIVITY": 2,
+    "LDA_UNAVAILABLE": 0,
+    "LDA_MALFUNCTION": 0,
+    "LDA_UNAVAILABLE_QUIET": 0,
+    "ADJUSTING_CAMERA": 0,
+    "LDW_EXIST": 1,
   }
   return packer.make_can_msg("LKAS_HUD", 0, values)
