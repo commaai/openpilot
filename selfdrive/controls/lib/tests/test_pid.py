b50c069570d230141e1b217e3c8c9a01bbdd8cd2
ClockeNessMnstr <locke.dftc@gmail.com>
2022-01-22 14:56:30 -0500

    noise test

--- selfdrive/controls/lib/tests/test_pid.py
+++ selfdrive/controls/lib/tests/test_pid.py
@@ -10,7 +10,7 @@ from selfdrive.controls.lib.pid import PIController
 gain_range = 10 # (1 / range -> 1 ->  range   )
 ratio = 4 #   I = (P / ratio -> P -> P * ratio)
 
-num_divs = 8# (2*n + 1)**2 graphs
+num_divs = 4# (2*n + 1)**2 graphs
 _p = [[0, num_divs, 2*num_divs],[1/gain_range, 1, gain_range]]
 
 
@@ -19,10 +19,10 @@ step_size = 0.5 # percent of output to request during tests.
 rate = 100 # number of steps per second
 ttm = 1 # seconds to max rate actuator
 rate_limit = limit / (ttm * rate) # max change in request per cycle
-sec = 15 # duration of data in sec
+sec = 60 # duration of data in sec
 start_offset = 0.1 #sec
 override = 5 # seconds
-noise_intensity = 2 # noise = rand() * (noise_intensity * rate_limit) / kp
+noise_intensity = 10 # noise = rand() * (noise_intensity * rate_limit) / kp
 
 # 1.0 is perfect FF
 
@@ -40,11 +40,11 @@ if __name__ == "__main__":
       debug_ramp = True
     if "plot" in sys.argv:
       plot_debug = True
-    if "debug_only" in sys.argv:
+    if "debug" in sys.argv:
       plot_all = False
     else:
       plot_all = True
-    if (plot_debug and not (debug_noise or debug_override or debug_ramp)) or "debug" in sys.argv:
+    if plot_debug and not (debug_noise or debug_override or debug_ramp):
       debug_noise = True
       debug_override = True
       debug_ramp = True
@@ -63,10 +63,6 @@ def ratelimit(new, last):
 
 class TestPID(unittest.TestCase):
   def test_error_noise(self):
-    #TODO New Tests
-    #TODO check that SS error is still 0
-    #TODO check that the saturated ramp up condition(s?) are not degraded. 
-
     self.assertFalse((debug_ramp or debug_override) and not debug_noise, msg="Test skipped for debugging")
     self.assertFalse(noise_intensity < 1, msg="Noise not disastrous enough")
     for i in range(0, 2*num_divs + 1):
@@ -76,44 +72,44 @@ class TestPID(unittest.TestCase):
         _i = [[0, num_divs, 2*num_divs], [kp/ratio, kp, ratio*kp]]
         ki = interp(j, _i[0], _i[1])
 
-        pid_n = PIController(([0, 1], [kp, kp]), ([0, 1], [ki, ki]), k_f=1, rate=rate, pos_limit=limit, neg_limit=-limit)
-        pid_r = PIController(([0, 1], [kp, kp]), ([0, 1], [ki, ki]), k_f=1, rate=rate, pos_limit=noise_intensity*limit, neg_limit=-noise_intensity*limit)
+        pid_n = PIController(([0, 1], [kp, kp]), ([0, 1], [ki, ki]), k_f=0, rate=rate, pos_limit=limit, neg_limit=-limit)
+        pid_c = PIController(([0, 1], [kp, kp]), ([0, 1], [ki, ki]), k_f=0, rate=rate, pos_limit=limit, neg_limit=-limit)
+        pid_r = PIController(([0, 1], [kp, kp]), ([0, 1], [ki, ki]), k_f=0, rate=rate, pos_limit=limit, neg_limit=-limit)
         output_n = FirstOrderFilter(0, 1, 1/rate)
+        output_c = FirstOrderFilter(0, 1, 1/rate)
         output_r = FirstOrderFilter(0, 1, 1/rate)
+        ss_error = 0
         pid_n_control = 0
+        pid_c_control = 0
         pid_r_control = 0
         last_pid_r_control = 0
-        if plot_debug and debug_noise:
-          x = []
-          y4 = []
-          y5 = []
-        for t in range(0, int(sec*rate)):
-          if plot_debug and debug_noise:
-            x.append(t / rate)
-            y4.append(output_n.x)
-            y5.append(output_r.x)
-
+        for t in range(0, int(noise_intensity*sec*rate)):
           target = 0
           if (t > start_offset*rate):
             target = step_size * limit
           
+          # threshold base on time and error_intensity. multiply runtime by intensity or divide error by intensity
           noise = 0
           if target != 0:
-            noise = noise_intensity * (rate_limit / kp) * ((random.randint(0, 200) - 100) / 100)
+            noise = noise_intensity * (rate_limit / kp) * ((random.randint(0, 200*gain_range) - 100*gain_range) / (100*gain_range))
 
           pid_n_control = ratelimit(pid_n.update(target+noise, output_n.x, last_output=pid_n_control, feedforward=target+noise), pid_n_control)
-          pid_r_control = pid_r.update(target+noise, output_r.x, last_output=pid_r_control, feedforward=target)
-          
-          output_n.update(pid_n_control)
-          output_r.update(ratelimit(pid_r_control, last_pid_r_control))
-          last_pid_r_control = pid_r_control
+          pid_c_control = ratelimit(pid_c.update(target, output_c.x, last_output=pid_c_control, feedforward=target), pid_c_control)
+          pid_r_control = pid_r.update(target+noise, output_r.x, last_output=pid_r_control, feedforward=target+noise)
 
-        if plot_debug and debug_noise and plot_all:
-          plt.plot(x, y5, 'k', label="No Noise")
-          plt.plot(x, y4, label="Noise")
-          plt.title(f"Noise Test: P = {kp:5.3}, I = {ki:5.3}")
-          plt.legend()
-          plt.show()
+          last_pid_r_control = ratelimit(pid_r_control, last_pid_r_control)
+
+          output_n.update(pid_n_control)
+          output_c.update(pid_c_control)
+          output_r.update(last_pid_r_control)
+
+          # calculate error of controller w/ and without rate limits against no noise output with limits
+          ss_error += ((output_c.x - output_n.x) - ss_error) / (t+1)
+          ss_error += ((output_r.x - output_n.x) - ss_error) / (t+1)
+
+        # assert that moving average of SS error is falling to zero
+        # threshold base on time and error_intensity. multiply runtime by intensity or divide error by intensity
+        self.assertTrue(abs(ss_error) < limit / rate, msg=f"Possible SS_error: {ss_error} detected in controller due to noise")
   
   def test_override(self):
     #TODO New Tests
@@ -161,7 +157,7 @@ class TestPID(unittest.TestCase):
           last_pid_r_control = pid_r_control
           output_r.update(pid_r_control)
 
-                    # override / hold output of controller at 0 
+          # override / hold output of controller at 0 
           if t > override*rate:
             last_pid_n_control = pid_n_control
             output_n.update(pid_n_control)
