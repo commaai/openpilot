39f1c568815576fc8e509b0e2278c118b0f15d43
ClockeNessMnstr <locke.dftc@gmail.com>
2022-01-22 16:06:20 -0500

    override test

--- selfdrive/controls/lib/tests/test_pid.py
+++ selfdrive/controls/lib/tests/test_pid.py
@@ -22,12 +22,17 @@ rate_limit = limit / (ttm * rate) # max change in request per cycle
 sec = 60 # duration of data in sec
 start_offset = 0.1 #sec
 override = 5 # seconds
+override_allowance_percent = 1
+override_allowance = override_allowance_percent / 100
+
 noise_intensity = 10 # noise = rand() * (noise_intensity * rate_limit) / kp
 
 # 1.0 is perfect FF
 
 if __name__ == "__main__":
-  plot_debug = False
+  plot_all = False
+  plot = False
+  debug = False
   debug_noise = False
   debug_override = False
   debug_ramp = False
@@ -39,23 +44,23 @@ if __name__ == "__main__":
     if "ramp" in sys.argv:
       debug_ramp = True
     if "plot" in sys.argv:
-      plot_debug = True
+      plot = True
     if "debug" in sys.argv:
-      plot_all = False
+      debug = True
     else:
       plot_all = True
-    if plot_debug and not (debug_noise or debug_override or debug_ramp):
+    if plot and not (debug_noise or debug_override or debug_ramp):
       debug_noise = True
       debug_override = True
       debug_ramp = True
-    if (plot_debug):
+    if (plot):
       num_divs = 4 # (2*n + 1)**2 graphs (81 graphs)
       _p = [[0, num_divs, 2*num_divs],[1/gain_range, 1, gain_range]]
     #remove args so unittest is not freaked out
     while len(sys.argv) > 1:
       sys.argv.pop()
   
-if plot_debug and (debug_noise or debug_override or debug_ramp):
+if plot and (debug_noise or debug_override or debug_ramp):
   import matplotlib.pyplot as plt
 
 def ratelimit(new, last):
@@ -129,54 +134,34 @@ class TestPID(unittest.TestCase):
         pid_r = PIController(([0, 1], [kp, kp]), ([0, 1], [ki, ki]), k_f=1, rate=rate, pos_limit=limit, neg_limit=-limit)
         output_n = FirstOrderFilter(0, 1, 1/rate)
         output_r = FirstOrderFilter(0, 1, 1/rate)
-        last_pid_n_control = 0
-        last_pid_r_control = 0
-
-        if plot_debug and debug_override:
-          x = []
-          x2 = []
-          y4 = []
-          y5 = []
+        pid_n_control = 0
+        pid_r_control = 0
+        sum_overshoot_error = 0
         for t in range(0, int(sec*rate)):
-          if plot_debug and debug_override:
-            x.append(t / rate)
-            x2.append(t / rate + override - start_offset)
-            y4.append(output_n.x)
-            y5.append(output_r.x)
-
           target = 0
           if (t > start_offset*rate):
             target = step_size * limit
 
-          pid_n_control_raw = pid_n.update(target, output_n.x, last_output=last_pid_n_control)
-          pid_n_control = clip(pid_n_control_raw, last_pid_n_control - rate_limit, last_pid_n_control + rate_limit)
-
-          pid_r_control_raw = pid_r.update(target, output_r.x, last_output=last_pid_r_control)
-          pid_r_control = clip(pid_r_control_raw, last_pid_r_control - rate_limit, last_pid_r_control + rate_limit)
-          
-          last_pid_r_control = pid_r_control
-          output_r.update(pid_r_control)
+          pid_n_control = ratelimit(pid_n.update(target, output_n.x, last_output=pid_n_control), pid_n_control)
 
           # override / hold output of controller at 0 
           if t > override*rate:
-            last_pid_n_control = pid_n_control
+            pid_r_control = ratelimit(pid_r.update(target, output_r.x, last_output=pid_r_control), pid_r_control)
+            output_r.update(pid_r_control)
             output_n.update(pid_n_control)
           else: 
-            last_pid_n_control = 0
-            
+            pid_n_control = 0
 
-        if plot_debug and debug_override and plot_all:
-          plt.plot(x2, y5, 'k', label="No Override")
-          plt.plot(x, y4, label="Override")
-          plt.title(f"Override Test: P = {kp:5.3}, I = {ki:5.3}")
-          plt.legend()
-          plt.show()
+          # only sum up overshoot in excess of the allowance of both the unclamped signal and the target. 
+          if output_n.x > target:
+            sum_overshoot_error += min(max(abs(output_r.x - output_n.x)-output_r.x*(override_allowance), 0), max(abs(target - output_n.x)-target*(override_allowance), 0)) / rate
+        
+        # assert that the overshoot characteristics of a new input response and releasing the controller on an overriden input are the same
+        self.assertFalse(sum_overshoot_error/target > override_allowance, msg=f"Exessive Overshoot measured after override. Exeeded {100*override_allowance}% : {100*sum_overshoot_error/target}% ")
 
   def test_ramp_up(self):
     #TODO New Tests
-    #TODO check that output is stable?
-    #TODO check that overshoot is reduced?
-    #TODO check that integrator motion is restricted or reversed?
+    #TODO Check that sum of error is low above target. 
 
     self.assertFalse((debug_override or debug_noise) and not debug_ramp, msg="Test skipped for debugging")
     for i in range(0, 2*num_divs + 1):
@@ -193,12 +178,12 @@ class TestPID(unittest.TestCase):
         last_pid_n_control = 0
         last_pid_r_control = 0
 
-        if plot_debug and debug_ramp:
+        if plot and debug_ramp:
           x = []
           y4 = []
           y5 = []
         for t in range(0, int(sec*rate)):
-          if plot_debug and debug_ramp:
+          if plot and debug_ramp:
             x.append(t / rate)
             y4.append(output_n.x)
             y5.append(output_r.x)
@@ -216,7 +201,7 @@ class TestPID(unittest.TestCase):
           output_n.update(pid_n_control)
           output_r.update(last_pid_r_control)
 
-        if plot_debug and debug_ramp and plot_all:
+        if plot and debug_ramp and plot_all:
           plt.plot(x, y5, 'k', label="No Rate Limit")
           plt.plot(x, y4, label="Rate Limit")
           plt.title(f"Slew Rate Limit Test: P = {kp:5.3}, I = {ki:5.3}")
