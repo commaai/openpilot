70f4220b0844f634696feef9889ee62528683bd5
Greg Hogan <gregjhogan@gmail.com>
2022-01-23 01:56:58 -0800

    undo openpilot_env.sh changes

--- tools/openpilot_env.sh
+++ tools/openpilot_env.sh
@@ -1,12 +1,24 @@
+# NOTE: none of this is needed any longer except the mac specific setup
+#       but we can't clean up this file without breaking old installations
 if [ -z "$OPENPILOT_ENV" ]; then
+  export PATH="$HOME/.pyenv/bin:$PATH"
+
+  # Pyenv suggests we place the below two lines in .profile before we source
+  # .bashrc, but there is no simple way to guarantee we do this correctly
+  # programmatically across heterogeneous systems. For end-user convenience,
+  # we add the lines here as a workaround.
+  # https://github.com/pyenv/pyenv/issues/1906
+  export PYENV_ROOT="$HOME/.pyenv"
+
   if [[ "$(uname)" == 'Linux' ]]; then
-    # no longer used so clean it up
-    sed -i '/openpilot_env\.sh$/d' "${HOME}/.$(basename ${SHELL})rc"
+    eval "$(pyenv virtualenv-init -)"
   elif [[ "$(uname)" == 'Darwin' ]]; then
     # msgq doesn't work on mac
     export ZMQ=1
     export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
   fi
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
 
   export OPENPILOT_ENV=1
 fi
