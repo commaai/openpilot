70f14182562b3cf1dc202f8b0faf5464c42fa7e6
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-01-24 14:49:47 -0800

    remove community features toggle (#23214)
    * remove community features toggle
    
    * bump cereal
    
    * little more
    
    * update refs
    old-commit-hash: 3e9cb7fa12b5f686fdcf6122ef5979d01c41f0a3

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -347,7 +347,6 @@ def setup_env(simulation=False):
   params.clear_all()
   params.put_bool("OpenpilotEnabledToggle", True)
   params.put_bool("Passive", False)
-  params.put_bool("CommunityFeaturesToggle", True)
 
   os.environ["NO_RADAR_SLEEP"] = "1"
   os.environ["REPLAY"] = "1"
