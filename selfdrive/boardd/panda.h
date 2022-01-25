183715ccea553b9107047d4a2f4e3adc58fae075
Shane Smiskol <shane@smiskol.com>
2022-01-25 03:09:13 -0800

    controlsd: verify unsafe mode consistency (#23563)
    * add unsafe mode check to controlsd
    
    bump submodules
    
    forgot to add to health_t struct
    
    bump
    
    ahh forgot to set it!
    
    bump
    
    bump
    
    clean up
    
    clean up
    
    bump
    
    * fix ordering
    
    * move to end
    
    * unsigned
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: 31d147dca2ada336d14c08ae35f5af510c309730

--- selfdrive/boardd/panda.h
+++ selfdrive/boardd/panda.h
@@ -45,6 +45,7 @@ struct __attribute__((packed)) health_t {
   uint8_t fault_status;
   uint8_t power_save_enabled;
   uint8_t heartbeat_lost;
+  uint16_t unsafe_mode;
 };
 
 struct __attribute__((packed)) can_header {
