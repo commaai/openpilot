5bf22bbd4daca2e561b0258ce07a8c79e9c2617a
IamMaxlee <97835797+IamMaxlee@users.noreply.github.com>
2022-01-25 21:35:20 +0300

    update

--- selfdrive/car/faw/carcontroller.py
+++ selfdrive/car/faw/carcontroller.py
@@ -1,42 +1,17 @@
-import numpy as np
 from cereal import car
-from selfdrive.config import Conversions as CV
-from selfdrive.car.interfaces import CarStateBase
-from opendbc.can.parser import CANParser
-from opendbc.can.can_define import CANDefine
-from selfdrive.car.volkswagen.values import DBC_FILES, CANBUS, NetworkLocation, TransmissionType, GearShifter, BUTTON_STATES, CarControllerParams
+from selfdrive.car import apply_std_steer_torque_limits
+from selfdrive.car.volkswagen import volkswagencan
+from selfdrive.car.volkswagen.values import DBC_FILES, CANBUS, MQB_LDW_MESSAGES, BUTTON_STATES, CarControllerParams as P
+from opendbc.can.packer import CANPacker
 
-class CarState(CarStateBase):
-  def __init__(self, CP):
-    super().__init__(CP)
 
-  def update(self, pt_cp, cam_cp, ext_cp, trans_type):
-    ret = car.CarState.new_message()
-    ret.wheelSpeeds = self.get_wheel_speeds(0,0,0,0)
+class CarController():
+  def __init__(self, dbc_name, CP, VM):
+    self.packer_pt = CANPacker(DBC_FILES.mqb)
 
-    ret.vEgoRaw = pt_cp.vl["NEW_MGS_1"]["Car_Speed"]
-    ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
+  def update(self, enabled, CS, frame, ext_bus, actuators, visual_alert, left_lane_visible, right_lane_visible, left_lane_depart, right_lane_depart):
+    """ Controls thread """
+    can_sends = []
+    new_actuators = actuators.copy()
 
-    return ret
-
-  @staticmethod
-  def get_can_parser(CP):
-    # this function generates lists for signal, messages and initial values
-    signals = [
-      # sig_name, sig_address, default
-      ("Car_Speed", "NEW_MSG_1", 0),       # Absolute steering angle
-    ]
-
-    checks = [
-
-    ]
-
-    return CANParser(DBC_FILES.mqb, signals, checks, CANBUS.pt)
-
-  @staticmethod
-  def get_cam_can_parser(CP):
-
-    signals = []
-    checks = []
-    return CANParser(DBC_FILES.mqb, signals, checks, CANBUS.cam)
- 
+    return new_actuators, can_sends
