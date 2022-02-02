e8749f95d67ed7fc614a3ab2ac3980927dde465f
darknight111 <75706333+darknight111@users.noreply.github.com>
2022-02-02 10:29:20 -0800

    Change steerRateCost
    0.5 increases the increments of change of the steering wheel. I.e a car should be able to move the steering wheel at a smaller angle to adjust for changes in trajectory. 
    
    0.5 is the preferred value over stock

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -77,7 +77,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
     ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.00]]
     ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
-    ret.steerRateCost = 1.0
+    ret.steerRateCost = 0.5
     ret.steerActuatorDelay = 0.1  # Default delay, not measured yet
     ret.enableGasInterceptor = 0x201 in fingerprint[0]
     # # Check for Electronic Parking Brake
