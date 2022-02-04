ea23f6a39289aa2bcfd30794849ef963b4194b0c
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2022-02-04 15:01:07 -0600

    CX-5 2022: No dashcam!
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from cereal import car
 from selfdrive.config import Conversions as CV
-from selfdrive.car.mazda.values import CAR, LKAS_LIMITS
+from selfdrive.car.mazda.values import CAR, LKAS_LIMITS, STEER_LOCKOUT_CAR
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.interfaces import CarInterfaceBase
 
@@ -22,7 +22,7 @@ class CarInterface(CarInterfaceBase):
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.mazda)]
     ret.radarOffCan = True
 
-    ret.dashcamOnly = candidate not in (CAR.CX9_2021,)
+    ret.dashcamOnly = candidate in STEER_LOCKOUT_CAR
 
     ret.steerActuatorDelay = 0.1
     ret.steerRateCost = 1.0
