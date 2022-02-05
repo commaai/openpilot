ceab1b4e2ddd49614aaa879db8645579a16743da
deanlee <deanlee3@gmail.com>
2022-02-05 11:12:09 +0800

    fix repeating the same message

--- selfdrive/logcatd/logcatd_android.cc
+++ selfdrive/logcatd/logcatd_android.cc
@@ -15,13 +15,9 @@ int main() {
   PubMaster pm({"androidLog"});
 
   log_time last_log_time = {};
-  logger_list *logger_list = android_logger_list_alloc(ANDROID_LOG_RDONLY | ANDROID_LOG_NONBLOCK, 0, 0);
-
   while (!do_exit) {
     // setup android logging
-    if (!logger_list) {
-      logger_list = android_logger_list_alloc_time(ANDROID_LOG_RDONLY | ANDROID_LOG_NONBLOCK, last_log_time, 0);
-    }
+    logger_list *logger_list = android_logger_list_alloc_time(ANDROID_LOG_RDONLY | ANDROID_LOG_NONBLOCK, last_log_time, 0);
     assert(logger_list);
 
     struct logger *main_logger = android_logger_open(logger_list, LOG_ID_MAIN);
@@ -38,11 +34,16 @@ int main() {
     while (!do_exit) {
       log_msg log_msg;
       int err = android_logger_list_read(logger_list, &log_msg);
-      if (err <= 0) break;
+      if (err <= 0) {
+        break;
+      }
 
       AndroidLogEntry entry;
-      err = android_log_processLogBuffer(&log_msg.entry_v1, &entry);
-      if (err < 0) continue;
+      bool seen = log_msg.entry.sec == last_log_time.tv_sec && log_msg.entry.nsec == last_log_time.tv_nsec;
+      if (seen || android_log_processLogBuffer(&log_msg.entry_v1, &entry) < 0) {
+        continue;
+      }
+
       last_log_time.tv_sec = entry.tv_sec;
       last_log_time.tv_nsec = entry.tv_nsec;
 
@@ -60,12 +61,7 @@ int main() {
     }
 
     android_logger_list_free(logger_list);
-    logger_list = NULL;
     util::sleep_for(500);
-  }
-
-  if (logger_list) {
-    android_logger_list_free(logger_list);
   }
 
   return 0;
