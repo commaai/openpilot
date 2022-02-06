6c0939f51b2dc4e24babedc51e40865cba3177e0
Dean Lee <deanlee3@gmail.com>
2022-02-06 08:02:45 +0800

    logcatd_android.cc: fix repeating the same messages (#23713)
    * fix repeating the same message
    
    * cleanup
    
    * use blocking mode
    
    * check duplicate msg in test
    old-commit-hash: ee85e7026aefc6769a850c7cf1ce9a2f81b6402b

--- selfdrive/logcatd/tests/test_logcatd_android.py
+++ selfdrive/logcatd/tests/test_logcatd_android.py
@@ -4,6 +4,7 @@ import random
 import string
 import time
 import unittest
+import uuid
 
 import cereal.messaging as messaging
 from selfdrive.test.helpers import with_processes
@@ -18,38 +19,27 @@ class TestLogcatdAndroid(unittest.TestCase):
     time.sleep(1)
     messaging.drain_sock(sock)
 
+    sent_msgs = {}
     for _ in range(random.randint(2, 10)):
       # write some log messages
-      sent_msgs = {}
       for __ in range(random.randint(5, 50)):
+        tag = uuid.uuid4().hex
         msg = ''.join(random.choice(string.ascii_letters) for _ in range(random.randrange(2, 50)))
-        if msg in sent_msgs:
-          continue
-        sent_msgs[msg] = ''.join(random.choice(string.ascii_letters) for _ in range(random.randrange(2, 20)))
-        os.system(f"log -t '{sent_msgs[msg]}' '{msg}'")
+        sent_msgs[tag] = {'recv_cnt': 0, 'msg': msg}
+        os.system(f"log -t '{tag}' '{msg}'")
 
       time.sleep(1)
       msgs = messaging.drain_sock(sock)
       for m in msgs:
         self.assertTrue(m.valid)
         self.assertLess(time.monotonic() - (m.logMonoTime / 1e9), 30)
+        tag = m.androidLog.tag
+        if tag in sent_msgs:
+          sent_msgs[tag]['recv_cnt'] += 1
+          self.assertEqual(m.androidLog.message.strip(), sent_msgs[tag]['msg'])
 
-        try:
-          recv_msg = m.androidLog.message.strip()
-        except UnicodeDecodeError:
-          continue
-
-        if recv_msg not in sent_msgs:
-          continue
-
-        # see https://android.googlesource.com/platform/system/core/+/android-2.1_r1/liblog/logd_write.c#144
-        radio_msg = m.androidLog.id == 1 and m.androidLog.tag.startswith("use-Rlog/RLOG-")
-        if m.androidLog.tag == sent_msgs[recv_msg] or radio_msg:
-          del sent_msgs[recv_msg]
-
-      # ensure we received all the logs we sent
-      self.assertEqual(len(sent_msgs), 0)
-
+    for v in sent_msgs.values():
+      self.assertEqual(v['recv_cnt'], 1)
 
 if __name__ == "__main__":
   unittest.main()
