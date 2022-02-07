8bc922b92a5657a23f2b51650220e35ceddcc5eb
Willem Melching <willem.melching@gmail.com>
2022-02-07 12:34:55 +0100

    returning zero is fine

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -231,7 +231,7 @@ UIState::UIState(QObject *parent) : QObject(parent) {
 
   Params params;
   wide_camera = Hardware::TICI() ? params.getBool("EnableWideCamera") : false;
-  prime_type = std::stoi(params.get("PrimeType"));
+  prime_type = std::atoi(params.get("PrimeType").c_str());
 
   // update timer
   timer = new QTimer(this);
