48e97b7872f6b49cb915ddfae6b1392a70aec24e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-02-07 16:10:20 -0800

    small test_models.py speedup

--- selfdrive/test/test_models.py
+++ selfdrive/test/test_models.py
@@ -197,11 +197,11 @@ class TestCarModel(unittest.TestCase):
 
     checks = defaultdict(lambda: 0)
     for can in self.can_msgs:
+      CS = self.CI.update(CC, (can.as_builder().to_bytes(), ))
       for msg in can_capnp_to_can_list(can.can, src_filter=range(64)):
         to_send = package_can_msg(msg)
         ret = self.safety.safety_rx_hook(to_send)
         self.assertEqual(1, ret, f"safety rx failed ({ret=}): {to_send}")
-        CS = self.CI.update(CC, (can_list_to_can_capnp([msg, ]), ))
 
       # TODO: check rest of panda's carstate (steering, ACC main on, etc.)
 
