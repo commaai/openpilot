4e4cb07297485eac378058c6b4e60901e10cbd15
Willem Melching <willem.melching@gmail.com>
2022-02-09 20:27:58 +0100

    ui: configure tethering based on prime type (#23733)
    * Store prime type in param
    
    * set tethering metric based on prime type
    
    * update existing connections
    
    * returning zero is fine
    
    * always set
    
    * add callback on activate
    
    * call systemctl
    
    * match server enum
    
    * add delay
    
    * assume field is there
    
    * snake case

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -115,7 +115,6 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"GsmRoaming", PERSISTENT},
     {"HardwareSerial", PERSISTENT},
     {"HasAcceptedTerms", PERSISTENT},
-    {"HasPrime", PERSISTENT},
     {"IMEI", PERSISTENT},
     {"InstallDate", PERSISTENT},
     {"IsDriverViewEnabled", CLEAR_ON_MANAGER_START},
@@ -145,6 +144,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"PandaSignatures", CLEAR_ON_MANAGER_START},
     {"Passive", PERSISTENT},
     {"PrimeRedirected", PERSISTENT},
+    {"PrimeType", PERSISTENT},
     {"RecordFront", PERSISTENT},
     {"RecordFrontLock", PERSISTENT},  // for the internal fleet
     {"ReleaseNotes", PERSISTENT},
