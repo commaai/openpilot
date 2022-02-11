1a85afd60c2f0d3140dfa8e7cd574d6b88abd5e3
Jason Wen <haibin.wen3@gmail.com>
2022-02-11 10:51:50 -0500

    Resolve conflicts

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -74,7 +74,7 @@ class CarInterfaceBase(ABC):
   def get_std_params(candidate, fingerprint):
     ret = car.CarParams.new_message()
     ret.carFingerprint = candidate
-    ret.unsafeMode = 1
+    ret.unsafeMode = 0
 
     # standard ALC params
     ret.steerControlType = car.CarParams.SteerControlType.torque
