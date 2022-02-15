91b8f9fe62fa4ed1126e3bcdbaaaf4e73cd19ef1
Shane Smiskol <shane@smiskol.com>
2022-02-14 23:13:48 -0800

    formatting is for another PR

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -6,8 +6,7 @@ from selfdrive.car import CarInfo, dbc_dict
 Ecu = car.CarParams.Ecu
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
-
-class CarControllerParams():
+class CarControllerParams:
   # Allow small margin below -3.5 m/s^2 from ISO 15622:2018 since we
   # perform the closed loop control, and might need some
   # to apply some more braking if we're on a downhill slope.
