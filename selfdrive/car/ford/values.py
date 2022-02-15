24570ad8a766f97099e76020c77d6acf4a2d2305
Shane Smiskol <shane@smiskol.com>
2022-02-14 19:56:57 -0800

    Mazda, Ford

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -1,11 +1,19 @@
-from selfdrive.car import dbc_dict
+from enum import Enum
+
+from selfdrive.car import CarInfo, dbc_dict
 from cereal import car
 Ecu = car.CarParams.Ecu
 
 MAX_ANGLE = 87.  # make sure we never command the extremes (0xfff) which cause latching fault
 
-class CAR:
-  FUSION = "FORD FUSION 2018"
+
+class CAR(Enum):
+  FUSION = 0
+
+
+CAR_INFO = {
+  CAR.FUSION: CarInfo("Ford Fusion", {2018})
+}
 
 DBC = {
   CAR.FUSION: dbc_dict('ford_fusion_2018_pt', 'ford_fusion_2018_adas'),
