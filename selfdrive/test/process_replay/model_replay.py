bf3783a297429a1a6031ed0a469991e3afd54867
Harald Schafer <harald.the.engineer@gmail.com>
2022-02-16 15:43:19 -0800

    Tici must be tici

--- selfdrive/test/process_replay/model_replay.py
+++ selfdrive/test/process_replay/model_replay.py
@@ -49,7 +49,7 @@ def model_replay(lr, frs):
   vipc_server.create_buffers(VisionStreamType.VISION_STREAM_ROAD, 40, False, *(tici_f_frame_size if TICI else eon_f_frame_size))
   vipc_server.create_buffers(VisionStreamType.VISION_STREAM_DRIVER, 40, False, *(tici_d_frame_size if TICI else eon_d_frame_size))
   if TICI:
-    vipc_server.create_buffers(VisionStreamType.VISION_STREAM_WIDE_ROAD, 40, False, *(tici_f_frame_size if TICI else eon_f_frame_size))
+    vipc_server.create_buffers(VisionStreamType.VISION_STREAM_WIDE_ROAD, 40, False, *(tici_f_frame_size))
   vipc_server.start_listener()
 
   sm = messaging.SubMaster(['modelV2', 'driverState'])
