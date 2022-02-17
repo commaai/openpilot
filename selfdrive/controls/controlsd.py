0fa696ad93a81aa9bc9ee2638124e921efa0c7ee
Shane Smiskol <shane@smiskol.com>
2022-02-17 15:38:00 -0800

    interpolate for lower rate lateral plan
    interpolate for lower rate lateral plan

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -497,10 +497,12 @@ class Controls:
 
       # Steering PID loop and lateral MPC
       lat_active = self.active and not CS.steerWarning and not CS.steerError and CS.vEgo > self.CP.minSteerSpeed
+      lat_rcv_frames = self.sm.frame - self.sm.rcv_frame['longitudinalPlan']
       desired_curvature, desired_curvature_rate = get_lag_adjusted_curvature(self.CP, CS.vEgo,
                                                                              lat_plan.psis,
                                                                              lat_plan.curvatures,
-                                                                             lat_plan.curvatureRates)
+                                                                             lat_plan.curvatureRates,
+                                                                             lat_rcv_frames)
       actuators.steer, actuators.steeringAngleDeg, lac_log = self.LaC.update(lat_active, CS, self.CP, self.VM, params, self.last_actuators,
                                                                              desired_curvature, desired_curvature_rate)
     else:
