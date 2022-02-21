052f634696736181a10a9a5d93617f31acfe2154
Shane Smiskol <shane@smiskol.com>
2022-02-21 01:01:44 -0600

    Too little torque

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -16,7 +16,7 @@ def create_lta_steer_command(packer, apply_steer, steer_angle, driver_torque, st
   """Creates a CAN message for the Toyota LTA Steer Command."""
 
   percentage = interp(abs(driver_torque), [50, 100], [100, 0])
-  apply_steer = interp(percentage, [-20, 100], [steer_angle, apply_steer])
+  apply_steer = interp(percentage, [-10, 100], [steer_angle, apply_steer])
   values = {
     "COUNTER": raw_cnt,  # 0 to 62
     "SETME_X1": 1,
