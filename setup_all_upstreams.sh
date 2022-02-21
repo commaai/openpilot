e9abfec387ea99ca9eab093278ee550f077f112f
Jason Shuler <jshuler@gmail.com>
2022-02-21 03:12:09 -0500

    Add some helper scripts

---
+++ setup_all_upstreams.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+git remote add upstream https://github.com/commaai/openpilot.git
+cd cereal
+git remote add upstream https://github.com/commaai/cereal.git
+cd ..
+cd opendbc
+git remote add upstream https://github.com/commaai/opendbc.git
+cd ..
+cd panda
+git remote add upstream https://github.com/commaai/panda.git
+cd ..
