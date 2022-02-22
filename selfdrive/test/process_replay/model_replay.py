323ebd27b301295a02775d642b1a7f7fdf2d7365
Harald Schafer <harald.the.engineer@gmail.com>
2022-02-21 16:02:57 -0800

    Make function useable in offline test

--- selfdrive/test/process_replay/model_replay.py
+++ selfdrive/test/process_replay/model_replay.py
@@ -34,7 +34,7 @@ SEND_EXTRA_INPUTS = bool(os.getenv("SEND_EXTRA_INPUTS", "0"))
 VIPC_STREAM = {"roadCameraState": VisionStreamType.VISION_STREAM_ROAD, "driverCameraState": VisionStreamType.VISION_STREAM_DRIVER,
                "wideRoadCameraState": VisionStreamType.VISION_STREAM_WIDE_ROAD}
 
-def get_log_fn(ref_commit):
+def get_log_fn(ref_commit, TEST_ROUTE, tici=True):
   return f"{TEST_ROUTE}_{'model_tici' if TICI else 'model'}_{ref_commit}.bz2"
 
 
@@ -161,7 +161,7 @@ if __name__ == "__main__":
   if not update:
     with open(ref_commit_fn) as f:
       ref_commit = f.read().strip()
-    log_fn = get_log_fn(ref_commit)
+    log_fn = get_log_fn(ref_commit, TEST_ROUTE, tici=TICI)
     try:
       cmp_log = LogReader(BASE_URL + log_fn)
 
@@ -193,7 +193,7 @@ if __name__ == "__main__":
     print("Uploading new refs")
 
     new_commit = get_commit()
-    log_fn = get_log_fn(new_commit)
+    log_fn = get_log_fn(new_commit, TEST_ROUTE, tici=TICI)
     save_log(log_fn, log_msgs)
     try:
       upload_file(log_fn, os.path.basename(log_fn))
