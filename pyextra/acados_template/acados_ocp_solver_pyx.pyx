d915ede9128b5a0265489fe51322c85175326141
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-02-23 13:56:16 +0100

    acados_ocp_solver_pyx.pyx: load_iterate

--- pyextra/acados_template/acados_ocp_solver_pyx.pyx
+++ pyextra/acados_template/acados_ocp_solver_pyx.pyx
@@ -212,7 +212,15 @@ cdef class AcadosOcpSolverFast:
         """
         Loads the iterate stored in json file with filename into the ocp solver.
         """
-        raise NotImplementedError()
+        if not os.path.isfile(filename):
+            raise Exception('load_iterate: failed, file does not exist: ' + os.path.join(os.getcwd(), filename))
+
+        with open(filename, 'r') as f:
+            solution = json.load(f)
+
+        for key in solution.keys():
+            (field, stage) = key.split('_')
+            self.set(int(stage), field, np.array(solution[key]))
 
 
     def get_stats(self, field_):
