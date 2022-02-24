31e1cd8e67a402add2fd0bae61db9e54bdef2d11
Shane Smiskol <shane@smiskol.com>
2022-02-23 21:52:26 -0800

    steer in pre-enable state
    tuple
    
    order

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -458,7 +458,7 @@ class Controls:
           self.v_cruise_kph = initialize_v_cruise(CS.vEgo, CS.buttonEvents, self.v_cruise_kph_last)
 
     # Check if actuators are enabled
-    self.active = self.state == State.enabled or self.state == State.softDisabling
+    self.active = self.state in (State.preEnabled, State.enabled, State.softDisabling)
     if self.active:
       self.current_alert_types.append(ET.WARNING)
 
