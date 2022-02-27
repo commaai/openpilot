f539d9c0bce0b3b992ab1a3ff128de01fefa3c41
Robbe Derks <robbe.derks@gmail.com>
2022-02-27 19:51:59 +0100

    fix kF value for thermal PI controller (#23871)

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -240,7 +240,7 @@ def thermald_thread(end_event, hw_queue):
   thermal_config = HARDWARE.get_thermal_config()
 
   # TODO: use PI controller for UNO
-  controller = PIController(k_p=0, k_i=2e-3, neg_limit=-80, pos_limit=0, rate=(1 / DT_TRML))
+  controller = PIController(k_p=0, k_i=2e-3, k_f=1, neg_limit=-80, pos_limit=0, rate=(1 / DT_TRML))
 
   while not end_event.is_set():
     sm.update(PANDA_STATES_TIMEOUT)
