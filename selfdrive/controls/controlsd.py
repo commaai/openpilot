595f118aa4f6eebacaef4db77179c108ec725483
Shane Smiskol <shane@smiskol.com>
2022-03-02 00:43:19 -0800

    revert change

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -396,7 +396,7 @@ class Controls:
     # if stock cruise is completely disabled, then we can use our own set speed logic
     if not self.CP.pcmCruise:
       self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, CS.buttonEvents, self.button_timers, self.enabled, self.is_metric)
-    else:
+    elif CS.cruiseState.enabled:
       self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
 
     # decrement the soft disable timer at every step, as it's reset on
