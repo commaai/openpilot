197681ec23a17a11c1743f60f99fa1b944ad2a59
Shane Smiskol <shane@smiskol.com>
2022-03-10 00:16:28 -0800

    Volks

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -141,7 +141,7 @@ class CarState(CarStateBase):
 
     # Additional safety checks performed in CarInterface.
     self.parkingBrakeSet = bool(pt_cp.vl["Kombi_01"]["KBI_Handbremse"])  # FIXME: need to include an EPB check as well
-    ret.espDisabled = pt_cp.vl["ESP_21"]["ESP_Tastung_passiv"] != 0
+    ret.espDisabled = bool(pt_cp.vl["ESP_21"]["ESP_Tastung_passiv"])
 
     return ret
 
