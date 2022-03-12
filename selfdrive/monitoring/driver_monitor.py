bb320e948cb4f193b771c2c0223eab56f508f056
ZwX1616 <zwx1616@gmail.com>
2022-03-11 19:12:16 -0800

    tweak3

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -29,8 +29,8 @@ class DRIVER_MONITOR_SETTINGS():
     self._PARTIAL_FACE_THRESHOLD = 0.8 if TICI else 0.45
     self._EYE_THRESHOLD = 0.55
     self._SG_THRESHOLD = 0.88 if TICI else 0.86
-    self._BLINK_THRESHOLD = 0.8085 if TICI else 0.66
-    self._BLINK_THRESHOLD_SLACK = 0.9 if TICI else 0.8
+    self._BLINK_THRESHOLD = 0.58 if TICI else 0.575
+    self._BLINK_THRESHOLD_SLACK = 0.75
     self._BLINK_THRESHOLD_STRICT = self._BLINK_THRESHOLD
 
     self._EE_THRESH11 = 0.75 if TICI else 0.4
