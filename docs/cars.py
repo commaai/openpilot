7fdb4b421681ed06c233990a71bfdbfcca56bac0
Shane Smiskol <shane@smiskol.com>
2022-03-14 16:21:44 -0700

    move some variables to docs.py

--- docs/cars.py
+++ docs/cars.py
@@ -1,80 +1,19 @@
 #!/usr/bin/env python3
 import jinja2
 import os
-from collections import defaultdict, namedtuple
-from enum import Enum
+from collections import defaultdict
 from sortedcontainers import SortedList
 from typing import Dict
 
 from common.basedir import BASEDIR
 from common.params import Params
 from selfdrive.car.car_helpers import interfaces, get_interface_attr
-from selfdrive.car.gm.values import CAR as GM
 from selfdrive.car.hyundai.radar_interface import RADAR_START_ADDR as HKG_RADAR_START_ADDR
-from selfdrive.car.toyota.values import CAR as TOYOTA
-from selfdrive.car.volkswagen.values import CAR as VOLKSWAGEN
 from selfdrive.test.test_routes import non_tested_cars
-
-
-class Tier(Enum):
-  GOLD = "Gold"
-  SILVER = "Silver"
-  BRONZE = "Bronze"
-
-
-class Column(Enum):
-  MAKE = "Make"
-  MODEL = "Model"
-  PACKAGE = "Supported Package"
-  LONGITUDINAL = "openpilot Longitudinal"
-  FSR_LONGITUDINAL = "FSR Longitudinal"
-  FSR_STEERING = "FSR Steering"
-  STEERING_TORQUE = "Steering Torque"
-  SUPPORTED = "Actively Maintained"
-
-
-StarColumns = list(Column)[3:]
-CarFootnote = namedtuple("CarFootnote", ["text", "column", "star"], defaults=[None])
-
-
-def get_star_icon(variant):
-  return '<img src="assets/icon-star-{}.svg" width="22" />'.format(variant)
-
-
-def get_footnote(car_info, column) -> CarFootnote:
-  if car_info.footnotes is None:
-    return None
-
-  for fn in car_info.footnotes:
-    if CAR_FOOTNOTES[fn].column == column:
-      return CAR_FOOTNOTES[fn]
+from selfdrive.car.docs import Tier, Column, StarColumns, MAKES_GOOD_STEERING_TORQUE, CAR_FOOTNOTES, get_footnote, get_star_icon
 
 
 CARS_MD_OUT = os.path.join(BASEDIR, "docs", "CARS_generated.md")
-
-# TODO: which other makes?
-MAKES_GOOD_STEERING_TORQUE = ["toyota", "hyundai", "volkswagen"]
-CAR_FOOTNOTES = {
-  1: CarFootnote("When disconnecting the Driver Support Unit (DSU), openpilot Adaptive Cruise Control (ACC) will replace " \
-                 "stock Adaptive Cruise Control (ACC). NOTE: disconnecting the DSU disables Automatic Emergency Braking (AEB).",
-                 Column.LONGITUDINAL, star="half"),
-  2: CarFootnote("28mph for Camry 4CYL L, 4CYL LE and 4CYL SE which don't have Full-Speed Range Dynamic Radar Cruise Control.",
-                 Column.FSR_LONGITUDINAL),
-  3: CarFootnote("Requires an [OBD-II](https://comma.ai/shop/products/comma-car-harness) car harness and [community built ASCM harness]" \
-                 "(https://github.com/commaai/openpilot/wiki/GM#hardware). NOTE: disconnecting the ASCM disables Automatic Emergency Braking (AEB).",
-                 Column.MODEL),
-  4: CarFootnote("Not including the China market Kamiq, which is based on the (currently) unsupported PQ34 platform.",
-                 Column.MODEL),
-  5: CarFootnote("Not including the USA/China market Passat, which is based on the (currently) unsupported PQ35/NMS platform.",
-                 Column.MODEL),
-  6: CarFootnote("Model-years 2021 and beyond may have a new camera harness design, which isn't yet available from the comma " \
-                 "store. Before ordering, remove the Lane Assist camera cover and check to see if the connector is black " \
-                 "(older design) or light brown (newer design). For the newer design, in the interim, choose \"VW J533 Development\" " \
-                 "from the vehicle drop-down for a harness that integrates at the CAN gateway inside the dashboard.",
-                 Column.MODEL),
-  7: CarFootnote("An inaccurate steering wheel angle sensor makes precise control difficult.",
-                 Column.STEERING_TORQUE, star="half"),
-}
 
 
 class Car:
---
+++ selfdrive/car/docs.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+from collections import namedtuple
+from enum import Enum
+
+
+class Tier(Enum):
+  GOLD = "Gold"
+  SILVER = "Silver"
+  BRONZE = "Bronze"
+
+
+class Column(Enum):
+  MAKE = "Make"
+  MODEL = "Model"
+  PACKAGE = "Supported Package"
+  LONGITUDINAL = "openpilot Longitudinal"
+  FSR_LONGITUDINAL = "FSR Longitudinal"
+  FSR_STEERING = "FSR Steering"
+  STEERING_TORQUE = "Steering Torque"
+  SUPPORTED = "Actively Maintained"
+
+
+StarColumns = list(Column)[3:]
+CarFootnote = namedtuple("CarFootnote", ["text", "column", "star"], defaults=[None])
+
+
+def get_star_icon(variant):
+  return '<img src="assets/icon-star-{}.svg" width="22" />'.format(variant)
+
+
+def get_footnote(car_info, column) -> CarFootnote:
+  if car_info.footnotes is None:
+    return None
+
+  for fn in car_info.footnotes:
+    if CAR_FOOTNOTES[fn].column == column:
+      return CAR_FOOTNOTES[fn]
+
+
+# TODO: which other makes?
+MAKES_GOOD_STEERING_TORQUE = ["toyota", "hyundai", "volkswagen"]
+CAR_FOOTNOTES = {
+  1: CarFootnote("When disconnecting the Driver Support Unit (DSU), openpilot Adaptive Cruise Control (ACC) will replace " \
+                 "stock Adaptive Cruise Control (ACC). NOTE: disconnecting the DSU disables Automatic Emergency Braking (AEB).",
+                 Column.LONGITUDINAL, star="half"),
+  2: CarFootnote("28mph for Camry 4CYL L, 4CYL LE and 4CYL SE which don't have Full-Speed Range Dynamic Radar Cruise Control.",
+                 Column.FSR_LONGITUDINAL),
+  3: CarFootnote("Requires an [OBD-II](https://comma.ai/shop/products/comma-car-harness) car harness and [community built ASCM harness]" \
+                 "(https://github.com/commaai/openpilot/wiki/GM#hardware). NOTE: disconnecting the ASCM disables Automatic Emergency Braking (AEB).",
+                 Column.MODEL),
+  4: CarFootnote("Not including the China market Kamiq, which is based on the (currently) unsupported PQ34 platform.",
+                 Column.MODEL),
+  5: CarFootnote("Not including the USA/China market Passat, which is based on the (currently) unsupported PQ35/NMS platform.",
+                 Column.MODEL),
+  6: CarFootnote("Model-years 2021 and beyond may have a new camera harness design, which isn't yet available from the comma " \
+                 "store. Before ordering, remove the Lane Assist camera cover and check to see if the connector is black " \
+                 "(older design) or light brown (newer design). For the newer design, in the interim, choose \"VW J533 Development\" " \
+                 "from the vehicle drop-down for a harness that integrates at the CAN gateway inside the dashboard.",
+                 Column.MODEL),
+  7: CarFootnote("An inaccurate steering wheel angle sensor makes precise control difficult.",
+                 Column.STEERING_TORQUE, star="half"),
+}
