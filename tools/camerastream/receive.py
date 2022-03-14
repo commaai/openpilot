fa4b434e432f68dfc8944592c63f3594a9fad0b3
George Hotz <george@comma.ai>
2022-03-14 15:02:42 -0700

    camerastream: update receive.py with some options

--- tools/camerastream/receive.py
+++ tools/camerastream/receive.py
@@ -11,23 +11,32 @@ import cereal.messaging as messaging
 # also start bridge
 # then run this "./receive.py <ip>"
 
-SCALE = 1
-XMIN = 771
-XMAX = 1156
-YMIN = 483
-YMAX = 724
+if "FULL" in os.environ:
+  SCALE = 2
+  XMIN, XMAX = 0, 1927
+  YMIN, YMAX = 0, 1207
+else:
+  SCALE = 1
+  XMIN = 771
+  XMAX = 1156
+  YMIN = 483
+  YMAX = 724
 H, W = ((YMAX-YMIN+1)//SCALE, (XMAX-XMIN+1)//SCALE)
 
 if __name__ == '__main__':
   cameras = ['roadCameraState', 'wideRoadCameraState', 'driverCameraState']
+  if "CAM" in os.environ:
+    cam = int(os.environ['CAM'])
+    cameras = cameras[cam:cam+1]
   sm = messaging.SubMaster(cameras, addr=sys.argv[1])
-  win = Window(W*3, H)
-  bdat = np.zeros((H, W*3, 3), dtype=np.uint8)
+  win = Window(W*len(cameras), H)
+  bdat = np.zeros((H, W*len(cameras), 3), dtype=np.uint8)
 
   while 1:
     sm.update()
     for i,k in enumerate(cameras):
       if sm.updated[k]:
+        #print("update", k)
         bgr_raw = sm[k].image
         dat = np.frombuffer(bgr_raw, dtype=np.uint8).reshape(H, W, 3)[:, :, [2,1,0]]
         bdat[:, W*i:W*(i+1)] = dat
