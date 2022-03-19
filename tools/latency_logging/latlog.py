9892fa63fe2fefd24d62945c351c5b4b75dc7110
Lukas Petersson <lukas.petersson.1999@gmail.com>
2022-03-18 17:36:34 -0700

    graph tracked events

--- tools/latency_logging/latlog.py
+++ tools/latency_logging/latlog.py
@@ -4,23 +4,49 @@ from collections import defaultdict
 from tabulate import tabulate
 import matplotlib.patches as mpatches
 
-topics = ['roadCameraState', 'modelV2',  'lateralPlan', 'longitudinalPlan']
+topics = ['roadCameraState', 'wideRoadCameraState', 'driverCameraState', 'modelV2',  'lateralPlan', 'longitudinalPlan', 'carControl']
 frames = defaultdict(lambda: defaultdict(int))
 
 sm = messaging.SubMaster(topics)
-for _ in range(20):
+while len(frames) < 50:
     sm.update()
     for topic, is_updated in sm.updated.items():
         if is_updated:
-            frames[sm[topic].frameId][topic] = sm.logMonoTime[topic]
             if topic == topics[0]:
-                frames[sm[topic].frameId]["start"] = sm[topic].timestampSof
+                frames[sm[topic].frameId]["road_cam_start"] = sm[topic].timestampSof
+                frames[sm[topic].frameId]["road_cam_end"] = sm[topic].timestampEof
+            if topic == topics[1]:
+                frames[sm[topic].frameId]["wide_cam_start"] = sm[topic].timestampSof
+                frames[sm[topic].frameId]["wide_cam_end"] = sm[topic].timestampEof
+            if topic == topics[2]:
+                frames[sm[topic].frameId]["driver_cam_start"] = sm[topic].timestampSof
+                frames[sm[topic].frameId]["driver_cam_end"] = sm[topic].timestampEof
+            frames[sm[topic].frameId][topic] = sm.logMonoTime[topic]
                 
+avg_times = defaultdict(list)
 
+fig, gnt = plt.subplots()
+gnt.set_xlim(0, 150)
+gnt.set_ylim(0, len(frames))
+col_list = ['red', 'blue', 'green', 'cyan', 'magenta', 'yellow', 'olive', 'steelblue', 'deeppink', 'darkseagreen', 'coral', 'brown', 'gray']
+colors = {}
+for i, topic in enumerate(list(frames.values())[10].keys()):
+    colors[topic] = col_list[i]
+
+
+count = 0
 for frame, times in frames.items():
-    print(frame)
-    t0 = times["start"]
+    t0 = min(times.values())
+    events = {}
+    #for topic, time in dict(sorted(times.items(), key= lambda x: x[1])).items():
     for topic, time in times.items():
-        if topic != "start":
-            print("     ", topic, (time-t0)/1e6)
+        #print("     ", topic, (time-t0)/1e6)
+        events[((time-t0)/1e6, 0.3)] = colors[topic]
+        avg_times[topic].append((time-t0)/1e6)
+    gnt.broken_barh(events.keys(), (count, 0.9), facecolors=(events.values()))
+    count += 1
 
+print(tabulate([[topic, sum(avg_times[topic])/len(avg_times[topic]), max(avg_times[topic]), len(avg_times[topic])] for topic in colors.keys()], headers=["event", "avg", "max", "len"]))
+
+plt.legend(handles=[mpatches.Patch(color=list(colors.values())[i], label=list(colors.keys())[i]) for i in range(len(colors))])
+plt.show(block=True)
