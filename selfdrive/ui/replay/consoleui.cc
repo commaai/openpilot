d4f90e14c42cb0010f2fa30e55e5fa0d0b8ba884
Cameron Clough <cameronjclough@gmail.com>
2022-03-20 12:24:13 +0000

    wrong format type

--- selfdrive/ui/replay/consoleui.cc
+++ selfdrive/ui/replay/consoleui.cc
@@ -241,7 +241,7 @@ void ConsoleUI::updateProgressBar(uint64_t cur, uint64_t total, bool success) {
 
 void ConsoleUI::updateSummary() {
   const auto &route = replay->route();
-  mvwprintw(w[Win::Stats], 0, 0, "Route: %s, %d segments", qPrintable(route->name()), route->segments().size());
+  mvwprintw(w[Win::Stats], 0, 0, "Route: %s, %lu segments", qPrintable(route->name()), route->segments().size());
   mvwprintw(w[Win::Stats], 1, 0, "Car Fingerprint: %s", replay->carFingerprint().c_str());
   wrefresh(w[Win::Stats]);
 }
