d79236badda868d1984ace716f1a3f9ca1a7afd2
Gijs Koning <gijs-koning@live.nl>
2022-03-21 15:56:43 +0100

    Final fixes

--- tools/sim/test/test_bridge_connect.py
+++ tools/sim/test/test_bridge_connect.py
@@ -11,9 +11,11 @@ class TestBridgeConnect(unittest.TestCase):
   BRIDGE_DURATION = 50
 
   def test_connect_with_carla(self):
-    # Test connecting to Carla within 1 minute and return no RuntimeError
-    client = connect_carla_client(connect_timeout=50)
+    # Test connecting to Carla within 10 seconds and return no RuntimeError
+    client = connect_carla_client(connect_timeout=10)
     assert client is not None
+    # Will raise an error if not connected
+    client.load_world('Town04_Opt')
 
   def test_run_bridge(self):
     # Test bridge connect with carla and runs without any errors for BRIDGE_DURATION seconds
@@ -23,3 +25,7 @@ class TestBridgeConnect(unittest.TestCase):
     p.start()
 
     p.join(self.BRIDGE_DURATION + 1)  # to ensure script terminates
+
+
+if __name__ == "__main__":
+  unittest.main()
