790a58c4fbb508876f303c05744ba15dcfe7ce31
Lukas Petersson <lukas.petersson.1999@gmail.com>
2022-03-21 18:41:31 -0700

    bug fixes

--- tools/latency_logging/read_cloudlog.py
+++ tools/latency_logging/read_cloudlog.py
@@ -4,10 +4,10 @@ from tools.lib.logreader import LogReader
 
 import json
 
-r = Route("9f583b1d93915c31|2022-03-21--15-09-53")
+r = Route("9f583b1d93915c31|2022-03-21--18-30-38")
 lr = LogReader(r.log_paths()[0])
 
 for msg in lr:
   if msg.which() == "logMessage":
     msg_json = json.loads(msg.logMessage)
-    print(msg_json)
+    print(msg_json['msg'])
