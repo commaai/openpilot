c1d426179be5d39b8887a612fe8f59fe276b091c
Shane Smiskol <shane@smiskol.com>
2022-03-23 12:49:01 -0700

    fix script

--- scripts/count_cars.py
+++ scripts/count_cars.py
@@ -6,7 +6,7 @@ from selfdrive.car.docs import get_tier_car_info
 
 if __name__ == "__main__":
   tiers = get_tier_car_info()
-  cars = [car for tier_cars in tiers for car in tier_cars[1]]
+  cars = [car for tier_cars in tiers.values() for car in tier_cars]
 
   make_count = Counter(l.make for l in cars)
   print("\n", "*" * 20, len(cars), "total", "*" * 20, "\n")
