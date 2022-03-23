426a300c21439cc704984cc83409693388984ba6
Shane Smiskol <shane@smiskol.com>
2022-03-23 13:42:53 -0700

    docs: support for automatically generating website vehicles page (#24020)
    * add vehicles.vue template
    
    * add original vue file
    
    * stash
    
    * stash
    
    * clean up a bit
    
    * add template for now
    
    * implement footnotes and tier copy
    
    * no more generator
    
    * convert to 2 spaces
    
    * should work, now onto vue
    
    * does GH handle this html well?
    
    * fix
    
    * auto-generate descriptions and make tiers' maps non-str
    
    * remove old files
    
    * move template specific variable into templates, should be a bit simpler
    
    * js template is simplier too now
    
    js template is simplier too now
    
    js template is simplier too now
    
    * add video links from the nice car_info
    
    * make rows attributes
    
    * clean up
    
    * fix
    
    * remove template
    
    * experiment with video links in GH
    
    add image
    
    how does this look?
    
    fix
    
    * Revert "experiment with video links in GH"
    
    This reverts commit 8375e717b563f7b4ecf009ffae00174de1a0b8e9.
    
    * sort tier_car_info in place
    
    * unused Tuple
    
    * no type check
    
    * fix script
    old-commit-hash: 85d8997a8a66ef7652b1b186a72d9551292a24ad

--- scripts/count_cars.py
+++ scripts/count_cars.py
@@ -2,12 +2,12 @@
 from collections import Counter
 from pprint import pprint
 
-from selfdrive.car.docs import get_tier_car_rows
+from selfdrive.car.docs import get_tier_car_info
 
 if __name__ == "__main__":
-  tiers = list(get_tier_car_rows())
-  cars = [car for tier_cars in tiers for car in tier_cars[1]]
+  tiers = get_tier_car_info()
+  cars = [car for tier_cars in tiers.values() for car in tier_cars]
 
-  make_count = Counter(l[0] for l in cars)
+  make_count = Counter(l.make for l in cars)
   print("\n", "*" * 20, len(cars), "total", "*" * 20, "\n")
   pprint(make_count)
