d0878e4d498fb7097c11daa049fa4058be4eeda2
Jason Shuler <jshuler@gmail.com>
2022-03-25 15:00:27 -0400

    Revert HIGH_TORQUE

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -4,7 +4,7 @@ from math import fabs
 
 from common.conversions import Conversions as CV
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
-from selfdrive.car.gm.values import CAR, HIGH_TORQUE, CruiseButtons, \
+from selfdrive.car.gm.values import CAR, CruiseButtons, \
                                     AccState, CarControllerParams, NO_ASCM
 from selfdrive.car.interfaces import CarInterfaceBase
 
@@ -254,8 +254,6 @@ class CarInterface(CarInterfaceBase):
       ret.openpilotLongitudinalControl = False # ASCM vehicles use OP for long
       ret.radarOffCan = True # ASCM vehicles (typically) have radar
     
-    if candidate in HIGH_TORQUE:
-      ret.safetyConfigs[0].safetyParam = 1 # set appropriate safety param for increased torque limits to match values.py
          
     # TODO: get actual value, for now starting with reasonable value for
     # civic and scaling by mass and wheelbase
--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -6,58 +6,39 @@ from selfdrive.car import dbc_dict
 from selfdrive.car.docs_definitions import CarFootnote, CarInfo, Column
 Ecu = car.CarParams.Ecu
 
-class CarControllerParams():
-  def __init__(self, CP):  
-    self.STEER_MAX = 300  # Safety limit, not LKA max. Trucks use 600.
-    self.STEER_STEP = 2  # control frames per command
-    self.STEER_DELTA_UP = 7
-    self.STEER_DELTA_DOWN = 17
-    self.MIN_STEER_SPEED = 3.  # m/s
-    self.STEER_DRIVER_ALLOWANCE = 50
-    self.STEER_DRIVER_MULTIPLIER = 4
-    self.STEER_DRIVER_FACTOR = 100
-    self.NEAR_STOP_BRAKE_PHASE = 0.5  # m/s
+class CarControllerParams:
+  STEER_MAX = 300  # Safety limit, not LKA max. Trucks use 600.
+  STEER_STEP = 2  # control frames per command
+  STEER_DELTA_UP = 7
+  STEER_DELTA_DOWN = 17
+  MIN_STEER_SPEED = 3.  # m/s
+  STEER_DRIVER_ALLOWANCE = 50
+  STEER_DRIVER_MULTIPLIER = 4
+  STEER_DRIVER_FACTOR = 100
+  NEAR_STOP_BRAKE_PHASE = 0.5  # m/s
 
-    # Heartbeat for dash "Service Adaptive Cruise" and "Service Front Camera"
-    self.ADAS_KEEPALIVE_STEP = 100
-    self.CAMERA_KEEPALIVE_STEP = 100
+  # Heartbeat for dash "Service Adaptive Cruise" and "Service Front Camera"
+  ADAS_KEEPALIVE_STEP = 100
+  CAMERA_KEEPALIVE_STEP = 100
 
-    # Volt gasbrake lookups
-    self.MAX_GAS = 3072 # Safety limit, not ACC max. Stock ACC >4096 from standstill.
-    self.ZERO_GAS = 2048 # Coasting
-    self.MAX_BRAKE = 350 # ~ -3.5 m/s^2 with regen
+  # Volt gasbrake lookups
+  MAX_GAS = 3072 # Safety limit, not ACC max. Stock ACC >4096 from standstill.
+  ZERO_GAS = 2048 # Coasting
+  MAX_BRAKE = 350 # ~ -3.5 m/s^2 with regen
 
-    # Allow small margin below -3.5 m/s^2 from ISO 15622:2018 since we
-    # perform the closed loop control, and might need some
-    # to apply some more braking if we're on a downhill slope.
-    # Our controller should still keep the 2 second average above
-    # -3.5 m/s^2 as per planner limits
+  # Allow small margin below -3.5 m/s^2 from ISO 15622:2018 since we
+  # perform the closed loop control, and might need some
+  # to apply some more braking if we're on a downhill slope.
+  # Our controller should still keep the 2 second average above
+  # -3.5 m/s^2 as per planner limits
+  ACCEL_MAX = 2. # m/s^2
+  ACCEL_MIN = -4. # m/s^2
 
-    # TODO if this bump works, it belongs in interface per car
-    self.ACCEL_MAX = 2. # m/s^2
-    self.ACCEL_MIN = -4. # m/s^2
-    
-    if CP.carFingerprint in NO_ASCM:
-      pass
-      #self.STEER_DELTA_UP = 3          # ~0.75s time to peak torque (255/50hz/0.75s)
-      #self.STEER_DELTA_DOWN = 7       # ~0.3s from peak torque to zero
-    elif CP.carFingerprint in HIGH_TORQUE:
-      self.STEER_MAX = 600  # Safety limit, not LKA max. Trucks use 600.
-      self.STEER_DELTA_UP = 14
-      self.STEER_DELTA_DOWN = 34
-      self.STEER_DRIVER_ALLOWANCE = 100
-      self.STEER_DRIVER_MULTIPLIER = 4
-      self.STEER_DRIVER_FACTOR = 100
-    
-    #if CP.enableGasInterceptor:
-    #TODO: Pedal may need different accel_max and min?  
-
-    self.MAX_ACC_REGEN = 1404  # Max ACC regen is slightly less than max paddle regen
-    self.GAS_LOOKUP_BP = [-1., 0., self.ACCEL_MAX]
-    self.GAS_LOOKUP_V = [self.MAX_ACC_REGEN, self.ZERO_GAS, self.MAX_GAS]
-    self.BRAKE_LOOKUP_BP = [self.ACCEL_MIN, -1.]
-    self.BRAKE_LOOKUP_V = [self.MAX_BRAKE, 0.]
-
+  MAX_ACC_REGEN = 1404  # Max ACC regen is slightly less than max paddle regen
+  GAS_LOOKUP_BP = [-1., 0., ACCEL_MAX]
+  GAS_LOOKUP_V = [MAX_ACC_REGEN, ZERO_GAS, MAX_GAS]
+  BRAKE_LOOKUP_BP = [ACCEL_MIN, -1.]
+  BRAKE_LOOKUP_V = [MAX_BRAKE, 0.]
 
 STEER_THRESHOLD = 1.0
 
@@ -281,7 +262,6 @@ FINGERPRINTS = {
 
 EV_CAR = set([CAR.BOLT_NR, CAR.VOLT, CAR.VOLT_NR])
 NO_ASCM = set([CAR.VOLT_NR, CAR.MALIBU_NR, CAR.ACADIA_NR, CAR.BOLT_NR, CAR.EQUINOX_NR, CAR.TAHOE_NR, CAR.SILVERADO_NR, CAR.SUBURBAN])
-HIGH_TORQUE = set([CAR.SILVERADO_NR, CAR.SUBURBAN, CAR.TAHOE_NR])
 
 DBC = {
   CAR.HOLDEN_ASTRA: dbc_dict('gm_global_a_powertrain_generated', 'gm_global_a_object', chassis_dbc='gm_global_a_chassis', body_dbc='gm_global_a_lowspeed_1818125'),
