bd71c594a2b8d6ca9f8eacaf1d577cfd2a65bf68
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-03-24 23:23:29 -0700

    pylint: enforce indentation (W0311) (#24039)
    * pylint: enforce indentation (W0311)
    
    * few more
    old-commit-hash: 8af20af66ddaa6bc06d7f72f9134aa9afeed3ed8

--- selfdrive/car/nissan/carcontroller.py
+++ selfdrive/car/nissan/carcontroller.py
@@ -58,14 +58,14 @@ class CarController():
     self.last_angle = apply_angle
 
     if self.CP.carFingerprint in (CAR.ROGUE, CAR.XTRAIL, CAR.ALTIMA) and cruise_cancel:
-        can_sends.append(nissancan.create_acc_cancel_cmd(self.packer, self.car_fingerprint, CS.cruise_throttle_msg, frame))
+      can_sends.append(nissancan.create_acc_cancel_cmd(self.packer, self.car_fingerprint, CS.cruise_throttle_msg, frame))
 
     # TODO: Find better way to cancel!
     # For some reason spamming the cancel button is unreliable on the Leaf
     # We now cancel by making propilot think the seatbelt is unlatched,
     # this generates a beep and a warning message every time you disengage
     if self.CP.carFingerprint in (CAR.LEAF, CAR.LEAF_IC) and frame % 2 == 0:
-        can_sends.append(nissancan.create_cancel_msg(self.packer, CS.cancel_msg, cruise_cancel))
+      can_sends.append(nissancan.create_cancel_msg(self.packer, CS.cancel_msg, cruise_cancel))
 
     can_sends.append(nissancan.create_steering_control(
         self.packer, apply_angle, frame, c.enabled, self.lkas_max_torque))
