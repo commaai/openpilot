b66ba3f01a92e240f6fe3531d3777508c5b6454f
Harald Schafer <harald.the.engineer@gmail.com>
2022-03-25 23:13:17 -0700

    Tune change

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -19,7 +19,7 @@ class CarControllerParams:
 
   STEER_MAX = 1500
   STEER_DELTA_UP = 15       # 1.0s time to peak torque
-  STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+  STEER_DELTA_DOWN = 15     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
   STEER_ERROR_MAX = 350     # max delta between torque cmd and torque motor
 
 
