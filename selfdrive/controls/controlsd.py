49498aa7b5c7e5aed7a6ea767a0a9f10cb7dfdc7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-03-27 18:06:28 -0700

    controlsd: no cruise speed while main off (#24052)
    * controlsd: no cruise speed while main off
    
    * probably more accurate
    
    * update refs

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -403,8 +403,11 @@ class Controls:
     # if stock cruise is completely disabled, then we can use our own set speed logic
     if not self.CP.pcmCruise:
       self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, CS.buttonEvents, self.button_timers, self.enabled, self.is_metric)
-    elif CS.cruiseState.enabled:
-      self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
+    else:
+      if CS.cruiseState.available:
+        self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
+      else:
+        self.v_cruise_kph = 0
 
     # decrement the soft disable timer at every step, as it's reset on
     # entrance in SOFT_DISABLING state
