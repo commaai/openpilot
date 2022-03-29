8ab7b4225cb09fe51916e7f2b2c301eaaf147a98
Harald Schafer <harald.the.engineer@gmail.com>
2022-03-29 15:01:40 -0700

    Update to proper logging

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -8,26 +8,22 @@ CURVATURE_SCALE = 400
 class LatControlTorque(LatControl):
   def __init__(self, CP, CI):
     super().__init__(CP, CI)
-    self.pid = PIController((CP.lateralTuning.torque.kpBP, CP.lateralTuning.torque.kpV),
-                            (CP.lateralTuning.torque.kiBP, CP.lateralTuning.torque.kiV),
+    self.pid = PIController(([0.0], [CP.lateralTuning.torque.kp]),
+                            ([0.0], [CP.lateralTuning.torque.ki]),
                             k_f=CP.lateralTuning.torque.kf, pos_limit=1.0, neg_limit=-1.0)
     self.get_steer_feedforward = CI.get_steer_feedforward_function()
     self.steer_max = 1.0
     self.pid.pos_limit = self.steer_max
     self.pid.neg_limit = -self.steer_max
-    self.use_steering_angle = False
+    self.use_steering_angle = CP.lateralTuning.torque.useSteeringAngle
 
   def reset(self):
     super().reset()
     self.pid.reset()
 
   def update(self, active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk):
-    pid_log = log.ControlsState.LateralPIDState.new_message()
-    pid_log.steeringAngleDeg = float(CS.steeringAngleDeg)
-    pid_log.steeringRateDeg = float(CS.steeringRateDeg)
+    pid_log = log.ControlsState.LateralTorqueState.new_message()
 
-    #pid_log.steeringAngleDesiredDeg = angle_steers_des
-    #pid_log.angleError = angle_steers_des - CS.steeringAngleDeg
     if CS.vEgo < MIN_STEER_SPEED or not active:
       output_torque = 0.0
       pid_log.active = False
@@ -43,15 +39,14 @@ class LatControlTorque(LatControl):
 
       setpoint = desired_lateral_accel + CURVATURE_SCALE * desired_curvature
       measurement = actual_lateral_accel + CURVATURE_SCALE * actual_curvature
+      pid_log.error = setpoint - measurement
 
       output_torque = self.pid.update(setpoint, measurement, override=CS.steeringPressed,
                                       feedforward=desired_lateral_accel, speed=CS.vEgo)
-      pid_log.angleError = setpoint - measurement
       pid_log.active = True
       pid_log.p = self.pid.p
       pid_log.i = self.pid.i
       pid_log.f = self.pid.f
-      #TODO left is positive in this log
       pid_log.output = -output_torque
       pid_log.saturated = self._check_saturation(self.steer_max - abs(output_torque) < 1e-3, CS)
 
