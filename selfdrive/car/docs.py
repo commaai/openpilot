a769c911d2e0fac7b57d3ca57732402277e1413d
Shane Smiskol <shane@smiskol.com>
2022-03-30 14:37:00 -0700

    sort cars in lowercase

--- selfdrive/car/docs.py
+++ selfdrive/car/docs.py
@@ -47,7 +47,7 @@ def get_tier_car_info() -> Dict[Tier, List[CarInfo]]:
 
   # Sort cars by make and model + year
   for tier, cars in tier_car_info.items():
-    tier_car_info[tier] = sorted(cars, key=lambda x: x.make + x.model)
+    tier_car_info[tier] = sorted(cars, key=lambda x: (x.make + x.model).lower())
 
   return tier_car_info
 
