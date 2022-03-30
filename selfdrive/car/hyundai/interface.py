1e7389cd574693e31d3852650e56291f893867fb
Jason Wen <haibin.wen3@gmail.com>
2022-03-29 20:20:56 -0400

    Add car port: Hyundai Tucson Diesel 2019

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -146,6 +146,14 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.5
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+    elif candidate == CAR.TUCSON_DIESEL_2019:
+      ret.lateralTuning.pid.kf = 0.00005
+      ret.mass = 3633. * CV.LB_TO_KG
+      ret.wheelbase = 2.67
+      ret.steerRatio = 14.00 * 1.15
+      tire_stiffness_factor = 0.385
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
 
     # Kia
     elif candidate == CAR.KIA_SORENTO:
