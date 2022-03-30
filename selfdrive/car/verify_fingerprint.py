f7dcc1528d3f3e5a8c05a224c1bef38b5cc84f1e
Shane Smiskol <shane@smiskol.com>
2022-03-30 00:49:28 -0700

    updates

--- selfdrive/car/verify_fingerprint.py
+++ selfdrive/car/verify_fingerprint.py
@@ -1,26 +1,21 @@
 import argparse
 import sys
 
+from common.realtime import DT_CTRL
 from tools.lib.route import Route
 from tools.lib.logreader import MultiLogIterator
 
 
 # Verifies car ports and new fingerprints before they're merged
-# - [x] Fingerprints correctly and fully (i.e. not fuzzy)
-# - [ ] Checks engaged for longer than a minute without user input
+# - [ ] Fingerprints correctly and fully (i.e. not fuzzy)
+# - [x] Checks engaged for longer than a minute without user input
 # - [ ] Gas cancellation is immediate and reliable
 # - [ ] Runs through selfdrive/test/test_models.py
 
-def verify_route(route_name):
-  route = Route(route_name)
-  log_paths = [log for log in route.log_paths() if log is not None]
-  assert len(log_paths) > 0, "No uploaded rlogs in this route"
-
+def check_engagement(lr):
   carState = None
-  controlsState = None
   engaged_frames = 0  # counts frames openpilot was engaged with no user inputs
 
-  lr = MultiLogIterator(log_paths, sort_by_time=True)
   for msg in lr:
     if msg.which() == "carState":
       carState = msg.carState
@@ -35,7 +30,23 @@ def verify_route(route_name):
       else:
         engaged_frames = 0
 
-  assert engaged_frames >= 60, "Engaged time in route is not greater than a minute"
+  assert engaged_frames >= 60 / DT_CTRL, \
+    "Engaged time without user input was only {} seconds".format(round(engaged_frames * DT_CTRL, 1))
+
+
+def get_log_reader(route_name):
+  route = Route(route_name)
+  log_paths = [log for log in route.log_paths() if log is not None]
+  assert len(log_paths) > 0, "No uploaded rlogs in this route"
+
+  return MultiLogIterator(log_paths, sort_by_time=True)
+
+
+def verify_route(route_name):
+  lr = get_log_reader(route_name)
+  # check_fingerprint(lr)
+  lr.reset()
+  check_engagement(lr)
 
 
 if __name__ == "__main__":
