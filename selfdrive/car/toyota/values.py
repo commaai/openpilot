297b32379b5ce3b164ad7cfc588efdf0de332be4
Harald Schafer <harald.the.engineer@gmail.com>
2022-03-31 14:24:38 -0700

    Needed to prevent faults

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -23,7 +23,7 @@ class CarControllerParams:
   def __init__(self, CP):
     if CP.lateralTuning.which == 'torque':
       self.STEER_DELTA_UP = 15       # 1.0s time to peak torque
-      self.STEER_DELTA_DOWN = 15     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+      self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
     else:
       self.STEER_DELTA_UP = 10       # 1.5s time to peak torque
       self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
