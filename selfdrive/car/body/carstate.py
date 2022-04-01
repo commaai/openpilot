0b9790dd24d7824746aac42cd06f7fe3a331b3c2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-03-31 23:02:47 -0700

    locationd: body support (#24098)
    * body: locationd support
    
    * fewer hacks
    
    * my body is never still
    
    * body on localizer
    
    * minor change
    
    * move wait back to 5 secs
    
    * update refs
    
    * try again
    
    Co-authored-by: nuwandavek <vivekaithal44@gmail.com>
    Co-authored-by: Comma Device <device@comma.ai>

--- selfdrive/car/body/carstate.py
+++ selfdrive/car/body/carstate.py
@@ -14,7 +14,7 @@ class CarState(CarStateBase):
     ret.vEgoRaw = ((ret.wheelSpeeds.fl + ret.wheelSpeeds.fr) / 2.) * self.CP.wheelSpeedFactor
 
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    ret.standstill = abs(ret.vEgo) < 1
+    ret.standstill = False
 
     # irrelevant for non-car
     ret.doorOpen = False
