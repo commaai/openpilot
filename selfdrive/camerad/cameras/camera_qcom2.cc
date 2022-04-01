9a2c3fae3c87b758c8f918f2fb62cf5e915cd8e6
Lukas Petersson <lukas.petersson.1999@gmail.com>
2022-04-01 15:20:03 -0700

    nicer strings

--- selfdrive/camerad/cameras/camera_qcom2.cc
+++ selfdrive/camerad/cameras/camera_qcom2.cc
@@ -1083,10 +1083,10 @@ void process_road_camera(MultiCameraState *s, CameraState *c, int cnt) {
   if ((c == &s->road_cam && env_send_road) || (c == &s->wide_road_cam && env_send_wide_road)) {
     framed.setImage(get_frame_image(b));
   }
-  LOGT(c == &s->road_cam ? "RoadCamera" : "WideRoadCamera",": Image set");
+  LOGT(": Image set",c == &s->road_cam ? "RoadCamera" : "WideRoadCamera");
   if (c == &s->road_cam) {
     framed.setTransform(b->yuv_transform.v);
-    LOGT(c == &s->road_cam ? "RoadCamera" : "WideRoadCamera",": Transformed");
+    LOGT(": Transformed",c == &s->road_cam ? "RoadCamera" : "WideRoadCamera");
   }
   s->pm->send(c == &s->road_cam ? "roadCameraState" : "wideRoadCameraState", msg);
 
