72d836c319126d31d59694da96aacf7bb51820da
Comma Device <device@comma.ai>
2022-03-31 21:59:07 -0700

    minor change

--- selfdrive/car/body/carcontroller.py
+++ selfdrive/car/body/carcontroller.py
@@ -7,7 +7,7 @@ from opendbc.can.packer import CANPacker
 MAX_TORQUE = 500
 MAX_TORQUE_RATE = 50
 MAX_ANGLE_ERROR = 7
-INIT_WAIT_COUNTER = int(3. / DT_CTRL)
+INIT_WAIT = 3  # secs
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
@@ -25,7 +25,7 @@ class CarController():
 
     self.torque_r_filtered = 0.
     self.torque_l_filtered = 0.
-    self.wait_counter = INIT_WAIT_COUNTER
+    self.wait_counter = int(INIT_WAIT / DT_CTRL)
 
   @staticmethod
   def deadband_filter(torque, deadband):
