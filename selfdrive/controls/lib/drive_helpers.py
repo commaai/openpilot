981a88f1b5372e9170d7ff3e53da2035c282b3c4
Shane Smiskol <shane@smiskol.com>
2022-03-31 18:11:42 -0700

    better order

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -40,7 +40,7 @@ def rate_limit(new_value, last_value, dw_step, up_step):
   return clip(new_value, last_value + dw_step, last_value + up_step)
 
 
-def update_v_cruise(v_cruise_kph, v_ego, buttonEvents, button_timers, enabled, gas_pressed, metric):
+def update_v_cruise(v_cruise_kph, buttonEvents, button_timers, v_ego, gas_pressed, enabled, metric):
   # handle button presses. TODO: this should be in state_control, but a decelCruise press
   # would have the effect of both enabling and changing speed is checked after the state transition
   if not enabled:
