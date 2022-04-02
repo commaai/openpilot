3ed262e080047e091b538f8ca5ba4379d428e9d9
Shane Smiskol <shane@smiskol.com>
2022-04-01 20:51:58 -0700

    add preEnabled NO_ENTRY test

--- selfdrive/controls/tests/test_state_machine.py
+++ selfdrive/controls/tests/test_state_machine.py
@@ -3,17 +3,17 @@ import unittest
 
 from cereal import car, log
 from selfdrive.car.car_helpers import interfaces
-from selfdrive.controls.controlsd import Controls, ENABLED_STATES
+from selfdrive.controls.controlsd import Controls
 from selfdrive.controls.lib.events import ET
 
 State = log.ControlsState.OpenpilotState
 
 # The event types that maintain the current state
-MAINTAIN_STATES = {State.enabled: None, State.disabled: None, State.overriding: ET.OVERRIDE,
-                   State.softDisabling: ET.SOFT_DISABLE, State.preEnabled: ET.PRE_ENABLE}
-ALL_STATES = (State.disabled, *ENABLED_STATES)
+MAINTAIN_STATES = {State.enabled: None, State.disabled: None, State.softDisabling: ET.SOFT_DISABLE,
+                   State.preEnabled: ET.PRE_ENABLE}
+ALL_STATES = tuple(State.schema.enumerants.values())
 # The event types checked in DISABLED section of state machine
-ENABLE_EVENT_TYPES = (ET.ENABLE, ET.PRE_ENABLE, ET.OVERRIDE)
+ENABLE_EVENT_TYPES = (ET.ENABLE, ET.PRE_ENABLE)
 
 
 class Events:
@@ -26,7 +26,6 @@ class Events:
 
 
 class TestStateMachine(unittest.TestCase):
-  CS = car.CarState()
 
   def setUp(self):
     CarInterface, CarController, CarState = interfaces["mock"]
@@ -36,6 +35,7 @@ class TestStateMachine(unittest.TestCase):
     self.controlsd = Controls(CI=CI)
     self.controlsd.events = Events()
     self.controlsd.soft_disable_timer = 200  # make sure timer never causes state to change
+    self.CS = car.CarState()
 
   def test_immediate_disable(self):
     for state in ALL_STATES:
@@ -52,10 +52,18 @@ class TestStateMachine(unittest.TestCase):
       self.assertEqual(State.disabled, self.controlsd.state)
 
   def test_no_entry(self):
+    # Disabled
     for et in ENABLE_EVENT_TYPES:
       self.controlsd.events.et = [ET.NO_ENTRY, et]
       self.controlsd.state_transition(self.CS)
       self.assertEqual(self.controlsd.state, State.disabled)
+
+    # preEnabled also should consider NO_ENTRY
+    self.setUp()
+    self.controlsd.state = State.preEnabled
+    self.controlsd.events.et = [ET.NO_ENTRY]
+    self.controlsd.state_transition(self.CS)
+    self.assertEqual(self.controlsd.state, State.disabled)
 
   def test_maintain_states(self):
     for state in ALL_STATES:
