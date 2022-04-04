38e87aae770c81f3260b9a455666a5a665c7f934
Shane Smiskol <shane@smiskol.com>
2022-04-04 10:47:49 -0700

    assert car docs naming conventions

--- selfdrive/car/tests/test_docs.py
+++ selfdrive/car/tests/test_docs.py
@@ -5,13 +5,29 @@ from selfdrive.car.docs import CARS_MD_OUT, CARS_MD_TEMPLATE, generate_cars_md, 
 
 
 class TestCarDocs(unittest.TestCase):
-  def test_car_docs(self):
-    generated_cars_md = generate_cars_md(get_tier_car_info(), CARS_MD_TEMPLATE)
+  def setUp(self):
+    self.tier_cars = get_tier_car_info()
+
+  def test_generator(self):
+    generated_cars_md = generate_cars_md(self.tier_cars, CARS_MD_TEMPLATE)
     with open(CARS_MD_OUT, "r") as f:
       current_cars_md = f.read()
 
     self.assertEqual(generated_cars_md, current_cars_md,
                      "Run selfdrive/car/docs.py to generate new supported cars documentation")
+
+  def test_naming_conventions(self):
+    # Asserts standard car naming conventions
+    for tier, cars in self.tier_cars.items():
+      for car in cars:
+        tokens = car.model.lower().split(" ")
+        self.assertNotIn("phev", tokens, "Use `Plug-In Hybrid`")
+        self.assertNotIn("hev", tokens, "Use `Hybrid`")
+        self.assertNotIn("ev", tokens, "Use `Electric`")
+        if "plug-in hybrid" in car.model.lower():
+          self.assertIn("Plug-In Hybrid", car.model, "Use correct capitalization")
+        elif "plug-in" in car.model.lower():
+          self.assertIn("Plug-In", car.model, "Use correct capitalization")
 
 
 if __name__ == "__main__":
