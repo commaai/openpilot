ceeb49e5caa1a5631a1069ced264e2a9c20b9ca6
Igor Biletksyy <bs@privacy.im>
2022-04-04 14:47:32 -0700

    add crc and counter for body

--- selfdrive/car/body/bodycan.py
+++ selfdrive/car/body/bodycan.py
@@ -1,8 +1,18 @@
-def create_control(packer, torque_l, torque_r):
+from selfdrive.car import crc8_pedal
+
+
+def create_control(packer, torque_l, torque_r, idx):
   can_bus = 0
 
   values = {
     "TORQUE_L": torque_l,
     "TORQUE_R": torque_r,
+    "COUNTER": idx & 0xF,
   }
+
+  dat = packer.make_can_msg("TORQUE_CMD", 0, values)[2]
+
+  checksum = crc8_pedal(dat[:-1])
+  values["CHECKSUM"] = checksum
+
   return packer.make_can_msg("TORQUE_CMD", can_bus, values)
