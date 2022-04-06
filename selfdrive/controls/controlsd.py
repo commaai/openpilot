05aeb2ca1a6f6c78d4e6d62cf762cd4682d3a3e4
Comma Device <device@comma.ai>
2022-04-05 19:31:35 -0700

    control with joystick

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -355,8 +355,8 @@ class Controls:
 
       # Check if all manager processes are running
       not_running = {p.name for p in self.sm['managerState'].processes if not p.running}
-      if self.sm.rcv_frame['managerState'] and (not_running - IGNORE_PROCESSES):
-        self.events.add(EventName.processNotRunning)
+      #if self.sm.rcv_frame['managerState'] and (not_running - IGNORE_PROCESSES):
+      #  self.events.add(EventName.processNotRunning)
 
     # Only allow engagement with brake pressed when stopped behind another stopped car
     speeds = self.sm['longitudinalPlan'].speeds
