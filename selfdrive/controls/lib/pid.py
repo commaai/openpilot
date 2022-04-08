513b03374a70d4816185bbfe6e75ecef623476e7
Harald Schafer <harald.the.engineer@gmail.com>
2022-04-08 10:43:09 -0700

    Fix pid comments

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -6,9 +6,9 @@ from common.numpy_fast import clip, interp
 
 class PIDController():
   def __init__(self, k_p, k_i, k_f=0., k_d=0., pos_limit=1e308, neg_limit=-1e308, rate=100):
-    self._k_p = k_p  # proportional gain
-    self._k_i = k_i  # integral gain
-    self._k_d = k_d   # feedforward gain
+    self._k_p = k_p
+    self._k_i = k_i
+    self._k_d = k_d
     self.k_f = k_f   # feedforward gain
     if isinstance(self._k_p, Number):
       self._k_p = [[0], [self._k_p]]
