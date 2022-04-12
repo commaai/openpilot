7e5697fc41b7a21056647ad3af4d7e891e85d5ea
Shane Smiskol <shane@smiskol.com>
2022-04-11 20:51:56 -0700

    easier to read

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -59,7 +59,10 @@ class TestCarModel(unittest.TestCase):
       raise Exception(f"missing test route for {cls.car_model}")
 
     disable_radar = False
-    test_segs = [2, 1, 0] if cls.test_route.segment is None else [cls.test_route.segment]
+    test_segs = [2, 1, 0]
+    if cls.test_route.segment is not None:
+      test_segs = [cls.test_route.segment]
+
     for seg in test_segs:
       try:
         lr = LogReader(get_url(cls.test_route.route, seg))
