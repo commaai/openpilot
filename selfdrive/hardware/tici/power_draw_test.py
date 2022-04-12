95aa03ece2418fe4ca097f07dd3342420963f87e
Comma Device <device@comma.ai>
2022-04-12 15:13:17 -0700

    no star import

--- selfdrive/hardware/tici/power_draw_test.py
+++ selfdrive/hardware/tici/power_draw_test.py
@@ -3,7 +3,7 @@ import os
 import time
 import numpy as np
 from selfdrive.hardware.tici.hardware import Tici
-from selfdrive.hardware.tici.pins import *
+from selfdrive.hardware.tici.pins import GPIO
 from common.gpio import gpio_init, gpio_set
 
 def read_power():
@@ -42,7 +42,12 @@ def gpio_export(pin):
 
 
 if __name__ == "__main__":
+  gpio_export(GPIO.CAM0_AVDD_EN)
+  gpio_export(GPIO.CAM0_RSTN)
+  gpio_export(GPIO.CAM1_RSTN)
+  gpio_export(GPIO.CAM2_RSTN)
   print("hello")
+  os.system('kill $(pgrep -f "manager.py")')
   os.system('kill $(pgrep -f "python -m selfdrive.athena.manage_athenad")')
   os.system('kill $(pgrep -f "selfdrive.athena.athenad")')
   # stopping weston turns off lcd3v3
@@ -54,59 +59,55 @@ if __name__ == "__main__":
   t.initialize_hardware()
   t.set_power_save(True)
   t.set_screen_brightness(0)
-  gpio_init(GPIO_STM_RST_N, True)
-  gpio_init(GPIO_HUB_RST_N, True)
-  gpio_init(GPIO_UBLOX_PWR_EN, True)
-  gpio_init(GPIO_LTE_RST_N, True)
-  gpio_init(GPIO_LTE_PWRKEY, True)
-  gpio_export(GPIO_CAM0_AVDD_EN)
-  gpio_export(GPIO_CAM0_RSTN)
-  gpio_export(GPIO_CAM1_RSTN)
-  gpio_export(GPIO_CAM2_RSTN)
-  gpio_init(GPIO_CAM0_AVDD_EN, True)
-  gpio_init(GPIO_CAM0_RSTN, True)
-  gpio_init(GPIO_CAM1_RSTN, True)
-  gpio_init(GPIO_CAM2_RSTN, True)
+  gpio_init(GPIO.STM_RST_N, True)
+  gpio_init(GPIO.HUB_RST_N, True)
+  gpio_init(GPIO.UBLOX_PWR_EN, True)
+  gpio_init(GPIO.LTE_RST_N, True)
+  gpio_init(GPIO.LTE_PWRKEY, True)
+  gpio_init(GPIO.CAM0_AVDD_EN, True)
+  gpio_init(GPIO.CAM0_RSTN, True)
+  gpio_init(GPIO.CAM1_RSTN, True)
+  gpio_init(GPIO.CAM2_RSTN, True)
 
 
   os.system("sudo su -c 'echo 0 > /sys/kernel/debug/regulator/camera_rear_ldo/enable'")  # cam 1v2 off
-  gpio_set(GPIO_CAM0_AVDD_EN, False)    # cam 2v8 off
-  gpio_set(GPIO_LTE_RST_N, True)        # quectel off
-  gpio_set(GPIO_UBLOX_PWR_EN, False)    # gps off
-  gpio_set(GPIO_STM_RST_N, True)        # panda off
-  gpio_set(GPIO_HUB_RST_N, False)       # hub off
+  gpio_set(GPIO.CAM0_AVDD_EN, False)    # cam 2v8 off
+  gpio_set(GPIO.LTE_RST_N, True)        # quectel off
+  gpio_set(GPIO.UBLOX_PWR_EN, False)    # gps off
+  gpio_set(GPIO.STM_RST_N, True)        # panda off
+  gpio_set(GPIO.HUB_RST_N, False)       # hub off
   # cameras in reset
-  gpio_set(GPIO_CAM0_RSTN, False)
-  gpio_set(GPIO_CAM1_RSTN, False)
-  gpio_set(GPIO_CAM2_RSTN, False)
-  time.sleep(5)
+  gpio_set(GPIO.CAM0_RSTN, False)
+  gpio_set(GPIO.CAM1_RSTN, False)
+  gpio_set(GPIO.CAM2_RSTN, False)
+  time.sleep(8)
 
   print("baseline: ", read_power_avg())
-  gpio_set(GPIO_CAM0_AVDD_EN, True)
+  gpio_set(GPIO.CAM0_AVDD_EN, True)
   time.sleep(2)
   print("cam avdd: ", read_power_avg())
   os.system("sudo su -c 'echo 1 > /sys/kernel/debug/regulator/camera_rear_ldo/enable'")
   time.sleep(2)
   print("cam dvdd: ", read_power_avg())
-  gpio_set(GPIO_CAM0_RSTN, True)
-  gpio_set(GPIO_CAM1_RSTN, True)
-  gpio_set(GPIO_CAM2_RSTN, True)
+  gpio_set(GPIO.CAM0_RSTN, True)
+  gpio_set(GPIO.CAM1_RSTN, True)
+  gpio_set(GPIO.CAM2_RSTN, True)
   time.sleep(2)
   print("cams up:  ", read_power_avg())
-  gpio_set(GPIO_HUB_RST_N, True)
+  gpio_set(GPIO.HUB_RST_N, True)
   time.sleep(2)
   print("usb hub:  ", read_power_avg())
-  gpio_set(GPIO_STM_RST_N, False)
+  gpio_set(GPIO.STM_RST_N, False)
   time.sleep(5)
   print("panda:    ", read_power_avg())
-  gpio_set(GPIO_UBLOX_PWR_EN, True)
+  gpio_set(GPIO.UBLOX_PWR_EN, True)
   time.sleep(5)
   print("gps:      ", read_power_avg())
-  gpio_set(GPIO_LTE_RST_N, False)
+  gpio_set(GPIO.LTE_RST_N, False)
   time.sleep(1)
-  gpio_set(GPIO_LTE_PWRKEY, True)
+  gpio_set(GPIO.LTE_PWRKEY, True)
   time.sleep(1)
-  gpio_set(GPIO_LTE_PWRKEY, False)
+  gpio_set(GPIO.LTE_PWRKEY, False)
   time.sleep(5)
   print("quectel:  ", read_power_avg())
 
