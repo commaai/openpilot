f971fcd543dd5799d753801145b243a5830de411
Harald Schafer <harald.the.engineer@gmail.com>
2022-04-13 13:33:42 -0700

    change cost

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -61,7 +61,7 @@ class LateralPlanner:
       d_path_xyz = self.LP.get_d_path(v_ego, self.t_idxs, self.path_xyz)
     else:
       d_path_xyz = self.path_xyz
-    self.lat_mpc.set_weights(MPC_COST_LAT.PATH, MPC_COST_LAT.HEADING, self.steer_rate_cost)
+    self.lat_mpc.set_weights(MPC_COST_LAT.PATH, 0.15 * MPC_COST_LAT.HEADING, self.steer_rate_cost)
 
     y_pts = np.interp(v_ego * self.t_idxs[:LAT_MPC_N + 1], np.linalg.norm(d_path_xyz, axis=1), d_path_xyz[:, 1])
     heading_pts = np.interp(v_ego * self.t_idxs[:LAT_MPC_N + 1], np.linalg.norm(self.path_xyz, axis=1), self.plan_yaw)
