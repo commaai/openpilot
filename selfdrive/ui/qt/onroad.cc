de1e0dca8da8e8d2fc74696f44f503a89b8d47fe
Shane Smiskol <shane@smiskol.com>
2022-04-14 13:44:03 -0700

    tune design

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -308,7 +308,7 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
   const UIScene &scene = s->scene;
   // lanelines
   for (int i = 0; i < std::size(scene.lane_line_vertices); ++i) {
-    painter.setBrush(QColor::fromRgbF(1.0, 1.0, 1.0, scene.lane_line_probs[i]));
+    painter.setBrush(QColor::fromRgbF(1.0, 1.0, 1.0, std::clamp<float>(scene.lane_line_probs[i], 0.0, 0.7)));
     painter.drawPolygon(scene.lane_line_vertices[i].v, scene.lane_line_vertices[i].cnt);
   }
 
@@ -319,7 +319,7 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
   }
 
   // paint path
-  QLinearGradient bg(0, height(), 0, height() / 2);
+  QLinearGradient bg(0, height(), 0, height() / 4);
   if (scene.end_to_end) {
     const auto &orientation = (*s->sm)["modelV2"].getModelV2().getOrientation();
     float orientation_future = 0;
@@ -329,10 +329,11 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
     // straight: 101, in turns: 70
     const float curve_hue = fmax(70, 101 - (orientation_future * 310));
 
-    bg.setColorAt(0.0, QColor::fromHslF(148 / 360., 1.0, 0.5, 1.0));
-    bg.setColorAt(0.35, QColor::fromHslF(148 / 360., 1.0, 0.5, 0.9));
-    bg.setColorAt(0.9, QColor::fromHslF(curve_hue / 360., 1.0, 0.65, 0.8));
-    bg.setColorAt(1.0, QColor::fromHslF(curve_hue / 360., 1.0, 0.65, 0.4));
+    bg.setColorAt(0.0 / 1.5, QColor::fromHslF(148 / 360., 1.0, 0.5, 1.0));
+    bg.setColorAt(0.45 / 1.5, QColor::fromHslF(101 / 360., 1.0, 0.68, 0.8));
+    bg.setColorAt(0.7 / 1.5, QColor::fromHslF(curve_hue / 360., 1.0, 0.65, 0.6));
+//    bg.setColorAt(1 / 1.5, QColor::fromHslF(curve_hue / 360., 1.0, 0.65, 0.4));
+    bg.setColorAt(1.0, QColor::fromHslF(curve_hue / 360., 1.0, 0.65, 0.0));
   } else {
     bg.setColorAt(0, whiteColor());
     bg.setColorAt(1, whiteColor(0));
