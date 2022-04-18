566a96b341362bdf713e3501f8141177ac66fcc2
David Peterson <cellfreak@users.noreply.github.com>
2022-04-18 18:19:38 -0400

    Update values.py
    Add video_link :     SubaruCarInfo("Subaru Crosstrek 2018-19", good_torque=True),video_link="https://youtu.be/Agww7oE1k-s"),

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -40,7 +40,7 @@ CAR_INFO: Dict[str, Union[SubaruCarInfo, List[SubaruCarInfo]]] = {
   CAR.ASCENT: SubaruCarInfo("Subaru Ascent 2019-20"),
   CAR.IMPREZA: [
     SubaruCarInfo("Subaru Impreza 2017-19", good_torque=True),
-    SubaruCarInfo("Subaru Crosstrek 2018-19", good_torque=True),
+    SubaruCarInfo("Subaru Crosstrek 2018-19", good_torque=True),video_link="https://youtu.be/Agww7oE1k-s"),
   ],
   CAR.IMPREZA_2020: [
     SubaruCarInfo("Subaru Impreza 2020-21"),
