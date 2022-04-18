f96114ff4a1481f3b5961202d3e1d16b6774a56a
Shane Smiskol <shane@smiskol.com>
2022-04-18 13:19:05 -0700

    fix sl

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -333,7 +333,7 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
 
     bg.setColorAt(0.0, QColor::fromHslF(148 / 360., 0.94, 0.51, 0.4));
     bg.setColorAt(0.75 / 1.5, QColor::fromHslF(curve_hue / 360., 1.0, 0.68, 0.35));
-    bg.setColorAt(1.0, QColor::fromHslF(curve_hue / 360., 0.94, 0.51, 0.0));
+    bg.setColorAt(1.0, QColor::fromHslF(curve_hue / 360., 1.0, 0.68, 0.0));
   } else {
     bg.setColorAt(0, whiteColor());
     bg.setColorAt(1, whiteColor(0));
