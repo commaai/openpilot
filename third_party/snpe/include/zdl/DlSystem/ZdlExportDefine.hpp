0be683fd7258f44b909d1556a3894e827e3cf0ee
Comma Device <device@comma.ai>
2022-04-18 18:51:02 -0700

    update snpe to snpe-1.61.0.3358

---
+++ third_party/snpe/include/zdl/DlSystem/ZdlExportDefine.hpp
@@ -0,0 +1,13 @@
+//=============================================================================
+//
+//  Copyright (c) 2015, 2020 Qualcomm Technologies, Inc.
+//  All Rights Reserved.
+//  Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//=============================================================================
+
+#pragma once
+
+#ifndef ZDL_EXPORT
+#define ZDL_EXPORT
+#endif
