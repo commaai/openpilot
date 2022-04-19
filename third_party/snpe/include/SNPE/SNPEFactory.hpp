7921b2d197466e080190929173760cdd8c14ae8d
George Hotz <72895+geohot@users.noreply.github.com>
2022-04-18 19:31:10 -0700

    Update SNPE to 1.61.0.3358 (#24253)
    * update snpe to snpe-1.61.0.3358
    
    * fix new snpe build
    
    * fix snpe dir in files_tici
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 40cf4a7498686665cde255841f139bdfb1351566

--- third_party/snpe/include/SNPE/SNPEFactory.hpp
+++ third_party/snpe/include/SNPE/SNPEFactory.hpp
@@ -1,6 +1,6 @@
 //==============================================================================
 //
-//  Copyright (c) 2015-2020 Qualcomm Technologies, Inc.
+//  Copyright (c) 2015-2021 Qualcomm Technologies, Inc.
 //  All Rights Reserved.
 //  Confidential and Proprietary - Qualcomm Technologies, Inc.
 //
@@ -46,8 +46,6 @@ public:
     * current platform.
     *
     * @param[in] runtime The target runtime to check.
-    *
-    * @param[in] option Extent to perform runtime available check.
     *
     * @return True if the supplied runtime is available; false,
     *         otherwise.
@@ -122,6 +120,97 @@ public:
     *         otherwise.
     */
    static bool isGLCLInteropSupported();
+
+   static const char* getLastError();
+
+   /**
+    * Initializes logging with the specified log level.
+    * initializeLogging with level, is used on Android platforms
+    * and after successful initialization, SNPE
+    * logs are printed in android logcat logs.
+    *
+    * It is recommended to initializeLogging before creating any
+    * SNPE instances, in order to capture information related to
+    * core initialization. If this is called again after first
+    * time initialization, subsequent calls are ignored.
+    * Also, Logging can be re-initialized after a call to
+    * terminateLogging API by calling initializeLogging again.
+    *
+    * A typical usage of Logging life cycle can be
+    * initializeLogging()
+    *        any other SNPE API like isRuntimeAvailable()
+    * * setLogLevel() - optional - can be called anytime
+    *         between initializeLogging & terminateLogging
+    *		  SNPE instance creation, inference, destroy
+    * terminateLogging().
+    *
+    * Please note, enabling logging can have performance impact.
+    *
+    * @param[in] LogLevel_t Log level (LOG_INFO, LOG_WARN, etc.).
+    *
+    * @return True if successful, False otherwise.
+    */
+   static bool initializeLogging(const zdl::DlSystem::LogLevel_t& level);
+
+   /**
+    * Initializes logging with the specified log level and log path.
+    * initializeLogging with level & log path, is used on non Android
+    * platforms and after successful initialization, SNPE
+    * logs are printed in std output & into log files created in the
+    * log path.
+    *
+    * It is recommended to initializeLogging before creating any
+    * SNPE instances, in order to capture information related to
+    * core initialization. If this is called again after first
+    * time initialization, subsequent calls are ignored.
+    * Also, Logging can be re-initialized after a call to
+    * terminateLogging API by calling initializeLogging again.
+    *
+    * A typical usage of Logging life cycle can be
+    * initializeLogging()
+    *        any other SNPE API like isRuntimeAvailable()
+    * * setLogLevel() - optional - can be called anytime
+    *         between initializeLogging & terminateLogging
+    *		  SNPE instance creation, inference, destroy
+    * terminateLogging()
+    *
+    * Please note, enabling logging can have performance impact
+    *
+    * @param[in] LogLevel_t Log level (LOG_INFO, LOG_WARN, etc.).
+    *
+    * @param[in] Path of directory to store logs.
+    *      If path is empty, the default path is "./Log".
+    *      For android, the log path is ignored.
+    *
+    * @return True if successful, False otherwise.
+    */
+   static bool initializeLogging(const zdl::DlSystem::LogLevel_t& level, const std::string& logPath);
+
+   /**
+    * Updates the current logging level with the specified level.
+    * setLogLevel is optional, called anytime after initializeLogging
+    * and before terminateLogging, to update the log level set.
+    * Log levels can be updated multiple times by calling setLogLevel
+    * A call to setLogLevel() is ignored if it is made before
+    * initializeLogging() or after terminateLogging()
+    *
+    * @param[in] LogLevel_t Log level (LOG_INFO, LOG_WARN, etc.).
+    *
+    * @return True if successful, False otherwise.
+    */
+   static bool setLogLevel(const zdl::DlSystem::LogLevel_t& level);
+
+   /**
+    * Terminates logging.
+    *
+    * It is recommended to terminateLogging after initializeLogging
+    * in order to disable logging information.
+    * If this is called before initialization or after first time termination,
+    * calls are ignored.
+    *
+    * @return True if successful, False otherwise.
+    */
+   static bool terminateLogging(void);
 };
 
 /** @} */ /* end_addtogroup c_plus_plus_apis C++ */
