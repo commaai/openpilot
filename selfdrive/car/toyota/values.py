2174005f05c4cb55636c50080dc094f108f51547
HaraldSchafer <harald.the.engineer@gmail.com>
2022-04-19 19:34:31 -0700

    Lateral torque-based control with roll on TSS2 corolla and TSSP rav4 (#24260)
    * Initial commit
    
    * Fix bugs
    
    * Need more torque rate
    
    * Cleanup cray cray control
    
    * Write nicely
    
    * Chiiil
    
    * Not relevant for cray cray control
    
    * Do some logging
    
    * Seems like it has more torque than I thought
    
    * Bit more feedforward
    
    * Tune change
    
    * Retune
    
    * Retune
    
    * Little more chill
    
    * Add coroll
    
    * Add corolla
    
    * Give craycray a good name
    
    * Update to proper logging
    
    * D to the PI
    
    * Should be in radians
    
    * Add d
    
    * Start oscillations
    
    * Add D term
    
    * Only change torque rate limits for new tune
    
    * Add d logging
    
    * Should be enough
    
    * Wrong sign in D
    
    * Downtune a little
    
    * Needed to prevent faults
    
    * Add lqr rav4 to tune
    
    * Try derivative again
    
    * Data based retune
    
    * Data based retune
    
    * add friction compensation
    
    * Doesnt need too much P with friction comp
    
    * remove lqr
    
    * Remove kd
    
    * Fix tests
    
    * fix tests
    
    * Too much error
    
    * Get roll induced error under 1cm/deg
    
    * Too much jitter
    
    * Do roll comp
    
    * Add ki
    
    * Final update
    
    * Update refs
    
    * Cleanup latcontrol_torque a little more
    old-commit-hash: fe0bcdaef6b1fd3fc9d855aefeba755b35dfc222

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -18,9 +18,15 @@ class CarControllerParams:
   ACCEL_MIN = -3.5  # m/s2
 
   STEER_MAX = 1500
-  STEER_DELTA_UP = 10       # 1.5s time to peak torque
-  STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
   STEER_ERROR_MAX = 350     # max delta between torque cmd and torque motor
+
+  def __init__(self, CP):
+    if CP.lateralTuning.which == 'torque':
+      self.STEER_DELTA_UP = 15       # 1.0s time to peak torque
+      self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+    else:
+      self.STEER_DELTA_UP = 10       # 1.5s time to peak torque
+      self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
 
 
 class ToyotaFlags(IntFlag):
