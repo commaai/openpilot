db88843cbbc7bf9cd05f5dff32a2a3810ddfd903
Comma Device <device@comma.ai>
2022-04-20 20:39:38 -0700

    start v4l encoder

--- selfdrive/loggerd/encoder.h
+++ selfdrive/loggerd/encoder.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include "selfdrive/loggerd/loggerd.h"
 
 class VideoEncoder {
 public:
---
+++ selfdrive/loggerd/v4l_encoder.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "selfdrive/loggerd/encoder.h"
+#include "selfdrive/loggerd/loggerd.h"
+#include "selfdrive/loggerd/video_writer.h"
+
+class V4LEncoder : public VideoEncoder {
+public:
+  V4LEncoder(const char* filename, CameraType type, int width, int height, int fps, int bitrate, bool h265, int out_width, int out_height, bool write = true);
+  ~V4LEncoder();
+  int encode_frame(const uint8_t *y_ptr, const uint8_t *u_ptr, const uint8_t *v_ptr,
+                   int in_width, int in_height, uint64_t ts);
+  void encoder_open(const char* path);
+  void encoder_close();
+private:
+  const char* filename;
+  CameraType type;
+  int in_width_, in_height_;
+  int width, height, fps;
+  bool remuxing, write;
+};
