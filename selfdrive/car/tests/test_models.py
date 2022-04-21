17cb65c4b75987e4fa6630a02f384e27f1472d13
Shane Smiskol <shane@smiskol.com>
2022-04-20 22:28:56 -0700

    Toyota: resolve controls mismatch due to interceptor thresholds (#23710)
    * remove gas interceptor exception
    
    * panda
    
    * bump submodules
    
    * bump panda
    
    * forgot carstate change
    
    * revert
    
    * add test route
    
    * bump
    
    * update refs for CS.gas
    
    * bump submodules to master

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -209,13 +209,7 @@ class TestCarModel(unittest.TestCase):
 
       # TODO: check rest of panda's carstate (steering, ACC main on, etc.)
 
-      # TODO: make the interceptor thresholds in openpilot and panda match, then remove this exception
-      gas_pressed = CS.gasPressed
-      if self.CP.enableGasInterceptor and gas_pressed and not self.safety.get_gas_pressed_prev():
-        # panda intentionally has a higher threshold
-        if self.CP.carName == "toyota" and 15 < CS.gas < 15*1.5:
-          gas_pressed = False
-      checks['gasPressed'] += gas_pressed != self.safety.get_gas_pressed_prev()
+      checks['gasPressed'] += CS.gasPressed != self.safety.get_gas_pressed_prev()
 
       # TODO: remove this exception once this mismatch is resolved
       brake_pressed = CS.brakePressed
@@ -256,6 +250,7 @@ class TestCarModel(unittest.TestCase):
 
     failed_checks = {k: v for k, v in checks.items() if v > 0}
     self.assertFalse(len(failed_checks), f"panda safety doesn't agree with openpilot: {failed_checks}")
+
 
 if __name__ == "__main__":
   unittest.main()
