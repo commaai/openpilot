3063c70ff1b6277b002639679e18e2990b2296d2
Willem Melching <willem.melching@gmail.com>
2022-04-22 23:43:44 +0200

    updated: only run offroad & disable automatic git garbage collection (#24269)
    * updated: disable automatic git garbage collection
    
    * set gc.autoDetach false
    
    * add gc cloudlogs
    
    * trigger updated
    
    * handle exception
    
    * trigger updated
    
    * only while offroad
    
    * no trigger needed
    
    * trigger updated
    
    * check if git gc was interrupted
    
    * trigger updated
    
    * dont set params when shutting down
    
    * let manager clear params
    
    * no offroad check anymore
    
    * event is nicer to parse
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -130,7 +130,6 @@ def manager_thread() -> None:
 
   ensure_running(managed_processes.values(), started=False, not_run=ignore)
 
-  started_prev = False
   sm = messaging.SubMaster(['deviceState', 'carParams'], poll=['deviceState'])
   pm = messaging.PubMaster(['managerState'])
 
@@ -140,13 +139,6 @@ def manager_thread() -> None:
     started = sm['deviceState'].started
     driverview = params.get_bool("IsDriverViewEnabled")
     ensure_running(managed_processes.values(), started=started, driverview=driverview, notcar=sm['carParams'].notCar, not_run=ignore)
-
-    # trigger an update after going offroad
-    if started_prev and not started and 'updated' in managed_processes:
-      os.sync()
-      managed_processes['updated'].signal(signal.SIGHUP)
-
-    started_prev = started
 
     running = ' '.join("%s%s\u001b[0m" % ("\u001b[32m" if p.proc.is_alive() else "\u001b[31m", p.name)
                        for p in managed_processes.values() if p.proc)
