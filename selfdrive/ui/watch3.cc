66b158ab9e912a4dae58834f79f04f23529a0509
Willem Melching <willem.melching@gmail.com>
2022-04-22 15:18:19 +0200

    fix watch3

--- selfdrive/ui/watch3.cc
+++ selfdrive/ui/watch3.cc
@@ -19,7 +19,8 @@ int main(int argc, char *argv[]) {
   {
     QHBoxLayout *hlayout = new QHBoxLayout();
     layout->addLayout(hlayout);
-    hlayout->addWidget(new CameraViewWidget("navd", VISION_STREAM_MAP, false));
+    // TODO: make mapd output YUV
+    // hlayout->addWidget(new CameraViewWidget("navd", VISION_STREAM_MAP, false));
     hlayout->addWidget(new CameraViewWidget("camerad", VISION_STREAM_ROAD, false));
   }
 
