17141a8240bd8ec5defa1de2da937544c52ef0fe
Shane Smiskol <shane@smiskol.com>
2022-04-25 12:51:58 -0700

    3% faster at 1000000 loops

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -320,7 +320,7 @@ class CarInterface(CarInterfaceBase):
     # On some newer model years, the CANCEL button acts as a pause/resume button based on the PCM state
     # To avoid re-engaging when openpilot cancels, check user engagement intention via buttons
     # Main button also can trigger an engagement on these cars
-    allow_enable = any([btn in ENABLE_BUTTONS for btn in self.CS.cruise_buttons]) or any(self.CS.main_buttons)
+    allow_enable = any(btn in ENABLE_BUTTONS for btn in self.CS.cruise_buttons) or any(self.CS.main_buttons)
     events = self.create_common_events(ret, pcm_enable=self.CS.CP.pcmCruise, allow_enable=allow_enable)
 
     if self.CS.brake_error:
