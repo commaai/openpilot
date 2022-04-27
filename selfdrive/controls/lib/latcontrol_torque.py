1c422b9cbd146cca3bb4bf3a73c1f2ac059b9cda
Shane Smiskol <shane@smiskol.com>
2022-04-27 01:01:48 -0700

    not needed

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -76,4 +76,4 @@ class LatControlTorque(LatControl):
       pid_log.saturated = self._check_saturation(self.steer_max - abs(output_torque) < 1e-3, CS)
 
     # TODO left is positive in this convention
-    return -clip(output_torque, -self.steer_max, self.steer_max), 0.0, pid_log
+    return -output_torque, 0.0, pid_log
