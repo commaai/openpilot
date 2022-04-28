5957036902e7da9c3332e7fa453e35a8705fc32f
Harald Schafer <harald.the.engineer@gmail.com>
2022-04-28 16:40:28 -0700

    Give toyotas a little boost when starting

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -9,7 +9,7 @@ from selfdrive.car.toyota.values import CAR, STATIC_DSU_MSGS, NO_STOP_TIMER_CAR,
 from opendbc.can.packer import CANPacker
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
-
+CREEP_SPEED = 2.3
 
 class CarController:
   def __init__(self, dbc_name, CP, VM):
@@ -42,12 +42,17 @@ class CarController:
       else:
         PEDAL_SCALE = interp(CS.out.vEgo, [0.0, MIN_ACC_SPEED, MIN_ACC_SPEED + PEDAL_TRANSITION], [0.4, 0.5, 0.0])
       # offset for creep and windbrake
-      pedal_offset = interp(CS.out.vEgo, [0.0, 2.3, MIN_ACC_SPEED + PEDAL_TRANSITION], [-.4, 0.0, 0.2])
+      pedal_offset = interp(CS.out.vEgo, [0.0, CREEP_SPEED, MIN_ACC_SPEED + PEDAL_TRANSITION], [-.4, 0.0, 0.2])
       pedal_command = PEDAL_SCALE * (actuators.accel + pedal_offset)
       interceptor_gas_cmd = clip(pedal_command, 0., MAX_INTERCEPTOR_GAS)
+      boost = 0.0
     else:
       interceptor_gas_cmd = 0.
-    pcm_accel_cmd = clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
+      # Toyotas don't respond to small accel requests when stationary
+      start_boost = interp(CS.out.vEgo, [0.0, CREEP_SPEED, 2*CREEP_SPEED], [0.6, 0.6, 0.0])
+      is_accelerating = interp(actuators.accel, [0.0, 0.2], [0.0, 1.0])
+      boost = start_boost * is_accelerating
+    pcm_accel_cmd = clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX) + boost
 
     # steer torque
     new_steer = int(round(actuators.steer * CarControllerParams.STEER_MAX))
