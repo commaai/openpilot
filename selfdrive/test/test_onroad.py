25723de9e2bcdb2f0edc5b85a3ad2e0271c2a1a1
Willem Melching <willem.melching@gmail.com>
2022-05-02 16:31:29 +0200

    lower loggerd cpu usage

--- selfdrive/test/test_onroad.py
+++ selfdrive/test/test_onroad.py
@@ -22,7 +22,7 @@ from tools.lib.logreader import LogReader
 # Baseline CPU usage by process
 PROCS = {
   "selfdrive.controls.controlsd": 31.0,
-  "./loggerd": 70.0,
+  "./loggerd": 50.0,
   "./camerad": 26.0,
   "./locationd": 9.1,
   "selfdrive.controls.plannerd": 11.7,
