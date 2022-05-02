fbdf6dbdaa6389dffebc937d1f6fadfa1b9829a2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-05-02 13:23:29 -0700

    UI: support point releases
    old-commit-hash: 792f046da5119201a0119c5213eacc3569f411cd

--- selfdrive/ui/qt/util.cc
+++ selfdrive/ui/qt/util.cc
@@ -18,7 +18,7 @@ QString getBrand() {
 }
 
 QString getBrandVersion() {
-  return getBrand() + " v" + getVersion().left(14).trimmed();
+  return getBrand() + " v" + getVersion().left(16).trimmed();
 }
 
 QString getUserAgent() {
