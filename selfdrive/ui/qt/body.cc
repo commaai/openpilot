429297c4bc329fba42a8af3914879914c593cc1b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2022-05-01 20:10:59 -0700

    eyes and mouth containers in grid

--- selfdrive/ui/qt/body.cc
+++ selfdrive/ui/qt/body.cc
@@ -5,33 +5,88 @@
 
 #include "selfdrive/ui/qt/body.h"
 
-BodyWindow::BodyWindow(QWidget *parent) : QWidget(parent) {
-  layout = new QGridLayout(this);
-  layout->setMargin(0);
-  layout->setSpacing(0);
 
-  //setAttribute(Qt::WA_TransparentForMouseEvents, true);
-
-  battery = new QLabel();
-  battery->setStyleSheet("QLabel { font-size: 200px; }");
-  battery->setAlignment(Qt::AlignTop | Qt::AlignRight);
-
-  layout->addWidget(battery,0,2);
-  //layout->addWidget(rightEye,0,2);
-  //layout->addWidget(leftEye,0,2);
-  //layout->addWidget(mouth,0,2);
-
-  QObject::connect(uiState(), &UIState::uiUpdate, this, &BodyWindow::updateState);
+FaceDotMatrix::FaceDotMatrix(QWidget* parent) : QWidget(parent) {
+  QPalette pal = QPalette();
+  pal.setColor(QPalette::Window, Qt::black);
+  setAutoFillBackground(true); 
+  setPalette(pal);
+  setAttribute(Qt::WA_TransparentForMouseEvents, true);
 }
 
-void BodyWindow::paintEvent(QPaintEvent *e) {
+void FaceDotMatrix::paintEvent(QPaintEvent *e) {
   QPainter painter(this);
-  QPen linepen(Qt::red);
+  QPen linepen(Qt::white);
   linepen.setCapStyle(Qt::RoundCap);
   linepen.setWidth(30);
   painter.setRenderHint(QPainter::Antialiasing,true);
   painter.setPen(linepen);
-  painter.drawPoint(200,200);
+  painter.drawPoint(width()/2, height()/2);
+}
+
+Eye::Eye(QWidget* parent) : FaceDotMatrix(parent) {
+}
+
+void Eye::paintEvent(QPaintEvent *e) {
+  QPainter painter(this);
+  QPen linepen(Qt::white);
+  linepen.setCapStyle(Qt::RoundCap);
+  linepen.setWidth(56);
+  painter.setRenderHint(QPainter::Antialiasing,true);
+  painter.setPen(linepen);
+  painter.drawPoint(width()/2+100, height()/2+100);
+}
+
+Mouth::Mouth(QWidget* parent) : FaceDotMatrix(parent) {
+}
+
+void Mouth::paintEvent(QPaintEvent *e) {
+  printf("%d %d\n", height(), width());
+  QPainter painter(this);
+  QPen linepen(Qt::black);
+  linepen.setCapStyle(Qt::RoundCap);
+  linepen.setWidth(30);
+  painter.setRenderHint(QPainter::Antialiasing,true);
+  painter.setPen(linepen);
+  painter.drawPoint(0,0);
+}
+
+
+BodyWindow::BodyWindow(QWidget *parent) : QWidget(parent) {
+  layout = new QGridLayout(this);
+
+  QPalette pal = QPalette();
+  pal.setColor(QPalette::Window, Qt::black);
+  setAutoFillBackground(true); 
+  setPalette(pal);
+
+  setAttribute(Qt::WA_TransparentForMouseEvents, true);
+
+  leftEye = new Eye(this);
+  rightEye = new Eye(this);
+  mouth = new Mouth(this);
+
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 0, 0);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 0, 1);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 0, 2);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 0, 3);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 0, 4);
+  
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 1, 0);
+  layout->addWidget(leftEye,1,1);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 1, 2);
+  layout->addWidget(rightEye,1,3);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 1, 4);
+
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 2, 0);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 2, 1);
+  layout->addWidget(mouth,2,2);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 2, 3);
+  layout->addItem(new QSpacerItem(1,1,QSizePolicy::Expanding,QSizePolicy::Expanding), 2, 4);
+  
+  
+
+  QObject::connect(uiState(), &UIState::uiUpdate, this, &BodyWindow::updateState);
 }
 
 void BodyWindow::updateState(const UIState &s) {
@@ -39,23 +94,8 @@ void BodyWindow::updateState(const UIState &s) {
     return;
   }
 
-  const SubMaster &sm = *(s.sm);
+  // const SubMaster &sm = *(s.sm);
 
   // TODO: use carState.standstill when that's fixed
   //const bool standstill = std::abs(sm["carState"].getCarState().getVEgo()) < 0.01;
-
-  battery->setText(generateBatteryText(sm["carState"].getCarState().getFuelGauge()));
-}
-
-QString BodyWindow::generateBatteryText(float fuelGauge) {
-  int batteryOkDots = round(5*fuelGauge);
-  int batteryDotsRemain = 5 - batteryOkDots;
-  QString batteryDotsText = "<font color=\"white\">";
-  for (int i=0; i<batteryOkDots; i++)
-    batteryDotsText+="·";
-  batteryDotsText+="</font><font color=\"grey\">";
-  for (int i=0; i<batteryDotsRemain; i++)
-    batteryDotsText+="·";
-  batteryDotsText+="</font>";
-  return batteryDotsText;
 }
--- selfdrive/ui/qt/body.h
+++ selfdrive/ui/qt/body.h
@@ -6,6 +6,37 @@
 
 #include "selfdrive/ui/ui.h"
 
+class FaceDotMatrix : public QWidget {
+  Q_OBJECT
+
+public:
+  FaceDotMatrix(QWidget* parent = 0);
+
+protected:
+  virtual void paintEvent(QPaintEvent *event);
+};
+
+
+class Eye : public FaceDotMatrix {
+  Q_OBJECT
+
+public:
+  Eye(QWidget* parent = 0);
+
+protected:
+  void paintEvent(QPaintEvent *event);
+};
+
+class Mouth : public FaceDotMatrix {
+  Q_OBJECT
+
+public:
+  Mouth(QWidget* parent = 0);
+
+protected:
+  void paintEvent(QPaintEvent *event);
+};
+
 class BodyWindow : public QWidget {
   Q_OBJECT
 
@@ -14,14 +45,9 @@ public:
 
 private:
   QGridLayout *layout;
-  QLabel *battery;
-
-  int batteryDots;
-
-  QString generateBatteryText(float fuelGauge);
-
-protected:
-  void paintEvent(QPaintEvent *event);
+  Eye *leftEye;
+  Eye *rightEye;
+  Mouth *mouth;
 
 private slots:
   void updateState(const UIState &s);
