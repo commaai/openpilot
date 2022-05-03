fecbd20e3df5d199242291794cc7a61efdb9f436
Shane Smiskol <shane@smiskol.com>
2022-05-02 19:17:09 -0700

    no multiline can creation

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -51,20 +51,17 @@ class CarController():
 
     if CC.cruiseControl.cancel:
       # TODO: would be better to start from frame_2b3
-      new_msg = create_wheel_buttons(self.packer, self.frame, cancel=True)
-      can_sends.append(new_msg)
+      can_sends.append(create_wheel_buttons(self.packer, self.frame, cancel=True))
 
     # LKAS_HEARTBIT is forwarded by Panda so no need to send it here.
     # frame is 100Hz (0.01s period)
     if self.frame % 25 == 0:  # 0.25s period
       if CS.lkas_car_model != -1:
-        new_msg = create_lkas_hud(self.packer, CS.out.gearShifter, lkas_active,
-                                  CC.hudControl.visualAlert, self.hud_count, CS.lkas_car_model)
-        can_sends.append(new_msg)
+        can_sends.append(create_lkas_hud(self.packer, CS.out.gearShifter, lkas_active,
+                                         CC.hudControl.visualAlert, self.hud_count, CS.lkas_car_model))
         self.hud_count += 1
 
-    new_msg = create_lkas_command(self.packer, int(apply_steer), self.gone_fast_yet, CS.lkas_counter)
-    can_sends.append(new_msg)
+    can_sends.append(create_lkas_command(self.packer, int(apply_steer), self.gone_fast_yet, CS.lkas_counter))
 
     self.frame += 1
     self.prev_lkas_frame = CS.lkas_counter
