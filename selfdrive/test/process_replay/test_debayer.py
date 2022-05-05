8dea381a772a79edcc6a3fbc2a700e21fc0928b3
Joost Wooning <jwooning@gmail.com>
2022-05-05 16:05:37 +0200

    test for comparing debayer results to ref commit (#24420)
    * test for comparing debayering results to ref commit
    
    * fix run on device
    
    * print more useful diff
    
    * limit to diff output
    
    * remove env var
    
    * ref
    
    * more useful diff
    
    * use real local work size
    
    * remove pyopencl install
    
    * update ref
    
    * include rgb_to_yuv
    
    * fix diff
    
    * get kernel build out of loop
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 9da9b773eba344e9e59aeaa8a2c0235aadbd1f44

---
+++ selfdrive/test/process_replay/test_debayer.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+import os
+import sys
+import bz2
+import numpy as np
+
+import pyopencl as cl  # install with `PYOPENCL_CL_PRETEND_VERSION=2.0 pip install pyopencl`
+
+from selfdrive.hardware import PC, TICI
+from common.basedir import BASEDIR
+from selfdrive.test.openpilotci import BASE_URL, get_url
+from selfdrive.version import get_commit
+from tools.lib.logreader import LogReader
+from tools.lib.filereader import FileReader
+
+TEST_ROUTE = "8345e3b82948d454|2022-05-04--13-45-33"
+SEGMENT = 0
+
+FRAME_WIDTH = 1928
+FRAME_HEIGHT = 1208
+FRAME_STRIDE = 2896
+
+UV_WIDTH = FRAME_WIDTH // 2
+UV_HEIGHT = FRAME_HEIGHT // 2
+UV_SIZE = UV_WIDTH * UV_HEIGHT
+
+def get_frame_fn(ref_commit, test_route, tici=True):
+  return f"{test_route}_debayer{'_tici' if tici else ''}_{ref_commit}.bz2"
+
+def bzip_frames(frames):
+  data = bytes()
+  for y, u, v in frames:
+    data += y.tobytes()
+    data += u.tobytes()
+    data += v.tobytes()
+  return bz2.compress(data)
+
+def unbzip_frames(url):
+  with FileReader(url) as f:
+    dat = f.read()
+
+  data = bz2.decompress(dat)
+
+  res = []
+  for y_start in range(0, len(data), FRAME_WIDTH * FRAME_HEIGHT + UV_SIZE * 2):
+    u_start = y_start + FRAME_WIDTH * FRAME_HEIGHT
+    v_start = u_start + UV_SIZE
+
+    y = np.frombuffer(data[y_start: u_start], dtype=np.uint8).reshape((FRAME_HEIGHT, FRAME_WIDTH))
+    u = np.frombuffer(data[u_start: v_start], dtype=np.uint8).reshape((UV_HEIGHT, UV_WIDTH))
+    v = np.frombuffer(data[v_start: v_start + UV_SIZE], dtype=np.uint8).reshape((UV_HEIGHT, UV_WIDTH))
+
+    res.append((y, u, v))
+
+  return res
+
+def init_kernels():
+  ctx = cl.create_some_context()
+
+  with open(os.path.join(BASEDIR, 'selfdrive/camerad/cameras/real_debayer.cl')) as f:
+    build_args = ' -cl-fast-relaxed-math -cl-denorms-are-zero -cl-single-precision-constant' + \
+      f' -DFRAME_STRIDE={FRAME_STRIDE} -DRGB_WIDTH={FRAME_WIDTH} -DRGB_HEIGHT={FRAME_HEIGHT} -DCAM_NUM=0'
+    if PC:
+      build_args += ' -DHALF_AS_FLOAT=1'
+    debayer_prg = cl.Program(ctx, f.read()).build(options=build_args)
+
+  with open(os.path.join(BASEDIR, 'selfdrive/camerad/transforms/rgb_to_yuv.cl')) as f:
+    build_args = f' -cl-fast-relaxed-math -cl-denorms-are-zero -DWIDTH={FRAME_WIDTH} -DHEIGHT={FRAME_HEIGHT}' + \
+      f' -DUV_WIDTH={UV_WIDTH} -DUV_HEIGHT={UV_HEIGHT} -DRGB_STRIDE={FRAME_WIDTH*3}' + \
+      f' -DRGB_SIZE={FRAME_WIDTH*FRAME_HEIGHT}'
+    rgb_to_yuv_prg = cl.Program(ctx, f.read()).build(options=build_args)
+
+  return ctx, debayer_prg, rgb_to_yuv_prg
+
+def debayer_frame(ctx, debayer_prg, rgb_to_yuv_prg, data):
+  q = cl.CommandQueue(ctx)
+
+  rgb_buff = np.empty(FRAME_WIDTH * FRAME_HEIGHT * 3, dtype=np.uint8)
+  yuv_buff = np.empty(FRAME_WIDTH * FRAME_HEIGHT + UV_SIZE * 2, dtype=np.uint8)
+
+  cam_g = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=data)
+  rgb_wg = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, FRAME_WIDTH * FRAME_HEIGHT * 3)
+  yuv_g = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, FRAME_WIDTH * FRAME_HEIGHT + UV_SIZE * 2)
+
+  local_worksize = (16, 16) if TICI else (8, 8)
+  local_mem = cl.LocalMemory(648 if TICI else 400)
+  ev1 = debayer_prg.debayer10(q, (FRAME_WIDTH, FRAME_HEIGHT), local_worksize, cam_g, rgb_wg, local_mem, np.float32(42))
+  cl.enqueue_copy(q, rgb_buff, rgb_wg, wait_for=[ev1]).wait()
+  cl.enqueue_barrier(q)
+
+  rgb_rg = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=rgb_buff)
+  cl.enqueue_barrier(q)
+
+  ev3 = rgb_to_yuv_prg.rgb_to_yuv(q, (FRAME_WIDTH // 4, FRAME_HEIGHT // 4), None, rgb_rg, yuv_g)
+  cl.enqueue_barrier(q)
+
+  cl.enqueue_copy(q, yuv_buff, yuv_g, wait_for=[ev3]).wait()
+  cl.enqueue_barrier(q)
+
+  y = yuv_buff[:FRAME_WIDTH*FRAME_HEIGHT].reshape((FRAME_HEIGHT, FRAME_WIDTH))
+  u = yuv_buff[FRAME_WIDTH*FRAME_HEIGHT:FRAME_WIDTH*FRAME_HEIGHT+UV_SIZE].reshape((UV_HEIGHT, UV_WIDTH))
+  v = yuv_buff[FRAME_WIDTH*FRAME_HEIGHT+UV_SIZE:].reshape((UV_HEIGHT, UV_WIDTH))
+
+  return y, u, v
+
+def debayer_replay(lr):
+  ctx, debayer_prg, rgb_to_yuv_prg = init_kernels()
+
+  frames = []
+  for m in lr:
+    if m.which() == 'roadCameraState':
+      cs = m.roadCameraState
+      if cs.image:
+        data = np.frombuffer(cs.image, dtype=np.uint8)
+        img = debayer_frame(ctx, debayer_prg, rgb_to_yuv_prg, data)
+
+        frames.append(img)
+
+  return frames
+
+if __name__ == "__main__":
+  update = "--update" in sys.argv
+  replay_dir = os.path.dirname(os.path.abspath(__file__))
+  ref_commit_fn = os.path.join(replay_dir, "debayer_replay_ref_commit")
+
+  # load logs
+  lr = list(LogReader(get_url(TEST_ROUTE, SEGMENT)))
+
+  # run replay
+  frames = debayer_replay(lr)
+
+  # get diff
+  failed = False
+  diff = ''
+  yuv_i = ['y', 'u', 'v']
+  if not update:
+    with open(ref_commit_fn) as f:
+      ref_commit = f.read().strip()
+    frame_fn = get_frame_fn(ref_commit, TEST_ROUTE, tici=TICI)
+
+    try:
+      cmp_frames = unbzip_frames(BASE_URL + frame_fn)
+
+      if len(frames) != len(cmp_frames):
+        failed = True
+        diff += 'amount of frames not equal\n'
+
+      for i, (frame, cmp_frame) in enumerate(zip(frames, cmp_frames)):
+        for j in range(3):
+          fr = frame[j]
+          cmp_f = cmp_frame[j]
+          if fr.shape != cmp_f.shape:
+            failed = True
+            diff += f'frame shapes not equal for ({i}, {yuv_i[j]})\n'
+            diff += f'{ref_commit}: {cmp_f.shape}\n'
+            diff += f'HEAD: {fr.shape}\n'
+          elif not np.array_equal(fr, cmp_f):
+            failed = True
+            if np.allclose(fr, cmp_f, atol=1):
+              diff += f'frames not equal for ({i}, {yuv_i[j]}), but are all close\n'
+            else:
+              diff += f'frames not equal for ({i}, {yuv_i[j]})\n'
+
+            frame_diff = np.abs(np.subtract(fr, cmp_f))
+            diff_len = len(np.nonzero(frame_diff)[0])
+            if diff_len > 1000:
+              diff += f'different at a large amount of pixels ({diff_len})\n'
+            else:
+              diff += 'different at (frame, yuv, pixel, ref, HEAD):\n'
+              for k in zip(*np.nonzero(frame_diff)):
+                diff += f'{i}, {yuv_i[j]}, {k}, {cmp_f[k]}, {fr[k]}\n'
+
+      if failed:
+        print(diff)
+        with open("debayer_diff.txt", "w") as f:
+          f.write(diff)
+    except Exception as e:
+      print(str(e))
+      failed = True
+
+  # upload new refs
+  if update or failed:
+    from selfdrive.test.openpilotci import upload_file
+
+    print("Uploading new refs")
+
+    frames_bzip = bzip_frames(frames)
+
+    new_commit = get_commit()
+    frame_fn = os.path.join(replay_dir, get_frame_fn(new_commit, TEST_ROUTE, tici=TICI))
+    with open(frame_fn, "wb") as f2:
+      f2.write(frames_bzip)
+
+    try:
+      upload_file(frame_fn, os.path.basename(frame_fn))
+    except Exception as e:
+      print("failed to upload", e)
+
+  if update:
+    with open(ref_commit_fn, 'w') as f:
+      f.write(str(new_commit))
+
+    print("\nNew ref commit: ", new_commit)
+
+  sys.exit(int(failed))
