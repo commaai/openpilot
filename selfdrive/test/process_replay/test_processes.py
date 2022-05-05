07aa0a1c83f5d924272371d92bb36c0a961d9b55
Shane Smiskol <shane@smiskol.com>
2022-05-04 20:55:05 -0700

    clean up and make a diff

--- selfdrive/test/process_replay/test_processes.py
+++ selfdrive/test/process_replay/test_processes.py
@@ -32,19 +32,19 @@ original_segments = [
 ]
 
 segments = [
-  # ("BODY", "bd6a637565e91581|2022-04-04--22-05-08--0"),
-  # ("HYUNDAI", "fakedata|2022-01-20--17-49-04--0"),
-  # ("TOYOTA", "fakedata|2022-04-29--15-57-12--0"),
-  # ("TOYOTA2", "fakedata|2022-04-29--16-08-01--0"),
+  ("BODY", "bd6a637565e91581|2022-04-04--22-05-08--0"),
+  ("HYUNDAI", "fakedata|2022-01-20--17-49-04--0"),
+  ("TOYOTA", "fakedata|2022-04-29--15-57-12--0"),
+  ("TOYOTA2", "fakedata|2022-04-29--16-08-01--0"),
   ("TOYOTA3", "fakedata|2022-04-29--16-17-39--0"),
-  # ("HONDA", "fakedata|2022-01-20--17-56-40--0"),
-  # ("HONDA2", "fakedata|2022-04-29--16-31-55--0"),
-  # ("CHRYSLER", "fakedata|2022-01-20--18-00-11--0"),
-  # ("SUBARU", "fakedata|2022-01-20--18-01-57--0"),
-  # ("GM", "fakedata|2022-01-20--18-03-41--0"),
-  # ("NISSAN", "fakedata|2022-01-20--18-05-29--0"),
-  # ("VOLKSWAGEN", "fakedata|2022-01-20--18-07-15--0"),
-  # ("MAZDA", "fakedata|2022-01-20--18-09-32--0"),
+  ("HONDA", "fakedata|2022-01-20--17-56-40--0"),
+  ("HONDA2", "fakedata|2022-04-29--16-31-55--0"),
+  ("CHRYSLER", "fakedata|2022-01-20--18-00-11--0"),
+  ("SUBARU", "fakedata|2022-01-20--18-01-57--0"),
+  ("GM", "fakedata|2022-01-20--18-03-41--0"),
+  ("NISSAN", "fakedata|2022-01-20--18-05-29--0"),
+  ("VOLKSWAGEN", "fakedata|2022-01-20--18-07-15--0"),
+  ("MAZDA", "fakedata|2022-01-20--18-09-32--0"),
 ]
 
 BASE_URL = "https://commadataci.blob.core.windows.net/openpilotci/"
@@ -52,7 +52,7 @@ BASE_URL = "https://commadataci.blob.core.windows.net/openpilotci/"
 # dashcamOnly makes don't need to be tested until a full port is done
 excluded_interfaces = ["mock", "ford", "mazda", "tesla"]
 # run the full test (including checks) when no args given
-FULL_TEST = False  # len(sys.argv) <= 1 or CI
+FULL_TEST = len(sys.argv) <= 1
 
 
 def test_process(cfg, lr, cmp_log_fn, ignore_fields=None, ignore_msgs=None):
@@ -134,12 +134,12 @@ if __name__ == "__main__":
   try:
     ref_commit = open(os.path.join(PROC_REPLAY_DIR, "ref_commit")).read().strip()
   except FileNotFoundError:
-    print("couldn't find reference commit")
+    print("Couldn't find reference commit")
     sys.exit(1)
 
   cur_commit = get_commit()
   if cur_commit is None:
-    raise Exception("couldn't get current commit")
+    raise Exception("Couldn't get current commit")
 
   print(f"***** testing against commit {ref_commit} *****")
 
