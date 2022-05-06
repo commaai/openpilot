b2585a3f560b0a1ca8fac493d80a2aaf60e08cdb
Harald Schafer <harald.the.engineer@gmail.com>
2022-05-05 22:05:46 -0700

    Update heading cost

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -59,9 +59,11 @@ class LateralPlanner:
     # Calculate final driving path and set MPC costs
     if self.use_lanelines:
       d_path_xyz = self.LP.get_d_path(v_ego, self.t_idxs, self.path_xyz)
+      heading_cost = MPC_COST_LAT.HEADING
     else:
       d_path_xyz = self.path_xyz
-    self.lat_mpc.set_weights(MPC_COST_LAT.PATH, 0.15 * MPC_COST_LAT.HEADING, self.steer_rate_cost)
+      heading_cost = interp(v_ego, [5.0, 10.0], [MPC_COST_LAT.HEADING, 0.15])
+    self.lat_mpc.set_weights(MPC_COST_LAT.PATH, heading_cost, self.steer_rate_cost)
 
     y_pts = np.interp(v_ego * self.t_idxs[:LAT_MPC_N + 1], np.linalg.norm(d_path_xyz, axis=1), d_path_xyz[:, 1])
     heading_pts = np.interp(v_ego * self.t_idxs[:LAT_MPC_N + 1], np.linalg.norm(self.path_xyz, axis=1), self.plan_yaw)
