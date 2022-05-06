c2145e408d30e23bcaafe8e1ffb6d7235cf292e4
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-05-05 19:45:25 -0700

    use os module's xattr function

--- selfdrive/loggerd/xattr_cache.py
+++ selfdrive/loggerd/xattr_cache.py
@@ -1,15 +1,13 @@
+import os
 from typing import Dict, Tuple
-
-from common.xattr import getxattr as getattr1
-from common.xattr import setxattr as setattr1
 
 cached_attributes: Dict[Tuple, bytes] = {}
 def getxattr(path: str, attr_name: str) -> bytes:
   if (path, attr_name) not in cached_attributes:
-    response = getattr1(path, attr_name)
+    response = os.getxattr(path, attr_name)
     cached_attributes[(path, attr_name)] = response
   return cached_attributes[(path, attr_name)]
 
 def setxattr(path: str, attr_name: str, attr_value: bytes) -> None:
   cached_attributes.pop((path, attr_name), None)
-  return setattr1(path, attr_name, attr_value)
+  return os.setxattr(path, attr_name, attr_value)
