191fbd4f67e7f878b518effd14411779b9435339
Shane Smiskol <shane@smiskol.com>
2022-05-07 21:34:17 -0700

    Camry TSS2: use torque controller (#24268)
    * use measurements from harald
    
    * update from table
    
    * average of hybrid and non-hybrid

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -87,7 +87,10 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.7
       tire_stiffness_factor = 0.7933
       ret.mass = 3400. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
+      if candidate in (CAR.CAMRY_TSS2, CAR.CAMRYH_TSS2):
+        set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=2.4, FRICTION=0.05)
+      else:
+        set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
 
     elif candidate in (CAR.HIGHLANDER_TSS2, CAR.HIGHLANDERH_TSS2):
       stop_and_go = True
