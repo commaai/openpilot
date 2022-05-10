edce65c64e53f73ba4fa01a0a8d8a9044cad0603
gast04 <kurt.nistelberger@gmail.com>
2022-05-10 05:26:18 -0700

    change LSM gyro to interrupt

--- selfdrive/sensord/sensors/lsm6ds3_gyro.h
+++ selfdrive/sensord/sensors/lsm6ds3_gyro.h
@@ -6,21 +6,26 @@
 #define LSM6DS3_GYRO_I2C_ADDR       0x6A
 
 // Registers of the chip
-#define LSM6DS3_GYRO_I2C_REG_ID        0x0F
-#define LSM6DS3_GYRO_I2C_REG_CTRL2_G   0x11
-#define LSM6DS3_GYRO_I2C_REG_OUTX_L_G  0x22
+#define LSM6DS3_GYRO_I2C_REG_ID         0x0F
+#define LSM6DS3_ACCEL_I2C_REG_INT1_CTRL 0x0D
+#define LSM6DS3_GYRO_I2C_REG_CTRL2_G    0x11
+#define LSM6DS3_ACCEL_I2C_REG_STAT_REG  0x1E
+#define LSM6DS3_GYRO_I2C_REG_OUTX_L_G   0x22
 
 // Constants
 #define LSM6DS3_GYRO_CHIP_ID        0x69
 #define LSM6DS3TRC_GYRO_CHIP_ID     0x6A
 #define LSM6DS3_GYRO_ODR_104HZ      (0b0100 << 4)
+#define LSM6DS3_ACCEL_INT1_DRDY_G   0b10
+#define LSM6DS3_ACCEL_DRDY_GDA      0b10
 
 
 class LSM6DS3_Gyro : public I2CSensor {
+  int gpio_nr;
   uint8_t get_device_address() {return LSM6DS3_GYRO_I2C_ADDR;}
   cereal::SensorEventData::SensorSource source = cereal::SensorEventData::SensorSource::LSM6DS3;
 public:
-  LSM6DS3_Gyro(I2CBus *bus);
+  LSM6DS3_Gyro(I2CBus *bus, int gpio_nr);
   int init();
   void get_event(cereal::SensorEventData::Builder &event);
 };
