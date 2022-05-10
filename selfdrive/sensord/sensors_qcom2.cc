255f22f478fa414ecb4efeb864f5174bd100d984
gast04 <kurt.nistelberger@gmail.com>
2022-05-10 08:03:42 -0700

    add interrupt collector thread to fetch in parallel

--- selfdrive/sensord/sensors_qcom2.cc
+++ selfdrive/sensord/sensors_qcom2.cc
@@ -8,7 +8,6 @@
 
 // TODO: check if really needed
 #include <poll.h>
-#include <fcntl.h>
 
 #include "cereal/messaging/messaging.h"
 #include "common/i2c.h"
@@ -120,8 +119,8 @@ int sensor_loop() {
     return -1;
   }
 
-  BMX055_Accel bmx055_accel(i2c_bus_imu);
-  BMX055_Gyro bmx055_gyro(i2c_bus_imu);
+  BMX055_Accel bmx055_accel(i2c_bus_imu, 21);
+  BMX055_Gyro bmx055_gyro(i2c_bus_imu, 23);
   BMX055_Magn bmx055_magn(i2c_bus_imu);
   BMX055_Temp bmx055_temp(i2c_bus_imu);
 
@@ -135,23 +134,24 @@ int sensor_loop() {
 
   // Sensor init
   std::vector<std::pair<Sensor *, bool>> sensors_init; // Sensor, required
-  //sensors_init.push_back({&bmx055_accel, false});
-  //sensors_init.push_back({&bmx055_gyro, false});
+  sensors_init.push_back({&bmx055_accel, false});
+  sensors_init.push_back({&bmx055_gyro, false});
   sensors_init.push_back({&bmx055_magn, false});
-  //sensors_init.push_back({&bmx055_temp, false});
+  sensors_init.push_back({&bmx055_temp, false}); // TODO: read with interrupt gyro
 
-  //sensors_init.push_back({&lsm6ds3_accel, true});
-  //sensors_init.push_back({&lsm6ds3_gyro, true});
-  //sensors_init.push_back({&lsm6ds3_temp, true});
+  sensors_init.push_back({&lsm6ds3_accel, true});
+  sensors_init.push_back({&lsm6ds3_gyro, true});
+  sensors_init.push_back({&lsm6ds3_temp, true}); // TODO: read with interrupt gyro
 
-  //sensors_init.push_back({&mmc5603nj_magn, false});
+  sensors_init.push_back({&mmc5603nj_magn, false});
 
-  //sensors_init.push_back({&light, true});
+  sensors_init.push_back({&light, true});
 
   bool has_magnetometer = false;
 
   // Initialize sensors
   std::vector<Sensor *> sensors;
+  std::vector<Sensor *> interrupt_sensors;
   for (auto &sensor : sensors_init) {
     int err = sensor.first->init();
     if (err < 0) {
@@ -194,54 +194,11 @@ int sensor_loop() {
   while (!do_exit) {
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
+  // polling loop for non interrupt handled sensors
+  while (!do_exit) {
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-  const int num_events = sensors.size();
-
-  struct pollfd fdlist[3];
-
-  // assumed to be configured as exported, defined as input and trigger on raising edge
-  // TODO: cleanUp but works for now
-  int fd_accel = open("/sys/class/gpio/gpio21/value", O_RDONLY);
-  if (fd_accel < 0) { LOGE("FD ACCEL failed"); return 0; }
-  int fd_gyro  = open("/sys/class/gpio/gpio23/value", O_RDONLY);
-  if (fd_gyro < 0)  { LOGE("FD GYRO failed"); return 0; }
-  int fd_magn  = open("/sys/class/gpio/gpio87/value", O_RDONLY);
-  if (fd_magn < 0)  { LOGE("FD MAGN failed"); return 0; }
-
-  fdlist[0].fd = fd_accel;
-  fdlist[0].events = POLLPRI;
-
-  fdlist[1].fd = fd_gyro;
-  fdlist[1].events = POLLPRI;
-
-  fdlist[2].fd = fd_magn;
-  fdlist[2].events = POLLPRI;
-
-  uint64_t start_time = nanos_since_boot();
-  uint64_t f_avg = 0;
-  uint64_t a_cnt = 0;
-
-  while (1) {
-    int err;
-
-    /* events are received at 2kHz frequency, the bandwidth devider returns in the case of 125Hz
-       7 times the same data, this needs to be filtered or handled smart */
-    err = poll(fdlist, 3, -1);
-    if (-1 == err) {
-      return -1;
-    }
-
-    // TODO: check which interrupt triggered using revents
-
-    // timing measurement
-    uint64_t int_time = nanos_since_boot();
-    f_avg += int_time - start_time;
-    a_cnt++;
-    LOGE("t: %lu, %lu", int_time - start_time, f_avg/a_cnt);
-    start_time = int_time;
-
-    // we dont read from the fd as its reset automatically after 50us
-    // so we directly create the message and publish it
+    const int num_events = sensors.size();
     MessageBuilder msg;
     auto sensor_events = msg.initEvent().initSensorEvents(num_events);
 
