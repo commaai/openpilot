c643d7460938e0dee04e3578670f0d523cb2f4ba
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-05-12 22:08:04 -0700

    jenkins: workaround lfs bug

--- selfdrive/test/setup_device_ci.sh
+++ selfdrive/test/setup_device_ci.sh
@@ -55,15 +55,16 @@ cd $SOURCE_DIR
 
 rm -f .git/index.lock
 git reset --hard
-find . -maxdepth 1 -not -path './.git' -not -name '.' -not -name '..' -exec rm -rf '{}' \;
 git fetch --verbose origin $GIT_COMMIT
+find . -maxdepth 1 -not -path './.git' -not -name '.' -not -name '..' -exec rm -rf '{}' \;
 git reset --hard $GIT_COMMIT
 git checkout $GIT_COMMIT
 git clean -xdf
 git submodule update --init --recursive
 git submodule foreach --recursive "git reset --hard && git clean -xdf"
 
-git lfs prune
+git lfs pull
+(ulimit -n 65535 && git lfs prune)
 
 echo "git checkout done, t=$SECONDS"
 
