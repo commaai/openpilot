76c306a4266d8730e30c1f8ba7a03a25226e314e
Willem Melching <willem.melching@gmail.com>
2022-05-13 15:55:48 +0200

    linter fixes

--- selfdrive/debug/sensor_data_to_hist.py
+++ selfdrive/debug/sensor_data_to_hist.py
@@ -1,3 +1,9 @@
+'''
+printing the gap between interrupts in a histogram to check if the
+frequency is what we expect, the bmx is not interrup driven for as we
+get interrupts in a 2kHz rate.
+'''
+
 import argparse
 import sys
 from tools.lib.logreader import LogReader
@@ -7,47 +13,42 @@ import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
-'''
-printing the gap between interrupts in a histogram to check if the
-frequency is what we expect, the bmx is not interrup driven for as we
-get interrupts in a 2kHz rate.
-'''
 
 SRC_BMX = "bmx055"
 SRC_LSM = "lsm6ds3trc"
 
 def parseEvents(log_reader):
   bmx_data = {
-    "accel": [], 
+    "accel": [],
     "gyro":  []
-  } 
-  
+  }
+
   lsm_data = {
-    "accel": [], 
+    "accel": [],
     "gyro":  []
-  } 
+  }
 
   for m in log_reader:
     # only sensorEvents
     if m.which() != 'sensorEvents':
       continue
-  
+
     for se in m.sensorEvents:
       # convert data to dictionary
       d = se.to_dict()
-      
+
       if d["timestamp"] == 0:
         continue # empty event?
 
       if d["source"] == SRC_BMX and "acceleration" in d:
         bmx_data["accel"].append(d["timestamp"])
-  
+
       if d["source"] == SRC_BMX and "gyroUncalibrated" in d:
         bmx_data["gyro"].append(d["timestamp"])
-  
+
       if d["source"] == SRC_LSM and "acceleration" in d:
         lsm_data["accel"].append(d["timestamp"])
-  
+
       if d["source"] == SRC_LSM and "gyroUncalibrated" in d:
         lsm_data["gyro"].append(d["timestamp"])
 
@@ -89,11 +90,11 @@ if __name__ == "__main__":
   r = Route(route)
   #logs = [q_log if r_log is None else r_log for (q_log, r_log) in zip(r.qlog_paths(), r.log_paths())]
   logs = r.log_paths()
- 
+
   if len(logs) == 0:
     print("NO data routes")
     sys.exit(0)
-  
+
   route_id = args.route_id[0]
   if route_id > len(logs):
     print(f"RouteID: {route_id} out of range, max: {len(logs) -1}")
@@ -107,25 +108,25 @@ if __name__ == "__main__":
   bmx_gyro, bmx_gyro_diffs = cleanData(bmx_data["gyro"])
   lsm_accel, lsm_accel_diffs = cleanData(lsm_data["accel"])
   lsm_gyro, lsm_gyro_diffs = cleanData(lsm_data["gyro"])
-  
+
   # get out the averages
   logAvgValues(bmx_accel_diffs, "bmx accel")
   logAvgValues(bmx_gyro_diffs,  "bmx gyro ")
   logAvgValues(lsm_accel_diffs, "lsm accel")
   logAvgValues(lsm_gyro_diffs,  "lsm gyro ")
-  
+
   fig, axs = plt.subplots(1, 2, tight_layout=True)
   axs[0].hist(bmx_accel_diffs, bins=50)
   axs[0].set_title("bmx_accel")
   axs[1].hist(bmx_gyro_diffs,  bins=50)
   axs[1].set_title("bmx_gyro")
-  
+
   figl, axsl = plt.subplots(1, 2, tight_layout=True)
   axsl[0].hist(lsm_accel_diffs, bins=50)
   axsl[0].set_title("lsm_accel")
   axsl[1].hist(lsm_gyro_diffs,  bins=50)
   axsl[1].set_title("lsm_gyro")
-  
+
   print("check plot...")
   plt.show()
 
