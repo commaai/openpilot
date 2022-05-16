b0ada25e73385a8be0369acd50977b8631cac25d
Willem Melching <willem.melching@gmail.com>
2022-05-16 16:26:44 +0200

    Apply suggestions from code review

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -263,8 +263,8 @@ def out_of_space_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool,
 def overheat_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool, soft_disable_time: int) -> Alert:
   cpu = max(sm['deviceState'].cpuTempC, default=0.)
   gpu = max(sm['deviceState'].gpuTempC, default=0.)
-  temp = round(max((cpu, gpu, sm['deviceState'].memoryTempC)))
-  return NormalPermanentAlert("System Overheated", f"{temp} °C")
+  temp = max((cpu, gpu, sm['deviceState'].memoryTempC))
+  return NormalPermanentAlert("System Overheated", f"{temp:.0f} °C")
 
 
 def low_memory_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool, soft_disable_time: int) -> Alert:
