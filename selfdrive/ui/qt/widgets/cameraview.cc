5f0c1fca8036eb42838d28efcb9f07ba5949b36e
Shane Smiskol <shane@smiskol.com>
2022-05-21 00:02:25 -0700

    higher warmup

--- selfdrive/ui/qt/widgets/cameraview.cc
+++ selfdrive/ui/qt/widgets/cameraview.cc
@@ -223,17 +223,17 @@ void CameraViewWidget::paintGL() {
 //  qDebug() << "Cam frame id new:  " << (prev_cam_frame_id < cam_frame_id);
 
   if (draw_frame_id != cam_frame_id) {
-//    qDebug() << "WRROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONG FRAME!";
+    qDebug() << "WRROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONG FRAME!";
     wrong_frame += 1;
   } else {
     wrong_frame = std::max(wrong_frame - 1, 0);
   }
 
   // warm up of 1 second for all asserts
-  assert((draw_frame_id > prev_model_frame_id) || (cam_frame_id < (5 * 20)));  // never should draw duplicate model frames
-  assert((cam_frame_id >= prev_cam_frame_id) || (cam_frame_id < (5 * 20)));  // can draw same camera frames sometimes
+  assert((draw_frame_id > prev_model_frame_id) || (cam_frame_id < (10 * 20)));  // never should draw duplicate model frames
+  assert((cam_frame_id >= prev_cam_frame_id) || (cam_frame_id < (10 * 20)));  // can draw same camera frames sometimes
   // ok if drawing on same frame or if during startup
-  assert((draw_frame_id == cam_frame_id) || (cam_frame_id < (5 * 20)));
+  assert((draw_frame_id == cam_frame_id) || (cam_frame_id < (10 * 20)));
   // this allows touches, but you don't know if user touched, or we're just randomly drawing wrong frames
   // assert((draw_frame_id == cam_frame_id) || (draw_frame_id < 5) || ((wrong_frame <= 3)));
 
