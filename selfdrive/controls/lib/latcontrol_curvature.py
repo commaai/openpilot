b7466b4c62673337c89c51c11d611639e8ada3e5
Cameron Clough <cameronjclough@gmail.com>
2022-05-23 00:09:09 +0100

    calculate path angle in curvature controller

--- selfdrive/controls/lib/latcontrol_curvature.py
+++ selfdrive/controls/lib/latcontrol_curvature.py
@@ -10,24 +10,28 @@ class LatControlCurvature(LatControl):
 
     if CS.vEgo < MIN_STEER_SPEED or not active:
       curvature_log.active = False
-      curvature = -VM.calc_curvature(math.radians(CS.steeringAngleDeg), CS.vEgo, params.roll)
-      curvature_rate = 0
-      path_angle = 0
-      path_deviation = 0
+      curvature = 0.0
+      curvature_rate = 0.0
+      path_angle = 0.0
+      path_deviation = 0.0
     else:
       curvature_log.active = True
 
       curvatures = lat_plan.curvatures
       path_points = lat_plan.dPathPoints
 
+      position = model_v2.position
+
       # "road/lane curvature" is different from the immediate manoeuvre/path curvature
       # later curvature values are probably closer to this "road curvature" value
       curvature = -curvatures[6]
       path_deviation = -path_points[0] if len(path_points) > 0 else 0
 
+      # calculate the angle of the path (t = approx 1s)
+      path_angle = math.atan(-position.y[10] / position.x[10])
+
       # TODO
       curvature_rate = 0
-      path_angle = 0
 
     # TODO: calculate saturated, like latcontrol_angle
     curvature_log.saturated = False
