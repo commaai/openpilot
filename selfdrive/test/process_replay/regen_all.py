8c56bb9d638d871d6f05e3842f523704d1eb6983
Lukas Petersson <lukas.petersson.1999@gmail.com>
2022-05-23 14:44:49 -0700

    regen in parallel

--- selfdrive/test/process_replay/regen_all.py
+++ selfdrive/test/process_replay/regen_all.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
+import argparse
+import concurrent.futures
+
 from selfdrive.test.process_replay.regen import regen_and_save
 from selfdrive.test.process_replay.test_processes import original_segments as segments
 
-if __name__ == "__main__":
-  new_segments = []
-  for segment in segments:
-    route = segment[1].rsplit('--', 1)[0]
-    sidx = int(segment[1].rsplit('--', 1)[1])
-    print("Regen", route, sidx)
-    relr = regen_and_save(route, sidx, upload=True, use_route_meta=False)
+def regen_job(segment):
+  route = segment[1].rsplit('--', 1)[0]
+  sidx = int(segment[1].rsplit('--', 1)[1])
+  msg = "Regen  " + route + "  " + sidx + "\n"
+  relr = regen_and_save(route, sidx, upload=True, use_route_meta=False)
+  msg += "\n\n  " + "*"*30 + "  \n\n" + "New route:  " + relr
+  print(msg)
+  relr = relr.replace('/', '|')
+  return f'  ("{segment[0]}", "{relr}"), '
 
-    print("\n\n", "*"*30, "\n\n")
-    print("New route:", relr, "\n")
-    relr = relr.replace('/', '|')
-    new_segments.append(f'  ("{segment[0]}", "{relr}"), ')
-  print()
-  print()
-  print()
-  print('COPY THIS INTO test_processes.py')
-  for seg in new_segments:
-    print(seg)
+if __name__ == "__main__":
+  #TODO description
+  parser = argparse.ArgumentParser(description="")
+  parser.add_argument("-j", "--jobs", type=int, default=1)
+  args = parser.parse_args()
+  with concurrent.futures.ProcessPoolExecutor(max_workers=args.jobs) as pool:
+    p1 = pool.map(regen_job, segments)
+    print()
+    print()
+    print()
+    print('COPY THIS INTO test_processes.py')
+    for seg in p1:
+      print(seg)
