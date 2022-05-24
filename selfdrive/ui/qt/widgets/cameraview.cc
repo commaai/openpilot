2fa26a0ebc1d917c92e5228f2c7a881c974b23ae
Comma Device <device@comma.ai>
2022-05-24 14:23:13 -0700

    simpler and working

--- selfdrive/ui/qt/widgets/cameraview.cc
+++ selfdrive/ui/qt/widgets/cameraview.cc
@@ -50,7 +50,7 @@ const char frame_fragment_shader[] =
   "  float r = y + 1.402 * v;\n"
   "  float g = y - 0.344 * u - 0.714 * v;\n"
   "  float b = y + 1.772 * u;\n"
-  "  colorOut = vec4(r, g, vTexCoord.r*0.3, 1.0);\n"
+  "  colorOut = vec4(r, g, b, 1.0);\n"
   "}\n";
 
 const mat4 device_transform = {{
@@ -224,35 +224,10 @@ void CameraViewWidget::paintGL() {
 
   glUseProgram(program->programId());
 
-  //assert(eglDisplayExtensionEnabled(eglGetCurrentDisplay(), "EGL_KHR_gl_texture_2D_image"));
-  int i = 0; {
-    glActiveTexture(GL_TEXTURE0 + i);
-    //glBindTexture(GL_TEXTURE_2D, textures[i]);
-    //glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, latest_frame->egl_image);
-    #define GL_TEXTURE_EXTERNAL_OES           0x8D65
-    #define USE_TEX GL_TEXTURE_2D
-    glBindTexture(USE_TEX, textures[i]);
-    glEGLImageTargetTexture2DOES(USE_TEX, latest_frame->egl_image);
-    int w=-1,h=-1,fmt=-1,d=-1;
-    glGetTexLevelParameteriv(USE_TEX, 0, GL_TEXTURE_WIDTH, &w);
-    glGetTexLevelParameteriv(USE_TEX, 0, GL_TEXTURE_HEIGHT, &h);
-    glGetTexLevelParameteriv(USE_TEX, 0, GL_TEXTURE_DEPTH, &d);
-    glGetTexLevelParameteriv(USE_TEX, 0, GL_TEXTURE_INTERNAL_FORMAT, &fmt);
-    printf("%dx%dx%d fmt:%d\n", w, h, d, fmt);
-
-    glTexParameteri(USE_TEX, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(USE_TEX, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(USE_TEX, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameterf(USE_TEX, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    /*int w=-1, h=-1, fmt, rs=-1, gs=-1, bs=-1, sz;
-    glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_BUFFER_SIZE, &sz);
-    glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_INTERNAL_FORMAT, &fmt);
-    glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &w);
-    glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &h);
-    printf("%dx%d fmt:%d rs:%x gs:%x bs:%x  sz:%d\n", w, h, fmt, rs, gs, bs, sz);*/
-    //printf("bind image %p error %x\n", latest_frame->egl_image, glGetError());
-    assert(glGetError() == GL_NO_ERROR);
-  }
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, textures[0]);
+  glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, latest_frame->egl_image);
+  assert(glGetError() == GL_NO_ERROR);
 
   glUniformMatrix4fv(program->uniformLocation("uTransform"), 1, GL_TRUE, frame_mat.v);
   assert(glGetError() == GL_NO_ERROR);
@@ -260,7 +235,7 @@ void CameraViewWidget::paintGL() {
   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, (const void *)0);
   glDisableVertexAttribArray(0);
   glBindVertexArray(0);
-  glBindTexture(USE_TEX, 0);
+  glBindTexture(GL_TEXTURE_2D, 0);
   glActiveTexture(GL_TEXTURE0);
   glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
   glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
@@ -273,28 +248,26 @@ void CameraViewWidget::vipcConnected(VisionIpcClient *vipc_client) {
   stream_height = vipc_client->buffers[0].height;
   stream_stride = vipc_client->buffers[0].stride;
 
-  // glBindTexture(GL_TEXTURE_2D, textures[0]);
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  // glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  // glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, stream_width, stream_height, 0, GL_RED, GL_UNSIGNED_BYTE, nullptr);
-  // assert(glGetError() == GL_NO_ERROR);
+  glBindTexture(GL_TEXTURE_2D, textures[0]);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, stream_width, stream_height, 0, GL_RED, GL_UNSIGNED_BYTE, nullptr);
+  assert(glGetError() == GL_NO_ERROR);
 
-  // glBindTexture(GL_TEXTURE_2D, textures[1]);
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  // glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  // glTexImage2D(GL_TEXTURE_2D, 0, GL_RG8, stream_width/2, stream_height/2, 0, GL_RG, GL_UNSIGNED_BYTE, nullptr);
-  // assert(glGetError() == GL_NO_ERROR);
+  glBindTexture(GL_TEXTURE_2D, textures[1]);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RG8, stream_width/2, stream_height/2, 0, GL_RG, GL_UNSIGNED_BYTE, nullptr);
+  assert(glGetError() == GL_NO_ERROR);
 
   // import into OpenGL
   for (int i = 0; i < vipc_client->num_buffers; i++) {
     vipc_client->buffers[i].init_gl();
   }
-
-  assert(glGetError() == GL_NO_ERROR);
 
   updateFrameMat(width(), height());
 }
