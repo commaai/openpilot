c10a2ef2e40a49b527ab49b8b7fd4e924b847a2d
Shane Smiskol <shane@smiskol.com>
2022-05-24 17:08:06 -0700

    UI: draw camera on same frame from modelV2 (#24335)
    * draw camera on same frame from modelV2
    
    * fix reversing
    
    * optimze
    
    * sleep shorter
    
    * heavy debugging
    
    * clean up
    
    * fix ui hang
    
    * try this
    
    * more generic implementation
    
    * add unused flag back
    
    * draw latest frames when camerad is coming up with no modeld
    
    * only get modelV2 once
    
    * fix drawing
    
    * conflate, reduce buffer, and remove special case when starting
    
    * make tests pass
    
    * remove comments
    
    * frame_id is a uint32_t
    
    * not sure why passing to cameraview causes overflow
    
    * ternary, not too complicated
    
    * change to size
    
    * rename to frame
    
    * use unique_ptr
    
    * Revert "use unique_ptr"
    
    This reverts commit 955842d9511a26468d4deedd1807f096540cb40c.
    
    * Assert buffer size
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * use one deque with pairs and uint32_t
    
    * formatting
    
    * frame offset
    
    * Revert "frame offset"
    
    This reverts commit a2cff8a2d57a02f314d9f6adb7221b193a150e4c.
    
    * use array
    
    * add prints
    
    * Revert "add prints"
    
    This reverts commit d6978746e5c316fb2d5a619091c9aaf849c31967.
    
    * clean up
    
    * this handles all cases
    
    * we want to clip
    
    * fixes going backwards when replay or modeld lags multiple frames, then skips multiple frames
    
    fixes going backwards when replay or modeld lags multiple frames, then skips multiple frames
    
    * fix rare case where camera is behind model
    
    simpler comment
    
    * draw latest frames if we start with no model, then hang on current frame until model catches up
    
    fix
    
    * store if it's updated
    
    * accurate comment
    
    * reset to latest on connect and showevent
    
    * better order
    
    * use quint32 for sending signal function
    
    * use empty
    
    * draft
    
    * kinda works
    
    * works
    
    * one line
    
    * debug
    
    * clean up
    
    * revert this
    
    * to preserve behavior, think we need to poll these
    
    * revert
    
    * no timeout when modeld is not alive
    
    * Update selfdrive/ui/qt/onroad.cc
    
    * changes from other PR
    
    * see if we're drawing dup model or camera frames
    
    * looks good, just need to clean up
    
    * debug
    
    * clean up
    
    * clean up
    
    * revert these changes
    
    * fix
    
    * pretty simple, test on device
    
    * clean up
    
    * revert this
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -372,19 +372,21 @@ void NvgWindow::drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV
 }
 
 void NvgWindow::paintGL() {
+  UIState *s = uiState();
+  const cereal::ModelDataV2::Reader &model = (*s->sm)["modelV2"].getModelV2();
+  CameraViewWidget::setFrameId(model.getFrameId());
   CameraViewWidget::paintGL();
 
   QPainter painter(this);
   painter.setRenderHint(QPainter::Antialiasing);
   painter.setPen(Qt::NoPen);
 
-  UIState *s = uiState();
   if (s->worldObjectsVisible()) {
 
     drawLaneLines(painter, s);
 
     if (s->scene.longitudinal_control) {
-      auto leads = (*s->sm)["modelV2"].getModelV2().getLeadsV3();
+      const auto leads = model.getLeadsV3();
       if (leads[0].getProb() > .5) {
         drawLead(painter, leads[0], s->scene.lead_vertices[0]);
       }
