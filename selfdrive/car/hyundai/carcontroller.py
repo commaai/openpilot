baeaf33e6501968931db02ebe61c402d60a79522
Shane Smiskol <shane@smiskol.com>
2022-05-27 03:10:17 -0700

    allow max jerk and simulate future desired accel
    brake controller seems to take it into account for how fast to reach desired accel, or how close to follow aReqValue

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -105,20 +105,22 @@ class CarController:
 
       if self.frame % 2 == 0 and self.CP.openpilotLongitudinalControl:
         accel = actuators.accel
-        jerk = 0
+        accel_raw = accel
 
         if CC.longActive:
-          jerk = clip(2.0 * (accel - CS.out.aEgo), -12.7, 12.7)
-          if accel < 0:
-            accel = interp(accel - CS.out.aEgo, [-1.0, -0.5], [2 * accel, accel])
+          # brake controller seems to respond well to providing a simulated future acceleration
+          # clip to only increase ego accel to prevent oscillations
+          accel_raw = accel + (accel - CS.out.aEgo) / 2
+          accel_raw = min(accel_raw, accel) if accel < 0 else max(accel_raw, accel)
 
         accel = clip(accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
+        accel_raw = clip(accel_raw, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
 
         lead_visible = False
         stopping = actuators.longControlState == LongCtrlState.stopping
         set_speed_in_units = hud_control.setSpeed * (CV.MS_TO_MPH if CS.clu11["CF_Clu_SPEED_UNIT"] == 1 else CV.MS_TO_KPH)
-        can_sends.extend(hyundaican.create_acc_commands(self.packer, CC.enabled, accel, jerk, int(self.frame / 2), lead_visible,
-                                             set_speed_in_units, stopping, CS.out.gasPressed))
+        can_sends.extend(hyundaican.create_acc_commands(self.packer, CC.enabled, accel, accel_raw, int(self.frame / 2), lead_visible,
+                                                        set_speed_in_units, stopping, CS.out.gasPressed))
         self.accel = accel
 
       # 20 Hz LFA MFA message
