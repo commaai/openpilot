0b303c9ef76553840cc9fe031f3ce5c37b2ed502
Shane Smiskol <shane@smiskol.com>
2022-05-28 01:29:01 -0700

    always

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -110,8 +110,7 @@ class CarController:
 
         if CC.longActive:
           # brake controller seems to respond well to providing a simulated future acceleration
-          if abs(accel - CS.out.aEgo) > 0.5:
-            accel_raw = accel + (accel - CS.out.aEgo) / 2
+          accel_raw = accel + (accel - CS.out.aEgo) / 2
           jerk = clip(2.0 * (accel - CS.out.aEgo), -12.7, 12.7)
 
         accel = clip(accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
