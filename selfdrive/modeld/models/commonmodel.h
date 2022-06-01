2a6ffaf71db1b8c149daa30d27e4285fc3b9b178
George Hotz <72895+geohot@users.noreply.github.com>
2022-06-01 08:18:28 -0700

    nv12: encoderd avoids a full frame copy (#24519)
    * rgb to nv12
    
    * nv12 works (w memcpy)
    
    * correct now
    
    * no copy
    
    * fix nv12 with fast debayer
    
    * reverts of unused stuff
    
    * ui use nv12
    
    * comment out thumbnails for now
    
    * rebase fix
    
    * dm read nv12
    
    * model read nv12
    
    * fix ffmpeg encoder
    
    * thumbnails from nv12
    
    * replay to nv12
    
    * python framereader support nv12
    
    * remove hardcoded frame/buffer sizes
    
    * fix build
    
    * ffmpeg encoder fix buffers
    
    * small cleanup
    
    * reduce power usage test
    
    * fix cpu usage test
    
    * fix snapshot
    
    * fix loggerd test
    
    * bump cereal
    
    Co-authored-by: Comma Device <device@comma.ai>
    Co-authored-by: Joost Wooning <jwooning@gmail.com>
    old-commit-hash: ea5b8cdfb10686c972d8d2bc1778e6e6567e45a9

--- selfdrive/modeld/models/commonmodel.h
+++ selfdrive/modeld/models/commonmodel.h
@@ -25,7 +25,7 @@ class ModelFrame {
 public:
   ModelFrame(cl_device_id device_id, cl_context context);
   ~ModelFrame();
-  float* prepare(cl_mem yuv_cl, int width, int height, const mat3& transform, cl_mem *output);
+  float* prepare(cl_mem yuv_cl, int width, int height, int frame_stride, int frame_uv_offset, const mat3& transform, cl_mem *output);
 
   const int MODEL_WIDTH = 512;
   const int MODEL_HEIGHT = 256;
--- selfdrive/modeld/transforms/transform.h
+++ selfdrive/modeld/transforms/transform.h
@@ -19,7 +19,7 @@ void transform_init(Transform* s, cl_context ctx, cl_device_id device_id);
 void transform_destroy(Transform* transform);
 
 void transform_queue(Transform* s, cl_command_queue q,
-                     cl_mem yuv, int in_width, int in_height,
+                     cl_mem yuv, int in_width, int in_height, int in_stride, int in_uv_offset,
                      cl_mem out_y, cl_mem out_u, cl_mem out_v,
                      int out_width, int out_height,
                      const mat3& projection);
