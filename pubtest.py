9dfb26920e88cc3edf9de78176ce16caaef774a2
Lukas Petersson <lukas.petersson.1999@gmail.com>
2022-06-02 16:37:56 -0700

    remove files

--- pubtest.py
+++
@@ -1,7 +0,0 @@
-import cereal.messaging as messaging
-
-pm = messaging.PubMaster(['sensorEvents'])
-dat = messaging.new_message('sensorEvents', size=2)
-dat.sensorEvents[0] = {"gyro": {"v": [0.12, -0.22, 0.11]}}
-pm.send('sensorEvents', dat)
-print("sent")
--- subtest.py
+++
@@ -1,8 +0,0 @@
-import cereal.messaging as messaging
-
-# in subscriber
-sm = messaging.SubMaster(['sensorEvents'])
-while 1:
-  sm.update()
-  print(sm.updated)
-
