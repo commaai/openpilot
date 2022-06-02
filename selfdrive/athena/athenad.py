709897d36093e9ae8fdb985e2601151c65525aa1
Willem Melching <willem.melching@gmail.com>
2022-06-02 13:24:35 +0200

    Update selfdrive/athena/athenad.py
    Co-authored-by: Robbe Derks <robbe.derks@gmail.com>

--- selfdrive/athena/athenad.py
+++ selfdrive/athena/athenad.py
@@ -414,7 +414,7 @@ def primeActivated(activated):
 @dispatcher.add_method
 def setBandwithLimit(upload_speed_kbps, download_speed_kbps):
   if not AGNOS:
-    return {"success": 0, "error": "only supported on comma three"}
+    return {"success": 0, "error": "only supported on AGNOS"}
 
   try:
     HARDWARE.set_bandwidth_limit(upload_speed_kbps, download_speed_kbps)
