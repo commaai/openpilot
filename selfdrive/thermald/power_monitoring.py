e491546798686b68e4b331465cd4ce430901c455
Jason Shuler <jshuler@gmail.com>
2022-06-02 14:35:17 -0400

    Increasing offline time from 1 to 30 hrs

--- selfdrive/thermald/power_monitoring.py
+++ selfdrive/thermald/power_monitoring.py
@@ -18,7 +18,7 @@ CAR_CHARGING_RATE_W = 45
 VBATT_PAUSE_CHARGING = 11.0           # Lower limit on the LPF car battery voltage
 VBATT_INSTANT_PAUSE_CHARGING = 7.0    # Lower limit on the instant car battery voltage measurements to avoid triggering on instant power loss
 MAX_TIME_OFFROAD_S = 30*3600
-MIN_ON_TIME_S = 3600
+MIN_ON_TIME_S = 30*3600 # TODO: JJS Revert - for debugging. 30 hours instead of one
 
 class PowerMonitoring:
   def __init__(self):
