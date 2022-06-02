2528f470f76d8d75d39b0ed7c72b08f82ce25793
Willem Melching <willem.melching@gmail.com>
2022-06-02 09:33:15 +0200

    Update selfdrive/hardware/hw.h
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/hardware/hw.h
+++ selfdrive/hardware/hw.h
@@ -11,7 +11,7 @@ class HardwarePC : public HardwareNone {
 public:
   static std::string get_os_version() { return "openpilot for PC"; }
   static std::string get_name() { return "pc"; };
-  static cereal::InitData::DeviceType get_device_type() {return cereal::InitData::DeviceType::PC; };
+  static cereal::InitData::DeviceType get_device_type() { return cereal::InitData::DeviceType::PC; };
   static bool PC() { return true; }
   static bool TICI() { return util::getenv("TICI", 0) == 1; }
   static bool AGNOS() { return util::getenv("TICI", 0) == 1; }
