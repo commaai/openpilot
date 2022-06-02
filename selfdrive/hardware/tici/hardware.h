11dfce1b58427b393c007aab4c98249e8e0eb16e
Willem Melching <willem.melching@gmail.com>
2022-06-02 09:33:10 +0200

    Update selfdrive/hardware/tici/hardware.h
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/hardware/tici/hardware.h
+++ selfdrive/hardware/tici/hardware.h
@@ -17,9 +17,9 @@ public:
     return "AGNOS " + util::read_file("/VERSION");
   };
   static std::string get_name() { return "tici"; };
-  static cereal::InitData::DeviceType get_device_type() {return cereal::InitData::DeviceType::TICI; };
-  static int get_voltage() {return std::atoi(util::read_file("/sys/class/hwmon/hwmon1/in1_input").c_str()); };
-  static int get_current() {return std::atoi(util::read_file("/sys/class/hwmon/hwmon1/curr1_input").c_str()); };
+  static cereal::InitData::DeviceType get_device_type() { return cereal::InitData::DeviceType::TICI; };
+  static int get_voltage() { return std::atoi(util::read_file("/sys/class/hwmon/hwmon1/in1_input").c_str()); };
+  static int get_current() { return std::atoi(util::read_file("/sys/class/hwmon/hwmon1/curr1_input").c_str()); };
 
 
   static void reboot() { std::system("sudo reboot"); };
