25fb05a688575fbc8bddfdd6035c6d380a93017f
Shane Smiskol <shane@smiskol.com>
2022-06-01 21:04:01 -0700

    add test for whitelist

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -4,9 +4,9 @@ import unittest
 from parameterized import parameterized
 
 from cereal import car
-from selfdrive.car.car_helpers import interfaces
+from selfdrive.car.car_helpers import get_interface_attr, interfaces
 from selfdrive.car.fingerprints import FW_VERSIONS
-from selfdrive.car.fw_versions import match_fw_to_car
+from selfdrive.car.fw_versions import REQUESTS, match_fw_to_car
 
 CarFw = car.CarParams.CarFw
 Ecu = car.CarParams.Ecu
@@ -56,6 +56,29 @@ class TestFwFingerprint(unittest.TestCase):
             passed = False
 
     self.assertTrue(passed, "Blacklisted FW versions found")
+
+  def test_fw_request_whitelist(self):
+    passed = True
+    brands = set(r.brand for r in REQUESTS)
+    versions = get_interface_attr('FW_VERSIONS')
+    for brand in brands:
+      whitelist_ecus = [ecu for r in REQUESTS for ecu in r.whitelist_ecus if r.brand == brand]
+      if len(whitelist_ecus) == 0:
+        continue  # no whitelists for brand
+
+      # each ecu in fw versions needs to be whitelisted at least once
+      brand_ecu_types = set()
+      for fw_versions in versions['volkswagen'].values():
+        for (ecu_type, _, _) in fw_versions.keys():
+          brand_ecu_types.add(ecu_type)
+
+      ecus_not_in_whitelists = set(brand_ecu_types) - set(whitelist_ecus)
+      if len(ecus_not_in_whitelists):
+        ecu_strings = ", ".join([f'Ecu.{ECU_NAME[ecu]}' for ecu in ecus_not_in_whitelists])
+        print(f'{brand.title()} FW request does not whitelist ecus: {ecu_strings}')
+        passed = False
+
+    self.assertTrue(passed, "Not all ecus in FW versions found in whitelists")
 
 
 if __name__ == "__main__":
