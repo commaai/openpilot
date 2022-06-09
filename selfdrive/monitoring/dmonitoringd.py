8773fb430bca3a46e26d1e162d0a1789f4be1887
ZwX1616 <zwx1616@gmail.com>
2022-06-09 13:34:15 -0700

    auto choose

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -3,7 +3,6 @@ import gc
 
 import cereal.messaging as messaging
 from cereal import car
-from common.params import Params
 from common.realtime import set_realtime_priority
 from selfdrive.controls.lib.events import Events
 from selfdrive.locationd.calibrationd import Calibration
@@ -20,7 +19,7 @@ def dmonitoringd_thread(sm=None, pm=None):
   if sm is None:
     sm = messaging.SubMaster(['driverStateV2', 'liveCalibration', 'carState', 'controlsState', 'modelV2'], poll=['driverStateV2'])
 
-  driver_status = DriverStatus(rhd=Params().get_bool("IsRHD"))
+  driver_status = DriverStatus()
 
   sm['liveCalibration'].calStatus = Calibration.INVALID
   sm['liveCalibration'].rpyCalib = [0, 0, 0]
