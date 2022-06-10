d34f36ef25600cc5456edeafde864f7fc89cde3b
Shane Smiskol <shane@smiskol.com>
2022-06-10 14:31:25 -0700

    Revert tuning changes
    Revert tuning changes

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -257,6 +257,7 @@ class CarInterface(CarInterfaceBase):
                            get_safety_config(car.CarParams.SafetyModel.hyundaiHDA2)]
       tire_stiffness_factor = 0.65
 
+      ret.maxLateralAccel = 2.
       set_torque_tune(ret.lateralTuning, ret.maxLateralAccel, 0.01)
 
     # Genesis
