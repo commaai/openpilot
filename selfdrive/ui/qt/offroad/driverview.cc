fd9cce69cd81a7f621f779b6055164293410bdcb
jun980401 <ldj980401@naver.com>
2022-06-11 22:23:23 +0900

    Update driverview.cc

--- selfdrive/ui/qt/offroad/driverview.cc
+++ selfdrive/ui/qt/offroad/driverview.cc
@@ -53,7 +53,7 @@ void DriverViewScene::paintEvent(QPaintEvent* event) {
     p.setPen(Qt::white);
     p.setRenderHint(QPainter::TextAntialiasing);
     configFont(p, "Inter", 100, "Bold");
-    p.drawText(geometry(), Qt::AlignCenter, "camera starting");
+    p.drawText(geometry(), Qt::AlignCenter, "카메라 켜는중");
     return;
   }
 
