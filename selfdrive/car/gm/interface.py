f7edd5f3d3c2afe2cfd0c1013fddc1f7241978f5
HaraldSchafer <harald.the.engineer@gmail.com>
2022-06-10 17:57:23 -0700

    Car interface: set max lateral torque from table (#24789)
    * json
    
    * better naem
    
    * Read from table
    
    * formatting and default to nan
    
    * Generate docs
    
    * Read from table
    
    * this should be the same
    
    * Prius v is full
    
    * test we always set the tunes correctly
    
    add to release files
    
    * Set for all cars
    
    Set for all cars
    
    * Revert tuning changes
    
    Revert tuning changes
    
    * remove that
    
    * fixes
    
    * update ref commit for new maxLateralAccels
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    old-commit-hash: 3066ad81a8b2f89a22ddfc86a879395c573ce0f5

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -83,7 +83,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 17.7  # Stock 15.7, LiveParameters
       tire_stiffness_factor = 0.469 # Stock Michelin Energy Saver A/S, LiveParameters
       ret.centerToFront = ret.wheelbase * 0.45 # Volt Gen 1, TODO corner weigh
-      ret.maxLateralAccel = 1.6
 
       ret.lateralTuning.pid.kpBP = [0., 40.]
       ret.lateralTuning.pid.kpV = [0., 0.17]
@@ -111,7 +110,6 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.86
       ret.steerRatio = 14.4  # end to end is 13.46
       ret.centerToFront = ret.wheelbase * 0.4
-      ret.maxLateralAccel = 1.4
       ret.lateralTuning.pid.kf = 1.  # get_steer_feedforward_acadia()
 
     elif candidate == CAR.BUICK_REGAL:
