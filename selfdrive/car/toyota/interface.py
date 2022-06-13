318d4b7ca47221bc48a555c382340a2075e62834
Harald Schafer <harald.the.engineer@gmail.com>
2022-06-13 14:55:56 -0700

    Delete toyota tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from cereal import car
 from common.conversions import Conversions as CV
+from selfdrive.controls.lib.latcontrol_torque import set_torque_tune
 from panda import Panda
-from selfdrive.car.toyota.tunes import LatTunes, LongTunes, set_long_tune, set_lat_tune
+from selfdrive.car.toyota.tunes import LongTunes, set_long_tune
 from selfdrive.car.toyota.values import Ecu, CAR, ToyotaFlags, TSS2_CAR, RADAR_ACC_CAR, NO_DSU_CAR, MIN_ACC_SPEED, EPS_SCALE, EV_HYBRID_CAR, CarControllerParams
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.interfaces import CarInterfaceBase
@@ -32,6 +33,7 @@ class CarInterface(CarInterfaceBase):
 
     stop_and_go = False
     torque_params = CarInterfaceBase.get_torque_params(candidate)
+    steering_angle_deadzone_deg = 0.0
 
     if candidate == CAR.PRIUS:
       stop_and_go = True
@@ -40,7 +42,8 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.6371   # hand-tune
       ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
       ret.maxLateralAccel = 1.7
-      set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=ret.maxLateralAccel, FRICTION=0.06, steering_angle_deadzone_deg=1.0)
+      #TODO only override for bad angle sensor prius
+      steering_angle_deadzone_deg = 0.0
 
     elif candidate == CAR.PRIUS_V:
       stop_and_go = True
@@ -49,7 +52,8 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.5533
       ret.mass = 3340. * CV.LB_TO_KG + STD_CARGO_KG
       ret.maxLateralAccel = 1.8
-      set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=ret.maxLateralAccel, FRICTION=0.06)
+      # TODO override until there is enough data
+      torque_params = CarInterfaceBase.get_torque_params(CAR.PRIUS)
 
     elif candidate in (CAR.RAV4, CAR.RAV4H):
       stop_and_go = True if (candidate in CAR.RAV4H) else False
@@ -58,7 +62,6 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.5533
       ret.mass = 3650. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
       ret.maxLateralAccel = 1.8
-      set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=ret.maxLateralAccel, FRICTION=0.06)
 
     elif candidate == CAR.COROLLA:
       ret.wheelbase = 2.70
@@ -66,7 +69,6 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 2860. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
       ret.maxLateralAccel = 2.8
-      set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=ret.maxLateralAccel, FRICTION=0.024)
 
     elif candidate in (CAR.LEXUS_RX, CAR.LEXUS_RXH, CAR.LEXUS_RX_TSS2, CAR.LEXUS_RXH_TSS2):
       stop_and_go = True
@@ -75,7 +77,6 @@ class CarInterface(CarInterfaceBase):
       ret.wheelSpeedFactor = 1.035
       tire_stiffness_factor = 0.5533
       ret.mass = 4481. * CV.LB_TO_KG + STD_CARGO_KG  # mean between min and max
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
 
     elif candidate in (CAR.CHR, CAR.CHRH):
       stop_and_go = True
@@ -83,7 +84,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.6
       tire_stiffness_factor = 0.7933
       ret.mass = 3300. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_F)
 
     elif candidate in (CAR.CAMRY, CAR.CAMRYH, CAR.CAMRY_TSS2, CAR.CAMRYH_TSS2):
       stop_and_go = True
@@ -91,10 +91,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.7
       tire_stiffness_factor = 0.7933
       ret.mass = 3400. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
-      if candidate in (CAR.CAMRY_TSS2, CAR.CAMRYH_TSS2):
-        set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, torque_params['LAT_ACCEL_FACTOR'], torque_params['FRICTION'])
-      else:
-        set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
 
     elif candidate in (CAR.HIGHLANDER_TSS2, CAR.HIGHLANDERH_TSS2):
       stop_and_go = True
@@ -102,7 +98,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 16.0
       tire_stiffness_factor = 0.8
       ret.mass = 4700. * CV.LB_TO_KG + STD_CARGO_KG  # 4260 + 4-5 people
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_G)
 
     elif candidate in (CAR.HIGHLANDER, CAR.HIGHLANDERH):
       stop_and_go = True
@@ -110,7 +105,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 16.0
       tire_stiffness_factor = 0.8
       ret.mass = 4607. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid limited
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_G)
 
     elif candidate in (CAR.AVALON, CAR.AVALON_2019, CAR.AVALONH_2019, CAR.AVALON_TSS2, CAR.AVALONH_TSS2):
       # starting from 2019, all Avalon variants have stop and go
@@ -120,7 +114,9 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 14.8  # Found at https://pressroom.toyota.com/releases/2016+avalon+product+specs.download
       tire_stiffness_factor = 0.7983
       ret.mass = 3505. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_H)
+      # TODO override until there is enough data
+      if candidate == CAR.AVALON:
+        torque_params = CarInterfaceBase.get_torque_params(CAR.AVALON_2019)
 
     elif candidate in (CAR.RAV4_TSS2, CAR.RAV4_TSS2_2022, CAR.RAV4H_TSS2, CAR.RAV4H_TSS2_2022):
       stop_and_go = True
@@ -128,14 +124,17 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 14.3
       tire_stiffness_factor = 0.7933
       ret.mass = 3585. * CV.LB_TO_KG + STD_CARGO_KG  # Average between ICE and Hybrid
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_D)
 
       # 2019+ RAV4 TSS2 uses two different steering racks and specific tuning seems to be necessary.
       # See https://github.com/commaai/openpilot/pull/21429#issuecomment-873652891
+      # TODO different torque control for different firmware
       for fw in car_fw:
         if fw.ecu == "eps" and (fw.fwVersion.startswith(b'\x02') or fw.fwVersion in [b'8965B42181\x00\x00\x00\x00\x00\x00']):
-          set_lat_tune(ret.lateralTuning, LatTunes.PID_I)
           break
+
+      # TODO override until there is enough data
+      if candidate == CAR.RAV4_TSS2_2022:
+        torque_params = CarInterfaceBase.get_torque_params(CAR.RAV4_TSS2)
 
     elif candidate in (CAR.COROLLA_TSS2, CAR.COROLLAH_TSS2):
       stop_and_go = True
@@ -144,7 +143,6 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
       ret.maxLateralAccel = 2.0
-      set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=ret.maxLateralAccel, FRICTION=0.07)
 
     elif candidate in (CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2, CAR.LEXUS_ESH):
       stop_and_go = True
@@ -152,7 +150,9 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 16.0  # not optimized
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 3677. * CV.LB_TO_KG + STD_CARGO_KG  # mean between min and max
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_D)
+      # TODO override until there is enough data
+      if candidate == CAR.LEXUS_ESH:
+        torque_params = CarInterfaceBase.get_torque_params(CAR.LEXUS_ES_TSS2)
 
     elif candidate == CAR.SIENNA:
       stop_and_go = True
@@ -160,14 +160,15 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 15.5
       tire_stiffness_factor = 0.444
       ret.mass = 4590. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_J)
 
     elif candidate in (CAR.LEXUS_IS, CAR.LEXUS_RC):
       ret.wheelbase = 2.79908
       ret.steerRatio = 13.3
       tire_stiffness_factor = 0.444
       ret.mass = 3736.8 * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_L)
+      # TODO override until there is enough data
+      if candidate == CAR.LEXUS_RC:
+        torque_params = CarInterfaceBase.get_torque_params(CAR.LEXUS_IS)
 
     elif candidate == CAR.LEXUS_CTH:
       stop_and_go = True
@@ -175,7 +176,9 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 18.6
       tire_stiffness_factor = 0.517
       ret.mass = 3108 * CV.LB_TO_KG + STD_CARGO_KG  # mean between min and max
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_M)
+      # TODO override until there is enough data
+      torque_params = {'LAT_ACCEL_FACTOR': 2.5, 'FRICTION': 0.1}
+
 
     elif candidate in (CAR.LEXUS_NX, CAR.LEXUS_NXH, CAR.LEXUS_NX_TSS2, CAR.LEXUS_NXH_TSS2):
       stop_and_go = True
@@ -183,7 +186,11 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 14.7
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 4070 * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
+      # TODO override until there is enough data
+      if candidate == CAR.LEXUS_NXH:
+        torque_params = CarInterfaceBase.get_torque_params(CAR.LEXUS_NX)
+      elif candidate == CAR.LEXUS_NXH_TSS2:
+        torque_params = CarInterfaceBase.get_torque_params(CAR.LEXUS_NX_TSS2)
 
     elif candidate == CAR.PRIUS_TSS2:
       stop_and_go = True
@@ -191,7 +198,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.4   # True steerRatio from older prius
       tire_stiffness_factor = 0.6371   # hand-tune
       ret.mass = 3115. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_N)
 
     elif candidate == CAR.MIRAI:
       stop_and_go = True
@@ -199,7 +205,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 14.8
       tire_stiffness_factor = 0.8
       ret.mass = 4300. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
 
     elif candidate in (CAR.ALPHARD_TSS2, CAR.ALPHARDH_TSS2):
       stop_and_go = True
@@ -207,8 +212,11 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 14.2
       tire_stiffness_factor = 0.444
       ret.mass = 4305. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_J)
+      # TODO override until there is enough data
+      torque_params = CarInterfaceBase.get_torque_params(CAR.SIENNA)
 
+
+    set_torque_tune(ret.lateralTuning, torque_params['LAT_ACCEL_FACTOR'], torque_params['FRICTION'], steering_angle_deadzone_deg)
     ret.steerRateCost = 1.
     ret.centerToFront = ret.wheelbase * 0.44
 
--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -1,30 +1,10 @@
 #!/usr/bin/env python3
 from enum import Enum
-from selfdrive.controls.lib.latcontrol_torque import set_torque_tune
 
 class LongTunes(Enum):
   PEDAL = 0
   TSS2 = 1
   TSS = 2
-
-class LatTunes(Enum):
-  INDI_PRIUS = 0
-  LQR_RAV4 = 1
-  PID_A = 2
-  PID_B = 3
-  PID_C = 4
-  PID_D = 5
-  PID_E = 6
-  PID_F = 7
-  PID_G = 8
-  PID_I = 9
-  PID_H = 10
-  PID_J = 11
-  PID_K = 12
-  PID_L = 13
-  PID_M = 14
-  PID_N = 15
-  TORQUE = 16
 
 
 ###### LONG ######
@@ -47,61 +27,3 @@ def set_long_tune(tune, name):
     tune.kiV = [0.54, 0.36]
   else:
     raise NotImplementedError('This longitudinal tune does not exist')
-
-
-###### LAT ######
-def set_lat_tune(tune, name, MAX_LAT_ACCEL=2.5, FRICTION=0.01, steering_angle_deadzone_deg=0.0, use_steering_angle=True):
-  if name == LatTunes.TORQUE:
-    set_torque_tune(tune, MAX_LAT_ACCEL, FRICTION, steering_angle_deadzone_deg)
-  elif 'PID' in str(name):
-    tune.init('pid')
-    tune.pid.kiBP = [0.0]
-    tune.pid.kpBP = [0.0]
-    if name == LatTunes.PID_A:
-      tune.pid.kpV = [0.2]
-      tune.pid.kiV = [0.05]
-      tune.pid.kf = 0.00003
-    elif name == LatTunes.PID_C:
-      tune.pid.kpV = [0.6]
-      tune.pid.kiV = [0.1]
-      tune.pid.kf = 0.00006
-    elif name == LatTunes.PID_D:
-      tune.pid.kpV = [0.6]
-      tune.pid.kiV = [0.1]
-      tune.pid.kf = 0.00007818594
-    elif name == LatTunes.PID_F:
-      tune.pid.kpV = [0.723]
-      tune.pid.kiV = [0.0428]
-      tune.pid.kf = 0.00006
-    elif name == LatTunes.PID_G:
-      tune.pid.kpV = [0.18]
-      tune.pid.kiV = [0.015]
-      tune.pid.kf = 0.00012
-    elif name == LatTunes.PID_H:
-      tune.pid.kpV = [0.17]
-      tune.pid.kiV = [0.03]
-      tune.pid.kf = 0.00006
-    elif name == LatTunes.PID_I:
-      tune.pid.kpV = [0.15]
-      tune.pid.kiV = [0.05]
-      tune.pid.kf = 0.00004
-    elif name == LatTunes.PID_J:
-      tune.pid.kpV = [0.19]
-      tune.pid.kiV = [0.02]
-      tune.pid.kf = 0.00007818594
-    elif name == LatTunes.PID_L:
-      tune.pid.kpV = [0.3]
-      tune.pid.kiV = [0.05]
-      tune.pid.kf = 0.00006
-    elif name == LatTunes.PID_M:
-      tune.pid.kpV = [0.3]
-      tune.pid.kiV = [0.05]
-      tune.pid.kf = 0.00007
-    elif name == LatTunes.PID_N:
-      tune.pid.kpV = [0.35]
-      tune.pid.kiV = [0.15]
-      tune.pid.kf = 0.00007818594
-    else:
-      raise NotImplementedError('This PID tune does not exist')
-  else:
-    raise NotImplementedError('This lateral tune does not exist')
