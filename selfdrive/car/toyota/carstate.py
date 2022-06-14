fd3d9804b6fa6a784f5ae53f80f0d99fe4dde27f
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2022-06-14 23:22:15 +1000

    self

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -128,7 +128,7 @@ class CarState(CarStateBase):
       ret.leftBlindspot = (cp.vl["BSM"]["L_ADJACENT"] == 1) or (cp.vl["BSM"]["L_APPROACHING"] == 1)
       ret.rightBlindspot = (cp.vl["BSM"]["R_ADJACENT"] == 1) or (cp.vl["BSM"]["R_APPROACHING"] == 1)
 
-    if not self.CP.openpilotLongitudinalControl and CP.carFingerprint not in TSS2_CAR:
+    if not self.CP.openpilotLongitudinalControl and self.CP.carFingerprint not in TSS2_CAR:
       self.stock_resume_ready = (cp.vl["ACC_CONTROL"]["RELEASE_STANDSTILL"] == 1 and self.pcm_acc_status == 7)
 
     return ret
