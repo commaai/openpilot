35272ee466e24a2d57b2d1d74818997bb544b8c2
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2022-06-15 23:10:56 +0900

    ui: add black color define (#24866)
    old-commit-hash: 849ec17b20f8cae7f6fd6589456d7b00b07e794b

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -74,6 +74,7 @@ protected:
   void drawHud(QPainter &p);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
   inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
 
   double prev_draw_t = 0;
   FirstOrderFilter fps_filter;
