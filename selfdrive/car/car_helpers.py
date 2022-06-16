6b93ac276d4d1b6c4077477d5b4bb74949729741
Jason Young <jyoung8607@gmail.com>
2022-06-16 08:10:20 -0400

    #24706

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -90,7 +90,7 @@ def fingerprint(logcan, sendcan):
       if cached_params.carName == "mock":
         cached_params = None
 
-    if cached_params is not None and len(cached_params.carFw) > 0 and cached_params.carVin is not VIN_UNKNOWN:
+    if False and cached_params is not None and len(cached_params.carFw) > 0 and cached_params.carVin is not VIN_UNKNOWN:
       cloudlog.warning("Using cached CarParams")
       vin = cached_params.carVin
       car_fw = list(cached_params.carFw)
