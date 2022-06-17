07123712f85b931b37f4f54d179686543bf80671
ZwX1616 <zwx1616@gmail.com>
2022-06-17 14:28:03 -0700

    minor tweak

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -28,8 +28,8 @@ class DRIVER_MONITOR_SETTINGS():
 
     self._FACE_THRESHOLD = 0.7
     self._EYE_THRESHOLD = 0.65
-    self._SG_THRESHOLD = 0.925
-    self._BLINK_THRESHOLD = 0.861
+    self._SG_THRESHOLD = 0.9
+    self._BLINK_THRESHOLD = 0.87
 
     self._EE_THRESH11 = 0.75
     self._EE_THRESH12 = 3.25
