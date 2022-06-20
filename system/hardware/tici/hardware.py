9a612df683b215bbe6187a276bc0722fd4170825
Willem Melching <willem.melching@gmail.com>
2022-06-20 14:28:55 +0200

    already exported now

--- system/hardware/tici/hardware.py
+++ system/hardware/tici/hardware.py
@@ -462,13 +462,6 @@ class Tici(HardwareBase):
     sudo_write("N", "/sys/kernel/debug/msm_vidc/clock_scaling")
     sudo_write("Y", "/sys/kernel/debug/msm_vidc/disable_thermal_mitigation")
 
-    # *** export GPIO for sensors ***
-    # move to /usr/comma/gpio.sh on next OS update
-    sudo_write(str(GPIO.BMX055_ACCEL_INT), "/sys/class/gpio/export")
-    sudo_write(str(GPIO.BMX055_GYRO_INT), "/sys/class/gpio/export")
-    sudo_write(str(GPIO.BMX055_MAGN_INT), "/sys/class/gpio/export")
-    sudo_write(str(GPIO.LSM_INT), "/sys/class/gpio/export")
-
   def configure_modem(self):
     sim_id = self.get_sim_info().get('sim_id', '')
 
