b1c2401d1d0461d110ac7498c587441f5df16120
sshane <shane@smiskol.com>
2022-06-19 17:02:53 -0700

    check available is true if enabled is true

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -222,6 +222,8 @@ class TestCarModelBase(unittest.TestCase):
 
       checks['gasPressed'] += CS.gasPressed != self.safety.get_gas_pressed_prev()
 
+      checks['cruiseState'] += CS.cruiseState.enabled and not CS.cruiseState.available
+
       # TODO: remove this exception once this mismatch is resolved
       brake_pressed = CS.brakePressed
       if CS.brakePressed and not self.safety.get_brake_pressed_prev():
