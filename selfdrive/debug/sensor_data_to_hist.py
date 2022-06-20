a31b721878991df01dab44a2d43432577246c2aa
Willem Melching <willem.melching@gmail.com>
2022-06-20 17:29:39 +0200

    clean up plotting script

--- selfdrive/debug/sensor_data_to_hist.py
+++ selfdrive/debug/sensor_data_to_hist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 printing the gap between interrupts in a histogram to check if the
 frequency is what we expect, the bmx is not interrup driven for as we
@@ -6,27 +7,20 @@ get interrupts in a 2kHz rate.
 
 import argparse
 import sys
+from collections import defaultdict
+
 from tools.lib.logreader import LogReader
 from tools.lib.route import Route
 
-import matplotlib
-matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
-
 SRC_BMX = "bmx055"
-SRC_LSM = "lsm6ds3trc"
+SRC_LSM = "lsm6ds3"
+
 
 def parseEvents(log_reader):
-  bmx_data = {
-    "accel": [],
-    "gyro":  []
-  }
-
-  lsm_data = {
-    "accel": [],
-    "gyro":  []
-  }
+  bmx_data = defaultdict(list)
+  lsm_data = defaultdict(list)
 
   for m in log_reader:
     # only sensorEvents
@@ -41,16 +35,16 @@ def parseEvents(log_reader):
         continue # empty event?
 
       if d["source"] == SRC_BMX and "acceleration" in d:
-        bmx_data["accel"].append(d["timestamp"])
+        bmx_data["accel"].append(d["timestamp"] / 1e9)
 
       if d["source"] == SRC_BMX and "gyroUncalibrated" in d:
-        bmx_data["gyro"].append(d["timestamp"])
+        bmx_data["gyro"].append(d["timestamp"] / 1e9)
 
       if d["source"] == SRC_LSM and "acceleration" in d:
-        lsm_data["accel"].append(d["timestamp"])
+        lsm_data["accel"].append(d["timestamp"] / 1e9)
 
       if d["source"] == SRC_LSM and "gyroUncalibrated" in d:
-        lsm_data["gyro"].append(d["timestamp"])
+        lsm_data["gyro"].append(d["timestamp"] / 1e9)
 
   return bmx_data, lsm_data
 
@@ -74,33 +68,28 @@ def logAvgValues(data, sensor):
     return
 
   avg = sum(data) / len(data)
-  hz  = 1/(avg*10**-9)
+  hz  = 1 / avg
   print(f"{sensor}: data_points: {len(data)} avg [ns]: {avg} avg [Hz]: {hz}")
-
-
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("route", type=str, nargs=1, help="route name + segment number for offline usage")
-  parser.add_argument("route_id", type=int, nargs=1, help="route id")
+  parser.add_argument("route", type=str, help="route name")
+  parser.add_argument("segment", type=int, help="segment number")
   args = parser.parse_args()
 
-  route  = args.route[0]
-  r = Route(route)
-  #logs = [q_log if r_log is None else r_log for (q_log, r_log) in zip(r.qlog_paths(), r.log_paths())]
+  r = Route(args.route)
   logs = r.log_paths()
 
   if len(logs) == 0:
     print("NO data routes")
     sys.exit(0)
 
-  route_id = args.route_id[0]
-  if route_id > len(logs):
-    print(f"RouteID: {route_id} out of range, max: {len(logs) -1}")
+  if args.segment >= len(logs):
+    print(f"RouteID: {args.segment} out of range, max: {len(logs) -1}")
     sys.exit(0)
 
-  lr = LogReader(logs[route_id])
+  lr = LogReader(logs[args.segment])
   bmx_data, lsm_data = parseEvents(lr)
 
   # sort bmx accel data, and then cal all the diffs, and to a histogram of those
@@ -111,20 +100,20 @@ if __name__ == "__main__":
 
   # get out the averages
   logAvgValues(bmx_accel_diffs, "bmx accel")
-  logAvgValues(bmx_gyro_diffs,  "bmx gyro ")
+  logAvgValues(bmx_gyro_diffs, "bmx gyro ")
   logAvgValues(lsm_accel_diffs, "lsm accel")
-  logAvgValues(lsm_gyro_diffs,  "lsm gyro ")
+  logAvgValues(lsm_gyro_diffs, "lsm gyro ")
 
   fig, axs = plt.subplots(1, 2, tight_layout=True)
   axs[0].hist(bmx_accel_diffs, bins=50)
   axs[0].set_title("bmx_accel")
-  axs[1].hist(bmx_gyro_diffs,  bins=50)
+  axs[1].hist(bmx_gyro_diffs, bins=50)
   axs[1].set_title("bmx_gyro")
 
   figl, axsl = plt.subplots(1, 2, tight_layout=True)
   axsl[0].hist(lsm_accel_diffs, bins=50)
   axsl[0].set_title("lsm_accel")
-  axsl[1].hist(lsm_gyro_diffs,  bins=50)
+  axsl[1].hist(lsm_gyro_diffs, bins=50)
   axsl[1].set_title("lsm_gyro")
 
   print("check plot...")
