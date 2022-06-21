964c1e9e51149cf1f719317d88dbd9c8318b5fdc
Willem Melching <willem.melching@gmail.com>
2022-06-21 14:56:16 +0200

    simplify rect placement

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -22,15 +22,14 @@ void Sidebar::drawMetric(QPainter &p, const QPair<QString, QString> &label, QCol
   p.setPen(QColor(0xff, 0xff, 0xff));
   configFont(p, "Inter", 35, "SemiBold");
 
-  QRect label_top_rect = getTextRect(p, Qt::AlignCenter, label.first);
-  label_top_rect.moveCenter({rect.center().x() + 11, 0});
-  label_top_rect.moveTop(rect.top() + 19);
-  p.drawText(label_top_rect, Qt::AlignCenter, label.first);
+  QRect label_rect = getTextRect(p, Qt::AlignCenter, label.first);
+  label_rect.setWidth(218);
+  label_rect.moveLeft(rect.left() + 22);
+  label_rect.moveTop(rect.top() + 19);
+  p.drawText(label_rect, Qt::AlignCenter, label.first);
 
-  QRect label_bottom_rect = getTextRect(p, Qt::AlignCenter, label.second);
-  label_bottom_rect.moveCenter({rect.center().x() + 11, 0});
-  label_bottom_rect.moveTop(rect.top() + 65);
-  p.drawText(label_bottom_rect, Qt::AlignCenter, label.second);
+  label_rect.moveTop(rect.top() + 65);
+  p.drawText(label_rect, Qt::AlignCenter, label.second);
 }
 
 Sidebar::Sidebar(QWidget *parent) : QFrame(parent) {
