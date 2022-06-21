e23b37d3fe8dd3dd07b46a32a4f0564fabade1aa
Shane Smiskol <shane@smiskol.com>
2022-06-21 12:46:04 -0700

    0.1 should be pretty safe

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -647,7 +647,7 @@ class Controls:
     if self.joystick_mode and self.sm.rcv_frame['testJoystick'] > 0 and self.sm['testJoystick'].buttons[0]:
       CC.cruiseControl.cancel = True
     if len(speeds := self.sm['longitudinalPlan'].speeds):
-      CC.cruiseControl.resume = self.enabled and CS.cruiseState.standstill and speeds[-1] > 0.2
+      CC.cruiseControl.resume = self.enabled and CS.cruiseState.standstill and speeds[-1] > 0.1
 
     hudControl = CC.hudControl
     hudControl.setSpeed = float(self.v_cruise_kph * CV.KPH_TO_MS)
