ef3b6dfc656bf2b9eb9f88cda35410cf97b48097
Shane Smiskol <shane@smiskol.com>
2022-06-23 22:36:18 -0700

    clean up

--- selfdrive/ui/qt/window.cc
+++ selfdrive/ui/qt/window.cc
@@ -135,20 +135,5 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event) {
     device.resetInteractiveTimout();
   }
 
-//  if (event->type() == QEvent::LanguageChange) {
-//    obj->retranslateUi(this);
-//  }
-
   return false;
 }
-
-//void MainWindow::changeEvent(QEvent *event) {
-//  qDebug() << "changeEvent";
-//  if (event->type() == QEvent::LanguageChange) {
-//    qDebug() << "LanguageChange";
-////    setTrs();
-////    ui->retranslateUi(this);
-//  }
-//
-//  QWidget::changeEvent(event);
-//}
