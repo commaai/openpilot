57569f0051cd0f9a8daae47be560ec2fc0067f7f
Shane Smiskol <shane@smiskol.com>
2022-06-27 20:39:01 -0700

    debug

--- selfdrive/ui/tests/test_translations.cc
+++ selfdrive/ui/tests/test_translations.cc
@@ -1,5 +1,6 @@
 #include "catch2/catch.hpp"
 #include "selfdrive/ui/qt/window.h"
+#include <QDebug>
 
 const QString TEST_TEXT = "(WRAPPED_SOURCE_TEXT)";
 QRegExp RE_NUM("\\d*");
@@ -17,23 +18,26 @@ template <typename T>
 void checkTextWidgetType(MainWindow &w) {
   for (auto widget : w.findChildren<T>()) {
     const QString text = widget->text();
-    SECTION(text.toStdString()) {
-      bool isNumber = RE_NUM.exactMatch(text);
-      bool wrapped = text.contains(TEST_TEXT);
-      QString parentWidgets = getParentWidgets(widget).join("->");
-      if (!text.isEmpty() && !isNumber && !wrapped) {
-        FAIL(("\"" + text + "\" must be wrapped. Parent widgets: " + parentWidgets).toStdString());
-      }
-      // dynamic if source string wrapped, but UI adds text
-      if (wrapped && text != TEST_TEXT) {
-        WARN(("\"" + text + "\" is dynamic and needs a custom retranslate function. Parent widgets: " + parentWidgets).toStdString());
-      }
+    qWarning() << text;
+    bool isNumber = RE_NUM.exactMatch(text);
+    bool wrapped = text.contains(TEST_TEXT);
+    QString parentWidgets = getParentWidgets(widget).join("->");
+    if (!text.isEmpty() && !isNumber && !wrapped) {
+      FAIL(("\"" + text + "\" must be wrapped. Parent widgets: " + parentWidgets).toStdString());
+    }
+    // dynamic if source string wrapped, but UI adds text
+    if (wrapped && text != TEST_TEXT) {
+      WARN(("\"" + text + "\" is dynamic and needs a custom retranslate function. Parent widgets: " + parentWidgets).toStdString());
     }
   }
 }
 
 TEST_CASE("tr() wrap all strings") {
+  qWarning() << "TestCase";
   MainWindow w;
+  qWarning() << "MainWindow";
   checkTextWidgetType<QPushButton*>(w);
+  qWarning() << "QPushButton";
   checkTextWidgetType<QLabel*>(w);
+  qWarning() << "QLabel";
 }
