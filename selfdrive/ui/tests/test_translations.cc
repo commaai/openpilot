781d38eb57a7f491bc5a486fdb38148037e53883
Shane Smiskol <shane@smiskol.com>
2022-06-27 19:53:49 -0700

    add section so it prints multiple errors

--- selfdrive/ui/tests/test_translations.cc
+++ selfdrive/ui/tests/test_translations.cc
@@ -17,15 +17,17 @@ template <typename T>
 void checkTextWidgetType(MainWindow &w) {
   for (auto widget : w.findChildren<T>()) {
     const QString text = widget->text();
-    bool isNumber = RE_NUM.exactMatch(text);
-    bool wrapped = text.contains(TEST_TEXT);
-    QString parentWidgets = getParentWidgets(widget).join("->");
-    if (!text.isEmpty() && !isNumber && !wrapped) {
-      FAIL(("\"" + text + "\" must be wrapped. Parent widgets: " + parentWidgets).toStdString());
-    }
-    // dynamic if source string wrapped, but UI adds text
-    if (wrapped && text != TEST_TEXT) {
-      WARN(("\"" + text + "\" is dynamic and needs a custom retranslate function. Parent widgets: " + parentWidgets).toStdString());
+    SECTION(text.toStdString()) {
+      bool isNumber = RE_NUM.exactMatch(text);
+      bool wrapped = text.contains(TEST_TEXT);
+      QString parentWidgets = getParentWidgets(widget).join("->");
+      if (!text.isEmpty() && !isNumber && !wrapped) {
+        FAIL(("\"" + text + "\" must be wrapped. Parent widgets: " + parentWidgets).toStdString());
+      }
+      // dynamic if source string wrapped, but UI adds text
+      if (wrapped && text != TEST_TEXT) {
+        WARN(("\"" + text + "\" is dynamic and needs a custom retranslate function. Parent widgets: " + parentWidgets).toStdString());
+      }
     }
   }
 }
