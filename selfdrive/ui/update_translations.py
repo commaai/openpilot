cb82e76c75c476d4625416409e2d0a73dca9c5a0
Shane Smiskol <shane@smiskol.com>
2022-06-30 02:02:53 -0700

    Multilang prerequisites (#24999)
    * some supporting code for multilang
    
    * for now just english
    
    * test for missing language files
    
    * test for checking if ts file is up to date
    
    * Skip test if causes exception, other test catches this
    
    Test test
    
    Should also work
    
    should now fail
    
    revert
    
    rmn
    
    * add to files_common
    
    * fix files_common
    
    * newlines
    
    * no need to not update
    
    * comment
    
    * only english
    
    * double quotes
    
    * switch around
    old-commit-hash: 7178800d844469601cb779d8f76b568ff82ac766

---
+++ selfdrive/ui/update_translations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import json
+
+from common.basedir import BASEDIR
+
+UI_DIR = os.path.join(BASEDIR, "selfdrive", "ui")
+TRANSLATIONS_DIR = os.path.join(UI_DIR, "translations")
+LANGUAGES_FILE = os.path.join(TRANSLATIONS_DIR, "languages.json")
+
+
+def update_translations(release=False, suffix=""):
+  with open(LANGUAGES_FILE, "r") as f:
+    translation_files = json.load(f)
+
+  for name, file in translation_files.items():
+    if not len(file):
+      print(f"{name} has no translation file, skipping...")
+      continue
+
+    tr_file = os.path.join(TRANSLATIONS_DIR, f"{file}{suffix}.ts")
+    ret = os.system(f"lupdate -recursive {UI_DIR} -ts {tr_file}")
+    assert ret == 0
+
+    if release:
+      ret = os.system(f"lrelease {tr_file}")
+      assert ret == 0
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Update translation files for UI",
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument("--release", action="store_true", help="Create compiled QM translation files used by UI")
+  args = parser.parse_args()
+
+  update_translations(args.release)
