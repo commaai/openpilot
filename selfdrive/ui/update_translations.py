92459bf6e74aec130af7b96d099a4f0402e7ea86
Shane Smiskol <shane@smiskol.com>
2022-06-29 21:49:29 -0700

    no need to not update

--- selfdrive/ui/update_translations.py
+++ selfdrive/ui/update_translations.py
@@ -10,7 +10,7 @@ TRANSLATIONS_DIR = os.path.join(UI_DIR, 'translations')
 LANGUAGES_FILE = os.path.join(TRANSLATIONS_DIR, 'languages.json')
 
 
-def update_translations(no_update=False, release=False, suffix=''):
+def update_translations(release=False, suffix=''):
   with open(LANGUAGES_FILE, 'r') as f:
     translation_files = json.load(f)
 
@@ -18,14 +18,11 @@ def update_translations(no_update=False, release=False, suffix=''):
     if not len(file):
       print(f'{name} translation has no file, skipping...')
       continue
+
     tr_file = os.path.join(TRANSLATIONS_DIR, f'{file}{suffix}.ts')
-    if not no_update:
-      # if os.path.exists(tr_file):
-      #   print(f'Updating {name} translation: {tr_file}')
-      # else:
-      #   print(f'Creating {name} translation: {tr_file}')
-      ret = os.system(f'lupdate -recursive {UI_DIR} -ts {tr_file}')
-      assert ret == 0
+    ret = os.system(f'lupdate -recursive {UI_DIR} -ts {tr_file}')
+    assert ret == 0
+
     if release:
       ret = os.system(f'lrelease {tr_file}')
       assert ret == 0
@@ -34,8 +31,7 @@ def update_translations(no_update=False, release=False, suffix=''):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Update translation files for UI',
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('--no-update', action='store_true', help='Skip updating ts translation files')
   parser.add_argument('--release', action='store_true', help='Create final qm translation files used by UI')
   args = parser.parse_args()
 
-  update_translations(args.no_update, args.release)
+  update_translations(args.release)
