8b32e1b060e8c2c3c084393a569d4e78992ca598
Jason Shuler <jshuler@gmail.com>
2022-06-30 18:26:25 -0400

    GM: Lower LKA loopback CAN Error timing threshold to accommodate dropped packets (#24927)
    * LKA loopback timing to 10Hz
    
    * Typo
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -133,7 +133,9 @@ class CarState(CarStateBase):
     ]
 
     checks = [
-      ("ASCMLKASteeringCmd", 50),
+      ("ASCMLKASteeringCmd", 10), # 10 Hz is the stock inactive rate (every 100ms). 
+      #                             While active 50 Hz (every 20 ms) is normal
+      #                             EPS will tolerate around 200ms when active before faulting 
     ]
 
     return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, CanBus.LOOPBACK)
