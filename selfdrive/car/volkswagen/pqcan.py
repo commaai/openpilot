1528c87158e1af76f413d6c1043cfc06e0bb32d8
Jason Young <jyoung8607@gmail.com>
2022-07-02 14:52:53 -0400

    counters now directly supported in opendbc

--- selfdrive/car/volkswagen/pqcan.py
+++ selfdrive/car/volkswagen/pqcan.py
@@ -1,13 +1,12 @@
 def create_pq_steering_control(packer, bus, apply_steer, idx, lkas_enabled):
   values = {
-    "COUNTER": idx,
     "LM_Offset": abs(apply_steer),
     "LM_OffSign": 1 if apply_steer < 0 else 0,
     "HCA_Status": 5 if (lkas_enabled and apply_steer != 0) else 3,
     "Vib_Freq": 16,
   }
 
-  return packer.make_can_msg("HCA_1", bus, values)
+  return packer.make_can_msg("HCA_1", bus, values, idx)
 
 def create_pq_hud_control(packer, bus, hca_enabled, steering_pressed, hud_alert, left_lane_visible, right_lane_visible,
                           ldw_stock_values, left_lane_depart, right_lane_depart):
@@ -30,10 +29,9 @@ def create_pq_hud_control(packer, bus, hca_enabled, steering_pressed, hud_alert,
 
 def create_pq_acc_buttons_control(packer, bus, buttonStatesToSend, CS, idx):
   values = {
-    "COUNTER": idx,
     "GRA_Sender": CS.graSenderCoding,
     "GRA_Abbrechen": 1 if (buttonStatesToSend["cancel"] or CS.buttonStates["cancel"]) else 0,
     "GRA_Hauptschalt": CS.graHauptschalter,
   }
 
-  return packer.make_can_msg("GRA_Neu", bus, values)
+  return packer.make_can_msg("GRA_Neu", bus, values, idx)
