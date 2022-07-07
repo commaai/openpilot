7d042460a49d814fede6d126a614cb3e6ea9c4ab
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-07-06 23:42:07 -0700

    Ram 1500 (#24878)
    * RamInit
    
    * bump submodules
    
    * lil cleanup
    
    * clean up carstate formatting and platform grouping
    
    make tuple
    
    * give it a gold torque star (looks around 2.4 from rough data)
    
    * Dasm Fault
    
    * bump panda
    
    * more cleanup
    
    * cleanup car state
    
    * more cleanup
    
    * some fixes
    
    * remove more stuff
    
    * fix angle signal scaling and fix lkas control bit
    
    * bump panda
    
    * update those
    
    * same limits as pacifica
    
    * cleanup hud alert building
    
    * better fault logic
    
    * fix rate
    
    * set ahb
    
    * bring that back
    
    * update refs
    
    Co-authored-by: Jonathan <jraycec@gmail.com>
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 9b0acacf5e387593ce94dbad88000b5473511e22

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -1,8 +1,7 @@
-from cereal import car
 from opendbc.can.packer import CANPacker
 from selfdrive.car import apply_toyota_steer_torque_limits
 from selfdrive.car.chrysler.chryslercan import create_lkas_hud, create_lkas_command, create_cruise_buttons
-from selfdrive.car.chrysler.values import CAR, CarControllerParams
+from selfdrive.car.chrysler.values import RAM_CARS, CarControllerParams
 
 
 class CarController:
@@ -10,61 +9,51 @@ class CarController:
     self.CP = CP
     self.apply_steer_last = 0
     self.frame = 0
-    self.prev_lkas_frame = -1
-    self.hud_count = 0
-    self.car_fingerprint = CP.carFingerprint
-    self.gone_fast_yet = False
     self.steer_rate_limited = False
+
+    self.hud_count = 0
+    self.last_lkas_falling_edge = 0
+    self.lkas_active_prev = False
 
     self.packer = CANPacker(dbc_name)
 
-  def update(self, CC, CS):
-    # this seems needed to avoid steering faults and to force the sync with the EPS counter
-    if self.prev_lkas_frame == CS.lkas_counter:
-      return car.CarControl.Actuators.new_message(), []
-
-    actuators = CC.actuators
-
-    # steer torque
-    new_steer = int(round(actuators.steer * CarControllerParams.STEER_MAX))
-    apply_steer = apply_toyota_steer_torque_limits(new_steer, self.apply_steer_last,
-                                                   CS.out.steeringTorqueEps, CarControllerParams)
-    self.steer_rate_limited = new_steer != apply_steer
-
-    moving_fast = CS.out.vEgo > self.CP.minSteerSpeed  # for status message
-    if CS.out.vEgo > (self.CP.minSteerSpeed - 0.5):  # for command high bit
-      self.gone_fast_yet = True
-    elif self.car_fingerprint in (CAR.PACIFICA_2019_HYBRID, CAR.PACIFICA_2020, CAR.JEEP_CHEROKEE_2019):
-      if CS.out.vEgo < (self.CP.minSteerSpeed - 3.0):
-        self.gone_fast_yet = False  # < 14.5m/s stock turns off this bit, but fine down to 13.5
-    lkas_active = moving_fast and CC.enabled
-
-    if not lkas_active:
-      apply_steer = 0
-
-    self.apply_steer_last = apply_steer
-
+  def update(self, CC, CS, low_speed_alert):
     can_sends = []
+
+    # EPS faults if LKAS re-enables too quickly
+    lkas_active = CC.latActive and not low_speed_alert and (self.frame - self.last_lkas_falling_edge > 200)
 
     # *** control msgs ***
 
     if CC.cruiseControl.cancel:
-      can_sends.append(create_cruise_buttons(self.packer, CS.button_counter + 1, cancel=True))
+      bus = 2 if self.CP.carFingerprint in RAM_CARS else 0
+      can_sends.append(create_cruise_buttons(self.packer, CS.button_counter + 1, bus, cancel=True))
 
-    # LKAS_HEARTBIT is forwarded by Panda so no need to send it here.
-    # frame is 100Hz (0.01s period)
-    if self.frame % 25 == 0:  # 0.25s period
+    # HUD alerts
+    if self.frame % 25 == 0:
       if CS.lkas_car_model != -1:
-        can_sends.append(create_lkas_hud(self.packer, CS.out.gearShifter, lkas_active,
-                                         CC.hudControl.visualAlert, self.hud_count, CS.lkas_car_model))
+        can_sends.append(create_lkas_hud(self.packer, self.CP, lkas_active, CC.hudControl.visualAlert, self.hud_count, CS.lkas_car_model, CS.auto_high_beam))
         self.hud_count += 1
 
-    can_sends.append(create_lkas_command(self.packer, int(apply_steer), self.gone_fast_yet, CS.lkas_counter))
+    # steering
+    if self.frame % 2 == 0:
+      # steer torque
+      new_steer = int(round(CC.actuators.steer * CarControllerParams.STEER_MAX))
+      apply_steer = apply_toyota_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, CarControllerParams)
+      if not lkas_active:
+        apply_steer = 0
+      self.steer_rate_limited = new_steer != apply_steer
+      self.apply_steer_last = apply_steer
+
+      idx = self.frame // 2
+      can_sends.append(create_lkas_command(self.packer, self.CP, int(apply_steer), lkas_active, idx))
 
     self.frame += 1
-    self.prev_lkas_frame = CS.lkas_counter
+    if not lkas_active and self.lkas_active_prev:
+      self.last_lkas_falling_edge = self.frame
+    self.lkas_active_prev = lkas_active
 
-    new_actuators = actuators.copy()
-    new_actuators.steer = apply_steer / CarControllerParams.STEER_MAX
+    new_actuators = CC.actuators.copy()
+    new_actuators.steer = self.apply_steer_last / CarControllerParams.STEER_MAX
 
     return new_actuators, can_sends
