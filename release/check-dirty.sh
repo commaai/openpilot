7f17f1d7ed1c05eca1f537ae53a79daf84fd2709
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-07-08 12:20:36 -0700

    test for dirty working tree

--- release/check-dirty.sh
+++ release/check-dirty.sh
@@ -5,6 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
 if [ ! -z "$(git status --porcelain)" ]; then
-  echo "Dirty "
+  echo "Dirty working tree after build:"
+  git status --porcelain
   exit 1
 fi
