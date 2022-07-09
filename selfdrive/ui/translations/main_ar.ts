a2761ee1543ec94a662a89671d0aeaf71fa1f991
Shane Smiskol <shane@smiskol.com>
2022-07-09 01:57:47 -0700

    Fix language code

--- selfdrive/ui/translations/main_ar.ts
+++ selfdrive/ui/translations/main_ar.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko_KR">
+<TS version="2.1" language="ar">
 <context>
     <name>AbstractAlert</name>
     <message>
