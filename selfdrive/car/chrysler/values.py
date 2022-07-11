bdfaa1d1eee77d1f9fb5b9b2632c9988caba02e3
HaraldSchafer <harald.the.engineer@gmail.com>
2022-07-11 15:19:55 -0700

    Ram 1500 torque tune (#25117)
    * torque control again
    
    * 3mss per s
    
    * no bad sensors
    
    * tweaks
    
    * Need more checks before we can do this
    
    * update refs
    
    * only ram for now
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -26,10 +26,16 @@ class CAR:
 
 
 class CarControllerParams:
-  STEER_MAX = 261  # higher than this faults the EPS on Chrysler/Jeep. Ram DT allows more
-  STEER_DELTA_UP = 3
-  STEER_DELTA_DOWN = 3
-  STEER_ERROR_MAX = 80
+  def __init__(self, CP):
+    self.STEER_MAX = 261  # higher than this faults the EPS on Chrysler/Jeep. Ram DT allows more
+    self.STEER_ERROR_MAX = 80
+
+    if CP.carFingerprint in RAM_CARS:
+      self.STEER_DELTA_UP = 5
+      self.STEER_DELTA_DOWN = 5
+    else:
+      self.STEER_DELTA_UP = 3
+      self.STEER_DELTA_DOWN = 3
 
 STEER_THRESHOLD = 120
 
