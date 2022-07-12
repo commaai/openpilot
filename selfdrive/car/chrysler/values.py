ecac734160f719f59364a637b7cd781460456845
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-07-12 13:51:25 -0700

    update compatibility docs with VIN data (#25134)
    * start with genesis
    
    * chrysler
    
    * honda
    
    * toyota
    
    * subaru

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -53,8 +53,8 @@ CAR_INFO: Dict[str, Optional[Union[ChryslerCarInfo, List[ChryslerCarInfo]]]] = {
   CAR.PACIFICA_2018: ChryslerCarInfo("Chrysler Pacifica 2017-18"),
   CAR.PACIFICA_2020: ChryslerCarInfo("Chrysler Pacifica 2019-20"),
   CAR.JEEP_CHEROKEE: ChryslerCarInfo("Jeep Grand Cherokee 2016-18", video_link="https://www.youtube.com/watch?v=eLR9o2JkuRk"),
-  CAR.JEEP_CHEROKEE_2019: ChryslerCarInfo("Jeep Grand Cherokee 2019-20", video_link="https://www.youtube.com/watch?v=jBe4lWnRSu4"),
-  CAR.RAM_1500: ChryslerCarInfo("Ram 1500 2019-21"),
+  CAR.JEEP_CHEROKEE_2019: ChryslerCarInfo("Jeep Grand Cherokee 2019-21", video_link="https://www.youtube.com/watch?v=jBe4lWnRSu4"),
+  CAR.RAM_1500: ChryslerCarInfo("Ram 1500 2019-22"),
 }
 
 # Unique CAN messages:
