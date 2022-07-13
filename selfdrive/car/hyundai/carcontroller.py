82852340716e74418adec282b593be00d5099a4a
zunichky <zunichky@msu.edu>
2022-07-12 20:46:58 -0400

    Rework controller  to add back original busses

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -73,21 +73,32 @@ class CarController:
 
     if self.CP.carFingerprint in HDA2_CAR:
       # steering control
-      can_sends.append(hda2can.create_lkas(self.packer, CC.enabled, self.frame, CC.latActive, apply_steer))
+      if self.CP.carFingerprint in CAR.GENESIS_GV70:
+        can_sends.append(hda2can.create_lfa(self.packer, CC.enabled, self.frame, CC.latActive, apply_steer))
+      else:
+        can_sends.append(hda2can.create_lkas(self.packer, CC.enabled, self.frame, CC.latActive, apply_steer))
 
       if self.frame % 5 == 0:
         can_sends.append(hda2can.create_cam_0x2a4(self.packer, self.frame, CS.cam_0x2a4))
 
       # cruise cancel
       if (self.frame - self.last_button_frame) * DT_CTRL > 0.25:
+        #TODO: Improve this logic. Don't like bringing 
+        # the bus logic out here, but it's probably
+        # better than creating a new function
+        if self.CP.carFingerprint in CAR.GENESIS_GV70:
+          bus=4
+        else:
+          bus=5
+        
         if CC.cruiseControl.cancel:
           for _ in range(20):
-            can_sends.append(hda2can.create_buttons(self.packer, CS.buttons_counter+1, True, False))
+            can_sends.append(hda2can.create_buttons(self.packer, CS.buttons_counter+1, True, False, bus))
           self.last_button_frame = self.frame
 
         # cruise standstill resume
         elif CC.cruiseControl.resume:
-          can_sends.append(hda2can.create_buttons(self.packer, CS.buttons_counter+1, False, True))
+          can_sends.append(hda2can.create_buttons(self.packer, CS.buttons_counter+1, False, True, bus))
           self.last_button_frame = self.frame
     else:
 
--- selfdrive/car/hyundai/hda2can.py
+++ selfdrive/car/hyundai/hda2can.py
@@ -10,9 +10,21 @@ def create_lkas(packer, enabled, frame, lat_active, apply_steer):
     "NEW_SIGNAL_1": 0,
     "NEW_SIGNAL_2": 0,
   }
-  #return packer.make_can_msg("LKAS", 4, values, frame % 255)
-  return packer.make_can_msg("LFA", 4, values, frame % 255)  #12A
+  return packer.make_can_msg("LKAS", 4, values, frame % 255)
 
+def create_lfa(packer, enabled, frame, lat_active, apply_steer):
+  values = {
+    "LKA_MODE": 2,
+    "LKA_ICON": 2 if enabled else 1,
+    "TORQUE_REQUEST": apply_steer,
+    "LKA_ASSIST": 0,
+    "STEER_REQ": 1 if lat_active else 0,
+    "STEER_MODE": 0,
+    "SET_ME_1": 0,
+    "NEW_SIGNAL_1": 0,
+    "NEW_SIGNAL_2": 0,
+  }
+  return packer.make_can_msg("LFA", 4, values, frame % 255)
 
 def create_cam_0x2a4(packer, frame, camera_values):
   camera_values.update({
@@ -20,11 +32,11 @@ def create_cam_0x2a4(packer, frame, camera_values):
   })
   return packer.make_can_msg("CAM_0x2a4", 4, camera_values, frame % 255)
 
-def create_buttons(packer, cnt, cancel, resume):
+def create_buttons(packer, cnt, cancel, resume, bus=5):
   values = {
     "_COUNTER": cnt % 0xf,
     "SET_ME_1": 1,
     "DISTANCE_BTN": 1 if resume else 0,
     "PAUSE_RESUME_BTN": 1 if cancel else 0,
   }
-  return packer.make_can_msg("CRUISE_BUTTONS", 4, values) #ICF
+  return packer.make_can_msg("CRUISE_BUTTONS", bus, values)
