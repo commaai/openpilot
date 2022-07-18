a352580ecd2086fc454065684b4b42ada0cdb95a
Willem Melching <willem.melching@gmail.com>
2022-07-18 17:26:50 +0200

    fix controlsd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -163,6 +163,7 @@ class Controls:
     self.can_rcv_error = False
     self.soft_disable_timer = 0
     self.v_cruise_kph = 255
+    self.v_cruise_cluster_kph = 255
     self.v_cruise_kph_last = 0
     self.mismatch_counter = 0
     self.cruise_mismatch_counter = 0
