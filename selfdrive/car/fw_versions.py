c788dedb43c835da5ca42a41d85f9bfeaa52a0e6
Shane Smiskol <shane@smiskol.com>
2022-07-18 15:11:11 -0700

    comment

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -336,8 +336,7 @@ def match_fw_to_car(fw_versions, allow_fuzzy=True):
     exact_matches.append((False, match_fw_to_car_fuzzy))
 
   for exact_match, match_func in exact_matches:
-    # TODO: For each brand, attempt to fingerprint using FW returned from its queries
-    # Until erroneous FW versions are removed, try to fingerprint on all possible combinations
+    # TODO: For each brand, attempt to fingerprint using only FW returned from its queries
     matches = set()
     fw_versions_dict = build_fw_dict(fw_versions, filter_brand=None)
     matches |= match_func(fw_versions_dict)
