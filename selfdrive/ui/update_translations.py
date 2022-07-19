4a3dd13898cd9645ca7fdcbdef1c0bd1b3b27e20
Shane Smiskol <shane@smiskol.com>
2022-07-19 12:38:58 -0700

    Multilanguage: readme improvements (#25214)
    * catch2 needs to iterate through the whole function each time to test this (spams warnings)
    
    * Update readme
    
    * Add update the ui header
    
    * build in scons
    
    * release by default
    
    * scons in another pr
    
    Co-authored-by: Vincent Wright <vincentw@kmwperformance.com>
    old-commit-hash: 464db126a551639b036c4a5e4e494684c68f56f2

--- selfdrive/ui/update_translations.py
+++ selfdrive/ui/update_translations.py
@@ -10,7 +10,7 @@ TRANSLATIONS_DIR = os.path.join(UI_DIR, "translations")
 LANGUAGES_FILE = os.path.join(TRANSLATIONS_DIR, "languages.json")
 
 
-def update_translations(release=False, vanish=False, translations_dir=TRANSLATIONS_DIR):
+def update_translations(vanish=False, translations_dir=TRANSLATIONS_DIR):
   with open(LANGUAGES_FILE, "r") as f:
     translation_files = json.load(f)
 
@@ -26,16 +26,14 @@ def update_translations(release=False, vanish=False, translations_dir=TRANSLATIO
     ret = os.system(args)
     assert ret == 0
 
-    if release:
-      ret = os.system(f"lrelease {tr_file}")
-      assert ret == 0
+    ret = os.system(f"lrelease {tr_file}")
+    assert ret == 0
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Update translation files for UI",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument("--release", action="store_true", help="Create compiled QM translation files used by UI")
   parser.add_argument("--vanish", action="store_true", help="Remove translations with source text no longer found")
   args = parser.parse_args()
 
-  update_translations(args.release, args.vanish)
+  update_translations(args.vanish)
