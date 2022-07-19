95df2c9b485f20259bb672769a3a00b4791ef4dd
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-07-18 21:18:10 -0700

    updated: add branch switching (#25200)
    * switch param
    
    * add to ui
    
    * fix that
    
    * fetch for new branch
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: cc3857eb62cd169be05a4cb22c844a8b2ec94139

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -20,7 +20,7 @@ from selfdrive.manager.process_config import managed_processes
 from selfdrive.athena.registration import register, UNREGISTERED_DONGLE_ID
 from system.swaglog import cloudlog, add_file_handler
 from system.version import is_dirty, get_commit, get_version, get_origin, get_short_branch, \
-                              terms_version, training_version
+                              terms_version, training_version, is_tested_branch
 
 
 sys.path.append(os.path.join(BASEDIR, "pyextra"))
@@ -78,6 +78,7 @@ def manager_init() -> None:
   params.put("GitCommit", get_commit(default=""))
   params.put("GitBranch", get_short_branch(default=""))
   params.put("GitRemote", get_origin(default=""))
+  params.put_bool("IsTestedBranch", is_tested_branch())
 
   # set dongle id
   reg_res = register(show_spinner=True)
