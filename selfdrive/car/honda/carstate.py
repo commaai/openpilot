ad2c78d9d710a575d7f6bbbe21255fd5bce70959
Willem Melching <willem.melching@gmail.com>
2022-07-20 16:59:48 +0200

    use dash value and apply conversion

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -94,7 +94,10 @@ def get_can_signals(CP, gearbox_msg, main_on_sig_msg):
         ("ACC_CONTROL", 50),
       ]
 
-    signals += [("CAR_SPEED", "CAR_SPEED")]
+    signals += [
+      ("CAR_SPEED", "CAR_SPEED"),
+      ("IMPERIAL_UNIT", "CAR_SPEED"),
+    ]
     checks += [("CAR_SPEED", 10)]
 
   else:  # Nidec signals
@@ -208,9 +211,8 @@ class CarState(CarStateBase):
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
 
     if self.CP.carFingerprint in HONDA_BOSCH:
-      # Actual value shown on dashboard, only 2Hz, kph/mph based on ["CAR_SPEED"]["IMPERIAL_UNIT"]
-      # ret.vEgoCluster = cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] * CV.KPH_TO_MS
-      ret.vEgoCluster = cp.vl["CAR_SPEED"]["CAR_SPEED"] * CV.KPH_TO_MS
+      conversion = CV.MPH_TO_MS if cp.vl["CAR_SPEED"]["IMPERIAL_UNIT"] else CV.KPH_TO_MS
+      ret.vEgoCluster = cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] * conversion
 
     ret.steeringAngleDeg = cp.vl["STEERING_SENSORS"]["STEER_ANGLE"]
     ret.steeringRateDeg = cp.vl["STEERING_SENSORS"]["STEER_ANGLE_RATE"]
