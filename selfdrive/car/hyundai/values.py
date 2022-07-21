2d23b624a527ebad352f5fc5cb9883c7708b9b9a
Christian Gribneau <christian@gribneau.net>
2022-07-21 17:11:04 -0400

    Hyundai: add Genesis G90 2017 FW versions (#25183)
    * v2 fingerprint 2017 Genesis G90 Ultimate 5.0 AWD
    
    * remove Genesis G90 from 255 steering torque blocklist
    
    * Update selfdrive/car/hyundai/values.py
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    old-commit-hash: aec9b2cb8a65b2631b385de3a7a09dcc2342d584

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -858,6 +858,12 @@ FW_VERSIONS = {
       b'\xf1\x81640H0051\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
   },
+  CAR.GENESIS_G90: {
+    (Ecu.transmission, 0x7e1, None): [b'\xf1\x87VDGMD15866192DD3x\x88x\x89wuFvvfUf\x88vWwgwwwvfVgx\x87o\xff\xbc^\xf1\x81E14\x00\x00\x00\x00\x00\x00\x00\xf1\x00bcshcm49  E14\x00\x00\x00\x00\x00\x00\x00SHI0G50NB1tc5\xb7'],
+    (Ecu.fwdRadar, 0x7d0, None): [b'\xf1\x00HI__ SCC F-CUP      1.00 1.01 96400-D2100         '],
+    (Ecu.fwdCamera, 0x7c4, None): [b'\xf1\x00HI  LKAS AT USA LHD 1.00 1.00 95895-D2020 160302'],
+    (Ecu.engine, 0x7e0, None): [b'\xf1\x810000000000\x00'],
+  },
   CAR.KONA: {
     (Ecu.fwdRadar, 0x7d0, None): [b'\xf1\x00OS__ SCC F-CUP      1.00 1.00 95655-J9200         ', ],
     (Ecu.esp, 0x7d1, None): [b'\xf1\x816V5RAK00018.ELF\xf1\x00\x00\x00\x00\x00\x00\x00', ],
