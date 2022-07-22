fabeb016f080752d0b2e8aa6f8d2ff8e472ceaa4
Shane Smiskol <shane@smiskol.com>
2022-07-22 02:24:53 -0700

    now try a push

--- selfdrive/ui/translations/create_badges.py
+++ selfdrive/ui/translations/create_badges.py
@@ -28,13 +28,12 @@ if __name__ == "__main__":
       if UNFINISHED_TRANSLATION_TAG in line:
         unfinished_translations += 1
 
-    percent_finished = (1 - unfinished_translations / total_translations) * 100.
+    percent_finished = (1 - .5485) * 100.
     percent_finished = int(percent_finished * 10) / 10  # round down (99.99% shouldn't be 100%)
     color = "green" if percent_finished == 100 else "yellow" if percent_finished >= 70 else "red"
 
-    # TODO: just edit a local badge svg
     r = requests.get(f"https://img.shields.io/badge/LANGUAGE {name}-FINISHED: {percent_finished}%25-{color}")
-    assert r.status_code != 200, "Error downloading badge"
+    assert r.status_code == 200, "Error downloading badge"
 
     with open(os.path.join(BASEDIR, f"translation_badge_{file}.svg"), "wb") as f:
       f.write(r.content)
