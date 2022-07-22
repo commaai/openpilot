ab89c14f8cf29e9220a9918d7d3a1863e90830a0
Shane Smiskol <shane@smiskol.com>
2022-07-22 01:47:07 -0700

    draft

---
+++ selfdrive/ui/translations/create_badges.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import os
+import json
+import requests
+
+from common.basedir import BASEDIR
+from selfdrive.ui.update_translations import TRANSLATIONS_DIR, LANGUAGES_FILE
+
+TRANSLATION_TAG = "<translation"
+UNFINISHED_TRANSLATION_TAG = '<translation type="unfinished"'
+
+if __name__ == "__main__":
+  with open(LANGUAGES_FILE, "r") as f:
+    translation_files = json.load(f)
+
+  for name, file in translation_files.items():
+    if not len(file):
+      continue
+
+    with open(os.path.join(TRANSLATIONS_DIR, f"{file}.ts"), "r") as f:
+      tr_file = f.read()
+
+    total_translations = 0
+    unfinished_translations = 0
+    for line in tr_file.splitlines():
+      if TRANSLATION_TAG in line:
+        total_translations += 1
+      if UNFINISHED_TRANSLATION_TAG in line:
+        unfinished_translations += 1
+
+    percent_finished = (1 - unfinished_translations / total_translations) * 100.
+    percent_finished = int(percent_finished * 10) / 10  # round down (99.99% shouldn't be 100%)
+    if percent_finished == 100:
+      color = "green"
+    elif percent_finished >= 70:
+      color = "yellow"
+    else:
+      color = "red"
+
+    # TODO: just edit a local badge svg
+    r = requests.get(f"https://img.shields.io/badge/LANGUAGE {name}-FINISHED: {percent_finished}%25-{color}")
+    assert r.status_code == 200, "Error downloading badge"
+
+    with open(os.path.join(BASEDIR, f"translation_badge_{file}.svg"), "wb") as f:
+      f.write(r.content)
---
+++ selfdrive/ui/translations/create_badges.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+languages=( one two three )
