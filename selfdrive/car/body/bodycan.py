039a3a224a62b0e8b2ae48bbe869fcba60baedf8
Comma Device <device@comma.ai>
2022-07-27 14:57:56 -0700

    no need for idx

--- selfdrive/car/body/bodycan.py
+++ selfdrive/car/body/bodycan.py
@@ -6,7 +6,7 @@ def create_control(packer, torque_l, torque_r):
 
   return packer.make_can_msg("TORQUE_CMD", 0, values)
 
-def create_rpm_limit(packer, limit_l, limit_r, idx):
+def create_rpm_limit(packer, limit_l, limit_r):
   values = {
     "MAX_RPM_L": limit_l,
     "MAX_RPM_R": limit_r,
