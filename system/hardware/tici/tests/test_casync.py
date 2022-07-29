1ec263402e47733f8dd1fb65fda7a03bf963471a
Willem Melching <willem.melching@gmail.com>
2022-07-29 15:09:53 +0200

    test for chunk reuse

--- system/hardware/tici/tests/test_casync.py
+++ system/hardware/tici/tests/test_casync.py
@@ -13,12 +13,14 @@ class TestCasync(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
     cls.tmpdir = tempfile.TemporaryDirectory()
+    print(cls.tmpdir.name)
 
     # Build example contents
-    contents = []
-    for _ in range(128):
-      contents += [i for i in range(256)]
-    contents += [0] * 1024
+    chunk_a = [i % 256 for i in range(1024)] * 512
+    chunk_b = [(256 - i) % 256 for i in range(1024)] * 512
+    zeroes = [0] * (1024 * 128)
+    contents = chunk_a + chunk_b + zeroes + chunk_a
+
     cls.contents = bytes(contents)
 
     # Write to file
@@ -30,6 +32,12 @@ class TestCasync(unittest.TestCase):
     cls.manifest_fn = os.path.join(cls.tmpdir.name, 'orig.caibx')
     cls.store_fn = os.path.join(cls.tmpdir.name, 'store')
     subprocess.check_output(["casync", "make", "--compression=xz", "--store", cls.store_fn, cls.manifest_fn, cls.orig_fn])
+
+    target = casync.parse_caibx(cls.manifest_fn)
+    hashes = [c.sha.hex() for c in target]
+
+    # Ensure we have chunk reuse
+    assert len(hashes) > len(set(hashes))
 
   def setUp(self):
     if LOOPBACK is not None:
@@ -57,6 +65,7 @@ class TestCasync(unittest.TestCase):
 
   @unittest.skipUnless(LOOPBACK, "requires loopback device")
   def test_lo_already_done(self):
+    """Test that an already flashed target doesn't download any chunks"""
     target = casync.parse_caibx(self.manifest_fn)
     sources = []
 
@@ -72,6 +81,22 @@ class TestCasync(unittest.TestCase):
       self.assertEqual(f.read(len(self.contents)), self.contents)
 
     self.assertEqual(stats['target'], len(self.contents))
+
+  @unittest.skipUnless(LOOPBACK, "requires loopback device")
+  def test_lo_chunk_reuse(self):
+    """Test that chunks that are reused are only downloaded once"""
+    target = casync.parse_caibx(self.manifest_fn)
+    sources = []
+
+    sources += [('target', casync.FileChunkReader(self.target_lo), casync.build_chunk_dict(target))]
+    sources += [('remote', casync.RemoteChunkReader(self.store_fn), casync.build_chunk_dict(target))]
+
+    stats = casync.extract(target, sources, self.target_lo)
+
+    with open(self.target_lo, 'rb') as f:
+      self.assertEqual(f.read(len(self.contents)), self.contents)
+
+    self.assertLess(stats['remote'], len(self.contents))
 
 
 if __name__ == "__main__":
