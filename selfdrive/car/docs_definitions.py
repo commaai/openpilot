81688422e5b2b58ecd3972d08055f41257604b3d
Shane Smiskol <shane@smiskol.com>
2022-08-02 02:19:14 -0700

    Maintain min enable speed of 0

--- selfdrive/car/docs_definitions.py
+++ selfdrive/car/docs_definitions.py
@@ -152,7 +152,9 @@ class CarInfo:
       if self.min_enable_speed > 0:
         acc = f" <strong>while driving above {self.min_enable_speed * CV.MS_TO_MPH:.0f} mph</strong>"
       elif CP.carName not in ("nissan", "subaru", "toyota") or (CP.carName == "toyota" and CP.openpilotLongitudinalControl):
-        acc = " <strong>that automatically resumes from a stop</strong>"
+        acc = " that <strong>automatically resumes from a stop</strong>"
+      else:
+        acc = " <strong>to a stop</strong>"
 
       if self.row[Column.STEERING_TORQUE] != Star.FULL:
         sentence_builder += " This car may not be able to take tight turns on its own."
