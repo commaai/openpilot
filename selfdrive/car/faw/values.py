1af61a6a306602555674538751c9897214d55378
Jason Young <jyoung8607@gmail.com>
2022-08-09 16:47:25 -0400

    adjust ramp-rates and driver torque allowance

--- selfdrive/car/faw/values.py
+++ selfdrive/car/faw/values.py
@@ -15,9 +15,9 @@ class CarControllerParams:
 
   # TODO: placeholder values pending discovery of true EPS limits
   STEER_MAX = 300                # As-yet unknown fault boundary, guessing 300 / 3.0Nm for now
-  STEER_DELTA_UP = 3             # 3 unit/sec observed from factory LKAS, fault boundary unknown
-  STEER_DELTA_DOWN = 3           # 3 unit/sec observed from factory LKAS, fault boundary unknown
-  STEER_DRIVER_ALLOWANCE = 25
+  STEER_DELTA_UP = 6             # 10 unit/sec observed from factory LKAS, fault boundary unknown
+  STEER_DELTA_DOWN = 10          # 10 unit/sec observed from factory LKAS, fault boundary unknown
+  STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 3    # weight driver torque heavily
   STEER_DRIVER_FACTOR = 1        # from dbc
 
