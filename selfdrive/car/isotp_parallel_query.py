83406dd6a721eaabc867775d985dcdd9aff2b942
Jason Young <jyoung8607@gmail.com>
2022-08-10 15:38:02 -0400

    clean up timers on receipt of a non-0x78 UDS error

--- selfdrive/car/isotp_parallel_query.py
+++ selfdrive/car/isotp_parallel_query.py
@@ -135,6 +135,7 @@ class IsoTpParallelQuery:
             if self.debug:
               cloudlog.warning(f"iso-tp query response pending: {tx_addr}")
           else:
+            response_timeouts.pop(tx_addr, None)
             request_done[tx_addr] = True
             cloudlog.warning(f"iso-tp query bad response: {tx_addr} - 0x{dat.hex()}")
 
