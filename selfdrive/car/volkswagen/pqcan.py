35ac7f03f1894f29f3b7754a1b6dce57828aa8fa
Jason Young <jyoung8607@gmail.com>
2022-08-13 01:08:56 -0400

    consistency

--- selfdrive/car/volkswagen/pqcan.py
+++ selfdrive/car/volkswagen/pqcan.py
@@ -11,6 +11,7 @@ def create_steering_control(packer, bus, apply_steer, lkas_enabled):
 
 def create_lka_hud_control(packer, bus, ldw_stock_values, enabled, steering_pressed, hud_alert, hud_control):
   values = ldw_stock_values.copy()
+
   values.update({
     "LDW_Lampe_gelb": 1 if enabled and steering_pressed else 0,
     "LDW_Lampe_gruen": 1 if enabled and not steering_pressed else 0,
@@ -18,15 +19,18 @@ def create_lka_hud_control(packer, bus, ldw_stock_values, enabled, steering_pres
     "LDW_Lernmodus_rechts": 3 if hud_control.rightLaneDepart else 1 + hud_control.rightLaneVisible,
     "LDW_Textbits": hud_alert,
   })
+
   return packer.make_can_msg("LDW_Status", bus, values)
 
 
 def create_acc_buttons_control(packer, bus, gra_stock_values, idx, cancel=False, resume=False):
   values = gra_stock_values.copy()
 
-  values["COUNTER"] = idx
-  values["GRA_Abbrechen"] = cancel
-  values["GRA_Recall"] = resume
+  values.update({
+    "COUNTER": idx,
+    "GRA_Abbrechen": cancel,
+    "GRA_Recall": resume,
+  })
 
   return packer.make_can_msg("GRA_Neu", bus, values)
 
