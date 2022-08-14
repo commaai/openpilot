4412a7fee7cdec9dfe4b299d85c9066bb0a02022
Jason Wen <haibin.wen3@gmail.com>
2022-08-13 20:07:22 -0400

    Add comments

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -76,9 +76,10 @@ class CarController:
       if self.frame % 5 == 0 and self.CP.flags & HyundaiFlags.CANFD_HDA2:
         can_sends.append(hyundaicanfd.create_cam_0x2a4(self.packer, CS.cam_0x2a4))
 
-      # cruise cancel for non HDA CAN-FD
       if self.frame % 2 == 0 and not self.CP.flags & HyundaiFlags.CANFD_HDA2:
+        # cruise cancel for non HDA CAN-FD
         can_sends.append(hyundaicanfd.create_cruise_info(self.packer, CS.cruise_info_copy, CC.cruiseControl.cancel))
+        # LFA and HDA icons
         can_sends.append(hyundaicanfd.create_lfahda_cluster(self.packer, CC.enabled))
 
       # cruise cancel
