20c6f2758dc5bcfa25844944cd359a60b16e9278
Tim Wilson <twilsonco@gmail.com>
2022-08-16 11:40:39 -0600

    add liveParams pitch
    missed pitch
    
    cleanup
    
    bump cereal

--- selfdrive/locationd/models/constants.py
+++ selfdrive/locationd/models/constants.py
@@ -40,6 +40,7 @@ class ObservationKind:
   STEER_RATIO = 29  # [-]
   ROAD_FRAME_X_SPEED = 30  # (x) [m/s]
   ROAD_ROLL = 31  # [rad]
+  ROAD_PITCH = 36  # [rad]
 
   names = [
     'Unknown',
@@ -79,6 +80,7 @@ class ObservationKind:
     'NO accel',
     'ORB features wide camera',
     'ECEF_VEL',
+    'Road Pitch',
   ]
 
   @classmethod
