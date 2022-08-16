569de588d86f6e1830aea8399ba6ee63f4e065ad
Shane Smiskol <shane@smiskol.com>
2022-08-15 20:04:59 -0700

    rm that

--- selfdrive/test/process_replay/test_processes.py
+++ selfdrive/test/process_replay/test_processes.py
@@ -117,7 +117,6 @@ def format_diff(results, log_paths, ref_commit):
     for proc, diff in list(result.items()):
       # long diff
       diff2 += f"*** process: {proc} ***\n"
-      print(segment, proc)
       diff2 += f"\tref: {log_paths[segment][proc]['ref']}\n\n"
       diff2 += f"\tnew: {log_paths[segment][proc]['new']}\n\n"
 
