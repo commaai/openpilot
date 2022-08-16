599c57f5e390597a066fd74d2baa5d411cc25db9
Shane Smiskol <shane@smiskol.com>
2022-08-16 16:04:02 -0700

    Apply suggestions from code review

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -109,11 +109,11 @@ class CarController:
 
     else:
       # Stock longitudinal, integrated at camera
-      if (self.frame - self.last_button_frame) * DT_CTRL > 0.1:
+      if (self.frame - self.last_button_frame) * DT_CTRL > 0.2:
         if CC.cruiseControl.cancel:
           self.last_button_frame = self.frame
-          for i in range(5):
-            idx = (CS.buttons_counter + i + 1) % 4
+          idx = (CS.buttons_counter + 1) % 4
+          for i in range(10):
             can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.CAMERA, idx, CruiseButtons.CANCEL))
 
     # Show green icon when LKA torque is applied, and
