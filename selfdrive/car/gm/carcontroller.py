309d713c7c1a94ef2cd8b79a72c89dd1a295e4f0
Shane Smiskol <shane@smiskol.com>
2022-08-16 23:02:52 -0700

    never tried 10hz with same counter

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -109,10 +109,10 @@ class CarController:
 
     else:
       # Stock longitudinal, integrated at camera
-      if (self.frame - self.last_button_frame) * DT_CTRL > 0.04:
+      if (self.frame - self.last_button_frame) * DT_CTRL > 0.1:
         if CC.cruiseControl.cancel:
           self.last_button_frame = self.frame
-          idx = (CS.buttons_counter + 1) % 4
+          idx = (CS.buttons_counter + 0) % 4
           can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.CAMERA, idx, CruiseButtons.CANCEL))
 
     # Show green icon when LKA torque is applied, and
