3c3bf752db635f8e189bf89aac78c5bc021135cb
Igor Biletksyy <bs@privacy.im>
2022-08-17 21:31:53 -0700

    doc_definitions

--- selfdrive/car/docs_definitions.py
+++ selfdrive/car/docs_definitions.py
@@ -173,10 +173,7 @@ class CarInfo:
       return sentence_builder.format(car_model=f"{self.make} {self.model}", alc=alc, acc=acc)
 
     else:
-      if CP.carFingerprint == "COMMA BODY":
-        return "The body is a robotics dev kit that can run openpilot. <a href='https://www.commabody.com'>Learn more.</a>"
-      else:
-        raise Exception(f"This notCar does not have a detail sentence: {CP.carFingerprint}")
+      return "The body is a robotics dev kit that can run openpilot. <a href='https://www.commabody.com'>Learn more.</a>"
 
   def get_column(self, column: Column, star_icon: str, footnote_tag: str) -> str:
     item: Union[str, Star] = self.row[column]
