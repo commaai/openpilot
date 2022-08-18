747bcb013dabdb1f220dc97a37270b42cb4eae03
Shane Smiskol <shane@smiskol.com>
2022-08-18 15:12:18 -0700

    Car docs: show more information about cars (#25415)
    * back to actual information
    
    * remove some star code
    
    * round speeds
    
    * Try out showing lateral acceleration
    
    * remove that
    
    * Fix speed units
    
    * Add harness and try rounding to nearest half before scapping it
    
    fix
    
    ^2
    
    ^2
    
    * Add back steering torque star
    
    * Fix static analysis
    
    * auto-generate header
    
    * fix static analysis. set to Harness.none by default
    
    * rm

--- selfdrive/car/docs_definitions.py
+++ selfdrive/car/docs_definitions.py
@@ -2,7 +2,7 @@ import re
 from collections import namedtuple
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Dict, List, Optional, Tuple, Union, no_type_check
+from typing import Dict, List, Optional, Tuple, Union
 
 from cereal import car
 from common.conversions import Conversions as CV
@@ -15,20 +15,15 @@ NO_AUTO_RESUME_STOCK_LONG = {"toyota", "gm"}
 NO_AUTO_RESUME = NO_AUTO_RESUME_STOCK_LONG | {"nissan", "subaru"}
 
 
-class Tier(Enum):
-  GOLD = 0
-  SILVER = 1
-  BRONZE = 2
-
-
 class Column(Enum):
   MAKE = "Make"
   MODEL = "Model"
   PACKAGE = "Supported Package"
-  LONGITUDINAL = "openpilot ACC"
-  FSR_LONGITUDINAL = "Stop and Go"
-  FSR_STEERING = "Steer to 0"
+  LONGITUDINAL = "ACC"
+  FSR_LONGITUDINAL = "No ACC accel below"
+  FSR_STEERING = "No ALC below"
   STEERING_TORQUE = "Steering Torque"
+  HARNESS = "Harness"
 
 
 class Star(Enum):
@@ -37,9 +32,46 @@ class Star(Enum):
   EMPTY = "empty"
 
 
-StarColumns = list(Column)[3:]
-TierColumns = (Column.FSR_LONGITUDINAL, Column.FSR_STEERING, Column.STEERING_TORQUE)
-CarFootnote = namedtuple("CarFootnote", ["text", "column", "star"], defaults=[None])
+class Harness(Enum):
+  nidec = "Honda Nidec"
+  bosch_a = "Honda Bosch A"
+  bosch_b = "Honda Bosch B"
+  toyota = "Toyota"
+  subaru_a = "Subaru A"
+  subaru_b = "Subaru B"
+  fca = "FCA"
+  ram = "Ram"
+  vw = "VW"
+  j533 = "J533"
+  hyundai_a = "Hyundai A"
+  hyundai_b = "Hyundai B"
+  hyundai_c = "Hyundai C"
+  hyundai_d = "Hyundai D"
+  hyundai_e = "Hyundai E"
+  hyundai_f = "Hyundai F"
+  hyundai_g = "Hyundai G"
+  hyundai_h = "Hyundai H"
+  hyundai_i = "Hyundai I"
+  hyundai_j = "Hyundai J"
+  hyundai_k = "Hyundai K"
+  hyundai_l = "Hyundai L"
+  hyundai_m = "Hyundai M"
+  hyundai_n = "Hyundai N"
+  hyundai_o = "Hyundai O"
+  hyundai_p = "Hyundai P"
+  hyundai_q = "Hyundai Q"
+  custom = "Developer"
+  obd_ii = "OBD-II"
+  gm = "GM"
+  nissan_a = "Nissan A"
+  nissan_b = "Nissan B"
+  mazda = "Mazda"
+  ford_q3 = "Ford Q3"
+  ford_q4 = "Ford Q4"
+  none = "None"
+
+
+CarFootnote = namedtuple("CarFootnote", ["text", "column"], defaults=[None])
 
 
 def get_footnotes(footnotes: List[Enum], column: Column) -> List[Enum]:
@@ -83,7 +115,7 @@ class CarInfo:
   footnotes: List[Enum] = field(default_factory=list)
   min_steer_speed: Optional[float] = None
   min_enable_speed: Optional[float] = None
-  harness: Optional[Enum] = None
+  harness: Enum = Harness.none
 
   def init(self, CP: car.CarParams, all_footnotes: Dict[Enum, int]):
     # TODO: set all the min steer speeds in carParams and remove this
@@ -103,11 +135,11 @@ class CarInfo:
       Column.MAKE: self.make,
       Column.MODEL: self.model,
       Column.PACKAGE: self.package,
-      # StarColumns
-      Column.LONGITUDINAL: Star.FULL if CP.openpilotLongitudinalControl and not CP.radarOffCan else Star.EMPTY,
-      Column.FSR_LONGITUDINAL: Star.FULL if self.min_enable_speed <= 0. else Star.EMPTY,
-      Column.FSR_STEERING: Star.FULL if self.min_steer_speed <= 0. else Star.EMPTY,
+      Column.LONGITUDINAL: "openpilot" if CP.openpilotLongitudinalControl and not CP.radarOffCan else "Stock",
+      Column.FSR_LONGITUDINAL: f"{max(self.min_enable_speed * CV.MS_TO_MPH, 0):.0f} mph",
+      Column.FSR_STEERING: f"{max(self.min_steer_speed * CV.MS_TO_MPH, 0):.0f} mph",
       Column.STEERING_TORQUE: Star.EMPTY,
+      Column.HARNESS: self.harness.value,
     }
 
     # Set steering torque star from max lateral acceleration
@@ -115,26 +147,7 @@ class CarInfo:
     if CP.maxLateralAccel >= GOOD_TORQUE_THRESHOLD:
       self.row[Column.STEERING_TORQUE] = Star.FULL
 
-    if CP.notCar:
-      for col in StarColumns:
-        self.row[col] = Star.FULL
-
     self.all_footnotes = all_footnotes
-    for column in StarColumns:
-      # Demote if footnote specifies a star
-      for fn in get_footnotes(self.footnotes, column):
-        if fn.value.star is not None:
-          self.row[column] = fn.value.star
-
-    # openpilot ACC star doesn't count for tiers
-    full_stars = [s for col, s in self.row.items() if col in TierColumns].count(Star.FULL)
-    if full_stars == len(TierColumns):
-      self.tier = Tier.GOLD
-    elif full_stars == len(TierColumns) - 1:
-      self.tier = Tier.SILVER
-    else:
-      self.tier = Tier.BRONZE
-
     self.year_list = get_year_list(self.years)
     self.detail_sentence = self.get_detail_sentence(CP)
 
@@ -167,10 +180,9 @@ class CarInfo:
       else:
         raise Exception(f"This notCar does not have a detail sentence: {CP.carFingerprint}")
 
-  @no_type_check
   def get_column(self, column: Column, star_icon: str, footnote_tag: str) -> str:
     item: Union[str, Star] = self.row[column]
-    if column in StarColumns:
+    if isinstance(item, Star):
       item = star_icon.format(item.value)
     elif column == Column.MODEL and len(self.years):
       item += f" {self.years}"
@@ -182,61 +194,3 @@ class CarInfo:
 
     return item
 
-
-class Harness(Enum):
-  nidec = "Honda Nidec"
-  bosch_a = "Honda Bosch A"
-  bosch_b = "Honda Bosch B"
-  toyota = "Toyota"
-  subaru_a = "Subaru A"
-  subaru_b = "Subaru B"
-  fca = "FCA"
-  ram = "Ram"
-  vw = "VW"
-  j533 = "J533"
-  hyundai_a = "Hyundai A"
-  hyundai_b = "Hyundai B"
-  hyundai_c = "Hyundai C"
-  hyundai_d = "Hyundai D"
-  hyundai_e = "Hyundai E"
-  hyundai_f = "Hyundai F"
-  hyundai_g = "Hyundai G"
-  hyundai_h = "Hyundai H"
-  hyundai_i = "Hyundai I"
-  hyundai_j = "Hyundai J"
-  hyundai_k = "Hyundai K"
-  hyundai_l = "Hyundai L"
-  hyundai_m = "Hyundai M"
-  hyundai_n = "Hyundai N"
-  hyundai_o = "Hyundai O"
-  hyundai_p = "Hyundai P"
-  hyundai_q = "Hyundai Q"
-  custom = "Developer"
-  obd_ii = "OBD-II"
-  gm = "GM"
-  nissan_a = "Nissan A"
-  nissan_b = "Nissan B"
-  mazda = "Mazda"
-  ford_q3 = "Ford Q3"
-  ford_q4 = "Ford Q4"
-  none = "None"
-
-
-STAR_DESCRIPTIONS = {
-  "Gas & Brakes": {  # icon and row name
-    Column.FSR_LONGITUDINAL.value: [
-      [Star.FULL.value, "openpilot operates down to 0 mph."],
-      [Star.EMPTY.value, "openpilot operates only above a minimum speed. See your car's manual for the minimum speed."],
-    ],
-  },
-  "Steering": {
-    Column.FSR_STEERING.value: [
-      [Star.FULL.value, "openpilot can control the steering wheel down to 0 mph."],
-      [Star.EMPTY.value, "No steering control below certain speeds. See your car's manual for the minimum speed."],
-    ],
-    Column.STEERING_TORQUE.value: [
-      [Star.FULL.value, "Car has enough steering torque to comfortably take most highway turns."],
-      [Star.EMPTY.value, "Limited ability to make tighter turns."],
-    ],
-  },
-}
