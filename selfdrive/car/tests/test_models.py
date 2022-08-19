885eb3573e5fe9d8fd4b85406a9f49b4957fd109
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-08-19 13:44:07 -0700

    test models: check panda standstill

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -234,9 +234,7 @@ class TestCarModelBase(unittest.TestCase):
 
       checks['gasPressed'] += CS.gasPressed != self.safety.get_gas_pressed_prev()
       checks['cruiseState'] += CS.cruiseState.enabled and not CS.cruiseState.available
-      if self.CP.carName in ("honda", "toyota"):
-        # TODO: fix standstill mismatches for other makes
-        checks['standstill'] += CS.standstill == self.safety.get_vehicle_moving()
+      checks['standstill'] += CS.standstill == self.safety.get_vehicle_moving()
 
       # TODO: remove this exception once this mismatch is resolved
       brake_pressed = CS.brakePressed
