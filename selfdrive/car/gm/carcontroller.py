82c3a4d503705a89e7d9d12e4674d5f8aab2b9a6
Shane Smiskol <shane@smiskol.com>
2022-08-22 21:03:18 -0700

    Try GM auto-resume

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -113,6 +113,9 @@ class CarController:
         if CC.cruiseControl.cancel:
           self.last_button_frame = self.frame
           can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.CAMERA, CS.buttons_counter, CruiseButtons.CANCEL))
+        elif CC.cruiseControl.resume:
+          self.last_button_frame = self.frame
+          can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.CAMERA, CS.buttons_counter, CruiseButtons.RES_ACCEL))
 
     # Show green icon when LKA torque is applied, and
     # alarming orange icon when approaching torque limit.
