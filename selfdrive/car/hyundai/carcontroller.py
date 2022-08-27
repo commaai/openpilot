0f893f91bfc68bbe46c4a32df1bc5bcf710ef223
Jason Wen <haibin.wen3@gmail.com>
2022-08-27 15:38:36 -0400

    Hyundai: 2015-16 Genesis resume from standstill fix

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -53,6 +53,7 @@ class CarController:
     self.apply_steer_last = 0
     self.car_fingerprint = CP.carFingerprint
     self.last_button_frame = 0
+    self.resume_count = 0
 
   def update(self, CC, CS):
     actuators = CC.actuators
@@ -160,11 +161,19 @@ class CarController:
         if CC.cruiseControl.cancel:
           can_sends.append(hyundaican.create_clu11(self.packer, self.frame, CS.clu11, Buttons.CANCEL, self.CP.carFingerprint))
         elif CC.cruiseControl.resume:
-          # send resume at a max freq of 10Hz
-          if (self.frame - self.last_button_frame) * DT_CTRL > 0.1:
-            # send 25 messages at a time to increases the likelihood of resume being accepted
-            can_sends.extend([hyundaican.create_clu11(self.packer, self.frame, CS.clu11, Buttons.RES_ACCEL, self.CP.carFingerprint)] * 25)
-            self.last_button_frame = self.frame
+          if self.CP.carFingerprint == CAR.HYUNDAI_GENESIS:
+            if (self.frame - self.last_button_frame) > 5:
+              can_sends.append(hyundaican.create_clu11(self.packer, self.resume_count, CS.clu11, Buttons.RES_ACCEL, self.CP.carFingerprint))
+              self.resume_count += 1
+              if self.resume_count > 5:
+                self.last_button_frame = self.frame
+                self.resume_count = 0
+          else:
+            # send resume at a max freq of 10Hz
+            if (self.frame - self.last_button_frame) * DT_CTRL > 0.1:
+              # send 25 messages at a time to increases the likelihood of resume being accepted
+              can_sends.extend([hyundaican.create_clu11(self.packer, self.frame, CS.clu11, Buttons.RES_ACCEL, self.CP.carFingerprint)] * 25)
+              self.last_button_frame = self.frame
 
       if self.frame % 2 == 0 and self.CP.openpilotLongitudinalControl:
         # TODO: unclear if this is needed
