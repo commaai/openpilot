386163d09f6fbef8d27d7b5f09d6c38aa9ba5965
Shane Smiskol <shane@smiskol.com>
2022-08-30 23:33:35 -0700

    Chrysler: add missing hybrid ecus (#25606)
    * add missing hybrid ecus
    
    * rename to hcp
    
    rename to hcp
    old-commit-hash: c7bae57924e81856128d44f124a8e52e5aea2764

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -130,6 +130,11 @@ FINGERPRINTS = {
 }
 
 FW_VERSIONS = {
+  CAR.PACIFICA_2019_HYBRID: {
+    (Ecu.hcp, 0x7e2, None): [],
+    (Ecu.esp, 0x7e4, None): [],
+  },
+
   CAR.RAM_1500: {
     (Ecu.combinationMeter, 0x742, None): [
       b'68294063AH',
