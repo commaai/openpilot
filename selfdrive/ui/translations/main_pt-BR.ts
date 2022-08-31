23117b4f158b02ff346b1115506904fa40b8cdd0
AlexandreSato <66435071+AlexandreSato@users.noreply.github.com>
2022-08-31 19:50:56 -0300

    Multilang: improve pt-BR translation (#25623)
    * fix and improve pt-BR translation
    
    * Shorter phrase for Finish Setup
    
    * Concluir are better than Encerrar bacause means sucessfuly
    
    * improve pt-BR, DEVbrazilians use english as default
    
    * fix "atualizador" text cutoff
    
    * miss mark as finish on qt linguist
    old-commit-hash: 02bf3399956fa6cba84ba3c578771761b8d31c8e

--- selfdrive/ui/translations/main_pt-BR.ts
+++ selfdrive/ui/translations/main_pt-BR.ts
@@ -185,7 +185,7 @@
     <message>
         <location filename="../qt/offroad/settings.cc" line="138"/>
         <source>Change Language</source>
-        <translation>Mudar Língua</translation>
+        <translation>Mudar Linguagem</translation>
     </message>
     <message>
         <location filename="../qt/offroad/settings.cc" line="138"/>
@@ -1029,7 +1029,7 @@ trabalho definido</translation>
     <message>
         <location filename="../qt/offroad/settings.cc" line="253"/>
         <source>Switch Branch</source>
-        <translation>Trocar Ramo</translation>
+        <translation>Trocar Branch</translation>
     </message>
     <message>
         <location filename="../qt/offroad/settings.cc" line="253"/>
@@ -1040,12 +1040,12 @@ trabalho definido</translation>
         <location filename="../qt/offroad/settings.cc" line="253"/>
         <location filename="../qt/offroad/settings.cc" line="255"/>
         <source>The new branch will be pulled the next time the updater runs.</source>
-        <translation>O novo ramo será aplicado na próxima execução do atualizador.</translation>
+        <translation>A nova branch será aplicada ao verificar atualizações.</translation>
     </message>
     <message>
         <location filename="../qt/offroad/settings.cc" line="255"/>
         <source>Enter branch name</source>
-        <translation>Inserir o nome do ramo</translation>
+        <translation>Inserir o nome da branch</translation>
     </message>
     <message>
         <location filename="../qt/offroad/settings.cc" line="266"/>
@@ -1055,7 +1055,7 @@ trabalho definido</translation>
     <message>
         <location filename="../qt/offroad/settings.cc" line="266"/>
         <source>Uninstall %1</source>
-        <translation>Desintalando %1</translation>
+        <translation>Desintalar o %1</translation>
     </message>
     <message>
         <location filename="../qt/offroad/settings.cc" line="268"/>
