b6e355a9334ba883da60113240245a9de0beec9a
George Hotz <72895+geohot@users.noreply.github.com>
2022-08-30 17:57:14 -0700

    modeld: PC Thneed prereqs (#25615)
    * pc thneed prereqs
    
    * ugh, out of date
    
    * that can stay private
    
    * memcpy here is fine in SNPE variant
    
    * release files
    
    * thneed docs don't work anymore. they didn't look too useful
    
    Co-authored-by: Comma Device <device@comma.ai>

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -38,7 +38,7 @@ void model_init(ModelState* s, cl_device_id device_id, cl_context context) {
 #else
   s->m = std::make_unique<SNPEModel>("models/supercombo.dlc",
 #endif
-   &s->output[0], NET_OUTPUT_SIZE, USE_GPU_RUNTIME, true);
+   &s->output[0], NET_OUTPUT_SIZE, USE_GPU_RUNTIME, true, false, context);
 
 #ifdef TEMPORAL
   s->m->addRecurrent(&s->output[OUTPUT_SIZE], TEMPORAL_SIZE);
