c68161df4af3a18111fffcf7393a82f626ce7787
Jeroen Lammersma <jeroen@lammersma.dev>
2022-09-01 17:36:33 +0200

    Add 'nvme-cli' to common requirements

--- tools/ubuntu_setup.sh
+++ tools/ubuntu_setup.sh
@@ -66,7 +66,8 @@ function install_ubuntu_common_requirements() {
     libqt5x11extras5-dev \
     libreadline-dev \
     libdw1 \
-    valgrind
+    valgrind \
+    nvme-cli
 }
 
 # Install Ubuntu 22.04 LTS packages
