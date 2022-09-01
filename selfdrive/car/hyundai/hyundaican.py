7d1870175aeed4496722252adc37da884a2bd19e
Shane Smiskol <shane@smiskol.com>
2022-09-01 15:11:58 -0700

    fix

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -40,7 +40,7 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     # FcwOpt_USM 2 = Green car + lanes
     # FcwOpt_USM 1 = White car + lanes
     # FcwOpt_USM 0 = No car + lanes
-    values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 0  # fcw_usm
+    values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1  # fcw_usm
 
     # SysWarning 4 = keep hands on wheel
     # SysWarning 5 = keep hands on wheel (red)
