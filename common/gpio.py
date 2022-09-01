4bb399ba3c13953680522707bba662527fa771b7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-08-31 23:12:26 -0700

    pigeond tests (#25630)
    * start pigeond tests
    
    * sm checks
    
    * add some types
    
    * little more
    
    Co-authored-by: Comma Device <device@comma.ai>

--- common/gpio.py
+++ common/gpio.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 def gpio_init(pin: int, output: bool) -> None:
   try:
     with open(f"/sys/class/gpio/gpio{pin}/direction", 'wb') as f:
@@ -5,10 +7,19 @@ def gpio_init(pin: int, output: bool) -> None:
   except Exception as e:
     print(f"Failed to set gpio {pin} direction: {e}")
 
-
 def gpio_set(pin: int, high: bool) -> None:
   try:
     with open(f"/sys/class/gpio/gpio{pin}/value", 'wb') as f:
       f.write(b"1" if high else b"0")
   except Exception as e:
     print(f"Failed to set gpio {pin} value: {e}")
+
+def gpio_read(pin: int) -> Optional[bool]:
+  val = None
+  try:
+    with open(f"/sys/class/gpio/gpio{pin}/value", 'rb') as f:
+      val = bool(int(f.read().strip()))
+  except Exception as e:
+    print(f"Failed to set gpio {pin} value: {e}")
+
+  return val
