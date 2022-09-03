308f06c108e8b983dbfd7e6715b7160f6c6e7b97
Shane Smiskol <shane@smiskol.com>
2022-09-02 17:32:59 -0700

    send two?

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -115,8 +115,9 @@ class CarController:
           can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.CAMERA, CS.ascm_steering_button, CruiseButtons.CANCEL))
         elif CC.cruiseControl.resume:
           self.last_button_frame = self.frame
-          can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.CAMERA, CS.ascm_steering_button, CruiseButtons.RES_ACCEL))
-          can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.POWERTRAIN, CS.ascm_steering_button, CruiseButtons.RES_ACCEL))
+          for _ in range(2):
+            can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.CAMERA, CS.ascm_steering_button, CruiseButtons.RES_ACCEL))
+            can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.POWERTRAIN, CS.ascm_steering_button, CruiseButtons.RES_ACCEL))
 
     # Show green icon when LKA torque is applied, and
     # alarming orange icon when approaching torque limit.
