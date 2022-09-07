d9fd0473ca76cc4b16f1c1ff083a16dd84692cf1
hoomoose <94947902+hoomoose@users.noreply.github.com>
2022-09-07 15:59:48 -0600

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -84,7 +84,7 @@ function launch {
 
   # start manager
   cd selfdrive/manager
-  ./build.py && ./manager.py > tmux_log.txt
+  ./build.py && ./manager.py
 
   # if broken, keep on screen error
   while true; do sleep 1; done
