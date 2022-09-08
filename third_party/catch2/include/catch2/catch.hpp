41bf0b4b34923934400ad3f9aaef9a97fa29552b
Cameron Clough <cameronjclough@gmail.com>
2022-09-08 15:54:14 -0700

    bump catch2 headers to v2.13.9 (#25678)
    old-commit-hash: 9fb2c40cfdd5a4c05c65e90c7d91a2afd267e26b

--- third_party/catch2/include/catch2/catch.hpp
+++ third_party/catch2/include/catch2/catch.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1f66a0871edcb23231cba4b2fbf1bd7afa91488beed389c52a3bd641b89dae4
-size 650013
+oid sha256:27da57c7a06d09be8dd81fab7246b79e7892b6ae7e4e49ba8631f1d5a955e3fc
+size 657276
