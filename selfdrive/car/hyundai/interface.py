4b6fbd26a560f6b3aad23f3ea33d7918e8a2ec5f
HaraldSchafer <harald.the.engineer@gmail.com>
2022-09-07 19:09:37 -0700

    Improve Hyundai long control, and conform to longcontrol API boundaries (#25696)
    * Variety of experimental improvements
    
    * fix start accel
    
    * typo
    
    * No jerk, no go
    
    * Set jerk limit lower
    
    * Update ref
    old-commit-hash: da6c7311a38b1206be5443f8447f5424458671a5

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -43,13 +43,15 @@ class CarInterface(CarInterfaceBase):
     tire_stiffness_factor = 1.
 
     ret.stoppingControl = True
-    ret.vEgoStopping = 1.0
+    ret.startingState = True
+    ret.vEgoStarting = 0.1
+    ret.startAccel = 2.0
 
-    ret.longitudinalTuning.kpV = [0.1]
+    ret.longitudinalTuning.kpV = [0.5]
     ret.longitudinalTuning.kiV = [0.0]
-    ret.stopAccel = 0.0
 
-    ret.longitudinalActuatorDelayUpperBound = 1.0  # s
+    ret.longitudinalActuatorDelayLowerBound = 0.5  # s
+    ret.longitudinalActuatorDelayUpperBound = 0.5  # s
     if candidate in (CAR.SANTA_FE, CAR.SANTA_FE_2022, CAR.SANTA_FE_HEV_2022, CAR.SANTA_FE_PHEV_2022):
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 3982. * CV.LB_TO_KG + STD_CARGO_KG
