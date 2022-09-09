49ba799aa13b4a66055291f196418af46a28918c
Shane Smiskol <shane@smiskol.com>
2022-09-08 18:27:24 -0700

    fix tests

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -6,7 +6,7 @@ from parameterized import parameterized
 from cereal import car
 from selfdrive.car.car_helpers import get_interface_attr, interfaces
 from selfdrive.car.fingerprints import FW_VERSIONS
-from selfdrive.car.fw_versions import REQUESTS, FPV2_CONFIGS, match_fw_to_car
+from selfdrive.car.fw_versions import FPV2_CONFIGS, match_fw_to_car
 
 CarFw = car.CarParams.CarFw
 Ecu = car.CarParams.Ecu
@@ -59,14 +59,19 @@ class TestFwFingerprint(unittest.TestCase):
             for ecu in ecus.keys():
               self.assertNotEqual(ecu[0], Ecu.transmission, f"{car_model}: Blacklisted ecu: (Ecu.{ECU_NAME[ecu[0]]}, {hex(ecu[1])})")
 
-  def test_fw_request_ecu_whitelist(self):
-    for brand in set(r.brand for r in REQUESTS):
+  def test_fpv2_config(self):
+    for brand in VERSIONS:
       with self.subTest(brand=brand):
-        whitelisted_ecus = [ecu for r in REQUESTS for ecu in r.whitelist_ecus if r.brand == brand]
+        self.assertIn(brand, FPV2_CONFIGS.keys(), f"{brand} does not define a FPV2_CONFIG")
+
+  def test_fw_request_ecu_whitelist(self):
+    for brand, config in FPV2_CONFIGS.items():
+      with self.subTest(brand=brand):
+        whitelisted_ecus = set([ecu for r in config.requests for ecu in r.whitelist_ecus])
         brand_ecus = set([fw[0] for car_fw in VERSIONS[brand].values() for fw in car_fw])
 
         # each ecu in brand's fw versions needs to be whitelisted at least once
-        ecus_not_whitelisted = set(brand_ecus) - set(whitelisted_ecus)
+        ecus_not_whitelisted = brand_ecus - whitelisted_ecus
 
         ecu_strings = ", ".join([f'Ecu.{ECU_NAME[ecu]}' for ecu in ecus_not_whitelisted])
         self.assertFalse(len(whitelisted_ecus) and len(ecus_not_whitelisted),
