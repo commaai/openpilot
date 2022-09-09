a01b409b671a3a75593d1559c1444ffa140b3254
Jason Young <jyoung8607@gmail.com>
2022-09-08 21:57:59 -0400

    is this why I'm not leaving?

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -23,7 +23,6 @@ def long_control_state_trans(CP, active, long_control_state, v_ego, v_target,
 
   starting_condition = (v_target_1sec > CP.vEgoStarting and
                         accelerating and
-                        not cruise_standstill and
                         not brake_pressed)
   started_condition = v_ego > CP.vEgoStarting
 
