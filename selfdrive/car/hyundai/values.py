81e2e0fb1b8e0b650537fc6e5d068f8e8c588316
Shane Smiskol <shane@smiskol.com>
2022-09-13 13:41:03 -0700

    Better to do 2/3 and for all cars

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -29,10 +29,8 @@ class CarControllerParams:
       self.STEER_DRIVER_ALLOWANCE = 250
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 250
-      # These cars have significantly more torque than usual, limit jerk
-      if CP.carFingerprint in (CAR.KIA_EV6, CAR.IONIQ_5):
-        self.STEER_DELTA_UP = 2
-        self.STEER_DELTA_DOWN = 2
+      self.STEER_DELTA_UP = 2
+      self.STEER_DELTA_DOWN = 3
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
     # If the max stock LKAS request is <384, add your car to this list.
