800fc7f41be1a4271cceee7102d243ca4f72d00e
Shane Smiskol <shane@smiskol.com>
2022-09-12 18:56:19 -0700

    test both

--- selfdrive/car/tests/test_lateral_limits.py
+++ selfdrive/car/tests/test_lateral_limits.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from collections import defaultdict
 import importlib
 from parameterized import parameterized_class
 import unittest
@@ -13,8 +12,6 @@ from selfdrive.car.interfaces import get_torque_params
 MAX_LAT_UP_JERK = 2.5  # m/s^3
 MAX_LAT_UP_JERK_TOLERANCE = 0.1  # m/s^3
 MIN_LAT_DOWN_JERK = 2.0  # m/s^3
-
-jerks = defaultdict(dict)
 
 
 @parameterized_class('car_model', [(c,) for c in all_known_cars()])
@@ -42,13 +39,6 @@ class TestLateralLimits(unittest.TestCase):
     cls.control_params = CarControllerParams(CP)
     cls.torque_params = get_torque_params(cls.car_model)
 
-  @classmethod
-  def tearDownClass(cls):
-    for car, _jerks in jerks.items():
-      print(f'{car:37} - up jerk: {round(_jerks["up_jerk"], 2)} m/s^3, down jerk: {round(_jerks["down_jerk"], 2)} m/s^3')
-    print()
-    # print(dict(jerks))
-
   def _calc_jerk(self):
     # TODO: some cars don't send at 100hz, put steer rate/step into CCP to calculate this properly
     steer_step = self.control_params.STEER_STEP
@@ -60,9 +50,8 @@ class TestLateralLimits(unittest.TestCase):
 
   def test_jerk_limits(self):
     up_jerk, down_jerk = self._calc_jerk()
-    jerks[self.car_model] = {"up_jerk": up_jerk, "down_jerk": down_jerk}
     self.assertLess(up_jerk, MAX_LAT_UP_JERK + MAX_LAT_UP_JERK_TOLERANCE)
-    # self.assertGreater(down_jerk, MIN_LAT_DOWN_JERK)
+    self.assertGreater(down_jerk, MIN_LAT_DOWN_JERK)
 
 
 if __name__ == "__main__":
