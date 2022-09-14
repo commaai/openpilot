b5269f5e4b3df772463debc5370747a2af0526f5
deanlee <deanlee3@gmail.com>
2022-09-14 22:10:50 +0800

    create scanning label only once

--- selfdrive/ui/qt/offroad/networking.h
+++ selfdrive/ui/qt/offroad/networking.h
@@ -17,6 +17,8 @@ public:
 
 private:
   WifiManager *wifi = nullptr;
+  QVBoxLayout *list_layout = nullptr;
+  QLabel *scanning = nullptr;
   QVBoxLayout* main_layout;
   QPixmap lock;
   QPixmap checkmark;
