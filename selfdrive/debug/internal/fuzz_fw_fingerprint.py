4e66985dd84b3529a9dfc63332c4b7f91ea73af4
Shane Smiskol <shane@smiskol.com>
2022-09-15 16:37:06 -0700

    fix fuzz_fw_fingerprint script

--- selfdrive/debug/internal/fuzz_fw_fingerprint.py
+++ selfdrive/debug/internal/fuzz_fw_fingerprint.py
@@ -24,11 +24,11 @@ if __name__ == "__main__":
   wrong_match = 0
   confusions = defaultdict(set)
 
-  for _ in tqdm(range(1000)):
+  for _ in tqdm(range(50)):
     for candidate, fws in FWS.items():
       fw_dict = {}
-      for (tp, addr, subaddr), fw_list in fws.items():
-        fw_dict[(addr, subaddr)] = random.choice(fw_list)
+      for tp, fw_list in fws.items():
+        fw_dict[tp] = random.choice(fw_list)
 
       matches = match_fw_to_car_fuzzy(fw_dict, log=False, exclude=candidate)
 
@@ -48,5 +48,3 @@ if __name__ == "__main__":
   print(f"Total fuzz cases: {total}")
   print(f"Correct matches:  {match}")
   print(f"Wrong matches:    {wrong_match}")
-
-
