91283ad0b5413dcc0a25d7d9c76941f38c95e10a
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2022-09-15 23:16:54 -0400

    Updater: Reboot instead of shutdown to install new branch (#25804)
    Reboot instead of shutdown to install new branch
    old-commit-hash: b7d9f157faa33023ab07db561c155db0abb28c0b

--- selfdrive/ui/qt/offroad/software_settings.cc
+++ selfdrive/ui/qt/offroad/software_settings.cc
@@ -45,7 +45,7 @@ SoftwarePanel::SoftwarePanel(QWidget* parent) : ListWidget(parent) {
   installBtn = new ButtonControl(tr("Install Update"), tr("INSTALL"));
   connect(installBtn, &ButtonControl::clicked, [=]() {
     installBtn->setEnabled(false);
-    params.putBool("DoShutdown", true);
+    params.putBool("DoReboot", true);
   });
   addItem(installBtn);
 
