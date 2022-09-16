1c5996b087ce177bd7f7e6f58b1abc4d4ca0fb24
Shane Smiskol <shane@smiskol.com>
2022-09-15 19:23:00 -0700

    fix Bolt E(U)V centerToFront

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -156,7 +156,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1669. + STD_CARGO_KG
       ret.wheelbase = 2.63779
       ret.steerRatio = 16.8
-      ret.centerToFront = ret.wheelbase * 0.5
+      ret.centerToFront = 2.1  # measured
       tire_stiffness_factor = 1.0
       ret.steerActuatorDelay = 0.2
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
