bf5e07cf23819e2752435509c834239bdec54492
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2022-09-16 10:48:17 +1000

    Multilang: add missing Chinese e2e toggle translations (#25656)
    * japanese e2e toggle
    
    * chinese (simp)
    
    * chinese (trad)
    
    * OOPS
    
    * Mention experimental in details
    
    * Also in traditional
    
    * Exp.
    
    * suggestions
    
    * Apply suggestions from code review
    
    Co-authored-by: ZwX1616 <zwx1616@gmail.com>
    
    * add back removed translations from merge
    
    * Update selfdrive/ui/translations/main_zh-CHS.ts
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    Co-authored-by: ZwX1616 <zwx1616@gmail.com>
    old-commit-hash: 834f212903f5c06a9fe8d6caa6b6da52ac7a7d83

--- selfdrive/ui/translations/main_zh-CHT.ts
+++ selfdrive/ui/translations/main_zh-CHT.ts
@@ -1169,7 +1169,7 @@ location set</source>
     <message>
         <location line="+11"/>
         <source>🌮 End-to-end longitudinal (extremely alpha) 🌮</source>
-        <translation type="unfinished"></translation>
+        <translation>🌮 端對端縱向控制（實驗性功能） 🌮</translation>
     </message>
     <message>
         <location line="+6"/>
@@ -1184,7 +1184,7 @@ location set</source>
     <message>
         <location line="+42"/>
         <source>Let the driving model control the gas and brakes. openpilot will drive as it thinks a human would. Super experimental.</source>
-        <translation type="unfinished"></translation>
+        <translation>讓駕駛模型直接控製油門和剎車，openpilot將會模仿人類司機的駕駛方式。該功能仍非常實驗性。</translation>
     </message>
     <message>
         <location line="-55"/>
