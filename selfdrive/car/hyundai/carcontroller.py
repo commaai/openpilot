600ea7f3378136c83a0b72a486bdeb2b363c8dc1
Comma Device <device@comma.ai>
2022-09-20 10:47:26 -0700

    this is pretty good

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -97,7 +97,7 @@ class CarController:
         # cruise standstill resume
         elif CC.cruiseControl.resume:
           if not (self.CP.flags & HyundaiFlags.CANFD_ALT_BUTTONS):
-            for _ in range(5):
+            for _ in range(20):
               can_sends.append(hyundaicanfd.create_buttons(self.packer, CS.buttons_counter+1, Buttons.RES_ACCEL))
             self.last_button_frame = self.frame
     else:
