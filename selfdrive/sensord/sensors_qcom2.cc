75d52e351fa3618687b2a060c9b357364ad423dc
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-09-21 21:34:30 -0700

    pin sensord and lsm interrupt to core 1, for better timing

--- selfdrive/sensord/sensors_qcom2.cc
+++ selfdrive/sensord/sensors_qcom2.cc
@@ -218,5 +218,11 @@ int sensor_loop() {
 
 int main(int argc, char *argv[]) {
   setpriority(PRIO_PROCESS, 0, -18);
+
+  cpu_set_t mask;
+  CPU_ZERO(&mask);
+  CPU_SET(1, &mask);
+  sched_setaffinity(0, sizeof(mask), &mask);
+
   return sensor_loop();
 }
