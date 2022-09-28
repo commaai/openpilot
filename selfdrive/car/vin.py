ca698688a8fa814206aa526955541912691b2f72
Shane Smiskol <shane@smiskol.com>
2022-09-28 16:37:04 -0700

    Revert "VIN: query physical addresses (#25122)"
    This reverts commit 0697ca223974f2361ca655506ceab7d18917b9de.

--- selfdrive/car/vin.py
+++ selfdrive/car/vin.py
@@ -2,6 +2,7 @@
 import re
 
 import cereal.messaging as messaging
+from panda.python.uds import FUNCTIONAL_ADDRS
 from selfdrive.car.isotp_parallel_query import IsoTpParallelQuery
 from selfdrive.car.fw_query_definitions import StdQueries
 from system.swaglog import cloudlog
@@ -15,11 +16,10 @@ def is_valid_vin(vin: str):
 
 
 def get_vin(logcan, sendcan, bus, timeout=0.1, retry=5, debug=False):
-  addrs = [0x7e0, 0x7e2, 0x18da10f1, 0x18da0ef1]  # engine, VMCU, 29-bit engine, PGM-FI
   for i in range(retry):
     for request, response in ((StdQueries.UDS_VIN_REQUEST, StdQueries.UDS_VIN_RESPONSE), (StdQueries.OBD_VIN_REQUEST, StdQueries.OBD_VIN_RESPONSE)):
       try:
-        query = IsoTpParallelQuery(sendcan, logcan, bus, addrs, [request, ], [response, ], debug=debug)
+        query = IsoTpParallelQuery(sendcan, logcan, bus, FUNCTIONAL_ADDRS, [request, ], [response, ], functional_addr=True, debug=debug)
         for (addr, rx_addr), vin in query.get_data(timeout).items():
 
           # Honda Bosch response starts with a length, trim to correct length
