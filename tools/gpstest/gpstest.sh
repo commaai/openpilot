10da71c32763695088cb5fb34cb72066e255b60b
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-09-29 11:59:36 -0700

    remove prebuilt bins

--- tools/gpstest/gpstest.sh
+++ tools/gpstest/gpstest.sh
@@ -2,7 +2,7 @@
 
 LimeGPS_BIN=LimeGPS/LimeGPS
 if test -f "$LimeGPS_BIN"; then
-    LD_PRELOAD=lib/libLimeSuite.so $LimeGPS_BIN $@
+  LD_PRELOAD=LimeSuite/builddir/src/libLimeSuite.so $LimeGPS_BIN $@
 else
-    echo "LimeGPS binary not found, run setup.sh first"
+  echo "LimeGPS binary not found, run 'setup.sh' first"
 fi
