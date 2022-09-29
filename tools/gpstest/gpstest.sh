918d37317e3acff7aa65aa9b9f606a53e40c4b6f
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-09-28 22:07:47 -0700

    .

--- tools/gpstest/gpstest.sh
+++ tools/gpstest/gpstest.sh
@@ -4,5 +4,5 @@ LimeGPS_BIN=LimeGPS/LimeGPS
 if test -f "$LimeGPS_BIN"; then
     LD_PRELOAD=lib/libLimeSuite.so $LimeGPS_BIN $@
 else
-    echo "LimeGPS binary not found, run build.sh first"
+    echo "LimeGPS binary not found, run setup.sh first"
 fi
