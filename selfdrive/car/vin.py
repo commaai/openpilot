19d391db3eff7b282b6bf1da143835bebeb6f631
Shane Smiskol <shane@smiskol.com>
2022-09-30 11:45:48 -0700

    properly use

--- selfdrive/car/vin.py
+++ selfdrive/car/vin.py
@@ -2,6 +2,7 @@
 import re
 
 import cereal.messaging as messaging
+from panda.python.uds import FUNCTIONAL_ADDRS
 from selfdrive.car.isotp_parallel_query import IsoTpParallelQuery
 from selfdrive.car.fw_query_definitions import StdQueries
 from system.swaglog import cloudlog
@@ -19,7 +20,7 @@ def get_vin(logcan, sendcan, bus, timeout=0.1, retry=5, debug=False):
   for i in range(retry):
     for request, response in ((StdQueries.UDS_VIN_REQUEST, StdQueries.UDS_VIN_RESPONSE), (StdQueries.OBD_VIN_REQUEST, StdQueries.OBD_VIN_RESPONSE)):
       try:
-        query = IsoTpParallelQuery(sendcan, logcan, bus, addrs, [request, ], [response, ], functional_addr=True, debug=debug)
+        query = IsoTpParallelQuery(sendcan, logcan, bus, addrs, [request, ], [response, ], functional_addrs=FUNCTIONAL_ADDRS, debug=debug)
         for (addr, rx_addr), vin in query.get_data(timeout).items():
 
           # Honda Bosch response starts with a length, trim to correct length
