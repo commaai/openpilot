d9db76bfeed4f9355f8e895ff5a6a416914f50d9
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 08:34:56 +0000

    Adding tarfile member sanitization to extractall()

--- tools/plotjuggler/juggle.py
+++ tools/plotjuggler/juggle.py
@@ -44,7 +44,26 @@ def install():
         tmpf.write(chunk)
 
     with tarfile.open(tmp.name) as tar:
-      tar.extractall(path=INSTALL_DIR)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner) 
+          
+      
+      safe_extract(tar, path=INSTALL_DIR)
 
 
 def get_plotjuggler_version():
