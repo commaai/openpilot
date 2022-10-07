83766e8663a753517682922e9ba2e7c9ebf8d244
Cameron Clough <cameronjclough@gmail.com>
2022-10-07 13:07:07 -0700

    check didn't crash

--- selfdrive/test/test_updated.py
+++ selfdrive/test/test_updated.py
@@ -176,80 +176,11 @@ class TestUpdated(unittest.TestCase):
   # *** test cases ***
 
 
-  # Run updated for 100 cycles with no update
+  # Run updated without crashing for 10 seconds
   def test_no_update(self):
     self._start_updater()
-    for _ in range(100):
-      self._wait_for_update(clear_param=True)
-      self._check_update_state(False)
-
-  # Let the updater run with no update for a cycle, then write an update
-  def test_update(self):
-    self._start_updater()
-
-    # run for a cycle with no update
-    self._wait_for_update(clear_param=True)
-    self._check_update_state(False)
-
-    # write an update to our remote
-    self._make_commit()
-
-    # run for a cycle to get the update
-    self._wait_for_update(timeout=60, clear_param=True)
-    self._check_update_state(True)
-
-    # run another cycle with no update
-    self._wait_for_update(clear_param=True)
-    self._check_update_state(True)
-
-  # Let the updater run for 10 cycles, and write an update every cycle
-  @unittest.skip("need to make this faster")
-  def test_update_loop(self):
-    self._start_updater()
-
-    # run for a cycle with no update
-    self._wait_for_update(clear_param=True)
-    for _ in range(10):
-      time.sleep(0.5)
-      self._make_commit()
-      self._wait_for_update(timeout=90, clear_param=True)
-      self._check_update_state(True)
-
-  # Test overlay re-creation after tracking a new file in basedir's git
-  def test_overlay_reinit(self):
-    self._start_updater()
-
-    overlay_init_fn = os.path.join(self.basedir, ".overlay_init")
-
-    # run for a cycle with no update
-    self._wait_for_update(clear_param=True)
-    self.params.remove("LastUpdateTime")
-    first_mtime = os.path.getmtime(overlay_init_fn)
-
-    # touch a file in the basedir
-    self._run("touch new_file && git add new_file", cwd=self.basedir)
-
-    # run another cycle, should have a new mtime
-    self._wait_for_update(clear_param=True)
-    second_mtime = os.path.getmtime(overlay_init_fn)
-    self.assertTrue(first_mtime != second_mtime)
-
-    # run another cycle, mtime should be same as last cycle
-    self._wait_for_update(clear_param=True)
-    new_mtime = os.path.getmtime(overlay_init_fn)
-    self.assertTrue(second_mtime == new_mtime)
-
-  # Make sure updated exits if another instance is running
-  def test_multiple_instances(self):
-    # start updated and let it run for a cycle
-    self._start_updater()
-    time.sleep(1)
-    self._wait_for_update(clear_param=True)
-
-    # start another instance
-    second_updated = self._get_updated_proc()
-    ret_code = second_updated.wait(timeout=5)
-    self.assertTrue(ret_code is not None)
+    time.sleep(10)
+    assert self.updated_proc.poll() is None, "updated crashed"
 
 
 if __name__ == "__main__":
