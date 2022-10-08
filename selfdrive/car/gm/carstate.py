d5826b609c7a84430b6e8e376e7acb5b3cb9b043
Shane Smiskol <shane@smiskol.com>
2022-10-07 23:46:42 -0700

    set to 0 for testing

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -175,7 +175,7 @@ class CarState(CarStateBase):
     ]
 
     checks = [
-      ("ASCMLKASteeringCmd", 10), # 10 Hz is the stock inactive rate (every 100ms).
+      ("ASCMLKASteeringCmd", 0), # 10 Hz is the stock inactive rate (every 100ms).
       #                             While active 50 Hz (every 20 ms) is normal
       #                             EPS will tolerate around 200ms when active before faulting
     ]
