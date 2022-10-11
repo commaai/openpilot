e953f581a272b1e1bf3c7b421998eee7631e4b58
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-10-10 17:35:10 -0700

    fix setup.sh

--- tools/gpstest/setup.sh
+++ tools/gpstest/setup.sh
@@ -9,10 +9,7 @@ if [ ! -d LimeSuite ]; then
   cd LimeSuite
   # checkout latest version which has firmware updates available
   git checkout v20.10.0
-  cp ../mcu_error.patch .
-  cp ../reference_print.patch .
-  git apply mcu_error.patch
-  git apply reference_print.patch
+  git apply ../patches/LimeSuite/*
   mkdir builddir && cd builddir
   cmake -DCMAKE_BUILD_TYPE=Release ..
   make -j4
@@ -22,12 +19,7 @@ fi
 if [ ! -d LimeGPS ]; then
   git clone https://github.com/osqzss/LimeGPS.git
   cd LimeGPS
-
-  cp ../inc_ephem_array_size.patch .
-  cp ../makefile.patch .
-  git apply inc_ephem_array_size.patch
-  git apply makefile.patch
-
+  git apply ../patches/LimeGPS/*
   make
   cd ..
 fi
