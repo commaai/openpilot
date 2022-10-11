02c6922762fef41c8188a5a4f1f3267b76651330
Shane Smiskol <shane@smiskol.com>
2022-10-11 16:15:46 -0700

    fix

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -602,9 +602,7 @@ class Controls:
     # Check which actuators can be enabled
     CC.latActive = self.active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
                    CS.vEgo > self.CP.minSteerSpeed and not CS.standstill
-    if self.CP.openpilotLongitudinalControl:
-      CC.longActive = self.active and not self.events.any(ET.OVERRIDE_LONGITUDINAL)
-      CC.cruiseControl.override = self.enabled and self.events.any(ET.OVERRIDE_LONGITUDINAL)
+    CC.longActive = self.active and not self.events.any(ET.OVERRIDE_LONGITUDINAL) and self.CP.openpilotLongitudinalControl
 
     actuators = CC.actuators
     actuators.longControlState = self.LoC.long_control_state
@@ -707,6 +705,7 @@ class Controls:
     if len(angular_rate_value) > 2:
       CC.angularVelocity = angular_rate_value
 
+    CC.cruiseControl.override = self.enabled and not CC.longActive and self.CP.openpilotLongitudinalControl
     CC.cruiseControl.cancel = CS.cruiseState.enabled and (not self.enabled or not self.CP.pcmCruise)
     if self.joystick_mode and self.sm.rcv_frame['testJoystick'] > 0 and self.sm['testJoystick'].buttons[0]:
       CC.cruiseControl.cancel = True
