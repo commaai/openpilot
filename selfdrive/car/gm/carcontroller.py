6260fa524cbacfb3f0903cffc9f2f38c0a2dc680
Shane Smiskol <shane@smiskol.com>
2022-10-11 17:42:34 -0700

    GM camera ACC: no faults on start up 2.0
    And by 2.0 I mean we don't need to wait for blocked msg support to be merged first to merge this without regressing accidental single blocked msg count handling.

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -21,7 +21,7 @@ class CarController:
     self.frame = 0
     self.last_button_frame = 0
 
-    self.lka_steering_cmd_counter_last = -1
+    self.lka_steering_cmd_counter = 0
     self.lka_icon_status_last = (False, False)
 
     self.params = CarControllerParams()
@@ -42,10 +42,15 @@ class CarController:
     can_sends = []
 
     # Steering (50Hz)
+
+    # Initialize ASCMLKASteeringCmd counter using the camera
+    if self.frame == 0 and self.CP.networkLocation == NetworkLocation.fwdCamera:
+      self.lka_steering_cmd_counter = CS.camera_lka_steering_cmd_counter
+
     # Avoid GM EPS faults when transmitting messages too close together: skip this transmit if we just received the
     # next Panda loopback confirmation in the current CS frame.
-    if CS.lka_steering_cmd_counter != self.lka_steering_cmd_counter_last:
-      self.lka_steering_cmd_counter_last = CS.lka_steering_cmd_counter
+    if CS.loopback_lka_steering_cmd_updated:
+      self.lka_steering_cmd_counter += 1
     elif (self.frame % self.params.STEER_STEP) == 0:
       if CC.latActive:
         new_steer = int(round(actuators.steer * self.params.STEER_MAX))
@@ -54,10 +59,7 @@ class CarController:
         apply_steer = 0
 
       self.apply_steer_last = apply_steer
-      # GM EPS faults on any gap in received message counters. To handle transient OP/Panda safety sync issues at the
-      # moment of disengaging, increment the counter based on the last message known to pass Panda safety checks.
-      idx = (CS.lka_steering_cmd_counter + 1) % 4
-
+      idx = self.lka_steering_cmd_counter % 4
       can_sends.append(gmcan.create_steering_control(self.packer_pt, CanBus.POWERTRAIN, apply_steer, idx, CC.latActive))
 
     if self.CP.openpilotLongitudinalControl:
