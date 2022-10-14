da5a47011287ca7cf9285ba9db8f4316a13b8777
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-10-13 20:33:25 -0700

    fix supl

--- selfdrive/sensord/rawgps/rawgpsd.py
+++ selfdrive/sensord/rawgps/rawgpsd.py
@@ -112,8 +112,8 @@ def setup_quectel(diag: ModemDiag):
   mmcli("--command='AT+QGPSCFG=\"dpoenable\",0'")
   # don't automatically turn on GNSS on powerup
   mmcli("--command='AT+QGPSCFG=\"autogps\",0'")
-  mmcli("--location-enable-gps-raw --location-enable-gps-nmea")
   mmcli("--location-set-supl-server=supl.google.com:7276")
+  mmcli("--location-enable-gps-raw --location-enable-gps-nmea")
 
   # enable OEMDRE mode
   DIAG_SUBSYS_CMD_F = 75
