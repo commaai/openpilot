91a7bb4ea3f01f40a7d8e88e857d60ef090dad2c
Shane Smiskol <shane@smiskol.com>
2022-10-14 16:54:13 -0700

    GM camera ACC: raise brake pressed threshold (#26081)
    * Different brake pressed thresholds
    
    * comment
    
    * bump to master

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -51,7 +51,12 @@ class CarState(CarStateBase):
     # To avoid a cruise fault we need to match the ECM's brake pressed signal and threshold
     # https://static.nhtsa.gov/odi/tsbs/2017/MC-10137629-9999.pdf
     ret.brake = pt_cp.vl["ECMAcceleratorPos"]["BrakePedalPos"]
-    ret.brakePressed = ret.brake >= 8
+    if self.CP.networkLocation != NetworkLocation.fwdCamera:
+      ret.brakePressed = ret.brake >= 8
+    else:
+      # While car is braking, cancel button causes ECM to enter a soft disable state with a fault status.
+      # Match ECM threshold at a standstill to allow the camera to cancel earlier
+      ret.brakePressed = ret.brake >= 20
 
     # Regen braking is braking
     if self.CP.transmissionType == TransmissionType.direct:
