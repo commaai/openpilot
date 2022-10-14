1f00f592641d7b9aed4f671037ba997722e6c0b4
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-10-14 16:18:11 -0700

    add presetting of ublox position

--- selfdrive/sensord/pigeond.py
+++ selfdrive/sensord/pigeond.py
@@ -218,6 +218,21 @@ def initialize_pigeon(pigeon: TTYPigeon) -> bool:
         ))
         pigeon.send_with_ack(msg, ack=UBLOX_ASSIST_ACK)
 
+      last_pos = Params().get('LastGPSPosition')
+      if last_pos is not None:
+        cloudlog.warning("Sending LastGPSPosition to ublox")
+
+        # UBX-MGA-INI-POS_LLH
+        msg = add_ubx_checksum(b"\xB5\x62\x13\x40\x14\x00" + struct.pack("<BBxxiiiI",
+          0x01,
+          0x00,
+          int(last_pos["latitude"]*1e7),
+          int(last_pos["longitude"]*1e7),
+          int(last_pos["altiude"]*100),
+          500*100 # 500meters accuracy
+        ))
+        pigeon.send_with_ack(msg, ack=UBLOX_ASSIST_ACK)
+
       # try getting AssistNow if we have a token
       token = Params().get('AssistNowToken')
       if token is not None:
