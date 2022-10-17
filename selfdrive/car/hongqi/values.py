2713afb99e4bb5d988b6a494ab5c6537e9a88a4a
Jason Young <jyoung8607@gmail.com>
2022-10-17 15:01:17 -0400

    test UDS fingerprinting

--- selfdrive/car/hongqi/values.py
+++ selfdrive/car/hongqi/values.py
@@ -4,8 +4,10 @@ from enum import Enum
 from typing import Dict, List, Union
 
 from cereal import car
+from panda.python import uds
 from selfdrive.car import dbc_dict
 from selfdrive.car.docs_definitions import CarInfo, Harness
+from selfdrive.car.fw_query_definitions import FwQueryConfig, Request, p16
 
 Ecu = car.CarParams.Ecu
 GearShifter = car.CarState.GearShifter
@@ -48,3 +50,37 @@ class HongqiCarInfo(CarInfo):
 CAR_INFO: Dict[str, Union[HongqiCarInfo, List[HongqiCarInfo]]] = {
   CAR.HS5_G1: HongqiCarInfo("Hongqi HS5 2020"),
 }
+
+
+HONGQI_VERSION_REQUEST = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER]) + \
+  p16(uds.DATA_IDENTIFIER_TYPE.VEHICLE_MANUFACTURER_SPARE_PART_NUMBER)
+HONGQI_VERSION_RESPONSE = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER + 0x40])
+
+
+FW_QUERY_CONFIG = FwQueryConfig(
+  requests=[
+    Request(
+      [HONGQI_VERSION_REQUEST],
+      [HONGQI_VERSION_RESPONSE],
+    ),
+  ],
+)
+
+
+FW_VERSIONS = {
+  CAR.HS5_G1: {
+    (Ecu.engine, 0x7e0, None): [
+      b'3601015-DD21',
+    ],
+    (Ecu.transmission, 0x7e1, None): [
+      b'3611015-DD01\x00\x00\x00\x00',
+    ],
+    (Ecu.eps, 0x7a2, None): [
+      b'3418310-DD01\x00\x00\x00\x00',
+    ],
+    (Ecu.fwdCamera, 0x715, None): [
+      b'3616215-DD03-B  ',
+    ],
+  },
+}
+
