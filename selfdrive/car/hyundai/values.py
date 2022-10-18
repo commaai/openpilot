a037cdd9bc66622b5d30a10c105bf8ba28cb9f8d
Shane Smiskol <shane@smiskol.com>
2022-10-17 22:43:58 -0700

    add missing fwdRadar

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1369,6 +1369,9 @@ FW_VERSIONS = {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-N9240 14Q',
     ],
+    (Ecu.fwdRadar, 0x7d0, None): [
+      b'\xf1\x00NX4__               1.00 1.00 99110-N9100         ',
+    ],
     (Ecu.eps, 0x7d4, None): [
       b'\xf1\x00NX4 MDPS C 1.00 1.01 56300-P0100 2228',
     ],
