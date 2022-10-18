dfdd70f47962d1ef4ecbad5dfae5ec1713cfffbd
Shane Smiskol <shane@smiskol.com>
2022-10-18 11:28:46 -0700

    every 5 seconds

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -111,8 +111,8 @@ class CarController:
         can_sends += gmcan.create_adas_keepalive(CanBus.POWERTRAIN)
 
     else:
-      # Silence "Take Steering" alert sent by camera, forward PSCMStatus with HandsOffSWlDetectionStatus=1
-      if self.frame % 10 == 0:
+      # Silence "Take Steering" alert sent by camera, send PSCMStatus with HandsOffSWlDetectionStatus=1
+      if self.frame % 500 == 0:
         can_sends.append(gmcan.create_pscm_status(self.packer_pt, CS.pscm_status))
 
       # Stock longitudinal, integrated at camera
