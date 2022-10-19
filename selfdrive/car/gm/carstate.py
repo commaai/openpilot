d96d8f2a51f7467e52dedbdcbcb2fa9c2f6183f7
Shane Smiskol <shane@smiskol.com>
2022-10-19 00:29:16 -0700

    rm

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -19,7 +19,6 @@ class CarState(CarStateBase):
     self.loopback_lka_steering_cmd_updated = False
     self.camera_lka_steering_cmd_counter = 0
     self.buttons_counter = 0
-    self.brake_pressed = False
 
   def update(self, pt_cp, cam_cp, loopback_cp):
     ret = car.CarState.new_message()
