7b02bd82dd045de47ed7e462adc4eeb2ff113b50
Cameron Clough <cameronjclough@gmail.com>
2022-10-21 13:10:19 -0700

    jenkins correct branch

--- selfdrive/test/setup_device_ci.sh
+++ selfdrive/test/setup_device_ci.sh
@@ -57,8 +57,7 @@ rm -f .git/index.lock
 git reset --hard
 git fetch --verbose origin $GIT_COMMIT
 find . -maxdepth 1 -not -path './.git' -not -name '.' -not -name '..' -exec rm -rf '{}' \;
-git reset --hard $GIT_COMMIT
-git checkout $GIT_COMMIT
+git checkout --force --no-recurse-submodules -B $GIT_BRANCH $GIT_COMMIT
 git clean -xdff
 git submodule update --init --recursive
 git submodule foreach --recursive "git reset --hard && git clean -xdff"
