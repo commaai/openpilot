cf23b5236f62481e3ea08a357927735b601edc43
Cameron Clough <cameronjclough@gmail.com>
2022-10-25 12:52:08 -0700

    add Ford Bronco Sport 2021 fw
    VIN: 3FMCR9B61MRA17853
    
    62241b0c7fea4589|2022-10-25--13-40-05--0

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -19,7 +19,12 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 0.4
 
-    if candidate == CAR.ESCAPE_MK4:
+    if candidate == CAR.BRONCO_SPORT_MK1:
+      ret.wheelbase = 2.67
+      ret.steerRatio = 17.7  # learned
+      ret.mass = 1625 + STD_CARGO_KG
+
+    elif candidate == CAR.ESCAPE_MK4:
       ret.wheelbase = 2.71
       ret.steerRatio = 14.3  # Copied from Focus
       ret.mass = 1750 + STD_CARGO_KG
