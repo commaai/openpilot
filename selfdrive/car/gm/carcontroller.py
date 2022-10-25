d61cb706cd7d92804081726843c0d5ece5eca152
Shane Smiskol <shane@smiskol.com>
2022-10-24 23:47:44 -0700

    gate behind GM Cam

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -50,7 +50,7 @@ class CarController:
     # Steering (Active: 50Hz, inactive: 10Hz)
     # Attempt to sync with camera on startup at 50Hz, first few msgs are blocked
     steer_step = self.params.INACTIVE_STEER_STEP
-    if CC.latActive or not self.sent_lka_steering_cmd:
+    if CC.latActive or (not self.sent_lka_steering_cmd and self.CP.networkLocation == NetworkLocation.fwdCamera):
       steer_step = self.params.ACTIVE_STEER_STEP
 
     # Avoid GM EPS faults when transmitting messages too close together: skip this transmit if we just received the
