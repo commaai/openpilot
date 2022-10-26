fdeb2e849c1cfdd25d3e204354a798974f993ab0
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-10-26 13:08:24 -0700

    .

--- selfdrive/sensord/rawgps/rawgpsd.py
+++ selfdrive/sensord/rawgps/rawgpsd.py
@@ -112,7 +112,6 @@ def setup_quectel(diag: ModemDiag):
   mmcli("--command='AT+QGPSCFG=\"dpoenable\",0'")
   # don't automatically turn on GNSS on powerup
   mmcli("--command='AT+QGPSCFG=\"autogps\",0'")
-  mmcli("--location-set-supl-server=supl.google.com:7276")
   mmcli("--location-enable-gps-raw --location-enable-gps-nmea")
 
   # enable OEMDRE mode
