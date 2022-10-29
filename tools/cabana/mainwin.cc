ad4eeade84d76cd001915f620c88b23e5232ad82
deanlee <deanlee3@gmail.com>
2022-10-29 20:56:05 +0800

    cleanup

--- tools/cabana/mainwin.cc
+++ tools/cabana/mainwin.cc
@@ -22,10 +22,7 @@ MainWindow::MainWindow() : QWidget() {
   main_layout->addLayout(h_layout);
 
   splitter = new QSplitter(Qt::Horizontal, this);
-  splitter->setStretchFactor(0, 0);
-  // splitter->setStretchFactor(1, 1);
   splitter->setHandleWidth(11);
-  splitter->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
   messages_widget = new MessagesWidget(this);
   splitter->addWidget(messages_widget);
 
