7d2cde7a072084955c0478f93edff42df763e3bd
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-10-29 16:03:47 -0700

    some matching and jerk limit

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -1,3 +1,4 @@
+from common.numpy_fast import clip
 from selfdrive.car.hyundai.values import HyundaiFlags
 
 
@@ -52,10 +53,9 @@ def create_buttons(packer, CP, cnt, btn):
 def create_acc_cancel(packer, CP, cruise_info_copy):
   values = cruise_info_copy
   values.update({
-    "CRUISE_STATUS": 0,
-    "CRUISE_INACTIVE": 1,
+    "ACCMode": 4,
   })
-  return packer.make_can_msg("CRUISE_INFO", get_e_can_bus(CP), values)
+  return packer.make_can_msg("SCC_CONTROL", get_e_can_bus(CP), values)
 
 def create_lfahda_cluster(packer, CP, enabled):
   values = {
@@ -65,18 +65,25 @@ def create_lfahda_cluster(packer, CP, enabled):
   return packer.make_can_msg("LFAHDA_CLUSTER", get_e_can_bus(CP), values)
 
 
-def create_acc_control(packer, CP, enabled, accel, stopping, gas_override, set_speed):
-  cruise_status = 0 if not enabled else (4 if gas_override else 2)
+def create_acc_control(packer, CP, enabled, accel_last, accel, stopping, gas_override, set_speed):
+  # TODO: set and respect jerk limits
+  jerk = 0.5 / 50
   if not enabled or gas_override:
-    accel = 0
+    a_val, a_raw = 0, 0
+  else:
+    a_raw = accel
+    a_val = clip(accel, accel_last - jerk, accel_last + jerk)
+    if stopping:
+      a_raw = 0
+
   values = {
-    "CRUISE_STATUS": cruise_status,
+    "ACCMode": 0 if not enabled else (4 if gas_override else 2),
     "CRUISE_INACTIVE": 0 if enabled else 1,
     "CRUISE_MAIN": 1,
     "CRUISE_STANDSTILL": 0,
     "STOP_REQ": 1 if stopping else 0,
-    "ACCEL_REQ": accel,
-    "ACCEL_REQ2": accel,
+    "aReqValue": a_val,
+    "aReqRaw": a_raw,
     "SET_SPEED": set_speed,
     "DISTANCE_SETTING": 4,
 
@@ -91,7 +98,7 @@ def create_acc_control(packer, CP, enabled, accel, stopping, gas_override, set_s
     "NEW_SIGNAL_10": 4,
   }
 
-  return packer.make_can_msg("CRUISE_INFO", get_e_can_bus(CP), values)
+  return packer.make_can_msg("SCC_CONTROL", get_e_can_bus(CP), values)
 
 
 
