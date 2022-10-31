5ba3294fdecd1723cd03733dbed6738d0750c8b3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-10-30 18:20:07 -0700

    update refs

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -66,7 +66,6 @@ def create_lfahda_cluster(packer, CP, enabled):
 
 
 def create_acc_control(packer, CP, enabled, accel_last, accel, stopping, gas_override, set_speed):
-  # TODO: set and respect jerk limits
   jerk = 5
   jn = jerk / 50
   if not enabled or gas_override:
@@ -80,12 +79,11 @@ def create_acc_control(packer, CP, enabled, accel_last, accel, stopping, gas_ove
   values = {
     "ACCMode": 0 if not enabled else (2 if gas_override else 1),
     "MainMode_ACC": 1,
-    "CRUISE_STANDSTILL": 0,
     "StopReq": 1 if stopping else 0,
     "aReqValue": a_val,
     "aReqRaw": a_raw,
     "VSetDis": set_speed,
-    "DISTANCE_SETTING": 4,
+    "JerkLowerLimit": jerk if enabled else 1,
 
     "ACC_ObjDist": 1,
     "ObjValid": 0,
@@ -93,11 +91,8 @@ def create_acc_control(packer, CP, enabled, accel_last, accel, stopping, gas_ove
     "SET_ME_2": 0x4,
     "SET_ME_3": 0x3,
     "SET_ME_TMP_64": 0x64,
-
     "NEW_SIGNAL_10": 4,
-
-    # TODO: can both jerk limits be simultaneously high? stock doesn't seem to do this
-    "JerkLowerLimit": jerk if enabled else 1,
+    "DISTANCE_SETTING": 4,
   }
 
   return packer.make_can_msg("SCC_CONTROL", get_e_can_bus(CP), values)
