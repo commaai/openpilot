564033a64a8014242439bfab4128900fcf3945b7
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-10-30 23:34:04 -0700

    .

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -51,6 +51,11 @@ MapWindow::MapWindow(const QMapboxGLSettings &settings) : m_settings(settings), 
     last_position = *last_gps_position;
   }
 
+  auto last_gps_bearing = get_bearing_from_params();
+  if (last_gps_bearing) {
+    last_bearing = *last_gps_bearing;
+  }
+
   grabGesture(Qt::GestureType::PinchGesture);
   qDebug() << "MapWindow initialized";
 }
