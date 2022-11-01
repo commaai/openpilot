c171fe9f9a93ef0e6018de5703ed55f307a8cb72
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-10-31 23:01:22 -0700

    UI: draw onroad objects on right frame (#26306)
    * update before draw
    
    * fix a lot of janky
    
    * fix more flicker
    
    * cleanup
    
    * fix flicker when opening settings
    
    * simplify

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -87,6 +87,7 @@ const QColor bg_colors [] = {
 
 typedef struct UIScene {
   bool calibration_valid = false;
+  bool calibration_wide_valid  = false;
   bool wide_cam = true;
   mat3 view_from_calib = DEFAULT_CALIBRATION;
   mat3 view_from_wide_calib = DEFAULT_CALIBRATION;
@@ -181,3 +182,8 @@ public slots:
 };
 
 void ui_update_params(UIState *s);
+int get_path_length_idx(const cereal::ModelDataV2::XYZTData::Reader &line, const float path_height);
+void update_model(UIState *s, const cereal::ModelDataV2::Reader &model);
+void update_leads(UIState *s, const cereal::RadarState::Reader &radar_state, const cereal::ModelDataV2::XYZTData::Reader &line);
+void update_line_data(const UIState *s, const cereal::ModelDataV2::XYZTData::Reader &line,
+                      float y_off, float z_off, QPolygonF *pvd, int max_idx, bool allow_invert);
