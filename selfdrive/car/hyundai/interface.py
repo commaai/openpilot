8958d470bd4e849c1aad35350c5c856e67ada17d
hoomoose <94947902+hoomoose@users.noreply.github.com>
2022-11-01 22:03:39 -0600

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -266,7 +266,7 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def init(CP, logcan, sendcan):
-    if CP.openpilotLongitudinalControl and not (CP.flags & HyundaiFlags.CAMERA_SCC_CAR.value):
+    if CP.openpilotLongitudinalControl:
       addr, bus = 0x7d0, 0
       if CP.flags & HyundaiFlags.CANFD_HDA2.value:
         addr, bus = 0x730, 5
