1b6e37daa64c6fda8eaf0ade26d6d8c11274ef68
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2022-11-07 19:51:32 -0500

    Hyundai: update Ioniq 5 HDA I supported model years (#26376)
    * Hyundai: Add FW for 2023 Ioniq 5 HDA2
    
    * Update values.py
    
    * Update CARS.md
    
    * Update values.py
    
    * Update CARS.md

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -145,7 +145,7 @@ CAR_INFO: Dict[str, Optional[Union[HyundaiCarInfo, List[HyundaiCarInfo]]]] = {
   CAR.VELOSTER: HyundaiCarInfo("Hyundai Veloster 2019-20", min_enable_speed=5. * CV.MPH_TO_MS, harness=Harness.hyundai_e),
   CAR.SONATA_HYBRID: HyundaiCarInfo("Hyundai Sonata Hybrid 2020-22", "All", harness=Harness.hyundai_a),
   CAR.IONIQ_5: [
-    HyundaiCarInfo("Hyundai Ioniq 5 (without HDA II) 2022" , "Highway Driving Assist", harness=Harness.hyundai_k),
+    HyundaiCarInfo("Hyundai Ioniq 5 (without HDA II) 2022-23" , "Highway Driving Assist", harness=Harness.hyundai_k),
     HyundaiCarInfo("Hyundai Ioniq 5 (with HDA II) 2022-23", "Highway Driving Assist II", harness=Harness.hyundai_q),
   ],
   CAR.TUCSON_HYBRID_4TH_GEN: HyundaiCarInfo("Hyundai Tucson Hybrid 2022", "All", harness=Harness.hyundai_n),
