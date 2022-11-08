5960ba5def7c42b3944f96cf103a0c4a6e633754
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-11-08 12:49:22 -0800

    UI: widecam only in e2e mode (#26412)

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -567,6 +567,7 @@ void AnnotatedCameraWidget::paintGL() {
     } else if (v_ego > 15) {
       wide_cam_requested = false;
     }
+    wide_cam_requested = wide_cam_requested && s->scene.end_to_end_long;
     // TODO: also detect when ecam vision stream isn't available
     // for replay of old routes, never go to widecam
     wide_cam_requested = wide_cam_requested && s->scene.calibration_wide_valid;
