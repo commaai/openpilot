f3fb385a74d7cf0a7dd59c8efa881e287db50b5b
Dean Lee <deanlee3@gmail.com>
2022-11-10 06:19:11 +0800

    Cabana: support deleting message (#26418)
    delete message
    old-commit-hash: 59bf2fc0085420b13b7854d1a106c1706d7df265

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -45,9 +45,13 @@ ChartsWidget::ChartsWidget(QWidget *parent) : QWidget(parent) {
   QObject::connect(dbc(), &DBCManager::DBCFileChanged, [this]() { removeAll(nullptr); });
   QObject::connect(dbc(), &DBCManager::signalRemoved, this, &ChartsWidget::removeAll);
   QObject::connect(dbc(), &DBCManager::signalUpdated, this, &ChartsWidget::signalUpdated);
-  QObject::connect(dbc(), &DBCManager::msgUpdated, [this](const QString &msg_id) {
+  QObject::connect(dbc(), &DBCManager::msgRemoved, [this](uint32_t address) {
+    for (auto c : charts.toVector())
+      if (DBCManager::parseId(c->id).second == address) removeChart(c);
+  });
+  QObject::connect(dbc(), &DBCManager::msgUpdated, [this](uint32_t address) {
     for (auto c : charts) {
-      if (c->id == msg_id) c->updateTitle();
+      if (DBCManager::parseId(c->id).second == address) c->updateTitle();
     }
   });
   QObject::connect(can, &CANMessages::eventsMerged, this, &ChartsWidget::eventsMerged);
