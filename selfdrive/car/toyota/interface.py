052f136c77275f91c1311d83e676a099ea1bbc26
nuwandavek <vivekaithal44@gmail.com>
2022-11-08 23:38:00 -0800

    increase lag on prius

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -43,6 +43,7 @@ class CarInterface(CarInterfaceBase):
       for fw in car_fw:
         if fw.ecu == "eps" and not fw.fwVersion == b'8965B47060\x00\x00\x00\x00\x00\x00':
           steering_angle_deadzone_deg = 1.0
+          ret.steerActuatorDelay = 0.25
           CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning, steering_angle_deadzone_deg)
 
     elif candidate == CAR.PRIUS_V:
