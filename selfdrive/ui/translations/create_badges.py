2c208ec6f73a5bbc071617f65bbe7bea2da4a64a
Shane Smiskol <shane@smiskol.com>
2022-11-09 16:44:01 -0800

    set proper width

--- selfdrive/ui/translations/create_badges.py
+++ selfdrive/ui/translations/create_badges.py
@@ -2,6 +2,7 @@
 import json
 import os
 import requests
+import xml.etree.ElementTree as ET
 
 from common.basedir import BASEDIR
 from selfdrive.ui.update_translations import LANGUAGES_FILE, TRANSLATIONS_DIR
@@ -15,7 +16,8 @@ if __name__ == "__main__":
   with open(LANGUAGES_FILE, "r") as f:
     translation_files = json.load(f)
 
-  badge_svg = [f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="{len(translation_files) * BADGE_HEIGHT}">']
+  badge_svg = []
+  max_badge_width = 0  # keep track of max width to set parent element
   for idx, (name, file) in enumerate(translation_files.items()):
     with open(os.path.join(TRANSLATIONS_DIR, f"{file}.ts"), "r") as tr_f:
       tr_file = tr_f.read()
@@ -39,15 +41,18 @@ if __name__ == "__main__":
 
     r = requests.get(f"{SHIELDS_URL}/{badge_label}-{badge_message}-{color}", timeout=10)
     assert r.status_code == 200, "Error downloading badge"
+    content_svg = r.content.decode("utf-8")
+
+    max_badge_width = max(max_badge_width, int(ET.fromstring(content_svg).get("width")))
 
     # Make tag ids in each badge unique to combine them into one svg
-    content_svg = r.content.decode("utf-8")
     for tag in ("r", "s"):
       content_svg = content_svg.replace(f'id="{tag}"', f'id="{tag}{idx}"')
       content_svg = content_svg.replace(f'"url(#{tag})"', f'"url(#{tag}{idx})"')
 
     badge_svg.extend([f'<g transform="translate(0, {idx * BADGE_HEIGHT})">', content_svg, "</g>"])
 
+  badge_svg.insert(0, f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="{len(translation_files) * BADGE_HEIGHT}" width="{max_badge_width}">')
   badge_svg.append("</svg>")
 
   with open(os.path.join(BASEDIR, "translation_badge.svg"), "w") as badge_f:
