ab5e2b66e81e07e5822c5d0992d1bad28b310e60
Shane Smiskol <shane@smiskol.com>
2022-11-09 18:27:30 -0800

    Toyota: factor on cluster speed to match dash (#26408)
    * add a minimum factort
    
    * Update ref_commit
    old-commit-hash: e1001a3036fb93d6d0340d139d4ae2a1f2419d52

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -54,6 +54,7 @@ class CarState(CarStateBase):
     )
     ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr])
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
+    ret.vEgoCluster = ret.vEgo * 1.015  # minimum of all the cars
 
     ret.standstill = ret.vEgoRaw == 0
 
