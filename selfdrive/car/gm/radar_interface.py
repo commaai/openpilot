1a6c482c2c247d2b52409f6e64f16c06665140ac
Eric Brown <eric@ebrown.net>
2022-11-10 15:49:02 -0700

    Remove fingerprint check in radar parser, only check radarOffCan

--- selfdrive/car/gm/radar_interface.py
+++ selfdrive/car/gm/radar_interface.py
@@ -16,9 +16,6 @@ LAST_RADAR_MSG = RADAR_HEADER_MSG + NUM_SLOTS
 
 
 def create_radar_can_parser(car_fingerprint):
-  if car_fingerprint not in (CAR.VOLT, CAR.MALIBU, CAR.HOLDEN_ASTRA, CAR.ACADIA, CAR.CADILLAC_ATS, CAR.ESCALADE_ESV):
-    return None
-
   # C1A-ARS3-A by Continental
   radar_targets = list(range(SLOT_1_MSG, SLOT_1_MSG + NUM_SLOTS))
   signals = list(zip(['FLRRNumValidTargets',
