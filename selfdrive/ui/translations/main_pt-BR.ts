0c807a2a11680bbd1e4f0127f32fbbde698c4712
Shane Smiskol <shane@smiskol.com>
2022-11-09 20:07:05 -0800

    update translations
    update translations
    
    update translations

--- selfdrive/ui/translations/main_pt-BR.ts
+++ selfdrive/ui/translations/main_pt-BR.ts
@@ -377,7 +377,7 @@
     <message>
         <source>Get turn-by-turn directions displayed and more with a comma
 prime subscription. Sign up now: https://connect.comma.ai</source>
-        <translation>Obtenha instruções passo a passo exibidas e muito mais com 
+        <translation>Obtenha instruções passo a passo exibidas e muito mais com
 uma assinatura prime Inscreva-se agora:  https://connect.comma.ai</translation>
     </message>
     <message>
@@ -971,7 +971,7 @@ trabalho definido</translation>
         <translation>Upload dados da câmera voltada para o motorista e ajude a melhorar o algoritmo de monitoramentor.</translation>
     </message>
     <message>
-        <source>Experimental openpilot longitudinal control</source>
+        <source>Experimental openpilot Longitudinal Control</source>
         <translation>Controle longitudinal experimental openpilot</translation>
     </message>
     <message>
@@ -983,7 +983,7 @@ trabalho definido</translation>
         <translation>Habilite o controle longitudinal experimental para habilitar isso.</translation>
     </message>
     <message>
-        <source>Disengage On Accelerator Pedal</source>
+        <source>Disengage on Accelerator Pedal</source>
         <translation>Desacionar Com Pedal Do Acelerador</translation>
     </message>
     <message>
@@ -1007,7 +1007,7 @@ trabalho definido</translation>
         <translation>Exibir mapa do lado esquerdo quando a tela for dividida.</translation>
     </message>
     <message>
-        <source>Experimental mode</source>
+        <source>Experimental Mode</source>
         <translation>Modo Experimental</translation>
     </message>
     <message>
