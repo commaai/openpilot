8a6d924f20b093f995cea2b92420f1054efdcf05
nuwandavek <vivekaithal44@gmail.com>
2022-11-09 23:04:27 -0800

    split prius ecu more, set deadzone to 0

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -42,9 +42,11 @@ class CarInterface(CarInterfaceBase):
       # Only give steer angle deadzone to for bad angle sensor prius
       for fw in car_fw:
         if fw.ecu == "eps" and not fw.fwVersion == b'8965B47060\x00\x00\x00\x00\x00\x00':
-          steering_angle_deadzone_deg = 1.0
+          steering_angle_deadzone_deg = 0.0
           ret.steerActuatorDelay = 0.25
           CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning, steering_angle_deadzone_deg)
+        elif fw.ecu == "eps" and fw.fwVersion == b'8965B47060\x00\x00\x00\x00\x00\x00':
+          CarInterfaceBase.configure_torque_tune(CAR.PRIUS_060, ret.lateralTuning, steering_angle_deadzone_deg)
 
     elif candidate == CAR.PRIUS_V:
       stop_and_go = True
