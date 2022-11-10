bb9dc9a2eb69fba6f62739e6d63247c5badd68a4
Shane Smiskol <shane@smiskol.com>
2022-11-09 19:39:05 -0800

    Spell out AEB

--- selfdrive/ui/translations/main_pt-BR.ts
+++ selfdrive/ui/translations/main_pt-BR.ts
@@ -1016,11 +1016,15 @@ trabalho definido</translation>
     </message>
     <message>
         <source>WARNING: openpilot longitudinal control is experimental for this car and will disable AEB.</source>
-        <translation>AVISO: o controle longitudinal openpilot é experimental para este carro e irá desabilitar AEB.</translation>
+        <translation type="vanished">AVISO: o controle longitudinal openpilot é experimental para este carro e irá desabilitar AEB.</translation>
     </message>
     <message>
         <source>openpilot defaults to the car&apos;s built-in ACC instead of openpilot&apos;s longitudinal control on this car. Enable this to switch to openpilot longitudinal control.</source>
         <translation>O padrão do openpilot é o ACC integrado do carro em vez do controle longitudinal do openpilot neste carro. Habilite isto para alternar para controle longitudinal do openpilot.</translation>
+    </message>
+    <message>
+        <source>WARNING: openpilot longitudinal control is experimental for this car and will disable Automatic Emergency Braking (AEB).</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
