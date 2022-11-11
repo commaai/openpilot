1ea7e49e8dd2815e35487160d50da1e84e6a539e
Shane Smiskol <shane@smiskol.com>
2022-11-10 19:06:31 -0800

    controls: disengage on regen paddle independently (#26453)
    * add regen braking field that's just used to add a pedalPressed
    
    * bump
    
    * bump
    
    * Update ref_commit
    
    * we want the standstill check
    
    we want the standstill check
    
    * see what diff is now
    
    * Update ref_commit
    old-commit-hash: 74b6e22a7dc8e0b1ec051f00037506d23a017ae8

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -250,7 +250,8 @@ class Controls:
 
     # Disable on rising edge of accelerator or brake. Also disable on brake when speed > 0
     if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or \
-      (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)):
+      (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or \
+      (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
       self.events.add(EventName.pedalPressed)
 
     if CS.gasPressed:
