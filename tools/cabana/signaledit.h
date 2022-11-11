1e72c1fe529b2fe8aecc7deea1a31813cea1ad2b
deanlee <deanlee3@gmail.com>
2022-11-11 23:49:17 +0800

    auto update signal on field changes

--- tools/cabana/signaledit.h
+++ tools/cabana/signaledit.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <QAction>
 #include <QComboBox>
 #include <QDialog>
 #include <QLabel>
 #include <QLineEdit>
-#include <QPushButton>
 #include <QSpinBox>
+#include <QToolButton>
 
 #include "selfdrive/ui/qt/widgets/controls.h"
 
@@ -13,13 +14,17 @@
 #include "tools/cabana/dbcmanager.h"
 
 class SignalForm : public QWidget {
+  Q_OBJECT
 public:
   SignalForm(QWidget *parent);
-
   QLineEdit *name, *unit, *comment, *val_desc, *offset, *factor, *min_val, *max_val;
   QLabel *lsb, *msb;
   QSpinBox *size;
   QComboBox *sign, *endianness;
+  bool changed_by_user = false;
+
+ signals:
+  void changed();
 };
 
 class SignalEdit : public QWidget {
@@ -27,38 +32,35 @@ class SignalEdit : public QWidget {
 
 public:
   SignalEdit(int index, QWidget *parent = nullptr);
-  void setSignal(const QString &msg_id, const Signal *sig, bool show_form);
+  void setSignal(const QString &msg_id, const Signal *sig);
   void setChartOpened(bool opened);
-  void setFormVisible(bool show);
   void signalHovered(const Signal *sig);
-  inline bool isFormVisible() const { return form_container->isVisible(); }
   const Signal *sig = nullptr;
   QString msg_id;
 
 signals:
   void highlight(const Signal *sig);
   void showChart(const QString &name, const Signal *sig, bool show);
-  void showFormClicked();
   void remove(const Signal *sig);
   void save(const Signal *sig, const Signal &new_sig);
 
 protected:
+  void hideEvent(QHideEvent *event) override;
   void enterEvent(QEvent *event) override;
   void leaveEvent(QEvent *event) override;
   void saveSignal();
+  void updateForm(bool show);
+  void showFormClicked();
 
   SignalForm *form = nullptr;
   ElidedLabel *title;
-  QWidget *form_container;
   QLabel *icon;
   int form_idx = 0;
   bool chart_opened = false;
-  QPushButton *plot_btn;
+  QAction *plot_btn;
 };
 
 class SignalFindDlg : public QDialog {
-  Q_OBJECT
-
 public:
   SignalFindDlg(const QString &id, const Signal *signal, QWidget *parent);
 };
