f6189b32351b21ad528486ee69c55666a1ef0d55
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2022-11-11 14:40:21 -0500

    HKG: Bump Kia Telluride support to 2022 (#26467)

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -142,7 +142,7 @@ CAR_INFO: Dict[str, Optional[Union[HyundaiCarInfo, List[HyundaiCarInfo]]]] = {
   ],
   CAR.PALISADE: [
     HyundaiCarInfo("Hyundai Palisade 2020-22", "All", "https://youtu.be/TAnDqjF4fDY?t=456", harness=Harness.hyundai_h),
-    HyundaiCarInfo("Kia Telluride 2020", "All", harness=Harness.hyundai_h),
+    HyundaiCarInfo("Kia Telluride 2020-22", "All", harness=Harness.hyundai_h),
   ],
   CAR.VELOSTER: HyundaiCarInfo("Hyundai Veloster 2019-20", min_enable_speed=5. * CV.MPH_TO_MS, harness=Harness.hyundai_e),
   CAR.SONATA_HYBRID: HyundaiCarInfo("Hyundai Sonata Hybrid 2020-22", "All", harness=Harness.hyundai_a),
