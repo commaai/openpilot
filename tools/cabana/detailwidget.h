c9a379505748245c473c60ea750a44d2ef691ba5
Dean Lee <deanlee3@gmail.com>
2022-11-12 02:34:49 +0800

    Cabana: auto update signal on field changes (#26464)
    * auto update signal on field changes
    
    * better icon
    old-commit-hash: 3c507e8ad7a4de1d03402b178389de2bee9c4363

--- tools/cabana/detailwidget.h
+++ tools/cabana/detailwidget.h
@@ -11,8 +11,6 @@
 #include "tools/cabana/signaledit.h"
 
 class EditMessageDialog : public QDialog {
-  Q_OBJECT
-
 public:
   EditMessageDialog(const QString &msg_id, const QString &title, int size, QWidget *parent);
 
@@ -38,13 +36,12 @@ private:
   void removeSignal(const Signal *sig);
   void editMsg();
   void removeMsg();
-  void showForm();
   void updateState();
 
   QString msg_id;
   QLabel *name_label, *time_label, *warning_label;
   QWidget *warning_widget;
-  QWidget *signals_container;
+  QVBoxLayout *signals_layout;
   QTabBar *tabbar;
   QToolBar *toolbar;
   QAction *remove_msg_act;
