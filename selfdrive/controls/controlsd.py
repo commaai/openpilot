0f6bd78c5d73bbd875420c8575d04fae55d50737
Shane Smiskol <shane@smiskol.com>
2022-11-10 18:39:53 -0800

    we want the standstill check
    we want the standstill check

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -251,7 +251,7 @@ class Controls:
     # Disable on rising edge of accelerator or brake. Also disable on brake when speed > 0
     if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or \
       (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or \
-      (CS.regenBraking and not self.CS_prev.regenBraking):
+      (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
       self.events.add(EventName.pedalPressed)
 
     if CS.gasPressed:
