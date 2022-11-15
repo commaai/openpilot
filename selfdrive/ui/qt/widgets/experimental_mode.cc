1ad32be7b5857dc58da5e0477a3fbf5fdd86c405
Shane Smiskol <shane@smiskol.com>
2022-11-14 16:14:37 -0800

    draft

--- selfdrive/ui/qt/widgets/experimental_mode.cc
+++ selfdrive/ui/qt/widgets/experimental_mode.cc
@@ -6,47 +6,32 @@
 #include "common/params.h"
 #include "selfdrive/ui/qt/util.h"
 
-ExperimentalMode::ExperimentalMode(QWidget* parent) : QFrame(parent) {
-  metric_ = Params().getBool("IsMetric");
-
-  QVBoxLayout* main_layout = new QVBoxLayout(this);
+ExperimentalMode::ExperimentalMode(QWidget *parent) : QFrame(parent) {
+  QVBoxLayout *main_layout = new QVBoxLayout(this);
   main_layout->setContentsMargins(50, 50, 50, 60);
-  main_layout->addWidget(new QLabel("MODE: CHILL"));
-
-//  auto add_stats_layouts = [=](const QString &title, StatsLabels& labels) {
-//    QGridLayout* grid_layout = new QGridLayout;
-//    grid_layout->setVerticalSpacing(10);
-//    grid_layout->setContentsMargins(0, 10, 0, 10);
-//
-//    int row = 0;
-//    grid_layout->addWidget(newLabel(title, "title"), row++, 0, 1, 3);
-//    grid_layout->addItem(new QSpacerItem(0, 50), row++, 0, 1, 1);
-//
-//    grid_layout->addWidget(labels.routes = newLabel("0", "number"), row, 0, Qt::AlignLeft);
-//    grid_layout->addWidget(labels.distance = newLabel("0", "number"), row, 1, Qt::AlignLeft);
-//    grid_layout->addWidget(labels.hours = newLabel("0", "number"), row, 2, Qt::AlignLeft);
-//
-//    grid_layout->addWidget(newLabel((tr("Drives")), "unit"), row + 1, 0, Qt::AlignLeft);
-//    grid_layout->addWidget(labels.distance_unit = newLabel(getDistanceUnit(), "unit"), row + 1, 1, Qt::AlignLeft);
-//    grid_layout->addWidget(newLabel(tr("Hours"), "unit"), row + 1, 2, Qt::AlignLeft);
-//
-//    main_layout->addLayout(grid_layout);
-//  };
-//
-//  add_stats_layouts(tr("ALL TIME"), all_);
-//  main_layout->addStretch();
-//  add_stats_layouts(tr("PAST WEEK"), week_);
-//
-//  if (auto dongleId = getDongleId()) {
-//    QString url = CommaApi::BASE_URL + "/v1.1/devices/" + *dongleId + "/stats";
-//    RequestRepeater* repeater = new RequestRepeater(this, url, "ApiCache_ExperimentalMode", 30);
-//    QObject::connect(repeater, &RequestRepeater::requestDone, this, &ExperimentalMode::parseResponse);
-//  }
+  label = new QLabel;
+  button = new QPushButton;
+  label->setProperty("type", "title");
+//  main_layout->addWidget(label);
+  main_layout->addWidget(button);
 
   setStyleSheet(R"(
     ExperimentalMode {
-      background-color: #333333;
       border-radius: 10px;
+      background: #000000;
+    }
+    ExperimentalMode:pressed {
+      border-radius: 50px;
+      background: #ffffff;
+      color: #000000;
+    }
+    ExperimentalMode[experimental_mode="false"] {
+      background: qlineargradient(x1:0, y1:0, x2:1, y2:0,
+                  stop:0 #2395FF, stop:1 #14FFAB);
+    }
+    ExperimentalMode[experimental_mode="true"] {
+      background: qlineargradient(x1:0, y1:0, x2:1, y2:0,
+                  stop:0 #FF9B3F, stop:1 #DB3822);
     }
 
     QLabel[type="title"] { font-size: 51px; font-weight: 500; }
@@ -55,35 +40,14 @@ ExperimentalMode::ExperimentalMode(QWidget* parent) : QFrame(parent) {
   )");
 }
 
-void ExperimentalMode::updateStats() {
-  auto update = [=](const QJsonObject& obj, StatsLabels& labels) {
-    labels.routes->setText(QString::number((int)obj["routes"].toDouble()));
-    labels.distance->setText(QString::number(int(obj["distance"].toDouble() * (metric_ ? MILE_TO_KM : 1))));
-    labels.distance_unit->setText(getDistanceUnit());
-    labels.hours->setText(QString::number((int)(obj["minutes"].toDouble() / 60)));
-  };
-
-  QJsonObject json = stats_.object();
-  update(json["all"].toObject(), all_);
-  update(json["week"].toObject(), week_);
-}
-
-void ExperimentalMode::parseResponse(const QString& response, bool success) {
-  if (!success) return;
-
-  QJsonDocument doc = QJsonDocument::fromJson(response.trimmed().toUtf8());
-  if (doc.isNull()) {
-    qDebug() << "JSON Parse failed on getting past drives statistics";
-    return;
-  }
-  stats_ = doc;
-  updateStats();
-}
-
-void ExperimentalMode::showEvent(QShowEvent* event) {
-  bool metric = Params().getBool("IsMetric");
-  if (metric_ != metric) {
-    metric_ = metric;
-    updateStats();
-  }
+void ExperimentalMode::showEvent(QShowEvent *event) {
+  qDebug() << "showEvent!";
+//  experimental_mode = params.getBool("ExperimentalMode");
+  setProperty("experimental_mode", params.getBool("ExperimentalMode"));
+  qDebug() << "experimental_mode" << experimental_mode;
+  update();
+  label->setText(experimental_mode ? "MODE: EXPERIMENTAL" : "MODE: CHILL");
+  updateStyle();
+//  style()->unpolish(this);
+//  style()->polish(this);
 }
--- selfdrive/ui/qt/widgets/experimental_mode.h
+++ selfdrive/ui/qt/widgets/experimental_mode.h
@@ -1,13 +1,56 @@
 #pragma once
 
 #include <QLabel>
+#include <QFrame>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPainter>
+#include <QPushButton>
+#include <QMouseEvent>
+#include <QDebug>
+#include <QStyle>
+
+#include "common/params.h"
 
 class ExperimentalMode : public QFrame {
   Q_OBJECT
+  Q_PROPERTY(bool experimental_mode MEMBER experimental_mode);
 
 public:
   explicit ExperimentalMode(QWidget* parent = 0);
 
+signals:
+  void openSettings();
+  void pressed();
+  void released();
+  void clicked();
+
 private:
+  void updateStyle() {style()->unpolish(this); style()->polish(this);}
+
+  Params params;
+  bool experimental_mode;
   void showEvent(QShowEvent *event) override;
+  QLabel *label;
+  QPushButton *button;
+
+protected:
+  void mousePressEvent(QMouseEvent *event) override {
+    if (rect().contains(event->pos())) {
+      emit pressed();
+      qDebug() << "pressed";
+      updateStyle();
+    }
+  }
+
+  void mouseReleaseEvent(QMouseEvent *event) override {
+    emit released();
+    qDebug() << "released";
+    if (rect().contains(event->pos())) {
+      emit clicked();
+      emit openSettings();
+      qDebug() << "clicked";
+    }
+    updateStyle();
+  }
 };
