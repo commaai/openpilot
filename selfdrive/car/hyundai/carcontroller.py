9c5df76a6c8784af3bec64827a2dbab464d8ce39
hoomoose <94947902+hoomoose@users.noreply.github.com>
2022-11-15 17:19:30 -0700

    Hyundai: longitudinal support for all CAN-FD EV and Hybrids (#26345)
    * Update values.py
    
    * Update interface.py
    
    * Update interface.py
    
    * Update carcontroller.py
    
    * Update interface.py
    
    * Update interface.py
    
    * Update values.py
    
    * Update values.py
    
    * Update interface.py
    
    * Update values.py
    
    * Update interface.py
    
    * Update carcontroller.py
    
    * cleanup
    
    * update docs
    
    * bump panda
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -85,7 +85,7 @@ class CarController:
     # *** common hyundai stuff ***
 
     # tester present - w/ no response (keeps relevant ECU disabled)
-    if self.frame % 100 == 0 and self.CP.openpilotLongitudinalControl:
+    if self.frame % 100 == 0 and not (self.CP.flags & HyundaiFlags.CANFD_CAMERA_SCC.value) and self.CP.openpilotLongitudinalControl:
       addr, bus = 0x7d0, 0
       if self.CP.flags & HyundaiFlags.CANFD_HDA2.value:
         addr, bus = 0x730, 5
@@ -122,7 +122,8 @@ class CarController:
         can_sends.append(hyundaicanfd.create_lfahda_cluster(self.packer, self.CP, CC.enabled))
 
       if self.CP.openpilotLongitudinalControl:
-        can_sends.extend(hyundaicanfd.create_adrv_messages(self.packer, self.frame))
+        if hda2:
+          can_sends.extend(hyundaicanfd.create_adrv_messages(self.packer, self.frame))
         if self.frame % 2 == 0:
           can_sends.append(hyundaicanfd.create_acc_control(self.packer, self.CP, CC.enabled, self.accel_last, accel, stopping, CC.cruiseControl.override,
                                                            set_speed_in_units))
