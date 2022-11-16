6a3a374602e35f0625745b815c63ecf9d80a6e1f
Shane Smiskol <shane@smiskol.com>
2022-11-15 16:39:45 -0800

    not true

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -90,7 +90,7 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
   };
 
   for (auto &[param, title, desc, icon] : toggle_defs) {
-    auto toggle = new ParamControl(param, title, desc, icon, true, this);
+    auto toggle = new ParamControl(param, title, desc, icon, false, this);
 
     bool locked = params.getBool((param + "Lock").toStdString());
     toggle->setEnabled(!locked);
