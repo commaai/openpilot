198ff225bef77c33f8963ceefe5d5b1a43677cb3
ZwX1616 <zwx1616@gmail.com>
2022-11-16 16:57:36 -0800

    oops wheeltouch

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -294,7 +294,7 @@ class DriverStatus():
       self.hi_stds = 0
 
   def update_events(self, events, driver_engaged, ctrl_active, standstill):
-    if not ctrl_active: # reset only when on disengagement if red reached
+    if (driver_engaged and self.awareness > 0 and not self.active_monitoring_mode) or not ctrl_active: # reset only when on disengagement if red reached
       self._reset_awareness()
       return
 
