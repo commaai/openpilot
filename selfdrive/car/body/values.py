d4cc1bf646ccc894a9282c46d94dc121834a80b9
Igor Biletksyy <bs@privacy.im>
2022-11-19 16:14:56 -0800

    add new fw version

--- selfdrive/car/body/values.py
+++ selfdrive/car/body/values.py
@@ -46,10 +46,13 @@ FW_VERSIONS = {
   CAR.BODY: {
     (Ecu.engine, 0x720, None): [
       b'0.0.01',
-      b'02/27/2022'
+      b'02/27/2022',
+      b'0.3.00a',
     ],
+    # git hash of the firmware used
     (Ecu.debug, 0x721, None): [
-      b'166bd860' # git hash of the firmware used
+      b'166bd860',
+      b'dc780f85',
     ],
   },
 }
