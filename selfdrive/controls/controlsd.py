91ca275119cc7c2bb58d21463669dcfde79b1697
Shane Smiskol <shane@smiskol.com>
2022-11-29 00:09:14 -0800

    not sure if best way to fix, but enabled with current states defs is fine

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -585,7 +585,7 @@ class Controls:
     # Check which actuators can be enabled
     CC.latActive = self.active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
                    CS.vEgo > self.CP.minSteerSpeed and not CS.standstill
-    CC.longActive = self.active and not self.events.any(ET.OVERRIDE_LONGITUDINAL) and self.CP.openpilotLongitudinalControl
+    CC.longActive = self.enabled and not self.events.any(ET.OVERRIDE_LONGITUDINAL) and self.CP.openpilotLongitudinalControl
 
     actuators = CC.actuators
     actuators.longControlState = self.LoC.long_control_state
