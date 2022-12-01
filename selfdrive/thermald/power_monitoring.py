0e1b437013d1633b549cee644417105a49363738
EdwardApollo <51210836+EdwardApollo@users.noreply.github.com>
2022-12-01 12:38:17 -0500

    VBATT_PAUSE_CHARGING = 11.9
    With an assumed 0.1V drop between the battery and the comma device, this level errs on the side of battery protection, ensuring the device never drops below 12V solely due to the comma device.

--- selfdrive/thermald/power_monitoring.py
+++ selfdrive/thermald/power_monitoring.py
@@ -13,7 +13,7 @@ CAR_VOLTAGE_LOW_PASS_K = 0.011 # LPF gain for 45s tau (dt/tau / (dt/tau + 1))
 CAR_BATTERY_CAPACITY_uWh = 30e6
 CAR_CHARGING_RATE_W = 45
 
-VBATT_PAUSE_CHARGING = 11.8           # Lower limit on the LPF car battery voltage
+VBATT_PAUSE_CHARGING = 11.9           # Lower limit on the LPF car battery voltage
 VBATT_INSTANT_PAUSE_CHARGING = 7.0    # Lower limit on the instant car battery voltage measurements to avoid triggering on instant power loss
 MAX_TIME_OFFROAD_S = 30*3600
 MIN_ON_TIME_S = 3600
