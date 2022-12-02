59ed74ae4a7a51fcfa673393947b755d7b29c323
Dean Lee <deanlee3@gmail.com>
2022-12-03 05:15:32 +0800

    Cabanba: new dialog to manage chart series (#26675)
    * New dialog to manage chart series
    
    * connect to signal currentIndexChanged
    
    * cleanup
    
    use cbegin/cend

--- tools/cabana/chartswidget.h
+++ tools/cabana/chartswidget.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <QComboBox>
+#include <QDialogButtonBox>
 #include <QLabel>
+#include <QListWidget>
 #include <QGraphicsEllipseItem>
 #include <QGraphicsLineItem>
 #include <QGraphicsProxyWidget>
@@ -50,6 +53,7 @@ private slots:
   void msgUpdated(uint32_t address);
   void signalUpdated(const Signal *sig);
   void signalRemoved(const Signal *sig);
+  void manageSeries();
 
 private:
   QList<ChartView::SigItem>::iterator removeSeries(const QList<ChartView::SigItem>::iterator &it);
@@ -70,6 +74,7 @@ private:
   QGraphicsEllipseItem *track_ellipse;
   QGraphicsTextItem *value_text;
   QGraphicsProxyWidget *close_btn_proxy;
+  QGraphicsProxyWidget *manage_btn_proxy;
   std::pair<double, double> events_range = {0, 0};
   QList<SigItem> sigs;
  };
@@ -113,4 +118,22 @@ private:
   std::pair<double, double> display_range;
   std::pair<double, double> zoomed_range;
   bool use_dark_theme = false;
+};
+
+class SeriesSelector : public QDialog {
+  Q_OBJECT
+
+public:
+  SeriesSelector(QWidget *parent);
+  void addSeries(const QString &id, const QString& msg_name, const QString &sig_name);
+  QList<QStringList> series();
+
+private slots:
+  void msgSelected(int index);
+  void addSignal(QListWidgetItem *item);
+
+private:
+  QComboBox *msgs_combo;
+  QListWidget *sig_list;
+  QListWidget *chart_series;
 };
