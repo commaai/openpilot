7131fe5712e05712933fe646dbea2d8ede85835d
Shane Smiskol <shane@smiskol.com>
2022-12-01 16:30:20 -0800

    some clean up

--- system/micd.py
+++ system/micd.py
@@ -9,14 +9,17 @@ from system.swaglog import cloudlog
 
 RATE = 10
 DT_MIC = 1. / RATE
-REFERENCE_SPL = 2 * 10 ** -5  # newtons/m^2
+REFERENCE_SPL = 2e-5  # newtons/m^2
 SAMPLE_RATE = 44100
 
 
 def calculate_spl(measurements):
   # https://www.engineeringtoolbox.com/sound-pressure-d_711.html
   sound_pressure = np.sqrt(np.mean(measurements ** 2))  # RMS of amplitudes
-  sound_pressure_level = 20 * np.log10(sound_pressure / REFERENCE_SPL) if sound_pressure > 0 else 0  # dB
+  if sound_pressure > 0:
+    sound_pressure_level = 20 * np.log10(sound_pressure / REFERENCE_SPL)  # dB
+  else:
+    sound_pressure_level = 0
   return sound_pressure, sound_pressure_level
 
 
@@ -29,8 +32,7 @@ def apply_a_weighting(measurements: np.ndarray) -> np.ndarray:
   freqs = np.fft.fftfreq(measurements_windowed.size, d=1 / SAMPLE_RATE)
 
   # Calculate the A-weighting filter
-  # TODO: create global for 3.5041384e16
-  A = 3.5041384e16 * freqs ** 4 / ((freqs ** 2 + 20.598997 ** 2) * (freqs ** 2 + 12194 ** 2) * np.sqrt((freqs ** 2 + 107.65265 ** 2) * (freqs ** 2 + 737.86223 ** 2)))
+  A = 12194 ** 2 * freqs ** 4 / ((freqs ** 2 + 20.6 ** 2) * (freqs ** 2 + 12194 ** 2) * np.sqrt((freqs ** 2 + 107.7 ** 2) * (freqs ** 2 + 737.9 ** 2)))
   A /= np.max(A)  # Normalize the filter
 
   # Apply the A-weighting filter to the signal
