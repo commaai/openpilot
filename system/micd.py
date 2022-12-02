1c5b41b34449d68714543e105fa539736eac2430
Shane Smiskol <shane@smiskol.com>
2022-12-01 16:08:40 -0800

    some clean up

--- system/micd.py
+++ system/micd.py
@@ -10,71 +10,31 @@ from system.swaglog import cloudlog
 RATE = 10
 DT_MIC = 1. / RATE
 REFERENCE_SPL = 2 * 10 ** -5  # newtons/m^2
+SAMPLE_RATE = 44100
 
 
-
-def calc_loudness(measurements):
+def calculate_spl(measurements):
   # https://www.engineeringtoolbox.com/sound-pressure-d_711.html
   sound_pressure = np.sqrt(np.mean(measurements ** 2))  # RMS of amplitudes
   sound_pressure_level = 20 * np.log10(sound_pressure / REFERENCE_SPL) if sound_pressure > 0 else 0  # dB
   return sound_pressure, sound_pressure_level
 
 
-def a_weight(signal: np.ndarray) -> np.ndarray:
+def apply_a_weighting(measurements: np.ndarray) -> np.ndarray:
+  # Generate a Hanning window of the same length as the audio measurements
+  hanning_window = np.hanning(len(measurements))
+  measurements_windowed = measurements * hanning_window
+
   # Calculate the frequency axis for the signal
-  freqs = np.fft.fftfreq(signal.size, d=1 / 44100)
+  freqs = np.fft.fftfreq(measurements_windowed.size, d=1 / SAMPLE_RATE)
 
   # Calculate the A-weighting filter
+  # TODO: create global for 3.5041384e16
   A = 3.5041384e16 * freqs ** 4 / ((freqs ** 2 + 20.598997 ** 2) * (freqs ** 2 + 12194 ** 2) * np.sqrt((freqs ** 2 + 107.65265 ** 2) * (freqs ** 2 + 737.86223 ** 2)))
-  # A = 12194 ** 2 * freqs ** 4 / ((freqs ** 2 + 20.6 ** 2) * (freqs ** 2 + 12194 ** 2) * np.sqrt((freqs ** 2 + 107.7 ** 2) * (freqs ** 2 + 737.9 ** 2)))
   A /= np.max(A)  # Normalize the filter
 
   # Apply the A-weighting filter to the signal
-  return np.fft.ifft(np.fft.fft(signal) * A)
-
-def apply_a_weighting(signal: np.ndarray) -> np.ndarray:
-  # Compute the A-weighting filter
-  f = np.array([20.598997, 107.65265, 737.86223, 12194.217])
-  A1000 = 1.9997
-  num = (2 * np.pi * f) ** 2 * A1000
-  den = np.array([1, 2 * np.pi * f, (2 * np.pi * f)**2])
-  H = num / den
-
-  # Filter the signal with the A-weighting filter
-  return np.convolve(signal, H, mode="same")
-
-
-def extract_frequencies(audio_data, sample_rate):
-  # # Generate a Hanning window of the same length as the audio data
-  # hanning_window = np.hanning(len(audio_data))
-  #
-  # # Multiply the audio data by the Hanning window to apply the window
-  # audio_data_windowed = audio_data * hanning_window
-
-  # Perform the FFT on the windowed audio data
-  fft_result = np.fft.fft(audio_data)
-
-  # Get the absolute value of the complex FFT result
-  fft_magnitudes = np.abs(fft_result)
-
-  # Calculate the frequencies corresponding to the FFT result
-  fft_frequencies = np.fft.fftfreq(len(audio_data), d=1/sample_rate)
-
-  # Return the frequencies and their corresponding magnitudes
-  return (fft_frequencies, fft_magnitudes)
-
-def inverse_dft(frequencies, magnitudes, sample_rate=44100):
-  # Compute the length of the audio signal
-  signal_length = len(frequencies)
-
-  # Create an array of complex numbers representing the spectrum of the signal
-  spectrum = magnitudes * np.exp(1j * 2 * np.pi * frequencies / sample_rate)
-
-  # Compute the inverse Fourier transform of the spectrum
-  amplitudes = np.fft.ifft(spectrum)
-
-  # Return the time-domain amplitudes
-  return amplitudes
+  return np.fft.ifft(np.fft.fft(measurements_windowed) * A)
 
 
 class Mic:
@@ -84,21 +44,28 @@ class Mic:
 
     self.measurements = np.empty(0)
     self.spl_filter = FirstOrderFilter(0, 4, DT_MIC, initialized=False)
-    self.all_measurements = np.empty(0)
+    self.a_weighted_spl_filter = FirstOrderFilter(0, 4, DT_MIC, initialized=False)
 
   def update(self):
-    # self.measurements contains amplitudes from -1 to 1 which we use to
-    # calculate an uncalibrated sound pressure level
+    """
+    Using amplitude measurements, calculate an uncalibrated sound pressure and sound pressure level.
+    Then apply A-weighting to the raw amplitudes and run the same calculations again.
+
+    Logged A-weighted equivalents are rough approximations of the human-perceived loudness.
+    """
+
     if len(self.measurements) > 0:
-      self.all_measurements = np.concatenate((self.all_measurements, self.measurements))
-      np.save('/data/test_recording', self.all_measurements)
-      # https://www.engineeringtoolbox.com/sound-pressure-d_711.html
-      sound_pressure = np.sqrt(np.mean(self.measurements ** 2))  # RMS of amplitudes
-      sound_pressure_level = 20 * np.log10(sound_pressure / REFERENCE_SPL) if sound_pressure > 0 else 0  # dB
+      sound_pressure, sound_pressure_level = calculate_spl(self.measurements)
       self.spl_filter.update(sound_pressure_level)
+
+      measurements_weighted = apply_a_weighting(self.measurements)
+      sound_pressure_weighted, sound_pressure_level_weighted = calculate_spl(measurements_weighted)
+      self.a_weighted_spl_filter.update(sound_pressure_level_weighted)
     else:
       sound_pressure = 0
       sound_pressure_level = 0
+      sound_pressure_weighted = 0
+      sound_pressure_level_weighted = 0
 
     self.measurements = np.empty(0)
 
@@ -117,7 +84,7 @@ class Mic:
     if device is None:
       device = "sysdefault"
 
-    with sd.InputStream(device=device, channels=1, samplerate=44100, callback=self.callback) as stream:
+    with sd.InputStream(device=device, channels=1, samplerate=SAMPLE_RATE, callback=self.callback) as stream:
       cloudlog.info(f"micd stream started: {stream.samplerate=} {stream.channels=} {stream.dtype=} {stream.device=}")
       while True:
         self.update()
