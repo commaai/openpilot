a5f952c69b5bde4c86759886ba98e285fef07c5a
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2022-12-03 11:53:50 +0900

    remove unused space (#26680)
    old-commit-hash: 92296ce55d7e910c809658ad8f7eeaab6ded875c

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -20,7 +20,7 @@ const int bdr_s = 30;
 const int header_h = 420;
 const int footer_h = 280;
 
-const int UI_FREQ = 20;   // Hz
+const int UI_FREQ = 20; // Hz
 typedef cereal::CarControl::HUDControl::AudibleAlert AudibleAlert;
 
 const mat3 DEFAULT_CALIBRATION = {{ 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0 }};
@@ -78,7 +78,7 @@ typedef enum UIStatus {
 } UIStatus;
 
 const QColor bg_colors [] = {
-  [STATUS_DISENGAGED] =  QColor(0x17, 0x33, 0x49, 0xc8),
+  [STATUS_DISENGAGED] = QColor(0x17, 0x33, 0x49, 0xc8),
   [STATUS_OVERRIDE] = QColor(0x91, 0x9b, 0x95, 0xf1),
   [STATUS_ENGAGED] = QColor(0x17, 0x86, 0x44, 0xf1),
   [STATUS_WARNING] = QColor(0xDA, 0x6F, 0x25, 0xf1),
@@ -152,7 +152,6 @@ private:
 UIState *uiState();
 
 // device management class
-
 class Device : public QObject {
   Q_OBJECT
 
