9788beb9ef098a47c49f731a256a84d360edba32
Jason Young <jyoung8607@gmail.com>
2022-12-02 20:12:39 -0500

    Revert "Update selfdrive/car/volkswagen/interface.py"
    This reverts commit 14c7412bf74b23ae88c05cad40aafd4605a63c29.

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -230,7 +230,7 @@ class CarInterface(CarInterfaceBase):
       events.add(EventName.belowSteerSpeed)
 
     if self.CS.CP.openpilotLongitudinalControl:
-      if self.CP.minEnableSpeed > 0 and ret.vEgo < self.CP.minEnableSpeed + 2.0:
+      if ret.vEgo < self.CP.minEnableSpeed + 0.5:
         events.add(EventName.belowEngageSpeed)
       if c.enabled and ret.vEgo < self.CP.minEnableSpeed:
         events.add(EventName.speedTooLow)
