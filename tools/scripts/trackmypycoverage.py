1a120cf80ab488209549d4b75e5afc399a4ab25e
Nathan Frank <nwfrank@iastate.edu>
2022-12-04 14:27:09 -0600

    fixed indentations

--- tools/scripts/trackmypycoverage.py
+++ tools/scripts/trackmypycoverage.py
@@ -2,8 +2,8 @@ import os
 import configparser
 
 class results:
-    num_passed = 0
-    num_tested = 0
+  num_passed = 0
+  num_tested = 0
 
 # Generate report file
 report_file = open("./tools/scripts/report.txt","w")
@@ -14,64 +14,64 @@ report_file.write("======================================\n")
 report_file.close()
 
 def parse_mypy_ini():
-    config =  configparser.ConfigParser()
-    mypy_ini = os.getcwd() + "\\mypy.ini"
-    config.read(mypy_ini)
-    dirToScan = config['mypy']['files'].split(", ")
-    dirToExclude = config['mypy']['exclude'].split("/)|(")
+  config =  configparser.ConfigParser()
+  mypy_ini = os.getcwd() + "\\mypy.ini"
+  config.read(mypy_ini)
+  dirToScan = config['mypy']['files'].split(", ")
+  dirToExclude = config['mypy']['exclude'].split("/)|(")
     
-    # Remove the extra characters at the begining and end
-    dirToExclude[0] = dirToExclude[0][2:]
-    dirToExclude[-1] = dirToExclude[-1][:-2]
+  # Remove the extra characters at the begining and end
+  dirToExclude[0] = dirToExclude[0][2:]
+  dirToExclude[-1] = dirToExclude[-1][:-2]
     
-    return dirToScan, dirToExclude
+  return dirToScan, dirToExclude
 
 files_total = []
 
 def test_mypy_coverage(directory, exclude):
-    r = results()
-    files = os.listdir(directory)
-    for file in files:
-        f = directory + "/" + file
-        if file.endswith(".py"):
-            files_total.append(f)
-            r.num_tested += 1
-            b = os.system("mypy " + f + " --strict --txt-report ./tools/scripts")
-            # open both files
-            with open('./tools/scripts/index.txt','r') as indexfile, open('./tools/scripts/report.txt','a') as reportfile:
-                data = indexfile.readlines()
-                # read line 9 of from index.txt file and append line to report.txt file
-                reportfile.write(data[8])
-            if(b == 0):
-                r.num_passed += 1
+  r = results()
+  files = os.listdir(directory)
+  for file in files:
+    f = directory + "/" + file
+    if file.endswith(".py"):
+      files_total.append(f)
+      r.num_tested += 1
+      b = os.system("mypy " + f + " --strict --txt-report ./tools/scripts")
+      # open both files
+      with open('./tools/scripts/index.txt','r') as indexfile, open('./tools/scripts/report.txt','a') as reportfile:
+        data = indexfile.readlines()
+        # read line 9 of from index.txt file and append line to report.txt file
+        reportfile.write(data[8])
+        if(b == 0):
+          r.num_passed += 1
         elif(os.path.isdir(f) and file not in exclude):
-            temp = test_mypy_coverage(f, exclude)
-            r.num_passed += temp.num_passed
-            r.num_tested += temp.num_tested
-    return r
+          temp = test_mypy_coverage(f, exclude)
+          r.num_passed += temp.num_passed
+          r.num_tested += temp.num_tested
+  return r
 
 def output_file():
-    # Add to end of report
-    f = open('./tools/scripts/report.txt','a')
-    if r.num_tested == 0:
-        f.write("\nNo files tested")
-    else:
-        f.write("\nTested: " + str(r.num_tested) + " files\n")
-        f.write("Passed: " + str(r.num_passed))
-        f.write("\nPercentage Covered: " + str(round(float(r.num_passed/r.num_tested * 100),2)) + "%")
+  # Add to end of report
+  f = open('./tools/scripts/report.txt','a')
+  if r.num_tested == 0:
+    f.write("\nNo files tested")
+  else:
+    f.write("\nTested: " + str(r.num_tested) + " files\n")
+    f.write("Passed: " + str(r.num_passed))
+    f.write("\nPercentage Covered: " + str(round(float(r.num_passed/r.num_tested * 100),2)) + "%")
 
     # Delete index.txt
-    if os.path.exists("./tools/scripts/index.txt"):
-        os.remove("./tools/scripts/index.txt")
+  if os.path.exists("./tools/scripts/index.txt"):
+    os.remove("./tools/scripts/index.txt")
 
 dirToScan, dirToExclude = parse_mypy_ini()
 
 r = results()
 
 for dir in dirToScan:
-    temp = test_mypy_coverage("./" + dir, dirToExclude)
-    r.num_passed += temp.num_passed
-    r.num_tested += temp.num_tested
+  temp = test_mypy_coverage("./" + dir, dirToExclude)
+  r.num_passed += temp.num_passed
+  r.num_tested += temp.num_tested
 
 print(str(r.num_passed) + " / " + str(r.num_tested))
 
