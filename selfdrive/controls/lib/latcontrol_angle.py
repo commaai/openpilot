05d9a584cd47b7d14123a974576a9c11be8ee421
Shane Smiskol <shane@smiskol.com>
2022-12-09 18:16:26 -0800

    clean up

--- selfdrive/controls/lib/latcontrol_angle.py
+++ selfdrive/controls/lib/latcontrol_angle.py
@@ -20,34 +20,25 @@ class LatControlAngle(LatControl):
 
   def update(self, active, CS, VM, params, last_actuators, steer_limited, desired_curvature, desired_curvature_rate, llk):
     angle_log = log.ControlsState.LateralAngleState.new_message()
+    angle_log.steeringAngleDesiredDeg = float(CS.steeringAngleDeg)
+    angle_log.output = float(CS.steeringAngleDeg)
 
-    if CS.vEgo < MIN_STEER_SPEED or not active:
-      angle_log.active = False
-      angle_steers_des = float(CS.steeringAngleDeg)
-      output_angle = float(CS.steeringAngleDeg)
-    else:
+    if CS.vEgo >= MIN_STEER_SPEED and active:
+      angle_log.steeringAngleDesiredDeg = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll))
+      angle_log.steeringAngleDesiredDeg += params.angleOffsetDeg
+
+      angle_log.error = angle_log.steeringAngleDesiredDeg - CS.steeringAngleDeg
+
+      freeze_integrator = steer_limited or CS.steeringPressed or CS.vEgo < 5  # TODO check steer_limited
+      angle_log.output = self.pid.update(angle_log.error,
+                                         feedforward=angle_log.steeringAngleDesiredDeg,
+                                         speed=CS.vEgo,
+                                         freeze_integrator=freeze_integrator)
+
       angle_log.active = True
-      angle_steers_des = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll))
-      angle_steers_des += params.angleOffsetDeg
+      angle_log.p = self.pid.p
+      angle_log.i = self.pid.i
+      angle_control_saturated = abs(angle_log.steeringAngleDesiredDeg - CS.steeringAngleDeg) > STEER_ANGLE_SATURATION_THRESHOLD
+      angle_log.saturated = self._check_saturation(angle_control_saturated, CS, steer_limited)
 
-      angle_log.error = angle_steers_des - CS.steeringAngleDeg
-
-    freeze_integrator = steer_limited or CS.steeringPressed or CS.vEgo < 5
-    output_angle = self.pid.update(error,
-                                   feedforward=angle_steers_des,
-                                   speed=CS.vEgo,
-                                   freeze_integrator=freeze_integrator)
-
-    # if CS.vEgo < MIN_STEER_SPEED or not active:
-    #   angle_log.active = False
-    #   angle_steers_des = float(CS.steeringAngleDeg)
-    # else:
-    #   angle_log.active = True
-    #   angle_steers_des = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll))
-    #   angle_steers_des += params.angleOffsetDeg
-
-    angle_control_saturated = abs(angle_steers_des - CS.steeringAngleDeg) > STEER_ANGLE_SATURATION_THRESHOLD
-    angle_log.saturated = self._check_saturation(angle_control_saturated, CS, steer_limited)
-    # angle_log.steeringAngleDeg = float(CS.steeringAngleDeg)
-    angle_log.steeringAngleDesiredDeg = angle_steers_des
-    return 0, float(angle_steers_des), angle_log
+    return 0, float(angle_log.output), angle_log
