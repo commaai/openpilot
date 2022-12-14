4059d7c9243dc0fb4a713dde26a91694f632a8bf
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-12-14 14:59:14 -0800

    laikad update, renaming

--- selfdrive/locationd/laikad_helpers.py
+++ selfdrive/locationd/laikad_helpers.py
@@ -43,6 +43,67 @@ def pr_residual(measurements, posfix_functions, signal='C1C'):
   return Fx_pos
 
 
+def get_prr_sympy_func():
+  # implemting this without sympy.Matrix gives a 2x speedup at generation
+
+  # knowns, receiver position, satellite position, satellite velocity
+  ep_x, ep_y, ep_z = sympy.Symbol('ep_x'), sympy.Symbol('ep_y'), sympy.Symbol('ep_z')
+  est_pos = np.array([ep_x, ep_y, ep_z])
+  sp_x, sp_y, sp_z = sympy.Symbol('sp_x'), sympy.Symbol('sp_y'), sympy.Symbol('sp_z')
+  sat_pos = np.array([sp_x, sp_y, sp_z])
+  sv_x, sv_y, sv_z = sympy.Symbol('sv_x'), sympy.Symbol('sv_y'), sympy.Symbol('sv_z')
+  sat_vel = np.array([sv_x, sv_y, sv_z])
+  observables = sympy.Symbol('observables')
+  weight = sympy.Symbol('weight')
+
+  # unknown, receiver velocity
+  v_x, v_y, v_z = sympy.Symbol('v_x'), sympy.Symbol('v_y'), sympy.Symbol('v_z')
+  vel = np.array([v_x, v_y, v_z])
+  vel_o = sympy.Symbol('vel_o')
+
+  loss = sat_pos - est_pos
+  loss /= sympy.sqrt(loss.dot(loss))
+
+  nv = loss.dot(sat_vel - vel)
+  ov = (observables - vel_o)
+  res = (nv - ov)*weight
+
+  res = [res] + [sympy.diff(res, v) for v in [v_x, v_y, v_z, vel_o]]
+
+  return sympy.lambdify([
+      ep_x, ep_y, ep_z, sp_x, sp_y, sp_z,
+      sv_x, sv_y, sv_z, observables, weight,
+      v_x, v_y, v_z, vel_o
+    ],
+    res, modules=["numpy"])
+
+
+def prr_residual(measurements, est_pos, no_weight=False, signal='D1C'):
+  loss_func = get_prr_sympy_func()
+
+  def Fx_vel(vel, no_weight=no_weight):
+    vals, gradients = [], []
+
+    for meas in measurements:
+      if signal not in meas.observables or not np.isfinite(meas.observables[signal]):
+        continue
+
+      sat_pos = meas.sat_pos_final if meas.corrected else meas.sat_pos
+      weight = 1 if no_weight or meas.observables[signal] == 0 else (1 / meas.observables[signal])
+
+      val, *gradient = loss_func(est_pos[0], est_pos[1], est_pos[2],
+                                 sat_pos[0], sat_pos[1], sat_pos[2],
+                                 meas.sat_vel[0], meas.sat_vel[1], meas.sat_vel[2],
+                                 meas.observables[signal], weight,
+                                 vel[0], vel[1], vel[2], vel[3])
+      vals.append(val)
+      gradients.append(gradient)
+
+    return np.asarray(vals), np.asarray(gradients)
+
+  return Fx_vel
+
+
 def gauss_newton(fun, b, xtol=1e-8, max_n=25):
   for _ in range(max_n):
     # Compute function and jacobian on current estimate
@@ -87,3 +148,19 @@ def get_posfix_sympy_fun(constellation):
   res = [res] + [sympy.diff(res, v) for v in var]
 
   return sympy.lambdify([x, y, z, bc, bg, pr, sat_x, sat_y, sat_z, weight], res, modules=["numpy"])
+
+
+def calc_vel_fix(measurements, est_pos, min_measurements=6):
+  '''
+  Calculates gps velocity fix with WLS optimizer
+  returns:
+  0 -> list with velocities
+  1 -> pseudorange_rate errs
+  '''
+  if len(measurements) < min_measurements:
+    return [], []
+
+  Fx_vel = prr_residual(measurements, est_pos)
+  opt_vel = gauss_newton(Fx_vel, [0, 0, 0, 0])
+  residual, _ = Fx_vel(opt_vel, no_weight=True)
+  return opt_vel.tolist(), residual.tolist()
