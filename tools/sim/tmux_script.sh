8cd90afc43346ea247e535821fb1c411d19af976
Hamid Ebadi <hamid.ebadi@gmail.com>
2022-12-14 16:40:13 +0100

    Update tmux_script.sh
    #26503 #26595

--- tools/sim/tmux_script.sh
+++ tools/sim/tmux_script.sh
@@ -2,5 +2,5 @@
 tmux new -d -s carla-sim
 tmux send-keys "./launch_openpilot.sh" ENTER
 tmux neww
-tmux send-keys "./bridge.py $*" ENTER
+tmux send-keys "./bridge.py --high_quality --dual_camera $*" ENTER
 tmux a -t carla-sim
