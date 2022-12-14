41681e1de102e74d73fe7e13257d51681dcc8b4c
Shane Smiskol <shane@smiskol.com>
2022-12-14 01:56:57 -0800

    HKG CAN-FD: remove duplicate FW versions (#26793)
    * remove duplicate FW version
    
    * Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1447,7 +1447,6 @@ FW_VERSIONS = {
   CAR.KIA_EV6: {
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00CV1_ RDR -----      1.00 1.01 99110-CV000         ',
-      b'\xf1\x8799110CV000\xf1\x00CV1_ RDR -----      1.00 1.01 99110-CV000         ',
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00CV1 MFC  AT USA LHD 1.00 1.05 99210-CV000 211027',
@@ -1459,7 +1458,6 @@ FW_VERSIONS = {
   CAR.IONIQ_5: {
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00NE1_ RDR -----      1.00 1.00 99110-GI000         ',
-      b'\xf1\x8799110GI000\xf1\x00NE1_ RDR -----      1.00 1.00 99110-GI000         ',
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00NE1 MFC  AT USA LHD 1.00 1.02 99211-GI010 211206',
