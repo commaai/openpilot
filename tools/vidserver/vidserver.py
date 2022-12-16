29092674579eea614981b7cb3eba713c31f92468
nick <nick@egan.gg>
2022-12-16 10:54:26 -0500

    List routes in python instead of bash
    list routes, start segments
    
    list routes, start segments
    
    get segments for route
    
    done listing routes in python

--- tools/vidserver/vidserver.py
+++ tools/vidserver/vidserver.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python3
-import time
 import json
+import os
 import subprocess
+import time
 import threading
+
 from flask import Flask, send_from_directory, Response, request
+from selfdrive.loggerd.config import ROOT
+from selfdrive.loggerd.uploader import listdir_by_creation
+from tools.lib.route import SegmentName
+
+def all_segment_names(root=ROOT):
+  fake_dongle = "ffffffffffffffff"
+  segments = []
+  for segment in listdir_by_creation(root):
+    try:
+      absolute_path = str(os.path.join(root, fake_dongle + "|" + segment))
+      segments.append(SegmentName(absolute_path))
+    except:
+      pass
+  return segments
+
+def all_routes():
+  segment_names = all_segment_names()
+  route_names = [segment_name.route_name for segment_name in segment_names]
+  route_times = [route_name.time_str for route_name in route_names]
+  unique_routes = list(set(route_times))
+  return unique_routes
+
+def segments_in_route(route):
+  segment_names = [segment_name for segment_name in all_segment_names() if segment_name.time_str == route]
+  segments = [segment_name.time_str + "--" + str(segment_name.segment_num) for segment_name in segment_names]
+  return segments
+
+routes = all_routes()
+for route in routes:
+  print(route)
+  print(segments_in_route(route))
+
+exit()
 
 dir_server = "/data/openpilot/tools/vidserver/"
 dir_media = "/data/media/0/realdata/"
-
-def get_routes():
-  proc = subprocess.Popen(
-    [dir_server + "lsroute",
-      dir_media,
-    ], stdout=subprocess.PIPE
-  )
-  dat = proc.stdout.read().decode("utf-8").rstrip()
-  return dat.split("\n")
-
-def get_segments():
-  proc = subprocess.Popen(
-    [dir_server + "lssegs",
-      dir_media,
-    ], stdout=subprocess.PIPE
-  )
-  dat = proc.stdout.read().decode("utf-8").rstrip()
-  return dat.split("\n")
 
 app = Flask(__name__,)
 
