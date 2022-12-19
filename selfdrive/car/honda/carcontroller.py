4cd4ea0cdcd86a8f360848a0fb835c2730e917bf
royjr <royjr96@gmail.com>
2022-12-19 18:56:04 -0500

    remove stopped logic

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -128,18 +128,8 @@ class CarController:
   def update(self, CC, CS):
     actuators = CC.actuators
     hud_control = CC.hudControl
-
-    hud_v_cruise = hud_control.setSpeed * CV.MS_TO_MPH if hud_control.speedVisible else 255
-    if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
-      conversion = CV.MS_TO_KPH if CS.is_metric else CV.MS_TO_MPH
-      hud_v_cruise = hud_control.setSpeed * conversion if hud_control.speedVisible else 255
-      if CC.longActive and actuators.longControlState == LongCtrlState.stopping:
-        self.stopping_counter += 1
-        if self.stopping_counter > 400:
-          hud_v_cruise = 252
-      else:
-        self.stopping_counter = 0
-
+    conversion = CV.MS_TO_KPH if CS.is_metric else CV.MS_TO_MPH
+    hud_v_cruise = hud_control.setSpeed * conversion if hud_control.speedVisible else 255
     pcm_cancel_cmd = CC.cruiseControl.cancel
 
     if CC.longActive:
