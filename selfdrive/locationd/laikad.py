b6e3ab0c900e73ebab2cec8bbda0e9982500db71
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2022-12-27 13:35:55 -0800

    .

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -409,11 +409,8 @@ def main(sm=None, pm=None, qc=None):
 
       msg = process_msg(laikad, gnss_msg, sm.logMonoTime[raw_gnss_socket], replay)
       if msg is None:
-        # TODO: beautify this, locationd needs a valid message, otherwise engagement
-        # will not be allowed, either send nothing or a valid msg
-        # check this with replay tests
+        # TODO: beautify this, locationd needs a valid message
         msg = messaging.new_message("gnssMeasurements")
-        msg.valid = True
 
       pm.send('gnssMeasurements', msg)
 
