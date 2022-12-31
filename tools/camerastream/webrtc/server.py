4eaa081b487778de32698f7cd61fe5ddc6908740
Keyvan Fatehi <keyvanfatehi@gmail.com>
2022-12-30 19:59:02 -0800

    webrtc: cleaner

--- tools/camerastream/webrtc/server.py
+++ tools/camerastream/webrtc/server.py
@@ -4,60 +4,19 @@ import asyncio
 import json
 import logging
 import os
-import platform
 import ssl
 from typing import OrderedDict
-
 from aiohttp import web
-
 from aiortc import RTCPeerConnection, RTCSessionDescription, RTCRtpCodecCapability
-from aiortc.contrib.media import MediaPlayer, MediaRelay
-from aiortc.rtcrtpsender import RTCRtpSender
 from compressed_vipc_track import VisionIpcTrack
-from cereal.visionipc.visionipc_pyx import VisionStreamType # pylint: disable=no-name-in-module, import-error
 
 ROOT = os.path.dirname(__file__)
 
-
-relay = None
-webcam = None
-
-
-def create_local_tracks(play_from, decode):
-    global relay, webcam
-
-    if play_from:
-        player = MediaPlayer(play_from, decode=decode)
-        return player.audio, player.video
-    else:
-        if relay is None:
-            track = VisionIpcTrack("roadEncodeData", "192.168.88.249")
-        return None, track
-
-
-def force_codec(pc, sender):
-    # kind = forced_codec.split("/")[0]
-    # codecs = RTCRtpSender.getCapabilities(kind).codecs
-    transceiver = next(t for t in pc.getTransceivers() if t.sender == sender)
-    transceiver.setCodecPreferences(
-        # [codec for codec in codecs if codec.mimeType == forced_codec]
-        [RTCRtpCodecCapability(
-                        mimeType="video/H264",
-                        clockRate=90000,
-                        channels=None,
-                        parameters=OrderedDict([
-                        ("packetization-mode", "1"),
-                        ("level-asymmetry-allowed", "1"),
-                        ("profile-level-id", "42001f"),
-                        ])
-                    )]
-    )
-
+cams = ["roadEncodeData","wideRoadEncodeData","driverEncodeData"]
 
 async def index(request):
     content = open(os.path.join(ROOT, "index.html"), "r").read()
     return web.Response(content_type="text/html", text=content)
-
 
 async def javascript(request):
     content = open(os.path.join(ROOT, "client.js"), "r").read()
@@ -78,25 +37,26 @@ async def offer(request):
             await pc.close()
             pcs.discard(pc)
 
-    # open media source
-    audio, video = create_local_tracks(
-        args.play_from, decode=not args.play_without_decoding
+    # TODO: stream the microphone
+    audio = None
+
+    video = VisionIpcTrack(cams[int(args.cam)], args.addr)
+
+    video_sender = pc.addTrack(video)
+    transceiver = next(t for t in pc.getTransceivers() if t.sender == video_sender)
+    transceiver.setCodecPreferences(
+        # [codec for codec in codecs if codec.mimeType == forced_codec]
+        [RTCRtpCodecCapability(
+                        mimeType="video/H264",
+                        clockRate=90000,
+                        channels=None,
+                        parameters=OrderedDict([
+                        ("packetization-mode", "1"),
+                        ("level-asymmetry-allowed", "1"),
+                        ("profile-level-id", "42001f"),
+                        ])
+                    )]
     )
-
-    if audio:
-        audio_sender = pc.addTrack(audio)
-        if args.audio_codec:
-            force_codec(pc, audio_sender, args.audio_codec)
-        elif args.play_without_decoding:
-            raise Exception("You must specify the audio codec using --audio-codec")
-
-    if video:
-        video_sender = pc.addTrack(video)
-        # if args.video_codec:
-            # force_codec(pc, video_sender, args.video_codec)
-        force_codec(pc, video_sender)
-        # elif args.play_without_decoding:
-            # raise Exception("You must specify the video codec using --video-codec")
 
     await pc.setRemoteDescription(offer)
 
@@ -122,18 +82,16 @@ async def on_shutdown(app):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="WebRTC webcam demo")
+    parser = argparse.ArgumentParser(description="Decode video streams and broadcast via WebRTC")
+    parser.add_argument("addr", help="Address of comma three")
+
+    # Not implemented (yet?). Geo already made the PoC for this, it should be possible.
+    # parser.add_argument("--nvidia", action="store_true", help="Use nvidia instead of ffmpeg")
+
+    parser.add_argument("--cam", default="0", help="Camera to stream")
+
     parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
     parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-    parser.add_argument("--play-from", help="Read the media from a file and sent it."),
-    parser.add_argument(
-        "--play-without-decoding",
-        help=(
-            "Read the media without decoding it (experimental). "
-            "For now it only works with an MPEGTS container with only H.264 video."
-        ),
-        action="store_true",
-    )
     parser.add_argument(
         "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
     )
@@ -141,12 +99,6 @@ if __name__ == "__main__":
         "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
     )
     parser.add_argument("--verbose", "-v", action="count")
-    parser.add_argument(
-        "--audio-codec", help="Force a specific audio codec (e.g. audio/opus)"
-    )
-    parser.add_argument(
-        "--video-codec", help="Force a specific video codec (e.g. video/H264)"
-    )
 
     args = parser.parse_args()
 
