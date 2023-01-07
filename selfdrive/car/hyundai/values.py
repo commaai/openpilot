b74f2d102daa6acea69d0c673dbb0172f31897f3
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2023-01-06 19:54:56 -0500

    Hyundai: Add FW Versions for Tucson 2023 (#26887)

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1477,6 +1477,7 @@ FW_VERSIONS = {
   CAR.TUCSON_4TH_GEN: {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.01 99211-N9240 14T',
+      b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-CW010 14X',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00NX4__               1.01 1.00 99110-N9100         ',
