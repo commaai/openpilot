84720ec9b7b79566fc6788e503dda44aa1e63692
Rob Chouinard <gadjex@gmail.com>
2023-01-08 17:58:28 -0500

    Allow Honda Nidec vehicles with comma pedal to drive at max speed.
    Without this Honda Nidec vehicles with the comma pedal will drive around 2 mph under the max speed.

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -21,6 +21,8 @@ class CarInterface(CarInterfaceBase):
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
     if CP.carFingerprint in HONDA_BOSCH:
       return CarControllerParams.BOSCH_ACCEL_MIN, CarControllerParams.BOSCH_ACCEL_MAX
+    elif CP.enableGasInterceptor:
+      return CarControllerParams.NIDEC_ACCEL_MIN, CarControllerParams.NIDEC_ACCEL_MAX
     else:
       # NIDECs don't allow acceleration near cruise_speed,
       # so limit limits of pid to prevent windup
