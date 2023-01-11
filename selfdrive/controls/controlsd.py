a01b1a1c9339c7c4e5a8c6b1b13f3251e82e0665
Shane Smiskol <shane@smiskol.com>
2023-01-10 16:32:26 -0800

    check joystick for now

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -573,7 +573,7 @@ class Controls:
     # Check which actuators can be enabled
     standstill = CS.vEgo <= max(self.CP.minSteerSpeed, MIN_LATERAL_CONTROL_SPEED) or CS.standstill
     CC.latActive = self.active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
-                   (not standstill or self.CP.notCar)
+                   (not standstill or self.joystick_mode)
     CC.longActive = self.enabled and not self.events.any(ET.OVERRIDE_LONGITUDINAL) and self.CP.openpilotLongitudinalControl
 
     actuators = CC.actuators
