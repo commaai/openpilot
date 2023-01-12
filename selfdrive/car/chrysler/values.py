5f0db8e516f7c100536ce05145ca44f725666bd7
nuwandavek <vivekaithal44@gmail.com>
2023-01-12 15:08:32 -0800

    increase chrysler steer rate

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -46,8 +46,8 @@ class CarControllerParams:
       self.STEER_DELTA_DOWN = 6
       self.STEER_MAX = 261  # EPS allows more, up to 350?
     else:
-      self.STEER_DELTA_UP = 3
-      self.STEER_DELTA_DOWN = 3
+      self.STEER_DELTA_UP = 6
+      self.STEER_DELTA_DOWN = 6
       self.STEER_MAX = 261  # higher than this faults the EPS
 
 STEER_THRESHOLD = 120
