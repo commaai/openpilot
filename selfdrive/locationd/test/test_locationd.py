88423e25df8bfbd8e6a3275eb086f468cf9a59f1
Kurt Nistelberger <kurt.nistelberger@hotmail.com>
2023-01-13 12:37:38 -0700

    Laikad: set active (#26850)
    * laikad update, renaming
    
    * update locationd
    
    * fix naming
    
    * address PR comments
    
    * upsi
    
    * .
    
    * draft to fix replay
    
    * fix process relay to allow no response for messages
    
    * final fix for process replay
    
    * .
    
    * bump cereal
    
    * update process replay ref commit
    
    * reduce wait time
    
    * .
    
    * last ref change
    
    * move laikad helpers to laika
    
    * .
    
    * fix ublox test
    
    * update refs
    
    * add proper qcom replay support
    
    * fix gnss support if both is available
    
    * update refs
    
    * remove left over
    
    * revert laikad msg
    
    * move laika back to master
    
    * init
    
    * fix gps valid flag
    
    * change time
    
    * add gnss to ignore
    
    * remove gps_valid flag
    
    * .
    
    * adopt orientation reset threshold
    
    * .
    
    * update laikad
    
    * .
    
    * fix stanstill KF resets
    
    * test orienation reset count
    
    * update laika
    
    * bump cereal
    
    * fix process replay
    
    * update laika repo
    
    * remove handle gps
    
    * add extra logging for cache
    
    * .
    
    * add more log
    
    * .
    
    * .
    
    * update laika
    
    * dont remove gps code
    
    * inc min satellite count
    
    * update magic vals and add acc drop
    
    * update laika
    
    * upsi
    
    * rem
    
    * bump laika
    
    * use nav and correct
    
    * more fixes
    
    * use sftp
    
    * No more glonass
    
    * Revert "No more glonass"
    
    This reverts commit a76124da50a1e25f423ad1137c7a046e1d57811d.
    
    * nump laika
    
    * back support old ephemeris cache
    
    * add health to ephemeris message
    
    * bump laika
    
    * remove print
    
    * fix laikad tests
    
    * clean
    
    * remove extra log
    
    * bump laika
    
    * inc timeout for plotjuggler build
    
    * rem cache clear
    
    * .
    
    * enable gps after checks
    
    Co-authored-by: Kurt Nistelberger <kurt.nistelberger@gmail.com>
    Co-authored-by: Bruce Wayne <harald.the.engineer@gmail.com>
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/locationd/test/test_locationd.py
+++ selfdrive/locationd/test/test_locationd.py
@@ -8,13 +8,14 @@ import capnp
 import cereal.messaging as messaging
 from cereal.services import service_list
 from common.params import Params
+from common.transformations.coordinates import ecef2geodetic
 
 from selfdrive.manager.process_config import managed_processes
 
 
 class TestLocationdProc(unittest.TestCase):
   MAX_WAITS = 1000
-  LLD_MSGS = ['gpsLocationExternal', 'cameraOdometry', 'carState', 'liveCalibration',
+  LLD_MSGS = ['gnssMeasurements', 'cameraOdometry', 'carState', 'liveCalibration',
               'accelerometer', 'gyroscope', 'magnetometer']
 
   def setUp(self):
@@ -45,16 +46,14 @@ class TestLocationdProc(unittest.TestCase):
     except capnp.lib.capnp.KjException:
       msg = messaging.new_message(name, 0)
 
-    if name == "gpsLocationExternal":
-      msg.gpsLocationExternal.flags = 1
-      msg.gpsLocationExternal.verticalAccuracy = 1.0
-      msg.gpsLocationExternal.speedAccuracy = 1.0
-      msg.gpsLocationExternal.bearingAccuracyDeg = 1.0
-      msg.gpsLocationExternal.vNED = [0.0, 0.0, 0.0]
-      msg.gpsLocationExternal.latitude = self.lat
-      msg.gpsLocationExternal.longitude = self.lon
-      msg.gpsLocationExternal.unixTimestampMillis = t * 1e6
-      msg.gpsLocationExternal.altitude = self.alt
+    if name == "gnssMeasurements":
+      msg.gnssMeasurements.measTime = t
+      msg.gnssMeasurements.positionECEF.value = [self.x , self.y, self.z]
+      msg.gnssMeasurements.positionECEF.std = [0,0,0]
+      msg.gnssMeasurements.positionECEF.valid = True
+      msg.gnssMeasurements.velocityECEF.value = []
+      msg.gnssMeasurements.velocityECEF.std = [0,0,0]
+      msg.gnssMeasurements.velocityECEF.valid = True
     elif name == 'cameraOdometry':
       msg.cameraOdometry.rot = [0.0, 0.0, 0.0]
       msg.cameraOdometry.rotStd = [0.0, 0.0, 0.0]
@@ -67,9 +66,11 @@ class TestLocationdProc(unittest.TestCase):
     # first reset params
     Params().remove('LastGPSPosition')
 
-    self.lat = 30 + (random.random() * 10.0)
-    self.lon = -70 + (random.random() * 10.0)
-    self.alt = 5 + (random.random() * 10.0)
+    self.x = -2710700 + (random.random() * 1e5)
+    self.y = -4280600 + (random.random() * 1e5)
+    self.z = 3850300 + (random.random() * 1e5)
+    self.lat, self.lon, self.alt = ecef2geodetic([self.x, self.y, self.z])
+
     self.fake_duration = 90  # secs
     # get fake messages at the correct frequency, listed in services.py
     fake_msgs = []
@@ -83,10 +84,9 @@ class TestLocationdProc(unittest.TestCase):
     time.sleep(1)  # wait for async params write
 
     lastGPS = json.loads(Params().get('LastGPSPosition'))
-
-    self.assertAlmostEqual(lastGPS['latitude'], self.lat, places=3)
-    self.assertAlmostEqual(lastGPS['longitude'], self.lon, places=3)
-    self.assertAlmostEqual(lastGPS['altitude'], self.alt, places=3)
+    self.assertAlmostEqual(lastGPS['latitude'], self.lat, places=4)
+    self.assertAlmostEqual(lastGPS['longitude'], self.lon, places=4)
+    self.assertAlmostEqual(lastGPS['altitude'], self.alt, places=4)
 
 
 if __name__ == "__main__":
