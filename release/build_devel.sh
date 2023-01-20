f11d1d93ca769de389a8bbb7ab073a63fb46b4d0
chadgauth <chadgauth@gmail.com>
2023-01-19 20:56:26 -0600

    Feature/update scripts 4 zenpilot (#2)
    * update brand and scripts
    * Fix build all
    * remove intel test (permission)
    * fix build check
    * comment out valgrind test
    
    Co-authored-by: Chad Gauthier <chad.gauthier@crowdstreet.com>

--- release/build_devel.sh
+++ release/build_devel.sh
@@ -20,15 +20,15 @@ cd $TARGET_DIR
 cp -r $SOURCE_DIR/.git $TARGET_DIR
 pre-commit uninstall || true
 
-echo "[-] bringing master-ci and devel in sync T=$SECONDS"
+echo "[-] bringing experimental and release-candidate in sync T=$SECONDS"
 cd $TARGET_DIR
-git fetch --depth 1 origin master-ci
-git fetch --depth 1 origin devel
+git fetch --depth 1 origin experimental
+git fetch --depth 1 origin release-candidate
 
-git checkout -f --track origin/master-ci
-git reset --hard master-ci
-git checkout master-ci
-git reset --hard origin/devel
+git checkout -f --track origin/experimental
+git reset --hard experimental
+git checkout experimental
+git reset --hard origin/release-candidate
 git clean -xdff
 git lfs uninstall
 
@@ -77,7 +77,7 @@ fi
 
 if [ ! -z "$BRANCH" ]; then
   echo "[-] Pushing to $BRANCH T=$SECONDS"
-  git push -f origin master-ci:$BRANCH
+  git push -f origin experimental:$BRANCH
 fi
 
 echo "[-] done T=$SECONDS, ready at $TARGET_DIR"
