a49875253ad74179d608d3c040d6d26000966094
deanlee <deanlee3@gmail.com>
2023-01-21 19:57:51 +0800

    skip align charts if there only one chart

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -256,12 +256,14 @@ void ChartsWidget::removeAll() {
 }
 
 void ChartsWidget::alignCharts() {
-  int plot_left = 0;
-  for (auto c : charts) {
-    plot_left = qMax((qreal)plot_left, c->getYAsixLabelWidth());
-  }
-  for (auto c : charts) {
-    c->setPlotAreaLeftPosition(plot_left);
+  if (charts.size() > 1) {
+    int plot_left = 0;
+    for (auto c : charts) {
+      plot_left = qMax((qreal)plot_left, c->getYAsixLabelWidth());
+    }
+    for (auto c : charts) {
+      c->setPlotAreaLeftPosition(plot_left);
+    }
   }
 }
 
