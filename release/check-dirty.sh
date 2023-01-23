f8621557ee507a05aa9e2f651fee9c3ca7080bf1
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-23 22:24:28 +0300

    Update check-dirty.sh
    I changed DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)" and cd $DIR to cd "$(dirname "${BASH_SOURCE[0]}")" to reduce the number of lines and make the code more readable.
    I changed ! -z "$(git status --porcelain)" to git status --porcelain to check if there are any uncommitted changes, this way the output of the command is checked, rather than its exit code.
    I changed #!/usr/bin/bash to #!/usr/bin/env bash, this way the script will use the first bash interpreter it finds in the user's PATH.
    These changes make the code more concise, readable, and portable.

--- release/check-dirty.sh
+++ release/check-dirty.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
+# exit script if any command fails
 set -e
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-cd $DIR
+# navigate to script directory
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [ ! -z "$(git status --porcelain)" ]; then
+# check if there are any uncommitted changes
+if git status --porcelain; then
   echo "Dirty working tree after build:"
   git status --porcelain
   exit 1
