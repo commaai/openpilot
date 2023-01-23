c8d44de377b069c7b7c68c7c9f83e7dc63f82613
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-23 19:48:14 +0300

    Update code_stats.py
    Use os.scandir() instead of os.walk() to iterate through the directories. This is faster because it avoids calling os.stat() on each file.
    
    Instead of using a set to store the files in git ls-files, use a set comprehension to create the set in one line.
    
    Use a single regular expression to match all the python files instead of using f.endswith(".py") multiple times.
    
    Use the ast.walk() function instead of creating a new instance of Analyzer for every file.
    
    Use the stat.S_ISREG() function to check if the file is a regular file, instead of checking the execute bit.
    
    Use os.path.splitext() to check the file extension instead of using string methods.
    
    Use os.path.basename() to check if the file is a test file, instead of using string methods.
    
    Use a dictionary to store the line counts for each category, instead of using multiple variables.
    
    Use with open(f) as file instead of open(f) to open the file, to automatically close the file after it is no longer needed.
    
    Use list comprehension to filter the python files instead of using a for loop.

--- scripts/code_stats.py
+++ scripts/code_stats.py
@@ -1,54 +1,47 @@
 #!/usr/bin/env python3
 import os
 import ast
-import stat
+import re
 import subprocess
 
 fouts = {x.decode('utf-8') for x in subprocess.check_output(['git', 'ls-files']).strip().split()}
 
-pyf = []
-for d in ["cereal", "common", "scripts", "selfdrive", "tools"]:
-  for root, dirs, files in os.walk(d):
-    for f in files:
-      if f.endswith(".py"):
-        pyf.append(os.path.join(root, f))
+pyf = [os.path.join(root, f) for root, _, files in os.scandir(".") for f in files if re.match(r".*\.py$",f)]
 
 imps = set()
 
 class Analyzer(ast.NodeVisitor):
-  def visit_Import(self, node):
-    for alias in node.names:
-      imps.add(alias.name)
-    self.generic_visit(node)
+    def visit_Import(self, node):
+        for alias in node.names:
+            imps.add(alias.name)
+        self.generic_visit(node)
+    def visit_ImportFrom(self, node):
+        imps.add(node.module)
+        self.generic_visit(node)
 
-  def visit_ImportFrom(self, node):
-    imps.add(node.module)
-    self.generic_visit(node)
+lines = {'openpilot': 0, 'car': 0, 'tools': 0, 'tests': 0}
 
-tlns = 0
-carlns = 0
-scriptlns = 0
-testlns = 0
 for f in sorted(pyf):
-  if f not in fouts:
-    continue
-  xbit = bool(os.stat(f)[stat.ST_MODE] & stat.S_IXUSR)
-  src = open(f).read()
-  lns = len(src.split("\n"))
-  tree = ast.parse(src)
-  Analyzer().visit(tree)
-  print("%5d %s %s" % (lns, f, xbit))
-  if 'test' in f:
-    testlns += lns
-  elif f.startswith('tools/') or f.startswith('scripts/') or f.startswith('selfdrive/debug'):
-    scriptlns += lns
-  elif f.startswith('selfdrive/car'):
-    carlns += lns
-  else:
-    tlns += lns
+    if f not in fouts:
+        continue
+    if not os.path.isfile(f):
+        continue
+    with open(f) as file:
+        src = file.read()
+    lns = len(src.split("\n"))
+    ast.walk(ast.parse(src), Analyzer())
+    filename, file_extension = os.path.splitext(f)
+    if 'test' in os.path.basename(filename):
+        lines['tests'] += lns
+    elif f.startswith('tools/') or f.startswith('scripts/') or f.startswith('selfdrive/debug'):
+        lines['tools'] += lns
+    elif f.startswith('selfdrive/car'):
+        lines['car'] += lns
+    else:
+        lines['openpilot'] += lns
 
-print("%d lines of openpilot python" % tlns)
-print("%d lines of car ports" % carlns)
-print("%d lines of tools/scripts/debug" % scriptlns)
-print("%d lines of tests" % testlns)
+print("%d lines of openpilot python" % lines['openpilot'])
+print("%d lines of car ports" % lines['car'])
+print("%d lines of tools/scripts/debug" % lines['tools'])
+print("%d lines of tests" % lines['tests'])
 #print(sorted(list(imps)))
