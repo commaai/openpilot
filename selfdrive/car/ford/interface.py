80379e27b48989b1447a0509ad65f4f20cd89418
Cameron Clough <cameronjclough@gmail.com>
2023-01-23 16:41:14 -0800

    Revert "no dashcam only"
    This reverts commit 5f57cd11ef13f9b8d119da484ba8aa9754e1e2ad.

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -11,6 +11,9 @@ class CarInterface(CarInterfaceBase):
     ret.carName = "ford"
     ret.safetyConfigs = [get_safety_config(CarParams.SafetyModel.ford)]
 
+    # These cars are dashcam only until the port is finished
+    ret.dashcamOnly = True
+
     # curvature steering
     ret.steerControlType = CarParams.SteerControlType.curvature
     ret.steerActuatorDelay = 0.15
