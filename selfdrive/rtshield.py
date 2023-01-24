069848de51e3178ea5cff1ce4ab5747fd01f28f9
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-24 10:50:53 +0300

    Update rtshield.py

--- selfdrive/rtshield.py
+++ selfdrive/rtshield.py
@@ -5,17 +5,15 @@ from typing import NoReturn
 
 from common.realtime import set_core_affinity, set_realtime_priority
 
-# RT shield - ensure CPU 3 always remains available for RT processes
-#   runs as SCHED_FIFO with minimum priority to ensure kthreads don't
-#   get scheduled onto CPU 3, but it's always preemptible by realtime
-#   openpilot processes
-
 def main() -> NoReturn:
-  set_core_affinity([int(os.getenv("CORE", "3")), ])
-  set_realtime_priority(1)
+    # RT shield - ensure CPU 3 always remains available for RT processes
+    core = int(os.getenv("CORE", "3"))
+    set_core_affinity([core, ])
+    set_realtime_priority(1)
 
-  while True:
-    time.sleep(0.000001)
+    # Use a loop with a sleep time of 1ms instead of 0.000001 to reduce CPU usage
+    while True:
+        time.sleep(0.001)
 
 if __name__ == "__main__":
-  main()
+    main()
