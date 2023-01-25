98d1decfaf7e4490a69661759bf1e0a9d41f39b8
Shane Smiskol <shane@smiskol.com>
2023-01-24 22:30:29 -0800

    Honda FPv2: log extra UDS identifier (#27076)
    * Add debugging uds ID
    
    * add comment
    
    * remove spaces
    
    * try chaining the queries (temp debug)
    
    this works
    
    * works! - Revert "try chaining the queries (temp debug)"
    
    This reverts commit c53d0f64aecdbd6224b0fd524f96637d29320cd1.
    old-commit-hash: f2966e9ef4548c6365715f8e5bd55f4e4838dcdc

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -4,9 +4,10 @@ from typing import Dict, List, Optional, Union
 
 from cereal import car
 from common.conversions import Conversions as CV
+from panda.python import uds
 from selfdrive.car import dbc_dict
 from selfdrive.car.docs_definitions import CarFootnote, CarInfo, Column, Harness
-from selfdrive.car.fw_query_definitions import FwQueryConfig, Request, StdQueries
+from selfdrive.car.fw_query_definitions import FwQueryConfig, Request, StdQueries, p16
 
 Ecu = car.CarParams.Ecu
 VisualAlert = car.CarControl.HUDControl.VisualAlert
@@ -150,11 +151,25 @@ CAR_INFO: Dict[str, Optional[Union[HondaCarInfo, List[HondaCarInfo]]]] = {
   CAR.HONDA_E: HondaCarInfo("Honda e 2020", "All", min_steer_speed=3. * CV.MPH_TO_MS),
 }
 
+HONDA_VERSION_REQUEST = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER]) + \
+  p16(0xF112)
+HONDA_VERSION_RESPONSE = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER + 0x40]) + \
+  p16(0xF112)
+
 FW_QUERY_CONFIG = FwQueryConfig(
   requests=[
+    # Currently used to fingerprint
     Request(
       [StdQueries.UDS_VERSION_REQUEST],
       [StdQueries.UDS_VERSION_RESPONSE],
+      bus=1,
+    ),
+
+    # Data collection requests:
+    # Log extra identifiers for current ECUs
+    Request(
+      [HONDA_VERSION_REQUEST],
+      [HONDA_VERSION_RESPONSE],
       bus=1,
     ),
     # Query Nidec PT bus from camera for data collection
