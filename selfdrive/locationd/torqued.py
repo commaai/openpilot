3639e42ed538ee5d94ec2572cf27af6260633fad
nuwandavek <vivekaithal44@gmail.com>
2023-01-24 16:07:40 -0800

    add ram 1500 to torqued

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -37,6 +37,7 @@ MIN_ENGAGE_BUFFER = 2  # secs
 
 VERSION = 1  # bump this to invalidate old parameter caches
 ALLOWED_CARS = ['toyota', 'hyundai']
+ALLOWED_PLATFORMS = ['RAM 1500 5TH GEN']  # for adding individual platforms from a brand without torqued
 
 
 def slope2rot(slope):
@@ -115,7 +116,7 @@ class TorqueEstimator:
     self.offline_friction = 0.0
     self.offline_latAccelFactor = 0.0
     self.resets = 0.0
-    self.use_params = CP.carName in ALLOWED_CARS
+    self.use_params = (CP.carName in ALLOWED_CARS) or (CP.carFingerprint in ALLOWED_PLATFORMS)
 
     if CP.lateralTuning.which() == 'torque':
       self.offline_friction = CP.lateralTuning.torque.friction
