e90c7a487b109aba0a0856f0c1b586ab0900bdfa
Radosław Chybicki <rchybicki@gmail.com>
2023-01-26 21:30:46 +0100

    Santa Fe HEV 2022: add missing FW versions (#27050)
    * Santa Fe Hev 2022 Fingerprint
    
    * Concatenated second value for transmission
    
    * fixes
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -766,19 +766,23 @@ FW_VERSIONS = {
   },
   CAR.SANTA_FE_HEV_2022: {
     (Ecu.fwdRadar, 0x7d0, None): [
-      b'\xf1\x8799110CL500\xf1\x00TMhe SCC FHCUP      1.00 1.00 99110-CL500         ',
+      b'\xf1\x00TMhe SCC FHCUP      1.00 1.00 99110-CL500         ',
     ],
     (Ecu.eps, 0x7d4, None): [
       b'\xf1\x00TM  MDPS C 1.00 1.02 56310-CLAC0 4TSHC102',
+      b'\xf1\x00TM  MDPS R 1.00 1.05 57700-CL000 4TSHP105',
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
+      b'\xf1\x00TMH MFC  AT EUR LHD 1.00 1.06 99211-S1500 220727',
       b'\xf1\x00TMH MFC  AT USA LHD 1.00 1.03 99211-S1500 210224',
     ],
     (Ecu.transmission, 0x7e1, None): [
+      b'\xf1\x00PSBG2333  E16\x00\x00\x00\x00\x00\x00\x00TTM2H16UA3I\x94\xac\x8f',
       b'\xf1\x87959102T250\x00\x00\x00\x00\x00\xf1\x81E14\x00\x00\x00\x00\x00\x00\x00\xf1\x00PSBG2333  E14\x00\x00\x00\x00\x00\x00\x00TTM2H16SA2\x80\xd7l\xb2',
     ],
     (Ecu.engine, 0x7e0, None): [
       b'\xf1\x87391312MTC1',
+      b'\xf1\x87391312MTE0',
     ],
   },
   CAR.SANTA_FE_PHEV_2022: {
