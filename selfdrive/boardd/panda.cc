4e69844178215852e88310a9b2f05a5a5f3814fc
Willem Melching <willem.melching@gmail.com>
2023-01-30 15:08:35 +0100

    off by one

--- selfdrive/boardd/panda.cc
+++ selfdrive/boardd/panda.cc
@@ -237,7 +237,7 @@ void Panda::can_send(capnp::List<cereal::CanData>::Reader can_data_list) {
 
 bool Panda::can_receive(std::vector<can_frame>& out_vec) {
   // Check if enough space left in buffer to store RECV_SIZE data
-  assert(receive_buffer_size + RECV_SIZE < sizeof(receive_buffer));
+  assert(receive_buffer_size + RECV_SIZE <= sizeof(receive_buffer));
 
   int recv = handle->bulk_read(0x81, &receive_buffer[receive_buffer_size], RECV_SIZE);
   if (!comms_healthy()) {
