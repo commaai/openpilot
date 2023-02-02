24f4ada36d21a8ec841b2d866b1dc142baae8eb2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-02-02 10:25:20 -0800

    updated: fix brief notification of update for same version (#27182)

--- common/params.cc
+++ common/params.cc
@@ -171,7 +171,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Timezone", PERSISTENT},
     {"TrainingVersion", PERSISTENT},
     {"UbloxAvailable", PERSISTENT},
-    {"UpdateAvailable", CLEAR_ON_MANAGER_START},
+    {"UpdateAvailable", CLEAR_ON_MANAGER_START | CLEAR_ON_IGNITION_ON},
     {"UpdateFailedCount", CLEAR_ON_MANAGER_START},
     {"UpdaterState", CLEAR_ON_MANAGER_START},
     {"UpdaterFetchAvailable", CLEAR_ON_MANAGER_START},
