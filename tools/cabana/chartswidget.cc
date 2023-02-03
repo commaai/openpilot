27cd911c6b7814c47afa8ae2732cb9a995f5a88b
Dean Lee <deanlee3@gmail.com>
2023-02-04 03:40:35 +0800

    cabana: fix chart buttons occasionally did not respond to click. (#27198)
    * fix btn not responding on click
    
    * change icon from gear to list(menu)
    old-commit-hash: bd66f77a3b5f59c1b0f72a4a655f0ca883b35f11

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -322,7 +322,7 @@ ChartView::ChartView(QWidget *parent) : QChartView(nullptr, parent) {
 
   QToolButton *manage_btn = new QToolButton();
   manage_btn->setToolButtonStyle(Qt::ToolButtonIconOnly);
-  manage_btn->setIcon(utils::icon("gear"));
+  manage_btn->setIcon(utils::icon("list"));
   manage_btn->setAutoRaise(true);
   QMenu *menu = new QMenu(this);
   line_series_action = menu->addAction(tr("Line"), [this]() { setSeriesType(QAbstractSeries::SeriesTypeLine); });
@@ -613,7 +613,7 @@ void ChartView::leaveEvent(QEvent *event) {
 
 void ChartView::mousePressEvent(QMouseEvent *event) {
   if (event->button() == Qt::LeftButton && !chart()->plotArea().contains(event->pos()) &&
-      !manage_btn_proxy->widget()->underMouse() && !close_btn_proxy->widget()->underMouse()) {
+      !manage_btn_proxy->geometry().contains(event->pos()) && !close_btn_proxy->geometry().contains(event->pos())) {
     QMimeData *mimeData = new QMimeData;
     mimeData->setData(mime_type, QByteArray::number((qulonglong)this));
     QDrag *drag = new QDrag(this);
