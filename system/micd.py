677fd040345672716b5c2ccd07b6c58f9c6352b4
grekiki <96022003+GregorKikelj@users.noreply.github.com>
2023-02-03 22:13:32 +0100

    micd: Make sure function always returns >=0 db

--- system/micd.py
+++ system/micd.py
@@ -16,7 +16,7 @@ FILTER_DT = 1. / (SAMPLE_RATE / FFT_SAMPLES)
 def calculate_spl(measurements):
   # https://www.engineeringtoolbox.com/sound-pressure-d_711.html
   sound_pressure = np.sqrt(np.mean(measurements ** 2))  # RMS of amplitudes
-  if sound_pressure > 0:
+  if sound_pressure >= REFERENCE_SPL:
     sound_pressure_level = 20 * np.log10(sound_pressure / REFERENCE_SPL)  # dB
   else:
     sound_pressure_level = 0
