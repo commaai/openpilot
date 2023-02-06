60c02cf87b14fbf0001f2d5a8bfd4a67169adce7
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2023-02-05 21:58:19 -0500

    Toyota TSS2 longitudinal improvements (#108)
    * Toyota TSS2 longitudinal improvements
    
    * IDE doesn't like it

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -220,14 +220,14 @@ class CarInterface(CarInterfaceBase):
     tune.deadzoneBP = [0., 9.]
     tune.deadzoneV = [.0, .15]
     if candidate in TSS2_CAR or ret.enableGasInterceptor:
-      tune.kpBP = [0., 5., 20.]
-      tune.kpV = [1.3, 1.0, 0.7]
-      tune.kiBP = [0., 5., 12., 20., 27.]
-      tune.kiV = [.35, .23, .20, .17, .1]
+      tune.kpBP = [0., 5., 20., 30.]
+      tune.kpV = [1.3, 1.0, 0.7, 0.1]
+      tune.kiBP = [0., 8.3, 11.9, 13.8, 19.7, 30., 33., 40.]
+      tune.kiV = [.02, .03, .14, .18, .18, .06, .01, .001]
       if candidate in TSS2_CAR:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
-        ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.009  # reach stopping target smoothly
     else:
       tune.kpBP = [0., 5., 35.]
       tune.kiBP = [0., 35.]
