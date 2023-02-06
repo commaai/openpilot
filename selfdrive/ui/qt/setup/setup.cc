46e01db636c95c14a7443c102add0dd26e6e3fcd
Cameron Clough <cameronjclough@gmail.com>
2023-02-06 14:24:29 -0800

    setup: continue without wi-fi on ethernet

--- selfdrive/ui/qt/setup/setup.cc
+++ selfdrive/ui/qt/setup/setup.cc
@@ -178,7 +178,7 @@ QWidget * Setup::network_setup() {
   QObject::connect(request, &HttpRequest::requestDone, [=](const QString &, bool success) {
     cont->setEnabled(success);
     if (success) {
-      const bool cell = networking->wifi->currentNetworkType() == NetworkType::CELL;
+      const bool cell = networking->wifi->currentNetworkType() != NetworkType::WIFI;
       cont->setText(cell ? tr("Continue without Wi-Fi") : tr("Continue"));
     } else {
       cont->setText(tr("Waiting for internet"));
