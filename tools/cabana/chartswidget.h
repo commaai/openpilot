1a99fd36101fbe4cbe9853b1df5dfe6c59fea5c4
Dean Lee <deanlee3@gmail.com>
2023-02-07 08:39:16 +0800

    cabana: fixed wrong plot area after changed column count (#27228)

--- tools/cabana/chartswidget.h
+++ tools/cabana/chartswidget.h
@@ -33,7 +33,7 @@ public:
   void updateSeries(const Signal *sig = nullptr, const std::vector<Event*> *events = nullptr, bool clear = true);
   void updatePlot(double cur, double min, double max);
   void setSeriesType(QAbstractSeries::SeriesType type);
-  void updatePlotArea(int left = 0);
+  void updatePlotArea(int left);
 
   struct SigItem {
     QString msg_id;
@@ -51,6 +51,7 @@ signals:
   void zoomIn(double min, double max);
   void zoomReset();
   void remove();
+  void axisYLabelWidthChanged(int w);
 
 private slots:
   void msgRemoved(uint32_t address);
@@ -78,7 +79,7 @@ private:
   QXYSeries *createSeries(QAbstractSeries::SeriesType type);
   void updateSeriesPoints();
 
-  int y_label_width = 50;
+  int y_label_width = 0;
   int align_to = 0;
   QValueAxis *axis_x;
   QValueAxis *axis_y;
@@ -115,7 +116,7 @@ signals:
 
 private:
   void resizeEvent(QResizeEvent *event) override;
-  void alignCharts(bool force = false);
+  void alignCharts();
   void newChart();
   ChartView * createChart();
   void removeChart(ChartView *chart);
@@ -148,7 +149,7 @@ private:
   QAction *columns_cb_action;
   QComboBox *columns_cb;
   int column_count = 1;
-  int align_to = 0;
+  int current_column_count = 0;
 };
 
 class SeriesSelector : public QDialog {
