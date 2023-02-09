7d58b7169eb2d6b0629ec1bc91a510884f83be27
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2023-02-09 00:01:52 -0500

    Unused
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -2,7 +2,6 @@
 from cereal import car
 from panda import Panda
 from common.conversions import Conversions as CV
-from selfdrive.car.hyundai.hyundaicanfd import get_e_can_bus
 from selfdrive.car.hyundai.values import HyundaiFlags, CAR, DBC, CANFD_CAR, CAMERA_SCC_CAR, CANFD_RADAR_SCC_CAR, EV_CAR, HYBRID_CAR, LEGACY_SAFETY_MODE_CAR, Buttons
 from selfdrive.car.hyundai.radar_interface import RADAR_START_ADDR
 from selfdrive.car import STD_CARGO_KG, create_button_event, scale_tire_stiffness, get_safety_config
