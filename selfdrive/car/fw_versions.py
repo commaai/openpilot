1db0a3c0b1eccadb3b8dd645202e3f89f907a539
Shane Smiskol <shane@smiskol.com>
2023-02-14 14:40:50 -0800

    don't add logging FW to FW dict for fingerprinting

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -29,7 +29,7 @@ def chunks(l, n=128):
 def build_fw_dict(fw_versions, filter_brand=None):
   fw_versions_dict = defaultdict(set)
   for fw in fw_versions:
-    if filter_brand is None or fw.brand == filter_brand:
+    if (filter_brand is None or fw.brand == filter_brand) and not fw.logging:
       addr = fw.address
       sub_addr = fw.subAddress if fw.subAddress != 0 else None
       fw_versions_dict[(addr, sub_addr)].add(fw.fwVersion)
