df2d3090ee9f624b3142a514c8468cab8f33680a
Shane Smiskol <shane@smiskol.com>
2023-02-14 19:22:06 -0800

    PCM's perspective: random counter -> random counter

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -168,8 +168,7 @@ class CarController:
           # send resume at a max freq of 10Hz
           if (self.frame - self.last_button_frame) * DT_CTRL > 0.1:
             # send 25 messages at a time to increases the likelihood of resume being accepted
-            resume_count = self.frame - self.last_button_frame - 10 - 1
-            can_sends.extend([hyundaican.create_clu11(self.packer, resume_count, CS.clu11, Buttons.RES_ACCEL, self.CP.carFingerprint)] * 25)
+            can_sends.extend([hyundaican.create_clu11(self.packer, self.frame, CS.clu11, Buttons.RES_ACCEL, self.CP.carFingerprint)] * 25)
             if (self.frame - self.last_button_frame) * DT_CTRL > 0.15:
               self.last_button_frame = self.frame
 
