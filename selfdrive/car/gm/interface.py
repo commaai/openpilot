89934b35d04291384b7a287a41ce49905f321167
Shane Smiskol <shane@smiskol.com>
2023-02-15 16:27:00 -0800

    Car interfaces: assert reasonable centerToFront range (#27356)
    * assert reasonable centerToFront values
    
    * comment
    
    * fix Bolt centerToFront
    
    * Update ref_commit

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -213,7 +213,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1669. + STD_CARGO_KG
       ret.wheelbase = 2.63779
       ret.steerRatio = 16.8
-      ret.centerToFront = 2.15  # measured
+      ret.centerToFront = ret.wheelbase * 0.4
       tire_stiffness_factor = 1.0
       ret.steerActuatorDelay = 0.12
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
