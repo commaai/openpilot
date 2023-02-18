e95c2bfbfd12b1b9bb7c4f5bbbc947cb8970c9f3
jon-chuang <jon-chuang@users.noreply.github.com>
2023-02-18 15:54:39 +0800

    refactor

---
+++ tools/sim/run_bridge.py
@@ -0,0 +1,38 @@
+from multiprocessing import Queue
+from tools.sim.bridge.carla import CarlaBridge
+from tools.sim.bridge.metadrive import MetaDriveBridge
+from tools.sim.bridge.common import parse_args, SimulatorBridge
+from common.params import Params
+
+from typing import Any
+
+if __name__ == "__main__":
+  q: Any = Queue()
+  args = parse_args()
+
+  try:
+    simulator_bridge: SimulatorBridge
+    if args.simulator == "metadrive":
+      simulator_bridge = MetaDriveBridge(args)
+    elif args.simulator == "carla":
+      simulator_bridge = CarlaBridge(args)
+    else:
+      raise AssertionError("simulator type not supported")
+    p = simulator_bridge.run(q)
+
+    if args.joystick:
+      # start input poll for joystick
+      from tools.sim.lib.manual_ctrl import wheel_poll_thread
+
+      wheel_poll_thread(q)
+    else:
+      # start input poll for keyboard
+      from tools.sim.lib.keyboard_ctrl import keyboard_poll_thread
+
+      keyboard_poll_thread(q)
+    p.join()
+
+  finally:
+    # Try cleaning up the wide camera param
+    # in case users want to use replay after
+    Params().remove("WideCameraOnly")
