a6aac78b0d350056ae87e9b9533ad0f138f9e51f
deanlee <deanlee3@gmail.com>
2023-02-20 20:58:44 +0800

    show command's text in statusbar

--- tools/cabana/commands.h
+++ tools/cabana/commands.h
@@ -9,13 +9,13 @@ using namespace dbcmanager;
 
 class EditMsgCommand : public QUndoCommand {
 public:
-  EditMsgCommand(const MessageId &id, const QString &title, int size, QUndoCommand *parent = nullptr);
+  EditMsgCommand(const MessageId &id, const QString &name, int size, QUndoCommand *parent = nullptr);
   void undo() override;
   void redo() override;
 
 private:
   const MessageId id;
-  QString old_title, new_title;
+  QString old_name, new_name;
   int old_size = 0, new_size = 0;
 };
 
