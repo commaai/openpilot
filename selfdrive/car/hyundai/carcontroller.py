dd35865e0b0f79f21fc9990f4cc67f34c57a9ccb
Jason Wen <haibin.wen3@gmail.com>
2023-02-20 17:51:44 -0500

    Use new flag

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -177,7 +177,7 @@ class CarController:
         jerk = 3.0 if actuators.longControlState == LongCtrlState.pid else 1.0
         can_sends.extend(hyundaican.create_acc_commands(self.packer, CC.enabled, accel, jerk, int(self.frame / 2),
                                                         hud_control.leadVisible, set_speed_in_units, stopping,
-                                                        CC.cruiseControl.override, self.CP.carFingerprint in FEATURES["use_fca"]))
+                                                        CC.cruiseControl.override, self.CP.flags & HyundaiFlags.USE_FCA))
 
       # 20 Hz LFA MFA message
       if self.frame % 5 == 0 and self.CP.flags & HyundaiFlags.SEND_LFA.value:
@@ -185,7 +185,7 @@ class CarController:
 
       # 5 Hz ACC options
       if self.frame % 20 == 0 and self.CP.openpilotLongitudinalControl:
-        can_sends.extend(hyundaican.create_acc_opt(self.packer, self.CP.carFingerprint in FEATURES["use_fca"]))
+        can_sends.extend(hyundaican.create_acc_opt(self.packer, self.CP.flags & HyundaiFlags.USE_FCA))
 
       # 2 Hz front radar options
       if self.frame % 50 == 0 and self.CP.openpilotLongitudinalControl:
