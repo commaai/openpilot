4cf0ce33d58413d915a8d12623253dead3df76a1
Shane Smiskol <shane@smiskol.com>
2023-02-26 17:32:52 -0800

    steer limits: rename common dist to meas function (#27453)
    * rename function
    
    * make a wrapper function (ford uses dynamic up/down limits
    
    * make two functions consistent
    
    * make torque function convert to int
    old-commit-hash: 581fd62d2642629b23ac111112245a963202143f

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -1,6 +1,6 @@
 from opendbc.can.packer import CANPacker
 from common.realtime import DT_CTRL
-from selfdrive.car import apply_toyota_steer_torque_limits
+from selfdrive.car import apply_meas_steer_torque_limits
 from selfdrive.car.chrysler.chryslercan import create_lkas_hud, create_lkas_command, create_cruise_buttons
 from selfdrive.car.chrysler.values import RAM_CARS, CarControllerParams, ChryslerFlags
 
@@ -67,7 +67,7 @@ class CarController:
 
       # steer torque
       new_steer = int(round(CC.actuators.steer * self.params.STEER_MAX))
-      apply_steer = apply_toyota_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, self.params)
+      apply_steer = apply_meas_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, self.params)
       if not lkas_active or not lkas_control_bit:
         apply_steer = 0
       self.apply_steer_last = apply_steer
