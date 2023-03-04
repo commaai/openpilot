38e5bdd2e9f5ae3e324cfc09945a77d29c6e43b6
Jason Wen <haibin.wen3@gmail.com>
2023-03-03 19:14:26 -0500

    6b checksum

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -94,9 +94,8 @@ def create_lkas11_new(packer, frame, apply_steer, steer_req,
 
   dat = packer.make_can_msg("LKAS11", 4, values)[2]
 
-  # CRC Checksum as seen on 2019 Hyundai Santa Fe
-  dat = dat[:6] + dat[7:8]
-  checksum = hyundai_checksum(dat)
+  # Checksum of first 6 Bytes, as seen on 2018 Kia Sorento
+  checksum = sum(dat[:6]) % 256
 
   values.update({
     "CF_Lkas_Chksum": checksum,
@@ -134,8 +133,8 @@ def create_lfahda_mfc_new(packer, frame, enabled, lfahda_mfc):
 
   dat = packer.make_can_msg("LFAHDA_MFC", 4, values)[2]
 
-  dat = dat[:6] + dat[7:8]
-  checksum = hyundai_checksum(dat)
+  # Checksum of first 6 Bytes, as seen on 2018 Kia Sorento
+  checksum = sum(dat[:6]) % 256
 
   values.update({
     "CHECKSUM": checksum,
