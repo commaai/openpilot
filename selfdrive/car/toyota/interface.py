3125a28bf4b249dbde13d1e7645b12d3ac89b1dc
crispbee <84819466+crispbee@users.noreply.github.com>
2023-03-04 04:27:39 -0400

    Toyota: Add FW for 2023 RAV4 Hybrid (#27494)
    * Toyota: Add FW for 2023 RAV4 Hybrid
    
    * Set RAVH_TSS2_2022 to use angle SteerControlType

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -126,7 +126,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV = [0.6]
       ret.lateralTuning.pid.kiV = [0.1]
       ret.lateralTuning.pid.kf = 0.00007818594
-      if candidate == CAR.RAV4_TSS2_2022:
+      if candidate in (CAR.RAV4_TSS2_2022, CAR.RAV4H_TSS2_2022):
         ret.steerControlType = car.CarParams.SteerControlType.angle
 
       # 2019+ RAV4 TSS2 uses two different steering racks and specific tuning seems to be necessary.
