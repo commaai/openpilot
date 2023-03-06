af3e4aa700a8f202827536deab3c970be369a9bb
Shane Smiskol <shane@smiskol.com>
2023-03-05 16:57:33 -0800

    this too

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -517,7 +517,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
   }
 
   // paint path
-  double t = millis_since_boot();
+//  double t = millis_since_boot();
   QLinearGradient bg(0, height(), 0, 0);
   if (sm["controlsState"].getControlsState().getExperimentalMode()) {
 
@@ -556,7 +556,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
 
   painter.setBrush(bg);
   painter.drawPolygon(scene.track_vertices);
-  double dt = millis_since_boot() - t;
+//  double dt = millis_since_boot() - t;
 //  qDebug() << "Took" << dt << "ms to draw path";
 
   painter.restore();
