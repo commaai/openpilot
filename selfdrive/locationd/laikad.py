c10fc3e8a85b4a0b879b16d4f228699e9bf98e91
Bruce Wayne <harald.the.engineer@gmail.com>
2023-03-16 20:29:35 -0700

    sort ephem status list

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -141,7 +141,7 @@ class Laikad:
 
   def create_ephem_statuses(self):
     ephemeris_statuses = []
-    prns_to_check = self.astro_dog.get_all_ephem_prns()
+    prns_to_check = list(self.astro_dog.get_all_ephem_prns()).sort()
     for prn in prns_to_check:
       eph = self.astro_dog.get_eph(prn, self.last_report_time)
       if eph is not None:
