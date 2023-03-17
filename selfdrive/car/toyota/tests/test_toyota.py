bc04ab6a2179fdec760b84e2dbdb1054c24f8f91
Shane Smiskol <shane@smiskol.com>
2023-03-17 00:02:38 -0700

    Update selfdrive/car/toyota/tests/test_toyota.py

--- selfdrive/car/toyota/tests/test_toyota.py
+++ selfdrive/car/toyota/tests/test_toyota.py
@@ -5,7 +5,7 @@ from selfdrive.car.toyota.values import TSS2_CAR, ANGLE_CONTROL_CAR
 
 
 class TestToyotaInterfaces(unittest.TestCase):
-  def test_angle_tss2_sets(self):
+  def test_angle_car_set(self):
     self.assertTrue(len(ANGLE_CONTROL_CAR - TSS2_CAR) == 0)
 
 
