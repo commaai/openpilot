33105c96e001721ab60118a067c94c326f827113
Shane Smiskol <shane@smiskol.com>
2023-03-22 21:35:14 -0700

    use it in fw_versions

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 from collections import defaultdict
-from typing import Any, Optional, Set, Tuple
+from typing import Any, Optional, Set
 from tqdm import tqdm
 
 import panda.python.uds as uds
 from cereal import car
 from common.params import Params
-from selfdrive.car.ecu_addrs import get_ecu_addrs
+from selfdrive.car.ecu_addrs import EcuAddrBusType, get_ecu_addrs
 from selfdrive.car.interfaces import get_interface_attr
 from selfdrive.car.fingerprints import FW_VERSIONS
 from selfdrive.car.isotp_parallel_query import IsoTpParallelQuery
@@ -146,7 +146,7 @@ def match_fw_to_car(fw_versions, allow_exact=True, allow_fuzzy=True):
   return True, set()
 
 
-def get_present_ecus(logcan, sendcan, num_pandas=1) -> Set[Tuple[int, Optional[int], int]]:
+def get_present_ecus(logcan, sendcan, num_pandas=1) -> Set[EcuAddrBusType]:
   queries = list()
   parallel_queries = list()
   responses = set()
