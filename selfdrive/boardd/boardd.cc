03779b6f0d2c8b42fd2813178d19f0dcd1ba74c3
Shane Smiskol <shane@smiskol.com>
2023-03-22 17:21:07 -0700

    we need this to start off, unless we set multiplexing immediately

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -113,6 +113,12 @@ bool safety_setter_thread(std::vector<Panda *> pandas) {
     return false;
   }
 
+  // set to ELM327 for fingerprinting
+  for (int i = 0; i < pandas.size(); i++) {
+    const uint16_t safety_param = (i > 0) ? 1U : 0U;
+    pandas[i]->set_safety_model(cereal::CarParams::SafetyModel::ELM327, safety_param);
+  }
+
   // openpilot can switch between multiplexing modes for different FW queries,
   // set to ELM327 and wait for FW query to finish
   while (true) {
