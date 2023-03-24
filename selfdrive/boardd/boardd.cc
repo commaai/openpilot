a0e225e4afe971f92fa0980c3f89b1d9818f53a5
Shane Smiskol <shane@smiskol.com>
2023-03-23 21:14:57 -0700

    FPv2: don't multiplex second panda (#27663)
    * fix this way
    
    * or can fix this way
    
    * simplify
    
    * fix

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -127,8 +127,8 @@ bool safety_setter_thread(std::vector<Panda *> pandas) {
 
     bool obd_multiplexing_requested = p.getBool("ObdMultiplexingEnabled");
     if (obd_multiplexing_requested != obd_multiplexing_enabled) {
-      const uint16_t safety_param = obd_multiplexing_requested ? 0U : 1U;
       for (int i = 0; i < pandas.size(); i++) {
+        const uint16_t safety_param = (i > 0 || !obd_multiplexing_requested) ? 1U : 0U;
         pandas[i]->set_safety_model(cereal::CarParams::SafetyModel::ELM327, safety_param);
       }
       obd_multiplexing_enabled = obd_multiplexing_requested;
