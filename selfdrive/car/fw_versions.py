03b2918e7c8f221d67072abfeae63809a554ab10
Shane Smiskol <shane@smiskol.com>
2023-03-24 21:03:04 -0700

    match against extra ecus

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -39,8 +39,12 @@ def build_fw_dict(fw_versions, filter_brand=None):
 def get_brand_addrs():
   brand_addrs = defaultdict(set)
   for brand, cars in VERSIONS.items():
+    # Add ecus in database + extra ecus to match against
+    brand_addrs[brand] |= {(addr, sub_addr) for _, addr, sub_addr in FW_QUERY_CONFIGS[brand].extra_ecus}
+
     for fw in cars.values():
       brand_addrs[brand] |= {(addr, sub_addr) for _, addr, sub_addr in fw.keys()}
+
   return brand_addrs
 
 
