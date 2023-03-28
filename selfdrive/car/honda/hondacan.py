247fe10a60db4586dbfd1210bad6219a96c50035
Shane Smiskol <shane@smiskol.com>
2023-03-27 23:55:38 -0700

    one line is fine

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -23,9 +23,7 @@ def get_lkas_cmd_bus(car_fingerprint, radar_disabled=False):
 
 def get_cruise_speed_conversion(car_fingerprint: str, is_metric: bool) -> float:
   # on certain cars, CRUISE_SPEED changes to imperial with car's unit setting
-  if car_fingerprint in HONDA_BOSCH_RADARLESS and not is_metric:
-    return CV.MPH_TO_MS
-  return CV.KPH_TO_MS
+  return CV.MPH_TO_MS if car_fingerprint in HONDA_BOSCH_RADARLESS and not is_metric else CV.KPH_TO_MS
 
 
 def create_brake_command(packer, apply_brake, pump_on, pcm_override, pcm_cancel_cmd, fcw, car_fingerprint, stock_brake):
