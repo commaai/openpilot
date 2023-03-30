c0772f6bc4f3173ebaf9d6cb3f709d3a3035a5c1
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-03-30 13:50:54 -0700

    revert that

--- selfdrive/debug/set_car_params.py
+++ selfdrive/debug/set_car_params.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
   else:
     CP = car.CarParams.new_message()
     CP.openpilotLongitudinalControl = True
-    CP.experimentalLongitudinalAvailable = True
+    CP.experimentalLongitudinalAvailable = False
 
   cp_bytes = CP.to_bytes()
   for p in ("CarParams", "CarParamsCache", "CarParamsPersistent"):
