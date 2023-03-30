ed3811ac1f475226a7011a91ede098cfb057437c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-03-30 13:23:25 -0700

    update refs

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -439,10 +439,7 @@ def main(sm=None, pm=None):
 
   # disable until set as main gps source, to better analyze startup time
   use_internet = False  # "LAIKAD_NO_INTERNET" not in os.environ
-
   replay = "REPLAY" in os.environ
-  #if replay or "CI" in os.environ:
-  #  use_internet = True
 
   laikad = Laikad(save_ephemeris=not replay, auto_fetch_navs=use_internet, use_qcom=use_qcom)
 
