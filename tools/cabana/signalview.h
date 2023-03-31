26b952f8aa0d84c65b39e7df9359f2b169d938a3
deanlee <deanlee3@gmail.com>
2023-04-01 03:28:42 +0800

    show minmax value

--- tools/cabana/signalview.h
+++ tools/cabana/signalview.h
@@ -85,6 +85,7 @@ public:
   QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
   bool helpEvent(QHelpEvent *event, QAbstractItemView *view, const QStyleOptionViewItem &option, const QModelIndex &index) override;
   void drawSparkline(QPainter *painter, const QRect &rect, const QModelIndex &index) const;
+  void 	updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
   QValidator *name_validator, *double_validator;
   QFont small_font;
   const int color_label_width = 18;
