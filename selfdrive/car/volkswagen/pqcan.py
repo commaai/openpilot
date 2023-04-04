6c775efcf00e975c67ab58bc1e2830212a0466dc
Jason Young <jyoung8607@gmail.com>
2023-04-04 15:50:05 -0400

    VW PQ: Miscellaneous longitudinal fixes

--- selfdrive/car/volkswagen/pqcan.py
+++ selfdrive/car/volkswagen/pqcan.py
@@ -64,9 +64,10 @@ def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_cont
 
   values = {
     "ACS_Sta_ADR": acc_control,
-    "ACS_StSt_Info": acc_control != 1,
+    "ACS_StSt_Info": acc_enabled,
     "ACS_Typ_ACC": acc_type,
     "ACS_Anhaltewunsch": acc_type == 1 and stopping,
+    "ACS_FreigSollB": acc_enabled,
     "ACS_Sollbeschl": accel if acc_enabled else 3.01,
     "ACS_zul_Regelabw": 0.2 if acc_enabled else 1.27,
     "ACS_max_AendGrad": 3.0 if acc_enabled else 5.08,
@@ -83,8 +84,8 @@ def create_acc_hud_control(packer, bus, acc_hud_status, set_speed, lead_distance
     "ACA_Zeitluecke": 2,
     "ACA_V_Wunsch": set_speed,
     "ACA_gemZeitl": lead_distance,
-    # TODO: ACA_ID_StaACC, ACA_AnzDisplay, ACA_kmh_mph, ACA_PrioDisp, ACA_Aend_Zeitluecke
-    # display/display-prio handling probably needed to stop confusing the instrument cluster
+    "ACA_PrioDisp": 3,
+    # TODO: ACA_ID_StaACC, ACA_AnzDisplay, ACA_kmh_mph, ACA_Aend_Zeitluecke
     # kmh_mph handling probably needed to resolve rounding errors in displayed setpoint
   }
 
