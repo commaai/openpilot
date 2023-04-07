66f5f8ecfd77a6a6a61f5926b325dbfba11c7bf5
Comma Device <device@comma.ai>
2023-04-06 22:31:25 -0700

    check errors

--- system/hardware/tici/tests/test_amplifier.py
+++ system/hardware/tici/tests/test_amplifier.py
@@ -2,6 +2,7 @@
 import time
 import random
 import unittest
+import subprocess
 
 from panda import Panda
 from system.hardware.tici.hardware import Tici
@@ -10,28 +11,45 @@ from system.hardware.tici.amplifier import Amplifier
 
 class TestAmplifier(unittest.TestCase):
 
+  def setUp(self):
+    # clear dmesg
+    subprocess.check_call("sudo dmesg -C", shell=True)
+
+    self.panda = Panda()
+    self.panda.reset()
+
   def tearDown(self):
-    with Panda() as p:
-      p.set_siren(False)
+    self.panda.reset(reconnect=False)
+
+  def _check_for_i2c_errors(self, expected):
+    dmesg = subprocess.check_output("dmesg", shell=True, encoding='utf8')
+    i2c_lines = [l for l in dmesg.strip().splitlines() if 'i2c_geni a88000.i2c' in l]
+    i2c_str = '\n'.join(i2c_lines)
+    if not expected:
+      assert len(i2c_lines) == 0
+    else:
+      assert "i2c error :-107" in i2c_str or "Bus arbitration lost" in i2c_str
 
   def test_init_while_siren_play(self):
     amp = Amplifier(debug=True)
     r = amp.initialize_configuration(Tici().model)
     assert r
+    self._check_for_i2c_errors(False)
 
   def test_init_while_siren_play(self):
-    p = Panda()
     for _ in range(5):
-      p.set_siren(False)
+      self.panda.set_siren(False)
       time.sleep(0.1)
 
-      p.set_siren(True)
+      self.panda.set_siren(True)
       time.sleep(random.randint(0, 5))
 
       amp = Amplifier(debug=True)
       r = amp.initialize_configuration(Tici().model)
       assert r
 
+    # make sure we're a good test
+    self._check_for_i2c_errors(True)
 
 
 if __name__ == "__main__":
