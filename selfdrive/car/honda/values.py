25fa0fea379eed8cc31b68f66faa56fcca8a3cdc
alexandresato <sato_alexandre@hotmail.com>
2023-04-08 10:56:23 -0300

    remove FPv2 from brazilian HR-V

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -1423,35 +1423,25 @@ FW_VERSIONS = {
   },
   CAR.HRV_3G: {
     (Ecu.eps, 0x18DA30F1, None): [
-      b'39990-3M0-G110\x00\x00',
       b'39990-3W0-A030\x00\x00',
     ],
     (Ecu.gateway, 0x18DAEFF1, None): [
-      b'38897-3M0-M110\x00\x00',
       b'38897-3W1-A010\x00\x00',
     ],
     (Ecu.srs, 0x18DA53F1, None): [
-      b'77959-3M0-K840\x00\x00',
       b'77959-3V0-A820\x00\x00',
     ],
     (Ecu.combinationMeter, 0x18DA60F1, None): [
       b'78108-3V1-A220\x00\x00',
-      b'78109-3M0-M310\x00\x00',
-    ],
-    (Ecu.fwdRadar, 0x18DAB0F1, None): [
-      b'8S102-3M6-P030\x00\x00',
     ],
     (Ecu.vsa, 0x18DA28F1, None): [
-      b'57114-3M6-M010\x00\x00',
       b'57114-3W0-A040\x00\x00',
     ],
     (Ecu.transmission, 0x18DA1EF1, None): [
       b'28101-6EH-A010\x00\x00',
-      b'28101-6JC-M310\x00\x00',
     ],
     (Ecu.programmedFuelInjection, 0x18DA10F1, None): [
       b'37805-6CT-A710\x00\x00',
-      b'37805-6HZ-M630\x00\x00',
     ],
     (Ecu.electricBrakeBooster, 0x18DA2BF1, None): [
       b'46114-3W0-A020\x00\x00',
