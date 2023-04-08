ee79afa7991e0295bd0277bfca20403997462b99
alexandresato <sato_alexandre@hotmail.com>
2023-04-08 10:45:21 -0300

    Revert "change HONDA_BOSCH_ALT_BRAKE_SIGNAL to CP.flags"
    This reverts commit e0f8123b33db3218f27c4b64a7b2a0b1f740860f.

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -6,8 +6,7 @@ from common.numpy_fast import interp
 from opendbc.can.can_define import CANDefine
 from opendbc.can.parser import CANParser
 from selfdrive.car.honda.hondacan import get_cruise_speed_conversion, get_pt_bus
-# from selfdrive.car.honda.values import CAR, DBC, STEER_THRESHOLD, HONDA_BOSCH, HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_ALT_BRAKE_SIGNAL, HONDA_BOSCH_RADARLESS
-from selfdrive.car.honda.values import CAR, DBC, STEER_THRESHOLD, HONDA_BOSCH, HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_RADARLESS, HondaFlags
+from selfdrive.car.honda.values import CAR, DBC, STEER_THRESHOLD, HONDA_BOSCH, HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_ALT_BRAKE_SIGNAL, HONDA_BOSCH_RADARLESS
 from selfdrive.car.interfaces import CarStateBase
 
 TransmissionType = car.CarParams.TransmissionType
@@ -74,8 +73,7 @@ def get_can_signals(CP, gearbox_msg, main_on_sig_msg):
   else:
     checks.append((gearbox_msg, 100))
 
-  # if CP.carFingerprint in HONDA_BOSCH_ALT_BRAKE_SIGNAL:
-  if CP.flags & HondaFlags.HONDA_BOSCH_ALT_BRAKE_SIGNAL:
+  if CP.carFingerprint in HONDA_BOSCH_ALT_BRAKE_SIGNAL:
     signals.append(("BRAKE_PRESSED", "BRAKE_MODULE"))
     checks.append(("BRAKE_MODULE", 50))
 
@@ -255,8 +253,7 @@ class CarState(CarStateBase):
     else:
       ret.cruiseState.speed = cp.vl["CRUISE"]["CRUISE_SPEED_PCM"] * CV.KPH_TO_MS
 
-    # if self.CP.carFingerprint in HONDA_BOSCH_ALT_BRAKE_SIGNAL:
-    if CP.flags & HondaFlags.HONDA_BOSCH_ALT_BRAKE_SIGNAL:
+    if self.CP.carFingerprint in HONDA_BOSCH_ALT_BRAKE_SIGNAL:
       ret.brakePressed = cp.vl["BRAKE_MODULE"]["BRAKE_PRESSED"] != 0
     else:
       # brake switch has shown some single time step noise, so only considered when
