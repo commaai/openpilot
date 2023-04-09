3236956d714d209e9df964c753d51f2034650d56
Martin Lillepuu <martin@mlp.ee>
2023-04-09 10:25:30 +0300

    Add FPv2: Subaru Forester 2020

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -276,6 +276,7 @@ FW_VERSIONS = {
       b'\xa3  \x14\x01',
       b'\xf1\x00\xbb\r\x05',
       b'\xa3 \x18&\x00',
+      b'\xa3 \x19&\x00',
     ],
     (Ecu.eps, 0x746, None): [
       b'\x8d\xc0\x04\x00',
@@ -288,6 +289,7 @@ FW_VERSIONS = {
       b'\xf1\x00\xac\x02\x00',
       b'\x00\x00e!\x00\x00\x00\x00',
       b'\x00\x00e\x97\x00\x00\x00\x00',
+      b'\x00\x00e^\x1f@ !',
     ],
     (Ecu.engine, 0x7e0, None): [
       b'\xb6"`A\x07',
