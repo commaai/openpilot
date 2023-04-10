13691ad88af3c3dec0d6f2729e3f98e404596b11
Shane Smiskol <shane@smiskol.com>
2023-04-10 14:02:17 -0700

    fixes

--- selfdrive/debug/test_car_model.py
+++ selfdrive/debug/test_car_model.py
@@ -22,7 +22,7 @@ def create_test_models_suite(routes: List[Tuple[str, CarTestRoute]], ci=False) -
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Test any route against common issues with a new car port. " +
                                                "Uses selfdrive/car/tests/test_models.py")
-  parser.add_argument("route_or_segment_name", help="Specify route to run tests on")
+  parser.add_argument("route", help="Specify route to run tests on")
   parser.add_argument("--car", help="Specify car model for test route")
   parser.add_argument("--ci", action="store_true", help="Attempt to get logs using openpilotci, need to specify car")
   args = parser.parse_args()
@@ -30,9 +30,9 @@ if __name__ == "__main__":
     parser.print_help()
     sys.exit()
 
-  route_or_segment_name = SegmentName(args.route_or_segment_name.strip(), allow_route_name=True)
+  route_or_segment_name = SegmentName(args.route.strip(), allow_route_name=True)
   segment_num = route_or_segment_name.segment_num if route_or_segment_name.segment_num != -1 else None
-  test_route = CarTestRoute(route_or_segment_name.route_name, args.car, segment=segment_num)
+  test_route = CarTestRoute(route_or_segment_name.route_name.canonical_name, args.car, segment=segment_num)
   test_suite = create_test_models_suite([(args.car, test_route)], ci=args.ci)
 
   unittest.TextTestRunner().run(test_suite)
