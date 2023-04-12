e509d7befec33fc8ef4788d95abc5122d5865d4a
Shane Smiskol <shane@smiskol.com>
2023-04-11 21:28:22 -0700

    mark done if timed out

--- selfdrive/car/isotp_parallel_query.py
+++ selfdrive/car/isotp_parallel_query.py
@@ -137,15 +137,16 @@ class IsoTpParallelQuery:
             request_done[tx_addr] = True
             cloudlog.error(f"iso-tp query bad response: {tx_addr} - 0x{dat.hex()}")
 
-      # Break if all requests are done, we've timed out on all requests, or we've hit a max total timeout
-      if all(request_done.values()):
-        break
-
+      # Mark request done if address timed out
       cur_time = time.monotonic()
-      if cur_time - max(response_timeouts.values()) > 0:
-        for tx_addr in msgs:
+      for tx_addr in response_timeouts:
+        if cur_time - response_timeouts[tx_addr] > 0:
+          request_done[tx_addr] = True
           if request_counter[tx_addr] > 0 and not request_done[tx_addr]:
             cloudlog.error(f"iso-tp query timeout after receiving response: {tx_addr}")
+
+      # Break if all requests are done, or we've hit a max total timeout
+      if all(request_done.values()):
         break
 
       if cur_time - start_time > total_timeout:
