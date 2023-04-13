3e13b0be9de921336765fbb69b049f8f9bef7eac
Dean Lee <deanlee3@gmail.com>
2023-04-14 07:26:58 +0800

    cabana: support display hex bytes in multiple lines. (#27901)
    * display hex bytes in multiple lines
    
    * default is true
    
    * cleanup
    old-commit-hash: 860e843af6a3afd153790b7d8e3060b9b586299f

--- tools/cabana/settings.h
+++ tools/cabana/settings.h
@@ -24,6 +24,7 @@ public:
   int chart_series_type = 0;
   int theme = 0;
   int sparkline_range = 15; // 15 seconds
+  bool multiple_lines_bytes = true;
   QString last_dir;
   QString last_route_dir;
   QByteArray geometry;
