66a1f8fe58bf09649904d745a19906a56b4cadae
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-04-13 22:22:59 -0700

    use that

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -33,7 +33,7 @@ class CarInterface(CarInterfaceBase):
 
     if candidate in CANFD_CAR:
       # detect HDA2 with ADAS Driving ECU
-      if Ecu.adas in [fw.ecu for fw in car_fw]:
+      if hda2:
         ret.flags |= HyundaiFlags.CANFD_HDA2.value
       else:
         # non-HDA2
