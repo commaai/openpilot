ca826cc2a2e79ad662ac75cbbd4bcd23aac7527f
Shane Smiskol <shane@smiskol.com>
2023-04-13 21:31:02 -0700

    move to test_fw_fingerprint

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -7,12 +7,13 @@ from parameterized import parameterized
 from cereal import car
 from selfdrive.car.car_helpers import get_interface_attr, interfaces
 from selfdrive.car.fingerprints import FW_VERSIONS
-from selfdrive.car.fw_versions import FW_QUERY_CONFIGS, VERSIONS, match_fw_to_car
+from selfdrive.car.fw_versions import FW_QUERY_CONFIGS, match_fw_to_car
 
 CarFw = car.CarParams.CarFw
 Ecu = car.CarParams.Ecu
 
 ECU_NAME = {v: k for k, v in Ecu.schema.enumerants.items()}
+VERSIONS = get_interface_attr("FW_VERSIONS", ignore_none=True)
 
 
 class TestFwFingerprint(unittest.TestCase):
@@ -101,6 +102,41 @@ class TestFwFingerprint(unittest.TestCase):
         ecu_strings = ", ".join([f'Ecu.{ECU_NAME[ecu]}' for ecu in ecus_not_whitelisted])
         self.assertFalse(len(whitelisted_ecus) and len(ecus_not_whitelisted),
                          f'{brand.title()}: FW query whitelist missing ecus: {ecu_strings}')
+
+
+class TestFwFingerprintTiming(unittest.TestCase):
+  @parameterized.expand([(1,), (2,)])
+  def test_fw_query_timing(self, num_pandas):
+    for brand, config in FW_QUERY_CONFIGS.items():
+      with self.subTest(brand=brand, num_pandas=num_pandas):
+        requests = [r for r in config.requests if r.bus <= num_pandas * 4 - 1]
+        multi_panda_requests = [r for r in config.requests if r.bus > 3]
+
+        if not len(multi_panda_requests) and num_pandas > 1:
+          raise unittest.SkipTest("No multi-panda FW queries")
+
+        total_time = 0
+        obd_multiplexing = config.requests[0].obd_multiplexing  # only count the transitions
+        for r in requests:
+          # TODO: put FW versions in the config, duplicate logic
+          # subaddresses must be queried one by one
+          request_sub_addrs = set()
+          for brand_versions in VERSIONS[brand].values():
+            for ecu_type, addr, sub_addr in list(brand_versions) + config.extra_ecus:
+              # Only query ecus in whitelist if whitelist is not empty
+              if len(r.whitelist_ecus) == 0 or ecu_type in r.whitelist_ecus:
+                request_sub_addrs.add(sub_addr)
+
+          for _ in range(len(request_sub_addrs)):
+            total_time += 0.1
+
+            if r.obd_multiplexing != obd_multiplexing and r.bus % 4 == 1:
+              obd_multiplexing = r.obd_multiplexing
+              total_time += 0.1
+
+        total_time = round(total_time, 2)
+        self.assertLessEqual(total_time, 1.1)
+        print(f'{brand=}, {num_pandas=}, {len(requests)=}, total FW query time={total_time} seconds')
 
 
 if __name__ == "__main__":
