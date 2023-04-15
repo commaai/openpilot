88c77793ed893e6c73df87f1d4db5df442a1cacd
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2023-04-14 23:33:05 -0500

    Mazda: Fix BSM detection
    Submitted-by: Samuel <samuel95_54@hotmail.com>
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/car/mazda/carstate.py
+++ selfdrive/car/mazda/carstate.py
@@ -38,8 +38,8 @@ class CarState(CarStateBase):
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(can_gear, None))
 
     ret.genericToggle = bool(cp.vl["BLINK_INFO"]["HIGH_BEAMS"])
-    ret.leftBlindspot = cp.vl["BSM"]["LEFT_BS1"] == 1
-    ret.rightBlindspot = cp.vl["BSM"]["RIGHT_BS1"] == 1
+    ret.leftBlindspot = (cp.vl["BSM"]["LEFT_BS1"] == 1) or (cp.vl["BSM"]["LEFT_BS2"] == 1)
+    ret.rightBlindspot = (cp.vl["BSM"]["RIGHT_BS1"] == 1) or (cp.vl["BSM"]["RIGHT_BS2"] == 1)
     ret.leftBlinker, ret.rightBlinker = self.update_blinker_from_lamp(40, cp.vl["BLINK_INFO"]["LEFT_BLINK"] == 1,
                                                                       cp.vl["BLINK_INFO"]["RIGHT_BLINK"] == 1)
 
@@ -153,6 +153,8 @@ class CarState(CarStateBase):
         ("CTR", "CRZ_BTNS"),
         ("LEFT_BS1", "BSM"),
         ("RIGHT_BS1", "BSM"),
+        ("LEFT_BS2", "BSM"),
+        ("RIGHT_BS2", "BSM"),
       ]
 
       checks += [
