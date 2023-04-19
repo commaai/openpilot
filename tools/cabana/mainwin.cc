1045c7d836e298f5221d9a52c24514494668408f
Dean Lee <deanlee3@gmail.com>
2023-04-20 01:49:06 +0800

    cabana: startup stream chooser dialog (#27938)
    * new StreamDialog
    
    * choose dbc file
    
    * update last_dir
    
    * move to /streams
    
    * cleanup
    
    * add stretch
    
    * catch panda exception
    
    * cleanup
    
    * cleanup
    
    * small cleanup
    
    * fix pandaStream crash caused by a failed connection
    
    * static function to create stream widget
    
    * cleanup
    old-commit-hash: 590b1bc2062d8a7f1ec253e0d0c5ab94799524c1

--- tools/cabana/mainwin.cc
+++ tools/cabana/mainwin.cc
@@ -17,7 +17,8 @@
 #include <QWidgetAction>
 
 #include "tools/cabana/commands.h"
-#include "tools/cabana/route.h"
+#include "tools/cabana/streamselector.h"
+#include "tools/cabana/streams/replaystream.h"
 
 static MainWindow *main_win = nullptr;
 void qLogMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
@@ -248,12 +249,13 @@ void MainWindow::DBCFileChanged() {
 }
 
 void MainWindow::openRoute() {
-  OpenRouteDialog dlg(this);
+  StreamSelector dlg(this);
+  dlg.addStreamWidget(ReplayStream::widget(&can));
   if (dlg.exec()) {
     center_widget->clear();
     charts_widget->removeAll();
     statusBar()->showMessage(tr("Route %1 loaded").arg(can->routeName()), 2000);
-  } else if (dlg.failedToLoad()) {
+  } else if (dlg.failed()) {
     close();
   }
 }
--- tools/cabana/streams/abstractstream.h
+++ tools/cabana/streams/abstractstream.h
@@ -89,5 +89,16 @@ protected:
   std::deque<std::unique_ptr<char[]>> memory_blocks;
 };
 
+class AbstractOpenStreamWidget : public QWidget {
+  Q_OBJECT
+public:
+  AbstractOpenStreamWidget(AbstractStream **stream, QWidget *parent = nullptr) : stream(stream), QWidget(parent) {}
+  virtual bool open() = 0;
+  virtual QString title() = 0;
+
+protected:
+  AbstractStream **stream = nullptr;
+};
+
 // A global pointer referring to the unique AbstractStream object
 extern AbstractStream *can;
