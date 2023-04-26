5c74c4d75d7488568d27a6faf01111655157894f
Willem Melching <willem.melching@gmail.com>
2023-04-26 18:54:26 +0200

    cabana: hide/show columns using context menu (#28033)
    old-commit-hash: 8318d7207b96232894580cd5fd8da3f0ff7c0d70

--- tools/cabana/messageswidget.h
+++ tools/cabana/messageswidget.h
@@ -2,8 +2,10 @@
 
 #include <QAbstractTableModel>
 #include <QCheckBox>
+#include <QContextMenuEvent>
 #include <QHeaderView>
 #include <QLineEdit>
+#include <QMenu>
 #include <QSet>
 #include <QTreeView>
 
@@ -43,6 +45,7 @@ public:
   void drawBranches(QPainter *painter, const QRect &rect, const QModelIndex &index) const override {}
   void dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QVector<int> &roles = QVector<int>()) override;
   void updateBytesSectionSize();
+  void headerContextMenuEvent(const QPoint &pos);
 };
 
 class MessagesWidget : public QWidget {
