a0d2e4e711b5ca4f8e5267d6bf7c8f2e32765692
Cameron Clough <cameronjclough@gmail.com>
2023-05-04 17:21:36 -0700

    Ford: add Escape test route and remove dashcam only (#28113)
    * Ford: add Escape Mk4 test route
    
    * generate docs
    
    * remove from non_tested
    
    * fix model years
    
    * clearer packages for EU
    
    * docs
    
    * 2020 hybrid
    
    26b2cace68e36212
    
    1FMCU9DZ9LUA82589
    
    * 2021 phev
    
    1FMCU0LZXMUA80767
    
    f8eaaccd2a90aef8
    
    * add this too
    
    * more docs

--- selfdrive/car/tests/routes.py
+++ selfdrive/car/tests/routes.py
@@ -16,7 +16,6 @@ from selfdrive.car.body.values import CAR as COMMA
 
 # TODO: add routes for these cars
 non_tested_cars = [
-  FORD.ESCAPE_MK4,
   FORD.FOCUS_MK4,
   FORD.MAVERICK_MK1,
   GM.CADILLAC_ATS,
@@ -47,6 +46,7 @@ routes = [
   CarTestRoute("8fb5eabf914632ae|2022-08-04--17-28-53", CHRYSLER.RAM_HD, segment=6),
 
   CarTestRoute("54827bf84c38b14f|2023-01-25--14-14-11", FORD.BRONCO_SPORT_MK1),
+  CarTestRoute("f8eaaccd2a90aef8|2023-05-04--15-10-09", FORD.ESCAPE_MK4),
   CarTestRoute("62241b0c7fea4589|2022-09-01--15-32-49", FORD.EXPLORER_MK6),
   #TestRoute("f1b4c567731f4a1b|2018-04-30--10-15-35", FORD.FUSION),
 
