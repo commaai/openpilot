1e683a874351a6ca3bb2f26c57b91633dc602331
Shane Smiskol <shane@smiskol.com>
2023-05-09 17:29:08 -0700

    don't clear in boardd anymore
    fix

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -507,16 +507,13 @@ void panda_state_thread(PubMaster *pm, std::vector<Panda *> pandas, bool spoofin
       }
     }
 
-    // clear ignition-based params and set new safety on car start
+    // set new safety on car start
     if (ignition && !ignition_last) {
-      params.clearAll(CLEAR_ON_ONROAD_TRANSITION);
       if (!safety_future.valid() || safety_future.wait_for(0ms) == std::future_status::ready) {
         safety_future = std::async(std::launch::async, safety_setter_thread, pandas);
       } else {
         LOGW("Safety setter thread already running");
       }
-    } else if (!ignition && ignition_last) {
-      params.clearAll(CLEAR_ON_OFFROAD_TRANSITION);
     }
 
     ignition_last = ignition;
