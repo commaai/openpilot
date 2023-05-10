19d1de5de6bbe66d48b88bdd21a2d0fadfde6a76
Cameron Clough <cameronjclough@gmail.com>
2023-05-09 21:19:21 -0700

    match DBC

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -156,10 +156,10 @@ def create_acc_ui_msg(packer, CP, main_on: bool, enabled: bool, standstill: bool
   if CP.openpilotLongitudinalControl:
     values.update({
       "AccStopStat_D_Dsply": 2 if standstill else 0,              # Stopping status text
-      "AccTGap_B_Dsply": 0,                                       # Show distance control UI
+      "AccTGap_B_Dsply": 0,                                       # Show time gap control UI
       "AccFllwMde_B_Dsply": 1 if hud_control.leadVisible else 0,  # Lead indicator
       "AccStopMde_B_Dsply": 1 if standstill else 0,
-      "AccTGap_D_Dsply": 4,                                       # Fixed distance in UI
+      "AccTGap_D_Dsply": 4,                                       # Fixed time gap in UI
     })
 
   return packer.make_can_msg("ACCDATA_3", CANBUS.main, values)
