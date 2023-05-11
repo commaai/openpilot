b9fb45366e911c319c3f587bdb2ed0ca4d18f7da
Bruce Wayne <harald.the.engineer@gmail.com>
2023-05-11 11:20:10 -0700

    recalib

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -720,7 +720,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
 
   EventName.calibrationIncomplete: {
     ET.PERMANENT: calibration_incomplete_alert,
-    ET.SOFT_DISABLE: soft_disable_alert("Device remount detected: recalibrating"),
+    ET.SOFT_DISABLE: soft_disable_alert("Device Remount Detected: Recalibrating"),
     ET.NO_ENTRY: NoEntryAlert("Calibration in Progress"),
   },
   
