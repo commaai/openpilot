c6b3abbf4fbc679e9d003d20f58d4244cd79ebc2
Bruce Wayne <harald.the.engineer@gmail.com>
2023-05-12 14:49:54 -0700

    add speaker test

--- tools/joystick/web.py
+++ tools/joystick/web.py
@@ -167,11 +167,41 @@ def gen_audio():
     time.sleep(0.0001)
 
 
+def test_speaker():
+  volume = 0.5  # range [0.0, 1.0]
+  fs = 44100  # sampling rate, Hz, must be integer
+  duration = 5.0  # in seconds, may be float
+  f = 440.0  # sine frequency, Hz, may be float
+
+  # generate samples, note conversion to float32 array
+  samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+
+  # per @yahweh comment explicitly convert to bytes sequence
+  output_bytes = (volume * samples).tobytes()
+
+  # for paFloat32 sample values must be in range [-1.0, 1.0]
+  stream = p.open(format=pyaudio.paFloat32,
+                          channels=1,
+                                          rate=fs,
+                                                          output=True)
+
+  # play. May repeat with different volume values (if done interactively)
+  start_time = time.time()
+  stream.write(output_bytes)
+  print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+
+  stream.stop_stream()
+  stream.close()
+
+
+
+
 def main():
   #threading.Thread(target=handle_timeout, daemon=True).start()
   socketio.start_background_task(gen)
+  socketio.start_background_task(test_speaker)
   #socketio.start_background_task(target=gen_audio)
-  socketio.run(app, host="0.0.0.0")
+  socketio.run(app, host="0.0.0.0", ssl_context='adhoc')
 
 
 if __name__ == '__main__':
