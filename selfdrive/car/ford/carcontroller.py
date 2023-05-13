717bc04ddc330c43e794f28ee6ff3a287425112e
Cameron Clough <cameronjclough@gmail.com>
2023-05-13 16:20:34 -0700

    Ford: improved ACC UI for OP long (#28114)
    * Ford: improved ACC UI for OP long
    
    - Show "stopped" status message on IPC when in standstill, similar to stock
    - Prevent the distance control UI from appearing on button press (as
    this does not influence OP long).
    
    * set lead indicator and only override stock when OP is controlling long
    
    * match DBC

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -99,7 +99,9 @@ class CarController:
       can_sends.append(create_lkas_ui_msg(self.packer, main_on, CC.latActive, steer_alert, hud_control, CS.lkas_status_stock_values))
     # send acc ui msg at 5Hz or if ui state changes
     if (self.frame % CarControllerParams.ACC_UI_STEP) == 0 or send_ui:
-      can_sends.append(create_acc_ui_msg(self.packer, main_on, CC.latActive, hud_control, CS.acc_tja_status_stock_values))
+      can_sends.append(create_acc_ui_msg(self.packer, self.CP, main_on, CC.latActive,
+                                         CS.out.cruiseState.standstill, hud_control,
+                                         CS.acc_tja_status_stock_values))
 
     self.main_on_last = main_on
     self.lkas_enabled_last = CC.latActive
