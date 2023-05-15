fb07b4c418d4e783dc5b9fde6d8d0818ad0243c9
Shane Smiskol <shane@smiskol.com>
2023-05-15 13:33:45 -0700

    all offroad params also have manager start

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -35,6 +35,7 @@ def manager_init() -> None:
   params = Params()
   params.clear_all(ParamKeyType.CLEAR_ON_MANAGER_START)
   params.clear_all(ParamKeyType.CLEAR_ON_ONROAD_TRANSITION)
+  params.clear_all(ParamKeyType.CLEAR_ON_OFFROAD_TRANSITION)
 
   default_params: List[Tuple[str, Union[str, bytes]]] = [
     ("CompletedTrainingVersion", "0"),
