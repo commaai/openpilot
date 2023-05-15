c192c92a4e838b9d5b8b80bc86fb35a0f236f2b2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-05-15 13:10:36 -0700

    calibrationd: add offroad alert when remount detected (#28073)
    * calibrationd: add offroad alert for recalibration
    
    * Update common/params.cc

--- common/params.cc
+++ common/params.cc
@@ -202,6 +202,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Offroad_TemperatureTooHigh", CLEAR_ON_MANAGER_START},
     {"Offroad_UnofficialHardware", CLEAR_ON_MANAGER_START},
     {"Offroad_UpdateFailed", CLEAR_ON_MANAGER_START},
+    {"Offroad_Recalibration", CLEAR_ON_MANAGER_START | CLEAR_ON_ONROAD_TRANSITION},
 };
 
 } // namespace
