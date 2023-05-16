3a9e6d18a8d4b9c54d59a5dd381496d4d485cd3b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-05-16 12:45:37 -0700

    thermald: adjust thermal bands

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -40,8 +40,8 @@ HardwareState = namedtuple("HardwareState", ['network_type', 'network_info', 'ne
 # When exiting the bounds, we'll jump to the lower or higher band. Bands are ordered in the dict.
 THERMAL_BANDS = OrderedDict({
   ThermalStatus.green: ThermalBand(None, 80.0),
-  ThermalStatus.yellow: ThermalBand(75.0, 96.0),
-  ThermalStatus.red: ThermalBand(80.0, 107.),
+  ThermalStatus.yellow: ThermalBand(75.0, 97.0),
+  ThermalStatus.red: ThermalBand(85.0, 107.),
   ThermalStatus.danger: ThermalBand(94.0, None),
 })
 
