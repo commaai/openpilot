4a1273b63ba2e9163b50e0427bdaa7765177e146
nuwandavek <vivekaithal44@gmail.com>
2023-05-18 16:20:31 -0700

    style fixes

--- tools/joystick/web2.py
+++ tools/joystick/web2.py
@@ -6,24 +6,21 @@ import ssl
 import uuid
 import pyaudio
 import io
-import cv2
 import wave
 
 import time
 from aiohttp import web
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from aiortc.mediastreams import VideoStreamTrack, AudioStreamTrack, MediaStreamError
-from aiortc.contrib.media import MediaBlackhole, MediaRecorder
-from aiortc.codecs.opus import OpusEncoder
+from aiortc.contrib.media import MediaBlackhole
 from aiortc.rtcrtpsender import RTCRtpSender
 
 import numpy as np
 from av import VideoFrame, CodecContext, Packet
 from pydub import AudioSegment
-import sounddevice as sd
 import cereal.messaging as messaging
 from cereal.visionipc import VisionIpcClient, VisionStreamType
-from system.camerad.snapshot.snapshot import extract_image, get_yuv
+from system.camerad.snapshot.snapshot import get_yuv
 
 
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
@@ -35,15 +32,16 @@ logging.basicConfig(level=logging.INFO)
 pcs = set()
 
 IMG_H, IMG_W = 604, 964
-yuv = np.zeros((int(IMG_H*1.5), IMG_W), dtype=np.uint8)
+yuv = np.zeros((int(IMG_H * 1.5), IMG_W), dtype=np.uint8)
 AUDIO_RATE = 16000
 pm = messaging.PubMaster(['testJoystick'])
 sm = messaging.SubMaster(['carState'])
-SOUNDS  = {
+SOUNDS = {
   'engage': '../../selfdrive/assets/sounds/engage.wav',
   'disengage': '../../selfdrive/assets/sounds/disengage.wav',
   'error': '../../selfdrive/assets/sounds/warning_immediate.wav',
 }
+
 
 def force_codec(pc, sender, forced_codec='video/H264', stream_type="video"):
   codecs = RTCRtpSender.getCapabilities(stream_type).codecs
@@ -61,6 +59,7 @@ def yuv_from_buffer(yuv_img_raw, width, height, stride, uv_offset):
   yuv[:, :, 1:] -= 128
   return yuv
 
+
 class VideoTrack(VideoStreamTrack):
   def __init__(self):
     super().__init__()
@@ -70,7 +69,7 @@ class VideoTrack(VideoStreamTrack):
   async def recv(self):
     if os.environ.get('FAKE_CAMERA') == '1':
       frame = np.zeros((IMG_H, IMG_W, 3), np.uint8)
-    
+
     else:
       if not self.vipc_client.is_connected():
         self.vipc_client.connect(True)
@@ -79,11 +78,11 @@ class VideoTrack(VideoStreamTrack):
         frame = VideoFrame.from_ndarray(np.zeros((int(self.vipc_client.height * 1.5), self.vipc_client.width), np.uint8), format="yuv420p")
       else:
         y, u, v = get_yuv(yuv_img_raw, self.vipc_client.width, self.vipc_client.height, self.vipc_client.stride, self.vipc_client.uv_offset)
-        u = u.reshape(u.shape[0]//2, -1)
-        v = v.reshape(v.shape[0]//2, -1)
-        yuv[:IMG_H, :] = y[::2,::2]
-        yuv[IMG_H:int(IMG_H * 5 / 4), :] = u[::2,::2]
-        yuv[int(5 * IMG_H / 4):, :] = v[::2,::2]
+        u = u.reshape(u.shape[0] // 2, -1)
+        v = v.reshape(v.shape[0] // 2, -1)
+        yuv[:IMG_H, :] = y[::2, ::2]
+        yuv[IMG_H:int(IMG_H * 5 / 4), :] = u[::2, ::2]
+        yuv[int(5 * IMG_H / 4):, :] = v[::2, ::2]
         frame = VideoFrame.from_ndarray(yuv, format="yuv420p")
 
     pts, time_base = await self.next_timestamp()
@@ -104,7 +103,7 @@ class LiveSpeaker(MediaBlackhole):
     # print(self.buffer.getbuffer().nbytes)
     if self.buffer.getbuffer().nbytes < frame_count * self.channels * 2:
       buff = np.zeros((frame_count, 2), dtype=np.int16).tobytes()
-    elif self.buffer.getbuffer().nbytes > 115200: # 3x the usual read size
+    elif self.buffer.getbuffer().nbytes > 115200:  # 3x the usual read size
       self.buffer.seek(0)
       buff = self.buffer.read(frame_count * self.channels * 4)
       buff = buff[:frame_count * self.channels * 2]
@@ -137,7 +136,6 @@ class LiveSpeaker(MediaBlackhole):
     self.stream.stop_stream()
     self.stream.close()
     self.p.terminate()
-
 
 
 class MicTrack(AudioStreamTrack):
@@ -190,7 +188,6 @@ async def play_sound(sound):
     stream.start_stream()
     while stream.is_active():
       await asyncio.sleep(0)
-      pass
     stream.stop_stream()
     stream.close()
     p.terminate()
@@ -208,10 +205,11 @@ async def dummy_controls_msg(app):
       this_time = time.monotonic()
       if (app['mutable_vals']['last_send_time'] + 0.5) < this_time:
         dat = messaging.new_message('testJoystick')
-        dat.testJoystick.axes = [0,0]
+        dat.testJoystick.axes = [0, 0]
         dat.testJoystick.buttons = [False]
         pm.send('testJoystick', dat)
     await asyncio.sleep(0.1)
+
 
 async def start_background_tasks(app):
   app['bgtask_dummy_controls_msg'] = asyncio.create_task(dummy_controls_msg(app))
@@ -227,7 +225,7 @@ async def control_body(data):
   x = max(-1.0, min(1.0, data['x']))
   y = max(-1.0, min(1.0, data['y']))
   dat = messaging.new_message('testJoystick')
-  dat.testJoystick.axes = [x,y]
+  dat.testJoystick.axes = [x, y]
   dat.testJoystick.buttons = [False]
   pm.send('testJoystick', dat)
 
@@ -250,6 +248,7 @@ async def offer(request):
   @pc.on("datachannel")
   def on_datachannel(channel):
     request.app['mutable_vals']['remote_channel'] = channel
+
     @channel.on("message")
     async def on_message(message):
       data = json.loads(message)
@@ -288,7 +287,7 @@ async def offer(request):
 
   video_sender = pc.addTrack(VideoTrack())
   force_codec(pc, video_sender)
-  audio_sender = pc.addTrack(MicTrack())
+  _ = pc.addTrack(MicTrack())
 
   await pc.setRemoteDescription(offer)
   await speaker.start()
