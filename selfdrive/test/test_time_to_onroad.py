f962efd151f6c95d6387370d80817da66530b34b
Comma Device <device@comma.ai>
2023-05-20 16:56:00 -0700

    run few times

--- selfdrive/test/test_time_to_onroad.py
+++ selfdrive/test/test_time_to_onroad.py
@@ -13,20 +13,18 @@ from selfdrive.updated import OVERLAY_INIT
 from selfdrive.test.helpers import set_params_enabled
 
 
-@pytest.mark.repeat(1)
+@pytest.mark.repeat(5)
 def test_time_to_onroad():
   # setup
   set_params_enabled()
   OVERLAY_INIT.unlink(missing_ok=True)
   Path(os.path.join(BASEDIR, "prebuilt")).touch()
 
-  sm = messaging.SubMaster(['controlsState'])
-
-  # run launch script
+  # launch
   proc = subprocess.Popen(f"{BASEDIR}/launch_openpilot.sh", cwd=BASEDIR, start_new_session=True)
   start_time = time.monotonic()
+  sm = messaging.SubMaster(['controlsState'])
   try:
-    # TODO: wait for ui
     # wait for engageability
     with Timeout(30, "timed out waiting for engageable"):
       while True:
@@ -39,7 +37,7 @@ def test_time_to_onroad():
     # once we're enageable, must be for the next 5s
     for _ in range(500):
       sm.update(100)
-      assert sm['controlsState'].engageable and sm.alive['controlsState']
+      assert sm['controlsState'].engageable and sm.updated['controlsState']
   finally:
     pgrp = os.getpgid(proc.pid)
     os.killpg(pgrp, signal.SIGTERM)
