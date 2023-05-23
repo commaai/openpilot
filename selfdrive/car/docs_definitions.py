8ef5766a42670f9d469fb37e0c8fbf8628fb8f51
Maxime Desroches <desroches.maxime@gmail.com>
2023-05-22 21:24:45 -0700

    specify auto_resume

--- selfdrive/car/docs_definitions.py
+++ selfdrive/car/docs_definitions.py
@@ -214,6 +214,8 @@ class CarInfo:
   min_steer_speed: Optional[float] = None
   min_enable_speed: Optional[float] = None
 
+  auto_resume: Optional[bool] = None
+
   # all the parts needed for the supported car
   car_parts: CarParts = CarParts()
 
@@ -244,6 +246,9 @@ class CarInfo:
     if self.min_enable_speed is None:
       self.min_enable_speed = CP.minEnableSpeed
 
+    if self.auto_resume is None:
+      self.auto_resume = CP.autoResumeSng
+
     # hardware column
     hardware_col = "None"
     if self.car_parts.parts:
@@ -260,7 +265,7 @@ class CarInfo:
       Column.FSR_LONGITUDINAL: f"{max(self.min_enable_speed * CV.MS_TO_MPH, 0):.0f} mph",
       Column.FSR_STEERING: f"{max(self.min_steer_speed * CV.MS_TO_MPH, 0):.0f} mph",
       Column.STEERING_TORQUE: Star.EMPTY,
-      Column.AUTO_RESUME: Star.FULL if CP.autoResumeSng else Star.EMPTY,
+      Column.AUTO_RESUME: Star.FULL if self.auto_resume else Star.EMPTY,
       Column.HARDWARE: hardware_col,
       Column.VIDEO: self.video_link if self.video_link is not None else "",  # replaced with an image and link from template in get_column
     }
