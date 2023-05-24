61e70ded92cb764704536896b94c11c79b6daf26
wileytom65 <wiley_tom@yahoo.com>
2023-05-24 08:40:42 -0800

    Added 2023 Hyundai Sante_FE_HEV
    Additional info added for 2023my of Hyundai SANTA_FE_HEV (nonHDA2)

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -851,15 +851,31 @@ FW_VERSIONS = {
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00TMhe SCC FHCUP      1.00 1.00 99110-CL500         ',
     ],
+    (Ecu.fwdRadar, 0x7e1, None): [
+      b'\xf1\x00TMhe SCC FHCUP      1.00 1.00 99110-CL500         ', 
+      b'"\xf1\x00',
+    ],
+    (Ecu.fwdRadar, 0x7e1, None): [
+      b'\xf1\x8799110CL500\xf1\x00TMhe SCC FHCUP      1.00 1.00 99110-CL500         ', 
+      b'"\xf1\x87\xf1\x81\xf1\x00',
+    ],
     (Ecu.eps, 0x7d4, None): [
       b'\xf1\x00TM  MDPS C 1.00 1.02 56310-CLAC0 4TSHC102',
       b'\xf1\x00TM  MDPS R 1.00 1.05 57700-CL000 4TSHP105',
       b'\xf1\x00TM  MDPS C 1.00 1.02 56310-GA000 4TSHA100',
     ],
+    (Ecu.eps, 0x7e1, None): [
+      b'\xf1\x00TM  MDPS C 1.00 1.02 56310-GA000 4TSHA100',
+      b'"\xf1\x00',
+    ],
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00TMH MFC  AT EUR LHD 1.00 1.06 99211-S1500 220727',
       b'\xf1\x00TMH MFC  AT USA LHD 1.00 1.03 99211-S1500 210224',
       b'\xf1\x00TMA MFC  AT USA LHD 1.00 1.03 99211-S2500 220414',
+    ],
+    (Ecu.fwdCamera, 0x7e0, None): [
+      b'\xf1\x00TMA MFC  AT USA LHD 1.00 1.03 99211-S2500 220414',
+      b'"\xf1\x00',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xf1\x00PSBG2333  E16\x00\x00\x00\x00\x00\x00\x00TTM2H16UA3I\x94\xac\x8f',
@@ -867,11 +883,24 @@ FW_VERSIONS = {
       b'\xf1\x00PSBG2333  E16\x00\x00\x00\x00\x00\x00\x00TTM2H16SA3\xa3\x1b\xe14',
       b'\xf1\x8795441-3D120\x00\xf1\x81E16\x00\x00\x00\x00\x00\x00\x00\xf1\x00PSBG2333  E16\x00\x00\x00'                                        b'\x00\x00\x00\x00TTM2H16SA3\xa3\x1b\xe14',
     ],
+    (Ecu.transmission, 0x7c4, None): [
+      b'\xf1\x00PSBG2333  E16\x00\x00\x00\x00\x00\x00\x00TTM2H16SA3\xa3\x1b\xe14',
+      b'"\xf1\x00',
+    ],
+    (Ecu.transmission, 0x7b0, None): [
+      b'\xf1\x8795441-3D120\x00\xf1\x81E16\x00\x00\x00\x00\x00\x00\x00\xf1\x00PSBG2333  E16\x00\x00\x00',
+      b'"\xf1\x87\xf1\x81\xf1\x00',
+    ],
     (Ecu.engine, 0x7e0, None): [
       b'\xf1\x87391312MTC1',
       b'\xf1\x87391312MTE0',
       b'\xf1\x87391312MTL0', 
     ],
+    (Ecu.engine, 0x7d0, None): [
+      b'\xf1\x87391312MTL0',
+      b'"\xf1\x87\xf1\x81\xf1\x00',
+    ],
+
   },
   CAR.SANTA_FE_PHEV_2022: {
     (Ecu.fwdRadar, 0x7d0, None): [
