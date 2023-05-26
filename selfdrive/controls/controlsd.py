6c9132c7b756a4c2a4e3ee6684c1c238793e88c7
sshane <shane@smiskol.com>
2023-05-26 02:27:50 -0700

    only write once

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -286,7 +286,8 @@ class Controls:
       if cal_status == log.LiveCalibrationData.Status.uncalibrated:
         self.events.add(EventName.calibrationIncomplete)
       elif cal_status == log.LiveCalibrationData.Status.recalibrating:
-        set_offroad_alert("Offroad_Recalibration", True)
+        if self.params.get("Offroad_Recalibration") is None:
+          set_offroad_alert("Offroad_Recalibration", True)
         self.events.add(EventName.calibrationRecalibrating)
       else:
         self.events.add(EventName.calibrationInvalid)
