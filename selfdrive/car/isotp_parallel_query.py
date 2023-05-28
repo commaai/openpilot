c86872a9ae64abc8efc47171a4516aa4a373941c
Shane Smiskol <shane@smiskol.com>
2023-05-27 19:06:05 -0700

    clean up

--- selfdrive/car/isotp_parallel_query.py
+++ selfdrive/car/isotp_parallel_query.py
@@ -120,7 +120,6 @@ class IsoTpParallelQuery:
 
         counter = request_counter[tx_addr]
         expected_response = self.response[counter]
-        # response_valid = dat[:len(expected_response)] == expected_response
         response_valid = dat.startswith(expected_response)
 
         if response_valid:
