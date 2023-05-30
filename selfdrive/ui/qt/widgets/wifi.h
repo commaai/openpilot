801f4854090d100b268da6234b5970255b6438bb
Cameron Clough <cameronjclough@gmail.com>
2023-05-30 16:44:46 -0700

    WiFiPrompt: update using network type

--- selfdrive/ui/qt/widgets/wifi.h
+++ selfdrive/ui/qt/widgets/wifi.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <QFrame>
+#include <QStackedLayout>
 #include <QWidget>
+
+#include "selfdrive/ui/ui.h"
 
 class WiFiPromptWidget : public QFrame {
   Q_OBJECT
 
 public:
   explicit WiFiPromptWidget(QWidget* parent = 0);
+
+public slots:
+  void updateState(const UIState &s);
+
+protected:
+  QStackedLayout *stack;
 };
