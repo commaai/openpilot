2a1c9f77e4f6d7ca3dac5c8aaf4baa45d0b54539
Cameron Clough <cameronjclough@gmail.com>
2023-05-31 16:26:26 -0700

    commit snapshots

---
+++ selfdrive/ui/tests/rebase_snapshot.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+UI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
+SNAPSHOT_TOOL=tests/ui_snapshot
+SNAPSHOT_DIR=$UI_DIR/tests/snapshots
+
+cd $UI_DIR
+
+TEST_CASE=$1
+
+if [ -z "$TEST_CASE" ]; then
+  >&2 echo "Usage: rebase_snapshot.sh <test_case>"
+  exit 1
+fi
+
+TEST_SETUP=$SNAPSHOT_DIR/$TEST_CASE.sh
+TEST_SNAPSHOT=$SNAPSHOT_DIR/$TEST_CASE.png
+
+echo "Updating $TEST_CASE..."
+
+bash $TEST_SETUP
+$SNAPSHOT_TOOL $TEST_SNAPSHOT
---
+++ selfdrive/ui/tests/snapshots/base.sh
@@ -0,0 +1,2 @@
+echo -n 0 > ~/.comma/params/d/ExperimentalMode
+echo -n 0 > ~/.comma/params/d/PrimeType
