2ed95aa3c291f1941ddf0bf1fcc68323e91dc856
Cameron Clough <cameronjclough@gmail.com>
2023-05-31 15:23:37 -0700

    remove prime type arg

--- selfdrive/ui/tests/test_ui_snapshot.h
+++ selfdrive/ui/tests/test_ui_snapshot.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include <functional>
 #include <QWidget>
-
-#include "selfdrive/ui/ui.h"
-
-struct TestCase {
-  std::function<void()> setupFunc;
-  std::string name;
-};
 
 void saveWidgetAsImage(QWidget *widget, const QString &fileName);
