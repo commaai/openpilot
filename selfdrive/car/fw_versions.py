7674a38967c00a7ea856f914a76e3560d767bb67
Shane Smiskol <shane@smiskol.com>
2023-06-01 21:57:53 -0700

    or this unfortunately

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -95,7 +95,7 @@ def match_fw_to_car_fuzzy(fw_versions_dict, log=True, exclude=None):
     return set()
 
 
-def match_fw_to_car_exact(fw_versions_dict: Dict[Tuple[int, Optional[int]], Set[bytes]]) -> Set[str]:
+def match_fw_to_car_exact(fw_versions_dict):
   """Do an exact FW match. Returns all cars that match the given
   FW versions for a list of "essential" ECUs. If an ECU is not considered
   essential the FW version can be missing to get a fingerprint, but if it's present it
