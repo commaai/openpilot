48052d162f8b3a68978d80c53a8be834eb0c635c
Shane Smiskol <shane@smiskol.com>
2023-06-02 13:06:23 -0700

    remove

--- selfdrive/car/isotp_parallel_query.py
+++ selfdrive/car/isotp_parallel_query.py
@@ -148,8 +148,7 @@ class IsoTpParallelQuery:
               cloudlog.error(f"iso-tp query timeout after receiving partial response: {tx_addr}")
             elif tx_addr in addrs_responded:
               cloudlog.error(f"iso-tp query timeout while receiving response: {tx_addr}")
-            else:
-              cloudlog.error(f"iso-tp query timeout with no response: {tx_addr}")
+            # timeout with no response
           request_done[tx_addr] = True
 
       # Break if all requests are done (finished or timed out)
