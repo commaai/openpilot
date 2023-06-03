a47e04244d24822ebdddf04cd398db50fbf05e1f
Justin Newberry <jnewberry0502@gmail.com>
2023-06-03 13:59:20 +0000

    lets revert that

--- selfdrive/car/subaru/carstate.py
+++ selfdrive/car/subaru/carstate.py
@@ -56,7 +56,6 @@ class CarState(CarStateBase):
     cp_cruise = cp_body if self.car_fingerprint in GLOBAL_GEN2 else cp
     ret.cruiseState.enabled = cp_cruise.vl["CruiseControl"]["Cruise_Activated"] != 0
     ret.cruiseState.available = cp_cruise.vl["CruiseControl"]["Cruise_On"] != 0
-
     ret.cruiseState.speed = cp_cruise.vl["Cruise_Status"]["Cruise_Set_Speed"] * CV.KPH_TO_MS
 
     if (self.car_fingerprint in PREGLOBAL_CARS and cp.vl["Dash_State2"]["UNITS"] == 1) or \
@@ -76,7 +75,7 @@ class CarState(CarStateBase):
     else:
       ret.steerFaultTemporary = cp.vl["Steering_Torque"]["Steer_Warning"] == 1
       ret.cruiseState.nonAdaptive = cp_cam.vl["ES_DashStatus"]["Conventional_Cruise"] == 1
-      ret.cruiseState.standstill = cp_body.vl["Engine_Stop_Start"]["Cruise_Hold"] == 1
+      ret.cruiseState.standstill = cp_cam.vl["ES_DashStatus"]["Cruise_State"] == 3
       ret.stockFcw = cp_cam.vl["ES_LKAS_State"]["LKAS_Alert"] == 2
       self.es_lkas_state_msg = copy.copy(cp_cam.vl["ES_LKAS_State"])
 
@@ -99,14 +98,12 @@ class CarState(CarStateBase):
       ("RR", "Wheel_Speeds"),
       ("Brake", "Brake_Status"),
       ("Cruise_Set_Speed", "Cruise_Status"),
-      ("Cruise_Hold", "Engine_Stop_Start"),
     ]
     checks = [
       ("CruiseControl", 20),
       ("Wheel_Speeds", 50),
       ("Brake_Status", 50),
       ("Cruise_Status", 20),
-      ("Engine_Stop_Start", 50)
     ]
 
     return signals, checks
