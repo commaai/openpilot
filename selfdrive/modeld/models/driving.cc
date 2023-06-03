4de14c113068776981315fc44fc62addc11b338b
YassineYousfi <yyousfi1@binghamton.edu>
2023-06-02 20:55:13 -0700

    hot coffee model (#28296)
    * 1061b1c7-b944-43e3-a940-b56b64d66f54/700
    
    * bump cereal
    
    * bump cereal
    
    * make mypy happy
    
    * write TODO
    
    * read height from written params
    
    * fix certain_if_calib logic and unit tests
    
    * factor moving_avg_with_linear_decay
    
    * remove whitespace
    
    * update model ref commit
    
    * default value for CI
    
    * update process replay ref commit
    
    * update process replay ref commit
    
    * update process replay ref commit
    
    * bump cereal
    old-commit-hash: 7925232a349388f3435940c90989f957746d918a

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -390,15 +390,18 @@ void posenet_publish(PubMaster &pm, uint32_t vipc_frame_id, uint32_t vipc_droppe
   const auto &v_std = net_outputs.pose.velocity_std;
   const auto &r_std = net_outputs.pose.rotation_std;
   const auto &t_std = net_outputs.wide_from_device_euler.std;
+  const auto &road_transform_trans_mean = net_outputs.road_transform.position_mean;
+  const auto &road_transform_trans_std = net_outputs.road_transform.position_std;
 
   auto posenetd = msg.initEvent(valid && (vipc_dropped_frames < 1)).initCameraOdometry();
   posenetd.setTrans({v_mean.x, v_mean.y, v_mean.z});
   posenetd.setRot({r_mean.x, r_mean.y, r_mean.z});
   posenetd.setWideFromDeviceEuler({t_mean.x, t_mean.y, t_mean.z});
+  posenetd.setRoadTransformTrans({road_transform_trans_mean.x, road_transform_trans_mean.y, road_transform_trans_mean.z});
   posenetd.setTransStd({exp(v_std.x), exp(v_std.y), exp(v_std.z)});
   posenetd.setRotStd({exp(r_std.x), exp(r_std.y), exp(r_std.z)});
   posenetd.setWideFromDeviceEulerStd({exp(t_std.x), exp(t_std.y), exp(t_std.z)});
-
+  posenetd.setRoadTransformTransStd({exp(road_transform_trans_std.x), exp(road_transform_trans_std.y), exp(road_transform_trans_std.z)});
 
   posenetd.setTimestampEof(timestamp_eof);
   posenetd.setFrameId(vipc_frame_id);
