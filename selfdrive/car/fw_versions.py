50709cc7738144b49235f5536b427a6be3164949
Shane Smiskol <shane@smiskol.com>
2023-06-05 23:24:21 -0700

    continue checking if candidate switches on these, that's fine to check

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -71,7 +71,7 @@ def match_fw_to_car_fuzzy(fw_versions_dict, log=True, exclude=None):
       for f in fws:
         all_fw_versions[(addr[1], addr[2], f)].append(candidate)
 
-  matched_ecus = []
+  matched_ecus = set()
   candidate = None
   for addr, versions in fw_versions_dict.items():
     for version in versions:
@@ -79,8 +79,8 @@ def match_fw_to_car_fuzzy(fw_versions_dict, log=True, exclude=None):
       ecu_key = (addr[0], addr[1])
       candidates = all_fw_versions[(*ecu_key, version)]
 
-      if len(candidates) == 1 and ecu_key not in matched_ecus:
-        matched_ecus.append(ecu_key)
+      if len(candidates) == 1:
+        matched_ecus.add(ecu_key)
         if candidate is None:
           candidate = candidates[0]
         # We uniquely matched two different cars. No fuzzy match possible
