1486fcf40b20eca8fd7c1a37baebfb02ea33aefa
Justin Newberry <jnewberry0502@gmail.com>
2023-06-06 01:47:29 +0000

    messed that up too

--- selfdrive/car/subaru/carstate.py
+++ selfdrive/car/subaru/carstate.py
@@ -87,7 +87,7 @@ class CarState(CarStateBase):
 
     ret.cruiseState.enabled = cp_cruise.vl["CruiseControl"]["Cruise_Activated"] != 0
     ret.cruiseState.available = cp_cruise.vl["CruiseControl"]["Cruise_On"] != 0
-    ret.cruiseState.speed = cp_body.vl["Cruise_Status"]["Cruise_Set_Speed"] * CV.KPH_TO_MS
+    ret.cruiseState.speed = cp_cruise.vl["Cruise_Status"]["Cruise_Set_Speed"] * CV.KPH_TO_MS
 
     if (self.car_fingerprint in PREGLOBAL_CARS and cp.vl["Dash_State2"]["UNITS"] == 1) or \
       (self.car_fingerprint not in PREGLOBAL_CARS and cp.vl["Dashlights"]["UNITS"] == 1):
@@ -348,6 +348,7 @@ class CarState(CarStateBase):
         ("Standstill", "ES_Distance"),
         ("Signal3", "ES_Distance"),
         ("Close_Distance", "ES_Distance"),
+        ("Signal4", "ES_Distance"),
         ("Standstill_2", "ES_Distance"),
         ("Cruise_Fault", "ES_Distance"),
         ("Signal5", "ES_Distance"),
