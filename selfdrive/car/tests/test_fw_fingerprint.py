059fd859f2ab697f52dc4754c905af1ffe1beee9
Shane Smiskol <shane@smiskol.com>
2023-06-06 17:23:23 -0700

    basic messy test

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -10,7 +10,8 @@ from cereal import car
 from common.params import Params
 from selfdrive.car.car_helpers import interfaces
 from selfdrive.car.fingerprints import FW_VERSIONS
-from selfdrive.car.fw_versions import FW_QUERY_CONFIGS, VERSIONS, match_fw_to_car, get_fw_versions
+from selfdrive.car.fw_versions import FW_QUERY_CONFIGS, FUZZY_EXCLUDE_ECUS, VERSIONS, match_fw_to_car, \
+                                      get_fw_versions
 
 CarFw = car.CarParams.CarFw
 Ecu = car.CarParams.Ecu
@@ -32,20 +33,47 @@ class TestFwFingerprint(unittest.TestCase):
     self.assertEqual(len(candidates), 1, f"got more than one candidate: {candidates}")
     self.assertEqual(candidates[0], expected)
 
+  # @parameterized.expand([(b, c, e[c]) for b, e in VERSIONS.items() for c in e])
+  # def test_exact_match(self, brand, car_model, ecus):
+  #   CP = car.CarParams.new_message()
+  #   for _ in range(20):
+  #     fw = []
+  #     for ecu, fw_versions in ecus.items():
+  #       if not len(fw_versions):
+  #         raise unittest.SkipTest("Car model has no FW versions")
+  #       ecu_name, addr, sub_addr = ecu
+  #       fw.append({"ecu": ecu_name, "fwVersion": random.choice(fw_versions), 'brand': brand,
+  #                  "address": addr, "subAddress": 0 if sub_addr is None else sub_addr})
+  #     CP.carFw = fw
+  #     _, matches = match_fw_to_car(CP.carFw)
+  #     self.assertFingerprints(matches, car_model)
+
   @parameterized.expand([(b, c, e[c]) for b, e in VERSIONS.items() for c in e])
-  def test_fw_fingerprint(self, brand, car_model, ecus):
+  def test_fuzzy_match(self, brand, car_model, ecus):
+    # if car_model != 'HYUNDAI SONATA 2020':
+    # if not car_model.startswith('HYUNDAI'):
+    #   raise unittest.SkipTest("Only test HYUNDAI SONATA 2020")
     CP = car.CarParams.new_message()
-    for _ in range(200):
-      fw = []
-      for ecu, fw_versions in ecus.items():
-        if not len(fw_versions):
-          raise unittest.SkipTest("Car model has no FW versions")
-        ecu_name, addr, sub_addr = ecu
-        fw.append({"ecu": ecu_name, "fwVersion": random.choice(fw_versions), 'brand': brand,
+    fw = []
+    for ecu, fw_versions in ecus.items():
+      if len(fw_versions) < 2:
+        raise unittest.SkipTest("Car model has no FW versions")
+      if ecu[0] in FUZZY_EXCLUDE_ECUS:
+        continue
+      ecu_name, addr, sub_addr = ecu
+      for random_version in random.sample(fw_versions, 2):
+        fw.append({"ecu": ecu_name, "fwVersion": random_version, 'brand': brand,
                    "address": addr, "subAddress": 0 if sub_addr is None else sub_addr})
-      CP.carFw = fw
-      _, matches = match_fw_to_car(CP.carFw)
-      self.assertFingerprints(matches, car_model)
+    CP.carFw = fw
+
+    _, matches = match_fw_to_car(CP.carFw, allow_exact=False)
+    # self.assertFingerprints(matches, car_model)
+    print([(f['address'], f['subAddress']) for f in fw])
+    # if len([(f['address'], f['subAddress']) for f in fw]) < 2:
+    if len([(f['address'], f['subAddress']) for f in fw]) < 2:
+      print('asserting true')
+      self.assertTrue(len(matches) != 1, (car_model, [(f['address'], f['subAddress']) for f in fw]))
+
 
   def test_no_duplicate_fw_versions(self):
     for car_model, ecus in FW_VERSIONS.items():
@@ -114,68 +142,68 @@ class TestFwFingerprint(unittest.TestCase):
                          f'{brand.title()}: FW query whitelist missing ecus: {ecu_strings}')
 
 
-class TestFwFingerprintTiming(unittest.TestCase):
-  @staticmethod
-  def _benchmark(brand, num_pandas, n):
-    params = Params()
-    fake_socket = FakeSocket()
-
-    times = []
-    for _ in range(n):
-      params.put_bool("ObdMultiplexingEnabled", True)
-      thread = threading.Thread(target=get_fw_versions, args=(fake_socket, fake_socket, brand), kwargs=dict(num_pandas=num_pandas))
-      thread.start()
-      t = time.perf_counter()
-      while thread.is_alive():
-        time.sleep(0.02)
-        if not params.get_bool("ObdMultiplexingChanged"):
-          params.put_bool("ObdMultiplexingChanged", True)
-      times.append(time.perf_counter() - t)
-
-    return round(sum(times) / len(times), 2)
-
-  def _assert_timing(self, avg_time, ref_time, tol):
-    self.assertLess(avg_time, ref_time + tol)
-    self.assertGreater(avg_time, ref_time - tol, "Performance seems to have improved, update test refs.")
-
-  def test_fw_query_timing(self):
-    tol = 0.1
-    total_ref_time = 4.6
-    brand_ref_times = {
-      1: {
-        'body': 0.1,
-        'chrysler': 0.3,
-        'ford': 0.2,
-        'honda': 0.5,
-        'hyundai': 0.7,
-        'mazda': 0.1,
-        'nissan': 0.3,
-        'subaru': 0.1,
-        'tesla': 0.2,
-        'toyota': 0.7,
-        'volkswagen': 0.2,
-      },
-      2: {
-        'hyundai': 1.1,
-      }
-    }
-
-    total_time = 0
-    for num_pandas in (1, 2):
-      for brand, config in FW_QUERY_CONFIGS.items():
-        with self.subTest(brand=brand, num_pandas=num_pandas):
-          multi_panda_requests = [r for r in config.requests if r.bus > 3]
-          if not len(multi_panda_requests) and num_pandas > 1:
-            raise unittest.SkipTest("No multi-panda FW queries")
-
-          avg_time = self._benchmark(brand, num_pandas, 10)
-          total_time += avg_time
-          self._assert_timing(avg_time, brand_ref_times[num_pandas][brand], tol)
-          print(f'{brand=}, {num_pandas=}, {len(config.requests)=}, avg FW query time={avg_time} seconds')
-
-    with self.subTest(brand='all_brands'):
-      self._assert_timing(total_time, total_ref_time, tol)
-      print(f'all brands, total FW query time={total_time} seconds')
+# class TestFwFingerprintTiming(unittest.TestCase):
+#   @staticmethod
+#   def _benchmark(brand, num_pandas, n):
+#     params = Params()
+#     fake_socket = FakeSocket()
+#
+#     times = []
+#     for _ in range(n):
+#       params.put_bool("ObdMultiplexingEnabled", True)
+#       thread = threading.Thread(target=get_fw_versions, args=(fake_socket, fake_socket, brand), kwargs=dict(num_pandas=num_pandas))
+#       thread.start()
+#       t = time.perf_counter()
+#       while thread.is_alive():
+#         time.sleep(0.02)
+#         if not params.get_bool("ObdMultiplexingChanged"):
+#           params.put_bool("ObdMultiplexingChanged", True)
+#       times.append(time.perf_counter() - t)
+#
+#     return round(sum(times) / len(times), 2)
+#
+#   def _assert_timing(self, avg_time, ref_time, tol):
+#     self.assertLess(avg_time, ref_time + tol)
+#     self.assertGreater(avg_time, ref_time - tol, "Performance seems to have improved, update test refs.")
+#
+#   def test_fw_query_timing(self):
+#     tol = 0.1
+#     total_ref_time = 4.6
+#     brand_ref_times = {
+#       1: {
+#         'body': 0.1,
+#         'chrysler': 0.3,
+#         'ford': 0.2,
+#         'honda': 0.5,
+#         'hyundai': 0.7,
+#         'mazda': 0.1,
+#         'nissan': 0.3,
+#         'subaru': 0.1,
+#         'tesla': 0.2,
+#         'toyota': 0.7,
+#         'volkswagen': 0.2,
+#       },
+#       2: {
+#         'hyundai': 1.1,
+#       }
+#     }
+#
+#     total_time = 0
+#     for num_pandas in (1, 2):
+#       for brand, config in FW_QUERY_CONFIGS.items():
+#         with self.subTest(brand=brand, num_pandas=num_pandas):
+#           multi_panda_requests = [r for r in config.requests if r.bus > 3]
+#           if not len(multi_panda_requests) and num_pandas > 1:
+#             raise unittest.SkipTest("No multi-panda FW queries")
+#
+#           avg_time = self._benchmark(brand, num_pandas, 10)
+#           total_time += avg_time
+#           self._assert_timing(avg_time, brand_ref_times[num_pandas][brand], tol)
+#           print(f'{brand=}, {num_pandas=}, {len(config.requests)=}, avg FW query time={avg_time} seconds')
+#
+#     with self.subTest(brand='all_brands'):
+#       self._assert_timing(total_time, total_ref_time, tol)
+#       print(f'all brands, total FW query time={total_time} seconds')
 
 
 if __name__ == "__main__":
