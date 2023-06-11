c4bc3964a614cbdfddcb06930a7d170411cceb07
Harald Schäfer <harald.the.engineer@gmail.com>
2023-06-10 23:44:01 -0700

    Encoderd: encoders just encode, no writing (#28482)
    Encoders dont write, they encode
    old-commit-hash: 1fd7542eae10e78fb363548da428a9c7a907638e

--- system/loggerd/encoder/ffmpeg_encoder.h
+++ system/loggerd/encoder/ffmpeg_encoder.h
@@ -17,8 +17,8 @@ extern "C" {
 class FfmpegEncoder : public VideoEncoder {
  public:
   FfmpegEncoder(const char* filename, CameraType type, int in_width, int in_height, int fps,
-                int bitrate, cereal::EncodeIndex::Type codec, int out_width, int out_height, bool write) :
-                VideoEncoder(filename, type, in_width, in_height, fps, bitrate, cereal::EncodeIndex::Type::BIG_BOX_LOSSLESS, out_width, out_height, write) { encoder_init(); }
+                int bitrate, cereal::EncodeIndex::Type codec, int out_width, int out_height) :
+                VideoEncoder(filename, type, in_width, in_height, fps, bitrate, cereal::EncodeIndex::Type::BIG_BOX_LOSSLESS, out_width, out_height) { encoder_init(); }
   ~FfmpegEncoder();
   void encoder_init();
   int encode_frame(VisionBuf* buf, VisionIpcBufExtra *extra);
