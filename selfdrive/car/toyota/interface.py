b51ddc4e703d99e34d23d27267f366cba3c048bb
Shane Smiskol <shane@smiskol.com>
2023-06-15 13:33:45 -0700

    add back proportional

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -232,8 +232,8 @@ class CarInterface(CarInterfaceBase):
     tune.deadzoneBP = [0., 9.]
     tune.deadzoneV = [.0, .15]
     if candidate in TSS2_CAR or ret.enableGasInterceptor:
-      tune.kpBP = [0.]  # [0., 5., 20.]
-      tune.kpV = [0.]  # [1.3, 1.0, 0.7]
+      tune.kpBP = [0., 5., 20.]
+      tune.kpV = [1.3, 1.0, 0.7]
       tune.kiBP = [0., 5., 12., 20., 27.]
       tune.kiV = [.35, .23, .20, .17, .1]
       if candidate in TSS2_CAR:
