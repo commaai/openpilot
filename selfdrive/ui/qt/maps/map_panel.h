35210faf291dc3c6324b46ced4da63ea1755735f
Cameron Clough <cameronjclough@gmail.com>
2023-06-20 22:15:39 +0100

    lines

--- selfdrive/ui/qt/maps/map_panel.h
+++ selfdrive/ui/qt/maps/map_panel.h
@@ -6,10 +6,12 @@
 
 class MapPanel : public QFrame {
   Q_OBJECT
+
 public:
   explicit MapPanel(const QMapboxGLSettings &settings, QWidget *parent = nullptr);
 
   bool isShowingMap() const;
+
 private:
   QStackedLayout *content_stack;
 };
