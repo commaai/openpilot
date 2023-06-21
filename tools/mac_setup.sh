3d73d4d82f05a67b879bce0f41dc2b41dccd9aa6
Kacper Rączy <gfw.kra@gmail.com>
2023-06-20 17:36:24 -0700

    Remove manual casadi build from mac_setup

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -84,29 +84,6 @@ $ROOT/update_requirements.sh
 eval "$(pyenv init --path)"
 echo "[ ] installed python dependencies t=$SECONDS"
 
-# install casadi
-VENV=`poetry env info --path`
-PYTHON_VER=3.8
-PYTHON_VERSION=$(cat $ROOT/.python-version)
-if [ ! -f "$VENV/include/casadi/casadi.hpp" ]; then
-  echo "-- casadi manual install"
-  cd /tmp/ && curl -L https://github.com/casadi/casadi/archive/refs/tags/ge6.tar.gz --output casadi.tar.gz
-  tar -xzf casadi.tar.gz
-  cd casadi-ge6/ && mkdir -p build && cd build
-  cmake .. \
-    -DWITH_PYTHON=ON \
-    -DWITH_EXAMPLES=OFF \
-    -DCMAKE_INSTALL_PREFIX:PATH=$VENV \
-    -DPYTHON_PREFIX:PATH=$VENV/lib/python$PYTHON_VER/site-packages \
-    -DPYTHON_LIBRARY:FILEPATH=$HOME/.pyenv/versions/$PYTHON_VERSION/lib/libpython$PYTHON_VER.dylib \
-    -DPYTHON_EXECUTABLE:FILEPATH=$HOME/.pyenv/versions/$PYTHON_VERSION/bin/python \
-    -DPYTHON_INCLUDE_DIR:PATH=$HOME/.pyenv/versions/$PYTHON_VERSION/include/python$PYTHON_VER \
-    -DCMAKE_CXX_FLAGS="-ferror-limit=0" -DCMAKE_C_FLAGS="-ferror-limit=0"
-  CFLAGS="-ferror-limit=0" make -j$(nproc) && make install
-else
-  echo "----   casadi found in venv. skipping build   ----"
-fi
-
 echo
 echo "----   OPENPILOT SETUP DONE   ----"
 echo "Open a new shell or configure your active shell env by running:"
