6cfa5bfe352071216106217a35b2d6fae19a8d90
Shane Smiskol <shane@smiskol.com>
2023-06-21 22:03:28 -0700

    we query abs

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -254,10 +254,9 @@ FW_QUERY_CONFIG = FwQueryConfig(
     # - HV Battery (0x713, 0x747)
     # - Motor Generator (0x716, 0x724)
     # Responds to KWP:
-    # - Air Conditioner (0x7c4),
-    # - Steering Angle Sensor (0x7b3),
-    # - ABS/VSC/TRAC (0x7b0),
-    # - EPS/EMPS (0x7A0, 0x7a1),
+    # - Air Conditioner (0x7c4)
+    # - Steering Angle Sensor (0x7b3)
+    # - EPS/EMPS (0x7A0, 0x7a1)
     # - SRS Airbag (0x780, 0x784)
     # These have been seen responding to UDS query
     (Ecu.epb, 0x750, 0x2c),  # Electronic Parking Brake
