9028326c74e5e4cd43982dc9b0f88c74e2d9eff4
Shane Smiskol <shane@smiskol.com>
2023-06-22 03:59:14 -0700

    Toyota: add combination meter and SRS airbag ECUs (#28633)
    * add combination meter and srs airbags
    
    * whitelist
    old-commit-hash: af42caa394f414a067706802d08169b83ca1cda5

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -224,19 +224,21 @@ FW_QUERY_CONFIG = FwQueryConfig(
     Request(
       [StdQueries.SHORT_TESTER_PRESENT_REQUEST, TOYOTA_VERSION_REQUEST_KWP],
       [StdQueries.SHORT_TESTER_PRESENT_RESPONSE, TOYOTA_VERSION_RESPONSE_KWP],
-      whitelist_ecus=[Ecu.fwdCamera, Ecu.fwdRadar, Ecu.dsu, Ecu.abs, Ecu.eps, Ecu.epb, Ecu.telematics, Ecu.hybrid],
+      whitelist_ecus=[Ecu.fwdCamera, Ecu.fwdRadar, Ecu.dsu, Ecu.abs, Ecu.eps, Ecu.epb, Ecu.telematics,
+                      Ecu.hybrid, Ecu.srs, Ecu.combinationMeter],
       bus=0,
     ),
     Request(
       [StdQueries.SHORT_TESTER_PRESENT_REQUEST, StdQueries.OBD_VERSION_REQUEST],
       [StdQueries.SHORT_TESTER_PRESENT_RESPONSE, StdQueries.OBD_VERSION_RESPONSE],
-      whitelist_ecus=[Ecu.engine, Ecu.epb, Ecu.telematics, Ecu.hybrid],
+      whitelist_ecus=[Ecu.engine, Ecu.epb, Ecu.telematics, Ecu.hybrid, Ecu.srs, Ecu.combinationMeter],
       bus=0,
     ),
     Request(
       [StdQueries.TESTER_PRESENT_REQUEST, StdQueries.DEFAULT_DIAGNOSTIC_REQUEST, StdQueries.EXTENDED_DIAGNOSTIC_REQUEST, StdQueries.UDS_VERSION_REQUEST],
       [StdQueries.TESTER_PRESENT_RESPONSE, StdQueries.DEFAULT_DIAGNOSTIC_RESPONSE, StdQueries.EXTENDED_DIAGNOSTIC_RESPONSE, StdQueries.UDS_VERSION_RESPONSE],
-      whitelist_ecus=[Ecu.engine, Ecu.fwdRadar, Ecu.fwdCamera, Ecu.abs, Ecu.eps, Ecu.epb, Ecu.telematics, Ecu.hybrid],
+      whitelist_ecus=[Ecu.engine, Ecu.fwdRadar, Ecu.fwdCamera, Ecu.abs, Ecu.eps, Ecu.epb, Ecu.telematics,
+                      Ecu.hybrid, Ecu.srs, Ecu.combinationMeter],
       bus=0,
     ),
   ],
@@ -249,21 +251,22 @@ FW_QUERY_CONFIG = FwQueryConfig(
   extra_ecus=[
     # All known ECUs on a late-model Toyota vehicle not queried here:
     # Responds to UDS:
-    # - Combination Meter (0x7c0)
     # - HV Battery (0x713, 0x747)
     # - Motor Generator (0x716, 0x724)
     # Responds to KWP:
     # - Air Conditioner (0x7c4)
     # - Steering Angle Sensor (0x7b3)
     # - EPS/EMPS (0x7a0, 0x7a1)
-    # - SRS Airbag (0x780, 0x784)
 
     # These have been seen responding to UDS query
-    # TODO: if these hybrid ECUs always exist together, remove one
+    # TODO: if these duplicate ECUs always exist together, remove one
     (Ecu.hybrid, 0x712, None),  # Hybrid Control Assembly & Computer
     (Ecu.hybrid, 0x7d2, None),  # Hybrid Control Assembly & Computer 2
+    (Ecu.srs, 0x780, None),     # SRS Airbag
+    (Ecu.srs, 0x784, None),     # SRS Airbag 2
     (Ecu.epb, 0x750, 0x2c),     # Electronic Parking Brake
     (Ecu.telematics, 0x750, 0xc7),
+    (Ecu.combinationMeter, 0x7c0, None),
   ],
 )
 
