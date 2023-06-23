c370f62bc23053469948a8f6df86bbec7cc0594f
Maxime Desroches <desroches.maxime@gmail.com>
2023-06-23 10:20:24 -0700

    determinism

--- selfdrive/test/process_replay/test_fuzzy.py
+++ selfdrive/test/process_replay/test_fuzzy.py
@@ -12,7 +12,7 @@ import selfdrive.test.process_replay.process_replay as pr
 # These processes currently fail because of unrealistic data breaking assumptions
 # that openpilot makes causing error with NaN, inf, int size, array indexing ...
 # TODO: Make each one testable
-NOT_TESTED = ['controlsd', 'plannerd', 'calibrationd', 'dmonitoringd', 'paramsd', 'laikad']
+NOT_TESTED = ['controlsd', 'plannerd', 'calibrationd', 'dmonitoringd', 'paramsd', 'laikad', 'dmonitoringmodeld', 'modeld', 'torqued', 'radard']
 TEST_CASES = [(cfg.proc_name, cfg) for cfg in pr.CONFIGS if cfg.proc_name not in NOT_TESTED]
 
 class TestFuzzProcesses(unittest.TestCase):
@@ -21,7 +21,7 @@ class TestFuzzProcesses(unittest.TestCase):
   @given(st.data())
   @settings(phases=[Phase.generate, Phase.target], max_examples=50, deadline=1000, suppress_health_check=[HealthCheck.too_slow, HealthCheck.data_too_large])
   def test_fuzz_process(self, proc_name, cfg, data):
-    msgs = data.draw(FuzzyGenerator.get_random_event_msg(required=cfg.pubs, real_floats=True))
+    msgs = FuzzyGenerator.get_random_event_msg(data.draw, events=cfg.pubs, real_floats=True)
     lr = [log.Event.new_message(**m).as_reader() for m in msgs]
     cfg.timeout = 5
     pr.replay_process(cfg, lr, TOYOTA.COROLLA_TSS2, disable_progress=True)
