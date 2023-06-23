d1c19dfda972bf7404436862f9de59f9d9dc8a55
Dean Lee <deanlee3@gmail.com>
2023-06-24 06:27:51 +0800

    cabana: properly handle unix signals (#28657)
    old-commit-hash: fe91ea1139a5ff3f832a73f09bbdd77a78533acc

--- tools/cabana/util.cc
+++ tools/cabana/util.cc
@@ -1,7 +1,11 @@
 #include "tools/cabana/util.h"
 
 #include <array>
+#include <csignal>
+#include <sys/socket.h>
+#include <unistd.h>
 
+#include <QDebug>
 #include <QColor>
 #include <QFontDatabase>
 #include <QLocale>
@@ -144,6 +148,40 @@ void TabBar::closeTabClicked() {
     }
   }
 }
+
+// UnixSignalHandler
+
+UnixSignalHandler::UnixSignalHandler(QObject *parent) : QObject(nullptr) {
+  if (::socketpair(AF_UNIX, SOCK_STREAM, 0, sig_fd)) {
+    qFatal("Couldn't create TERM socketpair");
+  }
+
+  sn = new QSocketNotifier(sig_fd[1], QSocketNotifier::Read, this);
+  connect(sn, SIGNAL(activated(QSocketDescriptor)), this, SLOT(handleSigTerm()));
+  std::signal(SIGINT, signalHandler);
+  std::signal(SIGTERM, UnixSignalHandler::signalHandler);
+}
+
+UnixSignalHandler::~UnixSignalHandler() {
+  ::close(sig_fd[0]);
+  ::close(sig_fd[1]);
+}
+
+void UnixSignalHandler::signalHandler(int s) {
+  ::write(sig_fd[0], &s, sizeof(s));
+}
+
+void UnixSignalHandler::handleSigTerm() {
+  sn->setEnabled(false);
+  int tmp;
+  ::read(sig_fd[1], &tmp, sizeof(tmp));
+
+  printf("\nexiting...\n");
+  qApp->closeAllWindows();
+  qApp->exit();
+}
+
+// NameValidator
 
 NameValidator::NameValidator(QObject *parent) : QRegExpValidator(QRegExp("^(\\w+)"), parent) {}
 
