284cbb5cf491917ef85028def1d3d68725b90b7d
Jason Wen <haibin.wen3@gmail.com>
2023-06-24 00:03:56 -0400

    add FW for US carnival 2023

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1857,9 +1857,11 @@ FW_VERSIONS = {
   CAR.KIA_CARNIVAL_4TH_GEN: {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00KA4CMFC  AT CHN LHD 1.00 1.01 99211-I4000 210525',
+      b'\xf1\x00KA4 MFC  AT USA LHD 1.00 1.06 99210-R0000 220221',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00KA4c SCC FHCUP      1.00 1.01 99110-I4000         ',
+      b'\xf1\x00KA4_ SCC FHCUP      1.00 1.03 99110-R0000         ',
     ],
   },
 }
