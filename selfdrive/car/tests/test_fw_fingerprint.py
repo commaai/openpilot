8561cbffe524f3e79f2ad2d132a757d09414b085
Shane Smiskol <shane@smiskol.com>
2023-06-24 04:50:00 -0700

    one line

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -180,9 +180,8 @@ class TestFwFingerprintTiming(unittest.TestCase):
     fake_socket = FakeSocket()
     brand_time = 0
     for _ in range(self.N):
-      thread = threading.Thread(target=get_fw_versions, args=(fake_socket, fake_socket, brand),
-                                kwargs=dict(num_pandas=num_pandas))
-      brand_time += self._run_thread(thread)
+      brand_time += self._run_thread(threading.Thread(target=get_fw_versions, args=(fake_socket, fake_socket, brand),
+                                                      kwargs=dict(num_pandas=num_pandas)))
 
     return round(brand_time / self.N, 2)
 
