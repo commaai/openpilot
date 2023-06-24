ea220074156bb79106e23fb6590dcc6c26a27c16
Shane Smiskol <shane@smiskol.com>
2023-06-24 03:44:16 -0700

    not sure what i want this to do yet
    not sure what i want this to do yet

--- selfdrive/car/toyota/tests/test_toyota.py
+++ selfdrive/car/toyota/tests/test_toyota.py
@@ -14,19 +14,20 @@ class TestToyotaInterfaces(unittest.TestCase):
 
 
 class TestToyotaFingerprint(unittest.TestCase):
-  def test_essential_ecus(self):
-    # Asserts ECU keys essential for fuzzy fingerprinting are available on all platforms
-    essential_ecus = {Ecu.fwdRadar, Ecu.fwdCamera, Ecu.engine}
-    for car_model, ecus in FW_VERSIONS.items():
-      with self.subTest(car_model=car_model):
-        missing_ecus = essential_ecus - {ecu[0] for ecu in ecus}
-        self.assertEqual(missing_ecus, set())
-        # for platform_code_ecu in PLATFORM_CODE_ECUS:
-        #   if platform_code_ecu in (Ecu.fwdRadar, Ecu.eps) and car_model == CAR.HYUNDAI_GENESIS:
-        #     continue
-        #   if platform_code_ecu == Ecu.eps and car_model in no_eps_platforms:
-        #     continue
-        #   self.assertIn(platform_code_ecu, [e[0] for e in ecus])
+  pass
+  # def test_essential_ecus(self):
+  #   # Asserts ECU keys essential for fuzzy fingerprinting are available on all platforms
+  #   essential_ecus = {Ecu.fwdRadar, Ecu.fwdCamera, Ecu.engine}
+  #   for car_model, ecus in FW_VERSIONS.items():
+  #     with self.subTest(car_model=car_model):
+  #       missing_ecus = essential_ecus - {ecu[0] for ecu in ecus}
+  #       self.assertEqual(missing_ecus, set())
+  #       # for platform_code_ecu in PLATFORM_CODE_ECUS:
+  #       #   if platform_code_ecu in (Ecu.fwdRadar, Ecu.eps) and car_model == CAR.HYUNDAI_GENESIS:
+  #       #     continue
+  #       #   if platform_code_ecu == Ecu.eps and car_model in no_eps_platforms:
+  #       #     continue
+  #       #   self.assertIn(platform_code_ecu, [e[0] for e in ecus])
 
 
 if __name__ == "__main__":
