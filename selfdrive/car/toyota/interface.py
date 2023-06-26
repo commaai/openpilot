8c07e77012f3ce50b68275c4254cfa912cbe1df8
Shane Smiskol <shane@smiskol.com>
2023-06-26 14:32:13 -0700

    Revert "this also works, but it adds a bunch of indirection"
    This reverts commit 822095b96569faf10a6841cf3892b5db29f0c01b.

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -218,10 +218,10 @@ class CarInterface(CarInterfaceBase):
 
     # if the smartDSU is detected, openpilot can send ACC_CONTROL and the smartDSU will block it from the DSU or radar.
     # since we don't yet parse radar on TSS2 radar-based ACC cars, gate longitudinal behind experimental toggle
-    ret.experimentalLongitudinalAvailable = bool(ret.flags & ToyotaFlags.SMART_DSU) and candidate in RADAR_ACC_CAR
     use_sdsu = bool(ret.flags & ToyotaFlags.SMART_DSU)
-    if ret.experimentalLongitudinalAvailable:
-      use_sdsu = experimental_long
+    if candidate in RADAR_ACC_CAR:
+      ret.experimentalLongitudinalAvailable = use_sdsu
+      use_sdsu = use_sdsu and experimental_long
 
     # openpilot longitudinal enabled by default:
     #  - non-(TSS2 radar ACC cars) w/ smartDSU installed
