5d04bee89d2324b09494dc50c5f2246356dad146
Shane Smiskol <shane@smiskol.com>
2023-06-26 12:53:37 -0700

    use it here

--- selfdrive/car/toyota/radar_interface.py
+++ selfdrive/car/toyota/radar_interface.py
@@ -6,9 +6,6 @@ from selfdrive.car.interfaces import RadarInterfaceBase
 
 
 def _create_radar_can_parser(car_fingerprint):
-  if DBC[car_fingerprint]['radar'] is None:
-    return None
-
   if car_fingerprint in TSS2_CAR:
     RADAR_A_MSGS = list(range(0x180, 0x190))
     RADAR_B_MSGS = list(range(0x190, 0x1a0))
@@ -42,7 +39,7 @@ class RadarInterface(RadarInterfaceBase):
 
     self.valid_cnt = {key: 0 for key in self.RADAR_A_MSGS}
 
-    self.rcp = _create_radar_can_parser(CP.carFingerprint)
+    self.rcp = None if CP.radarUnavailable else _create_radar_can_parser(CP.carFingerprint)
     self.trigger_msg = self.RADAR_B_MSGS[-1]
     self.updated_messages = set()
 
