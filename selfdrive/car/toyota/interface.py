9e60868666a20b6bcb747cb07a3d15d738c1e366
Shane Smiskol <shane@smiskol.com>
2023-06-27 18:10:02 -0700

    rm

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -138,7 +138,6 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.9
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.steerControlType = SteerControlType.angle
 
     elif candidate in (CAR.LEXUS_ES, CAR.LEXUS_ESH, CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2):
       if candidate not in (CAR.LEXUS_ES,):  # TODO: LEXUS_ES may have sng
