69cb4db559c193de5e57a33ba593e350e1276eda
Shane Smiskol <shane@smiskol.com>
2023-06-27 18:10:26 -0700

    try 98?

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -37,7 +37,7 @@ def create_lta_steer_command(packer, apply_steer, steer_req, limit_torque, op_pa
     # TODO: figure out why 99 is so much less torque than 100
     # "SETME_X64": 99 if limit_torque else 100,
     # "SETME_X64": op_params.get("SETME_X64"),
-    "SETME_X64": 99,  # 99 if limit_torque else (lt_val if frame % op_params.get('TLD_V3') == 0 else 100),
+    "SETME_X64": 98,  # 99 if limit_torque else (lt_val if frame % op_params.get('TLD_V3') == 0 else 100),
 
     # TODO: need to understand this better, it's always 1.5-2x higher than angle cmd
     # TODO: revisit on 2023 RAV4
