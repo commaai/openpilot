34d896895ffabdc4bf5ca7838e89a36b38d61b11
Shane Smiskol <shane@smiskol.com>
2023-06-28 23:30:39 -0700

    simple test

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -120,17 +120,6 @@ class TestFwFingerprint(unittest.TestCase):
         with self.subTest(car_model=car_model):
           self.assertFalse(len(bad_ecus), f'{car_model}: Fingerprints contain ECUs added for data collection: {bad_ecus}')
 
-  def test_fw_query_config_requests(self):
-    # Asserts equal length request and response lists, types match between request and response pairs,
-    # and if a callable exists, it must be at the end
-    for brand, config in FW_QUERY_CONFIGS.items():
-      for request_obj in config.requests:
-        self.assertEqual(len(request_obj.request), len(request_obj.response))
-        for idx, (request, response) in enumerate(zip(request_obj.request, request_obj.response)):
-          self.assertEqual(type(request), type(response))
-          if idx < len(request_obj.request) - 1:
-            self.assertTrue(isinstance(request, bytes))
-
   def test_blacklisted_ecus(self):
     blacklisted_addrs = (0x7c4, 0x7d0)  # includes A/C ecu and an unknown ecu
     for car_model, ecus in FW_VERSIONS.items():
@@ -170,6 +159,12 @@ class TestFwFingerprint(unittest.TestCase):
         ecu_strings = ", ".join([f'Ecu.{ECU_NAME[ecu]}' for ecu in ecus_not_whitelisted])
         self.assertFalse(len(whitelisted_ecus) and len(ecus_not_whitelisted),
                          f'{brand.title()}: ECUs not in any FW query whitelists: {ecu_strings}')
+
+  def test_fw_requests(self):
+    # Asserts equal length request and response lists
+    for brand, config in FW_QUERY_CONFIGS.items():
+      for request_obj in config.requests:
+        self.assertEqual(len(request_obj.request), len(request_obj.response))
 
 
 class TestFwFingerprintTiming(unittest.TestCase):
