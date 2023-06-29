7182a65a53e40919fbc1c0d99041d16fecdf6c25
Shane Smiskol <shane@smiskol.com>
2023-06-28 22:31:52 -0700

    revert

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -213,7 +213,7 @@ class TestFwFingerprintTiming(unittest.TestCase):
     print(f'get_vin, query time={vin_time / self.N} seconds')
 
   def test_fw_query_timing(self):
-    total_ref_time = 6.5
+    total_ref_time = 6.1
     brand_ref_times = {
       1: {
         'body': 0.1,
@@ -225,7 +225,7 @@ class TestFwFingerprintTiming(unittest.TestCase):
         'nissan': 0.9,
         'subaru': 0.1,
         'tesla': 0.2,
-        'toyota': 2.0,
+        'toyota': 1.6,
         'volkswagen': 0.2,
       },
       2: {
