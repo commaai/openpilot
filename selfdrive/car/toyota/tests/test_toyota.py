c53dba3f3536d1a55eb3fb0db439641a080058b0
Shane Smiskol <shane@smiskol.com>
2023-06-28 22:00:21 -0700

    add test

--- selfdrive/car/toyota/tests/test_toyota.py
+++ selfdrive/car/toyota/tests/test_toyota.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+from cereal import car
 import unittest
 
-from selfdrive.car.toyota.values import DBC, TSS2_CAR, ANGLE_CONTROL_CAR
+from selfdrive.car.toyota.values import CAR, DBC, TSS2_CAR, ANGLE_CONTROL_CAR, FW_VERSIONS
+
+Ecu = car.CarParams.Ecu
 
 
 class TestToyotaInterfaces(unittest.TestCase):
@@ -14,6 +17,23 @@ class TestToyotaInterfaces(unittest.TestCase):
     for car, dbc in DBC.items():
       if car in TSS2_CAR:
         self.assertEqual(dbc["pt"], "toyota_nodsu_pt_generated")
+
+  def test_essential_ecus(self):
+    # Asserts standard ECUs are in the database for each platform
+    common_ecus = {Ecu.fwdRadar, Ecu.fwdCamera}
+    for car_model, ecus in FW_VERSIONS.items():
+      with self.subTest(car_model=car_model):
+        present_ecus = {ecu[0] for ecu in ecus}
+        missing_ecus = common_ecus - present_ecus
+        self.assertEqual(missing_ecus, set())
+
+        # Some exceptions for other common ECUs
+        if car_model not in (CAR.ALPHARD_TSS2,):
+          self.assertIn(Ecu.abs, present_ecus)
+        if car_model not in (CAR.MIRAI,):
+          self.assertIn(Ecu.engine, present_ecus)
+        if car_model not in (CAR.PRIUS_V, CAR.LEXUS_CTH):
+          self.assertIn(Ecu.eps, present_ecus)
 
 
 if __name__ == "__main__":
