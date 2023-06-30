155e0222d0c95d48fe1780768cc1ac791750479b
Irene Cheng Chi <12470297+cydia2020@users.noreply.github.com>
2023-07-01 06:43:42 +1000

    Toyota: Fix LDA message signal name, length, and description (#28755)
    * bump opendbc
    
    * LDA_SPEED_TOO_LOW -> LDA_MESSAGES
    
    * bump opendbc
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -87,7 +87,7 @@ def create_ui_command(packer, steer, chime, left_line, right_line, left_lane_dep
     "LANE_SWAY_SENSITIVITY": 2,
     "LANE_SWAY_TOGGLE": 1,
     "LDA_ON_MESSAGE": 0,
-    "LDA_SPEED_TOO_LOW": 0,
+    "LDA_MESSAGES": 0,
     "LDA_SA_TOGGLE": 1,
     "LDA_SENSITIVITY": 2,
     "LDA_UNAVAILABLE": 0,
