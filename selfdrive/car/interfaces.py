961e03ff91d3d2758b6d560f7ac4c3af3e6d292c
JSegill <jessica@segill.com>
2023-07-02 08:02:25 -0400

    test of freeze integrator while brake hold active
    commenting out change that disabled resuming OP while brake hold was active

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -258,8 +258,8 @@ class CarInterfaceBase(ABC):
       events.add(EventName.speedTooHigh)
     if cs_out.cruiseState.nonAdaptive:
       events.add(EventName.wrongCruiseMode)
-    if cs_out.brakeHoldActive and self.CP.openpilotLongitudinalControl:
-      events.add(EventName.brakeHold)
+    #if cs_out.brakeHoldActive and self.CP.openpilotLongitudinalControl:
+     # events.add(EventName.brakeHold)
     if cs_out.parkingBrake:
       events.add(EventName.parkBrake)
     if cs_out.accFaulted:
