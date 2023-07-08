398dd771144fab835ebab4feebb4461f759bd07f
Shane Smiskol <shane@smiskol.com>
2023-07-08 01:33:41 -0700

    Hyundai: blacklist bad camera FW (#28318)
    * cmt
    
    * common parts
    
    * add code and date
    
    * Revert "add code and date"
    
    This reverts commit 47f9ac84cae8d1e73dc2b5af7d5701adc83da6eb.
    
    * Revert "common parts"
    
    This reverts commit aa5683bbce5b5e8e8414f5b90df9add3998e98ab.
    
    * add back to santa cruz
    
    * todo
    
    * part number test
    
    * pass test
    
    * Update selfdrive/car/hyundai/tests/test_hyundai.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1766,7 +1766,6 @@ FW_VERSIONS = {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-N9210 14G',
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.01 99211-N9240 14T',
-      b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-CW010 14X',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00NX4__               1.00 1.00 99110-N9100         ',
