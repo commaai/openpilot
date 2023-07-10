c1d07a1829b133e9e9392c9f01436c394eb1ec21
mitchellgoffpc <mitchellgoffpc@gmail.com>
2023-07-10 13:52:55 -0700

    Revert "UI: Indicate when Nav on OP is enabled (#28665)"
    This reverts commit 987dca2fdb2f3495efaed8c8526b81c3643a9a74.
    
    old-commit-hash: 1690cf4af6c88c52eeec0c61eae10074a169d563

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -129,7 +129,6 @@ private:
   QColor bg = bg_colors[STATUS_DISENGAGED];
   QWidget *map = nullptr;
   QHBoxLayout* split;
-  bool navDisabled = false;
 
 private slots:
   void offroadTransition(bool offroad);
