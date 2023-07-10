1c294fe35ead1fb1d0192a000201b82d1bca68af
Shane Smiskol <shane@smiskol.com>
2023-07-10 15:53:11 -0700

    should be qlabel

--- selfdrive/ui/qt/widgets/controls.h
+++ selfdrive/ui/qt/widgets/controls.h
@@ -28,7 +28,7 @@ protected:
     if (rect().contains(event->pos())) {
       emit clicked();
     }
-    QWidget::mouseReleaseEvent(event);  // propagate event to parent
+    QLabel::mouseReleaseEvent(event);  // propagate event to parent
   }
   QString lastText_, elidedText_;
 };
