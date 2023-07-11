37ee12de525e94fb91b0736bb6089613745fa7d6
Shane Smiskol <shane@smiskol.com>
2023-07-11 14:49:19 -0700

    ui: show map with nav (#28867)
    * accept touch events in map settings
    
    * draft
    
    * draft 2.0
    
    * revert this stuff
    
    * need this
    
    * fix
    
    * clean up debug prints
    
    * fraft
    
    * only show map on closeSettings if nav
    
    * works!
    
    * can't do it here
    
    * I was going to connect signals from UIState to HomeWindow, then to OnroadWidget, but that seems like too much
    
    * just need to keep track of it in onroad, seems like the cleanest option
    
    * ideally we show the map where we set the scene variable, and that's in map.cc
    
    * old variable
    
    * comments
    
    * less confusing
    
    * app comment
    
    * add back
    
    * fix
    
    * move to top
    
    * remove this comment
    old-commit-hash: 34a5e626d6f55cd33bc292e4f9d4a21e1fd2972b

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -130,6 +130,15 @@ void MapWindow::updateState(const UIState &s) {
   const SubMaster &sm = *(s.sm);
   update();
 
+  // update navigate on openpilot status
+  if (sm.updated("modelV2")) {
+    bool nav_enabled = sm["modelV2"].getModelV2().getNavEnabled();
+    if (nav_enabled && !uiState()->scene.navigate_on_openpilot) {
+      emit requestVisible(true);  // Show map on rising edge of navigate on openpilot
+    }
+    uiState()->scene.navigate_on_openpilot = nav_enabled;
+  }
+
   if (sm.updated("liveLocationKalman")) {
     auto locationd_location = sm["liveLocationKalman"].getLiveLocationKalman();
     auto locationd_pos = locationd_location.getPositionGeodetic();
