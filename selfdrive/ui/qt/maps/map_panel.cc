a281c0cb1db6cf9970d597ab09ed7448d33ad941
Cameron Clough <cameronjclough@gmail.com>
2023-07-11 14:32:39 +0100

    default to settings open

--- selfdrive/ui/qt/maps/map_panel.cc
+++ selfdrive/ui/qt/maps/map_panel.cc
@@ -30,4 +30,7 @@ MapPanel::MapPanel(const QMapboxGLSettings &mapboxSettings, QWidget *parent) : Q
     content_stack->setCurrentIndex(0);
   });
   content_stack->addWidget(settings);
+
+  // default to settings open
+  content_stack->setCurrentIndex(1);
 }
