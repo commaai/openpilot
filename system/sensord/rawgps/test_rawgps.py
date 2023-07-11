9bd7edf2e098f10eeddda0f7a977672117e19276
Comma Device <device@comma.ai>
2023-07-11 00:35:38 -0700

    Add fix test

--- system/sensord/rawgps/test_rawgps.py
+++ system/sensord/rawgps/test_rawgps.py
@@ -13,6 +13,8 @@ from selfdrive.manager.process_config import managed_processes
 
 
 GOOD_SIGNAL = bool(int(os.getenv("GOOD_SIGNAL", '0')))
+UPDATE_MS = 100
+UPDATES_PER_S = 1000//UPDATE_MS
 
 
 class TestRawgpsd(unittest.TestCase):
@@ -21,34 +23,35 @@ class TestRawgpsd(unittest.TestCase):
     if not TICI:
       raise unittest.SkipTest
 
-    cls.sm = messaging.SubMaster(['qcomGnss', 'gpsLocation', 'gnssMeasurements'])
+    cls.sm_qcom_gnss = messaging.SubMaster(['qcomGnss'])
+    cls.sm_gps_location = messaging.SubMaster(['gpsLocation'])
+    cls.sm_gnss_measurements = messaging.SubMaster(['gnssMeasurements'])
 
   def tearDown(self):
     managed_processes['rawgpsd'].stop()
 
   def _wait_for_output(self, t=10):
-    self.sm.update(0)
-    for __ in range(t*10):
-      self.sm.update(100)
-      if self.sm.updated['qcomGnss']:
-        break
-    return self.sm.updated['qcomGnss']
+    self.sm_qcom_gnss.update(0)
+    for __ in range(t*UPDATES_PER_S):
+      self.sm_qcom_gnss.update(UPDATE_MS)
+      if self.sm_qcom_gnss.updated['qcomGnss']:
+        return True
 
   def _wait_for_location(self, t=10):
-    self.sm.update(0)
-    for __ in range(t*10):
-      self.sm.update(100)
-      if self.sm.updated['gpsLocation']:
-        break
-    return self.sm.updated['gpsLocation']
+    self.sm_gps_location.update(0)
+    for __ in range(t*UPDATES_PER_S):
+      self.sm_gps_location.update(UPDATE_MS)
+      if self.sm_gps_location.updated['gpsLocation'] and self.sm_gps_location['gpsLocation'].flags:
+        return True
+    return False
 
   def _wait_for_laikad_location(self, t=10):
-    self.sm.update(0)
-    for __ in range(t*10):
-      self.sm.update(100)
-      if self.sm.updated['gnssMeasurements']:
-        break
-    return self.sm.updated['gnssMeasurements']
+    self.sm_gnss_measurements.update(0)
+    for __ in range(t*UPDATES_PER_S):
+      self.sm_gnss_measurements.update(UPDATE_MS)
+      if self.sm_gnss_measurements.updated['gnssMeasurements'] and self.sm_gnss_measurements['gnssMeasurements'].positionECEF.valid:
+        return True
+    return False
 
   def test_wait_for_modem(self):
     os.system("sudo systemctl stop ModemManager lte")
@@ -95,7 +98,7 @@ class TestRawgpsd(unittest.TestCase):
     assert valid_duration == "10080"  # should be max time
 
     # TODO: time doesn't match up
-    assert injected_date[1:].startswith(datetime.datetime.now().strftime("%Y/%m/%d"))
+    assert injected_date[1:].startswith(datetime.datetime.utcnow().strftime("%Y/%m/%d"))
 
   @unittest.skipIf(not GOOD_SIGNAL, "No good GPS signal")
   def test_fix(self):
@@ -103,16 +106,15 @@ class TestRawgpsd(unittest.TestCase):
     at_cmd("AT+QGPSDEL=0")
 
     managed_processes['rawgpsd'].start()
-    assert self._wait_for_location(30)
-    assert self.sm['gpsLocation'].flags == 1
+    #managed_processes['laikad'].start()
+    assert self._wait_for_location(120)
+    assert self.sm_gps_location['gpsLocation'].flags == 1
     #module_fix = [self.sm['gpsLocation'].latitude, self.sm['gpsLocation'].longitude, self.sm['gpsLocation'].altitude]
-    managed_processes['laikad'].start()
-    assert self._wait_for_laikad_location(10)
-    assert self.sm['gnssMeasurements'].flags == 1
-    print(self.sm['gnssMeasurements'].gnssMeasurements)
-    managed_processes['laikad'].stop()
+    #assert self._wait_for_laikad_location(90)
+    #assert self.sm['gnssMeasurements'].flags == 1
+    #print(self.sm_gnss_measurements['gnssMeasurements'])
+    #managed_processes['laikad'].stop()
     managed_processes['rawgpsd'].stop()
 
-    
 if __name__ == "__main__":
   unittest.main(failfast=True)
