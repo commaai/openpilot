4544b40b7077b9aa7447c8fee1d0b254397eec65
Dean Lee <deanlee3@gmail.com>
2023-07-11 17:23:45 +0800

    ui/map: don't clear eta when map is hidden (#28878)

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -557,10 +557,6 @@ void MapETA::paintEvent(QPaintEvent *event) {
 }
 
 void MapETA::updateETA(float s, float s_typical, float d) {
-  eta_doc.clear();
-  setVisible(d >= MANEUVER_TRANSITION_THRESHOLD);
-  if (!isVisible()) return;
-
   // ETA
   auto eta_t = QDateTime::currentDateTime().addSecs(s).time();
   auto eta = format_24h ? std::array{eta_t.toString("HH:mm"), tr("eta")}
@@ -581,5 +577,7 @@ void MapETA::updateETA(float s, float s_typical, float d) {
 
   eta_doc.setHtml(QString(R"(<body><b>%1</b>%2 <span style="color:%3"><b>%4</b>%5</span> <b>%6</b>%7</body>)")
                       .arg(eta[0], eta[1], color, remaining[0], remaining[1], distance[0], distance[1]));
+
+  setVisible(d >= MANEUVER_TRANSITION_THRESHOLD);
   update();
 }
