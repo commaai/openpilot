4267993cf8564ddf3da388ca21028553f1d7097e
Shane Smiskol <shane@smiskol.com>
2023-07-11 19:33:24 -0700

    need to check longitudinal here now too

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -551,7 +551,7 @@ class Controls:
           else:
             self.state = State.enabled
           self.current_alert_types.append(ET.ENABLE)
-          self.v_cruise_helper.initialize_v_cruise(CS, self.experimental_mode)
+          self.v_cruise_helper.initialize_v_cruise(CS, self.experimental_mode and self.CP.openpilotLongitudinalControl)
 
     # Check if openpilot is engaged and actuators are enabled
     self.enabled = self.state in ENABLED_STATES
