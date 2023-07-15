08bafa9aa3a1285bc3b4f2553744002bb08a435d
Shane Smiskol <shane@smiskol.com>
2023-07-14 20:37:22 -0700

    draft

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -135,11 +135,12 @@ void MapWindow::updateState(const UIState &s) {
   const SubMaster &sm = *(s.sm);
   update();
 
-  // update navigate on openpilot status
+  // update navigate on openpilot status and path color
   if (sm.updated("modelV2")) {
     bool nav_enabled = sm["modelV2"].getModelV2().getNavEnabled();
-    if (nav_enabled && !uiState()->scene.navigate_on_openpilot) {
-      emit requestVisible(true);  // Show map on rising edge of navigate on openpilot
+    if (nav_enabled != uiState()->scene.navigate_on_openpilot) {
+      if (nav_enabled) emit requestVisible(true);  // Show map on rising edge of navigate on openpilot
+      m_map->setPaintProperty("navLayer", "line-color", nav_enabled ? QColor("#31ee73") : QColor("#31a1ee"));
     }
     uiState()->scene.navigate_on_openpilot = nav_enabled;
   }
