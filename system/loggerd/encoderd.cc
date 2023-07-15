e683906ed6005bcdfb1d1a5c33ac9b2dbdd24be2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-07-15 12:40:22 -0700

    Update system/loggerd/encoderd.cc

--- system/loggerd/encoderd.cc
+++ system/loggerd/encoderd.cc
@@ -60,8 +60,8 @@ void encoder_thread(EncoderdState *s, const LogCameraInfo &cam_info) {
     // init encoders
     if (encoders.empty()) {
       VisionBuf buf_info = vipc_client.buffers[0];
-      assert(buf_info.width > 0 && buf_info.height > 0);
       LOGW("encoder %s init %dx%d", cam_info.thread_name, buf_info.width, buf_info.height);
+      assert(buf_info.width > 0 && buf_info.height > 0);
 
       for (const auto &encoder_info : cam_info.encoder_infos) {
         encoders.push_back(new Encoder(encoder_info, buf_info.width, buf_info.height));
