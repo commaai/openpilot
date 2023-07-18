181f4abfe6e3a62997f2f7b8573ba605405668fe
Justin Newberry <justin@comma.ai>
2023-07-18 15:35:21 -0700

    minimal diff

--- common/time.py
+++ common/time.py
@@ -2,5 +2,5 @@ import datetime
 
 MIN_DATE = datetime.datetime(year=2023, month=6, day=1)
 
-def valid_datetime(d: datetime.datetime):
-  return d > MIN_DATE
+def valid_system_time():
+  return datetime.datetime.now() > MIN_DATE
