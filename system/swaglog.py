6cbdfbb1fdbf675926344ce15acad787b23c50c0
Bruce Wayne <harald.the.engineer@gmail.com>
2023-07-17 22:40:33 -0700

    suppress before reconnecting in thread

--- system/swaglog.py
+++ system/swaglog.py
@@ -10,9 +10,6 @@ import zmq
 from common.logging_extra import SwagLogger, SwagFormatter, SwagLogFileFormatter
 from system.hardware import PC
 
-# TODO suppresses warning about forking proc with zmq socket, fix root cause
-warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<zmq.*>")
-
 if PC:
   SWAGLOG_DIR = os.path.join(str(Path.home()), ".comma", "log")
 else:
@@ -94,6 +91,8 @@ class UnixDomainSocketHandler(logging.Handler):
 
   def emit(self, record):
     if os.getpid() != self.pid:
+      # TODO suppresses warning about forking proc with zmq socket, fix root cause
+      warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<zmq.*>")
       self.connect()
 
     msg = self.format(record).rstrip('\n')
