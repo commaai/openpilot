2cc54032e01aaa868be981b342544e7cfcddd40a
Shane Smiskol <shane@smiskol.com>
2023-07-17 19:52:23 -0700

    lower case company lcc

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -143,7 +143,7 @@ void MapWindow::updateState(const UIState &s) {
   if (sm.updated("modelV2")) {
     bool nav_enabled = sm["modelV2"].getModelV2().getNavEnabled();
     if (nav_enabled != uiState()->scene.navigate_on_openpilot) {
-      // Set path color on change, and show map on rising edge of navigate on openpilot
+      // set path color on change, and show map on rising edge of navigate on openpilot
       if (loaded_once) {
         m_map->setPaintProperty("navLayer", "line-color", getNavPathColor(nav_enabled));
       }
