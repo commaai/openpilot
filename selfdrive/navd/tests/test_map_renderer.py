04aa3d825ebe2b4c36b882893a9d499be053e7ff
Justin Newberry <justin@comma.ai>
2023-07-19 17:08:47 -0700

    cleanup test

--- selfdrive/navd/tests/test_map_renderer.py
+++ selfdrive/navd/tests/test_map_renderer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+from typing import Any
 import unittest
 import time
 
@@ -10,9 +11,9 @@ from selfdrive.manager.process_config import managed_processes
 LLK_DECIMATION = 10
 CACHE_PATH = "/data/mbgl-cache-navd.db"
 
-def gen_llk():
+def gen_llk(location):
   msg = messaging.new_message('liveLocationKalman')
-  msg.liveLocationKalman.positionGeodetic = {'value': [32.7174, -117.16277, 0], 'std': [0., 0., 0.], 'valid': True}
+  msg.liveLocationKalman.positionGeodetic = {'value': [*location, 0], 'std': [0., 0., 0.], 'valid': True}
   msg.liveLocationKalman.calibratedOrientationNED = {'value': [0., 0., 0.], 'std': [0., 0., 0.], 'valid': True}
   msg.liveLocationKalman.status = 'valid'
   return msg
@@ -21,34 +22,44 @@ import requests
 import threading
 import http.server
 
+
 class MapBoxInternetDisabledRequestHandler(http.server.BaseHTTPRequestHandler):
   INTERNET_ACTIVE = True
 
+  def setup(self):
+    if self.INTERNET_ACTIVE:
+      super().setup()
+  
+  def handle(self):
+    if self.INTERNET_ACTIVE:
+      super().handle()
+
+  def finish(self):
+    if self.INTERNET_ACTIVE:
+      super().finish()
+    
   def do_GET(self):
     url = f'https://api.mapbox.com{self.path}'
 
-    print(url, self.INTERNET_ACTIVE)
+    #token_regex = r'access_token=([\d\w.]*)'
+    #url = re.sub(token_regex, "access_token=invalid_token", url)
 
-    if self.INTERNET_ACTIVE:
-      headers = dict(self.headers)
-      headers["Host"] = "api.mapbox.com"
+    headers = dict(self.headers)
+    headers["Host"] = "api.mapbox.com"
 
-      r = requests.get(url, headers=headers, timeout=5)
+    r = requests.get(url, headers=headers, timeout=5)
 
-      if r.status_code != 200:
-        print(r.content)
-        print(r.headers)
-        print(headers)
+    self.send_response(r.status_code)
+    
+    self.end_headers()
 
-      self.send_response(r.status_code)
-      for key in r.headers:
-        self.send_header(key, r.headers[key])
+    self.wfile.write(r.content)
+  
+  def log_message(self, *args: Any) -> None:
+    return
 
-      self.end_headers()
-      self.wfile.write(r.content)
-
-    else:
-      self.send_response(404, "404 page not found (internet disabled)")
+  def log_error(self, *args: Any) -> None:
+    return
 
 
 class MapBoxInternetDisabledServer(threading.Thread):
@@ -59,6 +70,9 @@ class MapBoxInternetDisabledServer(threading.Thread):
   def stop(self):
     self.server.shutdown()
 
+LOCATION1 = (32.7174, -117.16277)
+LOCATION2 = (32.7558, -117.2037)
+
 
 class TestMapRenderer(unittest.TestCase):
   server = MapBoxInternetDisabledServer()
@@ -66,9 +80,10 @@ class TestMapRenderer(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
     assert "MAPBOX_TOKEN" in os.environ
+    cls.original_token = os.environ["MAPBOX_TOKEN"]
     cls.server.start()
-    time.sleep(1) # wait for server to be setup
-  
+    time.sleep(2) # wait for server to be fully started
+
   @classmethod
   def tearDownClass(cls) -> None:
     cls.server.stop()
@@ -80,14 +95,15 @@ class TestMapRenderer(unittest.TestCase):
 
     if os.path.exists(CACHE_PATH):
       os.remove(CACHE_PATH)
+    
+    self.location = LOCATION1
   
   def tearDown(self):
     managed_processes['mapsd'].stop()
   
   def _setup_test(self):
-    # start + sync up
     os.environ['MAPS_HOST'] = 'http://localhost:5000'
-
+    # start + sync up
     managed_processes['mapsd'].start()
 
     assert self.pm.wait_for_readers_to_update("liveLocationKalman", 10)
@@ -97,36 +113,35 @@ class TestMapRenderer(unittest.TestCase):
     self.vipc.recv()
   
   def _run_test(self, expect_valid):
-    self._setup_test()
-    self.__run_test(expect_valid)
+    starting_frame_id = None
 
-  def __run_test(self, expect_valid):
     # run test
-    prev_frame_id = -1
     for i in range(30*LLK_DECIMATION):
       frame_expected = (i+1) % LLK_DECIMATION == 0
 
       if self.sm.logMonoTime['mapRenderState'] == 0:
-        prev_valid = False
         prev_frame_id = -1
       else:
         prev_frame_id = self.sm['mapRenderState'].frameId
-        prev_valid = self.sm.valid['mapRenderState']
+      
+      if starting_frame_id is None:
+        starting_frame_id = prev_frame_id
 
-      llk = gen_llk()
+      llk = gen_llk(self.location)
       self.pm.send("liveLocationKalman", llk)
       self.pm.wait_for_readers_to_update("liveLocationKalman", 10)
       self.sm.update(1000 if frame_expected else 0)
       assert self.sm.updated['mapRenderState'] == frame_expected, "renderer running at wrong frequency"
 
       if not frame_expected:
-
         continue
+
+      frames_since_test_start = self.sm['mapRenderState'].frameId - starting_frame_id
 
       # give a few frames to go valid
-      if expect_valid and not self.sm.valid['mapRenderState'] and not prev_valid and self.sm['mapRenderState'].frameId < 5:
+      if frames_since_test_start < 5:
         continue
-
+        
       # check output
       assert self.sm.valid['mapRenderState'] == expect_valid
       assert self.sm['mapRenderState'].frameId == (prev_frame_id + 1)
@@ -141,37 +156,45 @@ class TestMapRenderer(unittest.TestCase):
       assert self.vipc.valid == expect_valid
       assert self.vipc.timestamp_sof == llk.logMonoTime
       assert self.vipc.frame_id == self.sm['mapRenderState'].frameId
-  
+
   def disable_internet(self):
     MapBoxInternetDisabledRequestHandler.INTERNET_ACTIVE = False
+    print("internet disabled")
   
   def enable_internet(self):
     MapBoxInternetDisabledRequestHandler.INTERNET_ACTIVE = True
+    print("internet enabled")
 
   def test_with_internet(self):
+    self.location = LOCATION1
     self.enable_internet()
+    self._setup_test()
     self._run_test(True)
 
   def test_with_no_internet(self):
-    try:
-      self.disable_internet()
-      self._run_test(False)
-    finally:
-      self.enable_internet()
+    self.location = LOCATION1
+    self.disable_internet()
+    self._setup_test()
+    self._run_test(False)
     
   def test_recover_from_no_internet(self):
+    self.location = LOCATION1
     self.enable_internet()
     self._setup_test()
-
-    self.__run_test(True)
+    
+    self._run_test(True)
 
     self.disable_internet()
-    time.sleep(2)
-    self.__run_test(False)
+
+    # change locations to force mapsd to refetch
+    self.location = LOCATION2
+    self._run_test(False)
 
     self.enable_internet()
-    time.sleep(2)
-    self.__run_test(True)
+    self._run_test(True)
+
+    self.location = LOCATION1
+    self._run_test(True)
 
 if __name__ == "__main__":
   unittest.main()
