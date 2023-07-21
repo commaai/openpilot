c25d0000d35c710135b776702756e6a5c672970b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-07-20 18:59:24 -0700

    keep ignore msgs

--- selfdrive/test/process_replay/compare_logs.py
+++ selfdrive/test/process_replay/compare_logs.py
@@ -59,6 +59,11 @@ def compare_logs(log1, log2, ignore_fields=None, ignore_msgs=None, tolerance=Non
     field_tolerances = {}
   default_tolerance = EPSILON if tolerance is None else tolerance
 
+  log1, log2 = (
+    [m for m in log if m.which() not in ignore_msgs]
+    for log in (log1, log2)
+  )
+
   if len(log1) != len(log2):
     cnt1 = Counter(m.which() for m in log1)
     cnt2 = Counter(m.which() for m in log2)
