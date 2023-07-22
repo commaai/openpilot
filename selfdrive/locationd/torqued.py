b3a099ae5e860c30794ea1fdda54903c5658c7a5
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-07-21 17:06:50 -0700

    torqued: fix cache loading

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -139,8 +139,8 @@ class TorqueEstimator:
     torque_cache = params.get("LiveTorqueParameters")
     if params_cache is not None and torque_cache is not None:
       try:
-        with log.Event.from_bytes(torque_cache).liveTorqueParameters as log_evt:
-          cache_ltp = log_evt
+        with log.Event.from_bytes(torque_cache) as log_evt:
+          cache_ltp = log_evt.liveTorqueParameters
         with car.CarParams.from_bytes(params_cache) as msg:
           cache_CP = msg
         if self.get_restore_key(cache_CP, cache_ltp.version) == self.get_restore_key(CP, VERSION):
