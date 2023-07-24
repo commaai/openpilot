982f42ce9869e7b99446fb73a771703bf5d3213b
Shane Smiskol <shane@smiskol.com>
2023-07-23 18:34:35 -0700

    disable_ecu: support sub-addresses (#29112)
    allow sub_addrs
    old-commit-hash: a31b1dcf16857ac94ce337fd5b6e1b4ab6b7d991

--- selfdrive/car/disable_ecu.py
+++ selfdrive/car/disable_ecu.py
@@ -7,22 +7,22 @@ EXT_DIAG_RESPONSE = b'\x50\x03'
 COM_CONT_RESPONSE = b''
 
 
-def disable_ecu(logcan, sendcan, bus=0, addr=0x7d0, com_cont_req=b'\x28\x83\x01', timeout=0.1, retry=10, debug=False):
+def disable_ecu(logcan, sendcan, bus=0, addr=0x7d0, sub_addr=None, com_cont_req=b'\x28\x83\x01', timeout=0.1, retry=10, debug=False):
   """Silence an ECU by disabling sending and receiving messages using UDS 0x28.
   The ECU will stay silent as long as openpilot keeps sending Tester Present.
 
   This is used to disable the radar in some cars. Openpilot will emulate the radar.
   WARNING: THIS DISABLES AEB!"""
-  cloudlog.warning(f"ecu disable {hex(addr)} ...")
+  cloudlog.warning(f"ecu disable {hex(addr), sub_addr} ...")
 
   for i in range(retry):
     try:
-      query = IsoTpParallelQuery(sendcan, logcan, bus, [addr], [EXT_DIAG_REQUEST], [EXT_DIAG_RESPONSE], debug=debug)
+      query = IsoTpParallelQuery(sendcan, logcan, bus, [(addr, sub_addr)], [EXT_DIAG_REQUEST], [EXT_DIAG_RESPONSE], debug=debug)
 
       for _, _ in query.get_data(timeout).items():
         cloudlog.warning("communication control disable tx/rx ...")
 
-        query = IsoTpParallelQuery(sendcan, logcan, bus, [addr], [com_cont_req], [COM_CONT_RESPONSE], debug=debug)
+        query = IsoTpParallelQuery(sendcan, logcan, bus, [(addr, sub_addr)], [com_cont_req], [COM_CONT_RESPONSE], debug=debug)
         query.get_data(0)
 
         cloudlog.warning("ecu disabled")
