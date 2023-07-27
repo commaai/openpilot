63d3075adbd1957817f2bbbccccd83d2e60642aa
Kacper Rączy <gfw.kra@gmail.com>
2023-07-27 14:43:29 -0700

    Add comment about universal binaries to build script

--- third_party/libyuv/build.sh
+++ third_party/libyuv/build.sh
@@ -5,3 +5,8 @@ git clone https://chromium.googlesource.com/libyuv/libyuv
 cd libyuv
 git reset --hard 4a14cb2e81235ecd656e799aecaaf139db8ce4a2
 cmake .
+
+## To create universal binary on Darwin:
+## ```
+## lipo -create -output Darwin/libyuv.a path-to-x64/libyuv.a path-to-arm64/libyuv.a
+## ```
