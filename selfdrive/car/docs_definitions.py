1e1cc638d8e9fa408a02a57d689c13c2219d823d
Justin Newberry <justin@comma.ai>
2023-08-01 00:06:21 -0700

    Car Docs: (optionally) Display a list of tools (#28782)
    * display tools
    
    * 5/16 also works
    
    * cleanup
    
    * static fix
    
    * fix static analysis
    
    * merge
    
    * just use basepart
    
    * use filter

--- selfdrive/car/docs_definitions.py
+++ selfdrive/car/docs_definitions.py
@@ -131,6 +131,11 @@ class Kit(EnumBase):
   red_panda_kit = BasePart("CAN FD panda kit", parts=[Device.red_panda, Accessory.harness_box, Cable.usb_a_2_a_cable, Cable.usbc_otg_cable, Cable.obd_c_cable_1_5ft])
 
 
+class Tool(EnumBase):
+  socket_8mm_deep = BasePart("Socket Wrench 8mm or 5/16 (deep)")
+  pry_tool = BasePart("Pry Tool")
+
+
 class PartType(Enum):
   accessory = Accessory
   cable = Cable
@@ -138,10 +143,9 @@ class PartType(Enum):
   device = Device
   kit = Kit
   mount = Mount
-
+  tool = Tool
 
 DEFAULT_CAR_PARTS: List[EnumBase] = [Device.three]
-
 
 @dataclass
 class CarParts:
@@ -268,9 +272,16 @@ class CarInfo:
     if self.car_parts.parts:
       model_years = self.model + (' ' + self.years if self.years else '')
       buy_link = f'<a href="https://comma.ai/shop/comma-three.html?make={self.make}&model={model_years}">Buy Here</a>'
-      car_parts_docs = self.car_parts.all_parts()
-      parts = '<br>'.join([f"- {car_parts_docs.count(part)} {part.value.name}" for part in sorted(set(car_parts_docs), key=lambda part: str(part.value.name))])
-      hardware_col = f'<details><summary>View</summary><sub>{parts}<br>{buy_link}</sub></details>'
+
+      tools_docs = list(filter(lambda part: type(part) is Tool, self.car_parts.all_parts()))
+      parts_docs = list(filter(lambda part: type(part) is not Tool, self.car_parts.all_parts()))
+
+      def display_func(parts):
+        return '<br>'.join([f"- {parts.count(part)} {part.value.name}" for part in sorted(set(parts), key=lambda part: str(part.value.name))])
+      
+      hardware_col = f'<details><summary>Parts</summary><sub>{display_func(parts_docs)}<br>{buy_link}</sub></details>'
+      if len(tools_docs):
+        hardware_col += f'<details><summary>Tools</summary><sub>{display_func(tools_docs)}</sub></details>'
 
     self.row: Dict[Enum, Union[str, Star]] = {
       Column.MAKE: self.make,
