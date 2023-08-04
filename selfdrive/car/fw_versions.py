082fe6868d3833923ff9b0b73b3fe703ef43f366
Shane Smiskol <shane@smiskol.com>
2023-08-04 16:10:49 -0700

    FPv2: fast fuzzy fingerprinting (#29233)
    * fast fuzzy fingerprinting
    
    * use match_fw_to_car helper
    
    * fix and comment
    
    * alone
    
    * Update selfdrive/car/fw_versions.py
    
    * Update selfdrive/car/fw_versions.py

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -242,8 +242,9 @@ def get_fw_versions_ordered(logcan, sendcan, ecu_rx_addrs, timeout=0.1, num_pand
 
     car_fw = get_fw_versions(logcan, sendcan, query_brand=brand, timeout=timeout, num_pandas=num_pandas, debug=debug, progress=progress)
     all_car_fw.extend(car_fw)
-    # Try to match using FW returned from this brand only
-    matches = match_fw_to_car_exact(build_fw_dict(car_fw))
+
+    # If there is a match using this brand's FW alone, finish querying early
+    _, matches = match_fw_to_car(car_fw, log=False)
     if len(matches) == 1:
       break
 
