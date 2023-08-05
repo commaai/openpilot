fd30ff13f0d54c350cd8507b8397f3344bbae15c
Kacper Rączy <gfw.kra@gmail.com>
2023-08-04 18:23:00 -0700

    Revert "radard: tie radard frequency to modelV2 (#29063)"
    This reverts commit 255f5c13f2d8c6796fd960b248856f11a3c1b05a.

--- selfdrive/car/ford/radar_interface.py
+++ selfdrive/car/ford/radar_interface.py
@@ -62,7 +62,7 @@ class RadarInterface(RadarInterfaceBase):
 
   def update(self, can_strings):
     if self.rcp is None:
-      return None
+      return super().update(None)
 
     vls = self.rcp.update_strings(can_strings)
     self.updated_messages.update(vls)
