89a7e5c7566a30d9ef86e80b10893deffb67bf66
Bruce Wayne <harald.the.engineer@gmail.com>
2023-08-05 06:56:28 -0700

    Revert "[commabody] Add new body teleop ui (#29119)"
    This reverts commit a7304d059cb2f4e57e64fcbabb4f9079f1ea659d.

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -72,7 +72,7 @@ procs = [
 
   # debug procs
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], onroad=False, callback=notcar),
-  PythonProcess("webjoystick", "tools.bodyteleop.web", onroad=False, callback=notcar),
+  PythonProcess("webjoystick", "tools.joystick.web", onroad=False, callback=notcar),
 ]
 
 managed_processes = {p.name: p for p in procs}
