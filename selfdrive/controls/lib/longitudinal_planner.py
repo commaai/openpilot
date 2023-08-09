ab5798c4648bd35c42cb0cb13d3e78764e311385
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-08-09 14:32:31 -0700

    Update selfdrive/controls/lib/longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -110,7 +110,8 @@ class LongitudinalPlanner:
       accel_limits = [A_CRUISE_MIN, get_max_accel(v_ego)]
       accel_limits_turns = limit_accel_in_turns(v_ego, sm['carState'].steeringAngleDeg, accel_limits, self.CP)
     else:
-      accel_limits = accel_limits_turns = [ACCEL_MIN, ACCEL_MAX]
+      accel_limits = [ACCEL_MIN, ACCEL_MAX]
+      accel_limits_turns = [ACCEL_MIN, ACCEL_MAX]
 
     if reset_state:
       self.v_desired_filter.x = v_ego
