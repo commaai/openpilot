a23177a3b57cf5c3fe0c479164248ed637bfc6b3
Dean Lee <deanlee3@gmail.com>
2023-08-11 17:57:44 +0800

    Update selfdrive/ui/qt/maps/map.cc
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -213,7 +213,7 @@ void MapWindow::updateState(const UIState &s) {
     // - API exception/no internet
     // - route response is empty
     // - any time navd is waiting for recompute_countdown
-    routing_problem = !sm.valid("navInstruction") && coordinate_from_param("NavDestination");
+    routing_problem = !sm.valid("navInstruction") && coordinate_from_param("NavDestination").has_value();
 
     if (sm.valid("navInstruction")) {
       auto i = sm["navInstruction"].getNavInstruction();
