5b29c3e502ec1bc070289249bb87c0713d179579
Shane Smiskol <shane@smiskol.com>
2023-08-11 02:34:15 -0700

    add todo

--- selfdrive/ui/qt/maps/map_instructions.cc
+++ selfdrive/ui/qt/maps/map_instructions.cc
@@ -111,6 +111,7 @@ void MapInstructions::updateInstructions(cereal::NavInstruction::Reader instruct
     bool active = lanes[i].getActive();
     const auto active_direction = lanes[i].getActiveDirection();
 
+    // TODO: Make more images based on active direction and combined directions
     QString fn = "lane_direction_";
     if (active_direction != cereal::NavInstruction::Direction::NONE) {
       fn += "turn_" + DIRECTIONS[active_direction];
