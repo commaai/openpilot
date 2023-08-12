d23ce1b4d923fc550785573dfda961a219a8c156
Marcus Millspaugh <137620142+spawahh@users.noreply.github.com>
2023-08-12 16:10:01 -0700

    Update values.py

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -277,6 +277,7 @@ FW_VERSIONS = {
       b'\x00\x00eq\x1f@ "',
       b'\x00\x00eq\x00\x00\x00\x00',
       b'\x00\x00e\x8f\x00\x00\x00\x00',
+      b'\xf1\x00\xac\x03\x00',
     ],
     (Ecu.engine, 0x7e0, None): [
       b'\xca!ap\a',
@@ -290,6 +291,7 @@ FW_VERSIONS = {
       b'\xe6!fp\x07',
       b'\xf3"fp\x07',
       b'\xe6"f0\x07',
+      b'\xe6"fp\x07',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xe6\xf5\004\000\000',
