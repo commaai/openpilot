2e0e57f126028d39413997b23be27c2e42ec35a1
deanlee <deanlee3@gmail.com>
2023-08-15 00:55:52 +0800

    use util::random_int

--- common/tests/test_ratekeeper.cc
+++ common/tests/test_ratekeeper.cc
@@ -4,19 +4,12 @@
 #include "common/timing.h"
 #include "common/util.h"
 
-int random_int(int min, int max) {
-  std::random_device dev;
-  std::mt19937 rng(dev());
-  std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
-  return dist(rng);
-}
-
 TEST_CASE("RateKeeper") {
   float freq = GENERATE(10, 50, 100);
   RateKeeper rk("Test RateKeeper", freq);
   for (int i = 0; i < freq; ++i) {
     double begin = seconds_since_boot();
-    util::sleep_for(random_int(0, 1000.0 / freq - 1));
+    util::sleep_for(util::random_int(0, 1000.0 / freq - 1));
     bool lagged = rk.keepTime();
     REQUIRE(std::abs(seconds_since_boot() - begin - (1 / freq)) < 1e-3);
     REQUIRE(lagged == false);
