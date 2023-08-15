0221eafaa09ea3f7728c156c911da61c05af5f00
Shane Smiskol <shane@smiskol.com>
2023-08-15 16:42:29 -0700

    can do all this manually

--- selfdrive/debug/check_can_parser_performance.py
+++ selfdrive/debug/check_can_parser_performance.py
@@ -7,6 +7,8 @@ from cereal import car
 from selfdrive.car.tests.routes import CarTestRoute
 from selfdrive.car.tests.test_models import TestCarModelBase
 from tools.plotjuggler.juggle import DEMO_ROUTE
+from selfdrive.car.car_helpers import FRAME_FINGERPRINT, interfaces
+from selfdrive.car.toyota.values import CAR as TOYOTA
 
 N_RUNS = 5
 
@@ -20,6 +22,10 @@ if __name__ == '__main__':
   tm = CarModelTestCase()
   tm.setUpClass()
 
+  CarInterface, CarController, CarState = interfaces[TOYOTA.COROLLA_TSS2]
+  CP = CarInterface.get_non_essential_params(TOYOTA.COROLLA_TSS2)
+  CI = CarInterface(CP, CarController, CarState)
+
   CC = car.CarControl.new_message()
 
   ets = []
@@ -27,8 +33,10 @@ if __name__ == '__main__':
     tm.setUp()
 
     start_t = time.process_time_ns()
-    for i, msg in enumerate(tm.can_msgs):
-      CS = tm.CI.update(CC, (msg.as_builder().to_bytes(),))
+    for msg in enumerate(tm.can_msgs):
+      for cp in CI.can_parsers:
+        if cp is not None:
+          cp.update_strings((msg.as_builder().to_bytes(),))
     ets.append((time.process_time_ns() - start_t) * 1e-6)
 
   print(f'{len(tm.can_msgs)} CAN packets, {N_RUNS} runs')
