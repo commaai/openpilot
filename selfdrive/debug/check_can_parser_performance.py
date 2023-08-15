33f68ce1989f40238a6c9aa97cfc6fae10c55b2a
Shane Smiskol <shane@smiskol.com>
2023-08-15 16:44:17 -0700

    but this is way simpler

--- selfdrive/debug/check_can_parser_performance.py
+++ selfdrive/debug/check_can_parser_performance.py
@@ -7,8 +7,6 @@ from cereal import car
 from selfdrive.car.tests.routes import CarTestRoute
 from selfdrive.car.tests.test_models import TestCarModelBase
 from tools.plotjuggler.juggle import DEMO_ROUTE
-from selfdrive.car.car_helpers import FRAME_FINGERPRINT, interfaces
-from selfdrive.car.toyota.values import CAR as TOYOTA
 
 N_RUNS = 5
 
@@ -21,20 +19,14 @@ class CarModelTestCase(TestCarModelBase):
 if __name__ == '__main__':
   tm = CarModelTestCase()
   tm.setUpClass()
-
-  CarInterface, CarController, CarState = interfaces[TOYOTA.COROLLA_TSS2]
-  CP = CarInterface.get_non_essential_params(TOYOTA.COROLLA_TSS2)
-  CI = CarInterface(CP, CarController, CarState)
+  tm.setUp()
 
   CC = car.CarControl.new_message()
-
   ets = []
   for _ in tqdm(range(N_RUNS)):
-    tm.setUp()
-
     start_t = time.process_time_ns()
-    for msg in enumerate(tm.can_msgs):
-      for cp in CI.can_parsers:
+    for msg in tm.can_msgs:
+      for cp in tm.CI.can_parsers:
         if cp is not None:
           cp.update_strings((msg.as_builder().to_bytes(),))
     ets.append((time.process_time_ns() - start_t) * 1e-6)
