486f7b07338fd5225ba9080a0b7b40f4007051e9
Shane Smiskol <shane@smiskol.com>
2023-08-15 16:23:48 -0700

    clean up

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -196,19 +196,7 @@ class TestCarModelBase(unittest.TestCase):
       # wait max of 2s for low frequency msgs to be seen
       if i > 200 or can_valid:
         can_invalid_cnt += not CS.canValid
-
     self.assertEqual(can_invalid_cnt, 0)
-    self.assertInterfaceTiming((time.process_time_ns() - start_time) * 1e-6)
-
-  def assertInterfaceTiming(self, total_time_ms):
-    """Asserts sane timing"""
-    total_time_thresh = (100, 7000)
-    per_packet_thresh = (0.10, 0.9)
-
-    # avg_time_msg = total_time_ms / len(self.can_msgs)
-    # for t, thresh in ((total_time_ms, total_time_thresh), (avg_time_msg, per_packet_thresh)):
-    #   self.assertLess(t, thresh[1])
-    #   self.assertGreater(t, thresh[0], "Performance seems to have improved, update test refs.")
 
   def test_radar_interface(self):
     os.environ['NO_RADAR_SLEEP'] = "1"
--- selfdrive/debug/check_car_interface_performance.py
+++ selfdrive/debug/check_car_interface_performance.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 import time
 from tqdm import tqdm
@@ -10,8 +11,7 @@ N_RUNS = 5
 
 
 class TestCarModelDemo(TestCarModelBase):
-  car_model = None
-  test_route = CarTestRoute(DEMO_ROUTE, 'TOYOTA COROLLA TSS2 2019')
+  test_route = CarTestRoute(DEMO_ROUTE, None)
   ci = False
 
 
@@ -23,9 +23,11 @@ if __name__ == '__main__':
   for _ in tqdm(range(N_RUNS)):
     tm.setUp()
 
+    # CarInterface.update includes CAN parsing and CarState.update
     start_t = time.process_time_ns()
     tm.test_car_interface()
     ets.append((time.process_time_ns() - start_t) * 1e-6)
+
   print(f'{len(tm.can_msgs)} CAN packets, {N_RUNS} runs')
   print(f'{np.mean(ets):.2f} mean ms, {max(ets):.2f} max ms, {min(ets):.2f} min ms, {np.std(ets):.2f} std ms')
   print(f'{np.mean(ets) / len(tm.can_msgs):.4f} mean ms / CAN packet')
