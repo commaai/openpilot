ec16925df93ecd7f7104ac7ce811c17f674101ac
Shane Smiskol <shane@smiskol.com>
2023-08-15 19:44:44 -0700

    fix mypy

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # pylint: disable=E1101
+import capnp
 import os
 import importlib
 import unittest
@@ -66,9 +67,11 @@ SKIP_ENV_VAR = "SKIP_LONG_TESTS"
 
 
 class TestCarModelBase(unittest.TestCase):
-  car_model = None
-  test_route = None
-  ci = True
+  car_model: Optional[str] = None
+  test_route: CarTestRoute = None
+  ci: bool = True
+
+  can_msgs: List[capnp.lib.capnp._DynamicStructReader]
 
   @unittest.skipIf(SKIP_ENV_VAR in os.environ, f"Long running test skipped. Unset {SKIP_ENV_VAR} to run")
   @classmethod
