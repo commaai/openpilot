3563292e6538a9aaf98800e28f9a6557994d8968
Shane Smiskol <shane@smiskol.com>
2023-08-17 16:26:30 -0700

    test_models: remove body standstill exception (#29453)
    * remove body exception
    
    * bump to master

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -322,9 +322,7 @@ class TestCarModelBase(unittest.TestCase):
       # TODO: check rest of panda's carstate (steering, ACC main on, etc.)
 
       checks['gasPressed'] += CS.gasPressed != self.safety.get_gas_pressed_prev()
-      if self.CP.carName not in ("body",):
-        # TODO: fix standstill mismatches for other makes
-        checks['standstill'] += CS.standstill == self.safety.get_vehicle_moving()
+      checks['standstill'] += CS.standstill == self.safety.get_vehicle_moving()
 
       # TODO: remove this exception once this mismatch is resolved
       brake_pressed = CS.brakePressed
