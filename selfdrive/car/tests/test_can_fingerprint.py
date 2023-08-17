ec4d82d8854e04bc73aa5219fc6499a5901348ac
Shane Smiskol <shane@smiskol.com>
2023-08-16 19:51:19 -0700

    comment

--- selfdrive/car/tests/test_can_fingerprint.py
+++ selfdrive/car/tests/test_can_fingerprint.py
@@ -10,14 +10,16 @@ from selfdrive.car.fingerprints import _FINGERPRINTS as FINGERPRINTS
 class TestCanFingerprint(unittest.TestCase):
   @parameterized.expand([(c, f) for c, f in FINGERPRINTS.items()])
   def test_can_fingerprint(self, car_model, fingerprints):
+    # Tests online fingerprinting function on offline fingerprints
     for fingerprint in fingerprints:  # can have multiple fingerprints for each platform
       can = messaging.new_message('can', 1)
       can.can = [log.CanData(address=address, dat=b'\x00' * length)
                  for address, length in fingerprint.items()]
-      fingerprint_iter = iter([can])
 
+      fingerprint_iter = iter([can])
       empty_can = messaging.new_message('can', 0)
       car_fingerprint, finger = can_fingerprint(lambda: next(fingerprint_iter, empty_can))
+
       self.assertEqual(car_fingerprint, car_model)
 
 
