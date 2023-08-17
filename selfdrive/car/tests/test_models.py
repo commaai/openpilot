99a61accfa12b41c2056e4523ed452bc4f020f86
Shane Smiskol <shane@smiskol.com>
2023-08-16 23:53:37 -0700

    undo

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -323,7 +323,7 @@ class TestCarModelBase(unittest.TestCase):
 
       checks['gasPressed'] += CS.gasPressed != self.safety.get_gas_pressed_prev()
       if self.CP.carName not in ("body",):
-        # # TODO: fix standstill mismatches for other makes
+        # TODO: fix standstill mismatches for other makes
         checks['standstill'] += CS.standstill == self.safety.get_vehicle_moving()
 
       # TODO: remove this exception once this mismatch is resolved
