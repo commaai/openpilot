be43fe788268a7018f2f9bd0f03d7e691f66250f
Shane Smiskol <shane@smiskol.com>
2023-08-17 20:24:39 -0700

    CivicParams -> ReferenceCarParams

--- selfdrive/car/__init__.py
+++ selfdrive/car/__init__.py
@@ -42,7 +42,7 @@ def gen_empty_fingerprint():
 
 # FIXME: hardcoding honda civic 2016 touring params so they can be used to
 # scale unknown params for other cars
-class CivicParams:
+class ReferenceCarParams:
   MASS = 1326. + STD_CARGO_KG
   WHEELBASE = 2.70
   CENTER_TO_FRONT = WHEELBASE * 0.4
@@ -55,18 +55,18 @@ class CivicParams:
 # TODO: get actual value, for now starting with reasonable value for
 # civic and scaling by mass and wheelbase
 def scale_rot_inertia(mass, wheelbase):
-  return CivicParams.ROTATIONAL_INERTIA * mass * wheelbase ** 2 / (CivicParams.MASS * CivicParams.WHEELBASE ** 2)
+  return ReferenceCarParams.ROTATIONAL_INERTIA * mass * wheelbase ** 2 / (ReferenceCarParams.MASS * ReferenceCarParams.WHEELBASE ** 2)
 
 
 # TODO: start from empirically derived lateral slip stiffness for the civic and scale by
 # mass and CG position, so all cars will have approximately similar dyn behaviors
 def scale_tire_stiffness(mass, wheelbase, center_to_front, tire_stiffness_factor):
   center_to_rear = wheelbase - center_to_front
-  tire_stiffness_front = (CivicParams.TIRE_STIFFNESS_FRONT * tire_stiffness_factor) * mass / CivicParams.MASS * \
-                         (center_to_rear / wheelbase) / (CivicParams.CENTER_TO_REAR / CivicParams.WHEELBASE)
+  tire_stiffness_front = (ReferenceCarParams.TIRE_STIFFNESS_FRONT * tire_stiffness_factor) * mass / ReferenceCarParams.MASS * \
+                         (center_to_rear / wheelbase) / (ReferenceCarParams.CENTER_TO_REAR / ReferenceCarParams.WHEELBASE)
 
-  tire_stiffness_rear = (CivicParams.TIRE_STIFFNESS_REAR * tire_stiffness_factor) * mass / CivicParams.MASS * \
-                        (center_to_front / wheelbase) / (CivicParams.CENTER_TO_FRONT / CivicParams.WHEELBASE)
+  tire_stiffness_rear = (ReferenceCarParams.TIRE_STIFFNESS_REAR * tire_stiffness_factor) * mass / ReferenceCarParams.MASS * \
+                        (center_to_front / wheelbase) / (ReferenceCarParams.CENTER_TO_FRONT / ReferenceCarParams.WHEELBASE)
 
   return tire_stiffness_front, tire_stiffness_rear
 
