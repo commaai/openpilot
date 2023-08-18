6253dca9eb2e936c5828de5cf96e6c988d1349a7
Shane Smiskol <shane@smiskol.com>
2023-08-18 14:33:13 -0700

    CAN fingerprint: timing test (#29461)
    * test fingerprint frames (timing)
    
    * guess not needed
    
    * it checks both buses
    
    * use case list
    
    * this is safe since we call current test only in the current loop, never after
    
    * prof
    
    * test this too

--- selfdrive/car/tests/test_can_fingerprint.py
+++ selfdrive/car/tests/test_can_fingerprint.py
@@ -3,7 +3,7 @@ from parameterized import parameterized
 import unittest
 
 from cereal import log, messaging
-from selfdrive.car.car_helpers import can_fingerprint
+from selfdrive.car.car_helpers import FRAME_FINGERPRINT, can_fingerprint
 from selfdrive.car.fingerprints import _FINGERPRINTS as FINGERPRINTS
 
 
@@ -24,6 +24,43 @@ class TestCanFingerprint(unittest.TestCase):
       self.assertEqual(car_fingerprint, car_model)
       self.assertEqual(finger[0], fingerprint)
       self.assertEqual(finger[1], fingerprint)
+      self.assertEqual(finger[2], {})
+
+  def test_timing(self):
+    # just pick any CAN fingerprinting car
+    car_model = 'CHEVROLET BOLT EUV 2022'
+    fingerprint = FINGERPRINTS[car_model][0]
+
+    cases = []
+
+    # case 1 - one match, make sure we keep going for 100 frames
+    can = messaging.new_message('can', 1)
+    can.can = [log.CanData(address=address, dat=b'\x00' * length, src=src)
+               for address, length in fingerprint.items() for src in (0, 1)]
+    cases.append((FRAME_FINGERPRINT, car_model, can))
+
+    # case 2 - no matches, make sure we keep going for 100 frames
+    can = messaging.new_message('can', 1)
+    can.can = [log.CanData(address=1, dat=b'\x00' * 1, src=src) for src in (0, 1)]  # uncommon address
+    cases.append((FRAME_FINGERPRINT, None, can))
+
+    # case 3 - multiple matches, make sure we keep going for 200 frames to try to eliminate some
+    can = messaging.new_message('can', 1)
+    can.can = [log.CanData(address=2016, dat=b'\x00' * 8, src=src) for src in (0, 1)]  # common address
+    cases.append((FRAME_FINGERPRINT * 2, None, can))
+
+    for expected_frames, car_model, can in cases:
+      with self.subTest(expected_frames=expected_frames, car_model=car_model):
+        frames = 0
+
+        def test():
+          nonlocal frames
+          frames += 1
+          return can  # noqa: B023
+
+        car_fingerprint, _ = can_fingerprint(test)
+        self.assertEqual(car_fingerprint, car_model)
+        self.assertEqual(frames, expected_frames + 2)  # TODO: fix extra frames
 
 
 if __name__ == "__main__":
