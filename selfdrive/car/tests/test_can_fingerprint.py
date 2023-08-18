fafe0f7c2f9385ab72d661f853ce2df96fcfb7f4
Shane Smiskol <shane@smiskol.com>
2023-08-18 00:10:52 -0700

    test fingerprint frames (timing)

--- selfdrive/car/tests/test_can_fingerprint.py
+++ selfdrive/car/tests/test_can_fingerprint.py
@@ -3,8 +3,19 @@ from parameterized import parameterized
 import unittest
 
 from cereal import log, messaging
-from selfdrive.car.car_helpers import can_fingerprint
+from selfdrive.car.car_helpers import FRAME_FINGERPRINT, can_fingerprint
 from selfdrive.car.fingerprints import _FINGERPRINTS as FINGERPRINTS
+
+
+def create_can_iter(fingerprint):
+  can = messaging.new_message('can', 1)
+  can.can = [log.CanData(address=address, dat=b'\x00' * length)
+             for address, length in fingerprint.items()]
+
+  fingerprint_iter = iter([can])
+  empty_can = messaging.new_message('can', 0)
+
+  return lambda: next(fingerprint_iter, empty_can)
 
 
 class TestCanFingerprint(unittest.TestCase):
@@ -21,6 +32,63 @@ class TestCanFingerprint(unittest.TestCase):
       car_fingerprint, finger = can_fingerprint(lambda: next(fingerprint_iter, empty_can))  # noqa: B023
 
       self.assertEqual(car_fingerprint, car_model)
+      self.assertEqual(finger[0], fingerprint)  # we only fake bus 0
+
+  def test_timing(self):
+    # just pick any CAN fingerprinting car
+    car_model = 'CHEVROLET BOLT EUV 2022'
+    fingerprint = FINGERPRINTS[car_model][0]
+
+    can = messaging.new_message('can', 1)
+    can.can = [log.CanData(address=address, dat=b'\x00' * length)
+               for address, length in fingerprint.items()]
+
+    frames = 0
+
+    def test():
+      nonlocal frames
+      frames += 1
+      return can
+
+    # can_iter = create_can_iter(fingerprint)
+    # car_fingerprint, _ = can_fingerprint(can_iter)
+    car_fingerprint, _ = can_fingerprint(test)
+    # if one match, make sure we keep going for 100 frames
+    self.assertEqual(frames, FRAME_FINGERPRINT + 2)  # TODO: not sure why weird offset
+
+    # test 2 - no matches
+    can = messaging.new_message('can', 1)
+    can.can = [log.CanData(address=1, dat=b'\x00' * 1)]  # weird address
+
+    frames = 0
+
+    def test():
+      nonlocal frames
+      frames += 1
+      return can
+
+    # print(fingerprint)
+    print(car_model, car_fingerprint, frames)
+    car_fingerprint, _ = can_fingerprint(test)
+    # if no matches, make sure we keep going for 100 frames
+    self.assertEqual(frames, FRAME_FINGERPRINT + 2)  # TODO: not sure why weird offset
+
+    # test 3 - multiple matches
+    can = messaging.new_message('can', 1)
+    can.can = [log.CanData(address=2016, dat=b'\x00' * 8)]  # common address
+
+    frames = 0
+
+    def test():
+      nonlocal frames
+      frames += 1
+      return can
+
+    # print(fingerprint)
+    print(car_model, car_fingerprint, frames)
+    car_fingerprint, _ = can_fingerprint(test)
+    # if multiple matches, make sure we keep going for 200 frames to try to eliminate some
+    self.assertEqual(frames, FRAME_FINGERPRINT * 2 + 2)  # TODO: not sure why weird offset
 
 
 if __name__ == "__main__":
