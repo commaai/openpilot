38f15237420e911294ff12f541529d21242e51aa
Shane Smiskol <shane@smiskol.com>
2023-08-18 20:57:42 -0700

    add Kia Niro Hybrid 2019 to legacy safety mode list (#29472)
    * add Kia Niro Hybrid 2019 to legacy safety mode list
    
    * list
    
    * test
    
    * update docs
    
    * space

--- selfdrive/car/hyundai/tests/test_hyundai.py
+++ selfdrive/car/hyundai/tests/test_hyundai.py
@@ -5,7 +5,7 @@ from cereal import car
 from selfdrive.car.fw_versions import build_fw_dict
 from selfdrive.car.hyundai.values import CAMERA_SCC_CAR, CANFD_CAR, CAN_GEARS, CAR, CHECKSUM, DATE_FW_ECUS, \
                                          EV_CAR, FW_QUERY_CONFIG, FW_VERSIONS, LEGACY_SAFETY_MODE_CAR, \
-                                         PLATFORM_CODE_ECUS, get_platform_codes
+                                         UNSUPPORTED_LONGITUDINAL_CAR, PLATFORM_CODE_ECUS, get_platform_codes
 
 Ecu = car.CarParams.Ecu
 ECU_NAME = {v: k for k, v in Ecu.schema.enumerants.items()}
@@ -37,7 +37,7 @@ NO_DATES_PLATFORMS = {
 
 class TestHyundaiFingerprint(unittest.TestCase):
   def test_canfd_not_in_can_features(self):
-    can_specific_feature_list = set.union(*CAN_GEARS.values(), *CHECKSUM.values(), LEGACY_SAFETY_MODE_CAR, CAMERA_SCC_CAR)
+    can_specific_feature_list = set.union(*CAN_GEARS.values(), *CHECKSUM.values(), LEGACY_SAFETY_MODE_CAR, UNSUPPORTED_LONGITUDINAL_CAR, CAMERA_SCC_CAR)
     for car_model in CANFD_CAR:
       self.assertNotIn(car_model, can_specific_feature_list, "CAN FD car unexpectedly found in a CAN feature list")
 
