4df2137a7606da0a58595cb995d0bcc211d11785
Shane Smiskol <shane@smiskol.com>
2023-08-21 23:19:16 -0700

    should keep it in bytes

--- selfdrive/car/ford/tests/test_ford.py
+++ selfdrive/car/ford/tests/test_ford.py
@@ -6,7 +6,7 @@ from typing import Dict, Iterable, Optional, Tuple
 import capnp
 
 from cereal import car
-from selfdrive.car.ford.values import CAR, FW_QUERY_CONFIG, FW_VERSIONS
+from selfdrive.car.ford.values import FW_QUERY_CONFIG, FW_VERSIONS
 
 Ecu = car.CarParams.Ecu
 
@@ -23,20 +23,20 @@ ECU_ADDRESSES = {
 
 ECU_FW_CORE = {
   Ecu.eps: [
-    "14D003",
+    b"14D003",
   ],
   Ecu.abs: [
-    "2D053",
+    b"2D053",
   ],
   Ecu.fwdRadar: [
-    "14D049",
+    b"14D049",
   ],
   Ecu.fwdCamera: [
-    "14F397",  # Ford Q3
-    "14H102",  # Ford Q4
+    b"14F397",  # Ford Q3
+    b"14H102",  # Ford Q4
   ],
   Ecu.engine: [
-    "14C204",
+    b"14C204",
   ],
 }
 
@@ -50,9 +50,7 @@ class TestFordFW(unittest.TestCase):
 
   @parameterized.expand(FW_VERSIONS.items())
   def test_fw_versions(self, car_model: str, fw_versions: Dict[Tuple[capnp.lib.capnp._EnumModule, int, Optional[int]], Iterable[bytes]]):
-    self.assertIn(car_model, CAR.__dict__.values())
-
-    for (ecu, addr, subaddr), ecu_rxs in fw_versions.items():
+    for (ecu, addr, subaddr), fws in fw_versions.items():
       self.assertIn(ecu, ECU_ADDRESSES, "Unknown ECU")
       self.assertEqual(addr, ECU_ADDRESSES[ecu], "ECU address mismatch")
       self.assertIsNone(subaddr, "Unexpected ECU subaddress")
@@ -62,15 +60,15 @@ class TestFordFW(unittest.TestCase):
       #   and likely the platform.
       # Core identifies the type of the item (e.g. 14D003 = PSCM, 14C204 = PCM).
       # Suffix specifies the version of the part. -AA would be followed by -AB.
-      #   Small increments in the suffix are usually be compatible.
+      #   Small increments in the suffix are usually compatible.
       # Details: https://forscan.org/forum/viewtopic.php?p=70008#p70008
-      for rx in ecu_rxs:
-        self.assertEqual(len(rx), 24, "Expected ECU response to be 24 bytes")
+      for fw in fws:
+        self.assertEqual(len(fw), 24, "Expected ECU response to be 24 bytes")
 
-        rx_parts = rx.rstrip(b'\x00').decode().split('-')
-        self.assertEqual(len(rx_parts), 3, "Expected FW to be in format: prefix-core-suffix")
+        fw_parts = fw.rstrip(b'\x00').split(b'-')
+        self.assertEqual(len(fw_parts), 3, "Expected FW to be in format: prefix-core-suffix")
 
-        prefix, core, suffix = rx_parts
+        prefix, core, suffix = fw_parts
         self.assertEqual(len(prefix), 4, "Expected FW prefix to be 4 characters")
         self.assertIn(len(core), (5, 6), "Expected FW core to be 5-6 characters")
         self.assertIn(core, ECU_FW_CORE[ecu], f"Unexpected FW core for {ecu}")
