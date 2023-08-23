ee7e74e9795d1c02ec860579bba26502aa061d82
Shane Smiskol <shane@smiskol.com>
2023-08-22 20:28:35 -0700

    or this

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -219,12 +219,10 @@ class CarInterface(CarInterfaceBase):
     # since we don't yet parse radar on TSS2 radar-based ACC cars, gate longitudinal behind experimental toggle
     use_sdsu = bool(ret.flags & ToyotaFlags.SMART_DSU)
     if candidate in RADAR_ACC_CAR:
-      ret.experimentalLongitudinalAvailable = True  # use_sdsu  # always alpha long if radar acc car (sdsu or radar disable)
-      if not experimental_long:
-        use_sdsu = False
-      else:
-        if not use_sdsu:
-          ret.flags |= ToyotaFlags.DISABLE_RADAR.value
+      ret.experimentalLongitudinalAvailable = True  # use_sdsu
+      use_sdsu = use_sdsu and experimental_long
+      if not use_sdsu and experimental_long:
+        ret.flags |= ToyotaFlags.DISABLE_RADAR.value
 
     # openpilot longitudinal enabled by default:
     #  - non-(TSS2 radar ACC cars) w/ smartDSU installed
