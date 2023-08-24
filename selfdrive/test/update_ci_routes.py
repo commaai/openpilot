7004a99bd53375a0d9d415331e880ed7b821ae42
Cameron Clough <cameronjclough@gmail.com>
2023-08-24 17:57:28 +0100

    remove azureutil

--- selfdrive/test/update_ci_routes.py
+++ selfdrive/test/update_ci_routes.py
@@ -8,7 +8,6 @@ from azure.storage.blob import BlockBlobService  # pylint: disable=import-error
 from selfdrive.car.tests.routes import routes as test_car_models_routes
 from selfdrive.locationd.test.test_laikad import UBLOX_TEST_ROUTE, QCOM_TEST_ROUTE
 from selfdrive.test.process_replay.test_processes import source_segments as replay_segments
-from xx.chffr.lib import azureutil  # pylint: disable=import-error
 from xx.chffr.lib.storage import _DATA_ACCOUNT_PRODUCTION, _DATA_ACCOUNT_CI, _DATA_BUCKET_PRODUCTION  # pylint: disable=import-error
 
 SOURCES = [
@@ -17,10 +16,24 @@ SOURCES = [
 ]
 
 
+def get_user_token(account_name, container_name):
+  try:
+    subprocess.check_call(["az", "account", "show"], stdout=subprocess.DEVNULL)
+  except (subprocess.CalledProcessError, FileNotFoundError):
+    raise Exception('Must run `az login` before calling get_user_token') from None
+
+  cli = "az storage container generate-sas "
+  cli += "--account-name " + account_name + " --name " + container_name + " "
+  cli += "--https-only --permissions lrw --expiry $(date -u '+%Y-%m-%dT%H:%M:%SZ' -d '+1 hour') --auth-mode login --as-user --output tsv"
+  sas_token = subprocess.check_output(cli, shell=True, stderr=subprocess.DEVNULL).decode().strip("\n")
+
+  return sas_token
+
+
 @lru_cache
 def get_azure_keys():
-  dest_key = azureutil.get_user_token(_DATA_ACCOUNT_CI, "openpilotci")
-  source_keys = [azureutil.get_user_token(account, bucket) for account, bucket in SOURCES]
+  dest_key = get_user_token(_DATA_ACCOUNT_CI, "openpilotci")
+  source_keys = [get_user_token(account, bucket) for account, bucket in SOURCES]
   service = BlockBlobService(_DATA_ACCOUNT_CI, sas_token=dest_key)
   return dest_key, source_keys, service
 
@@ -43,12 +56,26 @@ def upload_route(path, exclude_patterns=None):
   ] + [f"--exclude-pattern={p}" for p in exclude_patterns]
   subprocess.check_call(cmd)
 
+def list_all_blobs(blob_service, container, prefix=None):
+  marker = None
+  count = 0
+  while True:
+    batch = blob_service.list_blobs(container, prefix=prefix, marker=marker)
+    for blob in batch:
+      yield blob
+      count += 1
+    # note that it is extremely important that you grab the next marker after iterating
+    # (because list_blobs makes multiple requests and updates the marker after each request)
+    marker = batch.next_marker
+    if not marker:
+      break
+
 def sync_to_ci_public(route):
   dest_key, source_keys, service = get_azure_keys()
   key_prefix = route.replace('|', '/')
   dongle_id = key_prefix.split('/')[0]
 
-  if next(azureutil.list_all_blobs(service, "openpilotci", prefix=key_prefix), None) is not None:
+  if next(list_all_blobs(service, "openpilotci", prefix=key_prefix), None) is not None:
     return True
 
   print(f"Uploading {route}")
