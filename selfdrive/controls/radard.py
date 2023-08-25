74a1c7d95591665e4e9c1f0d4229f093843ed0af
Bruce Wayne <harald.the.engineer@gmail.com>
2023-08-25 13:57:33 -0700

    radard: generate kalman gain at init

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import importlib
 import math
+import numpy as np
 from collections import deque
 from typing import Optional, Dict, Any
 
 import capnp
 from cereal import messaging, log, car
-from openpilot.common.numpy_fast import interp
 from openpilot.common.params import Params
 from openpilot.common.realtime import Ratekeeper, Priority, config_realtime_process
 from openpilot.system.swaglog import cloudlog
 
-from openpilot.common.kalman.simple_kalman import KF1D
+from openpilot.common.kalman.simple_kalman import KF1D, get_kalman_gain
 
 
 # Default lead acceleration decay set to 50% at 1s
@@ -34,19 +34,9 @@ class KalmanParams:
     assert dt > .01 and dt < .2, "Radar time step must be between .01s and 0.2s"
     self.A = [[1.0, dt], [0.0, 1.0]]
     self.C = [1.0, 0.0]
-    #Q = np.matrix([[10., 0.0], [0.0, 100.]])
-    #R = 1e3
-    #K = np.matrix([[ 0.05705578], [ 0.03073241]])
-    dts = [dt * 0.01 for dt in range(1, 21)]
-    K0 = [0.12287673, 0.14556536, 0.16522756, 0.18281627, 0.1988689,  0.21372394,
-          0.22761098, 0.24069424, 0.253096,   0.26491023, 0.27621103, 0.28705801,
-          0.29750003, 0.30757767, 0.31732515, 0.32677158, 0.33594201, 0.34485814,
-          0.35353899, 0.36200124]
-    K1 = [0.29666309, 0.29330885, 0.29042818, 0.28787125, 0.28555364, 0.28342219,
-          0.28144091, 0.27958406, 0.27783249, 0.27617149, 0.27458948, 0.27307714,
-          0.27162685, 0.27023228, 0.26888809, 0.26758976, 0.26633338, 0.26511557,
-          0.26393339, 0.26278425]
-    self.K = [[interp(dt, dts, K0)], [interp(dt, dts, K1)]]
+    Q = [[10., 0.0], [0.0, 100.]]
+    R = 1e3
+    self.K = get_kalman_gain(dt, np.array(self.A), np.array([self.C]), np.array(Q), R)
 
 
 class Track:
