5033da5e273ff9697e0f78211e1eee4b9be8801e
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-08-24 17:00:26 -0700

    Added cython bindings for model runners and commonmodel (#29607)
    * Added cython bindings for model runners and commonmodel
    
    * Removed cython language_level=3
    
    * loop to set CXXFLAGS for both envs
    old-commit-hash: e2e39d100b32a6706e83c81a1ef3dcdd7ea4001f

---
+++ selfdrive/modeld/runners/snpemodel_pyx.pyx
@@ -0,0 +1,14 @@
+# distutils: language = c++
+# cython: c_string_encoding=ascii
+
+from libcpp cimport bool
+from libcpp.string cimport string
+
+from .snpemodel cimport SNPEModel as cppSNPEModel
+from selfdrive.modeld.models.commonmodel_pyx cimport CLContext
+from selfdrive.modeld.runners.runmodel_pyx cimport RunModel
+from selfdrive.modeld.runners.runmodel cimport RunModel as cppRunModel
+
+cdef class SNPEModel(RunModel):
+  def __cinit__(self, string path, float[:] output, int runtime, bool use_tf8, CLContext context):
+    self.model = <cppRunModel *> new cppSNPEModel(path, &output[0], len(output), runtime, use_tf8, context.context)
