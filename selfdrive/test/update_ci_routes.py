c9e227a9c062aa5e9569472e88d720d1160819de
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-08-25 14:01:10 -0700

    update azure-storage-blob (#29411)
    Co-authored-by: Cameron Clough <cameronjclough@gmail.com>

--- selfdrive/test/update_ci_routes.py
+++ selfdrive/test/update_ci_routes.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
-from functools import lru_cache
-import sys
 import subprocess
+import sys
+from functools import lru_cache
+from typing import Iterable, Optional
+
+from azure.storage.blob import ContainerClient
 from tqdm import tqdm
-from azure.storage.blob import BlockBlobService
 
 from openpilot.selfdrive.car.tests.routes import routes as test_car_models_routes
 from openpilot.selfdrive.locationd.test.test_laikad import UBLOX_TEST_ROUTE, QCOM_TEST_ROUTE
 from openpilot.selfdrive.test.process_replay.test_processes import source_segments as replay_segments
-from xx.chffr.lib import azureutil
-from xx.chffr.lib.storage import _DATA_ACCOUNT_PRODUCTION, _DATA_ACCOUNT_CI, _DATA_BUCKET_PRODUCTION
+from openpilot.selfdrive.test.openpilotci import (DATA_CI_ACCOUNT, DATA_CI_ACCOUNT_URL, DATA_CI_CONTAINER,
+                                                  get_azure_credential, get_container_sas)
+
+DATA_PROD_ACCOUNT = "commadata2"
+DATA_PROD_CONTAINER = "commadata2"
 
 SOURCES = [
-  (_DATA_ACCOUNT_PRODUCTION, _DATA_BUCKET_PRODUCTION),
-  (_DATA_ACCOUNT_CI, "commadataci"),
+  (DATA_PROD_ACCOUNT, DATA_PROD_CONTAINER),
+  (DATA_CI_ACCOUNT, DATA_CI_CONTAINER),
 ]
 
 
 @lru_cache
 def get_azure_keys():
-  dest_key = azureutil.get_user_token(_DATA_ACCOUNT_CI, "openpilotci")
-  source_keys = [azureutil.get_user_token(account, bucket) for account, bucket in SOURCES]
-  service = BlockBlobService(_DATA_ACCOUNT_CI, sas_token=dest_key)
-  return dest_key, source_keys, service
+  dest_container = ContainerClient(DATA_CI_ACCOUNT_URL, DATA_CI_CONTAINER, credential=get_azure_credential())
+  dest_key = get_container_sas(DATA_CI_ACCOUNT, DATA_CI_CONTAINER)
+  source_keys = [get_container_sas(*s) for s in SOURCES]
+  return dest_container, dest_key, source_keys
 
 
-def upload_route(path, exclude_patterns=None):
-  dest_key, _, _ = get_azure_keys()
+def upload_route(path: str, exclude_patterns: Optional[Iterable[str]] = None) -> None:
+  # TODO: use azure-storage-blob instead of azcopy, simplifies auth
+  dest_key = get_container_sas(DATA_CI_ACCOUNT, DATA_CI_CONTAINER)
   if exclude_patterns is None:
     exclude_patterns = ['*/dcamera.hevc']
 
@@ -37,28 +43,30 @@ def upload_route(path, exclude_patterns=None):
     "azcopy",
     "copy",
     f"{path}/*",
-    f"https://{_DATA_ACCOUNT_CI}.blob.core.windows.net/openpilotci/{destpath}?{dest_key}",
+    f"https://{DATA_CI_ACCOUNT}.blob.core.windows.net/{DATA_CI_CONTAINER}/{destpath}?{dest_key}",
     "--recursive=false",
     "--overwrite=false",
   ] + [f"--exclude-pattern={p}" for p in exclude_patterns]
   subprocess.check_call(cmd)
 
-def sync_to_ci_public(route):
-  dest_key, source_keys, service = get_azure_keys()
+
+def sync_to_ci_public(route: str) -> bool:
+  dest_container, dest_key, source_keys = get_azure_keys()
   key_prefix = route.replace('|', '/')
   dongle_id = key_prefix.split('/')[0]
 
-  if next(azureutil.list_all_blobs(service, "openpilotci", prefix=key_prefix), None) is not None:
+  if next(dest_container.list_blob_names(name_starts_with=key_prefix), None) is not None:
     return True
 
   print(f"Uploading {route}")
   for (source_account, source_bucket), source_key in zip(SOURCES, source_keys, strict=True):
+    # assumes az login has been run
     print(f"Trying {source_account}/{source_bucket}")
     cmd = [
       "azcopy",
       "copy",
       f"https://{source_account}.blob.core.windows.net/{source_bucket}/{key_prefix}?{source_key}",
-      f"https://{_DATA_ACCOUNT_CI}.blob.core.windows.net/openpilotci/{dongle_id}?{dest_key}",
+      f"https://{DATA_CI_ACCOUNT}.blob.core.windows.net/{DATA_CI_CONTAINER}/{dongle_id}?{dest_key}",
       "--recursive=true",
       "--overwrite=false",
       "--exclude-pattern=*/dcamera.hevc",
