507bcacf2eb9157aa733a8e6614cf93aa4bddb15
deanlee <deanlee3@gmail.com>
2023-08-27 13:21:05 +0800

    refactor DriverViewWidow to inherit from CameraWidget

--- selfdrive/ui/translations/main_fr.ts
+++ selfdrive/ui/translations/main_fr.ts
@@ -302,7 +302,7 @@
     </message>
 </context>
 <context>
-    <name>DriverViewScene</name>
+    <name>DriverViewWindow</name>
     <message>
         <source>camera starting</source>
         <translation>démarrage de la caméra</translation>
