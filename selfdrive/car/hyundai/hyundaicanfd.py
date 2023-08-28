99b4b0a3920a0b4598dde0741fc65a8e414bbfbf
Shane Smiskol <shane@smiskol.com>
2023-08-28 15:14:07 -0700

    Hyundai CAN FD: rename LKAS signal (#29685)
    * bump
    
    * now fix
    
    * comment
    old-commit-hash: 21ffd807b6f258d6f30c1228100bf2ed93e66309

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -46,7 +46,7 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_steer):
     "LKA_ASSIST": 0,
     "STEER_REQ": 1 if lat_active else 0,
     "STEER_MODE": 0,
-    "SET_ME_1": 0,
+    "HAS_LANE_SAFETY": 0,  # hide LKAS settings
     "NEW_SIGNAL_1": 0,
     "NEW_SIGNAL_2": 0,
   }
