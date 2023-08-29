68559c861e2d8e645eb361d47f2cae41105c7c14
Justin Newberry <justin@comma.ai>
2023-08-29 19:57:39 +0000

    fix the decorator

--- selfdrive/test/helpers.py
+++ selfdrive/test/helpers.py
@@ -73,15 +73,16 @@ def with_processes(processes, init_time=0, ignore_stopped=None):
 
 
 def temporary_mock_dir(mock_path):
-  def decorator(func):
-    def wrapper(*args, **kwargs):
+  def wrapper(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
       with tempfile.TemporaryDirectory() as temp_dir:
         cache_dir_patch = mock.patch(mock_path, temp_dir)
         cache_dir_patch.start()
         func(*args, **kwargs, temp_dir=temp_dir)
         cache_dir_patch.stop()
-      return wrapper
-  return decorator
+    return wrap
+  return wrapper
 
 temporary_cache_dir = temporary_mock_dir("openpilot.tools.lib.url_file.CACHE_DIR")
 temporary_swaglog_dir = temporary_mock_dir("openpilot.system.swaglog.SWAGLOG_DIR")
