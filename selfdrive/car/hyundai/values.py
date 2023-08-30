c65027635cc760498f7a83b667e22601d823bbdc
leZudiste <florian.alles@gmail.com>
2023-08-30 09:23:41 +0200

    HKG: Add FW versions for EU Tucson Hybrid 2021 (#29655)
    * Add Tucson Hybrid 2021 fingerprint
    
    * Fix missing doc
    
    * revert
    
    ---------
    
    Co-authored-by: florian alles <florian.alles@csgroup.eu>
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1830,10 +1830,12 @@ FW_VERSIONS = {
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-N9220 14K',
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.01 99211-N9100 14A',
       b'\xf1\x00NX4 FR_CMR AT USA LHD 1.00 1.00 99211-N9250 14W',
+      b'\xf1\x00NX4 FR_CMR AT EUR LHD 1.00 2.02 99211-N9000 14E',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00NX4__               1.00 1.00 99110-N9100         ',
       b'\xf1\x00NX4__               1.01 1.00 99110-N9100         ',
+      b'\xf1\x00NX4__               1.00 1.01 99110-N9000         ',
     ],
   },
   CAR.KIA_SPORTAGE_HYBRID_5TH_GEN: {
