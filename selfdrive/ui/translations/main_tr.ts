56f3b1b6fa165defa5d1db014c816a498edea112
Dean Lee <deanlee3@gmail.com>
2023-08-30 23:41:58 +0800

    ui/driverview: inherit from CameraWidget (#29661)
    old-commit-hash: 6c73a924fa7780fc31776060cf63cce3b85981cb

--- selfdrive/ui/translations/main_tr.ts
+++ selfdrive/ui/translations/main_tr.ts
@@ -302,7 +302,7 @@
     </message>
 </context>
 <context>
-    <name>DriverViewScene</name>
+    <name>DriverViewWindow</name>
     <message>
         <source>camera starting</source>
         <translation>kamera başlatılıyor</translation>
