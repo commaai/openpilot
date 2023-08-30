86e2e78227fde9a8593c5054cd664715ed0b9989
Shane Smiskol <shane@smiskol.com>
2023-08-29 22:18:30 -0700

    add note for blinker weirdness

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -202,8 +202,10 @@ class CarState(CarStateBase):
     ret.steeringPressed = self.update_steering_pressed(abs(ret.steeringTorque) > self.params.STEER_THRESHOLD, 5)
     ret.steerFaultTemporary = cp.vl["MDPS"]["LKA_FAULT"] != 0
 
-    left_blinker_sig = "LEFT_STALK" if self.CP.carFingerprint == CAR.KONA_EV_2ND_GEN else "LEFT_LAMP"
-    right_blinker_sig = "RIGHT_STALK" if self.CP.carFingerprint == CAR.KONA_EV_2ND_GEN else "RIGHT_LAMP"
+    # TODO: alt signal usage may be described by cp.vl['BLINKERS']['USE_ALT_LAMP']
+    left_blinker_sig, right_blinker_sig = "LEFT_LAMP", "RIGHT_LAMP"
+    if self.CP.carFingerprint == CAR.KONA_EV_2ND_GEN:
+      left_blinker_sig, right_blinker_sig = "LEFT_LAMP_ALT", "RIGHT_LAMP_ALT"
     ret.leftBlinker, ret.rightBlinker = self.update_blinker_from_lamp(50, cp.vl["BLINKERS"][left_blinker_sig],
                                                                       cp.vl["BLINKERS"][right_blinker_sig])
     if self.CP.enableBsm:
