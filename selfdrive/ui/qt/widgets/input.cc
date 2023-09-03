d829321c44570b2ce586945cfa345c633f730c32
Jason Wen <haibin.wen3@gmail.com>
2023-09-03 11:39:14 -0400

    update text right away

--- selfdrive/ui/qt/widgets/input.cc
+++ selfdrive/ui/qt/widgets/input.cc
@@ -244,13 +244,11 @@ ConfirmationDialog::ConfirmationDialog(const QString &prompt_text, const QString
 void ConfirmationDialog::confirmTimer() {
   if (confirm_timer > 0) {
     confirm_timer--;
+    confirm_btn->setText(confirm_text + " (" + QString::number(confirm_timer) + ")");
   }
-
   if (confirm_timer <= 0) {
     confirm_btn->setText(confirm_text);
     confirm_btn->setEnabled(true);
-  } else {
-    confirm_btn->setText(confirm_text + " (" + QString::number(confirm_timer) + ")");
   }
 }
 
