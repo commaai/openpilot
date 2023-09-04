acde832497841739097d64a4ab591e0fc4d3efe9
deanlee <deanlee3@gmail.com>
2023-09-04 21:18:27 +0800

    emit done on visible

--- selfdrive/ui/qt/offroad/driverview.cc
+++ selfdrive/ui/qt/offroad/driverview.cc
@@ -9,8 +9,12 @@ const int FACE_IMG_SIZE = 130;
 
 DriverViewWindow::DriverViewWindow(QWidget* parent) : CameraWidget("camerad", VISION_STREAM_DRIVER, true, parent) {
   face_img = loadPixmap("../assets/img_driver_face_static.png", {FACE_IMG_SIZE, FACE_IMG_SIZE});
-  QObject::connect(device(), &Device::interactiveTimeout, this, &DriverViewWindow::done);
   QObject::connect(this, &CameraWidget::clicked, this, &DriverViewWindow::done);
+  QObject::connect(device(), &Device::interactiveTimeout, this, [this]() {
+    if (isVisible()) {
+      emit done();
+    }
+  });
 }
 
 void DriverViewWindow::showEvent(QShowEvent* event) {
