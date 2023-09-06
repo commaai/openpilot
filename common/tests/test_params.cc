7d9684513689e48020692d84fa2a8fe3bdaa1b42
deanlee <deanlee3@gmail.com>
2023-09-06 13:49:15 +0800

    improve test case

--- common/tests/test_params.cc
+++ common/tests/test_params.cc
@@ -7,23 +7,19 @@ TEST_CASE("Params/asyncWriter") {
   char tmp_path[] = "/tmp/asyncWriter_XXXXXX";
   const std::string param_path = mkdtemp(tmp_path);
   Params params(param_path);
-
-  AsyncWriter async_writer;
   auto param_names = {"CarParams", "IsMetric"};
-  for (const auto &name : param_names) {
-    async_writer.queue({param_path, name, "1"});
-    // param is empty
-    REQUIRE(params.get(name).empty());
+  {
+    AsyncWriter async_writer;
+    for (const auto &name : param_names) {
+      async_writer.queue({param_path, name, "1"});
+      // param is empty
+      REQUIRE(params.get(name).empty());
+    }
+
+    // check if thread is running
+    REQUIRE(async_writer.future.valid());
+    REQUIRE(async_writer.future.wait_for(std::chrono::milliseconds(0)) == std::future_status::timeout);
   }
-
-  // check if thread is running
-  REQUIRE(async_writer.future.valid());
-  REQUIRE(async_writer.future.wait_for(std::chrono::milliseconds(0)) == std::future_status::timeout);
-
-  // wait for finish
-  async_writer.future.wait();
-  REQUIRE(async_writer.q.size() == 0);
-
   // check results
   for (const auto &name : param_names) {
     REQUIRE(params.get(name) == "1");
