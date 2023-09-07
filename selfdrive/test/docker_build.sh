2033c84e054d56f719c3631ffe1a44ae17a60d41
Kacper Rączy <gfw.kra@gmail.com>
2023-09-07 14:47:44 -0700

    TARGET ARCHITECTURE var

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -5,21 +5,24 @@ set -e
 # mkdir -p .ci_cache/scons_cache
 # sudo mount --bind /tmp/scons_cache/ .ci_cache/scons_cache
 
-if [ -n "$PUSH_IMAGE" ] && [ -z "$CURRENT_ARCH_BUILD" ]; then
+if [ -n "$PUSH_IMAGE" ] && [ -z "$TARGET_ARCHITECTURE" ]; then
   echo "PUSH_IMAGE is only supported for single arch builds"
   exit 1
 fi
 
 SCRIPT_DIR=$(dirname "$0")
 OPENPILOT_DIR=$SCRIPT_DIR/../../
-if [ -n "$CURRENT_ARCH_BUILD" ]; then
-  ARCH=$(uname -m)
-  TAG_SUFFIX="-$ARCH"
+if [ -n "$TARGET_ARCHITECTURE" ]; then
+  PLATFORM="linux/$TARGET_ARCHITECTURE"
+  TAG_SUFFIX="-$TARGET_ARCHITECTURE"
+else
+  PLATFORM="linux/$(uname -m)"
+  TAG_SUFFIX=""
 fi
 
 source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 
-DOCKER_BUILDKIT=1 docker buildx build --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
+DOCKER_BUILDKIT=1 docker buildx build --platform $PLATFORM --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
 
 if [ -n "$PUSH_IMAGE" ]; then
   docker push $REMOTE_TAG
