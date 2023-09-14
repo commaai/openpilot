0b1a4db137abbdc6cc2dfa933f65947e2b3055f0
Shane Smiskol <shane@smiskol.com>
2023-09-14 13:32:47 -0700

    fix condition

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -29,9 +29,9 @@ class CarInterface(CarInterfaceBase):
 
     # Default to angle control with a whitelist for EPSs that accept torque (made in Japan)
     # So far no ICE RAV4 2023 has been seen with this FW version
-    angle_cars_with_torque_eps = ANGLE_CONTROL_CAR & {CAR.RAV4H_TSS2_2023}
-    if candidate in angle_cars_with_torque_eps and not any(fw.ecu == "eps" and fw.fwVersion ==
-                                                           b'8965B42371\x00\x00\x00\x00\x00\x00' for fw in car_fw):
+    if candidate in ANGLE_CONTROL_CAR and (not any(fw.ecu == "eps" and fw.fwVersion ==
+                                                   b'8965B42371\x00\x00\x00\x00\x00\x00' for fw in car_fw) or
+                                           candidate != CAR.RAV4H_TSS2_2023):
       ret.dashcamOnly = True
       ret.steerControlType = SteerControlType.angle
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_TOYOTA_LTA
