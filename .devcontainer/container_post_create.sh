dcf7f7557d0e0d0ce65298d05c720f19ca85b24a
Kacper Rączy <gfw.kra@gmail.com>
2023-09-15 20:43:37 +0000

    Log when mac display override happens

--- .devcontainer/container_post_create.sh
+++ .devcontainer/container_post_create.sh
@@ -4,6 +4,7 @@ source .devcontainer/.host/.env
 
 # override display flag for mac
 if [[ $HOST_OS == darwin ]]; then
+  echo "Setting up DISPLAY override for macOS..."
   cat <<EOF >> /root/.bashrc
 if [ -n "\$DISPLAY" ]; then
   DISPLAY_NUM=\$(echo "\$DISPLAY" | awk -F: '{print \$NF}')
