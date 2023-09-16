6effedf9adaea0ce9957533360184ebd00c4f564
Shane Smiskol <shane@smiskol.com>
2023-09-15 19:10:22 -0700

    Revert "poll on peripheralState"
    This reverts commit 5eecb83e070dd51b550a47f3e0e7a1600b8da4e4.

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -34,7 +34,7 @@ NetworkStrength = log.DeviceState.NetworkStrength
 CURRENT_TAU = 15.   # 15s time constant
 TEMP_TAU = 5.   # 5s time constant
 DISCONNECT_TIMEOUT = 5.  # wait 5 seconds before going offroad after disconnect so you get an alert
-PERIPHERAL_STATE_TIMEOUT = round(1000 / service_list['peripheralState'].frequency * 1.5)  # 1.5x the expected peripheralState frequency
+PANDA_STATES_TIMEOUT = round(1000 / service_list['pandaStates'].frequency * 1.5)  # 1.5x the expected pandaState frequency
 
 ThermalBand = namedtuple("ThermalBand", ['min_temp', 'max_temp'])
 HardwareState = namedtuple("HardwareState", ['network_type', 'network_info', 'network_strength', 'network_stats',
@@ -168,7 +168,7 @@ def hw_state_thread(end_event, hw_queue):
 
 def thermald_thread(end_event, hw_queue) -> None:
   pm = messaging.PubMaster(['deviceState'])
-  sm = messaging.SubMaster(["peripheralState", "gpsLocationExternal", "controlsState", "pandaStates"], poll=["peripheralState"])
+  sm = messaging.SubMaster(["peripheralState", "gpsLocationExternal", "controlsState", "pandaStates"], poll=["pandaStates"])
 
   count = 0
 
@@ -209,7 +209,11 @@ def thermald_thread(end_event, hw_queue) -> None:
   fan_controller = None
 
   while not end_event.is_set():
-    sm.update(PERIPHERAL_STATE_TIMEOUT)
+    sm.update(PANDA_STATES_TIMEOUT)
+
+    # Run at 2Hz
+    if sm.frame % round(service_list['pandaStates'].frequency * DT_TRML) != 0:
+      continue
 
     pandaStates = sm['pandaStates']
     peripheralState = sm['peripheralState']
