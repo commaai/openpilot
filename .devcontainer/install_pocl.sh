3008dd6e0ce71d32d6f14f310f9d12197059a75e
Justin Newberry <justin@comma.ai>
2023-09-20 00:31:31 +0000

    cleanup the script

--- .devcontainer/install_pocl.sh
+++ .devcontainer/install_pocl.sh
@@ -4,17 +4,20 @@
 # adapted from: https://github.com/pocl/pocl/blob/main/tools/docker/Ubuntu/20_04.64bit
 
 LLVM_VERSION=12
-POCL_DIR=/opt/pocl
 
 apt update && apt install -y build-essential ocl-icd-libopencl1 cmake git pkg-config libclang-${LLVM_VERSION}-dev clang-${LLVM_VERSION} libclang-cpp${LLVM_VERSION}-dev llvm-${LLVM_VERSION}-dev make ninja-build ocl-icd-libopencl1 ocl-icd-dev ocl-icd-opencl-dev libhwloc-dev zlib1g zlib1g-dev dialog apt-utils
 
-cd /opt ; git clone https://github.com/pocl/pocl.git ; cd $POCL_DIR ; git checkout $GIT_COMMIT
-cd $POCL_DIR ; test -z "$GH_PR" || (git fetch origin +refs/pull/$GH_PR/merge && git checkout -qf FETCH_HEAD) && :
-cd $POCL_DIR ; mkdir b ; cd b; cmake -G Ninja -DWITH_LLVM_CONFIG=/usr/bin/llvm-config-${LLVM_VERSION} -DCMAKE_INSTALL_PREFIX=/usr ..
-cd $POCL_DIR/b ; ninja install
+cd /opt
+git clone https://github.com/pocl/pocl.git
+cd /opt/pocl
+git checkout $GIT_COMMIT
+test -z "$GH_PR" || (git fetch origin +refs/pull/$GH_PR/merge && git checkout -qf FETCH_HEAD)
+mkdir b && cd b
+cmake -G Ninja -DWITH_LLVM_CONFIG=/usr/bin/llvm-config-${LLVM_VERSION} -DCMAKE_INSTALL_PREFIX=/usr ..
+ninja install
 # removing this picks up PoCL from the system install, not the build dir
-cd $POCL_DIR/b ; rm -f CTestCustom.cmake
-cd $POCL_DIR/b ; ctest -j4 --output-on-failure -L internal
+rm -f CTestCustom.cmake
+ctest -j4 --output-on-failure -L internal
 
 apt install -y software-properties-common
 add-apt-repository -y ppa:ocl-icd/ppa
