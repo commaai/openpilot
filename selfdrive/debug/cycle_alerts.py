99405f36c5c2c6dbec6b816ad3613c10147b92bd
royjr <royjr96@gmail.com>
2023-09-23 09:55:07 -0400

    add paramsdTemporaryError

--- selfdrive/debug/cycle_alerts.py
+++ selfdrive/debug/cycle_alerts.py
@@ -26,6 +26,7 @@ def cycle_alerts(duration=200, is_metric=False):
     (EventName.wrongGear, ET.NO_ENTRY),
 
     (EventName.locationdTemporaryError, ET.SOFT_DISABLE),
+    (EventName.paramsdTemporaryError, ET.SOFT_DISABLE),
     (EventName.accFaulted, ET.IMMEDIATE_DISABLE),
 
     # DM sequence
