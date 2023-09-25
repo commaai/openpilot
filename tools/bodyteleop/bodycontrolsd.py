8ac1f70a090b0dd1cda5e0534d3fc0ce67cd369e
Comma Device <device@comma.ai>
2023-09-24 20:01:51 -0700

    log control inputs

--- tools/bodyteleop/bodycontrolsd.py
+++ tools/bodyteleop/bodycontrolsd.py
@@ -1,16 +1,21 @@
 import json
+import logging
 import time
 from cereal import messaging
 from openpilot.common.realtime import Ratekeeper
 
 TIME_GAP_THRESHOLD = 0.5
+last_control_send_time = time.monotonic()
+logger = logging.getLogger("pc")
+logging.basicConfig(level=logging.INFO)
 
-
-def send_control_message(pm, last_control_send_time, x, y):
+def send_control_message(pm, x, y):
+  global last_control_send_time
   msg = messaging.new_message('testJoystick')
   msg.testJoystick.axes = [x, y]
   msg.testJoystick.buttons = [False]
   pm.send('testJoystick', msg)
+  logger.info(f"bodycontrol (x, y): ({x}, {y})")
   last_control_send_time = time.monotonic()
 
 
@@ -18,16 +23,17 @@ def main():
   rk = Ratekeeper(20.0)
   pm = messaging.PubMaster(['testJoystick'])
   sm = messaging.SubMaster(['bodyReserved0'])
-  last_control_send_time = time.monotonic()
 
   while True:
-    now = time.monotonic()
-    if now > last_control_send_time + TIME_GAP_THRESHOLD:
-      send_control_message(pm, last_control_send_time, 0.0, 0.0)
     sm.update(0)
     if sm.updated['bodyReserved0']:
       controls = json.loads(sm['bodyReserved0'])
-      send_control_message(pm, last_control_send_time, controls['x'], controls['y'])
+      send_control_message(pm, controls['x'], controls['y'])
+
+    else:
+      now = time.monotonic()
+      if now > last_control_send_time + TIME_GAP_THRESHOLD:
+        send_control_message(pm, 0.0, 0.0)
 
     rk.keep_time()
 
