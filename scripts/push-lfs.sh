de6f7e7d0f527759cc40159abd8d4a57d8a8d71e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-09-25 16:06:32 -0700

    push to both

--- scripts/push-lfs.sh
+++ scripts/push-lfs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "WARNING: this is a script for internal use only."
+printf "WARNING: this is a script for internal use only.\n\n"
 
+printf "pushing to GitLab\n"
 git lfs push --all ssh://git@gitlab.com/commaai/openpilot-lfs.git
+
+printf "\npushing to GitHub\n"
+git lfs push --all git@github.com:commaai/openpilot.git
