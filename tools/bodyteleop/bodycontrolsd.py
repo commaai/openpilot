ea83c6f0a1b4a461f9cf9788815d05bb55d3636f
Comma Device <device@comma.ai>
2023-09-24 19:03:39 -0700

    fix pm error

--- tools/bodyteleop/bodycontrolsd.py
+++ tools/bodyteleop/bodycontrolsd.py
@@ -4,10 +4,9 @@ from cereal import messaging
 from openpilot.common.realtime import Ratekeeper
 
 TIME_GAP_THRESHOLD = 0.5
-last_control_send_time = time.monotonic()
 
 
-def send_control_message(x, y):
+def send_control_message(pm, last_control_send_time, x, y):
   msg = messaging.new_message('testJoystick')
   msg.testJoystick.axes = [x, y]
   msg.testJoystick.buttons = [False]
@@ -19,14 +18,16 @@ def main():
   rk = Ratekeeper(20.0)
   pm = messaging.PubMaster(['testJoystick'])
   sm = messaging.SubMaster(['bodyReserved0'])
+  last_control_send_time = time.monotonic()
+
   while True:
     now = time.monotonic()
     if now > last_control_send_time + TIME_GAP_THRESHOLD:
-      send_control_message(0.0, 0.0)
+      send_control_message(pm, last_control_send_time, 0.0, 0.0)
     sm.update(0)
     if sm.updated['bodyReserved0']:
       controls = json.loads(sm['bodyReserved0'])
-      send_control_message(controls['x'], controls['y'])
+      send_control_message(pm, last_control_send_time, controls['x'], controls['y'])
 
     rk.keep_time()
 
