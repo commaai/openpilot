af0b6037a81fb4f2c24f81faed128510f63958ad
Comma Device <device@comma.ai>
2023-09-24 18:07:26 -0700

    remove non waSD FROM WEB

--- tools/bodyteleop/bodycontrolsd.py
+++ tools/bodyteleop/bodycontrolsd.py
@@ -1,19 +1,31 @@
 import json
+import time
 from cereal import messaging
+
+TIME_GAP_THRESHOLD = 0.5
+
+last_control_send_time = time.monotonic()
+
+
+def send_control_message(x, y):
+  msg = messaging.new_message('testJoystick')
+  msg.testJoystick.axes = [x, y]
+  msg.testJoystick.buttons = [False]
+  pm.send('testJoystick', msg)
+  last_control_send_time = time.monotonic()
+
 
 def main():
   pm = messaging.PubMaster(['testJoystick'])
   sm = messaging.SubMaster(['bodyReserved0'])
-
   while True:
-      sm.update(0)
-
-      if sm.updated['bodyReserved0']:
-          controls = json.loads(sm['bodyReserved0'])
-          msg = messaging.new_message('testJoystick')
-          msg.testJoystick.axes = [controls['x'], controls['y']]
-          msg.testJoystick.buttons = [False]
-          pm.send('testJoystick', msg)
+    now = time.monotonic()
+    if now > last_control_send_time + TIME_GAP_THRESHOLD:
+      send_control_message(0.0, 0.0)
+    sm.update(0)
+    if sm.updated['bodyReserved0']:
+      controls = json.loads(sm['bodyReserved0'])
+      send_control_message(controls['x'], controls['y'])
 
 
 if __name__ == "__main__":
