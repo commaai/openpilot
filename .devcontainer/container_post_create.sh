94e6c151a9179c58d745da813364179ba19dc7da
Kacper Rączy <gfw.kra@gmail.com>
2023-09-25 20:34:51 +0000

    Fix display override on mac

--- .devcontainer/container_post_create.sh
+++ .devcontainer/container_post_create.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+TARGET_USER=batman
 source .devcontainer/.host/.env
 
 # override display flag for mac
 if [[ $HOST_OS == darwin ]]; then
   echo "Setting up DISPLAY override for macOS..."
-  cat <<EOF >> /root/.bashrc
+  cat <<EOF >> /home/$TARGET_USER/.bashrc
 if [ -n "\$DISPLAY" ]; then
   DISPLAY_NUM=\$(echo "\$DISPLAY" | awk -F: '{print \$NF}')
   export DISPLAY=host.docker.internal:\$DISPLAY_NUM
@@ -15,6 +16,5 @@ fi
 
 # These lines are temporary, to remain backwards compatible with old devcontainers
 # that were running as root and therefore had their caches written as root
-USER=batman
-sudo chown -R $USER: /tmp/scons_cache
-sudo chown -R $USER: /tmp/comma_download_cache
+sudo chown -R $TARGET_USER: /tmp/scons_cache
+sudo chown -R $TARGET_USER: /tmp/comma_download_cache
