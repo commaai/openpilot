279c6697a689263fc0de5d3d750c3267a3789d71
Justin Newberry <justin@comma.ai>
2023-09-26 02:57:27 +0000

    move to start script

--- .devcontainer/container_post_start.sh
+++ .devcontainer/container_post_start.sh
@@ -5,3 +5,10 @@ SUBMODULE_DIRS=$(git config --file .gitmodules --get-regexp path | awk '{ print 
 for DIR in $SUBMODULE_DIRS; do 
   git config --global --add safe.directory "$PWD/$DIR"
 done
+
+TARGET_USER=batman
+
+# These lines are temporary, to remain backwards compatible with old devcontainers
+# that were running as root and therefore had their caches written as root
+sudo chown -R $TARGET_USER: /tmp/scons_cache
+sudo chown -R $TARGET_USER: /tmp/comma_download_cache
