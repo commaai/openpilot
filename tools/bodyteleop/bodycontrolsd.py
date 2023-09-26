685a5c389ac12afa4ffc0b1d39e4bbd0bfd5b191
Kacper Rączy <gfw.kra@gmail.com>
2023-09-26 11:05:55 -0700

    Remove remote_ip from bodycontrolsd

--- tools/bodyteleop/bodycontrolsd.py
+++ tools/bodyteleop/bodycontrolsd.py
@@ -2,7 +2,6 @@
 import json
 import logging
 import time
-import argparse
 
 from cereal import messaging
 from openpilot.common.realtime import Ratekeeper
@@ -23,24 +22,20 @@ def send_control_message(pm, x, y, source):
   last_control_send_time = time.monotonic()
 
 
-def main(remote_ip=None):
+def main():
   rk = Ratekeeper(20.0)
   pm = messaging.PubMaster(['testJoystick'])
-  sm = messaging.SubMaster(['bodyReserved0'])
-  # if remote_ip:
-  #   sm_remote = messaging.SubMaster(['bodyReserved1'], addr=remote_ip)
+  sm = messaging.SubMaster(['bodyReserved0', 'bodyReserved1'])
 
   while True:
     sm.update(0)
-    # if remote_ip:
-    #   sm_remote.update(0)
 
     if sm.updated['bodyReserved0']:
       controls = json.loads(sm['bodyReserved0'])
       send_control_message(pm, controls['x'], controls['y'], 'wasd')
-    # elif remote_ip and sm_remote.updated['bodyReserved1']:
-    #   # ToDo: do something with the yolo outputs
-    #   print(sm_remote['bodyReserved1'])
+    elif sm.updated['bodyReserved1']:
+      # ToDo: do something with the yolo outputs
+      print(sm['bodyReserved1'])
     else:
       now = time.monotonic()
       if now > last_control_send_time + TIME_GAP_THRESHOLD:
@@ -50,8 +45,4 @@ def main(remote_ip=None):
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="Body control daemon")
-  parser.add_argument("addr", help="remote ip address")
-  args = parser.parse_args()
-
-  main(args.addr)
+  main()
