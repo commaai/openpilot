128fd0ef54967b5926509f1acbadce018abeceb2
Shane Smiskol <shane@smiskol.com>
2023-09-26 15:35:58 -0700

    elantra 2017 eps fault: try lower down delta

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -20,7 +20,7 @@ class CarControllerParams:
 
   def __init__(self, CP):
     self.STEER_DELTA_UP = 3
-    self.STEER_DELTA_DOWN = 7
+    self.STEER_DELTA_DOWN = 5
     self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 2
     self.STEER_DRIVER_FACTOR = 1
