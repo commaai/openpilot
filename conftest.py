36309fcf6f3abfc36b3817b0f7d905b0901b6078
Justin Newberry <justin@comma.ai>
2023-09-28 16:14:22 -0700

    fix tests in relative directories

--- conftest.py
+++ conftest.py
@@ -10,13 +10,13 @@ def global_setup_and_teardown():
     yield
 
 # there is currently no way to mark cpp tests, so we have to mark them here
-ADDITIONAL_EXPLICIT = ["selfdrive/ui/tests/test_translations", "selfdrive/ui/tests/test_sound"]
+ADDITIONAL_EXPLICIT = ["test_translations", "test_sound"]
 
 def pytest_collection_modifyitems(config, items):
   skipper = pytest.mark.skip(reason="Skipping explicit test since it was not run directly.")
   for item in items:
-    test_path = item.location[0]
-    is_explicit_test = "explicit" in item.keywords or test_path in ADDITIONAL_EXPLICIT
-    was_run_explicitly = test_path in config.option.file_or_dir
+    test_filename = item.fspath.basename
+    is_explicit_test = "explicit" in item.keywords or test_filename in ADDITIONAL_EXPLICIT
+    was_run_explicitly = test_filename in config.option.file_or_dir
     if is_explicit_test and not was_run_explicitly:
       item.add_marker(skipper)
