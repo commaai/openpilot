c401f478823792128990c31be5e32ac5116709c3
royjr <royjr96@gmail.com>
2023-09-28 18:57:27 -0400

    fingerprint

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1310,10 +1310,10 @@ FW_VERSIONS = {
   },
   CAR.KONA_2ND_GEN: {
     (Ecu.fwdRadar, 0x7d0, None): [
-      b'\xf1\x00PENDING',
+      b'\xf1\x00SX2_ RDR -----      1.00 1.02 99110-BE000         ',
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
-      b'\xf1\x00PENDING',
+      b'\xf1\x00SX2 MFC  AT USA LHD 1.00 1.03 99211-BE000 230517',
     ],
   },
   CAR.KIA_CEED:  {
