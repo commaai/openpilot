13e91077f7093cbcc7d57b12d322d17484aa2049
Kacper Rączy <gfw.kra@gmail.com>
2023-09-28 16:16:53 -0700

    Initialize virtualgl in bashrc

--- .devcontainer/container_post_create.sh
+++ .devcontainer/container_post_create.sh
@@ -3,7 +3,7 @@
 TARGET_USER=batman
 source .devcontainer/.host/.env
 
-# override display flag for mac
+# override display flag for mac hosts
 if [[ $HOST_OS == darwin ]]; then
   echo "Setting up DISPLAY override for macOS..."
   cat <<EOF >> /home/$TARGET_USER/.bashrc
@@ -11,6 +11,18 @@ if [ -n "\$DISPLAY" ]; then
   DISPLAY_NUM=\$(echo "\$DISPLAY" | awk -F: '{print \$NF}')
   export DISPLAY=host.docker.internal:\$DISPLAY_NUM
 fi
+EOF
+fi
+
+# setup virtualgl for mac hosts
+if [[ $HOST_OS == darwin ]]; then
+  echo "Setting up virtualgl for macOS..."
+  cat <<EOF >> /home/$TARGET_USER/.bashrc
+export VGL_PORT=10000
+export VGL_CLIENT=host.docker.internal
+export VGL_COMPRESS=rgb
+export VGL_DISPLAY=:0
+export VGL_FPS=60
 EOF
 fi
 
