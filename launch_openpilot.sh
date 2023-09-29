76b500f4f54210a0d8f0667d40f9fe97cf6a5eaa
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-09-28 22:25:19 -0700

    setup ssh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+echo -n 1 > /data/params/d/WheeledBody
+
+echo -n 1 > /data/params/d/SshEnabled
+cp /usr/comma/setup_keys /data/params/d/GithubSshKeys
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
-
