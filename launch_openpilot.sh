e97af9fbd500967581d45ea7f2f0850aa1da456f
Shane Smiskol <shane@smiskol.com>
2023-09-28 22:35:23 -0700

    disable updates

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -3,6 +3,7 @@
 echo -n 1 > /data/params/d/WheeledBody
 
 echo -n 1 > /data/params/d/SshEnabled
+echo -n 1 > /data/params/d/DisableUpdates
 cp /usr/comma/setup_keys /data/params/d/GithubSshKeys
 
 export PASSIVE="0"
