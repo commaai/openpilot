8dcd04e702d13b71cf9dd1c73acbcca88bb74cb3
Justin Newberry <justin@comma.ai>
2023-09-29 11:45:14 -0700

    Metadrive: move to a separate process (#30103)
    * split bridge into two files
    
    * fix metadrive
    
    * wip
    
    * fix tests too
    
    * separate process
    
    * sepeprate process
    
    * finish moving to another process
    
    * remove spammy output
    old-commit-hash: 9d3dbd245ab39a2893d61759e4ef6b50ab42a463

--- tools/sim/bridge/metadrive/metadrive_world.py
+++ tools/sim/bridge/metadrive/metadrive_world.py
@@ -1,35 +1,45 @@
-import math
+import ctypes
+import functools
+import multiprocessing
 import numpy as np
 import time
 
-from openpilot.tools.sim.lib.common import SimulatorState, World, vec3
+from multiprocessing import Pipe, Array
+from openpilot.tools.sim.bridge.metadrive.metadrive_process import metadrive_process, metadrive_state
+from openpilot.tools.sim.lib.common import SimulatorState, World
+from openpilot.tools.sim.lib.camerad import W, H
 
 
 class MetaDriveWorld(World):
-  def __init__(self, env, dual_camera = False):
+  def __init__(self, config, dual_camera = False):
     super().__init__(dual_camera)
-    self.env = env
-    self.dual_camera = dual_camera
+    self.camera_array = Array(ctypes.c_uint8, W*H*3)
+    self.road_image = np.frombuffer(self.camera_array.get_obj(), dtype=np.uint8).reshape((H, W, 3))
+
+    self.controls_send, self.controls_recv = Pipe()
+    self.state_send, self.state_recv = Pipe()
+
+    self.exit_event = multiprocessing.Event()
+
+    self.metadrive_process = multiprocessing.Process(name="metadrive process", target=
+                              functools.partial(metadrive_process, dual_camera, config,
+                                                self.camera_array, self.controls_recv, self.state_send, self.exit_event))
+    self.metadrive_process.start()
+
+    print("----------------------------------------------------------")
+    print("---- Spawning Metadrive world, this might take awhile ----")
+    print("----------------------------------------------------------")
+
+    self.state_recv.recv() # wait for a state message to ensure metadrive is launched
 
     self.steer_ratio = 15
-
     self.vc = [0.0,0.0]
-
     self.reset_time = 0
-
-  def get_cam_as_rgb(self, cam):
-    cam = self.env.engine.sensors[cam]
-    img = cam.perceive(self.env.vehicle, clip=False)
-    if type(img) != np.ndarray:
-      img = img.get() # convert cupy array to numpy
-    return img
+    self.should_reset = False
 
   def apply_controls(self, steer_angle, throttle_out, brake_out):
-    steer_metadrive = steer_angle * 1 / (self.env.vehicle.MAX_STEERING * self.steer_ratio)
-    steer_metadrive = np.clip(steer_metadrive, -1, 1)
-
     if (time.monotonic() - self.reset_time) > 5:
-      self.vc[0] = steer_metadrive
+      self.vc[0] = steer_angle
 
       if throttle_out:
         self.vc[1] = throttle_out/10
@@ -39,27 +49,26 @@ class MetaDriveWorld(World):
       self.vc[0] = 0
       self.vc[1] = 0
 
+    self.controls_send.send([*self.vc, self.should_reset])
+
   def read_sensors(self, state: SimulatorState):
-    state.velocity = vec3(x=float(self.env.vehicle.velocity[0]), y=float(self.env.vehicle.velocity[1]), z=0)
-    state.gps.from_xy(self.env.vehicle.position)
-    state.bearing = float(math.degrees(self.env.vehicle.heading_theta))
-    state.steering_angle = self.env.vehicle.steering * self.env.vehicle.MAX_STEERING
-    state.valid = True
+    while self.state_recv.poll(0):
+      md_state: metadrive_state = self.state_recv.recv()
+      state.velocity = md_state.velocity
+      state.bearing = md_state.bearing
+      state.steering_angle = md_state.steering_angle
+      state.gps.from_xy(md_state.position)
+      state.valid = True
 
   def read_cameras(self):
-    if self.dual_camera:
-     self.wide_road_image = self.get_cam_as_rgb("rgb_wide")
-    self.road_image = self.get_cam_as_rgb("rgb_road")
+    pass
 
   def tick(self):
-    obs, _, terminated, _, info = self.env.step(self.vc)
-
-    if terminated:
-      self.reset()
+    pass
 
   def reset(self):
-    self.env.reset()
-    self.reset_time = time.monotonic()
+    self.should_reset = True
 
   def close(self):
-    pass
+    self.exit_event.set()
+    self.metadrive_process.join()
