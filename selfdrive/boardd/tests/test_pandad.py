cefbbb056299890306f4fdf17669309f57dad120
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-10-03 13:25:09 -0700

    less overhead

--- selfdrive/boardd/tests/test_pandad.py
+++ selfdrive/boardd/tests/test_pandad.py
@@ -18,7 +18,9 @@ HERE = os.path.dirname(os.path.realpath(__file__))
 class TestPandad(unittest.TestCase):
 
   def setUp(self):
-    self._run_test(75)
+    # ensure panda is up
+    if len(Panda.list()) == 0:
+      self._run_test(60)
 
   def tearDown(self):
     managed_processes['pandad'].stop()
@@ -89,7 +91,10 @@ class TestPandad(unittest.TestCase):
 
   @phone_only
   def test_best_case_startup_time(self):
-    # should be fast since it's already up to date
+    # run once so we're setup
+    self._run_test(60)
+
+    # should be fast this time
     self._run_test(8)
 
   @phone_only
