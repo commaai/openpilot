06d151f4ef9aca4884c82f9faa76cb1b1e27c334
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-10-03 14:01:50 -0700

    fix pc

--- selfdrive/boardd/tests/test_pandad.py
+++ selfdrive/boardd/tests/test_pandad.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 import os
 import time
-import unittest
+import pytest
 
 import cereal.messaging as messaging
 from cereal import log
 from openpilot.common.gpio import gpio_set, gpio_init
 from panda import Panda, PandaDFU, PandaProtocolMismatch
-from openpilot.selfdrive.test.helpers import phone_only
 from openpilot.selfdrive.manager.process_config import managed_processes
-from openpilot.system.hardware import HARDWARE
+from openpilot.system.hardware import HARDWARE, PC
 from openpilot.system.hardware.tici.pins import GPIO
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
 
-class TestPandad(unittest.TestCase):
+@pytest.mark.skipif(PC, reason="needs a panda")
+class TestPandad:
 
   def setUp(self):
     # ensure panda is up
@@ -67,19 +67,16 @@ class TestPandad(unittest.TestCase):
 
     self._run_test(45)
 
-  @phone_only
   def test_in_dfu(self):
     HARDWARE.recover_internal_panda()
     self._run_test(60)
 
-  @phone_only
   def test_in_bootstub(self):
     with Panda() as p:
       p.reset(enter_bootstub=True)
       assert p.bootstub
     self._run_test()
 
-  @phone_only
   def test_internal_panda_reset(self):
     gpio_init(GPIO.STM_RST_N, True)
     gpio_set(GPIO.STM_RST_N, 1)
@@ -89,7 +86,6 @@ class TestPandad(unittest.TestCase):
 
     assert any(Panda(s).is_internal() for s in Panda.list())
 
-  @phone_only
   def test_best_case_startup_time(self):
     # run once so we're setup
     self._run_test(60)
@@ -97,7 +93,6 @@ class TestPandad(unittest.TestCase):
     # should be fast this time
     self._run_test(8)
 
-  @phone_only
   def test_protocol_version_check(self):
     if HARDWARE.get_device_type() == 'tici':
       self.skipTest("")
@@ -106,11 +101,9 @@ class TestPandad(unittest.TestCase):
     fn = os.path.join(HERE, "bootstub.panda_h7_spiv0.bin")
     self._flash_bootstub_and_test(fn, expect_mismatch=True)
 
-  @phone_only
   def test_release_to_devel_bootstub(self):
     self._flash_bootstub_and_test(None)
 
-  @phone_only
   def test_recover_from_bad_bootstub(self):
     self._go_to_dfu()
     with PandaDFU(None) as pd:
@@ -120,7 +113,3 @@ class TestPandad(unittest.TestCase):
     self._assert_no_panda()
 
     self._run_test(60)
-
-
-if __name__ == "__main__":
-  unittest.main()
