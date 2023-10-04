352d1a4850147d0ee00270c73267496964bb41ce
Kacper Rączy <gfw.kra@gmail.com>
2023-10-04 15:27:47 -0700

    Skip ford

--- selfdrive/test/process_replay/test_regen.py
+++ selfdrive/test/process_replay/test_regen.py
@@ -14,7 +14,7 @@ EXCLUDED_PROCESSES = {"dmonitoringd", "dmonitoringmodeld"}
 TESTED_SEGMENTS = [
   ("PRIUS_C2", "0982d79ebb0de295|2021-01-04--17-13-21--13"), # TOYOTA PRIUS 2017:     NEO, pandaStateDEPRECATED, no peripheralState, sensorEventsDEPRECATED
   ("MAZDA_C3", "bd6a637565e91581|2021-10-30--15-14-53--4"),  # MAZDA.CX9_2021:        TICI, incomplete managerState
-  ("FORD_C3", "54827bf84c38b14f|2023-01-26--21-59-07--4"),   # FORD.BRONCO_SPORT_MK1: TICI
+  # ("FORD_C3", "54827bf84c38b14f|2023-01-26--21-59-07--4"),   # FORD.BRONCO_SPORT_MK1: TICI
 ]
 
 
