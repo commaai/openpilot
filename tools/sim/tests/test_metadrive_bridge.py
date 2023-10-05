da4fb8a1ed67a96a33657997f3afc7842f2b97a4
Justin Newberry <justin@comma.ai>
2023-10-05 13:05:31 -0700

    fix sim tests

--- tools/sim/tests/test_metadrive_bridge.py
+++ tools/sim/tests/test_metadrive_bridge.py
@@ -3,13 +3,13 @@ import pytest
 import unittest
 
 from openpilot.tools.sim.run_bridge import parse_args
-from openpilot.tools.sim.bridge.metadrive.metadrive_bridge import MetaDriveBridge
 from openpilot.tools.sim.tests.test_sim_bridge import TestSimBridgeBase
 
 
 @pytest.mark.explicit
 class TestMetaDriveBridge(TestSimBridgeBase):
   def create_bridge(self):
+    from openpilot.tools.sim.bridge.metadrive.metadrive_bridge import MetaDriveBridge
     return MetaDriveBridge(parse_args([]))
 
 
