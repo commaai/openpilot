8b63b163ad8f2088ddc0b4ec7f70c6b37ffc68be
Shane Smiskol <shane@smiskol.com>
2023-10-04 20:13:08 -0700

    clean up

--- selfdrive/test/process_replay/compare_logs.py
+++ selfdrive/test/process_replay/compare_logs.py
@@ -63,8 +63,6 @@ def compare_logs(log1, log2, ignore_fields=None, ignore_msgs=None, tolerance=Non
   msgs_by_which_log2 = defaultdict(list)
 
   for msg1 in log1:
-    # if msg1.which() == 'carEvents':
-    #   continue
     msgs_by_which_log1[msg1.which()].append(msg1)
   for msg2 in log2:
     msgs_by_which_log2[msg2.which()].append(msg2)
@@ -73,16 +71,9 @@ def compare_logs(log1, log2, ignore_fields=None, ignore_msgs=None, tolerance=Non
     raise Exception(f"logs service keys don't match:\n\t\t{set(msgs_by_which_log1)}\n\t\t{set(msgs_by_which_log2)}")
 
   diff = []
-  print('IGNORE', ignore_fields)
   for which in msgs_by_which_log1.keys():
-    print(which, len(msgs_by_which_log1[which]), len(msgs_by_which_log2[which]))
     if len(msgs_by_which_log1[which]) == len(msgs_by_which_log2[which]):
-      print(which, 'log length equal')
       for msg1, msg2 in zip(msgs_by_which_log1[which], msgs_by_which_log2[which], strict=True):
-        # if msg1.which() != msg2.which():
-        #   raise Exception("msgs not aligned between logs")
-        # continue
-
         msg1 = remove_ignored_fields(msg1, ignore_fields)
         msg2 = remove_ignored_fields(msg2, ignore_fields)
 
@@ -108,51 +99,17 @@ def compare_logs(log1, log2, ignore_fields=None, ignore_msgs=None, tolerance=Non
           dd = list(filter(outside_tolerance, dd))
 
           diff.extend(dd)
+
     else:
-      print(which, 'print diff in logs')
-      new_msgs1 = [remove_ignored_fields(msg1, ignore_fields).as_reader().to_dict(verbose=True) for msg1 in msgs_by_which_log1[which]]
-      new_msgs2 = [remove_ignored_fields(msg2, ignore_fields).as_reader().to_dict(verbose=True) for msg2 in msgs_by_which_log2[which]]
-      added_msgs = [m2 for m2 in new_msgs2 if m2 not in new_msgs1]
-      removed_msgs = [m1 for m1 in new_msgs1 if m1 not in new_msgs2]
-      print('--- START ---')
-      for msg in added_msgs:
-        print('ADDED MSG', msg)
-      print('---')
-      for msg in removed_msgs:
-        print('REMOVED MSG', msg)
-      print('--- end ---')
-      dd_add = []
+      # Print new/removed messages
+      dict_msgs1 = [remove_ignored_fields(msg1, ignore_fields).as_reader().to_dict(verbose=True) for msg1 in msgs_by_which_log1[which]]
+      dict_msgs2 = [remove_ignored_fields(msg2, ignore_fields).as_reader().to_dict(verbose=True) for msg2 in msgs_by_which_log2[which]]
+
       dd = []
-      dd.extend([list(*dictdiffer.diff(r, {}, ignore=ignore_fields)) for r in removed_msgs])
-      dd.extend([list(*dictdiffer.diff({}, a, ignore=ignore_fields)) for a in added_msgs])
-      # diff.extend([('removed', which, list(dictdiffer.diff(r, {}, ignore=ignore_fields))[0][2]) for r in removed_msgs])
-      # diff.extend([('added', which, list(dictdiffer.diff({}, a, ignore=ignore_fields))[0][2]) for a in added_msgs])
+      dd.extend([list(*dictdiffer.diff(m1, {}, ignore=ignore_fields)) for m1 in dict_msgs1 if m1 not in dict_msgs2])
+      dd.extend([list(*dictdiffer.diff({}, m2, ignore=ignore_fields)) for m2 in dict_msgs2 if m2 not in dict_msgs1])
       dd = [(typ, which, dif) for typ, _, dif in dd]
       diff.extend(dd)
-      # diff.extend([list(*dictdiffer.diff(r, {}, ignore=ignore_fields)) for r in removed_msgs])
-      # diff.extend([list(*dictdiffer.diff({}, a, ignore=ignore_fields)) for a in added_msgs])
-      print('diff', diff)
-      break
-      # dd_add += [list(dictdiffer.diff([], a, ignore=ignore_fields)) for a in added_msgs]
-      # print('dd_add')
-      # print(dd_add)
-      # dd_add = list(dictdiffer.diff({}, added_msgs, ignore=ignore_fields))
-      # dd_add = [list(dictdiffer.diff(new_msgs1, new_msgs2, ignore=ignore_fields))]
-      # diff.append('Added messages:')
-      # diff.extend(map(str, added_msgs))
-      # diff.append('')
-      # diff.append('Added/removed messages:')
-      # diff.extend(map(str, removed_msgs))
-      # diff.extend(dd_add)
-      print('Added msgs:', len(added_msgs))
-      print('Removed msgs:', len(removed_msgs))
-      continue
-      print("Added msgs: {}".format("\n".join(added_msgs)))
-      print("Removed msgs: {}".format("\n".join(removed_msgs)))
-      # added_msgs = set(new_msgs2) - set(new_msgs1)
-      # removed_msgs = set(new_msgs1) - set(new_msgs2)
-      print('Added msgs:', len(added_msgs))
-      print('Removed msgs:', len(removed_msgs))
 
   return diff
 
