64b2bcb3dc41bff1e501d69a216042a7545f0d55
Shane Smiskol <shane@smiskol.com>
2023-10-06 14:59:39 -0700

    back to explicit test cases + assert specific exception msgs

--- selfdrive/test/process_replay/test_compare_logs.py
+++ selfdrive/test/process_replay/test_compare_logs.py
@@ -1,37 +1,70 @@
 #!/usr/bin/env python3
-from hypothesis import given, settings, strategies as st
 import unittest
 
 from cereal import messaging
-from openpilot.selfdrive.test.process_replay.compare_logs import compare_logs, format_diff, remove_ignored_fields
+from openpilot.selfdrive.test.process_replay.compare_logs import compare_logs, format_diff
 
 IGNORE_FIELDS = ["logMonoTime"]
 
 
 class TestCompareLogs(unittest.TestCase):
+  def setUp(self):
+    self.ref_logs = [self._msg('controlsState'), self._msg('carState'), self._msg('carControl'),
+                     self._msg('controlsState'), self._msg('carState'), self._msg('carControl')]
+
   @staticmethod
-  def _msg(which: str):
-    return messaging.new_message(which).as_reader()
+  def _msg(which: str, data: None | dict = None):
+    msg = messaging.new_message(which)
+    if data is not None:
+      getattr(msg, which).from_dict(data)
+    return msg.as_reader()
 
-  @settings(max_examples=1000, deadline=None)
-  @given(data=st.data())
-  def test_fuzzy_diff(self, data):
-    """compare_logs tries to format the diff nicely. check it fails for additions, removals, alignment"""
+  @staticmethod
+  def _get_failed(ref_logs: list, new_logs: list) -> bool:
+    diff = compare_logs(ref_logs, new_logs, ignore_fields=IGNORE_FIELDS)
+    _, _, failed = format_diff({"": {"": diff}}, {"": {"": {"ref": "", "new": ""}}}, {})
+    return failed
 
-    services = ['controlsState', 'carState', 'carControl']
+  def _assertException(self, ref_logs: list, new_logs: list, msg: str):
+    self.assertTrue(len(msg))
+    with self.assertRaises(Exception) as cm:
+      self._get_failed(ref_logs, new_logs)
+    self.assertIn(msg, str(cm.exception))
 
-    ref_logs = data.draw(st.lists(st.sampled_from(services).map(lambda x: self._msg(x)), max_size=6))
-    new_logs = data.draw(st.lists(st.sampled_from(services).map(lambda x: self._msg(x)), max_size=6))
+  def test_no_diff(self):
+    self.assertFalse(self._get_failed(self.ref_logs, self.ref_logs))
 
-    any_diff = [remove_ignored_fields(m, IGNORE_FIELDS).as_reader().to_dict(verbose=True) for m in ref_logs] != \
-               [remove_ignored_fields(m, IGNORE_FIELDS).as_reader().to_dict(verbose=True) for m in new_logs]
+  def test_addition(self):
+    # Adding any single message should fail
+    for which in ('controlsState', 'carState', 'carControl'):
+      with self.subTest(which=which):
+        new_logs = self.ref_logs + [self._msg(which)]
+        self._assertException(self.ref_logs, new_logs, "logs are not same length")
 
-    try:
-      diff = compare_logs(ref_logs, new_logs, ignore_fields=IGNORE_FIELDS)
-      _, _, failed = format_diff({"": {"": diff}}, {"": {"": {"ref": "", "new": ""}}}, {})
-    except Exception:
-      failed = True
-    self.assertEqual(failed, any_diff, "compare_logs didn't catch diff")
+  def test_removal(self):
+    # Removing any message should fail
+    for idx in range(len(self.ref_logs)):
+      with self.subTest(remove_idx=idx):
+        new_logs = self.ref_logs[0:idx] + self.ref_logs[idx + 1:len(self.ref_logs)]
+        self._assertException(self.ref_logs, new_logs, "logs are not same length")
+
+  def test_alignment(self):
+    # Msgs within each service type will match, but overall alignment should fail
+    new_logs = self.ref_logs[::-1]
+    self._assertException(self.ref_logs, new_logs, "msgs not aligned between logs")
+
+    # Try with different length
+    self._assertException(self.ref_logs, new_logs + [self._msg('controlsState')], "logs are not same length")
+
+    # Try with replaced msg
+    new_logs[0] = self._msg('controlsState')
+    self._assertException(self.ref_logs, new_logs, "msgs not aligned between logs")
+
+  def test_alignment_with_data(self):
+    # Now order of msgs within controlsState shouldn't match
+    self.ref_logs[0] = self._msg('controlsState', {'vCruise': 255})
+    new_logs = self.ref_logs[::-1]
+    self._assertException(self.ref_logs, new_logs, "msgs not aligned between logs")
 
 
 if __name__ == "__main__":
