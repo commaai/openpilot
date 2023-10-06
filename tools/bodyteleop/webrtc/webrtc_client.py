31d729ecf4183dcf7bbd2adeb69ea9e19b08611e
Kacper Rączy <gfw.kra@gmail.com>
2023-10-05 18:58:28 -0700

    webrtc client implementation

--- tools/bodyteleop/webrtc/webrtc_client.py
+++ tools/bodyteleop/webrtc/webrtc_client.py
@@ -5,25 +5,34 @@ class WebRTCCient:
   def __init__(self, address="127.0.0.1", port=8080, endpoint="livestream_driver"):
     self.peer_connection = aiortc.RTCPeerConnection()
     self.peer_connection.on("track", self.on_track)
+    self.video_track = None
 
   def on_track(self, track):
     if track.kind == "video":
-      self.peer_connection.addTrack(track)
+      self.video_track = track
     elif track.kind == "audio":
-      self.peer_connection.addTrack(track)
+      pass
 
   async def connect(self):
     offer = self.peer_connection.createOffer()
     await self.peer_connection.setLocalDescription(offer)
-    with aiohttp.ClientSession() as session:
-      answer = await session.get(f"http://{self.address}:{self.port}/{self.endpoint}", json=offer)
-      # TODO
+    async with aiohttp.ClientSession() as session:
+      response = await session.get(f"http://{self.address}:{self.port}/{self.endpoint}", json=offer)
+      async with response:
+        if response.status != 200:
+          raise Exception(f"Offer request failed with HTTP status code {response.status}")
+        answer = await response.json()
+        remote_offer = aiortc.RTCSessionDescription(sdp=answer.sdp, type=answer.type)
+        self.peer_connection.setRemoteDescription(remote_offer)
 
-  def recv(self, timeout_ms=100):
-    # TODO
-    pass
+  async def recv(self, timeout_ms=100):
+    if self.video_track is None:
+      raise Exception("No video track")
+
+    frame = await self.video_track.recv()
+    frame_arr = frame.to_ndarray(format="bgr24")
+    return frame_arr
 
   def is_connected(self):
-    # TODO
-    return False
+    return self.video_track is not None
 
