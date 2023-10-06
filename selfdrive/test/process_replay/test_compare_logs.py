c8a91fd21fda3cbdbcfe79d91ccbd9142172bfaa
Shane Smiskol <shane@smiskol.com>
2023-10-06 01:06:14 -0700

    more

--- selfdrive/test/process_replay/test_compare_logs.py
+++ selfdrive/test/process_replay/test_compare_logs.py
@@ -17,27 +17,6 @@ IGNORE_FIELDS = ["logMonoTime"]
 
 
 class TestCompareLogs(unittest.TestCase):
-  @classmethod
-  def setUpClass(cls):
-    cls.ref_logs = [
-      # cls._msg('sendcan', [(0, 0, 0, 0)], size=1),
-      cls._msg('controlsState', {'vCruise': 255}), cls._msg('carState', {'vEgo': 40.}), cls._msg('carControl', {'enabled': False}),
-      cls._msg('controlsState', {'vCruise': 255}), cls._msg('carState', {'vEgo': 41.}), cls._msg('carControl', {'enabled': False}),
-      cls._msg('controlsState', {'vCruise': 42.}), cls._msg('carState', {'vEgo': 42.}), cls._msg('carControl', {'enabled': True}),
-      cls._msg('controlsState', {'vCruise': 43.}), cls._msg('carState', {'vEgo': 42.}), cls._msg('carControl', {'enabled': True}),
-      cls._msg('controlsState', {'vCruise': 44.}), cls._msg('carState', {'vEgo': 43.}), cls._msg('carControl', {'enabled': True}),
-      cls._msg('controlsState', {'vCruise': 45.}), cls._msg('carState', {'vEgo': 44.}), cls._msg('carControl', {'enabled': True}),
-      cls._msg('controlsState', {'vCruise': 45.}), cls._msg('carState', {'vEgo': 45.}), cls._msg('carControl', {'enabled': True}),
-      cls._msg('controlsState', {'vCruise': 45.}), cls._msg('carState', {'vEgo': 45.}), cls._msg('carControl', {'enabled': True}),
-      cls._msg('controlsState', {'vCruise': 45.}), cls._msg('carState', {'vEgo': 43.}), cls._msg('carControl', {'enabled': False}),
-      cls._msg('controlsState', {'vCruise': 45.}), cls._msg('carState', {'vEgo': 39.}), cls._msg('carControl', {'enabled': False}),
-    ]
-    # print(cls.ref_logs)
-    # raise unittest.SkipTest
-
-  def setUp(self):
-    self.new_logs = copy.deepcopy(self.ref_logs)
-
   @staticmethod
   def _msg(which: str, data: None | dict = None, size: None | int = None):
     msg = messaging.new_message(which, size=size)
@@ -49,11 +28,6 @@ class TestCompareLogs(unittest.TestCase):
   def _get_failed(diff) -> bool:
     _, _, failed = format_diff({"": {"": diff}}, {"": {"": {"ref": "", "new": ""}}}, {})
     return failed
-
-  # def test_no_diff(self):
-  #   diff = compare_logs(self.ref_logs, self.new_logs, ignore_fields=IGNORE_FIELDS, ignore_msgs=[], tolerance=None)
-  #   print(diff)
-  #   self.assertFalse(self._get_failed(diff))
 
   def test_no_diff(self):
     ref_logs = [self._msg('controlsState'), self._msg('carState'), self._msg('carControl'),
