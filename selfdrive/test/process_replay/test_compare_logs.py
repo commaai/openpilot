b93482049b20bf6ec103555e15844321447f6001
Shane Smiskol <shane@smiskol.com>
2023-10-06 01:09:22 -0700

    one function to get failed stat

--- selfdrive/test/process_replay/test_compare_logs.py
+++ selfdrive/test/process_replay/test_compare_logs.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
-import copy
-import sys
-import math
-import capnp
-import numbers
-from cereal import log, messaging
 import unittest
-import dictdiffer
-from collections import defaultdict
-from typing import Dict
+
+from cereal import messaging
 from openpilot.selfdrive.test.process_replay.compare_logs import compare_logs, format_diff
 
 IGNORE_FIELDS = ["logMonoTime"]
@@ -27,38 +20,32 @@ class TestCompareLogs(unittest.TestCase):
     return msg.as_reader()
 
   @staticmethod
-  def _get_failed(diff) -> bool:
+  def _get_failed(ref_logs: list, new_logs: list) -> bool:
+    diff = compare_logs(ref_logs, new_logs, ignore_fields=IGNORE_FIELDS, ignore_msgs=[], tolerance=None)
     _, _, failed = format_diff({"": {"": diff}}, {"": {"": {"ref": "", "new": ""}}}, {})
     return failed
 
   def test_no_diff(self):
-    diff = compare_logs(self.ref_logs, self.ref_logs, ignore_fields=IGNORE_FIELDS, ignore_msgs=[], tolerance=None)
-    self.assertFalse(self._get_failed(diff))
+    self.assertFalse(self._get_failed(self.ref_logs, self.ref_logs))
 
   def test_addition(self):
     # Adding any single message should fail
     for which in ('controlsState', 'carState', 'carControl'):
       with self.subTest(which=which):
         new_logs = self.ref_logs + [self._msg(which)]
-        diff = compare_logs(self.ref_logs, new_logs, ignore_fields=IGNORE_FIELDS, ignore_msgs=[], tolerance=None)
-        print(diff)
-        self.assertTrue(self._get_failed(diff))
+        self.assertTrue(self._get_failed(self.ref_logs, new_logs))
 
   def test_removal(self):
     # Removing any message should fail
     for idx in range(len(self.ref_logs)):
       with self.subTest(remove_idx=idx):
         new_logs = self.ref_logs[0:idx] + self.ref_logs[idx + 1:len(self.ref_logs)]
-        diff = compare_logs(self.ref_logs, new_logs, ignore_fields=IGNORE_FIELDS, ignore_msgs=[], tolerance=None)
-        self.assertTrue(self._get_failed(diff))
+        self.assertTrue(self._get_failed(self.ref_logs, new_logs))
 
   def test_alignment(self):
     # Reverse ref logs and compare: overall alignment should fail
     new_logs = self.ref_logs[::-1]
-
-    diff = compare_logs(self.ref_logs, new_logs, ignore_fields=IGNORE_FIELDS, ignore_msgs=[], tolerance=None)
-    print(diff)
-    self.assertTrue(self._get_failed(diff))
+    self.assertTrue(self._get_failed(self.ref_logs, new_logs))
 
 
 if __name__ == "__main__":
