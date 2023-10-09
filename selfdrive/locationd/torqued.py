cd520b771fcdd2f0c3b4e70c4421e8ce9bd72a84
nuwandavek <vivekaithal44@gmail.com>
2023-10-08 17:00:07 -0700

    bugfix torqued

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -171,7 +171,7 @@ class TorqueEstimator:
         steer = np.interp(t, self.raw_points['carControl_t'], self.raw_points['steer_torque'])
         lateral_acc = (vego * yaw_rate) - (np.sin(roll) * ACCELERATION_DUE_TO_GRAVITY)
         if all(active) and (not any(steer_override)) and (vego > MIN_VEL) and (abs(steer) > STEER_MIN_THRESHOLD) and (abs(lateral_acc) <= LAT_ACC_THRESHOLD):
-          self.filtered_points.add_point(float(steer), float(steer), float(lateral_acc))
+          self.filtered_points.add_point(float(steer), float(lateral_acc))
 
   def get_msg(self, valid=True, with_points=False):
     msg = messaging.new_message('liveTorqueParameters')
