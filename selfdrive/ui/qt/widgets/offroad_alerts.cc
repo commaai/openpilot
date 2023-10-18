1b53fefc8ee6e04de774be01740ff3c8f1fcca7b
Po-Wen Kao <powen.kao.tw@gmail.com>
2023-10-18 21:26:09 +0800

    Avoid UI crash when asset is not available
    UI crash if wrong workspace is set or file is not avaliable.
    
    Signed-off-by: Po-Wen Kao <powen.kao.tw@gmail.com>

--- selfdrive/ui/qt/widgets/offroad_alerts.cc
+++ selfdrive/ui/qt/widgets/offroad_alerts.cc
@@ -68,6 +68,9 @@ int OffroadAlert::refresh() {
     QString json = util::read_file("../controls/lib/alerts_offroad.json").c_str();
     QJsonObject obj = QJsonDocument::fromJson(json.toUtf8()).object();
 
+    if (obj.empty())
+      return 0;
+
     // descending sort labels by severity
     std::vector<std::pair<std::string, int>> sorted;
     for (auto it = obj.constBegin(); it != obj.constEnd(); ++it) {
