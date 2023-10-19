1df51e826b6ef07d62cf19e3c09150d7c83db806
Comma Device <device@comma.ai>
2023-10-18 22:47:53 -0700

    more stuff

--- system/hardware/tici/esim.py
+++ system/hardware/tici/esim.py
@@ -7,21 +7,7 @@ import binascii
 import requests
 import serial
 import subprocess
-from typing import Optional
 
-from openpilot.system.swaglog import cloudlog
-
-class ATCommandFailed(Exception):
-  pass
-
-def at(cmd: str) -> Optional[str]:
-  for _ in range(1):
-    try:
-      return subprocess.check_output(f"mmcli -m any --timeout 30 --command='{cmd}'", shell=True, encoding='utf8')
-    except subprocess.CalledProcessError:
-      cloudlog.exception("at_command_failed")
-      time.sleep(1.0)
-  raise ATCommandFailed(f"failed to execute {cmd=}")
 
 def post(url, payload):
   print("POST to", url)
@@ -40,17 +26,10 @@ def post(url, payload):
   print("resp text", repr(r.text))
   r.raise_for_status()
 
-  #ret = f"HTTP/{r.raw.version.decode('utf-8')} {r.status_code}"
-  #ret += ''.join(f"{k}: {v}" for k, v in r.headers.items())
   ret = f"HTTP/1.1 {r.status_code}"
   ret += ''.join(f"{k}: {v}" for k, v in r.headers.items() if k != 'Connection')
   return ret.encode() + r.content
 
-def get_unsolicited_response():
-  # why doesn't modem manager directly return this?
-  time.sleep(0.1)
-  out = subprocess.check_output("journalctl -o cat -u ModemManager | grep QESIM", shell=True, encoding='utf8')
-  return out.split('+QESIM:')[-1].split('<CR><LF><CR><LF>OK<CR><LF>')[0]
 
 class LPA:
   def __init__(self):
@@ -62,6 +41,7 @@ class LPA:
   def at(self, cmd):
     print(f"==> sending {cmd}")
     self.dev.write(cmd.encode() + b'\r\n')
+
     r = self.dev.read(8192).strip()
     if b"OK" not in r and b"ERROR" not in r:
       time.sleep(7)
@@ -73,13 +53,10 @@ class LPA:
     return at(f'AT+QESIM="ota","{qr}"')
 
   def download(self, qr):
-    # TODO: double check this against the QR code
-    smdp = "smdp.io"
+    smdp = qr.split('$')[1]
     out = self.at(f'AT+QESIM="download","{qr}"')
-    for n in range(3):
-      print("\n\n\n!!!!!!!!!!!!!!!!!!", n, "\n")
+    for n in range(5):
       line = out.split("+QESIM: ")[1].split("\r\n\r\nOK")[0]
-      print("line", repr(line))
 
       parts = [x.strip().strip('"') for x in line.split(',', maxsplit=4)]
       print(repr(parts))
@@ -89,29 +66,28 @@ class LPA:
 
       r = post(f"https://{smdp}/{url}", payload)
       to_send = binascii.hexlify(r).decode()
-      print("\ngoing to module", repr(r), "\n", to_send, "\n")
 
-      max_trans_len = 1400
-      n_transfers = math.ceil(len(to_send) / max_trans_len)
-      for i in range(n_transfers):
-        print("****** doing ", i)
-        state = 1 if i < n_transfers-1 else 0
-        data = to_send[i * max_trans_len: (i+1)*max_trans_len]
-
+      chunk_len = 1400
+      for i in range(math.ceil(len(to_send) / chunk_len)):
+        state = 1 if (i+1)*chunk_len >= len(to_send) else 0
+        data = to_send[i * chunk_len : (i+1)*chunk_len]
         out = self.at(f'AT+QESIM="trans",{len(to_send)},{state},{i},{len(data)},"{data}"')
-        if out.endswith('+QESIM:"download",1'):
-          print("done, successfully loaded")
-          break
-        assert out.endswith('OK')
+        assert "OK" in out
 
-  def enable(self, profile):
-    pass
+      if '+QESIM:"download",1' in out:
+        raise Exception("profile install failed")
+      elif '+QESIM:"download",0' in out:
+        print("done, successfully loaded")
+        break
 
-  def disable(self, profile):
-    pass
+  def enable(self, iccid):
+    self.at(f'AT+QESIM="enable","{iccid}"')
 
-  def delete(self, profile):
-    pass
+  def disable(self, iccid):
+    self.at(f'AT+QESIM="disable","{iccid}"')
+
+  def delete(self, iccid):
+    self.at(f'AT+QESIM="delete","{iccid}"')
 
   def list_profiles(self):
     out = self.at('AT+QESIM="list"')
@@ -127,6 +103,7 @@ if __name__ == "__main__":
   subprocess.check_call("/usr/comma/lte/lte.sh start", shell=True)
   while not os.path.exists('/dev/ttyUSB2'):
     time.sleep(1)
+  time.sleep(3)
 
   lpa = LPA()
   print(lpa.list_profiles())
