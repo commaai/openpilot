89a505c088c617e1ead775e74eb755a94c3c8ba5
Kacper Rączy <gfw.kra@gmail.com>
2023-11-02 16:52:29 -0700

    stream updates following builders

--- tools/bodyteleop/webrtc/stream.py
+++ tools/bodyteleop/webrtc/stream.py
@@ -11,84 +11,52 @@ from typing import Callable, Awaitable, List, Any, Optional
 @dataclasses.dataclass
 class StreamingOffer:
   sdp: str
-  type: str
   video: List[str]
-  audio: bool
+
+
+class StreamingConfig:
+  expected_camera_tracks: List[str]
+  expected_audio_track: bool
+  incoming_audio_track: bool
+  incoming_datachannel: bool
+
+
+def config_from_offer(offer: StreamingOffer) -> StreamingConfig:
+  desc = aiortc.sdp.SessionDescription.parse(offer.sdp)
+  audio_tracks = [m for m in desc.media if m.kind == "audio"]
+  video_tracks = [m for m in desc.media if m.kind == "video" and m.direction in ["recvonly", "sendrecv"]]
+  application_tracks = [m for m in desc.media if m.kind == "application"]
+  has_incoming_audio_track = next((t for t in audio_tracks if t.direction in ["sendonly", "sendrecv"]), None) is not None
+  has_incoming_datachannel = len(application_tracks) > 0
+  expects_outgoing_audio_track = next((t for t in audio_tracks if t.direction in ["recvonly", "sendrecv"]), None) is not None
+  assert len(video_tracks) == len(offer.video), "Number of received video tracks does not match offer"
+
+  return StreamingConfig(offer.video, expects_outgoing_audio_track, has_incoming_audio_track, has_incoming_datachannel)
 
 
 ConnectionProvider = Callable[[StreamingOffer], Awaitable[aiortc.RTCSessionDescription]]
-
-
-class WebRTCStreamBuilder:
-  def __init__(self):
-    self.consumed_camera_tracks = set()
-    self.consume_audio = False
-    self.video_producing_tracks = []
-    self.audio_producing_tracks = []
-    self.data_channel = False
-    self.peer_connection = None
-
-  def add_video_consumer(self, camera_type):
-    assert camera_type in ["driver", "wideRoad", "road"]
-
-    self.consumed_camera_tracks.add(camera_type)
-
-  def add_audio_consumer(self):
-    self.consume_audio = True
-
-  def add_video_producer(self, track: aiortc.MediaStreamTrack):
-    assert track.kind == "video"
-    self.video_producing_tracks.append(track)
-
-  def add_audio_producer(self, track: aiortc.MediaStreamTrack):
-    assert track.kind == "audio"
-    self.audio_producing_tracks.append(track)
-
-  def add_messaging(self):
-    self.data_channel = True
-
-  def offer(self, connection_provider: ConnectionProvider):
-    return WebRTCOfferStream(
-      connection_provider,
-      self.consumed_camera_tracks,
-      self.consume_audio,
-      self.data_channel,
-      self.video_producing_tracks,
-      self.audio_producing_tracks
-    )
-
-  def answer(self, session: aiortc.RTCSessionDescription):
-    return WebRTCAnswerStream(
-      session,
-      self.consumed_camera_tracks,
-      self.consume_audio,
-      self.data_channel,
-      self.video_producing_tracks,
-      self.audio_producing_tracks
-    )
 
 
 class WebRTCBaseStream(abc.ABC):
   def __init__(self,
                consumed_camera_types: List[str],
                consume_audio: bool,
-               enable_messaging: bool,
                video_producer_tracks: List[aiortc.MediaStreamTrack],
-               audio_producer_tracks: List[aiortc.MediaStreamTrack]):
+               audio_producer_tracks: List[aiortc.MediaStreamTrack],
+               add_data_channel: bool):
     self.peer_connection = aiortc.RTCPeerConnection()
     self.media_relay = MediaRelay()
     self.expected_incoming_camera_types = consumed_camera_types
     self.expected_incoming_audio = consume_audio
-    self.expected_number_tracks_or_channels = None
+    self.expected_number_of_incoming_media = None
 
     self.incoming_camera_tracks = dict()
     self.incoming_audio_tracks = []
     self.outgoing_video_tracks = video_producer_tracks
     self.outgoing_audio_tracks = audio_producer_tracks
 
-    self.enable_messaging = enable_messaging
+    self.add_data_channel = add_data_channel
     self.messaging_channel = None
-    self.messaging_channel_is_incoming = False
     self.incoming_message_handlers = []
 
     self.incoming_media_ready_event = asyncio.Event()
@@ -99,13 +67,16 @@ class WebRTCBaseStream(abc.ABC):
     self.peer_connection.on("track", self._on_incoming_track)
 
     self.logger = logging.getLogger("WebRTCStream")
+    self.logger.setLevel(logging.DEBUG)
 
   def _log_debug(self, msg: Any, *args):
     self.logger.debug(f"{type(self)}() {msg}", *args)
 
   @property
   def _number_of_incoming_media(self):
-    return len(self.incoming_camera_tracks) + len(self.incoming_audio_tracks) + int(self.messaging_channel_is_incoming)
+    media = len(self.incoming_camera_tracks) + len(self.incoming_audio_tracks)
+    media += int(self.messaging_channel is not None) if not self.add_data_channel else 0
+    return media
 
   def _add_consumer_transceivers(self):
     for _ in self.expected_incoming_camera_types:
@@ -125,9 +96,6 @@ class WebRTCBaseStream(abc.ABC):
   def _add_messaging_channel(self, channel: Optional[aiortc.RTCDataChannel] = None):
     if not channel:
       channel = self.peer_connection.createDataChannel("data", ordered=True)
-      self.messaging_channel_is_incoming = False
-    else:
-      self.messaging_channel_is_incoming = True
 
     for handler in self.incoming_message_handlers:
       channel.on("message", self._create_channel_handler_wrapper(channel, handler))
@@ -171,20 +139,26 @@ class WebRTCBaseStream(abc.ABC):
     self._on_after_media()
 
   def _on_after_media(self):
-    if self._number_of_incoming_media == self.expected_number_tracks_or_channels:
+    if self._number_of_incoming_media == self.expected_number_of_incoming_media:
       self.incoming_media_ready_event.set()
 
   def _parse_incoming_streams(self, remote_sdp: str):
     desc = aiortc.sdp.SessionDescription.parse(remote_sdp)
     sending_medias = [m for m in desc.media if m.direction in ["sendonly", "sendrecv"]]
-    channel_medias = [m for m in desc.media if m.kind == "application"]
-    self.expected_number_tracks_or_channels = len(sending_medias) + len(channel_medias)
+    incoming_media_count = len(sending_medias)
+    if not self.add_data_channel:
+      channel_medias = [m for m in desc.media if m.kind == "application"]
+      incoming_media_count += len(channel_medias)
+    self.expected_number_of_incoming_media = incoming_media_count
 
   def has_incoming_video_track(self, camera_type: str) -> bool:
     return camera_type in self.incoming_camera_tracks
 
   def has_incoming_audio_track(self) -> bool:
     return len(self.incoming_audio_tracks) > 0
+
+  def has_messaging_channel(self) -> bool:
+    return self.messaging_channel is not None
 
   def get_incoming_video_track(self, camera_type: str, buffered: bool):
     assert camera_type in self.incoming_camera_tracks, "Video tracks are not enabled on this stream"
@@ -203,7 +177,7 @@ class WebRTCBaseStream(abc.ABC):
     return relay_track
 
   def get_messaging_channel(self):
-    assert self.enable_messaging, "Messaging is not enabled on this stream"
+    assert self.messaging_channel is not None, "Messaging channel is not enabled on this stream"
     assert self.is_started, "Stream must be started"
 
     return self.messaging_channel
@@ -223,7 +197,7 @@ class WebRTCBaseStream(abc.ABC):
     await self.connection_attempted_event.wait()
     if self.peer_connection.connectionState != 'connected':
       raise ValueError("Connection failed.")
-    if self.expected_number_tracks_or_channels:
+    if self.expected_number_of_incoming_media:
       await self.incoming_media_ready_event.wait()
 
   async def stop(self):
@@ -241,7 +215,7 @@ class WebRTCOfferStream(WebRTCBaseStream):
 
   async def start(self):
     self._add_consumer_transceivers()
-    if self.enable_messaging:
+    if self.add_data_channel:
       self._add_messaging_channel()
     self._add_producer_tracks()
 
@@ -251,9 +225,7 @@ class WebRTCOfferStream(WebRTCBaseStream):
 
     streaming_offer = StreamingOffer(
       sdp=actual_offer.sdp,
-      type=actual_offer.type,
       video=list(self.expected_incoming_camera_types),
-      audio=self.expected_incoming_audio,
     )
     remote_answer = await self.session_provider(streaming_offer)
     self._parse_incoming_streams(remote_sdp=remote_answer.sdp)
@@ -283,7 +255,6 @@ class WebRTCAnswerStream(WebRTCBaseStream):
       if m.kind != "video":
         continue
 
-
       preferred_codecs = [c for c in m.rtp.codecs if c.mimeType in preferred_codec_mimes]
       if len(preferred_codecs) == 0:
         raise ValueError(f"None of {preferred_codecs} codecs is supported in remote SDP")
@@ -296,6 +267,8 @@ class WebRTCAnswerStream(WebRTCBaseStream):
   async def start(self):
     assert self.peer_connection.remoteDescription is None, "Connection already established"
 
+    self._add_consumer_transceivers()
+
     # since we sent already encoded frames in some cases (e.g. livestream video tracks are in H264), we need to force aiortc to actually use it
     # we do that by overriding supported codec information on incoming sdp
     preferred_codecs = self._probe_video_codecs()
@@ -305,9 +278,6 @@ class WebRTCAnswerStream(WebRTCBaseStream):
     self._parse_incoming_streams(remote_sdp=self.session.sdp)
     await self.peer_connection.setRemoteDescription(self.session)
 
-    self._add_consumer_transceivers()
-    if self.enable_messaging:
-      self._add_messaging_channel()
     self._add_producer_tracks()
 
     answer = await self.peer_connection.createAnswer()
