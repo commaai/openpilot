b791c0296b5a57ab031cbae924446ec732eecb9f
Justin Newberry <justin@comma.ai>
2023-11-05 01:09:43 -0400

    Subaru: 2016 legacy fingerprint (#30386)
    legacy fw

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -470,6 +470,7 @@ FW_VERSIONS = {
       b'[\xba\xc4\x03',
       b'{\x97D\x00',
       b'[\x97D\000',
+      b'k\x9aD\x00',
     ],
     (Ecu.eps, 0x746, None): [
       b'[\xb0\x00\x01',
@@ -486,12 +487,14 @@ FW_VERSIONS = {
       b'\xa0+@p\x07',
       b'\xb4"@0\x07',
       b'\xa0"@q\a',
+      b'\xab+@p\x07',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xbe\xf2\x00p\x00',
       b'\xbf\xfb\xc0\x80\x00',
       b'\xbd\xf2\x00`\x00',
       b'\xbf\xf2\000\x80\000',
+      b'\xbe\xfb\xc0p\x00',
     ],
   },
   CAR.OUTBACK_PREGLOBAL: {
