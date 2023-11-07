713e297d5d13a935b1fb53a715d7d035b84bdc7a
Kacper Rączy <gfw.kra@gmail.com>
2023-11-07 15:37:33 -0800

    Update js files to at least support what worked previously

--- tools/bodyteleop/static/js/jsmain.js
+++ tools/bodyteleop/static/js/jsmain.js
@@ -9,10 +9,14 @@ document.addEventListener('keyup', (e)=>(handleKeyX(e.key.toLowerCase(), 0)));
 $(".keys").bind("mousedown touchstart", (e)=>handleKeyX($(e.target).attr('id').replace('key-', ''), 1));
 $(".keys").bind("mouseup touchend", (e)=>handleKeyX($(e.target).attr('id').replace('key-', ''), 0));
 $("#plan-button").click(executePlan);
+$(".sound").click((e)=>{
+  const sound = $(e.target).attr('id').replace('sound-', '')
+  return playSoundRequest(sound);
+});
 
 setInterval( () => {
   const dt = new Date().getTime();
-  if ((dt - last_ping) > 1000) {
+  if ((dt - lastPing) > 1000) {
     $(".pre-blob").removeClass('blob');
     $("#battery").text("-");
     $("#ping-time").text('-');
