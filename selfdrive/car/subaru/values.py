60b21b55d28473046825346845b609a5324d945c
Justin Newberry <justin@comma.ai>
2023-11-07 01:13:39 -0500

    Subaru: initial eyesight disable support (#30373)
    * initial eyesight disable
    
    * disable_eyesight
    
    * comment

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -55,6 +55,11 @@ class CarControllerParams:
 
 class SubaruFlags(IntFlag):
   SEND_INFOTAINMENT = 1
+  DISABLE_EYESIGHT = 2
+
+
+GLOBAL_ES_ADDR = 0x787
+GEN2_ES_BUTTONS_DID = b'\x11\x30'
 
 
 class CanBus:
