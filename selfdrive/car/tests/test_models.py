98c763fcf60182726c1a42471de3d12b50d82054
Shane Smiskol <shane@smiskol.com>
2023-11-15 18:58:54 -0800

    right

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -125,9 +125,6 @@ class TestCarModelBase(unittest.TestCase):
         # Log which can frame the panda safety mode left ELM327, for CAN validity checks
         elif msg.which() == 'pandaStates':
           for ps in msg.pandaStates:
-            if cls.car_safety_mode_frame is None and ps.safetyModel not in \
-              (SafetyModel.elm327, SafetyModel.noOutput):
-              cls.car_safety_mode_frame = len(can_msgs)
             if cls.elm_frame is None and ps.safetyModel != SafetyModel.elm327:
               cls.elm_frame = len(can_msgs)
             if cls.car_safety_mode_frame is None and ps.safetyModel not in \
@@ -135,9 +132,6 @@ class TestCarModelBase(unittest.TestCase):
               cls.car_safety_mode_frame = len(can_msgs)
 
         elif msg.which() == 'pandaStateDEPRECATED':
-          if cls.car_safety_mode_frame is None and msg.pandaStateDEPRECATED.safetyModel not in \
-            (SafetyModel.elm327, SafetyModel.noOutput):
-            cls.car_safety_mode_frame = len(can_msgs)
           if cls.elm_frame is None and msg.pandaStateDEPRECATED.safetyModel != SafetyModel.elm327:
             cls.elm_frame = len(can_msgs)
           if cls.car_safety_mode_frame is None and msg.pandaStateDEPRECATED.safetyModel not in \
