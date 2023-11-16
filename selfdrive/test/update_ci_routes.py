6ce34a0fb8ac9e2da200bd0a7c190d2a17f24479
Shane Smiskol <shane@smiskol.com>
2023-11-15 19:52:41 -0800

    check exist

--- selfdrive/test/update_ci_routes.py
+++ selfdrive/test/update_ci_routes.py
@@ -24,7 +24,7 @@ DATA_PROD_CONTAINER = "commadata2"
 # TODO: why is DATA_CI_ACCOUNT in this list? it will just copy from itself to itself...
 SOURCES = [
   (DATA_PROD_ACCOUNT, DATA_PROD_CONTAINER),
-  (DATA_CI_ACCOUNT, DATA_CI_CONTAINER),
+  # (DATA_CI_ACCOUNT, DATA_CI_CONTAINER),
 ]
 
 
@@ -39,12 +39,12 @@ def strip_log_data(data: bytes) -> bytes:
 
 
 @lru_cache
-def get_azure_keys():
-  source_container = ContainerClient(f"https://{DATA_PROD_ACCOUNT}.blob.core.windows.net", DATA_PROD_CONTAINER, credential=get_azure_credential())
+def get_azure_containers():
+  source_containers = []
+  for source_account, source_bucket in SOURCES:
+    source_containers.append(ContainerClient(f"https://{source_account}.blob.core.windows.net", source_bucket, credential=get_azure_credential()))
   dest_container = ContainerClient(DATA_CI_ACCOUNT_URL, DATA_CI_CONTAINER, credential=get_azure_credential())
-  dest_key = get_container_sas(DATA_CI_ACCOUNT, DATA_CI_CONTAINER)
-  source_keys = [get_container_sas(*s) for s in SOURCES]
-  return source_container, dest_container, dest_key, source_keys
+  return source_containers, dest_container
 
 
 def upload_route(path: str, exclude_patterns: Optional[Iterable[str]] = None) -> None:
@@ -68,36 +68,43 @@ def upload_route(path: str, exclude_patterns: Optional[Iterable[str]] = None) ->
 
 
 def sync_to_ci_public(route: str, strip_data: bool = False) -> bool:
-  source_container, dest_container, dest_key, source_keys = get_azure_keys()
+  source_containers, dest_container = get_azure_containers()
   key_prefix = route.replace('|', '/')
   dongle_id = key_prefix.split('/')[0]
 
-  if next(dest_container.list_blob_names(name_starts_with=key_prefix), None) is not None:# and route != 'ad5a3fa719bc2f83|2023-10-17--19-48-42':
+  if next(dest_container.list_blob_names(name_starts_with=key_prefix), None) is not None and route != 'ad5a3fa719bc2f83|2023-10-17--19-48-42':
     print('Already exists in dest container:', route)
     return True
 
-  # Get all blobs (rlogs) for this route, strip personally identifiable data, and upload to CI
   print(f"Downloading {route}")
-  blobs = list(source_container.list_blob_names(name_starts_with=key_prefix))
-  blobs = [b for b in blobs if not re.match(r'.*/dcamera.hevc', b)]
-  if not len(blobs):
+  for source_container in source_containers:
+    print(f'Trying {source_container.container_name}')
+    # Get all blobs (rlogs) for this route, strip personally identifiable data, and upload to CI
+    blobs = list(source_container.list_blob_names(name_starts_with=key_prefix))
+    blobs = [b for b in blobs if not re.match(r'.*/dcamera.hevc', b)]
+    print('blobs', blobs)
+    if len(blobs):
+      break
+  else:
     print(f'No segments found in source container: {DATA_PROD_ACCOUNT}')
     return False
-  print('blobs', blobs)
 
   fail = False
   for blob_name in tqdm(blobs):
+    # don't overwrite existing blobs, skip if exists
+    if dest_container.get_blob_client(blob_name).exists():
+      print('Already exists in dest container:', blob_name)
+      # TODO: did this fail before?
+      continue
+
     # print('deleting', blob_name, 'from container', dest_container.container_name)
     # dest_container.delete_blob(blob_name)
     data = source_container.download_blob(blob_name).readall()
     if strip_data:
       data = strip_log_data(data)
 
-    try:
-      dest_container.upload_blob(blob_name, data)
-    except azure.core.exceptions.ResourceExistsError:
-      print('Already exists in dest container:', blob_name)
-      fail = True
+    print(f'Uploading {blob_name} to {dest_container.container_name}')
+    dest_container.upload_blob(blob_name, data)
 
   print("Failed" if fail else "Success")
   return not fail
