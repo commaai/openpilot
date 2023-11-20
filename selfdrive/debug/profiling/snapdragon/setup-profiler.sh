233a8e0dd32c804e37a1b89e166128ed4d370efd
Justin Newberry <justin@comma.ai>
2023-11-20 10:51:42 -0800

    fix that

--- selfdrive/debug/profiling/snapdragon/setup-profiler.sh
+++ selfdrive/debug/profiling/snapdragon/setup-profiler.sh
@@ -2,19 +2,13 @@
 
 # install depends
 sudo apt update
-sudo apt-get install -y libc++1 libc++abi1 default-jre android-tools-adb libgtk-3-0
+sudo apt-get install libc++1 libc++abi1 default-jre android-tools-adb gtk-sharp2
 
 # setup mono
-sudo apt install ca-certificates gnupg
-sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt install apt-transport-https ca-certificates
+echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-sudo apt install -y mono-complete
-
-# download  nuget
-sudo wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -O ./nuget.exe
-
-# install depends
-mono nuget.exe install gtksourcesharp -Version 3.24.24.38 -OutputDirectory ~/.local/share/SDP
+sudo apt-get install -y mono-complete
 
 echo "Setup successful, you should now be able to run the profiler with cd SnapdragonProfiler and ./run_sdp.sh"
