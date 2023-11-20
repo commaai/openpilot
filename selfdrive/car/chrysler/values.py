2be6ee20219fca29b5ad359294d56aa38965ecef
Greg Hogan <gregjhogan@gmail.com>
2023-11-20 09:49:09 -0800

    chrysler: increase RAM DT max torque to safety limit

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -44,7 +44,7 @@ class CarControllerParams:
     elif CP.carFingerprint in RAM_DT:
       self.STEER_DELTA_UP = 6
       self.STEER_DELTA_DOWN = 6
-      self.STEER_MAX = 261  # EPS allows more, up to 350?
+      self.STEER_MAX = 350
     else:
       self.STEER_DELTA_UP = 3
       self.STEER_DELTA_DOWN = 3
