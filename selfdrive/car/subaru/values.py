afaddc9ef5d0eb40f2d6a778f35d351f4e54ac12
Justin Newberry <justin@comma.ai>
2023-11-29 14:53:43 -0800

    Subaru: add 2023 forester (#30554)
    23
    old-commit-hash: 44ef143ae7cc855d6f4ce4b8328b90ab4f125fec

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -126,7 +126,7 @@ CAR_INFO: Dict[str, Union[SubaruCarInfo, List[SubaruCarInfo]]] = {
   CAR.LEGACY_PREGLOBAL: SubaruCarInfo("Subaru Legacy 2015-18"),
   CAR.OUTBACK_PREGLOBAL: SubaruCarInfo("Subaru Outback 2015-17"),
   CAR.OUTBACK_PREGLOBAL_2018: SubaruCarInfo("Subaru Outback 2018-19"),
-  CAR.FORESTER_2022: SubaruCarInfo("Subaru Forester 2022", "All", car_parts=CarParts.common([CarHarness.subaru_c])),
+  CAR.FORESTER_2022: SubaruCarInfo("Subaru Forester 2022-23", "All", car_parts=CarParts.common([CarHarness.subaru_c])),
   CAR.OUTBACK_2023: SubaruCarInfo("Subaru Outback 2023", "All", car_parts=CarParts.common([CarHarness.subaru_d])),
   CAR.ASCENT_2023: SubaruCarInfo("Subaru Ascent 2023", "All", car_parts=CarParts.common([CarHarness.subaru_d])),
 }
@@ -671,7 +671,8 @@ FW_VERSIONS = {
       b'=\xc04\x02',
     ],
     (Ecu.fwdCamera, 0x787, None): [
-      b'\x04!\x01\x1eD\x07!\x00\x04,'
+      b'\x04!\x01\x1eD\x07!\x00\x04,',
+      b'\x04!\x08\x01.\x07!\x08\x022',
     ],
     (Ecu.engine, 0x7e0, None): [
       b'\xd5"a0\x07',
