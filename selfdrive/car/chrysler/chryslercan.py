fbebfd4ca81b9a3197630149cbc18ec9e055417f
Jason Young <jyoung8607@gmail.com>
2023-12-05 01:20:40 -0500

    only send LKAS_CONTROL_BIT for non-CUSW

--- selfdrive/car/chrysler/chryslercan.py
+++ selfdrive/car/chrysler/chryslercan.py
@@ -1,5 +1,5 @@
 from cereal import car
-from openpilot.selfdrive.car.chrysler.values import RAM_CARS
+from openpilot.selfdrive.car.chrysler.values import RAM_CARS, CUSW_CARS
 
 GearShifter = car.CarState.GearShifter
 VisualAlert = car.CarControl.HUDControl.VisualAlert
@@ -53,12 +53,15 @@ def create_lkas_hud(packer, CP, lkas_active, hud_alert, hud_count, car_model, au
 
 
 def create_lkas_command(packer, CP, apply_steer, lkas_control_bit):
-  # LKAS_COMMAND Lane-keeping signal to turn the wheel
-  enabled_val = 2 if CP.carFingerprint in RAM_CARS else 1
   values = {
     "STEERING_TORQUE": apply_steer,
-    "LKAS_CONTROL_BIT": enabled_val if lkas_control_bit else 0,
   }
+
+  if CP.carFingerprint not in CUSW_CARS:
+    enabled_val = 2 if CP.carFingerprint in RAM_CARS else 1
+    values.append({
+      "LKAS_CONTROL_BIT": enabled_val if lkas_control_bit else 0,
+    })
   return packer.make_can_msg("LKAS_COMMAND", 0, values)
 
 
