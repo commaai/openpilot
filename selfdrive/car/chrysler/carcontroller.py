3dfe78f8ac0bff27aa7585861cb699c6d197a157
Jason Young <jyoung8607@gmail.com>
2023-12-05 14:20:27 -0500

    another DAS_6 fix

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -2,7 +2,7 @@ from opendbc.can.packer import CANPacker
 from openpilot.common.realtime import DT_CTRL
 from openpilot.selfdrive.car import apply_meas_steer_torque_limits
 from openpilot.selfdrive.car.chrysler import chryslercan
-from openpilot.selfdrive.car.chrysler.values import RAM_CARS, CarControllerParams, ChryslerFlags
+from openpilot.selfdrive.car.chrysler.values import RAM_CARS, CarControllerParams, ChryslerFlags, CUSW_CARS
 
 
 class CarController:
@@ -40,7 +40,7 @@ class CarController:
 
     # HUD alerts
     if self.frame % 25 == 0:
-      if CS.lkas_car_model != -1:
+      if self.CP.carFingerprint in CUSW_CARS or CS.lkas_car_model != -1:
         can_sends.append(chryslercan.create_lkas_hud(self.packer, self.CP, lkas_active, CC.hudControl.visualAlert,
                                                      self.hud_count, CS.lkas_car_model, CS.auto_high_beam))
         self.hud_count += 1
