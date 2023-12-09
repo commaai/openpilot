27c0048d7b744d51bd6b6c17175c245ed99127f2
Shane Smiskol <shane@smiskol.com>
2023-12-09 14:36:40 -0800

    Apply suggestions from code review

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1157,13 +1157,13 @@ FW_VERSIONS = {
       b'\xf1\x00CK  MFC  AT AUS RHD 1.00 1.00 99211-J5500 210622',
       b'\xf1\x00CK  MFC  AT KOR LHD 1.00 1.00 99211-J5500 210622',
       b'\xf1\x00CK  MFC  AT USA LHD 1.00 1.00 99211-J5500 210622',
-      b'\xf1\x00CK  MFC  AT USA LHD 1.00 1.03 99211-J5000 201209'
+      b'\xf1\x00CK  MFC  AT USA LHD 1.00 1.03 99211-J5000 201209',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xf1\x87VCNLF11383972DK1vffV\x99\x99\x89\x98\x86eUU\x88wg\x89vfff\x97fff\x99\x87o\xff"\xc1\xf1\x81E30\x00\x00\x00\x00\x00\x00\x00\xf1\x00bcsh8p54  E30\x00\x00\x00\x00\x00\x00\x00SCK0T33GH0\xbe`\xfb\xc6',
       b'\xf1\x00bcsh8p54  E31\x00\x00\x00\x00\x00\x00\x00SCK0T33NH07\xdf\xf0\xc1',
       b'\xf1\x00bcsh8p54  E31\x00\x00\x00\x00\x00\x00\x00SCK0T25KH2B\xfbI\xe2',
-      b'\xf1\x00bcsh8p54  E31\x00\x00\x00\x00\x00\x00\x00TCK0T33NH0%g~\xd3'
+      b'\xf1\x00bcsh8p54  E31\x00\x00\x00\x00\x00\x00\x00TCK0T33NH0%g~\xd3',
     ],
   },
   CAR.PALISADE: {
