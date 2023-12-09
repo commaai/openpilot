fcd8b2b3eccb82076d19881368f7727af19c05d5
Shane Smiskol <shane@smiskol.com>
2023-12-09 05:42:57 -0800

    is jenkins on my branch?

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -319,6 +319,7 @@ class TestCarModelBase(unittest.TestCase):
   @given(data=st.data())
   @seed(1)  # for reproduction
   def test_panda_safety_carstate_fuzzy(self, data):
+    raise Exception
     if self.CP.enableGasInterceptor:  # TODO known failure for now
       raise unittest.SkipTest
     """
