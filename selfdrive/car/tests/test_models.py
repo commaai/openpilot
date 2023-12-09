f5497028bd566eda8762fbd08b69c8b8491ce052
Shane Smiskol <shane@smiskol.com>
2023-12-09 05:46:17 -0800

    50 is not bad for GH CI

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -36,7 +36,7 @@ NUM_JOBS = int(os.environ.get("NUM_JOBS", "1"))
 JOB_ID = int(os.environ.get("JOB_ID", "0"))
 INTERNAL_SEG_LIST = os.environ.get("INTERNAL_SEG_LIST", "")
 INTERNAL_SEG_CNT = int(os.environ.get("INTERNAL_SEG_CNT", "0"))
-MAX_EXAMPLES = int(os.environ.get('MAX_EXAMPLES', '20'))  # TODO 20 is not a lot at all, measure time in ci
+MAX_EXAMPLES = int(os.environ.get('MAX_EXAMPLES', '50'))
 
 
 def get_test_cases() -> List[Tuple[str, Optional[CarTestRoute]]]:
