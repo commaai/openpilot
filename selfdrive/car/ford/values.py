874ff88c8d9e7fd74185d0a402151d4ff3b7a3ec
Shane Smiskol <shane@smiskol.com>
2023-12-12 00:33:22 -0800

    add correct harness to f150 and angled mount

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -77,8 +77,11 @@ class FordCarInfo(CarInfo):
   car_parts: CarParts = field(default_factory=CarParts.common([CarHarness.ford_q3]))
 
   def init_make(self, CP: car.CarParams):
-    if CP.carFingerprint in (CAR.BRONCO_SPORT_MK1, CAR.MAVERICK_MK1):
-      self.car_parts = CarParts([Device.threex_angled_mount, CarHarness.ford_q3])
+    harness = CarHarness.ford_q4 if CP.carFingerprint in CANFD_CAR else CarHarness.ford_q3
+    if CP.carFingerprint in (CAR.BRONCO_SPORT_MK1, CAR.MAVERICK_MK1, CAR.F_150_MK14):
+      self.car_parts = CarParts([Device.threex_angled_mount, harness])
+    else:
+      self.car_parts = CarParts([Device.threex, harness])
 
 
 CAR_INFO: Dict[str, Union[CarInfo, List[CarInfo]]] = {
