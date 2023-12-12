2b449e578fd6455514e0ce1c146364eda437208c
Justin Newberry <justin@comma.ai>
2023-12-11 17:35:47 -0800

    no carla

--- tools/sim/run_bridge.py
+++ tools/sim/run_bridge.py
@@ -6,7 +6,6 @@ from typing import Any
 from multiprocessing import Queue
 
 from openpilot.tools.sim.bridge.common import SimulatorBridge
-from openpilot.tools.sim.bridge.carla.carla_bridge import CarlaBridge
 from openpilot.tools.sim.bridge.metadrive.metadrive_bridge import MetaDriveBridge
 
 
@@ -17,12 +16,6 @@ def parse_args(add_args=None):
   parser.add_argument('--dual_camera', action='store_true')
   parser.add_argument('--simulator', dest='simulator', type=str, default='metadrive')
 
-  # Carla specific
-  parser.add_argument('--town', type=str, default='Town04_Opt')
-  parser.add_argument('--spawn_point', dest='num_selected_spawn_point', type=int, default=16)
-  parser.add_argument('--host', dest='host', type=str, default=os.environ.get("CARLA_HOST", '127.0.0.1'))
-  parser.add_argument('--port', dest='port', type=int, default=2000)
-
   return parser.parse_args(add_args)
 
 if __name__ == "__main__":
@@ -30,9 +23,7 @@ if __name__ == "__main__":
   args = parse_args()
 
   simulator_bridge: SimulatorBridge
-  if args.simulator == "carla":
-    simulator_bridge = CarlaBridge(args)
-  elif args.simulator == "metadrive":
+  if args.simulator == "metadrive":
     simulator_bridge = MetaDriveBridge(args)
   else:
     raise AssertionError("simulator type not supported")
