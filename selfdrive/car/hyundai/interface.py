093062fff4cb6d86ab2231f8419bc763a3b18a8d
Shane Smiskol <shane@smiskol.com>
2023-12-11 16:09:44 -0800

    Update selfdrive/car/hyundai/interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -203,7 +203,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 16.
       ret.tireStiffnessFactor = 0.65
     elif candidate in (CAR.KIA_SPORTAGE_HYBRID_5TH_GEN, CAR.KIA_SPORTAGE_PHEV_5TH_GEN):
-      ret.mass = {CAR.KIA_SPORTAGE_PHEV_5TH_GEN: 1923., }.get(candidate, 1767.)  # HYBRID: SX Prestige trim support only
+      ret.mass = {CAR.KIA_SPORTAGE_PHEV_5TH_GEN: 1923., }.get(candidate, 1767.)
       ret.wheelbase = 2.756
       ret.steerRatio = 13.6
     elif candidate in (CAR.KIA_SORENTO_4TH_GEN, CAR.KIA_SORENTO_HEV_4TH_GEN, CAR.KIA_SORENTO_PHEV_4TH_GEN):
