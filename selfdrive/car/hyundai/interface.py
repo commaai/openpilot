7446c1b0ff7abe75d1f8abf1639be9ea18e829f8
Jason Wen <haibin.wen3@gmail.com>
2023-12-12 12:37:03 -0500

    wrong ecu

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -331,7 +331,7 @@ class CarInterface(CarInterfaceBase):
     if CP.openpilotLongitudinalControl and not (CP.flags & HyundaiFlags.CANFD_CAMERA_SCC.value):
       addr, bus = 0x7d0, 0
       if CP.flags & HyundaiFlags.CANFD_HDA2.value:
-        addr, bus = 0x730, CanBus(CP).ECAN
+        addr, bus = 0x7d0, CanBus(CP).ECAN
       disable_ecu(logcan, sendcan, bus=bus, addr=addr, com_cont_req=b'\x28\x83\x01')
 
     # for blinkers
