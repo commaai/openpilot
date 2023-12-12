8703c3255376b85360a9ad2893536e8a32e50c6d
Shane Smiskol <shane@smiskol.com>
2023-12-11 20:50:27 -0800

    try this?

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -40,14 +40,14 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_steer):
   ret = []
 
   values = {
-    "LKA_MODE": 2,
+    "LKA_MODE": 0,
     "LKA_ICON": 2 if enabled else 1,
     "TORQUE_REQUEST": apply_steer,
     "LKA_ASSIST": 0,
     "STEER_REQ": 1 if lat_active else 0,
     "STEER_MODE": 0,
     "HAS_LANE_SAFETY": 0,  # hide LKAS settings
-    "NEW_SIGNAL_1": 0,
+    "NEW_SIGNAL_1": 3 if lat_active else 0,  # TODO: can be other values
     "NEW_SIGNAL_2": 0,
   }
 
