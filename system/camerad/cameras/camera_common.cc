68370db0c17517063dd97330de16fdc851e48f20
deanlee <deanlee3@gmail.com>
2023-12-12 15:08:46 +0800

    cleanup includes

--- system/camerad/cameras/camera_common.cc
+++ system/camerad/cameras/camera_common.cc
@@ -1,23 +1,16 @@
 #include "system/camerad/cameras/camera_common.h"
 
-#include <unistd.h>
+#include <jpeglib.h>
 
 #include <cassert>
-#include <cstdio>
-#include <chrono>
 #include <string>
-#include <thread>
-
-#include "third_party/libyuv/include/libyuv.h"
-#include <jpeglib.h>
 
 #include "common/clutil.h"
 #include "common/swaglog.h"
 #include "common/util.h"
-#include "system/hardware/hw.h"
-#include "third_party/linux/include/msm_media_info.h"
-
 #include "system/camerad/cameras/camera_qcom2.h"
+#include "third_party/libyuv/include/libyuv.h"
+#include "third_party/linux/include/msm_media_info.h"
 #ifdef QCOM2
 #include "CL/cl_ext_qcom.h"
 #endif
