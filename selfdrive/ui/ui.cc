62525a60ee37ec0f7c2e78d622d746c5cb78c2b1
Dean Lee <deanlee3@gmail.com>
2023-12-14 06:51:09 +0800

    ui/onroad: avoid drawing invalid lanes and leads (#29666)
    old-commit-hash: 33987d4cc9a6de80d6f9b3a4bae7a159d182620a

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -207,6 +207,12 @@ static void update_state(UIState *s) {
     scene.light_sensor = std::max(100.0f - scale * cam_state.getExposureValPercent(), 0.0f);
   }
   scene.started = sm["deviceState"].getDeviceState().getStarted() && scene.ignition;
+
+  scene.world_objects_visible = scene.world_objects_visible ||
+                                (scene.started &&
+                                 sm.rcv_frame("liveCalibration") > scene.started_frame &&
+                                 sm.rcv_frame("modelV2") > scene.started_frame &&
+                                 sm.rcv_frame("uiPlan") > scene.started_frame);
 }
 
 void ui_update_params(UIState *s) {
@@ -233,6 +239,7 @@ void UIState::updateStatus() {
       scene.started_frame = sm->frame;
     }
     started_prev = scene.started;
+    scene.world_objects_visible = false;
     emit offroadTransition(!scene.started);
   }
 }
--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -158,6 +158,7 @@ typedef struct UIScene {
 
   float light_sensor;
   bool started, ignition, is_metric, map_on_left, longitudinal_control;
+  bool world_objects_visible = false;
   uint64_t started_frame;
 } UIScene;
 
@@ -167,9 +168,6 @@ class UIState : public QObject {
 public:
   UIState(QObject* parent = 0);
   void updateStatus();
-  inline bool worldObjectsVisible() const {
-    return sm->rcv_frame("liveCalibration") > scene.started_frame;
-  }
   inline bool engaged() const {
     return scene.started && (*sm)["controlsState"].getControlsState().getEnabled();
   }
