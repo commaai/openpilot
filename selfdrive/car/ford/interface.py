c74a312e007e6a5d750267148ac02937f8a2fd8f
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2023-12-14 15:41:27 -0500

    Update interface.py

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -35,6 +35,16 @@ class CarInterface(CarInterfaceBase):
     if candidate in CANFD_CAR:
       ret.safetyConfigs[-1].safetyParam |= Panda.FLAG_FORD_CANFD
 
+    # We have tested these values with CAN-FD Ford. This can be included for all vehicles after 
+    # we have validated that CAN (Q3) Ford users also enjoy these changes.
+    # This prevents massive amounts of brake pumping when coming to a stop.
+    if candidate in CANFD_CAR:
+      ret.longitudinalTuning.kpBP = [0.]
+      ret.longitudinalTuning.kpV = [0.5]
+      ret.longitudinalTuning.kiV = [0.]
+      ret.longitudinalTuning.deadzoneBP = [0., 9.]
+      ret.longitudinalTuning.deadzoneV = [.0, .20]
+    
     if candidate == CAR.BRONCO_SPORT_MK1:
       ret.wheelbase = 2.67
       ret.steerRatio = 17.7
@@ -76,19 +86,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 3.076
       ret.steerRatio = 17.0
       ret.mass = 1650
-      
-    # We have tested these values with CAN-FD Ford. This can be included for all vehicles after 
-    # we have validated that CAN (Q3) Ford users also enjoy these changes.
-    # This prevents massive amounts of brake pumping when coming to a stop.
-    if candidate in CANFD_CAR:
-      ret.longitudinalTuning.kpBP = [0.]
-      ret.longitudinalTuning.kpV = [0.5]
-      ret.longitudinalTuning.kiV = [0.]
-      ret.longitudinalTuning.deadzoneBP = [0., 9.]
-      ret.longitudinalTuning.deadzoneV = [.0, .20]
-      
-
-
+    
     else:
       raise ValueError(f"Unsupported car: {candidate}")
 
