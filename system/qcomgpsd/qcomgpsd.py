2f112edb6fa7118036a8a3f13201a6e52c385847
royjr <royjr96@gmail.com>
2023-12-17 03:10:51 -0500

    Revert "requests -> urllib3"
    This reverts commit 7ca39e618917986f50356519d2795a432e3a5280.

--- system/qcomgpsd/qcomgpsd.py
+++ system/qcomgpsd/qcomgpsd.py
@@ -5,8 +5,8 @@ import signal
 import itertools
 import math
 import time
-import urllib3
-from urllib3.exceptions import HTTPError
+import requests
+from requests.exceptions import RequestException
 import shutil
 import subprocess
 import datetime
@@ -102,23 +102,23 @@ def gps_enabled() -> bool:
     raise Exception("failed to execute QGPS mmcli command") from exc
 
 def download_assistance():
-  http = urllib3.PoolManager()
-
   try:
-    response = http.request('HEAD', ASSISTANCE_URL)
-    content_length = int(response.headers.get('content-length', 0))
+    response = requests.head(ASSISTANCE_URL, timeout=2)
+    bytes_n = int(response.headers.get('content-length', 0))
 
-    if content_length > 1e5:
+    if bytes_n > 1e5:
       cloudlog.error("Qcom assistance data larger than expected")
       return
 
-    with http.request('GET', ASSISTANCE_URL, preload_content=False) as resp, open(ASSIST_DATA_FILE_DOWNLOAD, 'wb') as out_file:
-        shutil.copyfileobj(resp, out_file)
+    response = requests.get(ASSISTANCE_URL, timeout=5, stream=True)
+    with open(ASSIST_DATA_FILE_DOWNLOAD, 'wb') as fp:
+      for chunk in response.iter_content(chunk_size=8192):
+        fp.write(chunk)
 
     os.rename(ASSIST_DATA_FILE_DOWNLOAD, ASSIST_DATA_FILE)
 
-  except HTTPError as e:
-    cloudlog.exception("Failed to download assistance file: {}".format(e))
+  except RequestException:
+    cloudlog.exception("Failed to download assistance file")
     return
 
 def downloader_loop(event):
