ab0b26d2b8dc8b3957812856917084af2cb3df0a
Dean Lee <deanlee3@gmail.com>
2023-12-18 14:42:52 +0800

    camerad:  cleanup includes (#30782)
    * cleanup includes
    
    * continue
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>

--- system/camerad/cameras/camera_common.cc
+++ system/camerad/cameras/camera_common.cc
@@ -1,12 +1,7 @@
 #include "system/camerad/cameras/camera_common.h"
 
-#include <unistd.h>
-
 #include <cassert>
-#include <cstdio>
-#include <chrono>
 #include <string>
-#include <thread>
 
 #include "third_party/libyuv/include/libyuv.h"
 #include <jpeglib.h>
@@ -14,7 +9,6 @@
 #include "common/clutil.h"
 #include "common/swaglog.h"
 #include "common/util.h"
-#include "system/hardware/hw.h"
 #include "third_party/linux/include/msm_media_info.h"
 
 #include "system/camerad/cameras/camera_qcom2.h"
