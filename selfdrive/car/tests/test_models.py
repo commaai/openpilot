901f91288202b61e1054d4bd37d8f2934716bc9c
Shane Smiskol <shane@smiskol.com>
2023-12-18 20:55:47 -0800

    cmt

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -348,6 +348,7 @@ class TestCarModelBase(unittest.TestCase):
         self.assertEqual(CS.gasPressed, self.safety.get_gas_pressed_prev())
 
       if self.safety.get_brake_pressed_prev() != prev_panda_brake:
+        # TODO: remove this exception once this mismatch is resolved
         brake_pressed = CS.brakePressed
         if CS.brakePressed and not self.safety.get_brake_pressed_prev():
           if self.CP.carFingerprint in (HONDA.PILOT, HONDA.RIDGELINE) and CS.brake > 0.05:
