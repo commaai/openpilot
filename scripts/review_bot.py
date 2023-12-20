12665098966bae429bac89b5c69660ab66c48292
user <user@archlinux>
2023-12-20 05:23:21 -0500

    moved graphql to python

--- scripts/review_bot.py
+++ scripts/review_bot.py
@@ -32,15 +32,53 @@ field_finder = re.compile(r"\*{2}(.+?)\*{2}")
 def find_field_set(content):
     return set(field_finder.findall(content))
 
+# use GraphQL to get pull request id
+def get_pull_request_graphql_id(accessToken,name,number):
+    owner,name = name.split('/')
+    query = f"""query {{
+      repository(owner:"{owner}", name:"{name}"){{
+        pullRequest(number: {number}) {{
+            id
+        }}
+      }}
+    }}"""
+    r = requests.post(endpoint, json={"query": query}, headers=headers)
+    return r.json()["data"]["repository"]["pullRequest"]["id"]
+
+# use GraphQL to set pull request as draft
+def set_pr_draft(accessToken,id):
+    query = f"""mutation {{
+        convertPullRequestToDraft(input:{{pullRequestId:"{id}"}}){{
+            pullRequest {{
+                id
+            }}
+        }}
+    }}"""
+    requests.post(endpoint, json={"query": query}, headers=headers)
+
+# use GraphQL to set pull request as ready
+def set_pr_ready(accessToken,id):
+    query = f"""mutation {{
+        markPullRequestReadyForReview(input:{{pullRequestId:"{id}"}}){{
+            pullRequest {{
+                id
+            }}
+        }}
+    }}"""
+    requests.post(endpoint, json={"query": query}, headers=headers)
+
 if __name__ == "__main__":
-    g = Github(os.environ['GITHUB_TOKEN'])
+    accessToken = os.environ['GITHUB_TOKEN']
+    g = Github(accessToken)
 
     pr_event = read_pr_event()
     repo_name = pr_event['repository']['full_name']
     pr_number = pr_event['pull_request']['number']
+    pr_id = get_pull_request_graphql_id(accessToken,repo_name,pr_number)
     pr_body = pr_event["pull_request"]["body"]
     pr = g.get_repo(repo_name).get_pull(pr_number)
     pr.add_to_labels(BOT_REVIEW_LABEL)
+    set_pr_draft(accessToken,pr_id)
 
     fields_in_pr_body = find_field_set(pr_body)
     combined_templates = read_template_file()
@@ -57,6 +95,7 @@ if __name__ == "__main__":
     if len(possible_template_matches) > 0:
         print("PR matches template(s): ",", ".join(possible_template_matches))
         pr.remove_from_labels(BOT_REVIEW_LABEL)
+        set_pr_ready(accessToken,pr_id)
         sys.exit(0) # Pass
     else:
         print("PR does not match any known templates")
