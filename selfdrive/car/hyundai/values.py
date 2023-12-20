d44e81c2727c15b2cfc43211f2b4ccf4cbe2158e
Shane Smiskol <shane@smiskol.com>
2023-12-19 21:44:46 -0800

    less torque

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -27,7 +27,7 @@ class CarControllerParams:
     self.STEER_STEP = 1  # 100 Hz
 
     if CP.carFingerprint in CANFD_CAR:
-      self.STEER_MAX = 270
+      self.STEER_MAX = 170
       self.STEER_DRIVER_ALLOWANCE = 250
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 250
