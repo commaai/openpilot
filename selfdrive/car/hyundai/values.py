dab3ff6d3d4e0846a21d9d9b3b61c384efe3fb74
Jason Wen <haibin.wen3@gmail.com>
2023-12-22 11:44:30 -0500

    more fw

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1403,6 +1403,7 @@ FW_VERSIONS = {
       b'\xf1\x00BD  MDPS C 1.00 1.08 56310M6300\x00 4BDDC108',
       b'\xf1\x00BDm MDPS C A.01 1.03 56310M7800\x00 4BPMC103',
       b'\xf1\x00BDm MDPS C A.01 1.01 56310M7800\x00 4BPMC101',
+      b'\xf1\x8756310M7800\x00\xf1\x00BDm MDPS C A.01 1.01 56310M7800\x00 4BPMC101',
     ],
     (Ecu.fwdCamera, 0x7C4, None): [
       b'\xf1\x00BD  LKAS AT USA LHD 1.00 1.04 95740-M6000 J33',
@@ -1420,12 +1421,15 @@ FW_VERSIONS = {
     (Ecu.abs, 0x7d1, None): [
       b'\xf1\x816VGRAH00018.ELF\xf1\x00\x00\x00\x00\x00\x00\x00',
       b'\xf1\x8758900-M7AB0 \xf1\x816VQRAD00127.ELF\xf1\x00\x00\x00\x00\x00\x00\x00',
+      b'\xf1\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xf1\x006V2B0_C2\x00\x006V2C6051\x00\x00CBD0N20NL1\x00\x00\x00\x00',
       b'\xf1\x816U2VC051\x00\x00\xf1\x006U2V0_C2\x00\x006U2VC051\x00\x00DBD0T16SS0\x00\x00\x00\x00',
       b"\xf1\x816U2VC051\x00\x00\xf1\x006U2V0_C2\x00\x006U2VC051\x00\x00DBD0T16SS0\xcf\x1e'\xc3",
       b'\xf1\x87CXNQAM5799135JB0wV\x84h\xdcwuw\xbbVsgw\x88w\x89hv\x7f\xff\x8b\xff\xff\xff\x9f=\xf1\x816V2C6051\x00\x00\xf1\x006V2B0_C2\x00\x006V2C6051\x00\x00CBD0N20NL1\x00\x00\x00\x00',
+      b'\xf1\x006V2B0_C2\x00\x006V2C6051\x00\x00CBD0N20NL1\x90@\xc6\xae',
+      b'\xf1\x87CXNQAM5799135JB0wV\x84h\xdcwuw\xbbVsgw\x88w\x89hv\x7f\xff\x8b\xff\xff\xff\x9f=\xf1\x816V2C6051\x00\x00\xf1\x006V2B0_C2\x00\x006V2C6051\x00\x00CBD0N20NL1\x90@\xc6\xae',
     ],
   },
   CAR.KIA_K5_2021: {
