22f77bde9234de524e0d0344d99f429a02f0345b
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2023-12-26 22:54:00 +1100

    prevent braking when releasing accelerator

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -586,7 +586,8 @@ class Controls:
     standstill = CS.vEgo <= max(self.CP.minSteerSpeed, MIN_LATERAL_CONTROL_SPEED) or CS.standstill
     CC.latActive = self.active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
                    (not standstill or self.joystick_mode)
-    CC.longActive = self.enabled and not self.events.contains(ET.OVERRIDE_LONGITUDINAL) and self.CP.openpilotLongitudinalControl
+    CC.longActive = self.enabled and (not self.events.contains(ET.OVERRIDE_LONGITUDINAL) or self.CP.carName == 'toyota') \
+                   and self.CP.openpilotLongitudinalControl
 
     actuators = CC.actuators
     actuators.longControlState = self.LoC.long_control_state
