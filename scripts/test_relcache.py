dbff2e5ecad25ce57f6a4970fd3d3d9b5af7d056
bongbui321 <hoang.bui.vh@gmail.com>
2023-12-26 14:55:58 -0500

    unittest

---
+++ scripts/test_relcache.py
@@ -0,0 +1,23 @@
+import sys
+import unittest
+
+OUTPUT_FILE = sys.argv[1]
+WORKSPACE = sys.argv[2]
+print("****WORKSPACE: ", WORKSPACE)
+
+class Test_RelCached(unittest.TestCase):
+  def relcache(self):
+    valid_start_strings = ['Compiling /', '[1/1] Cythonizing']
+    with open(OUTPUT_FILE, 'r') as f:
+      for line in f:
+        if line.startswith(valid_start_strings[0]) or line.startswith(valid_start_strings[1]):
+          continue
+        else:
+          words = line.split(" ")
+          for wrd in words:
+            if wrd == WORKSPACE:
+              print(line)
+              self.fail("Contains absolute path while building")
+
+if __name__ == "__main__":
+    unittest.main()
--- test.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    output_file = sys.argv[1]
-    with open(output_file) as f:
-        for line in f:
-            print(line)
