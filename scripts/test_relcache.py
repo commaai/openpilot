e7a906450783c85d9f93f89d94015547a54a43f3
bongbui321 <hoang.bui.vh@gmail.com>
2023-12-26 23:26:18 -0500

    to old update translation

--- scripts/test_relcache.py
+++ scripts/test_relcache.py
@@ -7,10 +7,10 @@ WORKSPACE = sys.argv[2]
 class Test_RelCached(unittest.TestCase):
 
   def test_relcache(self):
-    valid_start_strings = ['Compiling /', '[1/1] Cythonizing']
+    valid_start_strings = ('Compiling /', '[1/1] Cythonizing', 'Scanning directory ')
     with open(OUTPUT_FILE, 'r') as f:
       for line in f:
-        if line.startswith((valid_start_strings[0], valid_start_strings[1])):
+        if line.startswith(valid_start_strings):
           continue
         else:
           words = line.split(" ")
