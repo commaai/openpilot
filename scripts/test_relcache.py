b91e79dda6f68129a6110aed8f4aba6022058667
bongbui321 <hoang.bui.vh@gmail.com>
2023-12-27 11:18:03 -0500

    verbose error msg

--- scripts/test_relcache.py
+++ scripts/test_relcache.py
@@ -9,14 +9,14 @@ class Test_RelCached(unittest.TestCase):
   def test_relcache(self):
     valid_start_strings = ('Compiling /', '[1/1] Cythonizing', 'Scanning directory ')
     with open(OUTPUT_FILE, 'r') as f:
-      for line in f:
+      for i, line in enumerate(f):
         if line.startswith(valid_start_strings):
           continue
         else:
           words = line.split(" ")
           for wrd in words:
             if WORKSPACE in wrd:
-              self.fail("Contains absolute path while building")
+              self.fail(f"Contains absolute path for build on line {i+1}:\n {line}")
 
 if __name__ == "__main__":
   del sys.argv[1:]
