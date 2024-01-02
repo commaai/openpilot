78822ce2fbd3fe232eb8246fd0bcaed0d6f1c2b3
bongbui321 <hoang.bui.vh@gmail.com>
2024-01-02 15:50:41 -0500

    im sorry cpplint

--- tools/cabana/mainwin.cc
+++ tools/cabana/mainwin.cc
@@ -32,7 +32,7 @@ const char* get_opendbc_file_path(int distance_from_root) {
   char cwd[PATH_MAX];
   getcwd(cwd, sizeof(cwd));
   std::string to_root;
-  for (int i=0; i<distance_from_root;i++) {
+  for (int i=0; i<distance_from_root; i++) {
     to_root += "../";
   }
   static const std::string opendbc_file_path = std::string(cwd) + "/" + to_root + "opendbc";
