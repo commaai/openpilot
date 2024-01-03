2f8e66dbb842fd344ae78f22c21a60a3dfef2b6b
Dean Lee <deanlee3@gmail.com>
2024-01-04 05:14:29 +0800

    UI: single-threaded CameraView (#30397)
    * single-threaded CameraView
    
    * updateFrame
    
    * inherit from CameraWidget
    
    * clear frame on offroadTransition
    
    * check frame_id
    
    * optional
    
    * unique_ptr
    
    * CameraView
    
    * cleanup
    
    * cleanup
    
    * log skipping frame
    
    * fix driverview
    
    * disconnectVipc
    
    * debug inconsistent frame
    
    * skip frame if uiPlan is outdate
    
    * cleanup
    
    * set connected = false
    
    * support camerad reboot&seeking in replay
    
    * cleanup
    
    * qDebug
    
    * use deque
    
    * cleanup
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 17ac1d3c7b3b481826d51eb82aaf9dbcc3f3bd81

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -93,8 +93,6 @@ private:
   bool v_ego_cluster_seen = false;
   int status = STATUS_DISENGAGED;
   std::unique_ptr<PubMaster> pm;
-
-  int skip_frame_count = 0;
   bool wide_cam_requested = false;
 
 protected:
