e0759eee06e6f5ebd5fe97feede37bdf62c9c941
Justin Newberry <justin@comma.ai>
2024-01-05 14:33:59 -0800

    spelling

--- selfdrive/test/sanitize.py
+++ selfdrive/test/sanitize.py
@@ -1,8 +1,8 @@
-# Utilities for sanetizing routes of only essential data
+# Utilities for sanitizing routes of only essential data
 
 from openpilot.tools.lib.logreader import LogIterable
 
 PRESERVE_SERVICES = ["can", "carParams", "pandaStates", "pandaStateDEPRECATED"]
 
-def sanetize(lr: LogIterable) -> LogIterable:
+def sanitize(lr: LogIterable) -> LogIterable:
   return filter(lambda msg: msg.which() in PRESERVE_SERVICES, lr)
