f44a604c231d5c5a22af5738f3c7fb3fb36c7ca3
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2024-01-05 18:41:00 +1100

    match ACCEL_CMD_ALT with compensate if leadVisible

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -155,10 +155,10 @@ class CarController:
         can_sends.append(toyotacan.create_acc_cancel_command(self.packer))
       elif self.CP.openpilotLongitudinalControl:
         can_sends.append(toyotacan.create_accel_command(self.packer, pcm_accel_cmd, actuators.accel, pcm_cancel_cmd,
-                                                        self.standstill_req, lead, CS.acc_type, fcw_alert))
+                                                        self.standstill_req, lead, CS.acc_type, fcw_alert, hud_control.leadVisible))
         self.accel = pcm_accel_cmd
       else:
-        can_sends.append(toyotacan.create_accel_command(self.packer, 0, 0, pcm_cancel_cmd, False, lead, CS.acc_type, False))
+        can_sends.append(toyotacan.create_accel_command(self.packer, 0, 0, pcm_cancel_cmd, False, lead, CS.acc_type, False, False))
 
     if self.frame % 2 == 0 and self.CP.enableGasInterceptor and self.CP.openpilotLongitudinalControl:
       # send exactly zero if gas cmd is zero. Interceptor will send the max between read value and gas cmd.
