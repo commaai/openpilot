442cefa4c70df0bd2ede8d4f6fe4819cf96562dc
Justin Newberry <justin@comma.ai>
2024-01-05 16:07:39 -0800

    move to helpers

--- selfdrive/test/sanitize.py
+++
@@ -1,8 +0,0 @@
-# Utilities for sanitizing routes of only essential data for testing car ports and doing validation.
-
-from openpilot.tools.lib.logreader import LogIterable
-
-PRESERVE_SERVICES = ["can", "carParams", "pandaStates", "pandaStateDEPRECATED"]
-
-def sanitize(lr: LogIterable) -> LogIterable:
-  return filter(lambda msg: msg.which() in PRESERVE_SERVICES, lr)
