1979ac1f3bb1f44a6aec2f24497103448a123ca7
Justin Newberry <justin@comma.ai>
2024-01-09 10:57:47 -0800

    just use numpy directly

--- tools/lib/srreader.py
+++ tools/lib/srreader.py
@@ -1,4 +1,6 @@
 from functools import partial
+
+import numpy as np
 from openpilot.selfdrive.test.openpilotci import get_url
 from openpilot.tools.lib.logreader import LogReader
 from openpilot.tools.lib.route import Route, SegmentRange
@@ -12,42 +14,39 @@ def parse_start_end(sr: SegmentRange, route = None):
     assert start is not None and end is not None, "segment(s) must be provided for non-api sources"
     assert start >= 0 and end >= 0, "relative segment(s) not supported for non-api sources"
 
-  def parse_negative_indexing(i):
-    return route.max_seg_number - abs(i) + 1
+    return start, end
 
-  if start is None:
-    start = 0
-  else:
-    start = parse_negative_indexing(start) if start < 0 else start
+  segs = np.arange(route.max_seg_number+1)
 
-  if end is None:
-    end = route.max_seg_number if sr.start is None else start
-  else:
-    end = parse_negative_indexing(end) if end < 0 else end - 1
+  if start is not None:
+    if end is None:
+      return segs[start:start+1 if start >= 0 else None]
+    else:
+      return segs[start:end]
 
-  return start, end
+  return segs
 
 def comma_api_source(sr: SegmentRange, rlog=True):
   route = Route(sr.route_name)
-  start, end = parse_start_end(sr, route)
+  segs = parse_start_end(sr, route)
 
   log_paths = route.log_paths() if rlog else route.qlog_paths()
 
-  for seg in range(start, end+1):
+  for seg in segs:
     yield LogReader(log_paths[seg])
 
 comma_api_source_qlog = partial(comma_api_source, rlog=False)
 
 def internal_source(sr: SegmentRange):
-  start, end = parse_start_end(sr)
+  segs = parse_start_end(sr)
 
-  for seg in range(start, end+1):
+  for seg in segs:
     yield LogReader(f"cd:/{sr.dongle_id}/{sr.timestamp}/{seg}/rlog.bz2")
 
 def openpilotci_source(sr: SegmentRange):
-  start, end = parse_start_end(sr)
+  segs = parse_start_end(sr)
 
-  for seg in range(start, end+1):
+  for seg in segs:
     yield LogReader(get_url(sr.route_name, seg))
 
 class SegmentRangeReader:
--- tools/lib/tests/test_srreader.py
+++ tools/lib/tests/test_srreader.py
@@ -1,33 +1,37 @@
 import unittest
+import numpy as np
 from parameterized import parameterized
 from openpilot.tools.lib.route import Route, SegmentRange
 
 from openpilot.tools.lib.srreader import parse_start_end
 
 NUM_SEGS = 17 # number of segments in the test route
+ALL_SEGS = list(np.arange(NUM_SEGS))
 TEST_ROUTE = "344c5c15b34f2d8a/2024-01-03--09-37-12"
 
 class TestSegmentRangeReader(unittest.TestCase):
   @parameterized.expand([
-    (f"{TEST_ROUTE}", (0, NUM_SEGS-1)),
-    (f"{TEST_ROUTE.replace('/', '|')}", (0, NUM_SEGS-1)),
-    (f"{TEST_ROUTE}--0", (0, 0)),
-    (f"{TEST_ROUTE}--5", (5, 5)),
-    (f"{TEST_ROUTE}/0", (0, 0)),
-    (f"{TEST_ROUTE}/5", (5, 5)),
-    (f"{TEST_ROUTE}/0/10", (0, 9)),
-    (f"{TEST_ROUTE}/0/0", (0, -1)),
-    (f"{TEST_ROUTE}/4/6", (4, 5)),
-    (f"{TEST_ROUTE}/0/-1", (0, NUM_SEGS-1)),
-    (f"{TEST_ROUTE}/2/-1", (2, NUM_SEGS-1)),
-    (f"{TEST_ROUTE}/-1", (NUM_SEGS-1, NUM_SEGS-1)),
-    (f"{TEST_ROUTE}/-2/-1", (NUM_SEGS-2, NUM_SEGS-1)),
+    (f"{TEST_ROUTE}", ALL_SEGS),
+    (f"{TEST_ROUTE.replace('/', '|')}", ALL_SEGS),
+    (f"{TEST_ROUTE}--0", [0]),
+    (f"{TEST_ROUTE}--5", [5]),
+    (f"{TEST_ROUTE}/0", [0]),
+    (f"{TEST_ROUTE}/5", [5]),
+    (f"{TEST_ROUTE}/0/10", list(np.arange(0, 10))),
+    (f"{TEST_ROUTE}/0/0", []),
+    (f"{TEST_ROUTE}/4/6", [4,5]),
+    (f"{TEST_ROUTE}/0/-1", ALL_SEGS[:-1]),
+    (f"{TEST_ROUTE}/2/-1", ALL_SEGS[2:-1]),
+    (f"{TEST_ROUTE}/-1", [NUM_SEGS-1]),
+    (f"{TEST_ROUTE}/-2/-1", [NUM_SEGS-2]),
+    (f"{TEST_ROUTE}/-4/-2", [NUM_SEGS-4, NUM_SEGS-3]),
   ])
   def test_parse_start_end(self, segment_range, expected):
     sr = SegmentRange(segment_range)
     route = Route(sr.route_name)
-    actual = parse_start_end(sr, route)
-    self.assertEqual(actual, expected)
+    segs = parse_start_end(sr, route)
+    self.assertListEqual(list(segs), expected)
+
 
 if __name__ == "__main__":
   unittest.main()
