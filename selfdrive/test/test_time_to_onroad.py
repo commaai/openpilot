412908d43379ac3c5c1476a301a2c1ee1eab4bac
Comma Device <device@comma.ai>
2024-01-08 16:04:12 -0800

    test

--- selfdrive/test/test_time_to_onroad.py
+++ selfdrive/test/test_time_to_onroad.py
@@ -3,6 +3,7 @@ import os
 import pytest
 import time
 import subprocess
+from collections import defaultdict
 
 import cereal.messaging as messaging
 from openpilot.common.basedir import BASEDIR
@@ -17,30 +18,50 @@ def test_time_to_onroad():
   manager_path = os.path.join(BASEDIR, "selfdrive/manager/manager.py")
   proc = subprocess.Popen(["python", manager_path])
 
+  frame_ids = defaultdict(list)
+  socks = [messaging.sub_sock(s+'CameraState', conflate=False) for s in ('road', 'driver', 'wideRoad')]
+  def us():
+    for s in socks:
+      msgs = messaging.drain_sock(s)
+      for m in msgs:
+        cs = getattr(m, m.which())
+        frame_ids[m.which()].append(cs.frameId)
+
   start_time = time.monotonic()
   sm = messaging.SubMaster(['controlsState', 'deviceState', 'onroadEvents'])
   try:
     # wait for onroad
     with Timeout(20, "timed out waiting to go onroad"):
       while True:
-        sm.update(1000)
+        us()
+        sm.update(0)
         if sm['deviceState'].started:
           break
-        time.sleep(1)
+        time.sleep(0.01)
 
     # wait for engageability
-    with Timeout(10, "timed out waiting for engageable"):
-      while True:
-        sm.update(1000)
-        if sm['controlsState'].engageable:
-          break
-        time.sleep(1)
-    print(f"engageable after {time.monotonic() - start_time:.2f}s")
+    USER_DISABLE_EVENTS = {'speedTooLow', 'reverseGear', 'wrongGear', 'doorOpen', 'seatbeltNotLatched', 'cruiseDisabled', 'espDisabled'}
+    engageable = False
+    st = time.monotonic()
+    while (time.monotonic() - st) < 40.:
+      sm.update(10)
+      #evts = set([str(e.name) for e in sm['onroadEvents']])
+      for e in sm['onroadEvents']:
+        assert not (e.immediateDisable or e.softDisable), f"events: {sm['onroadEvents']}"
 
-    # once we're enageable, must be for the next few seconds
-    for _ in range(500):
-      sm.update(100)
-      assert sm['controlsState'].engageable, f"events: {sm['onroadEvents']}"
+      if not engageable:
+        if sm['controlsState'].engageable:
+          engageable = True
+        elif time.monotonic() - st > 10.:
+          assert sm['controlsState'].engageable, f"events: {sm['onroadEvents']}"
+      else:
+        assert sm.all_checks()
+        assert sm['controlsState'].engageable, f"events: {sm['onroadEvents']}"
+
+
+    #for k, v in frame_ids.items():
+    #  print(k, v)
+    #  assert len(set(v)) == len(v)
   finally:
     proc.terminate()
     if proc.wait(60) is None:
