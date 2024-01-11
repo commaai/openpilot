af77df09e02ee5a4c084b58f0863d22015a6d6cc
bongbui321 <hoang.bui.vh@gmail.com>
2024-01-11 01:04:13 -0500

    setup works

--- tools/sim/tests/test_sim_bridge.py
+++ tools/sim/tests/test_sim_bridge.py
@@ -9,6 +9,7 @@ from cereal import messaging
 from openpilot.common.basedir import BASEDIR
 
 SIM_DIR = os.path.join(BASEDIR, "tools/sim")
+max_time_per_step = 60
 
 class TestSimBridgeBase(unittest.TestCase):
   @classmethod
@@ -17,39 +18,35 @@ class TestSimBridgeBase(unittest.TestCase):
       raise unittest.SkipTest("Don't run this base class, run test_metadrive_bridge.py instead")
 
   def setUp(self):
-    self.processes = []
+    self.p_manager = subprocess.Popen("./launch_openpilot.sh", cwd=SIM_DIR)
+    self.sm = messaging.SubMaster(['controlsState', 'onroadEvents', 'managerState'])
+    self.q = Queue()
+    self.bridge = self.create_bridge()
+    self.bridge.started = Value('b', False)
+    self.p_bridge = self.bridge.run(self.q, retries=10)
+    self.processes = [self.p_manager, self.p_bridge]
 
   def test_engage(self):
     # Startup manager and bridge.py. Check processes are running, then engage and verify.
-    p_manager = subprocess.Popen("./launch_openpilot.sh", cwd=SIM_DIR)
-    self.processes.append(p_manager)
-
-    sm = messaging.SubMaster(['controlsState', 'onroadEvents', 'managerState'])
-    q = Queue()
-    bridge = self.create_bridge()
-    bridge.started = Value('b', False)
-    p_bridge = bridge.run(q, retries=10)
-    self.processes.append(p_bridge)
-
-    max_time_per_step = 60
+    self.q.put("reset")
 
     # Wait for bridge to startup
     start_waiting = time.monotonic()
-    while not bridge.started and time.monotonic() < start_waiting + max_time_per_step:
+    while not self.bridge.started and time.monotonic() < start_waiting + max_time_per_step:
       time.sleep(0.1)
-    self.assertEqual(p_bridge.exitcode, None, f"Bridge process should be running, but exited with code {p_bridge.exitcode}")
+    self.assertEqual(self.p_bridge.exitcode, None, f"Bridge process should be running, but exited with code {self.p_bridge.exitcode}")
 
     start_time = time.monotonic()
     no_car_events_issues_once = False
     car_event_issues = []
     not_running = []
     while time.monotonic() < start_time + max_time_per_step:
-      sm.update()
+      self.sm.update()
 
-      not_running = [p.name for p in sm['managerState'].processes if not p.running and p.shouldBeRunning]
-      car_event_issues = [event.name for event in sm['onroadEvents'] if any([event.noEntry, event.softDisable, event.immediateDisable])]
+      not_running = [p.name for p in self.sm['managerState'].processes if not p.running and p.shouldBeRunning]
+      car_event_issues = [event.name for event in self.sm['onroadEvents'] if any([event.noEntry, event.softDisable, event.immediateDisable])]
 
-      if sm.all_alive() and len(car_event_issues) == 0 and len(not_running) == 0:
+      if self.sm.all_alive() and len(car_event_issues) == 0 and len(not_running) == 0:
         no_car_events_issues_once = True
         break
 
@@ -61,11 +58,11 @@ class TestSimBridgeBase(unittest.TestCase):
     control_active = 0
 
     while time.monotonic() < start_time + max_time_per_step:
-      sm.update()
+      self.sm.update()
 
-      q.put("cruise_down")  # Try engaging
+      self.q.put("cruise_down")  # Try engaging
 
-      if sm.all_alive() and sm['controlsState'].active:
+      if self.sm.all_alive() and self.sm['controlsState'].active:
         control_active += 1
 
         if control_active == min_counts_control_active:
@@ -74,31 +71,21 @@ class TestSimBridgeBase(unittest.TestCase):
     self.assertEqual(min_counts_control_active, control_active, f"Simulator did not engage a minimal of {min_counts_control_active} steps was {control_active}")
   
   def test_drive_course(self):
-    p_manager = subprocess.Popen("./launch_openpilot.sh", cwd=SIM_DIR)
-    self.processes.append(p_manager)
-
-    sm = messaging.SubMaster(['controlsState', 'onroadEvents', 'managerState'])
-    q = Queue()
-    bridge = self.create_bridge()
-    bridge.started = Value('b', False)
-    p_bridge = bridge.run(q, retries=10)
-    self.processes.append(p_bridge)
-
-    max_time_per_step = 60
+    self.q.put("reset")
 
     start_waiting = time.monotonic()
-    while not bridge.started and time.monotonic() < start_waiting + max_time_per_step:
+    while not self.bridge.started and time.monotonic() < start_waiting + max_time_per_step:
       time.sleep(0.1)
-    self.assertEqual(p_bridge.exitcode, None, f"Bridge process should be running, but exited with code {p_bridge.exitcode}")
+    self.assertEqual(self.p_bridge.exitcode, None, f"Bridge process should be running, but exited with code {self.p_bridge.exitcode}")
 
     start_time = time.monotonic()
     user_disengage_once = False
     disengage_events = ('stockAeb', 'fcw', 'ldw')
 
     while time.monotonic() < start_time + max_time_per_step:
-      sm.update()
+      self.sm.update()
 
-      onroadEventNames = [e.name for e in sm['onroadEvents']]
+      onroadEventNames = [e.name for e in self.sm['onroadEvents']]
 
       if any(e in onroadEventNames for e in disengage_events):
         user_disengage_once = True
