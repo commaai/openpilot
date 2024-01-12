c7594445db540dee638b18d1b4a75dfeee89b908
Justin Newberry <justin@comma.ai>
2024-01-12 14:31:47 -0800

    dont render

--- tools/sim/bridge/metadrive/metadrive_bridge.py
+++ tools/sim/bridge/metadrive/metadrive_bridge.py
@@ -83,7 +83,7 @@ class MetaDriveBridge(SimulatorBridge):
   TICKS_PER_FRAME = 5
 
   def __init__(self, args):
-    self.should_render = True
+    self.should_render = False
 
     super(MetaDriveBridge, self).__init__(args)
 
