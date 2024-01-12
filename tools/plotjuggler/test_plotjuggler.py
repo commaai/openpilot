20e9969f8ea12eaa9422580d417772f5c9f58e6d
Justin Newberry <justin@comma.ai>
2024-01-11 20:02:47 -0500

    move plotjuggler to segment range reader (#30969)
    * pj
    
    * limit test to 2 segs
    
    * remove
    
    * fix test
    old-commit-hash: d7e7659852f14ca458605f13c2bdae90f5de5a45

--- tools/plotjuggler/test_plotjuggler.py
+++ tools/plotjuggler/test_plotjuggler.py
@@ -8,7 +8,7 @@ import unittest
 
 from openpilot.common.basedir import BASEDIR
 from openpilot.common.timeout import Timeout
-from openpilot.tools.plotjuggler.juggle import install
+from openpilot.tools.plotjuggler.juggle import DEMO_ROUTE, install
 
 PJ_DIR = os.path.join(BASEDIR, "tools/plotjuggler")
 
@@ -18,8 +18,8 @@ class TestPlotJuggler(unittest.TestCase):
     install()
 
     pj = os.path.join(PJ_DIR, "juggle.py")
-    with subprocess.Popen(f'QT_QPA_PLATFORM=offscreen {pj} --demo None 1 --qlog',
-                          stderr=subprocess.PIPE, shell=True, start_new_session=True) as p:
+    with subprocess.Popen(f'QT_QPA_PLATFORM=offscreen {pj} "{DEMO_ROUTE}/:2"',
+                           stderr=subprocess.PIPE, shell=True, start_new_session=True) as p:
       # Wait for "Done reading Rlog data" signal from the plugin
       output = "\n"
       with Timeout(180, error_msg=output):
