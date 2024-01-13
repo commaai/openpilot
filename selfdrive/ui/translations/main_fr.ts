8001f476709d54e98ef20f4b30fa4f2907a726a2
Korben <korben@korben.info>
2024-01-13 15:39:16 +0100

    Update main_fr.ts
    validation of new elements

--- selfdrive/ui/translations/main_fr.ts
+++ selfdrive/ui/translations/main_fr.ts
@@ -68,23 +68,23 @@
     </message>
     <message>
         <source>Hidden Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Réseau Caché</translation>
     </message>
     <message>
         <source>CONNECT</source>
-        <translation type="unfinished">CONNECTER</translation>
+        <translation>CONNECTER</translation>
     </message>
     <message>
         <source>Enter SSID</source>
-        <translation type="unfinished">Entrer le SSID</translation>
+        <translation>Entrer le SSID</translation>
     </message>
     <message>
         <source>Enter password</source>
-        <translation type="unfinished">Entrer le mot de passe</translation>
+        <translation>Entrer le mot de passe</translation>
     </message>
     <message>
         <source>for &quot;%1&quot;</source>
-        <translation type="unfinished">pour &quot;%1&quot;</translation>
+        <translation>pour &quot;%1&quot;</translation>
     </message>
 </context>
 <context>
