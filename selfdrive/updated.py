96293b300f774545c9256c420767f678bbc2d352
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-01-15 11:59:33 -0800

    fix

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -60,7 +60,7 @@ def run(cmd: List[str], cwd: Optional[str] = None) -> str:
   s = "systemd-run --user --scope"
   s += " -p CPUQuota=50% -p AllowedCPUs=0,1"
   s += " -p MemoryMax=200M -p MemorySwapMax=300M"
-  return subprocess.check_output(srun + " " + cmd, cwd=cwd, stderr=subprocess.STDOUT, encoding='utf8')
+  return subprocess.check_output(s + " " + cmd, cwd=cwd, stderr=subprocess.STDOUT, encoding='utf8')
 
 
 def set_consistent_flag(consistent: bool) -> None:
