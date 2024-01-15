2522d5ec5fc6b3509b8291cb77859152419d221c
Justin Newberry <jnewberry0502@gmail.com>
2024-01-14 20:34:04 -0800

    ensure size is same

--- selfdrive/car/subaru/tests/test_subaru.py
+++ selfdrive/car/subaru/tests/test_subaru.py
@@ -7,21 +7,14 @@ Ecu = car.CarParams.Ecu
 
 ECU_NAME = {v: k for k, v in Ecu.schema.enumerants.items()}
 
-def get_fw_sizes(platform):
-  return {
-    Ecu.abs: 4 if platform in PREGLOBAL_CARS else 5,
-    Ecu.eps: 5 if platform in {CAR.OUTBACK_2023} else 4,
-    Ecu.fwdCamera: 10 if platform in (LKAS_ANGLE | GLOBAL_GEN2) else 8,
-    Ecu.engine: 5,
-    Ecu.transmission: 5
-  }
 
 class TestSubaruFingerprint(unittest.TestCase):
   def test_fw_version_format(self):
     for platform, fws_per_ecu in FW_VERSIONS.items():
       for (ecu, _, _), fws in fws_per_ecu.items():
+        fw_size = len(fws[0])
         for fw in fws:
-          self.assertEqual(len(fw), get_fw_sizes(platform)[ecu], f"{platform}, {ECU_NAME[ecu]}")
+          self.assertEqual(len(fw), fw_size, f"{platform} {ecu}: {len(fw)} {fw_size}")
 
 
 if __name__ == "__main__":
