7854f96b928ff503b59b3e18f6386782ae21ea1c
jakethesnake420 <fastermaster12@gmail.com>
2024-01-16 23:16:27 -0600

    Track skipped samples

--- system/assistant/wakewordd.py
+++ system/assistant/wakewordd.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 import numpy as np
 from pathlib import Path
-from openpilot.common.realtime import Ratekeeper
-from openpilot.common.retry import retry
 from openpilot.system.assistant.openwakeword import Model
 from openpilot.system.assistant.openwakeword.utils import download_models
 from openpilot.common.params import Params
 from cereal import messaging
+from openpilot.system.micd import SAMPLE_BUFFER, SAMPLE_RATE
 
 
 RATE = 12.5
-SAMPLE_RATE = 16000
-SAMPLE_BUFFER = 1280 # (approx 100ms)
-
-
 class WakeWordListener:
   def __init__(self, model):
     model_path = Path(__file__).parent / f'models/{model}.onnx'
     melspec_model_path = Path(__file__).parent / 'models/melspectrogram.onnx'
     embedding_model_path = Path(__file__).parent / 'models/embedding_model.onnx'
-    self.owwModel = Model(wakeword_models=[model_path], melspec_model_path=melspec_model_path, embedding_model_path=embedding_model_path)
-    self.params = Params()
+    self.owwModel = Model(wakeword_models=[model_path], melspec_model_path=melspec_model_path, embedding_model_path=embedding_model_path, chunk_size=SAMPLE_BUFFER, sr=SAMPLE_RATE)
+
     self.sm = messaging.SubMaster(['microphoneRaw'])
+    self.params = Params()
+
+    self.frame_index = 0
+    self.frame_index_last = 0
 
 
   def update(self):
-    self.owwModel.predict(np.frombuffer(self.sm['microphoneRaw'].rawSample, dtype=np.int16))
+    self.frame_index = self.sm['microphoneRaw'].frameIndex
+    #print(f'{self.frame_index_last=}, {self.frame_index=}')
+    if not (self.frame_index_last == self.frame_index or
+            self.frame_index - self.frame_index_last == SAMPLE_BUFFER):
+      print(f'skipped {(self.frame_index - self.frame_index_last)//SAMPLE_BUFFER-1} sample(s)')
+    self.frame_index_last = self.frame_index
+    sample = np.frombuffer(self.sm['microphoneRaw'].rawSample, dtype=np.int16)
+    self.owwModel.predict(sample)
     for mdl in self.owwModel.prediction_buffer.keys():
         scores = list(self.owwModel.prediction_buffer[mdl])
         detected = scores[-1] >= 0.5
@@ -33,9 +39,7 @@ class WakeWordListener:
     #print(curr_score)
     if detected:
       print("wake word detected")
-    self.params.put_bool("WakeWordDetected", detected) 
-    
-    return detected
+    self.params.put_bool("WakeWordDetected", detected)
 
   def wake_word_listener_thread(self):
     while True:
