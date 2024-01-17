0f192bdbe0b884afc6ecc197b3ede978cefbaa30
Justin Newberry <justin@comma.ai>
2024-01-17 13:12:13 -0800

    keep this for now

--- tools/lib/openpilotci.py
+++ tools/lib/openpilotci.py
@@ -6,4 +6,7 @@ def get_url(*args, **kwargs):
 def upload_file(*args, **kwargs):
   return OpenpilotCIContainer.upload_file(*args, **kwargs)
 
+def upload_bytes(*args, **kwargs):
+  return OpenpilotCIContainer.upload_bytes(*args, **kwargs)
+
 BASE_URL = OpenpilotCIContainer.BASE_URL
