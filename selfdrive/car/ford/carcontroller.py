bc99516c3599c3ebe4a3ffb5e9e53dad1c3d25d9
Bruce Wayne <harald.the.engineer@gmail.com>
2024-01-21 23:26:38 -0800

    Ford updates

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -90,7 +90,7 @@ class CarController:
       if not CC.longActive or gas < CarControllerParams.MIN_GAS:
         gas = CarControllerParams.INACTIVE_GAS
       stopping = CC.actuators.longControlState == LongCtrlState.stopping
-      can_sends.append(fordcan.create_acc_msg(self.packer, self.CAN, CC.longActive, gas, accel, stopping, v_ego_kph=40 * CV.MPH_TO_KPH))
+      can_sends.append(fordcan.create_acc_msg(self.packer, self.CAN, CC.longActive, gas, accel, stopping, v_ego_kph=40 * CV.MS_TO_KPH))
 
     ### ui ###
     send_ui = (self.main_on_last != main_on) or (self.lkas_enabled_last != CC.latActive) or (self.steer_alert_last != steer_alert)
