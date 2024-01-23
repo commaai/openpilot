dcb28caa4d5a15cd4711c4fef7e8c5ed95835502
Justin Newberry <justin@comma.ai>
2024-01-23 09:59:07 -0800

    Revert "enable stdout"
    This reverts commit 7686d2c7652171ad473aa6518d69781ee52f5cd0.

--- selfdrive/test/setup_vsound.sh
+++ selfdrive/test/setup_vsound.sh
@@ -7,4 +7,4 @@
   # create a virtual null audio and set it to default device
   sudo pactl load-module module-null-sink sink_name=virtual_audio
   sudo pactl set-default-sink virtual_audio
-}
+} > /dev/null 2>&1
