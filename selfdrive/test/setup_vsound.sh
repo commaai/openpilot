3001d67e64f22bff0fe9cbc8f29a51aa00ab80e4
Justin Newberry <justin@comma.ai>
2024-01-23 09:59:34 -0800

    no cpu limit

--- selfdrive/test/setup_vsound.sh
+++ selfdrive/test/setup_vsound.sh
@@ -2,7 +2,7 @@
 
 {
   #start pulseaudio daemon
-  sudo pulseaudio -D
+  sudo pulseaudio -D --no-cpu-limit=yes
 
   # create a virtual null audio and set it to default device
   sudo pactl load-module module-null-sink sink_name=virtual_audio
