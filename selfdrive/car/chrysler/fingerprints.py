dcaec96887289a7dda2bcfa24764739a96b1ff32
Shane Smiskol <shane@smiskol.com>
2024-01-24 03:31:33 -0800

    2018 too

--- selfdrive/car/chrysler/fingerprints.py
+++ selfdrive/car/chrysler/fingerprints.py
@@ -122,8 +122,8 @@ FW_VERSIONS = {
     ],
     (Ecu.srs, 0x744, None): [
     ],
-    (Ecu.abs, 0x7e4, None): [
-    ],
+    # (Ecu.abs, 0x7e4, None): [
+    # ],
     (Ecu.fwdRadar, 0x753, None): [
     ],
     (Ecu.eps, 0x75a, None): [
