7c3de01e8248b70de8cde4d1a9347aaa0f67327e
Shane Smiskol <shane@smiskol.com>
2024-01-24 03:33:32 -0800

    clean up

--- selfdrive/car/chrysler/fingerprints.py
+++ selfdrive/car/chrysler/fingerprints.py
@@ -417,12 +417,3 @@ FW_VERSIONS = {
     ],
   },
 }
-
-
-for car in FINGERPRINTS:
-  if car not in FW_VERSIONS:
-    print('Add to FW_VERSIONS:', car.name, '   -   ', car.value)
-
-for car in FW_VERSIONS:
-  if car not in FINGERPRINTS:
-    print('Good, not in FINGERPRINTS', car.name, '   -   ', car.value)
