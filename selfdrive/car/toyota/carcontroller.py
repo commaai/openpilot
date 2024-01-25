875355d2203f06eadedfddf5d6ee8e966442468a
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2024-01-25 13:24:04 +1100

    fix misspelling

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -26,7 +26,7 @@ MAX_LTA_ANGLE = 94.9461  # deg
 MAX_LTA_DRIVER_TORQUE_ALLOWANCE = 150  # slightly above steering pressed allows some resistance when changing lanes
 
 # PCM compensatory force calculation threshold
-COMPENSTAORY_CALCULATION_THRESHOLD = -0.25  # m/s^2
+COMPENSATORY_CALCULATION_THRESHOLD = -0.25  # m/s^2
 
 
 class CarController:
@@ -125,7 +125,7 @@ class CarController:
     if not CC.longActive:
       self.prohibit_neg_calculation = True
     # don't reset until a reasonable compensatory value is reached
-    if CS.pcm_neutral_force > COMPENSTAORY_CALCULATION_THRESHOLD * self.CP.mass:
+    if CS.pcm_neutral_force > COMPENSATORY_CALCULATION_THRESHOLD * self.CP.mass:
       self.prohibit_neg_calculation = False
     # NO_STOP_TIMER_CAR will creep if compensation is applied when stopping or stopped, don't compensate when stopped or stopping
     should_compensate = True
