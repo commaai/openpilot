9db6a894c7ac82edaa3e907b23f706e19ff5b488
Shane Smiskol <shane@smiskol.com>
2024-01-25 23:57:00 -0800

    radar

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -173,7 +173,7 @@ FW_QUERY_CONFIG = FwQueryConfig(
     Request(
       [StdQueries.TESTER_PRESENT_REQUEST, StdQueries.UDS_VERSION_REQUEST],
       [StdQueries.TESTER_PRESENT_RESPONSE, StdQueries.UDS_VERSION_RESPONSE],
-      whitelist_ecus=[Ecu.camera],
+      whitelist_ecus=[Ecu.fwdRadar],
       bus=0,
       logging=True
     ),
