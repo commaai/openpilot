47b5db713b22d574d507dd474efc0983f981402f
Justin Newberry <justin@comma.ai>
2024-01-26 13:16:00 -0800

    no space

--- tools/lib/comma_car_segments.py
+++ tools/lib/comma_car_segments.py
@@ -4,7 +4,6 @@ import requests
 # Forks with additional car support can fork the commaCarSegments repo on huggingface or host the LFS files themselves
 COMMA_CAR_SEGMENTS_REPO = os.environ.get("COMMA_CAR_SEGMENTS_REPO", "https://huggingface.co/datasets/commaai/commaCarSegments")
 COMMA_CAR_SEGMENTS_BRANCH = os.environ.get("COMMA_CAR_SEGMENTS_BRANCH", "main")
-
 COMMA_CAR_SEGMENTS_LFS_INSTANCE = os.environ.get("COMMA_CAR_SEGMENTS_LFS_INSTANCE", COMMA_CAR_SEGMENTS_REPO)
 
 def get_comma_car_segments_database():
