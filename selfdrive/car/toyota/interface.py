61b391dca0d9a532ea2cfc3f422a399fb60f187c
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2024-01-26 11:00:46 +1100

    lower stoppingDecelRate for TSS 2+
    Thanks @AlexandreSato for testing
    
    Co-Authored-By: Alexandre Nobuharu Sato <66435071+alexandresato@users.noreply.github.com>

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -261,10 +261,11 @@ class CarInterface(CarInterfaceBase):
     tune = ret.longitudinalTuning
     tune.deadzoneBP = [0., 16., 20., 30.]
     tune.deadzoneV = [0., .03, .06, .15]
-    ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+    ret.stoppingDecelRate = 0.3  # This is okay for TSS-P
     if candidate in TSS2_CAR:
       ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
+      ret.stoppingDecelRate = 0.009  # reach stopping target smoothly
     tune.kpBP = [0.]
     tune.kpV = [1.]
     tune.kiBP = [0.]
