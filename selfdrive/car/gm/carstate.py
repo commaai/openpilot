9acd645c4bb53b5c73e14ef1df9680abc5d57bdb
garrettpall <76917194+garrettpall@users.noreply.github.com>
2024-01-27 23:56:10 -0500

    GM: Add BSM from Powertrain Bus (#30861)
    * Add GM BSM
    
    * Move BSM check to fwdCam only
    
    * Detect if BSM message if present
    
    * Remove hardcoding
    
    * Apply Commit Suggestion
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    
    * Cleanup Suggestions
    
    * bump opendbc
    
    * test out bsm
    
    * BCMBlindSpotMonitors -> BCMBlindSpotMonitor
    
    * Revert "test out bsm"
    
    This reverts commit 0e166530e60165269c1be200f48ead9918d5fe96.
    
    * up here
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    old-commit-hash: 58ef40eb2264a9d62a1715486677c342d5d113ce

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -113,6 +113,10 @@ class CarState(CarStateBase):
       if self.CP.pcmCruise:
         ret.cruiseState.nonAdaptive = cam_cp.vl["ASCMActiveCruiseControlStatus"]["ACCCruiseState"] not in (2, 3)
 
+    if self.CP.enableBsm:
+      ret.leftBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["LeftBSM"] == 1
+      ret.rightBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["RightBSM"] == 1
+
     return ret
 
   @staticmethod
@@ -145,6 +149,9 @@ class CarState(CarStateBase):
       ("PSCMSteeringAngle", 100),
       ("ECMAcceleratorPos", 80),
     ]
+
+    if CP.enableBsm:
+      messages.append(("BCMBlindSpotMonitor", 10))
 
     # Used to read back last counter sent to PT by camera
     if CP.networkLocation == NetworkLocation.fwdCamera:
