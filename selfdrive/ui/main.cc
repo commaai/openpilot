d2501b627d9acfa348558a1463b3ac0bcbcf423f
Adam MacBeth <amacbeth@gmail.com>
2024-01-28 10:06:03 -0800

    ui: Make ui independent of launch path
    Prior to this change using the wrong launch path would result in crashes
    since asset paths can't be found.

--- selfdrive/ui/main.cc
+++ selfdrive/ui/main.cc
@@ -1,4 +1,6 @@
 #include <sys/resource.h>
+#include <unistd.h>
+#include <libgen.h>
 
 #include <QApplication>
 #include <QTranslator>
@@ -10,6 +12,18 @@
 
 int main(int argc, char *argv[]) {
   setpriority(PRIO_PROCESS, 0, -20);
+
+  // Set working directory so asset paths are valid independent of launch path
+  char path[PATH_MAX];
+  memset(path, 0, sizeof(path));
+  if (readlink("/proc/self/exe", path, sizeof(path)) == -1) {
+    perror("readlink failed");
+    return EXIT_FAILURE;
+  }
+  if (chdir(dirname(path)) == -1) {
+    perror("chdir failed");
+    return EXIT_FAILURE;
+  }
 
   qInstallMessageHandler(swagLogMessageHandler);
   initApp(argc, argv);
