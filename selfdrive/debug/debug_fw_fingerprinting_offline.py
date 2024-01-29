6b0bf0fc761a7d1736147ea800c8494005b0525e
Shane Smiskol <shane@smiskol.com>
2024-01-29 14:16:00 -0800

    add

--- selfdrive/debug/debug_fw_fingerprinting_offline.py
+++ selfdrive/debug/debug_fw_fingerprinting_offline.py
@@ -27,13 +27,12 @@ def main(route: str, addrs: list[int]):
           if msg.logMonoTime != prev_mono_time:
             print()
             prev_mono_time = msg.logMonoTime
-          print(
-            f"{msg.logMonoTime} rxaddr={can.address}, bus={can.src}, {round((msg.logMonoTime - start_mono_time) * 1e-6, 2)} ms, 0x{can.dat.hex()}, {can.dat}, {len(can.dat)=}")
+          print(f"{msg.logMonoTime} rxaddr={can.address}, bus={can.src}, {round((msg.logMonoTime - start_mono_time) * 1e-6, 2)} ms, 0x{can.dat.hex()}, {can.dat}, {len(can.dat)=}")
 
 
 if __name__ == "__main__":
   # argparse:
-  parser = argparse.ArgumentParser(description='View ISO-TP communication between various ECUs given an address')
+  parser = argparse.ArgumentParser(description='View back and forth ISO-TP communication between various ECUs given an address')
   parser.add_argument('route', help='Route name')
   parser.add_argument('addrs', nargs='*', help='List of tx address to view (0x7e0 for engine)')
   args = parser.parse_args()
