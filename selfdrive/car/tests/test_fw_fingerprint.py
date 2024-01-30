d3a0fb771645d90036acc435bccb7a06acae6c48
Shane Smiskol <shane@smiskol.com>
2024-01-30 02:33:55 -0800

    fix ref

--- selfdrive/car/tests/test_fw_fingerprint.py
+++ selfdrive/car/tests/test_fw_fingerprint.py
@@ -246,7 +246,7 @@ class TestFwFingerprintTiming(unittest.TestCase):
 
   @pytest.mark.timeout(60)
   def test_fw_query_timing(self):
-    total_ref_time = 6.4
+    total_ref_time = 6.9
     brand_ref_times = {
       1: {
         'gm': 0.5,
