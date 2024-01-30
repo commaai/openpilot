9218401015cbd52c94478757fd24e35cfb9a41dd
Shane Smiskol <shane@smiskol.com>
2024-01-30 00:51:21 -0800

    consistency

--- selfdrive/car/fw_query_definitions.py
+++ selfdrive/car/fw_query_definitions.py
@@ -98,10 +98,10 @@ class FwQueryConfig:
         new_request.bus += 4
         self.requests.append(new_request)
 
-  def get_all_ecus(self, fw_versions: OfflineFwVersions, include_ecu_type: bool = False,
+  def get_all_ecus(self, offline_fw_versions: OfflineFwVersions, include_ecu_type: bool = False,
                    include_extra_ecus: bool = True) -> set[EcuAddrSubAddr | AddrType]:
     # Add ecus in database + extra ecus
-    brand_ecus = {ecu for ecus in fw_versions.values() for ecu in ecus}
+    brand_ecus = {ecu for ecus in offline_fw_versions.values() for ecu in ecus}
 
     if include_extra_ecus:
       brand_ecus |= set(self.extra_ecus)
