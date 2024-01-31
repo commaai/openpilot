becca38ebb78d81466d0d4fce54165a445999ce3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-01-30 17:33:49 -0800

    x86 stuff

--- third_party/maplibre-native-qt/build.sh
+++ third_party/maplibre-native-qt/build.sh
@@ -5,17 +5,17 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 ARCHNAME="x86_64"
 MAPLIBRE_FLAGS="-DMLN_QT_WITH_LOCATION=OFF"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  ARCHNAME="Darwin"
-elif [[ "$OSTYPE" == "aarch64"* ]]; then
-  ARCHNAME="aarch64"
-  MAPLIBRE_FLAGS="$MAPLIBRE_FLAGS -DCMAKE_SYSTEM_NAME=Android -DANDROID_ABI=arm64-v8a"
+if [ -f /AGNOS ]; then
+  ARCHNAME="larch64"
+  #MAPLIBRE_FLAGS="$MAPLIBRE_FLAGS -DCMAKE_SYSTEM_NAME=Android -DANDROID_ABI=arm64-v8a"
 fi
 
-if [ ! -d maplibre_repo ]; then
-  git clone git@github.com:maplibre/maplibre-native-qt.git $DIR/maplibre_repo
+cd $DIR
+if [ ! -d maplibre ]; then
+  git clone git@github.com:maplibre/maplibre-native-qt.git $DIR/maplibre
 fi
-cd $DIR/maplibre_repo
+
+cd maplibre
 git fetch --all
 git checkout 3726266e127c1f94ad64837c9dbe03d238255816
 git submodule update --depth=1 --recursive --init
@@ -23,7 +23,7 @@ git submodule update --depth=1 --recursive --init
 # build
 mkdir -p build
 cd build
-cmake $MAPLIBRE_FLAGS $DIR/maplibre_repo
+cmake $MAPLIBRE_FLAGS $DIR/maplibre
 make -j$(nproc)
 
 INSTALL_DIR="$DIR/$ARCHNAME"
@@ -32,9 +32,6 @@ mkdir -p $INSTALL_DIR
 
 rm -rf $INSTALL_DIR/lib $DIR/include
 mkdir -p $INSTALL_DIR/lib $INSTALL_DIR/include $DIR/include
-cp -r $DIR/maplibre_repo/build/src/core/*.so* $INSTALL_DIR/lib
-cp -r $DIR/maplibre_repo/build/src/core/include/* $INSTALL_DIR/include
-cp -r $DIR/maplibre_repo/src/**/*.hpp $DIR/include
-
-cd $DIR
-rm -rf maplibre_repo
+cp -r $DIR/maplibre/build/src/core/*.so* $INSTALL_DIR/lib
+cp -r $DIR/maplibre/build/src/core/include/* $INSTALL_DIR/include
+cp -r $DIR/maplibre/src/**/*.hpp $DIR/include
