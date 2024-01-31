9374b777988a8e478a82aa8d91a7d3ba8c860b56
Justin Newberry <justin@comma.ai>
2024-01-31 13:45:12 -0800

    rename to services

--- common/mock/__init__.py
+++ common/mock/__init__.py
@@ -6,8 +6,10 @@ example in common/tests/test_mock.py
 
 import functools
 import threading
+import time
 from typing import List, Union
 from cereal.messaging import PubMaster
+from cereal.services import SERVICE_LIST
 from openpilot.common.mock.generators import generate_liveLocationKalman
 
 
@@ -16,23 +18,27 @@ MOCK_GENERATOR = {
 }
 
 
-def generate_messages_loop(names: List[str], done: threading.Event):
-  pm = PubMaster(names)
+def generate_messages_loop(services: List[str], done: threading.Event):
+  pm = PubMaster(services)
+  i = 0
   while not done.is_set():
-    for name in names:
-      message = MOCK_GENERATOR[name]()
-      pm.send(name, message)
+    for s in services:
+      if i % 100 == SERVICE_LIST[s].frequency:
+        message = MOCK_GENERATOR[s]()
+        pm.send(s, message)
+    i += 1
+    time.sleep(1/100)
 
 
-def mock_messages(names: Union[List[str], str]):
-  if isinstance(names, str):
-    names = [names]
+def mock_messages(services: Union[List[str], str]):
+  if isinstance(services, str):
+    services = [services]
 
   def decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
       done = threading.Event()
-      t = threading.Thread(target=generate_messages_loop, args=(names, done))
+      t = threading.Thread(target=generate_messages_loop, args=(services, done))
       t.start()
       ret = func(*args, **kwargs)
       done.set()
