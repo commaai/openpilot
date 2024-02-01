6c406fe3d7e5a62896e962cf661f26b2309ef2f6
Eric Brown <eric@ebrown.net>
2024-01-31 19:30:14 -0700

    Update name

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -33,7 +33,7 @@ class CarState(CarStateBase):
     self.cruise_buttons = pt_cp.vl["ASCMSteeringButton"]["ACCButtons"]
     self.buttons_counter = pt_cp.vl["ASCMSteeringButton"]["RollingCounter"]
     self.pscm_status = copy.copy(pt_cp.vl["PSCMStatus"])
-    # 0 - unknown, 1 - forward, 2 - reverse, 3 - unsupported, 4 - fault
+    # 0 - stationary, 1 - forward, 2 - reverse, 3 - unsupported, 4 - fault
     self.moving_backward = (pt_cp.vl["EBCMWheelSpdRear"]["RLWheelDir"] == 2) or (pt_cp.vl["EBCMWheelSpdRear"]["RRWheelDir"] == 2)
 
     # Variables used for avoiding LKAS faults
