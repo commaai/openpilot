f80f17957c862057c6826e7a4584312e8110ab08
Shane Smiskol <shane@smiskol.com>
2024-02-01 00:22:27 -0800

    cmt

--- selfdrive/car/mazda/values.py
+++ selfdrive/car/mazda/values.py
@@ -67,7 +67,7 @@ class Buttons:
 
 FW_QUERY_CONFIG = FwQueryConfig(
   requests=[
-    # Bus 0 should be queried first, may fix spotty ABS response
+    # Bus 0 should be queried first, may fix spotty ABS response on CX-5 2022
     Request(
       [StdQueries.MANUFACTURER_SOFTWARE_VERSION_REQUEST],
       [StdQueries.MANUFACTURER_SOFTWARE_VERSION_RESPONSE],
