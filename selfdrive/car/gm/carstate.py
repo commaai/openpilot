a49498d514b4b531303367afb9e4be4c5bafbf9b
Eric Brown <eric@ebrown.net>
2024-01-31 19:32:09 -0700

    Add comment

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -33,6 +33,8 @@ class CarState(CarStateBase):
     self.cruise_buttons = pt_cp.vl["ASCMSteeringButton"]["ACCButtons"]
     self.buttons_counter = pt_cp.vl["ASCMSteeringButton"]["RollingCounter"]
     self.pscm_status = copy.copy(pt_cp.vl["PSCMStatus"])
+    # This is to avoid a fault where you engage while still moving backwards after shifting to D.
+    # An Equinox has been seen with an unsupported status, so only check if either wheel is in reverse (2)
     # 0 - stationary, 1 - forward, 2 - reverse, 3 - unsupported, 4 - fault
     self.moving_backward = (pt_cp.vl["EBCMWheelSpdRear"]["RLWheelDir"] == 2) or (pt_cp.vl["EBCMWheelSpdRear"]["RRWheelDir"] == 2)
 
