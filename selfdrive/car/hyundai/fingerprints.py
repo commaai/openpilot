d5821c681229fa4fbb0de6bade04e2aa1b736880
Shane Smiskol <shane@smiskol.com>
2024-02-06 03:47:51 -0600

    [bot] Fingerprints: add missing FW versions from new users (#31277)
    Export fingerprints

--- selfdrive/car/hyundai/fingerprints.py
+++ selfdrive/car/hyundai/fingerprints.py
@@ -1200,8 +1200,8 @@ FW_VERSIONS = {
   },
   CAR.KIA_NIRO_PHEV_2022: {
     (Ecu.engine, 0x7e0, None): [
-      b'\xf1\x816H6G6051\x00\x00\x00\x00\x00\x00\x00\x00',
       b'\xf1\x816H6G5051\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'\xf1\x816H6G6051\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xf1\x006U3H1_C2\x00\x006U3J9051\x00\x00PDE0G16NL3\x00\x00\x00\x00',
