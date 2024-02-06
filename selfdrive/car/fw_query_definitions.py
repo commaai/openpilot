53507b173b74380ad08cda80f450fcc511f0e6cb
Shane Smiskol <shane@smiskol.com>
2024-02-05 16:57:08 -0800

    consider ford abs

--- selfdrive/car/fw_query_definitions.py
+++ selfdrive/car/fw_query_definitions.py
@@ -14,7 +14,7 @@ EcuAddrSubAddr = Tuple[int, int, Optional[int]]
 LiveFwVersions = Dict[AddrType, Set[bytes]]
 OfflineFwVersions = Dict[str, Dict[EcuAddrSubAddr, List[bytes]]]
 
-STANDARD_VIN_ADDRS = [0x7e0, 0x7e2, 0x18da10f1, 0x18da0ef1]  # engine, VMCU, 29-bit engine, PGM-FI
+STANDARD_VIN_ADDRS = [0x7e0, 0x7e2, 0x760, 0x18da10f1, 0x18da0ef1]  # engine, hybrid controller, Ford abs, 29-bit engine, PGM-FI
 
 
 def p16(val):
