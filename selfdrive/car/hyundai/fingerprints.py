fd92c45b04c1e0e40aeae51e05d560cda6df6c82
dblacknc <github@1000.relay.net>
2024-02-06 04:10:10 -0500

    Add fingerprint and car info for 2021 Kia Niro PHEV (#31275)
    * Add 2021 Kia Niro Plug-in Hybrid
    
    * Added CAR.KIA_NIRO_PHEV_2021
    
    * Added CAR.KIA_NIRO_PHEV_2021
    
    Created new block and moved 2022 model to it as well, with correct mass and steerRatio values.
    
    * Added 2021 Kia Niro PHEV
    
    * Added 2021 Kia Niro PHEV
    
    For now, using 2020 Niro EV torque data
    
    * Update values.py
    
    Add '21 Kia Niro PHEV to HYBRID_CAR for gas signal and DBC info
    
    * Test route for 2021 Kia Niro PHEV
    
    * Add public test route for 2021 Kia Niro PHEV
    
    * Fixed typo in test route for 2021 Kia Niro PHEV
    
    * Move '21 Kia Niro PHEV car info under '22
    
    ...and remove other mention of '21 as distinct car
    
    * Added '21 Kia Niro PHEV fingerprint under '22
    
    ...and removed separate car definition
    
    * Removed '21 Kia Niro PHEV as separate car, now under '22
    
    * Removed '21 Kia Niro PHEV, now under '22 fingerprint
    
    * Removed test route for '21 Kia Niro PHEV
    
    * Update values.py
    
    Missed removing one more reference to '21 Kiro Niro PHEV, consolidating under '22.
    
    * Update selfdrive/car/hyundai/interface.py
    
    * Update selfdrive/car/hyundai/fingerprints.py
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    old-commit-hash: 75ea3f3e3ab6d32d4f6b0179a5dc8bc8664e64d8

--- selfdrive/car/hyundai/fingerprints.py
+++ selfdrive/car/hyundai/fingerprints.py
@@ -1201,15 +1201,18 @@ FW_VERSIONS = {
   CAR.KIA_NIRO_PHEV_2022: {
     (Ecu.engine, 0x7e0, None): [
       b'\xf1\x816H6G6051\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'\xf1\x816H6G5051\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xf1\x006U3H1_C2\x00\x006U3J9051\x00\x00PDE0G16NL3\x00\x00\x00\x00',
+      b'\xf1\x816U3J9051\x00\x00\xf1\x006U3H1_C2\x00\x006U3J9051\x00\x00PDE0G16NL3\x00\x00\x00\x00',
     ],
     (Ecu.eps, 0x7d4, None): [
       b'\xf1\x00DE  MDPS C 1.00 1.01 56310G5520\x00 4DEPC101',
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00DEP MFC  AT USA LHD 1.00 1.00 99211-G5500 210428',
+      b'\xf1\x00DEP MFC  AT USA LHD 1.00 1.06 99211-G5000 201028',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00DEhe SCC F-CUP      1.00 1.00 99110-G5600         ',
