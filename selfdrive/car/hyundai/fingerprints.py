d8e7005b7ae0f243f49d35dfd7eb5f8781dea57f
lssqla <155280020+lssqla@users.noreply.github.com>
2024-02-07 10:27:56 +0900

    Update fingerprints.py
    fffi

--- selfdrive/car/hyundai/fingerprints.py
+++ selfdrive/car/hyundai/fingerprints.py
@@ -1692,6 +1692,14 @@ FW_VERSIONS = {
       b'\xf1\x00GL3_ RDR -----      1.00 1.02 99110-L8000         ',
     ],
   },
+  CAR.KIA_K5_HEV_2024: {
+    (Ecu.fwdCamera, 0x7c4, None): [
+      b'\xf1\x00DL3HMFC AT KOR LHD 1.00 1.02 99210-L2500 230911'
+    ],
+    (Ecu.fwdRadar, 0x7d0, None): [
+      b'\xf1\x00DL3_ RDR ----- 1.00 1.01 99110-L2500 ',
+    ],
+  },
   CAR.STARIA_4TH_GEN: {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00US4 MFC  AT KOR LHD 1.00 1.06 99211-CG000 230524',
