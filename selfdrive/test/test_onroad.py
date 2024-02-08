85fe673ee881f11467dd68fc5bfe8f979e94db5b
Justin Newberry <justin@comma.ai>
2024-02-08 12:36:33 -0500

    bump uploader cpu (#31373)

--- selfdrive/test/test_onroad.py
+++ selfdrive/test/test_onroad.py
@@ -57,7 +57,7 @@ PROCS = {
   "selfdrive.boardd.pandad": 0,
   "selfdrive.statsd": 0.4,
   "selfdrive.navd.navd": 0.4,
-  "system.loggerd.uploader": (0.5, 10.0),
+  "system.loggerd.uploader": (0.5, 15.0),
   "system.loggerd.deleter": 0.1,
 }
 
