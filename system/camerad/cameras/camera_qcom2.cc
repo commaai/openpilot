e825d5d0f04a609b854382a49b87e03372ca71b3
Justin Newberry <justin@comma.ai>
2024-02-08 17:51:41 -0800

    isp assert

--- system/camerad/cameras/camera_qcom2.cc
+++ system/camerad/cameras/camera_qcom2.cc
@@ -605,6 +605,7 @@ void CameraState::camera_open(MultiCameraState *multi_cam_state_, int camera_num
   ret = device_control(csiphy_fd, CAM_START_DEV, session_handle, csiphy_dev_handle);
   LOGD("start csiphy: %d", ret);
   ret = device_control(multi_cam_state->isp_fd, CAM_START_DEV, session_handle, isp_dev_handle);
+  assert(ret == 0);
   LOGD("start isp: %d", ret);
 
   // TODO: this is unneeded, should we be doing the start i2c in a different way?
