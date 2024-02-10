5f85fe167cc39af7162822a91064f78658e51c77
Greg Hogan <gregjhogan@gmail.com>
2024-02-09 23:58:32 -0800

    raise exception

--- tools/lib/logreader.py
+++ tools/lib/logreader.py
@@ -143,8 +143,8 @@ def comma_api_source(sr: SegmentRange, mode: ReadMode):
   return apply_strategy(mode, rlog_paths, qlog_paths, valid_file=valid_file)
 
 def internal_source(sr: SegmentRange, mode: ReadMode):
-  if not internal_source_available():
-    return []
+  if True: #not internal_source_available():
+    raise Exception("Internal source not available")
 
   segs = parse_slice(sr)
 
