43acbc6fb7bab0f55463546f5fadb468d4fcd870
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-02-09 21:50:52 -0800

    bump cereal

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -80,7 +80,7 @@ class Controls:
                                    'driverMonitoringState', 'longitudinalPlan', 'liveLocationKalman',
                                    'managerState', 'liveParameters', 'radarState', 'liveTorqueParameters',
                                    'testJoystick'] + self.camera_packets + self.sensor_packets,
-                                  ignore_alive=ignore, ignore_avg_freq=['radarState', 'testJoystick'], ignore_valid=['testJoystick', ])
+                                  ignore_alive=ignore, ignore_avg_freq=ignore+['radarState', 'testJoystick'], ignore_valid=['testJoystick', ], freq=int(1/DT_CTRL))
 
     if CI is None:
       # wait for one pandaState and one CAN packet
