03977c02c2ea39d0c2a161d492f416ceef40797f
Irene <12470297+cydia2020@users.noreply.github.com>
2024-02-13 17:11:28 +1100

    reduce low speed oscillation

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -266,7 +266,7 @@ class CarInterface(CarInterfaceBase):
 
     tune = ret.longitudinalTuning
     tune.deadzoneBP = [0., 16., 20., 30.]
-    tune.deadzoneV = [0., .03, .06, .15]
+    tune.deadzoneV = [.04, .05, .08, .15]
     ret.stoppingDecelRate = 0.17  # This is okay for TSS-P
     if candidate in TSS2_CAR:
       ret.vEgoStopping = 0.25
