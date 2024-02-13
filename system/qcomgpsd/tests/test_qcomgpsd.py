1c1202bc5e5dfad7ffc98f504069f7ab4a6c3fd5
Comma Device <device@comma.ai>
2024-02-13 22:00:52 +0000

    ensure started

--- system/qcomgpsd/tests/test_qcomgpsd.py
+++ system/qcomgpsd/tests/test_qcomgpsd.py
@@ -30,6 +30,7 @@ class TestRawgpsd(unittest.TestCase):
     os.system("sudo systemctl restart ModemManager lte")
 
   def setUp(self):
+    os.system("sudo systemctl start systemd-resolved")
     at_cmd("AT+QGPSDEL=0")
     self.sm = messaging.SubMaster(['qcomGnss', 'gpsLocation', 'gnssMeasurements'])
 
