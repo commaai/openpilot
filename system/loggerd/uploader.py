2274f435e38b842d8aa23a5b4b50f03c6e3cdf8a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-02-13 23:17:58 -0800

    update test

--- system/loggerd/uploader.py
+++ system/loggerd/uploader.py
@@ -42,13 +42,16 @@ class FakeResponse:
     self.request = FakeRequest()
 
 
+def get_directory_sort(d: str) -> List[str]:
+  return [s.rjust(10, '0') for s in d.rsplit('--', 1)]
+
 def listdir_by_creation(d: str) -> List[str]:
   if not os.path.isdir(d):
     return []
 
   try:
     paths = [f for f in os.listdir(d) if os.path.isdir(os.path.join(d, f))]
-    paths = sorted(paths)
+    paths = sorted(paths, key=get_directory_sort)
     return paths
   except OSError:
     cloudlog.exception("listdir_by_creation failed")
