f42994a239ca82c8937725dd90be6c227c9f56d8
Shane Smiskol <shane@smiskol.com>
2024-02-14 05:42:47 -0800

    no abs on hybrid, and we have eps!

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -509,6 +509,13 @@ FW_QUERY_CONFIG = FwQueryConfig(
       obd_multiplexing=False,
     ),
   ],
+  # We lose these ECUs without the comma power on these cars.
+  # Note that we still attempt to match with them when they are present
+  non_essential_ecus={
+    Ecu.abs: [CAR.SONATA],
+    Ecu.engine: [CAR.SONATA, CAR.SONATA_HYBRID],
+    Ecu.transmission: [CAR.SONATA, CAR.SONATA_HYBRID],
+  },
   extra_ecus=[
     (Ecu.adas, 0x730, None),         # ADAS Driving ECU on HDA2 platforms
     (Ecu.parkingAdas, 0x7b1, None),  # ADAS Parking ECU (may exist on all platforms)
