2e8c62358ce8ab103d0c9ee29501f78d79d7f4cc
Shane Smiskol <shane@smiskol.com>
2024-02-17 06:03:12 -0600

    Honda Bosch: detect alternate brake signal (#31500)
    * Do Accord
    
    * add comment
    
    * good test
    
    * this catches the accord/accordh issue!
    
    * as expected, only if both radar&camera have in common FW does the test fail
    
    * cmt
    
    * clean up
    
    * better
    
    * Use HondaFlags
    
    * detect alt brake
    
    * for test
    
    * hypothesis isn't installed
    
    * test failure
    
    * works
    
    * Revert " works"
    
    This reverts commit bfc0d808abe548630e6507431f13b01e8a1316cb.
    
    * Revert "test failure"
    
    This reverts commit 10ab6eb63ccd411740751b742f31fd610397fe8e.
    
    * Revert "hypothesis isn't installed"
    
    This reverts commit d474cc3f0ed7e84fe8bd24f452b3315fd2b8d47c.
    
    * Revert "for test"
    
    This reverts commit 98e039f4fc0189ccb57c1dae6b344209ef15eb1c.
    
    * this is important too
    
    * clean up
    
    * more clean up
    
    * Update ref_commit

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -3,8 +3,9 @@ from cereal import car
 from panda import Panda
 from openpilot.common.conversions import Conversions as CV
 from openpilot.common.numpy_fast import interp
+from openpilot.selfdrive.car.honda.hondacan import get_pt_bus
 from openpilot.selfdrive.car.honda.values import CarControllerParams, CruiseButtons, HondaFlags, CAR, HONDA_BOSCH, HONDA_NIDEC_ALT_SCM_MESSAGES, \
-                                                                                            HONDA_BOSCH_ALT_BRAKE_SIGNAL, HONDA_BOSCH_RADARLESS
+                                                 HONDA_BOSCH_RADARLESS
 from openpilot.selfdrive.car import create_button_events, get_safety_config
 from openpilot.selfdrive.car.interfaces import CarInterfaceBase
 from openpilot.selfdrive.car.disable_ecu import disable_ecu
@@ -275,7 +276,8 @@ class CarInterface(CarInterfaceBase):
       raise ValueError(f"unsupported car {candidate}")
 
     # These cars use alternate user brake msg (0x1BE)
-    if candidate in HONDA_BOSCH_ALT_BRAKE_SIGNAL:
+    if 0x1BE in fingerprint[get_pt_bus(candidate)] and candidate in HONDA_BOSCH:
+      ret.flags |= HondaFlags.BOSCH_ALT_BRAKE.value
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_HONDA_ALT_BRAKE
 
     # These cars use alternate SCM messages (SCM_FEEDBACK AND SCM_BUTTON)
