f11f9aeed8a33ff7dfaf775d157afda83cfb1de2
Shane Smiskol <shane@smiskol.com>
2024-02-17 04:41:27 -0800

    no unknown

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -210,7 +210,6 @@ FW_QUERY_CONFIG = FwQueryConfig(
     Ecu.electricBrakeBooster: [CAR.ACCORD, CAR.CIVIC_BOSCH, CAR.CRV_5G],
     Ecu.shiftByWire: [CAR.ACCORD],
     Ecu.eps: [CAR.ACCORD],
-    Ecu.unknown: [CAR.ACCORD],  # TODO: we shouldn't be fingerprinting on unknown ECUs
     Ecu.srs: [CAR.ACCORD],
     Ecu.hud: [CAR.ACCORD],
   },
