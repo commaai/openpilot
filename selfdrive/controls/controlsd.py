58713bb4934cfd4a2f0fe0b780893551cae56f64
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-02-18 14:36:46 -0800

    self

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -313,7 +313,7 @@ class Controls:
         safety_mismatch = pandaState.safetyModel not in IGNORED_SAFETY_MODES
 
       # safety mismatch allows some time for boardd to set the safety mode and publish it back from panda
-      if (safety_mismatch and sm.frame*DT_CTRL > 10.) or pandaState.safetyRxChecksInvalid or self.mismatch_counter >= 200:
+      if (safety_mismatch and self.sm.frame*DT_CTRL > 10.) or pandaState.safetyRxChecksInvalid or self.mismatch_counter >= 200:
         self.events.add(EventName.controlsMismatch)
 
       if log.PandaState.FaultType.relayMalfunction in pandaState.faults:
