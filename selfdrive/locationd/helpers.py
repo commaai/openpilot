109d80414cfb22b2bf3be7413d3c9adaa4ab5ffc
Shane Smiskol <shane@smiskol.com>
2024-02-20 21:41:47 -0800

    this is redundant

--- selfdrive/locationd/helpers.py
+++ selfdrive/locationd/helpers.py
@@ -39,9 +39,7 @@ class PointBuckets:
     return individual_buckets_valid and total_points_valid
 
   def is_calculable(self) -> bool:
-    individual_buckets_valid = all(len(v) > 0 for v in self.buckets.values())
-    total_points_valid = self.__len__() > 0
-    return individual_buckets_valid and total_points_valid
+    return all(len(v) > 0 for v in self.buckets.values())
 
   def add_point(self, x: float, y: float, bucket_val: float) -> None:
     raise NotImplementedError
