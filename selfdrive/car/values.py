93290c41186f04e622216369201670d6af6f5c22
Justin Newberry <justin@comma.ai>
2024-02-23 09:58:34 -0800

    Hmm

---
+++ selfdrive/car/values.py
@@ -0,0 +1,14 @@
+from openpilot.selfdrive.car.body.values import CAR as BODY_CAR
+from openpilot.selfdrive.car.chrysler.values import CAR as CHRYSLER_CAR
+from openpilot.selfdrive.car.ford.values import CAR as FORD_CAR
+from openpilot.selfdrive.car.gm.values import CAR as GM_CAR
+from openpilot.selfdrive.car.honda.values import CAR as HONDA_CAR
+from openpilot.selfdrive.car.hyundai.values import CAR as HYUNDAI_CAR
+from openpilot.selfdrive.car.nissan.values import CAR as NISSAN_CAR
+from openpilot.selfdrive.car.subaru.values import CAR as SUBARU_CAR
+from openpilot.selfdrive.car.tesla.values import CAR as TESLA_CAR
+from openpilot.selfdrive.car.toyota.values import CAR as TOYOTA_CAR
+from openpilot.selfdrive.car.volkswagen.values import CAR as VOLKSWAGEN_CAR
+
+
+ALL_CAR = BODY_CAR | CHRYSLER_CAR | FORD_CAR | GM_CAR | HONDA_CAR | HYUNDAI_CAR | NISSAN_CAR | SUBARU_CAR | TESLA_CAR | TOYOTA_CAR | VOLKSWAGEN_CAR
