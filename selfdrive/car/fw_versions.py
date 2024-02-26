9e3c4c97ba45104ec5027a64fa149d5337cb1693
Cameron Clough <cameronjclough@gmail.com>
2024-02-26 20:13:38 +0000

    more implicit optional

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -260,8 +260,8 @@ def get_fw_versions_ordered(logcan, sendcan, ecu_rx_addrs: set[EcuAddrBusType], 
   return all_car_fw
 
 
-def get_fw_versions(logcan, sendcan, query_brand: str | None = None, extra: OfflineFwVersions | None = None, timeout: float = 0.1, \
-                    num_pandas: int = 1, debug: bool = False, progress: bool = False) -> list[capnp.lib.capnp._DynamicStructBuilder]:
+def get_fw_versions(logcan, sendcan, query_brand: str = None, extra: OfflineFwVersions = None, timeout: float = 0.1, num_pandas: int = 1, \
+                    debug: bool = False, progress: bool = False) -> list[capnp.lib.capnp._DynamicStructBuilder]:
   versions = VERSIONS.copy()
   params = Params()
 
