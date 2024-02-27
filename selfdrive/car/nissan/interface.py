d3f0f76a7e33832bb7312597f85b802b50f2d757
Justin Newberry <justin@comma.ai>
2024-02-27 13:16:06 -0500

    Nissan: move to platform config (#31599)
    * do nissan
    
    * cleanup + fix

--- selfdrive/car/nissan/interface.py
+++ selfdrive/car/nissan/interface.py
@@ -16,25 +16,13 @@ class CarInterface(CarInterfaceBase):
     ret.steerLimitTimer = 1.0
 
     ret.steerActuatorDelay = 0.1
-    ret.steerRatio = 17
 
     ret.steerControlType = car.CarParams.SteerControlType.angle
     ret.radarUnavailable = True
 
-    if candidate in (CAR.ROGUE, CAR.XTRAIL):
-      ret.mass = 1610
-      ret.wheelbase = 2.705
-      ret.centerToFront = ret.wheelbase * 0.44
-    elif candidate in (CAR.LEAF, CAR.LEAF_IC):
-      ret.mass = 1610
-      ret.wheelbase = 2.705
-      ret.centerToFront = ret.wheelbase * 0.44
-    elif candidate == CAR.ALTIMA:
+    if candidate == CAR.ALTIMA:
       # Altima has EPS on C-CAN unlike the others that have it on V-CAN
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_NISSAN_ALT_EPS_BUS
-      ret.mass = 1492
-      ret.wheelbase = 2.824
-      ret.centerToFront = ret.wheelbase * 0.44
 
     return ret
 
