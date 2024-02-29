1f6316470a54c2bbe21aff8f61361f36d0f96120
ZwX1616 <zwx1616@gmail.com>
2024-02-28 16:20:39 -0800

    clean up

--- system/camerad/sensors/ar0231.cc
+++ system/camerad/sensors/ar0231.cc
@@ -7,6 +7,8 @@
 
 namespace {
 
+const size_t AR0231_FRAME_WIDTH = 1928;
+const size_t AR0231_FRAME_HEIGHT = 1208;
 const size_t AR0231_REGISTERS_HEIGHT = 2;
 // TODO: this extra height is universal and doesn't apply per camera
 const size_t AR0231_STATS_HEIGHT = 2 + 8;
@@ -80,14 +82,14 @@ float ar0231_parse_temp_sensor(uint16_t calib1, uint16_t calib2, uint16_t data_r
 AR0231::AR0231() {
   image_sensor = cereal::FrameData::ImageSensor::AR0231;
   data_word = true;
-  frame_width = FRAME_WIDTH;
-  frame_height = FRAME_HEIGHT;
-  frame_stride = FRAME_STRIDE;
+  frame_width = AR0231_FRAME_WIDTH;
+  frame_height = AR0231_FRAME_HEIGHT;
+  frame_stride = (AR0231_FRAME_WIDTH * 12 / 8) + 4;
   extra_height = AR0231_REGISTERS_HEIGHT + AR0231_STATS_HEIGHT;
 
   registers_offset = 0;
   frame_offset = AR0231_REGISTERS_HEIGHT;
-  stats_offset = AR0231_REGISTERS_HEIGHT + FRAME_HEIGHT;
+  stats_offset = AR0231_REGISTERS_HEIGHT + AR0231_FRAME_HEIGHT;
 
   start_reg_array.assign(std::begin(start_reg_array_ar0231), std::end(start_reg_array_ar0231));
   init_reg_array.assign(std::begin(init_array_ar0231), std::end(init_array_ar0231));
