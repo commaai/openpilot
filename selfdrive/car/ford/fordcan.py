88c941a0976a462457c1b4b6330f0b89c10cff4e
Shane Smiskol <shane@smiskol.com>
2024-02-28 20:47:49 -0800

    Ford: remove unnecessary __init__
    old-commit-hash: 7687cafe8cc805a8be139c4d8a797bffd0df3fc8

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -5,9 +5,6 @@ HUDControl = car.CarControl.HUDControl
 
 
 class CanBus(CanBusBase):
-  def __init__(self, CP=None, fingerprint=None) -> None:
-    super().__init__(CP, fingerprint)
-
   @property
   def main(self) -> int:
     return self.offset
