3816b6a4b6ceda80f06d140c913c33a5d9ea40e3
Justin Newberry <justin@comma.ai>
2024-03-01 04:01:59 +0000

    basic platform config tests
    old-commit-hash: 00f2703bbb70d5d79d3b64cb6b2974efb36cb4d4

---
+++ selfdrive/car/tests/test_platform_configs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from openpilot.selfdrive.car.values import PLATFORMS
+
+
+class TestPlatformConfigs(unittest.TestCase):
+  def test_configs(self):
+
+    for platform in PLATFORMS.values():
+      with self.subTest(platform=str(platform)):
+        if hasattr(platform, "config"):
+
+          self.assertTrue(platform.config._frozen)
+          self.assertIn("pt", platform.config.dbc_dict)
+          self.assertTrue(len(platform.config.platform_str) > 0)
+
+          # enable when all cars have specs
+          #self.assertIsNotNone(platform.config.specs)
+
+
+if __name__ == "__main__":
+  unittest.main()
