ebe05f7640661e5df326325b2c9cb12e380366e8
Chris TenHarmsel <chris@tenharmsel.com>
2024-03-03 19:07:06 +0000

    Tweak max torque calculation

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -78,13 +78,14 @@ class CarController(CarControllerBase):
     # steering, so because we're "tricking" ADAS into thinking LKAS is always active, we need to make sure we're
     # applying torque when the driver is not actively steering.
     # The default value chosen here is based on observations of the stock LKAS system when it's engaged
+    # CS.out.steeringPressed and steeringTorque are based on the STEERING_COL_TORQUE value
     if not bool(CS.out.steeringPressed):
       self.lkas_max_torque = 130
     else:
       # Steering torque seems to be a different scale than applied torque, so we calculate a percentage
-      # based on observed "max" values (~|20|) and then apply that percentage to our normal max torque
-      driver_applied_torque_pct = min(abs(CS.out.steeringTorque) / 20.0, 1.0)
-      130 - (driver_applied_torque_pct * 130)
+      # based on observed "max" values (~|1200|) and then apply that percentage to our normal max torque
+      driver_applied_torque_pct = min(abs(CS.out.steeringTorque) / 1200.0, 1.0)
+      self.lkas_max_torque = 130 - (driver_applied_torque_pct * 130)
 
     if not CC.latActive:
       apply_angle = CS.out.steeringAngleDeg
