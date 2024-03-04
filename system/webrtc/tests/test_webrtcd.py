a50681282f1a3a6b970f84a8dcb4b289be078ce6
Kacper Rączy <gfw.kra@gmail.com>
2024-03-04 13:48:52 -0800

    Test case

--- system/webrtc/tests/test_webrtcd.py
+++ system/webrtc/tests/test_webrtcd.py
@@ -11,6 +11,7 @@ from openpilot.system.webrtc.webrtcd import get_stream
 
 import aiortc
 from teleoprtc import WebRTCOfferBuilder
+from parameterized import parameterized
 
 
 class TestWebrtcdProc(unittest.IsolatedAsyncioTestCase):
@@ -21,10 +22,16 @@ class TestWebrtcdProc(unittest.IsolatedAsyncioTestCase):
     except TimeoutError:
       self.fail("Timeout while waiting for awaitable to complete")
 
-  async def test_webrtcd(self):
+  @parameterized.expand([
+    (["testJoystick"], ["carState"]),
+    ([], ["carState"]),
+    (["testJoystick"], []),
+    ([], []),
+  ])
+  async def test_webrtcd(self, in_services, out_services):
     mock_request = MagicMock()
     async def connect(offer):
-      body = {'sdp': offer.sdp, 'cameras': offer.video, 'bridge_services_in': [], 'bridge_services_out': ['carState']}
+      body = {'sdp': offer.sdp, 'cameras': offer.video, 'bridge_services_in': in_services, 'bridge_services_out': out_services}
       mock_request.json.side_effect = AsyncMock(return_value=body)
       response = await get_stream(mock_request)
       response_json = json.loads(response.text)
