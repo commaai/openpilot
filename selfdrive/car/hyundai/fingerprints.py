e5f196ee2c16ee07b45569e4415665b070ca76ad
Jason Wen <haibin.wen3@gmail.com>
2024-03-04 23:34:55 -0500

    HKG: Add FW versions for Genesis GV70 2022 3.5T HDA2

--- selfdrive/car/hyundai/fingerprints.py
+++ selfdrive/car/hyundai/fingerprints.py
@@ -1617,11 +1617,13 @@ FW_VERSIONS = {
       b'\xf1\x00JK1 MFC  AT USA LHD 1.00 1.01 99211-AR200 220125',
       b'\xf1\x00JK1 MFC  AT USA LHD 1.00 1.01 99211-AR300 220125',
       b'\xf1\x00JK1 MFC  AT USA LHD 1.00 1.04 99211-AR000 210204',
+      b'\xf1\x00JK1 MFC  AT USA LHD 1.00 1.04 99211-AR100 210204',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00JK1_ SCC FHCUP      1.00 1.00 99110-AR200         ',
       b'\xf1\x00JK1_ SCC FHCUP      1.00 1.00 99110-AR300         ',
       b'\xf1\x00JK1_ SCC FHCUP      1.00 1.02 99110-AR000         ',
+      b'\xf1\x00JK1_ SCC -----      1.00 1.02 99110-AR100         ',
     ],
   },
   CAR.GENESIS_GV60_EV_1ST_GEN: {
