530f6e8b2f97529944845817f6574f936fe97539
Shane Smiskol <shane@smiskol.com>
2024-03-06 00:52:27 -0800

    safe

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -192,7 +192,8 @@ class CarInterface(CarInterfaceBase):
   def _update(self, c):
     ret = self.CS.update(self.cp, self.cp_cam)
 
-    ret.buttonEvents = create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise})
+    if self.CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR):
+      ret.buttonEvents = create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise})
 
     # events
     events = self.create_common_events(ret)
