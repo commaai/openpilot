70e5f52f23f208a08e34ceabc5adb437968e2331
Shane Smiskol <shane@smiskol.com>
2024-03-07 03:41:54 -0800

    stuff

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -43,8 +43,8 @@ class CarState(CarStateBase):
     self.prev_distance_button = 0
     self.distance_button = 0
 
-    self.distance_lines = 0
-    self.distance_lines_values = can_define.dv['PCM_CRUISE_2']['PCM_FOLLOW_DISTANCE']
+    self.pcm_follow_distance = 0
+    self.pcm_follow_distance_values = can_define.dv['PCM_CRUISE_2']['PCM_FOLLOW_DISTANCE']
 
     self.low_speed_lockout = False
     self.acc_type = 1
@@ -169,14 +169,16 @@ class CarState(CarStateBase):
     if self.CP.carFingerprint != CAR.PRIUS_V:
       self.lkas_hud = copy.copy(cp_cam.vl["LKAS_HUD"])
 
-    # distance button is wired to the ACC module (camera or radar)
-    self.prev_distance_button = self.distance_button
-    if self.CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR):
-      self.distance_button = cp_acc.vl["ACC_CONTROL"]["DISTANCE"]
+    if self.CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR) or (self.CP.flags & ToyotaFlags.SMART_DSU and not self.CP.flags & ToyotaFlags.RADAR_CAN_FILTER):
+      if self.CP.carFingerprint not in UNSUPPORTED_DSU_CAR:
+        self.pcm_follow_distance = cp.vl["PCM_CRUISE_2"]["PCM_FOLLOW_DISTANCE"]
 
-      self.distance_lines = cp.vl["PCM_CRUISE_2"]["PCM_FOLLOW_DISTANCE"]
-    elif self.CP.flags & ToyotaFlags.SMART_DSU and not self.CP.flags & ToyotaFlags.RADAR_CAN_FILTER:
-      self.distance_button = cp.vl["SDSU"]["FD_BUTTON"]
+      # distance button is wired to the ACC module (camera or radar)
+      self.prev_distance_button = self.distance_button
+      if self.CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR):
+        self.distance_button = cp_acc.vl["ACC_CONTROL"]["DISTANCE"]
+      else:
+        self.distance_button = cp.vl["SDSU"]["FD_BUTTON"]
 
     return ret
 
