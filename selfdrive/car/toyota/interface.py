71aa162dfd4b1336e2ae9ead693939bdcfdef6c5
Shane Smiskol <shane@smiskol.com>
2024-03-07 01:38:10 -0800

    add events for SDSU

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -173,7 +173,7 @@ class CarInterface(CarInterfaceBase):
   def _update(self, c):
     ret = self.CS.update(self.cp, self.cp_cam)
 
-    if self.CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR):
+    if self.CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR) or self.CP.flags & ToyotaFlags.SMART_DSU:
       ret.buttonEvents = create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise})
 
     # events
