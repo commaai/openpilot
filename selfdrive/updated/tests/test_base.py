b93f6ce4f6fd34d33f990e86bde22b5cec49f2da
Justin Newberry <justin@comma.ai>
2024-03-08 13:46:57 -0500

    updated: prep for new updater (#31695)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * proc
    
    * release
    
    * fix
    
    * this should move here
    
    * e2e update test
    
    * that too
    
    * fix
    
    * fix
    
    * fix running in docker
    
    * don't think GHA will work
    
    * also test switching branches
    
    * it's a test
    
    * lets not delete that yet
    
    * fix
    
    * fix2
    
    * fix
    
    * fix
    
    * tests too
    
    * fix
    
    * cleanup / init
    
    * test agnos update
    
    * test agnos
    
    * move this back up
    
    * no diff

--- selfdrive/updated/tests/test_base.py
+++ selfdrive/updated/tests/test_base.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import os
 import pathlib
 import shutil
@@ -33,12 +32,14 @@ def update_release(directory, name, version, agnos_version, release_notes):
   with open(directory / "launch_env.sh", "w") as f:
     f.write(f'export AGNOS_VERSION="{agnos_version}"')
 
-  run(["git", "add", "."], cwd=directory)
-  run(["git", "commit", "-m", f"openpilot release {version}"], cwd=directory)
-
 
 @pytest.mark.slow # TODO: can we test overlayfs in GHA?
-class TestUpdateD(unittest.TestCase):
+class BaseUpdateTest(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    if "Base" in cls.__name__:
+      raise unittest.SkipTest
+
   def setUp(self):
     self.tmpdir = tempfile.mkdtemp()
 
@@ -73,15 +74,15 @@ class TestUpdateD(unittest.TestCase):
   def setup_basedir_release(self, release):
     self.params = Params()
     self.set_target_branch(release)
-    run(["git", "clone", "-b", release, self.remote_dir, self.basedir])
 
   def update_remote_release(self, release):
-    update_release(self.remote_dir, release, *self.MOCK_RELEASES[release])
+    raise NotImplementedError("")
 
   def setup_remote_release(self, release):
-    run(["git", "init"], cwd=self.remote_dir)
-    run(["git", "checkout", "-b", release], cwd=self.remote_dir)
-    self.update_remote_release(release)
+    raise NotImplementedError("")
+
+  def additional_context(self):
+    raise NotImplementedError("")
 
   def tearDown(self):
     mock.patch.stopall()
@@ -134,7 +135,7 @@ class TestUpdateD(unittest.TestCase):
     self.setup_remote_release("release3")
     self.setup_basedir_release("release3")
 
-    with processes_context(["updated"]) as [updated]:
+    with self.additional_context(), processes_context(["updated"]) as [updated]:
       self._test_params("release3", False, False)
       self.wait_for_idle()
       self._test_params("release3", False, False)
@@ -150,7 +151,7 @@ class TestUpdateD(unittest.TestCase):
     self.setup_remote_release("release3")
     self.setup_basedir_release("release3")
 
-    with processes_context(["updated"]) as [updated]:
+    with self.additional_context(), processes_context(["updated"]) as [updated]:
       self._test_params("release3", False, False)
       self.wait_for_idle()
       self._test_params("release3", False, False)
@@ -177,7 +178,7 @@ class TestUpdateD(unittest.TestCase):
     self.setup_remote_release("master")
     self.setup_basedir_release("release3")
 
-    with processes_context(["updated"]) as [updated]:
+    with self.additional_context(), processes_context(["updated"]) as [updated]:
       self._test_params("release3", False, False)
       self.wait_for_idle()
       self._test_params("release3", False, False)
@@ -201,10 +202,11 @@ class TestUpdateD(unittest.TestCase):
     self.setup_remote_release("release3")
     self.setup_basedir_release("release3")
 
-    with mock.patch("openpilot.system.hardware.AGNOS", "True"), \
-         mock.patch("openpilot.system.hardware.tici.hardware.Tici.get_os_version", "1.2"), \
-         mock.patch("openpilot.system.hardware.tici.agnos.get_target_slot_number"), \
-         mock.patch("openpilot.system.hardware.tici.agnos.flash_agnos_update"), \
+    with self.additional_context(), \
+        mock.patch("openpilot.system.hardware.AGNOS", "True"), \
+        mock.patch("openpilot.system.hardware.tici.hardware.Tici.get_os_version", "1.2"), \
+        mock.patch("openpilot.system.hardware.tici.agnos.get_target_slot_number"), \
+        mock.patch("openpilot.system.hardware.tici.agnos.flash_agnos_update"), \
           processes_context(["updated"]) as [updated]:
 
       self._test_params("release3", False, False)
@@ -226,7 +228,3 @@ class TestUpdateD(unittest.TestCase):
 
       self._test_params("release3", False, True)
       self._test_update_params("release3", *self.MOCK_RELEASES["release3"])
-
-
-if __name__ == "__main__":
-  unittest.main()
