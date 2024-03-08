a7d043049b6747ada598ec230a016260e2eaf818
Justin Newberry <justin@comma.ai>
2024-03-08 21:20:30 +0000

    fix tinygrad

--- release/check-submodules.sh
+++ release/check-submodules.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 while read hash submodule ref; do
+  if [ "$submodule" == "tinygrad_repo" ]; then
+    # Skip checks for non-comma submodules
+    continue
+  fi
   git -C $submodule fetch --depth 1000 origin master
   git -C $submodule branch -r --contains $hash | grep "origin/master"
   if [ "$?" -eq 0 ]; then
