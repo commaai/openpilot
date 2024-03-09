8f07d797ad72ec7ab32bdb105871125e0a4735f2
wgenh <162819820+wgenh@users.noreply.github.com>
2024-03-09 18:28:02 +0000

    Refactor scripts/test_relcache.py

--- scripts/test_relcache.py
+++ scripts/test_relcache.py
@@ -9,14 +9,14 @@ class Test_RelCached(unittest.TestCase):
   def test_relcache(self):
     valid_start_strings = ('Compiling /', '[1/1] Cythonizing', 'Scanning directory ')
     with open(OUTPUT_FILE, 'r') as f:
-      for line in f:
+      for index, line in enumerate(f):
         if line.startswith(valid_start_strings):
           continue
         else:
           words = line.split(" ")
           for wrd in words:
             if WORKSPACE in wrd:
-                self.fail(f"Contains absolute path ({wrd}) while building")
+                self.fail(f"Contains absolute path ({wrd}) in line {index+1}\n")
 
 if __name__ == "__main__":
   del sys.argv[1:]
