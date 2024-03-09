593d5c96b9d9578957e212a1bef8385275022a32
Shane Smiskol <shane@smiskol.com>
2024-03-08 23:02:04 -0800

    more appropriate up here

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -141,9 +141,9 @@ class CarController(CarControllerBase):
       lead = hud_control.leadVisible or CS.out.vEgo < 12.  # at low speed we always assume the lead is present so ACC can be engaged
 
       # Press distance button until we are at the correct bar length. Only change while enabled to avoid skipping startup popup
-      if self.frame % 6 == 0:
+      if self.CP.openpilotLongitudinalControl and self.frame % 6 == 0:
         desired_distance = 4 - hud_control.leadDistanceBars
-        if self.CP.openpilotLongitudinalControl and CS.out.cruiseState.enabled and CS.pcm_follow_distance != desired_distance:
+        if CS.out.cruiseState.enabled and CS.pcm_follow_distance != desired_distance:
           self.distance_button = not self.distance_button
         else:
           self.distance_button = 0
