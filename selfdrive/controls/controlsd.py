9b0df58e1e9ca96c10fa2f5d01832d81d3646d22
Shane Smiskol <shane@smiskol.com>
2024-03-10 04:32:49 -0700

    fix

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -687,7 +687,7 @@ class Controls:
     hudControl.speedVisible = self.enabled
     hudControl.lanesVisible = self.enabled
     hudControl.leadVisible = self.sm['longitudinalPlan'].hasLead
-    hudControl.leadDistanceBars = self.sm['longitudinalPlan'].personality.raw + 1
+    hudControl.leadDistanceBars = self.personality + 1
 
     hudControl.rightLaneVisible = True
     hudControl.leftLaneVisible = True
