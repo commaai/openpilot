40ee2b39f339d8302317d794769cc8000dd8daf0
GRIFFIT807 <96277491+GRIFFIT807@users.noreply.github.com>
2024-03-11 11:19:34 -0500

    Update values.py

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -139,6 +139,7 @@ class CAR(Platforms):
       FordCarInfo("Ford Maverick Hybrid 2022", "LARIAT Luxury"),
       FordCarInfo("Ford Maverick 2023", "Co-Pilot360 Assist"),
       FordCarInfo("Ford Maverick Hybrid 2023", "Co-Pilot360 Assist"),
+      FordCarInfo("Ford Maverick 2024", "Co-Pilot360 Assist"),
     ],
     CarSpecs(mass=1650, wheelbase=3.076, steerRatio=17.0),
   )
