5f39a6f8be016c8a96ff377ead24a60029a9f0fb
Jason Wen <haibin.wen3@gmail.com>
2024-03-12 01:05:27 -0400

    Honda Longitudinal: Display personality in instrument cluster (#31799)
    * Honda Longitudinal: Display personality in instrument cluster
    
    * Simpler
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    
    * cleanup
    
    * Update selfdrive/car/honda/hondacan.py
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -143,7 +143,7 @@ def create_ui_commands(packer, CAN, CP, enabled, pcm_speed, hud, is_metric, acc_
     acc_hud_values = {
       'CRUISE_SPEED': hud.v_cruise,
       'ENABLE_MINI_CAR': 1 if enabled else 0,
-      'HUD_DISTANCE': 0,  # max distance setting on display
+      'HUD_DISTANCE': (hud.lead_distance_bars + 1) % 4,  # wraps to 0 at 4 bars
       'IMPERIAL_UNIT': int(not is_metric),
       'HUD_LEAD': 2 if enabled and hud.lead_visible else 1 if enabled else 0,
       'SET_ME_X01_2': 1,
