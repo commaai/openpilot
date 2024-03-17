1ea370c75eb575e5aaff30d4bdbdb17727e9fe39
wgenh <162819820+wgenh@users.noreply.github.com>
2024-03-17 10:28:54 +0000

    fix typo

--- system/loggerd/tests/test_deleter.py
+++ system/loggerd/tests/test_deleter.py
@@ -164,8 +164,8 @@ class TestDeleter(UploaderTestCase):
     index = 0
     for candidates_for_deletion in created_files:
       offset = len(candidates_for_deletion)
-      deleted = delete_order[index:index + offset] # Checking if the file has been deleted in the correct order.
-      self.assertCountEqual(candidates_for_deletion, deleted, "Some files or directories were deleted in a different order.")
+      deleted = delete_order[index:index + offset] # Checking if the files have been deleted in the correct order.
+      self.assertCountEqual(candidates_for_deletion, deleted, "Some files or directories have been deleted in a different order.")
       index += offset
 
 
