d6c175b80577874d86a346bf034d55200740b5a4
nhnn <nhnn@disroot.org>
2024-03-18 21:04:15 +0300

    change "mock" to "MOCK" and rename torque data of Nissan Leaf 2018 IC

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -21,7 +21,7 @@ class CarInterface(CarInterfaceBase):
 
     # safety config
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.chrysler)]
-    if candidate in RAM_HD_5TH_GEN:
+    if candidate in RAM_HD:
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_CHRYSLER_RAM_HD
     elif candidate in RAM_DT:
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_CHRYSLER_RAM_DT
