567093cef09c4173c60a3c9d0c223662fcb190b3
Justin Newberry <justin@comma.ai>
2024-03-19 22:43:24 +0000

    passes

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -23,7 +23,7 @@ class CarControllerParams:
       self.STEER_MAX = 1000
       self.STEER_DELTA_UP = 40
       self.STEER_DELTA_DOWN = 40
-    elif CP.carFingerprint == CAR.IMPREZA_2020:
+    elif CP.carFingerprint == CAR.SUBARU_IMPREZA_2020:
       self.STEER_MAX = 1439
     else:
       self.STEER_MAX = 2047
