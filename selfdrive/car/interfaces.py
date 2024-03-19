bb2154b731f18b04a08ae8720b6c781d75d56d80
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-03-19 14:10:16 -0700

    fix

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -89,7 +89,7 @@ class CarInterfaceBase(ABC):
     self.cp_loopback = self.CS.get_loopback_can_parser(CP)
     self.can_parsers = [self.cp, self.cp_cam, self.cp_adas, self.cp_body, self.cp_loopback]
 
-    dbc_name = "" if self.cp is None else self.cp.dbc_anme
+    dbc_name = "" if self.cp is None else self.cp.dbc_name
     self.CC: CarControllerBase = CarController(dbc_name, CP, self.VM)
 
   def apply(self, c: car.CarControl, now_nanos: int) -> tuple[car.CarControl.Actuators, list[tuple[int, int, bytes, int]]]:
