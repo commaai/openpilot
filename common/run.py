3ab117d599aedf4579a3f4b541d6d2b2672e4482
Justin Newberry <justin@comma.ai>
2024-03-20 19:43:58 -0400

    add get_build_metadata function (#31923)
    * version
    
    * Get build metadata
    
    * two lines
    
    * channel
    
    * cwd
    
    * default to unknown
    
    * dataclass
    old-commit-hash: 38d03b997958effea805a6c3860d2c5edaf2ae65

--- common/run.py
+++ common/run.py
@@ -1,13 +1,13 @@
 import subprocess
 
 
-def run_cmd(cmd: list[str]) -> str:
-  return subprocess.check_output(cmd, encoding='utf8').strip()
+def run_cmd(cmd: list[str], cwd=None) -> str:
+  return subprocess.check_output(cmd, encoding='utf8', cwd=cwd).strip()
 
 
-def run_cmd_default(cmd: list[str], default: str = "") -> str:
+def run_cmd_default(cmd: list[str], default: str = "", cwd=None) -> str:
   try:
-    return run_cmd(cmd)
+    return run_cmd(cmd, cwd=cwd)
   except subprocess.CalledProcessError:
     return default
 
