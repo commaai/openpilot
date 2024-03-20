b88ad88fecc3e6bc9d6f2fc670c3a8368a7ebc2c
Shane Smiskol <shane@smiskol.com>
2024-03-19 22:53:18 -0700

    only the first is missing srs?!

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -314,7 +314,7 @@ FW_QUERY_CONFIG = FwQueryConfig(
 
   # CRV_HYBRID:
   # ('eps', 'fwdCamera', 'fwdRadar', 'srs'): routes: 1794, dongles: {'9b19234732591434', 'e906fc18a593603a', 'c2b195c154e0ba54', 'd6c98b1ebf9916b9', 'db6ce4711c1b4ccb', 'a3367c53243b7b6a', '8f3f4683d7cfda74', 'b9ecad3ab8b3ed53', '11fa019479471a0a', '420cb5afd2f5f563', '16981d0978378bfe', '6ca0bbb0b026b5a5'}
-  # (): routes: 84, dongles: {'fe90f3d7403f2411'}  # NOTE: shouldn't be possible, but user has some old routes with everything except srs, ignoring for now
+  # (): routes: 84, dongles: {'fe90f3d7403f2411'}  # NOTE: shouldn't be possible, but user has some old routes with everything, ignoring for now
 
   # FIT
   # ('eps', 'fwdRadar', 'srs', 'vsa'): routes: 1545, dongles: {'df67bf140583e302', '8411411300bf023d', '48536bafa6cabe3e', 'ac04cdfc0c735c0b', '445d1ade497345f3', 'd98f9841b9db8943', 'a6883439bbd3d975', 'e56002a2b1eeed4d', '0ac44bcb111aed3f', 'f4cc9d3bfec4e712', '8085a4d3c9416184', '8a867b1f51563696'}
