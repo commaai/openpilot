228e9ea312b44dfff178e1983684e3cf38f1d5e8
Justin Newberry <justin@comma.ai>
2024-03-21 17:17:52 +0000

    dont commit that

--- release/copy_build_files.sh
+++ release/copy_build_files.sh
@@ -3,14 +3,12 @@
 SOURCE_DIR=$1
 TARGET_DIR=$2
 
-FILES_SRC="release/files_pc"
-
-# if [ -f /TICI ]; then
-#   FILES_SRC="release/files_tici"
-# else
-#   echo "no release files set"
-#   exit 1
-# fi
+if [ -f /TICI ]; then
+  FILES_SRC="release/files_tici"
+else
+  echo "no release files set"
+  exit 1
+fi
 
 cd $SOURCE_DIR
 cp -pR --parents $(cat release/files_*) $TARGET_DIR/
