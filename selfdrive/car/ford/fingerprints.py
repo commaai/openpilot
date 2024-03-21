d30681efdc232dc7a9fe941aa7aff8d439b7dfc3
Cameron Clough <cameronjclough@gmail.com>
2024-03-21 19:32:18 +0000

    add FW (using debug_fw_fingerprinting_offline)

--- selfdrive/car/ford/fingerprints.py
+++ selfdrive/car/ford/fingerprints.py
@@ -148,12 +148,16 @@ FW_VERSIONS = {
   },
   CAR.FORD_RANGER_MK2: {
     (Ecu.eps, 0x730, None): [
+      b'NL14-14D003-AE\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.abs, 0x760, None): [
+      b'PB3C-2D053-ZD\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.fwdRadar, 0x764, None): [
+      b'ML3T-14D049-AL\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.fwdCamera, 0x706, None): [
+      b'PJ6T-14H102-ABJ\x00\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
   }
 }
