9d128cb1bbb28fb0cb8da6725c291ebe285e7467
Cameron Clough <cameronjclough@gmail.com>
2024-03-21 19:16:34 +0000

    debug

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -15,7 +15,7 @@ class CarInterface(CarInterfaceBase):
   @staticmethod
   def _get_params(ret, candidate, fingerprint, car_fw, experimental_long, docs):
     ret.carName = "ford"
-    ret.dashcamOnly = bool(ret.flags & FordFlags.CANFD)
+    ret.dashcamOnly = False  # bool(ret.flags & FordFlags.CANFD)
 
     ret.radarUnavailable = True
     ret.steerControlType = car.CarParams.SteerControlType.angle
