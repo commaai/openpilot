05628df1dae41db43e38fb68eb54f8f17f3f0339
Justin Newberry <justin@comma.ai>
2024-03-22 20:21:59 +0000

    revert that

--- release/copy_build_files.sh
+++ release/copy_build_files.sh
@@ -3,7 +3,12 @@
 SOURCE_DIR=$1
 TARGET_DIR=$2
 
-FILE_SRC=release/files_pc
+if [ -f /TICI ]; then
+  FILES_SRC="release/files_tici"
+else
+  echo "no release files set"
+  exit 1
+fi
 
 cd $SOURCE_DIR
 cp -pR --parents $(cat release/files_common) $TARGET_DIR/
