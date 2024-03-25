61da0fd009e4991ac1021c619b82fd4a66cac306
Andrei Radulescu <andi.radulescu@gmail.com>
2024-03-25 18:26:19 +0200

    third_party: build scripts for libyuv and maplibre-native-qt (#31988)
    * updated maplibre build.sh
    
    * refactored libyuv build.sh
    old-commit-hash: 245cbe97c758f86c21e28d8edd6390b8c9da29db

--- third_party/libyuv/build.sh
+++ third_party/libyuv/build.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://chromium.googlesource.com/libyuv/libyuv
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+
+ARCHNAME=$(uname -m)
+if [ -f /TICI ]; then
+  ARCHNAME="larch64"
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  ARCHNAME="Darwin"
+fi
+
+cd $DIR
+if [ ! -d libyuv ]; then
+  git clone --single-branch https://chromium.googlesource.com/libyuv/libyuv
+fi
+
 cd libyuv
-git reset --hard 4a14cb2e81235ecd656e799aecaaf139db8ce4a2
+git checkout 4a14cb2e81235ecd656e799aecaaf139db8ce4a2
+
+# build
 cmake .
+make -j$(nproc)
+
+INSTALL_DIR="$DIR/$ARCHNAME"
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+
+rm -rf $DIR/include
+mkdir -p $INSTALL_DIR/lib
+cp $DIR/libyuv/libyuv.a $INSTALL_DIR/lib
+cp -r $DIR/libyuv/include $DIR
 
 ## To create universal binary on Darwin:
 ## ```
