3b237bd26a42e944187bb1e5cf9110f76e873857
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-03-26 20:42:25 +0100

    DAS_controlCounter fix

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -50,7 +50,6 @@ class CarController(CarControllerBase):
 
       if self.model3:
         counter = CS.das_control["DAS_controlCounter"]
-        self.last_das_control_counter = counter
         if self.last_das_control_counter != counter:
           self.last_das_control_counter = counter
           can_sends.extend(self.tesla_can.create_longitudinal_commands(acc_state, target_speed, min_accel, max_accel, counter))
