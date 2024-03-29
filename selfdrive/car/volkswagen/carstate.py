c9685dd297f682cdbf877deec209f1b5c98f234b
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2024-03-29 03:21:40 -0400

    VW MQB: Speed limiter is nonAdaptive (#31954)
    * VW MQB: Speed limiter is nonAdaptive
    
    * revise
    
    * Update selfdrive/car/volkswagen/carstate.py
    
    * Update selfdrive/car/volkswagen/carstate.py
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -119,6 +119,8 @@ class CarState(CarStateBase):
     # currently regulating speed (3), driver accel override (4), brake only (5)
     ret.cruiseState.available = pt_cp.vl["TSK_06"]["TSK_Status"] in (2, 3, 4, 5)
     ret.cruiseState.enabled = pt_cp.vl["TSK_06"]["TSK_Status"] in (3, 4, 5)
+    # Speed limiter mode; ECM faults if we command ACC while not pcmCruise
+    ret.cruiseState.nonAdaptive = bool(pt_cp.vl["TSK_06"]["TSK_Limiter_ausgewaehlt"])
     ret.accFaulted = pt_cp.vl["TSK_06"]["TSK_Status"] in (6, 7)
 
     self.esp_hold_confirmation = bool(pt_cp.vl["ESP_21"]["ESP_Haltebestaetigung"])
