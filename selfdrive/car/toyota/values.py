901ba7a7547751a17a85fbb507f69f3139225a89
Shane Smiskol <shane@smiskol.com>
2024-03-29 00:19:29 -0700

    1.5

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -16,7 +16,7 @@ PEDAL_TRANSITION = 10. * CV.MPH_TO_MS
 
 
 class CarControllerParams:
-  ACCEL_MAX = 2.0  # m/s2, lower than allowed 2.0 m/s2 for tuning reasons
+  ACCEL_MAX = 1.5  # m/s2, lower than allowed 2.0 m/s2 for tuning reasons
   ACCEL_MIN = -3.5  # m/s2
 
   STEER_STEP = 1
