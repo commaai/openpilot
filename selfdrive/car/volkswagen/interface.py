8eadf54b3b152e1261b9bbc3b3e3674d9f818ab9
Shane Smiskol <shane@smiskol.com>
2024-04-02 12:50:54 -0700

    fix

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -1,5 +1,6 @@
 from cereal import car
 from panda import Panda
+from openpilot.common.conversions import Conversions as CV
 from openpilot.selfdrive.car import get_safety_config
 from openpilot.selfdrive.car.interfaces import CarInterfaceBase
 from openpilot.selfdrive.car.volkswagen.values import CAR, CANBUS, NetworkLocation, TransmissionType, GearShifter, VolkswagenFlags, VolkswagenCarSpecs
