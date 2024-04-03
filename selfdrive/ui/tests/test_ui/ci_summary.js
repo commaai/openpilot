3057dc0fa542cdf166bd1c75f47eaef6403fd350
nhnn <nhnn@disroot.org>
2024-04-03 13:45:22 +0300

    remove useless console.logs

--- selfdrive/ui/tests/test_ui/ci_summary.js
+++ selfdrive/ui/tests/test_ui/ci_summary.js
@@ -21,14 +21,10 @@ module.exports = async ({ core, glob }) => {
 
   let languages = nodeFs.readdirSync(REPORT_DIR).map((x) => nodePath.basename(x));
 
-  console.log(REPORT_DIR, languages);
-
   for (let language_code of languages) {
     const language_name = Object.keys(knownLanguages).find(key => knownLanguages[key] === language_code);
     const language_dir = nodePath.resolve(REPORT_DIR, language_code);
     summary = summary.addHeading(language_name, '2');
-
-    console.log(`${language_name} (${language_code}) - ${language_dir}`)
 
     let table = [
       [{ data: 'Case', header: true }, { data: 'Screenshot', header: true }],
@@ -36,8 +32,6 @@ module.exports = async ({ core, glob }) => {
 
     const globber = await glob.create(language_dir + "/**/*");
     const files = await globber.glob();
-
-    console.log(language_dir + "/**/*", files);
 
     for (let file of files) {
       let relativeFile = file.split(language_dir + '/')[1].split(".png")[0];
