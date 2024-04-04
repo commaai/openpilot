e5f02fa21bfc0676e3a662af4cb85103ec1da1a2
Yassine <yassine.y10@gmail.com>
2024-04-04 16:08:26 -0700

    lint

--- selfdrive/test/process_replay/model_replay.py
+++ selfdrive/test/process_replay/model_replay.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 import os
 import sys
-import time
 from collections import defaultdict
 from typing import Any
 
-import cereal.messaging as messaging
 from openpilot.common.git import get_commit
-from openpilot.common.params import Params
 from openpilot.system.hardware import PC
-from openpilot.selfdrive.manager.process_config import managed_processes
 from openpilot.tools.lib.openpilotci import BASE_URL, get_url
 from openpilot.selfdrive.test.process_replay.compare_logs import compare_logs, format_diff
 from openpilot.selfdrive.test.process_replay.process_replay import get_process_config, replay_process
