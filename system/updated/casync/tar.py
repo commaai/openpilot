d10b50d0700a1b2d1e6723380735ee142c682b73
Justin Newberry <justin@comma.ai>
2024-04-04 19:35:49 +0000

    context mnager

--- system/updated/casync/tar.py
+++ system/updated/casync/tar.py
@@ -7,29 +7,27 @@ def include_default(_) -> bool:
   return True
 
 
-def create_tar_archive(fh: IO[bytes], directory: pathlib.Path, include: Callable[[pathlib.Path], bool] = include_default):
+def create_tar_archive(filename, directory: pathlib.Path, include: Callable[[pathlib.Path], bool] = include_default):
   """Creates a tar archive of a directory"""
 
-  tar = tarfile.open(fileobj=fh, mode='w')
-  for file in sorted(directory.rglob("*"), key=lambda f: f.stat().st_size if f.is_file() else 0, reverse=True):
-    if not include(file):
-      continue
-    relative_path = str(file.relative_to(directory))
-    if file.is_symlink():
-      info = tarfile.TarInfo(relative_path)
-      info.type = tarfile.SYMTYPE
-      info.linkpath = str(file.readlink())
-      tar.addfile(info)
+  with tarfile.open(filename, 'w') as tar:
+    for file in sorted(directory.rglob("*"), key=lambda f: f.stat().st_size if f.is_file() else 0, reverse=True):
+      if not include(file):
+        continue
+      relative_path = str(file.relative_to(directory))
+      if file.is_symlink():
+        info = tarfile.TarInfo(relative_path)
+        info.type = tarfile.SYMTYPE
+        info.linkpath = str(file.readlink())
+        tar.addfile(info)
 
-    elif file.is_file():
-      info = tarfile.TarInfo(relative_path)
-      info.size = file.stat().st_size
-      info.type = tarfile.REGTYPE
-      with file.open('rb') as f:
-        tar.addfile(info, f)
+      elif file.is_file():
+        info = tarfile.TarInfo(relative_path)
+        info.size = file.stat().st_size
+        info.type = tarfile.REGTYPE
+        with file.open('rb') as f:
+          tar.addfile(info, f)
 
-  tar.close()
-  fh.seek(0)
 
 
 def extract_tar_archive(fh: IO[bytes], directory: pathlib.Path):
