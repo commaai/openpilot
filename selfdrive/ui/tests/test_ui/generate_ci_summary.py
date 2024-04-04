fcad36fe52544e5f25fe10fc010aa0cd582c5ec7
nhnn <nhnn@disroot.org>
2024-04-04 22:11:21 +0300

    rewrite ci summary generator to python and remove js

---
+++ selfdrive/ui/tests/test_ui/generate_ci_summary.py
@@ -0,0 +1,23 @@
+import os
+import json
+import pathlib
+from openpilot.tools.lib.openpilotcontainers import OpenpilotCIContainer
+
+run_id = os.environ["GITHUB_RUN_ID"] + "-" + os.environ["GITHUB_RUN_ATTEMPT"]
+TEST_DIR = pathlib.Path(__file__).parent
+SCREENSHOTS_DIR = TEST_DIR / "report" / "screenshots"
+
+summary = open(os.environ["GITHUB_STEP_SUMMARY"], 'a+')
+languages = json.load(open(os.path.normpath(__file__ + "/../../../translations/languages.json"), "r"))
+
+summary.write("# UI screenshots\n\n")
+
+for language_dir in SCREENSHOTS_DIR.iterdir():
+  language_name = list(languages.keys())[list(languages.values()).index(language_dir.name)]
+  summary.write(f"## {language_name}\n\n")
+
+  for case in language_dir.glob("**/*"):
+    name = case.relative_to(SCREENSHOTS_DIR).replace("/", "-").replace(".png", "")
+    blob_name = f"{run_id}-{name}"
+    link = OpenpilotCIContainer.upload_file(str(case), blob_name, "image/png")
+    summary.write(f"### {name}\n\n![{name}]({link})\n\n")
