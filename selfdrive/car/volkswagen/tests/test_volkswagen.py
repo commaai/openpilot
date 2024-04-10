b4b7dcd0db72ecc5305a1fd548d0df11b80d4acb
Shane Smiskol <shane@smiskol.com>
2024-04-09 22:19:17 -0700

    test matching

--- selfdrive/car/volkswagen/tests/test_volkswagen.py
+++ selfdrive/car/volkswagen/tests/test_volkswagen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 
-from openpilot.selfdrive.car.volkswagen.values import CAR
+from openpilot.selfdrive.car.volkswagen.values import CAR, FW_QUERY_CONFIG
 
 
 class TestVolkswagenPlatformConfigs(unittest.TestCase):
@@ -16,6 +16,20 @@ class TestVolkswagenPlatformConfigs(unittest.TestCase):
             continue
           self.assertEqual(set(), platform.config.chassis_codes & comp.config.chassis_codes,
                            f"Shared chassis codes: {comp}")
+
+  def test_custom_fingerprinting(self):
+    matches = FW_QUERY_CONFIG.match_fw_to_car_custom(None, '0' * 17, None)
+    self.assertEqual(set(), matches, "Bad match")
+
+    for platform in CAR:
+      with self.subTest(platform=platform):
+        for chassis_code in platform.config.chassis_codes:
+          vin = ['0'] * 17
+          vin[6:8] = chassis_code
+          vin = ''.join(vin)
+
+          matches = FW_QUERY_CONFIG.match_fw_to_car_custom(None, vin, None)
+          self.assertEqual({platform}, matches, "Bad match")
 
 
 if __name__ == "__main__":
