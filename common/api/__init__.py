86985a4c1d09e4ada1d70bd6a92a2b32f2f7a9b3
Justin Newberry <justin@comma.ai>
2024-04-11 17:11:44 +0000

    revert

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -5,9 +5,7 @@ from datetime import datetime, timedelta
 from openpilot.system.hardware.hw import Paths
 from openpilot.system.version import get_version
 
-
 API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
-
 
 class Api():
   def __init__(self, dongle_id):
