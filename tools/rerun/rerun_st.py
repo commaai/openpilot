69bcb56baad88e4a1110b9260f49007f8b1dd81b
savojovic <jovo.savic00@gmail.com>
2024-04-12 15:00:05 +0200

    Adding dynamic blueprint creation and menu to choose what to plot

--- tools/rerun/rerun_st.py
+++ tools/rerun/rerun_st.py
@@ -1,9 +1,20 @@
-# TODO: install pip3 install rerun-sdk
+# TODO: add install pip3 install rerun-sdk
 import sys
 import json
 from openpilot.tools.lib.logreader import LogReader
 from openpilot.tools.lib.route import Route
 import rerun as rr
+import rerun.blueprint as rrb
+
+is_first_run = True
+topics = ['accelerometer', 'androidLog', 'cameraOdometry', 'can', 'carControl', 'carParams', 'carState', 'clocks', 'controlsState', 'deviceState',
+          'driverCameraState', 'driverEncodeIdx', 'driverMonitoringState', 'driverStateV2', 'errorLogMessage', 'gnssMeasurements', 'gpsLocationExternal',
+          'gyroscope', 'initData', 'lateralPlanDEPRECATED', 'lightSensor', 'liveCalibration', 'liveLocationKalman', 'liveParameters', 'liveTorqueParameters',
+          'liveTracks', 'logMessage', 'longitudinalPlan', 'magnetometer', 'managerState', 'mapRenderState', 'microphone', 'modelV2', 'navInstruction',
+          'navModel', 'navThumbnail', 'onroadEvents', 'pandaStates', 'peripheralState', 'procLog', 'qRoadEncodeIdx', 'radarState', 'roadCameraState',
+          'roadEncodeIdx', 'sendcan', 'sentinel', 'temperatureSensor', 'thumbnail', 'ubloxGnss', 'ubloxRaw', 'uiDebug', 'uiPlan', 'uploaderState',
+          'wideRoadCameraState', 'wideRoadEncodeIdx']
+
 
 def convertBytesToString(data):
   if isinstance(data, bytes):
@@ -15,47 +26,61 @@ def convertBytesToString(data):
   else:
     return data
 
+# Log data to rerun, here could be values be excluded form plotting
+def log_data(key, value):
+      rr.log(str(key), rr.Scalar(value))
+      ## Example of excluding a value from plotting
+      # if key.find("timestamp") == -1:
+      #   rr.log(str(key), rr.Scalar(value))
 
-def flatJson(data, logMonoTime, parent_key=''):
-    if isinstance(data, dict):
-        for k, v in data.items():
-            if parent_key:
-                key = f"{parent_key}.{k}"
-            else:
-                key = k
-            if isinstance(v, (dict, list)):
-                flatJson(v, logMonoTime, key)
-            else:
-                if isinstance(v, (int, float)):
-                  rr.set_time_sequence(k, logMonoTime)
-                  rr.log(k, rr.Scalar(v))
-                else:
-                  print("Not plottable value " + str(v))
-    elif isinstance(data, list):
-        for i, v in enumerate(data):
-            if isinstance(v, (int, float)):
-              rr.set_time_sequence(str(i), logMonoTime)
-              rr.log(f"{parent_key}[{i}]", rr.Scalar(v))
-            else:
-               print("Not plottable value " + str(v))
-    else:
-        if isinstance(v, (int, float)):
-          rr.log(parent_key, rr.Scalar(data))
-        else:
-          print("Not plottable value " + str(data))
 
-# TODO: Add "Space view" for every msg.which() property
-def addGraph(logPaths):
+# Log every json attribute
+def log_nested(obj, parent_key=''):
+    for k, v in obj.items():
+        if isinstance(v, dict):
+            log_nested(v, parent_key + k + "/")
+        elif isinstance(v, (int, float)):
+            log_data(parent_key + k, v)
+        elif isinstance(v, list):
+            for index, item in enumerate(v):
+                if isinstance(item, (int, float)):
+                    log_data("/" + parent_key + k + "/" + str(index), item)
+        # else:
+        #     print("Not a plottable value")
+
+
+# Create a blueprint for selected topics
+def createBlueprint(topicsToGraph):
+  print("Generating blueprint")
+  timeSeriesViews = []
+  for topic in topicsToGraph:
+      timeSeriesViews.append(rrb.TimeSeriesView(name=topic, origin=f"/{topic}/"))
+      rr.log(topic, rr.SeriesLine(name=topic), timeless=True)
+  blueprint = rrb.Blueprint(
+                          rrb.Vertical(
+                            *timeSeriesViews,
+                            rrb.SelectionPanel(expanded=False),
+                            rrb.TimePanel(expanded=False)))
+  return blueprint
+
+
+def addGraphs(logPaths, topicsToGraph):
+  print(f"Downloading logs [{len(logPaths)}]")
   counter = 1
   for logPath in logPaths:
     print(counter)
     counter+=1
     rlog = LogReader(logPath)
     for msg in rlog:
-      if msg.which() == "deviceState": # Select one property to plot, one space view available
+      global is_first_run
+      if is_first_run:
+        blueprint = createBlueprint(topicsToGraph)
+        rr.init("rerun_test",spawn=True, default_blueprint=blueprint)
+        is_first_run = False
+      if msg.which() in topicsToGraph:
         jsonMsg = json.loads(json.dumps(convertBytesToString(msg.to_dict())))
-        flatJson(jsonMsg, jsonMsg.get("logMonoTime"), parent_key='')
-        continue
+        rr.set_time_sequence(msg.which(), msg.logMonoTime)
+        log_nested(jsonMsg)
 
 
 if __name__ == '__main__':
@@ -64,9 +89,16 @@ if __name__ == '__main__':
     print("No route was provided, using demo route")
   else:
     route_name = sys.argv[1]
+  # Print menu
+  print(f"What topics would you like to graph? [0-{len(topics)-1}]")
+  for index, topic in enumerate(topics):
+    print(str(index) + ". " + topic)
+  # Get user input
+  topicIndexes = [int(x) for x in (input("Separate your inputs with a space: ")).split(" ")]
+  topicsToGraph = [topics[i] for i in topicIndexes]
+
   # Get logs for a route
   print("Getting route log paths")
-  rr.init("plot_example", spawn=True)
   route = Route(route_name)
   logPaths = route.log_paths()
-  addGraph(logPaths)
+  addGraphs(logPaths, topicsToGraph)
