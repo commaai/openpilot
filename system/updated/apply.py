bf11542fd9523920bd69f9baf932c69a87ca6e9f
Justin Newberry <justin@comma.ai>
2024-04-12 18:18:52 +0000

    actually apply the update

--- system/updated/apply.py
+++ system/updated/apply.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # applies the finalized updates from FINALIZED to their proper directory
-import os
 import pathlib
 
 from openpilot.system.updated.common import USERDATA, FINALIZED, get_valid_flag
@@ -11,7 +10,18 @@ if __name__ == "__main__":
     print("no valid update found in FINALIZED, skipping...")
     exit(0)
 
-  for f in pathlib.Path(FINALIZED).glob("*"):
-    if f.is_dir():
-      apply_dir = os.path.join(USERDATA, f.stem)
-      print(f"apply finalized update: {f} to {apply_dir}")
+  finalized = pathlib.Path(FINALIZED)
+  userdata = pathlib.Path(USERDATA)
+
+  previous_update = userdata / "previous_update"
+
+  previous_update.mkdir(exist_ok=True)
+
+  for finalized_dir in finalized.glob("*"):
+    if finalized_dir.is_dir():
+      previous_dir = previous_update / finalized_dir.stem
+      target_dir = userdata / finalized_dir.stem
+      print(f"apply finalized update: {finalized_dir} to {target_dir} and putting old update in {previous_dir}")
+
+      target_dir.rename(previous_dir)
+      finalized_dir.rename(target_dir)
