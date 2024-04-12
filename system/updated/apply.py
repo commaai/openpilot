95d18cade982a80539301bdb0e14498fa844ae46
Justin Newberry <justin@comma.ai>
2024-04-12 18:21:24 +0000

    remove old directory

--- system/updated/apply.py
+++ system/updated/apply.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # applies the finalized updates from FINALIZED to their proper directory
 import pathlib
+import shutil
 
 from openpilot.system.updated.common import USERDATA, FINALIZED, get_valid_flag, set_valid_flag
 
@@ -21,7 +22,11 @@ if __name__ == "__main__":
     if finalized_dir.is_dir():
       previous_dir = previous_update / finalized_dir.stem
       target_dir = userdata / finalized_dir.stem
+
       print(f"apply finalized update: {finalized_dir} to {target_dir} and putting old update in {previous_dir}")
+
+      if previous_dir.exists():
+        shutil.rmtree(previous_dir)
 
       target_dir.rename(previous_dir)
       finalized_dir.rename(target_dir)
