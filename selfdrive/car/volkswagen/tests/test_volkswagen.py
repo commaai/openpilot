02920d67b74410c273987ac7afffc37efddfddc1
Shane Smiskol <shane@smiskol.com>
2024-04-12 21:19:20 -0700

    Volkswagen: move FW pattern to test (#32191)
    move

--- selfdrive/car/volkswagen/tests/test_volkswagen.py
+++ selfdrive/car/volkswagen/tests/test_volkswagen.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import re
 import unittest
 
-from openpilot.selfdrive.car.volkswagen.values import SPARE_PART_FW_PATTERN
 from openpilot.selfdrive.car.volkswagen.fingerprints import FW_VERSIONS
+
+# TODO: determine the unknown groups
+SPARE_PART_FW_PATTERN = re.compile(b'\xf1\x87(?P<gateway>[0-9][0-9A-Z]{2})(?P<unknown>[0-9][0-9A-Z][0-9])(?P<unknown2>[0-9A-Z]{2}[0-9])([A-Z0-9]| )')
 
 
 class TestVolkswagenPlatformConfigs(unittest.TestCase):
