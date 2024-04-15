89f9181d6ef924a4cc824264c152eb4177ec0991
Justin Newberry <justin@comma.ai>
2024-04-15 16:44:43 -0700

    casync build: remove channel from build metadata json (#32212)
    * remove channel
    
    * fix
    
    * reset
    
    * remove this
    
    * no channel
    old-commit-hash: cedf98de5a2a948bd7d19b23ac964cf845a4769d

--- release/create_casync_release.py
+++ release/create_casync_release.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="creates a casync release")
   parser.add_argument("target_dir", type=str, help="target directory to build channel from")
   parser.add_argument("output_dir", type=str, help="output directory for the channel")
-  parser.add_argument("channel", type=str, help="what channel this build is")
   args = parser.parse_args()
 
   target_dir = pathlib.Path(args.target_dir)
@@ -21,7 +20,7 @@ if __name__ == "__main__":
   build_metadata = get_build_metadata()
   build_metadata.openpilot.build_style = "release" if os.environ.get("RELEASE", None) is not None else "debug"
 
-  create_build_metadata_file(target_dir, build_metadata, args.channel)
+  create_build_metadata_file(target_dir, build_metadata)
 
   digest, caibx = create_casync_release(target_dir, output_dir, build_metadata.canonical)
 
