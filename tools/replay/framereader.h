ce50e56c3b13ca0e0ddd38f03356f02eaf8eb84c
deanlee <deanlee3@gmail.com>
2024-04-17 17:17:34 +0800

    no longer cache all AVPacket instances in memory

--- tools/replay/framereader.h
+++ tools/replay/framereader.h
@@ -22,11 +22,9 @@ public:
   ~FrameReader();
   bool load(const std::string &url, bool no_hw_decoder = false, std::atomic<bool> *abort = nullptr, bool local_cache = false,
             int chunk_size = -1, int retries = 0);
-  bool load(const std::byte *data, size_t size, bool no_hw_decoder = false, std::atomic<bool> *abort = nullptr);
+  bool loadFromFile(const std::string &file, bool no_hw_decoder = false, std::atomic<bool> *abort = nullptr);
   bool get(int idx, VisionBuf *buf);
-  int getYUVSize() const { return width * height * 3 / 2; }
-  size_t getFrameCount() const { return packets.size(); }
-  bool valid() const { return valid_; }
+  size_t getFrameCount() const { return packets_info.size(); }
 
   int width = 0, height = 0;
 
@@ -36,16 +34,17 @@ private:
   AVFrame * decodeFrame(AVPacket *pkt);
   bool copyBuffers(AVFrame *f, VisionBuf *buf);
 
-  std::vector<AVPacket*> packets;
   std::unique_ptr<AVFrame, AVFrameDeleter>av_frame_, hw_frame;
   AVFormatContext *input_ctx = nullptr;
   AVCodecContext *decoder_ctx = nullptr;
-  int key_frames_count_ = 0;
-  bool valid_ = false;
-  AVIOContext *avio_ctx_ = nullptr;
 
   AVPixelFormat hw_pix_fmt = AV_PIX_FMT_NONE;
   AVBufferRef *hw_device_ctx = nullptr;
   int prev_idx = -1;
+  struct PacketInfo {
+    int flags;
+    int64_t pos;
+  };
+  std::vector<PacketInfo> packets_info;
   inline static std::atomic<bool> has_hw_decoder = true;
 };
