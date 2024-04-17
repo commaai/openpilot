9e3e5a94370c518854650ffb2e18f93b0d20b32a
Justin Newberry <justin@comma.ai>
2024-04-17 16:33:13 -0700

    consolidate casync build into fewer scripts and fix pc release build (#32225)
    * less scripts
    
    * better
    
    * fixes
    
    * naming
    
    * revert
    
    * cleanup
    
    * lets test it
    
    * fix that one
    
    * and rm
    
    * don't run this
    
    * fix
    
    * not here
    
    * revert testing
    
    * fix docs
    
    * default here too
    
    * t
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 62f053bc4d028dcdf89c6910b0acf72aafadca31

--- release/package_casync_agnos.py
+++ release/package_casync_agnos.py
@@ -8,27 +8,27 @@ import time
 from openpilot.common.basedir import BASEDIR
 from openpilot.system.hardware.tici.agnos import StreamingDecompressor, unsparsify, noop, AGNOS_MANIFEST_FILE
 from openpilot.system.updated.casync.common import create_casync_from_file
+from release.package_casync_build import upload_casync_release
 
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="creates a casync release")
-  parser.add_argument("output_dir", type=str, help="output directory for the channel")
-  parser.add_argument("working_dir", type=str, help="working directory")
   parser.add_argument("--manifest", type=str, help="json manifest to create agnos release from", \
                         default=str(pathlib.Path(BASEDIR) / AGNOS_MANIFEST_FILE))
   args = parser.parse_args()
 
-  output_dir = pathlib.Path(args.output_dir)
-  output_dir.mkdir(parents=True, exist_ok=True)
-
-  working_dir = pathlib.Path(args.working_dir)
-  working_dir.mkdir(parents=True, exist_ok=True)
-
   manifest_file = pathlib.Path(args.manifest)
 
-  with tempfile.NamedTemporaryFile(dir=str(working_dir)) as entry_file:
-    entry_path = pathlib.Path(entry_file.name)
+  with tempfile.TemporaryDirectory() as temp_dir:
+    working_dir = pathlib.Path(temp_dir)
+    casync_dir = working_dir / "casync"
+    casync_dir.mkdir()
+
+    agnos_casync_dir = casync_dir / "agnos"
+    agnos_casync_dir.mkdir()
+
+    entry_path = working_dir / "entry"
 
     with open(manifest_file) as f:
       manifest = json.load(f)
@@ -53,5 +53,7 @@ if __name__ == "__main__":
 
       start = time.monotonic()
       agnos_filename = os.path.basename(entry["url"]).split(".")[0]
-      create_casync_from_file(entry_path, output_dir, agnos_filename)
+      create_casync_from_file(entry_path, agnos_casync_dir, agnos_filename)
       print(f"created casnc in {time.monotonic() - start}")
+
+    upload_casync_release(casync_dir)
