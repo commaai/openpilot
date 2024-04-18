dc2abee2003629be6f100f526c78465afc5778b9
deanlee <deanlee3@gmail.com>
2024-04-18 13:45:21 +0800

    share decoder context
    cleanup includes

--- tools/replay/framereader.h
+++ tools/replay/framereader.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <memory>
 #include <string>
 #include <vector>
 
 #include "cereal/visionipc/visionbuf.h"
+#include "system/camerad/cameras/camera_common.h"
 #include "tools/replay/filereader.h"
 
 extern "C" {
@@ -12,39 +12,46 @@ extern "C" {
 #include <libavformat/avformat.h>
 }
 
-struct AVFrameDeleter {
-  void operator()(AVFrame* frame) const { av_frame_free(&frame); }
-};
+class VideoDecoder;
 
 class FrameReader {
 public:
   FrameReader();
   ~FrameReader();
-  bool load(const std::string &url, bool no_hw_decoder = false, std::atomic<bool> *abort = nullptr, bool local_cache = false,
+  bool load(CameraType type, const std::string &url, bool no_hw_decoder = false, std::atomic<bool> *abort = nullptr, bool local_cache = false,
             int chunk_size = -1, int retries = 0);
-  bool loadFromFile(const std::string &file, bool no_hw_decoder = false, std::atomic<bool> *abort = nullptr);
+  bool loadFromFile(CameraType type, const std::string &file, bool no_hw_decoder = false, std::atomic<bool> *abort = nullptr);
   bool get(int idx, VisionBuf *buf);
   size_t getFrameCount() const { return packets_info.size(); }
 
   int width = 0, height = 0;
 
-private:
-  bool initHardwareDecoder(AVHWDeviceType hw_device_type);
-  bool decode(int idx, VisionBuf *buf);
-  AVFrame * decodeFrame(AVPacket *pkt);
-  bool copyBuffers(AVFrame *f, VisionBuf *buf);
-
-  std::unique_ptr<AVFrame, AVFrameDeleter>av_frame_, hw_frame;
+  VideoDecoder *decoder_ = nullptr;
   AVFormatContext *input_ctx = nullptr;
-  AVCodecContext *decoder_ctx = nullptr;
-
-  AVPixelFormat hw_pix_fmt = AV_PIX_FMT_NONE;
-  AVBufferRef *hw_device_ctx = nullptr;
   int prev_idx = -1;
   struct PacketInfo {
     int flags;
     int64_t pos;
   };
   std::vector<PacketInfo> packets_info;
-  inline static std::atomic<bool> has_hw_decoder = true;
+};
+
+
+class VideoDecoder {
+public:
+  VideoDecoder();
+  ~VideoDecoder();
+  bool open(AVCodecParameters *codecpar, bool hw_decoder);
+  bool decode(FrameReader *reader, int idx, VisionBuf *buf);
+  int width = 0, height = 0;
+
+private:
+  bool initHardwareDecoder(AVHWDeviceType hw_device_type);
+  AVFrame *decodeFrame(AVPacket *pkt);
+  bool copyBuffer(AVFrame *f, VisionBuf *buf);
+
+  AVFrame *av_frame_, *hw_frame_;
+  AVCodecContext *decoder_ctx = nullptr;
+  AVPixelFormat hw_pix_fmt = AV_PIX_FMT_NONE;
+  AVBufferRef *hw_device_ctx = nullptr;
 };
