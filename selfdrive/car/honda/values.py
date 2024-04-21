96ac38250a4379d100563d696d156bee613b5a7e
Alexandre Nobuharu Sato <66435071+AlexandreSato@users.noreply.github.com>
2024-04-21 06:29:43 -0300

    Honda: Brazilian HR-V 2023 fingerprint (#32243)
    * fingerprint Honda HR-V 2023, brazilian market
    
    * update routes
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    old-commit-hash: 17ec4ad24ea446a99235223511792f8e9dfa16c7

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -165,7 +165,7 @@ class CAR(Platforms):
     [HondaCarDocs("Honda HR-V 2023", "All")],
     CarSpecs(mass=3125 * CV.LB_TO_KG, wheelbase=2.61, steerRatio=15.2, centerToFrontRatio=0.41, tireStiffnessFactor=0.5),
     dbc_dict('honda_civic_ex_2022_can_generated', None),
-    flags=HondaFlags.BOSCH_RADARLESS | HondaFlags.BOSCH_ALT_BRAKE,
+    flags=HondaFlags.BOSCH_RADARLESS,
   )
   ACURA_RDX_3G = HondaBoschPlatformConfig(
     [HondaCarDocs("Acura RDX 2019-22", "All", min_steer_speed=3. * CV.MPH_TO_MS)],
