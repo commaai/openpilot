46bc25f1b45db2f89be6ae4ff4d87fb20a7aeb8c
Northport IO <89657497+northportio@users.noreply.github.com>
2024-04-29 19:05:01 -0400

    Add Harness info

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -66,7 +66,7 @@ class FordCarDocs(CarDocs):
 
   def init_make(self, CP: car.CarParams):
     harness = CarHarness.ford_q4 if CP.flags & FordFlags.CANFD else CarHarness.ford_q3
-    if CP.carFingerprint in (CAR.FORD_BRONCO_SPORT_MK1, CAR.FORD_MAVERICK_MK1, CAR.FORD_F_150_MK14, CAR.FORD_F_150_LIGHTNING_MK1):
+    if CP.carFingerprint in (CAR.FORD_BRONCO_SPORT_MK1, CAR.FORD_MAVERICK_MK1, CAR.FORD_F_150_MK14, CAR.FORD_F_150_LIGHTNING_MK1, CAR.FORD_ESCAPE_MK4_23REFRESH):
       self.car_parts = CarParts([Device.threex_angled_mount, harness])
     else:
       self.car_parts = CarParts([Device.threex, harness])
