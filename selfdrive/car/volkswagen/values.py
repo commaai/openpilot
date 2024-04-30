35640a8aa459b06cb2ec851e8892786c6b91e539
Jason Young <jyoung8607@gmail.com>
2024-04-30 11:37:17 -0400

    wrong model year format

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -372,8 +372,8 @@ class CAR(Platforms):
   )
   SEAT_ATECA_MK1 = VolkswagenMQBPlatformConfig(
     [
-      VWCarDocs("SEAT Ateca 2016-2023"),
-      VWCarDocs("CUPRA Ateca 2018-2023"),
+      VWCarDocs("CUPRA Ateca 2018-23"),
+      VWCarDocs("SEAT Ateca 2016-23"),
       VWCarDocs("SEAT Leon 2014-20"),
     ],
     VolkswagenCarSpecs(mass=1300, wheelbase=2.64),
