ce62ce07685f0debf9bf4d9801430f99f5ec1113
Jason Young <jyoung8607@gmail.com>
2024-04-29 21:15:45 -0400

    CUPRA Ateca available from 2018

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -373,6 +373,7 @@ class CAR(Platforms):
   SEAT_ATECA_MK1 = VolkswagenMQBPlatformConfig(
     [
       VWCarDocs("SEAT Ateca 2016-2023"),
+      VWCarDocs("CUPRA Ateca 2018-2023"),
       VWCarDocs("SEAT Leon 2014-20"),
     ],
     VolkswagenCarSpecs(mass=1300, wheelbase=2.64),
