d15e2a0c68e23a6203bb2d64a30ec9962be8df19
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-05-06 16:34:34 -0700

    revert changes to release/

---
+++ release/verify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+CLEAR="\033[0m"
+
+BRANCHES="devel release3"
+for b in $BRANCHES; do
+  if git diff --quiet origin/$b origin/$b-staging && [ "$(git rev-parse origin/$b)" = "$(git rev-parse origin/$b-staging)" ]; then
+    printf "%-10s $GREEN ok $CLEAR\n" "$b"
+  else
+    printf "%-10s $RED mismatch $CLEAR\n" "$b"
+  fi
+done
