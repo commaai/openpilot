231408950701c62a172bed31a86cd700dd25cbd9
Shane Smiskol <shane@smiskol.com>
2024-05-08 15:40:00 -0700

    messyyy

--- tools/car_porting/auto_fingerprint.py
+++ tools/car_porting/auto_fingerprint.py
@@ -4,18 +4,8 @@ import argparse
 from collections import defaultdict
 from openpilot.selfdrive.debug.format_fingerprints import format_brand_fw_versions
 
-from openpilot.selfdrive.car.fw_versions import match_fw_to_car
-from openpilot.selfdrive.car.interfaces import get_interface_attr
+from openpilot.selfdrive.car.fw_versions import MODEL_TO_BRAND, match_fw_to_car
 from openpilot.tools.lib.logreader import LogReader, ReadMode
-
-
-ALL_FW_VERSIONS = get_interface_attr("FW_VERSIONS")
-ALL_CARS = get_interface_attr("CAR")
-
-PLATFORM_TO_PYTHON_CAR_NAME = {brand: {car.value: car.name for car in ALL_CARS[brand]} for brand in ALL_CARS}
-BRAND_TO_PLATFORMS = {brand: [car.value for car in ALL_CARS[brand]] for brand in ALL_CARS}
-PLATFORM_TO_BRAND = dict(sum([[(platform, brand) for platform in BRAND_TO_PLATFORMS[brand]] for brand in BRAND_TO_PLATFORMS], []))
-
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Auto fingerprint from a route")
@@ -24,10 +14,6 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   lr = LogReader(args.route, ReadMode.QLOG)
-
-  carFw = None
-  carVin = None
-  carPlatform = None
 
   platform: str | None = None
 
@@ -40,7 +26,7 @@ if __name__ == "__main__":
   carVin = CP.carVin
   carPlatform = CP.carFingerprint
 
-  if args.platform is None: # attempt to auto-determine platform with other fuzzy fingerprints
+  if args.platform is None:  # attempt to auto-determine platform with other fuzzy fingerprints
     _, possible_platforms = match_fw_to_car(carFw, carVin, log=False)
 
     if len(possible_platforms) != 1:
@@ -62,7 +48,7 @@ if __name__ == "__main__":
   print("Attempting to add fw version for: ", platform)
 
   fw_versions: dict[str, dict[tuple, list[bytes]]] = defaultdict(lambda: defaultdict(list))
-  brand = PLATFORM_TO_BRAND[platform]
+  brand = MODEL_TO_BRAND[platform]
 
   for fw in carFw:
     if fw.brand == brand and not fw.logging:
