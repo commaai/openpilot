3f9f33d25faf544fbed5f3fc40cbd601b6c1b2d6
Shane Smiskol <shane@smiskol.com>
2024-05-08 21:17:38 -0700

    format card

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -15,7 +15,6 @@ from openpilot.selfdrive.boardd.boardd import can_list_to_can_capnp
 from openpilot.selfdrive.car.car_helpers import get_car, get_one_can
 from openpilot.selfdrive.car.interfaces import CarInterfaceBase
 
-
 REPLAY = "REPLAY" in os.environ
 
 
@@ -28,7 +27,7 @@ class CarD:
     self.sm = messaging.SubMaster(['pandaStates'])
     self.pm = messaging.PubMaster(['sendcan', 'carState', 'carParams', 'carOutput'])
 
-    self.can_rcv_timeout_counter = 0      # conseuctive timeout count
+    self.can_rcv_timeout_counter = 0  # consecutive timeout count
     self.can_rcv_cum_timeout_counter = 0  # cumulative timeout count
 
     self.CC_prev = car.CarControl.new_message()
@@ -139,4 +138,3 @@ class CarD:
     self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=self.CS.canValid))
 
     self.CC_prev = CC
-
