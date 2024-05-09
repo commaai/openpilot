efd2f2d85b3d13f0120e054e6e5546f9fa24f105
Shane Smiskol <shane@smiskol.com>
2024-05-08 23:04:24 -0700

    Update selfdrive/controls/controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -440,7 +440,7 @@ class Controls:
 
     # All pandas not in silent mode must have controlsAllowed when openpilot is enabled
     if self.enabled and any(not ps.controlsAllowed for ps in self.sm['pandaStates']
-                            if ps.safetyModel not in IGNORED_SAFETY_MODES):
+           if ps.safetyModel not in IGNORED_SAFETY_MODES):
       self.mismatch_counter += 1
 
     return CS
