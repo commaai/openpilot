f798b3f0634d0560a16353e5f168cfe6a71432cd
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-05-13 14:58:36 -0700

    cleanup

--- tools/rerun/run.py
+++ tools/rerun/run.py
@@ -1,17 +1,12 @@
-from openpilot.tools.lib.logreader import LogReader
-from cereal.services import services
-from functools import partial
+#!/usr/bin/env python3
 import subprocess
 import sys
-import os
 import argparse
 import multiprocessing
-try:
-  import rerun as rr
-  import rerun.blueprint as rrb
-except Exception:
-  print("Rerun is not installed, try running --install")
-  sys.exit()
+from functools import partial
+
+from openpilot.tools.lib.logreader import LogReader
+from cereal.services import SERVICE_LIST
 
 
 NUM_CPUS = multiprocessing.cpu_count()
@@ -20,47 +15,40 @@ WHEEL_URL = "https://build.rerun.io/commit/660463d/wheels"
 
 
 def install():
+  # currently requires a preview release build
   subprocess.run([sys.executable, "-m", "pip", "install", "--pre", "-f", WHEEL_URL, "--upgrade", "rerun-sdk"], check=True)
-  print("Rerun installed, restarting script")
-  os.execv(sys.executable, [sys.executable] + sys.argv)
-  # subprocess.run([sys.executable, "-m", "pip", "install", "rerun-sdk"])
+  print("Rerun installed")
 
 
-topics = sorted(services.keys())
-is_first_run = True
-
-
-# Log dict message to rerun
 def log_msg(msg, parent_key=''):
-    stack = [(msg, parent_key)]
-    while stack:
-        current_msg, current_parent_key = stack.pop()
-        if isinstance(current_msg, list):
-            for index, item in enumerate(current_msg):
-                new_key = f"{current_parent_key}/{index}"
-                if isinstance(item, (int, float)):
-                    rr.log(str(new_key), rr.Scalar(item))
-                elif isinstance(item, dict):
-                    stack.append((item, new_key))
-        elif isinstance(current_msg, dict):
-            for key, value in current_msg.items():
-                new_key = f"{current_parent_key}/{key}"
-                if isinstance(value, (int, float)):
-                    rr.log(str(new_key), rr.Scalar(value))
-                elif isinstance(value, dict):
-                    stack.append((value, new_key))
-                elif isinstance(value, list):
-                    for index, item in enumerate(value):
-                        if isinstance(item, (int, float)):
-                            rr.log(f"{new_key}/{index}", rr.Scalar(item))
-        else:
-            pass  # Not a plottable value
+  stack = [(msg, parent_key)]
+  while stack:
+    current_msg, current_parent_key = stack.pop()
+    if isinstance(current_msg, list):
+      for index, item in enumerate(current_msg):
+        new_key = f"{current_parent_key}/{index}"
+        if isinstance(item, (int, float)):
+          rr.log(str(new_key), rr.Scalar(item))
+        elif isinstance(item, dict):
+          stack.append((item, new_key))
+    elif isinstance(current_msg, dict):
+      for key, value in current_msg.items():
+        new_key = f"{current_parent_key}/{key}"
+        if isinstance(value, (int, float)):
+          rr.log(str(new_key), rr.Scalar(value))
+        elif isinstance(value, dict):
+          stack.append((value, new_key))
+        elif isinstance(value, list):
+          for index, item in enumerate(value):
+            if isinstance(item, (int, float)):
+              rr.log(f"{new_key}/{index}", rr.Scalar(item))
+    else:
+      pass  # Not a plottable value
 
 
-# Create a blueprint for selected topics
 def createBlueprint():
   timeSeriesViews = []
-  for topic in topics:
+  for topic in sorted(SERVICE_LIST.keys()):
     timeSeriesViews.append(rrb.TimeSeriesView(name=topic, origin=f"/{topic}/", visible=False))
     rr.log(topic, rr.SeriesLine(name=topic), timeless=True)
     blueprint = rrb.Blueprint(rrb.Grid(rrb.Vertical(*timeSeriesViews,rrb.SelectionPanel(expanded=False),rrb.TimePanel(expanded=False)),
@@ -68,7 +56,6 @@ def createBlueprint():
   return blueprint
 
 
-# Log thumbnail, could be used for potential logging of any image
 def log_thumbnail(thumbnailMsg):
   bytesImgData = thumbnailMsg.get('thumbnail')
   rr.log("/thumbnail", rr.ImageEncoded(contents=bytesImgData))
@@ -91,15 +78,23 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="A helper to run rerun on openpilot routes",
                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--demo", action="store_true", help="Use the demo route instead of providing one")
-  parser.add_argument("--install", action="store_true", help="Install or update PlotJuggler + plugins")
+  parser.add_argument("--install", action="store_true", help="Install or update rerun")
   parser.add_argument("route_or_segment_name", nargs='?', help="The route or segment name to plot")
 
   if len(sys.argv) == 1:
     parser.print_help()
     sys.exit()
+
   args = parser.parse_args()
   if args.install:
     install()
+    sys.exit()
+
+  try:
+    import rerun as rr
+    import rerun.blueprint as rrb
+  except ImportError:
+    print("Rerun is not installed, run with --install first")
     sys.exit()
 
   route_or_segment_name = DEMO_ROUTE if args.demo else args.route_or_segment_name.strip()
