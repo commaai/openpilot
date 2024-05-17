f384a0bb702835846d8809c090d1302188e05f7e
purepani <pani0028@umn.edu>
2024-05-16 22:58:17 -0500

    Adds cache for nixos, nix-community, and cuda-maintainers

--- flake.nix
+++ flake.nix
@@ -35,5 +35,18 @@
     # e.g., 'packages/hello/default.nix' becomes '.#packages.hello'
     packages.${system}.default = packages;
     devShells.${system}.default = packages.devShell;
+
+    nixConfig = {
+    	extra-substituters = [
+		"https://cache.nixos.org/"
+		"https://nix-community.cachix.org"
+		"https://cuda-maintainers.org"
+	];
+	extra-trusted-public-keys = [
+		"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+		"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+		"cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+	];
+    };
   };
 }
