638a3819e9594a40fd0e6a8af9309d0ca513b0ab
Shane Smiskol <shane@smiskol.com>
2024-05-17 00:34:00 -0700

    stash

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -481,6 +481,7 @@ CONFIGS = [
     should_recv_callback=controlsd_rcv_callback,
     tolerance=NUMPY_TOLERANCE,
     processing_time=0.004,
+    # main_pub="carState",
   ),
   ProcessConfig(
     proc_name="card",
