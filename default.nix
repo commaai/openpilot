3bc0c5b859c949a361fc3913060fe9220c8d502c
purepani <pani0028@umn.edu>
2024-05-16 21:54:40 -0500

    Adds Nix project flake

---
+++ default.nix
@@ -0,0 +1,85 @@
+{
+  config,
+  dream2nix,
+  lib,
+  ...
+}:
+let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+in {
+  imports = [
+    dream2nix.modules.dream2nix.WIP-python-pdm
+  ];
+  inherit (pyproject.project) name version;
+  # select python 3.11
+  deps = {nixpkgs, ...}: {
+    python = lib.mkForce nixpkgs.python311;
+    python3 = lib.mkForce nixpkgs.python311;
+    qt = nixpkgs.qt6;
+    nodejs = nixpkgs.nodejs;
+  };
+  overrides = {
+  	pyaudio = {config, lib, ...}: {
+		deps = {nixpkgs, ...}: {
+			inherit (nixpkgs) portaudio;
+		};
+		mkDerivation = {
+			buildInputs = [
+				config.deps.portaudio
+			];
+		};
+	};
+	timezonefinder = {config, lib, ...}: {
+		mkDerivation.propagatedBuildInputs = [
+			config.deps.python.pkgs.poetry-core
+		];
+	};
+	casadi = {
+	    env.autoPatchelfIgnoreMissingDeps = [
+		"libhsl.so"
+		"libworhp.so"
+		"libsnopt7.so"
+		"libknitro.so"
+	    ];
+
+	};
+	onnxruntime-gpu = {config, lib, ...}: {
+		deps = {nixpkgs, ...}: {
+			inherit (nixpkgs.cudaPackages) cudatoolkit cuda_cccl libcublas libcurand libcusparse libcufft cudnn cuda_cudart;
+			inherit (nixpkgs) oneDNN re2 onnxruntime;
+		};
+		mkDerivation = {
+			src = config.deps.onnxruntime.dist;
+			 unpackPhase = ''
+			 	cp -r $src dist
+				chmod +w dist
+			'';
+			 buildInputs= with config.deps; [
+			 	onnxruntime.protobuf
+			 	re2
+				oneDNN
+				cudatoolkit
+				libcublas
+				libcurand
+				libcusparse
+				libcufft
+				cudnn
+				cuda_cudart
+			];
+		};
+
+	};
+  };
+  pdm.lockfile = ./pdm.lock;
+  pdm.pyproject = ./pyproject.toml;
+  mkDerivation = {
+    src = ./.;
+    propagatedBuildInputs = [
+      config.deps.python.pkgs.poetry-core
+
+    ];
+  };
+
+
+
+}
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "My flake with dream2nix packages";
+
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    #dream2nix.url = "path:/home/satwik/projects/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";#.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs @ {
+    self,
+    dream2nix,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    packages = dream2nix.lib.evalModules {
+      packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
+      modules = [
+        ./default.nix
+        {
+          paths.projectRoot = ./.;
+          paths.projectRootFile = "flake.nix";
+          paths.package = ./.;
+        }
+      ];
+    };
+  in {
+    # All packages defined in ./packages/<name> are automatically added to the flake outputs
+    # e.g., 'packages/hello/default.nix' becomes '.#packages.hello'
+    packages.${system}.default = packages;
+    devShells.${system}.default = packages.devShell;
+  };
+}
