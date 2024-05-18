40f711172d1058454644da5d923c11fc098482ba
cl0cks4fe <cl0cks4fe@gmail.com>
2024-05-18 10:25:19 -0700

    don't need to parameterize

--- tools/car_porting/test_car_model.py
+++ tools/car_porting/test_car_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 import os
-import pytest
-from parameterized import parameterized_class
 from typing import Any
 
 from openpilot.selfdrive.car.tests.routes import CarTestRoute
@@ -17,7 +15,6 @@ route_or_segment_name = SegmentName(route, allow_route_name=True)
 segment_num = route_or_segment_name.segment_num if route_or_segment_name.segment_num != -1 else None
 test_route = CarTestRoute(route_or_segment_name.route_name.canonical_name, car, segment=segment_num)
 
-@parameterized_class(('platform', 'test_route'), [(test_route.car_model, test_route)])
-@pytest.mark.xdist_group_class_property('test_route')
-class TestCarModel(TestCarModelBase):
-  pass
+class TestCarModelCarPorting(TestCarModelBase):
+  platform = test_route.car_model
+  test_route = test_route
