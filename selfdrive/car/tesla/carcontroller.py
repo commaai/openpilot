290825dae4d2e679cc06db0ebba45c0549e28359
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-05-18 17:44:30 +0200

    Cancel command can be sent directly, this will remove the neutral warning completely.

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -55,7 +55,7 @@ class CarController(CarControllerBase):
       pcm_cancel_cmd = True
 
     # Sent cancel request only if ACC is enabled
-    if self.frame % 10 == 0 and pcm_cancel_cmd and CS.acc_enabled:
+    if pcm_cancel_cmd and CS.acc_enabled:
       counter = int(CS.sccm_right_stalk_counter)
       can_sends.append(self.tesla_can.right_stalk_press((counter + 1) % 16 , 1))  # half up (cancel acc)
       can_sends.append(self.tesla_can.right_stalk_press((counter + 2) % 16, 0))  # to prevent neutral gear warning
