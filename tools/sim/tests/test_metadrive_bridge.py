18bb9637527476fe298bcdf3b0add5b6596a87ff
bongbui321 <hoang.bui.vh@gmail.com>
2024-05-18 09:19:48 -0400

    comment

--- tools/sim/tests/test_metadrive_bridge.py
+++ tools/sim/tests/test_metadrive_bridge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pytest
 import warnings
+# Since metadrive depends on pkg_resources, and pkg_resources is deprecated as an API
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 from openpilot.tools.sim.bridge.metadrive.metadrive_bridge import MetaDriveBridge
