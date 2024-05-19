905a7dabe379af43290146894378b47060cf9978
Shane Smiskol <shane@smiskol.com>
2024-05-19 00:55:42 -0700

    add back checks

--- selfdrive/test/process_replay/test_processes.py
+++ selfdrive/test/process_replay/test_processes.py
@@ -105,17 +105,15 @@ def test_process(cfg, lr, segment, ref_log_path, new_log_path, ignore_fields=Non
     raise Exception("failed on segment: " + segment) from e
 
   # check to make sure openpilot is engaged in the route
-  # if cfg.proc_name == "controlsd":
-  #   if not check_openpilot_enabled(log_msgs):
-  #     # FIXME: these segments should work, but the replay enabling logic is too brittle
-  #     if segment not in ("regen6CA24BC3035|2023-10-30--23-14-28--0", "regen7D2D3F82D5B|2023-10-30--23-15-55--0"):
-  #       return f"Route did not enable at all or for long enough: {new_log_path}", log_msgs
-  #
-  # if cfg.proc_name != 'ubloxd' or segment != 'regenBAE0915FE22|2024-05-18--04-33-11--0':
-  #   seen_msgs = {m.which() for m in log_msgs}
-  #   expected_msgs = set(cfg.subs)
-  #   if seen_msgs != expected_msgs:
-  #     return f"Expected messages: {expected_msgs}, but got: {seen_msgs}", log_msgs
+  if cfg.proc_name == "controlsd":
+    if not check_openpilot_enabled(log_msgs):
+      return f"Route did not enable at all or for long enough: {new_log_path}", log_msgs
+
+  if cfg.proc_name != 'ubloxd' or segment != 'regenBAE0915FE22|2024-05-18--04-33-11--0':
+    seen_msgs = {m.which() for m in log_msgs}
+    expected_msgs = set(cfg.subs)
+    if seen_msgs != expected_msgs:
+      return f"Expected messages: {expected_msgs}, but got: {seen_msgs}", log_msgs
 
   try:
     return compare_logs(ref_log_msgs, log_msgs, ignore_fields + cfg.ignore, ignore_msgs, cfg.tolerance), log_msgs
