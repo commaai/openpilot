d323233dee611a9b7c20a69d335d01e37353f120
Kacper Rączy <gfw.kra@gmail.com>
2024-05-23 15:18:48 -0700

    Use get(k, 0)

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -824,7 +824,7 @@ def check_most_messages_valid(msgs: LogIterable, threshold: float = 0.9) -> bool
 
   most_valid_for_service = {}
   for msg_type in msgs_counts.keys():
-    valid_share = msgs_valid_counts[msg_type] / msgs_counts[msg_type]
+    valid_share = msgs_valid_counts.get(msg_type, 0) / msgs_counts[msg_type]
     ok = valid_share >= threshold
     if not ok:
       print(f"WARNING: Service {msg_type} has {valid_share * 100:.2f}% valid messages, which is below threshold of {threshold * 100:.2f}%")
