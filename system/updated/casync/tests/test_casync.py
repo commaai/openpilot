e30663b2ab3d34c74c8029eb7b981bad066d2a81
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-05-25 12:01:31 -0700

    skip that

--- system/updated/casync/tests/test_casync.py
+++ system/updated/casync/tests/test_casync.py
@@ -14,6 +14,7 @@ from openpilot.system.updated.casync import tar
 LOOPBACK = os.environ.get('LOOPBACK', None)
 
 
+@pytest.mark.skip("not used yet")
 class TestCasync:
   @classmethod
   def setup_class(cls):
