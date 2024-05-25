f90655a1ff4a20e680d6f406b347462ca9cfd41a
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2024-05-24 21:38:23 -0700

    `ubuntu_setup`: fix `No module apt_pkg` error when setting up (#32526)
    * no apt_pkg fix
    
    * check arch
    
    * fix if
    
    * cleanup
    
    * reorder
    
    * increase shm size for selfdrive tests
    
    * add comment explaining reinstall
    
    * refine
    old-commit-hash: f4322666c6adc4dd2da569f483f6e108f18fe32a

--- tools/install_python_dependencies.sh
+++ tools/install_python_dependencies.sh
@@ -15,7 +15,10 @@ export MAKEFLAGS="-j$(nproc)"
 
 
 echo "update pip"
-if [ ! -z "\$VIRTUAL_ENV_ROOT" ] || [ ! -z "$INSTALL_DEADSNAKES_PPA" ] ; then
+if [ ! -z "$VIRTUAL_ENV_ROOT" ] || [ ! -z "$INSTALL_DEADSNAKES_PPA" ] ; then
+  if [ -z "$VIRTUAL_ENV_ROOT" ]; then
+    export VIRTUAL_ENV_ROOT="venv"
+  fi
   python3 -m venv --system-site-packages $VIRTUAL_ENV_ROOT
   source $VIRTUAL_ENV_ROOT/bin/activate
 fi
