fe7d3429abd85c120f0205093149eb9eabc81cb1
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-05-25 18:47:16 -0700

    Make release files a blacklist (#32540)
    * blacklist
    
    * little nicer
    
    * whitelist
    
    * cleanup
    
    * igore body
    
    * more skip

--- release/build_release.sh
+++ release/build_release.sh
@@ -9,13 +9,6 @@ cd $DIR
 BUILD_DIR=/data/openpilot
 SOURCE_DIR="$(git rev-parse --show-toplevel)"
 
-if [ -f /TICI ]; then
-  FILES_SRC="release/files_tici"
-else
-  echo "no release files set"
-  exit 1
-fi
-
 if [ -z "$RELEASE_BRANCH" ]; then
   echo "RELEASE_BRANCH is not set"
   exit 1
@@ -36,8 +29,7 @@ git checkout --orphan $RELEASE_BRANCH
 # do the files copy
 echo "[-] copying files T=$SECONDS"
 cd $SOURCE_DIR
-cp -pR --parents $(cat release/files_common) $BUILD_DIR/
-cp -pR --parents $(cat $FILES_SRC) $BUILD_DIR/
+cp -pR --parents $(./release/release_files.py) $TARGET_DIR/
 
 # in the directory
 cd $BUILD_DIR
@@ -76,6 +68,10 @@ find . -name 'moc_*' -delete
 find . -name '__pycache__' -delete
 rm -rf .sconsign.dblite Jenkinsfile release/
 rm selfdrive/modeld/models/supercombo.onnx
+
+find third_party/ -name '*x86*' -exec rm -r {} +
+find third_party/ -name '*Darwin*' -exec rm -r {} +
+
 
 # Restore third_party
 git checkout third_party/
