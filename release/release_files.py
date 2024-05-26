c94f90d29c08fcc3985a1ac7a289ac5027a261e5
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-05-25 17:20:20 -0700

    whitelist

--- release/release_files.py
+++ release/release_files.py
@@ -13,8 +13,12 @@ ROOT = HERE + "/.."
 blacklist = [
   "body/STL",
   "tools/cabana/",
+  "panda/examples/",
   "opendbc/generator",
   "third_party/acados/acados_template/gnsf",
+
+  "tools",
+  "tinygrad_repo",
 
   ".git$",  # for submodules
   ".git/",
@@ -24,15 +28,40 @@ blacklist = [
   ".vscode",
 ]
 
+
+# gets you through the blacklist
+whitelist = [
+  "tools/lib/",
+
+  "tinygrad_repo/openpilot/compile2.py",
+  "tinygrad_repo/extra/onnx.py",
+  "tinygrad_repo/extra/onnx_ops.py",
+  "tinygrad_repo/extra/thneed.py",
+  "tinygrad_repo/extra/utils.py",
+  "tinygrad_repo/tinygrad/codegen/kernel.py",
+  "tinygrad_repo/tinygrad/codegen/linearizer.py",
+  "tinygrad_repo/tinygrad/features/image.py",
+  "tinygrad_repo/tinygrad/features/search.py",
+  "tinygrad_repo/tinygrad/nn/*",
+  "tinygrad_repo/tinygrad/renderer/cstyle.py",
+  "tinygrad_repo/tinygrad/renderer/opencl.py",
+  "tinygrad_repo/tinygrad/runtime/lib.py",
+  "tinygrad_repo/tinygrad/runtime/ops_cpu.py",
+  "tinygrad_repo/tinygrad/runtime/ops_disk.py",
+  "tinygrad_repo/tinygrad/runtime/ops_gpu.py",
+  "tinygrad_repo/tinygrad/shape/*",
+  "tinygrad_repo/tinygrad/*.py",
+]
+
 if __name__ == "__main__":
   for f in Path(ROOT).glob("**/*"):
     if not (f.is_file() or f.is_symlink()):
       continue
-    #if any(x in str(f).split('/') for x in blacklisted_dirs):
-    #  continue
 
     rf = str(f.relative_to(ROOT))
-    if any(re.search(pattern, rf) for pattern in blacklist):
+    blacklisted = any(re.search(p, rf) for p in blacklist)
+    whitelisted = any(re.search(p, rf) for p in whitelist)
+    if blacklisted and not whitelisted:
       continue
 
     assert " " not in rf, rf
