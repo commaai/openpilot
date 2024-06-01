f90a46bde55544f354ff8413fd26ec182c1c0a1c
BBBmau <alvarez.mauriciotm@gmail.com>
2024-06-01 15:29:19 -0700

    add rm create_test_translations

--- selfdrive/ui/tests/create_test_translations.sh
+++ selfdrive/ui/tests/create_test_translations.sh
@@ -12,6 +12,7 @@ UNFINISHED="<translation type=\"unfinished\"><\/translation>"
 TRANSLATED="<translation>$TEST_TEXT<\/translation>"
 
 mkdir -p $UI_DIR/translations
+rm -f $TEST_TS_FILE $TEST_QM_FILE
 lupdate -recursive "$UI_DIR" -ts $TEST_TS_FILE
 sed -i "s/$UNFINISHED/$TRANSLATED/" $TEST_TS_FILE
 lrelease $TEST_TS_FILE
