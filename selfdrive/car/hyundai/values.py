28eb64e1fd3763cdbfdb3a5301c19d2a73c1ee8e
Riley Williams <williams.riley@icloud.com>
2024-06-05 17:34:02 -0400

    2024 IONIQ 6 (#32302)
    * Tweak carspecs for 2023 (and now also 24) ioniq 6 to reflect public info instead of cloning ioniq 5 specs
    
    * Regenerated car docs
    
    * revert specs *for now*
    
    ---------
    
    Co-authored-by: Riley Williams <riley@rileyww.dev>
    Co-authored-by: Riley Williams <riley@rileyw.dev>
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    old-commit-hash: 7381872a544f48daa3b1f26cf88a61714083801b

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -314,7 +314,7 @@ class CAR(Platforms):
     flags=HyundaiFlags.EV,
   )
   HYUNDAI_IONIQ_6 = HyundaiCanFDPlatformConfig(
-    [HyundaiCarDocs("Hyundai Ioniq 6 (with HDA II) 2023", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_p]))],
+    [HyundaiCarDocs("Hyundai Ioniq 6 (with HDA II) 2023-24", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_p]))],
     HYUNDAI_IONIQ_5.specs,
     flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE,
   )
