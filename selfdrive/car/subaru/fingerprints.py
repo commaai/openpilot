a418c9e40a273dc83efefb46a2ef5a034d9c92d3
Solomon Jones <31631573+eszjay@users.noreply.github.com>
2024-06-05 23:18:29 -0500

    Fingerprint for 2017 Subaru Forester (#32584)
    Fingerprint for 2017 Forester

--- selfdrive/car/subaru/fingerprints.py
+++ selfdrive/car/subaru/fingerprints.py
@@ -311,6 +311,7 @@ FW_VERSIONS = {
       b'\x00\x00d\xd3\x1f@ \t',
     ],
     (Ecu.engine, 0x7e0, None): [
+      b'\xa7"@0\x07',
       b'\xa7"@p\x07',
       b'\xa7)\xa0q\x07',
       b'\xba"@@\x07',
@@ -319,6 +320,7 @@ FW_VERSIONS = {
     (Ecu.transmission, 0x7e1, None): [
       b'\x1a\xf6F`\x00',
       b'\xda\xf2`\x80\x00',
+      b'\xda\xf2`p\x00',
       b'\xda\xfd\xe0\x80\x00',
       b'\xdc\xf2@`\x00',
       b'\xdc\xf2``\x00',
