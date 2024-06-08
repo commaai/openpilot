a96c86d6f5e8ec78d4968eeda48d92ca34d406c3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-06-08 15:20:30 -0700

    Update test_setproctitle.py

--- common/tests/test_setproctitle.py
+++ common/tests/test_setproctitle.py
@@ -1,11 +1,8 @@
 from openpilot.common.setproctitle import setproctitle, getproctitle, LINUX
+
 class TestProcTitle:
   def test_set_get_proctitle(self):
     if LINUX:
       name = 'TESTING'
       setproctitle(name)
       assert name == getproctitle()
-
-if __name__ == '__main__':
-  t = TestProcTitle()
-  t.test_set_get_proctitle()
