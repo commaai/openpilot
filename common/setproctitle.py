82d0ce7a4a5ea2702ec22f548af0e51ddbdb06d3
schlimeszn <138847413+schlimeszn@users.noreply.github.com>
2024-06-08 16:50:31 -0400

    fix lint

--- common/setproctitle.py
+++ common/setproctitle.py
@@ -8,6 +8,6 @@ def setproctitle(name: str):
   libc.prctl(PRCTL_CALL, str.encode(name), 0, 0, 0)
 
 def getproctitle():
-  with open('/proc/self/comm', 'r') as f:
+  with open('/proc/self/comm') as f:
     process_name = f.read().strip()
   return process_name
