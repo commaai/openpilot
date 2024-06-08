7b1cea7616a6a4b787bb7db0ff3aea8b30f7659f
schlimeszn <138847413+schlimeszn@users.noreply.github.com>
2024-06-08 17:09:55 -0400

    test only Linux

--- common/tests/test_setproctitle.py
+++ common/tests/test_setproctitle.py
@@ -1,9 +1,10 @@
-from openpilot.common.setproctitle import setproctitle, getproctitle
+from openpilot.common.setproctitle import setproctitle, getproctitle, LINUX
 class TestProcTitle:
   def test_set_get_proctitle(self):
-    name = 'TESTING'
-    setproctitle(name)
-    assert name == getproctitle()
+    if LINUX:
+      name = 'TESTING'
+      setproctitle(name)
+      assert name == getproctitle()
 
 if __name__ == '__main__':
   t = TestProcTitle()
