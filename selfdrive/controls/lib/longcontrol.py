c75a4cb8c7836959d0007cbfcfe2f8632fa496de
Bruce Wayne <harald.the.engineer@gmail.com>
2024-06-11 12:11:32 -0700

    Seperate

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -10,18 +10,13 @@ CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 LongCtrlState = car.CarControl.Actuators.LongControlState
 
 
-def long_control_state_trans(CP, active, long_control_state, v_ego, v_target,
-                             v_target_1sec, brake_pressed, cruise_standstill):
-  accelerating = v_target_1sec > v_target
-  planned_stop = (v_target < CP.vEgoStopping and
-                  v_target_1sec < CP.vEgoStopping and
-                  not accelerating)
+def long_control_state_trans(CP, active, long_control_state, v_ego,
+                             want_to_stop, brake_pressed, cruise_standstill):
   stay_stopped = (v_ego < CP.vEgoStopping and
                   (brake_pressed or cruise_standstill))
-  stopping_condition = planned_stop or stay_stopped
+  stopping_condition = want_to_stop or stay_stopped
 
-  starting_condition = (v_target_1sec > CP.vEgoStarting and
-                        accelerating and
+  starting_condition = (not want_to_stop and
                         not cruise_standstill and
                         not brake_pressed)
   started_condition = v_ego > CP.vEgoStarting
@@ -49,46 +44,51 @@ def long_control_state_trans(CP, active, long_control_state, v_ego, v_target,
 
   return long_control_state
 
-
-class LongControl:
-  def __init__(self, CP):
-    self.CP = CP
-    self.long_control_state = LongCtrlState.off  # initialized to off
-    self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
-                             (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
-                             k_f=CP.longitudinalTuning.kf, rate=1 / DT_CTRL)
-    self.v_pid = 0.0
-    self.last_output_accel = 0.0
-
-  def reset(self, v_pid):
-    """Reset PID controller and change setpoint"""
-    self.pid.reset()
-    self.v_pid = v_pid
-
-  def update(self, active, CS, long_plan, accel_limits, t_since_plan):
-    """Update longitudinal control. This updates the state machine and runs a PID loop"""
-    # Interp control trajectory
+def get_accel_from_plan(CP, long_plan, t_since_plan):
+   # Interp control trajectory
     speeds = long_plan.speeds
     if len(speeds) == CONTROL_N:
       v_target_now = interp(t_since_plan, CONTROL_N_T_IDX, speeds)
       a_target_now = interp(t_since_plan, CONTROL_N_T_IDX, long_plan.accels)
 
-      v_target = interp(self.CP.longitudinalActuatorDelay + t_since_plan, CONTROL_N_T_IDX, speeds)
-      a_target = 2 * (v_target - v_target_now) / self.CP.longitudinalActuatorDelay - a_target_now
+      v_target = interp(CP.longitudinalActuatorDelay + t_since_plan, CONTROL_N_T_IDX, speeds)
+      a_target = 2 * (v_target - v_target_now) / CP.longitudinalActuatorDelay - a_target_now
 
-      v_target_1sec = interp(self.CP.longitudinalActuatorDelay + t_since_plan + 1.0, CONTROL_N_T_IDX, speeds)
+      v_target_1sec = interp(CP.longitudinalActuatorDelay + t_since_plan + 1.0, CONTROL_N_T_IDX, speeds)
     else:
       v_target = 0.0
       v_target_now = 0.0
       v_target_1sec = 0.0
       a_target = 0.0
+    accelerating = v_target_1sec > v_target
+    want_to_stop = (v_target < CP.vEgoStopping and
+                    v_target_1sec < CP.vEgoStopping and
+                    not accelerating)
+    return a_target, want_to_stop
+
+
+class LongControl:
+  def __init__(self, CP):
+    self.CP = CP
+    self.long_control_state = LongCtrlState.off
+    self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
+                             (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
+                             k_f=CP.longitudinalTuning.kf, rate=1 / DT_CTRL)
+    self.last_output_accel = 0.0
+
+  def reset(self, v_pid):
+    self.pid.reset()
+
+  def update(self, active, CS, long_plan, accel_limits, t_since_plan):
+    """Update longitudinal control. This updates the state machine and runs a PID loop"""
+
 
     self.pid.neg_limit = accel_limits[0]
     self.pid.pos_limit = accel_limits[1]
+    a_target, want_to_stop = get_accel_from_plan(self.CP, long_plan, t_since_plan)
 
-    output_accel = self.last_output_accel
     self.long_control_state = long_control_state_trans(self.CP, active, self.long_control_state, CS.vEgo,
-                                                       v_target, v_target_1sec, CS.brakePressed,
+                                                       want_to_stop, CS.brakePressed,
                                                        CS.cruiseState.standstill)
 
     if self.long_control_state == LongCtrlState.off:
@@ -106,9 +106,7 @@ class LongControl:
       self.reset(CS.vEgo)
 
     elif self.long_control_state == LongCtrlState.pid:
-      self.v_pid = v_target_now
-
-      error = self.v_pid - CS.vEgo
+      error = a_target - CS.aEgo
       output_accel = self.pid.update(error, speed=CS.vEgo,
                                      feedforward=a_target)
 
