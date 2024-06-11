d47592e6acac5167df1569579a27be7c0f7d3e11
schlimeszn <138847413+schlimeszn@users.noreply.github.com>
2024-06-10 20:49:26 -0400

    delete proctitles

--- common/tests/test_setproctitle.py
+++
@@ -1,8 +0,0 @@
-from openpilot.common.setproctitle import setproctitle, getproctitle, LINUX
-
-class TestProcTitle:
-  def test_set_get_proctitle(self):
-    if LINUX:
-      name = 'TESTING'
-      setproctitle(name)
-      assert name == getproctitle()
