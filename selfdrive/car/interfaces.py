8ada7d16464ac8e6348006a0300a5f26ca2dd47e
Bruce Wayne <harald.the.engineer@gmail.com>
2024-06-11 20:57:23 -0700

    0 is better default

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -204,9 +204,9 @@ class CarInterfaceBase(ABC):
     ret.stoppingControl = True
     ret.longitudinalTuning.kf = 1.
     ret.longitudinalTuning.kpBP = [0.]
-    ret.longitudinalTuning.kpV = [1.]
+    ret.longitudinalTuning.kpV = [0.]
     ret.longitudinalTuning.kiBP = [0.]
-    ret.longitudinalTuning.kiV = [1.]
+    ret.longitudinalTuning.kiV = [0.]
     # TODO estimate car specific lag, use .15s for now
     ret.longitudinalActuatorDelay = 0.15
     ret.steerLimitTimer = 1.0
