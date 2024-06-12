3ed7bcab63e9e4fcb82b3f13dec0bb65bb693b0f
Agniv Sarkar <agni.spare@gmail.com>
2024-06-12 06:55:42 +0000

    Fixing tests.

--- selfdrive/debug/dump_car_docs.py
+++ selfdrive/debug/dump_car_docs.py
@@ -3,7 +3,9 @@ import argparse
 import os
 import requests
 
-def dump_car_docs(url, path):
+LINK_TO_MAIN_CARS = "https://raw.githubusercontent.com/commaai/openpilot/master/docs/CARS.md"
+
+def dump_car_docs(path, url=LINK_TO_MAIN_CARS):
   response = requests.get(url)
   MASTER_CARS_MD = os.path.join(path, "CARS.md")
   with open(MASTER_CARS_MD, 'wb') as file:
@@ -12,6 +14,6 @@ def dump_car_docs(url, path):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--path", required=True)
-  parser.add_argument("--url", required=False, default="https://raw.githubusercontent.com/commaai/openpilot/master/docs/CARS.md")
+  parser.add_argument("--url", required=False, default=LINK_TO_MAIN_CARS)
   args = parser.parse_args()
-  dump_car_docs(args.url, args.path)
+  dump_car_docs(args.path, args.url)
