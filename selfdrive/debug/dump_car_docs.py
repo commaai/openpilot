eaeef6317ebde6df77572ec72d70472bbdfc57dd
Agniv Sarkar <agni.spare@gmail.com>
2024-06-12 06:30:05 +0000

    Whoops! Fixed how it would get the data.

--- selfdrive/debug/dump_car_docs.py
+++ selfdrive/debug/dump_car_docs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 import argparse
-import pickle
+import os
+import requests
 
-from openpilot.selfdrive.car.docs import get_all_car_docs
-
-
-def dump_car_docs(path):
-  with open(path, 'wb') as f:
-    pickle.dump(get_all_car_docs(), f)
-  print(f'Dumping car info to {path}')
-
+def dump_car_docs(url, path):
+  response = requests.get(url)
+  MASTER_CARS_MD = os.path.join(path, "CARS.md")
+  with open(MASTER_CARS_MD, 'wb') as file:
+    file.write(response.content)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--path", required=True)
+  parser.add_argument("--url", required=False, default="https://raw.githubusercontent.com/commaai/openpilot/master/docs/CARS.md")
   args = parser.parse_args()
-  dump_car_docs(args.path)
+  dump_car_docs(args.url, args.path)
