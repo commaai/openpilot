cd8d440c86f868d9e32c9aea9e287a648e1ded2e
Shane Smiskol <shane@smiskol.com>
2024-06-13 22:43:24 -0700

    LogReader: support reading zst logs from Azure (#32745)
    * staging api
    
    * other stuff
    
    * ugh this should really not be duplicated, we already check the max segnum in Route class
    
    * better error message!!
    
    * more better
    
    * clean up
    
    * clean up
    
    * breaks again
    old-commit-hash: a0bbc005b62f1e6183ce4fd167e78b4fedb06df0

--- tools/lib/route.py
+++ tools/lib/route.py
@@ -9,7 +9,7 @@ from openpilot.tools.lib.auth_config import get_token
 from openpilot.tools.lib.api import CommaApi
 from openpilot.tools.lib.helpers import RE
 
-QLOG_FILENAMES = ['qlog', 'qlog.bz2']
+QLOG_FILENAMES = ['qlog', 'qlog.bz2', 'qlog.zst']
 QCAMERA_FILENAMES = ['qcamera.ts']
 LOG_FILENAMES = ['rlog', 'rlog.bz2', 'raw_log.bz2']
 CAMERA_FILENAMES = ['fcamera.hevc', 'video.hevc']
