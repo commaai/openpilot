34d91b22bf9c37d970f8198809c08516415d5fc1
Shane Smiskol <shane@smiskol.com>
2024-06-13 23:56:44 -0700

    clean up

--- tools/lib/api.py
+++ tools/lib/api.py
@@ -1,6 +1,6 @@
 import os
 import requests
-API_HOST = os.getenv('API_HOST', 'https://api.aks.comma.ai')
+API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
 
 class CommaApi:
   def __init__(self, token=None):
