0489b844021e09f5661e80ea85ea2ab0b9c65350
bongbui321 <hoang.bui.vh@gmail.com>
2024-06-22 19:40:22 -0400

    multiprocessing

--- tools/rerun/run.py
+++ tools/rerun/run.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 import multiprocessing
+import tqdm
 import rerun as rr
 import rerun.blueprint as rrb
 from functools import partial
@@ -18,42 +19,7 @@ from cereal.services import SERVICE_LIST
 
 NUM_CPUS = multiprocessing.cpu_count()
 DEMO_ROUTE = "a2a0ccea32023010|2023-07-27--13-01-19"
-
-"""
-TODO:
-  [x] design
-  [x] one probe
-  [x] hevc vids
-  [x] better naming
-  [x] size hint
-  [] multiprocessing qcameras speed for speed
-  [] type hint
-"""
-
-def log_msg(msg, parent_key=''):
-  stack = [(msg, parent_key)]
-  while stack:
-    current_msg, current_parent_key = stack.pop()
-    if isinstance(current_msg, list):
-      for index, item in enumerate(current_msg):
-        new_key = f"{current_parent_key}/{index}"
-        if isinstance(item, (int, float)):
-          rr.log(str(new_key), rr.Scalar(item))
-        elif isinstance(item, dict):
-          stack.append((item, new_key))
-    elif isinstance(current_msg, dict):
-      for key, value in current_msg.items():
-        new_key = f"{current_parent_key}/{key}"
-        if isinstance(value, (int, float)):
-          rr.log(str(new_key), rr.Scalar(value))
-        elif isinstance(value, dict):
-          stack.append((value, new_key))
-        elif isinstance(value, list):
-          for index, item in enumerate(value):
-            if isinstance(item, (int, float)):
-              rr.log(f"{new_key}/{index}", rr.Scalar(item))
-    else:
-      pass  # Not a plottable value
+RR_TIMELINE_NAME = "Timeline"
 
 
 class FrameType(IntEnum):
@@ -78,7 +44,6 @@ class FrameReaderWithTimestamps:
 
     self.ts = self._get_ts()
 
-
   def _get_ts(self):
     args = ["ffprobe", "-show_packets", "-probesize", "10M", self.camera_path]
     dat = subprocess.check_output(args)
@@ -91,7 +56,6 @@ class FrameReaderWithTimestamps:
       ret = [float(d.split('=')[1])*(i+1)+(self.segment*60) for i, d in enumerate(ret)]
     return ret
 
-
   def read_h264_stream(self):
     frame_sz = self.w * self.h * 3 // 2
 
@@ -103,7 +67,6 @@ class FrameReaderWithTimestamps:
       if len(dat) == 0:
         break
       yield dat
-
 
   def __iter__(self):
     for i, frame in enumerate(self.__frame_iter):
@@ -123,16 +86,23 @@ class CameraReader:
 
     self.__frs = {}
 
-
   def _get_fr(self, i):
     if i not in self.__frs:
       self.__frs[i] = FrameReaderWithTimestamps(self.camera_paths[i], segment=i, h=self.h, w=self.w, frame_type=self.frame_type)
     return self.__frs[i]
 
-
   def __iter__(self):
     for i in range(len(self.camera_paths)):
       yield from self._get_fr(i)
+
+  def _run_on_segment(self, func, i):
+    return func(self._get_fr(i))
+
+  def run_across_segments(self, num_processes, func):
+    with multiprocessing.Pool(num_processes) as pool:
+      num_segs = len(self.camera_paths)
+      for _ in tqdm.tqdm(pool.imap_unordered(partial(self._run_on_segment, func), range(num_segs)), total=num_segs):
+        pass
 
 
 CameraConfig = namedtuple("CameraConfig", ["qcam", "fcam", "ecam", "dcam"])
@@ -145,29 +115,53 @@ class CameraType(StrEnum):
   dcam = "dcamera"
 
 
+def log_msg(msg, parent_key=''):
+  stack = [(msg, parent_key)]
+  while stack:
+    current_msg, current_parent_key = stack.pop()
+    if isinstance(current_msg, list):
+      for index, item in enumerate(current_msg):
+        new_key = f"{current_parent_key}/{index}"
+        if isinstance(item, (int, float)):
+          rr.log(new_key, rr.Scalar(item))
+        elif isinstance(item, dict):
+          stack.append((item, new_key))
+    elif isinstance(current_msg, dict):
+      for key, value in current_msg.items():
+        new_key = f"{current_parent_key}/{key}"
+        if isinstance(value, (int, float)):
+          rr.log(new_key, rr.Scalar(value))
+        elif isinstance(value, dict):
+          stack.append((value, new_key))
+        elif isinstance(value, list):
+          for index, item in enumerate(value):
+            if isinstance(item, (int, float)):
+              rr.log(f"{new_key}/{index}", rr.Scalar(item))
+    else:
+      pass  # Not a plottable value
+
+
 class Rerunner:
   def __init__(self, route_or_segment_name, camera_config, enabled_services):
     self.enabled_services = [s.lower() for s in enabled_services]
     self.lr = LogReader(route_or_segment_name)
-    self.r = Route(route_or_segment_name)
+    r = Route(route_or_segment_name)
 
     self.qcam, self.fcam, self.ecam, self.dcam = camera_config
 
     self.camera_readers = {}
     if self.qcam:
-      self.camera_readers[CameraType.qcam] = CameraReader(self.r.qcamera_paths(), FrameType.h264_stream)
+      self.camera_readers[CameraType.qcam] = CameraReader(r.qcamera_paths(), FrameType.h264_stream)
     if self.fcam:
-      self.camera_readers[CameraType.fcam] = CameraReader(self.r.camera_paths())
+      self.camera_readers[CameraType.fcam] = CameraReader(r.camera_paths())
     if self.ecam:
-      self.camera_readers[CameraType.ecam] = CameraReader(self.r.ecamera_paths())
+      self.camera_readers[CameraType.ecam] = CameraReader(r.ecamera_paths())
     if self.dcam:
-      self.camera_readers[CameraType.dcam] = CameraReader(self.r.dcamera_paths())
-
+      self.camera_readers[CameraType.dcam] = CameraReader(r.dcamera_paths())
 
   def _start_rerun(self):
     self.blueprint = self._create_blueprint()
     rr.init("rerun_test", spawn=True)
-
 
   def _create_blueprint(self):
     blueprint = None
@@ -188,17 +182,14 @@ class Rerunner:
     )
     return blueprint
 
-
   @staticmethod
   @rr.shutdown_at_exit
   def _process_log_msgs(blueprint, enabled_services, lr):
     rr.init("rerun_test")
     rr.connect(default_blueprint=blueprint)
 
-    ret = []
     for msg in lr:
-      ret.append(msg)
-      rr.set_time_nanos("TIMELINE", msg.logMonoTime)
+      rr.set_time_nanos(RR_TIMELINE_NAME, msg.logMonoTime)
       msg_type = msg.which()
 
       if msg_type.lower() not in enabled_services:
@@ -209,17 +200,25 @@ class Rerunner:
       else:
         rr.log("/thumbnail", rr.ImageEncoded(contents=msg.to_dict()[msg.which()].get("thumbnail")))
 
-    return ret
+    return []
 
+  @staticmethod
+  @rr.shutdown_at_exit
+  def _process_cam_readers(blueprint, cam_type, h, w, fr):
+    print('in here')
+    rr.init("rerun_test")
+    rr.connect(default_blueprint=blueprint)
+
+    size_hint = (h, w)
+    for ts, frame in fr:
+      rr.set_time_nanos(RR_TIMELINE_NAME, int(ts * 1e9))
+      rr.log(cam_type, rr.ImageEncoded(contents=frame,format=rr.ImageFormat.NV12(size_hint)))
 
   def load_data(self):
     self._start_rerun()
     self.lr.run_across_segments(NUM_CPUS, partial(self._process_log_msgs, self.blueprint, self.enabled_services))
     for cam_type, cr in self.camera_readers.items():
-      size_hint = (cr.h, cr.w)
-      for ts, frame in cr:
-        rr.set_time_nanos("TIMELINE", int(ts * 1e9))
-        rr.log(cam_type, rr.ImageEncoded(contents=frame,format=rr.ImageFormat.NV12(size_hint)))
+      cr.run_across_segments(NUM_CPUS, partial(self._process_cam_readers, self.blueprint, cam_type, cr.h, cr.w))
 
 
 if __name__ == '__main__':
