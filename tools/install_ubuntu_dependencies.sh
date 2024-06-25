9562caaa9571051e45d2551a6c69a4be2899b126
Maxime Desroches <desroches.maxime@gmail.com>
2024-06-24 17:35:20 -0700

    fix qml

--- tools/install_ubuntu_dependencies.sh
+++ tools/install_ubuntu_dependencies.sh
@@ -53,6 +53,7 @@ function install_ubuntu_common_requirements() {
     ocl-icd-libopencl1 \
     ocl-icd-opencl-dev \
     portaudio19-dev \
+    qml-module-qtquick2 \
     qtmultimedia5-dev \
     qtdeclarative5-dev \
     qttools5-dev-tools \
