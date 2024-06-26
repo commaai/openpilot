fd3e2f3c40f41f9c2c3e8b095333b44b51915df4
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2024-06-26 12:28:13 -0400

    VW PQ: bugfix values.py entry

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -295,7 +295,6 @@ class CAR(Platforms):
     wmis={WMI.VOLKSWAGEN_USA_CAR},
   )
   VOLKSWAGEN_JETTA_SPORTWAGEN = VolkswagenPQPlatformConfig(
-    "VOLKSWAGEN JETTA SPORTWAGEN",
     [VWCarDocs("Volkswagen Jetta Sportwagen TDI 2011-2014")],
     VolkswagenCarSpecs(mass=1518, wheelbase=2.62, minSteerSpeed=50 * CV.KPH_TO_MS, minEnableSpeed=20 * CV.KPH_TO_MS),
     chassis_codes={"AJ"},
