303657aab5258697274d06b2c54b682cb228abdc
Maxime Desroches <desroches.maxime@gmail.com>
2024-06-25 20:04:20 -0700

    copy poetry api

--- scripts/uvlock.sh
+++ scripts/uvlock.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 ROOT=$DIR/../
 cd $ROOT
 
-UPGRADE_FLAG=$([ -n "$UPGRADE" ] && echo "--upgrade" || : )
+UPGRADE_FLAG=$([ "$1" != "--no-update" ] && echo "--upgrade" || : )
 uv pip compile --preview --all-extras $UPGRADE_FLAG pyproject.toml -o requirements.txt
