102188c245a9112b4221547060224621d8a6127f
Kacper Rączy <gfw.kra@gmail.com>
2024-06-28 19:27:58 -0700

    drivingModelData: new model packet for qlogs (#32821)
    * Mini model v2 prototype
    
    * Apply suggestions
    
    * Add meta
    
    * Support for new packet in model
    
    * Support in process replay
    
    * Meta
    
    * Add it to services
    
    * Update model replay
    
    * Add drivingModelData in model_replay
    
    * Update ref
    
    * MAX_FRAMES*3
    
    * No modelv2, but drivingModelData at 2hz
    
    * Polypath
    
    * Dont use keyword
    
    * Update model ref commit
    
    * xyz coeff
    
    * Fix field name
    
    * Update ref commit
    
    * Min qlog size 0.6
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 2059f986dff46254419c889eadcc82ecea1bce94

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -149,7 +149,7 @@ def main(demo=False):
     cloudlog.warning(f"connected extra cam with buffer size: {vipc_client_extra.buffer_len} ({vipc_client_extra.width} x {vipc_client_extra.height})")
 
   # messaging
-  pm = PubMaster(["modelV2", "cameraOdometry"])
+  pm = PubMaster(["modelV2", "drivingModelData", "cameraOdometry"])
   sm = SubMaster(["deviceState", "carState", "roadCameraState", "liveCalibration", "driverMonitoringState", "carControl"])
 
   publish_state = PublishState()
@@ -259,9 +259,10 @@ def main(demo=False):
 
     if model_output is not None:
       modelv2_send = messaging.new_message('modelV2')
+      drivingdata_send = messaging.new_message('drivingModelData')
       posenet_send = messaging.new_message('cameraOdometry')
-      fill_model_msg(modelv2_send, model_output, publish_state, meta_main.frame_id, meta_extra.frame_id, frame_id, frame_drop_ratio,
-                      meta_main.timestamp_eof, model_execution_time, live_calib_seen)
+      fill_model_msg(drivingdata_send, modelv2_send, model_output, publish_state, meta_main.frame_id, meta_extra.frame_id, frame_id,
+                     frame_drop_ratio, meta_main.timestamp_eof, model_execution_time, live_calib_seen)
 
       desire_state = modelv2_send.modelV2.meta.desireState
       l_lane_change_prob = desire_state[log.Desire.laneChangeLeft]
@@ -270,9 +271,12 @@ def main(demo=False):
       DH.update(sm['carState'], sm['carControl'].latActive, lane_change_prob)
       modelv2_send.modelV2.meta.laneChangeState = DH.lane_change_state
       modelv2_send.modelV2.meta.laneChangeDirection = DH.lane_change_direction
+      drivingdata_send.drivingModelData.meta.laneChangeState = DH.lane_change_state
+      drivingdata_send.drivingModelData.meta.laneChangeDirection = DH.lane_change_direction
 
       fill_pose_msg(posenet_send, model_output, meta_main.frame_id, vipc_dropped_frames, meta_main.timestamp_eof, live_calib_seen)
       pm.send('modelV2', modelv2_send)
+      pm.send('drivingModelData', drivingdata_send)
       pm.send('cameraOdometry', posenet_send)
 
     last_vipc_frame_id = meta_main.frame_id
