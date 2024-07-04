fe10730841fa925db78fe62881ebc9af1e31b898
Jason Young <jyoung8607@gmail.com>
2024-07-03 23:33:31 -0400

    increase min torque for driver override

--- selfdrive/car/fca_giorgio/carstate.py
+++ selfdrive/car/fca_giorgio/carstate.py
@@ -33,7 +33,7 @@ class CarState(CarStateBase):
     ret.steeringRateDeg = pt_cp.vl["EPS_1"]["STEERING_RATE"]
     ret.steeringTorque = pt_cp.vl["EPS_2"]["DRIVER_TORQUE"]
     ret.steeringTorqueEps = pt_cp.vl["EPS_3"]["EPS_TORQUE"]
-    ret.steeringPressed = ret.steeringTorque > 50
+    ret.steeringPressed = ret.steeringTorque > 80
     ret.yawRate = pt_cp.vl["ABS_2"]["YAW_RATE"]
     ret.steerFaultPermanent = bool(pt_cp.vl["EPS_2"]["LKA_FAULT"])
 
