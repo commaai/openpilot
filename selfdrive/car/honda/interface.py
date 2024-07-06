9812344dfcb30c62b4e23d39de58a3f522e22b2a
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2024-07-06 06:20:11 -0700

    fix

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -63,7 +63,7 @@ class CarInterface(CarInterfaceBase):
     if candidate == CAR.HONDA_ACCORD and 0x191 in fingerprint[CAN.pt]:
       ret.transmissionType = TransmissionType.cvt
     # New Civics can have manual transmission
-    elif candidate == CAR.HONDA.HONDA_CIVIC_2022 and 0x191 not in fingerprint[CAN.pt]:
+    elif candidate == CAR.HONDA_CIVIC_2022 and 0x191 not in fingerprint[CAN.pt]:
       ret.transmissionType = TransmissionType.manual
 
     # Certain Hondas have an extra steering sensor at the bottom of the steering rack,
