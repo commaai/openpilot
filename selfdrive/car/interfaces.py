3448d5c9314a843715b122d9448bf37d38aa0f34
Jason Wen <haibin.wen3@gmail.com>
2024-07-07 16:08:02 -0400

    new tune to fight oscillation

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -218,7 +218,7 @@ class CarInterfaceBase(ABC):
 
     tune.init('torque')
     tune.torque.useSteeringAngle = use_steering_angle
-    tune.torque.kp = 1.0
+    tune.torque.kp = 0.5
     tune.torque.kf = 1.0
     tune.torque.ki = 0.1
     tune.torque.friction = params['FRICTION']
