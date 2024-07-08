dbf095522f12659129a4e058d8371731f097c8dd
Dean Lee <deanlee3@gmail.com>
2024-07-09 05:50:31 +0800

    cabana: bug fixes and improvements (#32934)
    bug fixes and improvements
    old-commit-hash: 36815cc6d536ab8926f6335574ea587947c4e222

--- tools/cabana/utils/export.cc
+++ tools/cabana/utils/export.cc
@@ -10,11 +10,10 @@ namespace utils {
 void exportToCSV(const QString &file_name, std::optional<MessageId> msg_id) {
   QFile file(file_name);
   if (file.open(QIODevice::ReadWrite | QIODevice::Truncate)) {
-    const uint64_t start_time = can->routeStartTime();
     QTextStream stream(&file);
     stream << "time,addr,bus,data\n";
     for (auto e : msg_id ? can->events(*msg_id) : can->allEvents()) {
-      stream << QString::number((e->mono_time / 1e9) - start_time, 'f', 2) << ","
+      stream << QString::number(can->toSeconds(e->mono_time), 'f', 3) << ","
              << "0x" << QString::number(e->address, 16) << "," << e->src << ","
              << "0x" << QByteArray::fromRawData((const char *)e->dat, e->size).toHex().toUpper() << "\n";
     }
@@ -30,9 +29,8 @@ void exportSignalsToCSV(const QString &file_name, const MessageId &msg_id) {
       stream << "," << s->name;
     stream << "\n";
 
-    const uint64_t start_time = can->routeStartTime();
     for (auto e : can->events(msg_id)) {
-      stream << QString::number((e->mono_time / 1e9) - start_time, 'f', 2) << ","
+      stream << QString::number(can->toSeconds(e->mono_time), 'f', 3) << ","
              << "0x" << QString::number(e->address, 16) << "," << e->src;
       for (auto s : msg->sigs) {
         double value = 0;
