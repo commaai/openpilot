6c948548b3f66f77cd5a391e92e349a4bd7c55ec
Andrew Tec <21319730+tecandrew@users.noreply.github.com>
2024-07-09 14:55:10 -0700

    new CI for NextJS docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,4 +3,7 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx'
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  output: 'standalone',
+  basePath: '/openpilot-docs'
+})
