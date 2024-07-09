9c7b75c2755f9d8d9b3ed2bea6a29c40601fe8fd
Andrew Tec <21319730+tecandrew@users.noreply.github.com>
2024-07-09 09:02:36 -0700

    comma.ai green. add favicon. add some meta info.

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -6,20 +6,32 @@ export default {
   // https://nextra.site/docs/docs-theme/theme-configuration#seo-options
   useNextSeoProps() {
     return {
-      titleTemplate: 'openpilot – make driving chill'
+      // title: 'openpilot',
+      titleTemplate: 'openpilot - %s',
+      defaultTitle: 'openpilot',
+      description: 'open source driver assistance system that runs on 275+ car models',
+      canonical: 'docs.comma.ai',
+      twitter: {
+        cardType: 'summary_large_image',
+        handle: '@comma_ai'
+      }
     }
   },
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="openpilot" />
-      <meta property="og:description" content="make driving chill." />
+      {/* favicon */}
+      <link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADzSURBVHgB7ZZbEcIwEEUvGAAcRAIS6gAJ1EklgAOKAkBBcVAcdFBQHJTNFGaSfLS7zCb85Mycrz528+hNgUxGxpqsyIYcPrbknjSIjCE7p3CovbZFRKaKf+1jNVEyirtLok4raMC6gzKD0CuUkTbQQxnpEgzcFy+Z990gx0ARG0AdZDNgoIyZaKLHj0uwgJwSY9isyAd5Jgvy4tzzIjdISAF/9A33Qe4mnCOMX/amjdWAehDN4W5O9vRrUcJf/6hHckiYDyckpob/Q7JGQg5BcYNE2FE2/ypewY/dIxJOew3/UyuggOQssFn/xBgyd2QySrwBMPGV3eNmgPcAAAAASUVORK5CYII=" sizes="32x32" />
+      {/* analytics */}
+      <script type="text/javascript" defer="" src="https://plausible.io/js/script.outbound-links.js" data-domain="docs.comma.ai"></script>
+      <script defer="" src="https://static.hotjar.com/c/hotjar-3254988.js?sv=6"></script>
     </>
   ),
   logo: (
     <>
       <img
-        src="https://github.com/commaai.png?size=48"
+        // src="https://github.com/commaai.png?size=48"
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADzSURBVHgB7ZZbEcIwEEUvGAAcRAIS6gAJ1EklgAOKAkBBcVAcdFBQHJTNFGaSfLS7zCb85Mycrz528+hNgUxGxpqsyIYcPrbknjSIjCE7p3CovbZFRKaKf+1jNVEyirtLok4raMC6gzKD0CuUkTbQQxnpEgzcFy+Z990gx0ARG0AdZDNgoIyZaKLHj0uwgJwSY9isyAd5Jgvy4tzzIjdISAF/9A33Qe4mnCOMX/amjdWAehDN4W5O9vRrUcJf/6hHckiYDyckpob/Q7JGQg5BcYNE2FE2/ypewY/dIxJOew3/UyuggOQssFn/xBgyd2QySrwBMPGV3eNmgPcAAAAASUVORK5CYII="
         alt="comma.ai logo"
         width="24"
         height="24"
@@ -32,6 +44,11 @@ export default {
   ),
   project: {
     link: gitRepo
+  },
+  primaryHue: 115,
+  primarySaturation: {
+    light: 81, // tad easier to read on a white background
+    dark: 87.2
   },
   docsRepositoryBase: gitRepo + '/blob/master/docs',
   // TODO add x-twitter
