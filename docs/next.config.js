fe1a7713ea401aab3dea17fbb73c924da6ada500
Andrew Tec <21319730+tecandrew@users.noreply.github.com>
2024-07-09 15:28:45 -0700

    conditionally set basePath

--- docs/next.config.js
+++ docs/next.config.js
@@ -5,5 +5,13 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra({
   output: 'standalone',
-  basePath: '/openpilot-docs'
+  images: {
+    unoptimized: true,
+  },
+  // Conditionally set basePath based on ENV
+  ...(process.env.GITHUB_REPOSITORY
+    ? {
+      basePath: `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+    }
+    : {}),
 })
