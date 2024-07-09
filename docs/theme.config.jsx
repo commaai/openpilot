0f49a9654000cc287d15dc3cc4b88fb46d4cd64c
Andrew Tec <21319730+tecandrew@users.noreply.github.com>
2024-07-09 09:21:08 -0700

    SEO updates: use open graph. update favicon to use logo from github master branch

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -14,14 +14,31 @@ export default {
       twitter: {
         cardType: 'summary_large_image',
         handle: '@comma_ai'
+      },
+      opengraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://docs.comma.ai',
+        title: 'openpilot',
+        description: 'open source driver assistance system that runs on 275+ car models',
+        images: [
+          {
+            url: 'https://github.com/commaai/openpilot/assets/8762862/f09e6d29-db2d-4179-80c2-51e8d92bdb5c',
+            width: 1200,
+            height: 630,
+            alt: 'openpilot github'
+          }
+        ]
       }
     }
   },
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {/* favicon */}
-      <link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADzSURBVHgB7ZZbEcIwEEUvGAAcRAIS6gAJ1EklgAOKAkBBcVAcdFBQHJTNFGaSfLS7zCb85Mycrz528+hNgUxGxpqsyIYcPrbknjSIjCE7p3CovbZFRKaKf+1jNVEyirtLok4raMC6gzKD0CuUkTbQQxnpEgzcFy+Z990gx0ARG0AdZDNgoIyZaKLHj0uwgJwSY9isyAd5Jgvy4tzzIjdISAF/9A33Qe4mnCOMX/amjdWAehDN4W5O9vRrUcJf/6hHckiYDyckpob/Q7JGQg5BcYNE2FE2/ypewY/dIxJOew3/UyuggOQssFn/xBgyd2QySrwBMPGV3eNmgPcAAAAASUVORK5CYII=" sizes="32x32" />
+      {/* favicon - github openpilot logo  - extract from git lfs storage */}
+      <link rel="icon" type="image/png" href="https://media.githubusercontent.com/media/commaai/openpilot/master/selfdrive/assets/offroad/icon_openpilot.png" />
+      {/* alt favicon - comma.ai logo */}
+      {/* <link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADzSURBVHgB7ZZbEcIwEEUvGAAcRAIS6gAJ1EklgAOKAkBBcVAcdFBQHJTNFGaSfLS7zCb85Mycrz528+hNgUxGxpqsyIYcPrbknjSIjCE7p3CovbZFRKaKf+1jNVEyirtLok4raMC6gzKD0CuUkTbQQxnpEgzcFy+Z990gx0ARG0AdZDNgoIyZaKLHj0uwgJwSY9isyAd5Jgvy4tzzIjdISAF/9A33Qe4mnCOMX/amjdWAehDN4W5O9vRrUcJf/6hHckiYDyckpob/Q7JGQg5BcYNE2FE2/ypewY/dIxJOew3/UyuggOQssFn/xBgyd2QySrwBMPGV3eNmgPcAAAAASUVORK5CYII=" sizes="32x32" /> */}
       {/* analytics */}
       <script type="text/javascript" defer="" src="https://plausible.io/js/script.outbound-links.js" data-domain="docs.comma.ai"></script>
       <script defer="" src="https://static.hotjar.com/c/hotjar-3254988.js?sv=6"></script>
