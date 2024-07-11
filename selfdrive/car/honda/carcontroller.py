08fd77c431efeedb375cfea82f27f9571a7004ad
Shane Smiskol <shane@smiskol.com>
2024-07-11 02:58:12 -0700

    todo

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -78,6 +78,7 @@ def brake_pump_hysteresis(apply_brake, apply_brake_last, last_pump_ts, ts):
 
 
 def round_hud_set_speed(v_cruise_kph, is_metric):
+  # TODO: understand this better
   if not is_metric:
     v_cruise_kph = v_cruise_kph / 1.00584
     v_cruise_kph = round(v_cruise_kph / IMPERIAL_INCREMENT) * IMPERIAL_INCREMENT * 1.001
