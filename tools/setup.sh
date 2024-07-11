61cb4143033d46f05288221448e286391b6a89d7
Andrei Radulescu <andi.radulescu@gmail.com>
2024-07-11 13:59:39 +0300

    --single-branch for faster pull and some final instructions

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ ! -f launch_openpilot.sh ]; then
   if [ ! -d openpilot ]; then
-    git clone --recurse-submodules https://github.com/commaai/openpilot.git
+    git clone --single-branch --recurse-submodules https://github.com/commaai/openpilot.git
     git lfs pull
   fi
   cd openpilot
@@ -20,3 +20,12 @@ source .venv/bin/activate
 
 echo "Building openpilot"
 scons -u -j$(nproc)
+
+echo
+echo "----   OPENPILOT BUILDING DONE   ----"
+echo "To push changes to your fork, run the following commands:"
+echo "git remote remove origin"
+echo "git remote add origin git@github.com:<YOUR_USERNAME>/openpilot.git"
+echo "git fetch"
+echo "git commit -m \"first commit\""
+echo "git push"
