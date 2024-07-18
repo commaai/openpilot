3c48a6154b95f0ae2ab84fd916e02b704c74b258
Shane Smiskol <shane@smiskol.com>
2024-07-17 20:42:12 -0700

    increase longitudinalPlan decimation (#33009)
    change longitudinalPlan decimation

--- cereal/services.py
+++ cereal/services.py
@@ -38,7 +38,7 @@ _services: dict[str, tuple] = {
   "carState": (True, 100., 10),
   "carControl": (True, 100., 10),
   "carOutput": (True, 100., 10),
-  "longitudinalPlan": (True, 20., 5),
+  "longitudinalPlan": (True, 20., 10),
   "procLog": (True, 0.5, 15),
   "gpsLocationExternal": (True, 10., 10),
   "gpsLocation": (True, 1., 1),
