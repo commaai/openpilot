2399cf8b8e3d1569c761e5fb263ed3e733bf6647
Greg Hogan <gregjhogan@gmail.com>
2024-07-20 00:11:42 -0700

    lint

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -1,7 +1,6 @@
 from collections import namedtuple
 
 from cereal import car
-from openpilot.common.conversions import Conversions as CV
 from openpilot.common.numpy_fast import clip, interp
 from opendbc.can.packer import CANPacker
 from openpilot.selfdrive.car import DT_CTRL, rate_limit, make_tester_present_msg
