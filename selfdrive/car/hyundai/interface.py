0efb5b3310792862da11fd3b65e4c0da36f7f8be
Jason Wen <haibin.wen3@gmail.com>
2024-07-23 11:11:45 -0400

    higher startAccel

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -91,7 +91,7 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startingState = True
     ret.vEgoStarting = 0.1
-    ret.startAccel = 1.0
+    ret.startAccel = 2.0
     ret.longitudinalActuatorDelay = 0.5
 
     # *** feature detection ***
