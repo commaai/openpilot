bdb54e7358e7bf1c4aba01ec76ee9ecb8d9107d5
Shane Smiskol <shane@smiskol.com>
2024-07-26 19:11:01 -0700

    not in save_log either

--- tools/lib/logreader.py
+++ tools/lib/logreader.py
@@ -30,10 +30,8 @@ RawLogIterable = Iterable[bytes]
 def save_log(dest, log_msgs, compress=True):
   dat = b"".join(msg.as_builder().to_bytes() for msg in log_msgs)
 
-  if compress and dest.endswith(".bz2"):
+  if compress:
     dat = bz2.compress(dat)
-  elif compress and dest.endswith(".zst"):
-    dat = zstd.compress(dat, 10)
 
   with open(dest, "wb") as f:
     f.write(dat)
