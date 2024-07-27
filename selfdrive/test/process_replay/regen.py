f67c3db880fbd67a8ed624cdfeeb42b73263f6e4
Shane Smiskol <shane@smiskol.com>
2024-07-26 18:52:02 -0700

    Revert "just let it auto resolve"
    This reverts commit f296d62424227ad05facc62abc18a6f81b474e84.

--- selfdrive/test/process_replay/regen.py
+++ selfdrive/test/process_replay/regen.py
@@ -74,7 +74,7 @@ def setup_data_readers(
         assert device_type != "neo", "Driver camera not supported on neo segments. Use dummy dcamera."
         frs['driverCameraState'] = FrameReader(r.dcamera_paths()[sidx])
   else:
-    lr = LogReader(f"{route}/{sidx}/r")
+    lr = LogReader(f"cd:/{route.replace('|', '/')}/{sidx}/rlog.bz2")
     frs = {}
     if needs_road_cam:
       frs['roadCameraState'] = FrameReader(f"cd:/{route.replace('|', '/')}/{sidx}/fcamera.hevc")
