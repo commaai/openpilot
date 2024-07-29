d94a243a5378d681aaf5716c89fa84d1e0b1e418
Ayushmaan06 <142436337+Ayushmaan06@users.noreply.github.com>
2024-07-29 23:09:05 +0530

    Update test_vehicle_model.py

--- selfdrive/controls/lib/tests/test_vehicle_model.py
+++ selfdrive/controls/lib/tests/test_vehicle_model.py
@@ -3,10 +3,45 @@ import math
 
 import numpy as np
 #from control import StateSpace
-from custom_state_space import StateSpace
+#from custom_state_space import StateSpace
 from openpilot.selfdrive.car.honda.interface import CarInterface
 from openpilot.selfdrive.car.honda.values import CAR
 from openpilot.selfdrive.controls.lib.vehicle_model import VehicleModel, dyn_ss_sol, create_dyn_state_matrices
+
+
+class StateSpace:
+    def __init__(self, A, B, C, D):
+        self.A = np.array(A)
+        self.B = np.array(B)
+        self.C = np.array(C)
+        self.D = np.array(D)
+
+    def sample(self, dt):
+        """
+        Discretize the continuous-time state-space system using zero-order hold.
+        """
+        n = self.A.shape[0]
+        Ad = np.eye(n) + dt * self.A  # Simple discretization method
+        Bd = dt * self.B
+        return StateSpace(Ad, Bd, self.C, self.D)
+
+    def update(self, x, u):
+        """
+        Update the state `x` with input `u`.
+        x: current state (numpy array)
+        u: input (numpy array)
+        returns: next state (numpy array)
+        """
+        return self.A @ x + self.B @ u
+
+    def output(self, x, u):
+        """
+        Calculate the output `y` from state `x` and input `u`.
+        x: current state (numpy array)
+        u: input (numpy array)
+        returns: output (numpy array)
+        """
+        return self.C @ x + self.D @ u
 
 
 class TestVehicleModel:
