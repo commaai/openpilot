4f9651885ffceb7cd02e3e27b911798daea05d74
Maxime Desroches <desroches.maxime@gmail.com>
2024-07-30 15:58:30 -0700

    improve

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
@@ -9,11 +11,46 @@ if [ -z "$OPENPILOT_ROOT" ]; then
   OPENPILOT_ROOT="$(pwd)/openpilot"
 fi
 
+function show_motd() {
+cat << 'EOF'
+
+                      .~ssos+.
+                    +8888888888i,
+                   {888888888888o.
+                   h8888888888888k
+                   t888888888s888k
+                    `t88888d/ h88k
+                       ```    h88l
+                             ,88k`
+                            .d8h`
+                           +d8h
+                        _+d8h`
+                      ;y8h+`
+                      |-`
+
+              openpilot installer
+
+EOF
+}
+
+function ask_dir() {
+  echo -n "Enter directory in which to install openpilot (default $OPENPILOT_ROOT): "
+  read
+  OPENPILOT_ROOT=$(realpath "${REPLY:-$OPENPILOT_ROOT}/openpilot")
+}
+
 function check_dir() {
   echo "Checking for installation directory..."
   if [ -d "$OPENPILOT_ROOT" ]; then
-    echo -e " ↳ [${RED}✗${NC}] can't install openpilot in $OPENPILOT_ROOT !"
-    return 1
+    echo -e " ↳ [${RED}✗${NC}] Installation destination $OPENPILOT_ROOT already exists !"
+    read -p "       Would you like to attempt installation anyway? [Y/n] " -n 1 -r
+    echo -e "\n"
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      return 1
+    else
+      RETRY_INSTALLATION=1
+      return 0
+    fi
   fi
 
   echo -e " ↳ [${GREEN}✔${NC}] Successfully chosen $OPENPILOT_ROOT as installation directory\n"
@@ -30,10 +67,12 @@ function check_git() {
 }
 
 function git_clone() {
+  st="$(date +%s)"
   echo "Cloning openpilot..."
   if $(git clone --filter=blob:none https://github.com/commaai/openpilot.git "$OPENPILOT_ROOT"); then
     if [[ -f $OPENPILOT_ROOT/launch_openpilot.sh ]]; then
-      echo -e " ↳ [${GREEN}✔${NC}] Successfully cloned openpilot.\n"
+      et="$(date +%s)"
+      echo -e " ↳ [${GREEN}✔${NC}] Successfully cloned openpilot in $((et - st)) seconds.\n"
       return 0
     fi
   fi
@@ -46,14 +85,11 @@ function install_with_op() {
   cd $OPENPILOT_ROOT
   $OPENPILOT_ROOT/tools/op.sh install
   $OPENPILOT_ROOT/tools/op.sh setup
-
-  # make op usable right now
-  alias op="source $OPENPILOT_ROOT/tools/op.sh \"\$@\""
 }
 
-check_dir && check_git && git_clone && install_with_op
-
-unset OPENPILOT_ROOT
-unset RED
-unset GREEN
-unset NC
+show_motd
+ask_dir
+check_dir
+check_git
+[ -z $RETRY_INSTALLATION ] && git_clone
+install_with_op
