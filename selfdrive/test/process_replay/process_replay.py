130d0d2e3a969044632f860e57869fcc0fa2f270
Kacper Rączy <gfw.kra@gmail.com>
2024-07-29 17:12:05 -0700

    process_replay: regenerate locationd logs (#33134)
    * Filter out non-relevant services in most_messages_valid check
    
    * Set as comprehension
    
    * Update segments
    
    * Update ref commit
    
    * New check for hyundai2
    old-commit-hash: 6e185f4eea470fe56fc6a33de9e81779f70941ab

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -819,11 +819,15 @@ def check_openpilot_enabled(msgs: LogIterable) -> bool:
 
 
 def check_most_messages_valid(msgs: LogIterable, threshold: float = 0.9) -> bool:
+  relevant_services = {sock for cfg in CONFIGS for sock in cfg.subs}
   msgs_counts = Counter(msg.which() for msg in msgs)
   msgs_valid_counts = Counter(msg.which() for msg in msgs if msg.valid)
 
   most_valid_for_service = {}
   for msg_type in msgs_counts.keys():
+    if msg_type not in relevant_services:
+      continue
+
     valid_share = msgs_valid_counts.get(msg_type, 0) / msgs_counts[msg_type]
     ok = valid_share >= threshold
     if not ok:
