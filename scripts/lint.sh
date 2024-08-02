4c2d812540dad0e3adc2faaa656122beb23fe84e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-08-01 22:08:24 -0700

    no source

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,8 +4,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR/../
 
-source .venv/bin/activate
-
 # TODO: bring back rest of pre-commit checks:
 # https://github.com/commaai/openpilot/blob/4b11c9e914707df9def598616995be2a5d355a6a/.pre-commit-config.yaml#L2
 
