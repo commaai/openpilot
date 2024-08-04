823e1462b5dd8afa0093025401beaf0f383af32f
Andrew Tec <21319730+tecandrew@users.noreply.github.com>
2024-08-04 15:11:23 -0700

    move and fix version. collapse sidebar items. Inter font everywhere.

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
+      sans: ['Inter', 'sans-serif'],
       mono: ['Roboto Mono', 'monospace', 'Courier New', 'sans-serif']
     },
     extend: {
