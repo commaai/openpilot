6a2a877c09c601f8f1bd7453e43d74b22e7ee8d4
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-08-05 10:44:28 -0700

    Cleanup

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -5,7 +5,6 @@
 #include <algorithm>
 #include <cmath>
 
-#include "common/params.h"
 #include "common/swaglog.h"
 #include "selfdrive/ui/qt/onroad/buttons.h"
 #include "selfdrive/ui/qt/util.h"
@@ -22,12 +21,6 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   main_layout->addWidget(experimental_btn, 0, Qt::AlignTop | Qt::AlignRight);
 
   dm_img = loadPixmap("../assets/img_driver_face.png", {img_size + 5, img_size + 5});
-}
-
-// Checks if "Always-On Driver Monitoring" toggle is enabled in settings
-bool AnnotatedCameraWidget::isDMAlwaysOn() const {
-  Params params;
-  return params.getBool("AlwaysOnDM");
 }
 
 void AnnotatedCameraWidget::updateState(const UIState &s) {
@@ -244,7 +237,7 @@ void AnnotatedCameraWidget::drawDriverState(QPainter &painter, const UIState *s)
   const float arc_t_default = 6.7;
   const float arc_t_extend = 12.0;
 
-  bool show_dm_engaged = s->engaged() || isDMAlwaysOn();
+  bool show_dm_engaged = s->engaged() || scene.always_on_dm;
 
   const QColor ENGAGED_COLOR = QColor::fromRgbF(0.1, 0.945, 0.26, 0.6);
   const QColor DISENGAGED_COLOR = QColor::fromRgbF(0.545, 0.545, 0.545, 0.6);
