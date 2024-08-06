f3518662fdad7b45ac74aee6b43a1e68c48d21b6
Shane Smiskol <shane@smiskol.com>
2024-08-05 19:37:13 -0700

    prepare for variable control loop timesteps

--- selfdrive/car/car.py
+++ selfdrive/car/car.py
@@ -2,21 +2,26 @@ from dataclasses import dataclass
 from collections import namedtuple
 
 # from selfdrive.car.interfaces import get_interface_attr
-from selfdrive.car.interfaces import CarInterfaceBase
+from selfdrive.car.interfaces import CarInterfaceBase, DT_CTRL
 from openpilot.selfdrive.car.car_helpers import get_car, get_one_can
 
 
 class Car:
   """All encompassing class for interfacing with a vehicle."""
 
-  def __init__(self, logcan, sendcan):
+  def __init__(self, logcan, sendcan, dt_ctrl=DT_CTRL):
     """
     Inputs:
     - logcan: function that returns a list of the latest CAN messages
     - sendcan: function that sends a CAN message
+    - dt_ctrl: control loop time step
     """
     self.logcan = logcan
     self.sendcan = sendcan
+    self.dt_ctrl = dt_ctrl
+
+    # TODO: 100 Hz is currently the only supported rate
+    assert self.dt_ctrl == DT_CTRL
 
     self.CI: CarInterfaceBase | None = None
 
