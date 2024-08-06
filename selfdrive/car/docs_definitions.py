991dd023a0b19377fbda73aea7883aa9724b2118
Agniv Sarkar <agni.spare@gmail.com>
2024-08-06 03:43:37 -1000

    Update docs_definitions.py

--- selfdrive/car/docs_definitions.py
+++ selfdrive/car/docs_definitions.py
@@ -218,6 +218,35 @@ def split_name(name: str) -> tuple[str, str, str]:
     model = model[:match.start() - 1]
   return make, model, years
 
+def get_detail_sentence(
+    make: str,
+    model: str,
+    longitudinal: str,
+    min_enable_speed: float,
+    min_steer_speed: float,
+    steering_torque: Star,
+    auto_resume: bool
+  ) -> str:
+  """
+  Returns the detail sentence of a car.
+  """
+  sentence_builder = "openpilot upgrades your <strong>{car_model}</strong> with automated lane centering{alc} and adaptive cruise control{acc}."
+  if min_steer_speed > min_enable_speed:
+    alc = f" <strong>above {min_steer_speed * CV.MS_TO_MPH:.0f} mph</strong>," if min_steer_speed > 0 else " <strong>at all speeds</strong>,"
+  else:
+    alc = ""
+  acc = ""
+  if min_enable_speed > 0:
+    acc = f" <strong>while driving above {min_enable_speed * CV.MS_TO_MPH:.0f} mph</strong>"
+  elif auto_resume:
+    acc = " <strong>that automatically resumes from a stop</strong>"
+  if steering_torque != Star.FULL:
+    sentence_builder += " This car may not be able to take tight turns on its own."
+  exp_link = "<a href='https://blog.comma.ai/090release/#experimental-mode' target='_blank' class='link-light-new-regular-text'>Experimental mode</a>"
+  if longitudinal == "openpilot":
+    sentence_builder += f" Traffic light and stop sign handling is also available in {exp_link}."
+  return sentence_builder.format(car_model=f"{make} {model}", alc=alc, acc=acc)
+
 
 @dataclass
 class CarDocs:
@@ -321,30 +350,7 @@ class CarDocs:
 
   def get_detail_sentence(self, CP):
     if not CP.notCar:
-      sentence_builder = "openpilot upgrades your <strong>{car_model}</strong> with automated lane centering{alc} and adaptive cruise control{acc}."
-
-      if self.min_steer_speed > self.min_enable_speed:
-        alc = f" <strong>above {self.min_steer_speed * CV.MS_TO_MPH:.0f} mph</strong>," if self.min_steer_speed > 0 else " <strong>at all speeds</strong>,"
-      else:
-        alc = ""
-
-      # Exception for cars which do not auto-resume yet
-      acc = ""
-      if self.min_enable_speed > 0:
-        acc = f" <strong>while driving above {self.min_enable_speed * CV.MS_TO_MPH:.0f} mph</strong>"
-      elif self.auto_resume:
-        acc = " <strong>that automatically resumes from a stop</strong>"
-
-      if self.row[Column.STEERING_TORQUE] != Star.FULL:
-        sentence_builder += " This car may not be able to take tight turns on its own."
-
-      # experimental mode
-      exp_link = "<a href='https://blog.comma.ai/090release/#experimental-mode' target='_blank' class='link-light-new-regular-text'>Experimental mode</a>"
-      if CP.openpilotLongitudinalControl and not CP.experimentalLongitudinalAvailable:
-        sentence_builder += f" Traffic light and stop sign handling is also available in {exp_link}."
-
-      return sentence_builder.format(car_model=f"{self.make} {self.model}", alc=alc, acc=acc)
-
+      return get_detail_sentence(self.make, self.model, CP.longitudinalControl, CP.minEnableSpeed, CP.minSteerSpeed, CP.steeringTorque, CP.autoResumeSng)
     else:
       if CP.carFingerprint == "COMMA_BODY":
         return "The body is a robotics dev kit that can run openpilot. <a href='https://www.commabody.com'>Learn more.</a>"
