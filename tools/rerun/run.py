044d09722bd4dbf7ce784052f076c1c24d5c6c00
bongbui321 <hoang.bui.vh@gmail.com>
2024-08-06 20:42:39 -0400

    fix that

--- tools/rerun/run.py
+++ tools/rerun/run.py
@@ -164,9 +164,9 @@ if __name__ == '__main__':
   sr = SegmentRange(route_or_segment_name)
   r = Route(sr.route_name)
 
-  hevc_requested = any([v for v in camera_config[1:]])
+  hevc_requested = any(camera_config[1:])
   if len(sr.seg_idxs) > 1 and hevc_requested:
-    print("You're requesting more than 1 segment with hevc files," + \
+    print("You're requesting more than 1 segment with hevc videos," + \
           "please be aware that might take a lot of memory" + \
           "since rerun isn't yet well supported for high resolution video logging")
     response = input("Do you wish to continue? (Y/n): ")
