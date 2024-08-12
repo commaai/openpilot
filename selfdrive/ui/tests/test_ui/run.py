988d16a4c8b12c9dcde245929a3c2dc5344370cd
Shane Smiskol <shane@smiskol.com>
2024-08-11 23:54:40 -0500

    remove map from ui preview (#33265)
    * rm map from ui preview
    
    * fix
    old-commit-hash: 4e122ed2d11785010377e023c979b258e622acb1

--- selfdrive/ui/tests/test_ui/run.py
+++ selfdrive/ui/tests/test_ui/run.py
@@ -13,7 +13,6 @@ from cereal import messaging, car, log
 from msgq.visionipc import VisionIpcServer, VisionStreamType
 
 from cereal.messaging import SubMaster, PubMaster
-from openpilot.common.mock import mock_messages
 from openpilot.common.params import Params
 from openpilot.common.realtime import DT_MDL
 from openpilot.common.transformations.camera import DEVICE_CAMERAS
@@ -92,16 +91,8 @@ def setup_onroad(click, pm: PubMaster):
     pm.send(msg.which(), msg)
     server.send(cam_meta.stream, IMG_BYTES, cs.frameId, cs.timestampSof, cs.timestampEof)
 
-@mock_messages(['liveLocationKalman'])
-def setup_onroad_map(click, pm: PubMaster):
-  setup_onroad(click, pm)
-
-  click(500, 500)
-
-  time.sleep(UI_DELAY) # give time for the map to render
-
 def setup_onroad_sidebar(click, pm: PubMaster):
-  setup_onroad_map(click, pm)
+  setup_onroad(click, pm)
   click(500, 500)
 
 CASES = {
@@ -109,7 +100,6 @@ CASES = {
   "settings_device": setup_settings_device,
   "settings_network": setup_settings_network,
   "onroad": setup_onroad,
-  "onroad_map": setup_onroad_map,
   "onroad_sidebar": setup_onroad_sidebar
 }
 
