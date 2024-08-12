85885f09a8722d9a9f0d0be3d4233abbc71c9a90
Shane Smiskol <shane@smiskol.com>
2024-08-11 22:24:07 -0700

    doesn't work

--- selfdrive/ui/tests/test_ui/run.py
+++ selfdrive/ui/tests/test_ui/run.py
@@ -16,6 +16,7 @@ from cereal.messaging import SubMaster, PubMaster
 from openpilot.common.params import Params
 from openpilot.common.realtime import DT_MDL
 from openpilot.common.spinner import Spinner
+from openpilot.common.text_window import TextWindow
 from openpilot.common.transformations.camera import DEVICE_CAMERAS
 from openpilot.selfdrive.test.helpers import with_processes
 from openpilot.selfdrive.test.process_replay.vision_meta import meta_from_camera_state
@@ -29,7 +30,24 @@ EventName = car.CarEvent.EventName
 EVENTS_BY_NAME = {v: k for k, v in EventName.schema.enumerants.items()}
 
 
-def setup_common(click, pm: PubMaster):
+def click(ui, x, y, *args, **kwargs):
+  import pyautogui
+  pyautogui.click(ui.left + x, ui.top + y, *args, **kwargs)
+  time.sleep(UI_DELAY)  # give enough time for the UI to react
+
+
+def get_ui(window_title="ui"):
+  try:
+    return pywinctl.getWindowsWithTitle(window_title)[0]
+  except Exception as e:
+    print(f"failed to find ui window, assuming that it's in the top left (for Xvfb) {e}")
+    return namedtuple("bb", ["left", "top", "width", "height"])(0, 0, 2160, 1080)
+
+
+def setup_common(window_title="ui"):
+  sm = SubMaster(["uiDebug"])
+  pm = PubMaster(["deviceState", "pandaStates", "controlsState", 'roadCameraState', 'wideRoadCameraState', 'liveLocationKalman'])
+
   Params().put("DongleId", "123456789012345")
   dat = messaging.new_message('deviceState')
   dat.deviceState.started = True
@@ -43,22 +61,31 @@ def setup_common(click, pm: PubMaster):
 
   pm.send("deviceState", dat)
 
-def setup_homescreen(click, pm: PubMaster):
-  setup_common(click, pm)
+  while not sm.valid["uiDebug"]:
+    sm.update(1)
+  time.sleep(UI_DELAY)  # wait a bit more for the UI to start rendering
+  ui = get_ui(window_title)
+  return ui, pm
 
-def setup_settings_device(click, pm: PubMaster):
-  setup_common(click, pm)
 
-  click(100, 100)
+def setup_homescreen():
+  return setup_common()
 
-def setup_settings_network(click, pm: PubMaster):
-  setup_common(click, pm)
+def setup_settings_device():
+  ui, pm = setup_common()
 
-  setup_settings_device(click, pm)
-  click(300, 600)
+  click(ui, 100, 100)
+  return ui, pm
 
-def setup_onroad(click, pm: PubMaster):
-  setup_common(click, pm)
+def setup_settings_network():
+  ui, pm = setup_common()
+
+  setup_settings_device()
+  click(ui, 300, 600)
+  return ui, pm
+
+def setup_onroad():
+  ui, pm = setup_common()
 
   dat = messaging.new_message('pandaStates', 1)
   dat.pandaStates[0].ignitionLine = True
@@ -91,26 +118,43 @@ def setup_onroad(click, pm: PubMaster):
 
     pm.send(msg.which(), msg)
     server.send(cam_meta.stream, IMG_BYTES, cs.frameId, cs.timestampSof, cs.timestampEof)
+  time.sleep(2)
+  return ui, pm
 
-def setup_onroad_sidebar(click, pm: PubMaster):
-  setup_onroad(click, pm)
-  click(500, 500)
+def setup_onroad_sidebar():
+  ui, pm = setup_onroad()
+  click(ui, 500, 500)
+  return ui, pm
 
-def setup_spinner(click, pm: PubMaster):
+def setup_spinner():
+  setup_common()
   s = Spinner()
   s.update_progress(30, 100)
-  return s,
+  time.sleep(UI_DELAY)
+  ui = get_ui("_spinner")
+  return ui, None, s
 
 def close_spinner(s):
   s.close()
 
+def setup_text():
+  setup_common()
+  t = TextWindow("This is a text window.")
+  time.sleep(UI_DELAY)
+  ui = get_ui("_text")
+  return ui, None, t
+
+def close_text(t):
+  t.close()
+
 CASES = {
-  "homescreen": (setup_homescreen, None, "ui"),
-  "settings_device": (setup_settings_device, None, "ui"),
-  "settings_network": (setup_settings_network, None, "ui"),
-  "onroad": (setup_onroad, None, "ui"),
-  "onroad_sidebar": (setup_onroad_sidebar, None, "ui"),
-  "spinner": (setup_spinner, close_spinner, "_spinner"),
+  # "homescreen": (setup_homescreen, None),
+  # "settings_device": (setup_settings_device, None),
+  # "settings_network": (setup_settings_network, None),
+  "onroad": (setup_onroad, None),
+  "onroad_sidebar": (setup_onroad_sidebar, None),
+  "spinner": (setup_spinner, close_spinner),
+  "text": (setup_text, close_text),
 }
 
 TEST_DIR = pathlib.Path(__file__).parent
@@ -124,41 +168,39 @@ class TestUI:
     os.environ["SCALE"] = "1"
     sys.modules["mouseinfo"] = False
 
-  def setup(self, window_title):
-    self.sm = SubMaster(["uiDebug"])
-    self.pm = PubMaster(["deviceState", "pandaStates", "controlsState", 'roadCameraState', 'wideRoadCameraState', 'liveLocationKalman'])
-    while not self.sm.valid["uiDebug"]:
-      self.sm.update(1)
-    time.sleep(UI_DELAY) # wait a bit more for the UI to start rendering
-    try:
-      self.ui = pywinctl.getWindowsWithTitle(window_title)[0]
-    except Exception as e:
-      print(f"failed to find ui window, assuming that it's in the top left (for Xvfb) {e}")
-      self.ui = namedtuple("bb", ["left", "top", "width", "height"])(0,0,2160,1080)
+  # def setup(self, window_title):
+  #   self.sm = SubMaster(["uiDebug"])
+  #   self.pm = PubMaster(["deviceState", "pandaStates", "controlsState", 'roadCameraState', 'wideRoadCameraState', 'liveLocationKalman'])
+  #   while not self.sm.valid["uiDebug"]:
+  #     self.sm.update(1)
+  #   time.sleep(UI_DELAY) # wait a bit more for the UI to start rendering
+  #   try:
+  #     self.ui = pywinctl.getWindowsWithTitle(window_title)[0]
+  #   except Exception as e:
+  #     print(f"failed to find ui window, assuming that it's in the top left (for Xvfb) {e}")
+  #     self.ui = namedtuple("bb", ["left", "top", "width", "height"])(0,0,2160,1080)
 
-  def screenshot(self):
+  def screenshot(self, ui):
     import pyautogui
-    im = pyautogui.screenshot(region=(self.ui.left, self.ui.top, self.ui.width, self.ui.height))
+    im = pyautogui.screenshot(region=(ui.left, ui.top, ui.width, ui.height))
     assert im.width == 2160
     assert im.height == 1080
     img = np.array(im)
     im.close()
     return img
 
-  def click(self, x, y, *args, **kwargs):
-    import pyautogui
-    pyautogui.click(self.ui.left + x, self.ui.top + y, *args, **kwargs)
-    time.sleep(UI_DELAY) # give enough time for the UI to react
+  # def click(self, ui, x, y, *args, **kwargs):
+  #   import pyautogui
+  #   pyautogui.click(ui.left + x, ui.top + y, *args, **kwargs)
+  #   time.sleep(UI_DELAY) # give enough time for the UI to react
 
   @with_processes(["ui"])
-  def test_ui(self, name, setup_case, cleanup_case, window_title):
-    self.setup(window_title)
-
-    args = setup_case(self.click, self.pm)
+  def test_ui(self, name, setup_case, cleanup_case):
+    ui, pm, *args = setup_case()
 
     time.sleep(UI_DELAY) # wait a bit more for the UI to finish rendering
 
-    im = self.screenshot()
+    im = self.screenshot(ui)
     plt.imsave(SCREENSHOTS_DIR / f"{name}.png", im)
 
     if cleanup_case is not None:
@@ -184,8 +226,8 @@ def create_screenshots():
   SCREENSHOTS_DIR.mkdir(parents=True)
 
   t = TestUI()
-  for name, (setup, cleanup, window_title) in CASES.items():
-    t.test_ui(name, setup, cleanup, window_title)
+  for name, (setup, cleanup) in CASES.items():
+    t.test_ui(name, setup, cleanup)
 
 if __name__ == "__main__":
   print("creating test screenshots")
