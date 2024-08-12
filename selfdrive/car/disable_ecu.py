aab2c890ef03425fb1584b382f08d919b22f8ffd
Shane Smiskol <shane@smiskol.com>
2024-08-12 15:39:20 -0700

    fix these

--- selfdrive/car/disable_ecu.py
+++ selfdrive/car/disable_ecu.py
@@ -40,10 +40,12 @@ def disable_ecu(can_recv, can_send, bus=0, addr=0x7d0, sub_addr=None, com_cont_r
 if __name__ == "__main__":
   import time
   import cereal.messaging as messaging
+  from openpilot.selfdrive.car.card import can_comm_callbacks
   sendcan = messaging.pub_sock('sendcan')
   logcan = messaging.sub_sock('can')
+  can_callbacks = can_comm_callbacks(logcan, sendcan)
   time.sleep(1)
 
   # honda bosch radar disable
-  disabled = disable_ecu(logcan, sendcan, bus=1, addr=0x18DAB0F1, com_cont_req=b'\x28\x83\x03', timeout=0.5, debug=False)
+  disabled = disable_ecu(*can_callbacks, bus=1, addr=0x18DAB0F1, com_cont_req=b'\x28\x83\x03', timeout=0.5, debug=False)
   print(f"disabled: {disabled}")
