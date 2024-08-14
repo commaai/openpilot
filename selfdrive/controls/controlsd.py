f43941160dbbf85c2cdbc6969fc3affc64a169bc
Kacper Rączy <gfw.kra@gmail.com>
2024-08-14 15:18:29 -0700

    Remove usage of llk in ui

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -382,6 +382,7 @@ class Controls:
 
     # TODO: fix simulator
     if not SIMULATION or REPLAY:
+      # Not show in first 1.5 km to allow for driving out of garage. This event shows after 5 minutes
       gps_ok = self.sm.recv_frame[self.gps_location_service] > 0 and (self.sm.frame - self.sm.recv_frame[self.gps_location_service]) * DT_CTRL < 2.0
       if not gps_ok and self.sm['livePose'].inputsOK and (self.distance_traveled > 1500):
         self.events.add(EventName.noGps)
