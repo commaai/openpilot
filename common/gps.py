ef18f7e23b18e31e7e6f2bf9330500f7168c8d2d
Kacper Rączy <gfw.kra@gmail.com>
2024-08-14 16:47:31 -0700

    Helper type annot

--- common/gps.py
+++ common/gps.py
@@ -1,7 +1,7 @@
 from openpilot.common.params import Params
 
 
-def get_gps_location_service(params: Params):
+def get_gps_location_service(params: Params) -> str:
   if params.get_bool("UbloxAvailable"):
     return "gpsLocationExternal"
   else:
