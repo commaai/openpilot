ba3792e048d80323a625bf51016c22ccdc856c3b
Shane Smiskol <shane@smiskol.com>
2024-08-13 21:22:06 -0700

    eh it's fine to name it whatever

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -29,46 +29,46 @@ class CarState(CarStateBase):
     self.prev_distance_button = 0
     self.distance_button = 0
 
-  def update(self, cp, cp_cam, _, __, loopback_cp):
+  def update(self, pt_cp, cam_cp, _, __, loopback_cp):
     ret = car.CarState.new_message()
 
     self.prev_cruise_buttons = self.cruise_buttons
     self.prev_distance_button = self.distance_button
-    self.cruise_buttons = cp.vl["ASCMSteeringButton"]["ACCButtons"]
-    self.distance_button = cp.vl["ASCMSteeringButton"]["DistanceButton"]
-    self.buttons_counter = cp.vl["ASCMSteeringButton"]["RollingCounter"]
-    self.pscm_status = copy.copy(cp.vl["PSCMStatus"])
+    self.cruise_buttons = pt_cp.vl["ASCMSteeringButton"]["ACCButtons"]
+    self.distance_button = pt_cp.vl["ASCMSteeringButton"]["DistanceButton"]
+    self.buttons_counter = pt_cp.vl["ASCMSteeringButton"]["RollingCounter"]
+    self.pscm_status = copy.copy(pt_cp.vl["PSCMStatus"])
     # This is to avoid a fault where you engage while still moving backwards after shifting to D.
     # An Equinox has been seen with an unsupported status (3), so only check if either wheel is in reverse (2)
-    self.moving_backward = (cp.vl["EBCMWheelSpdRear"]["RLWheelDir"] == 2) or (cp.vl["EBCMWheelSpdRear"]["RRWheelDir"] == 2)
+    self.moving_backward = (pt_cp.vl["EBCMWheelSpdRear"]["RLWheelDir"] == 2) or (pt_cp.vl["EBCMWheelSpdRear"]["RRWheelDir"] == 2)
 
     # Variables used for avoiding LKAS faults
     self.loopback_lka_steering_cmd_updated = len(loopback_cp.vl_all["ASCMLKASteeringCmd"]["RollingCounter"]) > 0
     if self.loopback_lka_steering_cmd_updated:
       self.loopback_lka_steering_cmd_ts_nanos = loopback_cp.ts_nanos["ASCMLKASteeringCmd"]["RollingCounter"]
     if self.CP.networkLocation == NetworkLocation.fwdCamera:
-      self.pt_lka_steering_cmd_counter = cp.vl["ASCMLKASteeringCmd"]["RollingCounter"]
-      self.cam_lka_steering_cmd_counter = cp_cam.vl["ASCMLKASteeringCmd"]["RollingCounter"]
+      self.pt_lka_steering_cmd_counter = pt_cp.vl["ASCMLKASteeringCmd"]["RollingCounter"]
+      self.cam_lka_steering_cmd_counter = cam_pt_cp.vl["ASCMLKASteeringCmd"]["RollingCounter"]
 
     ret.wheelSpeeds = self.get_wheel_speeds(
-      cp.vl["EBCMWheelSpdFront"]["FLWheelSpd"],
-      cp.vl["EBCMWheelSpdFront"]["FRWheelSpd"],
-      cp.vl["EBCMWheelSpdRear"]["RLWheelSpd"],
-      cp.vl["EBCMWheelSpdRear"]["RRWheelSpd"],
+      pt_cp.vl["EBCMWheelSpdFront"]["FLWheelSpd"],
+      pt_cp.vl["EBCMWheelSpdFront"]["FRWheelSpd"],
+      pt_cp.vl["EBCMWheelSpdRear"]["RLWheelSpd"],
+      pt_cp.vl["EBCMWheelSpdRear"]["RRWheelSpd"],
     )
     ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr])
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
     # sample rear wheel speeds, standstill=True if ECM allows engagement with brake
     ret.standstill = ret.wheelSpeeds.rl <= STANDSTILL_THRESHOLD and ret.wheelSpeeds.rr <= STANDSTILL_THRESHOLD
 
-    if cp.vl["ECMPRDNL2"]["ManualMode"] == 1:
+    if pt_cp.vl["ECMPRDNL2"]["ManualMode"] == 1:
       ret.gearShifter = self.parse_gear_shifter("T")
     else:
-      ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(cp.vl["ECMPRDNL2"]["PRNDL2"], None))
+      ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(pt_cp.vl["ECMPRDNL2"]["PRNDL2"], None))
 
-    ret.brake = cp.vl["ECMAcceleratorPos"]["BrakePedalPos"]
+    ret.brake = pt_cp.vl["ECMAcceleratorPos"]["BrakePedalPos"]
     if self.CP.networkLocation == NetworkLocation.fwdCamera:
-      ret.brakePressed = cp.vl["ECMEngineStatus"]["BrakePressed"] != 0
+      ret.brakePressed = pt_cp.vl["ECMEngineStatus"]["BrakePressed"] != 0
     else:
       # Some Volt 2016-17 have loose brake pedal push rod retainers which causes the ECM to believe
       # that the brake is being intermittently pressed without user interaction.
@@ -78,51 +78,51 @@ class CarState(CarStateBase):
 
     # Regen braking is braking
     if self.CP.transmissionType == TransmissionType.direct:
-      ret.regenBraking = cp.vl["EBCMRegenPaddle"]["RegenPaddle"] != 0
+      ret.regenBraking = pt_cp.vl["EBCMRegenPaddle"]["RegenPaddle"] != 0
 
-    ret.gas = cp.vl["AcceleratorPedal2"]["AcceleratorPedal2"] / 254.
+    ret.gas = pt_cp.vl["AcceleratorPedal2"]["AcceleratorPedal2"] / 254.
     ret.gasPressed = ret.gas > 1e-5
 
-    ret.steeringAngleDeg = cp.vl["PSCMSteeringAngle"]["SteeringWheelAngle"]
-    ret.steeringRateDeg = cp.vl["PSCMSteeringAngle"]["SteeringWheelRate"]
-    ret.steeringTorque = cp.vl["PSCMStatus"]["LKADriverAppldTrq"]
-    ret.steeringTorqueEps = cp.vl["PSCMStatus"]["LKATorqueDelivered"]
+    ret.steeringAngleDeg = pt_cp.vl["PSCMSteeringAngle"]["SteeringWheelAngle"]
+    ret.steeringRateDeg = pt_cp.vl["PSCMSteeringAngle"]["SteeringWheelRate"]
+    ret.steeringTorque = pt_cp.vl["PSCMStatus"]["LKADriverAppldTrq"]
+    ret.steeringTorqueEps = pt_cp.vl["PSCMStatus"]["LKATorqueDelivered"]
     ret.steeringPressed = abs(ret.steeringTorque) > STEER_THRESHOLD
 
     # 0 inactive, 1 active, 2 temporarily limited, 3 failed
-    self.lkas_status = cp.vl["PSCMStatus"]["LKATorqueDeliveredStatus"]
+    self.lkas_status = pt_cp.vl["PSCMStatus"]["LKATorqueDeliveredStatus"]
     ret.steerFaultTemporary = self.lkas_status == 2
     ret.steerFaultPermanent = self.lkas_status == 3
 
     # 1 - open, 0 - closed
-    ret.doorOpen = (cp.vl["BCMDoorBeltStatus"]["FrontLeftDoor"] == 1 or
-                    cp.vl["BCMDoorBeltStatus"]["FrontRightDoor"] == 1 or
-                    cp.vl["BCMDoorBeltStatus"]["RearLeftDoor"] == 1 or
-                    cp.vl["BCMDoorBeltStatus"]["RearRightDoor"] == 1)
+    ret.doorOpen = (pt_cp.vl["BCMDoorBeltStatus"]["FrontLeftDoor"] == 1 or
+                    pt_cp.vl["BCMDoorBeltStatus"]["FrontRightDoor"] == 1 or
+                    pt_cp.vl["BCMDoorBeltStatus"]["RearLeftDoor"] == 1 or
+                    pt_cp.vl["BCMDoorBeltStatus"]["RearRightDoor"] == 1)
 
     # 1 - latched
-    ret.seatbeltUnlatched = cp.vl["BCMDoorBeltStatus"]["LeftSeatBelt"] == 0
-    ret.leftBlinker = cp.vl["BCMTurnSignals"]["TurnSignals"] == 1
-    ret.rightBlinker = cp.vl["BCMTurnSignals"]["TurnSignals"] == 2
+    ret.seatbeltUnlatched = pt_cp.vl["BCMDoorBeltStatus"]["LeftSeatBelt"] == 0
+    ret.leftBlinker = pt_cp.vl["BCMTurnSignals"]["TurnSignals"] == 1
+    ret.rightBlinker = pt_cp.vl["BCMTurnSignals"]["TurnSignals"] == 2
 
-    ret.parkingBrake = cp.vl["BCMGeneralPlatformStatus"]["ParkBrakeSwActive"] == 1
-    ret.cruiseState.available = cp.vl["ECMEngineStatus"]["CruiseMainOn"] != 0
-    ret.espDisabled = cp.vl["ESPStatus"]["TractionControlOn"] != 1
-    ret.accFaulted = (cp.vl["AcceleratorPedal2"]["CruiseState"] == AccState.FAULTED or
-                      cp.vl["EBCMFrictionBrakeStatus"]["FrictionBrakeUnavailable"] == 1)
+    ret.parkingBrake = pt_cp.vl["BCMGeneralPlatformStatus"]["ParkBrakeSwActive"] == 1
+    ret.cruiseState.available = pt_cp.vl["ECMEngineStatus"]["CruiseMainOn"] != 0
+    ret.espDisabled = pt_cp.vl["ESPStatus"]["TractionControlOn"] != 1
+    ret.accFaulted = (pt_cp.vl["AcceleratorPedal2"]["CruiseState"] == AccState.FAULTED or
+                      pt_cp.vl["EBCMFrictionBrakeStatus"]["FrictionBrakeUnavailable"] == 1)
 
-    ret.cruiseState.enabled = cp.vl["AcceleratorPedal2"]["CruiseState"] != AccState.OFF
-    ret.cruiseState.standstill = cp.vl["AcceleratorPedal2"]["CruiseState"] == AccState.STANDSTILL
+    ret.cruiseState.enabled = pt_cp.vl["AcceleratorPedal2"]["CruiseState"] != AccState.OFF
+    ret.cruiseState.standstill = pt_cp.vl["AcceleratorPedal2"]["CruiseState"] == AccState.STANDSTILL
     if self.CP.networkLocation == NetworkLocation.fwdCamera:
-      ret.cruiseState.speed = cp_cam.vl["ASCMActiveCruiseControlStatus"]["ACCSpeedSetpoint"] * CV.KPH_TO_MS
-      ret.stockAeb = cp_cam.vl["AEBCmd"]["AEBCmdActive"] != 0
+      ret.cruiseState.speed = cam_cp.vl["ASCMActiveCruiseControlStatus"]["ACCSpeedSetpoint"] * CV.KPH_TO_MS
+      ret.stockAeb = cam_cp.vl["AEBCmd"]["AEBCmdActive"] != 0
       # openpilot controls nonAdaptive when not pcmCruise
       if self.CP.pcmCruise:
-        ret.cruiseState.nonAdaptive = cp_cam.vl["ASCMActiveCruiseControlStatus"]["ACCCruiseState"] not in (2, 3)
+        ret.cruiseState.nonAdaptive = cam_cp.vl["ASCMActiveCruiseControlStatus"]["ACCCruiseState"] not in (2, 3)
 
     if self.CP.enableBsm:
-      ret.leftBlindspot = cp.vl["BCMBlindSpotMonitor"]["LeftBSM"] == 1
-      ret.rightBlindspot = cp.vl["BCMBlindSpotMonitor"]["RightBSM"] == 1
+      ret.leftBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["LeftBSM"] == 1
+      ret.rightBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["RightBSM"] == 1
 
     return ret
 
