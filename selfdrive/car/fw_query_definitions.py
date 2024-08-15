2ed567b0f576179cd8d03727f46ee4e579c2c989
Shane Smiskol <shane@smiskol.com>
2024-08-14 19:24:24 -0700

    move live fingerprint debugging code to debug/car (#33308)
    * remove cereal from fw_versions
    
    * fix it
    
    * do ecu_addrs and vin
    
    * do last one
    
    * move executable-ness
    
    * notexe
    
    * nice test

--- selfdrive/car/fw_query_definitions.py
+++ selfdrive/car/fw_query_definitions.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import capnp
 import copy
 from dataclasses import dataclass, field
--- selfdrive/car/vin.py
+++ selfdrive/car/vin.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import re
 
 from panda.python.uds import get_rx_addr_for_tx_addr, FUNCTIONAL_ADDRS
@@ -57,25 +56,3 @@ def get_vin(can_recv, can_send, buses, timeout=0.1, retry=2, debug=False):
     carlog.error(f"vin query retry ({i+1}) ...")
 
   return -1, -1, VIN_UNKNOWN
-
-
-if __name__ == "__main__":
-  import argparse
-  import time
-  import cereal.messaging as messaging
-  from openpilot.selfdrive.car.card import can_comm_callbacks
-
-  parser = argparse.ArgumentParser(description='Get VIN of the car')
-  parser.add_argument('--debug', action='store_true')
-  parser.add_argument('--bus', type=int, default=1)
-  parser.add_argument('--timeout', type=float, default=0.1)
-  parser.add_argument('--retry', type=int, default=5)
-  args = parser.parse_args()
-
-  sendcan = messaging.pub_sock('sendcan')
-  logcan = messaging.sub_sock('can')
-  can_callbacks = can_comm_callbacks(logcan, sendcan)
-  time.sleep(1)
-
-  vin_rx_addr, vin_rx_bus, vin = get_vin(*can_callbacks, (args.bus,), args.timeout, args.retry, debug=args.debug)
-  print(f'RX: {hex(vin_rx_addr)}, BUS: {vin_rx_bus}, VIN: {vin}')
