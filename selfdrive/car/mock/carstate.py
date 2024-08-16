151d112b39ad6643cf3c1525cb0d13c5ed2b4a07
Shane Smiskol <shane@smiskol.com>
2024-08-16 15:58:07 -0700

    move most of /car

--- selfdrive/car/mock/carstate.py
+++
@@ -1,7 +0,0 @@
-from openpilot.selfdrive.car import structs
-from openpilot.selfdrive.car.interfaces import CarStateBase
-
-
-class CarState(CarStateBase):
-  def update(self, *_) -> structs.CarState:
-    return structs.CarState()
