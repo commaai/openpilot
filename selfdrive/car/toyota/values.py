5cea99cb2d76e09a34f121d43c075b86941536ee
Shane Smiskol <shane@smiskol.com>
2024-08-16 00:18:37 -0700

    toyota: pycapnp enum which should be called
    old-commit-hash: f6780fca2fdd4998ed45a6295d64399e9fd23d41

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -32,7 +32,7 @@ class CarControllerParams:
   ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[5, 25], angle_v=[0.36, 0.26])
 
   def __init__(self, CP):
-    if CP.lateralTuning.which == 'torque':
+    if CP.lateralTuning.which() == 'torque':
       self.STEER_DELTA_UP = 15       # 1.0s time to peak torque
       self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
     else:
