87cdc51b03555258bee80e4b00cb3a5802d0310e
Shane Smiskol <shane@smiskol.com>
2024-08-20 20:30:39 -0700

    fix

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -174,11 +174,20 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
     // The first half of track_vertices are the points for the right side of the path
     const auto &acceleration = sm["modelV2"].getModelV2().getAcceleration().getX();
     const int max_len = std::min<int>(scene.track_vertices.length() / 2, acceleration.size());
+    qDebug() << "track vs accel:" << scene.track_vertices.length() / 2 << acceleration.size();
+
+    qDebug() << "\n";
+    qDebug() << "---max_len:" << max_len;
 
     for (int i = 0; i < max_len; ++i) {
+      qDebug() << "i:" << i;
       // Some points are out of frame
-      int track_idx = (scene.track_vertices.length() / 2) - i;  // flip idx to start from top
-      if (scene.track_vertices[track_idx].y() < 0 || scene.track_vertices[track_idx].y() > height()) continue;
+      int track_idx = max_len - i - 1;  // flip idx to start from bottom right
+      qDebug() << 0 << scene.track_vertices[track_idx].y() << height();
+
+      if (scene.track_vertices[track_idx].y() < 0 || scene.track_vertices[track_idx].y() > height()) {
+        continue;
+      }
 
       // Flip so 0 is bottom of frame
       float lin_grad_point = (height() - scene.track_vertices[track_idx].y()) / height();
@@ -192,6 +201,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
       float lightness = util::map_val(saturation, 0.0f, 1.0f, 0.95f, 0.62f);  // lighter when grey
       float alpha = util::map_val(lin_grad_point, 0.75f / 2.f, 0.75f, 0.4f, 0.0f);  // matches previous alpha fade
       bg.setColorAt(lin_grad_point, QColor::fromHslF(path_hue / 360., saturation, lightness, alpha));
+      qDebug() << "path_hue: " << path_hue << "saturation: " << saturation << "lightness: " << lightness << "alpha: " << alpha;
 
       // Skip a point, unless next is last
       i += (i + 2) < max_len ? 1 : 0;
