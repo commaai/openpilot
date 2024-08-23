b87414c74971082a8af73d913441a00f90037798
Maxime Desroches <desroches.maxime@gmail.com>
2024-08-23 15:19:21 -0700

    add back pre-commit hook (#33366)
    * add pre-commit
    
    * space
    
    * post
    
    * e

--- tools/setup.sh
+++ tools/setup.sh
@@ -158,6 +158,7 @@ function git_clone() {
 function install_with_op() {
   cd $OPENPILOT_ROOT
   $OPENPILOT_ROOT/tools/op.sh install
+  $OPENPILOT_ROOT/tools/op.sh post-commit
 
   LOG_FILE=$(mktemp)
 
