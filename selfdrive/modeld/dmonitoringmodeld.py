644cfee11fabefabb81de2fdd606504ee848fb09
Comma Device <device@comma.ai>
2024-08-27 21:34:46 -0700

    it is 01
    old-commit-hash: a160d81eb1a7e77abbef959b44f602610f68f665

--- selfdrive/modeld/dmonitoringmodeld.py
+++ selfdrive/modeld/dmonitoringmodeld.py
@@ -77,7 +77,7 @@ class ModelState:
     input_data[:] = buf_data[v_offset:v_offset+MODEL_HEIGHT, h_offset:h_offset+MODEL_WIDTH]
 
     t1 = time.perf_counter()
-    self.model.setInputBuffer("input_img", self.inputs['input_img'].astype(np.float32))
+    self.model.setInputBuffer("input_img", self.inputs['input_img'].astype(np.float32)/255.)
     self.model.execute()
     t2 = time.perf_counter()
     return self.output, t2 - t1
