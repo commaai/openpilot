4802c95b96e171af5bccbe0d2404cdc513790b90
Kacper Rączy <gfw.kra@gmail.com>
2024-08-30 18:43:16 -0700

    Test polling on cameraOdometry
    old-commit-hash: a78e8b7d61618177f15c9892e2fa1e51620deca8

--- selfdrive/locationd/locationd.py
+++ selfdrive/locationd/locationd.py
@@ -236,7 +236,7 @@ def main():
   DEBUG = bool(int(os.getenv("DEBUG", "0")))
 
   pm = messaging.PubMaster(['livePose'])
-  sm = messaging.SubMaster(['accelerometer', 'gyroscope', 'carState', 'liveCalibration', 'cameraOdometry'])
+  sm = messaging.SubMaster(['accelerometer', 'gyroscope', 'carState', 'liveCalibration', 'cameraOdometry'], poll="cameraOdometry")
 
   params = Params()
 
