f2476637275e651f250214c2c580aabc3e4d252a
Kacper Rączy <gfw.kra@gmail.com>
2024-09-04 16:24:33 -0700

    process_replay: liveLocationKalman migration (#33472)
    * migrate_livePose
    
    * Remove unused param
    
    * Dont migrate if livePose is there

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -649,7 +649,7 @@ def replay_process(
   else:
     cfgs = [cfg]
 
-  all_msgs = migrate_all(lr, old_logtime=True,
+  all_msgs = migrate_all(lr,
                          manager_states=True,
                          panda_states=any("pandaStates" in cfg.pubs for cfg in cfgs),
                          camera_states=any(len(cfg.vision_pubs) != 0 for cfg in cfgs))
