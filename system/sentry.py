c52c63776355ec7dde5486e7caf0cfd875fc66df
Chris Souers <csouers@gmail.com>
2024-09-10 00:45:13 -0400

    AF: sentry for fw id capture

--- system/sentry.py
+++ system/sentry.py
@@ -11,11 +11,32 @@ from openpilot.system.version import get_build_metadata, get_version
 
 
 class SentryProject(Enum):
+  # All sentry data goes to AF. comma doesn't want fork data anyway
   # python project
-  SELFDRIVE = "https://6f3c7076c1e14b2aa10f5dde6dda0cc4@o33823.ingest.sentry.io/77924"
+  SELFDRIVE = "https://741d934bafba6e550d348f17c32dc1dc@o1269754.ingest.sentry.io/4506477324533760"
   # native project
-  SELFDRIVE_NATIVE = "https://3e4b586ed21a4479ad5d85083b639bc6@o33823.ingest.sentry.io/157615"
+  SELFDRIVE_NATIVE = "https://e2e617f1ccebd58ed7aa8fbb15ed5b95@o1269754.ingest.sentry.io/4506477325582336"
+  # controlsd CP
+  AF = "https://21654306f32a4cc29d283e7e068cf27c@o1269754.ingest.sentry.io/6460006"
 
+
+def capture_message(msg: str, data: str, file: str) -> None:
+  try:
+    dongle = Params().get("DongleId", encoding='utf-8')
+    # Encode bytes to base64 for attachment
+    attachment = str(data).encode()
+    # Add attachment to the current scope
+    # with sentry_sdk.start_transaction() as transaction:
+    with sentry_sdk.configure_scope() as scope:
+      scope.add_attachment(
+          bytes=attachment,
+          filename=f'{dongle}_{file}.txt',
+          content_type="text/plain"
+      )
+    sentry_sdk.capture_message(f'{dongle}: {msg}')
+    sentry_sdk.flush()  # https://github.com/getsentry/sentry-python/issues/291
+  except Exception as e:
+    print(e)
 
 def report_tombstone(fn: str, message: str, contents: str) -> None:
   cloudlog.error({'tombstone': message})
@@ -43,9 +64,8 @@ def set_tag(key: str, value: str) -> None:
 
 def init(project: SentryProject) -> bool:
   build_metadata = get_build_metadata()
-  # forks like to mess with this, so double check
-  comma_remote = build_metadata.openpilot.comma_remote and "commaai" in build_metadata.openpilot.git_origin
-  if not comma_remote or not is_registered_device() or PC:
+  if not is_registered_device() or PC:
+    print('sentry: device or remote not allowed')
     return False
 
   env = "release" if build_metadata.tested_channel else "master"
@@ -67,7 +87,7 @@ def init(project: SentryProject) -> bool:
 
   sentry_sdk.set_user({"id": dongle_id})
   sentry_sdk.set_tag("dirty", build_metadata.openpilot.is_dirty)
-  sentry_sdk.set_tag("origin", build_metadata.openpilot.git_origin)
+  sentry_sdk.set_tag("origin", build_metadata.openpilot.git_normalized_origin)
   sentry_sdk.set_tag("branch", build_metadata.channel)
   sentry_sdk.set_tag("commit", build_metadata.openpilot.git_commit)
   sentry_sdk.set_tag("device", HARDWARE.get_device_type())
