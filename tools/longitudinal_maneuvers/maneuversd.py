a3d587b93d08bc850350f57199ed302a2b8e657f
Shane Smiskol <shane@smiskol.com>
2024-09-16 22:27:29 -0700

    maneuversd: check for longActive again

--- tools/longitudinal_maneuvers/maneuversd.py
+++ tools/longitudinal_maneuvers/maneuversd.py
@@ -31,7 +31,7 @@ class Maneuver:
   def get_accel(self, v_ego: float, long_active: bool, standstill: bool, cruise_standstill: bool) -> float:
     ready = abs(v_ego - self.initial_speed) < 0.3 and long_active and not cruise_standstill
     if self.initial_speed < 0.01:
-      ready = v_ego < 0.1 and standstill
+      ready = ready and standstill
     self._ready_cnt = (self._ready_cnt + 1) if ready else 0
 
     if self._ready_cnt > (3. / DT_MDL):
