5742b294328c7fb1776127e17f165bf3a6f60606
Jason Wen <haibin.wen3@gmail.com>
2024-09-17 13:21:33 -0400

    selfdrive: Fix state machine test imports

--- selfdrive/selfdrived/tests/test_state_machine.py
+++ selfdrive/selfdrived/tests/test_state_machine.py
@@ -1,6 +1,7 @@
 from cereal import log
 from openpilot.common.realtime import DT_CTRL
-from openpilot.selfdrive.controls.lib.selfdrive import StateMachine, SOFT_DISABLE_TIME
+from openpilot.selfdrive.selfdrived.selfdrived import StateMachine
+from openpilot.selfdrive.selfdrived.state import SOFT_DISABLE_TIME
 from openpilot.selfdrive.selfdrived.events import Events, ET, EVENTS, NormalPermanentAlert
 
 State = log.SelfdriveState.OpenpilotState
