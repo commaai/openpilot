c05cf102917998fc564a03ce08b99388514cb0c1
Shane Smiskol <shane@smiskol.com>
2024-09-17 16:16:38 -0700

    fix

--- selfdrive/ui/tests/test_runner.cc
+++ selfdrive/ui/tests/test_runner.cc
@@ -11,7 +11,8 @@ int main(int argc, char **argv) {
   QApplication app(argc, argv);
 
   QString language_file = "main_test_en";
-  qDebug() << "Loading language:" << language_file;
+  // pytest-cpp considers this as a test case
+  // qDebug() << "Loading language:" << language_file;
 
   QTranslator translator;
   QString translationsPath = QDir::cleanPath(qApp->applicationDirPath() + "/../translations");
