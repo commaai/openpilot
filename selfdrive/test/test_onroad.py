df5b45a2850a70cec2256bcbce91fbbe650e9161
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-09-16 18:05:07 -0700

    modeld has gone up a bit

--- selfdrive/test/test_onroad.py
+++ selfdrive/test/test_onroad.py
@@ -46,7 +46,7 @@ PROCS = {
   "selfdrive.locationd.paramsd": 9.0,
   "./sensord": 7.0,
   "selfdrive.controls.radard": 2.0,
-  "selfdrive.modeld.modeld": 13.0,
+  "selfdrive.modeld.modeld": 17.0,
   "selfdrive.modeld.dmonitoringmodeld": 8.0,
   "system.hardware.hardwared": 3.87,
   "selfdrive.locationd.calibrationd": 2.0,
