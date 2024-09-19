09c4ec25e771f9d53b2950e872d39ddf3235981f
deanlee <deanlee3@gmail.com>
2024-09-19 15:05:06 +0800

    remove CameraType from camera_common.h

--- tools/replay/logreader.h
+++ tools/replay/logreader.h
@@ -4,7 +4,6 @@
 #include <vector>
 
 #include "cereal/gen/cpp/log.capnp.h"
-#include "system/camerad/cameras/camera_common.h"
 #include "tools/replay/util.h"
 
 const CameraType ALL_CAMERAS[] = {RoadCam, DriverCam, WideRoadCam};
