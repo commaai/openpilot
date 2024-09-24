c37fbeb1d118cbacfe31c7772ad8ad42d36039f1
Kacper Rączy <gfw.kra@gmail.com>
2024-09-23 19:04:50 -0700

    Add additional condition

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -107,7 +107,7 @@ procs = [
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
   PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
-  PythonProcess("joystick", "tools.joystick.joystick_control", joystick),
+  PythonProcess("joystick", "tools.joystick.joystick_control", and_(joystick, iscar)),
 ]
 
 managed_processes = {p.name: p for p in procs}
