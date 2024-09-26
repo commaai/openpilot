d5d68a5d6d8286823abee8e13db7a44c2b6d42f8
Bruce Wayne <harald.the.engineer@gmail.com>
2024-09-26 16:45:38 -0700

    double line

--- selfdrive/modeld/models/commonmodel.cc
+++ selfdrive/modeld/models/commonmodel.cc
@@ -29,10 +29,7 @@ cl_mem* ModelFrame::prepare(cl_mem yuv_cl, int frame_width, int frame_height, in
                   yuv_cl, frame_width, frame_height, frame_stride, frame_uv_offset,
                   y_cl, u_cl, v_cl, MODEL_WIDTH, MODEL_HEIGHT, projection);
  CL_CHECK(clEnqueueCopyBuffer(q, input_frames_cl, input_frames_cl, MODEL_FRAME_SIZE * sizeof(uint8_t), 0, MODEL_FRAME_SIZE * sizeof(uint8_t), 0, nullptr, nullptr));
-
-
   loadyuv_queue(&loadyuv, q, y_cl, u_cl, v_cl, net_input_cl);
-  CL_CHECK(clEnqueueCopyBuffer(q, net_input_cl, input_frames_cl, 0,  MODEL_FRAME_SIZE * sizeof(uint8_t), MODEL_FRAME_SIZE * sizeof(uint8_t), 0, nullptr, nullptr));
 
   // NOTE: Since thneed is using a different command queue, this clFinish is needed to ensure the image is ready.
   clFinish(q);
