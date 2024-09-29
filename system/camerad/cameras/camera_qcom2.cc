1983c1f28d6c9036b54bf8df99dc2668d78ae3e6
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-09-29 10:06:23 -0700

    Update system/camerad/cameras/camera_qcom2.cc

--- system/camerad/cameras/camera_qcom2.cc
+++ system/camerad/cameras/camera_qcom2.cc
@@ -181,7 +181,6 @@ void CameraState::set_camera_exposure(float grey_frac) {
     // Simple brute force optimizer to choose sensor parameters to reach desired EV
     int min_g = std::max(gain_idx - 1, sensor->analog_gain_min_idx);
     int max_g = std::min(gain_idx + 1, sensor->analog_gain_max_idx);
-
     for (int g = min_g; g <= max_g; g++) {
       float gain = sensor->sensor_analog_gains[g] * get_gain_factor();
 
