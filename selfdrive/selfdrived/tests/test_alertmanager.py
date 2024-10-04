cf50d4ae19634f04c4484aed1eb2815c40d907d4
Shane Smiskol <shane@smiskol.com>
2024-10-04 16:57:55 -0700

    AlertManager: reset duration when re-adding an event (#33478)
    * fix alert duration when re-adding an alert before it's ended (personality)
    
    * clean up
    
    * messy test
    
    * clean up test
    
    * Revert "clean up test"
    
    This reverts commit e7c0f80cb3d0061558920cf2baf37c6ea6935c81.
    
    * better name
    
    * debug
    
    * Revert "debug"
    
    This reverts commit da8d561445f0f0d142cec99c583dda798973544c.
    
    * Reapply "clean up test"
    
    This reverts commit a7dba540f78d1c32317e5f89e83de5aac1136c38.
    
    * update refs

--- selfdrive/selfdrived/tests/test_alertmanager.py
+++ selfdrive/selfdrived/tests/test_alertmanager.py
@@ -32,8 +32,27 @@ class TestAlertManager:
         for frame in range(duration+10):
           if frame < add_duration:
             AM.add_many(frame, [alert, ])
-          AM.process_alerts(frame, {})
+          AM.process_alerts(frame, set())
 
           shown = AM.current_alert != EmptyAlert
           should_show = frame <= show_duration
           assert shown == should_show, f"{frame=} {add_duration=} {duration=}"
+
+      # check one case:
+      # - if alert is re-added to AM before it ends the duration is extended
+      if duration > 1:
+        AM = AlertManager()
+        show_duration = duration * 2
+        for frame in range(duration * 2 + 10):
+          if frame == 0:
+            AM.add_many(frame, [alert, ])
+
+          if frame == duration:
+            # add alert one frame before it ends
+            assert AM.current_alert == alert
+            AM.add_many(frame, [alert, ])
+          AM.process_alerts(frame, set())
+
+          shown = AM.current_alert != EmptyAlert
+          should_show = frame <= show_duration
+          assert shown == should_show, f"{frame=} {duration=}"
