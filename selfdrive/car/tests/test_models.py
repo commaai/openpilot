7556233cca6acc8484020e2ae667df95c25631fe
Shane Smiskol <shane@smiskol.com>
2024-10-03 22:47:03 -0700

    Reapply "move car.capnp to opendbc (#33722)" (#33728)
    * Reapply "move car.capnp to opendbc" (#33725)
    
    This reverts commit 9d52a5b485fc45d3b05f2021bbf1bd1c4f15c83e.
    
    * why can't i repro?!
    
    * Revert "why can't i repro?!"
    
    This reverts commit 0435d218f790faf7b7aaed27d05ab9ee67b087e6.
    
    * does this cause card to try and read it?
    
    * better place
    
    * wtf
    
    * Reapply "why can't i repro?!"
    
    This reverts commit d24fd5a0abf454f47d5591e3b39039fdc4d0251c.
    
    * also here

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -1,6 +1,4 @@
 import capnp
-import copy
-import dataclasses
 import os
 import pytest
 import random
@@ -20,7 +18,6 @@ from opendbc.car.car_helpers import FRAME_FINGERPRINT, interfaces
 from opendbc.car.honda.values import CAR as HONDA, HondaFlags
 from opendbc.car.values import Platform
 from opendbc.car.tests.routes import non_tested_cars, routes, CarTestRoute
-from openpilot.selfdrive.car.card import convert_to_capnp
 from openpilot.selfdrive.selfdrived.events import ET
 from openpilot.selfdrive.selfdrived.selfdrived import SelfdriveD
 from openpilot.selfdrive.pandad import can_capnp_to_list
@@ -187,7 +184,7 @@ class TestCarModelBase(unittest.TestCase):
     del cls.can_msgs
 
   def setUp(self):
-    self.CI = self.CarInterface(copy.deepcopy(self.CP), self.CarController, self.CarState)
+    self.CI = self.CarInterface(self.CP.copy(), self.CarController, self.CarState)
     assert self.CI
 
     Params().put_bool("OpenpilotEnabledToggle", self.openpilot_enabled)
@@ -195,7 +192,7 @@ class TestCarModelBase(unittest.TestCase):
     # TODO: check safetyModel is in release panda build
     self.safety = libpanda_py.libpanda
 
-    cfg = car.CarParams.SafetyConfig(**dataclasses.asdict(self.CP.safetyConfigs[-1]))
+    cfg = self.CP.safetyConfigs[-1]
     set_status = self.safety.set_safety_hooks(cfg.safetyModel.raw, cfg.safetyParam)
     self.assertEqual(0, set_status, f"failed to set safetyModel {cfg}")
     self.safety.init_tests()
@@ -220,7 +217,7 @@ class TestCarModelBase(unittest.TestCase):
     # TODO: also check for checksum violations from can parser
     can_invalid_cnt = 0
     can_valid = False
-    CC = structs.CarControl()
+    CC = structs.CarControl().as_reader()
 
     for i, msg in enumerate(self.can_msgs):
       CS = self.CI.update(can_capnp_to_list((msg.as_builder().to_bytes(),)))
@@ -312,17 +309,17 @@ class TestCarModelBase(unittest.TestCase):
 
     # Make sure we can send all messages while inactive
     CC = structs.CarControl()
-    test_car_controller(CC)
+    test_car_controller(CC.as_reader())
 
     # Test cancel + general messages (controls_allowed=False & cruise_engaged=True)
     self.safety.set_cruise_engaged_prev(True)
     CC = structs.CarControl(cruiseControl=structs.CarControl.CruiseControl(cancel=True))
-    test_car_controller(CC)
+    test_car_controller(CC.as_reader())
 
     # Test resume + general messages (controls_allowed=True & cruise_engaged=True)
     self.safety.set_controls_allowed(True)
     CC = structs.CarControl(cruiseControl=structs.CarControl.CruiseControl(resume=True))
-    test_car_controller(CC)
+    test_car_controller(CC.as_reader())
 
   # Skip stdout/stderr capture with pytest, causes elevated memory usage
   @pytest.mark.nocapture
@@ -409,7 +406,7 @@ class TestCarModelBase(unittest.TestCase):
     selfdrived = SelfdriveD(CP=self.CP)
     selfdrived.initialized = True
     for idx, can in enumerate(self.can_msgs):
-      CS = convert_to_capnp(self.CI.update(can_capnp_to_list((can.as_builder().to_bytes(), ))))
+      CS = self.CI.update(can_capnp_to_list((can.as_builder().to_bytes(), ))).as_reader()
       for msg in filter(lambda m: m.src in range(64), can.can):
         to_send = libpanda_py.make_CANPacket(msg.address, msg.src % 4, msg.dat)
         ret = self.safety.safety_rx_hook(to_send)
