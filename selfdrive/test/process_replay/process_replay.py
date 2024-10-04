d74564520019f9c1dc153ef438f0295534fadddf
Shane Smiskol <shane@smiskol.com>
2024-10-03 22:46:28 -0700

    also here

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -363,7 +363,7 @@ def get_car_params_callback(rc, pm, msgs, fingerprint):
     cached_params = None
     if has_cached_cp:
       with car.CarParams.from_bytes(cached_params_raw) as _cached_params:
-        cached_params = structs.CarParams(carName=_cached_params.carName, carFw=_cached_params.carFw, carVin=_cached_params.carVin)
+        cached_params = _cached_params
 
     CP = get_car(*can_callbacks, lambda obd: None, Params().get_bool("ExperimentalLongitudinalEnabled"), cached_params=cached_params).CP
 
