329a82a2268f0c5a883f4a9361172ee3d2e139a0
ningkoon <ningkun@live.com>
2024-10-09 21:06:16 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -109,10 +109,10 @@ class LongitudinalPlanner:
     return x, v, a, j, throttle_prob
 
   def update(self, sm):
-    if sm['controlsState'].experimentalMode:
+    if sm['selfdriveState'].experimentalMode:
       self.mpc.mode = self.dynamic_experimental_controller.get_mpc_mode(self.CP.radarUnavailable, sm['carState'], sm['radarState'].leadOne, sm['modelV2'], sm['controlsState'], sm['navInstruction'].maneuverDistance)
     else:
-      self.mpc.mode = 'blended' if sm['controlsState'].experimentalMode else 'acc'
+      self.mpc.mode = 'blended' if sm['selfdriveState'].experimentalMode else 'acc'
 
     if len(sm['carControl'].orientationNED) == 3:
       accel_coast = get_coast_accel(sm['carControl'].orientationNED[1])
