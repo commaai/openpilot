2960f39b4c81889c33a6db387ac832af040848b5
Comma Device <device@comma.ai>
2024-10-22 22:10:47 +0000

    gate that

--- system/camerad/cameras/spectra.cc
+++ system/camerad/cameras/spectra.cc
@@ -864,16 +864,18 @@ void SpectraCamera::configISP() {
   ife_cmd.init(m, 67984, 0x20,
                CAM_MEM_FLAG_HW_READ_WRITE | CAM_MEM_FLAG_KMD_ACCESS | CAM_MEM_FLAG_UMD_ACCESS | CAM_MEM_FLAG_CMD_BUF_TYPE,
                m->device_iommu, m->cdm_iommu, FRAME_BUF_COUNT);
-  ife_gamma_lut.init(m, 64*sizeof(uint32_t), 0x20,
-                     CAM_MEM_FLAG_HW_READ_WRITE | CAM_MEM_FLAG_KMD_ACCESS | CAM_MEM_FLAG_UMD_ACCESS | CAM_MEM_FLAG_CMD_BUF_TYPE,
-                     m->device_iommu, m->cdm_iommu, 3); // 3 for RGB
-  for (int i = 0; i < 3; i++) {
-    memcpy(ife_gamma_lut.ptr + ife_gamma_lut.size*i, sensor->gamma_lut_rgb.data(), ife_gamma_lut.size);
+  if (!is_raw) {
+    ife_gamma_lut.init(m, 64*sizeof(uint32_t), 0x20,
+                       CAM_MEM_FLAG_HW_READ_WRITE | CAM_MEM_FLAG_KMD_ACCESS | CAM_MEM_FLAG_UMD_ACCESS | CAM_MEM_FLAG_CMD_BUF_TYPE,
+                       m->device_iommu, m->cdm_iommu, 3); // 3 for RGB
+    for (int i = 0; i < 3; i++) {
+      memcpy(ife_gamma_lut.ptr + ife_gamma_lut.size*i, sensor->gamma_lut_rgb.data(), ife_gamma_lut.size);
+    }
+    ife_linearization_lut.init(m, sensor->linearization_lut.size(), 0x20,
+                               CAM_MEM_FLAG_HW_READ_WRITE | CAM_MEM_FLAG_KMD_ACCESS | CAM_MEM_FLAG_UMD_ACCESS | CAM_MEM_FLAG_CMD_BUF_TYPE,
+                               m->device_iommu, m->cdm_iommu);
+    memcpy(ife_linearization_lut.ptr, sensor->linearization_lut.data(), ife_linearization_lut.size);
   }
-  ife_linearization_lut.init(m, sensor->linearization_lut.size(), 0x20,
-                             CAM_MEM_FLAG_HW_READ_WRITE | CAM_MEM_FLAG_KMD_ACCESS | CAM_MEM_FLAG_UMD_ACCESS | CAM_MEM_FLAG_CMD_BUF_TYPE,
-                             m->device_iommu, m->cdm_iommu);
-  memcpy(ife_linearization_lut.ptr, sensor->linearization_lut.data(), ife_linearization_lut.size);
 
   config_ife(0, 1, true);
 }
