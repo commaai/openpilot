0252852b16cc24f56779bfd5b0c5d4c2ea53af37
Comma Device <device@comma.ai>
2024-10-22 23:46:28 +0000

    fix build

--- system/camerad/cameras/spectra.cc
+++ system/camerad/cameras/spectra.cc
@@ -499,7 +499,7 @@ void SpectraCamera::config_ife(int idx, int request_id, bool init) {
       if (init) {
         buf_desc[0].length = build_initial_config((unsigned char*)ife_cmd.ptr + buf_desc[0].offset, sensor.get(), patches);
       } else {
-        buf_desc[0].length = build_update((unsigned char*)ife_cmd.ptr + buf_desc[0].offset, cc, sensor.get(), patches);
+        buf_desc[0].length = build_update((unsigned char*)ife_cmd.ptr + buf_desc[0].offset, sensor.get(), patches);
       }
     }
 
