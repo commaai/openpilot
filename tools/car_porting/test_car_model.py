cfe7d199e5ce04149784189fca14852729ca5129
Shane Smiskol <shane@smiskol.com>
2024-11-01 16:31:56 -0700

    this too

--- tools/car_porting/test_car_model.py
+++ tools/car_porting/test_car_model.py
@@ -8,11 +8,11 @@ from openpilot.selfdrive.car.tests.test_models import TestCarModel
 from openpilot.tools.lib.route import SegmentName
 
 
-def create_test_models_suite(routes: list[CarTestRoute], ci=False) -> unittest.TestSuite:
+def create_test_models_suite(routes: list[CarTestRoute]) -> unittest.TestSuite:
   test_suite = unittest.TestSuite()
   for test_route in routes:
     # create new test case and discover tests
-    test_case_args = {"platform": test_route.car_model, "test_route": test_route, "test_route_on_bucket": ci}
+    test_case_args = {"platform": test_route.car_model, "test_route": test_route}
     CarModelTestCase = type("CarModelTestCase", (TestCarModel,), test_case_args)
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(CarModelTestCase))
   return test_suite
@@ -23,7 +23,6 @@ if __name__ == "__main__":
                                                "Uses selfdrive/car/tests/test_models.py")
   parser.add_argument("route_or_segment_name", help="Specify route to run tests on")
   parser.add_argument("--car", help="Specify car model for test route")
-  parser.add_argument("--ci", action="store_true", help="Attempt to get logs using openpilotci, need to specify car")
   args = parser.parse_args()
   if len(sys.argv) == 1:
     parser.print_help()
@@ -33,6 +32,6 @@ if __name__ == "__main__":
   segment_num = route_or_segment_name.segment_num if route_or_segment_name.segment_num != -1 else None
 
   test_route = CarTestRoute(route_or_segment_name.route_name.canonical_name, args.car, segment=segment_num)
-  test_suite = create_test_models_suite([test_route], ci=args.ci)
+  test_suite = create_test_models_suite([test_route])
 
   unittest.TextTestRunner().run(test_suite)
