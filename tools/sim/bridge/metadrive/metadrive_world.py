385eb6fa13e3fe34d000678302e66fe3600a92e5
Maxime Desroches <desroches.maxime@gmail.com>
2024-11-01 10:54:27 -0700

    try

--- tools/sim/bridge/metadrive/metadrive_world.py
+++ tools/sim/bridge/metadrive/metadrive_world.py
@@ -108,6 +108,7 @@ class MetaDriveWorld(World):
       time_check_threshold = 29
       current_time = time.monotonic()
       since_last_check = current_time - self.last_check_timestamp
+      print('moved:', self.distance_moved)
       if since_last_check >= time_check_threshold:
         if after_engaged_check and self.distance_moved == 0:
           self.status_q.put(QueueMessage(QueueMessageType.TERMINATION_INFO, {"vehicle_not_moving" : True}))
