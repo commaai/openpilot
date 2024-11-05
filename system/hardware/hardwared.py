12fd9e441f2d01dd1629f13e02e0aabf667e825d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-11-05 14:27:33 -0800

    only show storage missing for comma threes

--- system/hardware/hardwared.py
+++ system/hardware/hardwared.py
@@ -316,7 +316,7 @@ def hardware_thread(end_event, hw_queue) -> None:
     set_offroad_alert_if_changed("Offroad_TemperatureTooHigh", show_alert, extra_text=extra_text)
 
     # TODO: this should move to TICI.initialize_hardware, but we currently can't import params there
-    if TICI:
+    if TICI and HARDWARE.get_device_type() == "tici":
       if not os.path.isfile("/persist/comma/living-in-the-moment"):
         if not Path("/data/media").is_mount():
           set_offroad_alert_if_changed("Offroad_StorageMissing", True)
