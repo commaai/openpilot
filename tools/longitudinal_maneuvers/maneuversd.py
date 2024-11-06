455d01a048c94595324a968d49edde9df42520c7
Shane Smiskol <shane@smiskol.com>
2024-11-05 16:06:18 -0800

    fix

--- tools/longitudinal_maneuvers/maneuversd.py
+++ tools/longitudinal_maneuvers/maneuversd.py
@@ -86,6 +86,12 @@ class Maneuver:
 MANEUVERS = [
   Maneuver(
     "come to stop",
+    [Action(-0.5, 12)],
+    repeat=2,
+    initial_speed=5.,
+  ),
+  Maneuver(
+    "start from stop",
     [Action(1.5, 5)],
     repeat=2,
     initial_speed=0.,
@@ -98,16 +104,6 @@ MANEUVERS = [
       Action(1, 3), Action(-1, 3),
     ],
     repeat=2,
-    initial_speed=0.,
-  ),
-  Maneuver(
-    "creep: alternate between +1m/s^2 and -1m/s^2",
-    [
-      Action(1, 2), Action(-1, 2),
-      Action(1, 2), Action(-1, 2),
-      Action(1, 2), Action(-1, 2),
-    ],
-    repeat=1,
     initial_speed=0.,
   ),
   Maneuver(
