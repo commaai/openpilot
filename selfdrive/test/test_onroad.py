7461a3f7e220d99d0a250909add4a459d0c609dd
Maxime Desroches <desroches.maxime@gmail.com>
2024-11-07 16:19:08 -0800

    viz

--- selfdrive/test/test_onroad.py
+++ selfdrive/test/test_onroad.py
@@ -11,6 +11,7 @@ import numpy as np
 import zstandard as zstd
 from collections import Counter, defaultdict
 from pathlib import Path
+from tabulate import tabulate
 
 from cereal import car, log
 import cereal.messaging as messaging
@@ -241,10 +242,9 @@ class TestOnroad:
     assert len(veryslow) < 5, f"Too many slow frame draw times: {veryslow}"
 
   def test_cpu_usage(self, subtests):
-    result = "\n"
-    result += "------------------------------------------------\n"
-    result += "------------------ CPU Usage -------------------\n"
-    result += "------------------------------------------------\n"
+    print("\n------------------------------------------------")
+    print("------------------ CPU Usage -------------------")
+    print("------------------------------------------------")
 
     plogs_by_proc = defaultdict(list)
     for pl in self.msgs['procLog']:
@@ -256,9 +256,11 @@ class TestOnroad:
 
     cpu_ok = True
     dt = (self.msgs['procLog'][-1].logMonoTime - self.msgs['procLog'][0].logMonoTime) / 1e9
+    header = ['process', 'usage', 'expected', 'max allowed', 'test result']
+    rows = []
     for proc_name, expected in PROCS.items():
 
-      err = ""
+      err = "✅"
       usage = 0.
       x = plogs_by_proc[proc_name]
       if len(x) > 2:
@@ -267,15 +269,15 @@ class TestOnroad:
 
         max_allowed = max(expected * 1.8, expected + 5.0)
         if usage > max_allowed:
-          err = "USING MORE CPU THAN EXPECTED"
+          err = "❌ USING MORE CPU THAN EXPECTED ❌"
+          cpu_ok = False
 
       else:
-        err = "NO METRICS FOUND"
-
-      result += f"{proc_name.ljust(35)}  {usage=:5.2f}%  {expected=:5.2f}%  {max_allowed=:5.2f}%  {err}\n"
-      if len(err) > 0:
+        err = "❌ NO METRICS FOUND ❌"
         cpu_ok = False
-    result += "------------------------------------------------\n"
+
+      rows.append([proc_name, usage, expected, max_allowed, err])
+    print(tabulate(rows, header, tablefmt="simple_grid", stralign="center", numalign="center", floatfmt=".2f"))
 
     # Ensure there's no missing procs
     all_procs = {p.name for p in self.msgs['managerState'][0].managerState.processes if p.shouldBeRunning}
@@ -287,11 +289,9 @@ class TestOnroad:
     procs_tot = sum([(max(x) if isinstance(x, tuple) else x) for x in PROCS.values()])
     with subtests.test(name="total CPU"):
       assert procs_tot < MAX_TOTAL_CPU, "Total CPU budget exceeded"
-    result +=  "------------------------------------------------\n"
-    result += f"Total allocated CPU usage is {procs_tot}%, budget is {MAX_TOTAL_CPU}%, {MAX_TOTAL_CPU-procs_tot:.1f}% left\n"
-    result +=  "------------------------------------------------\n"
-
-    print(result)
+    print("------------------------------------------------")
+    print(f"Total allocated CPU usage is {procs_tot}%, budget is {MAX_TOTAL_CPU}%, {MAX_TOTAL_CPU-procs_tot:.1f}% left")
+    print("------------------------------------------------")
 
     assert cpu_ok
 
