62ed151457a1bdb832c41d341f3fc508c678f744
Maxime Desroches <desroches.maxime@gmail.com>
2024-11-11 20:47:23 -0800

    cons

--- selfdrive/test/process_replay/test_processes.py
+++ selfdrive/test/process_replay/test_processes.py
@@ -193,6 +193,7 @@ if __name__ == "__main__":
         if cfg.proc_name not in tested_procs:
           continue
 
+        # to speed things up, only test all segments on card
         if cfg.proc_name != 'card' and car_brand not in ['HYUNDAI', 'TOYOTA', 'HONDA', 'RAM', 'SUBARU', 'VOLKSWAGEN', 'FORD']:
           continue
 
