03870aa35c88a9e75d9362f6e7e2487aefda63df
AlexandreSato <sato_alexandre@hotmail.com>
2024-11-12 21:19:16 -0300

    fix errors in developerPanel

--- selfdrive/ui/qt/offroad/developer_panel.cc
+++ selfdrive/ui/qt/offroad/developer_panel.cc
@@ -3,6 +3,7 @@
 #include "selfdrive/ui/qt/offroad/developer_panel.h"
 #include "selfdrive/ui/qt/widgets/ssh_keys.h"
 #include "selfdrive/ui/qt/widgets/controls.h"
+#include "common/util.h"
 
 DeveloperPanel::DeveloperPanel(SettingsWindow *parent) : ListWidget(parent) {
   // SSH keys
@@ -24,13 +25,35 @@ DeveloperPanel::DeveloperPanel(SettingsWindow *parent) : ListWidget(parent) {
   addItem(longManeuverToggle);
 
   // Joystick and longitudinal maneuvers should be hidden on release branches
-  // also the toggles should be not available to change in onroad state
   const bool is_release = params.getBool("IsReleaseBranch");
+  for (auto btn : findChildren<ParamControl *>()) {
+    btn->setVisible(!is_release);
+  }
+
+  // Toggles should be not available to change in onroad state
   QObject::connect(uiState(), &UIState::offroadTransition, [=](bool offroad) {
     for (auto btn : findChildren<ParamControl *>()) {
-      btn->setVisible(!is_release);
       btn->setEnabled(offroad);
     }
   });
+}
 
+void DeveloperPanel::updateToggles() {
+  auto cp_bytes = params.get("CarParamsPersistent");
+  if (!cp_bytes.empty()) {
+    AlignedBuffer aligned_buf;
+    capnp::FlatArrayMessageReader cmsg(aligned_buf.align(cp_bytes.data(), cp_bytes.size()));
+    cereal::CarParams::Reader CP = cmsg.getRoot<cereal::CarParams>();
+
+    // longManeuverToggle should not be toggable if the car don't have longitudinal control
+    if (hasLongitudinalControl(CP)) {
+      longManeuverToggle->setEnabled(true);
+    }
+  } else {
+    longManeuverToggle->setEnabled(false);
+  }
+}
+
+void DeveloperPanel::showEvent(QShowEvent *event) {
+  updateToggles();
 }
--- selfdrive/ui/qt/offroad/developer_panel.h
+++ selfdrive/ui/qt/offroad/developer_panel.h
@@ -6,9 +6,12 @@ class DeveloperPanel : public ListWidget {
   Q_OBJECT
 public:
   explicit DeveloperPanel(SettingsWindow *parent);
+  void showEvent(QShowEvent *event) override;
 
 private:
   Params params;
   ParamControl* joystickToggle;
   ParamControl* longManeuverToggle;
+
+  void updateToggles();
 };
