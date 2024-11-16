863d86c10caafc60d99cc4b9e1267a688c7df2f9
Shane Smiskol <shane@smiskol.com>
2024-11-15 23:16:15 -0600

    Toyota: adaptive ACCEL_NET for new long tune (#34034)
    * bump
    
    * bump
    
    * fix that
    
    * should be a better way
    
    * raise

--- tools/cabana/dbc/generate_dbc_json.py
+++ tools/cabana/dbc/generate_dbc_json.py
@@ -2,12 +2,23 @@
 import argparse
 import json
 
+from opendbc.car import Bus
 from opendbc.car.fingerprints import MIGRATION
 from opendbc.car.values import PLATFORMS
 
 
 def generate_dbc_json() -> str:
-  dbc_map = {platform.name: platform.config.dbc_dict['pt'] for platform in PLATFORMS.values() if platform != "MOCK"}
+  dbc_map = {}
+  for platform in PLATFORMS.values():
+    if platform != "MOCK":
+      if Bus.pt in platform.config.dbc_dict:
+        dbc_map[platform.name] = platform.config.dbc_dict[Bus.pt]
+      elif Bus.main in platform.config.dbc_dict:
+        dbc_map[platform.name] = platform.config.dbc_dict[Bus.main]
+      elif Bus.party in platform.config.dbc_dict:
+        dbc_map[platform.name] = platform.config.dbc_dict[Bus.party]
+      else:
+        raise ValueError("Unknown main type")
 
   for m in MIGRATION:
     if MIGRATION[m] in dbc_map:
