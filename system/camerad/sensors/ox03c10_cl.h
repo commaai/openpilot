159286c51ad31d4d8907ebd77e1a64b3a9fa696a
Comma Device <device@comma.ai>
2024-12-06 01:09:41 +0000

    20 is fake

--- system/camerad/sensors/ox03c10_cl.h
+++ system/camerad/sensors/ox03c10_cl.h
@@ -2,28 +2,32 @@
 
 #define BIT_DEPTH 12
 #define BLACK_LVL 64
+#define PWL_MAX 3530 // 17-bit cutoff
 #define VIGNETTE_RSZ 1.0f
 
-float ox_lut_func(int x) {
+float ox_lut_func(int x_0) {
+  int x = clamp(x_0 - BLACK_LVL, 0, PWL_MAX);
+  float ret;
   if (x < 512) {
-    return x * 5.94873e-8;
+    ret = x * 5.94873e-8;
   } else if (512 <= x && x < 768) {
-    return 3.0458e-05 + (x-512) * 1.19913e-7;
+    ret = 3.0458e-05 + (x-512) * 1.19913e-7;
   } else if (768 <= x && x < 1536) {
-    return 6.1154e-05 + (x-768) * 2.38493e-7;
+    ret = 6.1154e-05 + (x-768) * 2.38493e-7;
   } else if (1536 <= x && x < 1792) {
-    return 0.0002448 + (x-1536) * 9.56930e-7;
+    ret = 0.0002448 + (x-1536) * 9.56930e-7;
   } else if (1792 <= x && x < 2048) {
-    return 0.00048977 + (x-1792) * 1.91441e-6;
+    ret = 0.00048977 + (x-1792) * 1.91441e-6;
   } else if (2048 <= x && x < 2304) {
-    return 0.00097984 + (x-2048) * 3.82937e-6;
+    ret = 0.00097984 + (x-2048) * 3.82937e-6;
   } else if (2304 <= x && x < 2560) {
-    return 0.0019601 + (x-2304) * 7.659055e-6;
+    ret = 0.0019601 + (x-2304) * 7.659055e-6;
   } else if (2560 <= x && x < 2816) {
-    return 0.0039207 + (x-2560) * 1.525e-5;
+    ret = 0.0039207 + (x-2560) * 1.525e-5;
   } else {
-    return 0.0078421 + (exp((x-2816)/273.0) - 1) * 0.0092421;
+    ret = 0.0078421 + (exp((x-2816)/273.0) - 1) * 0.0092421;
   }
+  return ret * 8.0;
 }
 
 float4 normalize_pv(int4 parsed, float vignette_factor) {
