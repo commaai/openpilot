b936b1d5ff08704b455deff7d0dde78e0056b4b6
BBBmau <alvarez.mauriciotm@gmail.com>
2024-12-05 17:29:59 -0800

    revert prefix change

--- third_party/raylib/include/raylib.h
+++ third_party/raylib/include/raylib.h
@@ -172,33 +172,33 @@
 
 // Some Basic Colors
 // NOTE: Custom raylib color palette for amazing visuals on WHITE background
-#define COLOR_LIGHTGRAY  CLITERAL(Color){ 200, 200, 200, 255 }   // Light Gray
-#define COLOR_GRAY       CLITERAL(Color){ 130, 130, 130, 255 }   // Gray
-#define COLOR_DARKGRAY   CLITERAL(Color){ 80, 80, 80, 255 }      // Dark Gray
-#define COLOR_YELLOW     CLITERAL(Color){ 253, 249, 0, 255 }     // Yellow
-#define COLOR_GOLD       CLITERAL(Color){ 255, 203, 0, 255 }     // Gold
-#define COLOR_ORANGE     CLITERAL(Color){ 255, 161, 0, 255 }     // Orange
-#define COLOR_PINK       CLITERAL(Color){ 255, 109, 194, 255 }   // Pink
-#define COLOR_RED        CLITERAL(Color){ 230, 41, 55, 255 }     // Red
-#define COLOR_MAROON     CLITERAL(Color){ 190, 33, 55, 255 }     // Maroon
-#define COLOR_GREEN      CLITERAL(Color){ 0, 228, 48, 255 }      // Green
-#define COLOR_LIME       CLITERAL(Color){ 0, 158, 47, 255 }      // Lime
-#define COLOR_DARKGREEN  CLITERAL(Color){ 0, 117, 44, 255 }      // Dark Green
-#define COLOR_SKYBLUE    CLITERAL(Color){ 102, 191, 255, 255 }   // Sky Blue
-#define COLOR_BLUE       CLITERAL(Color){ 0, 121, 241, 255 }     // Blue
-#define COLOR_DARKBLUE   CLITERAL(Color){ 0, 82, 172, 255 }      // Dark Blue
-#define COLOR_PURPLE     CLITERAL(Color){ 200, 122, 255, 255 }   // Purple
-#define COLOR_VIOLET     CLITERAL(Color){ 135, 60, 190, 255 }    // Violet
-#define COLOR_DARKPURPLE CLITERAL(Color){ 112, 31, 126, 255 }    // Dark Purple
-#define COLOR_BEIGE      CLITERAL(Color){ 211, 176, 131, 255 }   // Beige
-#define COLOR_BROWN      CLITERAL(Color){ 127, 106, 79, 255 }    // Brown
-#define COLOR_DARKBROWN  CLITERAL(Color){ 76, 63, 47, 255 }      // Dark Brown
+#define LIGHTGRAY  CLITERAL(Color){ 200, 200, 200, 255 }   // Light Gray
+#define GRAY       CLITERAL(Color){ 130, 130, 130, 255 }   // Gray
+#define DARKGRAY   CLITERAL(Color){ 80, 80, 80, 255 }      // Dark Gray
+#define YELLOW     CLITERAL(Color){ 253, 249, 0, 255 }     // Yellow
+#define GOLD       CLITERAL(Color){ 255, 203, 0, 255 }     // Gold
+#define ORANGE     CLITERAL(Color){ 255, 161, 0, 255 }     // Orange
+#define PINK       CLITERAL(Color){ 255, 109, 194, 255 }   // Pink
+#define RED        CLITERAL(Color){ 230, 41, 55, 255 }     // Red
+#define MAROON     CLITERAL(Color){ 190, 33, 55, 255 }     // Maroon
+#define GREEN      CLITERAL(Color){ 0, 228, 48, 255 }      // Green
+#define LIME       CLITERAL(Color){ 0, 158, 47, 255 }      // Lime
+#define DARKGREEN  CLITERAL(Color){ 0, 117, 44, 255 }      // Dark Green
+#define SKYBLUE    CLITERAL(Color){ 102, 191, 255, 255 }   // Sky Blue
+#define BLUE       CLITERAL(Color){ 0, 121, 241, 255 }     // Blue
+#define DARKBLUE   CLITERAL(Color){ 0, 82, 172, 255 }      // Dark Blue
+#define PURPLE     CLITERAL(Color){ 200, 122, 255, 255 }   // Purple
+#define VIOLET     CLITERAL(Color){ 135, 60, 190, 255 }    // Violet
+#define DARKPURPLE CLITERAL(Color){ 112, 31, 126, 255 }    // Dark Purple
+#define BEIGE      CLITERAL(Color){ 211, 176, 131, 255 }   // Beige
+#define BROWN      CLITERAL(Color){ 127, 106, 79, 255 }    // Brown
+#define DARKBROWN  CLITERAL(Color){ 76, 63, 47, 255 }      // Dark Brown
 
-#define COLOR_WHITE      CLITERAL(Color){ 255, 255, 255, 255 }   // White
-#define COLOR_BLACK      CLITERAL(Color){ 0, 0, 0, 255 }         // Black
-#define COLOR_BLANK      CLITERAL(Color){ 0, 0, 0, 0 }           // Blank (Transparent)
-#define COLOR_MAGENTA    CLITERAL(Color){ 255, 0, 255, 255 }     // Magenta
-#define COLOR_RAYWHITE   CLITERAL(Color){ 245, 245, 245, 255 }   // My own White (raylib logo)
+#define WHITE      CLITERAL(Color){ 255, 255, 255, 255 }   // White
+#define BLACK      CLITERAL(Color){ 0, 0, 0, 255 }         // Black
+#define BLANK      CLITERAL(Color){ 0, 0, 0, 0 }           // Blank (Transparent)
+#define MAGENTA    CLITERAL(Color){ 255, 0, 255, 255 }     // Magenta
+#define RAYWHITE   CLITERAL(Color){ 245, 245, 245, 255 }   // My own White (raylib logo)
 
 //----------------------------------------------------------------------------------
 // Structures Definition
