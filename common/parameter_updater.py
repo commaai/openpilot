ff659a1f57532ed29638d40411e3c94b6e137099
deanlee <deanlee3@gmail.com>
2024-12-09 03:37:53 +0800

    use ParameterUpdater in selfdrived

--- common/parameter_updater.py
+++ common/parameter_updater.py
@@ -1,41 +1,41 @@
 import threading
 import time
-from typing import Dict, Union
+
 from openpilot.common.params import Params
 
 
 class ParameterUpdater:
-  def __init__(self, params_to_update: Dict[str, str]):
+  def __init__(self, params_to_update: dict[str, str]):
     """
     params_to_update: A dictionary where keys are parameter names, and values are their types ('bool' or 'str').
     Example: {"IsMetric": "bool", "LongitudinalPersonality": "str"}
     """
     self.params = Params()
     self.params_to_update = params_to_update
-    self.param_values: Dict[str, Union[bool, str]] = {param: None for param in params_to_update}
+    self.param_values = {param: None for param in params_to_update}
 
     self._update()  # Initial update
 
     self.mutex = threading.Lock()
     self.stop_event = threading.Event()
-    self.update_thread = None
+    self.update_thread: threading.Thread | None = None
 
-  def start(self) -> None:
-    if self.update_thread is None or not self.update_thread.is_alive():
+  def start(self):
+    if self.update_thread is None:
       self.update_thread = threading.Thread(target=self._update_periodically, daemon=True)
       self.update_thread.start()
 
-  def stop(self) -> None:
-    if self.update_thread and self.update_thread.is_alive():
+  def stop(self):
+    if self.update_thread:
       self.stop_event.set()
       self.update_thread.join()
 
-  def get_param_value(self, param: str) -> Union[bool, str, None]:
+  def get_param_value(self, param: str):
     with self.mutex:
       return self.param_values.get(param)
 
-  def _update(self) -> None:
-    new_values: Dict[str, Union[bool, str]] = {}
+  def _update(self):
+    new_values: dict[str, bool | str | None] = {}
     for param, param_type in self.params_to_update.items():
       if param_type == "bool":
         new_values[param] = self.params.get_bool(param)
@@ -47,7 +47,7 @@ class ParameterUpdater:
     with self.mutex:
       self.param_values = new_values
 
-  def _update_periodically(self) -> None:
+  def _update_periodically(self):
     while not self.stop_event.is_set():
       self._update()
       time.sleep(0.1)
