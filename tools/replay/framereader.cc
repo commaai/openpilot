44fe8d0bf905da8980d34738e1816f7781d5efb6
deanlee <deanlee3@gmail.com>
2024-12-16 16:43:54 +0800

    exit early

--- tools/replay/framereader.cc
+++ tools/replay/framereader.cc
@@ -177,10 +177,14 @@ bool VideoDecoder::decode(FrameReader *reader, int idx, VisionBuf *buf) {
         break;
       }
     }
+
     auto pos = reader->packets_info[from_idx].pos;
-    if (avformat_seek_file(reader->input_ctx, 0, pos, pos, pos, AVSEEK_FLAG_BYTE) >= 0) {
-      avcodec_flush_buffers(decoder_ctx);
+    int ret = avformat_seek_file(reader->input_ctx, 0, pos, pos, pos, AVSEEK_FLAG_BYTE);
+    if (ret < 0) {
+      rError("Failed to seek to byte position %lld: %d", pos, AVERROR(ret));
+      return false;
     }
+    avcodec_flush_buffers(decoder_ctx);
   }
   reader->prev_idx = idx;
 
