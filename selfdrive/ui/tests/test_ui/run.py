5b99c8b185d8d4135d05fa0772c642d61d81898b
Jason Wen <haibin.wen3@gmail.com>
2025-01-10 11:25:32 -0500

    Revert "ci: generate Trips settings panel for ui preview" (#548)
    Revert "ci: generate Trips settings panel for ui preview (#546)"
    
    This reverts commit b29117c02a7320f6b9f437875dc423579db06926.

--- selfdrive/ui/tests/test_ui/run.py
+++ selfdrive/ui/tests/test_ui/run.py
@@ -231,7 +231,6 @@ CASES.update({
   "settings_sunnylink": setup_settings_sunnylink,
   "settings_sunnypilot": setup_settings_sunnypilot,
   "settings_sunnypilot_mads": setup_settings_sunnypilot_mads,
-  "settings_trips": setup_settings_trips,
 })
 
 TEST_DIR = pathlib.Path(__file__).parent
