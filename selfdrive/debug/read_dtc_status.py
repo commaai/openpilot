331e4d2a5e711a3ad5b352e9ec846e18072bcec8
Shane Smiskol <shane@smiskol.com>
2025-01-13 00:46:36 -0800

    imports

--- selfdrive/debug/read_dtc_status.py
+++ selfdrive/debug/read_dtc_status.py
@@ -2,9 +2,8 @@
 import sys
 import argparse
 from subprocess import check_output, CalledProcessError
+from opendbc.car.uds import UdsClient, SESSION_TYPE, DTC_REPORT_TYPE, DTC_STATUS_MASK_TYPE, get_dtc_num_as_str, get_dtc_status_names
 from panda import Panda
-from panda.python.uds import UdsClient, SESSION_TYPE, DTC_REPORT_TYPE, DTC_STATUS_MASK_TYPE
-from panda.python.uds import get_dtc_num_as_str, get_dtc_status_names
 
 parser = argparse.ArgumentParser(description="read DTC status")
 parser.add_argument("addr", type=lambda x: int(x,0))
