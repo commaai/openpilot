14bc916b773f9ba79510557293beac57274cd13a
deanlee <deanlee3@gmail.com>
2025-01-14 11:39:44 +0800

    revert auto fit screen size

--- system/ui/raylib/util.cc
+++ system/ui/raylib/util.cc
@@ -39,7 +39,7 @@ App::App(const char *title, int fps) {
   Hardware::set_brightness(65);
 
   // SetTraceLogLevel(LOG_NONE);
-  InitWindow(0, 0, title);
+  InitWindow(2160, 1080, title);
   SetTargetFPS(fps);
 
   // Load fonts
