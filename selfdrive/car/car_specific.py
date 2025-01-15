f503882ea32497a4c7b6575879bf4e8cfe72e0ef
Shane Smiskol <shane@smiskol.com>
2025-01-14 20:37:02 -0800

    rising edge

--- selfdrive/car/car_specific.py
+++ selfdrive/car/car_specific.py
@@ -215,8 +215,8 @@ class CarSpecificEvents:
       # Enable OP long on falling edge of enable buttons (defaults to accelCruise and decelCruise, overridable per-port)
       if not self.CP.pcmCruise and (b.type in enable_buttons and not b.pressed):
         events.add(EventName.buttonEnable)
-      # Disable on rising and falling edge of cancel for both stock and OP long
-      if b.type == ButtonType.cancel:
+      # Disable on rising edge of cancel for both stock and OP long
+      if b.type == ButtonType.cancel and b.pressed:
         events.add(EventName.buttonCancel)
 
     # Handle permanent and temporary steering faults
