a5f186853fb531ee7aa7e7731ea2de082ddea725
Dean Lee <deanlee3@gmail.com>
2025-01-18 23:02:20 +0800

    Revert "ui: show driver camera in popup on demand (#33459)"
    This reverts commit 5f3625436ceba9ca4d597e4ed10f906483462a46.

--- selfdrive/ui/qt/home.h
+++ selfdrive/ui/qt/home.h
@@ -8,6 +8,7 @@
 #include <QWidget>
 
 #include "common/params.h"
+#include "selfdrive/ui/qt/offroad/driverview.h"
 #include "selfdrive/ui/qt/body.h"
 #include "selfdrive/ui/qt/onroad/onroad_home.h"
 #include "selfdrive/ui/qt/sidebar.h"
@@ -52,6 +53,7 @@ signals:
 
 public slots:
   void offroadTransition(bool offroad);
+  void showDriverView(bool show);
   void showSidebar(bool show);
 
 protected:
@@ -63,6 +65,7 @@ private:
   OffroadHome *home;
   OnroadWindow *onroad;
   BodyWindow *body;
+  DriverViewWindow *driver_view;
   QStackedLayout *slayout;
 
 private slots:
