52f15a172d7e1807fcf20a2d445b2232a5bd114e
ichiroMax <cxk01765@nifty.com>
2025-01-30 10:00:43 +0900

    FIREHOSEモード！

--- selfdrive/ui/translations/main_ja.ts
+++ selfdrive/ui/translations/main_ja.ts
@@ -1075,11 +1075,11 @@ This may take up to a minute.</source>
     </message>
     <message>
         <source>FIREHOSE Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>FIREHOSEモード</translation>
     </message>
     <message>
         <source>Enable &lt;b&gt;FIREHOSE Mode&lt;/b&gt; to get your driving data in the training set.&lt;br&gt;&lt;br&gt;Follow these steps to get your device ready:&lt;br&gt;  1. Bring your device inside and connect to a good USB-C adapter&lt;br&gt;  2. Connect to Wi-Fi&lt;br&gt;  3. Enable this toggle&lt;br&gt;  4. Leave it connected for at least 30 minutes&lt;br&gt;&lt;br&gt;This toggle turns off once you restart your device. Repeat once a week for maximum effectiveness.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;FIREHOSEモード&lt;/b&gt;を有効にすると、あなたの運転が公式のトレーニングデータに追加されます。&lt;br&gt;&lt;br&gt;次の手順でデバイスを準備してください:&lt;br&gt;  1. デバイスを屋内に持ち込み、適切なUSB-Cアダプターに接続する&lt;br&gt;  2. Wi-Fiに接続する&lt;br&gt;  3. このスイッチを有効にする&lt;br&gt;  4. 少なくとも30分間接続したままにする&lt;br&gt;&lt;br&gt;このスイッチはデバイスを再起動すると無効になります。効果を最大化するためには毎週実行するのが望ましいです。</translation>
     </message>
 </context>
 <context>
