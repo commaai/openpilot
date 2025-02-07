1b21d8c9e4c3b1d92bbace08790061110979365a
Shane Smiskol <shane@smiskol.com>
2025-02-06 19:46:30 -0800

    rename?

--- tools/lib/optools/build.sh
+++ tools/lib/optools/build.sh
@@ -7,15 +7,15 @@ uv sync
 
 # TODO: just copy log.capnp and car.capnp with a simple loader file
 #  the code might want to be structured to pull the latest definitions and store to cache (every time? if too old? by flag?)
-rm -rf openpilot_logging/cereal/
-cp -rL ../../../cereal openpilot_logging/
+rm -rf optools/cereal/
+cp -rL ../../../cereal optools/
 
 # quick test
 # TODO: fix pythonpath
-export PYTHONPATH=/home/batman/openpilot/tools/lib/openpilot_logging/
-python openpilot_logging/logreader.py 1d3dc3e03047b0c7/000000dd--455f14369d/0/q > /dev/null
+export PYTHONPATH=/home/batman/openpilot/tools/lib/optools/
+#python optools/logreader.py 1d3dc3e03047b0c7/000000dd--455f14369d/0/q > /dev/null
 
 rm -rf dist/
 python -c "import setuptools; setuptools.setup()" sdist
 
-#twine upload dist/* --verbose
+twine upload dist/* --verbose
