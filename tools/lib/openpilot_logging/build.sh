c8318c2e29121a92dc30f8bb94e8b0287a883516
Shane Smiskol <shane@smiskol.com>
2025-02-06 19:36:02 -0800

    0.1.1 works but is messy

--- tools/lib/openpilot_logging/build.sh
+++ tools/lib/openpilot_logging/build.sh
@@ -5,9 +5,10 @@ uv venv
 source .venv/bin/activate
 uv sync
 
-rm -rf cereal/
-cp -vrL ../../../cereal .
-
+# TODO: just copy log.capnp and car.capnp with a simple loader file
+#  the code might want to be structured to pull the latest definitions and store to cache (every time? if too old? by flag?)
+rm -rf openpilot_logging/cereal/
+cp -rL ../../../cereal openpilot_logging/
 
 # quick test
 # TODO: fix pythonpath
@@ -17,4 +18,4 @@ python openpilot_logging/logreader.py 1d3dc3e03047b0c7/000000dd--455f14369d/0/q 
 rm -rf dist/
 python -c "import setuptools; setuptools.setup()" sdist
 
-# twine upload dist/* --verbose
+#twine upload dist/* --verbose
