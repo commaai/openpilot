aa50c209b519fafa0b2c6bd30bac574df0336ee0
deanlee <deanlee3@gmail.com>
2025-02-09 02:34:04 +0800

    add zst suffix to LOGS_SIZE_RATE

--- selfdrive/test/test_onroad.py
+++ selfdrive/test/test_onroad.py
@@ -100,8 +100,8 @@ TIMINGS = {
 }
 
 LOGS_SIZE_RATE = {
-  "qlog": 0.0083,
-  "rlog": 0.135,
+  "qlog.zst": 0.0083,
+  "rlog.zst": 0.135,
   "qcamera.ts": 0.03828,
 }
 LOGS_SIZE_RATE.update(dict.fromkeys(['ecamera.hevc', 'fcamera.hevc'], 1.2740))
