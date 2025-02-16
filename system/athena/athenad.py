a5011131826cd8d8a1d35db14515178c980a3969
BrainLess <116778989+BrainLessPea@users.noreply.github.com>
2025-02-16 11:58:10 +0530

    Update system/athena/athenad.py
    Co-authored-by: Maxime Desroches <desroches.maxime@gmail.com>

--- system/athena/athenad.py
+++ system/athena/athenad.py
@@ -767,10 +767,10 @@ def ws_manage(ws: WebSocket, end_event: threading.Event) -> None:
         # FIXME: TCP_USER_TIMEOUT is effectively 2x for some reason (32s), so it's mostly unused
         if sys.platform == 'linux':
           sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_USER_TIMEOUT, 16000 if onroad else 0)
-        if sys.platform == 'darwin':
-          sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPALIVE, 7 if onroad else 30)
-        else:
           sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 7 if onroad else 30)
+        elif sys.platform == 'darwin':
+          sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPALIVE, 7 if onroad else 30)
+          
         sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 7 if onroad else 10)
         sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 2 if onroad else 3)
 
