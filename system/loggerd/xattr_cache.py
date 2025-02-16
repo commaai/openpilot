0fdc7ff8a6bed7204b7be5046219dd2e736aece6
BrainLessPea <brainlesspea@gmail.com>
2025-02-16 11:33:45 +0530

    loggerd: switched to xatter module

--- system/loggerd/xattr_cache.py
+++ system/loggerd/xattr_cache.py
@@ -1,20 +1,13 @@
 import errno
 import sys
-if sys.platform == 'linux':
-  import os
-else:
-  import xattr
-
+import xattr
 _cached_attributes: dict[tuple, bytes | None] = {}
 
 def getxattr(path: str, attr_name: str) -> bytes | None:
   key = (path, attr_name)
   if key not in _cached_attributes:
     try:
-      if sys.platform == 'linux':
-        response = os.getxattr(path, attr_name)
-      else:
-        response = xattr.getxattr(path, attr_name)
+      response = xattr.getxattr(path, attr_name)
     except OSError as e:
       # ENODATA means attribute hasn't been set
       if e.errno == errno.ENODATA:
@@ -26,7 +19,4 @@ def getxattr(path: str, attr_name: str) -> bytes | None:
 
 def setxattr(path: str, attr_name: str, attr_value: bytes) -> None:
   _cached_attributes.pop((path, attr_name), None)
-  if sys.platform == 'linux':
-    os.setxattr(path, attr_name, attr_value)
-  else:
-    xattr.setxattr(path, attr_name, attr_value)
+  xattr.setxattr(path, attr_name, attr_value)
