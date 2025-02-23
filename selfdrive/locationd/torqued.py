d87200738e34ca4ccfcaeab8b52f88a431fce6bb
Comma Device <device@comma.ai>
2025-02-23 14:54:51 -0800

    brand&car

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -32,7 +32,8 @@ MIN_BUCKET_POINTS = np.array([100, 300, 500, 500, 500, 500, 300, 100])
 MIN_ENGAGE_BUFFER = 2  # secs
 
 VERSION = 1  # bump this to invalidate old parameter caches
-ALLOWED_CARS = ['toyota', 'hyundai']
+ALLOWED_BRANDS = ['toyota', 'hyundai']
+ALLOWED_CARS = ['CHEVROLET_BOLT_EUV']
 
 
 def slope2rot(slope):
@@ -71,7 +72,7 @@ class TorqueEstimator(ParameterEstimator):
     self.offline_friction = 0.0
     self.offline_latAccelFactor = 0.0
     self.resets = 0.0
-    self.use_params = CP.brand in ALLOWED_CARS and CP.lateralTuning.which() == 'torque'
+    self.use_params = (CP.brand in ALLOWED_BRANDS or CP.carFingerprint in ALLOWED_CARS) and CP.lateralTuning.which() == 'torque'
 
     if CP.lateralTuning.which() == 'torque':
       self.offline_friction = CP.lateralTuning.torque.friction
