9e6238f044215980c9431d0404c58e775e570bbc
Shane Smiskol <shane@smiskol.com>
2025-03-03 16:24:42 -0800

    get ready

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -109,12 +109,11 @@ class Controls:
     actuators.accel = float(self.LoC.update(CC.longActive, CS, long_plan.aTarget, long_plan.shouldStop, pid_accel_limits))
 
     # Steering PID loop and lateral MPC
-    self.desired_curvature, was_clipped = clip_curvature(CS.vEgo, self.desired_curvature, model_v2.action.desiredCurvature, lp.roll)
-    self.steer_limited = self.steer_limited or was_clipped
+    self.desired_curvature, curvature_limited = clip_curvature(CS.vEgo, self.desired_curvature, model_v2.action.desiredCurvature, lp.roll)
     actuators.curvature = self.desired_curvature
     steer, steeringAngleDeg, lac_log = self.LaC.update(CC.latActive, CS, self.VM, lp,
                                                        self.steer_limited, self.desired_curvature,
-                                                       self.calibrated_pose)  # TODO what if not available
+                                                       self.calibrated_pose, curvature_limited)  # TODO what if not available
     actuators.torque = float(steer)
     actuators.steeringAngleDeg = float(steeringAngleDeg)
     # Ensure no NaNs/Infs
