bbc129022ba55a9bddadcefe8d04934c1a754e5e
ZwX1616 <zwx1616@gmail.com>
2025-03-04 17:35:47 -0800

    Update selfdrive/test/process_replay/model_replay.py
    Co-authored-by: Maxime Desroches <desroches.maxime@gmail.com>

--- selfdrive/test/process_replay/model_replay.py
+++ selfdrive/test/process_replay/model_replay.py
@@ -19,7 +19,7 @@ from openpilot.tools.lib.framereader import FrameReader, NumpyFrameReader
 from openpilot.tools.lib.logreader import LogReader, save_log
 from openpilot.tools.lib.github_utils import GithubUtils
 
-TEST_ROUTE = "8494c69d3c710e81/000001d4--2648a9a404"
+TEST_ROUTE = "8494c69d3c710e81|000001d4--2648a9a404"
 SEGMENT = 4
 MAX_FRAMES = 100 if PC else 400
 
