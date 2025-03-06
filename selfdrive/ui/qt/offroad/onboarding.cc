ac0daf86aec3db6d180e7127d00be4538a8477c8
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-03-06 10:44:27 -0800

    match setup style

--- selfdrive/ui/qt/offroad/onboarding.cc
+++ selfdrive/ui/qt/offroad/onboarding.cc
@@ -83,27 +83,25 @@ void TrainingGuide::paintEvent(QPaintEvent *event) {
 }
 
 void TermsPage::showEvent(QShowEvent *event) {
-  // late init, building QML widget takes 200ms
-  if (layout()) {
-    return;
-  }
-
   QVBoxLayout *main_layout = new QVBoxLayout(this);
   main_layout->setContentsMargins(45, 35, 45, 45);
   main_layout->setSpacing(0);
 
-  QLabel *title = new QLabel(tr("Terms & Conditions"));
-  title->setStyleSheet("font-size: 90px; font-weight: 600;");
-  main_layout->addWidget(title);
+  QVBoxLayout *vlayout = new QVBoxLayout();
+  vlayout->setContentsMargins(165, 165, 165, 0);
+  main_layout->addLayout(vlayout);
 
-  QLabel *text = new QLabel(this);
-  text->setTextFormat(Qt::RichText);
-  text->setWordWrap(true);
-  text->setText(tr("View the Terms & Conditions at <a href='https://comma.ai/terms' style='color: #465BEA;'>https://comma.ai/terms</a>"));
-  text->setStyleSheet("font-size:50px; font-weight: 200; color: #C9C9C9; background-color:#1B1B1B; padding:50px 50px;");
-  text->setOpenExternalLinks(true);
-  main_layout->addWidget(text);
-  main_layout->addStretch();
+  QLabel *title = new QLabel(tr("Welcome to openpilot"));
+  title->setStyleSheet("font-size: 90px; font-weight: 500;");
+  vlayout->addWidget(title, 0, Qt::AlignTop | Qt::AlignLeft);
+
+  vlayout->addSpacing(90);
+  QLabel *desc = new QLabel(tr("You must accept the Terms and Conditions to use openpilot. Read the latest terms at <span style='color: #465BEA;'>https://comma.ai/terms</span> before continuing."));
+  desc->setWordWrap(true);
+  desc->setStyleSheet("font-size: 80px; font-weight: 300;");
+  vlayout->addWidget(desc, 0);
+
+  vlayout->addStretch();
 
   QHBoxLayout* buttons = new QHBoxLayout;
   buttons->setMargin(0);
