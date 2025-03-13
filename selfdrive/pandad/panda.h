f08dedcab1101460096c59fb4495d2e478178687
deanlee <deanlee3@gmail.com>
2025-03-13 18:23:06 +0800

    forward debug logs to cloudlog

--- selfdrive/pandad/panda.h
+++ selfdrive/pandad/panda.h
@@ -64,6 +64,8 @@ public:
   cereal::PandaState::PandaType get_hw_type();
   void set_safety_model(cereal::CarParams::SafetyModel safety_model, uint16_t safety_param=0U);
   void set_alternative_experience(uint16_t alternative_experience);
+  std::string serial_read(int port_number = 0);
+  void set_uart_baud(int uart, int rate);
   void set_fan_speed(uint16_t fan_speed);
   uint16_t get_fan_speed();
   void set_ir_pwr(uint16_t ir_pwr);
