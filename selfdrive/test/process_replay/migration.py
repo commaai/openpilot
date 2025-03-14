3f88c19f856302f1470b3077ac8fcbc3ca2fb916
Kacper Rączy <gfw.kra@gmail.com>
2025-03-14 15:16:44 -0700

    Fix again

--- selfdrive/test/process_replay/migration.py
+++ selfdrive/test/process_replay/migration.py
@@ -241,13 +241,16 @@ def migrate_gpsLocation(msgs):
 
 @migration(inputs=["deviceState", "initData"])
 def migrate_deviceState(msgs):
+  init_data = next((m.initData for _, m in msgs if m.which() == 'initData'), None)
+  device_state = next((m.deviceState for _, m in msgs if m.which() == 'deviceState'), None)
+  if init_data is None or device_state is None:
+    return [], [], []
+
   ops = []
-  dt = next((m.initData.deviceType for _, m in msgs if m.which() == 'initData'), None)
-  assert dt is not None, "initData message not found"
   for i, msg in msgs:
     if msg.which() == 'deviceState':
       n = msg.as_builder()
-      n.deviceState.deviceType = dt
+      n.deviceState.deviceType = init_data.deviceType
       ops.append((i, n.as_reader()))
   return ops, [], []
 
