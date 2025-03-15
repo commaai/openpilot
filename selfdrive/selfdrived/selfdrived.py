19c3d64bc87c8bb85a5df39e029250f436ce9536
Shane Smiskol <shane@smiskol.com>
2025-03-14 19:17:48 -0700

    always check radarState freq since it polls on modelV2 and sends at a fixed rate

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -80,7 +80,7 @@ class SelfdriveD:
                                    'managerState', 'liveParameters', 'radarState', 'liveTorqueParameters',
                                    'controlsState', 'carControl', 'driverAssistance', 'alertDebug'] + \
                                    self.camera_packets + self.sensor_packets + self.gps_packets,
-                                  ignore_alive=ignore, ignore_avg_freq=ignore+['radarState',],
+                                  ignore_alive=ignore, ignore_avg_freq=ignore,
                                   ignore_valid=ignore, frequency=int(1/DT_CTRL))
 
     # read params
