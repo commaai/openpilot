842c1525740554678a55b8ecae0672ae9b19dddd
Shane Smiskol <shane@smiskol.com>
2025-03-14 17:17:52 -0700

    already set valid from this

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -231,7 +231,8 @@ class RadarD:
       self.tracks[ids].update(rpt[0], rpt[1], rpt[2], v_lead, rpt[3])
 
     # *** publish radarState ***
-    self.radar_state_valid = sm.all_checks() and len(rr.errors) == 0
+    self.radar_state_valid = sm.all_checks()
+    print(sm.all_checks(), len(rr.errors))
     self.radar_state = log.RadarState.new_message()
     self.radar_state.mdMonoTime = sm.logMonoTime['modelV2']
     self.radar_state.radarErrors = list(rr.errors)
