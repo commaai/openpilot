b95140666bdb3376b35d39587db2411d4e3c4675
Jason Wen <haibin.wen3@gmail.com>
2025-03-17 00:20:13 -0400

    ci: remove sunnylink pairing QR code from UI preview (#673)

--- selfdrive/ui/tests/test_ui/run.py
+++ selfdrive/ui/tests/test_ui/run.py
@@ -212,11 +212,6 @@ def setup_settings_sunnylink_sponsor_button(click, pm: PubMaster, scroll=None):
   click(1967, 225)
   time.sleep(UI_DELAY)
 
-def setup_settings_sunnylink_pair_button(click, pm: PubMaster, scroll=None):
-  setup_settings_sunnylink(click, pm)
-  click(1967, 432)
-  time.sleep(UI_DELAY)
-
 def setup_settings_sunnypilot(click, pm: PubMaster, scroll=None):
   setup_settings_device(click, pm)
   click(278, 852)
@@ -279,7 +274,6 @@ CASES = {
 CASES.update({
   "settings_sunnylink": setup_settings_sunnylink,
   "settings_sunnylink_sponsor_button": setup_settings_sunnylink_sponsor_button,
-  "settings_sunnylink_pair_button": setup_settings_sunnylink_pair_button,
   "settings_sunnypilot": setup_settings_sunnypilot,
   "settings_sunnypilot_mads": setup_settings_sunnypilot_mads,
   "settings_trips": setup_settings_trips,
