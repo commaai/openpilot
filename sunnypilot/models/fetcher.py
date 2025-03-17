4a04819d964ec6360e2e1fc80c614fe4b8bdf01d
Jason Wen <haibin.wen3@gmail.com>
2025-03-17 00:36:32 -0400

    Redirect license to LICENSE.md (#672)
    * Redirect license to LICENSE.md
    
    * bump

--- sunnypilot/models/fetcher.py
+++ sunnypilot/models/fetcher.py
@@ -1,7 +1,9 @@
-# Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
-#
-# This file is part of sunnypilot and is licensed under the MIT License.
-# See the LICENSE.md file in the root directory for more details.
+"""
+Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+
+This file is part of sunnypilot and is licensed under the MIT License.
+See the LICENSE.md file in the root directory for more details.
+"""
 
 import json
 import time
