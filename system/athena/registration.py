122031288e185affa53d0fa85f79f209580385c1
deanlee <deanlee3@gmail.com>
2025-03-20 12:31:37 +0800

    remove wrappers

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -2,15 +2,18 @@
 import time
 import json
 import jwt
+import pyray as rl
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 
 from datetime import datetime, timedelta, UTC
 from openpilot.common.api import api_get
 from openpilot.common.params import Params
-from openpilot.common.spinner import Spinner
 from openpilot.selfdrive.selfdrived.alertmanager import set_offroad_alert
 from openpilot.system.hardware import HARDWARE, PC
 from openpilot.system.hardware.hw import Paths
+from openpilot.system.ui.lib.application import gui_app
+from openpilot.system.ui.spinner import Spinner
 from openpilot.common.swaglog import cloudlog
 
 
@@ -20,20 +23,8 @@ def is_registered_device() -> bool:
   dongle = Params().get("DongleId", encoding='utf-8')
   return dongle not in (None, UNREGISTERED_DONGLE_ID)
 
-
-def register(show_spinner=False) -> str | None:
-  """
-  All devices built since March 2024 come with all
-  info stored in /persist/. This is kept around
-  only for devices built before then.
-
-  With a backend update to take serial number instead
-  of dongle ID to some endpoints, this can be removed
-  entirely.
-  """
-  params = Params()
-
-  dongle_id: str | None = params.get("DongleId", encoding='utf8')
+def _get_dongle_id() -> str | None:
+  dongle_id: str | None = Params().get("DongleId", encoding='utf8')
   if dongle_id is None and Path(Paths.persist_root()+"/comma/dongle_id").is_file():
     # not all devices will have this; added early in comma 3X production (2/28/24)
     with open(Paths.persist_root()+"/comma/dongle_id") as f:
@@ -43,11 +34,19 @@ def register(show_spinner=False) -> str | None:
   if not pubkey.is_file():
     dongle_id = UNREGISTERED_DONGLE_ID
     cloudlog.warning(f"missing public key: {pubkey}")
-  elif dongle_id is None:
-    if show_spinner:
-      spinner = Spinner()
-      spinner.update("registering device")
 
+  return dongle_id
+
+def do_register(spinner = None, done_event=None) -> str | None:
+    """
+    All devices built since March 2024 come with all
+    info stored in /persist/. This is kept around
+    only for devices built before then.
+
+    With a backend update to take serial number instead
+    of dongle ID to some endpoints, this can be removed
+    entirely.
+    """
     # Create registration token, in the future, this key will make JWTs directly
     with open(Paths.persist_root()+"/comma/id_rsa.pub") as f1, open(Paths.persist_root()+"/comma/id_rsa") as f2:
       public_key = f1.read()
@@ -65,9 +64,10 @@ def register(show_spinner=False) -> str | None:
         cloudlog.exception("Error getting imei, trying again...")
         time.sleep(1)
 
-      if time.monotonic() - start_time > 60 and show_spinner:
-        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
+      if time.monotonic() - start_time > 60 and spinner:
+        spinner.set_text(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
 
+    dongle_id = None
     backoff = 0
     start_time = time.monotonic()
     while True:
@@ -89,14 +89,32 @@ def register(show_spinner=False) -> str | None:
         backoff = min(backoff + 1, 15)
         time.sleep(backoff)
 
-      if time.monotonic() - start_time > 60 and show_spinner:
-        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
+      if time.monotonic() - start_time > 60 and spinner:
+        spinner.set_text(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
 
+    return dongle_id
+
+def register(show_spinner=False) -> str | None:
+  dongle_id = _get_dongle_id()
+  if not dongle_id:
     if show_spinner:
-      spinner.close()
+      with ThreadPoolExecutor(max_workers=1) as executor:
+        gui_app.init_window("Register")
+        spinner = Spinner()
+        spinner.set_text("registering device")
+        future = executor.submit(do_register, spinner)
+        while not future.done():
+          rl.begin_drawing()
+          rl.clear_background(rl.BLACK)
+          spinner.render()
+          rl.end_drawing()
+        gui_app.close()
+        dongle_id = future.result()
+    else:
+      dongle_id = do_register()
 
   if dongle_id:
-    params.put("DongleId", dongle_id)
+    Params().put("DongleId", dongle_id)
     set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
   return dongle_id
 
--- system/manager/manager.py
+++ system/manager/manager.py
@@ -9,7 +9,6 @@ from cereal import log
 import cereal.messaging as messaging
 import openpilot.system.sentry as sentry
 from openpilot.common.params import Params, ParamKeyType
-from openpilot.common.text_window import TextWindow
 from openpilot.system.hardware import HARDWARE
 from openpilot.system.manager.helpers import unblock_stdout, write_onroad_params, save_bootlog
 from openpilot.system.manager.process import ensure_running
@@ -18,6 +17,7 @@ from openpilot.system.athena.registration import register, UNREGISTERED_DONGLE_I
 from openpilot.common.swaglog import cloudlog, add_file_handler
 from openpilot.system.version import get_build_metadata, terms_version, training_version
 from openpilot.system.hardware.hw import Paths
+from openpilot.system.ui.text import show_text_in_window
 
 
 def manager_init() -> None:
@@ -221,8 +221,7 @@ if __name__ == "__main__":
     # Show last 3 lines of traceback
     error = traceback.format_exc(-3)
     error = "Manager failed to start\n\n" + error
-    with TextWindow(error) as t:
-      t.wait_for_exit()
+    show_text_in_window(error)
 
     raise
 
