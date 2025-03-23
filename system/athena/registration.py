e6177c49aa08667e5c5450314244629b625f2b4f
Jason Wen <haibin.wen3@gmail.com>
2025-03-22 22:48:58 -0400

    athenad: avoid infinite loop waiting for comma registration (#701)
    Athenad: avoid infinite loop waiting for comma registration

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -91,6 +91,7 @@ def register(show_spinner=False) -> str | None:
 
       if time.monotonic() - start_time > 60 and show_spinner:
         spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
+        return UNREGISTERED_DONGLE_ID  # hotfix to prevent an infinite wait for registration
 
     if show_spinner:
       spinner.close()
