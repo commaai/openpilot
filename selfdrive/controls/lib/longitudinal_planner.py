ae296df5de0aa7c34868ae5878e487fae6fd76ba
Bruce Wayne <harald.the.engineer@gmail.com>
2025-03-26 11:33:06 -0700

    typo

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -179,8 +179,8 @@ class LongitudinalPlanner:
     action_t =  self.CP.longitudinalActuatorDelay + DT_MDL
     output_a_target_mpc, output_should_stop_mpc = get_accel_from_plan(self.v_desired_trajectory, self.a_desired_trajectory,
                                                                         action_t=action_t, vEgoStopping=self.CP.vEgoStopping)
-    output_a_target_e2e, output_should_stop_e2e = get_accel_from_plan(np.interp(CONTROL_N_T_IDX, ModelConstants.T_IDXS, model_msg.velocity.x),
-                                                                      np.interp(CONTROL_N_T_IDX, ModelConstants.T_IDXS, model_msg.acceleration.x),
+    output_a_target_e2e, output_should_stop_e2e = get_accel_from_plan(np.interp(CONTROL_N_T_IDX, ModelConstants.T_IDXS, sm['modelV2'].velocity.x),
+                                                                      np.interp(CONTROL_N_T_IDX, ModelConstants.T_IDXS, sm['modelV2'].acceleration.x),
                                                                       action_t=action_t, vEgoStopping=self.CP.vEgoStopping)
     if self.mode == 'acc':
       output_a_target = output_a_target_mpc
