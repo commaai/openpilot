3615b1a20371c8e074f591030fb56c770db7c08c
Jason Wen <haibin.wen3@gmail.com>
2025-04-14 14:50:21 -0400

    DM: Enforce monitoring of MADS state in driver monitoring (#818)
    * DM: Enforce monitoring of MADS state in driver monitoring
    
    Added `selfdriveStateSP.mads.enabled` to track MADS engagement. This ensures monitoring logic accounts for MADS alongside the existing selfdrive state checks, improving feature integration and event handling.
    
    * how

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -403,13 +403,13 @@ class DriverMonitoring:
       driver_state=sm['driverStateV2'],
       cal_rpy=sm['liveCalibration'].rpyCalib,
       car_speed=sm['carState'].vEgo,
-      op_engaged=sm['selfdriveState'].enabled
+      op_engaged=sm['selfdriveState'].enabled or sm['selfdriveStateSP'].mads.enabled
     )
 
     # Update distraction events
     self._update_events(
       driver_engaged=sm['carState'].steeringPressed or sm['carState'].gasPressed,
-      op_engaged=sm['selfdriveState'].enabled,
+      op_engaged=sm['selfdriveState'].enabled or sm['selfdriveStateSP'].mads.enabled,
       standstill=sm['carState'].standstill,
       wrong_gear=sm['carState'].gearShifter in [car.CarState.GearShifter.reverse, car.CarState.GearShifter.park],
       car_speed=sm['carState'].vEgo
