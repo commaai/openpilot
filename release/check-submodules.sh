d18ec24d4d0aaa0de5336072609fce6149c752cf
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-25 14:15:58 -0400

    test can ignition experiment 1: Check every message (and check positive case for CAN-ignition models)

--- release/check-submodules.sh
+++ release/check-submodules.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
+set -x
 
 while read hash submodule ref; do
   if [ "$submodule" = "tinygrad_repo" ]; then
     echo "Skipping $submodule"
+    continue
+  fi
+
+  if [ "$submodule" = "opendbc_repo" ]; then
+    git -C $submodule fetch origin e932b98abd16554246012c79007657d8903805bd
+    git -C $submodule checkout FETCH_HEAD
+    cat opendbc_repo/opendbc/safety/tests/libsafety/safety_helpers.py | grep ignition
+    ls -la opendbc
+    echo "$submodule ok (custom Aubrey version)"
+    continue
+  fi
+
+  if [ "$submodule" = "panda" ]; then
+    git -C $submodule fetch origin c66e9a46349d152199a4a0997f82f0bea3c9b5a6
+    git -C $submodule checkout FETCH_HEAD
+    echo "$submodule ok (custom Aubrey version)"
     continue
   fi
 
