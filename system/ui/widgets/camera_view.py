77b137914c89ab4665f9b12e067f5f0ca7d142b6
Cameron Clough <cameronjclough@gmail.com>
2025-05-02 16:09:06 +0100

    fixes

--- system/ui/widgets/camera_view.py
+++ system/ui/widgets/camera_view.py
@@ -1,19 +1,18 @@
 import threading
 import time
 from collections import deque
-from ctypes import c_int
 
 import numpy as np
 import raylib as rl
 from raylib import ffi
-from raylib.enums import PixelFormat
 from raylib.defines import RL_FLOAT
+from raylib.enums import PixelFormat
 
 from msgq.visionipc import VisionIpcClient, VisionStreamType, VisionBuf
 from openpilot.common.swaglog import cloudlog
 from openpilot.system.hardware import APPLE, TICI
-from openpilot.system.ui.lib.application import gui_app
 from openpilot.system.ui.lib import gl
+from openpilot.system.ui.lib.application import gui_app
 
 
 VERTEX_SHADER = f"""{'#version 330 core' if APPLE else '#version 300 es'}
@@ -59,19 +58,6 @@ FRAGMENT_SHADER = QCOM2_FRAGMENT_SHADER if TICI else PC_FRAGMENT_SHADER
 FRAME_BUFFER_SIZE = 5
 
 
-class CheckGL:
-  def __init__(self, name: str):
-    self.name = name
-
-  def __enter__(self):
-    rl.rlCheckErrors()
-    print("\n>> ", self.name)
-
-  def __exit__(self, exc_type, exc_val, exc_tb):
-    rl.rlCheckErrors()
-    print("<< ", self.name)
-
-
 class CameraView:
   def __init__(self, stream_name: str, stream_type: VisionStreamType):
     self.stream_name = stream_name
@@ -86,11 +72,11 @@ class CameraView:
     self.stream_height = 0
     self.stream_stride = 0
 
-    self.program: rl.Shader | None = None
-    self.vao: int | None = None
-    self.vbo: int | None = None
-    self.ibo: int | None = None
-    self.textures: list[gl.GLuint] | None = None  # Y and UV textures
+    self._shader: rl.Shader | None = None
+    self._vao: int = 0
+    self._vbo: int = 0
+    self._ebo: int = 0
+    self._textures: list[int] | None = None  # Y and UV textures
 
     self.vipc_client: VisionIpcClient | None = None
     self.vipc_thread = threading.Thread(target=self._vipc_thread_func, daemon=True)
@@ -101,48 +87,33 @@ class CameraView:
     self.vipc_thread.start()
 
   def _initialize_gl(self) -> None:
-    self.program = rl.LoadShaderFromMemory(VERTEX_SHADER.encode("utf-8"), FRAGMENT_SHADER.encode("utf-8"))
-    if not self.program:
-      raise RuntimeError("Failed to load shader program")
+    self._shader = rl.LoadShaderFromMemory(VERTEX_SHADER.encode("utf-8"), FRAGMENT_SHADER.encode("utf-8"))
 
     if self.requested_stream_type == VisionStreamType.VISION_STREAM_DRIVER:
       x1, x2, y1, y2 = 0.0, 1.0, 1.0, 0.0
     else:
       x1, x2, y1, y2 = 1.0, 0.0, 1.0, 0.0
 
-    self.vao = rl.rlLoadVertexArray()
-    rl.rlEnableVertexArray(self.vao)
-
-    vertices = np.array([
+    frame_coords = np.array([
       -1.0, -1.0, x2, y1,
       -1.0,  1.0, x2, y2,
        1.0,  1.0, x1, y2,
        1.0, -1.0, x1, y1,
     ], dtype=np.float32)  # fmt: skip
-    vertices_ptr = ffi.cast("float *", ffi.from_buffer(vertices))
-    self.vbo = rl.rlLoadVertexBuffer(vertices_ptr, vertices.nbytes, False)
-    rl.rlEnableVertexBuffer(self.vbo)
+    frame_indices = np.array([0, 2, 1, 0, 3, 2], dtype=np.uint16)
 
-    indices = np.array([0, 1, 2, 0, 2, 3], dtype=np.uint16)
-    indices_ptr = ffi.cast("short *", ffi.from_buffer(indices))
-    self.ibo = rl.rlLoadVertexBufferElement(indices_ptr, indices.nbytes, False)
-    rl.rlEnableVertexBufferElement(self.ibo)
+    self._vao = rl.rlLoadVertexArray()
+    rl.rlEnableVertexArray(self._vao)
+    self._vbo = rl.rlLoadVertexBuffer(ffi.cast("void *", frame_coords.ctypes.data), frame_coords.nbytes, False)
+    rl.rlEnableVertexAttribute(0)
+    rl.rlSetVertexAttribute(0, 2, RL_FLOAT, False, 4 * 4, 0)
+    rl.rlEnableVertexAttribute(1)
+    rl.rlSetVertexAttribute(1, 2, RL_FLOAT, False, 4 * 4, 2 * 4)
+    self._ebo = rl.rlLoadVertexBufferElement(ffi.cast("void *", frame_indices.ctypes.data), frame_indices.nbytes, False)
 
-    position_loc = rl.rlGetLocationAttrib(self.program.id, b"aPosition")
-    rl.rlEnableVertexAttribute(position_loc)
-    rl.rlSetVertexAttribute(position_loc, 2, RL_FLOAT, False, 4 * 4, 0)
-
-    tex_coord_loc = rl.rlGetLocationAttrib(self.program.id, b"aTexCoord")
-    rl.rlEnableVertexAttribute(tex_coord_loc)
-    rl.rlSetVertexAttribute(tex_coord_loc, 2, RL_FLOAT, False, 4 * 4, 2 * 4)
-
-    rl.rlDisableVertexArray()  # glBindVertexArray(0)
-
-    with CheckGL("create textures and update uniforms"):
-      rl.rlEnableShader(self.program.id)  # glUseProgram(id)
-      self.textures = gl.glGenTextures(2)
-      gl.glUniform1i(rl.GetShaderLocation(self.program, b"uTextureY"), 0)
-      gl.glUniform1i(rl.GetShaderLocation(self.program, b"uTextureUV"), 1)
+    rl.rlEnableShader(self._shader.id)
+    gl.glUniform1i(rl.rlGetLocationUniform(self._shader.id, b"uTextureY"), 0)
+    gl.glUniform1i(rl.rlGetLocationUniform(self._shader.id, b"uTextureUV"), 1)
 
   def _start_vipc_thread(self) -> None:
     self.vipc_thread_stop_event.clear()
@@ -184,30 +155,26 @@ class CameraView:
     self.stream_height = self.vipc_client.height or 0
     self.stream_stride = self.vipc_client.stride or 0
 
-    with CheckGL("_on_vipc_connected load textures"):
-      gl.glBindTexture(gl.GL_TEXTURE_2D, self.textures[0])
-      gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-      gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-      gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
-      gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
-      gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_R8, self.stream_width, self.stream_height, 0, gl.GL_RED, gl.GL_UNSIGNED_BYTE, 0)
+    if self._textures is not None:
+      for tex_id in self._textures:
+        rl.rlUnloadTexture(tex_id)
 
-      gl.glBindTexture(gl.GL_TEXTURE_2D, self.textures[1])
-      gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-      gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-      gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
-      gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
-      gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RG8, self.stream_width // 2, self.stream_height // 2, 0, gl.GL_RG, gl.GL_UNSIGNED_BYTE, 0)
+    self._textures = [
+      rl.rlLoadTexture(ffi.NULL, self.stream_width, self.stream_height, PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAYSCALE, 1),
+      rl.rlLoadTexture(ffi.NULL, self.stream_width // 2, self.stream_height // 2, PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAY_ALPHA, 0),
+    ]
 
+    # FIXME: why is this required
+    gl.glBindTexture(gl.GL_TEXTURE_2D, self._textures[1])
+    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RG8, self.stream_width // 2, self.stream_height // 2, 0, gl.GL_RG, gl.GL_UNSIGNED_BYTE, None)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
 
   def render(self) -> None:
-    rl.ClearBackground(rl.BLACK)
-
     if self.vipc_connected_event.is_set():
       self.vipc_connected_event.clear()
       self._on_vipc_connected()
 
-    if self.program is None or self.textures is None or self.vao is None or self.vbo is None or self.ibo is None:
+    if self._shader is None or self._textures is None or self._vao <= 0:
       return
 
     with self.frame_lock:
@@ -215,47 +182,29 @@ class CameraView:
         return
       frame_id, frame = self.frames[-1]
 
-    if self.prev_frame_id is not None:
-      if frame_id == self.prev_frame_id:
-        cloudlog.debug(f"Drawing same frame twice {frame_id}")
-      elif frame_id != self.prev_frame_id + 1:
-        cloudlog.debug(f"Skipped frame {frame_id}")
-    self.prev_frame_id = frame_id
+      if self.prev_frame_id is not None:
+        if frame_id == self.prev_frame_id:
+          cloudlog.debug(f"Drawing same frame twice {frame_id}")
+        elif frame_id != self.prev_frame_id + 1:
+          cloudlog.debug(f"Skipped frame {frame_id}")
+      self.prev_frame_id = frame_id
 
-    rl.rlViewport(0, 0, gui_app.width, gui_app.height)
-    rl.rlEnableVertexArray(self.vao)  # glBindVertexArray(vao)
-    rl.rlEnableShader(self.program.id)  # glUseProgram(id)
+      gl.glPixelStorei(gl.GL_UNPACK_ROW_LENGTH, self.stream_stride)
+      rl.rlActiveTextureSlot(0)
+      rl.rlUpdateTexture(self._textures[0], 0, 0, self.stream_width, self.stream_height,
+                         PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAYSCALE, ffi.cast("void *", frame.y.ctypes.data))
 
-    with CheckGL("update textures"):
-      buf_data = frame.data
-      y_ptr = ffi.cast("unsigned char *", ffi.from_buffer(buf_data))
-      uv_ptr = ffi.cast("unsigned char *", ffi.from_buffer(buf_data[frame.uv_offset:]))
+      gl.glPixelStorei(gl.GL_UNPACK_ROW_LENGTH, self.stream_stride // 2)
+      rl.rlActiveTextureSlot(1)
+      rl.rlUpdateTexture(self._textures[1], 0, 0, self.stream_width // 2, self.stream_height // 2,
+                         PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAY_ALPHA, ffi.cast("void *", frame.uv.ctypes.data))
 
-      gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, c_int(1))  # glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-      gl.glPixelStorei(gl.GL_UNPACK_ROW_LENGTH, c_int(self.stream_stride))  # glPixelStorei(GL_UNPACK_ROW_LENGTH, 2048)
-      rl.rlActiveTextureSlot(0)  # glActiveTexture(GL_TEXTURE0)
-      rl.rlUpdateTexture(self.textures[0], 0, 0, self.stream_width, self.stream_height,
-                         PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAYSCALE, y_ptr)  # glBindTexture(GL_TEXTURE_2D, id); glTexSubImage2D
-      gl.glPixelStorei(gl.GL_UNPACK_ROW_LENGTH, c_int(self.stream_stride // 2))  # glPixelStorei(GL_UNPACK_ROW_LENGTH, 1024)
-      rl.rlActiveTextureSlot(1)  # glActiveTexture(GL_TEXTURE1)
-      rl.rlUpdateTexture(self.textures[1], 0, 0, self.stream_width // 2, self.stream_height // 2,
-                         PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAY_ALPHA, uv_ptr)  # glBindTexture(GL_TEXTURE_2D, id); glTexSubImage2D
+      rl.rlEnableShader(self._shader.id)
+      rl.rlEnableVertexArray(self._vao)
 
-    # FIXME: add glUniformMatrix4fv
+      # FIXME: add glUniformMatrix4fv
 
-    rl.rlEnableVertexAttribute(0)  # glEnableVertexAttribArray(0)
-
-    with CheckGL("draw elements"):
       rl.rlDrawVertexArrayElements(0, 6, ffi.NULL)
-
-    rl.rlDisableVertexAttribute(0)  # glDisableVertexAttribArray(0)
-    rl.rlDisableVertexArray()  # glBindVertexArray(0)
-
-    # rl.rlDisableTexture()  # glDisable(GL_TEXTURE_2D); glBindTexture(GL_TEXTURE_2D, 0)
-    rl.rlEnableTexture(0)  # glEnable(GL_TEXTURE_2D); glBindTexture(GL_TEXTURE_2D, 0)
-    rl.rlActiveTextureSlot(0)  # glActiveTexture(GL_TEXTURE0)
-    gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, c_int(4))
-    gl.glPixelStorei(gl.GL_UNPACK_ROW_LENGTH, c_int(0))
 
   def close(self) -> None:
     self.vipc_thread_stop_event.set()
@@ -264,32 +213,33 @@ class CameraView:
       if self.vipc_thread.is_alive():
         print(f"[{self.active_stream_type}] Thread did not exit cleanly")
 
-    if self.program:
-      rl.UnloadShader(self.program)
-      self.program = None
+    if self._shader:
+      rl.UnloadShader(self._shader)
+      self._shader = None
 
-    if self.vao:
-      rl.rlUnloadVertexArray(self.vao)
-      self.vao = None
+    if self._vao > 0:
+      rl.rlUnloadVertexArray(self._vao)
+      self._vao = 0
 
-    if self.vbo:
-      rl.rlUnloadVertexBuffer(self.vbo)
-      self.vbo = None
+    if self._vbo > 0:
+      rl.rlUnloadVertexBuffer(self._vbo)
+      self._vbo = 0
 
-    if self.ibo:
-      rl.rlUnloadVertexBuffer(self.ibo)
-      self.ibo = None
+    if self._ebo > 0:
+      rl.rlUnloadVertexBuffer(self._ebo)
+      self._ebo = 0
 
-    if self.textures:
-      for tex_id in self.textures:
+    if self._textures:
+      for tex_id in self._textures:
         rl.rlUnloadTexture(tex_id)
-      self.textures = None
+      self._textures = None
 
 
 def run():
   gui_app.init_window("watch3")
   time.sleep(1)
   road_camera_view = CameraView("camerad", VisionStreamType.VISION_STREAM_ROAD)
+  # rl.rlDisableBackfaceCulling()
   for _ in gui_app.render():
     road_camera_view.render()
   road_camera_view.close()
