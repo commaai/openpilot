aa1b7907086b5c1fe5e7b07c92e5aa6090a40c23
Maxime Desroches <desroches.maxime@gmail.com>
2025-05-06 21:38:50 -0700

    revert agnos 12

--- system/loggerd/xattr_cache.py
+++ system/loggerd/xattr_cache.py
@@ -1,6 +1,5 @@
+import os
 import errno
-
-import xattr
 
 _cached_attributes: dict[tuple, bytes | None] = {}
 
@@ -8,10 +7,10 @@ def getxattr(path: str, attr_name: str) -> bytes | None:
   key = (path, attr_name)
   if key not in _cached_attributes:
     try:
-      response = xattr.getxattr(path, attr_name)
+      response = os.getxattr(path, attr_name)
     except OSError as e:
-      # ENODATA (Linux) or ENOATTR (macOS) means attribute hasn't been set
-      if e.errno == errno.ENODATA or (hasattr(errno, 'ENOATTR') and e.errno == errno.ENOATTR):
+      # ENODATA means attribute hasn't been set
+      if e.errno == errno.ENODATA:
         response = None
       else:
         raise
@@ -20,4 +19,4 @@ def getxattr(path: str, attr_name: str) -> bytes | None:
 
 def setxattr(path: str, attr_name: str, attr_value: bytes) -> None:
   _cached_attributes.pop((path, attr_name), None)
-  xattr.setxattr(path, attr_name, attr_value)
+  return os.setxattr(path, attr_name, attr_value)
