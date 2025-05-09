a440ee22b424fb4af3d2280cfbedc47f164700b1
Jenffy1990 <JENFFY@GMAIL.COM>
2025-05-09 11:26:33 -0400

    Update params.py

--- common/params.py
+++ common/params.py
@@ -3,23 +3,23 @@ assert Params
 assert ParamKeyType
 assert UnknownKeyName
 
-if __name__ == "__main__":
-  import sys
-
-  params = Params()
-  # Register new "LateralOnlyMode" boolean parameter (default: False)
-  try:
+# Register the LateralOnlyMode parameter at import time
+params = Params()
+try:
     params.add_bool("LateralOnlyMode", False)
-  except AttributeError:
-    # If add_bool isn't available in this Params implementation, skip registration
+except AttributeError:
+    # If add_bool isn't available, ignore and continue
     pass
 
-  key = sys.argv[1]
-  assert params.check_key(key), f"unknown param: {key}"
+if __name__ == "__main__":
+    import sys
 
-  if len(sys.argv) == 3:
-    val = sys.argv[2]
-    print(f"SET: {key} = {val}")
-    params.put(key, val)
-  elif len(sys.argv) == 2:
-    print(f"GET: {key} = {params.get(key)}")
+    key = sys.argv[1]
+    assert params.check_key(key), f"unknown param: {key}"
+
+    if len(sys.argv) == 3:
+        val = sys.argv[2]
+        print(f"SET: {key} = {val}")
+        params.put(key, val)
+    elif len(sys.argv) == 2:
+        print(f"GET: {key} = {params.get(key)}")
