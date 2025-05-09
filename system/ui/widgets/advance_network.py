45041b6ef57976e5e65f1b9b3d0d7c6ebdb93997
deanlee <deanlee3@gmail.com>
2025-05-09 12:50:46 +0800

    display ip address

--- system/ui/widgets/advance_network.py
+++ system/ui/widgets/advance_network.py
@@ -17,7 +17,8 @@ LINE_COLOR = rl.GRAY
 
 
 class AdvanceNetwork:
-  def __init__(self):
+  def __init__(self, wifi_manager: WifiManagerWrapper):
+    self._wifi_manager = wifi_manager
     self._scroll_panel = None
     self._tethering_toggle = Toggle(0, 0, initial_state=True)
     self._roaming_toggle = Toggle(0, 0)
@@ -34,7 +35,7 @@ class AdvanceNetwork:
     ]
 
   def _get_ip_address(self):
-    pass
+    return self._wifi_manager.ip_address
 
   def on_tethering_toggle(self):
     pass
@@ -58,10 +59,10 @@ class AdvanceNetwork:
     value = item.get("value")
     value_str = value() if callable(value) else value
 
-    label_text = f"{item['label']}{f': {value_str}' if value_str else ''}"
-    gui_label(item_rect, label_text, 50)
-
-    if "toggle" in item:
+    gui_label(item_rect, item['label'], 50)
+    if value_str is not None:
+      gui_label(button_rect, str(value_str), 35, alignment=rl.GuiTextAlignment.TEXT_ALIGN_RIGHT)
+    elif "toggle" in item:
       toggle = item["toggle"]
       if toggle.render(rl.Rectangle(button_rect.x + button_rect.width - 160, button_rect.y, 160, 80)):
         if "handler" in item:
@@ -69,12 +70,7 @@ class AdvanceNetwork:
 
     elif "button" in item:
       button_text = item["button"]
-      if gui_button(
-        button_rect,
-        button_text,
-        font_size=35,
-        border_radius=50,
-      ):
+      if gui_button(button_rect, button_text, font_size=35, border_radius=50):
         if "handler" in item:
           item["handler"]()
 
@@ -86,6 +82,7 @@ class AdvanceNetwork:
 
     for idx, item in enumerate(self._items):
       self._render_item(item, item_rect, button_rect)
+
       # Draw separator line between items (except after the last one)
       if idx < len(self._items) - 1:
         y_line = item_rect.y + ITEM_HEIGHT + ITEM_SPACING / 2 - 1
@@ -99,7 +96,7 @@ if __name__ == "__main__":
   gui_app.init_window("Advance Network Example")
   wifi_manager = WifiManagerWrapper()
   wifi_ui = WifiManagerUI(wifi_manager)
-  advance_network = AdvanceNetwork()
+  advance_network = AdvanceNetwork(wifi_manager)
   for _ in gui_app.render():
     # wifi_ui.render(rl.Rectangle(50, 50, gui_app.width - 100, gui_app.height - 100))
     advance_network.render(rl.Rectangle(50, 50, 1024, 768))
