3fd05d2aceb2fc52c96efce58ae3e7c44d882c4a
Maxime Desroches <desroches.maxime@gmail.com>
2025-05-13 21:05:17 -0700

    more robust modem

--- system/hardware/tici/hardware.py
+++ system/hardware/tici/hardware.py
@@ -297,13 +297,11 @@ class Tici(HardwareBase):
       return None
 
   def get_modem_temperatures(self):
-    if self.get_device_type() == "mici":
-      return []
     timeout = 0.2  # Default timeout is too short
     try:
       modem = self.get_modem()
       temps = modem.Command("AT+QTEMP", math.ceil(timeout), dbus_interface=MM_MODEM, timeout=timeout)
-      return list(map(int, temps.split(' ')[1].split(',')))
+      return list(filter(lambda t: t != 255, map(int, temps.split(' ')[1].split(','))))
     except Exception:
       return []
 
