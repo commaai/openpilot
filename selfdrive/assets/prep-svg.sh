62f5a59f7714b7d1c44a85f3e8fd06a7ce159502
Cameron Clough <cameronjclough@gmail.com>
2025-05-16 00:52:49 +0100

    selfdrive/assets: optimize SVGs (#35242)

--- selfdrive/assets/prep-svg.sh
+++ selfdrive/assets/prep-svg.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# sudo apt install scour
-
-for svg in $(find icons/ -type f | grep svg$); do
-  # scour doesn't support overwriting input file
-  scour $svg --remove-metadata $svg.tmp
-  mv $svg.tmp $svg
+for svg in $(find icons/ images/ -type f | grep svg$); do
+  bunx svgo $svg --multipass --pretty --indent 2
 
   # convert to PNG
+  # sudo apt install inkscape
   convert -background none -resize 400% -density 384 $svg "${svg%.svg}.png"
 done
