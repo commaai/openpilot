7888ffeb8e56c7bf90449c034be3b5a39e9184c0
Trey Moen <trey@moen.ai>
2025-05-15 16:54:40 -0700

    refactors

--- release/pack-raylib.py
+++ release/pack-raylib.py
@@ -7,7 +7,10 @@ from argparse import ArgumentParser
 
 from openpilot.common.basedir import BASEDIR
 
+
 ENTRYPOINT = 'main'
+INTERPRETER = '/usr/bin/env python3'
+
 
 if __name__ == '__main__':
   parser = ArgumentParser(prog='pack-raylib.py', description='Package a raylib UI into a portable executable.', epilog='comma.ai')
@@ -25,6 +28,6 @@ if __name__ == '__main__':
     print(f'{args.module} does not have a {ENTRYPOINT}() function')
     sys.exit(1)
 
-  module_entrypoint = f'{args.module}:{ENTRYPOINT}'
+  entry = f'{args.module}:{ENTRYPOINT}'
 
-  zipapp.create_archive(BASEDIR + '/openpilot', target=args.output, interpreter='/usr/bin/env python3', main=module_entrypoint)
+  zipapp.create_archive(BASEDIR + '/openpilot', target=args.output, interpreter=INTERPRETER, main=entry)
