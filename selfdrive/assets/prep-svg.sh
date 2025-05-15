735c227f3cc668c70af5a3817b1cf063261f880a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-05-15 09:53:20 -0700

    svg -> png script

--- selfdrive/assets/prep-svg.sh
+++ selfdrive/assets/prep-svg.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 # sudo apt install scour
 
@@ -6,4 +7,7 @@ for svg in $(find icons/ -type f | grep svg$); do
   # scour doesn't support overwriting input file
   scour $svg --remove-metadata $svg.tmp
   mv $svg.tmp $svg
+
+  # convert to PNG
+  convert -background none -resize 400% -density 384 $svg "${svg%.svg}.png"
 done
