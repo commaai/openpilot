6bf3ecd6f8e1a49572f851d04cce94f97d976a26
pencilpusher <83676301+jakethesnake420@users.noreply.github.com>
2025-05-15 06:03:11 -0500

    LogReader.segments() is a generator for entire segments

--- tools/lib/logreader.py
+++ tools/lib/logreader.py
@@ -338,6 +338,12 @@ class LogReader:
     for i in range(len(self.logreader_identifiers)):
       yield from self._get_lr(i)
 
+  def segments(self):
+      for i in range(len(self.logreader_identifiers)):
+          seg = self._get_lr(i)
+          with seg: # <- ensures ram freed ASAP
+              yield seg
+
   def _run_on_segment(self, func, i):
     return func(self._get_lr(i))
 
