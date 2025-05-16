6e712e3866b73b8011463cc177488a9673c55e0e
Cameron Clough <cameronjclough@gmail.com>
2025-05-16 01:21:19 +0100

    use inkscape

--- selfdrive/assets/prep-svg.sh
+++ selfdrive/assets/prep-svg.sh
@@ -3,11 +3,21 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+# sudo apt install inkscape
+
 for svg in $(find $DIR -type f | grep svg$); do
   bunx svgo $svg --multipass --pretty --indent 2
 
   # convert to PNG
-  # sudo apt install inkscape
-  convert -background none -density 384 "$svg" -resize 512x512 "${svg%.svg}.png"
-  optipng -o7 -strip all "${svg%.svg}.png"
+  png="${svg%.svg}.png"
+  width=$(inkscape --query-width "$svg")
+  height=$(inkscape --query-height "$svg")
+  if (( $(echo "$width > $height" | bc -l) )); then
+    export_dim="--export-width=512"
+  else
+    export_dim="--export-height=512"
+  fi
+  inkscape "$svg" --export-filename="$png" $export_dim
+
+  optipng -o7 -strip all "$png"
 done
