2a22f66f03718ad69925122b19d7e6994e7980a7
Cameron Clough <cameronjclough@gmail.com>
2025-05-16 19:06:50 +0100

    selfdrive/assets: use bootstrap icons, generate keyboard icons

--- selfdrive/assets/prep-svg.sh
+++ selfdrive/assets/prep-svg.sh
@@ -2,6 +2,31 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+ICONS_DIR="$DIR/icons"
+BOOTSTRAP_SVG="$DIR/../../third_party/bootstrap/bootstrap-icons.svg"
+
+ICON_IDS=(
+  arrow-down
+  arrow-right
+  backspace
+  shift
+  shift-fill
+)
+ICON_FILL_COLOR="#fff"
+
+for id in "${ICON_IDS[@]}"; do
+  # Extract symbol and convert to SVG
+  svg="${ICONS_DIR}/${id}.svg"
+  perl -0777 -ne "print \$& if /<symbol[^>]*id=\"$id\"[^>]*>.*?<\/symbol>/s" "$BOOTSTRAP_SVG" \
+  | sed "s/<symbol/<svg fill=\"$ICON_FILL_COLOR\"/; s/<\/symbol>/<\/svg>/" > "$svg"
+
+  # convert to PNG
+  png="${ICONS_DIR}/${id}.png"
+  inkscape "$svg" --export-filename="$png" --export-width=512
+  optipng -o7 -strip all "$png"
+
+  rm "$svg"
+done
 
 # sudo apt install inkscape
 
@@ -17,7 +42,7 @@ for svg in $(find $DIR -type f | grep svg$); do
   else
     export_dim="--export-height=512"
   fi
-  inkscape "$svg" --export-filename="$png" $export_dim
+  inkscape "$svg" --export-filename="$png" "$export_dim"
 
   optipng -o7 -strip all "$png"
 done
