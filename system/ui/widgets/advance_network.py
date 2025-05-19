07a2303a95e0e6e4319ca774faab65c13ca247fa
deanlee <deanlee3@gmail.com>
2025-05-20 02:30:27 +0800

    update gsm settings

--- system/ui/widgets/advance_network.py
+++ system/ui/widgets/advance_network.py
@@ -43,7 +43,6 @@ class AdvanceNetwork:
       {"label": "Enable Roaming", "toggle": self._roaming_toggle, "handler": self.on_roaming_toggle},
       {"label": "APN Setting", "button": "EDIT", "handler": self.on_apn_edit},
       {"label": "Cellular Metered", "toggle": self._cellular_toggle, "handler": self.on_cellular_toggle},
-      # {"label": "Hidden Network", "button": "CONNECT", "handler": self.on_hidden_network_connect},
     ]
 
   def _get_ip_address(self):
@@ -53,31 +52,56 @@ class AdvanceNetwork:
     self._wifi_manager.enable_tethering(self._tethering_toggle.get_state())
 
   def on_tethering_password_edit(self):
-    # Show the current password in the dialog
+    # TODO: Show the current password in the dialog
     self._state = AdvanceNetworkState.EDIT_TETHERING_PASSWORD
     print(self._wifi_manager.get_tethering_password())
-    self._keyboard.render("Enter new tethering password", "")
-
-  def _set_tethering_password(self, password):
-    self._wifi_manager.set_tethering_password(password)
+    result = self._keyboard.render("Enter new tethering password", "")
+    if result == 1:
+      self._wifi_manager.set_tethering_password(self._keyboard.get_text())
+      self._state = AdvanceNetworkState.NONE
+    elif result == 0:
+      self._state = AdvanceNetworkState.NONE
 
   def on_roaming_toggle(self):
     pass
 
   def on_apn_edit(self):
-    # self._dialog_manager.show_input_dialog(
-    #   "Enter APN", "leave blank for automatic configuration", callback=self._set_apn
-    # )
-    pass
-
-  def _set_apn(self, apn):
-    pass
+    self._state = AdvanceNetworkState.EDIT_APN
+    print(self._wifi_manager.get_tethering_password())
+    result = self._keyboard.render("Enter APN", "leave blank for automatic configuration")
+    if result == 1:
+      pass
+    elif result == 0:
+      self._state = AdvanceNetworkState.NONE
 
   def on_cellular_toggle(self):
     pass
 
   def on_hidden_network_connect(self):
+    # TODO: Implement this method
     pass
+
+  def render(self, rect: rl.Rectangle):
+    if self._state == AdvanceNetworkState.EDIT_TETHERING_PASSWORD:
+      self.on_tethering_password_edit()
+    elif self._state == AdvanceNetworkState.EDIT_APN:
+      self.on_apn_edit()
+    else:
+      item_rect = rl.Rectangle(rect.x, rect.y, rect.width, ITEM_HEIGHT)
+      button_rect = rl.Rectangle(
+        rect.x + rect.width - BUTTON_WIDTH, rect.y + (ITEM_HEIGHT - BUTTON_HEIGHT) / 2, BUTTON_WIDTH, BUTTON_HEIGHT
+      )
+
+      for idx, item in enumerate(self._items):
+        self._render_item(item, item_rect, button_rect)
+
+        # Draw separator line between items (except after the last one)
+        if idx < len(self._items) - 1:
+          y_line = item_rect.y + ITEM_HEIGHT + ITEM_SPACING / 2 - 1
+          rl.draw_line(int(item_rect.x), int(y_line), int(item_rect.x + item_rect.width), int(y_line), LINE_COLOR)
+
+        item_rect.y += ITEM_HEIGHT + ITEM_SPACING
+        button_rect.y += ITEM_HEIGHT + ITEM_SPACING
 
   def _render_item(self, item, item_rect, button_rect):
     value = item.get("value")
@@ -96,27 +120,6 @@ class AdvanceNetwork:
       if gui_button(button_rect, button_text, font_size=35, border_radius=50):
         if "handler" in item:
           item["handler"]()
-
-  def render(self, rect: rl.Rectangle):
-    if self._state == AdvanceNetworkState.EDIT_TETHERING_PASSWORD:
-      self.on_tethering_password_edit()
-
-    else:
-      item_rect = rl.Rectangle(rect.x, rect.y, rect.width, ITEM_HEIGHT)
-      button_rect = rl.Rectangle(
-        rect.x + rect.width - BUTTON_WIDTH, rect.y + (ITEM_HEIGHT - BUTTON_HEIGHT) / 2, BUTTON_WIDTH, BUTTON_HEIGHT
-      )
-
-      for idx, item in enumerate(self._items):
-        self._render_item(item, item_rect, button_rect)
-
-        # Draw separator line between items (except after the last one)
-        if idx < len(self._items) - 1:
-          y_line = item_rect.y + ITEM_HEIGHT + ITEM_SPACING / 2 - 1
-          rl.draw_line(int(item_rect.x), int(y_line), int(item_rect.x + item_rect.width), int(y_line), LINE_COLOR)
-
-        item_rect.y += ITEM_HEIGHT + ITEM_SPACING
-        button_rect.y += ITEM_HEIGHT + ITEM_SPACING
 
 
 if __name__ == "__main__":
