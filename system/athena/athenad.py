55ccc8745f9c357a870b66b7d0a14234d9b8bcfc
Trey Moen <50057480+greatgitsby@users.noreply.github.com>
2025-05-30 11:34:24 -0700

    Update system/athena/athenad.py

--- system/athena/athenad.py
+++ system/athena/athenad.py
@@ -555,7 +555,7 @@ def downloadEsimProfile(qr: str, profile_name: str):
 
 
 @dispatcher.add_method
-def switchEsimProfile(iccid: str):
+def setEsimProfile(iccid: str):
   HARDWARE.get_sim_lpa().switch_profile(iccid)
   HARDWARE.reboot_modem()
 
