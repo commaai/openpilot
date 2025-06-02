59d8403742fd3382e8bc958b3194fe33c2345b60
deanlee <deanlee3@gmail.com>
2025-06-03 02:02:23 +0800

    handle_mouse_click

--- selfdrive/ui/ui.py
+++ selfdrive/ui/ui.py
@@ -20,18 +20,13 @@ class UI:
     self._sidebar_rect = rl.Rectangle(0, 0, SIDEBAR_WIDTH, gui_app.height)
     self._content_rect = rl.Rectangle(SIDEBAR_WIDTH, 0, gui_app.width - SIDEBAR_WIDTH, gui_app.height)
 
-  def draw(self, sm, rect):
-    # Check if sidebar should be visible
-    if rl.check_collision_point_rec(rl.get_mouse_position(), self._content_rect):
-      if rl.is_mouse_button_pressed(rl.MOUSE_BUTTON_LEFT):
-        self._sidebar_visible = not self._sidebar_visible
-        if self._sidebar_visible:
-          self._content_rect.x = SIDEBAR_WIDTH
-          self._content_rect.width  = gui_app.width - SIDEBAR_WIDTH
-        else:
-          self._content_rect.x = 0
-          self._content_rect.width = gui_app.width
+  def render(self, sm, rect):
+    self._render_main_content(sm, rect)
 
+    if rl.is_mouse_button_pressed(rl.MOUSE_BUTTON_LEFT):
+      self._handle_mouse_click(rl.get_mouse_position())
+
+  def _render_main_content(self, sm, rect):
     if self._sidebar_visible:
       self._sidbar.draw(sm, self._sidebar_rect)
 
@@ -40,6 +35,15 @@ class UI:
     else:
       self._home_layout.render(self._content_rect, sm)
 
+  def _handle_mouse_click(self, pos: rl.Vector2):
+    if rl.check_collision_point_rec(pos, self._content_rect):
+      self._sidebar_visible = not self._sidebar_visible
+      if self._sidebar_visible:
+        self._content_rect.x = SIDEBAR_WIDTH
+        self._content_rect.width = gui_app.width - SIDEBAR_WIDTH
+      else:
+        self._content_rect.x = 0
+        self._content_rect.width = gui_app.width
 
 def main():
   gui_app.init_window("UI")
@@ -66,7 +70,7 @@ def main():
   ui = UI(sm)
   for _ in gui_app.render():
     sm.update(0)
-    ui.draw(sm, rl.Rectangle(0, 0, gui_app.width, gui_app.height))
+    ui.render(sm, rl.Rectangle(0, 0, gui_app.width, gui_app.height))
 
 
 if __name__ == "__main__":
