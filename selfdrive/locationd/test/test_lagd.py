2f01dba8396ad8d217a20f4a164e08435c60dd94
Shane Smiskol <shane@smiskol.com>
2025-06-02 20:19:05 -0700

    clean up

--- selfdrive/locationd/test/test_lagd.py
+++ selfdrive/locationd/test/test_lagd.py
@@ -101,12 +101,12 @@ class TestLagd:
         mocked_CP = mocker.Mock(steerActuatorDelay=0.8)
         estimator = LateralLagEstimator(mocked_CP, DT, min_recovery_buffer_sec=0.0, min_yr=0.0)
         process_messages(estimator, lag_frames, int(MIN_OKAY_WINDOW_SEC / DT) + BLOCK_NUM_NEEDED * BLOCK_SIZE)
-        # msg = estimator.get_msg(True)
-        # assert msg.liveDelay.status == 'estimated'
-        # assert np.allclose(msg.liveDelay.lateralDelay, lag_frames * DT, atol=0.01)
-        # assert np.allclose(msg.liveDelay.lateralDelayEstimate, lag_frames * DT, atol=0.01)
-        # assert np.allclose(msg.liveDelay.lateralDelayEstimateStd, 0.0, atol=0.01)
-        # assert msg.liveDelay.validBlocks == BLOCK_NUM_NEEDED
+        msg = estimator.get_msg(True)
+        assert msg.liveDelay.status == 'estimated'
+        assert np.allclose(msg.liveDelay.lateralDelay, lag_frames * DT, atol=0.01)
+        assert np.allclose(msg.liveDelay.lateralDelayEstimate, lag_frames * DT, atol=0.01)
+        assert np.allclose(msg.liveDelay.lateralDelayEstimateStd, 0.0, atol=0.01)
+        assert msg.liveDelay.validBlocks == BLOCK_NUM_NEEDED
 
   def test_estimator_masking(self, mocker):
     mocked_CP, lag_frames = mocker.Mock(steerActuatorDelay=0.8), random.randint(1, 19)
