9eba3be697aa14e866b13bc9bf69a20e39338eb1
Shane Smiskol <shane@smiskol.com>
2025-06-05 14:32:18 -0700

    comment

--- system/ui/lib/text_measure.py
+++ system/ui/lib/text_measure.py
@@ -4,6 +4,7 @@ _cache: dict[int, rl.Vector2] = {}
 
 
 def measure_text_cached(font: rl.Font, text: str, font_size: int, spacing: int = 0) -> rl.Vector2:
+  """Caches text measurements to avoid redundant calculations."""
   key = hash((font.texture.id, text, font_size, spacing))
   if key in _cache:
     return _cache[key]
