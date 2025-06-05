837a9ed947a515fa3e99e7ba055ca3e4e5b475ca
deanlee <deanlee3@gmail.com>
2025-06-05 17:11:22 +0800

    use global

--- common/watchdog.py
+++ common/watchdog.py
@@ -4,20 +4,19 @@ import struct
 from openpilot.system.hardware.hw import Paths
 
 WATCHDOG_FN = f"{Paths.shm_path()}/wd_"
-
+_LAST_KICK = 0.0
 
 def kick_watchdog():
+  global _LAST_KICK
   current_time = time.time()
-  if not hasattr(kick_watchdog, 'last_kick'):
-    kick_watchdog.last_kick = 0
 
-  if current_time - kick_watchdog.last_kick < 1.0:
+  if current_time - _LAST_KICK < 1.0:
     return
 
   try:
     with open(f"{WATCHDOG_FN}{os.getpid()}", 'wb') as f:
       f.write(struct.pack('<Q', int(current_time * 1e9)))
       f.flush()
-    kick_watchdog.last_kick = current_time
+    _LAST_KICK = current_time
   except OSError:
     pass
