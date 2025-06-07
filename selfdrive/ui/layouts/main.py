2031a3318890ae4959c8925ddf5a71e3b7dac3ba
Dean Lee <deanlee3@gmail.com>
2025-06-07 13:14:18 +0800

    ui: add experimental mode toggle button with visual indicator (#35446)
    * add experimental mode toggle button with visual indicator
    
    * merge master
    
    * implement a temporary state hold after mouse click"
    
    * move to seperate class
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/ui/layouts/main.py
+++ selfdrive/ui/layouts/main.py
@@ -38,7 +38,6 @@ class MainLayout(Widget):
     self._update_layout_rects(rect)
     self._handle_onroad_transition()
     self._render_main_content()
-    self._handle_input()
 
     if self._current_callback:
       self._current_callback()
@@ -51,6 +50,7 @@ class MainLayout(Widget):
     self._layouts[MainState.SETTINGS].set_callbacks(
       on_close=lambda: setattr(self, '_current_callback', self._set_mode_for_state)
     )
+    self._layouts[MainState.ONROAD].on_click = self._on_onrad_clicked
 
   def _update_layout_rects(self, rect):
     self._window_rect = rect
@@ -80,6 +80,9 @@ class MainLayout(Widget):
   def _on_flag_clicked(self):
     pass
 
+  def _on_onrad_clicked(self):
+    self._sidebar_visible = not self._sidebar_visible
+
   def _render_main_content(self):
     # Render sidebar
     if self._sidebar_visible:
@@ -87,11 +90,3 @@ class MainLayout(Widget):
 
     content_rect = self._content_rect if self._sidebar_visible else self._window_rect
     self._layouts[self._current_mode].render(content_rect)
-
-  def _handle_input(self):
-    if self._current_mode != MainState.ONROAD or not rl.is_mouse_button_pressed(rl.MouseButton.MOUSE_BUTTON_LEFT):
-      return
-
-    mouse_pos = rl.get_mouse_position()
-    if rl.check_collision_point_rec(mouse_pos, self._content_rect):
-      self._sidebar_visible = not self._sidebar_visible
