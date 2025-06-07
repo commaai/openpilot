052cdbb7e95da3913214bb2c1a7f9ff79be05cc7
Shane Smiskol <shane@smiskol.com>
2025-06-06 22:35:01 -0700

    rearrange

--- system/ui/lib/application.py
+++ system/ui/lib/application.py
@@ -31,18 +31,10 @@ class Widget(abc.ABC):
   def __init__(self):
     self._is_pressed = False
 
-  def _handle_mouse_release(self, mouse_pos: rl.Vector2) -> bool:
-    """Handle mouse release events, if applicable."""
-    return False
-
-  @abc.abstractmethod
-  def _render(self, rect: rl.Rectangle) -> bool | int | None:
-    """Render the widget within the given rectangle."""
-
   def render(self, rect: rl.Rectangle) -> bool | int | None:
-    """Render the widget within the given rectangle."""
     ret = self._render(rect)
 
+    # Keep track of whether mouse down started within the widget's rectangle
     mouse_pos = rl.get_mouse_position()
     if rl.is_mouse_button_pressed(rl.MouseButton.MOUSE_BUTTON_LEFT):
       if rl.check_collision_point_rec(mouse_pos, rect):
@@ -54,6 +46,14 @@ class Widget(abc.ABC):
         self._is_pressed = False
 
     return ret
+
+  @abc.abstractmethod
+  def _render(self, rect: rl.Rectangle) -> bool | int | None:
+    """Render the widget within the given rectangle."""
+
+  def _handle_mouse_release(self, mouse_pos: rl.Vector2) -> bool:
+    """Handle mouse release events, if applicable."""
+    return False
 
 
 class FontWeight(IntEnum):
