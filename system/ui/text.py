9579885500ae877f7d76b00cb4b1b28ddfec0df0
Shane Smiskol <shane@smiskol.com>
2025-06-06 22:06:01 -0700

    and text

--- system/ui/text.py
+++ system/ui/text.py
@@ -48,13 +48,12 @@ def wrap_text(text, font_size, max_width):
 
 class TextWindow(Widget):
   def __init__(self, text: str):
-    self._textarea_rect = rl.Rectangle(MARGIN, MARGIN, gui_app.width - MARGIN * 2, gui_app.height - MARGIN * 2)
-    self._wrapped_lines = wrap_text(text, FONT_SIZE, self._textarea_rect.width - 20)
-    self._content_rect = rl.Rectangle(0, 0, self._textarea_rect.width - 20, len(self._wrapped_lines) * LINE_HEIGHT)
     self._scroll_panel = GuiScrollPanel(show_vertical_scroll_bar=True)
-    self._scroll_panel._offset.y = -max(self._content_rect.height - self._textarea_rect.height, 0)
+    self._update_layout_rects(rl.Rectangle(0, 0, gui_app.width, gui_app.height))
 
-  def render(self, _: rl.Rectangle = None):
+  def render(self, rect: rl.Rectangle):
+    self._update_layout_rects(rect)
+
     scroll = self._scroll_panel.handle_scroll(self._textarea_rect, self._content_rect)
     rl.begin_scissor_mode(int(self._textarea_rect.x), int(self._textarea_rect.y), int(self._textarea_rect.width), int(self._textarea_rect.height))
     for i, line in enumerate(self._wrapped_lines):
@@ -64,7 +63,7 @@ class TextWindow(Widget):
       rl.draw_text_ex(gui_app.font(), line, position, FONT_SIZE, 0, rl.WHITE)
     rl.end_scissor_mode()
 
-    button_bounds = rl.Rectangle(gui_app.width - MARGIN - BUTTON_SIZE.x - SPACING, gui_app.height - MARGIN - BUTTON_SIZE.y, BUTTON_SIZE.x, BUTTON_SIZE.y)
+    button_bounds = rl.Rectangle(rect.width - MARGIN - BUTTON_SIZE.x - SPACING, rect.height - MARGIN - BUTTON_SIZE.y, BUTTON_SIZE.x, BUTTON_SIZE.y)
     ret = gui_button(button_bounds, "Exit" if PC else "Reboot", button_style=ButtonStyle.TRANSPARENT)
     if ret:
       if PC:
@@ -73,10 +72,16 @@ class TextWindow(Widget):
         HARDWARE.reboot()
     return ret
 
+  def _update_layout_rects(self, rect: rl.Rectangle):
+    self._textarea_rect = rl.Rectangle(MARGIN, MARGIN, rect.width - MARGIN * 2, rect.height - MARGIN * 2)
+    self._wrapped_lines = wrap_text(text, FONT_SIZE, self._textarea_rect.width - 20)
+    self._content_rect = rl.Rectangle(0, 0, self._textarea_rect.width - 20, len(self._wrapped_lines) * LINE_HEIGHT)
+    self._scroll_panel._offset.y = -max(self._content_rect.height - self._textarea_rect.height, 0)
+
 
 if __name__ == "__main__":
   text = sys.argv[1] if len(sys.argv) > 1 else DEMO_TEXT
   gui_app.init_window("Text Viewer")
   text_window = TextWindow(text)
   for _ in gui_app.render():
-    text_window.render()
+    text_window.render(rl.Rectangle(0, 0, gui_app.width, gui_app.height))
