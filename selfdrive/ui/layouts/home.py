e676676f090b90578a7cbc49b10a9ca0a4e3ac16
deanlee <deanlee3@gmail.com>
2025-06-08 15:17:53 +0800

    add ExpermentalModeButton

--- selfdrive/ui/layouts/home.py
+++ selfdrive/ui/layouts/home.py
@@ -4,6 +4,7 @@ from collections.abc import Callable
 from enum import IntEnum
 from openpilot.common.params import Params
 from openpilot.selfdrive.ui.widgets.offroad_alerts import UpdateAlert, OffroadAlert
+from openpilot.selfdrive.ui.widgets.exp_mode_button import ExperimentalModeButton
 from openpilot.selfdrive.ui.widgets.prime import PrimeWidget
 from openpilot.system.ui.lib.text_measure import measure_text_cached
 from openpilot.system.ui.lib.label import gui_label
@@ -50,6 +51,7 @@ class HomeLayout(Widget):
 
     self._prime_ad_widget = PrimeWidget()
 
+    self._exp_mode_button = ExperimentalModeButton()
     self._setup_callbacks()
 
   def _setup_callbacks(self):
@@ -176,19 +178,17 @@ class HomeLayout(Widget):
     self._prime_ad_widget.render(self.left_column_rect)
 
   def _render_right_column(self):
-    widget_height = (self.right_column_rect.height - SPACING) // 2
-
+    exp_height = 125
     exp_rect = rl.Rectangle(
-      self.right_column_rect.x, self.right_column_rect.y, self.right_column_rect.width, widget_height
+      self.right_column_rect.x, self.right_column_rect.y, self.right_column_rect.width, exp_height
     )
-    rl.draw_rectangle_rounded(exp_rect, 0.02, 10, PRIME_BG_COLOR)
-    gui_label(exp_rect, "Experimental Mode", 36, alignment=rl.GuiTextAlignment.TEXT_ALIGN_CENTER)
+    self._exp_mode_button.render(exp_rect)
 
     setup_rect = rl.Rectangle(
       self.right_column_rect.x,
-      self.right_column_rect.y + widget_height + SPACING,
+      self.right_column_rect.y + exp_height + SPACING,
       self.right_column_rect.width,
-      widget_height,
+      self.right_column_rect.height - exp_height - SPACING,
     )
     rl.draw_rectangle_rounded(setup_rect, 0.02, 10, PRIME_BG_COLOR)
     gui_label(setup_rect, "Setup", 36, alignment=rl.GuiTextAlignment.TEXT_ALIGN_CENTER)
