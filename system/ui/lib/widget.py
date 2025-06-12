654683473d4fa8e6ed99a940b039b48ef8d712a0
Shane Smiskol <shane@smiskol.com>
2025-06-11 23:59:46 -0700

    simple version

--- system/ui/lib/widget.py
+++ system/ui/lib/widget.py
@@ -11,19 +11,26 @@ class DialogResult(IntEnum):
 
 class Widget(abc.ABC):
   def __init__(self):
+    self._rect: rl.Rectangle = rl.Rectangle(0, 0, 0, 0)
     self._is_pressed = False
 
+  def set_rect(self, rect: rl.Rectangle):
+    self._rect = rect
+
   def render(self, rect: rl.Rectangle) -> bool | int | None:
-    ret = self._render(rect)
+    if rect is not None:
+      self.set_rect(rect)
+
+    ret = self._render(self._rect)
 
     # Keep track of whether mouse down started within the widget's rectangle
     mouse_pos = rl.get_mouse_position()
     if rl.is_mouse_button_pressed(rl.MouseButton.MOUSE_BUTTON_LEFT):
-      if rl.check_collision_point_rec(mouse_pos, rect):
+      if rl.check_collision_point_rec(mouse_pos, self._rect):
         self._is_pressed = True
 
     if rl.is_mouse_button_released(rl.MouseButton.MOUSE_BUTTON_LEFT):
-      if self._is_pressed and rl.check_collision_point_rec(mouse_pos, rect):
+      if self._is_pressed and rl.check_collision_point_rec(mouse_pos, self._rect):
         self._handle_mouse_release(mouse_pos)
       self._is_pressed = False
 
