6fbb39683d38a6432322650aa0a05832255b7d6a
Shane Smiskol <shane@smiskol.com>
2025-06-12 20:33:46 -0700

    Revert "use in model renderer"
    This reverts commit d35f774155c9875209d06b8cd0b4849b1d8a60c4.

--- selfdrive/ui/onroad/model_renderer.py
+++ selfdrive/ui/onroad/model_renderer.py
@@ -56,8 +56,6 @@ class ModelRenderer(Widget):
     self._lead_vehicles = [LeadVehicle(), LeadVehicle()]
     self._path_offset_z = HEIGHT_INIT[0]
 
-    self._render_lead_indicator = False
-
     # Initialize ModelPoints objects
     self._path = ModelPoints()
     self._lane_lines = [ModelPoints() for _ in range(4)]
@@ -85,37 +83,6 @@ class ModelRenderer(Widget):
     self._car_space_transform = transform.astype(np.float32)
     self._transform_dirty = True
 
-  def _update_state(self):
-    # Update state
-    sm = ui_state.sm
-    self._experimental_mode = sm['selfdriveState'].experimentalMode
-
-    live_calib = sm['liveCalibration']
-    self._path_offset_z = live_calib.height[0] if live_calib.height else HEIGHT_INIT[0]
-
-    if sm.updated['carParams']:
-      self._longitudinal_control = sm['carParams'].openpilotLongitudinalControl
-
-    model = sm['modelV2']
-    radar_state = sm['radarState'] if sm.valid['radarState'] else None
-    lead_one = radar_state.leadOne if radar_state else None
-    self._render_lead_indicator = self._longitudinal_control and radar_state is not None
-
-    # Update model data when needed
-    model_updated = sm.updated['modelV2']
-    if model_updated or sm.updated['radarState'] or self._transform_dirty:
-      if model_updated:
-        self._update_raw_points(model)
-
-      path_x_array = self._path.raw_points[:, 0]
-      if path_x_array.size == 0:
-        return
-
-      self._update_model(lead_one, path_x_array)
-      if self._render_lead_indicator:
-        self._update_leads(radar_state, path_x_array)
-      self._transform_dirty = False
-
   def _render(self, rect: rl.Rectangle):
     sm = ui_state.sm
 
@@ -129,11 +96,40 @@ class ModelRenderer(Widget):
       rect.x - CLIP_MARGIN, rect.y - CLIP_MARGIN, rect.width + 2 * CLIP_MARGIN, rect.height + 2 * CLIP_MARGIN
     )
 
+    # Update state
+    self._experimental_mode = sm['selfdriveState'].experimentalMode
+
+    live_calib = sm['liveCalibration']
+    self._path_offset_z = live_calib.height[0] if live_calib.height else HEIGHT_INIT[0]
+
+    if sm.updated['carParams']:
+      self._longitudinal_control = sm['carParams'].openpilotLongitudinalControl
+
+    model = sm['modelV2']
+    radar_state = sm['radarState'] if sm.valid['radarState'] else None
+    lead_one = radar_state.leadOne if radar_state else None
+    render_lead_indicator = self._longitudinal_control and radar_state is not None
+
+    # Update model data when needed
+    model_updated = sm.updated['modelV2']
+    if model_updated or sm.updated['radarState'] or self._transform_dirty:
+      if model_updated:
+        self._update_raw_points(model)
+
+      path_x_array = self._path.raw_points[:, 0]
+      if path_x_array.size == 0:
+        return
+
+      self._update_model(lead_one, path_x_array)
+      if render_lead_indicator:
+        self._update_leads(radar_state, path_x_array)
+      self._transform_dirty = False
+
     # Draw elements
     self._draw_lane_lines()
     self._draw_path(sm)
 
-    if self._render_lead_indicator:
+    if render_lead_indicator and radar_state:
       self._draw_lead_indicator()
 
   def _update_raw_points(self, model):
