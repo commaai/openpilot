6a37d8a89e6b8f0523dd7c3751f447a69758e872
Maxime Desroches <desroches.maxime@gmail.com>
2025-06-19 10:38:26 -0700

    fix framereader indent

--- tools/lib/framereader.py
+++ tools/lib/framereader.py
@@ -16,21 +16,21 @@ HEVC_SLICE_I = 2
 
 
 class LRUCache:
-    def __init__(self, capacity: int):
-      self._cache: OrderedDict = OrderedDict()
-      self.capacity = capacity
+  def __init__(self, capacity: int):
+    self._cache: OrderedDict = OrderedDict()
+    self.capacity = capacity
 
-    def __getitem__(self, key):
-      self._cache.move_to_end(key)
-      return self._cache[key]
+  def __getitem__(self, key):
+    self._cache.move_to_end(key)
+    return self._cache[key]
 
-    def __setitem__(self, key, value):
-      self._cache[key] = value
-      if len(self._cache) > self.capacity:
-          self._cache.popitem(last=False)
+  def __setitem__(self, key, value):
+    self._cache[key] = value
+    if len(self._cache) > self.capacity:
+        self._cache.popitem(last=False)
 
-    def __contains__(self, key):
-      return key in self._cache
+  def __contains__(self, key):
+    return key in self._cache
 
 
 def assert_hvec(fn: str) -> None:
