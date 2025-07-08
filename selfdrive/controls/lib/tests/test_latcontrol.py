20fdb686ca050ab70da1dd306ee679ad116ad600
Harald Schäfer <harald.the.engineer@gmail.com>
2025-07-08 14:12:46 -0700

    latcontrol torque: remove option to feed back on localizer (#35659)
    * Localizer is too laggy for control
    
    * typo
    
    * typo
    
    * fix test
    
    * fix imports
    
    * Revert "fix imports"
    
    This reverts commit 5074f8050170f974b451e00d9fdc752f09a47d57.
    
    * fix improt
    
    * import

--- selfdrive/controls/lib/tests/test_latcontrol.py
+++ selfdrive/controls/lib/tests/test_latcontrol.py
@@ -9,8 +9,6 @@ from opendbc.car.vehicle_model import VehicleModel
 from openpilot.selfdrive.controls.lib.latcontrol_pid import LatControlPID
 from openpilot.selfdrive.controls.lib.latcontrol_torque import LatControlTorque
 from openpilot.selfdrive.controls.lib.latcontrol_angle import LatControlAngle
-from openpilot.selfdrive.locationd.helpers import Pose
-from openpilot.common.mock.generators import generate_livePose
 
 
 class TestLatControl:
@@ -30,18 +28,15 @@ class TestLatControl:
 
     params = log.LiveParametersData.new_message()
 
-    lp = generate_livePose()
-    pose = Pose.from_live_pose(lp.livePose)
-
     # Saturate for curvature limited and controller limited
     for _ in range(1000):
-      _, _, lac_log = controller.update(True, CS, VM, params, False, 0, pose, True)
+      _, _, lac_log = controller.update(True, CS, VM, params, False, 0, True)
     assert lac_log.saturated
 
     for _ in range(1000):
-      _, _, lac_log = controller.update(True, CS, VM, params, False, 0, pose, False)
+      _, _, lac_log = controller.update(True, CS, VM, params, False, 0, False)
     assert not lac_log.saturated
 
     for _ in range(1000):
-      _, _, lac_log = controller.update(True, CS, VM, params, False, 1, pose, False)
+      _, _, lac_log = controller.update(True, CS, VM, params, False, 1, False)
     assert lac_log.saturated
