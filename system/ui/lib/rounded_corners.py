366b11fbdc106d2db520cae04a5a5b45769eb80d
David <dt052901@gmail.com>
2025-07-14 13:53:57 -0500

    feat: Add get_roundness util function

---
+++ system/ui/lib/rounded_corners.py
@@ -0,0 +1,10 @@
+import pyray as rl
+
+DEFAULT_BORDER_RADIUS = 10
+
+
+def get_roundness(rect: rl.Rectangle, border_radius: int) -> float:
+  """Calculate the roundness of a rectangle based on its width and height, given a border radius value in pixels.
+  This is used to standardize the roundness across rectangle with different sizes, since `draw_rectangle_rounded` doesn't use pixels.
+  """
+  return border_radius / (min(rect.width, rect.height) / 2)
