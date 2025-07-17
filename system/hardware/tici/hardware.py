a8fd55740bd7c88ecb9cc34546d2a72b840d3817
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-07-17 10:05:27 -0700

    Remove NVMe logging (#35740)

--- system/hardware/tici/hardware.py
+++ system/hardware/tici/hardware.py
@@ -1,4 +1,3 @@
-import json
 import math
 import os
 import subprocess
@@ -291,15 +290,6 @@ class Tici(HardwareBase):
     except Exception:
       return []
 
-  def get_nvme_temperatures(self):
-    ret = []
-    try:
-      out = subprocess.check_output("sudo smartctl -aj /dev/nvme0", shell=True)
-      dat = json.loads(out)
-      ret = list(map(int, dat["nvme_smart_health_information_log"]["temperature_sensors"]))
-    except Exception:
-      pass
-    return ret
 
   def get_current_power_draw(self):
     return (self.read_param_file("/sys/class/hwmon/hwmon1/power1_input", int) / 1e6)
