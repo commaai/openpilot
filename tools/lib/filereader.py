862a8162150e0722078fe8c6b12f0272c21f280f
Shane Smiskol <shane@smiskol.com>
2025-07-17 23:12:08 -0700

    LogReader: add data endpoint option (#35751)
    * add data endpoint option
    
    * clean up

--- tools/lib/filereader.py
+++ tools/lib/filereader.py
@@ -8,14 +8,14 @@ from openpilot.tools.lib.url_file import URLFile
 DATA_ENDPOINT = os.getenv("DATA_ENDPOINT", "http://data-raw.comma.internal/")
 
 
-def internal_source_available(url=DATA_ENDPOINT):
+def internal_source_available(url: str) -> bool:
   if os.path.isdir(url):
     return True
 
   try:
     hostname = urlparse(url).hostname
     port = urlparse(url).port or 80
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
       s.settimeout(0.5)
       s.connect((hostname, port))
     return True
