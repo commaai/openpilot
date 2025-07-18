7b9a2eba1587b6a700f9560cee823e958fee7c0d
Shane Smiskol <shane@smiskol.com>
2025-07-18 04:29:56 -0700

    clean up

--- selfdrive/car/tests/test_models.py
+++ selfdrive/car/tests/test_models.py
@@ -123,8 +123,7 @@ class TestCarModelBase(unittest.TestCase):
       segment_range = f"{cls.test_route.route}/{seg}"
 
       try:
-        sources = ([internal_source] if len(INTERNAL_SEG_LIST) else
-                   [openpilotci_source, comma_api_source])
+        sources = [internal_source] if len(INTERNAL_SEG_LIST) else [openpilotci_source, comma_api_source]
         lr = LogReader(segment_range, sources=sources, sort_by_time=True)
         return cls.get_testing_data_from_logreader(lr)
       except (LogsUnavailable, AssertionError):
