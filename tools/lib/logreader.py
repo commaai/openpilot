095047a42336d95d7be32fdd10d80e2e2d1575ba
Shane Smiskol <shane@smiskol.com>
2025-07-18 02:18:08 -0700

    clean up sources

--- tools/lib/logreader.py
+++ tools/lib/logreader.py
@@ -314,12 +314,8 @@ class LogReader:
   def __init__(self, identifier: str | list[str], default_mode: ReadMode = ReadMode.RLOG,
                sources: list[Source] = None, sort_by_time=False, only_union_types=False):
     if sources is None:
-      sources = [internal_source,
-                 openpilotci_source,
-                 comma_api_source,
-                 comma_car_segments_source,
-                 testing_closet_source,
-                 ]
+      sources = [internal_source, openpilotci_source, comma_api_source,
+                 comma_car_segments_source, testing_closet_source]
 
     self.default_mode = default_mode
     self.sources = sources
