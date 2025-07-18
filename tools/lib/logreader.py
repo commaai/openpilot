eb9cce1d845f624d66deed5e6f00a5a08265e8b3
Shane Smiskol <shane@smiskol.com>
2025-07-18 15:55:03 -0700

    support connect

--- tools/lib/logreader.py
+++ tools/lib/logreader.py
@@ -227,7 +227,24 @@ def auto_source(identifier: str, sources: list[Source], default_mode: ReadMode) 
 def parse_indirect(identifier: str) -> str:
   if "useradmin.comma.ai" in identifier:
     query = parse_qs(urlparse(identifier).query)
-    return query["onebox"][0]
+    identifier = query["onebox"][0]
+  elif "connect.comma.ai" in identifier:
+    path = urlparse(identifier).path.strip("/").split("/")
+    path = ['/'.join(path[:2]), *path[2:]]  # recombine log id
+
+    identifier = path[0]
+    if len(path) == 2:
+      # add selector
+      identifier = "/".join(path)
+    elif len(path) > 2:
+      # convert url with seconds to segments
+      start, end = int(path[1]) // 60, int(path[2]) // 60 + 1
+      identifier = f"{identifier}/{start}:{end}"
+
+      # add selector if it exists
+      if len(path) > 3:
+        identifier += f"/{path[3]}"
+
   return identifier
 
 
@@ -240,7 +257,9 @@ def parse_direct(identifier: str):
 class LogReader:
   def _parse_identifier(self, identifier: str) -> list[str]:
     # useradmin, etc.
+    print('old identifier:', identifier)
     identifier = parse_indirect(identifier)
+    print('new identifier:', identifier)
 
     # direct url or file
     direct_parsed = parse_direct(identifier)
