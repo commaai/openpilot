9c1cc545533555f272495d2bff226963c9eb7262
Shane Smiskol <shane@smiskol.com>
2025-07-22 18:39:30 -0700

    not sure why not here

--- system/hardware/hardwared.py
+++ system/hardware/hardwared.py
@@ -172,6 +172,7 @@ def hardware_thread(end_event, hw_queue) -> None:
   onroad_conditions: dict[str, bool] = {
     "ignition": False,
     "not_onroad_cycle": True,
+    "device_temp_good": True,
   }
   startup_conditions: dict[str, bool] = {}
   startup_conditions_prev: dict[str, bool] = {}
