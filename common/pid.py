871b1452b9d9513b22c1510d4837a307b68645bc
Shane Smiskol <shane@smiskol.com>
2025-07-27 00:38:23 -0700

    clean up

--- common/pid.py
+++ common/pid.py
@@ -1,11 +1,8 @@
 import numpy as np
 from numbers import Number
-from typing import Callable
-
 
 class PIDController:
-  def __init__(self, k_p, k_i, k_f=0., k_d=0., pos_limit: float | Callable[[float], float] = 1e308,
-               neg_limit: float | Callable[[float], float] = -1e308, rate=100):
+  def __init__(self, k_p, k_i, k_f=0., k_d=0., pos_limit=1e308, neg_limit=-1e308, rate=100):
     self._k_p = k_p
     self._k_i = k_i
     self._k_d = k_d
