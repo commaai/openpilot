2de7b5fc29d8428a840a45e3bd08acef943ec7b1
OpenPilot CI <ci@openpilot.ai>
2025-07-28 12:18:57 -0400

    Clean up openpilot directory - remove duplicated files

--- openpilot/common/gps.py
+++
@@ -1,8 +0,0 @@
-from openpilot.common.params import Params
-
-
-def get_gps_location_service(params: Params) -> str:
-    if params.get_bool("UbloxAvailable"):
-        return "gpsLocationExternal"
-    else:
-        return "gpsLocation"
--- openpilot/system/__init__.py
+++
@@ -1,1 +0,0 @@
-# System module for openpilot
