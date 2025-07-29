981c57d8e70e0f4e8d32fbdd0fbdfe435628b20d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-07-29 16:01:11 -0700

    init

---
+++ selfdrive/test/test_scenarios.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import numpy as np
+from collections import defaultdict
+from enum import Enum
+from typing import Callable
+from dataclasses import dataclass
+
+from openpilot.tools.lib.logreader import LogReader
+
+TEST_SEGMENT = "4019fff6e54cf1c7|00000123--4bc0d95ef6/5"
+
+class Assert(enum.Enum):
+  ENGAGED = 0
+  SOFT_DISABLE = 1
+
+@dataclass
+class Scenario:
+  name: str
+  get_msgs: Callable
+  asserts: list[Assert]
+
+scenarios = [
+  Scenario("all good", lambda x: x, [Assert.ENGAGED, ]),
+  Scenario("bad CAN msgs", lambda x: x, [Assert.ENGAGED, Assert.SOFT_DISABLE]),
+  Scenario("missing road cam", lambda x: x, [Assert.ENGAGED, Assert.SOFT_DISABLE]),
+  Scenario("DM red alert, user not disengaging", lambda x: x, [Assert.ENGAGED, Assert.SOFT_DISABLE]),
+]
+
+def run(msgs):
+  # TODO: need to process replay the whole stack
+  return msgs
+
+if __name__ == "__main__":
+  lr = list(LogReader(TEST_SEGMENT))
+
+  for s in scenarios:
+    inputs = s.get_msgs(lr)
+    output = run(inputs)
