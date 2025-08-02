b2cf2ce6f1eaea552f095fe0f49ff24bb62f78c6
Shane Smiskol <shane@smiskol.com>
2025-08-01 19:01:17 -0700

    clean up

--- selfdrive/test/process_replay/migration.py
+++ selfdrive/test/process_replay/migration.py
@@ -15,7 +15,7 @@ from openpilot.selfdrive.modeld.fill_model_msg import fill_xyz_poly, fill_lane_l
 from openpilot.selfdrive.test.process_replay.vision_meta import meta_from_encode_index
 from openpilot.selfdrive.controls.lib.longitudinal_planner import get_accel_from_plan, CONTROL_N_T_IDX
 from openpilot.system.manager.process_config import managed_processes
-from openpilot.tools.lib.logreader import LogIterable, CachedReader
+from openpilot.tools.lib.logreader import LogIterable
 
 MessageWithIndex = tuple[int, capnp.lib.capnp._DynamicStructReader]
 MigrationOps = tuple[list[tuple[int, capnp.lib.capnp._DynamicStructReader]], list[capnp.lib.capnp._DynamicStructReader], list[int]]
