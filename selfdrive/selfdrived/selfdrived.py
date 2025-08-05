7f4c00e0260d5d1392cf99b4d2a85ba66c01d3b9
Shane Smiskol <shane@smiskol.com>
2025-08-05 14:51:31 -0700

    cmt

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -57,8 +57,8 @@ def check_excessive_actuation(sm: messaging.SubMaster, CS: car.CarState, calibra
   roll_compensated_lateral_accel = (CS.vEgo * yaw_rate) - (math.sin(roll) * ACCELERATION_DUE_TO_GRAVITY)
 
   excessive_lat_actuation = False
-  print('vEgo', CS.vEgo, yaw_rate, roll)
-  print('roll_compensated_lateral_accel', roll_compensated_lateral_accel)
+  # print('vEgo', CS.vEgo, yaw_rate, roll)
+  # print('roll_compensated_lateral_accel', roll_compensated_lateral_accel)
   if sm['carControl'].latActive:
     if not CS.steeringPressed:
       if abs(roll_compensated_lateral_accel) > ISO_LATERAL_ACCEL * 2:
@@ -66,11 +66,11 @@ def check_excessive_actuation(sm: messaging.SubMaster, CS: car.CarState, calibra
 
   # livePose acceleration can be noisy due to bad mounting or aliased livePose measurements
   livepose_valid = abs(CS.aEgo - accel_calibrated) < 2
-  print('excessive_long_actuation', excessive_long_actuation, 'excessive_lat_actuation', excessive_lat_actuation, 'livepose_valid', livepose_valid)
+  # print('excessive_long_actuation', excessive_long_actuation, 'excessive_lat_actuation', excessive_lat_actuation, 'livepose_valid', livepose_valid)
   counter = counter + 1 if livepose_valid and (excessive_long_actuation or excessive_lat_actuation) else 0
 
-  if counter > 0:
-    print('counter', counter, excessive_long_actuation, excessive_lat_actuation, livepose_valid)
+  # if counter > 0:
+  #   print('counter', counter, excessive_long_actuation, excessive_lat_actuation, livepose_valid)
 
   return counter, counter > MIN_EXCESSIVE_ACTUATION_COUNT, roll_compensated_lateral_accel
 
