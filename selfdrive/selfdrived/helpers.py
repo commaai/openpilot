3114748449ddeb74c524cf5bb83dfcf9d6b514b4
Shane Smiskol <shane@smiskol.com>
2025-08-05 15:01:05 -0700

    two counters might be too much to return

--- selfdrive/selfdrived/helpers.py
+++ selfdrive/selfdrived/helpers.py
@@ -10,7 +10,7 @@ MIN_EXCESSIVE_ACTUATION_COUNT = int(0.25 / DT_CTRL)
 MIN_ENGAGE_BUFFER = int(1 / DT_CTRL)
 
 
-def check_excessive_actuation(sm: messaging.SubMaster, CS: car.CarState, calibrated_pose: Pose, counter: int) -> tuple[int, bool, float]:
+def check_excessive_actuation(sm: messaging.SubMaster, CS: car.CarState, calibrated_pose: Pose, excessive_counter: int, engaged_counter) -> tuple[int, int, bool]:
   # CS.aEgo can be noisy to bumps in the road, transitioning from standstill, losing traction, etc.
   # longitudinal
   accel_calibrated = calibrated_pose.acceleration.x
@@ -22,19 +22,17 @@ def check_excessive_actuation(sm: messaging.SubMaster, CS: car.CarState, calibra
   roll_compensated_lateral_accel = (CS.vEgo * yaw_rate) - (math.sin(roll) * ACCELERATION_DUE_TO_GRAVITY)
 
   excessive_lat_actuation = False
-  # print('vEgo', CS.vEgo, yaw_rate, roll)
-  # print('roll_compensated_lateral_accel', roll_compensated_lateral_accel)
-  if sm['carControl'].latActive:
-    if not CS.steeringPressed:
-      if abs(roll_compensated_lateral_accel) > ISO_LATERAL_ACCEL * 2:
-        excessive_lat_actuation = True
+  engaged_counter = engaged_counter + 1 if sm['carControl'].latActive and not CS.steeringPressed else 0
+  if engaged_counter > MIN_ENGAGE_BUFFER:
+    if abs(roll_compensated_lateral_accel) > ISO_LATERAL_ACCEL * 2:
+      excessive_lat_actuation = True
 
   # livePose acceleration can be noisy due to bad mounting or aliased livePose measurements
   livepose_valid = abs(CS.aEgo - accel_calibrated) < 2
   # print('excessive_long_actuation', excessive_long_actuation, 'excessive_lat_actuation', excessive_lat_actuation, 'livepose_valid', livepose_valid)
-  counter = counter + 1 if livepose_valid and (excessive_long_actuation or excessive_lat_actuation) else 0
+  excessive_counter = excessive_counter + 1 if livepose_valid and (excessive_long_actuation or excessive_lat_actuation) else 0
 
   # if counter > 0:
   #   print('counter', counter, excessive_long_actuation, excessive_lat_actuation, livepose_valid)
 
-  return counter, counter > MIN_EXCESSIVE_ACTUATION_COUNT, roll_compensated_lateral_accel
+  return excessive_counter, engaged_counter, excessive_counter > MIN_EXCESSIVE_ACTUATION_COUNT
