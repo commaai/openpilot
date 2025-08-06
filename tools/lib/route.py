f107849eaffe41cd9af27b34e47d9ee1eca841f5
Bruce Wayne <harald.the.engineer@gmail.com>
2025-08-05 20:05:25 -0700

    unused

--- tools/lib/route.py
+++ tools/lib/route.py
@@ -11,14 +11,6 @@ from openpilot.tools.lib.auth_config import get_token
 from openpilot.tools.lib.api import APIError, CommaApi
 from openpilot.tools.lib.helpers import RE
 
-QLOG_FILENAMES = ('qlog.bz2', 'qlog.zst', 'qlog')
-QCAMERA_FILENAMES = ('qcamera.ts',)
-LOG_FILENAMES = ('rlog.bz2', 'raw_log.bz2', 'rlog.zst', 'rlog')
-CAMERA_FILENAMES = ('fcamera.hevc', 'video.hevc')
-DCAMERA_FILENAMES = ('dcamera.hevc',)
-ECAMERA_FILENAMES = ('ecamera.hevc',)
-BOOTLOG_FILENAMES = ('bootlog.zst', 'bootlog.bz2', 'bootlog')
-
 FileNameTuple: TypeAlias = tuple[str, ...]
 class FileName:
   #TODO use the ones from route.py
