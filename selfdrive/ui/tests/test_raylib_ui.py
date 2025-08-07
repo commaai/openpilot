c5ac0f83b98750680fa074ed9f806d9966403441
Shane Smiskol <shane@smiskol.com>
2025-08-07 14:08:09 -0700

    rm
    rm

--- selfdrive/ui/tests/test_raylib_ui.py
+++ selfdrive/ui/tests/test_raylib_ui.py
@@ -2,7 +2,7 @@ from openpilot.system.ui.lib.application import gui_app
 from openpilot.selfdrive.ui.layouts.main import MainLayout
 
 
-def test_ui():
+def test_raylib_ui():
   """Test initialization of the UI widgets is successful."""
   gui_app.init_window("UI")
   MainLayout()
