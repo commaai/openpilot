7fd90a6717248f6937b5dd876c352f4cea6bd256
Shane Smiskol <shane@smiskol.com>
2025-08-07 15:15:52 -0700

    clean up exp

--- selfdrive/ui/widgets/exp_mode_button.py
+++ selfdrive/ui/widgets/exp_mode_button.py
@@ -31,24 +31,12 @@ class ExperimentalModeButton(Widget):
     rl.draw_rectangle_gradient_h(int(rect.x), int(rect.y), int(rect.width), int(rect.height),
                                  start_color, end_color)
 
-  # def _handle_interaction(self, rect):
-  #   mouse_pos = rl.get_mouse_position()
-  #   mouse_in_rect = rl.check_collision_point_rec(mouse_pos, rect)
-  #
-  #   # self.is_pressed = mouse_in_rect and rl.is_mouse_button_down(rl.MOUSE_BUTTON_LEFT)
-  #   return mouse_in_rect and rl.is_mouse_button_released(rl.MOUSE_BUTTON_LEFT)
-
   def _handle_mouse_release(self, mouse_pos):
     self.experimental_mode = not self.experimental_mode
     # TODO: Opening settings for ExperimentalMode
     self.params.put_bool("ExperimentalMode", self.experimental_mode)
 
   def _render(self, rect):
-    # if self._handle_interaction(rect):
-    #   self.experimental_mode = not self.experimental_mode
-    #   # TODO: Opening settings for ExperimentalMode
-    #   self.params.put_bool("ExperimentalMode", self.experimental_mode)
-
     rl.draw_rectangle_rounded(rect, 0.08, 20, rl.Color(255, 255, 255, 255))
 
     rl.begin_scissor_mode(int(rect.x), int(rect.y), int(rect.width), int(rect.height))
