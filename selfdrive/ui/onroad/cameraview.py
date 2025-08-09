2d8030de0b5171fa5827a33be5a3033eb55e5d04
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-08-09 15:00:36 -0700

    ui: move watch3 to raylib (#35967)
    * move to py
    
    * cleaner
    
    * clean that up

--- selfdrive/ui/onroad/cameraview.py
+++ selfdrive/ui/onroad/cameraview.py
@@ -141,6 +141,9 @@ class CameraView(Widget):
 
     self.client = None
 
+  def __del__(self):
+    self.close()
+
   def _calc_frame_matrix(self, rect: rl.Rectangle) -> np.ndarray:
     if not self.frame:
       return np.eye(3)
@@ -337,16 +340,7 @@ class CameraView(Widget):
 
 
 if __name__ == "__main__":
-  gui_app.init_window("watch3")
-  road_camera_view = CameraView("camerad", VisionStreamType.VISION_STREAM_ROAD)
-  driver_camera_view = CameraView("camerad", VisionStreamType.VISION_STREAM_DRIVER)
-  wide_road_camera_view = CameraView("camerad", VisionStreamType.VISION_STREAM_WIDE_ROAD)
-  try:
-    for _ in gui_app.render():
-      road_camera_view.render(rl.Rectangle(gui_app.width // 4, 0, gui_app.width // 2, gui_app.height // 2))
-      driver_camera_view.render(rl.Rectangle(0, gui_app.height // 2, gui_app.width // 2, gui_app.height // 2))
-      wide_road_camera_view.render(rl.Rectangle(gui_app.width // 2, gui_app.height // 2, gui_app.width // 2, gui_app.height // 2))
-  finally:
-    road_camera_view.close()
-    driver_camera_view.close()
-    wide_road_camera_view.close()
+  gui_app.init_window("camera view")
+  road = CameraView("camerad", VisionStreamType.VISION_STREAM_ROAD)
+  for _ in gui_app.render():
+    road.render(rl.Rectangle(0, 0, gui_app.width, gui_app.height))
