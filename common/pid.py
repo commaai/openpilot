9cc46a016d28806ae31370e0677f5de342a20ef9
Bruce Wayne <harald.the.engineer@gmail.com>
2025-08-14 13:48:39 -0700

    whitespace

--- common/pid.py
+++ common/pid.py
@@ -1,7 +1,6 @@
 import numpy as np
 from numbers import Number
 
-
 class PIDController:
   def __init__(self, k_p, k_i, k_f=0., k_d=0., pos_limit=1e308, neg_limit=-1e308, rate=100):
     self._k_p = k_p
