c085b8af19438956c15592828bd082803f43dfaf
Jimmy <9859727+Quantizr@users.noreply.github.com>
2025-08-19 09:18:32 -0700

    feedbackd: remove lkas toggle for this release (#36018)
    remove lkas toggle for this release

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -69,13 +69,6 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
       true,
     },
     {
-      "RecordAudioFeedback",
-      tr("Record Audio Feedback with LKAS button"),
-      tr("Press the LKAS button to record and share driving feedback with the openpilot team. When this toggle is disabled, the button acts as a bookmark button. The event will be highlighted in comma connect and the segment will be preserved on your device's storage.\n\nNote that this feature is only compatible with select cars."),
-      "../assets/icons/microphone.png",
-      false,
-    },
-    {
       "IsMetric",
       tr("Use Metric System"),
       tr("Display speed in km/h instead of mph."),
