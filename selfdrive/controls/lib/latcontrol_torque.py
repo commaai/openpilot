017c9c5af11a4889c6c8198d04cc4c7f6ed11105
felsager <d.felsager@gmail.com>
2025-08-21 14:11:13 -0700

    better comment

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -65,7 +65,7 @@ class LatControlTorque(LatControl):
       # do error correction in lateral acceleration space, convert at end to handle non-linear torque responses correctly
       pid_log.error = float(setpoint - measurement)
       ff = gravity_adjusted_lateral_accel
-      # latAccelOffset compensates 
+      # latAccelOffset corrects roll compensation bias from device mounting roll misalignment
       ff -= self.torque_params.latAccelOffset
       ff += get_friction(desired_lateral_accel - actual_lateral_accel, lateral_accel_deadzone, FRICTION_THRESHOLD, self.torque_params)
 
