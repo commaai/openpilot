69a2ca6c927df68d4bfa8cc57aa73a28ea371a31
felsager <d.felsager@gmail.com>
2025-08-22 14:42:20 -0700

    less tight threshold for lat accel offset estimation error

--- selfdrive/controls/tests/test_torqued_offset_convergence.py
+++ selfdrive/controls/tests/test_torqued_offset_convergence.py
@@ -4,7 +4,6 @@ from opendbc.car import ACCELERATION_DUE_TO_GRAVITY
 from opendbc.car import structs
 from opendbc.car.lateral import get_friction, FRICTION_THRESHOLD
 from openpilot.selfdrive.locationd.torqued import TorqueEstimator, POINTS_PER_BUCKET, STEER_BUCKET_BOUNDS
-np.random.seed(0)
 
 ROLL_BIAS_DEG = 1.0
 TOTAL_POINTS = POINTS_PER_BUCKET*len(STEER_BUCKET_BOUNDS)
@@ -17,16 +16,19 @@ roll_compensation_bias = ACCELERATION_DUE_TO_GRAVITY*float(np.sin(np.deg2rad(ROL
 gt_torque_tune = structs.CarParams.LateralTorqueTuning(latAccelFactor=2.0, latAccelOffset=-roll_compensation_bias, friction=0.2)
 
 def test_estimated_offset():
-  steer_torques = TORQUE_STD*np.random.randn(TOTAL_POINTS)
+  rng = np.random.default_rng(0)
+  steer_torques = rng.normal(scale=TORQUE_STD, size=TOTAL_POINTS)
   steer_torques = steer_torques[abs(steer_torques) < MAX_STEER]
-  la_errs = np.random.randn(steer_torques.size)
+  la_errs = rng.normal(size=steer_torques.size)
   frictions = np.array([get_friction(la_err, 0.0, FRICTION_THRESHOLD, gt_torque_tune) for la_err in la_errs])
   lat_accels = gt_torque_tune.latAccelFactor*steer_torques + gt_torque_tune.latAccelOffset + frictions
-  steer_torques += IN_OUT_STD*np.random.randn(steer_torques.size)
-  lat_accels += IN_OUT_STD*np.random.randn(steer_torques.size)
+  steer_torques += rng.normal(scale=IN_OUT_STD, size=steer_torques.size)
+  lat_accels += rng.normal(scale=IN_OUT_STD, size=steer_torques.size)
   est = TorqueEstimator(car.CarParams())
   for steer_torque, lat_accel in zip(steer_torques, lat_accels, strict=True):
     est.filtered_points.add_point(steer_torque, lat_accel)
   msg = est.get_msg()
   # TODO add lataccelfactor and friction check when we have more accurate estimates
-  assert abs(msg.liveTorqueParameters.latAccelOffsetRaw - gt_torque_tune.latAccelOffset) < 0.01
+  assert abs(msg.liveTorqueParameters.latAccelOffsetRaw - gt_torque_tune.latAccelOffset) < 0.03
+
+test_estimated_offset()
