b675d6b247be0f129e4f470af4fe62f90c239374
Shane Smiskol <shane@smiskol.com>
2025-08-22 20:41:24 -0700

    i can't believe we didn't check this...

--- system/ui/widgets/network.py
+++ system/ui/widgets/network.py
@@ -225,6 +225,8 @@ class WifiManagerUI(Widget):
       for n in self._networks:
         self._networks_buttons[n.ssid] = Button(n.ssid, partial(self._networks_buttons_callback, n), font_size=55, text_alignment=TextAlignment.LEFT,
                                                 button_style=ButtonStyle.NO_EFFECT)
+        if n.security_type == SecurityType.UNSUPPORTED:
+          self._networks_buttons[n.ssid].set_enabled(False)
         self._forget_networks_buttons[n.ssid] = Button("Forget", partial(self._forget_networks_buttons_callback, n), button_style=ButtonStyle.FORGET_WIFI,
                                                        font_size=45)
 
