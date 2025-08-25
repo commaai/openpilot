79dd2e05355fdef5c2dbb14dbc6fc905059ef378
felsager <d.felsager@gmail.com>
2025-08-25 10:15:29 -0700

    added helpers to torqued offset unit tests

--- selfdrive/controls/tests/test_torqued_lat_accel_offset.py
+++ selfdrive/controls/tests/test_torqued_lat_accel_offset.py
@@ -8,60 +8,67 @@ from openpilot.selfdrive.locationd.torqued import TorqueEstimator, MIN_BUCKET_PO
 
 np.random.seed(0)
 
-ROLL_BIAS_DEG = 1.0
-TOTAL_POINTS = POINTS_PER_BUCKET*len(STEER_BUCKET_BOUNDS)
-MAX_STEER = STEER_BUCKET_BOUNDS[-1][-1]
-TORQUE_STD = 0.1
-IN_OUT_STD = 0.1
+LA_ERR_STD = 1.0
+INPUT_NOISE_STD = 0.1
 V_EGO = 30.0
 
-# how much larger roll compensation is than it should be
-roll_compensation_bias = ACCELERATION_DUE_TO_GRAVITY*float(np.sin(np.deg2rad(ROLL_BIAS_DEG)))
-gt_torque_tune = structs.CarParams.LateralTorqueTuning(latAccelFactor=2.0, latAccelOffset=-roll_compensation_bias, friction=0.2)
+WARMUP_BUCKET_POINTS = (1.5*MIN_BUCKET_POINTS).astype(int)
+STRAIGHT_ROAD_LA_BOUNDS = (0.02, 0.03)
 
-def test_estimated_offset():
+ROLL_BIAS_DEG = 1.0
+ROLL_COMPENSATION_BIAS = ACCELERATION_DUE_TO_GRAVITY*float(np.sin(np.deg2rad(ROLL_BIAS_DEG)))
+TORQUE_TUNE = structs.CarParams.LateralTorqueTuning(latAccelFactor=2.0, latAccelOffset=0.0, friction=0.2)
+TORQUE_TUNE_BIASED = structs.CarParams.LateralTorqueTuning(latAccelFactor=2.0, latAccelOffset=-ROLL_COMPENSATION_BIAS, friction=0.2)
+
+
+def generate_inputs(torque_tune, la_err_std, input_noise_std=None):
   rng = np.random.default_rng(0)
-  steer_torques = rng.normal(scale=TORQUE_STD, size=TOTAL_POINTS)
-  steer_torques = steer_torques[abs(steer_torques) < MAX_STEER]
-  la_errs = rng.normal(scale=0.5, size=steer_torques.size)
-  frictions = np.array([get_friction(la_err, 0.0, FRICTION_THRESHOLD, gt_torque_tune) for la_err in la_errs])
-  lat_accels = gt_torque_tune.latAccelFactor*steer_torques + gt_torque_tune.latAccelOffset + frictions
-  steer_torques += rng.normal(scale=IN_OUT_STD, size=steer_torques.size)
-  lat_accels += rng.normal(scale=IN_OUT_STD, size=steer_torques.size)
+  steer_torques = np.concat([rng.uniform(bnd[0], bnd[1], pts) for bnd, pts in zip(STEER_BUCKET_BOUNDS, WARMUP_BUCKET_POINTS, strict=True)])
+  la_errs = rng.normal(scale=la_err_std, size=steer_torques.size)
+  frictions = np.array([get_friction(la_err, 0.0, FRICTION_THRESHOLD, torque_tune) for la_err in la_errs])
+  lat_accels = torque_tune.latAccelFactor*steer_torques + torque_tune.latAccelOffset + frictions
+  if input_noise_std is not None:
+    steer_torques += rng.normal(scale=input_noise_std, size=steer_torques.size)
+    lat_accels += rng.normal(scale=input_noise_std, size=steer_torques.size)
+  return steer_torques, lat_accels
+
+def get_warmed_up_estimator(steer_torques, lat_accels):
   est = TorqueEstimator(car.CarParams())
   for steer_torque, lat_accel in zip(steer_torques, lat_accels, strict=True):
     est.filtered_points.add_point(steer_torque, lat_accel)
-  msg = est.get_msg()
-  # TODO add lataccelfactor and friction check when we have more accurate estimates
-  assert abs(msg.liveTorqueParameters.latAccelOffsetRaw - gt_torque_tune.latAccelOffset) < 0.05
+  return est
 
-def test_straight_road_roll_bias():
+def simulate_straight_road_msgs(est):
   rng = np.random.default_rng(0)
-  steer_torques = np.concat([rng.uniform(bound[0], bound[1], int(points))
-                             for bound, points in zip(STEER_BUCKET_BOUNDS, 1.5*MIN_BUCKET_POINTS, strict=True)])
-  la_errs = rng.normal(size=steer_torques.size)
-  frictions = np.array([get_friction(la_err, 0.0, FRICTION_THRESHOLD, gt_torque_tune) for la_err in la_errs])
-  lat_accels = gt_torque_tune.latAccelFactor*steer_torques + frictions
-  est = TorqueEstimator(car.CarParams())
-  for steer_torque, lat_accel in zip(steer_torques, lat_accels, strict=True):
-    est.filtered_points.add_point(steer_torque, lat_accel)
+  carControl = messaging.new_message('carControl').carControl
+  carControl.latActive = True
+  carOutput = messaging.new_message('carOutput').carOutput
+  carState = messaging.new_message('carState').carState
+  carState.vEgo = V_EGO
+  carState.steeringPressed = False
+  livePose = messaging.new_message('livePose').livePose
   for i in range(2*POINTS_PER_BUCKET):
     t = i*DT_MDL
     sgn = (-1)**(i < POINTS_PER_BUCKET)
-    steer_torque = sgn * rng.uniform(0.02, 0.03)
-    lat_accel = gt_torque_tune.latAccelFactor * steer_torque
-    carControl = messaging.new_message('carControl').carControl
-    carControl.latActive = True
-    carOutput = messaging.new_message('carOutput').carOutput
+    steer_torque = sgn * rng.uniform(STRAIGHT_ROAD_LA_BOUNDS[0], STRAIGHT_ROAD_LA_BOUNDS[1])
+    lat_accel = TORQUE_TUNE.latAccelFactor * steer_torque
     carOutput.actuatorsOutput.torque = -steer_torque
-    carState = messaging.new_message('carState').carState
-    carState.vEgo = V_EGO
-    carState.steeringPressed = False
-    livePose = messaging.new_message('livePose').livePose
     livePose.orientationNED.x = float(np.deg2rad(ROLL_BIAS_DEG))
     livePose.angularVelocityDevice.z = lat_accel / V_EGO
     msgs = {'carControl': carControl, 'carOutput': carOutput, 'carState': carState, 'livePose': livePose}
     for which, msg in msgs.items():
       est.handle_log(t, which, msg)
+
+def test_estimated_offset():
+  steer_torques, lat_accels = generate_inputs(TORQUE_TUNE_BIASED, la_err_std=LA_ERR_STD, input_noise_std=INPUT_NOISE_STD)
+  est = get_warmed_up_estimator(steer_torques, lat_accels)
+  msg = est.get_msg()
+  # TODO add lataccelfactor and friction check when we have more accurate estimates
+  assert abs(msg.liveTorqueParameters.latAccelOffsetRaw - TORQUE_TUNE_BIASED.latAccelOffset) < 0.03
+
+def test_straight_road_roll_bias():
+  steer_torques, lat_accels = generate_inputs(TORQUE_TUNE, la_err_std=LA_ERR_STD, input_noise_std=INPUT_NOISE_STD)
+  est = get_warmed_up_estimator(steer_torques, lat_accels)
+  simulate_straight_road_msgs(est)
   msg = est.get_msg()
   assert (msg.liveTorqueParameters.latAccelOffsetRaw < -0.05) and np.isfinite(msg.liveTorqueParameters.latAccelOffsetRaw)
