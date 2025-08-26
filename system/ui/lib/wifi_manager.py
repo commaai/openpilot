c421a7fa3631b9cd31f423eac8ab8b7953d654a7
Shane Smiskol <shane@smiskol.com>
2025-08-25 17:42:02 -0700

    rm duplicate imports

--- system/ui/lib/wifi_manager.py
+++ system/ui/lib/wifi_manager.py
@@ -24,11 +24,6 @@ from openpilot.system.ui.lib.networkmanager import (NM, NM_PROPERTIES_IFACE, NM_
                                                     NM_DEVICE_STATE_REASON_NEW_ACTIVATION,
                                                     NMDeviceState)
 
-from openpilot.system.ui.lib.networkmanager import (NM, NM_PATH, NM_IFACE, NM_SETTINGS_PATH, NM_SETTINGS_IFACE,
-                                                    NM_CONNECTION_IFACE, NM_WIRELESS_IFACE, NM_PROPERTIES_IFACE,
-                                                    NM_DEVICE_IFACE, NM_DEVICE_STATE_REASON_SUPPLICANT_DISCONNECT,
-                                                    NMDeviceState)
-
 TETHERING_IP_ADDRESS = "192.168.43.1"
 DEFAULT_TETHERING_PASSWORD = "swagswagcomma"
 
