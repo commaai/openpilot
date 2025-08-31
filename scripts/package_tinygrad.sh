00e18122bdbe63571035ee28bfa9b5a5e3db3d91
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-08-31 09:04:00 -0700

    fix ruff

--- scripts/package_tinygrad.sh
+++ scripts/package_tinygrad.sh
@@ -11,6 +11,7 @@ COMMIT="$(grep tinygrad pyproject.toml | grep -oE '[0-9a-f]{40}')"
 # package tinygrad for AGNOS
 wget -O selfdrive/modeld/compile3.py \
   https://raw.githubusercontent.com/tinygrad/tinygrad/$COMMIT/examples/openpilot/compile3.py
+sed -i '1i#ruff: noqa' selfdrive/modeld/compile3.py
 
 uv sync
 cp -r .venv/lib/python3.11/site-packages/tinygrad/ third_party/
--- selfdrive/modeld/compile3.py
+++ selfdrive/modeld/compile3.py
@@ -1,3 +1,4 @@
+#ruff: noqa
 import os, sys, pickle, time
 import numpy as np
 if "FLOAT16" not in os.environ: os.environ["FLOAT16"] = "1"
