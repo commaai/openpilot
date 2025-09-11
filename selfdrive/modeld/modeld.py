cc665622e498744d95106840ddfcac3c680fe20a
felsager <d.felsager@gmail.com>
2025-09-11 11:47:30 -0700

    keep it clean

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -378,7 +378,6 @@ def main(demo=False):
       posenet_send = messaging.new_message('cameraOdometry')
 
       action = get_action_from_model(model_output, prev_action, lat_delay + DT_MDL, long_delay + DT_MDL, v_ego)
-
       prev_action = action
       fill_model_msg(drivingdata_send, modelv2_send, model_output, action,
                      publish_state, meta_main.frame_id, meta_extra.frame_id, frame_id,
