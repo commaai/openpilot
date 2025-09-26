f9f98d5d708fb15cf1ebef4bdace577f0e347658
Shane Smiskol <shane@smiskol.com>
2025-09-25 18:52:58 -0700

    damped filter

--- common/filter_simple.py
+++ common/filter_simple.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class FirstOrderFilter:
   def __init__(self, x0, rc, dt, initialized=True):
     self.x = x0
@@ -15,3 +18,41 @@ class FirstOrderFilter:
       self.initialized = True
       self.x = x
     return self.x
+
+
+class BounceFilter(FirstOrderFilter):
+  def __init__(self, x0, rc, dt, initialized=True):
+    super().__init__(x0, rc, dt, initialized)
+    # self.velocity = FirstOrderFilter(0.0, rc * 1.5, dt)
+    self.velocity = FirstOrderFilter(0.0, 0.1, dt, initialized)
+
+  def update(self, x):
+    prev_x = self.x
+    # new_x = super().update(x)
+    self.velocity.update((x - prev_x) / 10)
+    # self.velocity.x *= 0.9
+    self.velocity.update(0.0)
+    # self.velocity.update(0.0)
+    self.x += self.velocity.x
+    print(self.velocity.x)
+    # self.x = np.interp(abs(vel), [0, 100], [prev_x, new_x])
+    # print(vel)
+    # new_x += vel
+    # return new_x
+    # new_x = super().update(x)
+    # slow the initial move with velocity
+    return self.x
+
+  #
+  # def update(self, x):
+  #   prev_x = self.x
+  #   new_x = super().update(x)
+  #   vel = self.velocity.update(x - prev_x)
+  #   print(vel)
+  #   self.x = np.interp(abs(vel), [0, 100], [prev_x, new_x])
+  #   # print(vel)
+  #   # new_x += vel
+  #   # return new_x
+  #   # new_x = super().update(x)
+  #   # slow the initial move with velocity
+  #   return self.x
