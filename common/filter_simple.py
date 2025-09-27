8de8c3eb00383cb5b8fa18c41ddc4899dccc1e61
Shane Smiskol <shane@smiskol.com>
2025-09-26 21:38:59 -0700

    raylib: 20 FPS onroad (#36208)
    * 20
    
    * dynamic fps
    
    * flip
    
    * init to be safe
    
    * fix possible fps weirdness
    
    * gate on change
    
    * not now
    
    * rev

--- common/filter_simple.py
+++ common/filter_simple.py
@@ -1,16 +1,21 @@
 class FirstOrderFilter:
   def __init__(self, x0, rc, dt, initialized=True):
     self.x = x0
-    self.dt = dt
+    self._dt = dt
     self.update_alpha(rc)
     self.initialized = initialized
 
+  def update_dt(self, dt):
+    self._dt = dt
+    self.update_alpha(self._rc)
+
   def update_alpha(self, rc):
-    self.alpha = self.dt / (rc + self.dt)
+    self._rc = rc
+    self._alpha = self._dt / (self._rc + self._dt)
 
   def update(self, x):
     if self.initialized:
-      self.x = (1. - self.alpha) * self.x + self.alpha * x
+      self.x = (1. - self._alpha) * self.x + self._alpha * x
     else:
       self.initialized = True
       self.x = x
