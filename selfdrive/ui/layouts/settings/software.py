d70abf329ed8a1548f2c2ed20f3af5a359a665f5
Shane Smiskol <shane@smiskol.com>
2025-10-01 16:20:47 -0700

    clean up

--- selfdrive/ui/layouts/settings/software.py
+++ selfdrive/ui/layouts/settings/software.py
@@ -1,5 +1,5 @@
 import os
-from openpilot.common.params import Params
+from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.lib.application import gui_app
 from openpilot.system.ui.widgets import Widget, DialogResult
 from openpilot.system.ui.widgets.confirm_dialog import ConfirmDialog
@@ -11,18 +11,20 @@ class SoftwareLayout(Widget):
   def __init__(self):
     super().__init__()
 
-    self._params = Params()
-    self._download_btn = None
-    self._install_btn = None
-    self._version_item = None
+    # Create text item for current version
+    self._version_item = text_item("Current Version", ui_state.params.get("UpdaterCurrentDescription", "Unknown"))
+
+    # Create download button with initial state
+    self._download_btn = button_item("Download", "CHECK", callback=self._on_download_update)
+
+    # Create install button (initially hidden)
+    self._install_btn = button_item("Install Update", "INSTALL", callback=self._on_install_update)
+    self._install_btn.set_visible(False)
+
     items = self._init_items()
     self._scroller = Scroller(items, line_separator=True, spacing=0)
 
   def _init_items(self):
-    self._version_item = text_item("Current Version", "")
-    self._download_btn = button_item("Download", "CHECK", callback=self._on_download_update)
-    self._install_btn = button_item("Install Update", "INSTALL", callback=self._on_install_update)
-
     items = [
       self._version_item,
       self._download_btn,
@@ -33,19 +35,18 @@ class SoftwareLayout(Widget):
     return items
 
   def _render(self, rect):
-    self._update_labels()
     self._scroller.render(rect)
 
-  def _update_labels(self):
+  def _update_state(self):
     # Update current version
-    current_desc = self._params.get("UpdaterCurrentDescription", "Unknown")
+    current_desc = ui_state.params.get("UpdaterCurrentDescription", "Unknown")
     self._version_item.set_description(current_desc)
 
     # Update download button state
-    updater_state = self._params.get("UpdaterState", "idle")
-    failed_count = int(self._params.get("UpdateFailedCount", "0"))
-    fetch_available = self._params.get_bool("UpdaterFetchAvailable")
-    update_available = self._params.get_bool("UpdateAvailable")
+    updater_state = ui_state.params.get("UpdaterState", "idle")
+    failed_count = int(ui_state.params.get("UpdateFailedCount", "0"))
+    fetch_available = ui_state.params.get_bool("UpdaterFetchAvailable")
+    update_available = ui_state.params.get_bool("UpdateAvailable")
 
     if updater_state != "idle":
       self._download_btn.set_enabled(False)
@@ -58,7 +59,7 @@ class SoftwareLayout(Widget):
         self._download_btn.set_description("update available")
         self._download_btn.set_text("DOWNLOAD")
       else:
-        last_update = self._params.get("LastUpdateTime", "")
+        last_update = ui_state.params.get("LastUpdateTime", "")
         if last_update:
           # TODO: Format time ago
           self._download_btn.set_description(f"up to date, last checked {last_update}")
@@ -69,7 +70,7 @@ class SoftwareLayout(Widget):
 
     # Update install button
     if update_available:
-      new_desc = self._params.get("UpdaterNewDescription", "")
+      new_desc = ui_state.params.get("UpdaterNewDescription", "")
       self._install_btn.set_description(new_desc)
       self._install_btn.set_visible(True)
     else:
@@ -87,14 +88,14 @@ class SoftwareLayout(Widget):
   def _on_uninstall(self):
     def handle_uninstall_confirmation(result):
       if result == DialogResult.CONFIRM:
-        self._params.put_bool("DoUninstall", True)
+        ui_state.params.put_bool("DoUninstall", True)
 
     dialog = ConfirmDialog("Are you sure you want to uninstall?", "Uninstall")
     gui_app.set_modal_overlay(dialog, callback=handle_uninstall_confirmation)
 
   def _on_install_update(self):
     # Trigger reboot to install update
-    self._params.put_bool("DoReboot", True)
+    ui_state.params.put_bool("DoReboot", True)
 
   def _on_select_branch(self):
     pass
