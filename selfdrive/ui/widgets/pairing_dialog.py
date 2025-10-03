ddd9661648fcf27f9b297538310fae7168f12314
Shane Smiskol <shane@smiskol.com>
2025-10-02 20:53:41 -0700

    fix pairing dialog

--- selfdrive/ui/widgets/pairing_dialog.py
+++ selfdrive/ui/widgets/pairing_dialog.py
@@ -13,6 +13,19 @@ from openpilot.system.ui.lib.text_measure import measure_text_cached
 from openpilot.selfdrive.ui.ui_state import ui_state
 
 
+class IconButton(Widget):
+  def __init__(self, texture: rl.Texture):
+    super().__init__()
+    self._texture = texture
+
+  def _render(self, rect: rl.Rectangle) -> bool | int | None:
+    color = rl.Color(180, 180, 180, 150) if self.is_pressed else rl.WHITE
+    draw_x = rect.x + (rect.width - self._texture.width) / 2
+    draw_y = rect.y + (rect.height - self._texture.height) / 2
+    rl.draw_texture(self._texture, int(draw_x), int(draw_y), color)
+    return False
+
+
 class PairingDialog(Widget):
   """Dialog for device pairing with QR code."""
 
@@ -23,6 +36,8 @@ class PairingDialog(Widget):
     self.params = Params()
     self.qr_texture: rl.Texture | None = None
     self.last_qr_generation = 0
+    self._close_btn = IconButton(gui_app.texture("icons/close.png", 80, 80))
+    self._close_btn.set_click_callback(lambda: gui_app.set_modal_overlay(None))
 
   def _get_pairing_url(self) -> str:
     try:
@@ -78,19 +93,9 @@ class PairingDialog(Widget):
 
     # Close button
     close_size = 80
-    close_icon = gui_app.texture("icons/close.png", close_size, close_size)
-    close_rect = rl.Rectangle(content_rect.x, y, close_size, close_size)
-
-    mouse_pos = rl.get_mouse_position()
-    is_hover = rl.check_collision_point_rec(mouse_pos, close_rect)
-    is_pressed = rl.is_mouse_button_down(rl.MouseButton.MOUSE_BUTTON_LEFT)
-    is_released = rl.is_mouse_button_released(rl.MouseButton.MOUSE_BUTTON_LEFT)
-
-    color = rl.Color(180, 180, 180, 150) if (is_hover and is_pressed) else rl.WHITE
-    rl.draw_texture(close_icon, int(content_rect.x), int(y), color)
-
-    if (is_hover and is_released) or rl.is_key_pressed(rl.KeyboardKey.KEY_ESCAPE):
-      return 1
+    pad = 20
+    close_rect = rl.Rectangle(content_rect.x - pad, y - pad, close_size + pad * 2, close_size + pad * 2)
+    self._close_btn.render(close_rect)
 
     y += close_size + 40
 
