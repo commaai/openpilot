1b2b5226186dd9e60a4ba6481211c8710e200c2b
kostas.pats <35031825+kostas1507@users.noreply.github.com>
2025-10-08 16:55:13 -0700

    remove excess spaces
    Co-authored-by: Maxime Desroches <desroches.maxime@gmail.com>

--- system/webrtc/tests/test_stream_session.py
+++ system/webrtc/tests/test_stream_session.py
@@ -84,7 +84,7 @@ class TestStreamSession:
       if i == 0:
         start_ns = time.monotonic_ns()
         start_pts = packet.pts
-      assert abs(i+ packet.pts - (start_pts + (((time.monotonic_ns()-start_ns) * VIDEO_CLOCK_RATE)//1_000_000_000))) < 450 #5ms
+      assert abs(i + packet.pts - (start_pts + (((time.monotonic_ns() - start_ns) * VIDEO_CLOCK_RATE) // 1_000_000_000))) < 450 #5ms
       assert packet.size == 0
 
   def test_input_audio_track(self, mocker):
