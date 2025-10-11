41f562b513c539e9b0ab451a10f05a44a96c25fc
Shane Smiskol <shane@smiskol.com>
2025-10-11 05:37:42 -0700

    fixxx

--- system/ui/widgets/list_view.py
+++ system/ui/widgets/list_view.py
@@ -188,8 +188,14 @@ class DualButtonAction(ItemAction):
     button_width = (rect.width - button_spacing) / 2
     button_y = rect.y + (rect.height - button_height) / 2
 
+    # expand one to full width if other is not visible
     left_rect = rl.Rectangle(rect.x, button_y, button_width, button_height)
     right_rect = rl.Rectangle(rect.x + button_width + button_spacing, button_y, button_width, button_height)
+    if not self.left_button.is_visible:
+      right_rect.x = rect.x
+      right_rect.width = rect.width
+    elif not self.right_button.is_visible:
+      left_rect.width = rect.width
 
     # Render buttons
     self.left_button.render(left_rect)
