8c47d21d216cb26315fa880f8ac76c853e10dd12
Shane Smiskol <shane@smiskol.com>
2025-10-11 06:56:36 -0700

    flip!

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -221,7 +221,7 @@ if __name__ == "__main__":
     cloudlog.exception("Manager failed to start")
 
     try:
-      managed_processes['ui'].stop()
+      managed_processes['raylib_ui'].stop()
     except Exception:
       pass
 
