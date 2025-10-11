051866ef0ec0080d20cdef7a09057f6bc22130c2
Shane Smiskol <shane@smiskol.com>
2025-10-11 04:06:40 -0700

    speed

--- system/ui/lib/application.py
+++ system/ui/lib/application.py
@@ -73,33 +73,30 @@ class MouseEvent(NamedTuple):
 class MouseState:
   def __init__(self, scale: float = 1.0):
     self._scale = scale
-    self._events: deque[MouseEvent] = deque(maxlen=MOUSE_THREAD_RATE)  # bound event list
+    self._events: list[MouseEvent] = []
     self._prev_mouse_event: list[MouseEvent | None] = [None] * MAX_TOUCH_SLOTS
 
     self._rk = Ratekeeper(MOUSE_THREAD_RATE)
-    self._lock = threading.Lock()
-    self._exit_event = threading.Event()
+    self._do_exit = False
     self._thread = None
 
   def get_events(self) -> list[MouseEvent]:
-    with self._lock:
-      events = list(self._events)
-      self._events.clear()
+    events, self._events = self._events, []
     return events
 
   def start(self):
-    self._exit_event.clear()
+    self._do_exit = False
     if self._thread is None or not self._thread.is_alive():
       self._thread = threading.Thread(target=self._run_thread, daemon=True)
       self._thread.start()
 
   def stop(self):
-    self._exit_event.set()
+    self._do_exit = True
     if self._thread is not None and self._thread.is_alive():
       self._thread.join()
 
   def _run_thread(self):
-    while not self._exit_event.is_set():
+    while not self._do_exit:
       rl.poll_input_events()
       self._handle_mouse_event()
       self._rk.keep_time()
@@ -119,7 +116,8 @@ class MouseState:
       )
       # Only add changes
       if self._prev_mouse_event[slot] is None or ev[:-1] != self._prev_mouse_event[slot][:-1]:
-        with self._lock:
+        # Drop new events if we have too many queued
+        if len(self._events) < MOUSE_THREAD_RATE:
           self._events.append(ev)
         self._prev_mouse_event[slot] = ev
 
