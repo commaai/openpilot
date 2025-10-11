c30c5f244764fd0fe660c2364588b33a2146080b
Shane Smiskol <shane@smiskol.com>
2025-10-11 07:29:53 -0700

    bump double increase for tol

--- selfdrive/test/test_onroad.py
+++ selfdrive/test/test_onroad.py
@@ -32,7 +32,7 @@ CPU usage budget
 TEST_DURATION = 25
 LOG_OFFSET = 8
 
-MAX_TOTAL_CPU = 300.  # total for all 8 cores
+MAX_TOTAL_CPU = 315.  # total for all 8 cores
 PROCS = {
   # Baseline CPU usage by process
   "selfdrive.controls.controlsd": 16.0,
@@ -42,7 +42,7 @@ PROCS = {
   "./encoderd": 13.0,
   "./camerad": 10.0,
   "selfdrive.controls.plannerd": 8.0,
-  "selfdrive.ui.ui": 25.0,
+  "selfdrive.ui.ui": 24.0,
   "system.sensord.sensord": 13.0,
   "selfdrive.controls.radard": 2.0,
   "selfdrive.modeld.modeld": 22.0,
