74c5bfd87a7ab722571b1b593bb7e7748f88e07e
at <adriantabirta@gmail.com>
2025-10-16 01:30:47 +0300

    added deps fix

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -32,6 +32,10 @@ else
     brew up
 fi
 
+# openssl fix
+if brew list openssl@1.1 &>/dev/null; then brew unlink openssl@1.1; fi
+if brew list openssl@3 &>/dev/null; then brew link --overwrite openssl@3; fi
+
 brew bundle --file=- <<-EOS
 brew "git-lfs"
 brew "capnp"
