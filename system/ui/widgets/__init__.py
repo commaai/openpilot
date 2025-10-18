44fc9ec680391863ca710466847efd0eb4bceb80
Shane Smiskol <shane@smiskol.com>
2025-10-18 00:52:34 -0700

    clip to parent rect

--- system/ui/widgets/__init__.py
+++ system/ui/widgets/__init__.py
@@ -75,6 +75,7 @@ class Widget(abc.ABC):
     if changed:
       self._update_layout_rects()
 
+  @property
   def _hit_rect(self):
     # clip to parent rect
     if self._parent_rect is None:
@@ -84,7 +85,9 @@ class Widget(abc.ABC):
     y = max(self._rect.y, self._parent_rect.y)
     w = min(self._rect.x + self._rect.width, self._parent_rect.x + self._parent_rect.width) - x
     h = min(self._rect.y + self._rect.height, self._parent_rect.y + self._parent_rect.height) - y
-    return rl.Rectangle(x, y, w, h)
+    ret = rl.Rectangle(x, y, w, h)
+    rl.draw_rectangle_lines_ex(ret, 1, rl.RED)
+    return ret
 
   def render(self, rect: rl.Rectangle = None) -> bool | int | None:
     if rect is not None:
@@ -106,7 +109,7 @@ class Widget(abc.ABC):
         # Ignores touches/presses that start outside our rect
         # Allows touch to leave the rect and come back in focus if mouse did not release
         if mouse_event.left_pressed and self._touch_valid():
-          if rl.check_collision_point_rec(mouse_event.pos, self._rect):
+          if rl.check_collision_point_rec(mouse_event.pos, self._hit_rect):
             self._handle_mouse_press(mouse_event.pos)
             self.__is_pressed[mouse_event.slot] = True
             self.__tracking_is_pressed[mouse_event.slot] = True
@@ -117,18 +120,18 @@ class Widget(abc.ABC):
           self.__tracking_is_pressed[mouse_event.slot] = False
 
         elif mouse_event.left_released:
-          if self.__is_pressed[mouse_event.slot] and rl.check_collision_point_rec(mouse_event.pos, self._rect):
+          if self.__is_pressed[mouse_event.slot] and rl.check_collision_point_rec(mouse_event.pos, self._hit_rect):
             self._handle_mouse_release(mouse_event.pos)
           self.__is_pressed[mouse_event.slot] = False
           self.__tracking_is_pressed[mouse_event.slot] = False
 
         # Mouse/touch is still within our rect
-        elif rl.check_collision_point_rec(mouse_event.pos, self._rect):
+        elif rl.check_collision_point_rec(mouse_event.pos, self._hit_rect):
           if self.__tracking_is_pressed[mouse_event.slot]:
             self.__is_pressed[mouse_event.slot] = True
 
         # Mouse/touch left our rect but may come back into focus later
-        elif not rl.check_collision_point_rec(mouse_event.pos, self._rect):
+        elif not rl.check_collision_point_rec(mouse_event.pos, self._hit_rect):
           self.__is_pressed[mouse_event.slot] = False
 
     return ret
