5c850b43a994346c64a8d35068ad45f079fe9e5a
Bruce Wayne <harald.the.engineer@gmail.com>
2025-10-20 13:05:01 -0700

    torque gains not car specific

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -21,8 +21,8 @@ from openpilot.common.pid import PIDController
 # to be overcome to move it at all, this is compensated for too.
 
 KP = 1.0
-KI = 0.3
-KD = 0.0
+KI = 0.1
+KD = 0.3
 INTERP_SPEEDS = [1, 1.5, 2.0, 3.0, 5, 7.5, 10, 15, 30]
 KP_INTERP = [250, 120, 65, 30, 11.5, 5.5, 3.5, 2.0, KP]
 
