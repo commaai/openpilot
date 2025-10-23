855202d1deaa21f00bebb3870e995160eac23c6a
Shane Smiskol <shane@smiskol.com>
2025-10-22 18:09:44 -0700

    oh this is actually epic

--- system/ui/widgets/__init__.py
+++ system/ui/widgets/__init__.py
@@ -81,12 +81,7 @@ class Widget(abc.ABC):
     if self._parent_rect is None:
       return self._rect
 
-    x = max(self._rect.x, self._parent_rect.x)
-    y = max(self._rect.y, self._parent_rect.y)
-    w = min(self._rect.x + self._rect.width, self._parent_rect.x + self._parent_rect.width) - x
-    h = min(self._rect.y + self._rect.height, self._parent_rect.y + self._parent_rect.height) - y
-    ret = rl.Rectangle(x, y, w, h)
-    return ret
+    return rl.get_collision_rec(self._rect, self._parent_rect)
 
   def render(self, rect: rl.Rectangle = None) -> bool | int | None:
     if rect is not None:
