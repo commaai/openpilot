22f1fd0876fa3ac3dd10cbbc0814df3652b8567d
Shane Smiskol <shane@smiskol.com>
2025-10-22 17:23:01 -0700

    doesn't work

--- selfdrive/ui/tests/test_raylib_ui.py
+++ selfdrive/ui/tests/test_raylib_ui.py
@@ -1,17 +1,8 @@
 import time
-import pytest
 from openpilot.selfdrive.test.helpers import with_processes
-from openpilot.system.ui.lib.multilang import multilang
-from openpilot.common.params import Params
 
 
-@pytest.fixture(autouse=True)
-def set_language_param(language_code):
-  Params().put("LanguageSetting", language_code)
-
-
-@pytest.mark.parametrize("language_code", list(multilang.codes.keys()), ids=list(multilang.codes.keys()))
 @with_processes(["ui"])
-def test_raylib_ui(language_code):
+def test_raylib_ui():
   """Test initialization of the UI widgets is successful."""
   time.sleep(1)
