a63c40acbaaa1fcd0cd0b19120a9e37440fd3522
Maxime Desroches <desroches.maxime@gmail.com>
2025-11-03 16:36:45 -0800

    remove

--- common/api.py
+++ common/api.py
@@ -7,6 +7,10 @@ from openpilot.system.version import get_version
 
 API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
 
+       # file name : type
+KEYS = {"id_rsa" : "RS256",
+        "id_ecdsa" : "ES256"}
+
 class Api:
   def __init__(self, dongle_id):
     self.dongle_id = dongle_id
@@ -48,17 +52,11 @@ def api_get(endpoint, method='GET', timeout=None, access_token=None, **params):
   return requests.request(method, API_HOST + "/" + endpoint, timeout=timeout, headers=headers, params=params)
 
 def get_key_pair():
-  for key in ("ed25519", "id_rsa", "id_ecdsa"):
+  for key in KEYS:
     if os.path.isfile(Paths.persist_root() + f'/comma/{key}') and os.path.isfile(Paths.persist_root() + f'/comma/{key}.pub'):
       with open(Paths.persist_root() + f'/comma/{key}') as private, open(Paths.persist_root() + f'/comma/{key}.pub') as public:
         return key, private.read(), public.read()
   return None, None, None
 
 def get_jwt_algorithm(key_name):
-  if key_name == "id_rsa":
-     return "RS256"
-  elif key_name == "id_ecdsa":
-     return "ES256"
-  elif key_name == "ed25519":
-     return "EdDSA"
-  return None
+  return KEYS.get(key_name, None)
