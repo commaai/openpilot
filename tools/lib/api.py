71b23cf09381dee7942d0ba330cfc38268fd340b
Jason Wen <haibin.wen3@gmail.com>
2025-11-13 03:07:19 -0500

    tools: add retry mechanism for API requests

--- tools/lib/api.py
+++ tools/lib/api.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
 
 # TODO: this should be merged into common.api
@@ -12,16 +13,24 @@ class CommaApi:
       self.session.headers['Authorization'] = 'JWT ' + token
 
   def request(self, method, endpoint, **kwargs):
-    with self.session.request(method, API_HOST + '/' + endpoint, **kwargs) as resp:
-      resp_json = resp.json()
-      if isinstance(resp_json, dict) and resp_json.get('error'):
-        if resp.status_code in [401, 403]:
-          raise UnauthorizedError('Unauthorized. Authenticate with tools/lib/auth.py')
+    retries = 5
+    for i in range(retries):
+      with self.session.request(method, API_HOST + '/' + endpoint, **kwargs) as resp:
+        if 500 <= resp.status_code < 600 and i < (retries - 1):  # don't sleep on last attempt
+          print("API error, retrying in 1s")
+          time.sleep(1)
+          continue
 
-        e = APIError(str(resp.status_code) + ":" + resp_json.get('description', str(resp_json['error'])))
-        e.status_code = resp.status_code
-        raise e
-      return resp_json
+        resp_json = resp.json()
+        if isinstance(resp_json, dict) and resp_json.get('error'):
+          if resp.status_code in [401, 403]:
+            raise UnauthorizedError('Unauthorized. Authenticate with tools/lib/auth.py')
+
+          e = APIError(str(resp.status_code) + ":" + resp_json.get('description', str(resp_json['error'])))
+          e.status_code = resp.status_code
+          raise e
+        return resp_json
+    return None
 
   def get(self, endpoint, **kwargs):
     return self.request('GET', endpoint, **kwargs)
