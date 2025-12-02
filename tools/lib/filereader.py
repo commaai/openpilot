cfb0a1c18ce2c03f71085af251a600959aa08fae
Harald Schäfer <harald.the.engineer@gmail.com>
2025-12-01 23:11:03 -0800

    URLFile multirange (#36740)
    * url file multirange
    
    * cleanup urlfile
    
    * time
    
    * fixup
    
    * raise
    
    * Diskfile

--- tools/lib/filereader.py
+++ tools/lib/filereader.py
@@ -1,4 +1,5 @@
 import os
+import io
 import posixpath
 import socket
 from functools import cache
@@ -41,9 +42,17 @@ def file_exists(fn):
     return URLFile(fn).get_length_online() != -1
   return os.path.exists(fn)
 
+class DiskFile(io.BufferedReader):
+  def get_multi_range(self, ranges: list[tuple[int, int]]) -> list[bytes]:
+    parts = []
+    for r in ranges:
+      self.seek(r[0])
+      parts.append(self.read(r[1] - r[0]))
+    return parts
 
-def FileReader(fn, debug=False):
+def FileReader(fn):
   fn = resolve_name(fn)
   if fn.startswith(("http://", "https://")):
-    return URLFile(fn, debug=debug)
-  return open(fn, "rb")
+    return URLFile(fn)
+  else:
+    return DiskFile(open(fn, "rb"))
