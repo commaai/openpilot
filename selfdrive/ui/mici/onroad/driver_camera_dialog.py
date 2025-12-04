9541125ea12813852aefbdff66fd6e3554754c5b
Shane Smiskol <shane@smiskol.com>
2025-12-03 20:04:36 -0800

    clean up

--- selfdrive/ui/mici/onroad/driver_camera_dialog.py
+++ selfdrive/ui/mici/onroad/driver_camera_dialog.py
@@ -43,6 +43,7 @@ class DriverCameraDialog(NavWidget):
   def stop_dmonitoringmodeld(self):
     ui_state.params.put_bool("IsDriverViewEnabled", False)
     gui_app.set_modal_overlay(None)
+    device.remove_interactive_timeout_callback(self.stop_dmonitoringmodeld)
 
   def show_event(self):
     super().show_event()
