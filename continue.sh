0fbe49bc30486583d219c5177e1b9854082d3831
Maxime Desroches <desroches.maxime@gmail.com>
2025-12-04 00:48:35 -0800

    fix

--- continue.sh
+++ continue.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo chgrp gpu /dev/adsprpc-smd /dev/ion /dev/kgsl-3d0
+sudo chmod 660 /dev/adsprpc-smd /dev/ion /dev/kgsl-3d0
+
 echo -n 1 > /data/params/d/AdbEnabled
 echo -n 1 > /data/params/d/SshEnabled
 cp /usr/comma/setup_keys /data/params/d/GithubSshKeys
