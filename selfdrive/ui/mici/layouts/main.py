027c52ec1a38c4e58d06b5ea3bdd2df785944f61
deanlee <deanlee3@gmail.com>
2025-12-04 23:44:29 +0800

    only instantiate OnboardingWindow when onboarding is needed

--- selfdrive/ui/mici/layouts/main.py
+++ selfdrive/ui/mici/layouts/main.py
@@ -62,9 +62,8 @@ class MiciMainLayout(Widget):
     self._setup_callbacks()
 
     # Start onboarding if terms or training not completed
-    self._onboarding_window = OnboardingWindow()
-    if not self._onboarding_window.completed:
-      gui_app.set_modal_overlay(self._onboarding_window)
+    if not OnboardingWindow.completed():
+      gui_app.set_modal_overlay(OnboardingWindow())
 
   def _setup_callbacks(self):
     self._home_layout.set_callbacks(on_settings=self._on_settings_clicked)
