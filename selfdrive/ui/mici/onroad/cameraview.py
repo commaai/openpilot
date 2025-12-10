4c49457953b82ef75fd46f91e84a3aae5cbefe30
Shane Smiskol <shane@smiskol.com>
2025-12-09 16:56:24 -0800

    debug

--- selfdrive/ui/mici/onroad/cameraview.py
+++ selfdrive/ui/mici/onroad/cameraview.py
@@ -257,41 +257,41 @@ class CameraView(Widget):
     elif not self.client.is_connected():
       # ensure we clear the displayed frame when the connection is lost
       self.frame = None
-    #
-    # if not self.frame:
-    #   self._draw_placeholder(rect)
-    #   return
-    #
-    # tlog("recv")
-    #
-    # transform = self._calc_frame_matrix(rect)
-    # src_rect = rl.Rectangle(0, 0, float(self.frame.width), float(self.frame.height))
-    # # Flip driver camera horizontally
-    # if self._stream_type == VisionStreamType.VISION_STREAM_DRIVER:
-    #   src_rect.width = -src_rect.width
-    #
-    # # Calculate scale
-    # scale_x = rect.width * transform[0, 0]  # zx
-    # scale_y = rect.height * transform[1, 1]  # zy
-    #
-    # # Calculate base position (centered)
-    # x_offset = rect.x + (rect.width - scale_x) / 2
-    # y_offset = rect.y + (rect.height - scale_y) / 2
-    #
-    # x_offset += transform[0, 2] * rect.width / 2
-    # y_offset += transform[1, 2] * rect.height / 2
-    #
-    # dst_rect = rl.Rectangle(x_offset, y_offset, scale_x, scale_y)
-    #
-    # tlog("calc rects")
-    #
-    # # Render with appropriate method
-    # if TICI:
-    #   self._render_egl(src_rect, dst_rect)
-    # else:
-    #   self._render_textures(src_rect, dst_rect)
-    #
-    # tlog("render")
+
+    if not self.frame:
+      self._draw_placeholder(rect)
+      return
+
+    tlog("recv")
+
+    transform = self._calc_frame_matrix(rect)
+    src_rect = rl.Rectangle(0, 0, float(self.frame.width), float(self.frame.height))
+    # Flip driver camera horizontally
+    if self._stream_type == VisionStreamType.VISION_STREAM_DRIVER:
+      src_rect.width = -src_rect.width
+
+    # Calculate scale
+    scale_x = rect.width * transform[0, 0]  # zx
+    scale_y = rect.height * transform[1, 1]  # zy
+
+    # Calculate base position (centered)
+    x_offset = rect.x + (rect.width - scale_x) / 2
+    y_offset = rect.y + (rect.height - scale_y) / 2
+
+    x_offset += transform[0, 2] * rect.width / 2
+    y_offset += transform[1, 2] * rect.height / 2
+
+    dst_rect = rl.Rectangle(x_offset, y_offset, scale_x, scale_y)
+
+    tlog("calc rects")
+
+    # Render with appropriate method
+    if TICI:
+      self._render_egl(src_rect, dst_rect)
+    else:
+      self._render_textures(src_rect, dst_rect)
+
+    tlog("render")
 
     print("*** CameraView timings ***")
     for uid, tlist in times.items():
@@ -369,9 +369,12 @@ class CameraView(Widget):
         return False
       self.last_connection_attempt = current_time
 
-      if not self.client.connect(False) or not self.client.num_buffers:
+      t = time.monotonic()
+      ret = self.client.connect(False)
+      print((time.monotonic() - t) * 1000)
+      if not ret or not self.client.num_buffers:
         return False
-      return False
+      # return False
 
       cloudlog.debug(f"Connected to {self._name} stream: {self._stream_type}, buffers: {self.client.num_buffers}")
       self._initialize_textures()
