285069824b9135e34d11b1b632aba727b334f221
Matt Purnell <matt.purnell98@gmail.com>
2025-12-11 17:18:04 -0600

    lint

--- selfdrive/pandad/__init__.py
+++ selfdrive/pandad/__init__.py
@@ -1,4 +1,4 @@
 # C++ extension, now uses scons to build
 from openpilot.selfdrive.pandad.pandad import can_list_to_can_capnp, can_capnp_to_list
-assert can_list_to_can_capnp
-assert can_capnp_to_list
+assert can_list_to_can_capnp  # type: ignore[truthy-function]
+assert can_capnp_to_list  # type: ignore[truthy-function]
