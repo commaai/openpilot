1391434f54d446857f009d51f1919c2e7b58b652
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-12-11 11:22:08 -0800

    setup: fix uv install fail (#36839)
    * pipefail
    
    * curl retry

--- tools/install_python_dependencies.sh
+++ tools/install_python_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 # Increase the pip timeout to handle TimeoutError
 export PIP_DEFAULT_TIMEOUT=200
@@ -10,7 +10,7 @@ cd "$ROOT"
 
 if ! command -v "uv" > /dev/null 2>&1; then
   echo "installing uv..."
-  curl -LsSf https://astral.sh/uv/install.sh | sh
+  curl -LsSf --retry 5 --retry-delay 5 --retry-all-errors https://astral.sh/uv/install.sh | sh
   UV_BIN="$HOME/.local/bin"
   PATH="$UV_BIN:$PATH"
 fi
