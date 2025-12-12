1ecc11372613337be3b524602f696db6150a0990
Shane Smiskol <shane@smiskol.com>
2025-12-11 17:53:28 -0800

    why not format

--- common/api.py
+++ common/api.py
@@ -7,9 +7,10 @@ from openpilot.system.version import get_version
 
 API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
 
-       # name : jwt signature algorithm
-KEYS = {"id_rsa" : "RS256",
-        "id_ecdsa" : "ES256"}
+# name: jwt signature algorithm
+KEYS = {"id_rsa": "RS256",
+        "id_ecdsa": "ES256"}
+
 
 class Api:
   def __init__(self, dongle_id):
@@ -49,6 +50,7 @@ def api_get(endpoint, method='GET', timeout=None, access_token=None, **params):
   headers['User-Agent'] = "openpilot-" + get_version()
 
   return requests.request(method, API_HOST + "/" + endpoint, timeout=timeout, headers=headers, params=params)
+
 
 def get_key_pair():
   for key in KEYS:
