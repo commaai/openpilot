71b9ba41ac6f114f153ca79376a18b507e0fb774
Shane Smiskol <shane@smiskol.com>
2025-12-17 19:25:01 -0800

    hmm

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -932,13 +932,13 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
   # - CAN data is received, but some message are not received at the right frequency
   # If you're not writing a new car port, this is usually cause by faulty wiring
   EventName.canError: {
-    ET.IMMEDIATE_DISABLE: ImmediateDisableAlert("CAN Error"),
+    ET.IMMEDIATE_DISABLE: ImmediateDisableAlert("Vehicle Configuration Error"),
     ET.PERMANENT: Alert(
-      "CAN Error: Check Connections",
-      "",
+      "Vehicle Configuration Error",
+      "Unexpected CAN Messages",
       AlertStatus.normal, AlertSize.small,
       Priority.LOW, VisualAlert.none, AudibleAlert.none, 1., creation_delay=1.),
-    ET.NO_ENTRY: NoEntryAlert("CAN Error: Check Connections"),
+    ET.NO_ENTRY: NoEntryAlert("Vehicle Configuration Error"),
   },
 
   EventName.canBusMissing: {
