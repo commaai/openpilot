1234a09869ea407efbb78185bb178144a9eddd4a
Shane Smiskol <shane@smiskol.com>
2025-12-28 22:48:37 -0800

    simple kf (not really)

--- voacc_debugging.py
+++ voacc_debugging.py
@@ -1,6 +1,8 @@
-from tools.lib.logreader import LogReader
+from collections import deque
 import matplotlib.pyplot as plt
+from tools.lib.logreader import LogReader
 from selfdrive.controls.radard import RADAR_TO_CAMERA
+from openpilot.common.realtime import DT_MDL
 
 plt.ion()
 
@@ -9,11 +11,19 @@ plt.ion()
 lr = LogReader('https://connect.comma.ai/3f447b402cbe27b6/0000008d--b6a2350b41/572/950', sort_by_time=True)
 lr = sorted(lr, key=lambda m: m.logMonoTime)
 
-radar_data = []
-model_data = []
+CS = None
+
+radar_data = []  # from radard/radar
+model_data = []  # straight from model
+pred_data = []  # kf predictions derived from model
+
+dRel_deque = deque(maxlen=round(1.0 / DT_MDL))
 
 for msg in lr:
-  if msg.which() == 'radarState':
+  if msg.which() == 'carState':
+    CS = msg.carState
+
+  elif msg.which() == 'radarState':
     RS = msg.radarState
 
     if RS.leadOne.status:
@@ -22,10 +32,25 @@ for msg in lr:
   elif msg.which() == 'modelV2':
     MD = msg.modelV2
 
-    if len(MD.leadsV3):
-      lead = MD.leadsV3[0]
-      if lead.prob > 0.5:
-        model_data.append((msg.logMonoTime, lead.x[0] - RADAR_TO_CAMERA, lead.v[0], lead.a[0]))
+    if CS is None:
+      continue
+
+    if not len(MD.leadsV3):
+      continue
+
+    lead = MD.leadsV3[0]
+    if lead.prob > 0.5:
+      dRel = lead.x[0] - RADAR_TO_CAMERA
+      model_data.append((msg.logMonoTime, dRel, lead.v[0], lead.a[0]))
+
+      # simple kf prediction for vlead
+      if len(dRel_deque) == dRel_deque.maxlen:
+        vLead = CS.vEgo + (dRel - dRel_deque[0]) / (DT_MDL * len(dRel_deque))
+        pred_data.append((msg.logMonoTime, dRel, vLead, lead.a[0]))
+
+      dRel_deque.append(dRel)
+    else:
+      dRel_deque.clear()
 
 fig, ax = plt.subplots(3, 1, sharex=True)
 ax[0].plot([d[0] for d in radar_data], [d[1] for d in radar_data], label='radar dRel',
@@ -43,6 +68,9 @@ ax[1].plot([d[0] for d in radar_data], [d[2] for d in radar_data], label='radar 
 ax[1].plot([d[0] for d in model_data], [d[2] for d in model_data], label='model vLead',
            # marker='o', markersize=2, markeredgecolor='blue', markerfacecolor='blue'
            )
+ax[1].plot([d[0] for d in pred_data], [d[2] for d in pred_data], label='predicted vLead',
+           # marker='o', markersize=2, markeredgecolor='green', markerfacecolor='green'
+           )
 ax[1].set_ylabel('vLead (m/s)')
 ax[1].legend()
 
@@ -52,6 +80,9 @@ ax[2].plot([d[0] for d in radar_data], [d[3] for d in radar_data], label='radar 
 ax[2].plot([d[0] for d in model_data], [d[3] for d in model_data], label='model aLead',
            # marker='o', markersize=2, markeredgecolor='blue', markerfacecolor='blue'
            )
+# ax[2].plot([d[0] for d in pred_data], [d[3] for d in pred_data], label='predicted aLead',
+#          # marker='o', markersize=2, markeredgecolor='green', markerfacecolor='green'
+#          )
 ax[2].set_ylabel('aLead (m/s²)')
 ax[2].legend()
 
