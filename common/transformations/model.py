3ee7f1f809991f8449d0e6f280e26f810681dde7
Bruce Wayne <harald.the.engineer@gmail.com>
2026-01-07 16:07:58 -0800

    better img size for alpamayo

--- common/transformations/model.py
+++ common/transformations/model.py
@@ -11,21 +11,10 @@ MEDMODEL_INPUT_SIZE = (512, 256)
 MEDMODEL_YUV_SIZE = (MEDMODEL_INPUT_SIZE[0], MEDMODEL_INPUT_SIZE[1] * 3 // 2)
 MEDMODEL_CY = 47.6
 
-medmodel_fl = 910.0
+medmodel_fl = 955.0
 medmodel_intrinsics = np.array([
   [medmodel_fl,  0.0,  0.5 * MEDMODEL_INPUT_SIZE[0]],
   [0.0,  medmodel_fl,                   MEDMODEL_CY],
-  [0.0,  0.0,                                   1.0]])
-
-
-# BIG model
-BIGMODEL_INPUT_SIZE = (1024, 512)
-BIGMODEL_YUV_SIZE = (BIGMODEL_INPUT_SIZE[0], BIGMODEL_INPUT_SIZE[1] * 3 // 2)
-
-bigmodel_fl = 910.0
-bigmodel_intrinsics = np.array([
-  [bigmodel_fl,  0.0,  0.5 * BIGMODEL_INPUT_SIZE[0]],
-  [0.0,  bigmodel_fl,             256 + MEDMODEL_CY],
   [0.0,  0.0,                                   1.0]])
 
 
@@ -33,10 +22,10 @@ bigmodel_intrinsics = np.array([
 SBIGMODEL_INPUT_SIZE = (512, 256)
 SBIGMODEL_YUV_SIZE = (SBIGMODEL_INPUT_SIZE[0], SBIGMODEL_INPUT_SIZE[1] * 3 // 2)
 
-sbigmodel_fl = 455.0
+sbigmodel_fl = 955/4
 sbigmodel_intrinsics = np.array([
   [sbigmodel_fl,  0.0,  0.5 * SBIGMODEL_INPUT_SIZE[0]],
-  [0.0,  sbigmodel_fl,      0.5 * (256 + MEDMODEL_CY)],
+  [0.0,  sbigmodel_fl,      0.5 * (SBIGMODEL_INPUT_SIZE[1])],
   [0.0,  0.0,                                     1.0]])
 
 DM_INPUT_SIZE = (1440, 960)
@@ -46,17 +35,12 @@ dmonitoringmodel_intrinsics = np.array([
   [0.0, dmonitoringmodel_fl, DM_INPUT_SIZE[1]/2 - (_ar_ox_fisheye.height - DM_INPUT_SIZE[1])/2],
   [0.0,  0.0, 1.0]])
 
-bigmodel_frame_from_calib_frame = np.dot(bigmodel_intrinsics,
-  get_view_frame_from_calib_frame(0, 0, 0, 0))
-
 
 sbigmodel_frame_from_calib_frame = np.dot(sbigmodel_intrinsics,
   get_view_frame_from_calib_frame(0, 0, 0, 0))
 
 medmodel_frame_from_calib_frame = np.dot(medmodel_intrinsics,
   get_view_frame_from_calib_frame(0, 0, 0, 0))
-
-medmodel_frame_from_bigmodel_frame = np.dot(medmodel_intrinsics, np.linalg.inv(bigmodel_intrinsics))
 
 calib_from_medmodel = np.linalg.inv(medmodel_frame_from_calib_frame[:, :3])
 calib_from_sbigmodel = np.linalg.inv(sbigmodel_frame_from_calib_frame[:, :3])
