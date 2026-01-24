e9d0170044f619b13ab68f95660b212b865db96d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-01-24 11:48:04 -0800

    cleanup

--- selfdrive/pandad/pandad_api_impl.py
+++ selfdrive/pandad/pandad_api_impl.py
@@ -3,6 +3,28 @@ from cereal import log
 
 NO_TRAVERSAL_LIMIT = 2**64 - 1
 
+# Cache schema fields for faster access (avoids string lookup on each field access)
+_cached_reader_fields = None  # (address_field, dat_field, src_field) for reading
+_cached_writer_fields = None  # (address_field, dat_field, src_field) for writing
+
+
+def _get_reader_fields(schema):
+  """Get cached schema field objects for reading."""
+  global _cached_reader_fields
+  if _cached_reader_fields is None:
+    fields = schema.fields
+    _cached_reader_fields = (fields['address'], fields['dat'], fields['src'])
+  return _cached_reader_fields
+
+
+def _get_writer_fields(schema):
+  """Get cached schema field objects for writing."""
+  global _cached_writer_fields
+  if _cached_writer_fields is None:
+    fields = schema.fields
+    _cached_writer_fields = (fields['address'], fields['dat'], fields['src'])
+  return _cached_writer_fields
+
 
 def can_list_to_can_capnp(can_msgs, msgtype='can', valid=True):
   """Convert list of CAN messages to Cap'n Proto serialized bytes.
@@ -15,16 +37,22 @@ def can_list_to_can_capnp(can_msgs, msgtype='can', valid=True):
   Returns:
     Cap'n Proto serialized bytes
   """
-  dat = log.Event.new_message(
-    valid=valid,
-    logMonoTime=int(time.monotonic() * 1e9)
-  )
+  global _cached_writer_fields
 
+  dat = log.Event.new_message(valid=valid, logMonoTime=int(time.monotonic() * 1e9))
   can_data = dat.init(msgtype, len(can_msgs))
-  for i, msg in enumerate(can_msgs):
-    can_data[i].address = msg[0]
-    can_data[i].dat = msg[1]
-    can_data[i].src = msg[2]
+
+  # Cache schema fields on first call
+  if _cached_writer_fields is None and len(can_msgs) > 0:
+    _cached_writer_fields = _get_writer_fields(can_data[0].schema)
+
+  if _cached_writer_fields is not None:
+    addr_f, dat_f, src_f = _cached_writer_fields
+    for i, msg in enumerate(can_msgs):
+      f = can_data[i]
+      f._set_by_field(addr_f, msg[0])
+      f._set_by_field(dat_f, msg[1])
+      f._set_by_field(src_f, msg[2])
 
   return dat.to_bytes()
 
@@ -39,10 +67,22 @@ def can_capnp_to_list(strings, msgtype='can'):
   Returns:
     List of tuples [(nanos, [(address, data, src), ...]), ...]
   """
+  global _cached_reader_fields
   result = []
+
   for s in strings:
     with log.Event.from_bytes(s, traversal_limit_in_words=NO_TRAVERSAL_LIMIT) as event:
       frames = getattr(event, msgtype)
-      frame_list = [(f.address, f.dat, f.src) for f in frames]
+
+      # Cache schema fields on first frame for faster access
+      if _cached_reader_fields is None and len(frames) > 0:
+        _cached_reader_fields = _get_reader_fields(frames[0].schema)
+
+      if _cached_reader_fields is not None:
+        addr_f, dat_f, src_f = _cached_reader_fields
+        frame_list = [(f._get_by_field(addr_f), f._get_by_field(dat_f), f._get_by_field(src_f)) for f in frames]
+      else:
+        frame_list = []
+
       result.append((event.logMonoTime, frame_list))
   return result
