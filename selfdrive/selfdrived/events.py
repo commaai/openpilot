eb5a189e5b21eddcbce3367784cea5550a897c5d
Shane Smiskol <shane@smiskol.com>
2026-01-28 19:50:33 -0800

    copy exactly from existing ldw alert with prompt sound, black alert

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -479,10 +479,10 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
 
   EventName.stockLkas: {
     ET.PERMANENT: Alert(
-      "TAKE CONTROL",
       "Stock LKAS: Lane Departure Detected",
+      "",
       AlertStatus.userPrompt, AlertSize.small,
-      Priority.MID, VisualAlert.ldw, AudibleAlert.none, 2.),
+      Priority.LOW, VisualAlert.ldw, AudibleAlert.prompt, 3.),
     ET.NO_ENTRY: NoEntryAlert("Stock LKAS: Lane Departure Detected"),
   },
 
