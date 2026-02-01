52c4bfd206477812fbb022b35f739c3aaf924ba7
Karel Nagel <nagelkarel@gmail.com>
2026-02-01 18:36:06 +0200

    move classes out of main

--- system/athena/ble.py
+++ system/athena/ble.py
@@ -9,6 +9,11 @@ import time
 import random
 import secrets
 
+import dbus
+import dbus.exceptions
+import dbus.mainloop.glib
+import dbus.service
+from gi.repository import GLib
 from jsonrpc import JSONRPCResponseManager, dispatcher
 from openpilot.common.params import Params
 
@@ -106,14 +111,217 @@ def init_bluetooth():
   return False
 
 
-def main():
-  import dbus
-  import dbus.exceptions
-  import dbus.mainloop.glib
-  import dbus.service
-  from gi.repository import GLib
+class InvalidArgsException(dbus.exceptions.DBusException):
+  _dbus_error_name = "org.freedesktop.DBus.Error.InvalidArgs"
 
-  # Import shared RPC methods - registers them with dispatcher
+
+class Characteristic(dbus.service.Object):
+  def __init__(self, bus, index, uuid, flags, service):
+    self.path = service.path + "/char" + str(index)
+    self.uuid = uuid
+    self.service = service
+    self.flags = flags
+    self.value = []
+    dbus.service.Object.__init__(self, bus, self.path)
+
+  def get_properties(self):
+    return {GATT_CHRC_IFACE: {"Service": self.service.get_path(), "UUID": self.uuid, "Flags": self.flags}}
+
+  def get_path(self):
+    return dbus.ObjectPath(self.path)
+
+  @dbus.service.method(DBUS_PROP_IFACE, in_signature="s", out_signature="a{sv}")
+  def GetAll(self, interface):
+    if interface != GATT_CHRC_IFACE:
+      raise InvalidArgsException()
+    return self.get_properties()[GATT_CHRC_IFACE]
+
+  @dbus.service.method(GATT_CHRC_IFACE, in_signature="a{sv}", out_signature="ay")
+  def ReadValue(self, options):
+    return self.value
+
+  @dbus.service.method(GATT_CHRC_IFACE, in_signature="aya{sv}")
+  def WriteValue(self, value, options):
+    self.value = value
+
+  @dbus.service.method(GATT_CHRC_IFACE)
+  def StartNotify(self):
+    pass
+
+  @dbus.service.method(GATT_CHRC_IFACE)
+  def StopNotify(self):
+    pass
+
+  @dbus.service.signal(DBUS_PROP_IFACE, signature="sa{sv}as")
+  def PropertiesChanged(self, interface, changed, invalidated):
+    pass
+
+
+class Service(dbus.service.Object):
+  PATH_BASE = "/org/bluez/app/service"
+
+  def __init__(self, bus, index, uuid, primary):
+    self.path = self.PATH_BASE + str(index)
+    self.uuid = uuid
+    self.primary = primary
+    self.characteristics = []
+    dbus.service.Object.__init__(self, bus, self.path)
+
+  def get_properties(self):
+    return {
+      GATT_SERVICE_IFACE: {
+        "UUID": self.uuid,
+        "Primary": self.primary,
+        "Characteristics": dbus.Array([dbus.ObjectPath(c.get_path()) for c in self.characteristics], signature="o"),
+      }
+    }
+
+  def get_path(self):
+    return dbus.ObjectPath(self.path)
+
+  def add_characteristic(self, characteristic):
+    self.characteristics.append(characteristic)
+
+
+class RPCResponseCharacteristic(Characteristic):
+  def __init__(self, bus, index, service):
+    Characteristic.__init__(self, bus, index, RPC_RESPONSE_CHAR_UUID, ["notify"], service)
+    self.notifying = False
+
+  def StartNotify(self):
+    self.notifying = True
+
+  def StopNotify(self):
+    self.notifying = False
+
+  def send_response(self, response: str):
+    if not self.notifying:
+      return
+    data = response.encode("utf-8")
+    for i in range(0, len(data), BLE_MTU):
+      self.value = dbus.Array(data[i : i + BLE_MTU], signature="y")
+      self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": self.value}, [])
+
+
+class RPCRequestCharacteristic(Characteristic):
+  def __init__(self, bus, index, service, response_char):
+    Characteristic.__init__(self, bus, index, RPC_REQUEST_CHAR_UUID, ["write", "write-without-response"], service)
+    self.response_char = response_char
+    self.buffer = b""
+    self.current_token = None
+
+  def WriteValue(self, value, options):
+    self.buffer += bytes(value)
+    if len(self.buffer) > MAX_REQUEST_SIZE:
+      log(f"Buffer exceeded {MAX_REQUEST_SIZE} bytes, dropping")
+      self.buffer = b""
+      return
+    try:
+      text = self.buffer.decode("utf-8")
+      json.loads(text)
+      self.buffer = b""
+      self.process_request(text)
+    except (json.JSONDecodeError, UnicodeDecodeError):
+      pass
+
+  def process_request(self, request_text: str):
+    log(f"RPC: {request_text[:80]}...")
+    request_id = None
+    try:
+      req = json.loads(request_text)
+      method = req.get("method", "")
+      params = req.get("params", {})
+      request_id = req.get("id")
+
+      if isinstance(params, dict) and "token" in params:
+        self.current_token = params.pop("token")
+        request_text = json.dumps(req)
+
+      if method not in ("blePair", "echo"):
+        if not self.current_token or self.current_token not in get_authorized_tokens():
+          err = {"jsonrpc": "2.0", "error": {"code": -32001, "message": "Unauthorized: pair with device first"}, "id": request_id}
+          self.response_char.send_response(json.dumps(err))
+          return
+
+      from openpilot.system.athena.rpc_methods import set_transport
+      set_transport("ble")
+      response = JSONRPCResponseManager.handle(request_text, dispatcher)
+      self.response_char.send_response(response.json)
+    except Exception as e:
+      log(f"RPC error: {e}")
+      self.response_char.send_response(json.dumps({"jsonrpc": "2.0", "error": {"code": -32603, "message": str(e)}, "id": request_id}))
+
+
+class AthenaService(Service):
+  def __init__(self, bus, index):
+    Service.__init__(self, bus, index, ATHENA_SERVICE_UUID, True)
+    self.response_char = RPCResponseCharacteristic(bus, 1, self)
+    self.add_characteristic(RPCRequestCharacteristic(bus, 0, self, self.response_char))
+    self.add_characteristic(self.response_char)
+
+
+class Application(dbus.service.Object):
+  def __init__(self, bus):
+    self.path = "/"
+    self.services = []
+    dbus.service.Object.__init__(self, bus, self.path)
+    self.services.append(AthenaService(bus, 0))
+
+  def get_path(self):
+    return dbus.ObjectPath(self.path)
+
+  @dbus.service.method(DBUS_OM_IFACE, out_signature="a{oa{sa{sv}}}")
+  def GetManagedObjects(self):
+    response = {}
+    for service in self.services:
+      response[service.get_path()] = service.get_properties()
+      for chrc in service.characteristics:
+        response[chrc.get_path()] = chrc.get_properties()
+    return response
+
+
+class Advertisement(dbus.service.Object):
+  PATH_BASE = "/org/bluez/app/advertisement"
+
+  def __init__(self, bus, index, ad_manager):
+    self.path = self.PATH_BASE + str(index)
+    self.ad_manager = ad_manager
+    dbus.service.Object.__init__(self, bus, self.path)
+
+  def get_properties(self):
+    return {
+      LE_ADVERTISEMENT_IFACE: {
+        "Type": "peripheral",
+        "ServiceUUIDs": dbus.Array([ATHENA_SERVICE_UUID], signature="s"),
+        "LocalName": dbus.String(get_device_name()),
+        "IncludeTxPower": dbus.Boolean(True),
+      }
+    }
+
+  def get_path(self):
+    return dbus.ObjectPath(self.path)
+
+  @dbus.service.method(DBUS_PROP_IFACE, in_signature="s", out_signature="a{sv}")
+  def GetAll(self, interface):
+    if interface != LE_ADVERTISEMENT_IFACE:
+      raise InvalidArgsException()
+    return self.get_properties()[LE_ADVERTISEMENT_IFACE]
+
+  @dbus.service.method(LE_ADVERTISEMENT_IFACE)
+  def Release(self):
+    log("Advertisement released, re-registering...")
+    GLib.timeout_add(100, self._re_register)
+
+  def _re_register(self):
+    self.ad_manager.RegisterAdvertisement(
+      self.get_path(), {},
+      reply_handler=lambda: log("Advertising"),
+      error_handler=lambda e: log(f"Ad re-register failed: {e}"),
+    )
+    return False
+
+
+def main():
   from openpilot.system.athena import rpc_methods  # noqa: F401
 
   log(f"Starting BLE server: {get_device_name()}")
@@ -122,210 +330,8 @@ def main():
   if not init_bluetooth():
     return 1
 
-  mainloop = dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-  bus = dbus.SystemBus(mainloop=mainloop)
-
-  class InvalidArgsException(dbus.exceptions.DBusException):
-    _dbus_error_name = "org.freedesktop.DBus.Error.InvalidArgs"
-
-  class Characteristic(dbus.service.Object):
-    def __init__(self, bus, index, uuid, flags, service):
-      self.path = service.path + "/char" + str(index)
-      self.uuid = uuid
-      self.service = service
-      self.flags = flags
-      self.value = []
-      dbus.service.Object.__init__(self, bus, self.path)
-
-    def get_properties(self):
-      return {GATT_CHRC_IFACE: {"Service": self.service.get_path(), "UUID": self.uuid, "Flags": self.flags}}
-
-    def get_path(self):
-      return dbus.ObjectPath(self.path)
-
-    @dbus.service.method(DBUS_PROP_IFACE, in_signature="s", out_signature="a{sv}")
-    def GetAll(self, interface):
-      if interface != GATT_CHRC_IFACE:
-        raise InvalidArgsException()
-      return self.get_properties()[GATT_CHRC_IFACE]
-
-    @dbus.service.method(GATT_CHRC_IFACE, in_signature="a{sv}", out_signature="ay")
-    def ReadValue(self, options):
-      return self.value
-
-    @dbus.service.method(GATT_CHRC_IFACE, in_signature="aya{sv}")
-    def WriteValue(self, value, options):
-      self.value = value
-
-    @dbus.service.method(GATT_CHRC_IFACE)
-    def StartNotify(self):
-      pass
-
-    @dbus.service.method(GATT_CHRC_IFACE)
-    def StopNotify(self):
-      pass
-
-    @dbus.service.signal(DBUS_PROP_IFACE, signature="sa{sv}as")
-    def PropertiesChanged(self, interface, changed, invalidated):
-      pass
-
-  class Service(dbus.service.Object):
-    PATH_BASE = "/org/bluez/app/service"
-
-    def __init__(self, bus, index, uuid, primary):
-      self.path = self.PATH_BASE + str(index)
-      self.uuid = uuid
-      self.primary = primary
-      self.characteristics = []
-      dbus.service.Object.__init__(self, bus, self.path)
-
-    def get_properties(self):
-      return {
-        GATT_SERVICE_IFACE: {
-          "UUID": self.uuid,
-          "Primary": self.primary,
-          "Characteristics": dbus.Array([dbus.ObjectPath(c.get_path()) for c in self.characteristics], signature="o"),
-        }
-      }
-
-    def get_path(self):
-      return dbus.ObjectPath(self.path)
-
-    def add_characteristic(self, characteristic):
-      self.characteristics.append(characteristic)
-
-  class RPCResponseCharacteristic(Characteristic):
-    def __init__(self, bus, index, service):
-      Characteristic.__init__(self, bus, index, RPC_RESPONSE_CHAR_UUID, ["notify"], service)
-      self.notifying = False
-
-    def StartNotify(self):
-      self.notifying = True
-
-    def StopNotify(self):
-      self.notifying = False
-
-    def send_response(self, response: str):
-      if not self.notifying:
-        return
-      data = response.encode("utf-8")
-      for i in range(0, len(data), BLE_MTU):
-        self.value = dbus.Array(data[i : i + BLE_MTU], signature="y")
-        self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": self.value}, [])
-
-  class RPCRequestCharacteristic(Characteristic):
-    def __init__(self, bus, index, service, response_char):
-      Characteristic.__init__(self, bus, index, RPC_REQUEST_CHAR_UUID, ["write", "write-without-response"], service)
-      self.response_char = response_char
-      self.buffer = b""
-      self.current_token = None
-
-    def WriteValue(self, value, options):
-      self.buffer += bytes(value)
-      if len(self.buffer) > MAX_REQUEST_SIZE:
-        log(f"Buffer exceeded {MAX_REQUEST_SIZE} bytes, dropping")
-        self.buffer = b""
-        return
-      try:
-        text = self.buffer.decode("utf-8")
-        json.loads(text)
-        self.buffer = b""
-        self.process_request(text)
-      except (json.JSONDecodeError, UnicodeDecodeError):
-        pass
-
-    def process_request(self, request_text: str):
-      log(f"RPC: {request_text[:80]}...")
-      request_id = None
-      try:
-        req = json.loads(request_text)
-        method = req.get("method", "")
-        params = req.get("params", {})
-        request_id = req.get("id")
-
-        if isinstance(params, dict) and "token" in params:
-          self.current_token = params.pop("token")
-          request_text = json.dumps(req)
-
-        if method not in ("blePair", "echo"):
-          if not self.current_token or self.current_token not in get_authorized_tokens():
-            err = {"jsonrpc": "2.0", "error": {"code": -32001, "message": "Unauthorized: pair with device first"}, "id": request_id}
-            self.response_char.send_response(json.dumps(err))
-            return
-
-        from openpilot.system.athena.rpc_methods import set_transport
-        set_transport("ble")
-        response = JSONRPCResponseManager.handle(request_text, dispatcher)
-        self.response_char.send_response(response.json)
-      except Exception as e:
-        log(f"RPC error: {e}")
-        self.response_char.send_response(json.dumps({"jsonrpc": "2.0", "error": {"code": -32603, "message": str(e)}, "id": request_id}))
-
-  class AthenaService(Service):
-    def __init__(self, bus, index):
-      Service.__init__(self, bus, index, ATHENA_SERVICE_UUID, True)
-      self.response_char = RPCResponseCharacteristic(bus, 1, self)
-      self.add_characteristic(RPCRequestCharacteristic(bus, 0, self, self.response_char))
-      self.add_characteristic(self.response_char)
-
-  class Application(dbus.service.Object):
-    def __init__(self, bus):
-      self.path = "/"
-      self.services = []
-      dbus.service.Object.__init__(self, bus, self.path)
-      self.services.append(AthenaService(bus, 0))
-
-    def get_path(self):
-      return dbus.ObjectPath(self.path)
-
-    @dbus.service.method(DBUS_OM_IFACE, out_signature="a{oa{sa{sv}}}")
-    def GetManagedObjects(self):
-      response = {}
-      for service in self.services:
-        response[service.get_path()] = service.get_properties()
-        for chrc in service.characteristics:
-          response[chrc.get_path()] = chrc.get_properties()
-      return response
-
-  class Advertisement(dbus.service.Object):
-    PATH_BASE = "/org/bluez/app/advertisement"
-
-    def __init__(self, bus, index, ad_manager):
-      self.path = self.PATH_BASE + str(index)
-      self.ad_manager = ad_manager
-      dbus.service.Object.__init__(self, bus, self.path)
-
-    def get_properties(self):
-      return {
-        LE_ADVERTISEMENT_IFACE: {
-          "Type": "peripheral",
-          "ServiceUUIDs": dbus.Array([ATHENA_SERVICE_UUID], signature="s"),
-          "LocalName": dbus.String(get_device_name()),
-          "IncludeTxPower": dbus.Boolean(True),
-        }
-      }
-
-    def get_path(self):
-      return dbus.ObjectPath(self.path)
-
-    @dbus.service.method(DBUS_PROP_IFACE, in_signature="s", out_signature="a{sv}")
-    def GetAll(self, interface):
-      if interface != LE_ADVERTISEMENT_IFACE:
-        raise InvalidArgsException()
-      return self.get_properties()[LE_ADVERTISEMENT_IFACE]
-
-    @dbus.service.method(LE_ADVERTISEMENT_IFACE)
-    def Release(self):
-      log("Advertisement released, re-registering...")
-      GLib.timeout_add(100, self._re_register)
-
-    def _re_register(self):
-      self.ad_manager.RegisterAdvertisement(
-        self.get_path(), {},
-        reply_handler=lambda: log("Advertising"),
-        error_handler=lambda e: log(f"Ad re-register failed: {e}"),
-      )
-      return False
+  dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+  bus = dbus.SystemBus()
 
   app = Application(bus)
   service_manager = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, ADAPTER_PATH), GATT_MANAGER_IFACE)
