c3a53aaa2ba2858ce607530f724623fa9b7c6b35
Karel Nagel <nagelkarel@gmail.com>
2026-02-02 16:19:35 +0200

    use pythonprocess

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -84,7 +84,7 @@ def and_(*fns):
 
 procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
-  DaemonProcess("manage_ble", "system.athena.manage_ble", "BlePid"),
+  PythonProcess("manage_ble", "system.athena.manage_ble", always_run),
   NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], or_(notcar, webrtc)),
