369fccac786a67799193e9152488813c6df20414
elkoled <elkoled@gmail.com>
2026-02-04 15:25:47 -0800

    safe diffs in master

--- selfdrive/test/process_replay/test_processes.py
+++ selfdrive/test/process_replay/test_processes.py
@@ -220,4 +220,4 @@ if __name__ == "__main__":
       f.write(cur_commit)
     print(f"\n\nUpdated reference logs for commit: {cur_commit}")
 
-  sys.exit(int(failed))
+  sys.exit(0 if args.update_refs else int(failed))
