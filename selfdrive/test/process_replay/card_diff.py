29c69d37ca14d0baa919794dcf96437d563dd842
elkoled <elkoled@gmail.com>
2026-02-04 20:12:43 -0800

    copy formatting from car_diff

--- selfdrive/test/process_replay/card_diff.py
+++ selfdrive/test/process_replay/card_diff.py
@@ -16,5 +16,41 @@ CARD_CFG = get_process_config("card")
 NAN_FIELDS = {'aRel', 'yvRel'}
 
 
+class MsgWrap:
+  """adapter so to_dict() includes defaults"""
+  def __init__(self, msg):
+    self._msg = msg
+  def to_dict(self):
+    return self._msg.to_dict(verbose=True)
+
+
+def compare_card(ref_msgs, new_msgs):
+  ref, new = defaultdict(list), defaultdict(list)
+  for m in ref_msgs:
+    if m.which() in CARD_CFG.subs:
+      ref[m.which()].append(m)
+  for m in new_msgs:
+    if m.which() in CARD_CFG.subs:
+      new[m.which()].append(m)
+
+  diffs = []
+  for sub in CARD_CFG.subs:
+    for i, (r, n) in enumerate(zip(ref[sub], new[sub], strict=True)):
+      for d in compare_logs([r], [n], CARD_CFG.ignore, tolerance=CARD_CFG.tolerance):
+        if d[0] == "change":
+          path = ".".join(str(p) for p in d[1]) if isinstance(d[1], list) else d[1]
+          if path.split('.')[-1] in NAN_FIELDS:
+            continue
+          diffs.append((path, i, d[2], r.logMonoTime))
+  return diffs, ref, new
+
+
+def format_card(diffs, ref, new, field):
+  msg_type = field.split(".")[0]
+  ref = [(m.logMonoTime, MsgWrap(m)) for m in ref.get(msg_type, [])]
+  states = [MsgWrap(m) for m in new.get(msg_type, [])]
+  return format_diff(diffs, ref, states, field)
+
+
 if __name__ == "__main__":
   sys.exit(0)
