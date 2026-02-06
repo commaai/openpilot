1b37f40b73f9fb4dd854f71ffa07307560c537a1
elkoled <elkoled@gmail.com>
2026-02-05 21:31:03 -0800

    Revert "traceback"
    This reverts commit 03ace7a919fa8155d93108063c921b836d2fdecc.

--- selfdrive/test/process_replay/test_processes.py
+++ selfdrive/test/process_replay/test_processes.py
@@ -219,8 +219,8 @@ if __name__ == "__main__":
 
     try:
       diff_report(diffs, segments)
-    except Exception:
-      print(f"failed to generate diff report:\n{traceback.format_exc()}")
+    except Exception as e:
+      print(f"failed to generate diff report: {e}")
 
     if failed:
       print("TEST FAILED")
