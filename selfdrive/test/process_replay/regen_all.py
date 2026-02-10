cba9209bf4f7e4a717043aabdef50d0448e42d85
elkoled <elkoled@gmail.com>
2026-02-10 14:22:14 -0800

    fix lint

--- selfdrive/test/process_replay/regen_all.py
+++ selfdrive/test/process_replay/regen_all.py
@@ -12,12 +12,12 @@ from openpilot.selfdrive.test.process_replay.test_processes import FAKEDATA, sou
 from openpilot.tools.lib.route import SegmentName
 
 
-def regen_job(segment, upload, disable_tqdm):
+def regen_job(segment, disable_tqdm):
   with OpenpilotPrefix():
     sn = SegmentName(segment[1])
     fake_dongle_id = 'regen' + ''.join(random.choice('0123456789ABCDEF') for _ in range(11))
     try:
-      relr = regen_and_save(sn.route_name.canonical_name, sn.segment_num, upload=upload,
+      relr = regen_and_save(sn.route_name.canonical_name, sn.segment_num,
                             outdir=os.path.join(FAKEDATA, fake_dongle_id), disable_tqdm=disable_tqdm, dummy_driver_cam=True)
       relr = '|'.join(relr.split('/')[-2:])
       return f'  ("{segment[0]}", "{relr}"), '
@@ -33,7 +33,6 @@ if __name__ == "__main__":
 
   parser = argparse.ArgumentParser(description="Generate new segments from old ones")
   parser.add_argument("-j", "--jobs", type=int, default=1)
-  parser.add_argument("--no-upload", action="store_true")
   parser.add_argument("--whitelist-cars", type=str, nargs="*", default=all_cars,
                       help="Whitelist given cars from the test (e.g. HONDA)")
   parser.add_argument("--blacklist-cars", type=str, nargs="*", default=[],
@@ -45,7 +44,7 @@ if __name__ == "__main__":
   tested_segments = [(car, segment) for car, segment in segments if car in tested_cars]
 
   with concurrent.futures.ProcessPoolExecutor(max_workers=args.jobs) as pool:
-    p = pool.map(regen_job, tested_segments, [not args.no_upload] * len(tested_segments), [args.jobs > 1] * len(tested_segments))
+    p = pool.map(regen_job, tested_segments, [args.jobs > 1] * len(tested_segments))
     msg = "Copy these new segments into test_processes.py:"
     for seg in tqdm(p, desc="Generating segments", total=len(tested_segments)):
       msg += "\n" + str(seg)
