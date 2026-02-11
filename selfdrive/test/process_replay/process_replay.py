7d343fa3755eb3b243a88d21399bf2cc5bb1621c
Bruce Wayne <harald.the.engineer@gmail.com>
2026-02-11 10:42:36 -0800

    Fix interleaving

--- selfdrive/test/process_replay/process_replay.py
+++ selfdrive/test/process_replay/process_replay.py
@@ -307,7 +307,7 @@ class ProcessContainer:
             h, w = frs[m.which()].h, frs[m.which()].w
             stride, y_height, _, yuv_size = get_nv12_info(w, h)
             uv_offset = stride * y_height
-            padded_img = np.zeros((uv_offset + (h // 2), stride))
+            padded_img = np.zeros(((uv_offset //stride) + (h // 2), stride))
             padded_img[:h, :w] = img[:h * w].reshape((-1, w))
             padded_img[uv_offset // stride:uv_offset // stride + h // 2, :w] = img[h * w:].reshape((-1, w))
             img_bytes = np.zeros((yuv_size,), dtype=np.uint8)
