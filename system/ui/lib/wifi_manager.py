48a227d7c4ed78d4406933d4165b0e4b8b351721
Shane Smiskol <shane@smiskol.com>
2026-02-12 20:28:41 -0800

    has lag

--- system/ui/lib/wifi_manager.py
+++ system/ui/lib/wifi_manager.py
@@ -320,8 +320,8 @@ class WifiManager:
           if new_state == NMDeviceState.NEED_AUTH and change_reason == NM_DEVICE_STATE_REASON_SUPPLICANT_DISCONNECT and len(self._connecting_to_ssid):
             print('need auth!')
             self.forget_connection(self._connecting_to_ssid, block=True)
-            self._connecting_to_ssid = ""
             self._enqueue_callbacks(self._need_auth, self._connecting_to_ssid)
+            self._connecting_to_ssid = ""
 
           elif new_state == NMDeviceState.ACTIVATED:
             if len(self._activated):
