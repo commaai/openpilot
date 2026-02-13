a565d869659ae4a7b281d88589cffdd19b139ff1
Shane Smiskol <shane@smiskol.com>
2026-02-13 14:35:26 -0800

    disable touch while animating

--- system/ui/widgets/scroller.py
+++ system/ui/widgets/scroller.py
@@ -141,7 +141,8 @@ class Scroller(Widget):
 
   def add_widget(self, item: Widget) -> None:
     self._items.append(item)
-    item.set_touch_valid_callback(lambda: self.scroll_panel.is_touch_valid() and self.enabled and self._scrolling_to is None)
+    item.set_touch_valid_callback(lambda: self.scroll_panel.is_touch_valid() and self.enabled and self._scrolling_to is None
+                                          and self._overlay_filter.x < 0.01)
 
   def move_item(self, from_idx: int, to_idx: int) -> None:
     """Pop item at from_idx and insert at to_idx, with animated transition."""
@@ -244,7 +245,7 @@ class Scroller(Widget):
 
   def _get_scroll(self, visible_items: list[Widget], content_size: float) -> float:
     scroll_enabled = self._scroll_enabled() if callable(self._scroll_enabled) else self._scroll_enabled
-    self.scroll_panel.set_enabled(scroll_enabled and self.enabled)
+    self.scroll_panel.set_enabled(scroll_enabled and self.enabled and self._overlay_filter.x < 0.01)
     self.scroll_panel.update(self._rect, content_size)
     if not self._snap_items:
       return round(self.scroll_panel.get_offset())
