eb03b9439b7786e02c70167c4f20d14aad9c4c3d
felsager <d.felsager@gmail.com>
2026-02-16 14:46:14 -0800

    helpers

---
+++ tools/longitudinal_maneuvers/maneuver_helpers.py
@@ -0,0 +1,20 @@
+from enum import IntEnum
+
+class Axis(IntEnum):
+  TIME = 0
+  EGO_POSITION = 1
+  LEAD_DISTANCE= 2
+  EGO_V = 3
+  LEAD_V = 4
+  EGO_A = 5
+  D_REL = 6
+  EGO_J = 7
+
+axis_labels = {Axis.TIME: 'Time (s)',
+               Axis.EGO_POSITION: 'Ego position (m)',
+               Axis.LEAD_DISTANCE: 'Lead absolute position (m)',
+               Axis.EGO_V: 'Ego Velocity (m/s)',
+               Axis.LEAD_V: 'Lead Velocity (m/s)',
+               Axis.EGO_A: 'Ego acceleration (m/s^2)',
+               Axis.D_REL: 'Lead distance (m)',
+               Axis.EGO_J: 'Ego acceleration (m/s^3)'}
