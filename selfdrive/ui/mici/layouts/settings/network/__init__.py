61589eb16d9755ee2c483f3baf42710dbf27c9de
Shane Smiskol <shane@smiskol.com>
2026-02-17 21:55:33 -0800

    obt

--- selfdrive/ui/mici/layouts/settings/network/__init__.py
+++ selfdrive/ui/mici/layouts/settings/network/__init__.py
@@ -165,10 +165,9 @@ class NetworkLayoutMici(NavWidget):
     # Update wi-fi button with ssid and ip address
     # TODO: make sure we handle hidden ssids
     connecting_ssid = self._wifi_manager.connecting_to_ssid
-    connected_network = next((network for network in self._wifi_ui.networks.values() if network.is_connected), None)
-    # print('connecting_ssid', connecting_ssid, 'connected_network', connected_network)
+    connected_network = next((network for network in networks if network.is_connected), None)
     if connecting_ssid:
-      display_network = next((n for n in self._wifi_ui.networks.values() if n.ssid == connecting_ssid), None)
+      display_network = next((n for n in networks if n.ssid == connecting_ssid), None)
       self._wifi_button.set_text(normalize_ssid(connecting_ssid))
       self._wifi_button.set_value("connecting...")
     elif connected_network is not None:
