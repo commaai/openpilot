999e4d5c3f116b86e2ccfd536fe2807ab01a9e50
Shane Smiskol <shane@smiskol.com>
2026-02-18 19:07:30 -0800

    ensure we emit activated even if we fail to get conn path from dbus

--- system/ui/lib/wifi_manager.py
+++ system/ui/lib/wifi_manager.py
@@ -400,20 +400,20 @@ class WifiManager:
             self._update_networks()
 
             self._wifi_state.status = ConnectStatus.CONNECTED
-            self._enqueue_callbacks(self._activated)
 
             conn_path, _ = self._get_active_wifi_connection(self._conn_monitor)
             if conn_path is None:
               cloudlog.warning("Failed to get active wifi connection during ACTIVATED state")
-              continue
+              self._enqueue_callbacks(self._activated)
+            else:
+              self._wifi_state.ssid = next((s for s, p in self._connections.items() if p == conn_path), None)
+              self._enqueue_callbacks(self._activated)
 
-            self._wifi_state.ssid = next((s for s, p in self._connections.items() if p == conn_path), None)
-
-            # Persist volatile connections (created by AddAndActivateConnection2) to disk
-            conn_addr = DBusAddress(conn_path, bus_name=NM, interface=NM_CONNECTION_IFACE)
-            save_reply = self._conn_monitor.send_and_get_reply(new_method_call(conn_addr, 'Save'))
-            if save_reply.header.message_type == MessageType.error:
-              cloudlog.warning(f"Failed to persist connection to disk: {save_reply}")
+              # Persist volatile connections (created by AddAndActivateConnection2) to disk
+              conn_addr = DBusAddress(conn_path, bus_name=NM, interface=NM_CONNECTION_IFACE)
+              save_reply = self._conn_monitor.send_and_get_reply(new_method_call(conn_addr, 'Save'))
+              if save_reply.header.message_type == MessageType.error:
+                cloudlog.warning(f"Failed to persist connection to disk: {save_reply}")
 
           elif new_state == NMDeviceState.DEACTIVATING:
             if change_reason == NMDeviceStateReason.CONNECTION_REMOVED:
