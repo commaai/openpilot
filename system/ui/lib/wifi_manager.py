2b16c0f1354ce187e3fa2dbe8524f84dab62c438
Shane Smiskol <shane@smiskol.com>
2026-02-19 00:36:31 -0800

    Revert "reproduce race condition where connection removed signal takes a while to remove, then update networks keep is_saved true"
    This reverts commit cf7044ee955777db16434ab81c520bbe798c9164.

--- system/ui/lib/wifi_manager.py
+++ system/ui/lib/wifi_manager.py
@@ -618,13 +618,11 @@ class WifiManager:
       else:
         conn_addr = DBusAddress(conn_path, bus_name=NM, interface=NM_CONNECTION_IFACE)
         self._router_main.send_and_get_reply(new_method_call(conn_addr, 'Delete'))
-        # self._connections.pop(ssid, None)
 
       # FIXME: race here where ConnectionRemoved signal may arrive after we update all Network is_saved
       #  and keep the old ssid's is_saved=True
-      self._enqueue_callbacks(self._forgotten, ssid)
-      time.sleep(1.)
       self._update_networks()
+      self._enqueue_callbacks(self._forgotten, ssid)
 
     if block:
       worker()
