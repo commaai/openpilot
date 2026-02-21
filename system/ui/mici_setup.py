2ffd48f36082466ded6ac2b5db72520d975a4ff0
Shane Smiskol <shane@smiskol.com>
2026-02-20 16:49:50 -0800

    Revert "stash"
    This reverts commit 3cfb226ccb51869ed1f7d630b5fdd6725ad094d5.

--- system/ui/mici_setup.py
+++ system/ui/mici_setup.py
@@ -79,9 +79,7 @@ class NetworkConnectivityMonitor:
         try:
           request = urllib.request.Request(OPENPILOT_URL, method="HEAD")
           urllib.request.urlopen(request, timeout=1.0)
-          time.sleep(5)
-          # print('HAS INTERNET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-          # self.network_connected.set()
+          self.network_connected.set()
           if HARDWARE.get_network_type() == NetworkType.wifi:
             self.wifi_connected.set()
         except Exception:
