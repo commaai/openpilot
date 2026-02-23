3f2116eb20f185e19f104b56e9e06fb8e82cc2b1
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-02-22 21:48:41 -0800

    bye bye brew

--- tools/mac_setup.sh
+++ tools/mac_setup.sh
@@ -4,33 +4,11 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 ROOT="$(cd $DIR/../ && pwd)"
 
-# homebrew update is slow
-export HOMEBREW_NO_AUTO_UPDATE=1
-
 if [[ $SHELL == "/bin/zsh" ]]; then
   RC_FILE="$HOME/.zshrc"
 elif [[ $SHELL == "/bin/bash" ]]; then
   RC_FILE="$HOME/.bash_profile"
 fi
-
-# Install brew if required
-if [[ $(command -v brew) == "" ]]; then
-  echo "Installing Homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo "[ ] installed brew t=$SECONDS"
-
-  # make brew available now
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $RC_FILE
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-else
-    brew up
-fi
-
-brew bundle --file=- <<-EOS
-brew "coreutils"
-EOS
-
-echo "[ ] finished brew install t=$SECONDS"
 
 # install python dependencies
 $DIR/install_python_dependencies.sh
