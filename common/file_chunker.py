d4e7146310c9ab1303061e6e846d2e9018a161f4
Bruce Wayne <harald.the.engineer@gmail.com>
2026-02-23 14:23:42 -0800

    cleaner

--- common/file_chunker.py
+++ common/file_chunker.py
@@ -2,17 +2,19 @@ import math
 import os
 from pathlib import Path
 
-CHUNK_SIZE = 45 * 1024 * 1024  # 45MB, under GitHub's 50MB limit
+CHUNK_SIZE = 115 * 1024 * 1024  # 45MB, under GitHub's 50MB limit
 
 def get_chunk_name(name, idx, num_chunks):
   return f"{name}.chunk{idx+1:02d}of{num_chunks:02d}"
+
+def get_manifest_path(name):
+  return f"{name}.chunkmanifest"
 
 def get_chunk_paths(path, file_size):
   num_chunks = math.ceil(file_size / CHUNK_SIZE)
   return [get_chunk_name(path, i, num_chunks) for i in range(num_chunks)]
 
 def chunk_file(path, num_chunks):
-  manifest_path = f"{path}.chunkmanifest"
   with open(path, 'rb') as f:
     data = f.read()
   actual_num_chunks = max(1, math.ceil(len(data) / CHUNK_SIZE))
@@ -20,12 +22,12 @@ def chunk_file(path, num_chunks):
   for i in range(num_chunks):
     with open(get_chunk_name(path, i, num_chunks), 'wb') as f:
       f.write(data[i * CHUNK_SIZE:(i + 1) * CHUNK_SIZE])
-  Path(manifest_path).write_text(str(num_chunks))
+  Path(get_manifest_path(path)).write_text(str(num_chunks))
   os.remove(path)
 
 
 def read_file_chunked(path):
-  manifest_path = f"{path}.chunkmanifest"
+  manifest_path = get_manifest_path(path)
   if os.path.isfile(manifest_path):
     num_chunks = int(Path(manifest_path).read_text().strip())
     return b''.join(Path(get_chunk_name(path, i, num_chunks)).read_bytes() for i in range(num_chunks))
