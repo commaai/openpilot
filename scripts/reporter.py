a5b9045a48561f1a42c8e25ca933d4e464379b25
Bruce Wayne <harald.the.engineer@gmail.com>
2026-02-24 09:38:58 -0800

    Revert "rm onnx (#37285)"
    This reverts commit 23e1c4f49e63e550a44a85f0020b601bd9376a1d.

--- scripts/reporter.py
+++ scripts/reporter.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 import os
 import glob
-
-from tinygrad.nn.onnx import OnnxPBParser
+import onnx
 
 BASEDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
-
 MASTER_PATH = os.getenv("MASTER_PATH", BASEDIR)
 MODEL_PATH = "/selfdrive/modeld/models/"
 
-
-class MetadataOnnxPBParser(OnnxPBParser):
-  def _parse_ModelProto(self) -> dict:
-    obj = {"metadata_props": []}
-    for fid, wire_type in self._parse_message(self.reader.len):
-      match fid:
-        case 14:
-          obj["metadata_props"].append(self._parse_StringStringEntryProto())
-        case _:
-          self.reader.skip_field(wire_type)
-    return obj
-
-
 def get_checkpoint(f):
-  model = MetadataOnnxPBParser(f).parse()
-  metadata = {prop["key"]: prop["value"] for prop in model["metadata_props"]}
+  model = onnx.load(f)
+  metadata = {prop.key: prop.value for prop in model.metadata_props}
   return metadata['model_checkpoint'].split('/')[0]
-
 
 if __name__ == "__main__":
   print("| | master | PR branch |")
@@ -40,4 +24,8 @@ if __name__ == "__main__":
     fn = os.path.basename(f)
     master = get_checkpoint(MASTER_PATH + MODEL_PATH + fn)
     pr = get_checkpoint(BASEDIR + MODEL_PATH + fn)
-    print("|", fn, "|", f"[{master}](https://reporter.comma.life/experiment/{master})", "|", f"[{pr}](https://reporter.comma.life/experiment/{pr})", "|")
+    print(
+      "|", fn, "|",
+      f"[{master}](https://reporter.comma.life/experiment/{master})", "|",
+      f"[{pr}](https://reporter.comma.life/experiment/{pr})", "|"
+    )
