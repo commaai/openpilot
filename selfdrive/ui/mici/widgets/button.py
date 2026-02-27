8cdeac8e601430d46464fef9f73b73cd00d4a751
Shane Smiskol <shane@smiskol.com>
2026-02-26 16:10:32 -0800

    change

--- selfdrive/ui/mici/widgets/button.py
+++ selfdrive/ui/mici/widgets/button.py
@@ -218,9 +218,9 @@ class BigButton(Widget):
     self._label.render(label_rect)
 
     if self.value:
-      label_y = btn_y + self._rect.height - self.LABEL_VERTICAL_PADDING
-      sub_label_height = self._sub_label.get_content_height(self._width_hint())
-      sub_label_rect = rl.Rectangle(label_x, label_y - sub_label_height, self._width_hint(), sub_label_height)
+      label_y = btn_y + self.LABEL_VERTICAL_PADDING + self._label.get_content_height(self._width_hint())
+      sub_label_height = btn_y + self._rect.height - self.LABEL_VERTICAL_PADDING - label_y
+      sub_label_rect = rl.Rectangle(label_x, label_y, self._width_hint(), sub_label_height)
       self._sub_label.render(sub_label_rect)
 
     # ICON -------------------------------------------------------------------
