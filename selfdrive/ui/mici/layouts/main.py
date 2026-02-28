e52f5d5f250e073e304aebf401c229ed99cdb2d6
Shane Smiskol <shane@smiskol.com>
2026-02-27 17:12:24 -0800

    fix

--- selfdrive/ui/mici/layouts/main.py
+++ selfdrive/ui/mici/layouts/main.py
@@ -110,13 +110,13 @@ class MiciMainLayout(Scroller):
     if gui_app.get_active_widget() == self._onboarding_window:
       return
 
-    # Screen turns off on timeout, so pop immediately without animation
     if ui_state.started:
       # Don't pop if at standstill
       if not ui_state.sm["carState"].standstill:
-        gui_app.pop_widgets_to(self)
+        gui_app.request_pop_widgets_to(self)
         self._scroll_to(self._onroad_layout)
     else:
+      # Screen turns off on timeout offroad, so pop immediately without animation
       gui_app.pop_widgets_to(self)
       self._scroll_to(self._home_layout)
 
