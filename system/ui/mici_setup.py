95e2bf99371dac8aed903420db6138aafe07bf52
Shane Smiskol <shane@smiskol.com>
2026-03-05 01:44:49 -0800

    delay hide on wifi/internet

--- system/ui/mici_setup.py
+++ system/ui/mici_setup.py
@@ -364,10 +364,9 @@ class NetworkSetupPageBase(Scroller):
     self._wifi_button = WifiNetworkButton(self._wifi_manager)
     self._wifi_button.set_click_callback(lambda: gui_app.push_widget(self._wifi_ui))
 
-    self._show_time = 0.0
     self._prev_has_internet = False
     self._prev_wifi_connected = False
-    self._pending_has_internet_scroll = False
+    self._pending_has_internet_scroll: float | None = None
     self._pending_continue_grow_animation = False
     self._pending_wifi_grow_animation = False
 
@@ -395,7 +394,6 @@ class NetworkSetupPageBase(Scroller):
     super().show_event()
     # make sure we populate strength and ip immediately if already have wifi
     self._wifi_manager.set_active(True)
-    self._show_time = rl.get_time()
     self._prev_has_internet = False
     self._prev_wifi_connected = self._wifi_manager.wifi_state.status == ConnectStatus.CONNECTED
     self._pending_has_internet_scroll = False
@@ -419,14 +417,14 @@ class NetworkSetupPageBase(Scroller):
     # Dismiss WiFi UI and scroll on WiFi connect or internet gain
     wifi_connected = self._wifi_manager.wifi_state.status == ConnectStatus.CONNECTED
     if (has_internet and not self._prev_has_internet) or (wifi_connected and not self._prev_wifi_connected and not has_internet):
-      self._pending_has_internet_scroll = True
+      self._pending_has_internet_scroll = rl.get_time()
     self._prev_wifi_connected = wifi_connected
     self._prev_has_internet = has_internet
 
-    if self._pending_has_internet_scroll:
-      elapsed = rl.get_time() - self._show_time
-      if elapsed > 0.5:
-        self._pending_has_internet_scroll = False
+    if self._pending_has_internet_scroll is not None:
+      elapsed = rl.get_time() - self._pending_has_internet_scroll
+      if elapsed > 0.7:
+        self._pending_has_internet_scroll = None
 
         def scroll_to_end():
           self._scroller._layout()
