bfca300e4753ef356102cfdb0707442a5547e12c
Shane Smiskol <shane@smiskol.com>
2026-03-07 04:09:17 -0800

    clean up

--- system/ui/mici_setup.py
+++ system/ui/mici_setup.py
@@ -13,7 +13,7 @@ from collections.abc import Callable
 import pyray as rl
 
 from cereal import log
-from openpilot.common.filter_simple import FirstOrderFilter, BounceFilter
+from openpilot.common.filter_simple import BounceFilter
 from openpilot.system.hardware import HARDWARE, TICI
 from openpilot.common.realtime import config_realtime_process, set_core_affinity
 from openpilot.common.swaglog import cloudlog
