14bc2c933bf4555370201494da15b8c32557d025
Shane Smiskol <shane@smiskol.com>
2026-03-06 21:21:19 -0800

    reorder again

--- selfdrive/ui/mici/layouts/settings/device.py
+++ selfdrive/ui/mici/layouts/settings/device.py
@@ -85,9 +85,8 @@ def _engaged_confirmation_callback(callback: Callable, action_text: str):
       # TODO: check
       icon = "icons_mici/settings/comma_icon.png"
 
-    dlg: BigConfirmationDialog | BigDialog = BigConfirmationDialog(f"slide to\n{action_text.lower()}", icon, red=red,
-                                                                   exit_on_confirm=action_text == "reset",
-                                                                   confirm_callback=confirm_callback)
+    dlg: BigConfirmationDialog | BigDialog = BigConfirmationDialog(f"slide to\n{action_text.lower()}", icon, confirm_callback,
+                                                                   red=red, exit_on_confirm=action_text == "reset")
     gui_app.push_widget(dlg)
   else:
     dlg = BigDialog(f"Disengage to {action_text}", "")
