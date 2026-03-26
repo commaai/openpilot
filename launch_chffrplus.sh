2699ebff6ff8d969a03144adbf453a7349f892a1
Andrei Radulescu <andi.radulescu@gmail.com>
2026-03-26 16:07:29 +0200

    hardware: guard downstream-only devices in launch script and amp init

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -11,10 +11,10 @@ function agnos_init {
   # set success flag for current boot slot
   sudo abctl --set_success
 
-  # TODO: do this without udev in AGNOS
-  # udev does this, but sometimes we startup faster
-  sudo chgrp gpu /dev/adsprpc-smd /dev/ion /dev/kgsl-3d0
-  sudo chmod 660 /dev/adsprpc-smd /dev/ion /dev/kgsl-3d0
+  # udev does this, but sometimes we startup faster (downstream kernel only)
+  for dev in /dev/adsprpc-smd /dev/ion /dev/kgsl-3d0; do
+    [ -e "$dev" ] && sudo chgrp gpu "$dev" && sudo chmod 660 "$dev"
+  done
 
   # Check if AGNOS update is required
   if [ $(< /VERSION) != "$AGNOS_VERSION" ]; then
