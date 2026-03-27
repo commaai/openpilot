f790f12def64139e2838085561aaea3bd98fa842
Karel Nagel <nagelkarel@gmail.com>
2026-03-27 11:26:09 +0200

    add asius api toggle back

--- selfdrive/ui/layouts/settings/asius.py
+++ selfdrive/ui/layouts/settings/asius.py
@@ -1,8 +1,13 @@
 from openpilot.common.params import Params
-from openpilot.system.ui.widgets import Widget
+from openpilot.system.ui.widgets import Widget, DialogResult
 from openpilot.system.ui.widgets.list_view import toggle_item
 from openpilot.system.ui.widgets.scroller_tici import Scroller
+from openpilot.system.ui.widgets.confirm_dialog import ConfirmDialog
+from openpilot.system.ui.lib.application import gui_app
 from openpilot.system.ui.lib.multilang import tr, tr_noop
+
+ASIUS_API_HOST = "https://api.asius.ai"
+ASIUS_ATHENA_HOST = "wss://api.asius.ai"
 
 DESCRIPTIONS = {
   "EnableWebRTC": tr_noop("Allow remote live streaming via Connect."),
@@ -51,6 +56,16 @@ class AsiusLayout(Widget):
       self._toggles[param] = toggle
       self._items.append(toggle)
 
+    is_asius = self._params.get("APIHost") == ASIUS_API_HOST
+    self._api_toggle = toggle_item(
+      lambda: tr("Asius API"),
+      tr_noop("Use Asius API instead of comma. Requires reboot."),
+      is_asius,
+      callback=self._handle_api_toggle,
+      icon="../asius/asius.png",
+    )
+    self._items.append(self._api_toggle)
+
     self._scroller = Scroller(self._items, line_separator=True, spacing=0)
 
   def show_event(self):
@@ -61,6 +76,25 @@ class AsiusLayout(Widget):
     for param in self._toggle_defs:
       state = self._params.get_bool(param)
       self._toggles[param].action_item.set_state(state)
+    self._api_toggle.action_item.set_state(self._params.get("APIHost") == ASIUS_API_HOST)
 
   def _render(self, rect):
     self._scroller.render(rect)
+
+  def _handle_api_toggle(self, state: bool):
+    msg = tr("Switch to Asius API and reboot?") if state else tr("Switch to comma API and reboot?")
+
+    def confirm_callback(result: int):
+      if result == DialogResult.CONFIRM:
+        if state:
+          self._params.put("APIHost", ASIUS_API_HOST)
+          self._params.put("AthenaHost", ASIUS_ATHENA_HOST)
+        else:
+          self._params.remove("APIHost")
+          self._params.remove("AthenaHost")
+        self._params.remove("DongleId")
+        self._params.put_bool_nonblocking("DoReboot", True)
+      else:
+        self._api_toggle.action_item.set_state(not state)
+
+    gui_app.set_modal_overlay(ConfirmDialog(msg, tr("Switch and Reboot")), callback=confirm_callback)
--- selfdrive/ui/mici/layouts/settings/asius.py
+++ selfdrive/ui/mici/layouts/settings/asius.py
@@ -1,19 +1,29 @@
+from openpilot.common.params import Params
 from openpilot.system.ui.widgets.scroller import NavScroller
-from openpilot.selfdrive.ui.mici.widgets.button import BigParamControl, BigCircleParamControl
+from openpilot.system.ui.lib.application import gui_app
+from openpilot.selfdrive.ui.mici.widgets.button import BigParamControl, BigCircleParamControl, BigCircleToggle
+from openpilot.selfdrive.ui.mici.widgets.dialog import BigConfirmationDialog
 from openpilot.selfdrive.ui.ui_state import ui_state
+
+ASIUS_API_HOST = "https://api.asius.ai"
+ASIUS_ATHENA_HOST = "wss://api.asius.ai"
 
 
 class AsiusLayoutMici(NavScroller):
   def __init__(self):
     super().__init__()
+    self._params = Params()
 
     lane_turn_toggle = BigParamControl("lane turn desire", "LaneTurnDesire")
     coop_steering_toggle = BigParamControl("cooperative steering", "TeslaCoopSteering")
 
     self._webrtc_toggle = BigCircleParamControl("asius/webrtc_short.png", "EnableWebRTC", icon_size=(82, 82), icon_offset=(0, 12))
+    self._api_toggle = BigCircleToggle("asius/asius_short.png", toggle_callback=self._handle_api_toggle, icon_size=(82, 82), icon_offset=(0, 12))
+    self._api_toggle.set_checked(self._params.get("APIHost") == ASIUS_API_HOST)
 
     self._scroller.add_widgets([
       self._webrtc_toggle,
+      self._api_toggle,
       lane_turn_toggle,
       coop_steering_toggle,
     ])
@@ -32,8 +42,25 @@ class AsiusLayoutMici(NavScroller):
     ui_state.update_params()
     for key, item in self._refresh_toggles:
       item.set_checked(ui_state.params.get_bool(key))
+    self._api_toggle.set_checked(self._params.get("APIHost") == ASIUS_API_HOST)
 
   def _render(self, rect):
     self._update_toggles()
     super()._render(rect)
+
+  def _handle_api_toggle(self, state: bool):
+    title = "switch to asius api" if state else "switch to comma api"
+
+    def on_confirm():
+      if state:
+        self._params.put("APIHost", ASIUS_API_HOST)
+        self._params.put("AthenaHost", ASIUS_ATHENA_HOST)
+      else:
+        self._params.remove("APIHost")
+        self._params.remove("AthenaHost")
+      self._params.remove("DongleId")
+      self._params.put_bool_nonblocking("DoReboot", True)
+
+    dlg = BigConfirmationDialog(title, "icons_mici/settings/device/reboot.png", red=True, confirm_callback=on_confirm)
+    gui_app.set_modal_overlay(dlg, callback=lambda _: self._api_toggle.set_checked(self._params.get("APIHost") == ASIUS_API_HOST))
 
