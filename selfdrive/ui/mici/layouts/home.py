fca31bc7c0c85b3eab7f0e05ad5b9f674defaa34
elkoled <elkoled@gmail.com>
2026-04-14 16:25:53 -0700

    50/30 split

--- selfdrive/ui/mici/layouts/home.py
+++ selfdrive/ui/mici/layouts/home.py
@@ -14,7 +14,8 @@ from openpilot.system.version import RELEASE_BRANCHES
 
 HEAD_BUTTON_FONT_SIZE = 40
 HOME_PADDING = 8
-TOUCH_ZONE_WIDTH = 200
+SETTINGS_ZONE_WIDTH = 268
+ALERTS_ZONE_WIDTH = 160
 
 NetworkType = log.DeviceState.NetworkType
 
@@ -185,10 +186,10 @@ class MiciHomeLayout(Widget):
     if not self._did_long_press:
       relative_x = mouse_pos.x - self.rect.x
       has_alerts = self._alert_count_callback and self._alert_count_callback() > 0
-      if relative_x < TOUCH_ZONE_WIDTH:
+      if relative_x < SETTINGS_ZONE_WIDTH:
         if self._on_settings_click:
           self._on_settings_click()
-      elif has_alerts and relative_x > self.rect.width - TOUCH_ZONE_WIDTH:
+      elif has_alerts and relative_x > self.rect.width - ALERTS_ZONE_WIDTH:
         if self._on_alerts_click:
           self._on_alerts_click()
     self._did_long_press = False
