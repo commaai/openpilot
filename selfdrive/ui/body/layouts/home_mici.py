a6d65cfd1fc9070e6b6323a96942503abcb4d8c3
stefpi <19478336+stefpi@users.noreply.github.com>
2026-04-15 15:45:08 -0700

    save a line

--- selfdrive/ui/body/layouts/home_mici.py
+++ selfdrive/ui/body/layouts/home_mici.py
@@ -14,7 +14,7 @@ class MiciBodyHomeLayout(MiciHomeLayout):
     super().__init__()
     self.set_visible(False)
     self._branch_label.set_visible(False)
-    self._pair_message = UnifiedLabel("", font_size=36, text_color=rl.WHITE, font_weight=FontWeight.ROMAN, scroll=True)
+    self._pair_message = UnifiedLabel(" comma body ", font_size=36, text_color=rl.WHITE, font_weight=FontWeight.ROMAN, scroll=True)
 
   def _render(self, rect: rl.Rectangle):
     super()._render(rect)
@@ -22,5 +22,4 @@ class MiciBodyHomeLayout(MiciHomeLayout):
     rl.draw_rectangle_rec(self._branch_label._rect, rl.BLACK)
     self._pair_message.set_max_width(self._branch_label._max_width)
     self._pair_message._rect = self._branch_label._rect
-    self._pair_message.set_text(" comma body ")
     self._pair_message.render()
