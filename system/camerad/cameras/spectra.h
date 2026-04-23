bbf14ac8a8cfaef8e3a918ca915d77e2d2c90ff9
ZwX1616 <zwx1616@gmail.com>
2026-04-22 20:00:54 -0700

    disable for OX

--- system/camerad/cameras/spectra.h
+++ system/camerad/cameras/spectra.h
@@ -174,7 +174,7 @@ public:
   SpectraBuf bps_striping;
   SpectraBuf bps_linearization_lut;
   SpectraBuf bps_gamma_lut;
-  SpectraBuf bps_full_scratch;
+  SpectraBuf bps_fullres_dummy;
   std::vector<uint32_t> bps_lin_reg;
   std::vector<uint32_t> bps_ccm_reg;
 
