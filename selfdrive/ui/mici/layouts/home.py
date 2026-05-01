b870fa3041ca2f0e216f7129e7ca74bea3d6f53a
Shane Smiskol <shane@smiskol.com>
2026-05-01 01:56:19 -0700

    fix

--- selfdrive/ui/mici/layouts/home.py
+++ selfdrive/ui/mici/layouts/home.py
@@ -14,7 +14,6 @@ from openpilot.system.version import RELEASE_BRANCHES
 
 HEAD_BUTTON_FONT_SIZE = 40
 HOME_PADDING = 8
-SETTINGS_ZONE_WIDTH = 280
 ALERTS_ZONE_WIDTH = 180
 
 NetworkType = log.DeviceState.NetworkType
@@ -205,12 +204,11 @@ class MiciHomeLayout(Widget):
     if not self._did_long_press:
       relative_x = mouse_pos.x - self.rect.x
       has_alerts = self._alert_count_callback and self._alert_count_callback() > 0
-      if relative_x < SETTINGS_ZONE_WIDTH:
-        if self._on_settings_click:
-          self._on_settings_click()
-      elif has_alerts and relative_x > self.rect.width - ALERTS_ZONE_WIDTH:
+      if has_alerts and relative_x > self.rect.width - ALERTS_ZONE_WIDTH:
         if self._on_alerts_click:
           self._on_alerts_click()
+      elif self._on_settings_click:
+        self._on_settings_click()
     self._did_long_press = False
 
   def _get_version_text(self) -> tuple[str, str, str, str] | None:
@@ -272,3 +270,10 @@ class MiciHomeLayout(Widget):
     self._alerts_pill.set_position(self.rect.x + self.rect.width - self._alerts_pill.rect.width - HOME_PADDING,
                                    self.rect.y + self.rect.height - self._alerts_pill.rect.height)
     self._alerts_pill.render()
+
+    # DEBUG: alerts touch zone
+    has_alerts = self._alert_count_callback and self._alert_count_callback() > 0
+    if has_alerts:
+      zone = rl.Rectangle(self.rect.x + self.rect.width - ALERTS_ZONE_WIDTH, self.rect.y, ALERTS_ZONE_WIDTH, self.rect.height)
+      rl.draw_rectangle_rec(zone, rl.Color(255, 0, 0, 60))
+      rl.draw_rectangle_lines_ex(zone, 2, rl.Color(255, 0, 0, 200))
