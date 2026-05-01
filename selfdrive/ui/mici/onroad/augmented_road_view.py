63a8dbf87fabee48829eb03fd5ffb214004fdb8d
Shane Smiskol <shane@smiskol.com>
2026-05-01 00:29:51 -0700

    fix frame under text

--- selfdrive/ui/mici/onroad/augmented_road_view.py
+++ selfdrive/ui/mici/onroad/augmented_road_view.py
@@ -186,10 +186,6 @@ class AugmentedRoadView(CameraView):
 
   def _render(self, _):
     start_draw = time.monotonic()
-    self._switch_stream_if_needed(ui_state.sm)
-
-    # Update calibration before rendering
-    self._update_calibration()
 
     # Create inner content area with border padding
     self._content_rect = rl.Rectangle(
@@ -199,56 +195,53 @@ class AugmentedRoadView(CameraView):
       self.rect.height,
     )
 
-    # Enable scissor mode to clip all rendering within content rectangle boundaries
-    # This creates a rendering viewport that prevents graphics from drawing outside the border
-    rl.begin_scissor_mode(
-      int(self._content_rect.x),
-      int(self._content_rect.y),
-      int(self._content_rect.width),
-      int(self._content_rect.height)
-    )
-
-    # Render the base camera view
-    super()._render(self._content_rect)
-
-    # Draw all UI overlays
-    self._model_renderer.render(self._content_rect)
-
-    # Fade out bottom of overlays for looks
-    rl.draw_texture_ex(self._fade_texture, rl.Vector2(self._content_rect.x, self._content_rect.y), 0.0, 1.0, rl.WHITE)
-
-    alert_to_render, not_animating_out = self._alert_renderer.will_render()
-
-    # Hide DMoji when disengaged unless AlwaysOnDM is enabled
-    should_draw_dmoji = (not self._hud_renderer.drawing_top_icons() and ui_state.is_onroad() and
-                         (ui_state.status != UIStatus.DISENGAGED or ui_state.always_on_dm))
-    self._driver_state_renderer.set_should_draw(should_draw_dmoji)
-    self._driver_state_renderer.set_position(self._rect.x + 16, self._rect.y + 10)
-    self._driver_state_renderer.render()
-
-    self._hud_renderer.set_can_draw_top_icons(alert_to_render is None)
-    self._hud_renderer.set_wheel_critical_icon(alert_to_render is not None and not not_animating_out and
-                                               alert_to_render.visual_alert == car.CarControl.HUDControl.VisualAlert.steerRequired)
-    # TODO: have alert renderer draw offroad mici label below
     if ui_state.started:
+      self._switch_stream_if_needed(ui_state.sm)
+      self._update_calibration()
+
+      # Enable scissor mode to clip all rendering within content rectangle boundaries
+      # This creates a rendering viewport that prevents graphics from drawing outside the border
+      rl.begin_scissor_mode(
+        int(self._content_rect.x),
+        int(self._content_rect.y),
+        int(self._content_rect.width),
+        int(self._content_rect.height)
+      )
+
+      # Render the base camera view
+      super()._render(self._content_rect)
+
+      # Draw all UI overlays
+      self._model_renderer.render(self._content_rect)
+
+      # Fade out bottom of overlays for looks
+      rl.draw_texture_ex(self._fade_texture, rl.Vector2(self._content_rect.x, self._content_rect.y), 0.0, 1.0, rl.WHITE)
+
+      alert_to_render, not_animating_out = self._alert_renderer.will_render()
+
+      # Hide DMoji when disengaged unless AlwaysOnDM is enabled
+      should_draw_dmoji = (not self._hud_renderer.drawing_top_icons() and
+                           (ui_state.status != UIStatus.DISENGAGED or ui_state.always_on_dm))
+      self._driver_state_renderer.set_should_draw(should_draw_dmoji)
+      self._driver_state_renderer.set_position(self._rect.x + 16, self._rect.y + 10)
+      self._driver_state_renderer.render()
+
+      self._hud_renderer.set_can_draw_top_icons(alert_to_render is None)
+      self._hud_renderer.set_wheel_critical_icon(alert_to_render is not None and not not_animating_out and
+                                                 alert_to_render.visual_alert == car.CarControl.HUDControl.VisualAlert.steerRequired)
       self._alert_renderer.render(self._content_rect)
-    self._hud_renderer.render(self._content_rect)
+      self._hud_renderer.render(self._content_rect)
 
-    # Draw fake rounded border
-    rl.draw_rectangle_rounded_lines_ex(self._content_rect, 0.2 * 1.02, 10, 50, rl.BLACK)
+      # Draw fake rounded border
+      rl.draw_rectangle_rounded_lines_ex(self._content_rect, 0.2 * 1.02, 10, 50, rl.BLACK)
 
-    # End clipping region
-    rl.end_scissor_mode()
+      rl.end_scissor_mode()
 
-    # Custom UI extension point - add custom overlays here
-    # Use self._content_rect for positioning within camera bounds
-    self._confidence_ball.render(self.rect)
-
-    self._bookmark_icon.render(self.rect)
-
-    # Draw darkened background and text if not onroad
-    if not ui_state.started:
-      rl.draw_rectangle(int(self.rect.x), int(self.rect.y), int(self.rect.width), int(self.rect.height), rl.Color(0, 0, 0, 175))
+      self._confidence_ball.render(self.rect)
+      self._bookmark_icon.render(self.rect)
+    else:
+      # Offroad: solid black with label, never show stale camera/overlays
+      rl.draw_rectangle(int(self.rect.x), int(self.rect.y), int(self.rect.width), int(self.rect.height), rl.BLACK)
       self._offroad_label.render(self._rect)
 
     # publish uiDebug
