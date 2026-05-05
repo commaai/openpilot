34ceb4aa0feef458048d72aa03af0d6bb78c593b
Armandpl <adpl33@gmail.com>
2026-05-04 17:23:05 -0700

    oops2

--- selfdrive/ui/mici/onroad/augmented_road_view.py
+++ selfdrive/ui/mici/onroad/augmented_road_view.py
@@ -243,11 +243,6 @@ class AugmentedRoadView(CameraView):
 
     self._bookmark_icon.render(self.rect)
 
-    # Draw darkened background and text if not onroad
-    if not ui_state.started:
-      rl.draw_rectangle(int(self.rect.x), int(self.rect.y), int(self.rect.width), int(self.rect.height), rl.Color(0, 0, 0, 175))
-      self._offroad_label.render(self._rect)
-
   def _switch_stream_if_needed(self, sm):
     if sm['selfdriveState'].experimentalMode and WIDE_CAM in self.available_streams:
       v_ego = sm['carState'].vEgo
