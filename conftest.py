613379d72820d2dba8ba35c89cb142f35d16c7b0
hypery11 <4160414+hypery11@users.noreply.github.com>
2026-05-07 11:14:52 +0800

    speedup pytest collection

--- conftest.py
+++ conftest.py
@@ -7,18 +7,6 @@ from openpilot.common.prefix import OpenpilotPrefix
 from openpilot.system.manager import manager
 from openpilot.system.hardware import TICI, HARDWARE
 
-# TODO: pytest-cpp doesn't support FAIL, and we need to create test translations in sessionstart
-# pending https://github.com/pytest-dev/pytest-cpp/pull/147
-collect_ignore = [
-  "selfdrive/test/process_replay/test_processes.py",
-  "selfdrive/test/process_replay/test_regen.py",
-]
-collect_ignore_glob = [
-  "selfdrive/debug/*.py",
-  "selfdrive/modeld/*.py",
-]
-
-
 def pytest_sessionstart(session):
   # TODO: fix tests and enable test order randomization
   if session.config.pluginmanager.hasplugin('randomly'):
