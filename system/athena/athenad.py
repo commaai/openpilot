cdb7360b5fe2938c498631e368c78b4c5adda4fc
stefpi <19478336+stefpi@users.noreply.github.com>
2026-05-11 16:18:00 -0700

    request sound service from joystick stream

--- system/athena/athenad.py
+++ system/athena/athenad.py
@@ -571,7 +571,7 @@ def getNetworks():
 @dispatcher.add_method
 def startJoystickStream(sdp: str) -> dict:
   from openpilot.system.webrtc.webrtcd import StreamRequestBody
-  body = StreamRequestBody(sdp, ["driver"], ["testJoystick"], ["carState"])
+  body = StreamRequestBody(sdp, ["driver"], ["testJoystick", "soundRequest"], ["carState"])
   try:
     resp = requests.post(f"http://localhost:{WEBRTCD_PORT}/stream",
                        json=asdict(body), timeout=10)
