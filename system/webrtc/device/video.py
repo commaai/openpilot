429679654f1ebaf71c92f111f32aa68c79d512eb
stefpi <19478336+stefpi@users.noreply.github.com>
2026-05-11 15:15:26 -0700

    reduce diff

--- system/webrtc/device/video.py
+++ system/webrtc/device/video.py
@@ -14,6 +14,7 @@ class LiveStreamVideoStreamTrack(TiciVideoStreamTrack):
     super().__init__(camera_type, dt)
 
     self._sock = messaging.sub_sock("livestreamCameraEncodeData", conflate=True)
+    self._pts = 0
     self._t0_ns = time.monotonic_ns()
 
   async def recv(self):
@@ -23,14 +24,15 @@ class LiveStreamVideoStreamTrack(TiciVideoStreamTrack):
         break
       await asyncio.sleep(0.005)
 
-    encode_data = getattr(msg, msg.which())
+    evta = getattr(msg, msg.which())
 
-    packet = av.Packet(encode_data.header + encode_data.data)
+    packet = av.Packet(evta.header + evta.data)
     packet.time_base = self._time_base
 
-    pts = ((time.monotonic_ns() - self._t0_ns) * self._clock_rate) // 1_000_000_000
-    packet.pts = pts
-    self.log_debug("track sending frame %d", pts)
+    self._pts =  ((time.monotonic_ns() - self._t0_ns) * self._clock_rate) // 1_000_000_000
+    packet.pts = self._pts
+    self.log_debug("track sending frame %d", self._pts)
+
     return packet
 
   def codec_preference(self) -> str | None:
