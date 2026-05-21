ac9c25002cdec5b0b1177a2b243cb3afa63ed184
zhangxu <xu.zhang@dlssa.com>
2026-05-21 14:38:07 +0800

    - Merge multi-line formatted log print statement into single line to resolve ISC002 implicit string concatenation and UP034 extraneous parentheses lint errors.

--- tools/camerastream/compressed_vipc.py
+++ tools/camerastream/compressed_vipc.py
@@ -175,10 +175,7 @@ def decoder(addr, vipc_server, vst, nvidia, W, H, debug=False):
       pc_latency = (time.monotonic()-time_q[0])*1000
       time_q = time_q[1:]
       if debug:
-        print(f" 1 {evta.idx.encodeId:4d} {evt.logMonoTime/1e9:.3f} {evta.idx.timestampEof/1e6:.3f} "
-              f"roll {frame_latency:6.2f} ms latency {process_latency:6.2f} ms + {network_latency:6.2f} ms + {pc_latency:6.2f} ms "
-              f"= {process_latency+network_latency+pc_latency:6.2f} ms [{len(evta.data)} bytes] {sock_name}")
-
+          print(f" 1 {evta.idx.encodeId:4d} {evt.logMonoTime/1e9:.3f} {evta.idx.timestampEof/1e6:.3f} roll {frame_latency:6.2f} ms latency {process_latency:6.2f} ms + {network_latency:6.2f} ms + {pc_latency:6.2f} ms = {process_latency+network_latency+pc_latency:6.2f} ms [{len(evta.data)} bytes] {sock_name}")
 
 class CompressedVipc:
   def __init__(self, addr, vision_streams, server_name, nvidia=False, debug=False):
