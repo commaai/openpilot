c87f613659fa6d6f3d25214d05bdb085e14312c0
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-06-01 20:43:32 -0700

    agnos 18.4: pin pycapnp to 2.1.0 (#38126)
    * Revert "modeld: fix capnp memory leak (#38117)"
    
    This reverts commit 0f17a9879364c6f410df7ddf990380ad623a4c92.
    
    * Revert "fix memory leak from pycapnp 2.2+ (#38001)"
    
    This reverts commit 294cb687f6c894ff7af051455f929b9094b6dff1.
    
    * downgrade
    
    * agnos 18.4

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -57,11 +57,9 @@ def get_action_from_model(model_output: dict[str, np.ndarray], prev_action: log.
   else:
     desired_curvature = prev_action.desiredCurvature
 
-  action = log.ModelDataV2.Action()
-  action.desiredCurvature = float(desired_curvature)
-  action.desiredAcceleration = float(desired_accel)
-  action.shouldStop = bool(should_stop)
-  return action
+  return log.ModelDataV2.Action(desiredCurvature=float(desired_curvature),
+                                desiredAcceleration=float(desired_accel),
+                                shouldStop=bool(should_stop))
 
 
 class FrameMeta:
