ba28510977eb1e6392e29b9c58813953b45de347
armandpl <adpl33@gmail.com>
2026-06-10 16:48:57 -0700

    whitespace

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -325,7 +325,6 @@ def main(demo=False):
 
       fill_driving_model_data(drivingdata_send, modelv2_send)
       fill_pose_msg(posenet_send, model_output, meta_main.frame_id, vipc_dropped_frames, meta_main.timestamp_eof, live_calib_seen)
-
       pm.send('modelV2', modelv2_send)
       pm.send('drivingModelData', drivingdata_send)
       pm.send('cameraOdometry', posenet_send)
