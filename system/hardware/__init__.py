7ec2cc8d4fd4392acaf0279cf883819ecbfc0dd2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-06-20 15:59:11 -0700

    rm shim

--- system/hardware/__init__.py
+++
@@ -1,4 +0,0 @@
-from openpilot.common.hardware import AGNOS as AGNOS
-from openpilot.common.hardware import HARDWARE as HARDWARE
-from openpilot.common.hardware import PC as PC
-from openpilot.common.hardware import TICI as TICI
