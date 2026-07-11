e0b10c82fc2a6c0ccfce9ab82add31b2e03a8c3e
Bruce Wayne <harald.the.engineer@gmail.com>
2026-07-10 19:16:46 -0700

    lil smooth

--- openpilot/selfdrive/modeld/modeld.py
+++ openpilot/selfdrive/modeld/modeld.py
@@ -30,7 +30,7 @@ from openpilot.selfdrive.modeld.usbgpu_link import wait_usbgpu_link
 PROCESS_NAME = "openpilot.selfdrive.modeld.modeld"
 SEND_RAW_PRED = os.getenv('SEND_RAW_PRED')
 
-LAT_SMOOTH_SECONDS = 0.0
+LAT_SMOOTH_SECONDS = 0.1
 LONG_SMOOTH_SECONDS = 0.3
 MIN_LAT_CONTROL_SPEED = 0.3
 
