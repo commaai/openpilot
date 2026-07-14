10cda0378760fa3bc9772cd08413b2979552947f
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-14 15:34:42 -0700

    longcontrol: simplify state transitions and reset path

--- openpilot/selfdrive/controls/lib/longcontrol.py
+++ openpilot/selfdrive/controls/lib/longcontrol.py
@@ -12,36 +12,27 @@ LongCtrlState = car.CarControl.Actuators.LongControlState
 
 def long_control_state_trans(CP, active, long_control_state, v_ego,
                              should_stop, brake_pressed, cruise_standstill):
-  stopping_condition = should_stop
   starting_condition = (not should_stop and
                         not cruise_standstill and
                         not brake_pressed)
-  started_condition = v_ego > CP.vEgoStarting
 
   if not active:
-    long_control_state = LongCtrlState.off
+    return LongCtrlState.off
 
-  else:
-    if long_control_state == LongCtrlState.off:
-      if not starting_condition:
-        long_control_state = LongCtrlState.stopping
-      else:
-        if starting_condition and CP.startingState:
-          long_control_state = LongCtrlState.starting
-        else:
-          long_control_state = LongCtrlState.pid
+  if long_control_state == LongCtrlState.off:
+    if not starting_condition:
+      return LongCtrlState.stopping
+    return LongCtrlState.starting if CP.startingState else LongCtrlState.pid
 
-    elif long_control_state == LongCtrlState.stopping:
-      if starting_condition and CP.startingState:
-        long_control_state = LongCtrlState.starting
-      elif starting_condition:
-        long_control_state = LongCtrlState.pid
+  if long_control_state == LongCtrlState.stopping:
+    if starting_condition:
+      return LongCtrlState.starting if CP.startingState else LongCtrlState.pid
+    return LongCtrlState.stopping
 
-    elif long_control_state in [LongCtrlState.starting, LongCtrlState.pid]:
-      if stopping_condition:
-        long_control_state = LongCtrlState.stopping
-      elif started_condition:
-        long_control_state = LongCtrlState.pid
+  if should_stop:
+    return LongCtrlState.stopping
+  if long_control_state == LongCtrlState.starting and v_ego > CP.vEgoStarting:
+    return LongCtrlState.pid
   return long_control_state
 
 class LongControl:
