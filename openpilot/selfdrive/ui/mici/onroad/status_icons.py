1e0fe6c83443a232bfe933b02e6c8c807bb0952e
Bruce Wayne <harald.the.engineer@gmail.com>
2026-07-15 13:48:55 -0700

    ui: use raw speed limiter icons

--- openpilot/selfdrive/ui/mici/onroad/status_icons.py
+++ openpilot/selfdrive/ui/mici/onroad/status_icons.py
@@ -10,9 +10,6 @@ from openpilot.system.ui.widgets import Widget
 
 INACTIVE_COLOR = rl.Color(86, 86, 86, 255)
 INACTIVE_TEXTURE_TINT = rl.Color(140, 140, 140, 255)
-CRUISE_COLOR = rl.Color(0, 163, 255, 255)
-LEAD_COLOR = rl.Color(255, 190, 0, 255)
-E2E_COLOR = rl.Color(0, 255, 204, 255)
 
 
 class StatusIconColumn(Widget):
@@ -22,6 +19,11 @@ class StatusIconColumn(Widget):
     super().__init__()
 
     self._experimental_texture = gui_app.texture("icons_mici/experimental_mode.png", 48, 48)
+    self._speed_limiter_textures = {
+      SpeedLimiter.CRUISE: gui_app.texture("icons_mici/speed_limiter_cruise.png", 48, 48),
+      SpeedLimiter.LEAD: gui_app.texture("icons_mici/speed_limiter_lead.png", 48, 48),
+      SpeedLimiter.E2E: self._experimental_texture,
+    }
     self._egpu_texture = gui_app.texture("icons_mici/egpu.png", 50, 37)
     self._egpu_gray_texture = gui_app.texture("icons_mici/egpu_gray.png", 50, 37)
     self._personality_textures = tuple(
@@ -52,53 +54,9 @@ class StatusIconColumn(Widget):
     pos = rl.Vector2(center.x - texture.width / 2, center.y - texture.height / 2)
     rl.draw_texture_ex(texture, pos, 0.0, 1.0, tint)
 
-  @staticmethod
-  def _draw_cruise_icon(center: rl.Vector2, color: rl.Color):
-    gauge_center = rl.Vector2(center.x, center.y + 4)
-    rl.draw_ring(gauge_center, 14, 19, 180, 360, 32, color)
-    rl.draw_line_ex(gauge_center, rl.Vector2(center.x + 11, center.y - 6), 4, color)
-    rl.draw_circle(int(gauge_center.x), int(gauge_center.y), 4, color)
-
-  @staticmethod
-  def _draw_lead_icon(center: rl.Vector2, color: rl.Color, active: bool):
-    # Front-view car silhouette.
-    rl.draw_rectangle_rounded(rl.Rectangle(center.x - 12, center.y - 18, 24, 17), 0.35, 8, color)
-    rl.draw_rectangle_rounded(rl.Rectangle(center.x - 18, center.y - 8, 36, 23), 0.3, 8, color)
-    rl.draw_rectangle_rec(rl.Rectangle(center.x - 8, center.y - 14, 16, 9), rl.BLACK)
-    detail_color = rl.WHITE if active else color
-    rl.draw_circle(int(center.x - 10), int(center.y + 7), 3, detail_color)
-    rl.draw_circle(int(center.x + 10), int(center.y + 7), 3, detail_color)
-    rl.draw_rectangle_rec(rl.Rectangle(center.x - 19, center.y + 5, 4, 10), color)
-    rl.draw_rectangle_rec(rl.Rectangle(center.x + 15, center.y + 5, 4, 10), color)
-
-  @staticmethod
-  def _draw_e2e_icon(center: rl.Vector2, color: rl.Color):
-    # A compact network glyph for the end-to-end model.
-    start = rl.Vector2(center.x, center.y + 18)
-    left = rl.Vector2(center.x - 13, center.y)
-    right = rl.Vector2(center.x + 13, center.y)
-    end = rl.Vector2(center.x, center.y - 18)
-    for first, second in ((start, left), (start, right), (left, end), (right, end)):
-      rl.draw_line_ex(first, second, 4, color)
-    for point in (start, left, right, end):
-      rl.draw_circle(int(point.x), int(point.y), 5, color)
-
   def _draw_speed_limiter(self, center: rl.Vector2):
-    if not self._speed_limiter_active:
-      color = INACTIVE_COLOR
-    elif self._speed_limiter == SpeedLimiter.LEAD:
-      color = LEAD_COLOR
-    elif self._speed_limiter == SpeedLimiter.E2E:
-      color = E2E_COLOR
-    else:
-      color = CRUISE_COLOR
-
-    if self._speed_limiter == SpeedLimiter.LEAD:
-      self._draw_lead_icon(center, color, self._speed_limiter_active)
-    elif self._speed_limiter == SpeedLimiter.E2E:
-      self._draw_e2e_icon(center, color)
-    else:
-      self._draw_cruise_icon(center, color)
+    tint = rl.WHITE if self._speed_limiter_active else INACTIVE_COLOR
+    self._draw_texture_centered(self._speed_limiter_textures[self._speed_limiter], center, tint)
 
   def _render(self, _):
     panel = rl.Rectangle(
