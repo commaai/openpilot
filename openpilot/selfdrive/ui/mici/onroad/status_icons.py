869af87cda66ac9cb64034fe25562ed32c5b3304
Bruce Wayne <harald.the.engineer@gmail.com>
2026-07-15 21:37:53 -0700

    ui: fix status icon states

--- openpilot/selfdrive/ui/mici/onroad/status_icons.py
+++ openpilot/selfdrive/ui/mici/onroad/status_icons.py
@@ -7,14 +7,7 @@ from openpilot.system.ui.lib.application import gui_app
 from openpilot.system.ui.widgets import Widget
 
 
-INACTIVE_COLOR = rl.Color(86, 86, 86, 255)
-INACTIVE_TEXTURE_TINT = rl.Color(140, 140, 140, 255)
 PlanSource = log.LongitudinalPlan.LongitudinalPlanSource
-PERSONALITY_BARS = {
-  log.LongitudinalPersonality.relaxed: 1,
-  log.LongitudinalPersonality.standard: 2,
-  log.LongitudinalPersonality.aggressive: 3,
-}
 
 
 class StatusIconColumn(Widget):
@@ -27,7 +20,6 @@ class StatusIconColumn(Widget):
     self._cruise_texture = gui_app.texture("icons_mici/speed_limiter_cruise.png", 48, 48)
     self._lead_texture = gui_app.texture("icons_mici/speed_limiter_lead.png", 48, 48)
     self._egpu_texture = gui_app.texture("icons_mici/egpu.png", 50, 37)
-    self._egpu_gray_texture = gui_app.texture("icons_mici/egpu_gray.png", 50, 37)
     self._personality_textures = tuple(
       gui_app.texture(f"icons_mici/personality_{bar_count}.png", 56, 20)
       for bar_count in range(1, 4)
@@ -44,7 +36,17 @@ class StatusIconColumn(Widget):
     selfdrive_state_valid = sm.alive["selfdriveState"] and sm.valid["selfdriveState"]
     self._experimental_mode = selfdrive_state_valid and sm["selfdriveState"].experimentalMode
     personality_available = selfdrive_state_valid and ui_state.has_longitudinal_control
-    self._personality_bar_count = PERSONALITY_BARS.get(sm["selfdriveState"].personality, 0) if personality_available else 0
+    personality = sm["selfdriveState"].personality
+    if not personality_available:
+      self._personality_bar_count = 0
+    elif personality == log.LongitudinalPersonality.relaxed:
+      self._personality_bar_count = 1
+    elif personality == log.LongitudinalPersonality.standard:
+      self._personality_bar_count = 2
+    elif personality == log.LongitudinalPersonality.aggressive:
+      self._personality_bar_count = 3
+    else:
+      self._personality_bar_count = 0
     source = sm["longitudinalPlan"].longitudinalPlanSource
     if source in (PlanSource.lead0, PlanSource.lead1, PlanSource.lead2):
       self._speed_limiter_texture = self._lead_texture
@@ -72,13 +74,11 @@ class StatusIconColumn(Widget):
     center_x = panel.x + panel.width / 2
     centers = [rl.Vector2(center_x, panel.y + panel.height * (slot + 0.5) / 4) for slot in range(4)]
 
-    experimental_tint = rl.WHITE if self._experimental_mode else INACTIVE_COLOR
-    self._draw_texture_centered(self._experimental_texture, centers[0], experimental_tint)
-    limiter_tint = rl.WHITE if self._speed_limiter_active else INACTIVE_COLOR
-    self._draw_texture_centered(self._speed_limiter_texture, centers[1], limiter_tint)
-    egpu_texture = self._egpu_texture if self._using_egpu else self._egpu_gray_texture
-    egpu_tint = rl.WHITE if self._using_egpu else INACTIVE_TEXTURE_TINT
-    self._draw_texture_centered(egpu_texture, centers[2], egpu_tint)
-    personality_index = self._personality_bar_count - 1 if self._personality_bar_count else 2
-    personality_tint = rl.WHITE if self._personality_bar_count else INACTIVE_COLOR
-    self._draw_texture_centered(self._personality_textures[personality_index], centers[3], personality_tint)
+    if self._experimental_mode:
+      self._draw_texture_centered(self._experimental_texture, centers[0])
+    if self._speed_limiter_active:
+      self._draw_texture_centered(self._speed_limiter_texture, centers[1])
+    if self._using_egpu:
+      self._draw_texture_centered(self._egpu_texture, centers[2])
+    if self._personality_bar_count:
+      self._draw_texture_centered(self._personality_textures[self._personality_bar_count - 1], centers[3])
