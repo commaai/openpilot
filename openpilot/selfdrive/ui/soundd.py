3bf8e3f2ce24fa7c128db4f25c96da6e7c859bc0
elkoled <elkoled@gmail.com>
2026-07-20 14:13:16 -0700

    add space

--- openpilot/selfdrive/ui/soundd.py
+++ openpilot/selfdrive/ui/soundd.py
@@ -45,6 +45,7 @@ sound_list: dict[int, tuple[str, int | None, float]] = {
 
   AudibleAlert.preAlert: ("pre_alert.wav", 1, MAX_VOLUME),
   AudibleAlert.complete: ("complete.wav", 1, MAX_VOLUME),
+
   AudibleAlert.warningSoft: ("critical.wav", None, MAX_VOLUME),
   AudibleAlert.warningImmediate: ("dm_critical.wav", None, MAX_VOLUME),
 }
