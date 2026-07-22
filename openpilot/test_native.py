043d5ba35e493589adee658d0a6a4185057f8dc3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-21 21:43:06 -0700

    replace catch2 tests

--- openpilot/test_native.py
+++ openpilot/test_native.py
@@ -9,10 +9,8 @@ from openpilot.common.test import OpenpilotTestCase
 NATIVE_TESTS = (
   "openpilot/common/tests/test_common",
   "openpilot/selfdrive/pandad/tests/test_pandad_canprotocol",
-  "openpilot/system/loggerd/tests/test_logger",
   "openpilot/tools/cabana/tests/test_cabana",
   "openpilot/tools/cabana/tests/test_dbc_core",
-  "openpilot/tools/replay/tests/test_replay",
 )
 
 
