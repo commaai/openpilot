86ec7062f5ad5a7525f3320cce7807987a183d4c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-23 19:39:35 -0700

    rm that

--- openpilot/selfdrive/car/tests/big_cars_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(dirname "$0")
-BASEDIR=$(realpath "$SCRIPT_DIR/../../../../")
-cd $BASEDIR
-
-pytest -n logical --dist worksteal openpilot/selfdrive/car/tests/test_car_interfaces.py
