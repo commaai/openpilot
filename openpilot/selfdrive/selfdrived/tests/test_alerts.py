a95d42bdd0a97095b4ca03a64c9fd840523052d0
stefpi <19478336+stefpi@users.noreply.github.com>
2026-07-26 14:54:39 -0700

    red diff

--- openpilot/selfdrive/selfdrived/tests/test_alerts.py
+++ openpilot/selfdrive/selfdrived/tests/test_alerts.py
@@ -2,7 +2,6 @@ import copy
 import json
 import os
 import random
-from fontTools.ttLib import TTFont
 
 from openpilot.common.test import OpenpilotTestCase
 from openpilot.cereal import log
@@ -23,15 +22,6 @@ ALERTS = []
 for event_types in EVENTS.values():
   for alert in event_types.values():
     ALERTS.append(alert)
-
-def _load_font(path, size):
-  font = TTFont(path)
-  return font.getBestCmap(), font["hmtx"], font["head"].unitsPerEm, size
-
-def _text_width(font, text):
-  cmap, hmtx, upem, size = font
-  advance = sum(hmtx[cmap.get(ord(ch), ".notdef")][0] for ch in text)
-  return advance * size / upem
 
 
 class TestAlerts(OpenpilotTestCase):
@@ -55,38 +45,6 @@ class TestAlerts(OpenpilotTestCase):
       if not name.endswith("DEPRECATED"):
         fail_msg = f"{name} @{e} not in EVENTS"
         assert e in EVENTS.keys(), fail_msg
-
-  # ensure alert text doesn't exceed allowed width
-  def test_alert_text_length(self):
-    font_path = os.path.join(BASEDIR, "openpilot/selfdrive/assets/fonts")
-    regular_font_path = os.path.join(font_path, "Inter-SemiBold.ttf")
-    bold_font_path = os.path.join(font_path, "Inter-Bold.ttf")
-    semibold_font_path = os.path.join(font_path, "Inter-SemiBold.ttf")
-
-    max_text_width = 2160 - 300  # full screen width is usable, minus sidebar
-
-    fonts = {
-      AlertSize.small: [_load_font(semibold_font_path, 74)],
-      AlertSize.mid: [_load_font(bold_font_path, 88),
-                      _load_font(regular_font_path, 66)],
-    }
-
-    for alert in ALERTS:
-      if not isinstance(alert, Alert):
-        alert = alert(self.CP, self.CS, self.sm, False, 100, log.LongitudinalPersonality.standard)
-
-      # for full size alerts, both text fields wrap the text,
-      # so it's unlikely that they  would go past the max width
-      if alert.alert_size in (AlertSize.none, AlertSize.full):
-        continue
-
-      for i, txt in enumerate([alert.alert_text_1, alert.alert_text_2]):
-        if i >= len(fonts[alert.alert_size]):
-          break
-
-        width = _text_width(fonts[alert.alert_size][i], txt)
-        msg = f"type: {alert.alert_type} msg: {txt}"
-        assert width <= max_text_width, msg
 
   def test_alert_sanity_check(self):
     for event_types in EVENTS.values():
