f29eb5d25c2a0b774ca5935960b12675a89b224b
stef <19478336+stefpi@users.noreply.github.com>
2026-07-26 14:37:19 -0700

    ui: remove emoji support (#38361)
    * remove emoji support, replace with png
    
    * upload icons
    
    * fix icon left/right positioning
    
    * simplify rewrite
    
    * simplify firehose
    
    * simplify custom software set up text
    
    * lint
    
    * fix custom software
    
    * move fix to diff pr
    
    ---------
    
    Co-authored-by: elkoled <elkoled@gmail.com>

--- openpilot/system/ui/lib/text_measure.py
+++ openpilot/system/ui/lib/text_measure.py
@@ -1,6 +1,5 @@
 import pyray as rl
 from openpilot.system.ui.lib.application import FONT_SCALE, font_fallback
-from openpilot.system.ui.lib.emoji import find_emoji
 
 _cache: dict[int, rl.Vector2] = {}
 
@@ -13,24 +12,7 @@ def measure_text_cached(font: rl.Font, text: str, font_size: int, spacing: float
   if key in _cache:
     return _cache[key]
 
-  # Measure normal characters without emojis, then add standard width for each found emoji
-  emoji = find_emoji(text)
-  if emoji:
-    non_emoji_text = ""
-    last_index = 0
-    for start, end, _ in emoji:
-      non_emoji_text += text[last_index:start]
-      last_index = end
-    non_emoji_text += text[last_index:]
-  else:
-    non_emoji_text = text
-
-  result = rl.measure_text_ex(font, non_emoji_text, font_size * FONT_SCALE, spacing)  # noqa: TID251
-  if emoji:
-    result.x += len(emoji) * font_size * FONT_SCALE
-    # If just emoji assume a single line height
-    if result.y == 0:
-      result.y = font_size * FONT_SCALE
+  result = rl.measure_text_ex(font, text, font_size * FONT_SCALE, spacing)  # noqa: TID251
 
   _cache[key] = result
   return result
