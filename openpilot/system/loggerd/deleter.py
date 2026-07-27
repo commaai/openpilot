419dfb5865aadaca4f8d94e77a22f1733fa691c3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-26 21:09:05 -0700

    unittest speedups

--- openpilot/system/loggerd/deleter.py
+++ openpilot/system/loggerd/deleter.py
@@ -45,31 +45,36 @@ def get_preserved_segments(dirs_by_creation: list[str]) -> set[str]:
   return preserved
 
 
+def deleter_step() -> tuple[bool, str | None]:
+  out_of_bytes = get_available_bytes(default=MIN_BYTES + 1) < MIN_BYTES
+  out_of_percent = get_available_percent(default=MIN_PERCENT + 1) < MIN_PERCENT
+  out_of_space = out_of_percent or out_of_bytes
+  if not out_of_space:
+    return False, None
+
+  dirs = listdir_by_creation(Paths.log_root())
+  preserved_dirs = get_preserved_segments(dirs)
+
+  # remove the earliest directory we can
+  for delete_dir in sorted(dirs, key=lambda d: (d in DELETE_LAST, d in preserved_dirs)):
+    delete_path = os.path.join(Paths.log_root(), delete_dir)
+
+    if any(name.endswith(".lock") for name in os.listdir(delete_path)):
+      continue
+
+    try:
+      cloudlog.info(f"deleting {delete_path}")
+      shutil.rmtree(delete_path)
+      return True, delete_path
+    except OSError:
+      cloudlog.exception(f"issue deleting {delete_path}")
+  return True, None
+
+
 def deleter_thread(exit_event: threading.Event):
   while not exit_event.is_set():
-    out_of_bytes = get_available_bytes(default=MIN_BYTES + 1) < MIN_BYTES
-    out_of_percent = get_available_percent(default=MIN_PERCENT + 1) < MIN_PERCENT
-
-    if out_of_percent or out_of_bytes:
-      dirs = listdir_by_creation(Paths.log_root())
-      preserved_dirs = get_preserved_segments(dirs)
-
-      # remove the earliest directory we can
-      for delete_dir in sorted(dirs, key=lambda d: (d in DELETE_LAST, d in preserved_dirs)):
-        delete_path = os.path.join(Paths.log_root(), delete_dir)
-
-        if any(name.endswith(".lock") for name in os.listdir(delete_path)):
-          continue
-
-        try:
-          cloudlog.info(f"deleting {delete_path}")
-          shutil.rmtree(delete_path)
-          break
-        except OSError:
-          cloudlog.exception(f"issue deleting {delete_path}")
-      exit_event.wait(.1)
-    else:
-      exit_event.wait(30)
+    out_of_space, _ = deleter_step()
+    exit_event.wait(.1 if out_of_space else 30)
 
 
 def main():
