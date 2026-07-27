302515df108096ede771e302157b2e483038c440
Bruce Wayne <harald.the.engineer@gmail.com>
2026-07-27 15:35:17 -0700

    Support recurrent modeld KV caches

--- openpilot/selfdrive/modeld/modeld.py
+++ openpilot/selfdrive/modeld/modeld.py
@@ -232,7 +232,8 @@ class ModelState:
     if self.chestnut and not np.all(np.isfinite(model_output)):
       raise RuntimeError("model output not finite")
     outputs_dict = self.parser.parse_outputs(self.slice_outputs(model_output, self.output_slices))
-    self.npy['prev_feat'][:] = model_output[self.output_slices['hidden_state']]
+    if 'prev_feat' in self.npy:
+      self.npy['prev_feat'][:] = model_output[self.output_slices['hidden_state']]
 
     if SEND_RAW_PRED:
       outputs_dict['raw_pred'] = model_output.copy()
