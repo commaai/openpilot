7ea5ff27ce1e26fb73cb17ad9fa1f2797170581d
elkoled <elkoled@gmail.com>
2026-07-30 02:39:15 -0700

    green

--- openpilot/selfdrive/ui/mici/layouts/home.py
+++ openpilot/selfdrive/ui/mici/layouts/home.py
@@ -139,7 +139,7 @@ class MiciHomeLayout(Widget):
     self._version_text = self._get_version_text()
 
     self._experimental_icon = IconWidget("icons_mici/experimental_mode.png", (48, 48))
-    self._egpu_icon = IconWidget("icons_mici/egpu.png", (50, 37))
+    self._egpu_icon = IconWidget("icons_mici/egpu_green.png", (50, 37))
     self._egpu_icon_gray = IconWidget("icons_mici/egpu_gray.png", (50, 37))
     self._mic_icon = IconWidget("icons_mici/microphone.png", (32, 46))
     self._body_icon = IconWidget("icons_mici/body.png", (54, 37))
