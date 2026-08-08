68e1dfeed1c4189f9d6e62592fb7ad1923b3ca9e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-08-08 09:31:27 -0700

    stream_switch mutability: make ty happy

--- openpilot/selfdrive/ui/onroad/cameraview.py
+++ openpilot/selfdrive/ui/onroad/cameraview.py
@@ -72,7 +72,7 @@ class CameraView(Widget):
     self._name = name
     # Primary stream
     self.client = VisionIpcClient(name, stream_type, conflate=True)
-    self._stream_type = stream_type
+    self._stream_type: VisionStreamType = stream_type
     self.available_streams: list[VisionStreamType] = []
 
     # Target stream for switching
@@ -324,6 +324,7 @@ class CameraView(Widget):
       del self.client
 
     # Switch to target
+    assert self._target_client is not None and self._target_stream_type is not None
     self.client = self._target_client
     self._stream_type = self._target_stream_type
     self._texture_needs_update = True
