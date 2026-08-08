333ae8fc85a9c22c2dcc6e74981212bb429ddbc2
Shane Smiskol <shane@smiskol.com>
2026-08-07 19:26:10 -0700

    toggle

--- openpilot/selfdrive/ui/mici/layouts/settings/toggles.py
+++ openpilot/selfdrive/ui/mici/layouts/settings/toggles.py
@@ -44,6 +44,7 @@ class TogglesLayoutMici(NavScroller):
     self._personality_toggle = BigMultiParamToggle("driving personality", "LongitudinalPersonality", ["aggressive", "standard", "relaxed"])
     self._experimental_btn = BigToggle("experimental mode", initial_state=ui_state.params.get_bool("ExperimentalMode"),
                                        toggle_callback=self._on_experimental_mode)
+    aol_toggle = BigParamControl("always on lateral", "IsAolEnabled")
     is_metric_toggle = BigParamControl("use metric units", "IsMetric")
     ldw_toggle = BigParamControl("lane departure warnings", "IsLdwEnabled")
     always_on_dm_toggle = BigParamControl("always-on driver monitor", "AlwaysOnDM")
