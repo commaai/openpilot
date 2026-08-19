58997b4111a51d59f6dbcc6735ded7cdb6ffd87b
Shane Smiskol <shane@smiskol.com>
2026-08-18 17:23:50 -0700

    fetch big model in installer

--- openpilot/selfdrive/ui/installer/installer.cc
+++ openpilot/selfdrive/ui/installer/installer.cc
@@ -135,7 +135,7 @@ int doInstall() {
 
 int freshClone() {
   LOGD("Doing fresh clone");
-  std::string cmd = util::string_format("git clone --progress %s -b %s --depth=1 --recurse-submodules %s 2>&1",
+  std::string cmd = util::string_format("git clone --progress -c lfs.fetchexclude='' %s -b %s --depth=1 --recurse-submodules %s 2>&1",
                                         GIT_URL.c_str(), migrated_branch.c_str(), TMP_INSTALL_PATH);
   return executeGitCommand(cmd);
 }
@@ -144,6 +144,7 @@ int cachedFetch(const std::string &cache) {
   LOGD("Fetching with cache: %s", cache.c_str());
 
   run(util::string_format("cp -rp %s %s", cache.c_str(), TMP_INSTALL_PATH).c_str());
+  run(util::string_format("cd %s && git config --local lfs.fetchexclude ''", TMP_INSTALL_PATH).c_str());
   run(util::string_format("cd %s && git remote set-url origin %s", TMP_INSTALL_PATH, GIT_URL.c_str()).c_str());
   run(util::string_format("cd %s && git remote set-branches --add origin %s", TMP_INSTALL_PATH, migrated_branch.c_str()).c_str());
 
