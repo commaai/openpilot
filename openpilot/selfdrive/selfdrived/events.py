44f3e2baeaa0bb167fde3e5b47826cd8cbf66141
elkoled <elkoled@gmail.com>
2026-08-31 21:53:19 -0700

    Revert "selfdrived: prioritize big model failure alert"

--- openpilot/selfdrive/selfdrived/events.py
+++ openpilot/selfdrive/selfdrived/events.py
@@ -410,8 +410,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
 
   EventName.bigModelFailed: {
     ET.SOFT_DISABLE: soft_disable_alert("Big Model Failed"),
-    ET.PERMANENT: NormalPermanentAlert("Big Model Failed ", "Restart the car to retry,\nsmall model is still available",
-                                        duration=20., priority=Priority.MID),
+    ET.PERMANENT: NormalPermanentAlert("Big Model Failed ", "Restart the car to retry,\nsmall model is still available", duration=20.),
   },
 
   EventName.lateralManeuver: {
