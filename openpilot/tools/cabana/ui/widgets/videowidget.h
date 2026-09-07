889361501e4c0b06240a1adefcbaf377e9ee7d22
Trey Moen <trey@moen.ai>
2026-09-07 12:48:45 -0700

    cabana: remove duplicate declaration after rebase

--- openpilot/tools/cabana/ui/widgets/videowidget.h
+++ openpilot/tools/cabana/ui/widgets/videowidget.h
@@ -94,7 +94,6 @@ public:
 private:
   void showThumbnail(double seconds);
   void updateSliderThumbnail();  // the thumbnail follows the mouse over the slider
-  void showThumbnail(double seconds);
   std::string formatTime(double sec, bool include_milliseconds = false);
   void timeRangeChanged();
   void createCameraWidget();
