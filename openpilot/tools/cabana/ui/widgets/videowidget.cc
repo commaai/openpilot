abc0535724e7ccc0c73b6bdfd6c5bd981e934442
Trey Moen <trey@moen.ai>
2026-09-07 17:12:30 -0700

    cabana: round the outer video frame including letterboxing

--- openpilot/tools/cabana/ui/widgets/videowidget.cc
+++ openpilot/tools/cabana/ui/widgets/videowidget.cc
@@ -550,7 +550,7 @@ void StreamCameraView::drawScrubThumbnail(ImDrawList *p, double sec) {
   p->AddRectFilled(rect().Min, rect().Max, IM_COL32(0, 0, 0, 255), ImGui::GetStyle().ChildRounding);
   if (const RgbImage *image = thumbnailAt(sec)) {
     const VideoPlacement placement = videoPlacement(rect(), (float)image->width / image->height, settings.crop_video);
-    p->AddImage(big_thumbnail_texture_.ref(), placement.min, placement.max, placement.uv0, placement.uv1, IM_COL32_WHITE);
+    drawVideoFrame(p, big_thumbnail_texture_.ref(), rect(), placement);
     drawTime(p, rect(), sec);
   }
 }
