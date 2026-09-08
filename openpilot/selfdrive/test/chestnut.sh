d3e93ebc9ddf96cea5cff251011dc5a85ce3502c
elkoled <elkoled@gmail.com>
2026-09-08 15:54:30 -0700

    run model replay on chestnut in CI

--- openpilot/selfdrive/test/chestnut.sh
+++ openpilot/selfdrive/test/chestnut.sh
@@ -3,5 +3,5 @@ set -e
 
 TARGET=openpilot/selfdrive/modeld/models/big_driving_tinygrad.pkl.chunkmanifest
 rm -f "$TARGET"
-scons --cache-disable "$TARGET"
+SCONSFLAGS="-j2 --cache-disable" ./openpilot/system/manager/build.py
 test -s "$TARGET"
