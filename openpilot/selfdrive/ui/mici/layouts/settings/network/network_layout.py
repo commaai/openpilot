65cf58e1514676302045c4a1fddef9704a5ec04c
Trey Moen <trey@moen.ai>
2026-09-07 17:11:46 -0700

    ui: disable eSIM management for full prime

--- openpilot/selfdrive/ui/mici/layouts/settings/network/network_layout.py
+++ openpilot/selfdrive/ui/mici/layouts/settings/network/network_layout.py
@@ -103,6 +103,7 @@ class NetworkLayoutMici(NavScroller):
 
     show_cell_settings = ui_state.prime_state.get_type() in (PrimeType.NONE, PrimeType.LITE)
     self._wifi_manager.set_ipv4_forward(show_cell_settings)
+    self._esim_button.set_enabled(show_cell_settings and self._cellular_manager.is_euicc is not False)
     self._roaming_btn.set_visible(show_cell_settings)
     self._apn_btn.set_visible(show_cell_settings)
     self._cellular_metered_btn.set_visible(show_cell_settings)
