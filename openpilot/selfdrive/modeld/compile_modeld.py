960a7dafa5899e224a040b8e822bf3d556c6a949
Bruce Wayne <harald.the.engineer@gmail.com>
2026-09-11 16:44:55 -0700

    Move the driving warp wrapper into compile_warp

--- openpilot/selfdrive/modeld/compile_modeld.py
+++ openpilot/selfdrive/modeld/compile_modeld.py
@@ -11,7 +11,7 @@ from functools import partial
 import numpy as np
 
 from openpilot.selfdrive.modeld.helpers import dump_oob, load_oob
-from openpilot.selfdrive.modeld.compile_warp import NV12Frame, make_frame_prepare, _parse_size
+from openpilot.selfdrive.modeld.compile_warp import NV12Frame, make_warp, _parse_size
 
 def _patch_tinygrad_fetch_fw():
   import hashlib
@@ -93,23 +93,6 @@ def sample_skip(buf, frame_skip):
 
 def sample_desire(buf, frame_skip):
   return buf.reshape(-1, frame_skip, *buf.shape[1:]).max(1).flatten(0, 1).unsqueeze(0)
-
-
-def make_warp(nv12, model_w, model_h, layout="yuv420", border_fill=None):
-  frame_prepare = make_frame_prepare(nv12, model_w, model_h, layout, border_fill)
-
-  def warp(tfm, big_tfm, frame, big_frame):
-    tfm = tfm.to(Device.DEFAULT)
-    big_tfm = big_tfm.to(Device.DEFAULT)
-    frame = frame.to(Device.DEFAULT)
-    big_frame = big_frame.to(Device.DEFAULT)
-    Tensor.realize(tfm, big_tfm, frame, big_frame)
-
-    warped_frame = frame_prepare(frame, tfm).unsqueeze(0)
-    warped_big_frame = frame_prepare(big_frame, big_tfm).unsqueeze(0)
-    return Tensor.cat(warped_frame, warped_big_frame)
-
-  return warp
 
 
 def make_run_policy(model_runner, model_metadata, frame_skip):
