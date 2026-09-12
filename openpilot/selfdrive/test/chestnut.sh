10d171ee7724cfd259aeb1969d2f89f99f538d28
elkoled <elkoled@gmail.com>
2026-09-12 12:35:10 -0700

    Update Chestnut firmware before CI build

--- openpilot/selfdrive/test/chestnut.sh
+++ openpilot/selfdrive/test/chestnut.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+sudo python3 openpilot/system/hardware/chestnut/flash.py
+
 TARGET=openpilot/selfdrive/modeld/models/big_driving_tinygrad.pkl.chunkmanifest
 rm -f "$TARGET"
 SCONSFLAGS="-j4" ./openpilot/system/manager/build.py
