d06711be19b72688c6d8a4f485df98135352355e
stef <19478336+stefpi@users.noreply.github.com>
2026-09-18 02:58:15 -0400

    ui: prime menu (#38860)
    * Show pairing provider and Prime status in device settings
    
    * remove unnecessary
    
    * simplify
    
    * bespoke human code
    
    * clean
    
    * readme
    
    * prime layout
    
    * sepearte qr code stuff
    
    * move icons
    
    * move qr code into its own widget
    
    * clean
    
    * Fix prime menu lint
    
    * Update widget leak test for prime menu
    
    * fix lint
    
    * update to new theme
    
    * change names
    
    * fix font size
    
    * get rid of subtitle
    
    * adjust commacare icon
    
    * drop manage, its cleaner
    
    * edit text
    
    * clean
    
    * for github and apple always return generic
    
    * updates
    
    * go back to original
    
    * noice
    
    * better overwrite
    
    * close on unpairing if in management section
    
    * missing 20 px when icon is there
    
    * clean diff
    
    * fix lint

--- openpilot/selfdrive/ui/mici/layouts/settings/device/device_layout.py
+++ openpilot/selfdrive/ui/mici/layouts/settings/device/device_layout.py
@@ -1,19 +1,16 @@
 import os
 import pyray as rl
-import time
 from collections.abc import Callable
 from typing import Union
 
-from openpilot.common.api import Api
-from openpilot.common.swaglog import cloudlog
 from openpilot.common.basedir import BASEDIR
 from openpilot.common.params import Params
 from openpilot.common.time_helpers import system_time_valid
+from openpilot.selfdrive.ui.mici.layouts.settings.device.prime import PrimeScroller
 from openpilot.system.ui.widgets.scroller import NavRawScrollPanel, NavScroller
 from openpilot.selfdrive.ui.mici.widgets.info import InfoLayoutMici
-from openpilot.selfdrive.ui.mici.widgets.button import BigButton, BigCircleButton, GreyBigButton
+from openpilot.selfdrive.ui.mici.widgets.button import BigButton, BigCircleButton
 from openpilot.selfdrive.ui.mici.widgets.dialog import BigDialog, BigConfirmationDialog
-from openpilot.selfdrive.ui.mici.widgets.qr import QR
 from openpilot.selfdrive.ui.mici.onroad.cabin_camera_dialog import CabinCameraDialog
 from openpilot.selfdrive.ui.mici.layouts.onboarding import TrainingGuide, TermsPage
 from openpilot.system.ui.lib.application import gui_app, MousePos
@@ -99,86 +96,50 @@ class DeviceInfoLayoutMici(InfoLayoutMici):
     super().__init__("device ID", params.get("DongleId") or 'N/A', "serial", params.get("HardwareSerial") or 'N/A', width=380)
 
 
-class PairingDialog(NavScroller):
-  """Pair the device with comma connect."""
-
-  QR_REFRESH_INTERVAL = 300  # 5 minutes in seconds
-
-  def __init__(self):
-    super().__init__()
-    self._params = Params()
-    self._last_pairing_qr_generation = float("-inf")
-    self._qr = QR(self._get_pairing_url())
-    self._scroller._show_scroll_indicator = False
-    self._scroller.add_widgets([
-      self._qr,
-      GreyBigButton("finish setup", "scan to pair device\nwith connect",
-                    gui_app.texture("icons_mici/settings/device/green_settings.png", 64, 64)),
-      GreyBigButton("", "connect lets you review recent driving footage and bookmark events."),
-    ])
-
-  def _get_pairing_url(self) -> str:
-    try:
-      dongle_id = self._params.get("DongleId") or ""
-      token = Api(dongle_id).get_token({'pair': True})
-    except Exception as e:
-      cloudlog.warning(f"Failed to get pairing token: {e}")
-      token = ""
-    return f"https://connect.comma.ai/?pair={token}"
-
-  def _update_layout_rects(self):
-    super()._update_layout_rects()
-    self._qr.set_rect(rl.Rectangle(self._qr.rect.x, self._qr.rect.y, self._rect.height, self._rect.height))
-
-  def _render(self, rect: rl.Rectangle):
-    current_time = time.monotonic()
-    if current_time - self._last_pairing_qr_generation >= self.QR_REFRESH_INTERVAL:
-      self._qr._url = self._get_pairing_url()
-      if self._qr._texture and self._qr._texture.id != 0:
-        rl.unload_texture(self._qr._texture)
-      self._qr._texture = self._qr._generate_qr_code()
-      self._last_pairing_qr_generation = current_time
-    super()._render(rect)
-
-  def _update_state(self):
-    super()._update_state()
-    if ui_state.prime_state.is_paired() and not self.is_dismissing:
-      self.dismiss()
-
-
 class PairBigButton(BigButton):
   def __init__(self):
-    super().__init__("pair", "connect.comma.ai", gui_app.texture("icons_mici/settings/comma_icon.png", 33, 60))
-
-  def _get_label_font_size(self):
-    return 64
+    self._commacare_badge = gui_app.texture("icons_mici/settings/device/commacare.png", 27, 32)
+    self._show_commacare = False
+    self._comma_icon = gui_app.texture("icons_mici/settings/comma_icon.png", 33, 60)
+    self._provider_icons = {provider: gui_app.texture(f"icons_mici/settings/device/paired_{provider}.png", 64, 64)
+                           for provider in ("github", "google", "apple")}
+    super().__init__("pair to connect", "connect.comma.ai", self._comma_icon)
 
   def _update_state(self):
     super()._update_state()
 
+    self._show_commacare = (ui_state.prime_state.is_paired() and ui_state.prime_state.is_prime()
+                            and ui_state.prime_state.has_commacare())
     if ui_state.prime_state.is_paired():
+      self.set_icon(self._provider_icons.get(ui_state.prime_state.get_pairing_provider(), self._comma_icon))
       self.set_text("paired")
       if ui_state.prime_state.is_prime():
-        self.set_value("subscribed")
+        self.set_value("prime" if ui_state.prime_state.is_full_prime() else "prime lite")
       else:
-        self.set_value("upgrade to prime")
+        self.set_value("claim prime trial" if ui_state.prime_state.can_claim_prime_trial() else "upgrade to prime")
     else:
-      self.set_text("pair")
+      self.set_icon(self._comma_icon)
+      self.set_text("pair to connect")
       self.set_value("connect.comma.ai")
+
+  def _draw_subtitle(self, rect: rl.Rectangle):
+    if self._show_commacare:
+      y = rect.y + rect.height - self._commacare_badge.height
+      rl.draw_texture_v(self._commacare_badge, rl.Vector2(rect.x, y), rl.WHITE)
+      offset = self._commacare_badge.width + 14
+      rect = rl.Rectangle(rect.x + offset, rect.y, rect.width - offset, rect.height)
+    super()._draw_subtitle(rect)
 
   def _handle_mouse_release(self, mouse_pos: MousePos):
     super()._handle_mouse_release(mouse_pos)
 
-    # TODO: show ad dialog when clicked if not prime
-    if ui_state.prime_state.is_paired():
-      return
-    dlg: BigDialog | PairingDialog
+    dlg: BigDialog | PrimeScroller
     if not system_time_valid():
       dlg = BigDialog("", tr("Please connect to Wi-Fi to complete initial pairing."))
     elif UNREGISTERED_DONGLE_ID == (ui_state.params.get("DongleId") or UNREGISTERED_DONGLE_ID):
       dlg = BigDialog("", tr("Device must be registered with the comma.ai backend to pair."))
     else:
-      dlg = PairingDialog()
+      dlg = PrimeScroller()
     gui_app.push_widget(dlg)
 
 
