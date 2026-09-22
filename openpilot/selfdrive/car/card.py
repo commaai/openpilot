a682b09788d68bd2c898d02abbaf353138c33d5e
elkoled <elkoled@gmail.com>
2026-09-22 16:58:28 -0700

    card: wait for carControl before signaling controls ready

--- openpilot/selfdrive/car/card.py
+++ openpilot/selfdrive/car/card.py
@@ -245,7 +245,7 @@ class Car:
     self.state_publish(CS, RD)
 
     initialized = (not any(e.name == EventName.selfdriveInitializing for e in self.sm['onroadEvents']) and
-                   self.sm.seen['onroadEvents'])
+                   self.sm.seen['onroadEvents'] and self.sm.seen['carControl'])
     if not self.CP.passive and initialized:
       self.controls_update(CS, self.sm['carControl'])
 
